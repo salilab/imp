e4a5d5667b8abe06d9772c99d72bd28b688262c6
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2007-09-30 04:14:37 +0000

    update
    git-svn-id: https://svn.salilab.org/imp/trunk@2383 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- embed/trunk/examples/read_write_map.cc
+++ embed/trunk/examples/read_write_map.cc
@@ -1,22 +1,29 @@
 #include "DensityMap.h"
+#include "XplorReaderWriter.h"
 #include <iostream>
 #include <fstream>
 using namespace std;
 
 
 int main(int argc,char *argv[]) {
+
   const char* em_filenameI, *em_filenameO;
   em_filenameI = argv[1];
   em_filenameO = argv[2];
   // load the EM density map
   DensityMap scene;
   ifstream mapFile(em_filenameI);
-  scene.ReadXplor(mapFile,1);
+  XplorReaderWriter xrw;
+
+  scene.Read(mapFile,xrw);
+
   mapFile.close();
 		   
   // write the map
   ofstream outFile(em_filenameO);
-  scene.PrintXplorFormat(outFile);
+
+  scene.Write(outFile,xrw);
+
   outFile.close();
 
 		   
