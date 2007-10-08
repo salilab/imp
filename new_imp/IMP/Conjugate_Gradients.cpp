6916c5b553edfcbaaebf6e6c5a8336048a17bfd0
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2007-10-08 23:16:42 +0000

    Clean up code, primarily by replacing gotos with ifs.
    git-svn-id: https://svn.salilab.org/imp/trunk@92 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- new_imp/IMP/Conjugate_Gradients.cpp
+++ new_imp/IMP/Conjugate_Gradients.cpp
@@ -137,25 +137,25 @@ g20: f = get_score(model, model_data, float_indices, x, dx);
   }
 
 /* BEGIN THE MAJOR ITERATION LOOP. NCALLS IS USED TO GUARANTEE THAT
-  AT LEAST TWO POINTS HAVE BEEN TRIED WHEN NMETH=0. FMIN IS THE
+  AT LEAST TWO POINTS HAVE BEEN TRIED. minf IS THE
   CURRENT FUNCTION VALUE. */
 g40: minf = f;
   ncalls = ifun;
 
-/* BEGIN LINEAR SEARCH. ALPHA IS THE STEPLENGTH.
- SET ALPHA TO THE NONRESTART CONJUGATE GRADIENT ALPHA. */
-  alpha = alpha * dg / dg1;
+/* BEGIN LINEAR SEARCH. ALPHA IS THE STEPLENGTH. */
 
-/* IF A RESTART HAS BEEN PERFORMED, SET ALPHA=1.0. */
-  if (nrst == 1) {
-    alpha = 1.0;
-  }
-
-/* IF A GRADIENT DIRECTION IS USED, SET ALPHA=1.0/DSQRT(GSQ),
-  WHICH SCALES THE INITIAL SEARCH VECTOR TO UNITY. */
   if (rsw) {
+    /* IF A GRADIENT DIRECTION IS USED, SET ALPHA=1.0/DSQRT(GSQ),
+       WHICH SCALES THE INITIAL SEARCH VECTOR TO UNITY. */
     alpha = 1.0 / sqrt(gsq);
+  } else if (nrst == 1) {
+    /* IF A RESTART HAS BEEN PERFORMED, SET ALPHA=1.0. */
+    alpha = 1.0;
+  } else {
+    /* SET ALPHA TO THE NONRESTART CONJUGATE GRADIENT ALPHA. */
+    alpha = alpha * dg / dg1;
   }
+
 
 /* THE LINEAR SEARCH FITS A CUBIC TO F AND DAL, THE FUNCTION AND ITS
   DERIVATIVE AT ALPHA, AND TO FP AND DP,THE FUNCTION
@@ -183,19 +183,19 @@ g40: minf = f;
 /* BEGIN THE LINEAR SEARCH ITERATION.
   TEST FOR FAILURE OF THE LINEAR SEARCH. */
 
-g80: if (alpha * step > eps) {
-      goto g90;
-    }
+g80: if (alpha * step <= eps) {
 
-/* TEST IF DIRECTION IS A GRADIENT DIRECTION. */
-  if (!rsw) {
-    goto g20;
+  /* TEST IF DIRECTION IS A GRADIENT DIRECTION. */
+    if (!rsw) {
+      goto g20;
+    } else {
+      nflag = 2;
+      goto end;
+    }
   }
-  nflag = 2;
-  goto end;
 
 /* CALCULATE THE TRIAL POINT. */
-g90: for (i = 0; i < n; i++) {
+  for (i = 0; i < n; i++) {
     x[i] = estimate[i] + alpha * search[i];
   }
 
@@ -204,14 +204,13 @@ g90: for (i = 0; i < n; i++) {
 
 /* TEST IF THE MAXIMUM NUMBER OF FUNCTION CALLS HAVE BEEN USED. */
   ifun++;
-  if (ifun <= max_steps) {
-    goto g110;
+  if (ifun > max_steps) {
+    nflag = 1;
+    goto end;
   }
-  nflag = 1;
-  goto end;
 
 /* COMPUTE THE DERIVATIVE OF F AT ALPHA. */
-g110: dal = 0.0;
+  dal = 0.0;
   for (i = 0; i < n; i++) {
     dal += dx[i] * search[i];
   }
@@ -225,21 +224,19 @@ g110: dal = 0.0;
   }
 
 /* IF NOT, TEST WHETHER THE STEPLENGTH CRITERIA HAVE BEEN MET. */
-  if (f > (minf + 0.0001 * alpha * dg) || fabs(dal / dg) > 0.9) {
-    goto g130;
-  }
+  if (f <= (minf + 0.0001 * alpha * dg) && fabs(dal / dg) <= 0.9) {
 
 /* IF THEY HAVE BEEN MET, TEST IF TWO POINTS HAVE BEEN TRIED
-  IF NMETH=0 AND IF THE TRUE LINE MINIMUM HAS NOT BEEN FOUND. */
-  if (ifun - ncalls <= 1 && fabs(dal / dg) > eps) {
-    goto g130;
+   AND IF THE TRUE LINE MINIMUM HAS NOT BEEN FOUND. */
+    if (ifun - ncalls > 1 || fabs(dal / dg) <= eps) {
+      goto g170;
+    }
   }
-  goto g170;
 
 /* A NEW POINT MUST BE TRIED. USE CUBIC INTERPOLATION TO FIND
   THE TRIAL POINT AT. */
 
-g130: u1 = dp + dal - 3.0 * (fp - f) / (ap - alpha);
+  u1 = dp + dal - 3.0 * (fp - f) / (ap - alpha);
   u2 = u1 * u1 - dp * dal;
   if (u2 < 0.) {
     u2 = 0.;
@@ -248,43 +245,39 @@ g130: u1 = dp + dal - 3.0 * (fp - f) / (ap - alpha);
   at = alpha - (alpha - ap) * (dal + u2 - u1) / (dal - dp + 2. * u2);
 
 /* TEST WHETHER THE LINE MINIMUM HAS BEEN BRACKETED. */
-  if (dal / dp > 0.) {
-    goto g140;
-  }
+  if (dal / dp <= 0.) {
 
 /* THE MINIMUM HAS BEEN BRACKETED. TEST WHETHER THE TRIAL POINT LIES
   SUFFICIENTLY WITHIN THE BRACKETED INTERVAL.
   IF IT DOES NOT, CHOOSE AT AS THE MIDPOINT OF THE INTERVAL. */
-  if (at < (1.01 * std::min(alpha, ap)) || at > (0.99 * std::max(alpha, ap))) {
-    at = (alpha + ap) / 2.0;
-  }
-  goto g150;
+    if (at < (1.01 * std::min(alpha, ap))
+        || at > (0.99 * std::max(alpha, ap))) {
+      at = (alpha + ap) / 2.0;
+    }
 
 /* THE MINIMUM HAS NOT BEEN BRACKETED. TEST IF BOTH POINTS ARE
   GREATER THAN THE MINIMUM AND THE TRIAL POINT IS SUFFICIENTLY
   SMALLER THAN EITHER. */
-g140: if (dal > 0.0 && 0.0 < at && at < (0.99 * std::min(ap, alpha))) {
-    goto g150;
-  }
+  } else if (dal <= 0.0 || 0.0 >= at || at >= (0.99 * std::min(ap, alpha))) {
 
 /* TEST IF BOTH POINTS ARE LESS THAN THE MINIMUM AND THE TRIAL POINT
   IS SUFFICIENTLY LARGE. */
-  if (dal <= 0.0  &&  at > (1.01 * std::max(ap, alpha))) {
-    goto g150;
-  }
+    if (dal > 0.0 || at <= (1.01 * std::max(ap, alpha))) {
 
 /* IF THE TRIAL POINT IS TOO SMALL,DOUBLE THE LARGEST PRIOR POINT. */
-  if (dal <= 0.0) {
-    at = 2.0 * std::max(ap, alpha);
-  }
+      if (dal <= 0.0) {
+        at = 2.0 * std::max(ap, alpha);
+      }
 
 /* IF THE TRIAL POINT IS TOO LARGE, HALVE THE SMALLEST PRIOR POINT. */
-  if (dal > 0.) {
-    at = std::min(ap, alpha) / 2.0;
+      if (dal > 0.) {
+        at = std::min(ap, alpha) / 2.0;
+      }
+    }
   }
 
 /* SET AP=ALPHA, ALPHA=AT,AND CONTINUE SEARCH. */
-g150: ap = alpha;
+  ap = alpha;
   fp = f;
   dp = dal;
   alpha = at;
@@ -325,19 +318,18 @@ g170: gsq = xsq = 0.0;
 /* IF A RESTART IS INDICATED, SAVE THE CURRENT D AND Y
   AS THE BEALE RESTART VECTORS AND SAVE D'Y AND Y'Y
   IN W(NCONS+1) AND W(NCONS+2). */
-  if (nrst != n) {
-    goto g220;
-  }
-  w1 = w2 = 0.;
-  for (i = 0; i < n; i++) {
-    resy[i] = dx[i] - destimate[i];
-    ressearch[i] = search[i];
-    w1 += resy[i] * resy[i];
-    w2 += search[i] * resy[i];
+  if (nrst == n) {
+    w1 = w2 = 0.;
+    for (i = 0; i < n; i++) {
+      resy[i] = dx[i] - destimate[i];
+      ressearch[i] = search[i];
+      w1 += resy[i] * resy[i];
+      w2 += search[i] * resy[i];
+    }
   }
 
 /* CALCULATE  THE RESTART HESSIAN TIMES THE CURRENT GRADIENT. */
-g220: u1 = u2 = 0.0;
+  u1 = u2 = 0.0;
   for (i = 0; i < n; i++) {
     u1 -= ressearch[i] * dx[i] / w1;
     u2 += ressearch[i] * dx[i] * 2.0 / w2 - resy[i] * dx[i] / w1;
@@ -349,60 +341,59 @@ g220: u1 = u2 = 0.0;
 
 /* IF THIS IS A RESTART ITERATION,W(NX+I) CONTAINS THE NEW SEARCH
   VECTOR. */
-  if (nrst == n) {
-    goto g300;
-  }
+  if (nrst != n) {
 
 /* NOT A RESTART ITERATION. CALCULATE THE RESTART HESSIAN
   TIMES THE CURRENT Y. */
-  u1 = u2 = u3 = 0.0;
-  for (i = 0; i < n; i++) {
-    u1 -= (dx[i] - destimate[i]) * ressearch[i] / w1;
-    u2 = u2 - (dx[i] - destimate[i]) * resy[i] / w1
-          + 2.0 * ressearch[i] * (dx[i] - destimate[i]) / w2;
-    u3 += search[i] * (dx[i] - destimate[i]);
-  }
-  step = u4 = 0.;
-  for (i = 0; i < n; i++) {
-    step = (w2 / w1) * (dx[i] - destimate[i])
-           + u1 * resy[i] + u2 * ressearch[i];
-    u4 += step * (dx[i] - destimate[i]);
-    destimate[i] = step;
-  }
+    u1 = u2 = u3 = 0.0;
+    for (i = 0; i < n; i++) {
+      u1 -= (dx[i] - destimate[i]) * ressearch[i] / w1;
+      u2 = u2 - (dx[i] - destimate[i]) * resy[i] / w1
+           + 2.0 * ressearch[i] * (dx[i] - destimate[i]) / w2;
+      u3 += search[i] * (dx[i] - destimate[i]);
+    }
+    step = u4 = 0.;
+    for (i = 0; i < n; i++) {
+      step = (w2 / w1) * (dx[i] - destimate[i])
+             + u1 * resy[i] + u2 * ressearch[i];
+      u4 += step * (dx[i] - destimate[i]);
+      destimate[i] = step;
+    }
 
 /* CALCULATE THE DOUBLY UPDATED HESSIAN TIMES THE CURRENT
   GRADIENT TO OBTAIN THE SEARCH VECTOR. */
-  u1 = u2 = 0.0;
-  for (i = 0; i < n; i++) {
-    u1 -= search[i] * dx[i] / u3;
-    u2 += (1.0 + u4 / u3) * search[i] * dx[i] / u3 - destimate[i] * dx[i] / u3;
-  }
-  for (i = 0; i < n; i++) {
-    estimate[i] = estimate[i] - u1 * destimate[i] - u2 * search[i];
+    u1 = u2 = 0.0;
+    for (i = 0; i < n; i++) {
+      u1 -= search[i] * dx[i] / u3;
+      u2 += (1.0 + u4 / u3) * search[i] * dx[i] / u3
+            - destimate[i] * dx[i] / u3;
+    }
+    for (i = 0; i < n; i++) {
+      estimate[i] = estimate[i] - u1 * destimate[i] - u2 * search[i];
+    }
   }
 
 /* CALCULATE THE DERIVATIVE ALONG THE NEW SEARCH VECTOR. */
-g300: dg1 = 0.0;
+  dg1 = 0.0;
   for (i = 0; i < n; i++) {
     search[i] = estimate[i];
     dg1 += search[i] * dx[i];
   }
 
 /* IF THE NEW DIRECTION IS NOT A DESCENT DIRECTION,STOP. */
-  if (dg1 > 0.0) {
-    goto g320;
-  }
+  if (dg1 <= 0.0) {
 
 /* UPDATE NRST TO ASSURE AT LEAST ONE RESTART EVERY N ITERATIONS. */
-  if (nrst == n) {
-    nrst = 0;
+    if (nrst == n) {
+      nrst = 0;
+    }
+    nrst++;
+    rsw = false;
+    goto g40;
   }
-  nrst++;
-  rsw = false;
-  goto g40;
 
 /* ROUNDOFF HAS PRODUCED A BAD DIRECTION. */
-g320: nflag = 3;
+  nflag = 3;
 
 end:
   // If the 'best current estimate' is better than the current state, return
