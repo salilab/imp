1999713bb4f2ae321a81d851ed2f94b90908bed9
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2007-10-10 05:26:34 +0000

    Rework build system so that Modeller is not necessarily required; for the IMP kernel, allow building without Modeller at all (just run only the non-Modeller test cases in this instance).
    git-svn-id: https://svn.salilab.org/imp/trunk@98 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ new_imp/IMP/tests/run-imp-tests.py
@@ -0,0 +1,24 @@
+import unittest, sys, os, re
+
+sys.path.append('python_libs')
+
+def regressionTest():
+    """Run all tests in files called test_*.py in current directory and
+       subdirectories, but exclude Modeller tests"""
+    path = os.path.abspath(os.path.dirname(sys.argv[0]))
+    modobjs = []
+    for subdir in ['.'] + [x for x in os.listdir(path) if os.path.isdir(x)]:
+        if subdir != "modeller":
+            files = os.listdir(os.path.join(path, subdir))
+            test = re.compile("^test_.*\.py$", re.IGNORECASE)
+            files = filter(test.search, files)
+            modnames = [os.path.splitext(f)[0] for f in files]
+            sys.path.insert(0, subdir)
+            modobjs.extend([__import__(m) for m in modnames])
+            sys.path.pop(0)
+
+    tests = [unittest.defaultTestLoader.loadTestsFromModule(o) for o in modobjs]
+    return unittest.TestSuite(tests)
+
+if __name__ == "__main__":
+    unittest.main(defaultTest="regressionTest")
