9ff3ece7c3aeb23fda839bd2f0b1203a044a31e6
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2007-10-12 20:54:31 +0000

    Use the IMP optimizer rather than the Modeller one for the torus testcase.
    git-svn-id: https://svn.salilab.org/imp/trunk@132 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ new_imp/IMP/tests/torus/test_torus.py
@@ -0,0 +1,53 @@
+import unittest
+import random
+import os
+import IMP
+import IMP.test
+import IMP.utils
+
+class test_torus(IMP.test.IMPTestCase):
+    """Class to test torus restraints"""
+
+    def setUp(self):
+        """Build test model and optimizer"""
+        self.imp_model = IMP.Model()
+        self.particles = []
+        self.restraint_sets = []
+        self.rsrs = []
+
+        for p in range(12):
+            self.particles.append(IMP.utils.XYZParticle(self.imp_model,
+                                                        0., 0., 0.))
+        self.opt = IMP.ConjugateGradients()
+
+
+    def test_torus(self):
+        """All particles should be inside the specified torus"""
+        rs = IMP.RestraintSet("torus")
+        self.restraint_sets.append(rs)
+        self.imp_model.add_restraint_set(rs)
+
+        p_iter = IMP.ParticleIterator()
+        p_iter.reset(self.imp_model)
+        score_func_params = IMP.BasicScoreFuncParams("harmonic_upper_bound",
+                                                     0.0, 0.1)
+        while p_iter.next():
+            r = IMP.TorusRestraint(self.imp_model, p_iter.get(), 50, 10,
+                                   score_func_params)
+            self.rsrs.append(r)
+            rs.add_restraint(r)
+
+        # Randomize particle coordinates
+        for p in self.particles:
+            p.set_x(random.uniform(-50., 50.0))
+            p.set_y(random.uniform(-50., 50.0))
+            p.set_z(random.uniform(-50., 50.0))
+
+        self.opt.optimize(self.imp_model, 50, 1e-4)
+        for p in self.particles:
+            coord = (p.x(), p.y(), p.z())
+            self.assert_(self.TestInTorus(coord, 50, 10.001),
+                         "in torus condition")
+
+if __name__ == '__main__':
+    unittest.main()
