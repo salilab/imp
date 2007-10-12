64d929b3fe4ecb588a9d89d74e7e8d215daa69d4
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2007-10-12 22:30:48 +0000

    Use the IMP optimizer rather than the Modeller one for the connectivity testcase.
    git-svn-id: https://svn.salilab.org/imp/trunk@133 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- new_imp/IMP/tests/connectivity/test_connectivity.py
+++ new_imp/IMP/tests/connectivity/test_connectivity.py
@@ -1,32 +1,23 @@
-import modeller, unittest
-import modeller.optimizers
+import unittest
+import random
 import os
-import IMP.modeller_intf
-import IMP.test, IMP
+import IMP
+import IMP.test
+import IMP.utils
 
-# Class to test connectivity restraints
 class test_connectivity(IMP.test.IMPTestCase):
-    """test connectivity restraints"""
+    """Class to test connectivity restraints"""
 
     def setUp(self):
-        """set up Modeller with connectivity restraints """
-        modeller.log.level(0,0,0,0,0)
-
-        self.env = modeller.environ()
-        self.env.io.atom_files_directory = '../data/'
-        self.env.edat.dynamic_sphere = False
-        self.env.libs.topology.read(file='$(LIB)/top_heav.lib')
-        self.env.libs.parameters.read(file='$(LIB)/par.lib')
-
+        """Build test model and optimizer"""
         self.imp_model = IMP.Model()
         self.particles = []
         self.restraint_sets = []
         self.rsrs = []
 
-        self.t = self.env.edat.energy_terms
-        self.t.append(IMP.modeller_intf.IMP_Restraints(self.imp_model, self.particles))
-
-        self.modeller_model = IMP.modeller_intf.Create_Particles(12, self.env, self.imp_model, self.particles)
+        for p in range(12):
+            self.particles.append(IMP.utils.XYZParticle(self.imp_model,
+                                                        0., 0., 0.))
         p1 = self.particles[0]
         p1.add_float("radius", 1.0, False)
         p1.add_int("protein", 1)
@@ -87,16 +78,21 @@ class test_connectivity(IMP.test.IMPTestCase):
         p1.add_int("protein", 3)
         p1.add_int("id", 12)
 
-        self.atmsel = modeller.selection(self.modeller_model)
+        self.opt = IMP.ConjugateGradients()
 
-        self.opt = modeller.optimizers.conjugate_gradients()
+    def _randomize_model(self, particles):
+        """Randomize model particle positions"""
+        for p in particles:
+            p.set_x(random.uniform(-50., 50.0))
+            p.set_y(random.uniform(-50., 50.0))
+            p.set_z(random.uniform(-50., 50.0))
 
-
-    # connectivity
     def test_connectivity(self):
-        """ all particles in a single protein should be connected, and all proteins
-        should be connected, either directly or indirectly through other proteins """
-        self.atmsel.randomize_xyz(deviation=100.0)
+        """Test connectivity restraint.
+           All particles in a single protein should be connected, and all
+           proteins should be connected, either directly or indirectly
+           through other proteins."""
+        self._randomize_model(self.particles)
 
         rs = IMP.RestraintSet("connect")
         self.restraint_sets.append(rs)
@@ -107,59 +103,80 @@ class test_connectivity(IMP.test.IMPTestCase):
         particle_indexes = IMP.vectori()
         rsrs = []
 
-        score_func_params_ub = IMP.BasicScoreFuncParams("harmonic_upper_bound", 0.0, 0.1)
+        score_func_params_ub = IMP.BasicScoreFuncParams("harmonic_upper_bound",
+                                                        0.0, 0.1)
 
         # set up exclusion volumes
-        IMP.modeller_intf.Set_Up_Exclusion_Volumes(self.imp_model, self.particles, "radius", rsrs)
+        IMP.utils.Set_Up_Exclusion_Volumes(self.imp_model, self.particles,
+                                           "radius", rsrs)
 
         # connect 3 proteins together
         particle_indexes.clear()
         for i in range(12):
             particle_indexes.push_back(i)
-        rsrs.append(IMP.ConnectivityRestraint(self.imp_model, particle_indexes, "protein", "radius", score_func_params_ub))
+        rsrs.append(IMP.ConnectivityRestraint(self.imp_model, particle_indexes,
+                                              "protein", "radius",
+                                              score_func_params_ub))
 
         # connect particles in protein1 together
         particle_indexes.clear()
         for i in range(3):
             particle_indexes.push_back(i)
-        rsrs.append(IMP.ConnectivityRestraint(self.imp_model, particle_indexes, "id", "radius", score_func_params_ub))
+        rsrs.append(IMP.ConnectivityRestraint(self.imp_model, particle_indexes,
+                                              "id", "radius",
+                                              score_func_params_ub))
 
         # connect particles in protein2 together
         particle_indexes.clear()
         for i in range(3, 7):
             particle_indexes.push_back(i)
-        rsrs.append(IMP.ConnectivityRestraint(self.imp_model, particle_indexes, "id", "radius", score_func_params_ub))
+        rsrs.append(IMP.ConnectivityRestraint(self.imp_model, particle_indexes,
+                                              "id", "radius",
+                                              score_func_params_ub))
 
         # connect particles in protein3 together
         particle_indexes.clear()
         for i in range(7, 12):
             particle_indexes.push_back(i)
-        rsrs.append(IMP.ConnectivityRestraint(self.imp_model, particle_indexes, "id", "radius", score_func_params_ub))
+        rsrs.append(IMP.ConnectivityRestraint(self.imp_model, particle_indexes,
+                                              "id", "radius",
+                                              score_func_params_ub))
 
         # add restraints
         for i in range(len(rsrs)):
             rs.add_restraint(rsrs[i])
 
-        self.atmsel.randomize_xyz(deviation=100.0)
-        new_mdl = self.opt.optimize (self.atmsel, max_iterations=55, actions=None)
-        self.modeller_model.write (file='out_connectivity.pdb', model_format='PDB')
-
-        coords = self.LoadCoordinates('out_connectivity.pdb')
+        self._randomize_model(self.particles)
+        self.opt.optimize(self.imp_model, 55, 1e-4)
 
         # min distances
-        for i in range(len(coords)):
-            for j in range(i+1,len(coords)):
-                self.assert_(self.TestMinDistance(coords[i], coords[j], self.particles[i].get_float("radius") + self.particles[j].get_float("radius") - 0.05), "min distance for any pair condition")
+        for i in range(len(self.particles)):
+            p = self.particles[i]
+            icoord = (p.x(), p.y(), p.z())
+            irad = p.get_float("radius")
+            for j in range(i+1,len(self.particles)):
+                p = self.particles[j]
+                jcoord = (p.x(), p.y(), p.z())
+                jrad = p.get_float("radius")
+                self.assert_(self.TestMinDistance(icoord, jcoord,
+                                                  irad + jrad - 0.05),
+                             "min distance for any pair condition")
 
         # max distances
         d12 = 10000000
         d13 = 10000000
         d23 = 10000000
-        for i in range(len(coords)):
-            for j in range(i+1,len(coords)):
-                t1 = self.particles[i].get_int("protein")
-                t2 = self.particles[j].get_int("protein")
-                d = self.Distance(coords[i], coords[j]) - self.particles[i].get_float("radius") - self.particles[j].get_float("radius")
+        for i in range(len(self.particles)):
+            p = self.particles[i]
+            icoord = (p.x(), p.y(), p.z())
+            irad = p.get_float("radius")
+            t1 = p.get_int("protein")
+            for j in range(i+1,len(self.particles)):
+                p = self.particles[j]
+                jcoord = (p.x(), p.y(), p.z())
+                jrad = p.get_float("radius")
+                t2 = p.get_int("protein")
+                d = self.Distance(icoord, jcoord) - irad - jrad
                 if t1 == 1 and t2 == 2:
                     if d < d12:
                         d12 = d
@@ -179,7 +196,6 @@ class test_connectivity(IMP.test.IMPTestCase):
         if d23 < max_dist:
             sum = sum + 1
         self.assert_(sum >= 2, "min spanning tree for three particle types")
-        os.unlink('out_connectivity.pdb')
 
 if __name__ == '__main__':
     unittest.main()
