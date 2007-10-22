29745fa97968cb8fb962239da3f490e06d86ff7a
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2007-10-22 23:51:00 +0000

    Fix 'install' target (do not make it dependent on impEM) and update doxygen configuration for new source code directory location.
    git-svn-id: https://svn.salilab.org/imp/trunk@170 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- new_imp/src/mystdexcept.h
+++ new_imp/src/mystdexcept.h
@@ -1,17 +1,17 @@
 /**
- *  \file mystdexcept.h   \brief Simple wrapper for the <stdexcept> header.
+ *  \file mystdexcept.h   \brief Simple wrapper for the stdexcept header.
  *
  *  This header is to work around a g++4 bug (stdexcept does not declare its
  *  symbol visibility, preventing std exceptions from being thrown outside
  *  of the shared library when compiling with -fvisibility=hidden). Include
- *  this file rather than <stdexcept> in any C++ code.
+ *  this file rather than stdexcept in any C++ code.
  *
  *  References:
  *  - http://gcc.gnu.org/bugzilla/show_bug.cgi?id=26217
  *  - http://www.dribin.org/dave/blog/archives/2006/02/10/gcc_exception_bug_2/
  *
- *  Do NOT include <stdexcept> in any of your own headers, because even one
- *  copy of <stdexcept> with the 'default' symbol visibility in your program
+ *  Do NOT include stdexcept in any of your own headers, because even one
+ *  copy of stdexcept with the 'default' symbol visibility in your program
  *  will force default symbol visibility and break throwing of the exceptions
  *  outside of the shared library.
  *
