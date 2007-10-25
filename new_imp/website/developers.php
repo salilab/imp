39c6977ea0a3595726aaac0f2e5419e962a8d08f
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2007-10-25 00:26:01 +0000

    Split into subpages, and autogenerate from PHP sources.
    git-svn-id: https://svn.salilab.org/imp/trunk@179 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ new_imp/website/developers.php
@@ -0,0 +1,15 @@
+<?php
+  include "functions.inc";
+  write_header("developers");
+?>
+
+<h2>Developers</h2>
+
+<p>These pages are currently only for Sali lab members. Please
+<a href="contact.html">contact us</a> if you have questions about IMP.</p>
+
+<p class="login"><a href="https://salilab.org/internal/wiki/IMP">LOGIN</a></p>
+
+<?php
+  write_footer();
+?>
