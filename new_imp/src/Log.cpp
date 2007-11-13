191f2d0847c8bdb6d8021f6ca43fffa995aac518
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2007-11-13 09:07:57 +0000

    Implement the Log singleton class using a private constructor and a static method, to work more reliably across dynamic library boundaries.
    git-svn-id: https://svn.salilab.org/imp/trunk@209 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ new_imp/src/Log.cpp
@@ -0,0 +1,15 @@
+/**
+ *  \file Log.cpp   \brief Logging and error reporting support.
+ *
+ *  Copyright 2007 Sali Lab. All rights reserved.
+ *
+ */
+
+#include "IMP/log.h"
+
+namespace IMP {
+
+/* Initialize singleton pointer to NULL */
+Log* Log::logpt_ = NULL;
+
+} // namespace IMP
