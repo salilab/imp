7aacbbd617185317c1993e2e8f61fa3bc3710862
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2007-11-21 21:06:18 +0000

    Put each static on a separate line, to work around an MSVC bug.
    git-svn-id: https://svn.salilab.org/imp/trunk@228 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- new_imp/include/IMP/decorators/AtomDecorator.h
+++ new_imp/include/IMP/decorators/AtomDecorator.h
@@ -50,39 +50,156 @@ protected:
   };
 
   //! The types of PDB atoms supported
-  static AtomType AT_UNKNOWN, AT_N, AT_CA, AT_C, AT_O,
-    AT_H, AT_1H, AT_2H, AT_3H,
-    AT_HA, AT_1HA, AT_2HA,
-    AT_CB, AT_HB, AT_1HB, AT_2HB, AT_3HB,
-    AT_OXT, AT_CH3,
-    AT_CG, AT_CG1, AT_CG2, AT_HG, AT_1HG, AT_2HG, //AT_HG1,
-    AT_1HG1, AT_2HG1, AT_3HG1, AT_1HG2,
-    AT_2HG2, AT_3HG2, AT_OG, AT_OG1, AT_SG,
-    // AT_HD1, AT_HD2,
-    AT_CD, AT_CD1, AT_CD2,  AT_HD, AT_1HD, AT_2HD, AT_3HD, AT_1HD1,
-    AT_2HD1, AT_3HD1, AT_1HD2, AT_2HD2, AT_3HD2, AT_SD,
-    AT_OD1, AT_OD2, AT_ND1, AT_ND2,
-    AT_CE, AT_CE1, AT_CE2, AT_CE3, AT_HE, AT_1HE, AT_2HE,
-    AT_3HE, //AT_HE1, AT_HE2, AT_HE3,
-    AT_1HE2, AT_2HE2,
-    AT_OE1, AT_OE2, AT_NE, AT_NE1, AT_NE2,
-    AT_CZ, AT_CZ2, AT_CZ3, AT_NZ, AT_HZ, AT_1HZ, AT_2HZ,
-    AT_3HZ, // AT_HZ2, AT_HZ3,
-    AT_CH2, AT_NH1, AT_NH2, AT_OH, AT_HH, AT_1HH1,
-    AT_2HH1, AT_HH2, AT_1HH2, AT_2HH2,
-    AT_1HH3, AT_2HH3, AT_3HH3,
-    AT_P, AT_OP1, AT_OP2,
-    AT_O5p, AT_C5p, AT_H5p, AT_H5pp,
-    AT_C4p, AT_H4p, AT_O4p, AT_C1p, AT_H1p,
-    AT_C3p, AT_H3p, AT_O3p, AT_C2p, AT_H2p,
-    AT_H2pp, AT_O2p, AT_HO2p,
-    AT_N9, AT_C8, AT_H8,
-    AT_N7, AT_C5, AT_C4, AT_N3,
-    AT_C2, AT_H2, AT_N1, AT_C6, AT_N6,
-    AT_H61, AT_H62,
-    AT_O6, AT_H1, AT_N2, AT_H21, AT_H22,
-    AT_H6, AT_H5, AT_O2, AT_N4, AT_H41, AT_H42,
-    AT_H3, AT_O4, AT_C7, AT_H71, AT_H72, AT_H73;
+  /** \note each static must be on a separate line because of MSVC bug C2487:
+            see http://support.microsoft.com/kb/127900/
+   */
+  static AtomType AT_UNKNOWN;
+  static AtomType AT_N;
+  static AtomType AT_CA;
+  static AtomType AT_C;
+  static AtomType AT_O;
+  static AtomType AT_H;
+  static AtomType AT_1H;
+  static AtomType AT_2H;
+  static AtomType AT_3H;
+  static AtomType AT_HA;
+  static AtomType AT_1HA;
+  static AtomType AT_2HA;
+  static AtomType AT_CB;
+  static AtomType AT_HB;
+  static AtomType AT_1HB;
+  static AtomType AT_2HB;
+  static AtomType AT_3HB;
+  static AtomType AT_OXT;
+  static AtomType AT_CH3;
+  static AtomType AT_CG;
+  static AtomType AT_CG1;
+  static AtomType AT_CG2;
+  static AtomType AT_HG;
+  static AtomType AT_1HG;
+  static AtomType AT_2HG;
+  // static AtomType AT_HG1;
+  static AtomType AT_1HG1;
+  static AtomType AT_2HG1;
+  static AtomType AT_3HG1;
+  static AtomType AT_1HG2;
+  static AtomType AT_2HG2;
+  static AtomType AT_3HG2;
+  static AtomType AT_OG;
+  static AtomType AT_OG1;
+  static AtomType AT_SG;
+  // static AtomType AT_HD1;
+  // static AtomType AT_HD2;
+  static AtomType AT_CD;
+  static AtomType AT_CD1;
+  static AtomType AT_CD2;
+  static AtomType AT_HD;
+  static AtomType AT_1HD;
+  static AtomType AT_2HD;
+  static AtomType AT_3HD;
+  static AtomType AT_1HD1;
+  static AtomType AT_2HD1;
+  static AtomType AT_3HD1;
+  static AtomType AT_1HD2;
+  static AtomType AT_2HD2;
+  static AtomType AT_3HD2;
+  static AtomType AT_SD;
+  static AtomType AT_OD1;
+  static AtomType AT_OD2;
+  static AtomType AT_ND1;
+  static AtomType AT_ND2;
+  static AtomType AT_CE;
+  static AtomType AT_CE1;
+  static AtomType AT_CE2;
+  static AtomType AT_CE3;
+  static AtomType AT_HE;
+  static AtomType AT_1HE;
+  static AtomType AT_2HE;
+  static AtomType AT_3HE;
+  // static AtomType AT_HE1;
+  // static AtomType AT_HE2;
+  // static AtomType AT_HE3;
+  static AtomType AT_1HE2;
+  static AtomType AT_2HE2;
+  static AtomType AT_OE1;
+  static AtomType AT_OE2;
+  static AtomType AT_NE;
+  static AtomType AT_NE1;
+  static AtomType AT_NE2;
+  static AtomType AT_CZ;
+  static AtomType AT_CZ2;
+  static AtomType AT_CZ3;
+  static AtomType AT_NZ;
+  static AtomType AT_HZ;
+  static AtomType AT_1HZ;
+  static AtomType AT_2HZ;
+  static AtomType AT_3HZ;
+  // static AtomType AT_HZ2;
+  // static AtomType AT_HZ3;
+  static AtomType AT_CH2;
+  static AtomType AT_NH1;
+  static AtomType AT_NH2;
+  static AtomType AT_OH;
+  static AtomType AT_HH;
+  static AtomType AT_1HH1;
+  static AtomType AT_2HH1;
+  static AtomType AT_HH2;
+  static AtomType AT_1HH2;
+  static AtomType AT_2HH2;
+  static AtomType AT_1HH3;
+  static AtomType AT_2HH3;
+  static AtomType AT_3HH3;
+  static AtomType AT_P;
+  static AtomType AT_OP1;
+  static AtomType AT_OP2;
+  static AtomType AT_O5p;
+  static AtomType AT_C5p;
+  static AtomType AT_H5p;
+  static AtomType AT_H5pp;
+  static AtomType AT_C4p;
+  static AtomType AT_H4p;
+  static AtomType AT_O4p;
+  static AtomType AT_C1p;
+  static AtomType AT_H1p;
+  static AtomType AT_C3p;
+  static AtomType AT_H3p;
+  static AtomType AT_O3p;
+  static AtomType AT_C2p;
+  static AtomType AT_H2p;
+  static AtomType AT_H2pp;
+  static AtomType AT_O2p;
+  static AtomType AT_HO2p;
+  static AtomType AT_N9;
+  static AtomType AT_C8;
+  static AtomType AT_H8;
+  static AtomType AT_N7;
+  static AtomType AT_C5;
+  static AtomType AT_C4;
+  static AtomType AT_N3;
+  static AtomType AT_C2;
+  static AtomType AT_H2;
+  static AtomType AT_N1;
+  static AtomType AT_C6;
+  static AtomType AT_N6;
+  static AtomType AT_H61;
+  static AtomType AT_H62;
+  static AtomType AT_O6;
+  static AtomType AT_H1;
+  static AtomType AT_N2;
+  static AtomType AT_H21;
+  static AtomType AT_H22;
+  static AtomType AT_H6;
+  static AtomType AT_H5;
+  static AtomType AT_O2;
+  static AtomType AT_N4;
+  static AtomType AT_H41;
+  static AtomType AT_H42;
+  static AtomType AT_H3;
+  static AtomType AT_O4;
+  static AtomType AT_C7;
+  static AtomType AT_H71;
+  static AtomType AT_H72;
+  static AtomType AT_H73;
 
   AtomType get_type() const {
     return AtomType(get_particle()->get_value(type_key_));
