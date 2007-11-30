b5c79c32da0e05fd77e50ff55006c78cafaaaa9b
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2007-11-30 23:46:00 +0000

    Add script to checkout impEM for nightly builds.
    git-svn-id: https://svn.salilab.org/imp/trunk@2439 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ embed/trunk/tools/auto-build.sh
@@ -0,0 +1,40 @@
+#!/bin/sh
+
+# Script to get SVN HEAD and make a .tar.gz on a shared disk, for use by
+# autobuild scripts on our build machines.
+#
+# Should be run on the SVN server (or some other system that can access SVN
+# readonly without a password) from a crontab, e.g.
+#
+# 10 1 * * * /cowbell1/home/ben/impEM/tools/auto-build.sh
+#
+# In this case, the build user is in the apache group, and so has readonly
+# access to the repositories.
+
+VER=SVN
+IMPSVNDIR=file:///cowbell1/svn/impEM/trunk/
+
+TMPDIR=/var/tmp/modeller-build-$$
+MODINSTALL=/diva1/home/modeller/.${VER}-new
+IMPSRCTGZ=${MODINSTALL}/build/sources/impEM.tar.gz
+
+rm -rf ${TMPDIR}
+mkdir ${TMPDIR}
+cd ${TMPDIR}
+
+# Get top-most revision number (must be a nicer way of doing this?)
+rev=$(svn log -q --limit 1 ${IMPSVNDIR} |grep '^r' | cut -f 1 -d' ')
+
+# Get impEM code from SVN
+svn export -q -${rev} ${IMPSVNDIR} impEM
+
+# Write out a version file
+verfile="${MODINSTALL}/build/impEM-version"
+echo "${rev}" > $verfile
+
+# Write out a tarball:
+tar -czf ${IMPSRCTGZ} impEM
+
+# Cleanup
+cd /
+rm -rf ${TMPDIR}
