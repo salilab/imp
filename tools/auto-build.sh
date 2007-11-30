cce687b5a6d450393aeee6c90634b8e0b60af637
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2007-11-30 19:40:12 +0000

    When checking out, also get the revision number, and write it to imp-version.
    git-svn-id: https://svn.salilab.org/imp/trunk@242 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- tools/auto-build.sh
+++ tools/auto-build.sh
@@ -18,16 +18,19 @@ TMPDIR=/var/tmp/modeller-build-$$
 MODINSTALL=/diva1/home/modeller/.${VER}-new
 IMPSRCTGZ=${MODINSTALL}/build/sources/imp.tar.gz
 
-# Make build directory
-mkdir -m 0700 -p ${MODINSTALL}/build/sources
-mkdir -m 0755 -p ${MODINSTALL}/build/logs
-
 rm -rf ${TMPDIR}
 mkdir ${TMPDIR}
 cd ${TMPDIR}
 
+# Get top-most revision number (must be a nicer way of doing this?)
+rev=$(svn log -q --limit 1 ${IMPSVNDIR} |grep '^r' | cut -f 1 -d' ')
+
 # Get IMP code from SVN
-svn export -q ${IMPSVNDIR} imp
+svn export -q -${rev} ${IMPSVNDIR} imp
+
+# Write out a version file
+verfile="${MODINSTALL}/build/imp-version"
+echo "${rev}" > $verfile
 
 # Write out a tarball:
 tar -czf ${IMPSRCTGZ} imp
