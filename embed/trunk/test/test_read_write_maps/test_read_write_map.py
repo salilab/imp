89d2346e24c83a12ee7a78b454752163433d339d
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2007-12-03 02:35:18 +0000

    Add tests to make sure the EM and MRC file data is correct.
    git-svn-id: https://svn.salilab.org/imp/trunk@2450 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- embed/trunk/test/test_read_write_maps/test_read_write_map.py
+++ embed/trunk/test/test_read_write_maps/test_read_write_map.py
@@ -64,6 +64,9 @@ class test_write_mrc(unittest.TestCase):
 
 	
 class test_read_write_maps(unittest.TestCase):
+        def assertInTolerance(self, val, expected, tol):
+            diff = abs(val - expected)
+            self.assert_(diff < tol)
 
 	def test_em_read_write(self):
 	    """test em format read/write """
@@ -82,6 +85,7 @@ class test_read_write_maps(unittest.TestCase):
             self.assertEqual(header.voltage, 0.)
             self.assertEqual(header.Cs, 0.)
             self.assertEqual(header.Objectpixelsize, 1.)
+            self.assertInTolerance(scene.calcRMS(), 404.4, 0.1)
 	    scene.Write(out_filename,xrw);
             os.unlink(out_filename)
 
@@ -99,6 +103,7 @@ class test_read_write_maps(unittest.TestCase):
 		self.assertEqual(68,scene.get_header().nx)
 		self.assertEqual(65,scene.get_header().ny)
 		self.assertEqual(59,scene.get_header().nz)
+                self.assertInTolerance(scene.calcRMS(), 511.3, 0.1)
 		scene.Write(out_filename,mrc_rw)
                 os.unlink(out_filename)
 	
