42c4dc3fc5a7369f261d6bf2922b3ed02b0eeb9f
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2007-12-17 07:21:57 +0000

    Patch from Daniel, with cleanup by me: fix build system failure by moving initialization code to the correct place.
    git-svn-id: https://svn.salilab.org/imp/trunk@276 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- new_imp/src/base_types.cpp
+++ new_imp/src/base_types.cpp
@@ -11,10 +11,21 @@ namespace IMP
 {
 
 namespace internal {
+
+struct InitCheck {
+  InitCheck()
+  {
+    std::cout << "Initializing attribute table." << std::endl;
+    key_initialization_heuristic = IMP_KEY_INITIALIZATION_HEURISTIC;
+  }
+};
+
 unsigned int next_attribute_table_index_ = 0;
 std::vector<KeyData> attribute_key_data;
 double key_initialization_heuristic=0;
-}
+InitCheck init_check_;
+
+} // namespace internal
 
 IMP_DEFINE_KEY_TYPE(IntKey, Int);
 IMP_DEFINE_KEY_TYPE(FloatKey, Float);
