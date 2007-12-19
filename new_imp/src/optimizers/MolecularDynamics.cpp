6d72af2b52aca06403438da141208f2e45a8a679
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2007-12-19 07:13:19 +0000

    Add basic molecular dynamics loop, with a unit test.
    git-svn-id: https://svn.salilab.org/imp/trunk@286 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- new_imp/src/optimizers/MolecularDynamics.cpp
+++ new_imp/src/optimizers/MolecularDynamics.cpp
@@ -14,7 +14,7 @@ namespace IMP
 {
 
 //! Constructor
-MolecularDynamics::MolecularDynamics()
+MolecularDynamics::MolecularDynamics() : time_step_(4.0), temperature_(298.0)
 {
 }
 
@@ -76,22 +76,71 @@ void MolecularDynamics::setup_particles(Model& model)
 //! Perform a single dynamics step.
 void MolecularDynamics::step()
 {
-  IMP_failure("TODO: implement MD step", ErrorException());
+  // Assuming score is in kcal/mol, its derivatives in kcal/mol/angstrom,
+  // and mass is in kg/mol, conversion factor necessary to get accelerations
+  // in angstrom/fs/fs
+  static const Float unit_conversion = 4.1868e-7;
+
+  // Mass of Carbon-12, in kg/mol
+  static const Float c12mass = 0.012011;
+
+  // Multiplier to convert raw derivatives to acceleration
+  static const Float deriv_to_acceleration = -unit_conversion / c12mass;
+
+  for (std::vector<Particle *>::iterator iter = particles_.begin();
+       iter != particles_.end(); ++iter) {
+    Particle *p = *iter;
+    Float x = p->get_value(xkey_);
+    Float y = p->get_value(ykey_);
+    Float z = p->get_value(zkey_);
+    Float dvx = p->get_derivative(xkey_);
+    Float dvy = p->get_derivative(ykey_);
+    Float dvz = p->get_derivative(zkey_);
+
+    // calculate velocity at t+(delta t/2) from that at t-(delta t/2)
+    Float vx = p->get_value(vxkey_);
+    Float vy = p->get_value(vykey_);
+    Float vz = p->get_value(vzkey_);
+    vx += dvx * deriv_to_acceleration * time_step_;
+    vy += dvy * deriv_to_acceleration * time_step_;
+    vz += dvz * deriv_to_acceleration * time_step_;
+    p->set_value(vxkey_, vx);
+    p->set_value(vykey_, vy);
+    p->set_value(vzkey_, vz);
+
+    // get atomic shift
+    Float dx = vx * time_step_;
+    Float dy = vy * time_step_;
+    Float dz = vz * time_step_;
+
+    // calculate position at t+(delta t) from that at t
+    x += dx;
+    y += dy;
+    z += dz;
+    p->set_value(xkey_, x);
+    p->set_value(ykey_, y);
+    p->set_value(zkey_, z);
+  }
 }
 
 
 //! Optimize the model.
-/**     \param[in] max_steps   Maximum number of iterations before aborting.
-        \return score of the final state of the model.
+/** \param[in] max_steps   Maximum number of iterations before aborting.
+    \return score of the final state of the model.
  */
-Float MolecularDynamics::optimize(unsigned  int max_steps)
+Float MolecularDynamics::optimize(unsigned int max_steps)
 {
-  setup_particles(*get_model());
+  Model *model = get_model();
+  setup_particles(*model);
+
+  // get initial system score
+  Float score = model->evaluate(true);
 
   for (unsigned int i = 0; i < max_steps; ++i) {
     step();
+    score = model->evaluate(true);
   }
-  return 0.;
+  return score;
 }
 
 } // namespace IMP
--- new_imp/test/md_optimizer/test_md_optimizer.py
+++ new_imp/test/md_optimizer/test_md_optimizer.py
@@ -2,6 +2,46 @@ import unittest
 import IMP.utils
 import IMP.test, IMP
 
+xkey = IMP.FloatKey('x')
+ykey = IMP.FloatKey('y')
+zkey = IMP.FloatKey('z')
+vxkey = IMP.FloatKey('vx')
+
+# Conversion from derivatives (in kcal/mol/A) to acceleration (A/fs/fs)
+kcal2mod = 4.1868e-7
+# Mass of Carbon-12 (kg/mol)
+cmass = 0.012011
+
+class XTransRestraint(IMP.Restraint):
+    """Attempt to move the whole system along the x axis"""
+    def __init__(self, strength):
+        IMP.Restraint.__init__(self)
+        self.strength = strength
+
+    def evaluate(self, accum):
+        e = 0.
+        for p in self.get_model().get_particles():
+            e += p.get_value(xkey) * self.strength
+        if accum:
+            for p in self.get_model().get_particles():
+                p.add_to_derivative(xkey, self.strength, accum)
+                p.add_to_derivative(ykey, 0.0, accum)
+                p.add_to_derivative(zkey, 0.0, accum)
+        return e
+
+
+class WriteTrajState(IMP.State):
+    """Write system coordinates (trajectory) into a Python list"""
+    def __init__(self, traj):
+        IMP.State.__init__(self)
+        self.traj = traj
+    def update(self):
+        model = self.get_model()
+        self.traj.append([(p.get_value(xkey), p.get_value(ykey),
+                           p.get_value(zkey), p.get_value(vxkey)) \
+                           for p in model.get_particles()])
+
+
 class MolecularDynamicsTests(IMP.test.IMPTestCase):
     """Test molecular dynamics optimizer"""
 
@@ -14,7 +54,46 @@ class MolecularDynamicsTests(IMP.test.IMPTestCase):
                                                     -43.0, 65.0, 93.0))
         self.md = IMP.MolecularDynamics()
         self.md.set_model(self.model)
-        #self.md.set_temperature(300)
+        self.md.set_temperature(300)
+
+    def _check_trajectory(self, coor, traj, timestep, vxfunc):
+        """Check generated trajectory against that predicted using vxfunc"""
+        vx = 0.
+        msg = "Predicted coordinate %.5f doesn't match generated %.5f, " + \
+              "for step %d, coordinate %d[%d]"
+        velmsg = "Predicted velocity %.5f doesn't match generated %.5f, " + \
+                 "for step %d, particle %d"
+        for (num, step) in enumerate(traj):
+            for n in range(len(coor)):
+                coor[n][0] += vx * timestep
+                self.assert_(abs(vx - step[n][3]) <= 1e-3,
+                             velmsg % (vx, step[n][3], num, n))
+                for d in range(3):
+                    self.assert_(abs(coor[n][d] - step[n][d]) <= 1e-3,
+                                 msg % (coor[n][d], step[n][d], num, n, d))
+            vx = vxfunc(vx)
+
+    def _optimize_model(self, timestep):
+        """Run a short MD optimization on the model."""
+        traj = []
+        start = [[p.get_value(xkey), p.get_value(ykey), p.get_value(zkey)] \
+                 for p in self.model.get_particles()]
+        state = WriteTrajState(traj)
+        self.model.add_state(state)
+        self.md.set_time_step(timestep)
+        self.md.optimize(50)
+        return start, traj
+
+    def test_nonrigid_translation(self):
+        """Check that non-rigid MD translation is Newtonian"""
+        timestep = 4.0
+        strength = 50.0
+        r = XTransRestraint(strength)
+        self.model.add_restraint(r)
+        (start, traj) = self._optimize_model(timestep)
+        delttm = -timestep * kcal2mod / cmass
+        self._check_trajectory(start, traj, timestep,
+                               lambda a: a + strength * delttm)
 
     def test_non_xyz(self):
         """Should be unable to do MD on optimizable non-xyz attributes"""
@@ -23,8 +102,8 @@ class MolecularDynamicsTests(IMP.test.IMPTestCase):
         p.add_attribute(IMP.FloatKey("attr"), 0.0, True)
         self.assertRaises(ValueError, self.md.optimize, 50)
 
-    def test_basic_md(self):
-        """Test basic MD"""
+    def test_make_velocities(self):
+        """Test that MD generates particle velocities"""
         self.md.optimize(0)
         keys = [IMP.FloatKey(x) for x in ("vx", "vy", "vz")]
         for p in self.particles:
