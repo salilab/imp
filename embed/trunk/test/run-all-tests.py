f2945437bb7024ab55467e862fb7d1e150c61861
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-01-12 21:12:08 +0000

    Clean up Python code to conform to PEP 8. Remove unused particle_provider module.
    git-svn-id: https://svn.salilab.org/imp/trunk@2461 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- embed/trunk/test/run-all-tests.py
+++ embed/trunk/test/run-all-tests.py
@@ -1,4 +1,3 @@
-
 import unittest, sys, os, re
 
 def regressionTest():
@@ -7,16 +6,13 @@ def regressionTest():
     path = os.path.abspath(os.path.dirname(sys.argv[0]))
     modobjs = []
     for subdir in ['.'] + [x for x in os.listdir(path) if os.path.isdir(x)]:
-            files = os.listdir(os.path.join(path, subdir))	
-            test = re.compile("^test_.*\.py$", re.IGNORECASE)	
-            files = filter(test.search, files)
-            modnames = [os.path.splitext(f)[0] for f in files]	
-            sys.path.insert(0, subdir)	       
-            modobjs.extend([__import__(m) for m	 in modnames])
-            sys.path.pop(0)		  
-
-    
-
+        files = os.listdir(os.path.join(path, subdir))
+        test = re.compile("^test_.*\.py$", re.IGNORECASE)
+        files = filter(test.search, files)
+        modnames = [os.path.splitext(f)[0] for f in files]
+        sys.path.insert(0, subdir)
+        modobjs.extend([__import__(m) for m  in modnames])
+        sys.path.pop(0)
 
     tests = [unittest.defaultTestLoader.loadTestsFromModule(o) for o in modobjs]
     return unittest.TestSuite(tests)
--- embed/trunk/test/test_read_write_maps/test_read_write_map.py
+++ embed/trunk/test/test_read_write_maps/test_read_write_map.py
@@ -5,122 +5,92 @@ import unittest
 out_filename = os.path.abspath("data/aa.mrc")
 
 
+class MRCWriteTest(unittest.TestCase):
+    """Test MRC write function on sampled map"""
+    def setUp(self):
+        """ create particles """
+        self.particles = EM.particles_provider()
+        self.particles.append(9.,5.,5.,1.,1.)
+        self.particles.append(12.,9.,4.,1.,1.)
+        self.particles.append(4.,5.,5.,1.,1.)
 
-# #implement the particle access point class
-# class particles_provider( EM.ParticlesAccessPoint):
-#        def Init(self):
-#                self.x_=[9.0,12.0,4.0]
-#                self.y_=[5.0,9.0,5.0]
-#                self.z_=[5.0,4.0,5.0]
-#                self.w_=[1.0,1.0,1.0]
-#                self.r_=[1.0,1.0,1.0]
-#                self.size_=3
+        resolution = 2.0
+        voxel_size = 1.0
+        em_map = EM.SampledDensityMap(self.particles,
+                                      resolution,voxel_size)
+        em_map.calcRMS()
+        self.rms = em_map.get_header().rms
+        print "RMSD of original map = " + str(self.rms)
+        self.erw = EM.MRCReaderWriter()
+        em_map.Write(out_filename,self.erw)
 
-#        def get_size(self):
-#                return self.size_;
-#        def get_x(self,ind):
-#                return self.x_[ind];
-#        def get_y(self,ind):
-#                return self.y_[ind];
-
-#        def get_z(self,ind):
-#                return self.z_[ind];
-
-#        def get_r(self,ind):
-#                return self.r_[ind];
-
-#        def get_w(self,ind):
-#                return self.w_[ind];
+    def test_read(self):
+        """read map back in and check that rmsd is the same """
+        em_map = EM.DensityMap()
+        em_map.Read(out_filename,self.erw)
+        os.unlink(out_filename)
+        em_map.calcRMS()
+        rms2 = em_map.get_header().rms
+        print "RMSD of file = " + str(rms2)
+        self.assert_(abs(rms2 - self.rms) < 0.00001, "rms values differ!")
 
 
-class test_write_mrc(unittest.TestCase):
-	"""test mrc write function on sampled map  """
-	def setUp(self):
-           """ create particles """
-           self.particles = EM.particles_provider()
-           self.particles.append(9.,5.,5.,1.,1.)
-           self.particles.append(12.,9.,4.,1.,1.)
-           self.particles.append(4.,5.,5.,1.,1.)
+class ReadWriteMapsTests(unittest.TestCase):
+    def assertInTolerance(self, val, expected, tol, msg=None):
+        if msg == None:
+            msg = "%f differs from %f by more than %f" \
+                  % (val, expected, tol)
+        diff = abs(val - expected)
+        self.assert_(diff < tol, msg)
 
-           resolution = 2.0
-	   voxel_size = 1.0
-	   em_map = EM.SampledDensityMap(self.particles,
-					 resolution,voxel_size)
-	   em_map.calcRMS()
-	   self.rms = em_map.get_header().rms
-	   print "RMSD of original map = " + str(self.rms)
-	   self.erw = EM.MRCReaderWriter()
-	   em_map.Write(out_filename,self.erw)
+    def test_em_read_write(self):
+        """test em format read/write """
+        in_filename = os.path.abspath("data/three_particles_in.em")
+        out_filename = os.path.abspath("data/three_particles_out.em")
 
-	def test_read(self):
-		"""read map back in and check that rmsd is the same """
-		em_map = EM.DensityMap()
-		em_map.Read(out_filename,self.erw)
-                os.unlink(out_filename)
-		em_map.calcRMS()
-		rms2 = em_map.get_header().rms
-		print "RMSD of file = " + str(rms2)
-		self.assert_(abs(rms2 - self.rms) < 0.00001, "rms values differ!")
+        scene = EM.DensityMap()
+        xrw = EM.EMReaderWriter()
 
-	
-class test_read_write_maps(unittest.TestCase):
-        def assertInTolerance(self, val, expected, tol, msg=None):
-            if msg == None:
-                msg = "%f differs from %f by more than %f" \
-                      % (val, expected, tol)
-            diff = abs(val - expected)
-            self.assert_(diff < tol, msg)
+        scene.Read(in_filename,xrw)
+        header = scene.get_header()
+        self.assertEqual(header.nx, 55)
+        self.assertEqual(header.ny, 55)
+        self.assertEqual(header.nz, 55)
+        self.assertEqual(header.magic, 6)
+        self.assertEqual(header.voltage, 0.)
+        self.assertEqual(header.Cs, 0.)
+        self.assertEqual(header.Objectpixelsize, 1.)
+        self.assertInTolerance(scene.calcRMS(), 404.5, 1.0)
+        scene.Write(out_filename,xrw);
 
-	def test_em_read_write(self):
-	    """test em format read/write """
-	    in_filename = os.path.abspath("data/three_particles_in.em")
-	    out_filename = os.path.abspath("data/three_particles_out.em")
+        scene2 = EM.DensityMap()
+        scene2.Read(out_filename, xrw)
+        header2 = scene2.get_header()
+        self.assertEqual(header2.nx, header.nx)
+        self.assertEqual(header2.ny, header.ny)
+        self.assertEqual(header2.nz, header.nz)
+        self.assertEqual(header2.magic, header.magic)
+        self.assertInTolerance(scene2.calcRMS(), 404.5, 1.0)
+        os.unlink(out_filename)
 
-	    scene = EM.DensityMap()
-	    xrw = EM.EMReaderWriter()
+    #todo - add assert functions
 
-	    scene.Read(in_filename,xrw)
-            header = scene.get_header()
-            self.assertEqual(header.nx, 55)
-            self.assertEqual(header.ny, 55)
-            self.assertEqual(header.nz, 55)
-            self.assertEqual(header.magic, 6)
-            self.assertEqual(header.voltage, 0.)
-            self.assertEqual(header.Cs, 0.)
-            self.assertEqual(header.Objectpixelsize, 1.)
-            self.assertInTolerance(scene.calcRMS(), 404.5, 1.0)
-	    scene.Write(out_filename,xrw);
+    def test_mrc_read_write(self):
+        """test mrc format read/write """
+        in_filename =  os.path.abspath("data/1tdx_sampled.mrc")
+        out_filename =  os.path.abspath("data/1tdx_sampled_out.mrc")
+        scene = EM.DensityMap()
+        mrc_rw = EM.MRCReaderWriter()
+        scene.Read(in_filename,mrc_rw)
 
-            scene2 = EM.DensityMap()
-            scene2.Read(out_filename, xrw)
-            header2 = scene2.get_header()
-            self.assertEqual(header2.nx, header.nx)
-            self.assertEqual(header2.ny, header.ny)
-            self.assertEqual(header2.nz, header.nz)
-            self.assertEqual(header2.magic, header.magic)
-            self.assertInTolerance(scene2.calcRMS(), 404.5, 1.0)
-            os.unlink(out_filename)
+        # Check header size
+        self.assertEqual(68,scene.get_header().nx)
+        self.assertEqual(65,scene.get_header().ny)
+        self.assertEqual(59,scene.get_header().nz)
+        self.assertInTolerance(scene.calcRMS(), 511.0, 1.0)
+        scene.Write(out_filename,mrc_rw)
+        os.unlink(out_filename)
 
-	#todo - add assert functions
-
-	def test_mrc_read_write(self):
-	        """test mrc format read/write """
-		in_filename =  os.path.abspath("data/1tdx_sampled.mrc")
-		out_filename =  os.path.abspath("data/1tdx_sampled_out.mrc")
-		scene = EM.DensityMap()
-		mrc_rw = EM.MRCReaderWriter()
-		scene.Read(in_filename,mrc_rw)            
-            
-		# Check header size
-		self.assertEqual(68,scene.get_header().nx)
-		self.assertEqual(65,scene.get_header().ny)
-		self.assertEqual(59,scene.get_header().nz)
-                self.assertInTolerance(scene.calcRMS(), 511.0, 1.0)
-		scene.Write(out_filename,mrc_rw)
-                os.unlink(out_filename)
-	
 
 if __name__ == '__main__':
-    print 'start test_read_write_map'
     unittest.main()
-    print 'pass test_read_write_map'
