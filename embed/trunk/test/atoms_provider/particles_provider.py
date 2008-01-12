0c1f44929b76f48d1722d0dcabad5e9a6e181a67
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-01-12 21:12:08 +0000

    Clean up Python code to conform to PEP 8. Remove unused particle_provider module.
    git-svn-id: https://svn.salilab.org/imp/trunk@2461 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- embed/trunk/test/atoms_provider/particles_provider.py
+++ embed/trunk/test/atoms_provider/particles_provider.py
@@ -1,22 +1,20 @@
 import EM
 #implement the particle access point class
-class particles_provider(EM.ParticlesAccessPoint):
-	def read(self,fn):
-		self.particles=[map(float,line.split()) for line in file(fn) if(line[0]!='#')]
-	def get_size(self):
-		return len(self.particles)
-	def get_x(self,i):
-		return self.particles[i][0]
-	def get_y(self,i):
-		return self.particles[i][1]
-	def get_z(self,i):
-		return self.particles[i][2]
-	def get_r(self,i):
-		return self.particles[i][3]
-	def get_w(self,i):
-		return self.particles[i][4]
-	def translate(self,x,y,z):
-		for p in self.particles:
-			p[0]=p[0]+x;p[1]=p[1]+y;p[2]=p[2]+z;
-	
-
+class ParticlesProvider(EM.ParticlesAccessPoint):
+    def read(self,fn):
+        self.particles=[map(float,line.split()) for line in file(fn) if(line[0]!='#')]
+    def get_size(self):
+        return len(self.particles)
+    def get_x(self,i):
+        return self.particles[i][0]
+    def get_y(self,i):
+        return self.particles[i][1]
+    def get_z(self,i):
+        return self.particles[i][2]
+    def get_r(self,i):
+        return self.particles[i][3]
+    def get_w(self,i):
+        return self.particles[i][4]
+    def translate(self,x,y,z):
+        for p in self.particles:
+            p[0]=p[0]+x;p[1]=p[1]+y;p[2]=p[2]+z;
--- embed/trunk/test/test_sample_particles/test_sample_particles.py
+++ embed/trunk/test/test_sample_particles/test_sample_particles.py
@@ -8,35 +8,34 @@ import EM,os,sys
 out_filename = os.path.abspath("sample_particles/aa.em")
 
 
-class test_sample_particles(unittest.TestCase):
-	"""test particles sampling  """
+class SampleParticlesTest(unittest.TestCase):
+    """test particles sampling  """
 
-	def setUp(self):
-		 print 'start setup'
-		 """ create particles """
-		 self.particles_p = EM.particles_provider()
-		 self.particles_p.append(9.,5.,5.,1.,1.)
-		 self.particles_p.append(12.,9.,4.,1.,1.)
-		 self.particles_p.append(4.,5.,5.,1.,1.)
-		 print 'after setup'
+    def setUp(self):
+        print 'start setup'
+        """ create particles """
+        self.particles_p = EM.particles_provider()
+        self.particles_p.append(9.,5.,5.,1.,1.)
+        self.particles_p.append(12.,9.,4.,1.,1.)
+        self.particles_p.append(4.,5.,5.,1.,1.)
+        print 'after setup'
 
-	def test_sample(self):
-		""" test that the sampling works """
-		resolution = 2.0
-		voxel_size = 1.0
-		scene = EM.SampledDensityMap(self.particles_p,
-					     resolution,voxel_size)
-                
-		scene.std_normalize()
+    def test_sample(self):
+        """ test that the sampling works """
+        resolution = 2.0
+        voxel_size = 1.0
+        scene = EM.SampledDensityMap(self.particles_p,
+                                     resolution,voxel_size)
 
-		erw = EM.EMReaderWriter()
-		scene.Write(out_filename,erw)
+        scene.std_normalize()
+
+        erw = EM.EMReaderWriter()
+        scene.Write(out_filename,erw)
 
 
-		# the mean and std are not correct
-		self.assert_(scene.get_header().dmean == 0.0, "unexpected mean for the map")
-		self.assert_(scene.get_header().rms == 1.0, "unexpected rms for the map")
+        # the mean and std are not correct
+        self.assert_(scene.get_header().dmean == 0.0, "unexpected mean for the map")
+        self.assert_(scene.get_header().rms == 1.0, "unexpected rms for the map")
 
 if __name__ == '__main__':
     unittest.main()
-		 
