78b90ff8a2084767961cdee7de8c739bd9ae7ffd
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-01-12 01:51:38 +0000

    Patch from Daniel: I cleaned up the bond decorator code. Among other things I removed the length parameter with the expectation that I will add another similar set of decorators for custom connectors (not quite sure what to call them). These will represent arbitrary length attachments between particles. I also added some test code for the bond decorators.
    The connectors will have a length and stiffness parameter and will be handled
    all together by a restraint (along the lines of what was discussed with the
    non-bonded list). Normal inter-atomic bonds could be handled by generating
    the appropriate lengths from the names of the atoms involved in each atomic
    bond and adding the connector information.
    
    
    git-svn-id: https://svn.salilab.org/imp/trunk@320 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ kernel/test/decorators/test_bonds.py
@@ -0,0 +1,35 @@
+import unittest
+import IMP
+import IMP.test
+
+class DecoratorTests(IMP.test.TestCase):
+    def test_bonded(self):
+        """Check bonds """
+        m = IMP.Model()
+        pa=IMP.Particle()
+        m.add_particle(pa)
+        pb=IMP.Particle()
+        m.add_particle(pb)
+        da= IMP.BondedDecorator.create(pa)
+        db= IMP.BondedDecorator.create(pb)
+        self.assertEqual(da.get_number_of_bonds(), 0,
+                         "Wrong number of bonds on initialization")
+        IMP.bond(da, db, IMP.BondDecorator.COVALENT)
+        self.assertEqual(da.get_number_of_bonds(), 1,
+                         "Expected to find a bond")
+        self.assertEqual(da.get_number_of_bonds(), 1,
+                         "Expected to find a bond")
+        b= da.get_bond(0)
+        self.assertEqual(b, db.get_bond(0), "Not same bond object")
+        self.assertEqual(b.get_type(), IMP.BondDecorator.COVALENT,
+                         "Wrong bond type")
+        IMP.unbond(b)
+        self.assertEqual(da.get_number_of_bonds(), 0,
+                         "Expected not to find a bond")
+        self.assertEqual(da.get_number_of_bonds(), 0,
+                         "Expected not to find a bond")
+
+
+
+if __name__ == '__main__':
+    unittest.main()
