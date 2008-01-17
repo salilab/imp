c3eea25a2cf0b71006fd424aa128e2dcf4b9a890
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-01-17 22:25:57 +0000

    Patch from Daniel, with cleanup by me: pulled the basic distance pair score computation into an internal function defined in DistancePairScore.h. This function takes an offset and a scale, so the SphereDistancePairScore, DistancePairScore and bond restraints can all use it internally.
    git-svn-id: https://svn.salilab.org/imp/trunk@335 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/src/pair_scores/SphereDistancePairScore.cpp
+++ kernel/src/pair_scores/SphereDistancePairScore.cpp
@@ -6,13 +6,11 @@
  */
 
 #include "IMP/pair_scores/SphereDistancePairScore.h"
-#include "IMP/decorators/XYZDecorator.h"
 #include "IMP/UnaryFunction.h"
+#include "IMP/pair_scores/DistancePairScore.h"
 
 namespace IMP
 {
-
-static const Float MIN_DISTANCE = .00001;
 
 SphereDistancePairScore::SphereDistancePairScore(UnaryFunction *f,
                                                  FloatKey radius) :
@@ -23,46 +21,10 @@ SphereDistancePairScore::SphereDistancePairScore(UnaryFunction *f,
 Float SphereDistancePairScore::evaluate(Particle *a, Particle *b,
                                         DerivativeAccumulator *da)
 {
-  IMP_CHECK_OBJECT(f_);
-
-  Float d2 = 0, delta[3];
-  Float score;
-
-  XYZDecorator d0 = XYZDecorator::cast(a);
-  XYZDecorator d1 = XYZDecorator::cast(b);
-  for (int i = 0; i < 3; ++i) {
-    delta[i] = d0.get_coordinate(i) - d1.get_coordinate(i);
-    d2 += square(delta[i]);
-  }
-
-  Float distance = std::sqrt(d2);
   Float ra = a->get_value(radius_);
   Float rb = b->get_value(radius_);
-  Float shifted_distance = distance - (ra + rb);
-
-  // if needed, calculate the partial derivatives of the scores with respect
-  // to the particle attributes
-  // avoid division by zero if the distance is too small
-  if (da && distance >= MIN_DISTANCE) {
-    Float deriv;
-
-    score = (*f_)(shifted_distance, deriv);
-
-    for (int i = 0; i < 3; ++i) {
-      Float d = delta[i] / distance * deriv;
-      d0.add_to_coordinate_derivative(i, d, *da);
-      d1.add_to_coordinate_derivative(i, -d, *da);
-    }
-  }
-
-  else {
-    // calculate the score based on the distance feature
-    score = (*f_)(shifted_distance);
-  }
-
-  IMP_LOG(VERBOSE, "For " << a->get_index() << " and " << b->get_index()
-          << " distance: " << distance << " score: " << score << std::endl);
-  return score;
+  return internal::evaluate_distance_pair_score(a,b, da, f_.get(), 
+                                                ra+rb, 1);
 }
 
 void SphereDistancePairScore::show(std::ostream &out) const
