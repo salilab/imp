799784455069c52e471ecd16f11b2bdee86efc35
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-02-14 23:47:56 +0000

    Also check some common alternative locations for the Boost headers, and if found there, add to the cpp search path.
    git-svn-id: https://svn.salilab.org/imp/trunk@374 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- tools/boost.py
+++ tools/boost.py
@@ -15,20 +15,32 @@ def _check(context, version):
         version_n += int(v_arr[2])
 
     context.Message('Checking for Boost version >= %s... ' % (version))
-    ret = context.TryRun("""
-    #include <boost/version.hpp>
+    # Check some common locations for the boost headers, since there is no
+    # pkg-config script:
+    for dir in ['', '/opt/local/include', '/sw/include', '/usr/local/include']:
+        inc = os.path.join(dir, 'boost/version.hpp')
+        ret = context.TryRun("""
+        #include "%s"
 
-    int main()
-    {
-        return BOOST_VERSION >= %d ? 0 : 1;
-    }
-    """ % version_n, '.cpp')[0]
+        int main()
+        {
+            return BOOST_VERSION >= %d ? 0 : 1;
+        }
+        """ % (inc, version_n), '.cpp')[0]
+        if ret:
+            context.env['BOOST_CPPPATH'] = dir
+            if dir == '':
+                context.Result(ret)
+            else:
+                context.Result(dir)
+            return ret
     context.Result(ret)
     return ret
 
 def configure_check(env, version):
     custom_tests = {'CheckBoost':_check}
     conf = env.Configure(custom_tests=custom_tests)
+    env['BOOST_CPPPATH'] = ''
     if not env.GetOption('clean') and not env['OPTION_HELP'] \
        and conf.CheckBoost(version) is 0:
         Exit("Boost version >= %s is required to build IMP" % version)
