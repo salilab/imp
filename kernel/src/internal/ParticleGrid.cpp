3d34422e47f7f3d420ce39dd9ba7219137ccf9a8
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-02-21 08:20:44 +0000

    Revert part of previous patch; the code did not compile.
    git-svn-id: https://svn.salilab.org/imp/trunk@415 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/src/internal/ParticleGrid.cpp
+++ kernel/src/internal/ParticleGrid.cpp
@@ -23,7 +23,7 @@ ParticleGrid::ParticleGrid(float tvs): target_voxel_side_(tvs),
   fks.push_back(FloatKey("x"));
   fks.push_back(FloatKey("y"));
   fks.push_back(FloatKey("z"));
-  mc_= new MaxChangeScoreState(fks);
+  mc_= std::auto_ptr<MaxChangeScoreState>(new MaxChangeScoreState(fks));
 }
 
 ParticleGrid::ParticleGrid(): target_voxel_side_(0), grid_valid_(0)
