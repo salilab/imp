5c115685e91c54b25c2e0dcd0fcc394a2fb7c821
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-02-22 00:34:51 +0000

    Patch from Daniel, with cleanup by me: Rewrote ConnectivityRestraint to use the boost MST code and to fix a bug. The test has also been improved.
    git-svn-id: https://svn.salilab.org/imp/trunk@422 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/test/connectivity/test_connectivity.py
+++ kernel/test/connectivity/test_connectivity.py
@@ -31,7 +31,8 @@ class ConnectivityTests(IMP.test.TestCase):
         p0=IMP.Particles()
         p1=IMP.Particles()
         p2=IMP.Particles()
-        for i in range(12):
+        p3=IMP.Particles()
+        for i in range(13):
             p= IMP.Particle()
             m.add_particle(p)
             d= IMP.XYZDecorator.create(p)
@@ -45,6 +46,12 @@ class ConnectivityTests(IMP.test.TestCase):
             else :
                 p2.append(p)
                 p.add_attribute(rk, 3);
+        p= IMP.Particle()
+        m.add_particle(p)
+        d= IMP.XYZDecorator.create(p)
+        d.set_coordinates_are_optimized(True)
+        p3.append(p)
+        p.add_attribute(rk, 1);
 
         o = IMP.ConjugateGradients()
         o.set_threshold(1e-4)
@@ -60,19 +67,45 @@ class ConnectivityTests(IMP.test.TestCase):
         r.add_set(p0)
         r.add_set(p1)
         r.add_set(p2)
+        r.add_set(p3)
         o.optimize(1000)
         d01= self.min_distance(p0, p1)
         d02= self.min_distance(p0, p2)
         d12= self.min_distance(p1, p2)
+        d03= self.min_distance(p0, p3)
+        d13= self.min_distance(p1, p3)
+        d23= self.min_distance(p2, p3)
         ok01= (d01 < 3.5)
         ok02= (d02 < 4.5)
         ok12= (d12 < 5.5)
+        ok03= (d03 < 2.5)
+        ok13= (d13 < 3.5)
+        ok23= (d23 < 4.5)
+        print ok01
+        print ok02
+        print ok12
+        print ok03
+        print ok13
+        print ok23
+        sum = ok01 + ok02 + ok12 + ok03 + ok13 + ok23
+        print sum
         print d01
         print d02
         print d12
+        print d03
+        print d13
+        print d23
         score= m.evaluate(False)
-        self.assert_(ok01 and ok02 or ok01 and ok12 or ok12 and ok02,
-                     "Particles are too far")
+        self.assert_(sum >= 3,
+                     "Wrong number of close pairs")
+        self.assert_(ok01 or ok02 or ok03,
+                     "Point 0 is not connected")
+        self.assert_(ok01 or ok12 or ok13,
+                     "Point 1 is not connected")
+        self.assert_(ok02 or ok12 or ok23,
+                     "Point 2 is not connected")
+        self.assert_(ok03 or ok13 or ok23,
+                     "Point 3 is not connected")
         self.assert_(score < 10, "Score too high");
 
 
