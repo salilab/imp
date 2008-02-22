bc21359c022c00720e96d22a3a7fae4b7bb95893
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-02-22 00:34:51 +0000

    Patch from Daniel, with cleanup by me: Rewrote ConnectivityRestraint to use the boost MST code and to fix a bug. The test has also been improved.
    git-svn-id: https://svn.salilab.org/imp/trunk@422 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/src/restraints/ConnectivityRestraint.cpp
+++ kernel/src/restraints/ConnectivityRestraint.cpp
@@ -13,8 +13,10 @@
 #include "IMP/restraints/ConnectivityRestraint.h"
 #include "IMP/PairScore.h"
 
-#include <cmath>
-#include <set>
+#include <boost/graph/kruskal_min_spanning_tree.hpp>
+#include <boost/graph/adjacency_list.hpp>
+
+#include <limits>
 
 namespace IMP
 {
@@ -30,7 +32,8 @@ ConnectivityRestraint::~ConnectivityRestraint()
 {
 }
 
-struct Pair {
+struct Pair
+{
   typedef Pair This;
   Pair(){p_[0]=-1; p_[1]=-1;}
   Pair(int a, int b){
@@ -67,68 +70,57 @@ Float ConnectivityRestraint::evaluate(DerivativeAccumulator *accum)
 {
   IMP_CHECK_OBJECT(ps_.get());
   unsigned int num_sets= set_offsets_.size()-1;
-  typedef std::vector<std::pair<Pair, float> > Edges;
-  typedef std::map<Pair, Edges> EMap;
-  EMap edges;
+
+  typedef boost::adjacency_list<boost::vecS, boost::vecS,
+    boost::undirectedS, boost::no_property,
+    boost::property<boost::edge_weight_t, float> > Graph;
+  typedef boost::graph_traits<Graph>::edge_descriptor Edge;
+  typedef boost::graph_traits<Graph>::vertex_descriptor Vertex;
+  Graph g(Restraint::number_of_particles());
+
+  const float tag_weight= -std::numeric_limits<float>::max();
   for (unsigned int i=0; i< num_sets; ++i) {
+    for (unsigned int ii= set_offsets_[i]+1; ii < set_offsets_[i+1]; ++ii) {
+      Edge e = boost::add_edge(ii-1, ii, g).first;
+      boost::put(boost::edge_weight_t(), g, e, tag_weight);
+    }
     for (unsigned int j=0; j< i; ++j) {
-      Pair cp(i,j);
       for (unsigned int ii= set_offsets_[i]; ii != set_offsets_[i+1]; ++ii) {
         for (unsigned int ij= set_offsets_[j]; ij != set_offsets_[j+1]; ++ij) {
-          Pair pp(ii, ij);
           float d= ps_->evaluate(get_particle(ii), get_particle(ij), NULL);
-          edges[cp].push_back(std::make_pair(pp,d));
+          IMP_check(d != tag_weight, "ConnectivityRestraint uses the distance "
+                    << tag_weight  << " as a marker, so the distance function "
+                    << " should not return it. This can be fixed if you "
+                    << " complain.",
+                    ValueException("Tag weight matches distance in"\
+                                   " ConnectivityRestraint"));
+          IMP_LOG(VERBOSE, "ConnectivityRestraint edge between "
+                  << get_particle(ii)->get_index() << " and "
+                  << get_particle(ij)->get_index() << " with weight "
+                  << d << std::endl);
+          Edge e = boost::add_edge(ii, ij, g).first;
+          boost::put(boost::edge_weight_t(), g, e, d);
         }
       }
     }
   }
 
-  std::set<int> in;
-  in.insert(0);
-  Edges picked_edges;
-  while (in.size() != num_sets) {
-    float best=std::numeric_limits<float>::max();
-    Pair best_edge, best_sets;
-    IMP_assert(!edges.empty(), "No more edges :-(");
-    for (EMap::iterator it = edges.begin(); it != edges.end(); ++it) {
-      Pair pit= it->first;
-      /*IMP_LOG(VERBOSE, "Looking at edges between " << pit[0] << " " 
-        << pit[1] << std::endl);*/
-      IMP_assert(in.find(pit[0]) == in.end() 
-                 || in.find(pit[1]) == in.end(), 
-                 "Edge only involves already visited nodes");
-      if (in.find(pit[0]) == in.end() && in.find(pit[1]) == in.end()) {
-        // want to maintain a connected tree
-        continue;
-      }
-      for (unsigned int i=0; i< it->second.size(); ++i) {
-        if (it->second[i].second < best) {
-          best= it->second[i].second;
-          best_edge= it->second[i].first;
-          IMP_assert(best_edge != Pair(), "Bad pair");
-          best_sets= it->first;
-        }
-      }
-    }
-    int newi=best_sets[0];
-    if (in.find(newi) != in.end()) newi= best_sets[1];
-
-    IMP_LOG(VERBOSE, "Adding set " << newi << " with score " 
-            << best << std::endl);
-    picked_edges.push_back(std::make_pair(best_edge, best));
-    in.insert(newi);
-    IMP_assert(edges.find(best_sets) != edges.end(),
-               "Where did they go?");
-    /*IMP_LOG(VERBOSE, "Removing edges " << best_sets[0] << " " 
-      << best_sets[1] << std::endl);*/
-    edges.erase(best_sets);
-  }
+  std::vector<Edge> mst;
+  boost::kruskal_minimum_spanning_tree(g, std::back_inserter(mst));
 
   float sum=0;
   // could be more clever if accum is NULL
-  for (unsigned int i=0; i< picked_edges.size(); ++i) {
-    sum+= ps_->evaluate(get_particle(picked_edges[i].first[0]),
-                        get_particle(picked_edges[i].first[1]),
+  for (unsigned int index=0; index< mst.size(); ++index) {
+    if (boost::get(boost::edge_weight_t(), g, mst[index]) == tag_weight) {
+      continue;
+    }
+    int i= boost::target(mst[index], g);
+    int j= boost::source(mst[index], g);
+    IMP_LOG(VERBOSE, "ConnectivityRestraint edge between "
+            << get_particle(i)->get_index()
+            << " and " << get_particle(j)->get_index() << std::endl);
+    sum+= ps_->evaluate(get_particle(i),
+                        get_particle(j),
                         accum);
   }
   return sum;
