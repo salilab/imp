06e84558367f55accaff5ff9d9367ca23bddc60f
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-03-07 23:17:36 +0000

    Test * and - operators on Vector3D; remove unnecessary "only for Python" methods from the API.
    git-svn-id: https://svn.salilab.org/imp/trunk@465 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/test/misc/test_vector3d.py
+++ kernel/test/misc/test_vector3d.py
@@ -22,6 +22,8 @@ class Vector3DTests(IMP.test.TestCase):
         v1 = IMP.Vector3D(1.0, 2.0, 3.0)
         v2 = IMP.Vector3D(10.0, 1.0, 2.0)
         self.assertEqual(v1.scalar_product(v2), v2.scalar_product(v1))
+        self.assertEqual(v1.scalar_product(v2), v1 * v2)
+        self.assertEqual(v1.scalar_product(v2), v2 * v1)
         self.assertEqual(v1.scalar_product(v2), 18.0)
 
     def test_vector_product(self):
@@ -39,7 +41,7 @@ class Vector3DTests(IMP.test.TestCase):
         """Check Vector3D difference"""
         v1 = IMP.Vector3D(1.0, 2.0, 3.0)
         v2 = IMP.Vector3D(10.0, 1.0, 2.0)
-        diff = v1.difference(v2)
+        diff = v1 - v2
         expected_diff = (-9.0, 1.0, 1.0)
         for i in range(3):
             self.assertEqual(diff[i], expected_diff[i])
