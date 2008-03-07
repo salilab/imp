e36d3b9f3572f27e982cb13311591c69fe95ee35
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-03-07 22:54:27 +0000

    Add simple tests for Vector3D.
    git-svn-id: https://svn.salilab.org/imp/trunk@463 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ kernel/test/misc/test_vector3d.py
@@ -0,0 +1,48 @@
+import unittest
+import IMP
+import IMP.test
+import math
+
+class Vector3DTests(IMP.test.TestCase):
+    def test_magnitude(self):
+        """Check Vector3D magnitude"""
+        v = IMP.Vector3D(1.0, 2.0, 3.0)
+        self.assertEqual(v.get_squared_magnitude(), 14.0)
+        self.assertAlmostEqual(v.get_magnitude(), math.sqrt(14.0), places=1)
+
+    def test_component(self):
+        """Check Vector3D components"""
+        v = IMP.Vector3D(1.0, 2.0, 3.0)
+        self.assertEqual(v.get_component(0), 1.0)
+        self.assertEqual(v.get_component(1), 2.0)
+        self.assertEqual(v.get_component(2), 3.0)
+
+    def test_scalar_product(self):
+        """Check Vector3D scalar product"""
+        v1 = IMP.Vector3D(1.0, 2.0, 3.0)
+        v2 = IMP.Vector3D(10.0, 1.0, 2.0)
+        self.assertEqual(v1.scalar_product(v2), v2.scalar_product(v1))
+        self.assertEqual(v1.scalar_product(v2), 18.0)
+
+    def test_vector_product(self):
+        """Check Vector3D vector product"""
+        v1 = IMP.Vector3D(1.0, 2.0, 3.0)
+        v2 = IMP.Vector3D(10.0, 1.0, 2.0)
+        v12 = v1.vector_product(v2)
+        v21 = v2.vector_product(v1)
+        expected_v12 = (1.0, 28.0, -19.0)
+        for i in range(3):
+            self.assertEqual(v12.get_component(i), -v21.get_component(i))
+            self.assertEqual(v12.get_component(i), expected_v12[i])
+
+    def test_difference(self):
+        """Check Vector3D difference"""
+        v1 = IMP.Vector3D(1.0, 2.0, 3.0)
+        v2 = IMP.Vector3D(10.0, 1.0, 2.0)
+        diff = v1.difference(v2)
+        expected_diff = (-9.0, 1.0, 1.0)
+        for i in range(3):
+            self.assertEqual(diff.get_component(i), expected_diff[i])
+
+if __name__ == '__main__':
+    unittest.main()
