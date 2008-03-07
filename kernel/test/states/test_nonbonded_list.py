c5288be591f2eca16b4fb480372233b149c6c347
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-03-07 22:07:51 +0000

    Patch from Daniel: add a comment about the internal checks in the nonbonded list test.
    git-svn-id: https://svn.salilab.org/imp/trunk@460 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/test/states/test_nonbonded_list.py
+++ kernel/test/states/test_nonbonded_list.py
@@ -119,6 +119,8 @@ class TestNBL(IMP.test.TestCase):
         self.assertEqual(score, 25, "Wrong score")
     def test_spheres2(self):
         """Test the nonbonded list of spheres (num pairs)"""
+        # This uses the internal checks of the nonbonded list to
+        # verify correctness
         m= IMP.Model()
         rk= IMP.FloatKey("radius")
         for i in range(0,100):
