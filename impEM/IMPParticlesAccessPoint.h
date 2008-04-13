d9ec2b8eed36b4f70030d895a597e97c6ebef4bb
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-04-13 05:56:39 +0000

    Patch from Keren, with cleanup by me: Fix a bug in IMPParticlesAccessPoint. The problem was that we stored the particle ids in particle_indexes as [3,4,5,10] but the access functions get_x(ind), the ind referred to the absolute position in the vec (0,1,2,3). get_x tried to access particle with id 0 instead of id 3. Now fixed.
    git-svn-id: https://svn.salilab.org/imp/trunk@523 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- impEM/IMPParticlesAccessPoint.h
+++ impEM/IMPParticlesAccessPoint.h
@@ -46,33 +46,54 @@ public:
     return particle_indexes.size();
   }
   float get_x(int ind) const {
-    return model->get_particle(ind)->get_value(x_key);
+    int ind_val = vec_pos2particle_ind(ind);
+    return model->get_particle(ind_val)->get_value(x_key);
   }
   float get_y(int ind) const {
-    return model->get_particle(ind)->get_value(y_key);
+    int ind_val = vec_pos2particle_ind(ind);
+    return model->get_particle(ind_val)->get_value(y_key);
   }
   float get_z(int ind) const {
-    return model->get_particle(ind)->get_value(z_key);
+    int ind_val = vec_pos2particle_ind(ind);
+    return model->get_particle(ind_val)->get_value(z_key);
   }
   float get_r(int ind) const {
-    return model->get_particle(ind)->get_value(radius_key);
+    int ind_val = vec_pos2particle_ind(ind);
+    return model->get_particle(ind_val)->get_value(radius_key);
   }
   float get_w(int ind) const {
-    return model->get_particle(ind)->get_value(weight_key);
+    int ind_val = vec_pos2particle_ind(ind);
+    return model->get_particle(ind_val)->get_value(weight_key);
   }
   void set_x(unsigned int ind, float xval) {
-    model->get_particle(ind)->set_value(x_key,xval);
+    int ind_val = vec_pos2particle_ind(ind);
+    model->get_particle(ind_val)->set_value(x_key,xval);
   }
   void set_y(unsigned int ind, float yval) {
-    model->get_particle(ind)->set_value(y_key,yval);
+    int ind_val = vec_pos2particle_ind(ind);
+    model->get_particle(ind_val)->set_value(y_key,yval);
   }
   void set_z(unsigned int ind, float zval) {
-    model->get_particle(ind)->set_value(z_key,zval);
+    int ind_val = vec_pos2particle_ind(ind);
+    model->get_particle(ind_val)->set_value(z_key,zval);
   }
 
   FloatKey get_x_key() const { return x_key;}
   FloatKey get_y_key() const { return y_key;}
   FloatKey get_z_key() const { return z_key;}
+
+  //! Given a position in the particle_indexes vector, returns the stored index.
+  /** \param[in] position in the particle_indexes vector
+      \return particle_indexes[vec_pos]
+      \exception std::out_of_range vec_pos is out of range
+      For example if particle_indexes vector is [4,5,6] then
+      vec_pos2particle_ind(0) returns 4
+   */  
+  int vec_pos2particle_ind(int vec_pos) const {
+    IMP_assert(((unsigned int)vec_pos) < particle_indexes.size(),
+               "the requested vec_pos " << vec_pos << " is out of range" );
+    return particle_indexes[vec_pos];
+  }
 private:
 
   void create_keys(std::string radius_att_name_, std::string weight_att_name_);
