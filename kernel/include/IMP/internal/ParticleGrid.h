9c985a85e2b3c7eeb2aefb484e25a255b0b62fab
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-05-19 19:40:15 +0000

    Correct logic.
    git-svn-id: https://svn.salilab.org/imp/trunk@567 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/include/IMP/internal/ParticleGrid.h
+++ kernel/include/IMP/internal/ParticleGrid.h
@@ -157,9 +157,9 @@ public:
       return &temp_;
     }
     bool operator==(const ParticleVoxelIterator &o) const {
-      return cvoxel_== o.cvoxel_ && (cvoxel_ == evoxel_ 
-                                     && (o.cvoxel_ == o.evoxel_ 
-                                         || curp_ == o.curp_));
+      return cvoxel_ == o.cvoxel_
+             && ((cvoxel_ == evoxel_ && o.cvoxel_ == o.evoxel_)
+                 || curp_ == o.curp_);
     }
     bool operator!=(const ParticleVoxelIterator &o) const {
       return !operator==(o);
