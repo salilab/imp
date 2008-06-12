f3715c94598213557c709db41fd17a492e5f9c6a
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-06-12 16:08:45 +0000

    Minor cleanups to conform to coding standards.
    git-svn-id: https://svn.salilab.org/imp/trunk@2483 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- embed/trunk/src/emlib/CoarseCCatIntervals.cpp
+++ embed/trunk/src/emlib/CoarseCCatIntervals.cpp
@@ -1,8 +1,13 @@
 #include "CoarseCCatIntervals.h"
-	
-CoarseCCatIntervals::CoarseCCatIntervals(const int &ncd){
 
-  calls_counter=0;    // Number of times the evaluation has been called. The evaluation is only performed the first time and when calls_counter reaches eval_interval. Otherwise the stored_cc_ value is returned
+CoarseCCatIntervals::CoarseCCatIntervals(const int &ncd)
+{
+
+  // Number of times the evaluation has been called. The evaluation is only
+  // performed the first time and when calls_counter reaches eval_interval.
+  // Otherwise the stored_cc_ value is returned
+  calls_counter=0;
+
   stored_cc_ = 0.0;   
   dv_memory_allocated = false;
   allocate_derivatives_array(ncd);
@@ -12,19 +17,20 @@ CoarseCCatIntervals::CoarseCCatIntervals()
 {
   calls_counter=0;  
   stored_cc_ = 0.0;   
-  dv_memory_allocated=false;	  
+  dv_memory_allocated=false;
 }
- 
 
-void CoarseCCatIntervals::allocate_derivatives_array(int ncd) {
+
+void CoarseCCatIntervals::allocate_derivatives_array(int ncd)
+{
   if (dv_memory_allocated) 
     return;
   // Allocate memmory for the derivative terms if not done yet
   stored_dvx_=new float [ncd]; 
   stored_dvy_=new float [ncd]; 
   stored_dvz_=new float [ncd]; 
-  
-  for(int i=0;i<ncd;i++) {
+
+  for (int i=0;i<ncd;i++) {
      stored_dvx_[i] = 0.0;
      stored_dvy_[i] = 0.0;
      stored_dvz_[i] = 0.0;
@@ -32,54 +38,53 @@ void CoarseCCatIntervals::allocate_derivatives_array(int ncd) {
   dv_memory_allocated=true;
 }
 
-float CoarseCCatIntervals::evaluate(
-				    DensityMap &em_map, 
-				     SampledDensityMap &model_map,
-				     const ParticlesAccessPoint &access_p,
-				     std::vector<float> &dvx,std::vector<float>&dvy,std::vector<float>&dvz,
-				     float scalefac,
-				     bool lderiv,
-				     unsigned long eval_interval ){ // the interval size before recalculating the CC score
+float CoarseCCatIntervals::evaluate(DensityMap &em_map, 
+                                    SampledDensityMap &model_map,
+                                    const ParticlesAccessPoint &access_p,
+                                    std::vector<float> &dvx,
+                                    std::vector<float> &dvy,
+                                    std::vector<float> &dvz,
+                                    float scalefac, bool lderiv,
+                         // the interval size before recalculating the CC score
+                                    unsigned long eval_interval)
+{
 
 
   // If the function requires to be evaluated
-  if  (calls_counter%eval_interval ==0) {
+  if  (calls_counter % eval_interval == 0) {
 
-    // we should sync the derivatives. store_dv holds the EM contirbution of the derivatives. 
-    // The base evaluate function adds the EM contiorbution to the existing derivates.
+    // we should sync the derivatives. store_dv holds the EM contribution of
+    // the derivatives.  The base evaluate function adds the EM contribution
+    // to the existing derivatives.
 
-    stored_cc_ = CoarseCC::evaluate(
-			em_map,
-			model_map,
-			access_p,
-			dvx,dvy,dvz,
-			scalefac,
-			lderiv);
-	
+    stored_cc_ = CoarseCC::evaluate(em_map, model_map, access_p, dvx, dvy, dvz,
+                                    scalefac, lderiv);
+
     calls_counter = 1;
 
-    if (lderiv) { // sync the derivatives. Now remove the additional contributions ( currently stored in store_dv from the new values
+    if (lderiv) {
+      // sync the derivatives. Now remove the additional contributions
+      // (currently stored in store_dv) from the new values
       allocate_derivatives_array(access_p.get_size());
-      for(int i=0;i<access_p.get_size();i++) {
-	 stored_dvx_[i] = dvx[i];
-	 stored_dvy_[i] = dvy[i];
-	 stored_dvz_[i] = dvz[i];
-       }
+      for (int i=0;i<access_p.get_size();i++) {
+        stored_dvx_[i] = dvx[i];
+        stored_dvy_[i] = dvy[i];
+        stored_dvz_[i] = dvz[i];
+      }
     }
   }
-  // If the function does not require to evaluate, return the previously stored CCC and add the derivatives
-  // to the general terms
+  // If the function does not require to evaluate, return the previously
+  // stored CCC and add the derivatives to the general terms
   else {
-    for(int i=0;i<access_p.get_size();i++) {
+    for (int i=0;i<access_p.get_size();i++) {
       if (lderiv) {
-	dvx[i] = stored_dvx_[i];
-	dvy[i] = stored_dvy_[i];
-	dvz[i] = stored_dvz_[i];
+        dvx[i] = stored_dvx_[i];
+        dvy[i] = stored_dvy_[i];
+        dvz[i] = stored_dvz_[i];
       }
     }
     ++calls_counter;
   }
-  
+
   return stored_cc_;
 }
-
--- embed/trunk/src/emlib/DensityHeader.h
+++ embed/trunk/src/emlib/DensityHeader.h
@@ -8,19 +8,20 @@
 #include "def.h"
 
 
-// TODO - change so that the att will not be encoded but loaded from a conf file (Keren)
-
-class EMDLLEXPORT DensityHeader {
+/** \todo change so that the att will not be encoded but loaded from
+    a conf file (Keren) */
+class EMDLLEXPORT DensityHeader
+{
 
 public:
   DensityHeader() {
-    
+
     top_calculated=false;
     // Initialize some of the parameters of the header
     nx=0;ny=0;nz=0; 
     data_type=5; 
     nxstart=1; nystart=1; nzstart=1;
-    
+
     mx = nxstart + nx - 1; my = nystart + ny - 1; mz = nzstart + nz - 1;
     xorigin=0.0; yorigin=0.0; zorigin =0.0;
     alpha=90. ; beta=90. ; gamma=90.;
@@ -34,10 +35,13 @@ public:
     magic=6;
   }
 
-  // a copy constructor is not defined in a class, the compiler  itself defines one. This will ensure a shallow copy. 
-  //If the class does not have pointer variables with dynamically allocated memory, then one need not worry about defining a 
-  //copy constructor. It can be left to the compiler's discretion.
-  //But if the class has pointer variables and has some dynamic memory allocations, then it is a must to have a copy constructor.
+  // If a copy constructor is not defined in a class, the compiler itself
+  // defines one. This will ensure a shallow copy. 
+  // If the class does not have pointer variables with dynamically allocated
+  // memory, then one need not worry about defining a 
+  // copy constructor. It can be left to the compiler's discretion.
+  // But if the class has pointer variables and has some dynamic memory
+  // allocations, then it is a must to have a copy constructor.
 
   //! gets the upper bound of the grid
   /** \param[in] ind The dimension index x:=0,y:=1,z:=2
@@ -46,7 +50,8 @@ public:
   emreal get_top(int ind) const 
   { 
     if (!top_calculated) {
-      std::cerr << " DensityHeader::get_top  the top coordinates of the map have not been setup yet " << std::endl;
+      std::cerr << " DensityHeader::get_top  the top coordinates of the map "
+                << "have not been setup yet " << std::endl;
       throw 1;
     }
     if (ind==0) return xtop;
@@ -54,7 +59,7 @@ public:
     return ztop;
 
   }
-    
+
   //! Computes the maximum x,y,z coordinates of the grid.
   /**
    */
@@ -63,16 +68,22 @@ public:
   friend std::ostream& operator<<(std::ostream& s, const DensityHeader &v) {
     s<< "nx: " << v.nx << " ny: " << v.ny << " nz: " << v.nz << std::endl;
     s<<"data_type: " << v.data_type << std::endl;
-    s<<"nxstart: " << v.nxstart << " nystart: " << v.nystart <<" nzstart: " << v.nzstart << std::endl;
+    s <<"nxstart: " << v.nxstart << " nystart: " << v.nystart <<" nzstart: "
+      << v.nzstart << std::endl;
     s<<"mx: "<< v.mx <<" my:" << v.my << " mz: " << v.mz << std::endl;
-    s<< "xlen: " << v.xlen <<" ylen: " << v.ylen <<" zlen: " << v.zlen << std::endl;
-    s <<"alpha : " << v.alpha << " beta: " << v.beta <<" gamma: "<< v.gamma << std::endl;
-    s<< "mapc : " << v.mapc << " mapr: " << v.mapr <<" maps: " << v.maps << std::endl;
-    s << "dmin: " << v.dmin << " dmax: " << v.dmax << " dmean: " << v.dmean << std::endl;
+    s << "xlen: " << v.xlen <<" ylen: " << v.ylen <<" zlen: " << v.zlen
+      << std::endl;
+    s <<"alpha : " << v.alpha << " beta: " << v.beta <<" gamma: "<< v.gamma
+      << std::endl;
+    s << "mapc : " << v.mapc << " mapr: " << v.mapr <<" maps: " << v.maps
+      << std::endl;
+    s << "dmin: " << v.dmin << " dmax: " << v.dmax << " dmean: " << v.dmean
+      << std::endl;
     s <<"ispg: " << v.ispg << std::endl;
     s <<"nsymbt: " << v.nsymbt << std::endl;
     s << "user: " << v.user << std::endl;
-    s <<"xorigin: " << v.xorigin << " yorigin: "<< v.yorigin <<" zorigin: "<< v.zorigin << std::endl;
+    s <<"xorigin: " << v.xorigin << " yorigin: "<< v.yorigin <<" zorigin: "
+      << v.zorigin << std::endl;
     s <<"map: " << v.map << std::endl;
     s << "Objectpixelsize: " << v.Objectpixelsize << std::endl;
     s<< "machinestamp: " << v.machinestamp << std::endl;
@@ -90,33 +101,40 @@ public:
   static const unsigned short COMMENT_FIELD_SINGLE_SIZE    =  80;
 
   int nx,ny,nz; // map size
-  int data_type; //how many bits are used to store the density of a single voxel ( used in MRC format)
+  //! How many bits are used to store the density of a single voxel
+  //! (used in MRC format)
+  int data_type;
   int nxstart,nystart,nzstart; //number of first columns in map 
   int mx, my, mz; // Number of intervals along each dimension
   float xlen,ylen,zlen; //Cell dimensions (angstroms) 
   float alpha, beta, gamma; //Cell angles (degrees)
-  int mapc, mapr, maps; //Axes corresponding to columns (mapc), rows (mapr) and sections (maps) (1,2,3 for x,y,z)
+  //! Axes corresponding to columns (mapc), rows (mapr) and sections (maps)
+  //! (1,2,3 for x,y,z)
+  int mapc, mapr, maps;
   float dmin,dmax,dmean; //Minimum, maximum and mean density value
   int ispg; //space group number 0 or 1
   int nsymbt; //Number of bytes used for symmetry data
   int user[USER_FIELD_SIZE];//extra space used for anything
-  char map[MAP_FIELD_SIZE];	//character string 'MAP ' to identify file type
+  char map[MAP_FIELD_SIZE]; //character string 'MAP ' to identify file type
   int machinestamp; //machine stamp (0x11110000 bigendian, 0x44440000 little)
   float rms; //RMS deviation of map from mean density
   int nlabl; //Number of labels being used
-  char comments[COMMENT_FIELD_NUM_OF][COMMENT_FIELD_SINGLE_SIZE]; //text comments TODO: MRC: labels[10][80] - should it be a different field?
-  int magic; //magic byte for machine platform (~endian), OS-9=0, VAX=1, Convex=2, SGI=3, Sun=4, Mac(Motorola)=5, PC,IntelMac=6
+  //! text comments \todo MRC: labels[10][80] - should it be a different field?
+  char comments[COMMENT_FIELD_NUM_OF][COMMENT_FIELD_SINGLE_SIZE];
+  //! magic byte for machine platform (~endian), OS-9=0, VAX=1, Convex=2,
+  //! SGI=3, Sun=4, Mac(Motorola)=5, PC,IntelMac=6
+  int magic;
   float voltage; //Voltage of electron microscope
-  float Cs;	//Cs of microscope
+  float Cs;  //Cs of microscope
   float Aperture;  //Aperture used
-  float Magnification;	//Magnification
+  float Magnification;  //Magnification
   float Postmagnification; //Postmagnification (of energy filter)
   float Exposuretime; //Exposuretime
   float Objectpixelsize; //this is the actual pixelsize 
-  float Microscope;	//Microscope
+  float Microscope;  //Microscope
   float Pixelsize; //Pixelsize - used for the microscope CCD camera
-  float CCDArea;	//CCDArea
-  float Defocus;	//Defocus
+  float CCDArea;  //CCDArea
+  float Defocus;  //Defocus
   float Astigmatism;//Astigmatism
   float AstigmatismAngle; //Astigmatism Angle
   float FocusIncrement;//Focus-Increment
@@ -139,7 +157,7 @@ public:
   inline void set_yorigin(float y)  {yorigin=y; top_calculated=false;}
   inline void set_zorigin(float z)  {zorigin=z; top_calculated=false;}
   inline bool is_top_calculated() const { return top_calculated;}
-  
+
 protected:
   float xtop, ytop,ztop; // The upper bound for the x,y and z grid.
   float xorigin, yorigin, zorigin; //Origin used for transforms
