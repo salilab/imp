c5680710de867909267dafe543d1a958a78b7499
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-06-15 18:24:29 +0000

    Fix typos; hide implementation details; conform to standards for member variable naming.
    git-svn-id: https://svn.salilab.org/imp/trunk@2495 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- embed/trunk/src/emlib/ErrorHandling.h
+++ embed/trunk/src/emlib/ErrorHandling.h
@@ -1,5 +1,5 @@
-#ifndef _ERRORHANDELING_H
-#define _ERRORHANDELING_H
+#ifndef _ERRORHANDLING_H
+#define _ERRORHANDLING_H
 
 #include "EM_config.h"
 #include <string>
@@ -11,54 +11,55 @@
 
 
 //! Exception to throw when a variable has a wrong value
-class EMDLLEXPORT EMBED_WrongValue: public std::exception {
-// Using EMDLLEXPORT is necessary to make the object public when compiled in a 
-// dynamic share object in SWIG
-  public:
-  //! Stores the name of the variable that failed
-  std::string var;
-  //! Stores the value that make it fail
-  float value;
-
+class EMDLLEXPORT EMBED_WrongValue: public std::exception
+{
+public:
   virtual ~EMBED_WrongValue() throw() {};
 
   //! Constructor
-  /**
-     \param[in] var string with the name of the variable that failed
-     \param[in] value value of the variable that failed
-  */
-  EMBED_WrongValue(std::string variable,float val);
+  /** \param[in] var string with the name of the variable that failed
+      \param[in] value value of the variable that failed
+   */
+  EMBED_WrongValue(std::string variable, float val);
 
-   //! Error message
-    virtual const char* what() const throw() {
-      std::ostringstream msg;
-      msg << "Wrong value with variable: " << var << " " << value;
-      return msg.str().c_str();
-    }
- };
+  //! Error message
+  virtual const char* what() const throw() {
+    std::ostringstream msg;
+    msg << "Wrong value with variable: " << var_ << " " << value_;
+    return msg.str().c_str();
+  }
+
+protected:
+  //! Stores the name of the variable that failed
+  std::string var_;
+  //! Stores the value that made it fail
+  float value_;
+};
+
 
 //! Exception to throw when there are I/O problems
-class EMDLLEXPORT EMBED_IOException: public std::exception {
-  public:
-  //! Text to be printed when the what() function is called
-  std::string txt;
+class EMDLLEXPORT EMBED_IOException: public std::exception
+{
+public:
   virtual ~EMBED_IOException() throw() {};
 
   //! Constructor
-  /**
-     \param[in] s string used as text when function what() is called. 
-                  Is stored in txt().
-  */
+  /** \param[in] s string used as text when function what() is called. 
+   */
   EMBED_IOException(std::string s);
+
   //! Error message
-  /**
-     \return the error message
-  */
+  /** \return the error message
+   */
   virtual const char* what() const throw() {
     std::ostringstream msg;
-    msg <<  txt ;
+    msg << txt_;
     return msg.str().c_str();
   }
+
+protected:
+  //! Text to be printed when the what() function is called
+  std::string txt_;
 };
 
-#endif //_ERRORHANDELING_H
+#endif //_ERRORHANDLING_H
