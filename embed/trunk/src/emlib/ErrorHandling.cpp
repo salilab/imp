6dbb3ebc5740dc43825839f6ec6bec7515402f8d
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-06-15 18:24:29 +0000

    Fix typos; hide implementation details; conform to standards for member variable naming.
    git-svn-id: https://svn.salilab.org/imp/trunk@2495 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- embed/trunk/src/emlib/ErrorHandling.cpp
+++ embed/trunk/src/emlib/ErrorHandling.cpp
@@ -1,11 +1,13 @@
 #include "ErrorHandling.h"
 
 
-EMBED_WrongValue::EMBED_WrongValue(std::string variable,float val) {
-  var=variable;
-  value=val;
+EMBED_WrongValue::EMBED_WrongValue(std::string variable, float val)
+{
+  var_ = variable;
+  value_ = val;
 }
 
-EMBED_IOException::EMBED_IOException(std::string s) {
-  txt=s;
+EMBED_IOException::EMBED_IOException(std::string s)
+{
+  txt_ = s;
 }
