c8c4f06e1a144df9d9019ac7f872cf4c7e456c69
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-06-25 02:30:44 +0000

    Patch from Daniel, with cleanup by me: add a pair score which uses a particle refiner to refine each of the particles once before calling a passed pair score on each of the generated pairs.
    git-svn-id: https://svn.salilab.org/imp/trunk@607 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ kernel/test/particle_refiners/test_refine_once_ps.py
@@ -0,0 +1,35 @@
+import unittest
+import IMP
+import IMP.test
+
+rk = IMP.FloatKey("radius")
+
+class Test(IMP.test.TestCase):
+    """Tests for bond refiner"""
+
+
+    def test_rops(self):
+        """Make sure that bond cover coordinates are correct"""
+        IMP.set_log_level(IMP.VERBOSE)
+        m= IMP.Model()
+        pp= IMP.Particle()
+        m.add_particle(pp)
+        hpp= IMP.HierarchyDecorator.create(pp)
+        c=[]
+        for i in range(0,10):
+            p= IMP.Particle()
+            m.add_particle(p)
+            hp= IMP.HierarchyDecorator.create(p)
+            hpp.add_child(hp)
+            c.append(p)
+        pr= IMP.ChildrenParticleRefiner()
+        cps= IMP.test.ConstPairScore(1)
+        rps= IMP.RefineOncePairScore(pr, cps)
+        self.assertEqual(rps.evaluate(pp, pp, None), 100)
+
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
