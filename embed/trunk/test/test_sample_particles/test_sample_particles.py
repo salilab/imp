3e0b4ed931fd58add41de7a838aad31c49b8b749
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-07-24 18:47:30 +0000

    Correct output filename, and make sure that the EM code actually writes it out.
    git-svn-id: https://svn.salilab.org/imp/trunk@2503 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- embed/trunk/test/test_sample_particles/test_sample_particles.py
+++ embed/trunk/test/test_sample_particles/test_sample_particles.py
@@ -3,10 +3,8 @@
 
 
 import unittest
-import EM,os,sys
-
-out_filename = os.path.abspath("sample_particles/aa.em")
-
+import EM
+import os
 
 class SampleParticlesTest(unittest.TestCase):
     """test particles sampling  """
@@ -24,6 +22,8 @@ class SampleParticlesTest(unittest.TestCase):
         """ test that the sampling works """
         resolution = 2.0
         voxel_size = 1.0
+        out_filename = "aa.em"
+
         scene = EM.SampledDensityMap(self.particles_p,
                                      resolution,voxel_size)
 
@@ -31,6 +31,8 @@ class SampleParticlesTest(unittest.TestCase):
 
         erw = EM.EMReaderWriter()
         scene.Write(out_filename,erw)
+        # make sure that the output file is created
+        os.unlink(out_filename)
 
 
         # the mean and std are not correct
