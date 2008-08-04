46fc31f510b8924cb6cb1550f2c72250556fa4b8
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-08-04 18:20:27 +0000

    Add a single website contact for IMP queries.
    git-svn-id: https://svn.salilab.org/imp/trunk@642 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- website/contact.php
+++ website/contact.php
@@ -5,16 +5,9 @@
 
 <h2>Contacts</h2>
 
-<p>To learn more about IMP and how you can contribute, contact one of the
-following IMP architects:</p>
-
-<p>Ben Webb
-  <noscript><p><a href="mailto:benATsalilabDOTorg">benATsalilabDOTorg</a></p></noscript>
-  <script type="text/javascript">escramble("ben","salilab.org")</script>
-
-<p>Daniel Russel
-  <noscript><p><a href="mailto:drusselATsalilabDOTorg">drusselATsalilabDOTorg</a></p></noscript>
-  <script type="text/javascript">escramble("drussel","salilab.org")</script>
+<p>To learn more about IMP and how you can contribute, please contact us at:
+  <noscript><p><a href="mailto:impATsalilabDOTorg">impATsalilabDOTorg</a></p></noscript>
+  <script type="text/javascript">escramble("imp","salilab.org")</script>
 
 <?php
   write_footer();
