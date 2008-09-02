f06c68ad2aa7ff7356f27d0febba2a620bf0434a
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-09-02 22:51:37 +0000

    Provide pseudo builders to simplify the SConscripts for IMP modules.
    git-svn-id: https://svn.salilab.org/imp/trunk@697 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- tools/imp_module.py
+++ tools/imp_module.py
@@ -1,23 +1,13 @@
-"""Tools and Builders for IMP modules.
-
-   The IMPModuleExports Builder generates a header file used to mark classes
-   and functions for export, e.g.
-   env.IMPModuleExports('foo_exports.h', env.Value('foo'))
-   generates exports macros for the 'foo' module.
-
-   The IMPModuleVersionInfo Builder generates a source file and header to
-   return version information, e.g.
-   env.IMPModuleVersionInfo(('src/foo_version_info.cpp',
-                             'include/foo_version_info.h'),
-                            (env.Value('foo'), env.Value('Me'),
-                             env.Value('1.0')))
-   generates version information for the 'foo' module.
-"""
+"""Tools and Builders for IMP modules. See `IMPModule` for more information."""
 
 import os.path
+from SCons.Script import Builder, File
 
 def action_exports(target, source, env):
-    """Single source should be a Value, which is the name of the module."""
+    """The IMPModuleExports Builder generates a header file used to mark
+       classes and functions for export, e.g.
+       env.IMPModuleExports('foo_exports.h', env.Value('foo'))
+       generates exports macros for the 'foo' module."""
     name = source[0].get_contents()
     out = file(target[0].abspath, 'w')
     print >> out, """/*
@@ -64,7 +54,13 @@ def action_exports(target, source, env):
        'filename':os.path.basename(target[0].abspath)}
 
 def action_version_info(target, source, env):
-    """Two outputs (.cpp and .h) generated from three input Values"""
+    """The IMPModuleVersionInfo Builder generates a source file and header to
+       return version information, e.g.
+       env.IMPModuleVersionInfo(('src/foo_version_info.cpp',
+                                 'include/foo_version_info.h'),
+                                (env.Value('foo'), env.Value('Me'),
+                                 env.Value('1.0')))
+       generates version information for the 'foo' module."""
     module = source[0].get_contents()
     author = source[1].get_contents()
     version = source[2].get_contents()
@@ -106,14 +102,114 @@ extern IMP%(MODULE)sEXPORT VersionInfo %(module)s_version_info;""" \
 
     print >> h, "\n#endif  /* __IMP_%s_VERSION_INFO_H */" % module.upper()
 
+def IMPSharedLibrary(env, files, install=True):
+    """Build, and optionally also install, an IMP module's C++
+       shared library. This is only available from within an environment
+       created by `IMPSharedLibraryEnvironment`."""
+    module = env['IMP_MODULE']
+    lib = env.SharedLibrary('#/build/libs/imp_%s' % module,
+                            list(files) + [env['VER_CPP']])
+    if install:
+        libinst = env.Install(env['libdir'], lib)
+        env.Alias('%s-install' % module, [libinst])
+        return lib, libinst
+    else:
+        return lib
 
-def generate(env):
-    """Add builders and construction variables for the IMP module tool."""
-    from SCons.Builder import Builder
+def IMPSharedLibraryEnvironment(env):
+    """Create a customized environment suitable for building IMP module C++
+       shared libraries. Use the resulting object's `IMPSharedLibrary` pseudo
+       builder to actually build the shared library."""
+    from tools import get_sharedlib_environment
+    module = env['IMP_MODULE']
+    env = get_sharedlib_environment(env, 'IMP%s_EXPORTS' % module.upper(),
+                                    cplusplus=True)
+    env.AddMethod(IMPSharedLibrary)
+    return env
+
+def IMPHeaders(env, files):
+    """Install the given header files, plus any auto-generated files for this
+       IMP module."""
+    from tools.hierarchy import InstallHierarchy
+    module = env['IMP_MODULE']
+    includedir = os.path.join(env['includedir'], 'IMP')
+    inst = InstallHierarchy(env, includedir, env['IMP_MODULE'],
+                            env['IMP_MODULE_DESCRIPTION'],
+                            list(files) + [env['EXP_H'], env['VER_H']])
+    env.Alias('%s-install' % module, inst)
+    return inst
+
+def IMPPythonExtension(env, swig_interface):
+    """Build and install an IMP module's Python extension and the associated
+       wrapper file from a SWIG interface file. This is only available from
+       within an environment created by `IMPPythonExtensionEnvironment`."""
+    module = env['IMP_MODULE']
+    pyext = env.LoadableModule('#/build/libs/_IMP_%s' % module, swig_interface)
+    # .py file should also be generated:
+    gen_pymod = File('IMP_%s.py' % module)
+    env.Depends(gen_pymod, pyext)
+    # Place in libs directory:
+    pymod = env.InstallAs('#/build/libs/IMP/%s/__init__.py' % module, gen_pymod)
+
+    # Install the Python extension and module:
+    libinst = env.Install(env['pyextdir'], pyext)
+    pyinst = env.Install(os.path.join(env['pythondir'], 'IMP', module), pymod)
+    env.Alias('%s-install' % module, [libinst, pyinst])
+    return pyext, pymod
+
+def IMPPythonExtensionEnvironment(env):
+    """Create a customized environment suitable for building IMP module Python
+       extensions. Use the resulting object's `IMPPythonExtension` pseudo
+       builder to actually build the extension."""
+    from tools import get_pyext_environment
+    module = env['IMP_MODULE']
+    env = get_pyext_environment(env, 'IMP' + module.upper(), cplusplus=True)
+    env.Append(LIBS=['imp_%s' % module])
+    env.Append(SWIGPATH='#/build/include')
+    env.Append(SWIGFLAGS='-python -c++ -naturalvar')
+    env.AddMethod(IMPPythonExtension)
+    return env
+
+def IMPModule(env, module, author, version, description):
+    """Set up an IMP module. The module's SConscript gets its own
+       customized environment ('env') in which the following pseudo-builders
+       are available: IMPSharedLibraryEnvironment,
+       IMPPythonExtensionEnvironment and IMP.Headers."""
+    env = env.Clone()
     exports = Builder(action=action_exports)
     version_info = Builder(action=action_version_info)
     env.Append(BUILDERS = {'IMPModuleExports': exports,
                            'IMPModuleVersionInfo': version_info})
+
+    env['IMP_MODULE'] = module
+    env['IMP_MODULE_DESCRIPTION'] = description
+    env.Append(CPPPATH=['#/build/include', env['BOOST_CPPPATH']])
+    env.Append(LIBPATH=['#/build/libs'], LIBS=['imp'])
+    env.Help("""
+Type: 'scons %(module)s' to build and test the %(module)s extension module;
+      'scons %(module)s-test' to just test it;
+      'scons %(module)s-install' to install it.
+""" % {'module':module})
+    # Generate version information
+    env['VER_CPP'], env['VER_H'] = \
+        env.IMPModuleVersionInfo(('%s/src/%s_version_info.cpp' % (module,
+                                                                  module),
+                                  '%s/include/%s_version_info.h' % (module,
+                                                                    module)),
+                                 (env.Value(module), env.Value(author),
+                                  env.Value(version)))
+    # Generate exports header
+    env['EXP_H'] = env.IMPModuleExports('%s/include/%s_exports.h' % (module,
+                                                                     module),
+                                        env.Value(module))
+    env.AddMethod(IMPSharedLibraryEnvironment)
+    env.AddMethod(IMPPythonExtensionEnvironment)
+    env.AddMethod(IMPHeaders)
+    return env.SConscript('%s/SConscript' % module, exports='env')
+
+def generate(env):
+    """Add builders and construction variables for the IMP module tool."""
+    env.AddMethod(IMPModule)
 
 def exists(env):
     """Right now no external programs are needed"""
