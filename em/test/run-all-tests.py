24f7f21159b46b939de811f749acb19c987d3b2e
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-09-19 06:16:23 +0000

    Simplify the running of unit tests for IMP modules.
    git-svn-id: https://svn.salilab.org/imp/trunk@719 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- em/test/run-all-tests.py
+++ em/test/run-all-tests.py
@@ -1,15 +1,1 @@
-import unittest, sys, os, re
-
-def regressionTest():
-    path = os.path.abspath(os.path.dirname(sys.argv[0]))
-    files = os.listdir(path)
-    test = re.compile("^test_.*\.py$", re.IGNORECASE)
-    files = filter(test.search, files)
-    modnames = [os.path.splitext(f)[0] for f in files]
-
-    modobjs = [__import__(m) for m in modnames]
-    tests = [unittest.defaultTestLoader.loadTestsFromModule(o) for o in modobjs]
-    return unittest.TestSuite(tests)
-
-if __name__ == "__main__":
-    unittest.main(defaultTest="regressionTest")
+../../tools/run-all-tests.py
