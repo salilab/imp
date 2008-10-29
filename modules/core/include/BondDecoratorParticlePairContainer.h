537f985480f8cc1dff6a27da0038500ad131b962
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-10-29 23:45:35 +0000

    add the class
    git-svn-id: https://svn.salilab.org/imp/trunk@797 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/core/include/BondDecoratorParticlePairContainer.h
@@ -0,0 +1,37 @@
+/**
+ *  \file BondDecoratorParticlePairContainer.h
+ *  \brief A fake container for bonds
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ */
+
+#ifndef IMPCORE_BOND_DECORATOR_PARTICLE_PAIR_CONTAINER_H
+#define IMPCORE_BOND_DECORATOR_PARTICLE_PAIR_CONTAINER_H
+
+#include "core_exports.h"
+#include "ParticlePairContainer.h"
+#include "bond_decorators.h"
+#include "internal/core_version_info.h"
+
+IMPCORE_BEGIN_NAMESPACE
+
+//! A container that pretents to contain all bonds.
+/** This is to be used with a NonBondedList to exclude all bonded pairs.
+ */
+class IMPCOREEXPORT BondDecoratorParticlePairContainer :
+  public ParticlePairContainer
+{
+public:
+  BondDecoratorParticlePairContainer();
+
+  virtual ~BondDecoratorParticlePairContainer();
+
+  IMP_PARTICLE_PAIR_CONTAINER(internal::core_version_info)
+
+  virtual void show(std::ostream &out = std::cout) const;
+};
+
+
+IMPCORE_END_NAMESPACE
+
+#endif  /* IMPCORE_BOND_DECORATOR_PARTICLE_PAIR_CONTAINER_H */
---
+++ modules/core/src/BondDecoratorParticlePairContainer.cpp
@@ -0,0 +1,50 @@
+/**
+ *  \file BondDecoratorParticlePairContainer.cpp
+ *  \brief A fake container that returns true if a pair of particles are bonded
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ *
+ */
+
+#include "IMP/core/BondDecoratorParticlePairContainer.h"
+
+IMPCORE_BEGIN_NAMESPACE
+
+BondDecoratorParticlePairContainer
+::BondDecoratorParticlePairContainer(){
+}
+
+BondDecoratorParticlePairContainer::~BondDecoratorParticlePairContainer(){}
+
+bool BondDecoratorParticlePairContainer
+::get_contains_particle_pair(ParticlePair pp) const {
+  if (!BondedDecorator::is_instance_of(pp.first)
+      || ! BondedDecorator::is_instance_of(pp.second)) {
+    return false;
+  }
+
+  BondedDecorator ba(pp.first);
+  BondedDecorator bb(pp.second);
+  BondDecorator bd=get_bond(ba, bb);
+  std::cout << bd << std::endl;
+  return bd != BondDecorator();
+}
+
+unsigned int BondDecoratorParticlePairContainer
+::get_number_of_particle_pairs() const {
+  return 0;
+}
+
+ParticlePair BondDecoratorParticlePairContainer
+::get_particle_pair(unsigned int i) const {
+  throw InvalidStateException("BondDecoratorParticlePairContainer does" \
+                              " not contain any pairs");
+  return ParticlePair();
+}
+
+
+void BondDecoratorParticlePairContainer::show(std::ostream &out) const {
+  out << "BondDecoratorParticlePairContainer" << std::endl;
+}
+
+IMPCORE_END_NAMESPACE
