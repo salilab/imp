64728020056f45455be87ba2143f7e6f87c13285
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-10-29 18:51:18 +0000

    added container classes and associated restraints. The nonbonded lists will eventually use this. These will also replace NonbondedRestraint, BondedRestraint, List*Restraint BondedList*
    git-svn-id: https://svn.salilab.org/imp/trunk@790 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/core/include/internal/IndexingIterator.h
@@ -0,0 +1,118 @@
+/**
+ *  \file IndexingIterator.h   \brief A class to represent a voxel grid.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ *
+ */
+
+#ifndef IMPCORE_INDEXING_ITERATOR_H
+#define IMPCORE_INDEXING_ITERATOR_H
+
+#include "../macros.h"
+
+#include <IMP/RefCountedObject.h>
+
+#include <iterator>
+#include <limits>
+
+IMPCORE_BEGIN_INTERNAL_NAMESPACE
+
+template <class Accessor>
+class IndexingIterator {
+  Accessor a_;
+  unsigned int i_;
+  typedef IndexingIterator<Accessor> This;
+  bool is_default() const {
+    return i_!= std::numeric_limits<unsigned int>::max();
+  }
+public:
+  typedef const typename Accessor::result_type  value_type;
+  typedef unsigned int difference_type;
+  typedef const typename Accessor::result_type reference;
+  typedef const typename Accessor::result_type* pointer;
+  typedef std::random_access_iterator_tag iterator_category;
+
+  IndexingIterator(): i_(std::numeric_limits<unsigned int>::max()){}
+  IndexingIterator(Accessor a): a_(a), i_(0){}
+  IndexingIterator(Accessor a,
+                   unsigned int i): a_(a), i_(i){}
+
+  // check which is which
+  const This& operator++() {
+    ++i_;
+    return *this;
+  }
+
+  // check which is which
+  This operator++(int) {
+    This o= *this;
+    ++i_;
+    return o;
+  }
+
+  This operator+(unsigned int i) const {
+    return This(a_, i_+i);
+  }
+
+  unsigned int operator-(const This &o) const {
+    IMP_assert(a_== o.a_, "Don't subtract iterators from different containers");
+    return i_- o.i_;
+  }
+
+  bool operator==(const This &o) const {
+    IMP_assert(a_ == o.a_,
+               "Can only compare iterators from the same container");
+    return (i_== o.i_);
+  }
+
+  bool operator!=(const This &o) const {
+    return (i_!= o.i_);
+  }
+
+  bool operator<(const This &o) const {
+    IMP_assert(a_ == o.a_,
+               "Can only compare iterators from the same container");
+    IMP_assert(!is_default() && !o.is_default(),
+               "Ordering with uninitialized index is undefined");
+    return (i_< o.i_);
+  }
+
+  bool operator>(const This &o) const {
+    IMP_assert(a_ == o.a_,
+               "Can only compare iterators from the same container");
+    IMP_assert(!is_default() && !o.is_default(),
+               "Ordering with uninitialized index is undefined");
+    return (i_> o.i_);
+  }
+
+  bool operator>=(const This &o) const {
+    IMP_assert(a_ == o.a_,
+               "Can only compare iterators from the same container");
+    IMP_assert(!is_default() && !o.is_default(),
+               "Ordering with uninitialized index is undefined");
+    return (i_>= o.i_);
+  }
+
+  bool operator<=(const This &o) const {
+    IMP_assert(a_ == o.a_,
+               "Can only compare iterators from the same container");
+    IMP_assert(!is_default() && !o.is_default(),
+               "Ordering with uninitialized index is undefined");
+    return (i_<= o.i_);
+  }
+
+  reference operator*() const {
+    return a_(i_);
+  }
+
+  pointer operator->() const {
+    static value_type vt;
+    vt= a_(i_);
+    return &vt;
+  }
+};
+
+
+IMPCORE_END_INTERNAL_NAMESPACE
+
+#endif  /* IMPCORE_INDEXING_ITERATOR_H */
