dc6cc216fd307e9b5c212607cd558c1587e9cf44
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-10-30 23:30:37 +0000

    forgot to commit the test for the cgal finder
    git-svn-id: https://svn.salilab.org/imp/trunk@820 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/test/states/test_close_pair_finders.py
+++ modules/core/test/states/test_close_pair_finders.py
@@ -15,9 +15,10 @@ class TestBL(IMP.test.TestCase):
         return d-r0-r1
 
     def do_test_one(self, cpf):
-        IMP.set_log_level(IMP.VERBOSE)
+        IMP.set_log_level(IMP.TERSE)
         dist= random.uniform(0,2)
         cpf.set_distance(dist)
+        print 'Distance is ' + str(dist)
         rk= IMP.FloatKey("boo")
         cpf.set_radius_key(rk)
         m=IMP.Model()
@@ -55,18 +56,27 @@ class TestBL(IMP.test.TestCase):
         out= IMP.core.FilteredListParticlePairContainer()
 
         cpf.add_close_pairs(pc, pc2, out)
+        print "done bipartite " + str(out.get_number_of_particle_pairs())
         for i in range(0, len(ps)):
             for j in range(0,len(ps2)):
+                print "testing " + str(ps[i].get_index().get_index()) + " " + str(ps2[j].get_index().get_index())
                 d= self.get_distance(rk, ps[i], ps2[j])
+                print d
                 if d <= dist:
-                    self.assert_(out.get_contains_particle_pair(IMP.ParticlePair(ps[0],
-                                                                                 ps2[1])))
+                    self.assert_(out.get_contains_particle_pair(IMP.ParticlePair(ps[i],
+                                                                                 ps2[j])))
         print "done with bipartite test"
 
 
     def test_quadratic(self):
         """Testing QuadraticClosePairsFinder"""
         self.do_test_one(IMP.core.QuadraticClosePairsFinder())
+
+    def test_quadratic(self):
+        """Testing BoxSweepClosePairsFinder"""
+        if IMP.core.BoxSweepClosePairsFinder.get_is_implemented():
+            self.do_test_one(IMP.core.BoxSweepClosePairsFinder())
+
 
 if __name__ == '__main__':
     unittest.main()
