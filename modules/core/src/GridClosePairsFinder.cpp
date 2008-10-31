ad887b70df77247ba977be220e8152674289d30b
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-10-31 04:46:04 +0000

    add the grid adjacency finder
    git-svn-id: https://svn.salilab.org/imp/trunk@832 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/core/src/GridClosePairsFinder.cpp
@@ -0,0 +1,196 @@
+/**
+ *  \file GridClosePairsFinder.cpp
+ *  \brief Test all pairs.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ *
+ */
+
+#include "IMP/core/GridClosePairsFinder.h"
+#include "IMP/core/QuadraticClosePairsFinder.h"
+#include "IMP/core/XYZDecorator.h"
+
+#include "IMP/core/internal/ParticleGrid.h"
+#include <IMP/internal/Vector.h>
+
+IMPCORE_BEGIN_NAMESPACE
+
+namespace {
+  typedef IMP::internal::Vector<internal::ParticleGrid*> GVector;
+
+  static const unsigned int min_grid_size=100;
+
+  struct AddToList {
+    FilteredListParticlePairContainer *out_;
+    Particle *p_;
+    AddToList(FilteredListParticlePairContainer *o,
+              Particle *p): out_(o), p_(p){}
+    void operator()(Particle *p) {
+      out_->add_particle_pair(ParticlePair(p_, p));
+    }
+  };
+
+  struct AddToList2 {
+    FilteredListParticlePairContainer *out_;
+    AddToList2(FilteredListParticlePairContainer *o): out_(o){}
+    void operator()(Particle *a, Particle *b) {
+      out_->add_particle_pair(ParticlePair(a, b));
+    }
+  };
+
+
+
+  float grid_side_from_r(float r)
+  {
+    if (r==0) return 1;
+    else return r*1.6;
+  }
+
+  void grid_partition_points(ParticleContainer *c,
+                             FloatKey rk,
+                             GVector &bins)
+  {
+    if (c->get_number_of_particles() ==0) return;
+
+    float minr=std::numeric_limits<float>::max(), maxr=0;
+    for (unsigned int i=0; i< c->get_number_of_particles(); ++i) {
+      float r= c->get_particle(i)->get_value(rk);
+      if ( r > maxr) maxr=r;
+      if ( r > 0 && r < minr) minr=r;
+    }
+    minr= std::min(maxr, minr);
+    float curr=minr*2;
+    Floats cuts;
+    cuts.push_back(0);
+    do {
+      cuts.push_back(curr);
+      curr *= 2;
+    } while (curr < maxr);
+    cuts.push_back(2*maxr);
+
+    std::vector<Particles> ops(cuts.size());
+    for (unsigned int i=0; i< c->get_number_of_particles(); ++i) {
+      float r= c->get_particle(i)->get_value(rk);
+      bool found=false;
+      for (unsigned int j=0; ; ++j) {
+      IMP_assert(j< cuts.size(), "Internal error in ASNBLSS");
+      if (cuts[j] >= r) {
+        ops[j].push_back(c->get_particle(i));
+        found=true;
+        break;
+      }
+    }
+    IMP_assert(found, "Didn't put particle anywhere");
+  }
+  // consolidate
+  for (unsigned int i=1; i< ops.size(); ++i) {
+    if (ops[i-1].size() + ops[i].size() < min_grid_size) {
+      ops[i].insert(ops[i].end(), ops[i-1].begin(), ops[i-1].end());
+      ops[i-1].clear();
+    }
+  }
+  for (unsigned int i=0; i< cuts.size(); ++i) {
+    if (ops[i].empty()) continue;
+    float rmax=0;
+    for (unsigned int j=0; j< ops[i].size(); ++j) {
+      rmax= std::max(rmax, ops[i][j]->get_value(rk));
+    }
+    bins.push_back(new internal::ParticleGrid(grid_side_from_r(rmax), ops[i]));
+  }
+  IMP_LOG(VERBOSE, "Created " << bins.size() << " grids" << std::endl);
+  for (unsigned int i=0; i< bins.size(); ++i) {
+    IMP_LOG(VERBOSE, *bins[i] << std::endl);
+  }
+}
+
+
+void grid_generate_nbl(const internal::ParticleGrid *particle_bin,
+                       const internal::ParticleGrid *grid_bin,
+                       Float distance,
+                       FloatKey rk,
+                       FilteredListParticlePairContainer *out)
+{
+  IMP_CHECK_OBJECT(particle_bin);
+  IMP_CHECK_OBJECT(grid_bin);
+  IMP_LOG(VERBOSE, "Generate nbl for pair " << std::endl);
+  for (internal::ParticleGrid::ParticleVoxelIterator
+         it= particle_bin->particle_voxels_begin();
+       it != particle_bin->particle_voxels_end(); ++it) {
+    Particle *p= it->first;
+    AddToList f(out, p);
+    XYZDecorator d(p);
+    internal::ParticleGrid::VirtualIndex index
+      = grid_bin->get_virtual_index(d.get_coordinates());
+    IMP_LOG(VERBOSE, "Searching for " << p->get_index()
+            << " from " << index << std::endl);
+    grid_bin->apply_to_nearby(f, index,
+                              distance,
+                              false);
+  }
+
+}
+}
+
+GridClosePairsFinder::GridClosePairsFinder(){}
+
+GridClosePairsFinder::~GridClosePairsFinder(){}
+
+void GridClosePairsFinder
+::add_close_pairs(ParticleContainer *ca,
+                  ParticleContainer *cb,
+                  Float distance,
+                  FloatKey radius_key,
+                  FilteredListParticlePairContainer *out) const {
+  QuadraticClosePairsFinder qp;
+  qp.add_close_pairs(ca, cb, distance, radius_key, out);
+}
+
+void GridClosePairsFinder
+::add_close_pairs(ParticleContainer *c,
+                  Float distance,
+                  FloatKey radius_key,
+                  FilteredListParticlePairContainer *out) const {
+  IMP_LOG(TERSE, "Rebuilding NBL with Grid and cutoff "
+          << distance << std::endl );
+  IMP::internal::Vector<internal::ParticleGrid*> bins;
+
+  if (radius_key== FloatKey()) {
+    Particles ps(c->particles_begin(), c->particles_end());
+    /** \todo need to pick better value here.
+     */
+    bins.push_back(new internal::ParticleGrid(1.0, ps));
+
+  } else {
+    grid_partition_points(c, radius_key, bins);
+  }
+  for (unsigned int i=0; i< bins.size(); ++i) {
+    for (unsigned int j=i+1; j< bins.size(); ++j) {
+      grid_generate_nbl(bins[i], bins[j], distance, radius_key, out);
+    }
+
+    internal::ParticleGrid::Index last_index;
+    for (internal::ParticleGrid::ParticleVoxelIterator it
+           = bins[i]->particle_voxels_begin();
+         it != bins[i]->particle_voxels_end(); ++it) {
+      IMP_LOG(VERBOSE, "Searching with particle " << it->first->get_index()
+              << std::endl);
+      AddToList f(out, it->first);
+      bins[i]->apply_to_nearby(f, it->second,
+                               distance,
+                               true);
+
+      if (it->second != last_index) {
+        AddToList2 fp(out);
+        IMP_LOG(VERBOSE, "Searching in " << it->second
+                << std::endl);
+        bins[i]->apply_to_cell_pairs(fp, it->second);
+      }
+      last_index=it->second;
+    }
+  }
+}
+
+
+
+
+IMPCORE_END_NAMESPACE
--- modules/core/test/states/test_close_pair_finders.py
+++ modules/core/test/states/test_close_pair_finders.py
@@ -4,7 +4,11 @@ import IMP.core
 import os.path
 import random
 
-class TestBL(IMP.test.TestCase):
+class TestCPFL(IMP.test.TestCase):
+    def setUp(self):
+        IMP.test.TestCase.setUp(self)
+        IMP.set_log_level(IMP.TERSE)
+
     def get_distance(self, rk, p0, p1):
         r0=0
         r1=0
@@ -22,10 +26,9 @@ class TestBL(IMP.test.TestCase):
         rk= IMP.FloatKey("boo")
         #cpf.set_radius_key(rk)
         m=IMP.Model()
-        ps= self.create_particles_in_box(m, 30)
+        ps= self.create_particles_in_box(m, 200)
         for i in range(0, len(ps)):
-            if i%3 == 0: continue
-            ps[i].add_attribute(rk, random.uniform(0,2))
+            ps[i].add_attribute(rk, random.uniform(0,1))
         pc= IMP.core.ListParticleContainer(ps)
         pc.show()
         out= IMP.core.FilteredListParticlePairContainer()
@@ -39,18 +42,17 @@ class TestBL(IMP.test.TestCase):
                 d= self.get_distance(rk, ps[i], ps[j])
                 #d=1000
                 if d <= dist:
-                    print "searching for "+str(ps[i].get_index().get_index()) + " "\
-                        + str(ps[j].get_index().get_index())
+                    #print "searching for "+str(ps[i].get_index().get_index()) + " "\
+                    #    + str(ps[j].get_index().get_index())
                     #XYZDecorator(ps[
                     self.assert_(out.get_contains_particle_pair(IMP.ParticlePair(ps[i],
                                                                                  ps[j]))
                                  or
                                  out.get_contains_particle_pair(IMP.ParticlePair(ps[j],
                                                                                  ps[i])))
-        print "Done with all test"
-        ps2= self.create_particles_in_box(m, 30)
+        print "Done with all test "+str(out.get_number_of_particle_pairs())
+        ps2= self.create_particles_in_box(m, 200)
         for i in range(0, len(ps2)):
-            if i%3 == 0: continue
             ps2[i].add_attribute(rk, random.uniform(0,2))
         pc2= IMP.core.ListParticleContainer(ps2)
         out= IMP.core.FilteredListParticlePairContainer()
@@ -59,23 +61,26 @@ class TestBL(IMP.test.TestCase):
         print "done bipartite " + str(out.get_number_of_particle_pairs())
         for i in range(0, len(ps)):
             for j in range(0,len(ps2)):
-                print "testing " + str(ps[i].get_index().get_index()) + " " + str(ps2[j].get_index().get_index())
+                #print "testing " + str(ps[i].get_index().get_index()) + " " + str(ps2[j].get_index().get_index())
                 d= self.get_distance(rk, ps[i], ps2[j])
-                print d
+                #print d
                 if d <= dist:
                     self.assert_(out.get_contains_particle_pair(IMP.ParticlePair(ps[i],
                                                                                  ps2[j])))
         print "done with bipartite test"
-
-
     def test_quadratic(self):
         """Testing QuadraticClosePairsFinder"""
+        print "quadratic"
         self.do_test_one(IMP.core.QuadraticClosePairsFinder())
-
-    def test_quadratic(self):
+    def test_sweep(self):
         """Testing BoxSweepClosePairsFinder"""
         if IMP.core.BoxSweepClosePairsFinder.get_is_implemented():
+            print "have cgal"
             self.do_test_one(IMP.core.BoxSweepClosePairsFinder())
+    def test_grid(self):
+        """Testing GridClosePairsFinder"""
+        print "grid"
+        self.do_test_one(IMP.core.GridClosePairsFinder())
 
 
 if __name__ == '__main__':
