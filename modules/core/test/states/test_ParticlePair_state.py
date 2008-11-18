59a29f68f61bd4e1c6cba7ca84a57ea102b34a89
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-11-18 15:52:22 +0000

    oops, I had left debugging code in there
    git-svn-id: https://svn.salilab.org/imp/trunk@870 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/test/states/test_ParticlePair_state.py
+++ modules/core/test/states/test_ParticlePair_state.py
@@ -119,13 +119,7 @@ class ParticlePairContainerTest(IMP.test.TestCase):
         k= IMP.IntKey("thevalue")
         f= PairTestModifier(k)
         print "apply"
-        p= IMP.Particle()
-        m.add_particle(p)
-        f.apply(p)
         s= IMP.core.PairsScoreState(f, c)
-        p= IMP.Particle()
-        m.add_particle(p)
-        f.apply(p)
         self.assert_( not f.thisown)
         self.assert_( not c.thisown)
         m.add_score_state(s)
