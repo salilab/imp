f0794d4e7745d04324095a8b99df1e4bc6db7a7b
sjkim@SALILAB.ORG <sjkim@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-12-02 02:16:30 +0000

    initial setup files
    git-svn-id: https://svn.salilab.org/imp/trunk@914 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/saxs/include/saxs.h
@@ -0,0 +1,11 @@
+/**
+ *  \file saxs.h
+ *  \brief saxs initial header file.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ */
+
+#ifndef IMPSAXS_SAXS_H
+#define IMPSAXS_SAXS_H
+
+#endif  /* IMPSAXS_SAXS_H */
