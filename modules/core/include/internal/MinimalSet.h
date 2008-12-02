de5de5db793d9a35e9494ab0c556825c2ce577ab
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-12-02 01:21:53 +0000

    handle custom comparator
    git-svn-id: https://svn.salilab.org/imp/trunk@910 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/include/internal/MinimalSet.h
+++ modules/core/include/internal/MinimalSet.h
@@ -19,7 +19,7 @@ IMPCORE_BEGIN_INTERNAL_NAMESPACE
 
 /** Store the lowest n items seen so far.
  */
-template <class Score, class Data>
+template <class Score, class Data, class Less=std::less<Score> >
 class MinimalSet {
   unsigned int n_;
   typedef std::pair<Score, Data> MP;
@@ -27,18 +27,21 @@ class MinimalSet {
   Vector data_;
 
   struct CompareFirst {
+    Less l_;
     template <class T>
     bool operator()(const T &a, const T &b) const {
-      return a.first < b.first;
+      return l_(a.first, b.first);
     }
   };
+  CompareFirst cf_;
+  Less l_;
 
 public:
   MinimalSet(unsigned int n): n_(n){}
 
   bool can_insert(Score s) const {
     if (data_.size() < n_) return true;
-    else return data_.back().first > s;
+    else return l_(s, data_.back().first);
   }
 
   void insert(Score s, const Data &d) {
@@ -58,9 +61,15 @@ public:
   }
 
   const MP &operator[](unsigned int i) const {
+    IMP_check(i < data_.size(), "Index out of range", IndexException);
     return data_[i];
   }
 
+  void show(std::ostream &out= std::cout) const {
+    for (unsigned int i=0; i<n_; ++i) {
+      out << data_[i].first << " ";
+    }
+  }
 };
 
 IMPCORE_END_INTERNAL_NAMESPACE
