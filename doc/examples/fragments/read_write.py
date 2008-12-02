44069c4362b05ab8b9c09c004f595c03c2c0fdbf
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-12-02 23:38:04 +0000

    rearrange examples and add descriptions. The examples will be built by a script into doxygen pages once I can check things in.
    git-svn-id: https://svn.salilab.org/imp/trunk@924 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ doc/examples/fragments/read_write.py
@@ -0,0 +1,2 @@
+IMP.core.write(my_imp_model, "somefile")
+IMP.core.read("somefile", my_imp_model)
---
+++ doc/examples/simple_examples/molecular_hierarchy.py
@@ -0,0 +1,31 @@
+import IMP
+import IMP.core
+import IMP.modeller
+
+m = IMP.Model()
+mp0= IMP.modeller.read_pdb('doc/examples/simple_examples/single_protein.pdb', m)
+# get the 16th residue
+r16 = IMP.core.molecular_hierarchy_get_residue(mp0, 16)
+r16.show()
+
+atoms= IMP.core.molecular_hierarchy_get_by_type(mp0, IMP.core.MolecularHierarchyDecorator.ATOM)
+print len(atoms)
+
+#get the residues
+residues= IMP.core.molecular_hierarchy_get_by_type(mp0, IMP.core.MolecularHierarchyDecorator.RESIDUE)
+# set the coordinates and radius of each residue to enclose its atoms
+for r in residues:
+    d= IMP.core.XYZRDecorator.create(r)
+    atoms= IMP.core.molecular_hierarchy_get_by_type(IMP.core.MolecularHierarchyDecorator(r),
+                                                    IMP.core.MolecularHierarchyDecorator.ATOM)
+    IMP.core.set_enclosing_sphere(atoms, d)
+
+# load another copy
+mp1= IMP.modeller.read_pdb('doc/examples/simple_examples/single_protein.pdb', m)
+
+p = IMP.Particle()
+m.add_particle(p)
+rmp= IMP.core.MolecularHierarchyDecorator.create(p)
+rmp.set_type(IMP.core.MolecularHierarchyDecorator.ASSEMBLY)
+rmp.add_child(mp0)
+rmp.add_child(mp1)
