2ab9eb758937f1d6491ccb89807b8ed34cd6a3b0
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-12-03 22:22:31 +0000

    add an inverse method and test it. Change the verb method to rotate to be in line with translate and make sure there is a strong definition in IMP.core
    git-svn-id: https://svn.salilab.org/imp/trunk@943 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/core/test/transformation/test_rotation.py
@@ -0,0 +1,32 @@
+import unittest
+import IMP
+import IMP.utils
+import IMP.test
+import IMP.core
+import random
+
+class RigidTransformationTests(IMP.test.TestCase):
+    """Test particles"""
+
+    def test_rotation(self):
+        """Check that the rotation invese is an inverse"""
+        axis= IMP.random_vector_on_unit_sphere()
+        m= random.uniform(-1,1)
+        mag= m*m+ axis*axis
+        mag = mag**(.5)
+        axis/= mag
+        m/=mag
+        r= IMP.core.Rotation3D(m, axis[0], axis[1], axis[2])
+        ri= r.get_inverse()
+        v= IMP.random_vector_in_unit_box()
+        vt= r.rotate(v)
+        vti= ri.rotate(vt)
+        v.show()
+        vti.show()
+        vt.show()
+        self.assertInTolerance(vti[0], v[0], .1)
+        self.assertInTolerance(vti[1], v[1], .1)
+        self.assertInTolerance(vti[2], v[2], .1)
+
+if __name__ == '__main__':
+    unittest.main()
