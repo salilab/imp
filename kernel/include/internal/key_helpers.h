485ee59dcaf792f60d7411f0e1c529ecb63ddb4d
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-12-05 01:48:09 +0000

    Patch from Daniel, with minor cleanup by me: Move code in internal namespaces to internal headers (or into the IMP namespace as appropriate). The point is to make doxygen docs cleaner.
    git-svn-id: https://svn.salilab.org/imp/trunk@974 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ kernel/include/internal/key_helpers.h
@@ -0,0 +1,45 @@
+/**
+ *  \file key_helpers.h    \brief helpers for declaring keys.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ *
+ */
+
+#ifndef IMP_INTERNAL_KEY_HELPERS_H
+#define IMP_INTERNAL_KEY_HELPERS_H
+
+#include "../IMP_config.h"
+#include <map>
+
+IMP_BEGIN_INTERNAL_NAMESPACE
+/** \internal The data concerning keys.
+  */
+struct IMPDLLEXPORT KeyData
+{
+  typedef std::map<std::string, int> Map;
+  typedef std::vector<std::string> RMap;
+
+  void show(std::ostream &out= std::cout) const;
+  KeyData();
+  void assert_is_initialized() const;
+  unsigned int add_key(std::string str) {
+    unsigned int i= map_.size();
+    map_[str]=i;
+    rmap_.push_back(str);
+    return i;
+  }
+
+  const Map &get_map() const {return map_;}
+  const RMap &get_rmap() const {return rmap_;}
+
+private:
+  double heuristic_;
+  Map map_;
+  RMap rmap_;
+};
+
+IMPDLLEXPORT extern std::map<unsigned int, KeyData> key_data;
+
+IMP_END_INTERNAL_NAMESPACE
+
+#endif  /* IMP_INTERNAL_KEY_HELPERS_H */
