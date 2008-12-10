28795fafb3b32ed1e2dacb6ab2bba0709983c071
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-12-10 00:08:31 +0000

    add general mechanism to move derivatives to and from refined particles
    git-svn-id: https://svn.salilab.org/imp/trunk@994 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/core/include/DerivativesFromRefinedSingletonModifier.h
@@ -0,0 +1,44 @@
+/**
+ *  \file DerivativesFromRefinedSingletonModifier.h
+ *  \brief Accumulate the derivatives of the refined particles
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ */
+
+#ifndef IMPCORE_DERIVATIVES_FROM_REFINED_SINGLETON_MODIFIER_H
+#define IMPCORE_DERIVATIVES_FROM_REFINED_SINGLETON_MODIFIER_H
+
+#include "core_exports.h"
+#include "internal/core_version_info.h"
+
+#include "SingletonModifier.h"
+#include "XYZDecorator.h"
+#include <IMP/ParticleRefiner.h>
+#include <IMP/Pointer.h>
+
+IMPCORE_BEGIN_NAMESPACE
+
+//! Accumulate the derivatives of the refined particles.
+/** \relates DerivatvesToRefinedSingletonModifier
+ An example showing a how to use such a score state to maintain a cover
+ of the atoms of a protein by a sphere per residue.
+ \verbinclude simple_examples/cover_particles.py
+ */
+class IMPCOREEXPORT DerivativesFromRefinedSingletonModifier:
+public SingletonModifier
+{
+  Pointer<ParticleRefiner> r_;
+  FloatKeys ks_;
+public:
+  //! Copy ks from the particles returned by r.
+  DerivativesFromRefinedSingletonModifier(ParticleRefiner *r,
+                                          FloatKeys ks
+                                           = XYZDecorator::get_xyz_keys());
+  ~DerivativesFromRefinedSingletonModifier();
+
+  IMP_SINGLETON_MODIFIER(internal::core_version_info);
+};
+
+IMPCORE_END_NAMESPACE
+
+#endif  /* IMPCORE_DERIVATIVES_FROM_REFINED_SINGLETON_MODIFIER_H */
