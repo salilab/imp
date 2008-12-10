5e0e3aa3056a87835da5d714d7cc09ed090ce0a8
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-12-10 00:06:49 +0000

    add class to compute the centroid of a set of a refinable particle
    git-svn-id: https://svn.salilab.org/imp/trunk@992 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/core/include/CentroidOfRefinedSingletonModifier.h
@@ -0,0 +1,45 @@
+/**
+ *  \file CentroidOfRefinedSingletonModifier.h
+ *  \brief Set the coordinates of the particle to be the centoid of the
+ *  refined particles.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ */
+
+#ifndef IMPCORE_CENTROID_OF_REFINED_SINGLETON_MODIFIER_H
+#define IMPCORE_CENTROID_OF_REFINED_SINGLETON_MODIFIER_H
+
+#include "core_exports.h"
+#include "internal/core_version_info.h"
+
+#include "SingletonModifier.h"
+#include "XYZDecorator.h"
+#include <IMP/ParticleRefiner.h>
+#include <IMP/Pointer.h>
+
+IMPCORE_BEGIN_NAMESPACE
+
+//! Set the coordinates of the particle to be the centoid of the particles.
+/** An exception is thrown if there are no refined particles. The weight
+    FloatKey can be FloatKey and then all the weights will be equal.
+ */
+class IMPCOREEXPORT CentroidOfRefinedSingletonModifier:
+public SingletonModifier
+{
+  Pointer<ParticleRefiner> r_;
+  FloatKeys ks_;
+  FloatKey w_;
+public:
+  //! Set the keys ks to be the average of the refined particles.
+  CentroidOfRefinedSingletonModifier(ParticleRefiner *r,
+                                     FloatKey weight=FloatKey(),
+                                     FloatKeys ks
+                                      = XYZDecorator::get_xyz_keys());
+  ~CentroidOfRefinedSingletonModifier();
+
+  IMP_SINGLETON_MODIFIER(internal::core_version_info);
+};
+
+IMPCORE_END_NAMESPACE
+
+#endif  /* IMPCORE_CENTROID_OF_REFINED_SINGLETON_MODIFIER_H */
---
+++ modules/core/src/CentroidOfRefinedSingletonModifier.cpp
@@ -0,0 +1,61 @@
+/**
+ *  \file CentroidOfRefinedSingletonModifier.cpp
+ *  \brief CentroidOf a the refined particles with a sphere.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ */
+
+#include "IMP/core/CentroidOfRefinedSingletonModifier.h"
+
+#include "IMP/core/BondDecoratorListScoreState.h"
+#include "IMP/core/bond_decorators.h"
+#include "IMP/core/XYZRDecorator.h"
+
+IMPCORE_BEGIN_NAMESPACE
+
+CentroidOfRefinedSingletonModifier
+::CentroidOfRefinedSingletonModifier(ParticleRefiner *r,
+                                     FloatKey weight,
+                                     FloatKeys ks): r_(r),
+ks_(ks), w_(weight)
+{
+}
+
+CentroidOfRefinedSingletonModifier::~CentroidOfRefinedSingletonModifier()
+{
+}
+
+void CentroidOfRefinedSingletonModifier::apply(Particle *p)
+{
+  Particles ps = r_->get_refined(p);
+  unsigned int n= ps.size();
+  Float tw=0;
+  if (w_ != FloatKey()) {
+    for (unsigned int i=0; i< n; ++i) {
+      tw+= ps[i]->get_value(w_);
+    }
+  } else {
+    tw=1;
+  }
+  for (unsigned int j=0; j< ks_.size(); ++j) {
+    Float v=0;
+    for (unsigned int i=0; i< n; ++i) {
+      Float w;
+      if (w_ != FloatKey()) {
+        w= ps[i]->get_value(w_)/tw;
+      } else {
+        w= Float(1.0)/n;
+      }
+      v += ps[i]->get_value(ks_[j])*w;
+    }
+    p->set_value(ks_[j], v);
+  }
+  r_->cleanup_refined(p, ps);
+}
+
+void CentroidOfRefinedSingletonModifier::show(std::ostream &out) const
+{
+  out << "CentroidOfRefinedSingletonModifier" << std::endl;
+}
+
+IMPCORE_END_NAMESPACE
