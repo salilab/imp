bd2bde6213411e6a2a9311ebffc41d6ba81c97ff
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-12-10 00:09:12 +0000

    add a refiner to get the endpoints of a bond
    git-svn-id: https://svn.salilab.org/imp/trunk@995 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/core/include/BondEndpointsParticleRefiner.h
@@ -0,0 +1,34 @@
+/**
+ *  \file BondEndpointsParticleRefiner.h
+ *  \brief Return the endpoints of a bond.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ */
+
+#ifndef IMPCORE_BOND_ENDPOINTS_PARTICLE_REFINER_H
+#define IMPCORE_BOND_ENDPOINTS_PARTICLE_REFINER_H
+
+#include "core_exports.h"
+#include "internal/core_version_info.h"
+
+#include <IMP/ParticleRefiner.h>
+
+IMPCORE_BEGIN_NAMESPACE
+
+//! Return the endpoints of a bond.
+/**
+ \ingroup hierarchy
+ */
+class IMPCOREEXPORT BondEndpointsParticleRefiner : public ParticleRefiner
+{
+public:
+  BondEndpointsParticleRefiner();
+
+  virtual ~BondEndpointsParticleRefiner() {}
+
+  IMP_PARTICLE_REFINER(internal::core_version_info);
+};
+
+IMPCORE_END_NAMESPACE
+
+#endif  /* IMPCORE_BOND_ENDPOINTS_PARTICLE_REFINER_H */
