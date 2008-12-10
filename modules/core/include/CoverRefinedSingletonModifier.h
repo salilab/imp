04857ab86e47941183df7ee869ad64505df88574
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-12-10 00:07:12 +0000

    cover a set of refined particles in a sphere
    git-svn-id: https://svn.salilab.org/imp/trunk@993 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/core/include/CoverRefinedSingletonModifier.h
@@ -0,0 +1,57 @@
+/**
+ *  \file CoverRefinedSingletonModifier.h
+ *  \brief Cover a bond with a sphere.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ */
+
+#ifndef IMPCORE_COVER_REFINED_SINGLETON_MODIFIER_H
+#define IMPCORE_COVER_REFINED_SINGLETON_MODIFIER_H
+
+#include "core_exports.h"
+#include "internal/core_version_info.h"
+
+#include "SingletonModifier.h"
+#include "SingletonContainer.h"
+#include "XYZRDecorator.h"
+#include <IMP/ParticleRefiner.h>
+#include <IMP/Pointer.h>
+
+IMPCORE_BEGIN_NAMESPACE
+
+/** \brief This class sets the position and radius of each particle to
+ enclose the refined.
+
+ Set the coordinates and radius of the passed particle to cover the particles
+ listed by the particle refiner.
+ \relates DerivatvesFromRefinedSingletonModifier
+ An example showing a how to use such a score state to maintain a cover
+ of the atoms of a protein by a sphere per residue.
+ \verbinclude simple_examples/cover_particles.py
+
+ \note The particle passed must be both an XYZRDecorator.
+ \note This used the set_enclosing_sphere function and so produces
+  better results if the CGAL library is found.
+ */
+class IMPCOREEXPORT CoverRefinedSingletonModifier: public SingletonModifier
+{
+  Pointer<ParticleRefiner> ref_;
+  FloatKey rk_;
+  Float slack_;
+public:
+  CoverRefinedSingletonModifier(ParticleRefiner *ref,
+                                FloatKey rk
+                                =XYZRDecorator::get_default_radius_key(),
+                                Float slack=0);
+  ~CoverRefinedSingletonModifier();
+
+  IMP_SINGLETON_MODIFIER(internal::core_version_info);
+
+  void set_slack(Float slack) {
+    slack_=slack;
+  }
+};
+
+IMPCORE_END_NAMESPACE
+
+#endif  /* IMPCORE_COVER_REFINED_SINGLETON_MODIFIER_H */
---
+++ modules/core/src/CoverRefinedSingletonModifier.cpp
@@ -0,0 +1,45 @@
+/**
+ *  \file CoverRefinedSingletonModifier.cpp
+ *  \brief Cover a the refined particles with a sphere.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ */
+
+#include "IMP/core/CoverRefinedSingletonModifier.h"
+
+#include "IMP/core/BondDecoratorListScoreState.h"
+#include "IMP/core/bond_decorators.h"
+#include "IMP/core/XYZRDecorator.h"
+
+IMPCORE_BEGIN_NAMESPACE
+
+CoverRefinedSingletonModifier
+::CoverRefinedSingletonModifier(ParticleRefiner *ref,
+                                FloatKey rk,
+                                  Float slack): ref_(ref),
+                                                rk_(rk),
+                                                slack_(slack)
+{
+}
+
+CoverRefinedSingletonModifier::~CoverRefinedSingletonModifier()
+{
+}
+
+void CoverRefinedSingletonModifier::apply(Particle *p)
+{
+  XYZRDecorator dp(p, rk_);
+  IMP_check(ref_->get_can_refine(p), "Passed particles cannot be refined",
+            ValueException);
+  Particles ps= ref_->get_refined(p);
+  set_enclosing_sphere(ps, dp);
+  dp.set_radius(dp.get_radius()+slack_);
+  ref_->cleanup_refined(p, ps);
+}
+
+void CoverRefinedSingletonModifier::show(std::ostream &out) const
+{
+  out << "CoverRefinedSingletonModifier" << std::endl;
+}
+
+IMPCORE_END_NAMESPACE
