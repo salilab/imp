774a46682c3eb0983e6c1b3f26c9c329e84b434a
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-12-10 15:22:05 +0000

    Fix example.
    git-svn-id: https://svn.salilab.org/imp/trunk@1004 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- doc/examples/simple_examples/cover_particles.py
+++ doc/examples/simple_examples/cover_particles.py
@@ -3,14 +3,14 @@ import IMP.core
 import IMP.modeller
 
 m= IMP.Model()
-prot= IMP.modeller.read_pdb('single_protein.pdb')
+prot= IMP.modeller.read_pdb('single_protein.pdb', m)
 res= IMP.core.molecular_hierarchy_get_by_type(prot, IMP.core.MolecularHierarchyDecorator.RESIDUE)
 rc= IMP.core.ListSingletonContainer(res)
 for p in res:
     IMP.core.XYZRDecorator.create(p)
-ref= IMP.misc.ChildrenParticleRefiner()
+ref= IMP.core.ChildrenParticleRefiner()
 cover= IMP.core.CoverRefinedSingletonModifier(ref)
 propd= IMP.core.DerivativesToRefinedSingletonModifier(ref)
-state=IMP.core.SingletonsState(rc, cover, propd)
-m.add_core_state(state)
+state=IMP.core.SingletonsScoreState(rc, cover, propd)
+m.add_score_state(state)
 m.evaluate(False)
