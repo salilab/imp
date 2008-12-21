a80da1a5e6cf5b4fcda4ba9253e1d6457d6b1c1e
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-12-21 00:05:26 +0000

    add methods to generate Atom/Residue Types from string and make sure that the keys are initialized statically so that they are always available. This should work without the also submitted keys patch, but has not been tested without it.
    git-svn-id: https://svn.salilab.org/imp/trunk@1066 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/src/ResidueDecorator.cpp
+++ modules/core/src/ResidueDecorator.cpp
@@ -19,8 +19,7 @@ IMPCORE_BEGIN_NAMESPACE
 IntKey ResidueDecorator::type_key_;
 IntKey ResidueDecorator::index_key_;
 
-#define TYPE_INIT(STR) STR= ResidueType(#STR);
-#define TYPE_DEF(STR) ResidueType ResidueDecorator::STR;
+#define TYPE_DEF(STR) ResidueType ResidueDecorator::STR(#STR);
 TYPE_DEF(UNK);
 TYPE_DEF(GLY);
 TYPE_DEF(ALA);
@@ -48,7 +47,16 @@ TYPE_DEF(ADE);
 TYPE_DEF(URA);
 TYPE_DEF(CYT);
 TYPE_DEF(GUA);
-TYPE_DEF(THY)
+TYPE_DEF(THY);
+
+ResidueType residue_type_from_pdb_string(std::string nm) {
+  if (!ResidueType::get_key_exists(nm)) {
+    std::ostringstream oss;
+    oss<< "ResidueType " << nm << " does not exist.";
+    throw ValueException(oss.str().c_str());
+  }
+  return ResidueType(nm.c_str());
+}
 
 void ResidueDecorator::show(std::ostream &out, std::string pre) const
 {
@@ -61,34 +69,6 @@ void ResidueDecorator::show(std::ostream &out, std::string pre) const
 IMP_DECORATOR_INITIALIZE(ResidueDecorator, DecoratorBase,
                          { type_key_= IntKey("residue type");
                            index_key_= IntKey("residue index");
-                           TYPE_INIT(UNK);
-                           TYPE_INIT(GLY);
-                           TYPE_INIT(ALA);
-                           TYPE_INIT(VAL);
-                           TYPE_INIT(LEU);
-                           TYPE_INIT(ILE);
-                           TYPE_INIT(SER);
-                           TYPE_INIT(THR);
-                           TYPE_INIT(CYS);
-                           TYPE_INIT(MET);
-                           TYPE_INIT(PRO);
-                           TYPE_INIT(ASP);
-                           TYPE_INIT(ASN);
-                           TYPE_INIT(GLU);
-                           TYPE_INIT(GLN);
-                           TYPE_INIT(LYS);
-                           TYPE_INIT(ARG);
-                           TYPE_INIT(HIS);
-                           TYPE_INIT(PHE);
-                           TYPE_INIT(TYR);
-                           TYPE_INIT(TRP);
-                           TYPE_INIT(ACE);
-                           TYPE_INIT(NH2);
-                           TYPE_INIT(ADE);
-                           TYPE_INIT(URA);
-                           TYPE_INIT(CYT);
-                           TYPE_INIT(GUA);
-                           TYPE_INIT(THY);
                          })
 
 IMPCORE_END_NAMESPACE
