5decc5b9755255f2ba013d4d308dd0eab8c07753
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2008-12-31 00:25:34 +0000

    put the is_nan def back until Ben commits kernel patches
    git-svn-id: https://svn.salilab.org/imp/trunk@1100 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/include/utility.h
+++ modules/core/include/utility.h
@@ -13,8 +13,36 @@
 #include "config.h"
 #include <IMP/base_types.h>
 
+#ifdef __GNUC__
+#include <cmath>
+#endif
+
 IMPCORE_BEGIN_NAMESPACE
 
+//! Return true if a number is NaN
+/** With certain compiler settings the compiler can optimize
+ out a!=a (and certain intel chips had issues with it too).
+ */
+inline bool is_nan(const float& a) {
+  // Not all gcc versions include C99 math
+#if defined(_GLIBCXX_USE_C99_MATH) && defined(__GNUC__)
+  return std::isnan(a);
+#else
+  return a != a;
+#endif
+}
+
+//! Return true if a number is NaN
+/** With certain compiler settings the compiler can optimize
+ out a!=a (and certain intel chips had issues with it too).
+ */
+inline bool is_nan(const double& a) {
+#if defined(_GLIBCXX_USE_C99_MATH) && defined(__GNUC__)
+  return std::isnan(a);
+#else
+  return a != a;
+#endif
+}
 
 IMPCORE_END_NAMESPACE
 
