8c3aee3c48c77577271dec3518eedecd7c96fd3e
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-01-05 22:26:24 +0000

    Patch from Daniel: Use the boost isnan function when available. Also, parenticize the calls to isnan as that is a macro on some platforms (and boost says we should).
    git-svn-id: https://svn.salilab.org/imp/trunk@1109 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/include/utility.h
+++ kernel/include/utility.h
@@ -10,10 +10,15 @@
 
 #include "macros.h"
 
+#include <boost/version.hpp>
+
+#if BOOST_VERSION >= 103500
+#include <boost/math/special_functions/fpclassify.hpp>
+#else
 #ifdef __GNUC__
 #include <cmath>
-#endif
-
+#endif // __GNUC__
+#endif // BOOST_VERSION
 
 IMP_BEGIN_NAMESPACE
 
@@ -29,25 +34,18 @@ T square(T t)
 /** With certain compiler settings the compiler can optimize
  out a!=a (and certain intel chips had issues with it too).
  */
-inline bool is_nan(const float& a) {
+template <class T>
+inline bool is_nan(const T& a) {
+#if BOOST_VERSION >= 103500
+  return (boost::math::isnan)(a);
+#else
   // Not all gcc versions include C99 math
 #if defined(_GLIBCXX_USE_C99_MATH) && defined(__GNUC__)
-  return std::isnan(a);
+  return (std::isnan)(a);
 #else
   return a != a;
-#endif
-}
-
-//! Return true if a number is NaN
-/** With certain compiler settings the compiler can optimize
- out a!=a (and certain intel chips had issues with it too).
- */
-inline bool is_nan(const double& a) {
-#if defined(_GLIBCXX_USE_C99_MATH) && defined(__GNUC__)
-  return std::isnan(a);
-#else
-  return a != a;
-#endif
+#endif // C99
+#endif // BOOST_VERSION
 }
 
 IMP_END_NAMESPACE
