39ac9f97354e444df740121c650eeb045848aac5
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-01-10 17:14:41 +0000

    add the truncated harmonic. the test_deriv function in kernel test needs to be improved a bit to make the tests pass more reliable. Ben can add his favorite derivative evaluation scheme.
    git-svn-id: https://svn.salilab.org/imp/trunk@1151 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/core/include/TruncatedHarmonic.h
@@ -0,0 +1,86 @@
+/**
+ *  \file TruncatedHarmonic.h    \brief Truncated harmonic.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ */
+
+#ifndef IMPCORE_TRUNCATED_HARMONIC_H
+#define IMPCORE_TRUNCATED_HARMONIC_H
+
+#include "config.h"
+#include "internal/truncated_harmonic.h"
+#include <IMP/UnaryFunction.h>
+#include <IMP/utility.h>
+
+IMPCORE_BEGIN_NAMESPACE
+
+enum BoundDirection {LOWER, BOTH, UPPER};
+
+//! A function that is harmonic over an interval.
+/** This function is harmonic between center and threshold and then
+ asymptotically converges to the limit value.
+
+ The function form above the threshold is currently limit-b/(x-o)
+ where x is the offset from the center and b,o are constants chosen to
+ make the function smooth and continuous.  This form may change
+ without notice unless someone tells us it is important that it does
+ not.
+
+ \param[in] DIRECTION Whether to be an upper bound, lower bound, or
+ both directions.  It should be one of the BoundDirection enum
+ values. If it is LOWER, than the function is 0 for all values above
+ the passed center.
+ */
+template <int DIRECTION>
+class TruncatedHarmonic : public UnaryFunction {
+public:
+  /** \param[in] center The center point for the harmonic.
+   \param[in] k The spring constant for the harmonic.
+   \param[in] threshold How far the harmonic term extends from the center.
+   \param[in] limit The value to which the function converges above the
+   threshold.
+
+   \note I don't like having 4 floats on the initializer list, but
+   don't really see an alternative. There are a few sanity checks, so
+   the order is a bit hard to get wrong.
+   */
+  TruncatedHarmonic(Float center,
+                    Float k, Float threshold,
+                    Float limit):
+  d_(center, k, threshold, limit){
+  }
+  /** Set limit to a reasonable value. */
+  TruncatedHarmonic(Float center,
+                    Float k, Float threshold):
+  d_(center, k, threshold, k*square(threshold-center)){
+  }
+
+
+  virtual ~TruncatedHarmonic() {}
+
+  virtual Float evaluate(Float feature) const {
+    if (DIRECTION == LOWER && feature > d_.c_
+        || DIRECTION == UPPER && feature < d_.c_) return 0;
+    return d_.evaluate(feature);
+  }
+
+  virtual FloatPair evaluate_with_derivative(Float feature) const {
+    if (DIRECTION == LOWER && feature > d_.c_
+        || DIRECTION == UPPER && feature < d_.c_) return FloatPair(0,0);
+    return d_.evaluate_with_derivative(feature);
+  }
+
+  void show(std::ostream &out=std::cout) const {
+    out << "TruncatedHarmonic: " << d_ << std::endl;
+  }
+private:
+  internal::TruncatedHarmonicData d_;
+};
+
+typedef TruncatedHarmonic<UPPER> TruncatedHarmonicUpperBound;
+typedef TruncatedHarmonic<LOWER> TruncatedHarmonicLowerBound;
+typedef TruncatedHarmonic<BOTH> TruncatedHarmonicBound;
+
+IMPCORE_END_NAMESPACE
+
+#endif  /* IMPCORE_TRUNCATED_HARMONIC_H */
---
+++ modules/core/include/internal/truncated_harmonic.h
@@ -0,0 +1,75 @@
+/**
+ *  \file truncated_harmonic.h
+ *  \brief Helpers for the truncated harmonics.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ *
+ */
+
+#ifndef IMPCORE_INTERNAL_TRUNCATED_HARMONIC_H
+#define IMPCORE_INTERNAL_TRUNCATED_HARMONIC_H
+
+#include "../config.h"
+
+#include <IMP/base_types.h>
+
+IMPCORE_BEGIN_INTERNAL_NAMESPACE
+
+struct TruncatedHarmonicData {
+  TruncatedHarmonicData(Float center, Float k,
+                        Float threshold, Float limit): c_(center),
+  l_(limit), k_(k), t_(threshold) {
+    IMP_check(.5*k*square(threshold) < limit,
+              "The limit must be larger than the harmonic at the threshold",
+              ValueException);
+    IMP_check(k > 0, "The spring constant must be positive",
+              ValueException);
+    IMP_check(threshold >= 0, "The threshold must be non-negative.",
+              ValueException);
+    b_= .5*(-k_*k_*k_*square(t_*t_*t_)+6*square(k_)*square(t_*t_)*l_
+        -12*k_*square(t_*l_)+8*l_*l_*l_)/square(k_*t_);
+    o_= 2*(k_*square(t_) -l_)/(k_*t_);
+    IMP_assert(std::abs(evaluate(center+t_+.001)- evaluate(center+t_-.001))
+               < .1*evaluate(center+t_), "Values do not match at threshold "
+               << evaluate(center+t_+.001) << " and "
+               << evaluate(center+t_-.001));
+    IMP_assert(o_ <t_, "Something wrong with computed values for intermediates "
+               << o_ << " " << b_);
+    std::cout << o_ << " " << b_ << std::endl;
+  }
+
+  inline Float evaluate(Float feature) const {
+    Float x= std::abs(feature-c_);
+    if (x > t_) {
+      return l_ - b_/square(x-o_);
+    } else {
+      return .5*k_*square(x);
+    }
+  }
+
+  inline FloatPair evaluate_with_derivative(Float feature) const {
+    Float x= (feature-c_);
+    Float deriv;
+    if (std::abs(x) > t_) {
+      deriv= 2*b_/(square(std::abs(x)-o_)*(std::abs(x)-o_));
+      if (feature < c_) deriv=-deriv;
+    } else {
+      deriv= k_*x;
+    }
+    return FloatPair(evaluate(feature), deriv);
+  }
+
+
+  Float c_, l_, k_, b_, o_, t_;
+};
+
+std::ostream &operator<<(std::ostream &out, const TruncatedHarmonicData &d) {
+  out << "center: " << d.c_ << " limit: " << d.l_ << " k: " << d.k_;
+  return out;
+}
+
+
+
+IMPCORE_END_INTERNAL_NAMESPACE
+
+#endif  /* IMPCORE_INTERNAL_TRUNCATED_HARMONIC_H */
