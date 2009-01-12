4df65eefb55d194598cf03adc221958d21b3a668
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-01-12 21:19:25 +0000

    Put SAXS inputs in the input directory; remove hard-coded paths; clean up outputs.
    git-svn-id: https://svn.salilab.org/imp/trunk@1169 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/saxs/test/test_saxs.py
+++ modules/saxs/test/test_saxs.py
@@ -4,6 +4,7 @@ import IMP.test
 import IMP.modeller
 import IMP.core
 import IMP.saxs
+import os
 
 class PDBReadTest(IMP.test.TestCase):
 
@@ -11,18 +12,23 @@ class PDBReadTest(IMP.test.TestCase):
         """Check reading a pdb with one protein"""
         m = IMP.Model()
 
-        mp= IMP.modeller.read_pdb(self.get_test_file('single_protein.pdb'), m)
+        mp= IMP.modeller.read_pdb(
+                      self.get_input_file_name('single_protein.pdb'), m)
         #IMP.core.show_molecular_hierarchy(mp)
 
         saxsdata = IMP.saxs.SaxsData(m, mp)
         saxsdata.initialize(0.009, 0.325, 100,
             505, 15, 'heav',
-            'modules/saxs/test/formfactors-int_tab_solvation.lib',
+            self.get_input_file_name('formfactors-int_tab_solvation.lib'),
             'uniform', 0.0, 0.5, 0.0,
             'real', False)
 
         saxsdata.saxs_intensity('i_s_single_protein.txt', False)
         #saxs.saxs_read(filename='i_s.txt')
+
+        # Clean up outputs
+        for f in ('i_s_single_protein.txt', 'p_r.txt'):
+            os.unlink(f)
 
 """
         saxsdata.ini_saxs(atmsel=atmsel, s_min= 0.009, s_max=0.325, maxs=100,
