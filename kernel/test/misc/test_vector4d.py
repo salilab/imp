789f1bd727f325f0f345c291a09d873b43636382
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-01-20 23:34:11 +0000

    Patch from Daniel, with bugfixes and unit tests from me: Support more vector functionality in higher dimensions, specifically choosing points on spheres and exporting a 4D vector to Python.
    git-svn-id: https://svn.salilab.org/imp/trunk@1224 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ kernel/test/misc/test_vector4d.py
@@ -0,0 +1,108 @@
+import unittest
+import IMP
+import IMP.test
+import math
+
+class Vector4DTests(IMP.test.TestCase):
+    def test_magnitude(self):
+        """Check Vector4D magnitude"""
+        v = IMP.Vector4D(1.0, 2.0, 3.0, 4.0)
+        self.assertEqual(v.get_squared_magnitude(), 30.0)
+        self.assertAlmostEqual(v.get_magnitude(), math.sqrt(30.0), places=1)
+
+    def test_component(self):
+        """Check Vector4D components"""
+        v = IMP.Vector4D(1.0, 2.0, 3.0, 4.0)
+        self.assertEqual(v[0], 1.0)
+        self.assertEqual(v[1], 2.0)
+        self.assertEqual(v[2], 3.0)
+        self.assertEqual(v[3], 4.0)
+        v[0] = 10.0
+        self.assertEqual(v[0], 10.0)
+
+    def test_scalar_product(self):
+        """Check Vector4D scalar product"""
+        v1 = IMP.Vector4D(1.0, 2.0, 3.0, 4.0)
+        v2 = IMP.Vector4D(10.0, 1.0, 2.0, 3.0)
+        self.assertInTolerance(v1.scalar_product(v2), v2.scalar_product(v1),
+                               .1)
+        self.assertInTolerance(v1.scalar_product(v2), v1 * v2,
+                               .1)
+        self.assertInTolerance(v1.scalar_product(v2), v2 * v1, .1)
+        self.assertInTolerance(v1.scalar_product(v2), 30.0, .1)
+
+    def test_vector_product(self):
+        """Vector4D vector product should not work"""
+        v1 = IMP.Vector4D(1.0, 2.0, 3.0, 4.0)
+        v2 = IMP.Vector4D(10.0, 1.0, 2.0, 3.0)
+        self.assertRaises(ValueError, v1.vector_product, v2)
+
+    def test_difference(self):
+        """Check Vector4D difference"""
+        v1 = IMP.Vector4D(1.0, 2.0, 3.0, 4.0)
+        v2 = IMP.Vector4D(10.0, 1.0, 2.0, 3.0)
+        diff = v1 - v2
+        v1 -= v2
+        expected_diff = IMP.Vector4D(-9.0, 1.0, 1.0, 1.0)
+        self.assertInTolerance((diff-expected_diff).get_magnitude(),
+                               0, .1)
+        self.assertInTolerance((v1-expected_diff).get_magnitude(),
+                               0, .1)
+
+    def test_addition(self):
+        """Check Vector4D addition"""
+        v1 = IMP.Vector4D(1.0, 2.0, 3.0, 4.0)
+        idv1 = id(v1)
+        cppobj = str(v1.this)
+        v2 = IMP.Vector4D(10.0, 1.0, 2.0, 3.0)
+        sum = v1 + v2
+        v1 += v2
+        # Inplace addition should not change the Python object identity:
+        self.assertEqual(id(v1), idv1)
+        # The underlying C++ object pointer should be unchanged too:
+        self.assertEqual(str(v1.this), cppobj)
+        expected_sum = IMP.Vector4D(11.0, 3.0, 5.0, 7.0)
+        self.assertInTolerance((sum-expected_sum).get_magnitude(),
+                               0, .1)
+        self.assertInTolerance((v1-expected_sum).get_magnitude(),
+                               0, .1)
+
+    def test_scalar_multiplication(self):
+        """Check Vector4D multiplication by a scalar"""
+        v1 = IMP.Vector4D(1.0, 2.0, 3.0, 4.0)
+        idv1 = id(v1)
+        s1 = 3.0
+        prod = v1 * s1
+        v1 *= s1
+        # Inplace multiplication should not change the Python object identity:
+        self.assertEqual(id(v1), idv1)
+        expected_prod = (3.0, 6.0, 9.0, 12.0)
+        for i in range(3):
+            self.assertInTolerance(prod[i], expected_prod[i],
+                                   .1)
+            self.assertInTolerance(v1[i], expected_prod[i],
+                                   .1)
+
+    def test_scalar_division(self):
+        """Check Vector4D division by a scalar"""
+        v1 = IMP.Vector4D(3.0, 6.0, 9.0, 27.0)
+        idv1 = id(v1)
+        s1 = 3.0
+        prod = v1 / s1
+        v1 /= s1
+        # Inplace division should not change the Python object identity:
+        self.assertEqual(id(v1), idv1)
+        expected_prod = (1.0, 2.0, 3.0, 9.0)
+        for i in range(3):
+            self.assertEqual(prod[i], expected_prod[i])
+            self.assertEqual(v1[i], expected_prod[i])
+
+    def test_distance(self):
+        """Distance should not work for 4D vectors"""
+        v1 = IMP.Vector4D(3.0, 6.0, 9.0, 0.0)
+        v2 = IMP.Vector4D(1.0, 2.0, 3.0, 10.0)
+        self.assertRaises(TypeError, IMP.squared_distance, v1, v2)
+
+
+if __name__ == '__main__':
+    unittest.main()
