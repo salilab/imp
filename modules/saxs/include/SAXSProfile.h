83b71c038deb5128495462a60617b7ecb8f59ef6
dina@SALILAB.ORG <dina@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-01-20 23:35:46 +0000

    saxs stuff
    git-svn-id: https://svn.salilab.org/imp/trunk@1225 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/saxs/include/SAXSProfile.h
+++ modules/saxs/include/SAXSProfile.h
@@ -7,76 +7,108 @@
 #ifndef IMPSAXS_SAXS_PROFILE_H
 #define IMPSAXS_SAXS_PROFILE_H
 
+#include "config.h"
+#include "FormFactorTable.h"
+#include "RadialDistributionFunction.h"
+
 #include <IMP/Model.h>
 
 #include <iostream>
 #include <vector>
 
-using std::ostream;
-using std::istream;
-using std::endl;
-using std::vector;
+IMPSAXS_BEGIN_NAMESPACE
 
-#include "FormFactorTable.h"
+class RadialDistributionFunction;
 
 /* Basic profile class, can be initialized from the input file
-   (experimental or theoretical) or computed from the Model or
-   RigidBodies of the Model (theoretical)
+   (experimental or theoretical) or computed from a set of Model
+   Particles (theoretical)
 */
 class SAXSProfile {
 public:
   //! init from file
-  SAXSProfile(const string& file_name);
+  SAXSProfile(const String& file_name);
 
   //! init for theoretical profile
-  SAXSProfile(float smin, float smax, float delta);
+  SAXSProfile(Float smin, Float smax, Float delta, FormFactorTable* ff_table);
 
+private:
   class IntensityEntry {
   public:
     IntensityEntry() : s_(0.0), intensity_(0.0), error_(1.0) {}
-    IntensityEntry(float s) : s_(s), intensity_(0.0), error_(1.0) {}
+    IntensityEntry(Float s) : s_(s), intensity_(0.0), error_(1.0) {}
 
-    friend ostream& operator<<(ostream& s, const IntensityEntry& e) {
-      return s << e.s_ << " , " << e.intensity_ << " , " << e.error_ << endl;
-    }
-
-    friend istream& operator>>(istream& s, IntensityEntry& e) {
-      return s >> e.s_ >> e.intensity_ >> e.error_;
-    }
-
-    float get_s() const { return s_; }
-    float get_intensity() const { return intensity_; }
-    float get_error() const { return error_; }
-
-    void set_s(float s) { s_ = s; }
-    void set_intensity(float intensity) { intensity_ = intensity; }
-    void set_error(float error) { error_ = error; }
-
-  protected:
-    float s_;
-    float intensity_;
-    float error_;
+    Float s_;
+    Float intensity_;
+    Float error_;
   };
 
-  //! reads SAXS profile from file
-  void read_SAXS_file(const string& file_name);
+  friend std::ostream& operator<<(std::ostream& s, const IntensityEntry& e);
 
+  friend std::istream& operator>>(std::istream& s, IntensityEntry& e);
+
+public:
   //! computes theoretical profile
-  void calculate_profile(const vector<IMP::Particle*>& particles);
+  void calculate_profile(const std::vector<Particle*>& particles) {
+    calculate_profile_real(particles);
+  }
 
   //! computes theoretical profile contribution from iter-molecular
   // interactions between the particles
-  void calculate_profile(const vector<IMP::Particle*>& particles1,
-                         const vector<IMP::Particle*>& particles2);
+  void calculate_profile(const std::vector<Particle*>& particles1,
+                         const std::vector<Particle*>& particles2) {
+    calculate_profile_real(particles1, particles2);
+  }
+
+  //! reads SAXS profile from file
+  void read_SAXS_file(const String& file_name);
+
+  //! print to file
+  void write_SAXS_file(const String& file_name);
+
+  //! compute chi value (assumes the same sampling range!)
+  Float compute_chi_score(const SAXSProfile& profile) const;
+
+  //! return sampling resolution
+  Float get_delta_s() const { return delta_s_; }
+
+  //! return minimal sampling point
+  Float get_min_s() const { return min_s_; }
+
+  //! return maximal sampling point
+  Float get_max_s() const { return max_s_; }
 
 private:
   void init();
 
+  void calculate_profile_reciprocal(const std::vector<Particle*>& particles);
+
+  void calculate_profile_reciprocal(const std::vector<Particle*>& particles1,
+                                    const std::vector<Particle*>& particles2);
+
+  void calculate_profile_real(const std::vector<Particle*>& particles);
+
+  void calculate_profile_real(const std::vector<Particle*>& particles1,
+                              const std::vector<Particle*>& particles2);
+
+  void radial_distribution_2_profile(const RadialDistributionFunction& r_dist);
+
+  Float sinc(Float value) {
+    if(fabs(value) < 0.0000000000000001) return 1.0;
+    return sin(value)/value;
+  }
+
+  void write_SAXS_fit_file(const String& file_name,
+                           const SAXSProfile& saxs_profile,
+                           const Float c) const;
+
 protected:
-  vector<IntensityEntry> profile_; // the profile
-  float min_s_, max_s_; // minimal and maximal s values  in the profile
-  float delta_s_; // profile sampling resolution
+  std::vector<IntensityEntry> profile_; // the profile
+  Float min_s_, max_s_; // minimal and maximal s values  in the profile
+  Float delta_s_; // profile sampling resolution
   FormFactorTable* ff_table_; // pointer to form factors table
 };
+
+IMPSAXS_END_NAMESPACE
 
 #endif /* IMPSAXS_SAXS_PROFILE_H */
