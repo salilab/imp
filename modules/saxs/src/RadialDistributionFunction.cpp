56b26c07cac3af58ac0f1a6ad34daf436ba583e0
dina@SALILAB.ORG <dina@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-01-20 23:51:42 +0000

    saxs stuff
    git-svn-id: https://svn.salilab.org/imp/trunk@1231 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/saxs/src/RadialDistributionFunction.cpp
@@ -0,0 +1,103 @@
+/**
+ * \file RadialDistributionFunction \brief computes radial distribution function
+ * required for SAXS profile computation
+ *
+ * Copyright 2007-8 Sali Lab. All rights reserved.
+ *
+ */
+#include <IMP/saxs/RadialDistributionFunction.h>
+
+#include <IMP/Vector3D.h>
+#include <IMP/core/XYZDecorator.h>
+
+IMPSAXS_BEGIN_NAMESPACE
+
+RadialDistributionFunction::RadialDistributionFunction(Float bin_size,
+                                              FormFactorTable * ff_table):
+bin_size_(bin_size), ff_table_(ff_table)
+{
+  distribution_.reserve(dist2index(50.0));      // start with ~50A
+}
+
+void RadialDistributionFunction::add_to_distribution(Float dist, Float value)
+{
+  unsigned int index = dist2index(dist);
+  if (index >= distribution_.size()) {
+    distribution_.reserve(2 * index);   // to avoid many re-allocations
+    distribution_.resize(index + 1, 0);
+  }
+  distribution_[index] += value;
+}
+
+void RadialDistributionFunction::calculate_distribution(
+                                const std::vector<Particle*>& particles)
+{
+  // TODO: add imp macro instead
+  std::cerr << "start distribution calculation for "
+      << particles.size() << " particles" << std::endl;
+
+  // copy coordinates in advance, to avoid n^2 copy operations
+  std::vector < Vector3D > coordinates;
+  for (unsigned int i = 0; i < particles.size(); i++) {
+    coordinates.push_back(core::XYZDecorator::cast(particles[i]).
+                          get_coordinates());
+  }
+
+  // iterate over pairs of atoms
+  // loop1
+  for (unsigned int i = 0; i < coordinates.size(); i++) {
+    Float factor1 = ff_table_->get_form_factor(particles[i]);
+    // loop2
+    for (unsigned int j = i + 1; j < coordinates.size(); j++) {
+      Float factor2 = ff_table_->get_form_factor(particles[j]);
+      Float dist = distance(coordinates[i], coordinates[j]);
+      add_to_distribution(dist, 2 * factor1 * factor2);
+    }                           // end of loop 2
+
+    // add autocorrelation part
+    add_to_distribution(0.0, factor1 * factor1);
+  }                             // end of loop1
+}
+
+void RadialDistributionFunction::calculate_distribution(
+                             const std::vector<Particle*>&particles1,
+                             const std::vector<Particle*>&particles2)
+{
+  // TODO: change to IMP macro
+  std::cerr << "start distribution calculation for "
+      << particles1.size() << " + " << particles2.size()
+      << " particles" << std::endl;
+
+  // copy coordinates in advance, to avoid n^2 copy operations
+  std::vector < Vector3D > coordinates1, coordinates2;
+  for (unsigned int i = 0; i < particles1.size(); i++) {
+    coordinates1.push_back(core::XYZDecorator::cast(particles1[i]).
+                           get_coordinates());
+  }
+  for (unsigned int i = 0; i < particles2.size(); i++) {
+    coordinates2.push_back(core::XYZDecorator::cast(particles2[i]).
+                           get_coordinates());
+  }
+
+  // iterate over pairs of atoms
+  // loop1
+  for (unsigned int i = 0; i < coordinates1.size(); i++) {
+    Float factor1 = ff_table_->get_form_factor(particles1[i]);
+    // loop2
+    for (unsigned int j = i + 1; j < coordinates2.size(); j++) {
+      Float factor2 = ff_table_->get_form_factor(particles2[j]);
+      Float dist = distance(coordinates1[i], coordinates2[j]);
+      add_to_distribution(dist, 2 * factor1 * factor2);
+    }                           // end of loop 2
+  }                             // end of loop1
+}
+
+void RadialDistributionFunction::show(std::ostream & out, std::string prefix)
+                                                                       const {
+  for (unsigned int i = 0; i < distribution_.size(); i++) {
+    out << prefix << " dist " << index2dist(i) << " " << distribution_[i]
+        << std::endl;
+  }
+}
+
+IMPSAXS_END_NAMESPACE
