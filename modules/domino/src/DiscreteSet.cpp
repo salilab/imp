d5ae5fd9d34f1185619745778f642d18f9d230cf
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-01-21 22:42:49 +0000

    add get_state function
    git-svn-id: https://svn.salilab.org/imp/trunk@1254 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/domino/src/DiscreteSet.cpp
+++ modules/domino/src/DiscreteSet.cpp
@@ -27,6 +27,14 @@ void DiscreteSet::add_state(Particle* p) {
   states_.push_back(p);
 }
 
+Particle * DiscreteSet::get_state(long state_ind) const {
+  std::stringstream err_msg;
+  err_msg <<"DiscreteSet::get_state the input state index: " << state_ind;
+  err_msg << " is out of range ( " << states_.size() << " ) " ;
+  IMP_assert(state_ind<states_.size(),err_msg.str());
+  return states_[state_ind];
+}
+
 Float DiscreteSet::get_state_val(long state_ind, IMP::FloatKey key) const {
   std::stringstream err_msg;
   err_msg <<"DiscreteSet::get_state_val the input state index: " << state_ind;
