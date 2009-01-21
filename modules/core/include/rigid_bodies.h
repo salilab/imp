eccb48298f5d6dc9df4592b32d1235082f7817f6
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-01-21 00:25:33 +0000

    add initial rigid body code
    git-svn-id: https://svn.salilab.org/imp/trunk@1239 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/core/include/rigid_bodies.h
@@ -0,0 +1,189 @@
+/**
+ *  \file rigid_bodies.h
+ *  \brief functionality for defining rigid bodies
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ */
+
+#ifndef IMPCORE_RIGID_BODIES_H
+#define IMPCORE_RIGID_BODIES_H
+
+#include "config.h"
+#include "internal/rigid_bodies.h"
+
+#include <IMP/core/XYZDecorator.h>
+
+#include <IMP/Vector3D.h>
+#include <IMP/algebra/Rotation3D.h>
+
+IMPCORE_BEGIN_NAMESPACE
+
+//! A class to store rigid body-related data
+class IMPCOREEXPORT RigidBodyTraits {
+  Pointer<internal::RigidBodyData> d_;
+ public:
+  //! Create as set of rigid body data using the prefix as an identifier
+  /** The radius and mass keys can be default keys, in which case the
+      masses will be considered equal and the radii 0, respectively.
+  */
+  RigidBodyTraits(std::string prefix="default_rigid",
+                  FloatKey mass=FloatKey(),
+                  FloatKey radius=FloatKey());
+  //! Get the keys used to store the offset coordinates
+  const FloatKeys& get_local_coordinate_keys() const {
+    return d_->child_keys_;
+  }
+  //! Get the keys used to store the offset quaternion
+  const FloatKeys& get_quaternion_keys() const {
+    return d_->quaternion_;
+  }
+  //! Get the key used to store the radius
+  FloatKey get_radius_key() const {
+    return d_->radius_;
+  }
+  //! Get the key used to store the radius
+  FloatKey get_mass_key() const {
+    return d_->mass_;
+  }
+  bool get_has_required_attributes_for_body(Particle *p) const;
+  bool get_has_required_attributes_for_member(Particle* p) const;
+  void add_required_attributes_for_body(Particle *p) const;
+  void add_required_attributes_for_member(Particle *p) const;
+};
+
+class RigidMemberDecorator;
+
+//! A decorator for a rigid body
+/** The body is modeled as a collection of balls of radius and mass
+    given by the RigidBodyTraits. I, the inertial tensor, is
+    computed and diagonalized, but not stored. It can be stored if
+    someone wants it.
+ */
+class IMPCOREEXPORT RigidBodyDecorator: public XYZDecorator {
+ public:
+  IMP_DECORATOR_TRAITS(RigidBodyDecorator, XYZDecorator,
+                       RigidBodyTraits, traits,
+                       internal::get_default_rigid_body_traits());
+
+  //! Create a new rigid bid
+  /** \param[in] p The particle to make into a rigid body
+      \param[in] members The particles which will be members of the body.
+      They must already be XYZDecorator particles.
+      \param[in] tr The traits class to use.
+      The initial position and orientation of p is computed, as are the
+      relative positions of the member particles. The member particles
+      do not already need to RigidMemberDecorator particles.
+   */
+  static RigidBodyDecorator create(Particle *p,
+                            const Particles &members,
+              RigidBodyTraits tr= internal::get_default_rigid_body_traits());
+
+  //! Update the coordinates and orientation of the body from the members
+  /** The member particles are then snapped to their rigid locations.
+
+      \param[in] members The particles making up the rigid body. All
+      the particles must be RigidMemberDecorators.
+   */
+  void set_transformation(const Particles &members);
+
+  ~RigidBodyDecorator();
+
+  //!Return true of the particle is a rigid body
+  static bool is_instance_of(Particle *p,
+                             RigidBodyTraits traits
+                             =internal::get_default_rigid_body_traits()) {
+    return traits.get_has_required_attributes_for_body(p);
+  }
+
+  // swig doesn't support using, so the method is wrapped
+  //! Get the coordinates of the particle
+  Vector3D get_coordinates() const {
+    return XYZDecorator::get_coordinates();
+  }
+
+  //! Return the location of a member particle given the current position
+  /** This method computes the coordinates of p given its internal coordinates
+      and the current position and orientation of the rigid body.
+   */
+  Vector3D get_coordinates(RigidMemberDecorator p) const;
+
+  //! Get the transformation implied by the rigid body
+  IMP::algebra::Transformation3D get_transformation() const;
+
+  //! Set the current orientation and translation
+  void set_transformation(const IMP::algebra::Transformation3D &tr);
+
+};
+
+IMP_OUTPUT_OPERATOR(RigidBodyDecorator);
+
+
+//! A decorator for a particle that is part of a rigid body
+class IMPCOREEXPORT RigidMemberDecorator: public XYZDecorator {
+ public:
+  IMP_DECORATOR_TRAITS(RigidMemberDecorator, XYZDecorator,
+                       RigidBodyTraits, traits,
+                       internal::get_default_rigid_body_traits());
+
+  //! Return the current orientation of the body
+  Vector3D get_internal_coordinates() const {
+    return Vector3D(get_particle()
+                    ->get_value(get_traits().get_local_coordinate_keys()[0]),
+                    get_particle()
+                    ->get_value(get_traits().get_local_coordinate_keys()[1]),
+                    get_particle()
+                    ->get_value(get_traits().get_local_coordinate_keys()[2]));
+  }
+
+  //! set the internal coordinates for this member
+  void set_internal_coordinates(const Vector3D &v) const {
+    get_particle()->set_value(get_traits().get_local_coordinate_keys()[0],
+                              v[0]);
+    get_particle()->set_value(get_traits().get_local_coordinate_keys()[1],
+                              v[1]);
+    get_particle()->set_value(get_traits().get_local_coordinate_keys()[2],
+                              v[2]);
+  }
+
+  //! set the coordinates of the body
+  // this is here since swig does like using statements
+  void set_coordinates(const Vector3D &center) {
+    XYZDecorator::set_coordinates(center);
+  }
+
+  //! Set the coordinates from the internal coordinates
+  void set_coordinates(const Vector3D &center,
+                       const IMP::algebra::Rotation3D &rot) {
+    set_coordinates(center+rot.rotate(get_internal_coordinates()));
+  }
+  ~RigidMemberDecorator();
+
+  //! return the mass or 1 if there is no mass key defined
+  Float get_mass() const {
+    if (get_traits().get_mass_key() != FloatKey()) {
+      return get_particle()->get_value(get_traits().get_mass_key());
+    } else return 1;
+  }
+
+  //! return the radius or 0 if there is no radius key defined
+  Float get_radius() const {
+    if (get_traits().get_radius_key() != FloatKey()) {
+      return get_particle()->get_value(get_traits().get_radius_key());
+    } else return 0;
+  }
+
+  //! return true if it is a rigid member
+  static bool is_instance_of(Particle *p,
+                             RigidBodyTraits traits
+                             =internal::get_default_rigid_body_traits()) {
+    return traits.get_has_required_attributes_for_member(p);
+  }
+};
+
+IMP_OUTPUT_OPERATOR(RigidMemberDecorator);
+
+
+
+IMPCORE_END_NAMESPACE
+
+#endif  /* IMPCORE_RIGID_BODIES_H */
---
+++ modules/core/src/rigid_bodies.cpp
@@ -0,0 +1,255 @@
+/**
+ *  \file rigid_bodies.cpp
+ *  \brief Support for rigid bodies.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ *
+ */
+
+#include "IMP/core/rigid_bodies.h"
+#include <IMP/algebra/internal/tnt_array2d.h>
+#include <IMP/algebra/internal/tnt_array2d_utils.h>
+#include <IMP/algebra/internal/jama_eig.h>
+#include <IMP/algebra/geometric_alignment.h>
+#include <boost/foreach.hpp>
+
+IMPCORE_BEGIN_NAMESPACE
+
+
+RigidBodyTraits::RigidBodyTraits(std::string pre,
+                                 FloatKey mass,
+                                 FloatKey radius) {
+  d_= new internal::RigidBodyData();
+  d_->child_keys_.resize(3);
+  d_->child_keys_[0]= FloatKey((pre+"local_x").c_str());
+  d_->child_keys_[1]= FloatKey((pre+"local_y").c_str());
+  d_->child_keys_[2]= FloatKey((pre+"local_z").c_str());
+  d_->quaternion_.resize(4);
+  d_->quaternion_[0]= FloatKey((pre+"quaternion_0").c_str());
+  d_->quaternion_[1]= FloatKey((pre+"quaternion_1").c_str());
+  d_->quaternion_[2]= FloatKey((pre+"quaternion_2").c_str());
+  d_->quaternion_[3]= FloatKey((pre+"quaternion_3").c_str());
+  d_->mass_= mass;
+  d_->radius_= radius;
+}
+
+bool RigidBodyTraits::get_has_required_attributes_for_body(Particle *p) const {
+  for (unsigned int i=0; i< 4; ++i) {
+    if (!p->has_attribute(d_->quaternion_[i])) return false;
+  }
+  for (unsigned int i=0; i< 3; ++i) {
+    if (!p->has_attribute(XYZDecorator::get_xyz_keys()[i]))
+      return false;
+  }
+  return true;
+}
+
+
+
+bool
+RigidBodyTraits::get_has_required_attributes_for_member(Particle *p) const {
+  for (unsigned int i=0; i< 3; ++i) {
+    if (!p->has_attribute(d_->child_keys_[i])) return false;
+  }
+  if (d_->mass_ != FloatKey() && !p->has_attribute(d_->mass_)) return false;
+  for (unsigned int i=0; i< 3; ++i) {
+     if (!p->has_attribute(XYZDecorator::get_xyz_keys()[i]))
+       return false;
+  }
+  return true;
+}
+
+void RigidBodyTraits::add_required_attributes_for_body(Particle *p) const {
+  for (unsigned int i=0; i< 4; ++i) {
+    p->add_attribute(d_->quaternion_[i], 0);
+  }
+  IMP_assert(get_has_required_attributes_for_body(p),
+             "Particles must already be XYZDecorator particles");
+}
+
+void RigidBodyTraits::add_required_attributes_for_member(Particle *p) const {
+  for (unsigned int i=0; i< 3; ++i) {
+    p->add_attribute(d_->child_keys_[i], 0);
+  }
+  IMP_assert(get_has_required_attributes_for_member(p),
+             "Particles must already be XYZDecorator particles");
+}
+
+
+
+typedef IMP::algebra::internal::TNT::Array2D<double> Matrix;
+
+Matrix compute_I(const std::vector<RigidMemberDecorator> &ds,
+                 RigidBodyTraits tr,
+                 const Vector3D &center,
+                 const IMP::algebra::Rotation3D &rot) {
+  Matrix I(3,3, 0.0);
+  BOOST_FOREACH(RigidMemberDecorator cm, ds) {
+    Float r= cm.get_radius();
+    Float m= cm.get_mass();
+    Vector3D cv=rot.rotate(cm.get_coordinates()-center);
+
+    Matrix Is(3,3, 0.0);
+    for (unsigned int i=0; i<3; ++i) {
+      for (unsigned int j=0; j<3; ++j) {
+        Is[i][j]= -m*cv[i]*cv[j];
+        if (i==j) {
+          Is[i][j]+= m*cv.get_squared_magnitude() + .4*m*square(r);
+        }
+      }
+    }
+    I+= Is;
+  }
+  return I;
+}
+
+
+
+RigidBodyDecorator RigidBodyDecorator::create(Particle *p,
+                                              const Particles &members,
+                                              RigidBodyTraits tr){
+  IMP_check(!tr.get_has_required_attributes_for_body(p),
+            "The RigidBody is already set up.",
+            InvalidStateException);
+  tr.add_required_attributes_for_body(p);
+
+  std::vector<RigidMemberDecorator> ds;
+  RigidBodyDecorator d(p, tr);
+
+  BOOST_FOREACH(Particle *mp, members) {
+    IMP_check(!tr.get_has_required_attributes_for_member(p),
+              "Particle " << p->get_index() << " is already part of "
+              << "a conflicting rigid body",
+              InvalidStateException);
+    tr.add_required_attributes_for_member(mp);
+    ds.push_back(RigidMemberDecorator(mp, tr));
+  }
+
+  // compute center of mass
+  Vector3D v(0,0,0);
+  Float mass=0;
+  BOOST_FOREACH(RigidMemberDecorator cm, ds) {
+    v+= cm.get_coordinates()*cm.get_mass();
+    mass+= cm.get_mass();
+  }
+  v/= mass;
+  IMP_LOG(VERBOSE, "Center of mass is " << v << std::endl);
+  // for a sphere 2/5 m r^2 (diagopnal)
+  // parallel axis theorem
+  // I'ij= Iij+M(v^2delta_ij-vi*vj)
+  // compute I
+  Matrix I = compute_I(ds, tr, v, IMP::algebra::identity_rotation());
+  IMP_LOG(VERBOSE, "Initial I is " << I << std::endl);
+  // diagonalize it
+  IMP::algebra::internal::JAMA::Eigenvalue<double> eig(I);
+  Matrix rm;
+  eig.getV(rm);
+  // use the R as the initial orientation
+  IMP::algebra::Rotation3D rot
+    = IMP::algebra::rotation_from_matrix(rm[0][0], rm[0][1], rm[0][2],
+                                         rm[1][0], rm[1][1], rm[1][2],
+                                         rm[2][0], rm[2][1], rm[2][2]);
+  IMP_LOG(VERBOSE, "Initial rotation is " << rot << std::endl);
+  IMP::algebra::Rotation3D roti= rot.get_inverse();
+
+  Matrix I2= compute_I(ds, tr, v, roti);
+  std::cout << I << std::endl;
+  std::cout << I2 << std::endl;
+  d.set_transformation(IMP::algebra::Transformation3D(rot, v));
+  IMP_LOG(VERBOSE, "Particle is " << d << std::endl);
+
+  BOOST_FOREACH(RigidMemberDecorator cm, ds) {
+    Vector3D cv=cm.get_coordinates()-v;
+    Vector3D lc= roti.rotate(cv);
+    cm.set_internal_coordinates(lc);
+     IMP_LOG(VERBOSE, "Member particle is " << cm << std::endl);
+  }
+
+  IMP_IF_CHECK(EXPENSIVE) {
+    BOOST_FOREACH(RigidMemberDecorator cm, ds) {
+      Vector3D v= cm.get_coordinates();
+      Vector3D nv= d.get_coordinates(cm);
+      IMP_assert((v-nv).get_squared_magnitude() < .1,
+                 "Bad initial orientation "
+                 << d.get_transformation() << std::endl
+                 << v << std::endl
+                 << nv);
+    }
+  }
+  return d;
+}
+
+
+IMP::algebra::Transformation3D
+RigidBodyDecorator::get_transformation() const {
+  IMP::algebra::Rotation3D rot(get_particle()
+                               ->get_value(traits_.get_quaternion_keys()[0]),
+                               get_particle()
+                               ->get_value(traits_.get_quaternion_keys()[1]),
+                               get_particle()
+                               ->get_value(traits_.get_quaternion_keys()[2]),
+                               get_particle()
+                               ->get_value(traits_.get_quaternion_keys()[3]));
+  return IMP::algebra::Transformation3D(rot, get_coordinates());
+}
+
+Vector3D RigidBodyDecorator::get_coordinates(RigidMemberDecorator p) const{
+  Vector3D lp= p.get_internal_coordinates();
+  IMP::algebra::Transformation3D tr= get_transformation();
+  return tr.transform(lp);
+}
+
+void RigidBodyDecorator
+::set_transformation(const IMP::algebra::Transformation3D &tr) {
+  VectorD<4> v= tr.get_rotation().get_quaternion();
+  get_particle()->set_value(get_traits().get_quaternion_keys()[0], v[0]);
+  get_particle()->set_value(get_traits().get_quaternion_keys()[1], v[1]);
+  get_particle()->set_value(get_traits().get_quaternion_keys()[2], v[2]);
+  get_particle()->set_value(get_traits().get_quaternion_keys()[3], v[3]);
+  set_coordinates(tr.get_translation());
+}
+
+
+
+
+
+
+
+void RigidBodyDecorator::set_transformation(const Particles &members) {
+  Vector3Ds cur, local;
+  BOOST_FOREACH(Particle *p, members) {
+    RigidMemberDecorator d(p);
+    cur.push_back(d.get_coordinates());
+    local.push_back(d.get_internal_coordinates());
+  }
+  IMP::algebra::Transformation3D tr
+    = IMP::algebra::rigid_align_first_to_second(local, cur);
+  set_transformation(tr);
+  BOOST_FOREACH(Particle *p, members) {
+    RigidMemberDecorator d(p);
+    d.set_coordinates(tr.transform(d.get_internal_coordinates()));
+  }
+}
+
+
+
+
+RigidBodyDecorator::~RigidBodyDecorator(){}
+RigidMemberDecorator::~RigidMemberDecorator(){}
+
+
+
+
+
+
+void RigidBodyDecorator::show(std::ostream &out, std::string prefix) const {
+  out << prefix << "Rigid body " << get_transformation()
+      << std::endl;;
+}
+
+void RigidMemberDecorator::show(std::ostream &out, std::string prefix) const {
+  out << prefix << "RigidMember " << get_coordinates() << " "
+      << get_internal_coordinates() << std::endl;;
+}
+
+IMPCORE_END_NAMESPACE
