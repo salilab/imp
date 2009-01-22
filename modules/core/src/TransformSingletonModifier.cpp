31f4afd6da93f6732b5aa129d6f33200d4ebfac5
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-01-22 21:42:25 +0000

    Remove TransfomrationFunction. Instead add TransformSingletonModifier that is a SingletonModifier. Test case and build files have been updated accordingly.
    git-svn-id: https://svn.salilab.org/imp/trunk@1262 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/core/src/TransformSingletonModifier.cpp
@@ -0,0 +1,38 @@
+/**
+ *  \file TransformSingletonParticle.cpp
+ *  \brief Transform a particle
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ */
+
+
+#include <IMP/core/TransformSingletonModifier.h>
+#include "IMP/core/XYZDecorator.h"
+
+IMPCORE_BEGIN_NAMESPACE
+
+TransformSingletonModifier::TransformSingletonModifier(
+     const algebra::Transformation3D &t,
+     bool ignore_non_xyz)
+{
+  t_=t;
+  ignore_non_xyz_=ignore_non_xyz_;
+}
+
+void TransformSingletonModifier::apply(Particle *p)
+{
+  if (!XYZDecorator::is_instance_of(p)) {
+    IMP_assert(ignore_non_xyz_,"The particle does not have XYZ attributes");
+    return;
+  }
+  XYZDecorator xyz = XYZDecorator(p);
+  xyz.set_coordinates(t_.transform(xyz.get_coordinates()));
+}
+void TransformSingletonModifier::show(std::ostream &out) const
+{
+  out<<"TransformParticle :";
+  t_.show(out);
+  out<<std::endl;
+}
+
+IMPCORE_END_NAMESPACE
