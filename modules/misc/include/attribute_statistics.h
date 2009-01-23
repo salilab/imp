fada71d445e4a1ac79497b0920ef4fabe66c77ec
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-01-23 00:46:01 +0000

    it helps to add the files to svn
    git-svn-id: https://svn.salilab.org/imp/trunk@1269 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/misc/include/attribute_statistics.h
@@ -0,0 +1,29 @@
+/**
+ *  \file attribute_statistics.h
+ *  \brief Functions to get report statistics about the used attributes.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ */
+
+#ifndef IMPMISC_ATTRIBUTE_STATISTICS_H
+#define IMPMISC_ATTRIBUTE_STATISTICS_H
+
+#include "config.h"
+
+#include <IMP/Model.h>
+
+IMPMISC_BEGIN_NAMESPACE
+
+//! Print statistics about the attributes used in Particles.
+/** For each type of attribute, the function prints the names of all
+    the attributes and a list of how many particles has each pattern
+    of attributes. Since the attributes are currently stored in
+    arrays, a pattern is printed as a "-" if the particle doesn't have
+    that attribute and a "X" if it does.
+ */
+IMPMISCEXPORT void show_attribute_usage(Model *m, std::ostream &out= std::cout);
+
+
+IMPMISC_END_NAMESPACE
+
+#endif  /* IMPMISC_ATTRIBUTE_STATISTICS_H */
---
+++ modules/misc/src/attribute_statistics.cpp
@@ -0,0 +1,128 @@
+/**
+ *  \file attribute_statistics.cpp
+ *  \brief Score particles with respect to a tunnel.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ *
+ */
+
+#include <IMP/misc/attribute_statistics.h>
+#include <boost/dynamic_bitset.hpp>
+
+IMPMISC_BEGIN_NAMESPACE
+
+typedef boost::dynamic_bitset<> BS;
+
+struct GetFloat {
+  typedef std::pair<Particle::FloatKeyIterator,
+                    Particle::FloatKeyIterator> Range;
+  Range operator()(Particle *p) const {
+    return Range(p->float_keys_begin(), p->float_keys_end());
+  }
+};
+
+struct GetString {
+  typedef std::pair<Particle::StringKeyIterator,
+                    Particle::StringKeyIterator> Range;
+  Range operator()(Particle *p) const {
+    return Range(p->string_keys_begin(), p->string_keys_end());
+  }
+};
+
+struct GetParticle {
+  typedef std::pair<Particle::ParticleKeyIterator,
+                    Particle::ParticleKeyIterator> Range;
+  Range operator()(Particle *p) const {
+    return Range(p->particle_keys_begin(), p->particle_keys_end());
+  }
+};
+
+
+struct GetInt {
+  typedef std::pair<Particle::IntKeyIterator,
+                    Particle::IntKeyIterator> Range;
+  Range operator()(Particle *p) const {
+    return Range(p->int_keys_begin(), p->int_keys_end());
+  }
+};
+
+template <class It>
+BS get_bitset(It b, It e) {
+  BS ret;
+  for (It c= b; c != e; ++c) {
+    unsigned int i= c->get_index();
+    if (i >= ret.size()) {
+      ret.resize(i+1, false);
+    }
+    ret.set(i);
+  }
+  return ret;
+}
+
+
+struct LexLess {
+  bool operator()(const BS &a, const BS &b) {
+    if (a.size() < b.size()) return true;
+    else if (a.size() > b.size()) return false;
+    else return a< b;
+  }
+};
+typedef std::map<BS, unsigned int, LexLess> Map;
+
+template <class Get>
+Map get_attribute_usage(Get g, Model *m) {
+  Map bss;
+  for (Model::ParticleIterator pit= m->particles_begin();
+       pit != m->particles_end(); ++pit) {
+    BS cur= get_bitset(g(*pit).first, g(*pit).second);
+    if (bss.find(cur) == bss.end()) {
+      bss[cur]=1;
+    } else {
+      ++bss[cur];
+    }
+  }
+  return bss;
+}
+
+template <class Key>
+void show_map(const Map &m, std::ostream &out) {
+  size_t maxi=0;
+  for (Map::const_iterator it= m.begin(); it != m.end(); ++it) {
+    maxi= std::max(maxi, it->first.size());
+  }
+  for (size_t i=0; i< maxi; ++i) {
+    out << "\"" << Key::get_string(i) << "\" ";
+  }
+  out << std::endl;
+  for (Map::const_iterator it= m.begin(); it != m.end(); ++it) {
+    out << it->second << ": ";
+    BS bs= it->first;
+    for (unsigned int i=0; i< bs.size(); ++i) {
+      if (bs[i]) {
+        out << "X";
+      } else {
+        out << "-";
+      }
+    }
+    out << std::endl;
+  }
+}
+
+void show_attribute_usage(Model *m, std::ostream &out) {
+
+  Map fm= get_attribute_usage(GetFloat(), m);
+  Map sm= get_attribute_usage(GetString(), m);
+  Map pm= get_attribute_usage(GetParticle(), m);
+  Map im= get_attribute_usage(GetInt(), m);
+
+  out << "Float:\n";
+  show_map<FloatKey>(fm, out);
+  out << "String:\n";
+  show_map<StringKey>(sm, out);
+  out << "Int:\n";
+  show_map<IntKey>(im, out);
+  out << "Particle:\n";
+  show_map<ParticleKey>(pm, out);
+}
+
+IMPMISC_END_NAMESPACE
