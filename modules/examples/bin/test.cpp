c491be3032ebfe41bd6bf53d0ff4d320c4012036
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-01-28 22:59:53 +0000

    make it actually build
    git-svn-id: https://svn.salilab.org/imp/trunk@1329 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/examples/bin/test.cpp
+++ modules/examples/bin/test.cpp
@@ -4,7 +4,8 @@
 
 #include <IMP.h>
 #include <IMP/core.h>
-#include <IMP/examples/MyRestraint.h>
+#include <IMP/examples/ExampleRestraint.h>
+#include <IMP/core/ListPairContainer.h>
 
 using namespace IMP;
 int main(){
@@ -16,9 +17,11 @@ int main(){
   core::XYZDecorator d1= core::XYZDecorator::create(p1);
   core::DistancePairScore *dps
     = new core::DistancePairScore(new core::Linear(0,1));
-  examples::MyRestraint *r= new examples::MyRestraint(dps, p0, p1);
-  d0.set_coordinates(Vector3D(0,0,0));
-  d1.set_coordinates(Vector3D(0,0,1));
+  core::ListPairContainer *pc= new core::ListPairContainer();
+  pc->add_particle_pair(ParticlePair(p0, p1));
+  examples::ExampleRestraint *r= new examples::ExampleRestraint(dps, pc);
+  d0.set_coordinates(algebra::Vector3D(0,0,0));
+  d1.set_coordinates(algebra::Vector3D(0,0,1));
 
   m->add_restraint(r);
   IMP_assert(std::abs(m->evaluate(false) -1) < .01, "Out of range");
