d3fd0fceaaface5f077d7754d34507a86da55120
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-01-28 16:31:56 +0000

    add identity_transformation function
    git-svn-id: https://svn.salilab.org/imp/trunk@1306 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/include/Transformation3D.h
+++ modules/algebra/include/Transformation3D.h
@@ -63,6 +63,9 @@ IMP_OUTPUT_OPERATOR(Transformation3D)
 IMPALGEBRAEXPORT Transformation3D
 transformation_from_rotation_around_vector(const Rotation3D &rot,
                                            const Vector3D &center);
+inline Transformation3D identity_transformation() {
+  return Transformation3D(identity_rotation(),Vector3D(0.0,0.0,0.0));
+}
 
 IMPALGEBRA_END_NAMESPACE
 
