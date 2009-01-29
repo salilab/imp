cc077cf79ff2e7dc355c71a5a744d7c3de9587cc
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-01-29 08:06:46 +0000

    add a class for handling permutations of transformation to particles
    git-svn-id: https://svn.salilab.org/imp/trunk@1343 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/domino/src/TransformationPermutationSampler.cpp
@@ -0,0 +1,22 @@
+/**
+ *  \file TransformationPermutationSampler.h
+ *  \brief The class samples all combinations of a discrete set to particles.
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ */
+#include "IMP/domino/TransformationPermutationSampler.h"
+IMPDOMINO_BEGIN_NAMESPACE
+
+TransformationPermutationSampler::TransformationPermutationSampler(
+        DiscreteSet *ds,Particles *ps, bool trans_from_orig)
+        :PermutationSampler(ds,ps),tu_(ps,trans_from_orig)
+{}
+void TransformationPermutationSampler::move2state(const CombState *cs) {
+  Particle *p;
+  for (std::map<Particle *,unsigned int>::const_iterator
+        it = cs->get_data()->begin();it != cs->get_data()->end(); it++) {
+    p = it->first;
+    tu_.move2state(p,ds_->get_state(it->second));
+  }
+}
+
+IMPDOMINO_END_NAMESPACE
