8211216c2850c8c0c85cba2d4bac6bc9a9942f81
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-01-29 07:36:05 +0000

    add a class for holding a discrete sampling space of rigid transformations
    git-svn-id: https://svn.salilab.org/imp/trunk@1341 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/domino/include/TransformationMappedDiscreteSet.h
@@ -0,0 +1,34 @@
+/**
+ * \file  TransformationMappedDiscreteSet.h
+ * \brief Holds a discrete sampling space of rigid transformations.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ */
+
+#ifndef IMPDOMINO_TRANSFORMATION_MAPPED_DISCRETE_SET_H
+#define IMPDOMINO_TRANSFORMATION_MAPPED_DISCRETE_SET_H
+
+#include "IMP/Particle.h"
+#include <map>
+#include  <sstream>
+#include "IMP/base_types.h"
+#include "config.h"
+#include "DiscreteSet.h"
+#include <IMP/algebra/Transformation3D.h>
+#include "IMP/domino/MappedDiscreteSet.h"
+
+IMPDOMINO_BEGIN_NAMESPACE
+
+class IMPDOMINOEXPORT TransformationMappedDiscreteSet : public MappedDiscreteSet
+{
+public:
+  //! Constructor
+  /**
+  /param[in] ps_target particles to be mapped on a discrete set
+   */
+  TransformationMappedDiscreteSet(Particles *ps_target);
+};
+
+IMPDOMINO_END_NAMESPACE
+
+#endif  /* IMPDOMINO_TRANSFORMATION_MAPPED_DISCRETE_SET_H */
