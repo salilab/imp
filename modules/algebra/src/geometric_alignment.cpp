9b69285f83324e0a6215ce5915916a88a99149e1
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-01-30 02:28:05 +0000

    throw an exception if the point sets are degenerate rather than sometimes returning garbage
    git-svn-id: https://svn.salilab.org/imp/trunk@1363 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/src/geometric_alignment.cpp
+++ modules/algebra/src/geometric_alignment.cpp
@@ -10,6 +10,7 @@
 #include "IMP/algebra/geometric_alignment.h"
 #include <IMP/algebra/internal/tnt_array2d.h>
 #include <IMP/algebra/internal/jama_svd.h>
+#include <IMP/log.h>
 
 IMPALGEBRA_BEGIN_NAMESPACE
 
@@ -26,10 +27,13 @@ rigid_align_first_to_second(const std::vector<Vector3D> &from,
     //double x= p_it->x();
     center_from+= from[i];
     center_to += to[i];
+    IMP_LOG(VERBOSE, i << ": (" << from[i] << ") (" << to[i] << ")\n");
   }
   center_from = center_from/from.size();
   center_to = center_to/to.size();
 
+  IMP_LOG(VERBOSE, "Centers are (" << center_from << ") (" << center_to
+          << ")\n");
   std::vector<Vector3D> shifted_from(from.size()), shifted_to(to.size());
   for (unsigned int i=0; i< from.size(); ++i) {
     shifted_from[i]= from[i]-center_from;
@@ -51,10 +55,35 @@ rigid_align_first_to_second(const std::vector<Vector3D> &from,
     }
   }
 
+  IMP_LOG(VERBOSE, "H is " << H << std::endl);
+
   internal::JAMA::SVD<double> svd(H);
   internal::TNT::Array2D<double> U(3, 3), V(3, 3);
   svd.getU(U);
   svd.getV(V);
+
+  IMP_LOG(VERBOSE, "SVD is " << U << std::endl << V << std::endl);
+
+  internal::TNT::Array1D<double> SV;
+  svd.getSingularValues(SV);
+  double det= SV[0]*SV[1]*SV[2];
+  IMP_check(det > .00001,
+            "Degenerate point set. I can't align them. Sorry.",
+            ValueException);
+
+
+  IMP_IF_LOG(VERBOSE) {
+    internal::TNT::Array2D<double> Sigma(3,3, 0.0);
+
+    for (unsigned int i=0; i < 3; ++i) {
+      Sigma[i][i]= SV[i];
+    }
+
+    IMP_LOG(VERBOSE, "Reconstructed is "
+            << internal::TNT::matmult(internal::TNT::matmult(U,Sigma),
+                                      internal::TNT::transpose(V))
+            << std::endl);
+  }
 
   // the rotation matrix is R = VU^T
   internal::TNT::Array2D<double> UT = internal::TNT::transpose(U);
@@ -63,6 +92,7 @@ rigid_align_first_to_second(const std::vector<Vector3D> &from,
 
   // check for reflection
   if (determinant(rot) < 0) {
+    IMP_LOG(VERBOSE, "Flipping matrix"<<std::endl);
     internal::TNT::Array2D<double> VT = internal::TNT::transpose(V);
     internal::TNT::Array2D<double> UVT = internal::TNT::matmult(U, VT);
     internal::TNT::Array2D<double> S(3, 3);
@@ -72,13 +102,29 @@ rigid_align_first_to_second(const std::vector<Vector3D> &from,
     rot = internal::TNT::matmult(internal::TNT::matmult(U, S), VT);
   }
 
+  IMP_LOG(VERBOSE, "Rotation matrix is " << rot << std::endl);
+
   Rotation3D rotation
     = rotation_from_matrix(rot[0][0], rot[0][1], rot[0][2],
                            rot[1][0], rot[1][1], rot[1][2],
                            rot[2][0], rot[2][1], rot[2][2]);
+  IMP_LOG(VERBOSE, "Rotation is " << rotation << std::endl);
   Vector3D translation=center_to - rotation.rotate(center_from);
 
-  return Transformation3D(rotation, translation);
+  Transformation3D ret(rotation, translation);
+  // must be removed from final code
+  IMP_IF_CHECK(EXPENSIVE) {
+    for (unsigned int i=0; i< from.size(); ++i) {
+      Vector3D ft= ret.transform(from[i]);
+      IMP_assert((ft-to[i]).get_squared_magnitude()
+                 < std::max(from[i].get_squared_magnitude(),
+                            to[i].get_squared_magnitude()) *.0001,
+                 "The transform does not align the vectors "
+                 << from[i] << ": " << to[i] << ": " << ft
+                 << std::endl);
+    }
+  }
+  return ret;
 }
 
 
