62497ff8d7a9342798124ee256f9a63bc78b41bb
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-02-06 01:36:51 +0000

    add LogPairScore which just keeps track of what particles it is called with
    git-svn-id: https://svn.salilab.org/imp/trunk@1461 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/misc/include/LogPairScore.h
@@ -0,0 +1,60 @@
+/**
+ *  \file LogPairScore.h
+ *  \brief Track the particles pairs passed to the pair score.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ */
+
+#ifndef IMPMISC_LOG_PAIR_SCORE_H
+#define IMPMISC_LOG_PAIR_SCORE_H
+
+#include "config.h"
+
+#include <IMP/PairScore.h>
+#include <map>
+
+IMPMISC_BEGIN_NAMESPACE
+
+//! Track the pairs of particles passed.
+/** Primarily for testing.
+ */
+class LogPairScore : public PairScore
+{
+  mutable std::map<ParticlePair, unsigned int> map_;
+ public:
+  //! create with an empty map
+  LogPairScore(){}
+  virtual ~LogPairScore(){}
+  virtual Float evaluate(Particle *a, Particle *b,
+                         DerivativeAccumulator *) const {
+    ParticlePair pp(a,b);
+    if (map_.find(pp) == map_.end()) {
+      map_[pp]=0;
+    }
+    ++map_[pp];
+  }
+  //! Get a list of all pairs (without multiplicity)
+  ParticlePairs get_particle_pairs() const {
+    ParticlePairs ret;
+    for (std::map<ParticlePair, unsigned int>::const_iterator
+           it = map_.begin(); it != map_.end(); ++it) {
+      ret.push_back(it->first);
+    }
+    return ret;
+  }
+  //! Clear the lst of pairs
+  void clear() {
+    map_.clear();
+  }
+  virtual void show(std::ostream &out=std::cout) const {
+    out << "LogPS"<<std::endl;
+  }
+  //! Return true if the pair is in the list
+  bool get_contains(const ParticlePair &pp) const {
+    return map_.find(pp) != map_.end();
+  }
+};
+
+IMPMISC_END_NAMESPACE
+
+#endif  /* IMPMISC_LOG_PAIR_SCORE_H */
--- modules/misc/pyext/misc.i
+++ modules/misc/pyext/misc.i
@@ -37,3 +37,4 @@ namespace IMP::misc {
 %include "IMP/misc/LowestRefinedPairScore.h"
 %include "IMP/misc/RefineOncePairScore.h"
 %include "IMP/misc/attribute_statistics.h"
+%include "IMP/misc/LogPairScore.h"
