038728ae97fae650cb764800c971d0e6860b432e
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-02-06 21:53:48 +0000

    replace old website with new
    git-svn-id: https://svn.salilab.org/imp/trunk@1479 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- website/index.php
+++ website/index.php
@@ -1,56 +1,159 @@
-<?php
-  include "functions.inc";
-  write_header("index");
+<?PHP
+   include("inc/conf.inc.php");
+   include("inc/site.inc.php");
+   include("inc/misc.inc.php");
+   $page = $_GET['page'];
+   if (!$page) { $page = "home"; }
 ?>
 
-<h2>Purpose of IMP</h2>
 
-<p><img class="figure" style="float:right" src="exp_cycle.png"
-        alt="experimental cycle">
-</p>
+<?PHP
+   if ($page == "home") {
+   } elseif ($page == "imp") {
+   } elseif ($page == "groups") {
+   } elseif ($page == "pubs") {
+   } else {
+     #ini_set('display_errors',1);
+     #error_reporting(E_ALL|E_STRICT);
+     header("HTTP/1.1 404 Not Found");
+     echo "<!DOCTYPE HTML PUBLIC \"-//IETF//DTD HTML 2.0//EN\">";
+     echo "<html><head>";
+     echo "<title>404 Not Found</title>";
+     echo "</head><body>";
+     echo "<h1>Not Found</h1>";
+     echo "<p>The requested page ";
+     # should be OK since it is just being sent back to the user
+     echo $page;
+     echo " was not found on this server.</p>";
+     echo "<hr>";
+     echo "</body></html>";
 
-<p>Our broad goal is to contribute to a comprehensive structural
-characterization of biomolecules ranging in size and complexity from small
-peptides to large macromolecular assemblies, such as the ribosome and the
-<a href="/npc.html">nuclear pore complex (NPC)</a>.
-Detailed structural characterization of assemblies
-is generally impossible by any single existing experimental or computational
-method. We suggest that this barrier can be overcome by hybrid approaches
-that integrate data from diverse biochemical and biophysical experiments
-(eg, x-ray crystallography, NMR spectroscopy, electron microscopy,
-immuno-electron microscopy, footprinting, chemical cross-linking,
-FRET spectroscopy, small angle X-ray scattering, immunoprecipitation,
-and genetic interactions). Even a coarse characterization of the configuration
-of macromolecular components in a complex (ie, the molecular architecture)
-helps to elucidate the principles that underlie cellular processes, in
-addition to providing a necessary starting point for a higher resolution
-description.</p>
-
-<p><a href="alber_fig_5b.jpg">
-<img class="linkfigure" style="float:left" src="alber_fig_5b.jpg" alt="NPC"
-     width="434" height="364">
-</a></p>
-
-<p>We formulate the hybrid approach to structure determination as an
-optimization problem, the solution of which requires three main components:
-the representation of the assembly, the scoring function, and the optimization
-method. The ensemble of solutions to the optimization problem embodies the
-most accurate structural characterization given the available information.
-A preliminary version of this approach was used to determine the configuration
-of the 456 proteins in the yeast NPC14. The key challenges remain translating
-experimental data into restraints on the structure of the assembly, combining
-these spatial restraints into a single scoring function, optimizing the
-scoring function, and analyzing the resulting ensemble of solutions.</p>
-
-<p>To address these challenges, we are creating an Integrative Modeling
-Platform (IMP). IMP is designed to allow mixing and matching of existing
-modeling components as well as easy adding of new functionality. It will
-support a wide variety of assembly representations and input data. It will
-provide infrastructure that encourages and supports contributions from
-other laboratories and allows integration with existing modeling packages.</p>
-
-<p style="clear:both">
-
-<?php
-  write_footer();
+     exit;
+   }
 ?>
+
+<title>IMP Community</title>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<meta name="description" content="IMP Community site for the development of an Integrative Modeling Platform" />
+<meta name="keywords" content="IMP, Integrative, Modeling, Platform" />
+<link href="style/imp.css" rel="stylesheet" type="text/css" />
+<link rel="shortcut icon" type="image/x-icon" href="images/favicon.ico" />
+</head>
+
+<script><!--
+/* e-mail obfuscation adapted from code by Jason Johnston:
+   http://lojjic.net/blog/20030828-142754.rdf.html
+*/
+function linkEmail() {
+  if(!document.getElementsByTagName) return;
+  var allElts = document.getElementsByTagName("*");
+  if(allElts.length == 0 && document.all) 
+    allElts = document.all; //hack for IE5
+  for(var i=0; i<allElts.length; i++) {
+    var elt = allElts[i];
+    var className = elt.className || elt.getAttribute("class") 
+      || elt.getAttribute("className");
+    if(className && className.match(/\be-mail\b/)
+        && elt.firstChild.nodeType == 3) {
+      var alls = elt.firstChild.nodeValue;
+      fs= alls.split("|")
+      addr = fs[0].replace(/\ at\ /i, "@")
+        .replace(/\ (dot|period)\ /gi, ".");
+      var lnk = document.createElement("a");
+      lnk.setAttribute("href","mailto:"+addr);
+      lnk.appendChild(document.createTextNode(fs[1]));
+      elt.replaceChild(lnk, elt.firstChild);
+    }
+  }
+}
+window.onload = linkEmail;
+-->
+</script>
+
+<body>
+
+
+<center>
+
+<div id="nav">
+   <table class="tnav">
+      <tr>
+         <td><a href="<? echo $GLOBALS["home"]; ?>"><img src="images/imp_logo.png"></a></td>
+         <td>
+            <div id="links">
+               <?PHP GetLinks(); ?>
+            </div>
+         </td>
+      </tr>
+   </table>
+</div>
+
+
+<div id="headerline">
+</div>
+
+<div id="container">
+  <table class="tcon">
+      <tr>
+         <td class="twel" colspan="3">
+            <?PHP
+               if ($page == "home") {
+                  echo "<h2>Welcome</h2>
+                        <div id=\"goal\">";
+                  PrintFile("pages/goal.txt");              
+                  echo "</div>";
+               } elseif ($page == "imp") {
+                  #echo "<h2>Integrative Modeling Platform</h2>
+                  echo "<div id=\"imp\">";
+                  PrintFile("pages/imp.txt");              
+                  echo "</div>"; 
+               } elseif ($page == "groups") {
+                  echo "<h2>IMP Community groups</h2>
+                        <div id=\"groups\">";
+                  PrintFile("pages/groups.txt");              
+                  echo "</div>"; 
+               } elseif ($page == "pubs") {
+                  echo "<h2>IMP Publications</h2>
+                        <div id=\"pubs\">";
+                  GetPublications();              
+                  echo "</div>"; 
+               } else {
+                  error_log("Invalid page snuck through"+$page);
+               }
+            ?>
+         </td>
+      </tr>
+   <?PHP
+      if ($page == "home") {
+      echo "<tr>
+               <td class=\"tnews\">
+                  <h3>News</h3>
+                  <div id=\"high\">";
+                     PrintFile("pages/news.txt");
+      echo "      </div>
+               </td>
+               <td class=\"thres\">
+                  <h3>Highlighted research</h3>
+                  <div id=\"high\">";
+                     PrintFile("pages/hresearch.txt");
+      echo "      </div>
+               </td>
+               <td class=\"thdev\">
+                  <h3>Highlighted development</h3>
+                  <div id=\"high\">";
+                     PrintFile("pages/hdevelopment.txt");
+      echo "      </div>
+               </td>
+            </tr>";
+      }
+   ?>
+   </table>
+</div>
+
+<div id="footer">
+   <?PHP PrintFile("pages/footer.txt"); ?>
+</div>
+
+</center>
+</body>
+</html>
