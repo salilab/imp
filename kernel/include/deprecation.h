994f76d902887796b7f7bfa0ac4779a6fcb84675
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-02-07 00:11:30 +0000

    streamline documentation a bit. Rearranged the user/developer docs and removed a lot of redundant text from the developer docs. Unfortunately, it is still a bit long
    git-svn-id: https://svn.salilab.org/imp/trunk@1484 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/include/deprecation.h
+++ kernel/include/deprecation.h
@@ -17,14 +17,30 @@ IMP_BEGIN_NAMESPACE
 //! Toggle printing of warnings on using deprecated classes
 /** If set to true (the default) a warning is printed every
     time a class marked as deprecated is used.
+    \sa IMP_DEPRECATED
  */
 IMPEXPORT void set_print_deprecation_messages(bool tf);
 
 IMP_END_NAMESPACE
 
-/** Mark the class as deprecated. It will print out a message.
-    \param[in] old_classname The class which is deprecated.
-    \param[in] replacement_classname The class which replaces is.
+/** \brief Mark the class as deprecated. It will print out a message.
+
+  From time to time, \imp is updated in ways that break backward
+  compatibility.  In certain cases we will leave the old functionality
+  in so as not to break existing code which uses \imp. Such code is
+  said to be "deprecated". See \salilab{imp/doc/doxygen/deprecated.html,
+  the deprecated class list} for a list of such classes.
+
+  Deprecated classes are marked in a variety of ways:
+  - They are listed in the "Deprecated List" in the "Related Pages" tab.
+  - They are noted as deprecated in their documentation.
+  - They print an warning when an instance is constructed or the function
+    is called.
+
+  The warnings can be turned off using the
+  IMP::core::set_print_deprecation_messages function.
+  \param[in] old_classname The class which is deprecated.
+  \param[in] replacement_classname The class which replaces is.
  */
 #define IMP_DEPRECATED(old_classname, replacement_classname)           \
   if (::IMP::internal::get_print_deprecation_message(#old_classname)) { \
