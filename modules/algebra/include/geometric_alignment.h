88b315b55506c71a3784eae4a3c98c8ad88420df
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-02-11 02:33:04 +0000

    add docs. I can't build the formula on my mac, lets see if latex works for the nightly build
    git-svn-id: https://svn.salilab.org/imp/trunk@1500 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/include/geometric_alignment.h
+++ modules/algebra/include/geometric_alignment.h
@@ -18,6 +18,18 @@
 
 IMPALGEBRA_BEGIN_NAMESPACE
 
+//! Compute the rigid transform bringing the first point set to the second
+/** The points are assumed to be corresponding (that is, from[0] is aligned
+    to to[0] etc.). The alignment computed is that which minimized the
+    sum of squared distances between corresponding points. That is
+    \f$ \min_T \sum \left|T\left(f\left[i\right]\right)-t[i]\right|^2 \f$.
+
+    If the point sets are only 1 or 2 dimensional, the alignment algorithm
+    is unstable and not guaranteed to work. A warning is printed in this
+    case.
+
+    \throw ValueException if from.size() != to.size().
+ */
 IMPALGEBRAEXPORT Transformation3D
 rigid_align_first_to_second(const std::vector<Vector3D> &from,
                             const std::vector<Vector3D> &to);
