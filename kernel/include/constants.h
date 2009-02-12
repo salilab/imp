ff08a2eba13807762889ad9564a67891a3beb551
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-02-12 23:25:10 +0000

    it helps to include all the files
    git-svn-id: https://svn.salilab.org/imp/trunk@1521 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ kernel/include/constants.h
@@ -0,0 +1,28 @@
+/**
+ *  \file constants.h    \brief Various useful constants.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ *
+ */
+
+#ifndef IMP_CONSTANTS_H
+#define IMP_CONSTANTS_H
+
+#include "config.h"
+#include <cmath>
+
+IMP_BEGIN_NAMESPACE
+
+//! Avagadro's number
+static const double NA=6.02214179e-23;
+
+//! pi
+#if defined(_GLIBCXX_USE_C99_MATH) && defined(__GNUC__)
+static const double PI= M_PI;
+#else
+static const double PI = 3.1415926535897931;
+#endif // C99
+
+IMP_END_NAMESPACE
+
+#endif  /* IMP_CONSTANTS_H */
