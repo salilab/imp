d15913d0756fe7b241964d1eb0c98fae92f4db9f
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-02-13 02:03:29 +0000

    refactor tunnel
    git-svn-id: https://svn.salilab.org/imp/trunk@1528 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/misc/src/TunnelSingletonScore.cpp
+++ modules/misc/src/TunnelSingletonScore.cpp
@@ -17,10 +17,6 @@ IMPMISC_BEGIN_NAMESPACE
 TunnelSingletonScore::TunnelSingletonScore(UnaryFunction *f, FloatKey r) :
     f_(f), rk_(r)
 {
-  coordinate_=0;
-  center_= algebra::Vector3D(0,0,0);
-  height_=0;
-  radius_=0;
 }
 
 
@@ -28,81 +24,41 @@ TunnelSingletonScore::TunnelSingletonScore(UnaryFunction *f, FloatKey r) :
 Float TunnelSingletonScore::evaluate(Particle *p,
                                      DerivativeAccumulator *accum) const
 {
-  Float ret=0;
   core::XYZDecorator d(p);
   Float radius=0;
   if (rk_ != FloatKey() && p->has_attribute(rk_)) {
     radius = p->get_value(rk_);
   }
   IMP_LOG(VERBOSE, "Tunnel singleton score on particle " << d << std::endl);
-  Float hr= height_+radius;
-  if (d.get_coordinate(coordinate_) < center_[coordinate_] + hr
-      && d.get_coordinate(coordinate_) > center_[coordinate_] - hr) {
-    Float sd=0;
-    for (int i=1; i< 3; ++i) {
-      int oc= (i+coordinate_)%3;
-      sd+= square(d.get_coordinate(oc)- center_[oc]);
-    }
-    sd= std::sqrt(sd);
-    IMP_LOG(VERBOSE, "The distance is " << sd << " and radius "
-            << radius_ << std::endl);
-    if (sd > radius_-radius) {
-      Float rd= sd-radius_;
-      Float hdu= center_[coordinate_] + height_
-        - d.get_coordinate(coordinate_);
-      Float hdd=  d.get_coordinate(coordinate_)
-        + height_- center_[coordinate_];
-      algebra::Vector3D deriv(0,0,0);
-      Float score=0;
-      Float deriv_scalar=0;
-      /*! \todo Clean up these tests so I am not dependent on two expressions
-        being the same and evaluating to the same thing */
-      // look below if changed
-      Float dist= -std::min(std::min(rd, hdu), hdd) - radius;
-      if (accum) {
-        boost::tie(score, deriv_scalar)= f_->evaluate_with_derivative(dist);
-      } else {
-        score= f_->evaluate(dist);
-      }
-
-      // kind if evil
-      if (dist== -rd -radius) {
-        algebra::Vector3D v= (d.get_coordinates() -
-                                   center_).get_unit_vector();
-        for (int i=0; i< 2; ++i) {
-          int oc= (i+coordinate_+1)%3;
-          deriv[oc]= v[oc]*deriv_scalar;
-        }
-      } else {
-        // kind of evil
-        if (dist == -hdu -radius) {
-          deriv_scalar= -deriv_scalar;
-        }
-        deriv[coordinate_]= deriv_scalar;
-      }
-
-      if (accum) {
-        for (unsigned int i=0; i< 3; ++i) {
-          d.add_to_coordinate_derivative(i, deriv[i], *accum);
-        }
-      }
-      ret+= score;
-    } else {
-      IMP_LOG(VERBOSE, "Particle " << p->get_name()
-              << " is in channel" << std::endl);
-    }
+  Float dist= tr_.get_distance(d.get_coordinates());
+  if (dist > radius) {
+    IMP_LOG(VERBOSE, "Particle is too far: " << radius << " < " << dist
+            << std::endl);
+    return 0;
   } else {
-    IMP_LOG(VERBOSE, "Particle " << p->get_name()
-            << " is outside of slab" << std::endl);
+    Float adist= radius-dist;
+    if (accum) {
+      algebra::Vector3D v
+        = tr_.get_displacement_unit_vector(d.get_coordinates());
+
+      FloatPair val= f_->evaluate_with_derivative(-adist);
+      IMP_LOG(VERBOSE, "Result of eval on " << adist << " "
+              << radius << " " << dist
+              << " " << v << " " << val.first << " " << val.second
+              << std::endl);
+      algebra::Vector3D dv= v*val.second;
+      d.add_to_coordinates_derivative(dv, *accum);
+      return val.first;
+    } else {
+      Float val= f_->evaluate(-adist);
+      return val;
+    }
   }
-  return ret;
 }
 
 void TunnelSingletonScore::show(std::ostream& out) const
 {
-  out << "Tunnel score :" << *f_
-      << " " << center_ << " " << height_ << " "
-      << coordinate_ << std::endl;
+  out << "Tunnel score :" << *f_ << std::endl;
 }
 
 IMPMISC_END_NAMESPACE
