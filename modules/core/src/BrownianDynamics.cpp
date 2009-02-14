b1bf6c3b168b1623525596443209014ba82fb6a8
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-02-14 02:53:55 +0000

    simplify and clean up BD
    git-svn-id: https://svn.salilab.org/imp/trunk@1545 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/src/BrownianDynamics.cpp
+++ modules/core/src/BrownianDynamics.cpp
@@ -27,7 +27,7 @@ unit::Shift<unit::Multiply<unit::Pascal,
 
 
 BrownianDynamics::BrownianDynamics() :
-  max_change_(10), max_dt_(1e7), cur_dt_(max_dt_), cur_time_(0),
+  dt_(1e7), cur_time_(0),
   T_(IMP::internal::DEFAULT_TEMPERATURE)
 {
 }
@@ -42,9 +42,7 @@ IMP_LIST_IMPL(BrownianDynamics, Particle, particle, Particle*,,,);
 
 void BrownianDynamics::set_time_step(unit::Femtosecond t)
 {
-  time_steps_.clear();
-  max_dt_ = t;
-  cur_dt_= max_dt_;
+  dt_= t;
 }
 
 void BrownianDynamics::setup_particles()
@@ -77,125 +75,6 @@ void BrownianDynamics::setup_particles()
  */
 
 
-bool BrownianDynamics::step()
-{
-  std::vector<algebra::Vector3D> new_pos(get_number_of_particles());
-  bool noshrink=true;
-  while (!propose_step(new_pos)) {
-    cur_dt_= cur_dt_/2.0;
-    noshrink=false;
-  }
-
-  for (unsigned int i=0; i< get_number_of_particles(); ++i) {
-    XYZDecorator d(get_particle(i));
-    for (unsigned int j=0; j< 3; ++j) {
-      d.set_coordinate(j, new_pos[i][j]);
-    }
-  }
-  return noshrink;
-}
-
-
-//! Perform a single dynamics step.
-bool BrownianDynamics::propose_step(std::vector<algebra::Vector3D>&
-                                    new_pos)
-{
-  FloatKeys xyzk=XYZDecorator::get_xyz_keys();
-
-  IMP_LOG(VERBOSE, "dt is " << cur_dt_ << std::endl);
-
-
-  for (unsigned int i=0; i< get_number_of_particles(); ++i) {
-    Particle *p= get_particle(i);
-    XYZDecorator d(p);
-    IMP_IF_CHECK(CHEAP) {
-      for (unsigned int j=0; j< 3; ++j) {
-        // GDB 6.6 prints infinity as 0 on 64 bit machines. Grumble.
-        /*int szf= sizeof(Float);
-        int szi= sizeof(int);
-        Float one=1;*/
-        Float mx= std::numeric_limits<Float>::max();
-        Float c= d.get_coordinate(j);
-        bool ba= (c != c);
-        bool bb = (c >= mx);
-        bool bc= -d.get_coordinate(j) >= std::numeric_limits<Float>::max();
-        if (ba || bb || bc ) {
-          IMP_WARN("Bad value for coordinate in Brownian Dynamics on "
-                   << "particle " << p->get_name() << std::endl);
-          throw ValueException("Bad coordinate value");
-        }
-      }
-    }
-
-    //double xi= 6*pi*eta*radius; // kg/s
-    DiffusionDecorator dd(p);
-    unit::SquareCentimeterPerSecond D= dd.get_D();
-    IMP_check(D.get_value() > 0
-              && D.get_value() < std::numeric_limits<Float>::max(),
-              "Bad diffusion coefficient on particle " << p->get_name(),
-              ValueException);
-    unit::Angstrom sigma(compute_sigma_from_D(D));
-    IMP_IF_CHECK(EXPENSIVE) {
-      unit::Angstrom osigma(sqrt(2.0*D*cur_dt_));
-      IMP_check(sigma - osigma
-                 <= .01* sigma,
-                 "Sigma computations don't match " << sigma
-                 << " "
-                << sqrt(2.0*D*cur_dt_),
-                ErrorException);
-    }
-    IMP_LOG(VERBOSE, p->get_name() << ": sigma is "
-            << sigma << std::endl);
-    boost::normal_distribution<double> mrng(0, sigma.get_value());
-    boost::variate_generator<RandomNumberGenerator&,
-      boost::normal_distribution<double> >
-      sampler(random_number_generator, mrng);
-
-    //std::cout << p->get_name() << std::endl;
-
-    unit::Angstrom delta[3];
-
-    for (unsigned j = 0; j < 3; ++j) {
-      unit::KilocaloriePerAngstromPerMol
-        force( -d.get_coordinate_derivative(j));
-      unit::Femtonewton nforce
-        = unit::convert_Cal_to_J(force/unit::ATOMS_PER_MOL);
-      unit::Angstrom R(sampler());
-      unit::Angstrom force_term(nforce*cur_dt_*D/kt());
-      //std::cout << "Force term is " << force_term << " and R is "
-      //<< R << std::endl;
-      unit::Angstrom dr= force_term +  R; //std::sqrt(2*kb*T_*ldt/xi) *
-      // get back from meters
-      delta[j]=dr;
-    }
-    //unit::Angstrom max_motion= unit::Scalar(4)*sigma;
-    /*std::cout << "delta is " << delta << " mag is "
-      << delta.get_magnitude() << " sigma " << sigma << std::endl;*/
-
-    IMP_LOG(VERBOSE, "For particle " << p->get_name()
-            << " delta is " << delta[0] << " " << delta[1] << " " << delta[2]
-            << " from a force of "
-            << "[" << d.get_coordinate_derivative(0)
-            << ", " << d.get_coordinate_derivative(1)
-            << ", " << d.get_coordinate_derivative(2) << "]" << std::endl);
-
-    unit::SquareAngstrom t= square(delta[0]);
-
-    unit::SquareAngstrom magnitude2=square(delta[0])+square(delta[1])
-      +square(delta[2]);
-
-    //unit::SquaredAngstrom max_motion2= square(max_motion);
-    if (magnitude2 > square(max_change_)) {
-      return false;
-    }
-    for (unsigned int j=0; j< 3; ++j) {
-      new_pos[i][j]= d.get_coordinate(j) + delta[j].get_value();
-    }
-  }
-  return true;
-}
-
-
 //! Optimize the model.
 /** \param[in] max_steps   Maximum number of iterations before aborting.
     \return score of the final state of the model.
@@ -206,7 +85,7 @@ Float BrownianDynamics::optimize(unsigned int max_steps)
            ValueException);
   setup_particles();
   IMP_LOG(TERSE, "Running brownian dynamics on " << get_number_of_particles()
-          << " particles with a step of " << cur_dt_ << std::endl);
+          << " particles with a step of " << dt_ << std::endl);
   setup_particles();
   for (unsigned int i = 0; i < max_steps; ++i) {
     take_step();
@@ -218,22 +97,89 @@ Float BrownianDynamics::optimize(unsigned int max_steps)
 void BrownianDynamics::take_step() {
   update_states();
   get_model()->evaluate(true);
-  if (step()) ++ num_const_dt_;
 
-  cur_time_= cur_time_+cur_dt_;
-  if (num_const_dt_ == 10) {
-    num_const_dt_=0;
-    cur_dt_= std::min(cur_dt_*2.0, max_dt_);
-  }
-  {
-    unit::Femtosecond lb(max_dt_/2.0);
-    std::vector<int>::size_type d=0;
-    while (lb > cur_dt_) {
-      lb = lb/2.0;
-      ++d;
+ FloatKeys xyzk=XYZDecorator::get_xyz_keys();
+
+ IMP_LOG(VERBOSE, "dt is " << dt_ << std::endl);
+
+
+ for (unsigned int i=0; i< get_number_of_particles(); ++i) {
+   Particle *p= get_particle(i);
+   XYZDecorator d(p);
+   IMP_IF_CHECK(CHEAP) {
+     for (unsigned int j=0; j< 3; ++j) {
+       // GDB 6.6 prints infinity as 0 on 64 bit machines. Grumble.
+       /*int szf= sizeof(Float);
+         int szi= sizeof(int);
+         Float one=1;*/
+       Float mx= std::numeric_limits<Float>::max();
+       Float c= d.get_coordinate(j);
+       bool ba= (c != c);
+       bool bb = (c >= mx);
+       bool bc= -d.get_coordinate(j) >= std::numeric_limits<Float>::max();
+       if (ba || bb || bc ) {
+         IMP_WARN("Bad value for coordinate in Brownian Dynamics on "
+                  << "particle " << p->get_name() << std::endl);
+         throw ValueException("Bad coordinate value");
+       }
+     }
+   }
+
+   //double xi= 6*pi*eta*radius; // kg/s
+   DiffusionDecorator dd(p);
+   unit::SquareCentimeterPerSecond D= dd.get_D();
+   IMP_check(D.get_value() > 0
+             && D.get_value() < std::numeric_limits<Float>::max(),
+             "Bad diffusion coefficient on particle " << p->get_name(),
+             ValueException);
+   unit::Angstrom sigma(compute_sigma_from_D(D));
+   IMP_IF_CHECK(EXPENSIVE) {
+     unit::Angstrom osigma(sqrt(2.0*D*dt_));
+     IMP_check(sigma - osigma
+               <= .01* sigma,
+               "Sigma computations don't match " << sigma
+               << " "
+               << sqrt(2.0*D*dt_),
+               ErrorException);
+   }
+   IMP_LOG(VERBOSE, p->get_name() << ": sigma is "
+           << sigma << std::endl);
+   boost::normal_distribution<double> mrng(0, sigma.get_value());
+   boost::variate_generator<RandomNumberGenerator&,
+     boost::normal_distribution<double> >
+     sampler(random_number_generator, mrng);
+
+   //std::cout << p->get_name() << std::endl;
+
+   unit::Angstrom delta[3];
+
+   for (unsigned j = 0; j < 3; ++j) {
+     unit::KilocaloriePerAngstromPerMol
+       force( -d.get_coordinate_derivative(j));
+     unit::Femtonewton nforce
+       = unit::convert_Cal_to_J(force/unit::ATOMS_PER_MOL);
+     unit::Angstrom R(sampler());
+     unit::Angstrom force_term(nforce*dt_*D/kt());
+     //std::cout << "Force term is " << force_term << " and R is "
+     //<< R << std::endl;
+     unit::Angstrom dr= force_term +  R; //std::sqrt(2*kb*T_*ldt/xi) *
+     // get back from meters
+     delta[j]=dr;
+   }
+   //unit::Angstrom max_motion= unit::Scalar(4)*sigma;
+   /*std::cout << "delta is " << delta << " mag is "
+     << delta.get_magnitude() << " sigma " << sigma << std::endl;*/
+
+   IMP_LOG(VERBOSE, "For particle " << p->get_name()
+           << " delta is " << delta[0] << " " << delta[1] << " " << delta[2]
+           << " from a force of "
+           << "[" << d.get_coordinate_derivative(0)
+           << ", " << d.get_coordinate_derivative(1)
+           << ", " << d.get_coordinate_derivative(2) << "]" << std::endl);
+
+   for (unsigned int j=0; j< 3; ++j) {
+     d.set_coordinate(j, d.get_coordinate(j) + delta[j].get_value());
     }
-    time_steps_.resize(std::max(time_steps_.size(), d+1), 0);
-    ++time_steps_[d];
   }
 }
 
@@ -243,7 +189,6 @@ void BrownianDynamics::simulate(float max_time)
             ValueException);
   setup_particles();
   unit::Femtosecond mt(max_time);
-  num_const_dt_=0;
   while (cur_time_ < mt) {
     take_step();
   }
@@ -282,16 +227,16 @@ unit::Angstrom
 BrownianDynamics
 ::compute_sigma_from_D(unit::SquareCentimeterPerSecond D) const
 {
-  return sqrt(2.0*D*cur_dt_); //6*xi*kb*T_;
+  return sqrt(2.0*D*dt_); //6*xi*kb*T_;
 }
 
 unit::KilocaloriePerAngstromPerMol BrownianDynamics
 ::get_force_scale_from_D(unit::SquareCentimeterPerSecond D) const
 {
-  // force motion is f*cur_dt_*D/kT
-  // sigma*kT/(cur_dt_*D)
+  // force motion is f*dt_*D/kT
+  // sigma*kT/(dt_*D)
   unit::Angstrom s=compute_sigma_from_D(D);
-  unit::Piconewton pn= s*kt()/(max_dt_*D);
+  unit::Piconewton pn= s*kt()/(dt_*D);
   unit::YoctoKilocaloriePerAngstrom yc=unit::convert_J_to_Cal(pn);
   return unit::operator*(unit::ATOMS_PER_MOL,yc);
 }
