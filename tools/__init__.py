c7f316c574a529ca4e9c4c31cc76dd9c4ac0677b
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-02-16 23:18:13 +0000

    actually, get rid of the opt flag all the time as the general opt flags are propagated to the build of the wrapper
    git-svn-id: https://svn.salilab.org/imp/trunk@1555 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- tools/__init__.py
+++ tools/__init__.py
@@ -416,8 +416,7 @@ def get_pyext_environment(env, mod_prefix, cplusplus=False):
         # Remove options that don't work with C++ code:
         if cplusplus:
             opt = opt.replace("-Wstrict-prototypes", "")
-        if e.get('build', 'release') == 'debug':
-            opt=opt.replace("-O2", "")
+        opt=opt.replace("-O2", "")
         e.Replace(CC=cc, CXX=cxx, LDMODULESUFFIX=so)
         e.Replace(CPPFLAGS=basecflags.split() + opt.split())
         if e['PLATFORM'] is 'posix' and e['rpath']:
