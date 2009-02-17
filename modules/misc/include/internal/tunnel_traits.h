5a5b25cacc997aff81adfd57e2d3142b001ad619
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-02-17 23:01:21 +0000

    compute more accurate derivative vectors
    git-svn-id: https://svn.salilab.org/imp/trunk@1561 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/misc/include/internal/tunnel_traits.h
+++ modules/misc/include/internal/tunnel_traits.h
@@ -47,10 +47,15 @@ class TunnelTraits {
   double get_distance(const algebra::Vector3D &v) const {
     double hd= std::abs(get_height(v))-height_;
     double ret=0;
+    double r2=get_squared_distance_to_center(v);
     if (hd > 0) {
-      ret= hd;
+      if (r2 < square(radius_)) {
+        ret= std::sqrt(square(radius_-std::sqrt(r2))+ hd*hd);
+      } else {
+        ret= hd;
+      }
     } else {
-      Float rd= radius_-std::sqrt(get_squared_distance_to_center(v));
+      Float rd= radius_-std::sqrt(r2);
       if (rd > 0) return rd;
       if (rd < hd) {
         // closer to slab
@@ -59,7 +64,7 @@ class TunnelTraits {
         return rd;
       }
     }
-    IMP_LOG(VERBOSE, "Distance of " << v << " is " << ret << std::endl);
+    //IMP_LOG(VERBOSE, "Distance of " << v << " is " << ret << std::endl);
     return ret;
   }
   /*algebra::Vector3D get_closest_point(const algebra::Vector3D &v) const {
@@ -90,10 +95,10 @@ class TunnelTraits {
   algebra::Vector3D
   get_displacement_unit_vector(const algebra::Vector3D &v) const {
     double h= get_height(v);
-    double r= std::sqrt(get_squared_distance_to_center(v));
-    IMP_LOG(VERBOSE, "Distances are " << std::abs(std::abs(h)-height_)
-            << " and " << std::abs(r - radius_) << std::endl);
-    if (std::abs(std::abs(h)-height_) < std::abs(r - radius_)) {
+    double r2= get_squared_distance_to_center(v);
+    IMP_LOG(VERBOSE, "Distances are " << h*h
+            << " and " << r2 << std::endl);
+    if (std::abs(h)>height_) {
       // close to slab
       algebra::Vector3D diff(0,0,0);
       if (h > 0) {
@@ -101,20 +106,37 @@ class TunnelTraits {
       } else {
         diff[coordinate_]=-1;
       }
+      if (r2 < square(radius_)) {
+        algebra::Vector3D pv=(get_pole_point(v)-v).get_unit_vector();
+        pv= pv*std::abs(std::sqrt(r2)-radius_);
+        diff= diff.get_unit_vector()*(std::abs(h)-height_)+pv;
+        diff= diff.get_unit_vector();
+      } else {
+
+      }
       IMP_LOG(VERBOSE, "Displacement in slab is " << diff << std::endl);
+      {
+        algebra::Vector3D vb= v- diff*get_distance(v);
+        IMP_assert(std::abs(get_distance(vb)) < .01,
+                   "Stepping back should be close to surface: "
+                   << "stepping from " << v
+                   << " along " << diff
+                   << " of distance " << get_distance(v)
+                   << " gives distance " << get_distance(vb));
+      }
       return diff;
     } else {
       // close to cylinder
       algebra::Vector3D diff= (get_pole_point(v)-v);
       if (diff.get_squared_magnitude() < .00001){
-        algebra::Vector3D diff=algebra::Vector3D(0,0,0);
-        diff[coordinate_]=1;
+        diff=algebra::Vector3D(0,0,0);
+        diff[(coordinate_+1)%3]=1;
+        IMP_LOG(TERSE, "Particle is close to center" << diff << std::endl);
       } else {
         diff = diff.get_unit_vector();
       }
-      algebra::Vector3D ret= diff;
-      IMP_LOG(VERBOSE, "Displacement in cylinder is " << ret << std::endl);
-      return ret;
+      IMP_LOG(VERBOSE, "Displacement in cylinder is " << diff << std::endl);
+      return diff;
     }
   }
   double get_height(const algebra::Vector3D &v) const {
