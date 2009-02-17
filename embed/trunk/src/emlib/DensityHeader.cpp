c80bafd7f2824ba0b0b85fd165fd060e5df4dd6d
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-02-17 05:38:52 +0000

    add a function that updated the header of the map dimensions
    git-svn-id: https://svn.salilab.org/imp/trunk@2521 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- embed/trunk/src/emlib/DensityHeader.cpp
+++ embed/trunk/src/emlib/DensityHeader.cpp
@@ -10,3 +10,17 @@ void DensityHeader::compute_xyz_top()
   ztop=zorigin+Objectpixelsize*nz;
   top_calculated = true;
   }
+
+void DensityHeader::update_map_dimensions(int nnx,int nny,int nnz) {
+  nx=nnx;
+  ny=nny;
+  nz=nnz;
+  mx = nxstart + nx - 1; 
+  my = nystart + ny - 1; 
+  mz = nzstart + nz - 1;
+  top_calculated=false;
+  compute_xyz_top();
+  xlen=nx*Objectpixelsize;
+  ylen=ny*Objectpixelsize;
+  zlen=nz*Objectpixelsize;
+}
