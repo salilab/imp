3df698ce9509642e70c3355aa9b992277ed41b51
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-02-21 09:50:05 +0000

    updated surface density map code
    git-svn-id: https://svn.salilab.org/imp/trunk@2538 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- embed/trunk/src/emlib/SurfaceShellDensityMap.cpp
+++ embed/trunk/src/emlib/SurfaceShellDensityMap.cpp
@@ -2,61 +2,53 @@
 #include "limits.h"
 
 SurfaceShellDensityMap::SurfaceShellDensityMap() : SampledDensityMap() {
-  set_parameters();
 }
 
-SurfaceShellDensityMap::SurfaceShellDensityMap(const DensityHeader &header) 
+SurfaceShellDensityMap::SurfaceShellDensityMap(const DensityHeader &header)
                            : SampledDensityMap(header) {
-  set_parameters();
   set_neighbor_mask();
 }
 
 SurfaceShellDensityMap::SurfaceShellDensityMap(
       const ParticlesAccessPoint &access_p,
-      int num_shells, float resolution, float voxel_size,
-      int sig_cutoff)
-      :SampledDensityMap(access_p,resolution,voxel_size,sig_cutoff)
+      float voxel_size,int num_shells)
+  :SampledDensityMap(access_p,_RESOLUTION,voxel_size,_SIG_CUTOFF)
 {
-  set_parameters();
+  num_shells_=num_shells;
   set_neighbor_mask();
 }
 
 
 //TODO : think about the values for delta as a function of resolution
-void SurfaceShellDensityMap::set_neighbor_mask(emreal delta) {
+void SurfaceShellDensityMap::set_neighbor_mask() {
   for (int x = -1; x <= 1; x++) {
     for (int y = -1; y <= 1; y++) {
       for (int z = -1; z <= 1; z++) {
-        if (x == 0 && y == 0 && z == 0) 
+        if (x == 0 && y == 0 && z == 0)
           continue;
         neighbor_shift_.push_back(
               z * header_.nx * header_.ny + y * header_.nx + x);
-        neighbor_dist_.push_back(delta * sqrt((1.0*x*x + y*y + z*z)));
+        neighbor_dist_.push_back(
+            header_.Objectpixelsize * sqrt((1.0*x*x + y*y + z*z)));
       }
     }
-  }  
+  }
 }
-void SurfaceShellDensityMap::set_parameters() {
-  background_val_=-50.0;
-  surface_val_=0.0;
-}
-
-
-// TODO: binaries should be a special case of resample, either make 
+// TODO: binaries should be a special case of resample, either make
 //template or pass pointer to voxel update function
 // TODO: pass the background value as well to the general resample function
-
+//TODO: make this function faster
 void SurfaceShellDensityMap::binaries(const ParticlesAccessPoint &access_p,
-                                 float scene_val, float bg_val) {
-  reset_data(bg_val);
+                                 float scene_val) {
+  reset_data(_BACKGROUND_VAL);
   calc_all_voxel2loc();
   int  ivox, ivoxx, ivoxy, ivoxz, iminx, imaxx, iminy, imaxy, iminz, imaxz;
 
   // actual sampling
   float tmpx,tmpy,tmpz;
-  int nxny=header_.nx*header_.ny; 
+  int nxny=header_.nx*header_.ny;
   int znxny;
-  float rsq,tmp;  
+  float rsq,tmp;
 
   const  KernelParameters::Parameters *params;
   for (int ii=0; ii<access_p.get_size(); ii++) {
@@ -84,7 +76,7 @@ void SurfaceShellDensityMap::binaries(const ParticlesAccessPoint &access_p,
           tmpy=y_loc_[ivox] - access_p.get_y(ii);
           tmpz=z_loc_[ivox] - access_p.get_z(ii);
           rsq = tmpx*tmpx+tmpy*tmpy+tmpz*tmpz;
-          tmp = EXP(-rsq * params->get_inv_sigsq()); 
+          tmp = EXP(-rsq * params->get_inv_sigsq());
           //tmp = exp(-rsq * params->get_inv_sigsq());
           // if statement to ensure even sampling within the box
           if (tmp>kernel_params_.get_lim())
@@ -107,7 +99,7 @@ bool SurfaceShellDensityMap::has_background_neighbor(long voxel_ind) {
   for (unsigned int j = 0; j < neighbor_shift_.size(); j++) {
     n_voxel_ind = voxel_ind + neighbor_shift_[j];
     if ((n_voxel_ind>-1) && (n_voxel_ind<num_voxels)) {
-      if (data_[n_voxel_ind] == background_val_) {
+      if (data_[n_voxel_ind] == _BACKGROUND_VAL) {
         return true;
       }
     }
@@ -116,11 +108,11 @@ bool SurfaceShellDensityMap::has_background_neighbor(long voxel_ind) {
 }
 
 void SurfaceShellDensityMap::set_surface_shell(std::vector<long> *shell) {
-  //a voxel is part of the outher shell if it has at least one 
+  //a voxel is part of the outher shell if it has at least one
   //background nieghbor
-  for(long i=0;i<header_.nx * header_.ny * header_.nz;i++) {
+  for(long i=0;i<get_number_of_voxels();i++) {
     if (has_background_neighbor(i)) {
-      data_[i] = surface_val_;
+      data_[i] = _SURFACE_VAL;
       shell->push_back(i);
     }
   }
@@ -129,55 +121,52 @@ void SurfaceShellDensityMap::set_surface_shell(std::vector<long> *shell) {
 
 // Computes distance function for the molecule.
 // The voxels corresponding to surface points are given zero distance
-void SurfaceShellDensityMap::compute_shells(
-            const ParticlesAccessPoint &access_p,
-            int num_shells) {
-
-  //all scene voxels will be assigned the value -background_val_ 
+void SurfaceShellDensityMap::resample(const ParticlesAccessPoint &access_p) {
+  //all scene voxels will be assigned the value -background_val_
   //(which is positive and larger than 0)
-  //TODO - change here, the value of the inner voxels should not be 
+  //TODO - change here, the value of the inner voxels should note be
   //should not be ns*2 but the largest of the inner shell
-  binaries(access_p,num_shells*2,background_val_);
+  binaries(access_p,num_shells_*2);
 
-  //find the voxeles that are part of the surface, so we'll have 
+  //find the voxeles that are part of the surface, so we'll have
   //background, surface and interior voxels
-  std::vector<long> curr_shell_voxels; 
+  std::vector<long> curr_shell_voxels;
   //all of the voxels that are part of the current shell
   set_surface_shell(&curr_shell_voxels);
   //all of the voxels that are part of the next shell
-  std::vector<long> next_shell_voxels; 
+  std::vector<long> next_shell_voxels;
   //keeps the shell index for each of the data voxels
-  std::vector<int> shell_voxels; 
-  shell_voxels.insert(
-             shell_voxels.end(),header_.nx * header_.ny * header_.nz,-1);
-  for(long i=0;i<header_.nx * header_.ny * header_.nz;i++) {
-    if (data_[i] == surface_val_) {
+  std::vector<int> shell_voxels;
+  shell_voxels.insert(shell_voxels.end(),get_number_of_voxels(),-1);
+  for(long i=0;i<get_number_of_voxels();i++) {
+    if (data_[i] == _SURFACE_VAL) {
       shell_voxels[i]=0;
     }
   }
-
   long n_voxel_ind,voxel_ind;
-  float val; //the value is the distance of the voxel from the surface
+  float dist_from_surface; //the value is the distance of the voxel
+                           //from the surface
   std::vector<long> *curr_p = &curr_shell_voxels;
   std::vector<long> *next_p = &next_shell_voxels;
   std::vector<long> *tmp_p;
-  long num_voxels = header_.nx * header_.ny * header_.nz;
-  for (int s_ind = 0; s_ind <num_shells; s_ind++) {
-    // update voxels with current layer distance and insert indexes 
+  long num_voxels = get_number_of_voxels();
+  for (int s_ind = 0; s_ind <num_shells_; s_ind++) {
+    // update voxels with current layer distance and insert indexes
     //for next shell
-    for(std::vector<long>::iterator it =  curr_p->begin(); 
+    for(std::vector<long>::iterator it =  curr_p->begin();
                                     it != curr_p->end();it++) {
       voxel_ind = *it;
       for (unsigned int j = 0; j < neighbor_shift_.size(); j++) {
         n_voxel_ind = voxel_ind + neighbor_shift_[j];
+                     //the index of the neighbor
         if ((n_voxel_ind>-1)&&(n_voxel_ind<num_voxels)) {
-            val = data_[voxel_ind] + neighbor_dist_[j];
+            dist_from_surface = data_[voxel_ind] + neighbor_dist_[j];
             //if the stored distance of the voxel (voxel_ind) from the surface
             //is larger than the current calculated one, update
-            if (data_[n_voxel_ind] > val) {
-              data_[n_voxel_ind] = val;
+            if (data_[n_voxel_ind] > dist_from_surface) {
+              data_[n_voxel_ind] = dist_from_surface;
               // set the voxels for the next shell
-              if (shell_voxels[n_voxel_ind] < s_ind + 1) { 
+              if (shell_voxels[n_voxel_ind] < s_ind + 1) {
                 next_p->push_back(n_voxel_ind);
                 shell_voxels[n_voxel_ind] = s_ind + 1;
               }
