2c3fa957b29160efb38007840c285030eb3cd2bf
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-02-22 20:09:38 +0000

    add classes to display segments/cylinders
    git-svn-id: https://svn.salilab.org/imp/trunk@1625 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/display/include/CylinderGeometry.h
@@ -0,0 +1,62 @@
+/**
+ *  \file CylinderGeometry.h
+ *  \brief Represent an CylinderDecorator particle with a sphere
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ */
+
+#ifndef IMPDISPLAY_CYLINDER_GEOMETRY_H
+#define IMPDISPLAY_CYLINDER_GEOMETRY_H
+
+#include "config.h"
+#include "macros.h"
+
+#include "internal/version_info.h"
+#include "geometry.h"
+#include <IMP/PairContainer.h>
+#include <IMP/algebra/Vector3D.h>
+
+IMPDISPLAY_BEGIN_NAMESPACE
+
+//! Represent a static sphere (or point)
+/**
+ */
+class IMPDISPLAYEXPORT CylinderGeometry: public Geometry
+{
+  algebra::Vector3D p_[2];
+  double radius_;
+  std::string name_;
+public:
+  //! Create a static cylinder or segment
+  /** If the radius is skipped, it is a segment. If the color is skipped,
+      the default color (gray) is used.
+  */
+  CylinderGeometry(const algebra::Vector3D& p0,
+                   const algebra::Vector3D& p1,
+                   double radius=0,
+                   Color color
+                   = Color(),
+                   std::string name= std::string());
+
+  virtual ~CylinderGeometry();
+
+  virtual Float get_size() const {
+    return radius_;
+  }
+
+  std::string get_name() const {
+    return name_;
+  }
+
+  virtual unsigned int get_dimension() const;
+  virtual algebra::Vector3D get_vertex(unsigned int i) const;
+  virtual VersionInfo get_version_info() const
+  {return internal::version_info;}
+  virtual void show(std::ostream &out=std::cout) const;
+
+  //IMP_GEOMETRY(internal::version_info)
+};
+
+IMPDISPLAY_END_NAMESPACE
+
+#endif  /* IMPDISPLAY_CYLINDER_GEOMETRY_H */
---
+++ modules/display/src/CylinderGeometry.cpp
@@ -0,0 +1,38 @@
+/**
+ *  \file CylinderGeometry.cpp
+ *  \brief XXXX.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ *
+ */
+
+#include "IMP/display/CylinderGeometry.h"
+
+
+IMPDISPLAY_BEGIN_NAMESPACE
+
+CylinderGeometry::CylinderGeometry(const algebra::Vector3D& p0,
+                                   const algebra::Vector3D& p1,
+                                   double radius,
+                                   Color color,
+                                   std::string name): radius_(radius),
+                                                      name_(name){
+  if (color != Color()) set_color(color);
+  p_[0]= p0;
+  p_[1]= p1;
+}
+
+CylinderGeometry::~CylinderGeometry(){}
+
+void CylinderGeometry::show(std::ostream &out) const {
+  out << "CylinderGeometry" << std::endl;
+}
+
+unsigned int CylinderGeometry::get_dimension() const{
+  return 1;
+}
+algebra::Vector3D CylinderGeometry::get_vertex(unsigned int i) const {
+  IMP_assert(i < 2, "Invalid vertex of cylinder");
+  return p_[i];
+}
+IMPDISPLAY_END_NAMESPACE
