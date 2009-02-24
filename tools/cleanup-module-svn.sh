17f730d801f70d2093f6e6d485a23b2aa8fb2dc6
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-02-24 19:04:18 +0000

    add to exclusions list
    git-svn-id: https://svn.salilab.org/imp/trunk@1656 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- tools/cleanup-module-svn.sh
+++ tools/cleanup-module-svn.sh
@@ -1,7 +1,23 @@
 #!/bin/sh
 echo "Module is " $1
+tmpfile="/tmp/props"
 svn propset svn:ignore config.h modules/$1/include
 svn propset svn:ignore version_info.h modules/$1/include/internal
-svn propset svn:ignore version_info.cpp modules/$1/src/internal
-echo "IMP.$1.py\n$1_config.i\n$1_wrap.cc\n$1_wrap.h\n*.os\n*.obj" > /tmp/props
-svn propset svn:ignore --file /tmp/props modules/$1/pyext
+echo "version_info.cpp
+link_0.cpp
+link_1.cpp" > $tmpfile
+svn propset svn:ignore --file $tmpfile  modules/$1/src/internal
+rm -rf $tmpfile
+echo "\*.os
+\*.obj" > $tmpfile
+svn propset svn:ignore --file $tmpfile  modules/$1/src/
+rm -rf $tmpfile
+echo "IMP.$1.py
+$1_config.i
+$1_wrap.cc
+$1_wrap.h
+*.os
+*.obj" > $tmpfile
+svn propset svn:ignore --file $tmpfile modules/$1/pyext
+svn propset svn:ignore "test.passed" modules/$1/test
+rm -rf $tmpfile
