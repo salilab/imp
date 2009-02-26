21169844f3ce9a3ede3e09a2ac99f963a0843bcb
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-02-26 20:18:14 +0000

    make macros work outside of display namespace
    git-svn-id: https://svn.salilab.org/imp/trunk@1690 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/display/include/macros.h
+++ modules/display/include/macros.h
@@ -1,6 +1,6 @@
 /**
  *  \file display/macros.h
- *  \brief XXXXXXXXXXXXXX
+ *  \brief macros for display classes
  *
  *  Copyright 2007-8 Sali Lab. All rights reserved.
  */
@@ -18,36 +18,36 @@
     in the destructor of your class.
  */
 #define IMP_WRITER(version)                                             \
-  virtual void add_geometry(Geometry *g);                               \
-  void on_open(std::string name);                               \
+  virtual void add_geometry(IMP::display::Geometry *g);                 \
+  void on_open(std::string name);                                       \
   virtual void on_close();                                              \
-  virtual VersionInfo get_version_info() const {return version;}        \
+  virtual IMP::VersionInfo get_version_info() const {return version;}   \
   virtual void show(std::ostream &out=std::cout) const;
 
 //! Since swig doesn't support using, this redefines the geometry methods
-#define IMP_WRITER_ADD_GEOMETRY                                 \
-  virtual void add_geometry(const Geometries &g) {              \
-    Writer::add_geometry(g);                                    \
-  }                                                             \
-  virtual void add_geometry(CompoundGeometry *cg) {             \
-    Writer::add_geometry(cg);                                   \
-  }                                                             \
-  virtual void add_geometry(const CompoundGeometries &g) {      \
-    Writer::add_geometry(g);                                    \
+#define IMP_WRITER_ADD_GEOMETRY                                         \
+  virtual void add_geometry(const IMP::display::Geometries &g) {        \
+    Writer::add_geometry(g);                                            \
+  }                                                                     \
+  virtual void add_geometry(IMP::display::CompoundGeometry *cg) {       \
+    Writer::add_geometry(cg);                                           \
+  }                                                                     \
+  virtual void add_geometry(const IMP::display::CompoundGeometries &g) { \
+    Writer::add_geometry(g);                                            \
   }
 
 //! Define information for an Geometry object
 #define IMP_GEOMETRY(version)                                           \
   virtual unsigned int get_dimension() const;                           \
-  virtual algebra::Vector3D get_vertex(unsigned int i) const;           \
-  virtual VersionInfo get_version_info() const {return version;}        \
+  virtual IMP::algebra::Vector3D get_vertex(unsigned int i) const;      \
+  virtual IMP::VersionInfo get_version_info() const {return version;}   \
   virtual unsigned int get_number_of_vertices() const;                  \
   virtual void show(std::ostream &out=std::cout) const;
 
 //! Define information for an Geometry object
 #define IMP_COMPOUND_GEOMETRY(version)                                  \
-  virtual Geometries get_geometry() const;                   \
-  virtual VersionInfo get_version_info() const {return version;}        \
+  virtual IMP::display::Geometries get_geometry() const;                \
+  virtual IMP::VersionInfo get_version_info() const {return version;}   \
   virtual void show(std::ostream &out=std::cout) const;
 
 
