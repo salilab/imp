fa9082dc6e65131f9e5b278dcf7e97e50fa09ac4
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-02-27 01:09:31 +0000

    style updates
    git-svn-id: https://svn.salilab.org/imp/trunk@1708 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/include/Cylinder3D.h
+++ modules/algebra/include/Cylinder3D.h
@@ -10,46 +10,41 @@
 #include <IMP/algebra/Vector3D.h>
 #include <IMP/algebra/Transformation3D.h>
 #include <iostream>
-#include <cmath>
+#include <IMP/constants.h>
 
-
-/** We represent a cylinder by :
-       center point
-       main direction
-       radius size
-       length
-this is a right cylinder
+/** The cylinder is represented by two points that define
+the cylinder direction and height. The radius of the cylinder
+is set by a third parameter.
 
 todo:
-- get_point(int)
-- remove _get{high,low}*
-- doc whether area includes caps
-- remove get_bild_string
-- get_main_direction->get_direction
-- remove PI
 - document everything
 - fix overview docs
 */
 IMPALGEBRA_BEGIN_NAMESPACE
-const double PI = 4.0*std::atan(1.0);
 class IMPALGEBRAEXPORT Cylinder3D
 {
  public:
-  Cylinder3D(const Vector3D &start,const Vector3D &end, Float radius);
+  Cylinder3D(const Vector3D &start,const Vector3D &end, double radius);
+  //! Get a transformation from the cylinder reference frame to the base one.
+  /**
+  The transformation places the cylinder direction on the Z-axis
+  */
   Transformation3D get_transformation_to_base_reference_frame() const;
   Vector3D get_center() const {return start_+(end_-start_)*0.5;}
-  Vector3D get_main_direction() const {return (end_-start_).get_unit_vector();}
-  Float get_radius() const {return radius_;}
-  Float get_height() const {return (end_-start_).get_magnitude();}
-  std::string get_bild_string() const;
-  Float get_area() const;
-  Float get_volume() const;
-  Vector3D get_low_base_point() const {return start_;}
-  Vector3D get_high_base_point() const {return start_;}
+  Vector3D get_direction() const {return (end_-start_).get_unit_vector();}
+  double get_radius() const {return radius_;}
+  double get_height() const {return (end_-start_).get_magnitude();}
+  double get_surface_area() const;
+  double get_volume() const;
+  //! Return one of the two points that define the cylinder
+  /**
+    /param[in] the point index (0 or 1).
+   */
+  Vector3D get_point(int i) const;
  protected:
   Vector3D start_;
   Vector3D end_;
-  Float radius_;
+  double radius_;
 };
 
 IMPALGEBRA_END_NAMESPACE
--- modules/algebra/src/Cylinder3D.cpp
+++ modules/algebra/src/Cylinder3D.cpp
@@ -9,11 +9,11 @@
 IMPALGEBRA_BEGIN_NAMESPACE
 Cylinder3D::Cylinder3D(const Vector3D &start,
                        const Vector3D &end,
-                       Float radius)
+                       double radius)
            :start_(start),end_(end),radius_(radius){
 }
 Transformation3D Cylinder3D::get_transformation_to_base_reference_frame() const{
-  Vector3D main_dir = get_main_direction();
+  Vector3D main_dir = get_direction();
   Vector3D vertical_dir = get_vertical_vector(main_dir);
   Transformation3D move2zero= Transformation3D(
                                 identity_rotation(),-get_center());
@@ -24,22 +24,29 @@ Transformation3D Cylinder3D::get_transformation_to_base_reference_frame() const{
                                      vertical_dir,
                                      vector_product(main_dir,vertical_dir),
                                      Vector3D(0.0,0.0,0.0));
-  return rigid_trans_inv.get_inverse().multiply(move2zero);
+  return rigid_trans_inv.get_inverse().compose(move2zero);
 }
 
-Float Cylinder3D::get_area() const {
+double Cylinder3D::get_surface_area() const {
   return 2.0*PI*radius_ * get_height() +
     2.0*PI *radius_*radius_;
 }
-Float Cylinder3D::get_volume() const {
+double Cylinder3D::get_volume() const {
   return PI *radius_*radius_ * get_height();
 }
-std::string Cylinder3D::get_bild_string() const {
-  std::ostringstream s;
-  s<<".cylinder " << start_[0] << " " << start_[1] << " " << start_[2]
-   << " " << end_[0] << " "<< end_[1]<<" " <<end_[2]<< " " << radius_
-   << " open "<<std::endl;
-  return s.str();
+// std::string Cylinder3D::get_bild_string() const {
+//   std::ostringstream s;
+//   s<<".cylinder " << start_[0] << " " << start_[1] << " " << start_[2]
+//    << " " << end_[0] << " "<< end_[1]<<" " <<end_[2]<< " " << radius_
+//    << " open "<<std::endl;
+//   return s.str();
+// }
+Vector3D Cylinder3D::get_point(int i) const {
+  IMP_assert((i == 1 || i == 0), "Invalid points index");
+  if (i==0) {
+    return start_;
+  }
+  return end_;
 }
 
 IMPALGEBRA_END_NAMESPACE
