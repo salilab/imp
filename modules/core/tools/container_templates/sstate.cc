d714ae65ac1544983434e65f36babbbf53d29c3b
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-03 07:07:33 +0000

    make show show more
    git-svn-id: https://svn.salilab.org/imp/trunk@1764 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/tools/container_templates/sstate.cc
+++ modules/core/tools/container_templates/sstate.cc
@@ -11,6 +11,7 @@
  */
 
 #include "IMP/core/GroupnameScoreState.h"
+#include "IMP/internal/container_helpers.h"
 
 IMPCORE_BEGIN_NAMESPACE
 
@@ -46,7 +47,14 @@ void GroupnameScoreState::do_after_evaluate(DerivativeAccumulator *da)
 }
 
 void GroupnameScoreState::show(std::ostream &out) const {
-  out << "GroupnameScoreState" << std::endl;
+  out << "GroupnameScoreState with ";
+  if (f_) out << *f_;
+  else out << "NULL";
+  out << " and ";
+  if (af_) out << *af_;
+  else out << "NULL";
+  out << " on ";
+  out << IMP::internal::streamable(v_).get_name() << std::endl;
 }
 
 IMPCORE_END_NAMESPACE
