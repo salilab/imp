3f118e7ca5d485334c9fd2811c1ce5a82b4fa004
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-03 07:40:23 +0000

    add helper function to make setup easier
    git-svn-id: https://svn.salilab.org/imp/trunk@1768 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/src/CoverRefined.cpp
+++ modules/core/src/CoverRefined.cpp
@@ -9,15 +9,18 @@
 
 #include "IMP/core/bond_decorators.h"
 #include "IMP/core/XYZRDecorator.h"
+#include "IMP/core/FixedParticleRefiner.h"
+#include "IMP/core/SingletonScoreState.h"
+#include "IMP/core/DerivativesToRefined.h"
 
 IMPCORE_BEGIN_NAMESPACE
 
 CoverRefined
 ::CoverRefined(ParticleRefiner *ref,
-                                FloatKey rk,
-                                  Float slack): ref_(ref),
-                                                rk_(rk),
-                                                slack_(slack)
+               FloatKey rk,
+               Float slack): ref_(ref),
+                             rk_(rk),
+                             slack_(slack)
 {
 }
 
@@ -28,6 +31,7 @@ CoverRefined::~CoverRefined()
 void CoverRefined::apply(Particle *p) const
 {
   XYZRDecorator dp(p, rk_);
+  IMP_CHECK_OBJECT(ref_.get());
   IMP_check(ref_->get_can_refine(p), "Passed particles cannot be refined",
             ValueException);
   Particles ps= ref_->get_refined(p);
@@ -38,7 +42,36 @@ void CoverRefined::apply(Particle *p) const
 
 void CoverRefined::show(std::ostream &out) const
 {
-  out << "CoverRefined" << std::endl;
+  out << "CoverRefined with "
+      << *ref_ << " and " << rk_ << std::endl;
 }
+
+
+Particle* create_cover_particle(Model *m, const Particles &ps,
+                                FloatKey radius_key, Float slack) {
+  IMP_check(!ps.empty(), "Need at least one particle to cover",
+            ValueException);
+  IMP_IF_CHECK(EXPENSIVE) {
+    for (unsigned int i=0; i< ps.size(); ++i) {
+      IMP_check(XYZRDecorator::is_instance_of(ps[i], radius_key),
+                "Particles must have radius attribute " << radius_key,
+                ValueException);
+    }
+  }
+  Particle *p= new Particle(m);
+  XYZRDecorator d= XYZRDecorator::create(p, algebra::Vector3D(0,0,0),
+                                         0, radius_key);
+  FixedParticleRefiner *fpr= new FixedParticleRefiner(ps);
+
+  CoverRefined *cr= new CoverRefined(fpr, radius_key, slack);
+  DerivativesToRefined *dtr= new DerivativesToRefined(fpr,
+                                    XYZDecorator::get_xyz_keys());
+  SingletonScoreState *sss= new SingletonScoreState(cr, dtr, p);
+  m->add_score_state(sss);
+  IMP_assert(fpr->get_refined(p).size() == ps.size(),
+             "FixedPR is broken");
+  return p;
+}
+
 
 IMPCORE_END_NAMESPACE
--- modules/core/test/modifiers/test_cover_refiner.py
+++ modules/core/test/modifiers/test_cover_refiner.py
@@ -8,6 +8,16 @@ import random
 class TestREFCover(IMP.test.TestCase):
     """Tests for RefinerCover"""
 
+    def check_cover(self, p, ps):
+        d= IMP.core.XYZRDecorator(p)
+        d.show()
+        for cp in ps:
+            dc= IMP.core.XYZRDecorator(cp)
+            dc.show()
+            #d.get_sphere().get_center().show()
+            #dc.get_sphere().get_center().show()
+            self.assert_(d.get_sphere().get_contains(dc.get_sphere()))
+
     def test_it(self):
         """Test cover refined"""
         m= IMP.Model()
@@ -28,8 +38,23 @@ class TestREFCover(IMP.test.TestCase):
             d.set_radius(random.uniform(0,1))
             hd.add_child(IMP.core.HierarchyDecorator.create(p))
         r= IMP.core.ChildrenParticleRefiner(IMP.core.HierarchyDecorator.get_default_traits())
-        c= IMP.core.CentroidOfRefined(r)
+        #c= IMP.core.CentroidOfRefined(r)
+        c= IMP.core.CoverRefined(r, IMP.core.XYZRDecorator.get_default_radius_key(), 1)
         c.apply(pp)
+        self.check_cover(pp, ps)
 
+    def test_it(self):
+        """Test cover refined helper"""
+        m= IMP.Model()
+        IMP.set_log_level(IMP.MEMORY)
+        n= random.randrange(1,10)
+        ps=IMP.core.create_xyzr_particles(m, 10, 1)
+        p= IMP.core.create_cover_particle(m, ps, IMP.core.XYZRDecorator.get_default_radius_key(), 1);
+        ss= m.get_score_states()
+        for s in ss:
+            s.show()
+        print "eval"
+        m.evaluate(True)
+        self.check_cover(p, ps)
 if __name__ == '__main__':
     unittest.main()
