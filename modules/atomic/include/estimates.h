eb5eef3b3ed57705070ba02458ee4506ede79ebf
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-04 02:26:46 +0000

    create the atomic module to put the estimate function (and since it is going to be created anyway). I'll copy the decorators over soon.
    git-svn-id: https://svn.salilab.org/imp/trunk@1785 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/atomic/include/estimates.h
@@ -0,0 +1,47 @@
+/**
+ *  \file estimates.h
+ *  \brief Estimates of various physical quantities.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ */
+
+#ifndef IMPATOMIC_ESTIMATES_H
+#define IMPATOMIC_ESTIMATES_H
+
+#include "config.h"
+#include <IMP/base_types.h>
+
+IMPATOMIC_BEGIN_NAMESPACE
+
+//! Estimate the volume of a protein from its mass
+/** The estimate uses a formula published in
+
+    <b>Average protein density is a molecular-weight-dependent function.</b>
+
+    Fischer H, Polikarpov I, Craievich AF.
+
+    The mass density of proteins is a relevant basic biophysical
+    quantity. It is also a useful input parameter, for example, for
+    three-dimensional structure determination by protein
+    crystallography and studies of protein oligomers in solution by
+    analytic ultracentrifugation. We have performed a critical
+    analysis of published, theoretical, and experimental
+    investigations about this issue and concluded that the average
+    density of proteins is not a constant as often assumed. For
+    proteins with a molecular weight below 20 kDa, the average density
+    exhibits a positive deviation that increases for decreasing
+    molecular weight. A simple molecular-weight-depending function is
+    proposed that provides a more accurate estimate of the average
+    protein density.
+
+    \note I don't like the abbreviations in the name, but it is too
+    long without them.
+
+    \note It is not clear that this belongs in IMP.atomic, but it doesn't
+    fit at all anywhere else.
+*/
+IMPATOMICEXPORT double volume_in_A3_from_mass_in_kDa(double v);
+
+IMPATOMIC_END_NAMESPACE
+
+#endif  /* IMPATOMIC_ESTIMATES_H */
