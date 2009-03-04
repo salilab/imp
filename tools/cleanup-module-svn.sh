6d261c961eae18d50fb8aac0ae2ef5b9a51237ca
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-04 03:47:54 +0000

    Ignore generated files.
    git-svn-id: https://svn.salilab.org/imp/trunk@1789 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- tools/cleanup-module-svn.sh
+++ tools/cleanup-module-svn.sh
@@ -3,7 +3,8 @@ echo "Module is " $1
 tmpfile="/tmp/props"
 svn propset svn:ignore config.h modules/$1/include
 svn propset svn:ignore version_info.h modules/$1/include/internal
-echo "version_info.cpp
+echo "*.os
+version_info.cpp
 link_0.cpp
 link_1.cpp" > $tmpfile
 svn propset svn:ignore --file $tmpfile  modules/$1/src/internal
@@ -19,5 +20,8 @@ $1_wrap.h
 *.os
 *.obj" > $tmpfile
 svn propset svn:ignore --file $tmpfile modules/$1/pyext
-svn propset svn:ignore "test.passed" modules/$1/test
+rm -rf $tmpfile
+echo "*.pyc
+test.passed" > $tmpfile
+svn propset svn:ignore --file $tmpfile modules/$1/test
 rm -rf $tmpfile
