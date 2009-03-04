985503c1771ace5c3e46e0ce7083a855353cfa86
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-04 19:41:28 +0000

    fix paths in error messages
    git-svn-id: https://svn.salilab.org/imp/trunk@1794 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- tools/run-all-tests.py
+++ tools/run-all-tests.py
@@ -1,13 +1,9 @@
 import unittest, sys, os, re
-#global path
+global path
 
 def regressionTest():
     """Run all tests in files called test_*.py in current directory and
        subdirectories"""
-    path ="."
-    if path == '':
-        path = '.'
-    # Tell test cases where to find any input files needed
     os.environ['TEST_DIRECTORY'] = path
     modobjs = []
     for subdir in [''] + [x for x in os.listdir(path) \
@@ -24,7 +20,7 @@ def regressionTest():
     return unittest.TestSuite(tests)
 
 if __name__ == "__main__":
-    #global path
-    #path = os.path.dirname(sys.argv[1])
-    #sys.argv=[sys.argv[0]]+sys.argv[2:]
+    path = sys.argv[1]
+    print "Print running tests in " + path
+    sys.argv=[sys.argv[0]]+sys.argv[2:]
     unittest.main(defaultTest="regressionTest")
