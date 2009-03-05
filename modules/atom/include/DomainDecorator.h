de683f82d0ce87782ee8751ee15cf3b2e45a7437
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-05 20:02:55 +0000

    add DomainDecorator
    git-svn-id: https://svn.salilab.org/imp/trunk@1826 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/atom/include/DomainDecorator.h
@@ -0,0 +1,61 @@
+/**
+ *  \file DomainDecorator.h
+ *  \brief A decorator for associating a MolecularHiearchy piece with a domain
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ */
+
+#ifndef IMPATOM_DOMAIN_DECORATOR_H
+#define IMPATOM_DOMAIN_DECORATOR_H
+
+#include "config.h"
+
+#include "internal/version_info.h"
+#include <IMP/Decorator.h>
+
+IMPATOM_BEGIN_NAMESPACE
+
+//! A decorator to associate a particle with a part of a protein
+/** The decorator stores the indexes of the first and last residues
+    in this domain.
+ */
+class IMPATOMEXPORT DomainDecorator: public Decorator
+{
+  class Data {
+    Data(): begin("domain_begin"),
+            end("domain_end"){}
+    IntKey begin, end;
+  };
+  static const Data &get_data();
+public:
+  //! Create a domain covering the range [b, e)
+  static DomainDecorator create(Particle *p, Int b, Int e) {
+    p->add_attribute(get_data().begin(), b);
+    p->add_attribute(get_data().end(), e);
+    return DomianDecorator(p);
+  }
+
+  virtual ~DomainDecorator();
+
+  static bool is_instance_of(Particle *p) {
+    return p->has_attribute(get_data().begin)
+      && p->has_attribute(get_data().end);
+  }
+
+  //! Get the index of the first residue in the domain
+  Int get_begin_index() const {
+    return get_particle()->get_attribute(get_data().begin);
+  }
+
+  //! Get the index of the first residue not in the domain
+  Int get_end_index() const {
+    return get_particle()->get_attribute(get_data().end);
+  }
+
+  IMP_DECORATOR(internal::version_info)
+};
+
+
+IMPATOM_END_NAMESPACE
+
+#endif  /* IMPATOM_DOMAIN_DECORATOR_H */
---
+++ modules/atom/src/DomainDecorator.cpp
@@ -0,0 +1,29 @@
+/**
+ *  \file DomainDecorator.cpp
+ *  \brief XXXX.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ *
+ */
+
+#include "IMP/atom/DomainDecorator.h"
+
+
+IMPATOM_BEGIN_NAMESPACE
+
+DomainDecorator::DomainDecorator(){
+}
+
+DomainDecorator::~DomainDecorator(){}
+
+const DomainDecorator::Data& DomainDecorator::get_data() {
+  static Data data;
+  return data;
+}
+
+void DomainDecorator::show(std::ostream &out) const {
+  out << "DomainDecorator: " << get_begin_index()
+      << " to " << get_end_index() << std::endl;
+}
+
+IMPATOM_END_NAMESPACE
