93fbc1bd7768cfdf5a1e93c02a0b73d952792dc3
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-05 10:11:11 +0000

    skip -O3 flag if not needed for build mode
    git-svn-id: https://svn.salilab.org/imp/trunk@1817 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- tools/__init__.py
+++ tools/__init__.py
@@ -425,6 +425,7 @@ def get_pyext_environment(env, mod_prefix, cplusplus=False):
         # This is mildly unsafe if there happens to be another compiler which uses these
         # for something different.
         opt=opt.replace("-O2", "")
+        opt=opt.replace("-O3", "")
         opt=opt.replace("-g", "")
         e.Replace(CC=cc, CXX=cxx, LDMODULESUFFIX=so)
         e.Replace(CPPFLAGS=basecflags.split() + opt.split())
