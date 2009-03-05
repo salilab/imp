7b9182693ea122191bf94aea9baa44f9ee019451
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-05 06:24:52 +0000

    remove tests of deprecated functionality
    git-svn-id: https://svn.salilab.org/imp/trunk@1808 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/test/decorators/test_hierarchy.py
+++ modules/core/test/decorators/test_hierarchy.py
@@ -46,9 +46,9 @@ class DecoratorTests(IMP.test.TestCase):
         self.assertRaises(ValueError, IMP.core.residue_type_from_pdb_string, "blah")
 
     def test_types(self):
-        at= IMP.core.atom_type_from_pdb_string("CA")
-        self.assertRaises(ValueError, IMP.core.atom_type_from_pdb_string, "blah")
-        lt= IMP.core.residue_type_from_pdb_string("LYS")
-        self.assertRaises(ValueError, IMP.core.residue_type_from_pdb_string, "blah")
+        at= IMP.atom.atom_type_from_pdb_string("CA")
+        self.assertRaises(ValueError, IMP.atom.atom_type_from_pdb_string, "blah")
+        lt= IMP.atom.residue_type_from_pdb_string("LYS")
+        self.assertRaises(ValueError, IMP.atom.residue_type_from_pdb_string, "blah")
 if __name__ == '__main__':
     unittest.main()
