13feab57955fba7d7863a8854cab2991e899e2d3
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-07 00:06:13 +0000

    add utility functions such as get_centroid and tranform of proteins
    git-svn-id: https://svn.salilab.org/imp/trunk@1857 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/atom/include/utilities.h
@@ -0,0 +1,32 @@
+/**
+ *  \file atom/utilities.h
+ *  \brief Functions to perform simple functions on proteins
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ *
+ */
+#ifndef IMPATOM_UTILITIES_H
+#define IMPATOM_UTILITIES_H
+
+#include "config.h"
+#include "internal/pdb.h"
+#include "MolecularHierarchyDecorator.h"
+#include <IMP/algebra/Transformation3D.h>
+#include <IMP/Model.h>
+#include <IMP/Particle.h>
+
+IMPATOM_BEGIN_NAMESPACE
+
+//! Get the centroid of a molecule
+IMPATOMEXPORT algebra::Vector3D
+get_centroid(const MolecularHierarchyDecorator &m);
+
+//! Transform a molecule
+IMPATOMEXPORT
+void transform(const MolecularHierarchyDecorator &m,
+               const algebra::Transformation3D &t);
+
+
+IMPATOM_END_NAMESPACE
+
+#endif /* IMPATOM_UTILITIES_H */
---
+++ modules/atom/src/utilities.cpp
@@ -0,0 +1,36 @@
+/**
+ *  \file atom/utilities.cpp
+ *  \brief Functions to perform simple functions on proteins
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ *
+ */
+#include "IMP/atom/utilities.h"
+#include <IMP/algebra/Vector3D.h>
+#include <IMP/core/XYZDecorator.h>
+IMPATOM_BEGIN_NAMESPACE
+
+IMPATOMEXPORT algebra::Vector3D
+get_centroid(const MolecularHierarchyDecorator &m) {
+ algebra::Vector3D centroid(0.0,0.0,0.0);
+ Particles ps =
+   get_by_type(m, MolecularHierarchyDecorator::ATOM);
+ for (Particles::iterator it = ps.begin(); it != ps.end(); it++) {
+   centroid = centroid + core::XYZDecorator::cast(*it).get_coordinates();
+  }
+ return centroid*(1.0/ps.size());
+}
+IMPATOMEXPORT
+void transform(const MolecularHierarchyDecorator &m,
+               const algebra::Transformation3D &t) {
+  Particles ps =
+    get_by_type(m, MolecularHierarchyDecorator::ATOM);
+  algebra::Vector3D xyz_new;
+  core::XYZDecorator xyz_d;
+  for (Particles::iterator it = ps.begin(); it != ps.end(); it++) {
+    xyz_d = core::XYZDecorator::cast(*it);
+    xyz_new = t.transform(xyz_d.get_coordinates());
+    xyz_d.set_coordinates(xyz_new);
+  }
+}
+IMPATOM_END_NAMESPACE
