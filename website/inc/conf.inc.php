2809dfbba3bb739be5a043a3baf70db738452a83
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-10 22:28:43 +0000

    Fix include paths.
    git-svn-id: https://svn.salilab.org/imp/trunk@1884 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- website/inc/conf.inc.php
+++ website/inc/conf.inc.php
@@ -2,5 +2,5 @@
    # Server configuration. Addapt as needed
    global $home,$home_dir;
    $home     = "http://www.salilab.org/imp/";
-   $home_dir = "/guitar3/home/www/html/imp";
+   $home_dir = "/guitar3/home/www/imp";
 ?>
