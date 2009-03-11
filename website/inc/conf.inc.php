34eac314f43ee20d23f1730101c47f05ded0c7e1
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-11 00:00:55 +0000

    Fix typo.
    git-svn-id: https://svn.salilab.org/imp/trunk@1890 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- website/inc/conf.inc.php
+++ website/inc/conf.inc.php
@@ -1,5 +1,5 @@
 <?PHP
-   # Server configuration. Addapt as needed
+   # Server configuration. Adapt as needed
    global $home,$home_dir;
    $home     = "http://www.salilab.org/imp/";
    $home_dir = "/guitar3/home/www/imp";
