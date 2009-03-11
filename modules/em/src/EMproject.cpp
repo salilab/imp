6098fa7d527954ee0e7fb5bd90f348b3bb4884dd
javi@SALILAB.ORG <javi@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-11 22:43:50 +0000

    cpp file for EMproject
    git-svn-id: https://svn.salilab.org/imp/trunk@1898 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/em/src/EMproject.cpp
@@ -0,0 +1,12 @@
+/**
+ *  \file EMproject.cpp
+ *  \brief Projection operation for 3D volumes
+ *  \author Javier Velazquez-Muriel
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+*/
+
+#include <IMP/em/EMproject.h>
+
+IMPEM_BEGIN_NAMESPACE
+
+IMPEM_END_NAMESPACE
