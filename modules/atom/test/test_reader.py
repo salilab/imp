a08e4a97adf856f0f75eabf923be2a763e761bed
dina@SALILAB.ORG <dina@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-12 22:43:04 +0000

    improvements to show of MolecularHierarchyDecorator
    git-svn-id: https://svn.salilab.org/imp/trunk@1918 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/test/test_reader.py
+++ modules/atom/test/test_reader.py
@@ -8,7 +8,7 @@ import time
 
 class PDBReadTest(IMP.test.TestCase):
 
-    def test_saxs(self):
+    def test_read(self):
         """Check reading a pdb with one protein"""
         m = IMP.Model()
 
@@ -17,7 +17,7 @@ class PDBReadTest(IMP.test.TestCase):
                                         m, IMP.atom.NonWaterSelector())
         print "number of particles"
         print m.get_number_of_particles()
-        #IMP.core.show_molecular_hierarchy(mp)
+        IMP.atom.show_molecular_hierarchy(mp)
 
         m2 = IMP.Model()
         mp= IMP.atom.read_pdb(self.get_input_file_name("input.pdb"),
