2ab1b426b30ddb7e196325db2a2bce55594916bf
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-12 02:16:04 +0000

    Make Matrix2D inplace operators work correctly in Python.
    git-svn-id: https://svn.salilab.org/imp/trunk@1900 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/test/matrix/test_matrix2d.py
+++ modules/algebra/test/matrix/test_matrix2d.py
@@ -1,22 +1,27 @@
 import unittest
 import IMP.test
 import IMP.algebra as alg
-import math
-
-
-
-def print_matrix2D(m):
-    r1=range(m.get_start(0),m.get_finish(0)+1)
-    r2=range(m.get_start(1),m.get_finish(1)+1)
-    for i in r1:
-        for j in r2:
-            print m[i,j],
-        print "\n"
-
 
 class Matrix2DTests(IMP.test.TestCase):
+    def make_matrix(self, v):
+        m = alg.matrix2D(len(v), len(v[0]))
+        for row in range(len(v)):
+            for col in range(len(v[0])):
+                m[row,col] = v[row][col]
+        return m
+
+    def assert_matrix_equal(self, m, expected):
+        for row in range(len(expected)):
+            for col in range(len(expected[0])):
+                self.assertEqual(m[row,col], expected[row][col])
+
+    def assert_matrix_in_tolerance(self, m, expected, tol):
+        for row in range(len(expected)):
+            for col in range(len(expected[0])):
+                self.assertInTolerance(m[row,col], expected[row][col], tol)
+
     def test_sizes(self):
-        """Check proper creation an resizing"""
+        """Check proper creation and resizing"""
         m = alg.matrix2D(3,2)
         self.assertEqual(m.get_columns(), 2)
         m.resize(5,7)
@@ -33,75 +38,91 @@ class Matrix2DTests(IMP.test.TestCase):
         m[-1,-4]=34.5
         self.assertEqual(m[-1,-4],34.5)
 
-    def test_matrices_operations(self):
-        """Check matrix3D operations"""
-        m1 = alg.matrix2D(2,2)
-        m2 = alg.matrix2D(2,2)
-        result = alg.matrix2D(2,2)
-        m1[0,0]=1;m1[0,1]=2;m1[1,0]=3;m1[1,1]=4
-        m2[0,0]=1;m2[0,1]=2;m2[1,0]=3;m2[1,1]=4
+    def test_addition(self):
+        """Check Matrix2D addition"""
+        m1 = self.make_matrix([[1,2], [3,4]])
+        m2 = self.make_matrix([[1,2], [3,4]])
+        idm1 = id(m1)
         result = m1 + m2
-        self.assertEqual(result[0,0],2)
-        self.assertEqual(result[0,1],4)
-        self.assertEqual(result[1,0],6)
-        self.assertEqual(result[1,1],8)
+        m1 += m2
+        # Inplace operation should not change the Python object identity
+        self.assertEqual(id(m1), idm1)
+        for r in (result, m1):
+            self.assert_matrix_equal(r, [[2,4], [6,8]])
+
+    def test_subtraction(self):
+        """Check Matrix2D subtraction"""
+        m1 = self.make_matrix([[1,2], [3,4]])
+        m2 = self.make_matrix([[1,2], [3,4]])
+        idm1 = id(m1)
         result = m1 - m2
-        self.assertEqual(result[0,0],0)
-        self.assertEqual(result[0,1],0)
-        self.assertEqual(result[1,0],0)
-        self.assertEqual(result[1,1],0)
+        m1 -= m2
+        self.assertEqual(id(m1), idm1)
+        for r in (result, m1):
+            self.assert_matrix_equal(r, [[0,0], [0,0]])
+
+    def test_multiplication(self):
+        """Check Matrix2D multiplication"""
+        m1 = self.make_matrix([[1,2], [3,4]])
+        m2 = self.make_matrix([[1,2], [3,4]])
+        idm1 = id(m1)
         result = m1 * m2
-        self.assertEqual(result[0,0],1)
-        self.assertEqual(result[0,1],4)
-        self.assertEqual(result[1,0],9)
-        self.assertEqual(result[1,1],16)
+        m1 *= m2
+        self.assertEqual(id(m1), idm1)
+        for r in (result, m1):
+            self.assert_matrix_equal(r, [[1,4], [9,16]])
+
+    def test_division(self):
+        """Check Matrix2D division"""
+        m1 = self.make_matrix([[1,2], [3,4]])
+        m2 = self.make_matrix([[1,2], [3,4]])
+        idm1 = id(m1)
         result = m1 / m2
-        self.assertEqual(result[0,0],1)
-        self.assertEqual(result[0,1],1)
-        self.assertEqual(result[1,0],1)
-        self.assertEqual(result[1,1],1)
+        m1 /= m2
+        self.assertEqual(id(m1), idm1)
+        for r in (result, m1):
+            self.assert_matrix_equal(r, [[1,1], [1,1]])
 
-#     print_matrix2D(m1)
-#     print_matrix2D(m2)
-#     print_matrix2D(m1+m2)
-#     print_matrix2D(m1-m2)
-#     print_matrix2D(m1*m2)
-#     print_matrix2D(m1/m2)
-
-
-    def test_matrix_scalar_operations(self):
-        """Check matrix2D and scalar operations"""
-        m1 = alg.matrix2D(2,2)
-        m1[0,0]=1;m1[0,1]=2;m1[1,0]=3;m1[1,1]=4
-        result = alg.matrix2D(2,2)
-        result = m1 * 3.0
-
+    def test_scalar_addition(self):
+        """Check Matrix2D scalar addition"""
+        m1 = self.make_matrix([[1,2], [3,4]])
+        idm1 = id(m1)
         result = m1 + 3
-        self.assertEqual(result[0,0],4)
-        self.assertEqual(result[0,1],5)
-        self.assertEqual(result[1,0],6)
-        self.assertEqual(result[1,1],7)
+        m1 += 3
+        self.assertEqual(id(m1), idm1)
+        for r in (result, m1):
+            self.assert_matrix_equal(r, [[4,5], [6,7]])
+
+    def test_scalar_subtraction(self):
+        """Check Matrix2D scalar subtraction"""
+        m1 = self.make_matrix([[1,2], [3,4]])
+        idm1 = id(m1)
         result = m1 - 3
-        self.assertEqual(result[0,0],-2)
-        self.assertEqual(result[0,1],-1)
-        self.assertEqual(result[1,0],0)
-        self.assertEqual(result[1,1],1)
+        m1 -= 3
+        self.assertEqual(id(m1), idm1)
+        for r in (result, m1):
+            self.assert_matrix_equal(r, [[-2,-1], [0,1]])
+
+    def test_scalar_multiplication(self):
+        """Check Matrix2D scalar multiplication"""
+        m1 = self.make_matrix([[1,2], [3,4]])
+        idm1 = id(m1)
         result = m1 * 3
-        self.assertEqual(result[0,0],3)
-        self.assertEqual(result[0,1],6)
-        self.assertEqual(result[1,0],9)
-        self.assertEqual(result[1,1],12)
+        m1 *= 3
+        self.assertEqual(id(m1), idm1)
+        for r in (result, m1):
+            self.assert_matrix_equal(r, [[3,6], [9,12]])
+
+    def test_scalar_division(self):
+        """Check Matrix2D scalar division"""
+        m1 = self.make_matrix([[1,2], [3,4]])
+        idm1 = id(m1)
         result = m1 / 3
-        self.assertInTolerance(result[0,0],0.333,0.001)
-        self.assertInTolerance(result[0,1],0.666,0.001)
-        self.assertInTolerance(result[1,0],1.000,0.001)
-        self.assertInTolerance(result[1,1],1.333,0.001)
-
-#     print_matrix2D(m1-3)
-#     print_matrix2D(m1+3)
-#     print_matrix2D(m1*3)
-#     print_matrix2D(m1/3)
-
+        m1 /= 3
+        self.assertEqual(id(m1), idm1)
+        for r in (result, m1):
+            self.assert_matrix_in_tolerance(r, [[0.333,0.666], [1.0,1.333]],
+                                            0.001)
 
 if __name__ == '__main__':
     unittest.main()
