13f01aaac2bae2d5f1f6684e60aaeb9b4531701a
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-12 16:17:27 +0000

    Fix and test inplace Matrix3D operators.
    git-svn-id: https://svn.salilab.org/imp/trunk@1913 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/pyext/Matrix3D.i
+++ modules/algebra/pyext/Matrix3D.i
@@ -1,8 +1,41 @@
 %ignore IMP::algebra::Matrix3D::operator=;
 
+/* Provide our own implementations for some operators */
+%ignore IMP::algebra::Matrix3D::operator+=;
+%ignore IMP::algebra::Matrix3D::operator-=;
+%ignore IMP::algebra::Matrix3D::operator*=;
+%ignore IMP::algebra::Matrix3D::operator/=;
+
 // Matrix3D
 %include "IMP/algebra/MultiArray.h"
 %include "IMP/algebra/Matrix3D.h"
+
+/* Make sure that we return the original Python object from C++ inplace
+   operators (not a new Python proxy around the same C++ object) */
+namespace IMP {
+ namespace algebra {
+  %feature("shadow") Matrix3D::__iadd__ %{
+    def __iadd__(self, *args):
+        $action(self, *args)
+        return self
+  %}
+  %feature("shadow") Matrix3D::__imul__ %{
+    def __imul__(self, *args):
+        $action(self, *args)
+        return self
+  %}
+  %feature("shadow") Matrix3D::__idiv__ %{
+    def __idiv__(self, *args):
+        $action(self, *args)
+        return self
+  %}
+  %feature("shadow") Matrix3D::__isub__ %{
+    def __isub__(self, *args):
+        $action(self, *args)
+        return self
+  %}
+ }
+}
 
 // Extend Matrix3D with _internal_get and _internal_set
 %extend IMP::algebra::Matrix3D<float> {
@@ -13,6 +46,16 @@
     self->operator()(i, j, k) = val;
   }
 
+  /* Ignore any C++ return value from inplace operators, so that SWIG does not
+     generate a new SWIG wrapper for the return value (see above). */
+  void __iadd__(const Matrix3D<float> &x) { self->operator+=(x); }
+  void __iadd__(float x) { self->operator+=(x); }
+  void __imul__(const Matrix3D<float> &x) { self->operator*=(x); }
+  void __imul__(float x) { self->operator*=(x); }
+  void __idiv__(const Matrix3D<float> &x) { self->operator/=(x); }
+  void __idiv__(float x) { self->operator/=(x); }
+  void __isub__(const Matrix3D<float> &x) { self->operator-=(x); }
+  void __isub__(float x) { self->operator-=(x); }
 
   // Compose the python calls to _internal_get and _internal_set
   %pythoncode {
--- modules/algebra/test/matrix/test_matrix3d.py
+++ modules/algebra/test/matrix/test_matrix3d.py
@@ -4,17 +4,27 @@ import IMP.algebra as alg
 import math
 
 
-
-def print_matrix3D(m):
-
-    for i in xrange(m.get_start(0),m.get_finish(0)+1):
-        print "Layer :",i
-        for j in xrange(m.get_start(1),m.get_finish(1)+1):
-            for k in xrange(m.get_start(2),m.get_finish(2)+1):
-                print m[i,j,k],
-            print "\n"
-
 class Matrix3DTests(IMP.test.TestCase):
+    def make_matrix(self, v):
+        m = alg.matrix3D(len(v), len(v[0]), len(v[0][0]))
+        for z in range(len(v)):
+            for y in range(len(v[0])):
+                for x in range(len(v[0][0])):
+                    m[z,y,x] = v[z][y][x]
+        return m
+
+    def assert_matrix_equal(self, m, expected):
+        for z in range(len(expected)):
+            for y in range(len(expected[0])):
+                for x in range(len(expected[0][0])):
+                    self.assertEqual(m[z,y,x], expected[z][y][x])
+
+    def assert_matrix_in_tolerance(self, m, expected, tol):
+        for z in range(len(expected)):
+            for y in range(len(expected[0])):
+                for x in range(len(expected[0][0])):
+                    self.assertInTolerance(m[z,y,x], expected[z][y][x], tol)
+
     def test_sizes(self):
         """Check proper creation an resizing"""
         m = alg.matrix3D(3,2,3)
@@ -30,191 +40,114 @@ class Matrix3DTests(IMP.test.TestCase):
         m[2,1,6]=34.5
         self.assertEqual(m[2,1,6],34.5)
 
-    def test_matrices_operations(self):
-        """Check matrix3D operations"""
-        m1 = alg.matrix3D(2,2,4)
-        m2 = alg.matrix3D(2,2,4)
-        result = alg.matrix3D(2,2,4)
-
-        for i in xrange(m1.get_start(0),m1.get_finish(0)+1):
-            for j in xrange(m1.get_start(1),m1.get_finish(1)+1):
-                for k in xrange(m1.get_start(2),m1.get_finish(2)+1):
-                    m1[i,j,k]=i+j+k
-                    m2[i,j,k]=i+j+k-1
-
+    def test_addition(self):
+        """Check Matrix3D addition"""
+        m1 = self.make_matrix([[[0,1,2,3],[1,2,3,4]],
+                               [[1,2,3,4],[2,3,4,5]]])
+        m2 = self.make_matrix([[[-1,0,1,2],[0,1,2,3]],
+                               [[0,1,2,3], [1,2,3,4]]])
+        idm1 = id(m1)
         result = m1 + m2
-        self.assertEqual(result[0,0,0],-1)
-        self.assertEqual(result[0,0,1],1)
-        self.assertEqual(result[0,0,2],3)
-        self.assertEqual(result[0,0,3],5)
-        self.assertEqual(result[0,1,0],1)
-        self.assertEqual(result[0,1,1],3)
-        self.assertEqual(result[0,1,2],5)
-        self.assertEqual(result[0,1,3],7)
-        self.assertEqual(result[1,0,0],1)
-        self.assertEqual(result[1,0,1],3)
-        self.assertEqual(result[1,0,2],5)
-        self.assertEqual(result[1,0,3],7)
-        self.assertEqual(result[1,1,0],3)
-        self.assertEqual(result[1,1,1],5)
-        self.assertEqual(result[1,1,2],7)
-        self.assertEqual(result[1,1,3],9)
+        m1 += m2
+        # Inplace operation should not change the Python object identity
+        self.assertEqual(id(m1), idm1)
+        for r in (result, m1):
+            self.assert_matrix_equal(r, [[[-1,1,3,5],[1,3,5,7]],
+                                         [[1,3,5,7], [3,5,7,9]]])
 
+    def test_subtraction(self):
+        """Check Matrix3D subtraction"""
+        m1 = self.make_matrix([[[0,1,2,3],[1,2,3,4]],
+                               [[1,2,3,4],[2,3,4,5]]])
+        m2 = self.make_matrix([[[-1,0,1,2],[0,1,2,3]],
+                               [[0,1,2,3], [1,2,3,4]]])
+        idm1 = id(m1)
         result = m1 - m2
-        self.assertEqual(result[0,0,0],1)
-        self.assertEqual(result[0,0,1],1)
-        self.assertEqual(result[0,0,2],1)
-        self.assertEqual(result[0,0,3],1)
-        self.assertEqual(result[0,1,0],1)
-        self.assertEqual(result[0,1,1],1)
-        self.assertEqual(result[0,1,2],1)
-        self.assertEqual(result[0,1,3],1)
-        self.assertEqual(result[1,0,0],1)
-        self.assertEqual(result[1,0,1],1)
-        self.assertEqual(result[1,0,2],1)
-        self.assertEqual(result[1,0,3],1)
-        self.assertEqual(result[1,1,0],1)
-        self.assertEqual(result[1,1,1],1)
-        self.assertEqual(result[1,1,2],1)
-        self.assertEqual(result[1,1,3],1)
+        m1 -= m2
+        self.assertEqual(id(m1), idm1)
+        for r in (result, m1):
+            self.assert_matrix_equal(r, [[[1,1,1,1],[1,1,1,1]],
+                                         [[1,1,1,1],[1,1,1,1]]])
+
+    def test_multiplication(self):
+        """Check Matrix3D multiplication"""
+        m1 = self.make_matrix([[[0,1,2,3],[1,2,3,4]],
+                               [[1,2,3,4],[2,3,4,5]]])
+        m2 = self.make_matrix([[[-1,0,1,2],[0,1,2,3]],
+                               [[0,1,2,3], [1,2,3,4]]])
+        idm1 = id(m1)
         result = m1 * m2
-        self.assertEqual(result[0,0,0],0)
-        self.assertEqual(result[0,0,1],0)
-        self.assertEqual(result[0,0,2],2)
-        self.assertEqual(result[0,0,3],6)
-        self.assertEqual(result[0,1,0],0)
-        self.assertEqual(result[0,1,1],2)
-        self.assertEqual(result[0,1,2],6)
-        self.assertEqual(result[0,1,3],12)
-        self.assertEqual(result[1,0,0],0)
-        self.assertEqual(result[1,0,1],2)
-        self.assertEqual(result[1,0,2],6)
-        self.assertEqual(result[1,0,3],12)
-        self.assertEqual(result[1,1,0],2)
-        self.assertEqual(result[1,1,1],6)
-        self.assertEqual(result[1,1,2],12)
-        self.assertEqual(result[1,1,3],20)
+        m1 *= m2
+        self.assertEqual(id(m1), idm1)
+        for r in (result, m1):
+            self.assert_matrix_equal(r, [[[0,0,2,6], [0,2,6,12]],
+                                         [[0,2,6,12],[2,6,12,20]]])
+
+    def test_division(self):
+        """Check Matrix3D division"""
+        m1 = self.make_matrix([[[0,0,2,6], [0,2,6,12]],
+                               [[0,2,6,12],[2,6,12,20]]])
+        m2 = self.make_matrix([[[-1,1,1,2],[1,1,2,3]],
+                               [[1,1,2,3], [1,2,3,4]]])
+        idm1 = id(m1)
         result = m1 / m2
-        self.assertInTolerance(result[0,0,0],0,0.001)
-#    self.assertInTolerance(result[0,0,1],inf,0.001)
-        self.assertInTolerance(result[0,0,2],2,0.001)
-        self.assertInTolerance(result[0,0,3],1.5,0.001)
-#    self.assertInTolerance(result[0,1,0],inf,.001)
-        self.assertInTolerance(result[0,1,1],2.0,0.001)
-        self.assertInTolerance(result[0,1,2],1.5,0.001)
-        self.assertInTolerance(result[0,1,3],1.333,0.001)
-#    self.assertInTolerance(result[1,0,0],inf,0.001)
-        self.assertInTolerance(result[1,0,1],2,0.001)
-        self.assertInTolerance(result[1,0,2],1.5,0.001)
-        self.assertInTolerance(result[1,0,3],1.333,0.001)
-        self.assertInTolerance(result[1,1,0],2,0.001)
-        self.assertInTolerance(result[1,1,1],1.5,0.001)
-        self.assertInTolerance(result[1,1,2],1.333,0.001)
-        self.assertInTolerance(result[1,1,3],1.25,0.001)
+        m1 /= m2
+        self.assertEqual(id(m1), idm1)
+        for r in (result, m1):
+            self.assert_matrix_in_tolerance(r, [[[0,0,2,3],[0,2,3,4]],
+                                                [[0,2,3,4],[2,3,4,5]]], 0.001)
 
-#     print "m1"
-#     print_matrix3D(m1)
-#     print "m2"
-#     print_matrix3D(m2)
-#     print "m1+m2"
-#     print_matrix3D(m1+m2)
-#     print "m1-m2"
-#     print_matrix3D(m1-m2)
-#     print "m1*m2"
-#     print_matrix3D(m1*m2)
-#     print "m1/m2"
-#     print_matrix3D(m1/m2)
-
-
-    def test_matrix_scalar_operations(self):
-        """Check matrix3D and scalar operations"""
-        """Check matrix3D operations"""
-        m1 = alg.matrix3D(2,2,4)
-        result = alg.matrix3D(2,2,4)
-
-        for i in xrange(m1.get_start(0),m1.get_finish(0)+1):
-            for j in xrange(m1.get_start(1),m1.get_finish(1)+1):
-                for k in xrange(m1.get_start(2),m1.get_finish(2)+1):
-                    m1[i,j,k]=i+j+k
-
-        result = m1 - 3
-        self.assertEqual(result[0,0,0],-3)
-        self.assertEqual(result[0,0,1],-2)
-        self.assertEqual(result[0,0,2],-1)
-        self.assertEqual(result[0,0,3],0)
-        self.assertEqual(result[0,1,0],-2)
-        self.assertEqual(result[0,1,1],-1)
-        self.assertEqual(result[0,1,2],0)
-        self.assertEqual(result[0,1,3],1)
-        self.assertEqual(result[1,0,0],-2)
-        self.assertEqual(result[1,0,1],-1)
-        self.assertEqual(result[1,0,2],0)
-        self.assertEqual(result[1,0,3],1)
-        self.assertEqual(result[1,1,0],-1)
-        self.assertEqual(result[1,1,1],0)
-        self.assertEqual(result[1,1,2],1)
-        self.assertEqual(result[1,1,3],2)
-
+    def test_scalar_addition(self):
+        """Check Matrix3D scalar addition"""
+        m1 = self.make_matrix([[[0,1,2,3],[1,2,3,4]],
+                               [[1,2,3,4],[2,3,4,5]]])
+        idm1 = id(m1)
         result = m1 + 3
-        self.assertEqual(result[0,0,0],3)
-        self.assertEqual(result[0,0,1],4)
-        self.assertEqual(result[0,0,2],5)
-        self.assertEqual(result[0,0,3],6)
-        self.assertEqual(result[0,1,0],4)
-        self.assertEqual(result[0,1,1],5)
-        self.assertEqual(result[0,1,2],6)
-        self.assertEqual(result[0,1,3],7)
-        self.assertEqual(result[1,0,0],4)
-        self.assertEqual(result[1,0,1],5)
-        self.assertEqual(result[1,0,2],6)
-        self.assertEqual(result[1,0,3],7)
-        self.assertEqual(result[1,1,0],5)
-        self.assertEqual(result[1,1,1],6)
-        self.assertEqual(result[1,1,2],7)
-        self.assertEqual(result[1,1,3],8)
+        m1 += 3
+        self.assertEqual(id(m1), idm1)
+        for r in (result, m1):
+            self.assert_matrix_equal(r, [[[3,4,5,6],[4,5,6,7]],
+                                         [[4,5,6,7],[5,6,7,8]]])
 
+    def test_scalar_subtraction(self):
+        """Check Matrix3D scalar subtraction"""
+        m1 = self.make_matrix([[[0,1,2,3],[1,2,3,4]],
+                               [[1,2,3,4],[2,3,4,5]]])
+        idm1 = id(m1)
+        result = m1 - 3
+        m1 -= 3
+        self.assertEqual(id(m1), idm1)
+        for r in (result, m1):
+            self.assert_matrix_equal(r, [[[-3,-2,-1,0],[-2,-1,0,1]],
+                                         [[-2,-1,0,1], [-1,0,1,2]]])
+
+    def test_scalar_multiplication(self):
+        """Check Matrix3D scalar multiplication"""
+        m1 = self.make_matrix([[[0,1,2,3],[1,2,3,4]],
+                               [[1,2,3,4],[2,3,4,5]]])
+        idm1 = id(m1)
         result = m1 * 3
-        self.assertEqual(result[0,0,0],0)
-        self.assertEqual(result[0,0,1],3)
-        self.assertEqual(result[0,0,2],6)
-        self.assertEqual(result[0,0,3],9)
-        self.assertEqual(result[0,1,0],3)
-        self.assertEqual(result[0,1,1],6)
-        self.assertEqual(result[0,1,2],9)
-        self.assertEqual(result[0,1,3],12)
-        self.assertEqual(result[1,0,0],3)
-        self.assertEqual(result[1,0,1],6)
-        self.assertEqual(result[1,0,2],9)
-        self.assertEqual(result[1,0,3],12)
-        self.assertEqual(result[1,1,0],6)
-        self.assertEqual(result[1,1,1],9)
-        self.assertEqual(result[1,1,2],12)
-        self.assertEqual(result[1,1,3],15)
+        m1 *= 3
+        self.assertEqual(id(m1), idm1)
+        for r in (result, m1):
+            self.assert_matrix_equal(r, [[[0,3,6,9], [3,6,9,12]],
+                                         [[3,6,9,12],[6,9,12,15]]])
 
+    def test_scalar_division(self):
+        """Check Matrix3D scalar division"""
+        m1 = self.make_matrix([[[0,1,2,3],[1,2,3,4]],
+                               [[1,2,3,4],[2,3,4,5]]])
+        idm1 = id(m1)
         result = m1 / 3
-        self.assertInTolerance(result[0,0,0],0.000,0.001)
-        self.assertInTolerance(result[0,0,1],0.333,0.001)
-        self.assertInTolerance(result[0,0,2],0.666,0.001)
-        self.assertInTolerance(result[0,0,3],1.000,0.001)
-        self.assertInTolerance(result[0,1,0],0.333,0.001)
-        self.assertInTolerance(result[0,1,1],0.666,0.001)
-        self.assertInTolerance(result[0,1,2],1.000,0.001)
-        self.assertInTolerance(result[0,1,3],1.333,0.001)
-        self.assertInTolerance(result[1,0,0],0.333,0.001)
-        self.assertInTolerance(result[1,0,1],0.666,0.001)
-        self.assertInTolerance(result[1,0,2],1.000,0.001)
-        self.assertInTolerance(result[1,0,3],1.333,0.001)
-        self.assertInTolerance(result[1,1,0],0.666,0.001)
-        self.assertInTolerance(result[1,1,1],1.000,0.001)
-        self.assertInTolerance(result[1,1,2],1.333,0.001)
-        self.assertInTolerance(result[1,1,3],1.666,0.001)
-
-#     print_matrix3D(m1-3)
-#     print_matrix3D(m1+3)
-#     print_matrix3D(m1*3)
-#     print_matrix3D(m1/3)
-
-
+        m1 /= 3
+        self.assertEqual(id(m1), idm1)
+        for r in (result, m1):
+            self.assert_matrix_in_tolerance(r,
+                                            [[[0.000,0.333,0.666,1.000],
+                                              [0.333,0.666,1.000,1.333]],
+                                             [[0.333,0.666,1.000,1.333],
+                                              [0.666,1.000,1.333,1.666]]],
+                                            0.001)
 
 if __name__ == '__main__':
     unittest.main()
