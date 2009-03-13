37ae5e851827b47f87c7ac073b650bddf0f7f8c5
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-13 17:38:52 +0000

    forgot to check in updated example
    git-svn-id: https://svn.salilab.org/imp/trunk@1940 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- examples/simple_examples/rigid_bodies.py
+++ examples/simple_examples/rigid_bodies.py
@@ -8,13 +8,6 @@ cpr= IMP.core.ChildrenParticleRefiner(IMP.atom.MolecularHierarchyDecorator.get_t
 mp0= IMP.atom.read_pdb('examples/simple_examples/single_protein.pdb', m)
 residues= IMP.atom.get_by_type(mp0, IMP.atom.MolecularHierarchyDecorator.RESIDUE)
 rbt= IMP.core.RigidBodyTraits("residues")
-rbs=IMP.core.ListSingletonContainer()
-for r in residues:
-    IMP.core.XYZDecorator.create(r)
-    rb=IMP.core.RigidBodyDecorator.create(r, cpr, rbt)
-    rbs.add_particle(rb.get_particle())
-    print r
-print "done with setup"
-
+rbs=IMP.core.ListSingletonContainer(residues)
 IMP.core.setup_rigid_bodies(m, rbs, cpr, rbt)
 print "all done"
