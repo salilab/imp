f9b1c335ea4b542fe0d57b10abd1641bcdd202ee
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-13 02:44:48 +0000

    update Cone3D test case to support changes
    git-svn-id: https://svn.salilab.org/imp/trunk@1927 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/test/test_cone.py
+++ modules/algebra/test/test_cone.py
@@ -13,27 +13,34 @@ class ConeTests(IMP.test.TestCase):
         IMP.test.TestCase.setUp(self)
 
 
-    def test_close_cone_construction(self):
-        """Check that close cones are constructed correctly"""
-        tip = IMP.algebra.Vector3D(0.0,0.0,0.0)
-        direction = IMP.algebra.Vector3D(1.0,0.0,0.0)
-        h=5.0;angle=math.pi/3;
-        cone = IMP.algebra.SphericalCone3D(tip,direction,angle,h)
-        self.assertEqual((cone.get_tip()-tip).get_magnitude() < 0.01,True)
-        self.assertEqual(cone.is_bound(),True)
-        self.assertEqual(cone.get_contains(IMP.algebra.Vector3D(0.5,0.5,0.5)),True)
-        self.assertEqual(cone.get_contains(IMP.algebra.Vector3D(0.5,1.0,9.5)),False)
-        self.assertEqual(cone.get_contains(IMP.algebra.Vector3D(10.0,1.0,0.5)),False)
-    def test_open_cone_construction(self):
-        """Check that open cones are constructed correctly"""
-        tip = IMP.algebra.Vector3D(0.0,0.0,0.0)
-        direction = IMP.algebra.Vector3D(1.0,0.0,0.0)
-        angle=math.pi/3;
-        cone = IMP.algebra.SphericalCone3D(tip,direction,angle)
-        self.assertEqual((cone.get_tip()-tip).get_magnitude() < 0.01,True)
-        self.assertEqual(cone.is_bound(),False)
-        self.assertEqual(cone.get_contains(IMP.algebra.Vector3D(0.5,0.5,0.5)),True)
-        self.assertEqual(cone.get_contains(IMP.algebra.Vector3D(0.5,1.0,9.5)),False)
-        self.assertEqual(cone.get_contains(IMP.algebra.Vector3D(10.0,1.0,0.5)),True)
+    def test_cone_construction(self):
+        """Check that cones on Z are constructed correctly"""
+        s = IMP.algebra.Segment3D(IMP.algebra.Vector3D(0.0,0.0,0.0),
+                                  IMP.algebra.Vector3D(0.0,0.0,5.0))
+        cone = IMP.algebra.Cone3D(s,4.0)
+        self.assertEqual((cone.get_tip()-s.get_point(0)).get_magnitude() < 0.01,True)
+        self.assertEqual(cone.get_contains(IMP.algebra.Vector3D(0.0,0.0,3.0)),True)
+        self.assertEqual(cone.get_contains(IMP.algebra.Vector3D(0.5,0.5,3.0)),True)
+
+        self.assertEqual(cone.get_contains(IMP.algebra.Vector3D(1.0,1.0,-3.0)),False)
+    def test_sphere_patch(self):
+        s = IMP.algebra.Segment3D(IMP.algebra.Vector3D(0.0,0.0,0.0),
+                                  IMP.algebra.Vector3D(0.0,0.0,5.0))
+        cone = IMP.algebra.Cone3D(s,4.0)
+        cone_pln = cone.get_intersecting_plane()
+        cone_sphere = cone.get_bounding_sphere()
+        sp = IMP.algebra.Sphere3DPatch(cone_sphere,cone_pln)
+        for v in IMP.algebra.uniform_cover(sp,3):
+            self.assertEqual(cone.get_bounding_sphere().get_contains(v),True)
+    def test_sphere_patch2(self):
+        s = IMP.algebra.Segment3D(IMP.algebra.random_vector_in_sphere(IMP.algebra.Vector3D(0.0,5.0,8.0),4.0),
+                                  IMP.algebra.random_vector_in_sphere(IMP.algebra.Vector3D(7.0,1.0,3.0),5.0))
+        cone = IMP.algebra.Cone3D(s,4.0)
+        cone_pln = cone.get_intersecting_plane()
+        cone_sphere = cone.get_bounding_sphere()
+        sp = IMP.algebra.Sphere3DPatch(cone_sphere,cone_pln)
+        for v in IMP.algebra.uniform_cover(sp,3):
+            self.assertEqual(cone.get_bounding_sphere().get_contains(v),True)
+
 if __name__ == '__main__':
     unittest.main()
