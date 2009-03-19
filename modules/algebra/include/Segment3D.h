703ea3488f8d18d6fe6f289e4f86380596623b4d
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-19 20:13:31 +0000

    fix names to standard and make methods consistent with other classes. And document a few more things
    git-svn-id: https://svn.salilab.org/imp/trunk@2022 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/include/Segment3D.h
+++ modules/algebra/include/Segment3D.h
@@ -19,21 +19,42 @@ class IMPALGEBRAEXPORT Segment3D
  public:
   Segment3D(){}
   Segment3D(const Vector3D &start,const Vector3D &end);
-  //! Get the start/end point of the segment
-  Vector3D get_point(unsigned int i) const;
-  Vector3D get_middle_point() const {return (start_+get_direction()/2);}
-  //! Get the direction of the line
-  Vector3D get_direction() const {return end_-start_;}
+  //! Get the start=0/end=1 point of the segment
+  Vector3D get_point(unsigned int i) const {
+#if IMP_SWIG_WRAPPER
+    IMP_check(i<2,"invalid point index",ValueException);
+#else
+    IMP_assert(i<2, "Invalid point index");
+#endif
+    return p_[i];
+  }
+  Vector3D get_middle_point() const {return (p_[0]+get_direction()/2);}
+  //! Get the unnormalized direction of the segment
+  Vector3D get_direction() const {return p_[1]-p_[0];}
   //! Get the lenght of the line
-  double lenght() const;
-  //! Get the distance between a segment and a point
-  double distance(const Vector3D &p) const;
-  //! Get the projected point on a segment
-  Vector3D projection(const Vector3D &p) const;
+  double get_length() const;
+  void show(std::ostream &out=std::cout) const {
+    out << spaces_io(p_[0]) << ": " << spaces_io(p_[1]);
+  }
  private:
-  Vector3D start_;
-  Vector3D end_;
+  Vector3D p_[2];
 };
+
+IMP_OUTPUT_OPERATOR(Segment3D)
+
+
+//! Get the distance between a segment and a point
+IMPALGEBRAEXPORT double distance(const Segment3D &s, const Vector3D &p);
+
+//! Get the distance between two segments
+IMPALGEBRAEXPORT double distance(const Segment3D &a, const Segment3D &b);
+
+//! Get the projected point on a segment
+/** It doesn't really make sense for a segment. I vote it goes away.
+    This produces the wrong answer for points that don't project between
+    the endpoints.
+ */
+IMPALGEBRAEXPORT Vector3D projection(const Segment3D &s, const Vector3D &p);
 
 IMPALGEBRA_END_NAMESPACE
 
--- modules/algebra/src/Segment3D.cpp
+++ modules/algebra/src/Segment3D.cpp
@@ -7,25 +7,124 @@
 #include <IMP/algebra/Segment3D.h>
 IMPALGEBRA_BEGIN_NAMESPACE
 Segment3D::Segment3D(const Vector3D &start,const Vector3D &end) {
-  start_=start;
-  end_=end;
-}
-Vector3D Segment3D::get_point(unsigned int i) const {
-  IMP_check(i<2,"invalid point index",ValueException);
-  if (i==0) return start_;
-  return end_;
-}
-double Segment3D::lenght() const {
-  return (start_-end_).get_magnitude();
-}
-Vector3D Segment3D::projection(const Vector3D &p) const{
-  Vector3D d = get_direction().get_unit_vector();
-  double t = d*(start_-p);
-  return Vector3D(start_+t * d);
+  p_[0]=start;
+  p_[1]=end;
 }
 
-double Segment3D::distance(const Vector3D &p) const{
-  return (start_-projection(p)).get_magnitude();
+double Segment3D::get_length() const {
+  return (p_[0]-p_[1]).get_magnitude();
+}
+Vector3D projection(const Segment3D &s, const Vector3D &p) {
+  Vector3D d = s.get_direction().get_unit_vector();
+  double t = d*(s.get_point(0)-p);
+  return Vector3D(s.get_point(0)+t * d);
+}
+
+
+namespace {
+  Segment3D shortest_segment(const Segment3D &s, const algebra::Vector3D &p) {
+    algebra::Vector3D vs= s.get_point(1)- s.get_point(0);
+    algebra::Vector3D vps= p- s.get_point(0);
+    double f= vps*vs/(vs*vs);
+    if (f<= 0) return Segment3D(s.get_point(0), p);
+    else if (f>=1) return Segment3D(s.get_point(1), p);
+    else {
+      algebra::Vector3D ps= s.get_point(0) + vs*f;
+      return Segment3D(ps, p);
+    }
+  }
+
+  /*
+    Calculate the line segment PaPb that is the shortest route between
+    two lines P1P2 and P3P4. Calculate also the values of mua and mub where
+    Pa = P1 + mua (P2 - P1)
+    Pb = P3 + mub (P4 - P3)
+    Return FALSE if no solution exists.
+  */
+  Segment3D shortest_segment(const Segment3D &sa,
+                             const Segment3D &sb) {
+    const double eps= .0001;
+    algebra::Vector3D va= sa.get_point(1) - sa.get_point(0);
+    algebra::Vector3D vb= sb.get_point(1) - sb.get_point(0);
+    double ma= va*va;
+    double mb= vb*vb;
+    if (ma < eps) {
+      if (mb < eps) {
+        return Segment3D(sa.get_point(0), sb.get_point(0));
+      } else {
+        Segment3D sr =shortest_segment(sb, sa.get_point(0));
+        return Segment3D(sr.get_point(1), sr.get_point(0));
+      }
+    } else if (mb < eps) {
+      return shortest_segment(sa, sb.get_point(0));
+    }
+
+    algebra::Vector3D vfirst = sa.get_point(0)- sb.get_point(0);
+
+    IMP_LOG(VERBOSE, vfirst << " | " << va << " | " << vb << std::endl);
+
+    double dfb = vfirst*vb;
+    double dab = vb*va;
+    double dfa = vfirst*va;
+
+    double denom = ma * mb - dab * dab;
+    if (std::abs(denom) < eps) {
+      return Segment3D(sa.get_point(0), sb.get_point(0));
+    }
+    double numer = dfb * dab - dfa * mb;
+
+    double fa = numer / denom;
+    double fb = (dfb + dab * fa) / mb;
+
+    /*
+      denom = d2121 * d4343 - d4321 * d4321;
+      numer = d1343 * d4321 - d1321 * d4343;
+
+      *mua = numer / denom;
+      *mub = (d1343 + d4321 * (*mua)) / d4343;
+
+      pa->x = p1.x + *mua * p21.x;
+      pa->y = p1.y + *mua * p21.y;
+      pa->z = p1.z + *mua * p21.z;
+      pb->x = p3.x + *mub * p43.x;
+      pb->y = p3.y + *mub * p43.y;
+      pb->z = p3.z + *mub * p43.z;
+    */
+
+    algebra::Vector3D ra;
+    if (fa < 1 && fa > 0) {
+      ra = sa.get_point(0) + fa *va;
+    } else if (fa <=0) {
+      ra= sa.get_point(0);
+    } else {
+      ra= sa.get_point(1);
+    }
+    algebra::Vector3D rb;
+    if (fb < 1 && fb > 0) {
+      rb = sb.get_point(0) + fb *vb;
+    } else if (fa <=0) {
+      rb= sb.get_point(0);
+    } else {
+      rb= sb.get_point(1);
+    }
+
+    IMP_LOG(VERBOSE, fa << " " << fb << std::endl);
+
+    return Segment3D(ra, rb);
+  }
+
+}
+
+
+double distance(const Segment3D &s, const Vector3D &p) {
+  Segment3D ss= shortest_segment(s, p);
+  return (ss.get_point(0) - ss.get_point(1)).get_magnitude();
+}
+
+
+double distance(const Segment3D &a, const Segment3D &b) {
+  Segment3D s= shortest_segment(a, b);
+  return (s.get_point(0) - s.get_point(1)).get_magnitude();
 }
 
 IMPALGEBRA_END_NAMESPACE
