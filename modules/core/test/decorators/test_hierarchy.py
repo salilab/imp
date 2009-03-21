96d30ffd83cdfaeeedf79ddaf60da1ba9fccc6ea
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-21 00:01:00 +0000

    use particle names
    git-svn-id: https://svn.salilab.org/imp/trunk@2060 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/test/decorators/test_hierarchy.py
+++ modules/core/test/decorators/test_hierarchy.py
@@ -9,14 +9,12 @@ class DecoratorTests(IMP.test.TestCase):
         m = IMP.Model()
         pp=IMP.Particle(m)
         ppd= IMP.core.HierarchyDecorator.create(pp)
-        ppnd= IMP.core.NameDecorator.create(pp)
-        ppnd.set_name("root")
+        pp.set_name("root")
         for i in range(0,10):
             pc=IMP.Particle(m)
             pcd= IMP.core.HierarchyDecorator.create(pc)
             ppd.add_child(pcd)
-            pcnd= IMP.core.NameDecorator.create(pc)
-            pcnd.set_name(str("child "+str(i)))
+            pc.set_name(str("child "+str(i)))
 
         self.assertEqual(ppd.get_number_of_children(), 10,
                          "Number of children is not as expected")
@@ -27,14 +25,12 @@ class DecoratorTests(IMP.test.TestCase):
         m = IMP.Model()
         pp=IMP.Particle(m)
         ppd= IMP.core.HierarchyDecorator.create(pp, t)
-        ppnd= IMP.core.NameDecorator.create(pp)
-        ppnd.set_name("root")
+        pp.set_name("root")
         for i in range(0,10):
             pc=IMP.Particle(m)
             pcd= IMP.core.HierarchyDecorator.create(pc, t)
             ppd.add_child(pcd)
-            pcnd= IMP.core.NameDecorator.create(pc)
-            pcnd.set_name(str("child "+str(i)))
+            pc.set_name(str("child "+str(i)))
 
         self.assertEqual(ppd.get_number_of_children(), 10,
                          "Number of children is not as expected")
