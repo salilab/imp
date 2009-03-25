3fc5e95b93a3463301d0551a11f926b3b62d8bf2
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-25 20:00:43 +0000

    add helper function to aid in creating temporaries
    git-svn-id: https://svn.salilab.org/imp/trunk@2098 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/include/Pointer.h
+++ kernel/include/Pointer.h
@@ -72,6 +72,12 @@ public:
   }
 };
 
+//! Make a ref counted pointer to an object. Useful for temporaries.
+template <class T>
+Pointer<T> make_pointer(T*t) {
+  return Pointer<T>(t);
+}
+
 IMP_END_NAMESPACE
 
 #endif  /* IMP_POINTER_H */
