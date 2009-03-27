5a8a73220d3eb9096ff1c41f350776220babd181
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-27 17:28:35 +0000

    forgot one
    git-svn-id: https://svn.salilab.org/imp/trunk@2165 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- examples/chain.py
+++ examples/chain.py
@@ -32,7 +32,7 @@ nbl= IMP.core.ClosePairsScoreState(chain)
 nbli= m.add_score_state(nbl)
 # Exclude bonds from closest pairs
 fl= nbl.get_close_pairs_container()
-fl.add_pair_container(IMP.atom.BondPairContainer())
+fl.add_pair_filter(IMP.atom.BondPairContainer())
 
 # Set up excluded volume
 ps= IMP.core.SphereDistancePairScore(IMP.core.HarmonicLowerBound(0,1))
