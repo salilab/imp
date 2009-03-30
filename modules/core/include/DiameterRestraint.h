ab4ed3454bba12e55d6d95cff7eb8a175d16b9c0
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-03-30 22:04:59 +0000

    clean up diameter restraint and update example
    git-svn-id: https://svn.salilab.org/imp/trunk@2189 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/include/DiameterRestraint.h
+++ modules/core/include/DiameterRestraint.h
@@ -20,12 +20,18 @@
 IMPCORE_BEGIN_NAMESPACE
 
 //! Restrain the diameter of a set of points
-/**
+/** This class also serves as an example of how to build restraints which
+    have internal ScoreStates and perhaps more than one actual restraint
+    object.
  */
 class IMPCOREEXPORT DiameterRestraint: public Restraint
 {
-  internal::ManageRestraint r_;
-  internal::ManageScoreState ss_;
+  Pointer<Restraint> r_;
+  Pointer<ScoreState> ss_;
+  Pointer<Particle> p_;
+  Float diameter_;
+  Pointer<SingletonContainer> sc_;
+  Pointer<UnaryFunction> f_;
 public:
   //! Use f to restraint sc to be withing diameter of one another
   /** f should have a minimum at 0 and be an upper bound-style function.
@@ -36,6 +42,8 @@ public:
   IMP_RESTRAINT(DiameterRestraint, internal::version_info);
 
   virtual ParticlesList get_interacting_particles() const;
+
+  void set_model(Model *m);
 };
 
 
--- modules/core/src/DiameterRestraint.cpp
+++ modules/core/src/DiameterRestraint.cpp
@@ -63,29 +63,47 @@ namespace {
 }
 
 DiameterRestraint::DiameterRestraint(UnaryFunction *f,
-                                     SingletonContainer *sc, Float diameter){
+                                     SingletonContainer *sc,
+                                     Float diameter):diameter_(diameter),
+                                                     sc_(sc), f_(f){
   IMP_check(sc->get_number_of_particles()>2,
             "Need at least two particles to restrain diameter",
             ValueException);
   IMP_check(diameter>0, "The diameter must be positive",
             ValueException);
-  Model *m= sc->get_particle(0)->get_model();
-  Particle *p= new Particle(m);
-  ss_.set(create_cover(p,
-                       new FixedRefiner(Particles(sc->particles_begin(),
-                                                  sc->particles_end())),
-                       FloatKey("diameter_radius"),
-                       0), NULL);
-  m->add_score_state(ss_);
-  // make pairs from special generator
-  Float radius= diameter/2.0;
-  SPairContainer *sp= new SPairContainer(sc, p);
-  r_.set(new PairsRestraint(new ShiftedDistancePairScore(f, radius), sp),
-         m);
+}
+
+void DiameterRestraint::set_model(Model *m) {
+  if (m) {
+    IMP_LOG(TERSE, "Creating components of DiameterRestraint" << std::endl);
+    Model *m= sc_->get_particle(0)->get_model();
+
+    // make pairs from special generator
+    Float radius= diameter_/2.0;
+
+    p_= new Particle(m);
+    SPairContainer *sp= new SPairContainer(sc_, p_);
+    r_=new PairsRestraint(new ShiftedDistancePairScore(f_, radius), sp);
+    ss_=create_cover(p_,
+                     new FixedRefiner(Particles(sc_->particles_begin(),
+                                                sc_->particles_end())),
+                     FloatKey("diameter_radius"),
+                     0);
+    m->add_score_state(ss_);
+    r_->set_was_owned(true);
+  } else {
+    IMP_LOG(TERSE, "Removing components of DiameterRestraint" << std::endl);
+    m->remove_score_state(ss_);
+    m->remove_particle(p_);
+    ss_=NULL;
+    p_=NULL;
+    r_=NULL;
+  }
+  Restraint::set_model(m);
 }
 
 Float DiameterRestraint::evaluate(DerivativeAccumulator *da) {
-  return 0;
+  return r_->evaluate(da);
 }
 
 void DiameterRestraint::show(std::ostream &out) const {
@@ -94,7 +112,7 @@ void DiameterRestraint::show(std::ostream &out) const {
 
 ParticlesList DiameterRestraint::get_interacting_particles() const {
   // all the interactions are returned by the actual restraint
-  return ParticlesList();
+  return r_->get_interacting_particles();
 }
 
 IMPCORE_END_NAMESPACE
