3eac56bb72f769264d269a0add2d291d34c6a776
dina@SALILAB.ORG <dina@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-04-03 22:38:58 +0000

    diameter improvement
    git-svn-id: https://svn.salilab.org/imp/trunk@2208 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/src/utility.cpp
+++ modules/core/src/utility.cpp
@@ -10,17 +10,14 @@
 #include <IMP/core/XYZDecorator.h>
 IMPCORE_BEGIN_NAMESPACE
 
-IMPCOREEXPORT algebra::Vector3D
-centroid(const Particles &ps) {
+algebra::Vector3D centroid(const Particles &ps) {
  algebra::Vector3D cen(0.0,0.0,0.0);
  for (Particles::const_iterator it = ps.begin(); it != ps.end();it++) {
-   if (core::XYZDecorator::is_instance_of(*it)) {
-     cen = cen + core::XYZDecorator(*it).get_coordinates();
-   }
+   cen = cen + core::XYZDecorator(*it).get_coordinates();
  }
- return cen*(1.0/ps.size());
+ return cen/ps.size();
 }
-IMPCOREEXPORT
+
 void transform(const Particles &ps,
                const algebra::Transformation3D &t) {
   algebra::Vector3D xyz_new;
@@ -33,33 +30,25 @@ void transform(const Particles &ps,
     }
   }
 }
-IMPCOREEXPORT
+
 algebra::Segment3D diameter(const Particles &ps) {
-  algebra::Vector3D lower_bound,upper_bound;
-   //read the points and determine the dimentions of the map
-  for (int i=0;i<3;i++) {
-    lower_bound[i]=INT_MAX;
-    upper_bound[i]=INT_MIN;
-  }
-  IMP::Float dist=INT_MIN;
-  IMP::algebra::Vector3D xyz1;
-  IMP::algebra::Vector3D xyz2;
-  for (IMP::Particles::const_iterator it = ps.begin();it != ps.end();it++){
-    if (core::XYZDecorator::is_instance_of(*it)){
-      xyz1 = IMP::core::XYZDecorator(*it).get_coordinates();
-      for (IMP::Particles::const_iterator it1 = it+1;it1 != ps.end();it1++){
-        if (core::XYZDecorator::is_instance_of(*it1)){
-          xyz2 = IMP::core::XYZDecorator(*it1).get_coordinates();
-          if (IMP::algebra::distance(xyz1,xyz2) >dist) {
-            lower_bound = xyz1;
-            upper_bound = xyz2;
-            dist = IMP::algebra::distance(xyz1,xyz2);
-          }
-        }
+  Particle *p1,*p2;
+  double max_squared_dist=0.0;
+
+  for (Particles::const_iterator it = ps.begin();it != ps.end();it++){
+    algebra::Vector3D xyz1 = XYZDecorator(*it).get_coordinates();
+    for (Particles::const_iterator it1 = it+1;it1 != ps.end();it1++){
+      algebra::Vector3D xyz2 = XYZDecorator(*it1).get_coordinates();
+      double curr_squared_dist = algebra::squared_distance(xyz1,xyz2);
+      if (curr_squared_dist > max_squared_dist) {
+        p1 = *it;
+        p2 = *it1;
+        max_squared_dist = curr_squared_dist;
       }
     }
   }
-  return algebra::Segment3D(lower_bound,upper_bound);
+  return algebra::Segment3D(XYZDecorator(p1).get_coordinates(),
+                            XYZDecorator(p2).get_coordinates());
 }
 
 IMPCORE_END_NAMESPACE
