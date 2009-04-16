7711d937d057c32e27a0cda117ae08c495d34a92
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-04-16 06:38:38 +0000

    simplify and document a bit better
    git-svn-id: https://svn.salilab.org/imp/trunk@2342 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/include/stream.h
+++ kernel/include/stream.h
@@ -12,87 +12,69 @@
 #include <fstream>
 #include <iostream>
 
-#if defined(SWIG)
+#if defined(IMP_DOXYGEN) || defined(SWIG)
 namespace std {
-  class ostream{
+  /** @name Streams for Python
+
+      C++ uses streams as its standard abstraction for input and
+      output. These cannot be trivially mapped onto the Python
+      IO layer. Instead we export the C++ stream classes to python.
+
+      \note The stream classes are in the std namespace in C++ but the
+      \imp Python module.
+      @{
+  */
+  //! Base class for all output streams
+  /** \see istream
+      \note The stream classes are in the std namespace in C++ but the
+      \imp Python module.
+  */
+  class ostream {
     ostream();
   };
+  //! Base class for all input streams
+  /** \see istream
+      \note The stream classes are in the std namespace in C++ but the
+      \imp Python module.
+  */
   class istream{
     istream();
   };
+  //! An output stream to a file.
   struct ofstream: public ostream {
+    //! Open the file with the given name.
+    /** No exception is thrown on failure. You should check using
+        is_open() instead.*/
     ofstream(char*name);
     bool is_open() const;
     void close();
     void open(char *name);
   };
+  //! An input stream from a file
   struct ifstream: public istream {
+    //! Open the file with the given name for input
+    /** No exception is thrown on failure. You should check using
+        is_open() instead.*/
     ifstream(char*name);
     bool is_open() const;
     void close();
     void open(char *name);
   };
+  //! An output stream to a string
   struct ostringstream: public ostream {
     ostringstream();
+    //! Get the string containing what has been output
     std::string str() const;
   };
+  //! An input stream to read from a string
   struct istringstream: public istream {
+    //! Read from the string str
     istringstream(std::string str);
   };
+
+
+  /** @} */
 }
-#endif
-
-#ifdef IMP_DOXYGEN
-/** @name Streams for Python
-
-    C++ uses streams as its standard abstraction for input and
-    output. These cannot be trivially mapped onto the Python
-    IO layer. Instead we export the C++ stream classes to python.
-    @{
-*/
-//! Base class for all output streams
-class ostream {
-  ostream();
-};
-//! Base class for all input streams
-class istream{
-  istream();
-};
-//! An output stream to a file.
-struct ofstream: public ostream {
-  //! Open the file with the given name.
-  /** No exception is thrown on failure. You should check using
-      is_open() instead.*/
-  ofstream(char*name);
-  bool is_open() const;
-  void close();
-  void open(char *name);
-};
-//! An input stream from a file
-struct ifstream: public istream {
-  //! Open the file with the given name for input
-  /** No exception is thrown on failure. You should check using
-      is_open() instead.*/
-  ifstream(char*name);
-  bool is_open() const;
-  void close();
-  void open(char *name);
-};
-//! An output stream to a string
-struct ostringstream: public ostream {
-  ostringstream();
-  //! Get the string containing what has been output
-  std::string str() const;
-};
-//! An input stream to read from a string
-struct istringstream: public istream {
-  //! Read from the string str
-  istringstream(std::string str);
-};
-
-
-/** @} */
-
 #endif
 
 #endif  /* IMP_STREAM_H */
