581d43a5e84cf45fcfa044c09590703d84917b84
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-04-17 20:53:44 +0000

    Add more comments.
    git-svn-id: https://svn.salilab.org/imp/trunk@2360 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/test/misc/test_random.py
+++ kernel/test/misc/test_random.py
@@ -8,10 +8,15 @@ class RandomTests(IMP.test.TestCase):
         IMP.random_number_generator.seed(24)
         a1 = IMP.random_number_generator()
         a2 = IMP.random_number_generator()
+        # In principle these values could be the same, I suppose, but it is
+        # rather unlikely!
         self.assertNotEqual(a1, a2)
+        # Reseeding should give a predictable sequence
         IMP.random_number_generator.seed(24)
         a3 = IMP.random_number_generator()
+        a4 = IMP.random_number_generator()
         self.assertEqual(a1, a3)
+        self.assertEqual(a2, a4)
 
 if __name__ == '__main__':
     unittest.main()
