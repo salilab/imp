e4bf6ddc5c6f661a20ef16d0d006da414a513bda
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-04-21 19:41:13 +0000

    add class to accelerate collision detection between rigid bodies. will move into separate header later
    git-svn-id: https://svn.salilab.org/imp/trunk@2579 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/include/internal/rigid_bodies.h
+++ modules/core/include/internal/rigid_bodies.h
@@ -11,6 +11,7 @@
 #include "../config.h"
 #include "../XYZDecorator.h"
 #include "../HierarchyDecorator.h"
+#include <IMP/algebra/Sphere3D.h>
 
 IMPCORE_BEGIN_INTERNAL_NAMESPACE
 
@@ -84,6 +85,44 @@ inline void add_required_attributes_for_member(Particle *p) {
   HierarchyDecorator::create(p, rigid_body_data().htraits_);
   XYZDecorator::cast(p);
 }
+
+
+class IMPCOREEXPORT RigidBodyParticleData {
+  struct Data {
+    std::vector<int> storage_;
+    algebra::Sphere3D s_;
+  };
+  std::vector<Data> data_;
+public:
+  RigidBodyParticleData();
+  void set_sphere(unsigned int ni, const algebra::Sphere3D &s);
+  void set_leaf(unsigned int ni, const std::vector<unsigned int> &ids);
+  unsigned int add_children(unsigned int ni, unsigned int num_children);
+  bool get_is_leaf(unsigned int ni) const;
+  unsigned int get_number_of_particles(unsigned int ni) const;
+  unsigned int get_number_of_children(unsigned int ni) const;
+  unsigned int get_child(unsigned int ni, unsigned int i) const;
+  unsigned int get_particle(unsigned int ni, unsigned int i) const;
+  const algebra::Sphere3D &get_sphere(unsigned int ni) const;
+  void show_tree(std::ostream &out) const;
+};
+
+
+typedef std::vector<unsigned int> SphereIndexes;
+typedef std::vector<SphereIndexes> SpheresSplit;
+
+
+class IMPCOREEXPORT RigidBodyCollisionData {
+  std::map<Particle*, RigidBodyParticleData> data_;
+public:
+  // for a non rigid body
+  void add_data(Particle *p, const algebra::Sphere3D &s);
+  void add_data(Particle *p);
+  RigidBodyParticleData &get_data(Particle *p);
+  const RigidBodyParticleData &get_data(Particle *p) const;
+  bool has_data(Particle *p) const;
+  void show(std::ostream &out) const;
+};
 
 
 IMPCORE_END_INTERNAL_NAMESPACE
