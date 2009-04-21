b624a6d1d2f179d10ff634b6ead997ae93837120
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-04-21 19:39:58 +0000

    add collision detection benchmarks. not really tested yet
    git-svn-id: https://svn.salilab.org/imp/trunk@2578 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ benchmarks/rigid_collisons.cpp
@@ -0,0 +1,82 @@
+/**
+ * Copyright 2007-9 Sali Lab. All rights reserved.
+ */
+
+#include <IMP.h>
+#include <IMP/core.h>
+#include <IMP/algebra.h>
+#include <IMP/atom.h>
+#include <boost/timer.hpp>
+
+using namespace IMP;
+using namespace IMP::core;
+using namespace IMP::algebra;
+using namespace IMP::atom;
+
+void test_one(Model *m,
+              std::vector<RigidBodyDecorator> rbs,
+              ListSingletonContainer *lsc,
+              float side) {
+  Vector3D minc(0,0,0), maxc(side, side, side);
+  unsigned int reps=10;
+  boost::timer t;
+  for (unsigned int k=0; k< reps; ++k) {
+    for (unsigned int i=0; i< rbs.size(); ++i) {
+      Vector3D t= random_vector_in_box(minc, maxc);
+      Rotation3D r= random_rotation();
+      Transformation3D tr(r, t);
+      rbs[i].set_transformation(tr);
+    }
+
+    m->evaluate(false);
+  }
+
+  std::cout << " took " << t.elapsed()/reps
+            << std::endl;
+}
+
+int main() {
+  Pointer<Model> m(new Model());
+  Particles atoms;
+  std::vector<RigidBodyDecorator> rbs;
+  for (int i=0; i< 10; ++i) {
+    MolecularHierarchyDecorator mhd
+      = read_pdb("benchmarks/input/single_protein.pdb", m);
+    Particles catoms= get_by_type(mhd, MolecularHierarchyDecorator::ATOM);
+    atoms.insert(atoms.end(), catoms.begin(), catoms.begin());
+    ScoreState *ss= create_rigid_body(mhd.get_particle(),
+                                       catoms);
+    m->add_score_state(ss);
+    rbs.push_back(RigidBodyDecorator(mhd.get_particle()));
+    set_enclosing_sphere(XYZRDecorator::create(mhd.get_particle()),
+                         catoms);
+  }
+  IMP_NEW(lsc, ListSingletonContainer, ());
+  IMP_NEW(qcpf, BoxSweepClosePairsFinder, ());
+  ClosePairsScoreState *cpss= new ClosePairsScoreState(lsc);
+  cpss->set_close_pairs_finder(qcpf);
+  m->add_score_state(cpss);
+  IMP_NEW(pr, PairsRestraint,
+          (new DistancePairScore(new Harmonic(0,1)),
+           cpss->get_close_pairs_container()));
+  m->add_restraint(pr);
+  {
+    lsc->set_particles(atoms);
+    std::cout << "Box:" << std::endl;
+    test_one(m, rbs, lsc, 100);
+    test_one(m, rbs, lsc, 1000);
+  }
+  {
+    Particles rbsp(rbs.size());
+    for (unsigned int i=0; i< rbs.size(); ++i){
+      rbsp[i]= rbs[i].get_particle();
+    }
+    lsc->set_particles(rbsp);
+    RigidClosePairScore *rcps= new RigidClosePairScore(pr->get_pair_score());
+    pr->set_pair_score(rcps);
+    std::cout << "Hierarchy:" << std::endl;
+    test_one(m, rbs, lsc, 100);
+    test_one(m, rbs, lsc, 1000);
+  }
+  return 0;
+}
