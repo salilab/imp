1bd8ddd34ee03f0109bc89060aaedf4fbc9a1f35
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-04-22 22:06:16 +0000

    add example for rigid collisions
    git-svn-id: https://svn.salilab.org/imp/trunk@2629 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ examples/simple_examples/rigid_collisions.py
@@ -0,0 +1,41 @@
+import IMP
+import IMP.core
+import IMP.atom
+
+# This example addes a restraint on nonbonded interactions
+# Since it is between two rigid bodies, internal interactions are ignored
+
+m= IMP.Model()
+# The particles in the rigid bodies
+rbps0= IMP.core.create_xyzr_particles(m, 50, 1)
+rbps1= IMP.core.create_xyzr_particles(m, 50, 1)
+
+rbp0= IMP.Particle(m)
+rbp1= IMP.Particle(m)
+
+rbss0 = IMP.core.create_rigid_body(rbp0, rbps0)
+m.add_score_state(rbss0)
+
+rbss1 = IMP.core.create_rigid_body(rbp1, rbps1)
+m.add_score_state(rbss1)
+
+lsc= IMP.core.ListSingletonContainer()
+lsc.add_particle(rbp0)
+lsc.add_particle(rbp1)
+
+# Set up the nonbonded list
+nbl= IMP.core.ClosePairsScoreState(lsc, IMP.core.RigidClosePairsFinder())
+m.add_score_state(nbl)
+# Set the amount particles need to move before the list is updated
+nbl.set_slack(2.0)
+
+# Set up excluded volume
+ps= IMP.core.SphereDistancePairScore(IMP.core.HarmonicLowerBound(0,1))
+evr= IMP.core.PairsRestraint(ps, nbl.get_close_pairs_container())
+evri= m.add_restraint(evr)
+
+# Set up optimizer
+o= IMP.core.ConjugateGradients()
+o.set_model(m)
+
+o.optimize(1000)
