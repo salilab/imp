ac995d844ad02a99569bdc1167f0d444d5d12d55
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-04-22 14:50:25 +0000

    - Ignore *.pyc files in test subdirectories. - Don't fall over when run on Python-only modules.
    git-svn-id: https://svn.salilab.org/imp/trunk@2612 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- tools/cleanup-module-svn.sh
+++ tools/cleanup-module-svn.sh
@@ -1,19 +1,23 @@
 #!/bin/sh
 echo "Module is " $1
 tmpfile="/tmp/props"
-svn propset svn:ignore config.h modules/$1/include
-svn propset svn:ignore version_info.h modules/$1/include/internal
-echo "*.os
+if test -d modules/$1/include; then
+  svn propset svn:ignore config.h modules/$1/include
+  svn propset svn:ignore version_info.h modules/$1/include/internal
+fi
+if test -d modules/$1/src; then
+  echo "*.os
 *.obj
 version_info.cpp
 link_0.cpp
 link_1.cpp" > $tmpfile
-svn propset svn:ignore --file $tmpfile  modules/$1/src/internal
-rm -rf $tmpfile
-echo "*.os
+  svn propset svn:ignore --file $tmpfile  modules/$1/src/internal
+  rm -rf $tmpfile
+  echo "*.os
 *.obj" > $tmpfile
-svn propset svn:ignore --file $tmpfile  modules/$1/src/
-rm -rf $tmpfile
+  svn propset svn:ignore --file $tmpfile  modules/$1/src/
+  rm -rf $tmpfile
+fi
 echo "IMP.$1.py
 $1_config.i
 $1_wrap.cc
@@ -26,3 +30,8 @@ echo "*.pyc
 test.passed" > $tmpfile
 svn propset svn:ignore --file $tmpfile modules/$1/test
 rm -rf $tmpfile
+for dir in modules/$1/test/*; do
+  if test -d "$dir" -a "$dir" != "modules/$1/test/input"; then
+    svn propset svn:ignore "*.pyc" "$dir"
+  fi
+done
