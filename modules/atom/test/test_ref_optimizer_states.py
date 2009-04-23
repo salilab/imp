ed491399b8740b7bef8f3a074f51bd4f9afed99a
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-04-23 00:58:41 +0000

    Make sure that refcounting of OptimizerStates works correctly in Python.
    git-svn-id: https://svn.salilab.org/imp/trunk@2640 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/atom/test/test_ref_optimizer_states.py
@@ -0,0 +1,85 @@
+import unittest
+import IMP
+import IMP.utils
+import IMP.test
+import IMP.atom
+
+class RefCountTests(IMP.test.TestCase):
+    """Test refcounting of OptimizerStates"""
+
+    def test_simple(self):
+        """Check reference counting of OptimizerStates"""
+        refcnt = IMP.test.RefCountChecker(self)
+        ps = IMP.Particles()
+        s = IMP.atom.VelocityScalingOptimizerState(ps, 300., 10)
+        o = IMP.atom.MolecularDynamics()
+        o.add_optimizer_state(s)
+        refcnt.assert_number(2)
+        # Optimizer should hold a ref to state, so nothing should be freed
+        # until it is
+        del s
+        refcnt.assert_number(2)
+        del o
+        refcnt.assert_number(0)
+
+    def test_delete_optimizer_constructor(self):
+        """Constructed Python states should survive optimizer deletion"""
+        refcnt = IMP.test.RefCountChecker(self)
+        o = IMP.atom.MolecularDynamics()
+        ps = IMP.Particles()
+        s = IMP.atom.VelocityScalingOptimizerState(ps, 300., 10)
+        o.add_optimizer_state(s)
+        self.assertEqual(s.get_ref_count(), 2)
+        refcnt.assert_number(2)
+        # New state s should not go away until we free the Python reference
+        del o
+        refcnt.assert_number(1)
+        self.assertEqual(s.get_ref_count(), 1)
+        del s
+        refcnt.assert_number(0)
+
+    def test_delete_optimizer_iterator(self):
+        """OptimizerStates from iterators should survive Optimizer deletion"""
+        refcnt = IMP.test.RefCountChecker(self)
+        o = IMP.atom.MolecularDynamics()
+        ps = IMP.Particles()
+        s = IMP.atom.VelocityScalingOptimizerState(ps, 300., 10)
+        o.add_optimizer_state(s)
+        del s
+        # Now create new Python OptimizerState s from C++ iterator
+        # This calls swig::from() internally, which is modified by template
+        # specialization in our SWIG interface.
+        s = o.get_optimizer_states().__iter__().value()
+        # Python reference s plus C++ reference from o
+        self.assertEqual(s.get_ref_count(), 2)
+        del o
+        # Now only the Python reference s should survive
+        self.assertEqual(s.get_ref_count(), 1)
+        refcnt.assert_number(1)
+        del s
+        refcnt.assert_number(0)
+
+    def test_delete_optimizer_accessor(self):
+        "OptimizerStates from vector accessors should survive Optimizer del."
+        refcnt = IMP.test.RefCountChecker(self)
+        o = IMP.atom.MolecularDynamics()
+        ps = IMP.Particles()
+        s = IMP.atom.VelocityScalingOptimizerState(ps, 300., 10)
+        o.add_optimizer_state(s)
+        del s
+        # Now create new Python OptimizerState s from a C++ vector accessor
+        # These accessors call specific methods in the SWIG wrapper which
+        # are modified by typemaps in our interface.
+        s = o.get_optimizer_states()[0]
+        # Python reference s plus C++ reference from o
+        self.assertEqual(s.get_ref_count(), 2)
+        del o
+        # Now only the Python reference s should survive
+        self.assertEqual(s.get_ref_count(), 1)
+        refcnt.assert_number(1)
+        del s
+        refcnt.assert_number(0)
+
+
+if __name__ == '__main__':
+    unittest.main()
