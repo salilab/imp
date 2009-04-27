890f591ed90635c14b856cfcd6434afb5d79ba33
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-04-27 22:14:37 +0000

    don't use modeller
    git-svn-id: https://svn.salilab.org/imp/trunk@2684 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/misc/test/test_statistics.py
+++ modules/misc/test/test_statistics.py
@@ -2,7 +2,7 @@ import unittest
 import IMP
 import IMP.test
 import IMP.misc
-import IMP.modeller
+import IMP.atom
 import math
 
 class TunnelTest(IMP.test.TestCase):
@@ -11,7 +11,7 @@ class TunnelTest(IMP.test.TestCase):
     def test_score(self):
         """Test printing usage"""
         m= IMP.Model()
-        mh= IMP.modeller.read_pdb(self.get_input_file_name('single_protein.pdb'), m)
+        mh= IMP.atom.read_pdb(self.get_input_file_name('single_protein.pdb'), m)
         IMP.misc.show_attribute_usage(m)
 
 if __name__ == '__main__':
