975baf8c6bd9747b36a2106d9dd8b825657a6952
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-05-06 00:19:30 +0000

    make shortest_segment stuff usable outside of the .cpp file, but still hidden
    git-svn-id: https://svn.salilab.org/imp/trunk@2770 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/algebra/include/internal/shortest_segment.h
@@ -0,0 +1,27 @@
+/**
+ *  \file cgal_predicates.h
+ *  \brief predicates implemented using CGAL
+ *  Copyright 2007-9 Sali Lab. All rights reserved.
+*/
+
+#ifndef IMPALGEBRA_INTERNAL_SHORTEST_SEGMENT_H
+#define IMPALGEBRA_INTERNAL_SHORTEST_SEGMENT_H
+
+#include "../config.h"
+#include <IMP/base_types.h>
+#include "../Segment3D.h"
+
+IMPALGEBRA_BEGIN_INTERNAL_NAMESPACE
+
+// defined in Segment3D.cpp
+
+IMPALGEBRAEXPORT
+Segment3D shortest_segment(const Segment3D &s,
+                           const Vector3D &p);
+
+IMPALGEBRAEXPORT
+Segment3D shortest_segment(const Segment3D &sa,
+                           const Segment3D &sb);
+IMPALGEBRA_END_INTERNAL_NAMESPACE
+
+#endif  /* IMPALGEBRA_INTERNAL_SHORTEST_SEGMENT_H */
