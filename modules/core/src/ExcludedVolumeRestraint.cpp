4bb8b90c39fc57cc56f58e244a44519dc62afec1
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-05-16 00:42:01 +0000

    add class to simplify setting up excluded volume interactions
    git-svn-id: https://svn.salilab.org/imp/trunk@2855 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/core/src/ExcludedVolumeRestraint.cpp
@@ -0,0 +1,80 @@
+/**
+ *  \file ExcludedVolumeRestraint.cpp
+ *  \brief XXXX.
+ *
+ *  Copyright 2007-9 Sali Lab. All rights reserved.
+ *
+ */
+
+#include "IMP/core/ExcludedVolumeRestraint.h"
+#include <IMP/PairContainer.h>
+#include <IMP/core/XYZR.h>
+#include <IMP/core/FixedRefiner.h>
+#include <IMP/core/PairsRestraint.h>
+#include <IMP/core/CoverRefined.h>
+#include <IMP/core/HarmonicLowerBound.h>
+#include <IMP/core/RigidClosePairsFinder.h>
+#include <IMP/core/SphereDistancePairScore.h>
+#include <IMP/core/DistancePairScore.h>
+#include <IMP/core/rigid_bodies.h>
+#include <IMP/core/internal/evaluate_distance_pair_score.h>
+#include <boost/lambda/lambda.hpp>
+
+IMPCORE_BEGIN_NAMESPACE
+
+ExcludedVolumeRestraint::ExcludedVolumeRestraint(SingletonContainer *sc,
+                                                 double k):
+  sc_(sc), k_(k){
+}
+
+void ExcludedVolumeRestraint::set_model(Model *m) {
+  if (m) {
+    IMP_LOG(TERSE, "Creating components of ExcludedVolumeRestraint"
+            << std::endl);
+    IMP_NEW(HarmonicLowerBound, hlb, (0,k_));
+    IMP_NEW(RigidClosePairsFinder, rcpf, ());
+    ss_= new ClosePairsScoreState(sc_);
+    ss_->set_close_pairs_finder(rcpf);
+    IMP_NEW(SphereDistancePairScore, sdps, (hlb));
+    pr_= new PairsRestraint(sdps, ss_->get_close_pairs_container());
+
+    for (SingletonContainer::ParticleIterator it= sc_->particles_begin();
+         it != sc_->particles_end(); ++it) {
+      if (RigidBody::is_instance_of(*it)) {
+        RigidBody rb(*it);
+        if (!XYZR::is_instance_of(*it)) {
+          XYZR d= XYZR::create(*it);
+          set_enclosing_radius(d, rb.get_member_particles());
+        }
+      }
+    }
+
+    m->add_score_state(ss_);
+    pr_->set_model(m);
+  } else {
+    IMP_LOG(TERSE, "Removing components of ExcludedVolumeRestraint"
+            << std::endl);
+    IMP_CHECK_OBJECT(ss_.get());
+    IMP_CHECK_OBJECT(pr_.get());
+    m->remove_score_state(ss_);
+    ss_=NULL;
+    pr_=NULL;
+  }
+  Restraint::set_model(m);
+}
+
+Float ExcludedVolumeRestraint::evaluate(DerivativeAccumulator *da) {
+  IMP_OBJECT_LOG;
+  IMP_CHECK_OBJECT(pr_.get());
+  return pr_->evaluate(da);
+}
+
+void ExcludedVolumeRestraint::show(std::ostream &out) const {
+  out << "ExcludedVolumeRestraint" << std::endl;
+}
+
+ParticlesList ExcludedVolumeRestraint::get_interacting_particles() const {
+  return pr_->get_interacting_particles();
+}
+
+IMPCORE_END_NAMESPACE
