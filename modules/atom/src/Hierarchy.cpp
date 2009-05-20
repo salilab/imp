3dc7c1ee9e3e227c34b7b810e9fe7b01819f467c
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-05-20 00:13:15 +0000

    add code to simplify a molecular hierarchy. Definitely in beta. Also add methods to get back various representatations from a molecular hierarchy to start having a standard;
    git-svn-id: https://svn.salilab.org/imp/trunk@2871 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/src/Hierarchy.cpp
+++ modules/atom/src/Hierarchy.cpp
@@ -17,6 +17,11 @@
 #include <IMP/core/Harmonic.h>
 #include <IMP/core/ConnectivityRestraint.h>
 #include <IMP/core/DistancePairScore.h>
+#include <IMP/algebra/Sphere3D.h>
+#include <IMP/core/internal/Grid3D.h>
+#include <debug/vector>
+
+#include <boost/random/uniform_int.hpp>
 
 #include <sstream>
 #include <set>
@@ -36,7 +41,7 @@ IntKey Hierarchy::get_type_key() {
 }
 
 void Hierarchy::show(std::ostream &out,
-                                       std::string prefix) const
+                     std::string prefix) const
 {
   if (*this == Hierarchy()) {
     out << "NULL Molecular Hierarchy node";
@@ -266,6 +271,69 @@ Hierarchy clone(Hierarchy d) {
   n=.5*(3*V+2*PI*r^3*f^3-6*PI*r^3*f^2)/((-3*f^2+f^3+2)*r^3*PI)
  */
 
+IMPATOMEXPORT Particles
+get_detailed_representation(Hierarchy hi){
+  Particles ret;
+  std::vector<Hierarchy> front;
+  front.push_back(hi);
+  do {
+    Hierarchy h= front.back();
+    front.pop_back();
+    //IMP_LOG(VERBOSE, "Trying " << h << std::endl);
+    if (h.get_number_of_children() > 0
+        && core::XYZ::is_instance_of(h.get_child(0).get_particle())) {
+      front.insert(front.end(),
+                   h.children_begin(),
+                   h.children_end());
+    } else if (core::XYZ::is_instance_of(h.get_particle())) {
+      ret.push_back(h.get_particle());
+    } else {
+       front.insert(front.end(),
+                   h.children_begin(),
+                   h.children_end());
+    }
+  } while (!front.empty());
+  IMP_LOG(VERBOSE, "Detailed representation has size " << ret.size()
+          << std::endl);
+  return ret;
+}
+
+IMPATOMEXPORT Particles
+get_simplified_representation(Hierarchy h) {
+  Particles ret;
+  std::vector<Hierarchy> front;
+  front.push_back(h);
+  do {
+    Hierarchy h= front.back();
+    front.pop_back();
+    //IMP_LOG(VERBOSE, "Trying " << h << std::endl);
+    if (core::XYZ::is_instance_of(h.get_particle())) {
+      ret.push_back(h.get_particle());
+    } else {
+      front.insert(front.end(),
+                   h.children_begin(),
+                   h.children_end());
+    }
+  } while (!front.empty());
+  return ret;
+}
+
+algebra::BoundingBox3D bounding_box(const Hierarchy &h,
+                                    FloatKey r) {
+  Particles rep= get_simplified_representation(h);
+  algebra::BoundingBox3D bb;
+  for (unsigned int i=0; i< rep.size(); ++i) {
+    core::XYZR xyzr= core::XYZR::cast(rep[i], r);
+    if (xyzr) {
+      bb+= algebra::bounding_box(xyzr.get_sphere());
+    } else if (core::XYZ::is_instance_of(rep[i])) {
+      bb+= algebra::BoundingBox3D(core::XYZ(rep[i]).get_coordinates());
+    }
+  }
+  IMP_LOG(VERBOSE, "Bounding box is " << bb << std::endl);
+  return bb;
+}
+
 
 namespace {
   std::pair<int, double> compute_n(double V, double r, double f) {
@@ -311,6 +379,334 @@ IMPATOMEXPORT Restraint* create_protein(Particle *p,
   core::ConnectivityRestraint* cr= new core::ConnectivityRestraint(dps);
   cr->set_particles(ps);
   return cr;
+}
+
+
+namespace {
+  typedef __gnu_debug::vector<algebra::Sphere3D> Spheres;
+  typedef __gnu_debug::vector<algebra::Vector3D> Vectors;
+  typedef __gnu_debug::vector<unsigned int> Indexes;
+
+
+  double cell_size(double resolution) {
+    return resolution/6;
+  }
+
+  algebra::Sphere3D get_sphere(Particle *p) {
+    if (core::XYZR::is_instance_of(p)) {
+      return core::XYZR(p).get_sphere();
+    } else {
+      return algebra::Sphere3D(core::XYZ(p).get_coordinates(),
+                               1);
+    }
+  }
+
+
+  void teleport(Spheres &centers, double resolution) {
+    double minr= std::numeric_limits<double>::max();
+      double maxr=-minr;
+      int minc=-1;
+      int maxc=-1;
+      for (unsigned int i=0; i< centers.size(); ++i) {
+        if (centers[i].get_radius() < minr) {
+          minr= centers[i].get_radius();
+          minc= i;
+        }
+        if (centers[i].get_radius() > maxr) {
+          maxr= centers[i].get_radius();
+          maxc=i;
+        }
+      }
+      IMP_LOG(VERBOSE, "Teleporting " << centers[minc] << " to ");
+      // move minr to near maxr
+      algebra::Vector3D offset(0,0, cell_size(resolution));
+      centers[minc]=algebra::Sphere3D(centers[maxc].get_center()
+                                      +offset,
+                                      centers[maxc].get_radius());
+      centers[maxc]=algebra::Sphere3D(centers[maxc].get_center()
+                                      -offset,
+                                      centers[maxc].get_radius());
+      IMP_LOG(VERBOSE, centers[minc] << std::endl);
+  }
+
+  void rasterize_io(Hierarchy in, double res,
+                    Vectors &inside,
+                    Vectors &outside) {
+
+    algebra::BoundingBox3D bb= bounding_box(in);
+    bb+= res;
+    typedef IMP::core::internal::Grid3D<bool> Grid;
+    Grid grid(cell_size(res), bb.get_corner(0), bb.get_corner(1),
+              false);
+    Particles leaves= get_detailed_representation(in);
+    algebra::Vector3D ones(1,1,1);
+    for (unsigned int i=0; i< leaves.size(); ++i) {
+      algebra::Sphere3D s= get_sphere(leaves[i]);
+      //s= algebra::Sphere3D(s.get_center(), s.get_radius());
+      algebra::Vector3D lb= s.get_center()-s.get_radius()*ones;
+      algebra::Vector3D ub= s.get_center()+s.get_radius()*ones;
+      Grid::VirtualIndex lbi= grid.get_virtual_index(lb);
+      Grid::VirtualIndex ubt=grid.get_virtual_index(ub);
+      Grid::VirtualIndex ubi(ubt[0]+1, ubt[1]+1, ubt[2]+1);
+      for (Grid::IndexIterator it= grid.indexes_begin(lbi, ubi);
+           it != grid.indexes_end(lbi, ubi); ++it) {
+        if (!grid.get_voxel(*it)) {
+          algebra::Vector3D center= grid.get_center(*it);
+          if (s.get_contains(center)) {
+            grid.get_voxel(*it)=true;
+          }
+        }
+      }
+      grid.get_voxel(grid.get_index(s.get_center()))=true;
+    }
+    for (Grid::IndexIterator it= grid.all_indexes_begin();
+           it != grid.all_indexes_end(); ++it) {
+      if (grid.get_voxel(*it)) {
+        inside.push_back(grid.get_center(*it));
+      } else {
+        outside.push_back(grid.get_center(*it));
+      }
+    }
+  }
+
+  void sort_outside(const Vectors &outside,
+                    const Spheres &centers,
+                    std::vector< std::vector<float> > &dists) {
+    for (unsigned int i=0; i< centers.size(); ++i) {
+      dists[i].resize(outside.size());
+      for (unsigned int j=0; j< outside.size(); ++j) {
+        dists[i][j]= distance(outside[j], centers[i].get_center());
+      }
+      std::sort(dists[i].begin(), dists[i].end());
+      /*IMP_LOG(VERBOSE, "Sphere " << i << " has lb of " << dists[i].front()
+        << " and ub of " << dists[i].back() << std::endl);*/
+    }
+  }
+
+  int assign(const std::vector< std::vector<float> > &dists,
+             const Spheres &centers,
+             const Indexes &offsets,
+             const algebra::Vector3D &v) {
+    double best_score=std::numeric_limits<double>::max();
+    int best_center=-1;
+    unsigned int best_offset=-1;
+    for (unsigned int j=0; j< centers.size(); ++j) {
+      float dist = distance(v, centers[j].get_center());
+      std::vector<float>::const_iterator it= std::upper_bound(dists[j].begin(),
+                                                              dists[j].end(),
+                                                              dist);
+      /*IMP_LOG(VERBOSE, "Point " << i << " center " << j
+        << " dist " << dist << " bound " << *it << std::endl);*/
+      int score;
+      /*if (it -dists[j].begin() < offsets[j]) {
+        score=0;
+        } else {*/
+      score= std::distance(dists[j].begin(), it);
+      if (score < offsets[j]) score=0;
+      else score= score-offsets[j];
+      //}
+      if (score < best_score) {
+        best_score=score;
+        best_center=j;
+        best_offset= score;
+      }
+    }
+    return best_center;
+  }
+
+
+  void compute_assignments(const Vectors &inside,
+                           const std::vector< std::vector<float> > &dists,
+                           const Spheres &centers,
+                           Indexes &offsets,
+                           Indexes &assignments) {
+     for (unsigned int i=0; i< inside.size(); ++i) {
+       assignments[i]= assign(dists, centers, offsets, inside[i]);
+       float dist= distance(centers[assignments[i]].get_center(), inside[i]);
+       unsigned int score= std::upper_bound(dists[assignments[i]].begin(),
+                                            dists[assignments[i]].end(),
+                                            dist)
+         - dists[assignments[i]].begin();
+       offsets[assignments[i]]= std::max(offsets[assignments[i]], score);
+    }
+  }
+  void recompute_centers(const Vectors &inside, const Indexes &assignments,
+                         double resolution,
+                         Spheres &centers) {
+    boost::uniform_int<int> r(0, inside.size()-1);
+    for (unsigned int i=0; i< centers.size(); ++i) {
+      algebra::Sphere3Ds cur;
+      for (unsigned int j=0; j< inside.size(); ++j) {
+        if (assignments[j]==i) {
+          cur.push_back(algebra::Sphere3D(inside[j], 0));
+        }
+      }
+      if (cur.empty()) {
+        int index= r(random_number_generator);
+        centers[i]= algebra::Sphere3D(inside[index],
+                                      0);
+      } else if (cur.size() ==1) {
+        centers[i]= algebra::Sphere3D(cur[0].get_center(),
+                                      cell_size(resolution));
+      } else {
+        centers[i]= enclosing_sphere(cur);
+      }
+    }
+  }
+
+  void write_spheres(Spheres &centers,
+                     unsigned int index) {
+    std::ostringstream oss;
+    oss << "Spheres." << index << ".bild";
+    std::ofstream out(oss.str().c_str());
+    for (unsigned int i=0; i< centers.size(); ++i) {
+      out << ".sphere " << spaces_io(centers[i].get_center())
+          << " " << centers[i].get_radius() << std::endl;
+    }
+  }
+
+  void write_outside(const Vectors &vs) {
+    std::ostringstream oss;
+    oss << "Outside"
+        << ".bild";
+    std::ofstream out(oss.str().c_str());
+    out << ".color 10\n";
+    for (unsigned int i=0; i< vs.size(); ++i) {
+      out << ".sphere " << spaces_io(vs[i])
+          << " " << .5 << std::endl;
+    }
+  }
+
+  void write_assignments(const Vectors &inside,
+                         const Indexes &assignments,
+                         unsigned int index) {
+    std::ostringstream oss;
+    oss << "Assignments." << index << ".bild";
+    std::ofstream out(oss.str().c_str());
+    for (unsigned int i=0; i< assignments.size(); ++i) {
+      out << ".color " << assignments[i]*5 << std::endl;
+      out << ".sphere " << spaces_io(inside[i])
+          << " " << .5 << std::endl;
+    }
+  }
+
+  double compute_score(const Spheres &centers,
+                       const std::vector<std::vector<float> > &dists) {
+    double sum=0;
+    for (unsigned int i=0; i< centers.size(); ++i) {
+      double score= std::upper_bound(dists[i].begin(),
+                                     dists[i].end(),
+                                     centers[i].get_radius())-dists[i].begin();
+      sum+= score;
+    }
+    return sum;
+  }
+}
+
+/** Simplify a molecular hierarchy to be represented by fewer
+    balls.
+    given current center locations, perform iterations of:
+    - assign each point in order of distance to the nearest center to the
+    center where addition minimizes outside volume (nkE)
+    - recenter centers (nk)
+    - if no improvement, move a center to split the largest sphere
+
+    To measure error:
+    - sort all outside points based on all their distances to each center
+      (knlogn)
+    - see how many new ones are needed, E=(1)
+
+    Then:
+    - take each amino acid, figure out which sphere it is closest to
+    add it as a child of that sphere with no coordinates
+ */
+Hierarchy simplify_protein(Hierarchy in,
+                           double resolution) {
+  // compute outside points
+  Vectors outside;
+  Vectors inside;
+  IMP_LOG(VERBOSE, "Rasterizing protein " << std::flush);
+  rasterize_io(in, resolution, inside, outside);
+  write_outside(outside);
+  IMP_LOG(VERBOSE, inside.size() << " inside and " << outside.size()
+          << " outside " << std::endl);
+  std::random_shuffle(inside.begin(), inside.end());
+  // compute initial number of centers
+  int num_centers=inside.size()/40;
+  IMP_LOG(VERBOSE, num_centers << " centers" << std::endl);
+
+  // produce random initial centers
+  Spheres centers(num_centers);
+  boost::uniform_int<int> r(0, inside.size()-1);
+  for (unsigned int i=0; i< centers.size(); ++i) {
+    int index= r(random_number_generator);
+    centers[i]= algebra::Sphere3D(inside[index], 10000);
+  }
+  write_spheres(centers, 0);
+  Indexes assignments(inside.size());
+  double old_score= std::numeric_limits<double>::max();
+  std::vector< std::vector<float> > dists(centers.size());
+  for (unsigned int i=0; i<100; ++i) {
+  // generate sorted lists of distance
+
+    IMP_LOG(VERBOSE, "Sorting outside " << std::endl);
+    sort_outside(outside, centers, dists);
+    Indexes offsets(centers.size());
+    double new_score= compute_score(centers, dists);
+    IMP_LOG(VERBOSE, "Score is " << new_score << std::endl);
+
+    if (new_score==0) break;
+    if (old_score <= new_score) {
+      IMP_LOG(VERBOSE, "teleporting " << std::endl);
+      teleport(centers, resolution);
+      sort_outside(outside, centers, dists);
+    }
+    old_score=new_score;
+    IMP_LOG(VERBOSE, "Computing assignments " << std::endl);
+    compute_assignments(inside, dists, centers, offsets,  assignments);
+    write_assignments(inside, assignments, i);
+    IMP_LOG(VERBOSE, "Recomputing centers " << std::endl);
+    recompute_centers(inside, assignments, resolution, centers);
+    write_spheres(centers, i+1);
+  }
+
+  IMP_NEW(Particle, root, (in.get_particle()->get_model()));
+  Hierarchy h= Hierarchy::create(root, in.get_type());
+  for (unsigned int i=0; i< centers.size(); ++i) {
+    IMP_NEW(Particle, c, (in.get_particle()->get_model()));
+    Hierarchy hc= Hierarchy::create(c, Hierarchy::FRAGMENT);
+    h.add_child(hc);
+    core::XYZR::create(c, centers[i]);
+  }
+
+  Particles l= get_detailed_representation(in);
+  for (unsigned int j=0; j < l.size(); ++j) {
+    Particle *rp=NULL;
+    Atom ad= Atom::cast(l[j]);
+    if (ad && ad.get_atom_type() == AT_CA) {
+      rp= get_residue(Atom(l[j])).get_particle();
+    } else if (Residue::is_instance_of(l[j])) {
+      rp=l[j];
+    }
+    if (rp) {
+      IMP_NEW(Particle, res, (rp->get_model()));
+      Residue residue= Residue::create(res, Residue(rp));
+      double close_distance=10000;
+      int close_center=-1;
+      for (unsigned int i=0; i< centers.size(); ++i) {
+        algebra::Sphere3D s= get_sphere(l[j]);
+        double d= algebra::distance(s, centers[i]);
+        if (d < close_distance) {
+          close_distance=d;
+          close_center=i;
+        }
+      }
+      h.get_child(close_center).add_child(residue);
+    }
+  }
+  write_spheres(centers, 1000);
+
+  return h;
 }
 
 
---
+++ modules/atom/test/test_simplify.py
@@ -0,0 +1,17 @@
+import unittest
+import IMP
+import IMP.test
+import IMP.core
+import IMP.atom
+
+class DecoratorTests(IMP.test.TestCase):
+    def test_simplify(self):
+        """Test protein simplification"""
+        IMP.set_log_level(IMP.VERBOSE)
+        m= IMP.Model()
+        p= IMP.atom.read_pdb(self.get_input_file_name('single_protein.pdb'), m)
+        IMP.atom.simplify_protein(p, 30)
+
+
+if __name__ == '__main__':
+    unittest.main()
