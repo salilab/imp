2b6e59bcb193d7b27f5022e1694497d1388ae559
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-05-26 21:43:25 +0000

    it doesn't commit unless you add it first
    git-svn-id: https://svn.salilab.org/imp/trunk@2923 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ kernel/test/misc/test_key.py
@@ -0,0 +1,18 @@
+import unittest
+import IMP
+import IMP.test
+import os.path
+
+class DirectoriesTests(IMP.test.TestCase):
+    def test_data_directory(self):
+        """Test key aliases"""
+        k0= IMP.IntKey("0")
+        k1= IMP.IntKey("1")
+        ak1= IMP.IntKey.add_alias(k0, "0p")
+        k2= IMP.IntKey("2")
+        self.assert_(IMP.IntKey("0p") == k0)
+        self.assert_(IMP.IntKey("2") == k2)
+        self.assert_(k2 != ak1)
+
+if __name__ == '__main__':
+    unittest.main()
