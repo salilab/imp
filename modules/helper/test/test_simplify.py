56b50687b13942a6886e72e6939a18e60f1c00bc
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-06-05 22:54:54 +0000

    fix bug introduced by silly optimization
    git-svn-id: https://svn.salilab.org/imp/trunk@2949 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/helper/test/test_simplify.py
+++ modules/helper/test/test_simplify.py
@@ -3,6 +3,7 @@ import IMP
 import IMP.test
 import IMP.core
 import IMP.atom
+import IMP.helper
 
 class DecoratorTests(IMP.test.TestCase):
     def test_simplify(self):
@@ -10,7 +11,14 @@ class DecoratorTests(IMP.test.TestCase):
         IMP.set_log_level(IMP.VERBOSE)
         m= IMP.Model()
         p= IMP.atom.read_pdb(self.get_input_file_name('single_protein.pdb'), m)
-        IMP.helper.simplified(p, 30)
+        s= IMP.helper.simplified(p, 10)
+        ls= IMP.core.get_leaves(s)
+        for q in ls:
+            d= IMP.core.XYZR(q)
+            print ".sphere " +str(d.get_coordinates()[0]) + " "\
+                + str(d.get_coordinates()[1]) + " "\
+                + str(d.get_coordinates()[2]) + " "\
+                + str(d.get_radius())
 
 
 if __name__ == '__main__':
