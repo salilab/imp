8e1683d578a9aeda353869c2aadcaa3c9bc9e4e8
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-06-22 23:59:22 +0000

    Refcount returned pointers to IMP.em.DensityMap objects. Together with the previous commit, this fixes bug #69.
    git-svn-id: https://svn.salilab.org/imp/trunk@3069 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/em/test/test_refcount.py
@@ -0,0 +1,23 @@
+import unittest
+import IMP.em
+import IMP.test
+
+class RefCountTests(IMP.test.TestCase):
+    """Test refcounting of IMP.em classes"""
+
+    def test_read_density_map(self):
+        """Check refcounting of IMP.em.DensityMap objects read from files"""
+        refcnt = IMP.test.RefCountChecker(self)
+        erw = IMP.em.EMReaderWriter()
+        # ReaderWriters are not refcounted
+        refcnt.assert_number(0)
+        map = IMP.em.read_map(self.get_input_file_name("in.em"), erw)
+        refcnt.assert_number(1)
+        # read_map does not keep a C++ reference, so Python's should
+        # be the only one
+        self.assertEqual(map.get_ref_count(), 1)
+        del map
+        refcnt.assert_number(0)
+
+if __name__ == '__main__':
+    unittest.main()
