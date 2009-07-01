d433c47213accbe15bfa62aef0853ec0009f3d5a
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-07-01 22:50:03 +0000

    add polyline
    git-svn-id: https://svn.salilab.org/imp/trunk@3115 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/display/src/CylinderGeometry.cpp
+++ modules/display/src/CylinderGeometry.cpp
@@ -32,4 +32,29 @@ algebra::Vector3D CylinderGeometry::get_vertex(unsigned int i) const {
 Float CylinderGeometry::get_size() const {
   return c_.get_radius();
 }
+
+
+
+PolyCylinderGeometry::PolyCylinderGeometry(const algebra::Vector3Ds &c,
+                                           double r): c_(c), r_(r) {
+}
+
+void PolyCylinderGeometry::show(std::ostream &out) const {
+  out << "CylinderGeometry" << std::endl;
+}
+
+unsigned int PolyCylinderGeometry::get_dimension() const{
+  return 1;
+}
+unsigned int PolyCylinderGeometry::get_number_of_vertices() const{
+  return c_.size();
+}
+algebra::Vector3D PolyCylinderGeometry::get_vertex(unsigned int i) const {
+  return c_[i];
+}
+
+
+Float PolyCylinderGeometry::get_size() const {
+  return r_;
+}
 IMPDISPLAY_END_NAMESPACE
