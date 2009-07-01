c1520bb51d187e4e8bd7dc4cdbae0b7f933dc200
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-07-01 17:34:16 +0000

    add support to write out a pymol animation:
    git-svn-id: https://svn.salilab.org/imp/trunk@3112 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/display/include/CGOWriter.h
+++ modules/display/include/CGOWriter.h
@@ -18,6 +18,34 @@
 
 IMPDISPLAY_BEGIN_NAMESPACE
 
+
+
+//! Write a CGO file with an animation of the geometry
+/** This write behaves a bit differently than the others in that it
+    only writes one file during the lifetime of the object. Calls to
+    Write::set_file_name() simply cause it to move on to the next
+    frame rather than to actually open a new file.
+
+    The frames will be numbered from 1.
+ */
+class IMPDISPLAYEXPORT CGOAnimationWriter: public Writer
+{
+  std::string name_;
+  unsigned int count_;
+  bool initialized_;
+public:
+  //! write to a file using the name to  name the files
+  CGOAnimationWriter(std::string file_name=std::string(),
+            std::string pymol_name="geometry");
+
+  IMP_WRITER(CGOAnimationWriter, internal::version_info)
+
+  IMP_WRITER_ADD_GEOMETRY
+
+  void set_file_name(std::string fname);
+};
+
+
 //! Write a CGO file with the geometry
 /** The cgo file format is a simple format for displaying geometry in Pymol.
     The cgo writer supports points, spheres, cyliners, and segments.
@@ -25,8 +53,11 @@ IMPDISPLAY_BEGIN_NAMESPACE
  */
 class IMPDISPLAYEXPORT CGOWriter: public Writer
 {
+  friend class CGOAnimationWriter;
   std::string name_;
   unsigned int count_;
+
+  static void write_geometry(Geometry *g, std::ostream &out);
 public:
   //! write to a file using the name to  name the files
   CGOWriter(std::string file_name=std::string(),
--- modules/display/src/CGOWriter.cpp
+++ modules/display/src/CGOWriter.cpp
@@ -11,6 +11,57 @@
 
 IMPDISPLAY_BEGIN_NAMESPACE
 
+
+CGOAnimationWriter::CGOAnimationWriter(std::string file_name,
+                                       std::string name): Writer(file_name),
+                                                          name_(name),
+                                                          initialized_(false){
+  IMP_check(!file_name.empty(), "CGOAnimationWrite must be passed a "
+            << "file name on construction.", ValueException);
+  IMP_check(!name.empty(), "CGOAnimationWrite must be passed a "
+            << "object name on construction.", ValueException);
+  count_=1;
+}
+
+
+void CGOAnimationWriter::show(std::ostream &out) const {
+  out << "CGOAnimationWriter" << std::endl;
+}
+
+void CGOAnimationWriter::on_open() {
+  get_stream() << "from pymol.cgo import *\nfrom pymol import cmd\n";
+}
+
+void CGOAnimationWriter::on_close() {
+  get_stream() << "]\n\ncmd.load_cgo(model,'" << name_
+               << "', " << count_ << ")\n";
+}
+
+void CGOAnimationWriter::set_file_name(std::string) {
+  if (initialized_) {
+    on_close();
+    ++count_;
+  } else {
+    initialized_=true;
+  }
+  get_stream() << "model= [\n";
+}
+
+void CGOAnimationWriter::add_geometry(Geometry *g) {
+  IMP_CHECK_OBJECT(g);
+  CGOWriter::write_geometry(g, get_stream());
+}
+
+
+
+
+
+
+
+
+
+
+
 CGOWriter::CGOWriter(std::string file_name,
                      std::string name): Writer(file_name),
                                         name_(name){
@@ -34,19 +85,17 @@ void CGOWriter::on_close() {
   ++count_;
 }
 
-void CGOWriter::add_geometry(Geometry *g) {
-  IMP_CHECK_OBJECT(g);
-  Pointer<Geometry> gp(g);
-  if (gp->get_dimension() ==0) {
+void CGOWriter::write_geometry(Geometry *g, std::ostream &out) {
+  if (g->get_dimension() ==0) {
     for (unsigned int i=0; i< g->get_number_of_vertices(); ++i) {
       algebra::Vector3D v=g->get_vertex(i);
-      get_stream() << "COLOR, " << g->get_color().get_red()
+      out << "COLOR, " << g->get_color().get_red()
                << ", " << g->get_color().get_green()
                << ", " << g->get_color().get_blue()
                << ",\n";
       double r= .1;
       if (g->get_size() != 0) r= g->get_size();
-      get_stream() << "SPHERE, " << algebra::commas_io(v) << ", " << r
+      out << "SPHERE, " << algebra::commas_io(v) << ", " << r
                      << ",\n";
     }
   } else if (g->get_dimension() ==1) {
@@ -57,38 +106,43 @@ void CGOWriter::add_geometry(Geometry *g) {
       } else {
         r= .01*(g->get_vertex(i-1)- g->get_vertex(i)).get_magnitude();
       }
-      get_stream() << "CYLINDER,\n"
+      out << "CYLINDER,\n"
                    << algebra::commas_io(g->get_vertex(i-1)) << ",\n"
                    << algebra::commas_io(g->get_vertex(i)) << ",\n"
                    << r << ",\n";
-      get_stream() << g->get_color().get_red()
+      out << g->get_color().get_red()
                    << ", " << g->get_color().get_green()
                    << ", " << g->get_color().get_blue()
                    << ",\n";
-      get_stream() << g->get_color().get_red()
+      out << g->get_color().get_red()
                    << ", " << g->get_color().get_green()
                    << ", " << g->get_color().get_blue()
                    << ",\n";
     }
   } else if (g->get_dimension() ==2) {
     if (g->get_number_of_vertices() >2) {
-      get_stream() << "BEGIN, TRIANGLE_FAN, ";
+      out << "BEGIN, TRIANGLE_FAN, ";
       algebra::Vector3D n=
         vector_product(g->get_vertex(1)-g->get_vertex(0),
                        g->get_vertex(2)-g->get_vertex(0)).get_unit_vector();
-      get_stream() << "COLOR, " << g->get_color().get_red()
+      out << "COLOR, " << g->get_color().get_red()
                    << ", " << g->get_color().get_green()
                    << ", " << g->get_color().get_blue()
                    << ",\n";
-      get_stream() << "NORMAL, " << algebra::commas_io(n)
+      out << "NORMAL, " << algebra::commas_io(n)
                    << ",\n";
       for (unsigned int i=0; i< g->get_number_of_vertices(); ++i) {
-        get_stream() << "VERTEX, " << algebra::commas_io(g->get_vertex(i))
+        out << "VERTEX, " << algebra::commas_io(g->get_vertex(i))
                      << ", ";
       }
-      get_stream() << "END,\n";
+      out << "END,\n";
     }
   }
+}
+
+void CGOWriter::add_geometry(Geometry *g) {
+  IMP_CHECK_OBJECT(g);
+  write_geometry(g, get_stream());
 }
 
 
