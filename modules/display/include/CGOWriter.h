72f000316496cb91fb81cd55bbe49bea35308638
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-07-06 18:45:29 +0000

    no need for second animation class
    git-svn-id: https://svn.salilab.org/imp/trunk@3155 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/display/include/CGOWriter.h
+++ modules/display/include/CGOWriter.h
@@ -18,41 +18,6 @@
 
 IMPDISPLAY_BEGIN_NAMESPACE
 
-
-
-//! Write a CGO file with an animation of the geometry
-/** This write behaves a bit differently than the others in that it
-    only writes one file during the lifetime of the object. Calls to
-    Write::set_file_name() simply cause it to move on to the next
-    frame rather than to actually open a new file.
-
-    The frames will be numbered from 1.
-
-    \note This class currently counts as a bit of a hack and so
-    may not behave as expected always.
- */
-class IMPDISPLAYEXPORT CGOAnimationWriter: public Writer
-{
-  std::string name_;
-  unsigned int count_;
-  bool initialized_;
-public:
-  //! write to a file using the name to  name the files
-  /** The pymol_name parameter is the name used to identify it
-      within pymol. */
-  CGOAnimationWriter(std::string file_name);
-
-  IMP_WRITER(CGOAnimationWriter, internal::version_info)
-
-  IMP_WRITER_ADD_GEOMETRY
-
- //! Set the name used to identify the model in pymol
-  void set_name(std::string name) {
-    name_=name;
-  }
-};
-
-
 //! Write a CGO file with the geometry
 /** The cgo file format is a simple format for displaying geometry in Pymol.
     The cgo writer supports points, spheres, cyliners, and segments.
@@ -69,12 +34,12 @@ public:
   //! write to a file using the name to  name the files
   CGOWriter(std::string file_name=std::string());
 
-  IMP_WRITER(CGOWriter, internal::version_info)
-
-    //! Set the name used to identify the model in pymol
+  //! Set the name used to identify the model in pymol
   void set_name(std::string name) {
     name_=name;
   }
+
+  IMP_WRITER(CGOWriter, internal::version_info)
 
   IMP_WRITER_ADD_GEOMETRY
 };
