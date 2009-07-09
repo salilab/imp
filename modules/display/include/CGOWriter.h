46b9f0a3f3a4bda86f67b3d1ddf92d96ee7921f5
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-07-09 23:12:55 +0000

    improve docs and remove unused name
    git-svn-id: https://svn.salilab.org/imp/trunk@3189 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/display/include/CGOWriter.h
+++ modules/display/include/CGOWriter.h
@@ -21,23 +21,24 @@ IMPDISPLAY_BEGIN_NAMESPACE
 //! Write a CGO file with the geometry
 /** The cgo file format is a simple format for displaying geometry in Pymol.
     The cgo writer supports points, spheres, cyliners, and segments.
-    The file name should end in ".py".
+    The file name should end in ".pym".
+
+    The geometry is assembled into objects in pymol based on the passed
+    names. For example, all geometry named "box" becomes one pymol object.
+    If many files are loaded into python defining the same objects, they
+    become sequential frames in a movie. The frame numbers are determined
+    sequentially from the file load order (so they can form a subset of
+    the generated files).
  */
 class IMPDISPLAYEXPORT CGOWriter: public Writer
 {
   friend class CGOAnimationWriter;
-  std::string name_;
   unsigned int count_;
 
   static void write_geometry(Geometry *g, std::ostream &out);
 public:
   //! write to a file using the name to  name the files
   CGOWriter(std::string file_name=std::string());
-
-  //! Set the name used to identify the model in pymol
-  void set_name(std::string name) {
-    name_=name;
-  }
 
   IMP_WRITER(CGOWriter, internal::version_info)
 
