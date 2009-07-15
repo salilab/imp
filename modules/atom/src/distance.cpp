e58b5d75119087560f4053b1b7dc2cc5e9774825
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-07-15 19:23:08 +0000

    remove Hierarchy version
    git-svn-id: https://svn.salilab.org/imp/trunk@3243 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/src/distance.cpp
+++ modules/atom/src/distance.cpp
@@ -19,19 +19,5 @@ double rmsd(const core::XYZs& m1 ,const core::XYZs& m2) {
   }
   return std::sqrt(rmsd / m1.size());
 }
-double rmsd(const Hierarchy& m1 ,const Hierarchy& m2) {
-  IMP::Particles ps1,ps2;
-  ps1 = IMP::core::get_leaves(m1);
-  ps2 = IMP::core::get_leaves(m2);
-  IMP_check(ps1.size()==ps2.size(),
-            "proteins given as input to rmsd calculation "
-            <<"should be of the same size", ValueException);
-  float rmsd=0.0;
-  for(unsigned int i=0;i<ps1.size();i++) {
-    rmsd += IMP::algebra::squared_distance(core::XYZ(ps1[i]).get_coordinates(),
-                                        core::XYZ(ps2[i]).get_coordinates());
-  }
-  return std::sqrt(rmsd / ps1.size());
-}
 
 IMPATOM_END_NAMESPACE
