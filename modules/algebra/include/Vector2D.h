50e706cd1137a2984151a76c266ce9851745d5cd
javi@SALILAB.ORG <javi@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-07-16 20:22:10 +0000

    A vector in 2D
    git-svn-id: https://svn.salilab.org/imp/trunk@3259 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/include/Vector2D.h
+++ modules/algebra/include/Vector2D.h
@@ -18,18 +18,12 @@
 
 IMPALGEBRA_BEGIN_NAMESPACE
 
-//! A location in 2D
+//! A location in 2D (x,y)
 typedef VectorD<2> Vector2D;
 
 //! A collection of Vector2D
 typedef std::vector<Vector2D> Vector2Ds;
 
-//! Returns the dot product
-/** \relatesalso Vector2D
- */
-inline double dot_product(const Vector2D& p1, const Vector2D& p2) {
-  return (p1[0]*p2[0]+p1[1]*p2[1]);
-}
 
 IMPALGEBRA_END_NAMESPACE
 
