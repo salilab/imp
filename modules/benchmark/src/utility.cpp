0b8f78a45a2a9b4cb02d90f95c1e17d0421a340f
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-07-16 23:24:31 +0000

    remove tests where the time can't be reliably measured
    git-svn-id: https://svn.salilab.org/imp/trunk@3264 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/benchmark/src/utility.cpp
+++ modules/benchmark/src/utility.cpp
@@ -5,11 +5,16 @@
 
 #include <IMP/benchmark/utility.h>
 #include <iostream>
-
+#include <boost/format.hpp>
+#include <IMP/log.h>
 IMPBENCHMARK_BEGIN_NAMESPACE
 
 void report(std::string name, double value, double check) {
-  std::cout << name << ", " << value << ", " << check << std::endl;
+  if (value < 0) {
+    IMP_WARN("Negative value passed: " << value);
+    value=0;
+  }
+  std::cout << boost::format("%s,%f,%e\n")%name % value % check;
 }
 
 
