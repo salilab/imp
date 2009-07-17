f7b480077d3af66db6fea64401e92c55ac7eb077
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-07-17 19:14:44 +0000

    indent log strings
    git-svn-id: https://svn.salilab.org/imp/trunk@3270 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/include/internal/log_internal.h
+++ kernel/include/internal/log_internal.h
@@ -20,43 +20,6 @@
 IMP_BEGIN_INTERNAL_NAMESPACE
 
 
-class IMPEXPORT Log: boost::noncopyable
-{
-public:
-  static Log& get();
-
-  std::ostream &get_stream(unsigned int l) {
-    if (target_== 0) {
-      return std::cout;
-    } else if (target_== 1) {
-      return std::cerr;
-    } else {
-      return fstream_;
-    }
-  }
-
-  unsigned int get_target() {
-    return target_;
-  }
-  void set_target(unsigned int k) {
-    target_=k;
-  }
-  void set_filename(std::string k) {
-    fstream_.open(k.c_str());
-    if (!fstream_) {
-      std::cerr << "Error opening log file " << k << std::endl;
-    }
-  }
-
-private:
-
-  Log(unsigned int target)  : target_(target) {}
-  unsigned int target_;
-  std::ofstream fstream_;
-};
-
-
-
 IMP_END_INTERNAL_NAMESPACE
 
 #endif  /* IMP_INTERNAL_LOG_INTERNAL_H */
