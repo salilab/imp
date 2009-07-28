7b9ab5cbb05c212273bb39d993280f968611a101
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-07-28 23:45:19 +0000

    put example data in the data repository to help out of source builds
    git-svn-id: https://svn.salilab.org/imp/trunk@3312 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- examples/simple_examples/cover_particles.py
+++ examples/simple_examples/cover_particles.py
@@ -5,7 +5,8 @@ import IMP.atom
 import IMP.helper
 
 m= IMP.Model()
-prot= IMP.atom.read_pdb('examples/simple_examples/single_protein.pdb', m)
+prot= IMP.atom.read_pdb(IMP.get_data_directory()\
+                           +'examples/simple_examples/single_protein.pdb', m)
 res= IMP.atom.get_by_type(prot, IMP.atom.Hierarchy.RESIDUE)
 rc= IMP.core.ListSingletonContainer(res)
 for p in res:
