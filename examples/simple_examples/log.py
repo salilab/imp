35d0117c382055e3d270508ad1cef8dec729fe93
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-07-29 18:43:08 +0000

    add missing log example
    git-svn-id: https://svn.salilab.org/imp/trunk@3331 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ examples/simple_examples/log.py
@@ -0,0 +1,22 @@
+import IMP
+import IMP.display
+import IMP.core
+import IMP.core
+
+m= IMP.Model()
+c= IMP.core.ListSingletonContainer(ps)(IMP.core.create_xyzr_particles(m, 20, 5))
+
+log= IMP.display.LogOptimizerState(IMP.display.CGOWriter(), "log_file.%04d.pym")
+g= IMP.display.XYZRsGeometry(c)
+g.set_name("my particles")
+g.set_color(IMP.display.Color(1,0,0))
+log.add_geometry(g)
+log.write("initial.pym")
+
+r= IMP.core.ExcludedVolumeRestraint(c)
+m.add_restraint(r)
+
+o= IMP.core.ConjugateGradients()
+o.set_model(m)
+o.add_optimizer_state(log)
+o.optimize(10000)
