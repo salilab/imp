f2032ecc6eb2c18d1b7156f464e469fde39ca71b
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-08-05 19:42:24 +0000

    benchmark a semi-realistic simulation and raw evaluation
    git-svn-id: https://svn.salilab.org/imp/trunk@3429 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/benchmark/bin/benchmark_brownian.cpp
@@ -0,0 +1,51 @@
+/**
+ * Copyright 2007-9 Sali Lab. All rights reserved.
+ */
+#include <IMP.h>
+#include <IMP/core.h>
+#include <IMP/algebra.h>
+#include <IMP/atom.h>
+#include <boost/timer.hpp>
+#include <IMP/benchmark/utility.h>
+
+using namespace IMP;
+using namespace IMP::core;
+using namespace IMP::algebra;
+using namespace IMP::atom;
+
+
+void test(int n) {
+  set_log_level(SILENT);
+  IMP_NEW(Model, m, ());
+  Particles ps= create_xyzr_particles(m, n, .1);
+  IMP_NEW(ListSingletonContainer, lsc, (ps));
+  for (unsigned int i=0; i< ps.size(); ++i) {
+    Diffusion::create(ps[i], 1e-6);
+  }
+  IMP_NEW(ClosePairsScoreState, cpss, (lsc));
+  m->add_score_state(cpss);
+  cpss->set_slack(.1);
+
+  IMP_NEW(DistancePairScore, dps, (new HarmonicLowerBound(0, 1)));
+  IMP_NEW(PairsRestraint, pr, (dps, cpss->get_close_pairs_container()));
+  m->add_restraint(pr);
+  SimulationParameters sp= SimulationParameters::create(new Particle(m));
+  sp.set_maximum_time_step_in_femtoseconds(1e3);
+  IMP_NEW(BrownianDynamics, bd, (sp, lsc));
+  bd->set_model(m);
+  double runtime=0, total=0;
+  IMP_TIME(
+             {
+               total+=bd->simulate(1e6);
+             }, runtime);
+  std::ostringstream oss;
+  oss << "bd " << n;
+  IMP::benchmark::report(oss.str(), runtime, total);
+}
+
+
+int main(int argc, char **argv) {
+  test(100);
+  test(1000);
+  return 0;
+}
