1a334b54022e39fe466646efab13d59ef8ad9011
dina@SALILAB.ORG <dina@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-08-07 18:19:24 +0000

    better printing of atom without element + test
    git-svn-id: https://svn.salilab.org/imp/trunk@3479 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/test/test_element.py
+++ modules/atom/test/test_element.py
@@ -3,6 +3,8 @@ from StringIO import StringIO
 import IMP
 import IMP.test
 import IMP.atom
+import IMP.core
+import IMP.algebra
 
 class ElementTableTest(IMP.test.TestCase):
 
@@ -20,6 +22,23 @@ class ElementTableTest(IMP.test.TestCase):
             mass += a.get_mass()
         print mass
         self.assertInTolerance(mass, 13346.4, 1.0)
+
+    def test_element_write(self):
+        """Check writing to pdb unknown elements"""
+        m = IMP.Model()
+        rp = IMP.Particle(m)
+        ap = IMP.Particle(m)
+        cp = IMP.Particle(m);
+        chain = IMP.atom.Chain.create(cp, 'A')
+        residue = IMP.atom.Residue.create(rp)
+        atom = IMP.atom.Atom.create(ap, IMP.atom.AT_CA)
+        xyz = IMP.core.XYZ.create(ap)
+        hcd = IMP.atom.Hierarchy.cast(cp)
+        hrd = IMP.atom.Hierarchy.cast(rp)
+        had = IMP.atom.Hierarchy.cast(ap)
+        hcd.add_child(hrd)
+        hrd.add_child(had)
+        print atom.get_pdb_string()
 
 if __name__ == '__main__':
     unittest.main()
