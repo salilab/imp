1c953b657edf4e0ed16d9dbc4a52614e540dcced
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-08-07 23:06:28 +0000

    - Today, matrices appear to be double again. - Don't try to exactly compare two floating point values.
    git-svn-id: https://svn.salilab.org/imp/trunk@3481 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/em/test/test_project.py
+++ modules/em/test/test_project.py
@@ -26,7 +26,7 @@ def print_matrix2D(m,name="None"):
 
 
 def create_matrix(z,y,x):
-    m1 = IMP.algebra.Matrix3D_f(z,y,x)
+    m1 = IMP.algebra.Matrix3D(z,y,x)
     for i in range(m1.get_start(0),m1.get_finish(0)+1):
         for j in range(m1.get_start(1),m1.get_finish(1)+1):
             for k in range(m1.get_start(2),m1.get_finish(2)+1):
@@ -36,7 +36,7 @@ def create_matrix(z,y,x):
     return m1
 
 def create_cube(z,y,x):
-    m1 = IMP.algebra.Matrix3D_f(z,y,x)
+    m1 = IMP.algebra.Matrix3D(z,y,x)
     m1.centered_start()
     for i in range(-2,2+1):
         for j in range(-2,2+1):
@@ -66,7 +66,7 @@ class EMprojectTests(IMP.test.TestCase):
         m1 = create_matrix(dz,dy,dx)
         shift = IMP.algebra.Vector3D(0,0,0)
         tolerance = 1e-6
-        result = IMP.algebra.Matrix2D_f()
+        result = IMP.algebra.Matrix2D()
         angles = IMP.algebra.rotation_from_fixed_zyz(0,0,0) # Z
         IMP.em.project_given_rotation1(m1,result,dy,dx,
                                           angles,shift,tolerance)
@@ -82,7 +82,7 @@ class EMprojectTests(IMP.test.TestCase):
         m1 = create_matrix(dz,dy,dx)
         shift = IMP.algebra.Vector3D(0,0,0)
         tolerance = 1e-6
-        result = IMP.algebra.Matrix2D_f()
+        result = IMP.algebra.Matrix2D()
         angles = IMP.algebra.rotation_from_fixed_zyz(0,math.pi,0) # -z
         IMP.em.project_given_rotation1(m1,
                           result,dy,dx,angles,shift ,tolerance)
@@ -99,14 +99,14 @@ class EMprojectTests(IMP.test.TestCase):
         m1 = create_matrix(dz,dy,dx)
         shift = IMP.algebra.Vector3D(0,0,0)
         tolerance = 1e-6
-        result = IMP.algebra.Matrix2D_f()
+        result = IMP.algebra.Matrix2D()
         angles = IMP.algebra.rotation_from_fixed_zyz(0,-math.pi/2,0) # x
         IMP.em.project_given_rotation1(m1,result,dy,dz,
                                           angles,shift ,tolerance)
 #        print_matrix2D(result,"test_project_X")
         for i in range(0,dy):
             for j in range(0,dz):
-                self.assertEqual(result[i,j],9*(4+i+dz-1-j))
+                self.assertInTolerance(result[i,j],9*(4+i+dz-1-j), 1e-4)
 
 
     def test_project_X_opposite(self):
@@ -116,14 +116,14 @@ class EMprojectTests(IMP.test.TestCase):
         m1 = create_matrix(dz,dy,dx)
         shift = IMP.algebra.Vector3D(0,0,0)
         tolerance = 1e-6
-        result = IMP.algebra.Matrix2D_f()
+        result = IMP.algebra.Matrix2D()
         angles = IMP.algebra.rotation_from_fixed_zyz(0,math.pi/2,0) # -x
         IMP.em.project_given_rotation1(m1,result,dy,dz,
                                           angles,shift ,tolerance)
 #        print_matrix2D(result,"test_project_-X")
         for i in range(0,dy):
             for j in range(0,dz):
-                self.assertEqual(result[i,dz-1-j],9*(4+i+dz-1-j))
+                self.assertInTolerance(result[i,dz-1-j],9*(4+i+dz-1-j), 1e-4)
 
 
     def test_project_Y(self):
@@ -133,7 +133,7 @@ class EMprojectTests(IMP.test.TestCase):
         m1 = create_matrix(dz,dy,dx)
         shift = IMP.algebra.Vector3D(0,0,0)
         tolerance = 1e-6
-        result = IMP.algebra.Matrix2D_f()
+        result = IMP.algebra.Matrix2D()
         angles = IMP.algebra.rotation_from_fixed_zyz(math.pi/2,math.pi/2,0) # y
         IMP.em.project_given_rotation1(m1,result,dx,dz,
                                           angles,shift ,tolerance)
@@ -150,7 +150,7 @@ class EMprojectTests(IMP.test.TestCase):
         m1 = create_matrix(dz,dy,dx)
         shift = IMP.algebra.Vector3D(0,0,0)
         tolerance = 1e-6
-        result = IMP.algebra.Matrix2D_f()
+        result = IMP.algebra.Matrix2D()
         angles = IMP.algebra.rotation_from_fixed_zyz(math.pi/2,-math.pi/2,0) # -y
         IMP.em.project_given_rotation1(m1,result,dx,dz,
                                           angles,shift ,tolerance)
@@ -165,13 +165,13 @@ class EMprojectTests(IMP.test.TestCase):
         m1 = create_cube(7,7,9)
         shift = IMP.algebra.Vector3D(0,0,0)
         tolerance = 1e-6
-        result = IMP.algebra.Matrix2D_f()
+        result = IMP.algebra.Matrix2D()
         ydim=11; xdim=11
         direction = IMP.algebra.Vector3D(1,1,1)
         IMP.em.project_given_direction1(m1,result,ydim,xdim,
                                        direction,shift ,tolerance)
 #        print_matrix2D(result,"project_direction")
-        opp_result = IMP.algebra.Matrix2D_f()
+        opp_result = IMP.algebra.Matrix2D()
         direction = IMP.algebra.Vector3D(-1,-1,-1)
         IMP.em.project_given_direction1(m1,opp_result,ydim,xdim,
                                        direction,shift ,tolerance)
