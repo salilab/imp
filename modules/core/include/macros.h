b871553908a293b3dcc5088ca5a98ee4ef647752
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-08-09 17:46:05 +0000

    add mover macro
    git-svn-id: https://svn.salilab.org/imp/trunk@3503 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/include/macros.h
+++ modules/core/include/macros.h
@@ -9,4 +9,16 @@
 #ifndef IMPCORE_MACROS_H
 #define IMPCORE_MACROS_H
 
+/** Helper macro for implementing IMP::core::Mover. In
+    addition to the IMP_OBJECT methods, it declares
+    - Mover::propose_move()
+    - Mover::accept_move()
+    - Mover::reject_move()
+ */
+#define IMP_MOVER(Name, version_info)                   \
+  virtual void propose_move(Float size);                \
+  virtual void accept_move();                           \
+  virtual void reject_move();                           \
+  IMP_OBJECT(Name, version_info)
+
 #endif  /* IMPCORE_MACROS_H */
