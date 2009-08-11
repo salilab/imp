b2eb8afd51c15d98486833e0a8ffbd21fb2dd7c4
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-08-11 21:44:01 +0000

    expose prefix stream internall to use it in decorators and other places
    git-svn-id: https://svn.salilab.org/imp/trunk@3550 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ kernel/include/internal/PrefixStream.h
@@ -0,0 +1,73 @@
+/**
+ *  \file PrefixStream.h
+ *  \brief A stream which prefixes each line with a string.
+ *
+ *  Copyright 2007-9 Sali Lab. All rights reserved.
+ *
+ */
+
+#ifndef IMP_INTERNAL_PREFIX_STREAM_H
+#define IMP_INTERNAL_PREFIX_STREAM_H
+
+#include "../config.h"
+
+#include <boost/iostreams/categories.hpp>
+#include <boost/iostreams/operations.hpp>
+#include <boost/iostreams/filtering_stream.hpp>
+
+IMP_BEGIN_INTERNAL_NAMESPACE
+
+class PrefixStream:
+  public boost::iostreams::filtering_stream<boost::iostreams::output>,
+  public boost::noncopyable  {
+  typedef boost::iostreams::filtering_stream<boost::iostreams::output> P;
+  std::ostream *out_;
+  std::string prefix_;
+
+  struct IndentFilter: public boost::iostreams::output_filter {
+    PrefixStream *ps_;
+    bool to_indent_;
+    IndentFilter(PrefixStream *ps): ps_(ps), to_indent_(false){};
+    template <typename Sink>
+    bool put(Sink &sink, char c) {
+      if (c=='\n') {
+        to_indent_=true;
+      } else if (to_indent_) {
+        for (unsigned int i=0; i< ps_->prefix_.size(); ++i) {
+          boost::iostreams::put(sink, ps_->prefix_[i]);
+        }
+        to_indent_=false;
+      }
+      return boost::iostreams::put(sink, c);
+    }
+  };
+
+  struct LogSink: boost::iostreams::sink {
+    PrefixStream *ps_;
+    LogSink(PrefixStream *ps): ps_(ps){}
+    unsigned int write(const char *s, std::streamsize n) {
+      ps_->out_->write(s, n);
+      return n;
+    }
+  };
+  friend class LogSink;
+  friend class IndentFilter;
+ public:
+  PrefixStream(std::ostream *out): out_(out) {
+    P::push(IndentFilter(this));
+    P::push(LogSink(this));
+  }
+  std::string get_prefix() const {
+    return prefix_;
+  }
+  void set_prefix(std::string prefix) {
+    prefix_=prefix;
+  }
+  void set_stream(std::ostream *out) {
+    out_=out;
+  }
+};
+
+IMP_END_INTERNAL_NAMESPACE
+
+#endif  /* IMP_INTERNAL_PREFIX_STREAM_H */
---
+++ kernel/src/internal/PrefixStream.cpp
@@ -0,0 +1,13 @@
+/**
+ *  \file constants.cpp  \brief Various useful constants.
+ *
+ *  Copyright 2007-9 Sali Lab. All rights reserved.
+ *
+ */
+
+#include "IMP/internal/PrefixStream.h"
+
+IMP_BEGIN_INTERNAL_NAMESPACE
+
+
+IMP_END_INTERNAL_NAMESPACE
