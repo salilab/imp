850a962feeedcf43975f02a4a014d9f9dc522023
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-08-11 21:44:01 +0000

    expose prefix stream internall to use it in decorators and other places
    git-svn-id: https://svn.salilab.org/imp/trunk@3550 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/src/log.cpp
+++ kernel/src/log.cpp
@@ -8,9 +8,7 @@
 #include "IMP/log.h"
 #include "IMP/internal/log_internal.h"
 #include "IMP/exception.h"
-#include <boost/iostreams/categories.hpp>
-#include <boost/iostreams/operations.hpp>
-#include <boost/iostreams/filtering_stream.hpp>
+#include "IMP/internal/PrefixStream.h"
 
 IMP_BEGIN_INTERNAL_NAMESPACE
 
@@ -18,79 +16,46 @@ LogLevel log_level= TERSE;
 LogTarget log_target= COUT;
 unsigned int log_indent=0;
 
-namespace {
-  double initialized=11111111;
-  std::ofstream fstream;
-  boost::iostreams::filtering_stream<boost::iostreams::output> filtering;
-
-  struct IndentFilter: public boost::iostreams::output_filter {
-    bool to_indent;
-    IndentFilter(): to_indent(false){};
-    template <typename Sink>
-    bool put(Sink &sink, char c) {
-      if (c=='\n') {
-        to_indent=true;
-      } else if (to_indent) {
-        for (unsigned int i=0; i< log_indent; ++i) {
-          boost::iostreams::put(sink, ' ');
-        }
-        to_indent=false;
-      }
-      return boost::iostreams::put(sink, c);
-    }
-  };
-
-  struct LogSink: boost::iostreams::sink {
-    unsigned int write(const char *s, std::streamsize n) {
-      if (log_target== COUT) {
-        std::cout.write(s, n);
-      } else if (log_target== CERR) {
-        std::cerr.write(s, n);
-      } else {
-        fstream.write(s, n);
-      }
-      return n;
-    }
-  };
-
-  struct SetupStream {
-    SetupStream() {
-      filtering.push(IndentFilter());
-      filtering.push(LogSink());
-    }
-  } stream_setup_helper;
-}
-
-
 IMP_END_INTERNAL_NAMESPACE
 
 IMP_BEGIN_NAMESPACE
 
+namespace {
+  double initialized=11111111;
+  std::ofstream fstream;
+  internal::PrefixStream stream(&std::cout);
+}
 
 void set_log_target(LogTarget l)
 {
-  internal::log_target=l;
+  if (l== COUT) {
+    stream.set_stream(&std::cout);
+  } else if (l==CERR) {
+    stream.set_stream(&std::cerr);
+  } else {
+    stream.set_stream(&fstream);
+  }
 }
 
 void set_log_file(std::string l) {
   if (!l.empty()) {
-    internal::fstream.open(l.c_str());
-    if (!internal::fstream.is_open()) {
+    fstream.open(l.c_str());
+    if (!fstream.is_open()) {
       IMP_failure("Error opening log file " << l, ValueException);
     } else {
       internal::log_target=FILE;
     }
   } else {
-    internal::fstream.close();
+    fstream.close();
     internal::log_target=COUT;
   }
 }
 
 void log_write(std::string str) {
-  IMP_assert(internal::initialized=11111111,
+  IMP_assert(initialized=11111111,
              "You connot use the log before main is called.");
-  internal::filtering.write(str.c_str(), str.size());
-  internal::filtering.strict_sync();
+  stream.write(str.c_str(), str.size());
+  stream.strict_sync();
 }
 
 IMP_END_NAMESPACE
