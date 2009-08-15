284a34663022856abd766770da2a78031f111fc5
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-08-15 22:13:59 +0000

    Update name
    git-svn-id: https://svn.salilab.org/imp/trunk@3618 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/examples/cover_particles.py
+++ modules/core/examples/cover_particles.py
@@ -7,7 +7,7 @@ import IMP.helper
 m= IMP.Model()
 prot= IMP.atom.read_pdb(IMP.get_data_directory()\
                          +'/atom/example_protein.pdb', m)
-res= IMP.atom.get_by_type(prot, IMP.atom.Hierarchy.RESIDUE)
+res= IMP.atom.get_by_type(prot, IMP.atom.Hierarchy.AMINOACID)
 rc= IMP.core.ListSingletonContainer(res)
 for p in res:
     IMP.core.XYZR.setup_particle(p.get_particle())
--- modules/core/examples/rigid_bodies.py
+++ modules/core/examples/rigid_bodies.py
@@ -7,7 +7,7 @@ m= IMP.Model()
 
 mp0= IMP.atom.read_pdb(IMP.get_data_directory()\
                            +'/atom/example_protein.pdb', m)
-residues= IMP.atom.get_by_type(mp0, IMP.atom.Hierarchy.RESIDUE)
+residues= IMP.atom.get_by_type(mp0, IMP.atom.Hierarchy.AMINOACID)
 rbs=IMP.core.ListSingletonContainer(residues)
 IMP.helper.create_rigid_bodies(rbs, IMP.core.LeavesRefiner(IMP.atom.Hierarchy.get_traits()))
 
