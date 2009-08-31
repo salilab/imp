b8b398d461005bcef45cae855dc10feda77fa6fe
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-08-31 21:44:12 +0000

    add score to keep things in a box
    git-svn-id: https://svn.salilab.org/imp/trunk@3696 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/core/src/BoundingBox3DSingletonScore.cpp
@@ -0,0 +1,59 @@
+/**
+ *  \file BoundingBox3DSingletonScore.cpp
+ *  \brief XXXX.
+ *
+ *  Copyright 2007-8 Sali Lab. All rights reserved.
+ *
+ */
+
+#include "IMP/core/BoundingBox3DSingletonScore.h"
+#include "IMP/core/XYZ.h"
+#include <IMP/core/internal/evaluate_distance_pair_score.h>
+
+#include <boost/lambda/lambda.hpp>
+
+IMPCORE_BEGIN_NAMESPACE
+
+BoundingBox3DSingletonScore
+::BoundingBox3DSingletonScore(UnaryFunction *f,
+                            const algebra::BoundingBox3D &bb ): f_(f), bb_(bb){
+  IMP_check(std::abs(f_->evaluate(0)) <.1, "The unary function should return "
+            " 0 when passed a value of 0. Not " << f_->evaluate(0),
+            InvalidStateException);
+}
+
+double BoundingBox3DSingletonScore::evaluate(Particle *p,
+                                           DerivativeAccumulator *da) const {
+  core::XYZ d(p);
+  algebra::Vector3D cp;
+  bool outside=false;
+  for (unsigned int i=0; i< 3; ++i) {
+    if (bb_.get_corner(0)[i] > d.get_coordinate(i)) {
+      cp[i]=bb_.get_corner(0)[i];
+      outside=true;
+    } else if (bb_.get_corner(1)[i] < d.get_coordinate(i)) {
+      cp[i]=bb_.get_corner(1)[i];
+      outside=true;
+    } else {
+      cp[i]= d.get_coordinate(i);
+    }
+  }
+  if (outside) {
+    algebra::Vector3D deriv;
+    double v= internal::compute_distance_pair_score(d.get_coordinates()-cp,
+                                                    f_,&deriv,
+                                                    boost::lambda::_1);
+    if (da) {
+      d.add_to_derivatives(deriv, *da);
+    }
+    return v;
+  } else {
+    return 0;
+  }
+}
+
+void BoundingBox3DSingletonScore::show(std::ostream &out) const {
+  out << "BoundingBox3DSingletonScore" << std::endl;
+}
+
+IMPCORE_END_NAMESPACE
---
+++ modules/core/test/scores/test_box.py
@@ -0,0 +1,32 @@
+import unittest
+import IMP
+import IMP.test
+import IMP.algebra
+import IMP.core
+
+class DistanceTests(IMP.test.TestCase):
+    """Test the symmetry restraint"""
+    def test_symmetry(self):
+        """Test the box score"""
+        m= IMP.Model()
+        p= IMP.Particle(m)
+        d= IMP.core.XYZ.setup_particle(p)
+        bbi= IMP.algebra.BoundingBox3D(IMP.algebra.Vector3D(10,10,10),
+                                       IMP.algebra.Vector3D(20,20,20))
+        bbo= IMP.algebra.BoundingBox3D(IMP.algebra.Vector3D(0,0,0),
+                                       IMP.algebra.Vector3D(30,30,30))
+
+        d.set_coordinates(IMP.algebra.random_vector_in_box(bbo))
+        d.get_coordinates().show()
+        d.set_coordinates_are_optimized(True)
+        s= IMP.core.BoundingBox3DSingletonScore(IMP.core.Harmonic(0,1), bbi)
+        m.add_restraint(IMP.core.SingletonRestraint(s, p))
+        o= IMP.core.ConjugateGradients()
+        o.set_model(m)
+        o.optimize(100)
+        for i in range(0,3):
+            self.assert_(d.get_coordinate(i) >9.9)
+            self.assert_(d.get_coordinate(i) <20.1)
+        d.get_coordinates().show()
+if __name__ == '__main__':
+    unittest.main()
