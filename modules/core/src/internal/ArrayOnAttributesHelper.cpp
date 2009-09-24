7054548a3a4465b628536b01edd55999f33e0975
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-09-24 23:06:07 +0000

    don't ref count traits data since it is small and of constant number
    git-svn-id: https://svn.salilab.org/imp/trunk@3786 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/core/src/internal/ArrayOnAttributesHelper.cpp
@@ -0,0 +1,13 @@
+/**
+ *  \file ParticleGrid.cpp
+ *  \brief A grid for keeping track of particles.
+ *
+ *  Copyright 2007-9 Sali Lab. All rights reserved.
+ */
+
+#include <IMP/core/internal/ArrayOnAttributesHelper.h>
+
+IMPCORE_BEGIN_INTERNAL_NAMESPACE
+VectorOfRefCounted<ArrayDataBase*> array_datas;
+
+IMPCORE_END_INTERNAL_NAMESPACE
