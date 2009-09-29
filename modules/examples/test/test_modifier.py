88d84443a30bd3ed2f4e7202512984f5945e2931
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-09-29 20:21:16 +0000

    add test
    git-svn-id: https://svn.salilab.org/imp/trunk@3807 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/examples/test/test_modifier.py
@@ -0,0 +1,32 @@
+import unittest
+import IMP
+import IMP.test
+import IMP.algebra
+import IMP.core
+import IMP.examples
+
+class DistanceTests(IMP.test.TestCase):
+    """Test the symmetry restraint"""
+    def test_symmetry(self):
+        """Test example modifier"""
+        IMP.set_log_level(IMP.MEMORY)
+        m= IMP.Model()
+        print "creating particle"
+        p= IMP.Particle(m)
+        print "created"
+        bbb= IMP.algebra.BoundingBox3D(IMP.algebra.Vector3D(0,0,0),
+                                       IMP.algebra.Vector3D(100,100,100))
+        sbb= IMP.algebra.BoundingBox3D(IMP.algebra.Vector3D(20,30,40),
+                                       IMP.algebra.Vector3D(30,39,48))
+        print "setting up"
+        d= IMP.core.XYZ.setup_particle(p, IMP.algebra.random_vector_in_box(bbb))
+        print "creating m"
+        mod= IMP.examples.ExampleSingletonModifier(sbb)
+        print "applying"
+        mod.apply(d.get_particle())
+        for i in range(0,3):
+            self.assert_(d.get_coordinate(i) >= sbb.get_corner(0)[i])
+            self.assert_(d.get_coordinate(i) <= sbb.get_corner(1)[i])
+
+if __name__ == '__main__':
+    unittest.main()
