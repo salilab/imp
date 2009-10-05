544a8ed5bf3a4c1efc3aa272121086dc18253b5e
javi@SALILAB.ORG <javi@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-10-05 20:04:15 +0000

    noise randomness improved
    git-svn-id: https://svn.salilab.org/imp/trunk@3841 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/em/include/noise.h
+++ modules/em/include/noise.h
@@ -41,49 +41,84 @@ IMPEM_BEGIN_NAMESPACE
   * \endcode
   * \note Tested with MultiArray, Matrix2D and Matrix3D
   */
+//template<typename T>
+//void add_noise(T& data, double op1, double op2,
+//               const String& mode = "uniform", double df = 3)
+//{
+//  if (mode == "uniform") {
+//    // Distribution
+//    boost::uniform_real<> unif_dist(op1, op2);
+//    // Put generator and distribution together
+//    typedef boost::variate_generator < RandomNumberGenerator,
+//                      boost::uniform_real<> > rnd_unif_generator;
+//    rnd_unif_generator rnd_unif(random_number_generator, unif_dist);
+//
+//    Int D = data.dimensionality;
+//    // add noise
+//    std::vector<Int> idx(D); // access index
+//    while (IMP::algebra::internal::roll_inds(idx,
+//                                             data.shape(),
+//                                             data.index_bases())) {
+//      data(idx) += rnd_unif();
+//    }
+//  } else if (mode == "gaussian") {
+//    // Distribution
+//    boost::normal_distribution<> gauss_dist(op1, op2);
+//    // Put generator and distribution together
+//    typedef boost::variate_generator < RandomNumberGenerator,
+//    boost::normal_distribution<> > rnd_gauss_generator;
+//    rnd_gauss_generator rnd_gauss(random_number_generator, gauss_dist);
+//
+//    Int D = data.dimensionality;
+//    // add noise
+//    std::vector<Int> idx(D); // access index
+//    while (IMP::algebra::internal::roll_inds(idx,
+//                                             data.shape(),
+//                                             data.index_bases())) {
+//      data(idx) += rnd_gauss();
+//    }
+//  } else if (mode == "student") {
+//  } else {
+//    String msg = "MultiArray::add_noise: Mode " + mode + " not supported.";
+//    throw ErrorException(msg.c_str());
+//  }
+//}
+
 template<typename T>
-void add_noise(T& data, double op1, double op2,
+void add_noise(T& v, double op1, double op2,
                const String& mode = "uniform", double df = 3)
 {
+  // Generator
+//  typedef boost::rand48 base_generator_type;
+  typedef boost::mt19937 base_generator_type;
+  base_generator_type generator;
+  generator.seed(static_cast<unsigned long>(std::time(NULL)));
+  // Distribution types
+  typedef boost::uniform_real<> unif_distribution;
+  typedef boost::normal_distribution<> norm_distribution;
+  // Variate generators (put generator and distributions together)
+  typedef boost::variate_generator < base_generator_type&,
+                         unif_distribution  > unif_var_generator;
+  typedef boost::variate_generator < base_generator_type&,
+                         norm_distribution  > norm_var_generator;
   if (mode == "uniform") {
-    // Distribution
-    boost::uniform_real<> unif_dist(op1, op2);
-    // Put generator and distribution together
-    typedef boost::variate_generator < RandomNumberGenerator,
-    boost::uniform_real<> > rnd_unif_generator;
-    rnd_unif_generator rnd_unif(random_number_generator, unif_dist);
-
-    Int D = data.dimensionality;
-    // add noise
-    std::vector<Int> idx(D); // access index
-    while (IMP::algebra::internal::roll_inds(idx,
-                                             data.shape(),
-                                             data.index_bases())) {
-      data(idx) += rnd_unif();
-    }
+    unif_distribution dist(op1, op2);
+    unif_var_generator random_var(generator,dist);
+    // Add the random numbers
+    for (unsigned long i=0;i<v.num_elements();++i) {v.data()[i]+=random_var();}
   } else if (mode == "gaussian") {
-    // Distribution
-    boost::normal_distribution<> gauss_dist(op1, op2);
-    // Put generator and distribution together
-    typedef boost::variate_generator < RandomNumberGenerator,
-    boost::normal_distribution<> > rnd_gauss_generator;
-    rnd_gauss_generator rnd_gauss(random_number_generator, gauss_dist);
-
-    Int D = data.dimensionality;
-    // add noise
-    std::vector<Int> idx(D); // access index
-    while (IMP::algebra::internal::roll_inds(idx,
-                                             data.shape(),
-                                             data.index_bases())) {
-      data(idx) += rnd_gauss();
-    }
+    norm_distribution dist(op1, op2);
+    norm_var_generator random_var(generator, dist);
+    // Add the random numbers
+    for (unsigned long i=0;i<v.num_elements();++i) {v.data()[i]+=random_var();}
   } else if (mode == "student") {
   } else {
     String msg = "MultiArray::add_noise: Mode " + mode + " not supported.";
     throw ErrorException(msg.c_str());
   }
-}
 
+
+}
 
 IMPEM_END_NAMESPACE
 #endif  /* IMPEM_NOISE_H */
