3acecaed789073e78d8af9b6495be44186e2f1c5
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-10-05 22:53:58 +0000

    add Transformation decorator
    git-svn-id: https://svn.salilab.org/imp/trunk@3849 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/domino/src/Transformation.cpp
@@ -0,0 +1,51 @@
+/**
+ *  \file XYZ.cpp   \brief Transformation decorator.
+ *
+ *  Copyright 2007-9 Sali Lab. All rights reserved.
+ *
+ */
+
+#include <IMP/domino/Transformation.h>
+#include <boost/assign/list_of.hpp>
+#include <IMP/core/XYZ.h>
+
+IMPDOMINO_BEGIN_NAMESPACE
+
+void Transformation::show(std::ostream &out) const
+{
+  algebra::VectorD<7> v;
+  Particle *p=get_particle();
+  v[0]=p->get_value(get_rotation_key(0));
+  v[1]=p->get_value(get_rotation_key(1));
+  v[2]=p->get_value(get_rotation_key(2));
+  v[3]=p->get_value(get_rotation_key(3));
+  v[4]=p->get_value(get_translation_key(0));
+  v[5]=p->get_value(get_translation_key(1));
+  v[6]=p->get_value(get_translation_key(2));
+  out << "(" <<algebra::commas_io(v)<<")";
+}
+
+const FloatKeys& Transformation::get_rotation_keys() {
+  static FloatKeys rks=boost::assign::list_of(FloatKey("a"))
+    (FloatKey("b"))
+    (FloatKey("c"))
+    (FloatKey("d"));
+  return rks;
+}
+const FloatKeys& Transformation::get_translation_keys() {
+  return IMP::core::XYZ::get_xyz_keys();
+}
+//! get a Transformation3D object from the decorator
+algebra::Transformation3D Transformation::get_transformation() {
+  Particle *p=get_particle();
+  return algebra::Transformation3D(
+       algebra::Rotation3D(p->get_value(get_rotation_key(0)),
+                           p->get_value(get_rotation_key(1)),
+                           p->get_value(get_rotation_key(2)),
+                           p->get_value(get_rotation_key(3))),
+       algebra::Vector3D(p->get_value(get_translation_key(0)),
+                         p->get_value(get_translation_key(1)),
+                         p->get_value(get_translation_key(2))));
+}
+
+IMPDOMINO_END_NAMESPACE
