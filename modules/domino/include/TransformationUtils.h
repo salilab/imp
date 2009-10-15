55e2e83ffa357603f4f22e64a702508eb7ebc676
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-10-15 20:02:10 +0000

    add test cases for TransformationUtils and use Transformation decorator in TransformationUtils
    git-svn-id: https://svn.salilab.org/imp/trunk@3919 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/domino/include/TransformationUtils.h
+++ modules/domino/include/TransformationUtils.h
@@ -1,23 +1,25 @@
 /**
  *  \file TransformationUtils.h
- *  \brief Handeling tranformation of hierarchical particles
+ *  \brief Handeling tranformation of rigid bodies
  *  Copyright 2007-9 Sali Lab. All rights reserved.
  */
 
 #ifndef IMPDOMINO_TRANSFORMATION_UTILS_H
 #define IMPDOMINO_TRANSFORMATION_UTILS_H
-#include "IMP/domino/TransformationMappedDiscreteSet.h"
 #include <IMP/algebra/Transformation3D.h>
+#include <IMP/domino/Transformation.h>
 #include "IMP/Particle.h"
 #include "IMP/base_types.h"
 #include "IMP/domino/CombState.h"
+#include <IMP/core/rigid_bodies.h>
+
 IMPDOMINO_BEGIN_NAMESPACE
-class TransformationUtils {
+class IMPDOMINOEXPORT TransformationUtils {
 public:
   TransformationUtils(){}
   //! Constructor
   /**
-  /param[in] ps particles to apply transformation on.
+  /param[in] ps particles to apply transformation on, each should be RigidBody
   /param[in] trans_from_orig if true then a tranformations are applied from
                                the original position/orientation rather than
                                the current one.
@@ -25,19 +27,17 @@ public:
   TransformationUtils(Particles *ps,bool trans_from_orig = false);
   //! Transform a particle
   /**
-  /param[in] p_sample The particle to apply the trasnformation all. The
-            transformation is applied on all of the leaves of the particle.
-  /param[in] t_trans a particle that holds the transformation (has xyz and abcd
-             attributes)
+  /param[in] p_sample The particle to apply the trasnformation on,
+                      should be core::RigidBody
+  /param[in] t_trans a particle that holds the transformation,
+                     should be domino::Transformation
    */
   void move2state(Particle *p_sample, Particle *p_trans);
 protected:
   //! Apply a transformation on all of the leaves of a particle
-  void apply(Particle *p , const algebra::Transformation3D &t);
-  algebra::Transformation3D get_trans(Particle *p) const;
+  void apply(core::RigidBody *p , const algebra::Transformation3D &t);
   bool go_back_;
-  std::map<Particle *,algebra::Transformation3D> last_transform_;
-  std::vector<FloatKey> atts_;
+  std::map<Particle *,algebra::Transformation3D> starting_transform_;
 };
 IMPDOMINO_END_NAMESPACE
 #endif  /* IMPDOMINO_TRANSFORMATION_UTILS_H */
--- modules/domino/src/TransformationUtils.cpp
+++ modules/domino/src/TransformationUtils.cpp
@@ -7,59 +7,48 @@
  */
 #include "IMP/domino/TransformationUtils.h"
 #include "IMP/core/Transform.h"
-#include "IMP/atom/Hierarchy.h"
-#include "IMP/core/Hierarchy.h"
 #include <IMP/core/XYZ.h>
+
 IMPDOMINO_BEGIN_NAMESPACE
 TransformationUtils::TransformationUtils(Particles *ps, bool trans_from_orig)
+  :go_back_(trans_from_orig)
 {
-  //translation attributes
-  atts_.push_back(FloatKey("x"));
-  atts_.push_back(FloatKey("y"));
-  atts_.push_back(FloatKey("z"));
-  //rotation attributes
-  atts_.push_back(FloatKey("a"));
-  atts_.push_back(FloatKey("b"));
-  atts_.push_back(FloatKey("c"));
-  atts_.push_back(FloatKey("d"));
-  go_back_=trans_from_orig;
-  if (go_back_) {
+  if (go_back_) { //save all initial transformations
     for (Particles::iterator it = ps->begin(); it != ps->end(); it++) {
-      last_transform_[*it]=algebra::identity_transformation();
+      starting_transform_[*it]=core::RigidBody(*it).get_transformation();
     }
   }
 }
 
-void TransformationUtils::move2state(Particle *p_sample, Particle *p_trans) {
+void TransformationUtils::move2state(Particle *p_sample,
+                                     Particle *p_trans) {
+  core::RigidBody rb(p_sample);
+  Transformation t(p_trans);
   if (go_back_) {
-    IMP_INTERNAL_CHECK(last_transform_.find(p_sample) != last_transform_.end(),
-               " TransformationUtils::move2state the particle is not found");
-    apply(p_sample,last_transform_[p_sample]);
-    last_transform_[p_sample]=get_trans(p_trans).get_inverse();
+    std::map<Particle *,algebra::Transformation3D>::const_iterator fi=
+      starting_transform_.find(p_sample);
+    apply(&rb,IMP::algebra::compose(
+      (Transformation(p_trans)).get_transformation(),
+      starting_transform_[p_sample]));
   }
-  algebra::Transformation3D t = get_trans(p_trans);
-  apply(p_sample,t);
+  else{
+    apply(&rb,(Transformation(p_trans)).get_transformation());
+  }
 }
 
-void TransformationUtils::apply(Particle *p,const algebra::Transformation3D &t)
+void TransformationUtils::apply(core::RigidBody *rb,
+                                const algebra::Transformation3D &t)
 {
-  IMP_NEW(core::Transform,  tsm, (t));
-  Particles ps = core::get_leaves(
-                 atom::Hierarchy::decorate_particle(p));
-//   core::GravityCenterScoreState g(p, FloatKey(),ps);
-//   g.update_position();
-  tsm->apply(ps);
+  IMP_IF_LOG(VERBOSE) {
+    IMP_LOG(VERBOSE,"TransformationUtils::apply name:"<<
+            rb->get_particle()->get_name()<< " tranformation:");
+    IMP_LOG_WRITE(VERBOSE,t.show());
+    IMP_LOG(VERBOSE,std::endl);
+  }
+  rb->set_transformation(t);
+  IMP_LOG(VERBOSE,"TransformationUtils::after move2state t1:");
+  IMP_LOG_WRITE(VERBOSE,IMP::core::XYZ(rb->get_particle()).get_coordinates());
+  IMP_LOG(VERBOSE,std::endl);
 }
-
-algebra::Transformation3D TransformationUtils::get_trans(Particle *p) const {
-  IMP_INTERNAL_CHECK(atts_.size()==7,
-      "TransformationUtils::get_trans the attribute list is not initialized");
-  algebra::Rotation3D rot(p->get_value(atts_[3]),p->get_value(atts_[4]),
-                          p->get_value(atts_[5]),p->get_value(atts_[6]));
-  algebra::Vector3D vec = core::XYZ::decorate_particle(p).get_coordinates();
-  algebra::Transformation3D t3d(rot,vec);
-  return t3d;
-}
-
 
 IMPDOMINO_END_NAMESPACE
