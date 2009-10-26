a2733290ede41e42a48179fb3e4d146588dc1578
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-10-26 20:29:39 +0000

    update test global minimum
    git-svn-id: https://svn.salilab.org/imp/trunk@3945 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/domino/test/test_global_minimum_1.py
+++ modules/domino/test/test_global_minimum_1.py
@@ -8,12 +8,13 @@ class DOMINOTests(IMP.test.TestCase):
     def setUp(self):
         """Set up model and particles"""
         IMP.test.TestCase.setUp(self)
+        IMP.set_log_level(IMP.SILENT)
+        self.num_particles=5
         self.sampler = my_optimizer.my_optimizer(
                          self.get_input_file_name("simple_jt1.txt"),
                          self.get_input_file_name("simple_jt1_restraints.txt"),
-                         5)
+                         self.num_particles)
         self.infered_score=self.sampler.optimize()
-
     def test_global_min(self):
         try:
             min_score2 = self.sampler.exhaustive_search()
@@ -27,8 +28,8 @@ class DOMINOTests(IMP.test.TestCase):
             print >> sys.stderr, detail
 
 
-    def test_inference(self):
-        score = -103.099998474
+    def test_inference_1(self):
+        score = -148.600001335
         self.assert_( abs(self.infered_score -score) < 0.1 , "the score of the minimum configuration as calculated by the inference is wrong " + str(self.infered_score) + " != " + str(score))
 
 
