c1acc18469544804f1e6294749f848c1a5a55b61
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-10-26 20:30:50 +0000

    test case for transformation utils
    git-svn-id: https://svn.salilab.org/imp/trunk@3947 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/domino/test/test_transformation_utils.py
+++ modules/domino/test/test_transformation_utils.py
@@ -7,36 +7,26 @@ import IMP.core
 import IMP.atom
 import IMP.helper
 import time
+import helpers
 
 class TransformationUtilsTests(IMP.test.TestCase):
 
     def __set_rigid_bodies__(self):
-        self.rbs = IMP.Particles()
-        names=["A","B","C","D"]
-        for i,mh in enumerate(self.mhs):
-            rb_p = IMP.Particle(self.mdl)
-            rb_state = IMP.helper.create_rigid_body(rb_p,
-                            IMP.core.XYZs(IMP.core.get_leaves(mh)))
-            self.mdl.add_score_state(rb_state)
-            self.rbs.append(rb_p)
+        self.rbs=helpers.set_rigid_bodies(self.mhs,self.mdl)
 
     def __set_components__(self):
-        self.mhs = IMP.atom.Hierarchies()
-        sel = IMP.atom.CAlphaSelector()
-        for fn,name in [["1z5s_A.pdb","A"],["1z5s_B.pdb","B"],
-                   ["1z5s_C.pdb","C"],["1z5s_D.pdb","D"]]:
-            self.mhs.append(IMP.atom.read_pdb(self.open_input_file(fn),
-                                              self.mdl,sel))
-            IMP.atom.add_radii(self.mhs[-1])
+        self.mhs = helpers.read_components([
+            self.open_input_file("1z5s_A.pdb"),
+            self.open_input_file("1z5s_B.pdb"),
+            self.open_input_file("1z5s_C.pdb"),
+            self.open_input_file("1z5s_D.pdb")],self.mdl)
 
     def __set_components_copy__(self):
-        self.mhs_copy = IMP.atom.Hierarchies()
-        sel = IMP.atom.CAlphaSelector()
-        for fn,name in [["1z5s_A.pdb","A"],["1z5s_B.pdb","B"],
-                   ["1z5s_C.pdb","C"],["1z5s_D.pdb","D"]]:
-            self.mhs_copy.append(IMP.atom.read_pdb(self.open_input_file(fn),
-                                              self.mdl,sel))
-            IMP.atom.add_radii(self.mhs_copy[-1])
+        self.mhs_copy = helpers.read_components([
+            self.open_input_file("1z5s_A.pdb"),
+            self.open_input_file("1z5s_B.pdb"),
+            self.open_input_file("1z5s_C.pdb"),
+            self.open_input_file("1z5s_D.pdb")],self.mdl)
 
     def setUp(self):
 
@@ -71,6 +61,62 @@ class TransformationUtilsTests(IMP.test.TestCase):
                 #return the copy to ref for the next round
                 for xyz in xyz_copy:
                     xyz.set_coordinates(rand_t.get_inverse().transform(xyz.get_coordinates()))
+
+
+    def test_transformation_on_rigid_bodies(self):
+        '''test transformation utils on rigid bodies'''
+        for j in range(5): # 5 test cases
+            for i in range(4):
+                #generate random transformation
+                rand_t=IMP.algebra.Transformation3D(IMP.algebra.random_rotation(),
+                                                    IMP.algebra.random_vector_in_unit_box())
+                #transform the copy molecule
+                xyz_copy=IMP.core.XYZs(IMP.core.get_leaves(self.mhs_copy[i]))
+                for xyz in xyz_copy:
+                    xyz.set_coordinates(rand_t.transform(xyz.get_coordinates()))
+                #transform the rigid body
+                xyz_orig=IMP.core.XYZs(IMP.core.get_leaves(self.mhs[i]))
+                rand_t_p = IMP.domino.Transformation.setup_particle(IMP.Particle(self.mdl),rand_t)
+                self.tu.move2state(self.rbs[i],rand_t_p.get_particle())
+                self.mdl.evaluate(None) #to make sure that the rigid bodies score states are updated
+                #check that the rmsd is 0
+                self.assert_(IMP.atom.rmsd(xyz_copy,xyz_orig) < 0.001,
+                           "the molecules are expected to have the same placement")
+                #return the copy to ref for the next round
+                for xyz in xyz_copy:
+                    xyz.set_coordinates(rand_t.get_inverse().transform(xyz.get_coordinates()))
+
+
+    def test_transformation_on_rigid_bodies2(self):
+        '''test transformation utils on rigid bodies'''
+        tu = IMP.domino.TransformationUtils(self.rbs)
+        for j in range(5):
+            for i in range(4):
+                trans = helpers.create_random_transformation()
+                state_p=IMP.Particle(self.mdl)
+                IMP.domino.Transformation.setup_particle(state_p,trans)
+
+                #transform the copy molecule
+                xyz_copy=IMP.core.XYZs(IMP.core.get_leaves(self.mhs_copy[i]))
+                for xyz in xyz_copy:
+                    xyz.set_coordinates(trans.transform(xyz.get_coordinates()))
+
+
+                #transform the rigid body
+                xyz_orig=IMP.core.XYZs(IMP.core.get_leaves(self.mhs[i]))
+                self.tu.move2state(self.rbs[i],state_p)
+            self.mdl.evaluate(None) #to make sure that the rigid bodies score states are updated
+            #check that the rmsd is 0
+            self.assert_(IMP.atom.rmsd(xyz_copy,xyz_orig) < 0.001,
+                         "the molecules are expected to have the same placement")
+            #return the copy to ref for the next round
+            for xyz in xyz_copy:
+                xyz.set_coordinates(trans.get_inverse().transform(xyz.get_coordinates()))
+
+
+
+
+
 
 if __name__ == '__main__':
     unittest.main()
