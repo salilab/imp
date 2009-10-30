dc3b54665a9d8f19257080befa444ccce4637e8b
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-10-30 19:31:22 +0000

    update it too
    git-svn-id: https://svn.salilab.org/imp/trunk@4016 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/examples/cover_particles.py
+++ modules/core/examples/cover_particles.py
@@ -8,11 +8,10 @@ m= IMP.Model()
 prot= IMP.atom.read_pdb(IMP.get_data_directory()\
                          +'/atom/example_protein.pdb', m)
 res= IMP.atom.get_by_type(prot, IMP.atom.RESIDUE_TYPE)
-rc= IMP.core.ListSingletonContainer(res)
 for p in res:
     IMP.core.XYZR.setup_particle(p.get_particle())
 mtr=IMP.atom.Hierarchy.get_traits()
 pr= IMP.core.ChildrenRefiner(mtr)
-for r in rc.get_particles:
+for r in rc.get_particles():
     IMP.core.Cover.setup_particle(r, pr)
 m.evaluate(False)
