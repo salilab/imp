62880c8cb4bd722600a4cdd38a5f4bee65ab2a65
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-10-30 18:25:57 +0000

    add tests for new decorators
    git-svn-id: https://svn.salilab.org/imp/trunk@4010 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/core/test/decorators/test_centroid.py
@@ -0,0 +1,22 @@
+import unittest
+import IMP
+import IMP.test
+import IMP.core
+import IMP.algebra
+import random
+
+class TestREFCover(IMP.test.TestCase):
+    """Tests for RefinerCover"""
+
+    def test_it(self):
+        """Test centroid of refined decorator"""
+        m= IMP.Model()
+        IMP.set_log_level(IMP.MEMORY)
+        n= random.randrange(1,10)
+        ps=IMP.core.create_xyzr_particles(m, 10, 1)
+        fpr= IMP.core.FixedRefiner(ps)
+        p= IMP.core.Centroid.setup_particle(IMP.Particle(m), fpr)
+        print "eval"
+        m.evaluate(True)
+if __name__ == '__main__':
+    unittest.main()
---
+++ modules/core/test/decorators/test_cover_refiner.py
@@ -0,0 +1,32 @@
+import unittest
+import IMP
+import IMP.test
+import IMP.core
+import IMP.algebra
+import random
+
+class TestREFCover(IMP.test.TestCase):
+    """Tests for RefinerCover"""
+
+    def check_cover(self, p, ps):
+        d= IMP.core.XYZR(p)
+        d.show()
+        for cp in ps:
+            dc= IMP.core.XYZR(cp.get_particle())
+            dc.show()
+            #d.get_sphere().get_center().show()
+            #dc.get_sphere().get_center().show()
+            self.assert_(d.get_sphere().get_contains(dc.get_sphere()))
+    def test_it(self):
+        """Test cover refined decorator"""
+        m= IMP.Model()
+        IMP.set_log_level(IMP.MEMORY)
+        n= random.randrange(1,10)
+        ps=IMP.core.create_xyzr_particles(m, 10, 1)
+        p=IMP.Particle(m)
+        ss= IMP.core.Cover.setup_particle(p,
+                                          IMP.core.FixedRefiner(ps))
+        m.evaluate(True)
+        self.check_cover(p, ps)
+if __name__ == '__main__':
+    unittest.main()
