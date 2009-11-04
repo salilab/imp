8f844ace0a3fc4d892def44196dd72593910b699
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-11-04 15:21:30 +0000

    Scan more grid points to try and find the best fit.
    git-svn-id: https://svn.salilab.org/imp/trunk@4058 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/em/test/test_fitting_grid_search_pdb.py
+++ modules/em/test/test_fitting_grid_search_pdb.py
@@ -49,7 +49,7 @@ class ProteinRigidFittingTest(IMP.test.TestCase):
             self.particles,self.imp_model,
             self.radius_key, self.weight_key,
             self.scene,
-            fr,8,8,2)
+            fr,7,7,2)
 
         #test that if you apply the transformation on the original configuration you get the same result
         # (in rmsd and score)
