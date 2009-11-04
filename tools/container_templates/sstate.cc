cfd7e2198d5ea4e0b3205c7343d444c4d9167b71
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-11-04 01:42:59 +0000

    update for new dependency formulation
    git-svn-id: https://svn.salilab.org/imp/trunk@4046 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- tools/container_templates/sstate.cc
+++ tools/container_templates/sstate.cc
@@ -44,7 +44,7 @@ void GroupnameScoreState::do_after_evaluate(DerivativeAccumulator *da)
   IMP_CHECK_OBJECT(af_);
   if (da) {
     IMP::internal::ContainerTraits<Classname>
-      ::apply(af_.get(), v_, da);
+      ::apply(af_.get(), v_, *da);
   }
   IMP_LOG(TERSE, "End GroupnamesScoreState::after_evaluate" << std::endl);
 }
@@ -66,19 +66,51 @@ ObjectsTemp GroupnameScoreState::get_output_objects() const {
 }
 
 ParticlesTemp GroupnameScoreState::get_input_particles() const {
-  ParticlesTemp ret0, ret1;
-  if (f_) ret0= IMP::internal::get_input_particles(v_, f_.get());
-  if (af_) ret1= IMP::internal::get_input_particles(v_, af_.get());
-  ret0.insert(ret0.end(), ret1.begin(), ret1.end());
-  return ret0;
+  ParticlesTemp ret;
+  if (f_) {
+    ret= IMP::internal::get_input_particles(v_, f_.get());
+    IMP_IF_CHECK(USAGE) {
+      if (af_) {
+        ParticlesTemp oret= IMP::internal::get_output_particles(v_, af_.get());
+        std::sort(ret.begin(), ret.end());
+        std::sort(oret.begin(), oret.end());
+        ParticlesTemp t;
+        std::set_union(ret.begin(), ret.end(), oret.begin(), oret.end(),
+                       std::back_inserter(t));
+        IMP_USAGE_CHECK(t.size() == oret.size(), "The particles written by "
+                        << " the after modifier in " << get_name() << " must "
+                        << "be a subset of those read by the before modifier.",
+                        UsageException);
+      }
+    }
+  } else {
+    ret= IMP::internal::get_output_particles(v_, af_.get());
+  }
+  return ret;
 }
 
 ParticlesTemp GroupnameScoreState::get_output_particles() const {
-  ParticlesTemp ret0, ret1;
-  if (f_) ret0= IMP::internal::get_input_particles(v_, f_.get());
-  if (af_) ret1= IMP::internal::get_input_particles(v_, af_.get());
-  ret0.insert(ret0.end(), ret1.begin(), ret1.end());
-  return ret0;
+   ParticlesTemp ret;
+  if (f_) {
+    ret= IMP::internal::get_output_particles(v_, f_.get());
+    IMP_IF_CHECK(USAGE) {
+      if (af_) {
+        ParticlesTemp oret= IMP::internal::get_input_particles(v_, af_.get());
+        std::sort(ret.begin(), ret.end());
+        std::sort(oret.begin(), oret.end());
+        ParticlesTemp t;
+        std::set_union(ret.begin(), ret.end(), oret.begin(), oret.end(),
+                       std::back_inserter(t));
+        IMP_USAGE_CHECK(t.size() == oret.size(), "The particles read by "
+                      << " the after modifier in " << get_name() << " must "
+                        << "be a subset of those written by the before modifier.",
+                        UsageException);
+      }
+    }
+  } else {
+    ret= IMP::internal::get_input_particles(v_, af_.get()); 
+  }
+  return ret;
 }
 
 void GroupnameScoreState::show(std::ostream &out) const {
--- tools/container_templates/state.cc
+++ tools/container_templates/state.cc
@@ -66,19 +66,51 @@ ObjectsTemp GroupnamesScoreState::get_output_objects() const {
 }
 
 ParticlesTemp GroupnamesScoreState::get_input_particles() const {
-  ParticlesTemp ret0, ret1;
-  if (f_) ret0= IMP::internal::get_input_particles(c_, f_.get());
-  if (af_) ret1= IMP::internal::get_input_particles(c_, af_.get());
-  ret0.insert(ret0.end(), ret1.begin(), ret1.end());
-  return ret0;
+  ParticlesTemp ret;
+  if (f_) {
+    ret= IMP::internal::get_input_particles(c_, f_.get());
+    IMP_IF_CHECK(USAGE) {
+      if (af_) {
+        ParticlesTemp oret= IMP::internal::get_output_particles(c_, af_.get());
+        std::sort(ret.begin(), ret.end());
+        std::sort(oret.begin(), oret.end());
+        ParticlesTemp t;
+        std::set_union(ret.begin(), ret.end(), oret.begin(), oret.end(),
+                       std::back_inserter(t));
+        IMP_USAGE_CHECK(t.size() == oret.size(), "The particles written by "
+                        << " the after modifier in " << get_name() << " must "
+                        << "be a subset of those read by the before modifier.",
+                        UsageException);
+      }
+    }
+  } else {
+    ret= IMP::internal::get_output_particles(c_, af_.get());
+  }
+  return ret;
 }
 
 ParticlesTemp GroupnamesScoreState::get_output_particles() const {
-  ParticlesTemp ret0, ret1;
-  if (f_) ret0= IMP::internal::get_output_particles(c_, f_.get());
-  if (af_) ret1= IMP::internal::get_output_particles(c_, af_.get());
-  ret0.insert(ret0.end(), ret1.begin(), ret1.end());
-  return ret0;
+  ParticlesTemp ret;
+  if (f_) {
+    ret= IMP::internal::get_output_particles(c_, f_.get());
+    IMP_IF_CHECK(USAGE) {
+      if (af_) {
+        ParticlesTemp oret= IMP::internal::get_input_particles(c_, af_.get());
+        std::sort(ret.begin(), ret.end());
+        std::sort(oret.begin(), oret.end());
+        ParticlesTemp t;
+        std::set_union(ret.begin(), ret.end(), oret.begin(), oret.end(),
+                       std::back_inserter(t));
+        IMP_USAGE_CHECK(t.size() == oret.size(), "The particles read by "
+                      << " the after modifier in " << get_name() << " must "
+                        << "be a subset of those written by the before modifier.",
+                        UsageException);
+      }
+    }
+  } else {
+    ret= IMP::internal::get_input_particles(c_, af_.get()); 
+  }
+  return ret;
 }
 
 void GroupnamesScoreState::show(std::ostream &out) const {
