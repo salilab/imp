4334b0ccdd136ed18cc578d2de6be1baf21dc5e1
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-11-25 08:05:59 +0000

    add checks and missing files
    git-svn-id: https://svn.salilab.org/imp/trunk@4142 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/domino/include/RestraintEvaluatorFromFile.h
@@ -0,0 +1,67 @@
+/**
+ *  \file RestraintEvaluatorFromFiles.h   \brief A restraint evaluataor.
+ *
+ *  Copyright 2007-9 Sali Lab. All rights reserved.
+ *
+ */
+
+#ifndef IMPDOMINO_RESTRAINT_EVALUATOR_FROM_FILE_H
+#define IMPDOMINO_RESTRAINT_EVALUATOR_FROM_FILE_H
+
+#include "config.h"
+#include "DiscreteSampler.h"
+#include <IMP/domino/RestraintEvaluatorI.h>
+#include <IMP/base_types.h>
+#include <vector>
+#include <sstream>
+#include <algorithm>
+#include <boost/algorithm/string.hpp>
+#include <boost/algorithm/string/split.hpp>
+IMPDOMINO_BEGIN_NAMESPACE
+
+IMPDOMINOEXPORT void write_combinations(const std::string &filename,
+                       const Combinations *combs,Particles &ps);
+/**
+The format of the combinations file is:
+A|B|| (the header line, contains the names of the particles)
+0|0|0.3 (data line: For A in state 0, B in state 0, the restraint score is 0.3)
+0|1|1.6
+1|0|2.9
+1|1|3.1
+ */
+IMPDOMINOEXPORT void read_combinations(const std::string &filename,
+                                       Combinations *combs,const Particles &ps);
+//! A read restraint values from precalculated files
+/**
+ */
+class IMPDOMINOEXPORT RestraintEvaluatorFromFile : public RestraintEvaluatorI
+{
+public:
+  //! Constructor
+  //RestraintEvaluatorFromFile(){}
+  //~RestraintEvaluatorFromFile(){}
+  //! Add link between a restraint and its precalculated scores
+  /**\param [in] r A restraint
+     \param [in] filename The restraint scores filename
+   */
+  void set_restraint_file(Restraint *r,char *filename);
+  std::string get_restraint_file(Restraint *r) const;
+ //! Score restraint with combinations of states
+  /** \param [in] comb_states the combination of states
+      \param [in] r the restraint to score
+      \param [in] ps the particles that scored by the restraint
+      \param[out] comb_values the restraint values for the combiations
+   */
+  void calc_scores(const Combinations &comb_states,
+                   CombinationValues &comb_values,Restraint *r,
+                   const Particles &ps);
+protected:
+  //TODO - this function should be changed once we will have the
+  //new kernel functionalities.
+ void update_score_states(IMP::Particles &ps);
+ std::map<Restraint*,std::string> restraint_data_;
+};
+
+IMPDOMINO_END_NAMESPACE
+
+#endif /* IMPDOMINO_RESTRAINT_EVALUATOR_FROM_FILE_H */
---
+++ modules/domino/src/RestraintEvaluator.cpp
@@ -0,0 +1,53 @@
+/**
+ *  \file RestraintEvaluator.cpp
+ *  \brief A restraint evaluator file
+ *
+ *  Copyright 2007-9 Sali Lab. All rights reserved.
+ *
+ */
+#include "IMP/domino/RestraintEvaluator.h"
+#include <IMP/log.h>
+IMPDOMINO_BEGIN_NAMESPACE
+
+
+void RestraintEvaluator::calc_scores(const Combinations &comb_states,
+                         CombinationValues &comb_values,
+                         Restraint *r, const Particles &ps) {
+  for(Combinations::const_iterator it = comb_states.begin();
+      it != comb_states.end(); it++) {
+    const CombState *cs = it->second;
+    ds_->move2state(cs);
+    //update all relevant score states -
+    //should be removed in the near future (TODO)
+    update_score_states(ps);
+    comb_values[cs->partial_key(&ps)]=r->unprotected_evaluate(NULL);
+  }
+}
+
+
+ScoreStates get_used_score_states(const IMP::Particles &ps, Model *m) {
+  ScoreStates used_score_states;
+  std::map<Particle*,int> ps_map;
+  for(IMP::Particles::const_iterator it=ps.begin(); it != ps.end(); it++) {
+    ps_map[*it]=1;
+  }
+  //iterating over all score states
+  for(ScoreStates::const_iterator it = m->score_states_begin();
+      it != m->score_states_end(); it++) {
+    IMP::Particles used_particles = (*it)->get_input_particles();
+    IMP::Particles::const_iterator up_it = used_particles.begin();
+    bool not_added=true;
+    //iterating over the used particles
+    while(not_added && up_it != used_particles.end()) {
+      IMP::Particles::const_iterator ps_it = ps.begin();
+      //iterating over the input particles
+      if (ps_map.find(*up_it) != ps_map.end()) {
+        not_added=false;
+        used_score_states.push_back(*it);
+      }//end input particles iteration
+      up_it++;
+    }//end used particles iteration
+  }//end of score states iteration
+  return used_score_states;
+}
+IMPDOMINO_END_NAMESPACE
