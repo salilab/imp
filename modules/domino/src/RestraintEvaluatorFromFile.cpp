925a1efcb92c7f00f83e11a31a9fc9878de2a728
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-11-26 18:02:26 +0000

    get rid of warnings
    git-svn-id: https://svn.salilab.org/imp/trunk@4158 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/domino/src/RestraintEvaluatorFromFile.cpp
+++ modules/domino/src/RestraintEvaluatorFromFile.cpp
@@ -57,7 +57,7 @@ void read_combinations(const std::string &filename, Combinations *combs,
               IOException);
   }
   //read the file
-  int status;
+  //int status;
   std::string line;
   //read the header line
   getline(scores_file, line);
@@ -71,7 +71,7 @@ void read_combinations(const std::string &filename, Combinations *combs,
   IMP_INTERNAL_CHECK(ps.size()==split_vec.size()-1,
       "the number of particles in the file does not corresponde to the "
       <<"number of input particles"<<std::endl);
-  for(int i=0;i<ps.size();i++){
+  for(unsigned int i=0;i<ps.size();i++){
     IMP_INTERNAL_CHECK(ps[i]->get_value(node_name_key())==split_vec[i],
           "wrong particle name:"<<ps[i]->get_value(node_name_key())
            <<":"<<split_vec[i]<<":"<<std::endl);
@@ -81,7 +81,7 @@ void read_combinations(const std::string &filename, Combinations *combs,
     if (!getline(scores_file, line)) break;
     boost::split(split_vec, line, boost::is_any_of("|"));
     calc_state = new CombState();
-    for(int i=0;i<ps.size();i++){
+    for(unsigned int i=0;i<ps.size();i++){
       calc_state->add_data_item(ps[i],atoi(split_vec[i].c_str()));
     }
     calc_state->update_total_score(0.,
