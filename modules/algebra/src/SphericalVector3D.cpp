124e6af55b3735be573b55253a117593fca4a5c7
dina@SALILAB.ORG <dina@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-11-26 05:50:01 +0000

    semi sphere sampling
    git-svn-id: https://svn.salilab.org/imp/trunk@4153 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/src/SphericalVector3D.cpp
+++ modules/algebra/src/SphericalVector3D.cpp
@@ -11,20 +11,20 @@ Vector3D SphericalVector3D::get_cartesian_coordinates() {
   return Vector3D(v_[0]*cos(v_[2])*sin(v_[1]),
                   v_[0]*sin(v_[2])*sin(v_[1]),
                   v_[0]*cos(v_[1]));
-};
+}
 
 void SphericalVector3D::set_cartesian_coordinates(Vector3D& v) {
   v_[0] = v.get_magnitude();
   v_[1] = atan2(sqrt(v[0]*v[0]+v[1]*v[1]),v[2]);
   v_[2] = atan2(v[1],v[0]);
-};
+}
 
 bool SphericalVector3D::check(double r, double tetha, double psi) {
   if(r<0) {return false;}
   if(tetha< -PI || tetha>PI) {return false;}
   if(psi< -2*PI || psi>2*PI) {return false;}
   return true;
-};
+}
 
 
 void quasi_evenly_spherical_distribution(unsigned long N,
@@ -46,6 +46,27 @@ void quasi_evenly_spherical_distribution(unsigned long N,
     vs[k-1][1]=theta;
     vs[k-1][2]=psi;
   }
-};
+}
+
+void quasi_evenly_semi_spherical_distribution(unsigned long N,
+                              SphericalVector3Ds &vs,double r) {
+  vs.resize(N);
+  double theta,psi;
+  for (unsigned long k=1;k<=N;++k) {
+    double h = 2*((double)k-1)/(2*N-1) - 1.0;
+    theta = acos(h);
+    if(k==1) {
+      psi=0;
+    } else {
+      psi=(vs[k-2][2] + 3.6/sqrt((double)2*N*(1.0-h*h)));
+      int div = psi/(2*PI);
+      psi -= div*2*PI;
+    }
+    // Set the values the spherical vector
+    vs[k-1][0]=r;
+    vs[k-1][1]=theta;
+    vs[k-1][2]=psi;
+  }
+}
 
 IMPALGEBRA_END_NAMESPACE
