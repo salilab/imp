e2bdb5b038fa741329eb2aa44765fff881fbb2fe
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-11-26 00:27:01 +0000

    add explanatory note
    git-svn-id: https://svn.salilab.org/imp/trunk@4147 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/include/distance.h
+++ modules/atom/include/distance.h
@@ -7,13 +7,16 @@
  */
 #ifndef IMPATOM_DISTANCE_H
 #define IMPATOM_DISTANCE_H
+
 #include "config.h"
 #include <IMP/core/XYZ.h>
 #include "Hierarchy.h"
+
 IMPATOM_BEGIN_NAMESPACE
 //! Calculate the root mean square deviation between two sets of 3D points.
 /**
-\note the function assumes correspondence between two sets of points
+\note the function assumes correspondence between two sets of points and does
+not perform rigid alignment.
  */
 IMPATOMEXPORT double rmsd(const core::XYZs& m1 ,const core::XYZs& m2);
 
