286bba3f04e4856bdedb03e84f7c6f1f1509222d
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-11-26 17:04:02 +0000

    no implicit conversions to particle using swig
    git-svn-id: https://svn.salilab.org/imp/trunk@4154 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/domino/test/test_transformation_utils.py
+++ modules/domino/test/test_transformation_utils.py
@@ -59,7 +59,7 @@ class TransformationUtilsTests(IMP.test.TestCase):
 
                 #transform the rigid body
                 xyz_orig=IMP.core.XYZs(IMP.core.get_leaves(self.mhs[i]))
-                self.tu.move2state(self.rbs[i],state_p)
+                self.tu.move2state(self.rbs[i].get_particle(),state_p)
             self.mdl.evaluate(False) #to make sure that the rigid bodies score states are updated
             #check that the rmsd is 0
             self.assert_(IMP.atom.rmsd(xyz_copy,xyz_orig) < 0.001,
