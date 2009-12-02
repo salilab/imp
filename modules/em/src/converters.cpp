f279fb55ff6f77a6e6166d6fa95c715222796800
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-12-02 05:06:44 +0000

    add radius to Voxel decorator
    git-svn-id: https://svn.salilab.org/imp/trunk@4172 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/em/src/converters.cpp
+++ modules/em/src/converters.cpp
@@ -13,6 +13,8 @@ IMPEM_BEGIN_NAMESPACE
 void density2particles(DensityMap &dmap, Float threshold,
                               Particles &ps, Model *m) {
   Float x,y,z,val;
+  Float voxel_size=dmap.get_header()->Objectpixelsize;
+  Float r=sqrt(3.*voxel_size*voxel_size);
   FloatKey dens_key = Voxel::get_density_key();
   for (long i=0;i<dmap.get_number_of_voxels();i++) {
     val = dmap.get_value(i);
@@ -21,7 +23,7 @@ void density2particles(DensityMap &dmap, Float threshold,
       x = dmap.voxel2loc(i,0);
       y = dmap.voxel2loc(i,1);
       z = dmap.voxel2loc(i,2);
-      Voxel::setup_particle(p,IMP::algebra::Vector3D(x,y,z),val);
+      Voxel::setup_particle(p,IMP::algebra::Vector3D(x,y,z),r,val);
       ps.push_back(p);
     }
   }
