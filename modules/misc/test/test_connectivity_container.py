b8624f30339be284bb2d7719a7b36e3123a07488
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-12-04 01:46:31 +0000

    add experimental faster connectivity support
    git-svn-id: https://svn.salilab.org/imp/trunk@4184 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/misc/test/test_connectivity_container.py
@@ -0,0 +1,50 @@
+import unittest
+import IMP
+import IMP.test
+import IMP.misc
+import IMP.atom
+import math
+
+class TunnelTest(IMP.test.TestCase):
+    """Tests for tunnel scores"""
+
+    def test_score(self):
+        """Test connectivity"""
+        m= IMP.Model()
+        ps= IMP.core.create_xyzr_particles(m, 10, .1)
+        lsc=IMP.core.ListSingletonContainer(ps)
+        cpc=IMP.misc.ConnectingPairContainer(lsc, .1)
+        #m.add_restraint(pr)
+        m.evaluate(False)
+        for pp in cpc.get_particle_pairs():
+            print pp
+            # for p0 in ps:
+            #     for p1 in ps:
+            #         if p0 != p1:
+            #             d0= IMP.core.XYZR(p0)
+            #             d1= IMP.core.XYZR(p1)
+            #             if IMP.core.distance(d0, d1) < 0:
+            #                 print str(p0)+" " +str(p1)
+            #                 self.assert_(cpc.get_contains_particle_pair(ParticlePair(p0, p1))
+            #                 or cpc.get_contains_particle_pair(ParticlePair(p1, p0)))
+        cg= IMP.core.ConjugateGradients(m)
+        ub= IMP.core.HarmonicUpperBound(-.1, 1)
+        sd= IMP.core.SphereDistancePairScore(ub)
+        pr= IMP.core.PairsRestraint(sd, cpc)
+        m.add_restraint(pr)
+        cg.set_threshold(.001)
+        cg.optimize(1000)
+        for p0 in ps:
+            for p1 in ps:
+                if p0 != p1:
+                    d0= IMP.core.XYZR(p0.get_particle())
+                    d1= IMP.core.XYZR(p1.get_particle())
+                    self.assert_(IMP.core.distance(d0, d1) < 0)
+                    print str(p0)+" " +str(p1)
+                    #self.assert_(cpc.get_contains_particle_pair(IMP.ParticlePair(p0.get_particle(), p1.get_particle()))
+                                 #or cpc.get_contains_particle_pair(IMP.ParticlePair(p1.get_particle(), p0.get_particle())))
+
+
+
+if __name__ == '__main__':
+    unittest.main()
