9967184bdadca0de2038e71c25759c219ff2510c
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-12-09 21:02:35 +0000

    add class to rep ellipsoids
    git-svn-id: https://svn.salilab.org/imp/trunk@4197 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/algebra/include/Ellipsoid3D.h
@@ -0,0 +1,58 @@
+/**
+ *  \file Ellipsoid3D.h   \brief Simple 3D ellipsoid class.
+ *
+ *  Copyright 2007-9 Sali Lab. All rights reserved.
+ *
+ */
+#ifndef IMPALGEBRA_ELLIPSOID_3D_H
+#define IMPALGEBRA_ELLIPSOID_3D_H
+
+#include "Cylinder3D.h"
+#include <cmath>
+#include <IMP/constants.h>
+#include "internal/cgal_predicates.h"
+#include "BoundingBoxD.h"
+#include "Rotation3D.h"
+
+IMPALGEBRA_BEGIN_NAMESPACE
+
+/** Represent an ellispoid in 3D.
+  */
+class IMPALGEBRAEXPORT Ellipsoid3D: public UninitializedDefault {
+public:
+  Ellipsoid3D(){
+#if IMP_BUILD < IMP_FAST
+    radii_[0]= std::numeric_limits<double>::quiet_NaN();
+#endif
+  }
+  Ellipsoid3D(const Vector3D& center,double radius_x,
+              double radius_y, double radius_z,
+              const Rotation3D &rot);
+  double get_radius(unsigned int i) const {
+    IMP_USAGE_CHECK(!is_nan(radii_[0]),
+              "Attempt to use uninitialized ellipsoid.",
+              InvalidStateException);
+    return radii_[i];
+  }
+  const Vector3D &get_center() const {return center_;}
+  const Rotation3D &get_rotation() const {
+    return rot_;
+  }
+  IMP_SHOWABLE_INLINE({
+      out << "(" << spaces_io(center_) << ": " << get_radius(0)
+          << ", " << get_radius(1) << ", " << get_radius(2)
+          << ")";
+    })
+private:
+  Vector3D center_;
+  double radii_[3];
+  Rotation3D rot_;
+};
+
+IMP_OUTPUT_OPERATOR(Ellipsoid3D);
+
+typedef std::vector<Ellipsoid3D> Ellipsoid3Ds;
+
+IMPALGEBRA_END_NAMESPACE
+
+#endif /* IMPALGEBRA_ELLIPSOID_3D_H */
