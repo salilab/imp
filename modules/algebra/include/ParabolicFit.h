b500c30e52433899f5566474e411c4ff357163b1
dina@SALILAB.ORG <dina@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-12-11 01:22:18 +0000

    Parabolic fit class
    git-svn-id: https://svn.salilab.org/imp/trunk@4214 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/algebra/include/ParabolicFit.h
@@ -0,0 +1,57 @@
+/**
+ * \file ParabolicFit.h
+ * \brief fit the data with parabola
+ *
+ * Copyright 2007-9 Sali Lab. All rights reserved.
+ *
+ */
+#ifndef IMPALGEBRA_PARABOLIC_FIT_H
+#define IMPALGEBRA_PARABOLIC_FIT_H
+
+#include "Vector2D.h"
+
+#include <vector>
+
+IMPALGEBRA_BEGIN_NAMESPACE
+
+//! Calculate parabola that fits best the input data points
+class IMPALGEBRAEXPORT ParabolicFit {
+public:
+  //! Constructor
+  /**
+     \param[in] data vector of pairs (Vector2D) with x and their
+     corresponding y values
+   */
+  ParabolicFit(const algebra::Vector2Ds& data);
+
+  //! fit error
+  double get_fit_error(double x) const { return error_; }
+
+  //! get a value (a*x^2)
+  double get_a() const { return a_; }
+
+  //! get b value (b*x)
+  double get_b() const { return b_; }
+
+  //! get c value (constant)
+  double get_c() const { return c_; }
+
+  //! show equation
+  void show(std::ostream &out=std::cout) const {
+    out << "y = " << a_ << "x^2 + " << b_ << "x + " << c_ << std::endl;
+    out << "Error = " << error_ << std::endl;
+  }
+
+private:
+  void find_regression();
+  void evaluate_error();
+
+ private:
+  const algebra::Vector2Ds& data_;
+  double a_, b_, c_;
+  double error_;
+};
+
+IMPALGEBRA_END_NAMESPACE
+
+#endif /* IMPALGEBRA_PARABOLIC_FIT_H */
