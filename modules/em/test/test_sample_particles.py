adee7e820a5db45420df1ccfc9f4279b34b19419
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-12-13 19:15:54 +0000

    fix test case, for some reason all points were set to 0. In addition, we now test the sampling makes sense
    git-svn-id: https://svn.salilab.org/imp/trunk@4226 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/em/test/test_sample_particles.py
+++ modules/em/test/test_sample_particles.py
@@ -19,9 +19,9 @@ class SampleTests(IMP.test.TestCase):
         for i, (x,y,z) in enumerate(((9.0, 9.0, 9.0),
                                      (12.0, 3.0, 3.0),
                                      (3.0, 12.0, 12.0))):
-            p = self.create_point_particle(self.imp_model, 0, 0, 0)
+            p = self.create_point_particle(self.imp_model, x,y,z)
             p.add_attribute(IMP.FloatKey("radius"), 1.0, False)
-            p.add_attribute(IMP.FloatKey("weight"), 1.0)
+            p.add_attribute(IMP.FloatKey("weight"), 10.0)
             p.add_attribute(IMP.IntKey("id"), i)
             p.add_attribute(IMP.IntKey("protein"), 1)
             self.particles.append(p)
@@ -33,12 +33,13 @@ class SampleTests(IMP.test.TestCase):
 
     def test_sample_map(self):
         """Check that reading a map back in preserves the stdevs"""
-        resolution=3.
+        resolution=1.
         voxel_size=1.
         access_p = IMP.em.IMPParticlesAccessPoint(self.particles,
                               IMP.FloatKey("radius"), IMP.FloatKey("weight"))
-
         model_map = IMP.em.SampledDensityMap(access_p, resolution, voxel_size)
+        for p in self.particles:
+            self.assert_(model_map.get_value(IMP.core.XYZ(p).get_coordinates())>1.,"map was not sampled correctly")
         model_map.calcRMS()
         erw = IMP.em.EMReaderWriter()
         IMP.em.write_map(model_map, "xxx.em",erw)
@@ -48,7 +49,7 @@ class SampleTests(IMP.test.TestCase):
         self.assert_(abs(em_map.get_header().rms - \
                          model_map.get_header().rms) < .000001,
                      "standard deviations of maps differ")
-        os.unlink("xxx.em")
+        #os.unlink("xxx.em")
 
 if __name__ == '__main__':
     unittest.main()
