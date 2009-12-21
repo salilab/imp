085f11f061957e770299ab114d9b03977b885344
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-12-21 08:19:05 +0000

    add resolution as a parameter and add test cases
    git-svn-id: https://svn.salilab.org/imp/trunk@4271 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/em/test/test_surface_sample_particles.py
@@ -0,0 +1,50 @@
+import IMP
+import IMP.test
+import sys
+import IMP.em
+import unittest
+import os
+
+class SampleTests(IMP.test.TestCase):
+    """Tests for sampled density maps"""
+
+    def setUp(self):
+        """initialize IMP environment create particles"""
+        IMP.test.TestCase.setUp(self)
+        #init IMP model ( the environment)
+        self.imp_model = IMP.Model()
+        self.particles = IMP.Particles()
+        ## -  create a set of three particles in imp
+        npart = 3
+        for i, (x,y,z) in enumerate(((9.0, 9.0, 9.0),
+                                     (9.0, 5.0, 9.0),
+                                     (5.0, 9.0, 9.0),
+                                     (9.0, 9.0, 5.0),
+                                     (12.0, 3.0, 3.0),
+                                     (15.0, 3.0, 3.0),
+                                     (12.0, 6.0, 3.0),
+                                     (3.0, 12.0, 12.0))):
+            p = self.create_point_particle(self.imp_model, x,y,z)
+            p.add_attribute(IMP.FloatKey("radius"), 1.0, False)
+            p.add_attribute(IMP.FloatKey("weight"), 10.0)
+            p.add_attribute(IMP.IntKey("id"), i)
+            p.add_attribute(IMP.IntKey("protein"), 1)
+            self.particles.append(p)
+        self.particle_indexes = IMP.Ints()
+        self.particle_indexes.clear()
+        for i in range(npart):
+            self.particle_indexes.push_back(i)
+        print "initialization done ..."
+
+    def test_sample_map(self):
+        """Check that reading a map back in preserves the stdevs"""
+        resolution=1.
+        voxel_size=1.
+        access_p = IMP.em.IMPParticlesAccessPoint(self.particles,
+                              IMP.FloatKey("radius"), IMP.FloatKey("weight"))
+        model_map = IMP.em.SurfaceShellDensityMap(access_p, resolution, voxel_size)
+        for p in self.particles:
+            self.assert_(model_map.get_value(IMP.core.XYZ(p).get_coordinates())>2.,"map was not sampled correctly")
+
+if __name__ == '__main__':
+    unittest.main()
