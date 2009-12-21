37604dec12d5f683229d4ee6cf4e35e0fde85074
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-12-21 09:36:43 +0000

    add checks
    git-svn-id: https://svn.salilab.org/imp/trunk@4274 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/multifit/src/weighted_excluded_volume.cpp
+++ modules/multifit/src/weighted_excluded_volume.cpp
@@ -15,12 +15,14 @@ IMPMULTIFIT_BEGIN_NAMESPACE
 void add_surface_index(core::Hierarchy mh, Float resolution, Float apix,
                 FloatKey shell_key,
                 FloatKey radius_key,FloatKey weight_key)  {
-  std::cout<<"apix:"<<apix<<" resolution: " << resolution << std::endl;
   Particles ps = core::get_leaves(mh);
   em::IMPParticlesAccessPoint access_p(ps,radius_key,weight_key);
   em::SurfaceShellDensityMap shell_map(access_p,resolution,apix);
 
   for(unsigned int i=0; i<ps.size(); i++) {
+    IMP_INTERNAL_CHECK(not ps[i]->has_attribute(shell_key),
+     "Particle " << ps[i]->get_name() <<
+     " already has shell attribute" << std::endl);
     ps[i]->add_attribute(
       shell_key,shell_map.get_value(core::XYZ(ps[i]).get_coordinates()));
   }
