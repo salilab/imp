fc280c63c4d0bf5f7e8995b139d9c7e797a359e0
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-12-21 21:09:14 +0000

    create weighted excluded volume restraint
    git-svn-id: https://svn.salilab.org/imp/trunk@4284 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/multifit/include/weighted_excluded_volume.h
+++ modules/multifit/include/weighted_excluded_volume.h
@@ -13,6 +13,7 @@
 #include <IMP/algebra/Transformation3D.h>
 #include <IMP/Model.h>
 #include <IMP/core/Hierarchy.h>
+#include <IMP/core/rigid_bodies.h>
 #include <IMP/atom/Mass.h>
 #include "config.h"
 IMPMULTIFIT_BEGIN_NAMESPACE
@@ -29,6 +30,17 @@ IMPMULTIFITEXPORT void add_surface_index(core::Hierarchy mhd,
                 FloatKey radius_key=core::XYZR::get_default_radius_key(),
                 FloatKey weight_key=atom::Mass::get_mass_key()) ;
 
+//! create a  weighted excluded volume restraint between two rigid bodies
+/**
+/param[in] rb1 the first rigid body
+/param[in] rb2 the second rigid body
+/param[in] shell_key the attribute that stored the particles
+                     surface level with respect to its molecule
+ */
+IMPMULTIFITEXPORT IMP::Restraint* create_weighted_excluded_volume_restraint(
+                core::RigidBody *rb1,
+                core::RigidBody *rb2,
+                FloatKey shell_key= FloatKey("surf_ind")) ;
 
 IMPMULTIFIT_END_NAMESPACE
 #endif /* IMPMULTIFIT_WEIGHTED_EXCLUDED_VOLUME_H */
--- modules/multifit/src/weighted_excluded_volume.cpp
+++ modules/multifit/src/weighted_excluded_volume.cpp
@@ -8,6 +8,14 @@
  */
 #include <IMP/multifit/weighted_excluded_volume.h>
 #include <IMP/em/SurfaceShellDensityMap.h>
+#include <IMP/core/ListSingletonContainer.h>
+#include <IMP/core/CloseBipartitePairContainer.h>
+#include <IMP/core/RigidClosePairsFinder.h>
+#include <IMP/core/LeavesRefiner.h>
+#include <IMP/core/HarmonicLowerBound.h>
+#include <IMP/core/SphereDistancePairScore.h>
+#include <IMP/core/PairsRestraint.h>
+#include <IMP/atom/Hierarchy.h>
 #include <IMP/em/SampledDensityMap.h>
 #include <IMP/em/IMPParticlesAccessPoint.h>
 #include <IMP/em/MRCReaderWriter.h>
@@ -26,5 +34,29 @@ void add_surface_index(core::Hierarchy mh, Float resolution, Float apix,
     ps[i]->add_attribute(
       shell_key,shell_map.get_value(core::XYZ(ps[i]).get_coordinates()));
   }
+}
+
+IMP::Restraint *create_weighted_excluded_volume_restraint(
+   core::RigidBody *rb1,
+   core::RigidBody *rb2,
+   FloatKey shell_key) {
+  IMP::Model* mdl=rb1->get_particle()->get_model();
+  //generate the list singleton contrainers
+  Particles ps1,ps2;
+  ps1.push_back(rb1->get_particle());
+  ps2.push_back(rb2->get_particle());
+  IMP_NEW(core::ListSingletonContainer,ls1,(ps1));
+  IMP_NEW(core::ListSingletonContainer,ls2,(ps2));
+  //set up the nonbonded list
+  IMP_NEW(core::LeavesRefiner,leaves_refiner,(atom::Hierarchy::get_traits()));
+  IMP_NEW(core::RigidClosePairsFinder, close_pair_finder,(leaves_refiner));
+  IMP_NEW(core::CloseBipartitePairContainer,nbl,
+          (ls1,ls2,2.,close_pair_finder));
+  //Set up excluded volume
+  IMP_NEW(core::HarmonicLowerBound,hlb,(0,1));
+  IMP_NEW(core::SphereDistancePairScore,ps,(hlb));
+  IMP_NEW(core::PairsRestraint,evr,(ps, nbl));
+  mdl->add_restraint(evr);
+  return evr;
 }
 IMPMULTIFIT_END_NAMESPACE
