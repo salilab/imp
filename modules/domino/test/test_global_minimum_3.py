9c5bd55051c3de6d422ebb4b785450abb6db7887
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2009-12-30 19:41:55 +0000

    testcase for exhustive enumerator function in DominoOptimizer
    git-svn-id: https://svn.salilab.org/imp/trunk@4346 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/domino/test/test_global_minimum_3.py
@@ -0,0 +1,30 @@
+import sys
+import unittest
+import IMP
+import IMP.test
+import my_optimizer
+IMP.set_log_level(IMP.SILENT)#VERBOSE)
+class DOMINOTests(IMP.test.TestCase):
+    def setUp(self):
+        """Set up model and particles"""
+        IMP.test.TestCase.setUp(self)
+        self.sampler = my_optimizer.my_optimizer(
+                          self.get_input_file_name("simple_jt2.txt"),
+                          self.get_input_file_name("simple_jt2_restraints.txt"),
+                          8)
+        self.infered_score = self.sampler.optimize()
+    def test_inference_2(self):
+        self.sampler.d_opt.set_sampling_space(self.sampler.discrete_sampler)
+        combs=IMP.domino.CombStates()
+        self.sampler.d_opt.exhaustive_enumeration(combs)
+        #select the minimum one
+        min_comb=None
+        min_score=10000
+        for comb in combs:
+            if min_score>comb.get_total_score():
+                min_score =  comb.get_total_score()
+                min_comb=comb
+        self.assert_( abs(self.infered_score -min_score) < 0.1 , "the score of the minimum configuration as calculated by the inference is wrong " + str(self.infered_score) + " != " + str(min_score))
+
+if __name__ == '__main__':
+    unittest.main()
