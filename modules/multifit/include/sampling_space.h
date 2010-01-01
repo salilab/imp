d2b5074b541e126a903aa33cf951a6a793249983
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-01-01 03:51:35 +0000

    add functions to support MultiFit sampling space
    git-svn-id: https://svn.salilab.org/imp/trunk@4354 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/multifit/include/sampling_space.h
@@ -0,0 +1,43 @@
+/**
+ *  \file sampling_space.h
+ *  \brief handles settting sampling space for different mappings
+ *
+ *  Copyright 2007-9 Sali Lab. All rights reserved.
+ *
+ */
+
+#ifndef IMPMULTIFIT_SAMPLING_SPACE_H
+#define IMPMULTIFIT_SAMPLING_SPACE_H
+
+#include <IMP/algebra/Transformation3D.h>
+#include <IMP/Model.h>
+#include <IMP/domino/TransformationMappedDiscreteSet.h>
+#include <IMP/domino/TransformationCartesianProductSampler.h>
+#include "config.h"
+
+IMPMULTIFIT_BEGIN_NAMESPACE
+
+//! Set a mapped discrete sampling space
+/**
+/param[in] full_smpl_space the full sampling space
+/param[in] anchor_points the assembly anchor points
+/param[in] mapping the mapping of components to anchor points
+/note the function creates a new sampling space composed of all transformations
+of a component that are close enough to its mapped anchor point.
+For more details check out Lasker, JMB, 2009
+ */
+IMPMULTIFITEXPORT domino::TransformationMappedDiscreteSet*
+ create_mapped_discrete_set(
+  domino::TransformationMappedDiscreteSet* full_smpl_space,
+  const Particles &asmb_anchor_points, const Particles &components,
+  const Ints &mapping, Float dist_t=10.);
+
+IMPMULTIFITEXPORT domino::TransformationCartesianProductSampler*
+ create_mapped_sampling_space(
+  domino::TransformationMappedDiscreteSet* full_smpl_space,
+  const Particles &asmb_anchor_points, const Particles &components,
+  const Ints &mapping, Float dist_t=10.);
+
+IMPMULTIFIT_END_NAMESPACE
+
+#endif /* IMPMULTIFIT_SAMPLING_SPACE_H */
