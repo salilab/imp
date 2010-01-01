2c4d6c654275397a57099f2209f71c6d9d60d065
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-01-01 01:49:06 +0000

    add needed flags for tests
    git-svn-id: https://svn.salilab.org/imp/trunk@4351 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/domino/test/test_global_minimum_3.py
+++ modules/domino/test/test_global_minimum_3.py
@@ -16,7 +16,20 @@ class DOMINOTests(IMP.test.TestCase):
     def test_inference_2(self):
         self.sampler.d_opt.set_sampling_space(self.sampler.discrete_sampler)
         combs=IMP.domino.CombStates()
-        self.sampler.d_opt.exhaustive_enumeration(combs)
+        self.sampler.d_opt.exhaustive_enumeration(combs,True)
+        #select the minimum one
+        min_comb=None
+        min_score=10000
+        for comb in combs:
+            if min_score>comb.get_total_score():
+                min_score =  comb.get_total_score()
+                min_comb=comb
+        self.assert_( abs(self.infered_score -min_score) < 0.1 , "the score of the minimum configuration as calculated by the inference is wrong " + str(self.infered_score) + " != " + str(min_score))
+
+    def test_inference_2(self):
+        self.sampler.d_opt.set_sampling_space(self.sampler.discrete_sampler)
+        combs=IMP.domino.CombStates()
+        self.sampler.d_opt.exhaustive_enumeration(combs,True,True)
         #select the minimum one
         min_comb=None
         min_score=10000
