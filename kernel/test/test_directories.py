91bac1d1897bfd5a92b8fff4dc5783ba19d23705
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-01-06 06:23:01 +0000

    fix exception type. Not sure why we are testing that anyway...
    git-svn-id: https://svn.salilab.org/imp/trunk@4436 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/test/test_directories.py
+++ kernel/test/test_directories.py
@@ -12,7 +12,7 @@ class DirectoriesTests(IMP.test.TestCase):
 
     def test_bad_data_directory(self):
         """get_data_directory should raise an error if it does not exist"""
-        self.assertRaises(IMP.IOException, IMP.get_data_path, "not/exist")
+        self.assertRaises(IMP.InternalException, IMP.get_data_path, "not/exist")
 
 if __name__ == '__main__':
     unittest.main()
