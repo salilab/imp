7478245dc0d6b20bd62b9cc938f1d547e6d42a1e
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-01-15 06:15:03 +0000

    Add a decorator to wrap particles that have a defined Lennard-Jones well depth.
    git-svn-id: https://svn.salilab.org/imp/trunk@4542 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/atom/include/LennardJones.h
@@ -0,0 +1,55 @@
+/**
+ *  \file atom/LennardJones.h
+ *  \brief A decorator for a particle that has a Lennard-Jones potential well.
+ *
+ *  Copyright 2007-2010 Sali Lab. All rights reserved.
+ *
+ */
+
+#ifndef IMPATOM_LENNARD_JONES_H
+#define IMPATOM_LENNARD_JONES_H
+
+#include "config.h"
+#include <IMP/core/internal/utility.h>
+#include <IMP/core/XYZR.h>
+
+IMPATOM_BEGIN_NAMESPACE
+
+//! A decorator for a particle that has a Lennard-Jones potential well.
+/** Such particles must be XYZR particles (they must have a position and
+    a radius) but need not be true atoms.
+    \ingroup helper
+    \ingroup decorators
+ */
+class IMPATOMEXPORT LennardJones: public IMP::core::XYZR
+{
+public:
+  IMP_DECORATOR(LennardJones, IMP::core::XYZR)
+
+  /** Create a decorator with the passed well depth.
+      The particle is assumed to already have x,y,z,r attributes.
+   */
+  static LennardJones setup_particle(Particle *p, Float well_depth=0) {
+    IMP_USAGE_CHECK(XYZR::particle_is_instance(p),
+                    "Particle must already be an XYZR particle",
+                    ValueException);
+    p->add_attribute(get_well_depth_key(), well_depth);
+    return LennardJones(p);
+  }
+
+  IMP_DECORATOR_GET_SET(well_depth, get_well_depth_key(), Float, Float);
+
+  //! Return true if the particle is an instance of a LennardJones
+  static bool particle_is_instance(Particle *p) {
+    return XYZR::particle_is_instance(p)
+           && p->has_attribute(get_well_depth_key());
+  }
+
+  static FloatKey get_well_depth_key();
+};
+
+IMP_OUTPUT_OPERATOR(LennardJones);
+
+IMPATOM_END_NAMESPACE
+
+#endif  /* IMPATOM_LENNARD_JONES_H */
---
+++ modules/atom/test/test_lennard_jones_decorator.py
@@ -0,0 +1,48 @@
+import unittest
+import IMP
+import IMP.test
+import IMP.core
+import IMP.atom
+import IMP.algebra
+
+def _make_test_decorator(m):
+    p = IMP.Particle(m)
+    v = IMP.algebra.Vector3D(1.0, 2.0, 3.0)
+    s = IMP.algebra.Sphere3D(v, 4.0)
+    IMP.core.XYZR.setup_particle(p, s)
+    return IMP.atom.LennardJones.setup_particle(p, 0.5)
+
+
+class LennardJonesDecoratorTests(IMP.test.TestCase):
+    """Test the LennardJones decorator"""
+
+    def test_create(self):
+        """Check creation of LennardJones decorators"""
+        m = IMP.Model()
+        p = IMP.Particle(m)
+        s = IMP.algebra.Sphere3D(IMP.algebra.Vector3D(1.0, 2.0, 3.0), 4.0)
+        # Can create decorators using well depth, only if the particle is
+        # already an XYZ
+        p = IMP.Particle(m)
+        IMP.core.XYZR.setup_particle(p, s)
+        IMP.atom.LennardJones.setup_particle(p, 0.5)
+        p = IMP.Particle(m)
+        self.assertRaises(IMP.UsageException,
+                          IMP.atom.LennardJones.setup_particle, p, 0.5)
+
+    def test_get_set(self):
+        """Check get/set methods of LennardJones decorators"""
+        m = IMP.Model()
+        c = _make_test_decorator(m)
+        self.assertInTolerance(c.get_well_depth(), 0.5, 1e-6)
+        c.set_well_depth(2.5)
+        self.assertInTolerance(c.get_well_depth(), 2.5, 1e-6)
+
+    def test_show(self):
+        """Check show method of LennardJones decorators"""
+        m = IMP.Model()
+        c = _make_test_decorator(m)
+        self.assert_(' well depth= ' in str(c))
+
+if __name__ == '__main__':
+    unittest.main()
