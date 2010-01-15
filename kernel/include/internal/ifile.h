3563ceeef0d136f2766800475068901684a5e2ce
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-01-15 00:29:18 +0000

    change file name to make the debugger happier and go back to throwing exceptions early as it seems to work, not sure why I thought it didn't
    git-svn-id: https://svn.salilab.org/imp/trunk@4537 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/include/internal/ifile.h
+++ kernel/include/internal/ifile.h
@@ -1,13 +1,13 @@
 /**
- *  \file internal/file.h
+ *  \file internal/ifile.h
  *  \brief Control display of deprecation information.
  *
  *  Copyright 2007-2010 Sali Lab. All rights reserved.
  *
  */
 
-#ifndef IMP_INTERNAL_FILE_H
-#define IMP_INTERNAL_FILE_H
+#ifndef IMP_INTERNAL_IFILE_H
+#define IMP_INTERNAL_IFILE_H
 
 #include "../config.h"
 #include "../RefCounted.h"
@@ -15,7 +15,7 @@
 #include <memory>
 
 IMP_BEGIN_INTERNAL_NAMESPACE
-template <class S, class FS>
+template <class S, class FS, int ZeroIsWrite>
 struct FileStorage: public RefCounted {
   mutable std::auto_ptr<FS> ptr_;
   OwnerPointer<Object> ref_;
@@ -24,7 +24,14 @@ struct FileStorage: public RefCounted {
                                   out_(*ptr_)
   {
     if (!out_) {
-      IMP_THROW("Unable to open file " << fname,
+      std::string verb;
+      if (ZeroIsWrite==0) {
+        verb="writing";
+      } else {
+        verb="reading";
+      }
+      IMP_THROW("Unable to open file " << fname
+                << " for " << verb << ".",
                 IOException);
     }
   }
@@ -37,11 +44,12 @@ struct FileStorage: public RefCounted {
 
 
 
-template <class Stream, class FStream>
+template <class Stream, class FStream, int ZeroIsWrite>
 class TextIO {
-  typedef internal::FileStorage<Stream, FStream> Data;
+  typedef internal::FileStorage<Stream, FStream, ZeroIsWrite> Data;
   Pointer<Data > data_;
-  typedef TextIO<Stream, FStream> This;
+  std::string name_;
+  typedef TextIO<Stream, FStream, ZeroIsWrite> This;
  public:
   template <class Proxy>
   TextIO(Proxy p) {
@@ -51,12 +59,12 @@ class TextIO {
     }
   }
   TextIO(){}
-  TextIO(std::string file_name) {
-    try {
+  TextIO(std::string file_name): name_(file_name) {
+    //try {
       data_=new Data(file_name);
-    } catch (IOException e) {
+      /*} catch (...) {
       IMP_WARN("Unable to open file \"" << file_name << "\"" << std::endl);
-    }
+      }*/
   }
   TextIO(Stream &str) {
     try {
@@ -64,10 +72,11 @@ class TextIO {
     } catch (IOException e) {
     }
   }
-  TextIO(const This& o): data_(o.data_){}
+  TextIO(const This& o): data_(o.data_), name_(o.name_){}
   IMP_COMPARISONS_1(data_);
   TextIO operator=(const TextIO &o) {
     data_=o.data_;
+    name_=o.name_;
     return *this;
   }
   operator Stream &() {
@@ -80,7 +89,19 @@ class TextIO {
     if (data_) {
       return data_->out_;
     } else {
-      IMP_THROW("Error writing to file.",
+      std::string verb;
+      if (ZeroIsWrite==0) {
+        verb="writing to";
+      } else {
+        verb="reading from";
+      }
+      std::string name;
+      if (!name_.empty()) {
+        name=name_;
+      } else {
+        name="stream";
+      }
+      IMP_THROW("IO error " << verb << " " << name,
                 IOException);
     }
   }
@@ -88,4 +109,4 @@ class TextIO {
 
 IMP_END_INTERNAL_NAMESPACE
 
-#endif /* IMP_INTERNAL_FILE_H */
+#endif /* IMP_INTERNAL_IFILE_H */
--- kernel/src/file.cpp
+++ kernel/src/file.cpp
@@ -15,15 +15,23 @@ TextOutput::TextOutput(const char *c): P(std::string(c)){}
 TextOutput::TextOutput(std::string c): P(c){}
 TextOutput::TextOutput(std::ostream &in):P(in){}
 TextOutput::TextOutput(TextProxy<std::ostream> out): P(out){}
-TextOutput::TextOutput(int) {}
-TextOutput::TextOutput(double) {}
+TextOutput::TextOutput(int) {
+  IMP_THROW("Wrong argument type", IOException);
+}
+TextOutput::TextOutput(double) {
+  IMP_THROW("Wrong argument type", IOException);
+}
 
 TextInput::TextInput(const char *c): P(std::string(c)){}
 TextInput::TextInput(std::string c): P(c){}
 TextInput::TextInput(std::istream &in):P(in){}
 TextInput::TextInput(TextProxy<std::istream> out): P(out){}
-TextInput::TextInput(int) {}
-TextInput::TextInput(double) {}
+TextInput::TextInput(int) {
+ IMP_THROW("Wrong argument type", IOException);
+}
+TextInput::TextInput(double) {
+  IMP_THROW("Wrong argument type", IOException);
+}
 
 
 
