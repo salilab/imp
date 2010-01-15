b6ab38a7db38f1c851fb124ca98ce99e48a75740
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-01-15 06:15:03 +0000

    Add a decorator to wrap particles that have a defined Lennard-Jones well depth.
    git-svn-id: https://svn.salilab.org/imp/trunk@4542 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/atom/src/LennardJones.cpp
@@ -0,0 +1,24 @@
+/**
+ *  \file atom/LennardJones.cpp
+ *  \brief A decorator for a particle that has a Lennard-Jones potential well.
+ *
+ *  Copyright 2007-2010 Sali Lab. All rights reserved.
+ *
+ */
+
+#include "IMP/atom/LennardJones.h"
+
+IMPATOM_BEGIN_NAMESPACE
+
+FloatKey LennardJones::get_well_depth_key() {
+  static FloatKey k("lennard_jones_well_depth");
+  return k;
+}
+
+void LennardJones::show(std::ostream &out) const
+{
+  XYZR::show(out);
+  out << " Lennard-Jones well depth= " << get_well_depth();
+}
+
+IMPATOM_END_NAMESPACE
