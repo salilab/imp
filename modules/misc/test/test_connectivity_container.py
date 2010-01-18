479d154d8cb934b196cdf810750efee64ccdbd6b
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-01-18 03:28:17 +0000

    tweak test case
    git-svn-id: https://svn.salilab.org/imp/trunk@4588 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/misc/test/test_connectivity_container.py
+++ modules/misc/test/test_connectivity_container.py
@@ -13,6 +13,8 @@ class TunnelTest(IMP.test.TestCase):
         IMP.set_log_level(IMP.VERBOSE)
         m= IMP.Model()
         ps= IMP.core.create_xyzr_particles(m, 10, .1)
+        for p in ps:
+            p.set_coordinates_are_optimized(True)
         lsc=IMP.core.ListSingletonContainer(ps)
         cpc=IMP.misc.ConnectingPairContainer(lsc, .1, False)
         #m.add_restraint(pr)
@@ -29,19 +31,26 @@ class TunnelTest(IMP.test.TestCase):
             #                 self.assert_(cpc.get_contains_particle_pair(ParticlePair(p0, p1))
             #                 or cpc.get_contains_particle_pair(ParticlePair(p1, p0)))
         cg= IMP.core.ConjugateGradients(m)
-        ub= IMP.core.HarmonicUpperBound(-.1, 1)
-        sd= IMP.core.SphereDistancePairScore(ub)
+        ub= IMP.core.HarmonicUpperBound(0, 1)
+        sd= IMP.core.DistancePairScore(ub)
         pr= IMP.core.PairsRestraint(sd, cpc)
         m.add_restraint(pr)
-        cg.set_threshold(.001)
+        print "added"
+        print pr.evaluate(False)
+        cg.set_threshold(.00001)
         cg.optimize(1000)
+        for pp in cpc.get_particle_pairs():
+            pp.show()
+            print
         for p0 in ps:
             for p1 in ps:
                 if p0 != p1:
-                    d0= IMP.core.XYZR(p0.get_particle())
-                    d1= IMP.core.XYZR(p1.get_particle())
+                    d0= IMP.core.XYZ(p0.get_particle())
+                    d1= IMP.core.XYZ(p1.get_particle())
+                    print d0
+                    print d1
+                    print IMP.ParticlePair(p0.get_particle(), p1.get_particle())
                     self.assert_(IMP.core.distance(d0, d1) <= 0.1)
-                    print str(p0)+" " +str(p1)
                     #self.assert_(cpc.get_contains_particle_pair(IMP.ParticlePair(p0.get_particle(), p1.get_particle()))
                                  #or cpc.get_contains_particle_pair(IMP.ParticlePair(p1.get_particle(), p0.get_particle())))
 
