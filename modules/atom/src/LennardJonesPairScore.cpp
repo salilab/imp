aa710a6a714a7f39a13a7100e0037499184ea72e
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-01-26 02:06:18 +0000

    Add a basic implementation of a Lennard-Jones potential.
    git-svn-id: https://svn.salilab.org/imp/trunk@4653 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/atom/src/LennardJonesPairScore.cpp
@@ -0,0 +1,54 @@
+/**
+ *  \file LennardJonesPairScore.cpp
+ *  \brief Lennard-Jones score between a pair of particles.
+ *
+ *  Copyright 2007-2010 Sali Lab. All rights reserved.
+ */
+
+#include <IMP/atom/LennardJonesPairScore.h>
+#include <IMP/atom/smoothing_functions.h>
+#include <IMP/atom/LennardJones.h>
+
+IMPATOM_BEGIN_NAMESPACE
+
+Float LennardJonesPairScore::evaluate(const ParticlePair &p,
+                                      DerivativeAccumulator *da) const
+{
+  LennardJones lj0(p[0]);
+  LennardJones lj1(p[1]);
+
+  algebra::Vector3D delta = lj0.get_coordinates() - lj1.get_coordinates();
+  double distsqr = delta.get_squared_magnitude();
+  double dist = std::sqrt(distsqr);
+  double dist6 = distsqr * distsqr * distsqr;
+  double dist12 = dist6 * dist6;
+
+  double A, B;
+  get_factors(lj0, lj1, A, B);
+  double repulsive = A / dist12;
+  double attractive = B / dist6;
+  double score = repulsive - attractive;
+
+  if (da) {
+    DerivativePair d =
+            (*smoothing_function_)(score,
+                                   (6.0 * attractive - 12.0 * repulsive) / dist,
+                                   dist);
+    algebra::Vector3D deriv = d.second * delta / dist;
+    lj0.add_to_derivatives(deriv, *da);
+    lj1.add_to_derivatives(-deriv, *da);
+    return d.first;
+  } else {
+    return (*smoothing_function_)(score, dist);
+  }
+}
+
+void LennardJonesPairScore::show(std::ostream &out) const
+{
+  out << "LennardJonesPairScore with attractive_weight "
+      << attractive_weight_ << ", repulsive weight " << repulsive_weight_
+      << " using ";
+  (*smoothing_function_).show(out);
+}
+
+IMPATOM_END_NAMESPACE
