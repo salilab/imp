a874e574432b1ef016cf679c946f75415ff4e3c9
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-01-26 02:06:18 +0000

    Add a basic implementation of a Lennard-Jones potential.
    git-svn-id: https://svn.salilab.org/imp/trunk@4653 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/atom/include/LennardJonesPairScore.h
@@ -0,0 +1,83 @@
+/**
+ *  \file LennardJonesPairScore.h
+ *  \brief Lennard-Jones score between a pair of particles.
+ *
+ *  Copyright 2007-2010 Sali Lab. All rights reserved.
+ */
+
+#ifndef IMPATOM_LENNARD_JONES_PAIR_SCORE_H
+#define IMPATOM_LENNARD_JONES_PAIR_SCORE_H
+
+#include "config.h"
+#include <IMP/PairScore.h>
+#include <IMP/Pointer.h>
+#include <IMP/atom/LennardJones.h>
+#include <IMP/atom/smoothing_functions.h>
+
+IMPATOM_BEGIN_NAMESPACE
+
+//! Lennard-Jones score between a pair of particles.
+/** The two particles in the pair must be LennardJones particles.
+    The form of the potential is \f[
+       \epsilon \left[ w_{rep} \left(\frac{r_{min}}{r}\right)^{12}
+                       - 2 w_{att} \left(\frac{r_{min}}{r}\right)^{6}\right]
+       \frac{1}{4\pi\epsilon_0\epsilon_r} \frac{q_i q_j}{|r_{ij}|}
+    \f] where \f$\epsilon\f$ is the depth of the well between the
+    two particles, \f$r_{min}\f$ the sum of the particles' radii, \f$r\f$
+    the interparticle distance, and \f$w_{rep}\f$ and \f$w_{att}\f$ the
+    weights on the repulsive and attractive parts of the potential respectively;
+    both weights are 1.0 by default.
+
+    The well depth is the geometric mean of the individual particles' well
+    depths (as extracted by LennardJones::get_well_depth).
+
+    Note that because this score uses radii and well depths set in the particles
+    themselves, the strength of the interaction cannot be changed for a
+    particular pair of atoms (as is done in the CHARMM forcefield with the
+    rarely-used NBFIX directive, for example). If the well depth or radius of
+    a single particle is modifed, that will affect its interaction with all
+    particles.
+ */
+class IMPATOMEXPORT LennardJonesPairScore : public PairScore
+{
+  IMP::internal::OwnerPointer<SmoothingFunction> smoothing_function_;
+  double repulsive_weight_, attractive_weight_;
+
+  // Calculate A, B factors from particle well depths and radii
+  // It may be appropriate to cache these for speed since the particle
+  // attributes rarely change and square roots are expensive
+  inline void get_factors(const LennardJones &lj0, const LennardJones &lj1,
+                          double &A, double &B) const {
+    double well_depth = std::sqrt(lj0.get_well_depth() * lj1.get_well_depth());
+    double rmin = lj0.get_radius() + lj1.get_radius();
+    // probably faster than pow(rmin, 6) on systems that don't
+    // have pow(double, int)
+    double rmin6 = rmin * rmin * rmin * rmin * rmin * rmin;
+    double rmin12 = rmin6 * rmin6;
+
+    A = well_depth * rmin12 * repulsive_weight_;
+    B = 2.0 * well_depth * rmin6 * attractive_weight_;
+  }
+
+public:
+  LennardJonesPairScore(SmoothingFunction *f)
+    : smoothing_function_(f), repulsive_weight_(1.0), attractive_weight_(1.0) {}
+
+  void set_repulsive_weight(double repulsive_weight) {
+    repulsive_weight_ = repulsive_weight;
+  }
+
+  double get_repulsive_weight() const { return repulsive_weight_; }
+
+  void set_attractive_weight(double attractive_weight) {
+    attractive_weight_ = attractive_weight;
+  }
+
+  double get_attractive_weight() const { return attractive_weight_; }
+
+  IMP_SIMPLE_PAIR_SCORE(LennardJonesPairScore, get_module_version_info())
+};
+
+IMPATOM_END_NAMESPACE
+
+#endif  /* IMPATOM_LENNARD_JONES_PAIR_SCORE_H */
---
+++ modules/atom/test/test_lennard_jones_pair_score.py
@@ -0,0 +1,111 @@
+import unittest
+import StringIO
+import random
+import math
+import IMP
+import IMP.test
+import IMP.atom
+import IMP.core
+
+from test_coulomb import place_xyzs
+
+def make_test_pair_score(min_distance=9.0, max_distance=10.0):
+    m = IMP.Model()
+    p0 = IMP.Particle(m)
+    sph = IMP.algebra.Sphere3D(IMP.algebra.Vector3D(0,0,0), 1.0)
+    IMP.core.XYZR.setup_particle(p0, sph)
+    d0 = IMP.atom.LennardJones.setup_particle(p0, 1.0)
+    p1 = IMP.Particle(m)
+    IMP.core.XYZR.setup_particle(p1, sph)
+    d1 = IMP.atom.LennardJones.setup_particle(p1, 1.0)
+    sm = IMP.atom.ForceSwitch(min_distance, max_distance);
+    c = IMP.atom.LennardJonesPairScore(sm)
+    r = IMP.core.PairRestraint(c, IMP.ParticlePair(p0, p1))
+    m.add_restraint(r)
+    return m, d0, d1, c
+
+class LennardJonesPairScoreTests(IMP.test.TestCase):
+    """Test the LennardJonesPairScore"""
+
+    def test_methods(self):
+        """Check LennardJonesPairScore methods"""
+        sm = IMP.atom.ForceSwitch(9.0, 10.0)
+        c = IMP.atom.LennardJonesPairScore(sm)
+        out = StringIO.StringIO()
+        c.show(out)
+        self.assert_('LennardJonesPairScore with attractive_weight' \
+                     in out.getvalue())
+
+    def test_get_set(self):
+        """Check LennardJonesPairScore get/set methods"""
+        sm = IMP.atom.ForceSwitch(9.0, 10.0)
+        c = IMP.atom.LennardJonesPairScore(sm)
+        self.assertEqual(c.get_repulsive_weight(), 1.0)
+        c.set_repulsive_weight(5.0)
+        self.assertEqual(c.get_repulsive_weight(), 5.0)
+        self.assertEqual(c.get_attractive_weight(), 1.0)
+        c.set_attractive_weight(10.0)
+        self.assertEqual(c.get_attractive_weight(), 10.0)
+
+    def test_value(self):
+        """Check score value of LennardJonesPairScore"""
+        m, d0, d1, c = make_test_pair_score()
+
+        box = IMP.algebra.Vector3D(10.0, 20.0, 30.0)
+        for r0 in (2.0, 1.0):
+            d0.set_radius(r0)
+            for r1 in (2.0, 1.0):
+                d1.set_radius(r1)
+                rmin = r0 + r1
+                for wd0 in (0.0, 1.0, 2.0):
+                    d0.set_well_depth(wd0)
+                    for wd1 in (0.0, 1.0, 2.0):
+                        d1.set_well_depth(wd1)
+                        wd = math.sqrt(wd0 * wd1)
+                        for att in (0.0, 0.5, 1.0):
+                            c.set_attractive_weight(att)
+                            for rep in (0.0, 0.5, 1.0):
+                                c.set_repulsive_weight(rep)
+                                for r in (3.0, 4.0, 5.0):
+                                    place_xyzs(d0, d1, box, r)
+                                    score = m.evaluate(False)
+                                    expected = wd * (rep * (rmin/r)**12
+                                                     - 2.0 * att * (rmin/r)**6)
+                                    self.assertInTolerance(score, expected,
+                                                           1e-2)
+
+    def test_derivatives(self):
+        """Check derivatives of LennardJonesPairScore"""
+        m, d0, d1, c = make_test_pair_score(4.0, 6.0)
+        # Place one particle at the origin and the other at a random position
+        # between 1 and 6 angstroms away (not too close since the derivatives
+        # are too large there)
+        d0.set_coordinates(IMP.algebra.Vector3D(0,0,0))
+        d1.set_coordinates(IMP.algebra.random_vector_on_unit_sphere() \
+                           * (random.random() * 5.0 + 1.0))
+        self.assertXYZDerivativesInTolerance(m, d0, 2.0)
+        self.assertXYZDerivativesInTolerance(m, d1, 2.0)
+
+    def test_smoothing(self):
+        """Check smoothing of LennardJonesPairScore"""
+        m, d0, d1, c = make_test_pair_score()
+        smm, smd0, smd1, smc = make_test_pair_score(min_distance=4.0,
+                                                    max_distance=5.0)
+        box = IMP.algebra.Vector3D(10.0, 20.0, 30.0)
+        def place_all(dist):
+            place_xyzs(d0, d1, box, dist)
+            smd0.set_coordinates(d0.get_coordinates())
+            smd1.set_coordinates(d1.get_coordinates())
+
+        # For dist <= min_distance, scores should be identical
+        for dist in (3.0, 3.5, 4.0):
+            place_all(dist)
+            self.assertInTolerance(m.evaluate(False), smm.evaluate(False), 1e-6)
+
+        # For dist > max_distance, smoothed score should be zero
+        place_all(5.5)
+        self.assertEqual(smm.evaluate(False), 0.0)
+        self.assertNotEqual(m.evaluate(False), 0.0)
+
+if __name__ == '__main__':
+    unittest.main()
