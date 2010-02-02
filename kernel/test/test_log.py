1f33e4cdfa51730bd318127f2bf485e4cb8d4802
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-02-02 07:50:00 +0000

    - Remove unnecessary prints. - Use more descriptive class and method names. - Make sure we run the test at a defined log level, since otherwise the   string comparison assertions may fail. - Add a test for the fix in r4695 (segfault when changing log targets at   log level MEMORY).
    git-svn-id: https://svn.salilab.org/imp/trunk@4696 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/test/test_log.py
+++ kernel/test/test_log.py
@@ -4,31 +4,40 @@ import IMP.test
 import sys
 from StringIO import StringIO
 
-class DirectoriesTests(IMP.test.TestCase):
+class LogTests(IMP.test.TestCase):
 
-    def test_data_directory(self):
+    def test_log_targets(self):
         """Test log targets"""
+        log_level = IMP.get_log_level()
+        # Make sure we are not logging at level MEMORY, since that will add
+        # extra logs (for ref/unref) and cause assertions to fail
+        IMP.set_log_level(IMP.VERBOSE)
         s= StringIO()
         t=IMP.SetLogTarget(s)
         IMP.log_write("Hey there\n")
         IMP.log_write("Big guy")
-        #IMP.set_log_target()
         del t
-        print s.getvalue()
-        print "del"
         self.assertEqual(s.getvalue(), "Hey there\nBig guy")
         del s
-        print "done del"
         IMP.log_write("what's up")
-        print "start again"
         s= StringIO()
         t= IMP.SetLogTarget(s)
-        print "write"
         IMP.log_write("Hey there\n")
         del s
-        print "post del write"
         IMP.log_write("Big guy")
-        #IMP.reset_log_target()
+        IMP.set_log_level(log_level)
+
+    def test_log_targets_memory(self):
+        """Test log targets at MEMORY log level"""
+        log_level = IMP.get_log_level()
+        IMP.set_log_level(IMP.MEMORY)
+        s= StringIO()
+        t=IMP.SetLogTarget(s)
+        # r4694 or earlier will crash here, since the 'Unrefing object' log
+        # message emitted at MEMORY log level will attempt to be displayed
+        # using the log target that is in the process of being freed
+        del t
+        IMP.set_log_level(log_level)
 
 if __name__ == '__main__':
     unittest.main()
