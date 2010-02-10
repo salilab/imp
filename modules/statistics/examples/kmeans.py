c6d8c02a0ba780bc3a4fe3d53097b376243ba180
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-02-10 01:04:25 +0000

    provide an example
    git-svn-id: https://svn.salilab.org/imp/trunk@4737 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/statistics/examples/kmeans.py
@@ -0,0 +1,21 @@
+import IMP.algebra
+import IMP.statistics
+
+# generate some clusters of points
+vs= IMP.algebra.Vector3Ds()
+centers=(IMP.algebra.Vector3D(0,0,0),
+         IMP.algebra.Vector3D(10,15,20),
+         IMP.algebra.Vector3D(60,30,12))
+for i in range(0,3):
+    for j in range(0,100):
+        vs.push_back(IMP.algebra.random_vector_in_sphere(centers[i], 10))
+
+# cluster them into 3 clusters
+e= IMP.statistics.Vector3DEmbedding(vs)
+c= IMP.statistics.get_lloyds_kmeans(e,
+                                    3, 1000)
+
+# print out the cluster results
+print c.get_cluster_center(0)
+print c.get_cluster_center(1)
+print c.get_cluster_center(2)
