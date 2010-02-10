3b68d4c1559e67ba12f0445366def323867acf1d
etjioe@SALILAB.ORG <etjioe@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-02-10 21:11:04 +0000

    Add get_rigid_body_by_id and get_all_rigid_bodies methods.
    git-svn-id: https://svn.salilab.org/imp/trunk@4757 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/restrainer/test/test_get_rigid_body.py
@@ -0,0 +1,46 @@
+import sys
+import os
+import unittest
+import IMP
+import IMP.test
+import IMP.restrainer
+
+def change_to_test_directory():
+    test_directory = os.environ.get('TEST_DIRECTORY')
+    if not test_directory:
+        test_directory = os.path.dirname(os.path.abspath(sys.argv[0]))
+    os.chdir(test_directory)
+
+class RestraintTest(IMP.test.TestCase):
+    def setUp(self):
+        """Place all data from XML files into the IMP Model"""
+
+        IMP.test.TestCase.setUp(self)
+
+        change_to_test_directory()
+
+        RepParser = IMP.restrainer.XMLRepresentation(self.get_input_file_name('pdb_representation.xml'))
+        RestraintParser = IMP.restrainer.XMLRestraint(self.get_input_file_name('pdb_connectivity_restraint.xml'))
+
+        self.representation = RepParser.run()
+        self.restraint = RestraintParser.run()
+
+        self.Model = self.representation.to_model()
+        self.restraint.add_to_representation(self.representation)
+
+
+    def test_show(self):
+        """Check get_rigid_body_by_id and get_all_rigid_bodies methods"""
+
+        rbd = self.restraint.get_rigid_body_by_id ("Protein1")
+        rbd.show()
+
+        rbs = self.restraint.get_all_rigid_bodies()
+        rbs[0].show()
+        self.assertEqual(len(rbs), 6)
+
+        self.Model.show()
+        self.Model.evaluate(False)
+
+if __name__ == '__main__':
+    unittest.main()
