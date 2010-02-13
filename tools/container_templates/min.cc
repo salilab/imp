3c2e96fba4994fd36a4d15176d383b065d602252
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-02-13 01:41:46 +0000

    remove unused header
    git-svn-id: https://svn.salilab.org/imp/trunk@4777 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- tools/container_templates/min.cc
+++ tools/container_templates/min.cc
@@ -12,7 +12,6 @@
 #include "IMP/core/MinMaxGroupnameRestraint.h"
 #include "IMP/core/internal/MinimalSet.h"
 #include <IMP/internal/container_helpers.h>
-#include <IMP/internal/utility.h>
 
 
 IMPCORE_BEGIN_NAMESPACE
