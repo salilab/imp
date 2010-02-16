8f56f29a605e10b3404fab88837ccde91b0b6b65
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-02-16 19:54:18 +0000

    clean up file stuff a bit
    git-svn-id: https://svn.salilab.org/imp/trunk@4838 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/test/test_file.py
+++ kernel/test/test_file.py
@@ -9,29 +9,29 @@ class DirectoriesTests(IMP.test.TestCase):
     def test_data_directory(self):
         """Test that conversions to input files work properly"""
         IMP.set_log_level(IMP.MEMORY)
-        v= IMP.test_ifile(self.get_input_file_name("text"))
+        v= IMP._test_ifile(self.get_input_file_name("text"))
         self.assertEqual(v, "word")
-        v= IMP.test_ifile(self.open_input_file("text"))
+        v= IMP._test_ifile(self.open_input_file("text"))
         self.assertEqual(v, "word")
-        self.assertRaises(TypeError, IMP.test_ifile, "notafile" )
+        self.assertRaises(TypeError, IMP._test_ifile, "notafile" )
         s=StringIO("hi there")
-        v= IMP.test_ifile(s)
+        v= IMP._test_ifile(s)
         self.assertEqual(v, "hithere")
 
     def test_odata_directory(self):
         """Test that conversions to output files work properly"""
         IMP.set_log_level(IMP.MEMORY)
-        IMP.test_ofile("ofile_test")
-        self.assertRaises(IOError, IMP.test_ofile, "nodir/hi" )
+        IMP._test_ofile("ofile_test")
+        self.assertRaises(IOError, IMP._test_ofile, "nodir/hi" )
         f= open("hi", "w")
-        IMP.test_ofile(f)
+        IMP._test_ofile(f)
         s = StringIO()
-        IMP.test_ofile(s)
+        IMP._test_ofile(s)
         self.assert_(s.getvalue().startswith("hi\n"))
-        self.assertRaises(TypeError, IMP.test_ofile, 1)
+        self.assertRaises(TypeError, IMP._test_ofile, 1)
         class NoMethods(object):
             pass
-        self.assertRaises(TypeError, IMP.test_ofile, NoMethods)
+        self.assertRaises(TypeError, IMP._test_ofile, NoMethods)
         del f
         os.unlink('ofile_test')
         os.unlink('hi')
