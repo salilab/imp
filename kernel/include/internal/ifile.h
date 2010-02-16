80d4b7a22ffac973d4c5a7252b8a8e8e5e590c75
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-02-16 19:54:18 +0000

    clean up file stuff a bit
    git-svn-id: https://svn.salilab.org/imp/trunk@4838 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/include/internal/ifile.h
+++ kernel/include/internal/ifile.h
@@ -21,61 +21,6 @@ struct IOStorage: public RefCounted {
   virtual ~IOStorage(){}
 };
 
-template <class BaseStream, class FileStream>
-struct LazyFileStorage: public IOStorage<BaseStream> {
-  std::string name_;
-  bool open_;
-  FileStream stream_;
-  LazyFileStorage(std::string name): name_(name), open_(false){}
-  BaseStream& get_stream() {
-    if (open_) {
-      stream_.open(name_.c_str());
-      if (!stream_) {
-        IMP_THROW("Unabe to open file " << name_,
-                  IOException);
-      }
-      open_=true;
-    }
-    return stream_;
-  }
-};
-
-template <class BaseStream, class FileStream>
-struct FileStorage: public IOStorage<BaseStream> {
-  FileStream stream_;
-  FileStorage(std::string name): stream_(name.c_str()){
-    if (!stream_) {
-      IMP_THROW("Unabe to open file " << name,
-                IOException);
-    }
-  }
-  BaseStream& get_stream() {
-    return stream_;
-  }
-};
-
-template <class BaseStream>
-struct StreamStorage: public IOStorage<BaseStream> {
-  BaseStream &stream_;
-  StreamStorage(BaseStream &stream): stream_(stream){}
-  BaseStream& get_stream() {
-    return stream_;
-  }
-};
-
-template <class BaseStream>
-struct OwnedStreamStorage: public IOStorage<BaseStream>{
-  BaseStream &stream_;
-  OwnerPointer<Object> ref_;
-  OwnedStreamStorage(BaseStream &stream,
-                     Object*o): stream_(stream),
-                                ref_(o){}
-  BaseStream& get_stream() {
-    return stream_;
-  }
-};
-
-
 
 IMP_END_INTERNAL_NAMESPACE
 
--- kernel/src/file.cpp
+++ kernel/src/file.cpp
@@ -8,17 +8,73 @@
 
 #include <IMP/file.h>
 
-
 IMP_BEGIN_NAMESPACE
 
+namespace {
+template <class BaseStream, class FileStream>
+struct LazyFileStorage: public internal::IOStorage<BaseStream> {
+  std::string name_;
+  bool open_;
+  FileStream stream_;
+  LazyFileStorage(std::string name): name_(name), open_(false){}
+  BaseStream& get_stream() {
+    if (!open_) {
+      stream_.open(name_.c_str());
+      if (!stream_) {
+        IMP_THROW("Unabe to open file " << name_,
+                  IOException);
+      }
+      open_=true;
+    }
+    return stream_;
+  }
+};
+
+template <class BaseStream, class FileStream>
+struct FileStorage: public internal::IOStorage<BaseStream> {
+  FileStream stream_;
+  FileStorage(std::string name): stream_(name.c_str()){
+    if (!stream_) {
+      IMP_THROW("Unabe to open file " << name,
+                IOException);
+    }
+  }
+  BaseStream& get_stream() {
+    return stream_;
+  }
+};
+
+template <class BaseStream>
+struct StreamStorage: public internal::IOStorage<BaseStream> {
+  BaseStream &stream_;
+  StreamStorage(BaseStream &stream): stream_(stream){}
+  BaseStream& get_stream() {
+    return stream_;
+  }
+};
+
+template <class BaseStream>
+struct OwnedStreamStorage: public internal::IOStorage<BaseStream>{
+  BaseStream &stream_;
+  internal::OwnerPointer<Object> ref_;
+  OwnedStreamStorage(BaseStream &stream,
+                     Object*o): stream_(stream),
+                                ref_(o){}
+  BaseStream& get_stream() {
+    return stream_;
+  }
+};
+
+}
+
 TextOutput::TextOutput(const char *c):
-  out_(new internal::LazyFileStorage<std::ostream, std::ofstream>(c)){}
+  out_(new LazyFileStorage<std::ostream, std::ofstream>(c)){}
 TextOutput::TextOutput(std::string c):
-  out_(new internal::LazyFileStorage<std::ostream, std::ofstream>(c)){}
+  out_(new LazyFileStorage<std::ostream, std::ofstream>(c)){}
 TextOutput::TextOutput(std::ostream &in):
-  out_(new internal::StreamStorage<std::ostream>(in)){}
+  out_(new StreamStorage<std::ostream>(in)){}
 TextOutput::TextOutput(TextProxy<std::ostream> out):
-  out_(new internal::OwnedStreamStorage<std::ostream>(*out.str_,
+  out_(new OwnedStreamStorage<std::ostream>(*out.str_,
                                                       out.ptr_)){}
 TextOutput::TextOutput(int) {
   IMP_THROW("Wrong argument type", IOException);
@@ -28,14 +84,14 @@ TextOutput::TextOutput(double) {
 }
 
 TextInput::TextInput(const char *c):
-  in_(new internal::FileStorage<std::istream, std::ifstream>(c)){}
+  in_(new FileStorage<std::istream, std::ifstream>(c)){}
 TextInput::TextInput(std::string c):
-  in_(new internal::FileStorage<std::istream, std::ifstream>(c)){}
+  in_(new FileStorage<std::istream, std::ifstream>(c)){}
 TextInput::TextInput(std::istream &in):
-  in_(new internal::StreamStorage<std::istream>(in)){}
+  in_(new StreamStorage<std::istream>(in)){}
 TextInput::TextInput(TextProxy<std::istream> out):
-  in_(new internal::OwnedStreamStorage<std::istream>(*out.str_,
-                                                     out.ptr_)){}
+  in_(new OwnedStreamStorage<std::istream>(*out.str_,
+                                           out.ptr_)){}
 TextInput::TextInput(int) {
  IMP_THROW("Wrong argument type", IOException);
 }
