6cde877f17af4b445b44007228cfcbbf8a2c6f16
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-02-16 00:25:37 +0000

    simplify textio support
    git-svn-id: https://svn.salilab.org/imp/trunk@4827 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/src/file.cpp
+++ kernel/src/file.cpp
@@ -11,10 +11,15 @@
 
 IMP_BEGIN_NAMESPACE
 
-TextOutput::TextOutput(const char *c): P(std::string(c)){}
-TextOutput::TextOutput(std::string c): P(c){}
-TextOutput::TextOutput(std::ostream &in):P(in){}
-TextOutput::TextOutput(TextProxy<std::ostream> out): P(out){}
+TextOutput::TextOutput(const char *c):
+  out_(new internal::LazyFileStorage<std::ostream, std::ofstream>(c)){}
+TextOutput::TextOutput(std::string c):
+  out_(new internal::LazyFileStorage<std::ostream, std::ofstream>(c)){}
+TextOutput::TextOutput(std::ostream &in):
+  out_(new internal::StreamStorage<std::ostream>(in)){}
+TextOutput::TextOutput(TextProxy<std::ostream> out):
+  out_(new internal::OwnedStreamStorage<std::ostream>(*out.str_,
+                                                      out.ptr_)){}
 TextOutput::TextOutput(int) {
   IMP_THROW("Wrong argument type", IOException);
 }
@@ -22,10 +27,15 @@ TextOutput::TextOutput(double) {
   IMP_THROW("Wrong argument type", IOException);
 }
 
-TextInput::TextInput(const char *c): P(std::string(c)){}
-TextInput::TextInput(std::string c): P(c){}
-TextInput::TextInput(std::istream &in):P(in){}
-TextInput::TextInput(TextProxy<std::istream> out): P(out){}
+TextInput::TextInput(const char *c):
+  in_(new internal::FileStorage<std::istream, std::ifstream>(c)){}
+TextInput::TextInput(std::string c):
+  in_(new internal::FileStorage<std::istream, std::ifstream>(c)){}
+TextInput::TextInput(std::istream &in):
+  in_(new internal::StreamStorage<std::istream>(in)){}
+TextInput::TextInput(TextProxy<std::istream> out):
+  in_(new internal::OwnedStreamStorage<std::istream>(*out.str_,
+                                                     out.ptr_)){}
 TextInput::TextInput(int) {
  IMP_THROW("Wrong argument type", IOException);
 }
