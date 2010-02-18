8c7013dd4a30cc954e3e153d173f3f89a5fb3fa3
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-02-18 18:24:51 +0000

    restore default behavior to skipping water and improve writing of heterogen residues
    git-svn-id: https://svn.salilab.org/imp/trunk@4897 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/test/test_pdb.py
+++ modules/atom/test/test_pdb.py
@@ -36,7 +36,6 @@ class PDBReadWriteTest(IMP.test.TestCase):
         IMP.atom.add_bonds(mp)
         bds = IMP.atom.get_internal_bonds(mp)
         self.assertEqual(bds.size(), 0)
-
     def test_read_het(self):
         """Check reading a pdb with one protein and a hetatm"""
         m = IMP.Model()
@@ -53,8 +52,15 @@ class PDBReadWriteTest(IMP.test.TestCase):
         #self.assertEqual(bds.size(), 1020)
         IMP.atom.add_radii(mp)
         IMP.atom.show_molecular_hierarchy(mp)
-
-
+    def test_read_non_water(self):
+        """Check that the default pdb reader skips waters"""
+        IMP.set_log_level(IMP.VERBOSE)
+        m= IMP.Model()
+        mp= IMP.atom.read_pdb(self.open_input_file("protein_water.pdb"),
+                              m)
+        a= IMP.atom.get_leaves(mp)
+        IMP.atom.write_pdb(mp, self.get_tmp_file_name("water_write.pdb"))
+        self.assertEqual(len(a), 13328)
     def test_write(self):
         """Simple test of writing a PDB"""
         m = IMP.Model()
