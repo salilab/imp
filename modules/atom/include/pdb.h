b983dccbd4813905f4d1f2b31e7c2780a93d8d1f
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-02-18 04:29:53 +0000

    move pdb selectors in PDB header and rename them to differentiate them from mol2 selectors
    git-svn-id: https://svn.salilab.org/imp/trunk@4882 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/include/pdb.h
+++ modules/atom/include/pdb.h
@@ -9,15 +9,173 @@
 #define IMPATOM_PDB_H
 
 #include "config.h"
-#include "selectors.h"
 #include "Hierarchy.h"
 #include "Atom.h"
 #include "element.h"
+#include "internal/pdb.h"
 #include <IMP/file.h>
 #include <IMP/Model.h>
 #include <IMP/Particle.h>
 
 IMPATOM_BEGIN_NAMESPACE
+
+
+//! Select which atoms to read from a PDB file
+/** Selector is a general purpose class used to select records from a PDB
+    file. Using descendants of this class one may implement arbitrary
+    selection functions with operator() and pass them to PDB reading functions
+    for object selection. Simple selectors can be used to build more complicated
+    ones. Inheritence means "AND" unless otherwise noted (that is, the
+    CAlphaPDBSelector takes all non-alternate C-alphas since it inherits from
+    NonAlternativePDBSelector).
+
+    PDBSelectors are designed to be temporary objects and should never be
+    stored.
+    \see read_pdb
+*/
+class IMPATOMEXPORT PDBSelector {
+ public:
+  //! Return true if the line should be processed
+  virtual bool operator()(const std::string& pdb_line) const=0;
+  virtual ~PDBSelector();
+};
+
+//! A PDB PDBSelector that ignores all alternative location atoms.
+class NonAlternativePDBSelector : public PDBSelector {
+ public:
+  bool operator()(const std::string& pdb_line) const {
+    return ((internal::atom_alt_loc_indicator(pdb_line) == ' ') ||
+            (internal::atom_alt_loc_indicator(pdb_line) == 'A'));
+  }
+};
+
+//! Defines a selector that will pick only C-alpha atoms.
+class CAlphaPDBSelector : public NonAlternativePDBSelector {
+ public:
+  bool operator() (const std::string& pdb_line) const {
+    if (!NonAlternativePDBSelector::operator()(pdb_line)) return false;
+    const std::string type = internal::atom_type(pdb_line);
+    return (type[1] == 'C' && type[2] == 'A' && type[3] == ' ');
+  }
+};
+
+//! Defines a selector that will pick only C-beta atoms.
+class CBetaPDBSelector: public NonAlternativePDBSelector {
+ public:
+  bool operator() (const std::string& pdb_line) const {
+    if (!NonAlternativePDBSelector::operator()(pdb_line)) return false;
+    const std::string type = internal::atom_type(pdb_line);
+    return (type[1] == 'C' && type[2] == 'B' && type[3] == ' ');
+  }
+};
+
+//! Defines a selector that will pick only C atoms. (not Ca or Cb)
+class CPDBSelector: public NonAlternativePDBSelector {
+ public:
+  bool operator()(const std::string& pdb_line) const {
+    if (!NonAlternativePDBSelector::operator()(pdb_line)) return false;
+    const std::string type = internal::atom_type(pdb_line);
+    return (type[1] == 'C' && type[2] == ' ' && type[3] == ' ');
+  }
+};
+
+//! Defines a selector that will pick only N atoms.
+class NPDBSelector: public NonAlternativePDBSelector {
+ public:
+  bool operator()(const std::string& pdb_line) const {
+    if (!NonAlternativePDBSelector::operator()(pdb_line)) return false;
+    const std::string type = internal::atom_type(pdb_line);
+    return (type[1] == 'N' && type[2] == ' ' && type[3] == ' ');
+  }
+};
+
+//! Defines a selector that will pick every atom.
+class AllPDBSelector : public PDBSelector {
+ public:
+  bool operator()(const std::string& pdb_line) const { return true; }
+};
+
+//! PDBSelector that picks atoms of given chains.
+class ChainPDBSelector : public NonAlternativePDBSelector {
+ public:
+  ChainPDBSelector(const std::string &chains): chains_(chains) {}
+  bool operator()(const std::string& pdb_line) const {
+    if (!NonAlternativePDBSelector::operator()(pdb_line)) return false;
+    for(int i=0; i < (int)chains_.length(); i++) {
+      if(internal::atom_chain_id(pdb_line) == chains_[i])
+        return true;
+    }
+    return false;
+  }
+ private:
+  std::string chains_;
+};
+
+//! PDBSelector that check if the line is water record
+class WaterPDBSelector : public NonAlternativePDBSelector {
+ public:
+  bool operator()(const std::string& pdb_line) const {
+    if (!NonAlternativePDBSelector::operator()(pdb_line)) return false;
+    const std::string res_name = internal::atom_residue_name(pdb_line);
+    return ((res_name[0]=='H' && res_name[1] =='O' && res_name[2]=='H') ||
+            (res_name[0]=='D' && res_name[1] =='O' && res_name[2]=='D'));
+  }
+};
+
+//! PDBSelector that check if the line is hydrogen record
+class HydrogenPDBSelector : public NonAlternativePDBSelector {
+ public:
+  bool operator()(const std::string& pdb_line) const {
+    if (!NonAlternativePDBSelector::operator()(pdb_line)) return false;
+    return (pdb_line[internal::atom_type_field_+1] == 'H' ||
+            pdb_line[internal::atom_type_field_+1] == 'D');
+  }
+};
+
+//! PDBSelector that picks non water and non hydrogen atoms
+class NonWaterNonHydrogenPDBSelector : public NonAlternativePDBSelector {
+ public:
+  bool operator()(const std::string& pdb_line) const {
+    if (!NonAlternativePDBSelector::operator()(pdb_line)) return false;
+    WaterPDBSelector w;
+    HydrogenPDBSelector h;
+    return (! w(pdb_line) && ! h(pdb_line));
+  }
+};
+
+//! PDBSelector that picks non water atoms
+class NonWaterPDBSelector : public PDBSelector {
+ public:
+  bool operator()(const std::string& pdb_line) const {
+    WaterPDBSelector w;
+    return( ! w(pdb_line));
+  }
+};
+
+//! A PDB PDBSelector that picks only Phosphate atoms.
+class PPDBSelector : public PDBSelector {
+ public:
+  bool operator()(const std::string& pdb_line) const {
+    const std::string type = internal::atom_type(pdb_line);
+    return (type[1] == 'P' && type[2] == ' ');
+  }
+};
+
+#if 0
+
+//! Only select lines liked by both selectors
+/** The two passed selectors are not deleted by this selector. */
+class AndPDBSelector: public PDBSelector {
+  const PDBSelector &a, &b;
+public:
+  AndPDBSelector(const PDBSelector &a,
+                 const PDBSelector &b): a_(a), b_(b) {
+  }
+  bool operator()(const std::string& pdb_line) const {
+    return a_(pdb_line)&& b_(pdb_line);
+  }
+};
+#endif
 
 /** @name PDB Reading
 
@@ -32,7 +190,7 @@ IMPATOM_BEGIN_NAMESPACE
     can produce warnings on files which are not valid. It will attempt to read
     such files, but all bets are off.
 
-    When reading PDBs, Selector objects can be used to choose to only process
+    When reading PDBs, PDBSelector objects can be used to choose to only process
     certain record types. See the class documentation for more information.
 
     Set the IMP::LogLevel to IMP::VERBOSE to see details of parse errors.
@@ -52,9 +210,8 @@ IMPATOMEXPORT Hierarchy read_pdb(TextInput in,
 IMPATOMEXPORT Hierarchy
 read_pdb(TextInput in,
          Model* model,
-         const Selector& selector,
-         bool select_first_model = true,
-         bool ignore_alternatives = true);
+         const PDBSelector& selector,
+         bool select_first_model = true);
 
 
 
@@ -62,8 +219,7 @@ read_pdb(TextInput in,
  */
 IMPATOMEXPORT Hierarchies read_multimodel_pdb(TextInput in,
                                               Model *model,
-                                              const Selector& selector,
-                                              bool ignore_alternatives=true);
+                                              const PDBSelector& selector);
 /** @} */
 
 /** @name PDB Writing
@@ -77,6 +233,9 @@ IMPATOMEXPORT Hierarchies read_multimodel_pdb(TextInput in,
     eg only ATOM/HETATM lines are printed for all Atom particles
     in the hierarchy. Complain if your favorite program can't read them and
     we might fix it.
+
+    When no PDB selector is supplied for reading, the
+    IgnoreAlternativesPDBSelector is used.
 */
 //!@{
 
@@ -95,6 +254,9 @@ IMPATOMEXPORT void write_multimodel_pdb(
                         const Hierarchies& mhd, TextOutput out);
 
 /** @} */
+
+
+#ifndef IMP_DOXYGEN
 
 /**
    This function returns a string in PDB ATOM format
@@ -116,6 +278,10 @@ IMPATOMEXPORT std::string pdb_string(const algebra::Vector3D& v,
   /note http://www.bmsc.washington.edu/CrystaLinks/man/pdb/guide2.2_frame.html
 */
 IMPATOMEXPORT std::string conect_record_string(int,int);
+#endif
+
+
+
 
 
 IMPATOM_END_NAMESPACE
