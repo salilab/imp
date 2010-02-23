44a03bfb5b39265b93c2b22af6d4c2398e27be8c
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-02-23 07:22:08 +0000

    change name to fit other classes and allow us to generalize the dimension later
    git-svn-id: https://svn.salilab.org/imp/trunk@4950 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/include/SpherePatch3D.h
+++ modules/algebra/include/SpherePatch3D.h
@@ -1,11 +1,11 @@
 /**
- *  \file Sphere3DPatch.h   \brief Simple 3D sphere patch class.
+ *  \file SpherePatch3D.h   \brief Simple 3D sphere patch class.
  *
  *  Copyright 2007-2010 Sali Lab. All rights reserved.
  *
  */
-#ifndef IMPALGEBRA_SPHERE_3DPATCH_H
-#define IMPALGEBRA_SPHERE_3DPATCH_H
+#ifndef IMPALGEBRA_SPHERE_PATCH_3D_H
+#define IMPALGEBRA_SPHERE_PATCH_3D_H
 
 #include "Sphere3D.h"
 #include "Plane3D.h"
@@ -17,33 +17,33 @@ A patch of a sphere is defined as all points above the plane and on the sphere.
    \ingroup uninitialized_default
    \ingroup noncomparable
  */
-class IMPALGEBRAEXPORT Sphere3DPatch {
+class IMPALGEBRAEXPORT SpherePatch3D {
 public:
-  Sphere3DPatch(){}
+  SpherePatch3D(){}
   //! Constructor
   /**
   /param[in] sph            the sphere to cut a patch from
   /param[in] crossing_plane the plane that crosses the sphere
   */
-  Sphere3DPatch(const Sphere3D &sph, const Plane3D& crossing_plane);
+  SpherePatch3D(const SphereD<3> &sph, const Plane3D& crossing_plane);
   //! Return true if the point is contained in the patch
   /** Note that the point must be on the sphere (this is not necessarily
       checked).
    */
-  bool get_contains(const Vector3D &p) const;
+  bool get_contains(const VectorD<3> &p) const;
   Plane3D  get_plane() const {return crossing_plane_;}
-  Sphere3D get_sphere() const {return Sphere3D(sph_);}
+  SphereD<3> get_sphere() const {return SphereD<3>(sph_);}
   void show(std::ostream &out=std::cout) const {
     sph_.show();
     crossing_plane_.show();
   }
   //! Get a point which is on the boundary of the patch
-  Vector3D get_boundary_point() const;
+  VectorD<3> get_boundary_point() const;
 protected:
-  Sphere3D sph_;
+  SphereD<3> sph_;
   Plane3D crossing_plane_;
 };
 
 IMPALGEBRA_END_NAMESPACE
 
-#endif  /* IMPALGEBRA_SPHERE_3DPATCH_H */
+#endif  /* IMPALGEBRA_SPHERE_PATCH_3D_H */
