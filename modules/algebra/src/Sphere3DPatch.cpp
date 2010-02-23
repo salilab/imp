a610b5ccbf238fb648b6dea418041747885add54
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-02-23 07:22:08 +0000

    change name to fit other classes and allow us to generalize the dimension later
    git-svn-id: https://svn.salilab.org/imp/trunk@4950 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/src/Sphere3DPatch.cpp
+++ modules/algebra/src/Sphere3DPatch.cpp
@@ -1,29 +1,29 @@
 /**
- *  \file Sphere3DPatch.cpp   \brief Simple 3D sphere patch class.
+ *  \file SpherePatch3D.cpp   \brief Simple 3D sphere patch class.
  *
  *  Copyright 2007-2010 Sali Lab. All rights reserved.
  *
  */
 
 #include <IMP/utility.h>
-#include "IMP/algebra/Sphere3DPatch.h"
+#include "IMP/algebra/SpherePatch3D.h"
 
 IMPALGEBRA_BEGIN_NAMESPACE
-Sphere3DPatch::Sphere3DPatch(const Sphere3D &sph,
+SpherePatch3D::SpherePatch3D(const SphereD<3> &sph,
   const Plane3D& crossing_plane) {
   crossing_plane_ = crossing_plane;
   sph_=sph;
 }
-bool Sphere3DPatch::get_contains(const Vector3D &p) const {
+bool SpherePatch3D::get_contains(const VectorD<3> &p) const {
   //check that the point is above the plane (the direction on the normal)
   return (crossing_plane_.get_is_above(p));
 }
-Vector3D Sphere3DPatch::get_boundary_point() const {
+VectorD<3> SpherePatch3D::get_boundary_point() const {
   //v - a vector on the plane
-  Vector3D
+  VectorD<3>
     v = orthogonal_vector(crossing_plane_.get_normal()).get_unit_vector();
-  Vector3D p = crossing_plane_.get_point_on_plane(); // a point on the plane
-  Vector3D cen = sph_.get_center();
+  VectorD<3> p = crossing_plane_.get_point_on_plane(); // a point on the plane
+  VectorD<3> cen = sph_.get_center();
   //a point q is both on the plane and on the sphere if (q-center)*(q-center)=R
   double c = -2.*cen*p+cen*cen+p*p-sph_.get_radius()*sph_.get_radius();
   double b = 2.*(p*v+cen*v);
