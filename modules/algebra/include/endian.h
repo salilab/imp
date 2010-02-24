89d76a3c97242774a858a452e85beafd29eca2e2
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-02-24 19:42:32 +0000

    make names more standard and provide grouping for documentation
    git-svn-id: https://svn.salilab.org/imp/trunk@5003 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/include/endian.h
+++ modules/algebra/include/endian.h
@@ -18,17 +18,20 @@
 
 IMPALGEBRA_BEGIN_NAMESPACE
 
+/** \name Endian
+    \imp provides a variety of functionality to manage byte order in
+    input and output data.
+    @{
+*/
+
 //! Reads from file in normal or reverse order
-/** \ingroup LittleBigEndian
- * If the reverse parameter is true, the data will be read in reverse order.
+/** If the reverse parameter is true, the data will be read in reverse order.
  */
 void IMPALGEBRAEXPORT reversed_read(void *dest, size_t size,
                     size_t nitems, std::ifstream& f, bool reverse);
 
 //! Writes to a file in normal or reversed order
 /**
- * \ingroup LittleBigEndian
- *
  * This function is the same as fread from C, but at the end there is a flag
  * saying if data should be read in reverse order or not.
  *
@@ -38,30 +41,22 @@ void IMPALGEBRAEXPORT reversed_write(const void* src,size_t size,size_t nitems,
               std::ofstream& f,bool reverse = false);
 
 
-
-IMP_NO_DOXYGEN(void IMPALGEBRAEXPORT byte_swap(unsigned char* b, int n);)
+#if !defined(SWIG) && !defined(IMP_DOXYGEN)
+void IMPALGEBRAEXPORT byte_swap(unsigned char* b, int n);
+#endif
 
 //! Conversion between little and big endian. Goes both ways
-/**
- * \ingroup LittleBigEndian
- */
 template <class T>
-inline  void convert_between_little_and_big_endian(T &x) {
+inline  void get_swapped_endian(T &x) {
   byte_swap((unsigned char*)& x,sizeof(T));
 }
 
 //! Returns 1 if machine is big endian else 0
-/**
- * \ingroup LittleBigEndian
- */
-bool IMPALGEBRAEXPORT is_big_endian();
+bool IMPALGEBRAEXPORT get_is_big_endian();
 
 //! Returns 1 if machine is little endian else 0
-/**
- * \ingroup LittleBigEndian
- */
-bool IMPALGEBRAEXPORT is_little_endian();
-
+bool IMPALGEBRAEXPORT get_is_little_endian();
+/** @} */
 
 IMPALGEBRA_END_NAMESPACE
 
