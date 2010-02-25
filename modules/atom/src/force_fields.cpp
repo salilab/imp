95e9e9dccc1fc4c6e00f73e09fd4806ba48a280f
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-02-25 20:19:29 +0000

    Keep the old add_radii interface working for SAXS.
    git-svn-id: https://svn.salilab.org/imp/trunk@5041 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/src/force_fields.cpp
+++ modules/atom/src/force_fields.cpp
@@ -6,6 +6,8 @@
  */
 
 #include <IMP/atom/force_fields.h>
+#include <IMP/atom/CharmmParameters.h>
+#include <IMP/atom/charmm_topology.h>
 
 IMPATOM_BEGIN_NAMESPACE
 
@@ -20,6 +22,13 @@ void add_bonds(Hierarchy d, const ForceFieldParameters* ffp)
 void add_radii(Hierarchy d, const ForceFieldParameters* ffp,
                 FloatKey radius_key)
 {
+  // Temporary hack to maintain old interface for SAXS
+  const CharmmParameters *cp = dynamic_cast<const CharmmParameters *>(ffp);
+  if (cp) {
+    IMP::Pointer<CHARMMTopology> top = cp->make_topology(d);
+    top->apply_default_patches(cp);
+    top->add_atom_types(d);
+  }
   ffp->add_radii(d, radius_key);
 }
 
