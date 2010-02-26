3652afdf52b0fccb4280f6a4facd5bf46291442f
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-02-26 03:18:31 +0000

    add an example on how to generate simulated maps
    git-svn-id: https://svn.salilab.org/imp/trunk@5060 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/em/examples/pdb2density.py
@@ -0,0 +1,16 @@
+import IMP.em
+import IMP.core
+import IMP.atom
+m= IMP.Model()
+#read protein
+sel=IMP.atom.NonWaterPDBSelector()
+mh=IMP.atom.read_pdb("input.pdb",m,sel)
+#add radius info to each atom, otherwise the resampling would fail.
+IMP.atom.add_radii(mh)
+ps= IMP.Particles(IMP.core.get_leaves(mh))
+#decide on resolution and spacing you would like to simulate to
+resolution=10.
+apix=1.5
+dmap=IMP.em.particles2density(ps,resolution,apix)
+#write out the map in the favorite format (xplor, mrc, em and spider are supported)
+IMP.em.write_map(dmap,"example.mrc",IMP.em.MRCReaderWriter())
