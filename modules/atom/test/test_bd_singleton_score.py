2e7d53cc958287e5e7ddcf759df5a58d4340f323
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-02-27 07:41:15 +0000

    Make sure that derivatives of the BondSingletonScore are correct.
    git-svn-id: https://svn.salilab.org/imp/trunk@5085 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/test/test_bd_singleton_score.py
+++ modules/atom/test/test_bd_singleton_score.py
@@ -4,6 +4,7 @@ import IMP.test
 import IMP.core
 import IMP.algebra
 import IMP.atom
+import random
 
 class DistanceTests(IMP.test.TestCase):
     """Test the symmetry restraint"""
@@ -30,6 +31,28 @@ class DistanceTests(IMP.test.TestCase):
         d0.set_coordinates(IMP.algebra.Vector3D(0,0,0))
         d1.set_coordinates(IMP.algebra.Vector3D(0,0,2))
         self.assertEqual(ss.evaluate(b.get_particle(), None), -2)
+
+    def test_bd_deriv(self):
+        """Test derivatives of the bond SingletonScore"""
+        m= IMP.Model()
+        p0= IMP.Particle(m)
+        p1= IMP.Particle(m)
+        d0= IMP.core.XYZ.setup_particle(p0)
+        d1= IMP.core.XYZ.setup_particle(p1)
+        b0= IMP.atom.Bonded.setup_particle(p0)
+        b1= IMP.atom.Bonded.setup_particle(p1)
+        b= IMP.atom.custom_bond(b0, b1, 1.0, 20.0)
+        ss= IMP.atom.BondSingletonScore(IMP.core.Harmonic(0,1))
+        r = IMP.core.SingletonRestraint(ss, b.get_particle())
+        m.add_restraint(r)
+
+        d0.set_coordinates(IMP.algebra.Vector3D(0,0,0))
+        d1.set_coordinates(
+           IMP.algebra.get_random_vector_on(IMP.algebra.get_unit_sphere_3d()) \
+           * (random.random() * 4.0 + 2.0))
+
+        self.assertXYZDerivativesInTolerance(m, d0, 2.0, 5.0)
+        self.assertXYZDerivativesInTolerance(m, d1, 2.0, 5.0)
 
 if __name__ == '__main__':
     unittest.main()
