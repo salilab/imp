9e3ca63db1a12dde546c5172505cf82eb491ba54
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-02-27 20:42:05 +0000

    assertAlmostEqual did not do what was expected of it (its third argument is number of decimal places). The test now fails though
    git-svn-id: https://svn.salilab.org/imp/trunk@5106 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/em/test/test_kernel_parameters.py
+++ modules/em/test/test_kernel_parameters.py
@@ -9,28 +9,28 @@ class KernelParametersTest(IMP.test.TestCase):
     """Class to test Gaussian kernel parameters"""
 
     def test_for_resolution_of_10(self):
-        self.assertAlmostEqual(self.kp.get_rsig(),4.24660921,0.001)
-        self.assertAlmostEqual(self.kp.get_rsig()*self.kp.get_rsig(),
+        self.assertInTolerance(self.kp.get_rsig(),4.24660921,0.001)
+        self.assertInTolerance(self.kp.get_rsig()*self.kp.get_rsig(),
                                self.kp.get_rsigsq(),0.001)
-        self.assertAlmostEqual(self.kp.get_timessig(),3,0.001)
-        self.assertAlmostEqual(1./self.kp.get_rsigsq(),
+        self.assertInTolerance(self.kp.get_timessig(),3,0.001)
+        self.assertInTolerance(1./self.kp.get_rsigsq(),
                                self.kp.get_inv_rsigsq(),0.001)
-        self.assertAlmostEqual(self.kp.get_sq2pi3(),1./math.sqrt(8.*math.pi*math.pi*math.pi))
-        self.assertAlmostEqual(self.kp.get_rnormfac(),self.kp.get_sq2pi3()/76.549,0.001)
-        self.assertAlmostEqual(self.kp.get_rkdist(),12.738,0.001)
-        self.assertAlmostEqual(self.kp.get_lim(),0.011108,0.001)
+        self.assertInTolerance(self.kp.get_sq2pi3(),1./math.sqrt(8.*math.pi*math.pi*math.pi))
+        self.assertInTolerance(self.kp.get_rnormfac(),self.kp.get_sq2pi3()/76.549,0.001)
+        self.assertInTolerance(self.kp.get_rkdist(),12.738,0.001)
+        self.assertInTolerance(self.kp.get_lim(),0.011108,0.001)
     def test_for_resolution_of_10_and_radius_of_1(self):
         rad=1.
         self.kp.set_params(rad)
         kp=self.kp.get_params(rad)
         print dir(kp)
         print type(kp)
-        self.assertAlmostEqual(kp.get_vsig(),1./math.sqrt(2)*rad,0.001)
-        self.assertAlmostEqual(kp.get_vsig()*kp.get_vsig(),
+        self.assertInTolerance(kp.get_vsig(),1./math.sqrt(2)*rad,0.001)
+        self.assertInTolerance(kp.get_vsig()*kp.get_vsig(),
                              kp.get_vsigsq(),0.001)
-        self.assertAlmostEqual(0.5/(kp.get_vsigsq()+self.kp.get_rsigsq()),
+        self.assertInTolerance(0.5/(kp.get_vsigsq()+self.kp.get_rsigsq()),
                                kp.get_inv_sigsq(),0.001)
-        self.assertAlmostEqual(kp.get_kdist(),12.738,0.001)
+        self.assertInTolerance(kp.get_kdist(),12.738,0.001)
     def test_get_parameters_by_radius(self):
         rad=[1.2,1.5,2.,2.2]
         for r in rad:
