4b86f57f9ca5ac6881a00bcefdbdbd24eededd3d
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-02-28 22:53:23 +0000

    mean -> ideal.
    git-svn-id: https://svn.salilab.org/imp/trunk@5120 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/test/test_charmm_parameters.py
+++ modules/atom/test/test_charmm_parameters.py
@@ -16,13 +16,13 @@ class CHARMMParametersTests(IMP.test.TestCase):
         for types in [('CT1', 'CT2'), ('CT2', 'CT1')]:
             bond = p.get_bond_parameters(*types)
             self.assertInTolerance(bond.force_constant, 222.500, 1e-4)
-            self.assertInTolerance(bond.mean, 1.5380, 1e-5)
+            self.assertInTolerance(bond.ideal, 1.5380, 1e-5)
 
         self.assertEqual(p.get_angle_parameters('garbage', 'CT2', 'CT3'), None)
         for types in [('OM', 'CM', 'FE'), ('FE', 'CM', 'OM')]:
             bond = p.get_angle_parameters(*types)
             self.assertInTolerance(bond.force_constant, 35.000, 1e-4)
-            self.assertInTolerance(bond.mean, 180.0000, 1e-5)
+            self.assertInTolerance(bond.ideal, 180.0000, 1e-5)
         self.assertEqual(p.get_angle_parameters('OM', 'FE', 'CM'), None)
 
         self.assertEqual(len(p.get_dihedral_parameters('garbage', 'C',
@@ -32,17 +32,17 @@ class CHARMMParametersTests(IMP.test.TestCase):
         self.assertEqual(len(bonds), 2)
         self.assertInTolerance(bonds[0].force_constant, 2.7500, 1e-4)
         self.assertEqual(bonds[0].multiplicity, 2)
-        self.assertInTolerance(bonds[0].mean, 180.00, 1e-5)
+        self.assertInTolerance(bonds[0].ideal, 180.00, 1e-5)
         self.assertInTolerance(bonds[1].force_constant, 0.3000, 1e-4)
         self.assertEqual(bonds[1].multiplicity, 4)
-        self.assertInTolerance(bonds[1].mean, 0.00, 1e-5)
+        self.assertInTolerance(bonds[1].ideal, 0.00, 1e-5)
 
         # Check wildcards
         bonds = p.get_dihedral_parameters('OM', 'CT1', 'NH3', 'FE')
         self.assertEqual(len(bonds), 1)
         self.assertInTolerance(bonds[0].force_constant, 0.1000, 1e-4)
         self.assertEqual(bonds[0].multiplicity, 3)
-        self.assertInTolerance(bonds[0].mean, 0.00, 1e-5)
+        self.assertInTolerance(bonds[0].ideal, 0.00, 1e-5)
 
         self.assertEqual(p.get_improper_parameters('garbage', 'C',
                                                    'CT2', 'CT3'), None)
@@ -51,14 +51,14 @@ class CHARMMParametersTests(IMP.test.TestCase):
             bond = p.get_improper_parameters(*types)
             self.assertInTolerance(bond.force_constant, 20.800, 1e-4)
             self.assertEqual(bond.multiplicity, 0)
-            self.assertInTolerance(bond.mean, 0.0000, 1e-5)
+            self.assertInTolerance(bond.ideal, 0.0000, 1e-5)
         # Check wildcards
         for types in [('CPB', 'CPA', 'NPH', 'C'),
                       ('CPB', 'CT2', 'CT3', 'C')]:
             bond = p.get_improper_parameters(*types)
             self.assertInTolerance(bond.force_constant, 90.000, 1e-4)
             self.assertEqual(bond.multiplicity, 0)
-            self.assertInTolerance(bond.mean, 0.0000, 1e-5)
+            self.assertInTolerance(bond.ideal, 0.0000, 1e-5)
         self.assertEqual(p.get_improper_parameters('NPH', 'CPA', 'CPB', 'CPA'),
                          None)
 
