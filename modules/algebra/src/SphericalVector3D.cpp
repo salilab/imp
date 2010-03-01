ce6dbcfc8f806727f1699dafd3cdc868703fe4ba
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-03-01 06:28:34 +0000

    remove unexported functions
    git-svn-id: https://svn.salilab.org/imp/trunk@5131 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/src/SphericalVector3D.cpp
+++ modules/algebra/src/SphericalVector3D.cpp
@@ -27,46 +27,5 @@ bool SphericalVector3D::check(double r, double theta, double psi) {
 }
 
 
-void quasi_evenly_spherical_distribution(unsigned long N,
-                              SphericalVector3Ds &vs,double r) {
-  vs.resize(N);
-  double theta,psi;
-  for (unsigned long k=1;k<=N;++k) {
-    double h = -1 + 2*((double)k-1)/((double)N-1);
-    theta = acos(h);
-    if( k==1 || k==N) {
-      psi=0;
-    } else {
-      psi=(vs[k-2][2] + 3.6/sqrt((double)N*(1-h*h)));
-      int div = static_cast<int>( psi / (2*PI) );
-      psi -= static_cast<double>(div)*(2*PI);
-    }
-    // Set the values the spherical vector
-    vs[k-1][0]=r;
-    vs[k-1][1]=theta;
-    vs[k-1][2]=psi;
-  }
-}
-
-void quasi_evenly_semi_spherical_distribution(unsigned long N,
-                              SphericalVector3Ds &vs,double r) {
-  vs.resize(N);
-  double theta,psi;
-  for (unsigned long k=1;k<=N;++k) {
-    double h = 2*((double)k-1)/(2*N-1) - 1.0;
-    theta = acos(h);
-    if(k==1) {
-      psi=0;
-    } else {
-      psi=(vs[k-2][2] + 3.6/sqrt((double)2*N*(1.0-h*h)));
-      int div = psi/(2*PI);
-      psi -= div*2*PI;
-    }
-    // Set the values the spherical vector
-    vs[k-1][0]=r;
-    vs[k-1][1]=theta;
-    vs[k-1][2]=psi;
-  }
-}
 
 IMPALGEBRA_END_NAMESPACE
