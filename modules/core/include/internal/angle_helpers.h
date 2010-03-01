af2f0b7239e113a9b554c4fe5397e2cbd2fa98ca
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-03-01 17:48:32 +0000

    Pull out calculation of angle and derivatives so it can be used by other classes.
    git-svn-id: https://svn.salilab.org/imp/trunk@5155 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/include/internal/angle_helpers.h
+++ modules/core/include/internal/angle_helpers.h
@@ -9,11 +9,60 @@
 #define IMPCORE_ANGLE_HELPERS_H
 
 #include "../config.h"
-#include "IMP/constants.h"
+#include "../XYZ.h"
+#include <IMP/constants.h>
+#include <IMP/algebra/Vector3D.h>
+#include <cmath>
 
 #include <cmath>
 
 IMPCORE_BEGIN_INTERNAL_NAMESPACE
+
+//! Calculate the angle between the given three XYZ particles.
+/** \return the angle.
+    If derv0 is non-NULL, all three algebra::VectorD<3> parameters are filled
+    in on return with the derivatives with respect to the XYZ particles.
+ */
+inline double angle(const XYZ &d0, const XYZ &d1, const XYZ &d2,
+                    algebra::VectorD<3> *derv0, algebra::VectorD<3> *derv1,
+                    algebra::VectorD<3> *derv2)
+{
+  algebra::VectorD<3> rij = d1.get_vector_to(d0);
+  algebra::VectorD<3> rkj = d1.get_vector_to(d2);
+
+  double scalar_product = rij*rkj;
+  double mag_rij = rij.get_magnitude();
+  double mag_rkj = rkj.get_magnitude();
+  double mag_product = mag_rij * mag_rkj;
+
+  // avoid division by zero
+  double cosangle = std::abs(mag_product) > 1e-12 ? scalar_product / mag_product
+                                                  : 0.0;
+
+  // avoid range error for acos
+  cosangle = std::max(std::min(cosangle, static_cast<double>(1.0)),
+                      static_cast<double>(-1.0));
+
+  double angle = std::acos(cosangle);
+
+  if (derv0) {
+    algebra::VectorD<3> unit_rij = rij.get_unit_vector();
+    algebra::VectorD<3> unit_rkj = rkj.get_unit_vector();
+
+    double sinangle = std::abs(std::sin(angle));
+
+    double fact_ij = sinangle * mag_rij;
+    double fact_kj = sinangle * mag_rkj;
+    // avoid division by zero
+    fact_ij = std::max(static_cast<double>(1e-12), fact_ij);
+    fact_kj = std::max(static_cast<double>(1e-12), fact_kj);
+
+    *derv0 = (unit_rij * cosangle - unit_rkj) / fact_ij;
+    *derv2 = (unit_rkj * cosangle - unit_rij) / fact_kj;
+    *derv1 = - (*derv0) - (*derv2);
+  }
+  return angle;
+}
 
 //! Return the difference between two angles
 /** Both angles, and the difference, are given in radians.
--- modules/core/src/AngleTripletScore.cpp
+++ modules/core/src/AngleTripletScore.cpp
@@ -7,6 +7,7 @@
 
 #include <IMP/core/AngleTripletScore.h>
 #include <IMP/core/XYZ.h>
+#include <IMP/core/internal/angle_helpers.h>
 #include <IMP/algebra/Vector3D.h>
 
 #include <IMP/UnaryFunction.h>
@@ -28,48 +29,19 @@ Float AngleTripletScore::evaluate(const ParticleTriplet &p,
   XYZ d1 = XYZ::decorate_particle(p[1]);
   XYZ d2 = XYZ::decorate_particle(p[2]);
 
-  algebra::VectorD<3> rij = d1.get_vector_to(d0);
-  algebra::VectorD<3> rkj = d1.get_vector_to(d2);
-
-  Float scalar_product = rij*rkj;
-  Float mag_rij = rij.get_magnitude();
-  Float mag_rkj = rkj.get_magnitude();
-  Float mag_product = mag_rij * mag_rkj;
-
-  // avoid division by zero
-  Float cosangle = std::abs(mag_product) > 1e-12 ? scalar_product / mag_product
-                                                 : 0.0;
-
-  // avoid range error for acos
-  cosangle = std::max(std::min(cosangle, static_cast<Float>(1.0)),
-                      static_cast<Float>(-1.0));
-
-  Float angle = std::acos(cosangle);
   Float score;
 
   if (da) {
+    algebra::VectorD<3> derv0, derv1, derv2;
+    double angle = internal::angle(d0, d1, d2, &derv0, &derv1, &derv2);
+
     Float deriv;
     boost::tie(score, deriv) = f_->evaluate_with_derivative(angle);
-
-    algebra::VectorD<3> unit_rij = rij.get_unit_vector();
-    algebra::VectorD<3> unit_rkj = rkj.get_unit_vector();
-
-    Float sinangle = std::abs(std::sin(angle));
-
-    Float fact_ij = sinangle * mag_rij;
-    Float fact_kj = sinangle * mag_rkj;
-    // avoid division by zero
-    fact_ij = std::max(static_cast<Float>(1e-12), fact_ij);
-    fact_kj = std::max(static_cast<Float>(1e-12), fact_kj);
-
-    algebra::VectorD<3> derv0 = deriv * (unit_rij * cosangle
-                                       - unit_rkj) / fact_ij;
-    algebra::VectorD<3> derv2 = deriv * (unit_rkj * cosangle
-                                       - unit_rij) / fact_kj;
-    d0.add_to_derivatives(derv0, *da);
-    d1.add_to_derivatives(-derv0 - derv2, *da);
-    d2.add_to_derivatives(derv2, *da);
+    d0.add_to_derivatives(derv0 * deriv, *da);
+    d1.add_to_derivatives(derv1 * deriv, *da);
+    d2.add_to_derivatives(derv2 * deriv, *da);
   } else {
+    double angle = internal::angle(d0, d1, d2, NULL, NULL, NULL);
     score = f_->evaluate(angle);
   }
   return score;
