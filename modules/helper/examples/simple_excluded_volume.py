0694a2e0fe54f4bd87fa18db9e76214c92dee95c
etjioe@SALILAB.ORG <etjioe@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-03-03 07:25:06 +0000

    Add helper examples.
    git-svn-id: https://svn.salilab.org/imp/trunk@5222 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/helper/examples/simple_excluded_volume.py
@@ -0,0 +1,30 @@
+#-- File: simple_excluded_volume.py --#
+
+import IMP
+import IMP.core
+import IMP.atom
+import IMP.helper
+
+m= IMP.Model()
+
+sel = IMP.atom.CAlphaPDBSelector()
+p0 = IMP.atom.read_pdb(
+         IMP.helper.get_example_path("input.pdb"), m, sel)
+p1 = IMP.atom.read_pdb(
+         IMP.helper.get_example_path("input.pdb"), m, sel)
+
+mhs = IMP.atom.Hierarchies()
+mhs.append(p0)
+mhs.append(p1)
+
+ps = IMP.Particles()
+for mh in mhs:
+    ps.append(mh.get_particle())
+
+rbs = IMP.helper.set_rigid_bodies(mhs)
+
+sev = IMP.helper.create_simple_excluded_volume_on_rigid_bodies(rbs)
+
+r = sev.get_restraint()
+
+m.add_restraint(r)
