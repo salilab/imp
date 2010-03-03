a4e17569eed5a8ddb9b6b7fd11d3d87d3bb2117d
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-03-03 23:24:38 +0000

    remove unused and underspecified method
    git-svn-id: https://svn.salilab.org/imp/trunk@5251 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/src/utility.cpp
+++ modules/core/src/utility.cpp
@@ -18,26 +18,4 @@ algebra::VectorD<3> get_centroid(const XYZsTemp &ps) {
  return cen/ps.size();
 }
 
-algebra::Segment3D get_diameter(const XYZsTemp &ps) {
-  IMP_USAGE_CHECK(!ps.empty(), "Can't compute the diameter if you don't give "
-            "me any particles.");
-  XYZ p1,p2;
-  double max_squared_dist=0.0;
-
-  for (XYZsTemp::const_iterator it = ps.begin();it != ps.end();it++){
-    algebra::VectorD<3> xyz1 = it->get_coordinates();
-    for (XYZsTemp::const_iterator it1 = it+1;it1 != ps.end();it1++){
-      algebra::VectorD<3> xyz2 = it1->get_coordinates();
-      double curr_squared_dist = algebra::get_squared_distance(xyz1,xyz2);
-      if (curr_squared_dist > max_squared_dist) {
-        p1 = *it;
-        p2 = *it1;
-        max_squared_dist = curr_squared_dist;
-      }
-    }
-  }
-  return algebra::Segment3D(p1.get_coordinates(),
-                            p2.get_coordinates());
-}
-
 IMPCORE_END_NAMESPACE
