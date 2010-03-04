b5964bb67d18e22792d2fd2f74a9329cccdfec96
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-03-04 21:28:28 +0000

    Don't use old, deprecated functions.
    git-svn-id: https://svn.salilab.org/imp/trunk@5313 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/em/test/test_derivs.py
+++ modules/em/test/test_derivs.py
@@ -11,6 +11,15 @@ import IMP.em
 import unittest
 import os
 
+def copy_to_modeller(env, particles):
+    fh = open("temp_particles.pdb", "w")
+    for i in range(len(particles)):
+        fh.write("ATOM  %5d  N   ALA     0       0.000   0.000   0.000  "
+                 "1.00  0.00           C  \n" % (i))
+    fh.close()
+    mdl = modeller.model(env, file='temp_particles.pdb')
+    os.unlink('temp_particles.pdb')
+    return mdl
 
 class DerivativesTest(IMP.test.TestCase):
     """check the agreement of numerical and analytical
@@ -28,14 +37,18 @@ class DerivativesTest(IMP.test.TestCase):
         #init IMP model ( the environment)
         self.imp_model = IMP.Model()
         self.particles = IMP.Particles()
+        ## -  create a set of three particles in imp
+        for i in range(3):
+            self.particles.append(IMP.Particle(self.imp_model))
+
         #add IMP Restraints into the modeller scoring function
         t = self.env.edat.energy_terms
         t.append(IMP.modeller.IMPRestraints(self.particles))
-        ## -  create a set of three particles in imp
-        npart = 3
-        self.modeller_model = IMP.modeller.create_particles(npart,
-                                  self.env, self.imp_model, self.particles)
-        # - add the particles attributes ( other than X,Y,Z)
+
+        # Load the same model into Modeller
+        self.modeller_model = copy_to_modeller(self.env, self.particles)
+
+        # - add the particles attributes
         rad = 1.0
         wei = 1.0
         rad_key=IMP.FloatKey("radius")
@@ -48,17 +61,15 @@ class DerivativesTest(IMP.test.TestCase):
 
         for i,p_data in enumerate([[9.0,9.0,9.0,rad,wei,1],[12.0,3.0,3.0,rad,wei,1],[3.0,12.0,12.0,rad,wei,1]]):
             p=self.particles[i]
-            p.set_value(x_key,p_data[0])
-            p.set_value(y_key,p_data[1])
-            p.set_value(z_key,p_data[2])
+            p.add_attribute(x_key,p_data[0])
+            p.add_attribute(y_key,p_data[1])
+            p.add_attribute(z_key,p_data[2])
             p.add_attribute(rad_key,p_data[3])
             p.add_attribute(wei_key,p_data[4])
             p.add_attribute(prot_key,p_data[5])
             self.particles[i].add_attribute(id_key,i)
 
 
-        IMP.modeller.copy_imp_coords_to_modeller(self.particles,self.modeller_model.atoms)
-        #modeller_model.write(file='xxx.pdb')
         self.atmsel = modeller.selection(self.modeller_model)
         print "initialization done ..."
 
