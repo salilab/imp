f8762c49bea6f1800fb7decacfa1bc1f81d08176
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-03-04 21:36:50 +0000

    Add example for using IMP restraints in Modeller.
    git-svn-id: https://svn.salilab.org/imp/trunk@5314 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/modeller/examples/imp_restraints_in_modeller.py
@@ -0,0 +1,32 @@
+import modeller
+import IMP
+import IMP.core
+import IMP.modeller
+
+# Set up Modeller and build a model from the GGCC primary sequence
+e = modeller.environ()
+e.edat.dynamic_sphere = False
+e.libs.topology.read('${LIB}/top_heav.lib')
+e.libs.parameters.read('${LIB}/par.lib')
+modmodel = modeller.model(e)
+modmodel.build_sequence('GGCC')
+
+# Set up IMP and load the Modeller model in as a new Hierarchy
+m = IMP.Model()
+protein = IMP.modeller.ModelLoader(modmodel).load_atoms(m)
+
+# Create a simple IMP distance restraint between the first and last atoms
+atoms = IMP.atom.get_by_type(protein, IMP.atom.ATOM_TYPE)
+r = IMP.core.DistanceRestraint(IMP.core.Harmonic(10.0, 1.0),
+                               atoms[0].get_particle(),
+                               atoms[-1].get_particle())
+m.add_restraint(r)
+
+# Use the IMPRestraints class to add all of the IMP restraints to the
+# Modeller scoring function
+t = modmodel.env.edat.energy_terms
+t.append(IMP.modeller.IMPRestraints(atoms.get_particles()))
+
+# Calculate the Modeller energy (score) for the whole protein
+sel = modeller.selection(modmodel)
+sel.energy()
