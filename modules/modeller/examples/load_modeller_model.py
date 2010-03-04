c439ac9232bb304961687b33210bde2cd9d644f0
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-03-04 22:19:35 +0000

    Add example for reading Modeller models and restraints.
    git-svn-id: https://svn.salilab.org/imp/trunk@5322 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/modeller/examples/load_modeller_model.py
@@ -0,0 +1,31 @@
+import modeller
+import IMP
+import IMP.modeller
+
+# Set up Modeller and build a model from the GGCC primary sequence
+e = modeller.environ()
+e.edat.dynamic_sphere = True
+e.libs.topology.read('${LIB}/top_heav.lib')
+e.libs.parameters.read('${LIB}/par.lib')
+modmodel = modeller.model(e)
+modmodel.build_sequence('GGCC')
+# Generate Modeller stereochemistry
+sel = modeller.selection(modmodel)
+modmodel.restraints.make(sel, restraint_type='STEREO', spline_on_site=False)
+
+# Set up IMP and use the ModelLoader class to load the atom coordinates
+# from Modeller into IMP as a new Hierarchy
+m = IMP.Model()
+loader = IMP.modeller.ModelLoader(modmodel)
+protein = loader.load_atoms(m)
+
+# Load each Modeller static restraint in as an IMP Restraint
+for r in loader.load_static_restraints():
+    m.add_restraint(r)
+
+# Load each Modeller dynamic restraint (soft-sphere in this case) in as an
+# equivalent IMP Restraint
+for r in loader.load_dynamic_restraints():
+    m.add_restraint(r)
+
+print m.evaluate(False)
