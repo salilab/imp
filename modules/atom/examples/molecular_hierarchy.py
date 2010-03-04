99c493db70f905508aa856fd1faf89ff0482a54f
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-03-04 02:08:56 +0000

    make example more interesting
    git-svn-id: https://svn.salilab.org/imp/trunk@5269 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/examples/molecular_hierarchy.py
+++ modules/atom/examples/molecular_hierarchy.py
@@ -4,30 +4,33 @@ import IMP.atom
 
 m = IMP.Model()
 mp0= IMP.atom.read_pdb(IMP.atom.get_example_path('example_protein.pdb'), m)
-# get the 16th residue
-chain= mp0.get_child(0)
+# get the 16th residue of the first chain
+hchain= IMP.atom.get_by_type(mp0, IMP.atom.CHAIN_TYPE)[0]
+# decorate the chain particle with an IMP.atom.Chain decorator.
+# unfortunately, our python wrapper does not handle converseions properly
+# as a result you have to manually get the particle for that chain
+chain=IMP.atom.Chain(hchain.get_particle())
 r16 = IMP.atom.get_residue(chain, 16)
 r16.show()
 
+# get all the atoms
 atoms= IMP.atom.get_by_type(mp0, IMP.atom.ATOM_TYPE)
-print atoms
-aa=IMP.atom.Hierarchies()
-print aa
-print len(atoms)
+# I didn't really have anything interesting to do with them...
 
-#get the residues
-residues= IMP.atom.get_by_type(mp0, IMP.atom.RESIDUE_TYPE)
-# set the coordinates and radius of each residue to enclose its atoms
-for r in residues:
-    d= IMP.core.XYZR.setup_particle(r.get_particle())
-    atoms= IMP.atom.get_by_type(IMP.atom.Hierarchy(r),
-                                IMP.atom.ATOM_TYPE)
-    IMP.core.set_enclosing_sphere(d, IMP.core.XYZs(atoms))
+# create a new version of the protein that is coarsened (one particle per residue)
+smp0= IMP.atom.create_simplified_along_backbone(chain, 1)
+
+# we don't need mp0 any more
+IMP.atom.destroy(mp0)
 
 # load another copy
-mp1= IMP.atom.read_pdb(IMP. atom.get_example_path('example_protein.pdb'), m)
+mp1= IMP.atom.read_pdb(IMP.atom.get_example_path('example_protein.pdb'), m)
 
+# make this one rigid
+IMP.atom.setup_as_rigid_body(mp1)
+
+# create a hierarchy which contains the two proteins
 p = IMP.Particle(m)
 rmp= IMP.atom.Hierarchy.setup_particle(p)
-rmp.add_child(mp0)
+rmp.add_child(smp0)
 rmp.add_child(mp1)
