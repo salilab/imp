05e299d4d357ab820e413c84e46c2f4f3992e2ce
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-03-04 21:59:17 +0000

    Fix and test the ModellerRestraints class.
    git-svn-id: https://svn.salilab.org/imp/trunk@5316 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/modeller/test/test_modeller_restraints.py
@@ -0,0 +1,35 @@
+import unittest
+import modeller
+import IMP
+import IMP.test
+import IMP.core
+import IMP.modeller
+from test_rsr_file_read import assertSimilarModellerIMPScores
+
+class ModellerRestraintsTests(IMP.test.TestCase):
+    """Check using Modeller restraints in IMP"""
+
+    def test_modeller_restraints(self):
+        """Check using Modeller restraints in IMP"""
+        e = modeller.environ()
+        e.edat.dynamic_sphere = False
+        e.libs.topology.read('${LIB}/top_heav.lib')
+        e.libs.parameters.read('${LIB}/par.lib')
+        modmodel = modeller.model(e)
+        modmodel.build_sequence('GGCC')
+        feat = modeller.features.distance(modmodel.atoms[0], modmodel.atoms[-1])
+        r = modeller.forms.gaussian(feature=feat, mean=10.0, stdev=1.0,
+                                    group=modeller.physical.xy_distance)
+        modmodel.restraints.add(r)
+
+        m = IMP.Model()
+        protein = IMP.modeller.ModelLoader(modmodel).load_atoms(m)
+        atoms = IMP.atom.get_by_type(protein, IMP.atom.ATOM_TYPE)
+        m.add_restraint(IMP.modeller.ModellerRestraints(m, modmodel,
+                                                        atoms.get_particles()))
+
+        assertSimilarModellerIMPScores(self, modmodel, protein)
+        self.assertInTolerance(m.evaluate(False), 5.7837, 1e-3)
+
+if __name__ == '__main__':
+    unittest.main()
