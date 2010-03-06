0969a0e889000515689c9478680a9f71d468e36c
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-03-06 01:27:49 +0000

    Add methods to fix tests.
    git-svn-id: https://svn.salilab.org/imp/trunk@5383 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/test/test_plural.py
+++ modules/algebra/test/test_plural.py
@@ -2,13 +2,38 @@ import unittest
 import IMP.algebra
 import IMP.test
 
-class DirectoriesTests(IMP.test.TestCase):
+class PluralTests(IMP.test.TestCase):
 
-    def test_data_directory(self):
+    def assertPlural(self, module, objs):
+        not_found = []
+        for f in dir(module):
+            if f[0].upper()== f[0] and f[1].upper() != f[1]\
+                    and  "_" not in f and not f.endswith("_swigregister")\
+                    and f not in objs and not f.endswith("s")\
+                    and not f.endswith("Temp") and not f.endswith("Iterator")\
+                    and not f.endswith("Exception"):
+                if f+"s" not in dir(module):
+                    not_found.append(f)
+        self.assert_(len(not_found) == 0, str(not_found))
+
+    def assertShow(self, module, objs):
+        not_found = []
+        for f in dir(module):
+            if f[0].upper()== f[0] and f[1].upper() != f[1]\
+                    and  "_" not in f and not f.endswith("_swigregister")\
+                    and f not in objs and not f.endswith("s")\
+                    and not f.endswith("Temp") and not f.endswith("Iterator")\
+                    and not f.endswith("Exception"):
+                if not hasattr(getattr(module, f), 'show'):
+                    not_found.append(f)
+        self.assert_(len(not_found) == 0, str(not_found))
+
+
+    def test_plural(self):
         """Test all algebra objects have plural version"""
         return self.assertPlural(IMP.algebra, ['FixedXYZ', 'FixedZYZ', 'LinearFit', 'Matrix2D', 'Matrix3D', 'MultiArray3D', 'NearestNeighbor2D', 'NearestNeighbor3D', 'NearestNeighbor4D', 'ParabolicFit', 'BoundingBox2D', 'BoundingBox3D', 'BoundingBox4D'])
 
-    def test_2(self):
+    def test_show(self):
         """Test all algebra objects have show"""
         return self.assertShow(IMP.algebra, ['AxisAnglePair', 'Matrix3D', 'MultiArray3D', 'NearestNeighbor2D', 'NearestNeighbor3D', 'NearestNeighbor4D'])
 
