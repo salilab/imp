9442258d99ca03073b0254ba9150a3a2beb2cb0a
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-03-10 01:49:12 +0000

    replace the doxygen preprocessor with a simple one of our own to see if that makes the parsing more reliable
    git-svn-id: https://svn.salilab.org/imp/trunk@5496 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ doc/doxygen/preprocess.py
@@ -0,0 +1,80 @@
+#!/usr/bin/python
+import sys
+import re
+import StringIO
+import os
+import glob
+
+def handle_namespace(infile, outfile):
+    #print "filtering ", sys.argv[1]
+    txt= infile.read()
+    lines = txt.splitlines()
+    beg= re.compile("IMP[ABCDEFGHIJKLMNOPQRSTUVWXYZ]*_BEGIN_NAMESPACE")
+    end= re.compile("IMP[ABCDEFGHIJKLMNOPQRSTUVWXYZ]*_END_NAMESPACE")
+    export= re.compile("IMP[ABCDEFGHIJKLMNOPQRSTUVWXYZ_]*EXPORT")
+    merge=re.compile("\\\\\\\\$")
+    for l in lines:
+        #if l=="IMP_BEGIN_NAMESPACE":
+        #    print  >> outfile, "#1"
+        #    outfile.write("namespace IMP {\n")
+        #elif l=="IMP_END_NAMESPACE":
+        #    print  >> outfile, "#2"
+        #    outfile.write("}\n")
+        if beg.match(l):
+            #print "match 0"
+            #print  >> outfile, "#3"
+            mname= l[3:l.find("_")]
+            outfile.write("namespace IMP {\n")
+            if mname!="":
+                outfile.write("namespace"+ mname.lower()+ "{\n")
+        elif end.match(l):
+            #print "match 1"
+            #print  >> outfile, "#4"
+            outfile.write("}\n}\n")
+        elif merge.search(l):
+            #print>>sys.stderr, "merging ", l
+            outfile.write(l)
+        elif export.search(l):
+            #print "match 2"
+            #print  >> outfile, "#5"
+            mo= export.search(l)
+            #print "found", mo.start, mo.end
+            outfile.write(l[0:mo.start()])
+            outfile.write(l[mo.end():])
+            outfile.write("\n")
+        elif l.startswith("#include"):
+            #print  >> outfile, "#6"
+            pass
+        else:
+            #print  >> outfile, "#7"
+            #print "HITHERE"
+            outfile.write(l)
+            outfile.write("\n")
+
+def cleanup(infile):
+    txt= infile.read()
+    lines = txt.splitlines()
+    for l in lines:
+        if len(l)>0 and l[0]=="#":
+            pass
+        else:
+            print l.replace("##", "").replace("#", "")
+
+def main():
+    txt= open(sys.argv[1])
+    if sys.argv[1].endswith("macros.h"):
+        print txt
+    else:
+        pns= StringIO.StringIO()
+        defs= glob.glob('doc/doxygen/*.def')
+        macros=[]
+        for d in defs:
+            macros.append("-imacros")
+            macros.append(d)
+        handle_namespace(txt, pns)
+        open("/tmp/pns.h", "w").write(pns.getvalue())
+        os.system("cpp -C "+ " ".join(macros) +" /tmp/pns.h > /tmp/cppout")
+        cleanup(open("/tmp/cppout", "r"))
+
+if __name__ == '__main__':
+    main()
