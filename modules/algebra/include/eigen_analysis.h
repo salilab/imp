bf51c9513568f395a565b99fdeb5d482fe1f2b45
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-03-15 23:13:06 +0000

    add centroid to PrincipalComponentAnalysis clas
    git-svn-id: https://svn.salilab.org/imp/trunk@5516 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/include/eigen_analysis.h
+++ modules/algebra/include/eigen_analysis.h
@@ -18,21 +18,27 @@ IMPALGEBRA_BEGIN_NAMESPACE
 class IMPALGEBRAEXPORT PrincipalComponentAnalysis {
 public:
   PrincipalComponentAnalysis(){}
-  PrincipalComponentAnalysis(const VectorD<3> &pc1,const VectorD<3> &pc2,
-       const VectorD<3> &pc3,VectorD<3> values) : eigen_values_(values){
+  PrincipalComponentAnalysis(
+    const VectorD<3> &pc1,const VectorD<3> &pc2,
+    const VectorD<3> &pc3,VectorD<3> values,
+    Vector3D centroid) : eigen_values_(values), centroid_(centroid){
     eigen_vecs_.push_back(pc1);
     eigen_vecs_.push_back(pc2);
     eigen_vecs_.push_back(pc3);
   }
   void show(std::ostream& out = std::cout) const {
     out << "Eigen values: ";
-    eigen_values_.show();
+    eigen_values_.show(out);
     out<<std::endl<<"First eigen vector : ";
-    eigen_vecs_[0].show();
+    eigen_vecs_[0].show(out);
     out<<std::endl<<"Second eigen vector : ";
-    eigen_vecs_[1].show();
+    eigen_vecs_[1].show(out);
     out<<std::endl<<"Third eigen vector : ";
-    eigen_vecs_[2].show();
+    eigen_vecs_[2].show(out);
+    out<<std::endl<<"eigen values : ";
+    eigen_values_.show(out);
+    out<<std::endl<<"centroid : ";
+    centroid_.show(out);
   }
   VectorD<3> get_principal_component(unsigned int i) const {
     IMP_INTERNAL_CHECK(i>=0 && i<3, "index is not between 0, 1 or 2");
@@ -42,9 +48,11 @@ public:
     IMP_INTERNAL_CHECK(i>=0 && i<3, "index is not between 0, 1 or 2");
     return eigen_values_[i];
   }
+  inline Vector3D get_centroid() const {return centroid_;}
 protected:
   std::vector<VectorD<3> > eigen_vecs_;
   VectorD<3> eigen_values_;
+  Vector3D centroid_;
 };
 
 
