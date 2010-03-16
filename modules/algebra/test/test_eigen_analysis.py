7f02866d6105acd4b48f91ec7ed7c00e2fea1a96
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-03-16 21:39:23 +0000

    more testing for PCA
    git-svn-id: https://svn.salilab.org/imp/trunk@5534 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/test/test_eigen_analysis.py
+++ modules/algebra/test/test_eigen_analysis.py
@@ -47,5 +47,28 @@ class EigenAnalysisTests(IMP.test.TestCase):
         self.assert_(ed.get_principal_value(1)>ed.get_principal_value(2))
         self.assertInTolerance(IMP.algebra.get_distance(vs_mean,ed.get_centroid()),0.,.1)
 
+
+    def test_that_pca_is_invariante_to_rotation(self):
+        """Testing that the eigen values are invariante to rotation """
+        vs = []
+        bb= IMP.algebra.BoundingBox3D(
+              IMP.algebra.Vector3D(-100.,-100.,-100.),
+              IMP.algebra.Vector3D(100.,100.,100.))
+        for i in range(50):
+            vs.append(IMP.algebra.get_random_vector_in(bb))
+        vs_mean=IMP.algebra.Vector3D(0.,0.,0.)
+        for i in range(50):
+            vs_mean = vs_mean+vs[i]
+        vs_mean = vs_mean/50
+        ed1 = IMP.algebra.get_principal_components(vs)
+        rand_rot = IMP.algebra.get_random_rotation_3d()
+        rand_trans=IMP.algebra.Transformation3D(rand_rot,IMP.algebra.Vector3D(0.,0.,0.))
+        for i in range(50):
+            vs[i] = rand_trans.get_transformed(vs[i])
+        ed2 = IMP.algebra.get_principal_components(vs)
+        self.assertInTolerance(ed1.get_principal_value(0),ed2.get_principal_value(0),0.2)
+        self.assertInTolerance(ed1.get_principal_value(1),ed2.get_principal_value(1),0.2)
+        self.assertInTolerance(ed1.get_principal_value(2),ed2.get_principal_value(2),0.2)
+
 if __name__ == '__main__':
     unittest.main()
