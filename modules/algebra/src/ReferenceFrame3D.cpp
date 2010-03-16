1b153791577becfdd418290c700556bc95789e94
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-03-16 17:50:14 +0000

    add reference frame
    git-svn-id: https://svn.salilab.org/imp/trunk@5527 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/algebra/src/ReferenceFrame3D.cpp
@@ -0,0 +1,12 @@
+/**
+ *  \file Rotation3D.cpp   \brief Simple 3D rotation class.
+ *
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+ *
+ */
+
+#include "IMP/algebra/ReferenceFrame3D.h"
+
+IMPALGEBRA_BEGIN_NAMESPACE
+ReferenceFrame3D::~ReferenceFrame3D(){}
+IMPALGEBRA_END_NAMESPACE
