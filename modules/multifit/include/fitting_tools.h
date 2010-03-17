ff68de8bc490113117d21040ca42cdae1e326bdc
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-03-17 23:10:17 +0000

    missing files
    git-svn-id: https://svn.salilab.org/imp/trunk@5544 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/multifit/include/fitting_tools.h
@@ -0,0 +1,38 @@
+/**
+ *  \file fitting_tools.h
+ *  \brief tools for handling fitting records
+ *
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+ *
+ */
+
+#ifndef IMPMULTIFIT_FITTING_TOOLS_H
+#define IMPMULTIFIT_FITTING_TOOLS_H
+
+#include <IMP/algebra/Transformation3D.h>
+#include <IMP/base_types.h>
+#include <IMP/core/Hierarchy.h>
+#include <IMP/Object.h>
+#include <IMP/atom/Hierarchy.h>
+#include "multifit_config.h"
+#include "FittingSolutionRecord.h"
+IMPMULTIFIT_BEGIN_NAMESPACE
+
+//! prune solutions by distance to an anchor point
+/**
+\param[in] fit_sols initial fitting solutions
+\param[in] mh the molecule the fitting solutions apply for
+\param[in] ap anchor point for which the transformed mh (fit) should be
+              close to
+\param[in] dist all fits such that the distance between ap and the fit center
+                is smaller than dist will be included
+\param[out] the pruned fitting solutions
+ */
+IMPMULTIFITEXPORT FittingSolutionRecords get_close_to_point(
+  const FittingSolutionRecords &fit_sols,
+  atom::Hierarchy mh,
+  IMP::Particle *ap,
+  Float dist);
+
+IMPMULTIFIT_END_NAMESPACE
+#endif /* IMPMULTIFIT_FITTING_TOOLS_H */
