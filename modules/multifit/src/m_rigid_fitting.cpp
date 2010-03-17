f21ffa1cd97e4158a636980ef96eb1a5ed358758
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-03-17 22:30:26 +0000

    fast rigid fitting PCA based
    git-svn-id: https://svn.salilab.org/imp/trunk@5543 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/multifit/src/m_rigid_fitting.cpp
@@ -0,0 +1,85 @@
+/**
+ *  \file m_rigid_fitting.cpp
+ *  \brief Fast Rigid fitting functionalities
+ *
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+ *
+ */
+
+#include <IMP/multifit/m_rigid_fitting.h>
+#include <IMP/em/CoarseCC.h>
+#include <IMP/em/SampledDensityMap.h>
+#include <IMP/algebra/vector_generators.h>
+#include <IMP/SingletonModifier.h>
+#include <IMP/core/Transform.h>
+#include <IMP/atom/pdb.h>
+#include <IMP/algebra/geometric_alignment.h>
+#include <IMP/em/converters.h>
+#include <IMP/algebra/eigen_analysis.h>
+#include <IMP/algebra/ReferenceFrame3D.h>
+IMPMULTIFIT_BEGIN_NAMESPACE
+
+em::FittingSolutions pca_based_rigid_fitting(
+    Particles &ps, em::DensityMap *em_map,
+    Float threshold, bool refine_fit,
+    FloatKey rad_key, FloatKey wei_key) {
+  //find the pca of the density
+  algebra::Vector3Ds dens_vecs = em::density2vectors(*em_map,threshold);
+  algebra::PrincipalComponentAnalysis dens_pca =
+     algebra::get_principal_components(dens_vecs);
+  //find the pca of the protein
+  algebra::Vector3Ds ps_vecs;
+  core::XYZs ps_xyz =  core::XYZs(ps);
+  for (core::XYZs::iterator it = ps_xyz.begin(); it != ps_xyz.end(); it++) {
+    ps_vecs.push_back(it->get_coordinates());
+  }
+  algebra::PrincipalComponentAnalysis ps_pca =
+     algebra::get_principal_components(ps_vecs);
+  IMP_IF_LOG(IMP::VERBOSE) {
+    IMP_LOG(IMP::VERBOSE,"in pca_based_rigid_fitting, density PCA:"<<std::endl);
+    IMP_LOG_WRITE(IMP::VERBOSE,dens_pca.show());
+    IMP_LOG(IMP::VERBOSE,"particles PCA:"<<std::endl);
+    IMP_LOG_WRITE(IMP::VERBOSE,ps_pca.show());
+  }
+  // orient the protein to the pca of the density (18 options)
+  em::FittingSolutions fs;
+  for(int i1=0;i1<3;i1++) {
+    for(int i2=i1+1;i2<3;i2++) {
+         algebra::ReferenceFrame3D ps_rf(algebra::Transformation3D(
+         algebra::get_rotation_from_x_y_axes(
+            ps_pca.get_principal_component(i1),
+            ps_pca.get_principal_component(i2)),
+            ps_pca.get_centroid()));
+        for(int j1=0;j1<3;j1++) {
+          for(int j2=0;j2<3;j2++) {
+            if(j1 != j2) {
+             algebra::ReferenceFrame3D dens_rf(algebra::Transformation3D(
+                algebra::get_rotation_from_x_y_axes(
+                  dens_pca.get_principal_component(j1),
+                  dens_pca.get_principal_component(j2)),
+                  dens_pca.get_centroid()));
+              algebra::Transformation3D ps2dens =
+                get_transformation_from_first_to_second(ps_rf,dens_rf);
+              algebra::Transformation3D ps2dens_inv = ps2dens.get_inverse();
+              for(unsigned int xyz_i=0;xyz_i<ps_xyz.size();xyz_i++){
+                ps_xyz[xyz_i].set_coordinates(
+                  ps2dens.get_transformed(ps_xyz[xyz_i].get_coordinates()));
+              }
+              Float fit_score =
+                em::compute_fitting_score(ps,em_map,rad_key,wei_key);
+              for(unsigned int xyz_i=0;xyz_i<ps_xyz.size();xyz_i++){
+                ps_xyz[xyz_i].set_coordinates(
+                  ps2dens_inv.get_transformed(ps_xyz[xyz_i].get_coordinates()));
+              }
+              fs.add_solution(ps2dens,fit_score);
+            }//j1 != j2
+         }//j2
+       }//j1
+    }//i2
+  }//i1
+  fs.sort();
+  return fs;
+}
+
+
+IMPMULTIFIT_END_NAMESPACE
---
+++ modules/multifit/test/test_rigid_fitting_pca.py
@@ -0,0 +1,72 @@
+import unittest
+import sys,os
+import IMP
+import IMP.em
+import IMP.test
+import IMP.core
+import IMP.atom
+import IMP.multifit
+import IMP.helper
+
+
+class ProteinRigidFittingTest(IMP.test.TestCase):
+    """Class to test EM correlation restraint"""
+
+    def load_density_map(self):
+        mrw = IMP.em.MRCReaderWriter()
+        self.scene = IMP.em.read_map(self.get_input_file_name("1f7dA00_8.mrc"), mrw)
+        self.scene.get_header_writable().set_resolution(8.)
+        self.scene.update_voxel_size(1.5)
+    def load_protein(self,pdb_filename):
+        self.mp= IMP.atom.read_pdb(self.open_input_file(pdb_filename),
+                              self.imp_model, IMP.atom.NonWaterPDBSelector())
+        IMP.atom.add_radii(self.mp)
+        self.mp_ref= IMP.atom.read_pdb(self.open_input_file(pdb_filename),
+                              self.imp_model, IMP.atom.NonWaterPDBSelector())
+        IMP.atom.add_radii(self.mp_ref)
+
+        self.radius_key = IMP.core.XYZR.get_default_radius_key()
+        self.weight_key = IMP.atom.Mass.get_mass_key()
+        self.ps = IMP.Particles(IMP.core.get_leaves(self.mp))
+
+    def setUp(self):
+        """Build test model and optimizer"""
+        IMP.test.TestCase.setUp(self)
+        IMP.set_log_level(IMP.VERBOSE)
+        self.imp_model = IMP.Model()
+        self.load_density_map()
+        self.load_protein("1f7dA00.pdb")
+
+
+    def test_pca_based_rigid_fitting(self):
+        """test pca based fitting"""
+        #randomize protein placement
+        rand_t = IMP.algebra.Transformation3D(
+            IMP.algebra.get_random_rotation_3d(),
+            IMP.algebra.get_random_vector_in(
+              IMP.algebra.BoundingBox3D(
+                IMP.algebra.Vector3D(-10.,-10.,-10.),
+                IMP.algebra.Vector3D(10.,10.,10.))))
+        xyz=IMP.core.XYZsTemp(self.ps)
+        for x in xyz:
+            x.set_coordinates(rand_t.get_transformed(x.get_coordinates()))
+        xyz_ref=IMP.core.XYZsTemp(IMP.core.get_leaves(self.mp_ref))
+        #fit protein
+        fs = IMP.multifit.pca_based_rigid_fitting(self.ps,self.scene,0.15)
+        #check that the rmsd to the reference is low
+        self.assert_(fs.get_number_of_solutions()>0)
+        for i in range(fs.get_number_of_solutions()):
+            fit_t = fs.get_transformation(i)
+            fit_t_inv = fit_t.get_inverse()
+            for x in xyz:
+                x.set_coordinates(fit_t.get_transformed(x.get_coordinates()))
+            print "rmsd: ",IMP.atom.get_rmsd(xyz_ref,xyz), " score: ",fs.get_score(i)
+            # if i==0:
+            #     self.assertInTolerance(IMP.atom.get_rmsd(xyz_ref,xyz),0.,6.)
+            #     self.assertInTolerance(fs.get_score(i),1.,0.3)
+            for x in xyz:
+                x.set_coordinates(fit_t_inv.get_transformed(x.get_coordinates()))
+
+
+if __name__ == '__main__':
+    unittest.main()
