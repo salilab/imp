638f1e2351404eb8a1aa7d950603e2c8a0debccd
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-03-19 02:43:43 +0000

    add copy_map function
    git-svn-id: https://svn.salilab.org/imp/trunk@5548 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/em/src/DensityMap.cpp
+++ modules/em/src/DensityMap.cpp
@@ -845,5 +845,29 @@ DensityMap* get_resampled(DensityMap *in, double scaling) {
   ret->set_name(in->get_name() +" resampled");
   return ret.release();
 }
+void DensityMap::copy_map(const DensityMap &other) {
+  header_ = other.header_;
+  long size = get_number_of_voxels();
+  data_.reset(new emreal[size]);
+  std::copy(other.data_.get(), other.data_.get()+size, data_.get());
+  loc_calculated_ = other.loc_calculated_;
+  if (loc_calculated_) {
+    x_loc_.reset(new float[size]);
+    y_loc_.reset(new float[size]);
+    z_loc_.reset(new float[size]);
+    std::copy(other.x_loc_.get(), other.x_loc_.get()+size, x_loc_.get());
+    std::copy(other.y_loc_.get(), other.y_loc_.get()+size, y_loc_.get());
+    std::copy(other.z_loc_.get(), other.z_loc_.get()+size, z_loc_.get());
+  } else {
+    x_loc_.reset();
+    y_loc_.reset();
+    z_loc_.reset();
+  }
+
+  data_allocated_ = other.data_allocated_;
+  normalized_ = other.normalized_;
+  rms_calculated_ = other.rms_calculated_;
+
+}
 
 IMPEM_END_NAMESPACE
