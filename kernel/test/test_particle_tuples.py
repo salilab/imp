5b65b15f68c424fe4b7f2ffe02bec4f8a9c89de3
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-03-25 21:13:16 +0000

    Correct initialization of ParticleQuad objects, and test all ParticleTuple constructors and methods.
    git-svn-id: https://svn.salilab.org/imp/trunk@5563 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ kernel/test/test_particle_tuples.py
@@ -0,0 +1,51 @@
+import unittest
+import IMP
+import IMP.test
+
+class ParticleTupleTests(IMP.test.TestCase):
+    """Test ParticleTuples"""
+
+    def test_pairs(self):
+        """Test ParticlePair objects"""
+        m = IMP.Model()
+        p1 = IMP.Particle(m)
+        p2 = IMP.Particle(m)
+        p = IMP.ParticlePair(p1, p2)
+        self.assertEqual(p.get_dimension(), 2)
+        self.assertEqual(p[0], p1)
+        self.assertEqual(p[1], p2)
+        self.assertEqual(p[0], p.get(0))
+        self.assert_(isinstance(p.get_name(), str))
+
+    def test_triplets(self):
+        """Test ParticleTriplet objects"""
+        m = IMP.Model()
+        p1 = IMP.Particle(m)
+        p2 = IMP.Particle(m)
+        p3 = IMP.Particle(m)
+        p = IMP.ParticleTriplet(p1, p2, p3)
+        self.assertEqual(p.get_dimension(), 3)
+        self.assertEqual(p[0], p1)
+        self.assertEqual(p[1], p2)
+        self.assertEqual(p[2], p3)
+        self.assertEqual(p[0], p.get(0))
+        self.assert_(isinstance(p.get_name(), str))
+
+    def test_quads(self):
+        """Test ParticleQuad objects"""
+        m = IMP.Model()
+        p1 = IMP.Particle(m)
+        p2 = IMP.Particle(m)
+        p3 = IMP.Particle(m)
+        p4 = IMP.Particle(m)
+        p = IMP.ParticleQuad(p1, p2, p3, p4)
+        self.assertEqual(p.get_dimension(), 4)
+        self.assertEqual(p[0], p1)
+        self.assertEqual(p[1], p2)
+        self.assertEqual(p[2], p3)
+        self.assertEqual(p[3], p4)
+        self.assertEqual(p[0], p.get(0))
+        self.assert_(isinstance(p.get_name(), str))
+
+if __name__ == '__main__':
+    unittest.main()
