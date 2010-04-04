9c9d3f79eccd8c21bc0a5388d08ba751c4e29ba8
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-04-04 07:30:54 +0000

    add support for FittingSolutionRecords
    git-svn-id: https://svn.salilab.org/imp/trunk@5579 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/multifit/src/DataContainer.cpp
+++ modules/multifit/src/DataContainer.cpp
@@ -9,16 +9,20 @@
 #include <IMP/atom/force_fields.h>
 #include <IMP/atom/pdb.h>
 #include <IMP/helper/simplify_restraint.h>
+#include <IMP/multifit/fitting_solutions_reader_writer.h>
 IMPMULTIFIT_BEGIN_NAMESPACE
 DataContainer::DataContainer(const SettingsData &settings) {
   mdl_= new IMP::Model();
   IMP::atom::NonWaterPDBSelector sel;
   for(int i=0;i<settings.get_number_of_component_headers();i++) {
+    ComponentHeader comp = settings.get_component_header(i);
     atom::Hierarchy mh = atom::read_pdb(
-      settings.get_component_header(i).get_filename(),mdl_,sel);
+      comp.get_filename(),mdl_,sel);
     atom::add_radii(mh);
     atom::setup_as_rigid_body(mh);
     mhs_.push_back(mh);
+    recs_[mh.get_particle()]=
+      read_fitting_solutions(comp.get_transformations_fn().c_str());
   }
   AssemblyHeader asmb_h = settings.get_assembly_header();
   dens_ = helper::load_em_density_map(asmb_h.get_dens_fn().c_str(),
