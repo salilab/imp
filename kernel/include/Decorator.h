4acdc455068e066876951e29278478de888945d7
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-04-09 23:40:32 +0000

    get rid of decorator with traits class and use tag instead
    git-svn-id: https://svn.salilab.org/imp/trunk@5630 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/include/Decorator.h
+++ kernel/include/Decorator.h
@@ -14,7 +14,6 @@
 #include "Particle.h"
 #include "Constraint.h"
 #include "Model.h"
-#include "internal/IndexingIterator.h"
 
 IMP_BEGIN_NAMESPACE
 
@@ -89,7 +88,7 @@ lazily (at the time of the first use), and not be created as static
 variables. The reason for this is that initialized attribute keys result
 in space being allocated in decorators, even before they are used.\n\n
 Implementors should consult IMP::example::ExampleDecorator,
-IMP_DECORATOR(), IMP_DECORATOR_TRAITS(), IMP_DECORATOR_GET(),
+IMP_DECORATOR(), IMP_DECORATOR_WITH_TRAITS(), IMP_DECORATOR_GET(),
 IMP_DECORATOR_ARRAY_DECL().
 
 \advanceddoc Lists of decorators are reference counted even though the
@@ -108,22 +107,60 @@ class Decorator
 {
 private:
   Particle *particle_;
-#if !defined(SWIG) && !defined(IMP_DOXYGEN)
-  friend bool operator==(Decorator, Particle*);
-#endif
 protected:
   Decorator(Particle *p): particle_(p) {}
   Decorator() :particle_(NULL)
   {}
+  int compare(Particle *o) const {
+    if (o < particle_) return -1;
+    else if (o > particle_) return 1;
+    else return 0;
+  }
 public:
 #ifdef _MSC_VER
   // needed to get Particle in VectorOfRefCounted
   typedef Particle* ParticleP;
 #endif
-
-  IMP_NO_DOXYGEN(typedef Decorator This);
-
-  IMP_COMPARISONS_1(particle_);
+#ifndef IMP_DOXYGEN
+  bool __eq__(Particle* o) const {
+    return operator==(o);
+  }
+  bool __ne__(Particle* o) const {
+    return operator!=(o);
+  }
+  bool __lt__(Particle* o) const {
+    return operator<(o);
+  }
+  bool __gt__(Particle* o) const {
+    return operator>(o);
+  }
+  bool __ge__(Particle* o) const {
+    return operator>=(o);
+  }
+  bool __le__(Particle* o) const {
+    return operator<=(o);
+  }
+#ifndef SWIG
+  bool operator==(Particle* o) const {
+    return (compare(o) == 0);
+  }
+  bool operator!=(Particle* o) const {
+    return (compare(o) != 0);
+  }
+  bool operator<(Particle* o) const {
+    return (compare(o) <0);
+  }
+  bool operator>(Particle* o) const {
+    return (compare(o) > 0);
+  }
+  bool operator>=(Particle* o) const {
+    return !(compare(o) < 0);
+  }
+  bool operator<=(Particle* o) const {
+    return !(compare(o) > 0);
+  }
+#endif
+#endif
 
   /** \name Methods provided by the Decorator class
       The following methods are provided by the Decorator class.
@@ -212,8 +249,9 @@ public:
   IMP_NO_DOXYGEN(bool is_null() const {return !particle_;});
   IMP_NO_DOXYGEN(typedef void (Decorator::*bool_type)() const);
   IMP_NO_DOXYGEN(void safe_bool_function() const {});
+  IMP_NO_DOXYGEN(typedef boost::false_type DecoratorHasTraits);
 };
-
+#if 0
 
 /** Certain decorators require \quote{traits} to customize their behavior.
 These traits typically are used to allow one decorator class to provide
@@ -225,7 +263,7 @@ are equal.
 */
 template <class Base, class Traits>
 class DecoratorWithTraits: public Base {
-  typedef DecoratorWithTraits<Base,Traits> This;
+  typedef DecoratorWithTraits<Base,Traits> T;
   Traits traits_;
   int compare(const DecoratorWithTraits<Base, Traits> &o) const {
     if (Base::is_null() || o.is_null()){
@@ -244,270 +282,17 @@ protected:
   DecoratorWithTraits(Traits tr): traits_(tr){}
   DecoratorWithTraits(Particle *p, Traits tr): Base(p), traits_(tr){}
 public:
+  IMP_NO_DOXYGEN(typedef T This);
+  typedef Base DecoratorTraitsBase;
   typedef Traits DecoratorTraits;
   const Traits& get_decorator_traits() const {return traits_;}
+  IMP_NO_DOXYGEN(typedef boost::true_type DecoratorHasTraits);
   IMP_COMPARISONS;
 };
+#endif
 
 
 #ifndef IMP_DOXYGEN
-inline bool operator==(Decorator d, Particle *p) {
-  return d.particle_==p;
-}
-inline bool operator==(Particle *p, Decorator d) {
-  return d==p;
-}
-
-
-#if !defined(SWIG)
-#define IMP_DECORATORS_METHODS(test, on_add_decorator, on_add_particle, \
-                               swap)                                    \
-  struct Accessor {                                                     \
-  typedef Accessor This;                                                \
-  typedef WrappedDecorator result_type;                                 \
-  typedef unsigned int argument_type;                                   \
-  result_type operator()(argument_type i) const {                       \
-    return o_->operator[](i);                                           \
-  }                                                                     \
-  Accessor(ThisDecorators *pc): o_(pc){}                                \
-  Accessor(): o_(NULL){}                                                \
-  IMP_COMPARISONS_1(o_);                                                \
-private:                                                                \
-/* This should be ref counted, but swig memory management
-   is broken */                                                         \
-ThisDecorators* o_;                                                     \
-};                                                                      \
-void check(Particle *p) {                                               \
-  IMP_USAGE_CHECK(test,                                                 \
-                  "Particle \"" << (p)->get_name()                      \
-                  << "\" missing required attributes");                 \
-}                                                                       \
-template <class It>                                                     \
-void check(It b, It e) {                                                \
-  for (It c= b; c!= e; ++c) {                                           \
-    check(*c);                                                          \
-  }                                                                     \
-}                                                                       \
-public:                                                                 \
-typedef const WrappedDecorator const_reference;                         \
-typedef WrappedDecorator value_type;                                    \
-typedef Proxy reference;                                                \
-const ParticlesTemp &get_particles() const {return *this;}              \
-void push_back(WrappedDecorator d) {                                    \
-  on_add_decorator;                                                     \
-  typename ParentDecorators::value_type pd=d;                           \
-  ParentDecorators::push_back(pd);                                      \
-}                                                                       \
-void push_back(Particle *p) {                                           \
-  check(p);                                                             \
-  on_add_particle;                                                      \
-  typename ParentDecorators::value_type pd=WrappedDecorator(p);         \
-  ParentDecorators::push_back(pd);                                      \
-}                                                                       \
-void set(unsigned int i, WrappedDecorator d) {                          \
-  typename ParentDecorators::value_type pd=d;                           \
-  ParentDecorators::operator[](i)= pd;                                  \
-}                                                                       \
-WrappedDecorator back() const {                                         \
-  IMP_USAGE_CHECK(!ParentDecorators::empty(),                           \
-                  "Can't call back on empty Decorators");               \
-  return WrappedDecorator(ParentDecorators::back());                    \
-}                                                                       \
-WrappedDecorator front() const {                                        \
-  IMP_USAGE_CHECK(!ParentDecorators::empty(),                           \
-                  "Can't call front on empty Decorators");              \
-  return WrappedDecorator(ParentDecorators::front());                   \
-}                                                                       \
-typedef internal::IndexingIterator<Accessor> iterator;                  \
-typedef internal::IndexingIterator<Accessor> const_iterator;            \
-iterator begin() const {                                                \
-  return iterator(Accessor(const_cast<ThisDecorators*>(this)), 0);      \
-}                                                                       \
-iterator end() const {                                                  \
-  return iterator(Accessor(const_cast<ThisDecorators*>(this)),          \
-                  ParentDecorators::size());                            \
-}                                                                       \
-template <class It>                                                     \
-void insert(iterator loc, It b, It e) {                                 \
-  check(b,e);                                                           \
-  for (It c=b; c!= e; ++c) {                                            \
-    on_add_particle;                                                    \
-  }                                                                     \
-  ParentDecorators::insert(ParentDecorators::begin()+(loc-begin()),     \
-                           b, e);                                       \
-}                                                                       \
-void swap_with(ThisDecorators &o) {                                     \
-  swap;                                                                 \
-  ParentDecorators::swap_with(o);                                       \
-}                                                                       \
-
-#else
-#define IMP_DECORATORS_METHODS(test, on_add_decorator, on_add_particle, \
-                               swap)                                    \
-  public:                                                               \
-  const ParticlesTemp &get_particles() const;                           \
-  void push_back(WrappedDecorator d);                                   \
-  void push_back(Particle *p);                                          \
-  WrappedDecorator back() const;                                        \
-  WrappedDecorator front() const;
-
-#endif
-
-
-template <class WrappedDecorator, class ParentDecorators>
-class Decorators: public ParentDecorators {
-  typedef Decorators<WrappedDecorator, ParentDecorators> ThisDecorators;
-  struct Proxy: public WrappedDecorator {
-    typedef typename ParentDecorators::reference Ref;
-    Ref d_;
-    Proxy(Ref t):
-      WrappedDecorator(t), d_(t){
-    }
-    Proxy(Ref p, bool): WrappedDecorator(), d_(p){}
-    void operator=(WrappedDecorator v) {
-      WrappedDecorator::operator=(v);
-      d_=v;
-    }
-#ifdef _MSC_VER
-    // for VC, it can't otherwise figure out the conversion chain
-    operator Particle*() {
-      if (WrappedDecorator()==*this) return NULL;
-      else return WrappedDecorator::get_particle();
-    }
-#endif
-  };
-  Proxy get_proxy(unsigned int i) {
-    if (ParentDecorators::operator[](i)) {
-      return Proxy(ParentDecorators::operator[](i));
-    } else {
-      return Proxy(ParentDecorators::operator[](i), false);
-    }
-  }
-
-  IMP_DECORATORS_METHODS(WrappedDecorator::particle_is_instance(p),,,);
-  public:
-  explicit Decorators(const Particles &ps) {
-    check(ps.begin(), ps.end());
-    ParentDecorators::reserve(ps.size());
-    for (unsigned int i=0; i< ps.size(); ++i) {
-      push_back(ps[i]);
-    }
-  }
-  explicit Decorators(const ParticlesTemp &ds) {
-    check(ds.begin(), ds.end());
-    ParentDecorators::reserve(ds.size());
-    for (unsigned int i=0; i< ds.size(); ++i) {
-      push_back(ds[i]);
-    }
-  }
-  explicit Decorators(unsigned int i): ParentDecorators(i, WrappedDecorator()){}
-  explicit Decorators(WrappedDecorator d): ParentDecorators(1, d){}
-  explicit Decorators(unsigned int n,
-                      WrappedDecorator d): ParentDecorators(n, d){}
-  Decorators(){}
-#ifndef SWIG
-  Proxy
-  operator[](unsigned int i) {
-    return get_proxy(i);
-  }
-#endif
-
-#ifndef SWIG
-  WrappedDecorator operator[](unsigned int i) const {
-    return WrappedDecorator(ParentDecorators::operator[](i));
-  }
-#endif
-};
-
-IMP_SWAP_2(Decorators);
-
-template <class WrappedDecorator, class ParentDecorators, class Traits>
-class DecoratorsWithTraits: public ParentDecorators {
-  typedef DecoratorsWithTraits<WrappedDecorator, ParentDecorators,
-                               Traits> ThisDecorators;
-
-  struct Proxy: public WrappedDecorator {
-    typedef typename ParentDecorators::reference Ref;
-    Ref d_;
-    Proxy(Ref t, Traits tr):
-      WrappedDecorator(t, tr), d_(t){
-    }
-    Proxy(Ref p, bool): WrappedDecorator(), d_(p){}
-    void operator=(WrappedDecorator v) {
-      // traits should match, but not checked
-      WrappedDecorator::operator=(v);
-      d_=v;
-    }
-#ifdef _MSC_VER
-    // for VC, it can't otherwise figure out the conversion chain
-    operator Particle*() {
-      if (WrappedDecorator()==*this) return NULL;
-      else return WrappedDecorator::get_particle();
-    }
-#endif
-  };
-  Proxy get_proxy(unsigned int i, Traits t) {
-    if (ParentDecorators::operator[](i)) {
-      return Proxy(ParentDecorators::operator[](i), t);
-    } else {
-      return Proxy(ParentDecorators::operator[](i), false);
-    }
-  }
-  Traits tr_;
-  bool has_traits_;
-  IMP_DECORATORS_METHODS(WrappedDecorator::particle_is_instance(p, tr_),{
-      if (!has_traits_) {
-        tr_= d.get_traits();
-        has_traits_=true;
-      } else {
-        IMP_USAGE_CHECK(tr_ == d.get_traits(),
-                        "Traits don't match");
-      }
-    },{
-      IMP_USAGE_CHECK(has_traits_, "Need to add a decorator first to get "
-                      << "traits class.");
-    }, {
-      std::swap(tr_, o.tr_);
-      std::swap(has_traits_, o.has_traits_);
-    });
-  public:
-  explicit DecoratorsWithTraits(Traits tr): tr_(tr), has_traits_(true){}
-  explicit DecoratorsWithTraits(WrappedDecorator d): ParentDecorators(1,d),
-                                              tr_(d.get_traits()),
-                                              has_traits_(true){}
-  explicit DecoratorsWithTraits(unsigned int n, WrappedDecorator d):
-    ParentDecorators(n, d),
-    tr_(d.get_traits()),
-    has_traits_(true) {}
-  DecoratorsWithTraits(const Particles &ps,
-                       Traits tr): tr_(tr), has_traits_(true) {
-    ParentDecorators::resize(ps.size());
-    for (unsigned int i=0; i< ps.size(); ++i) {
-      ParentDecorators::operator[](i)=WrappedDecorator(ps[i], tr);
-    }
-  }
-  DecoratorsWithTraits(unsigned int i,
-                       Traits tr): ParentDecorators(i), tr_(tr),
-                                   has_traits_(true){}
-  DecoratorsWithTraits(): has_traits_(false){}
-
-#ifndef SWIG
-  Proxy
-  operator[](unsigned int i) {
-    IMP_USAGE_CHECK(has_traits_, "Can only use operator[] on a decorator "
-                    << "container "
-                    << "which is non-empty. This is a bug, but hard to fix.");
-    return get_proxy(i, tr_);
-  }
-  WrappedDecorator operator[](unsigned int i) const {
-    return WrappedDecorator(ParentDecorators::operator[](i), tr_);
-  }
-#endif
-};
-
-
-IMP_SWAP_3(DecoratorsWithTraits);
-
 
 /** A class to add ref counting to a decorator */
 template <class D>
@@ -566,5 +351,23 @@ public:                                                                 \
 #endif
 
 IMP_END_NAMESPACE
+
+#if !defined(SWIG) && !defined IMP_DOXYGEN
+IMP_BEGIN_INTERNAL_NAMESPACE
+inline void unref(Decorator d) {
+  return unref(static_cast<Particle*>(d));
+}
+inline void release(Decorator d) {
+  return release(static_cast<Particle*>(d));
+}
+inline void ref(Decorator d) {
+  return ref(static_cast<Particle*>(d));
+}
+
+IMP_END_INTERNAL_NAMESPACE
+
+#endif
+
+#include "Decorators.h"
 
 #endif  /* IMP_DECORATOR_H */
