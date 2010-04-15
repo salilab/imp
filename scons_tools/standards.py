10a2126066715e3a8a05cb4a5606cf13cfebc76e
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-04-15 18:18:55 +0000

    clean up tests
    git-svn-id: https://svn.salilab.org/imp/trunk@5717 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- scons_tools/standards.py
+++ scons_tools/standards.py
@@ -10,32 +10,39 @@ def _action_check_standards(target, source, env):
     env.Execute(" ".join([x.path for x in source]))
 
 def _print_check_standards(target, source, env):
-    print "Checking standards"
+    pass
 
 CheckStandards = Builder(action=Action(_action_check_standards,
                                        _print_check_standards))
 
 
-def get_standards_files(env):
-    files= [File("#/tools/check-standards.py")]\
-        + Glob("#/modules/*/src/*.cpp")\
-        + Glob("#/modules/*/src/*/*.cpp")\
-        + Glob("#/modules/*/include/*.h")\
-        + Glob("#/modules/*/include/*/*.h")\
-        + Glob("#/modules/*/examples/*.py")\
-        + Glob("#/modules/*/examples/*/*.py")\
-        + Glob("#/kernel/src/*.cpp")\
-        + Glob("#/kernel/src/*/*.cpp")\
-        + Glob("#/kernel/include/*.h")\
-        + Glob("#/kernel/include/*/*.h")\
-        + Glob("#/kernel/examples/*.py")\
-        + Glob("#/kernel/examples/*/*.py")\
-        + Glob("#/modules/*/bin/*.cpp")\
-        + Glob("#/kernel/bin/*.cpp")
+def setup_standards(env):
+    env.Append(BUILDERS = {'CheckStandards':CheckStandards})
+    # Check code for coding standards:
+    patterns=["#/modules/*/src/*.cpp",
+              "#/modules/*/src/*/*.cpp",
+              "#/modules/*/include/*.h",
+              "#/modules/*/include/*/*.h",
+              "#/modules/*/examples/*.py",
+              "#/modules/*/examples/*/*.py",
+              "#/kernel/src/*.cpp",
+              "#/kernel/src/*/*.cpp",
+              "#/kernel/include/*.h",
+              "#/kernel/include/*/*.h",
+              "#/kernel/examples/*.py",
+              "#/kernel/examples/*/*.py",
+              "#/modules/*/bin/*.cpp",
+              "#/kernel/bin/*.cpp"]
     outfiles=[]
-    for f in files:
-        if str(f).endswith("config.h") or str(f).endswith("version_info.h"):
-            pass
-        else:
-            outfiles.append(f)
-    return outfiles
+    for g in patterns:
+        files= Glob(g)
+        for f in files:
+            if str(f).endswith("config.h") or str(f).endswith("version_info.h"):
+                pass
+            else:
+                outfiles.append(f)
+    for i in range(0, len(outfiles)):
+        standards = env.CheckStandards(target='standards'+str(i)+'.passed',
+                                       source=["#/tools/check-standards.py"]+[outfiles[i]])
+        env.Alias('standards', standards)
+    env.AlwaysBuild(env.Alias('standards'))
