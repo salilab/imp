c175026198f65a2efef26efd8f17c04058b2ebd6
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-04-20 19:12:15 +0000

    Don't call get_example_path twice.
    git-svn-id: https://svn.salilab.org/imp/trunk@5724 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/domino/examples/four_proteins_optimization.py
+++ modules/domino/examples/four_proteins_optimization.py
@@ -79,8 +79,7 @@ for prot_name,chain_id in [[IMP.domino.get_example_path("1z5s_A.pdb"),"A"],
                            [IMP.domino.get_example_path("1z5s_B.pdb"),"B"],
                            [IMP.domino.get_example_path("1z5s_C.pdb"),"C"],
                            [IMP.domino.get_example_path("1z5s_D.pdb"),"D"]]:
-    mhs.append(IMP.atom.read_pdb(IMP.domino.get_example_path(prot_name),
-                                 mdl,sel))
+    mhs.append(IMP.atom.read_pdb(prot_name, mdl,sel))
     mhs[-1].get_particle().add_attribute(IMP.domino.node_name_key(),chain_id)
     IMP.atom.add_radii(mhs[-1])
     IMP.atom.setup_as_rigid_body(mhs[-1])
