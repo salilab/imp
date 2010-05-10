9f2fff44d89388fc4ddf2a2b82ea245133904bb0
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-05-10 11:47:10 +0000

    allow in-place transformation of particles in the function: get_number_of_particles_outside_of_the_density
    git-svn-id: https://svn.salilab.org/imp/trunk@5778 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/em/src/envelope_penetration.cpp
+++ modules/em/src/envelope_penetration.cpp
@@ -10,14 +10,20 @@
 IMPEM_BEGIN_NAMESPACE
 
 long get_number_of_particles_outside_of_the_density(DensityMap *dmap,
-                                                    const Particles &ps) {
+     const Particles &ps,const IMP::algebra::Transformation3D &t,float thr) {
   IMP_LOG(IMP::VERBOSE,"start calculating the how many particles out of "
          <<ps.size()<<" in density"<< std::endl);
   long out_of_dens=0;
   core::XYZsTemp xyz = core::XYZsTemp(ps);
   for (unsigned int i = 0; i < ps.size(); ++i) {
-    if (!dmap->is_part_of_volume(xyz[i].get_coordinates())) {
+    IMP::algebra::Vector3D vec = t.get_transformed(xyz[i].get_coordinates());
+    if (!dmap->is_part_of_volume(vec)) {
       out_of_dens++;
+    }
+    else {
+      if (dmap->get_value(vec)<thr) {
+        out_of_dens++;
+      }
     }
   }
   IMP_LOG(IMP::VERBOSE,"the number of particles outside of the density is:"
