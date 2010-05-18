6f5e3d5836ef42721f3cd8547adfe957143edcbe
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-05-18 06:16:06 +0000

    split configuration set into a single version and the multiple version and don't change particle attributes which don't differ so that incremental scoring working faster
    git-svn-id: https://svn.salilab.org/imp/trunk@5802 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ kernel/src/Configuration.cpp
@@ -0,0 +1,54 @@
+/**
+ *  \file Configuration.cpp
+ *  \brief Storage of a model, its restraints,
+ *                         constraints and particles.
+ *
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+ *
+ */
+
+#include "IMP/Configuration.h"
+#include "IMP/internal/utility.h"
+
+IMP_BEGIN_NAMESPACE
+
+
+
+Configuration::Configuration(Model *m, std::string name): Object(name),
+  model_(m){
+  for (Model::ParticleIterator it= model_->particles_begin();
+       it != model_->particles_end(); ++it) {
+    PP pp(*it);
+    base_[pp]= internal::ParticleData(*it);
+  }
+}
+
+
+
+
+void Configuration::load_configuration() const {
+  IMP_OBJECT_LOG;
+  for (Model::ParticleIterator it= model_->particles_begin();
+       it != model_->particles_end(); ++it) {
+    PP pp(*it);
+    if (base_.find(pp) == base_.end()) {
+      model_->remove_particle(*it);
+    }
+  }
+  std::set<Particle*> active(model_->particles_begin(),
+                             model_->particles_end());
+  for (DataMap::const_iterator it= base_.begin(); it != base_.end(); ++it) {
+    PP pp(it->first);
+    if (active.find(pp) == active.end()) {
+      model_->restore_particle(it->first);
+    }
+    it->second.apply(it->first);
+  }
+}
+
+
+void Configuration::do_show(std::ostream &out) const {
+  out <<  "configuration" << std::endl;
+}
+
+IMP_END_NAMESPACE
