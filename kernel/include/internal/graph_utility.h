c05f74675b4b93d8e3a5644e97c4f87aca22bea5
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-06-10 21:27:29 +0000

    forgotten file
    git-svn-id: https://svn.salilab.org/imp/trunk@5847 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ kernel/include/internal/graph_utility.h
@@ -0,0 +1,42 @@
+/**
+ *  \file internal/graph_utility.h
+ *  \brief Various useful utilities
+ *
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+ */
+
+#ifndef IMP_INTERNAL_GRAPH_UTILITY_H
+#define IMP_INTERNAL_GRAPH_UTILITY_H
+
+#include "../kernel_config.h"
+#include "../Particle.h"
+#include <boost/format.hpp>
+#include <algorithm>
+#include <sstream>
+#include <boost/graph/graphviz.hpp>
+
+
+IMP_BEGIN_INTERNAL_NAMESPACE
+
+template <class Graph>
+class ObjectNameWriter {
+  typedef typename boost::property_map<Graph,
+                                       boost::vertex_name_t>::type VertexMap;
+  VertexMap om_;
+public:
+  ObjectNameWriter( const Graph&g): om_(boost::get(boost::vertex_name,
+                                                   const_cast<Graph&>(g))){}
+  void operator()(std::ostream& out, int v) const {
+    out << "[label=\"" << boost::get(om_, v)->get_name() << "\"]";
+  }
+};
+
+template <class Graph>
+void show_as_graphviz(const Graph &g, std::ostream &out) {
+  ObjectNameWriter<Graph> onw(g);
+  boost::write_graphviz(out, g, onw);
+}
+
+IMP_END_INTERNAL_NAMESPACE
+
+#endif  /* IMP_INTERNAL_GRAPH_UTILITY_H */
