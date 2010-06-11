787a41a47e1ab6c5679ba3033f1f98fe4a5de94f
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-06-11 05:30:42 +0000

    improve filtering:
    git-svn-id: https://svn.salilab.org/imp/trunk@5859 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/domino2/examples/six_particles_optimization.py
+++ modules/domino2/examples/six_particles_optimization.py
@@ -1,37 +1,34 @@
 import IMP
 import IMP.domino2
 import IMP.core
-NUM_PARTICLES=6
-NUM_STATES=40
 
 #set restraints
 def setup_restraints(m, ps):
     pairs=[[0,1],[0,2],[1,2],[2,3],[3,4],[4,5],[3,5]]
-    sf = IMP.core.Harmonic(1.0, 0.5)
+    sf = IMP.core.Harmonic(1.0, 1)
     for pair in pairs:
         r=IMP.core.DistanceRestraint(sf, ps[pair[0]], ps[pair[1]])
         m.add_restraint(r)
 
 IMP.set_log_level(IMP.TERSE)
 m=IMP.Model()
+m.set_log_level(IMP.SILENT)
 print "create sampler"
 s=IMP.domino2.DominoSampler(m)
 #1. set up the particles
 print "setting up particles"
 ps=IMP.Particles()
-for i in range(NUM_PARTICLES):
+for i in range(0,6):
     p=IMP.Particle(m)
     IMP.core.XYZ.setup_particle(p,IMP.algebra.Vector3D(0.,0.,0.))
     ps.append(p)
 
 #2. set up the discrete set of states
 print "setting up a discrete set of states"
-lb=IMP.algebra.Vector3D(-10.,-10.,-10.)
-ub=IMP.algebra.Vector3D(10.,10.,10.)
-bb=IMP.algebra.BoundingBox3D(lb,ub)
 vs=[]
-for i in range(NUM_STATES):
-    vs.append(IMP.algebra.get_random_vector_in(bb))
+for i in range(0,4):
+    vs.append(IMP.algebra.Vector3D(i,0,0))
+    vs.append(IMP.algebra.Vector3D(i,1,0))
 
 print "create states"
 states= IMP.domino2.XYZsStates(vs)
@@ -44,7 +41,8 @@ print "setting up restraints"
 setup_restraints(m, ps)
 
 #5. optimize
-s.set_maximum_score(1)
+s.set_maximum_score(.2)
+s.set_log_level(IMP.VERBOSE)
 print "sampling"
 cs=s.get_sample()
 
--- modules/domino2/test/test_sampling.py
+++ modules/domino2/test/test_sampling.py
@@ -28,10 +28,6 @@ class DOMINOTests(IMP.test.TestCase):
         for p in ps:
             pst.set_particle_states(p, particle_state)
         sst= IMP.domino2.DefaultSubsetStatesTable(pst)
-        for p in ps:
-            for q in ps:
-                if p != q:
-                    sst.add_equivalency(p,q)
         m.add_restraint(IMP.core.DistanceRestraint(IMP.core.Harmonic(1,1), ps[0], ps[1]))
         m.add_restraint(IMP.core.DistanceRestraint(IMP.core.Harmonic(1,1), ps[1], ps[2]))
 
@@ -40,5 +36,8 @@ class DOMINOTests(IMP.test.TestCase):
         sampler.set_subset_states_table(sst)
         sampler.set_maximum_score(.1)
         cs= sampler.get_sample()
+        self.assert_((IMP.core.distance(IMP.core.XYZ(ps[0]),IMP.core.XYZ(ps[1]))-1)**2 < .1)
+        self.assert_((IMP.core.distance(IMP.core.XYZ(ps[1]),IMP.core.XYZ(ps[2]))-1)**2 < .1)
+        # test that they are the right distance
 if __name__ == '__main__':
     unittest.main()
