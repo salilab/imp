ea73f31b24f263ff02252ea34a710eb9bea369cb
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-06-19 02:52:29 +0000

    forgotten files
    git-svn-id: https://svn.salilab.org/imp/trunk@5897 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/domino2/test/test_bandb_sampler.py
@@ -0,0 +1,54 @@
+import sys
+import unittest
+import IMP
+import IMP.test
+import IMP.domino2
+import IMP.core
+import IMP.atom
+import IMP.helper
+import time
+
+class TrivialParticleStates(IMP.domino2.ParticleStates):
+    def __init__(self, n):
+        IMP.domino2.ParticleStates.__init__(self)
+        self.key= IMP.IntKey("hi")
+        self.n=n
+    def get_number_of_states(self):
+        return self.n
+    def load_state(self, i, p):
+        if p.has_attribute(self.key):
+            p.set_value(self.key, i)
+        else:
+            p.add_attribute(self.key, i)
+    def do_show(self, stream):
+        pass
+
+class DOMINOTests(IMP.test.TestCase):
+
+    def test_global_min1(self):
+        """Testing branch and bound sampler"""
+        m= IMP.Model()
+        ps=[]
+        ns=5
+        np=4
+        for i in range(0,np):
+            ps.append(IMP.Particle(m))
+        pst= IMP.domino2.ParticleStatesTable()
+        dsst= IMP.domino2.BranchAndBoundSampler(m, pst)
+        for p in ps:
+            pst.set_particle_states(p, TrivialParticleStates(ns))
+        cs= dsst.get_sample()
+        self.assertEqual(cs.get_number_of_configurations(), ns**len(ps))
+        all_states=[]
+        for i in range(0, cs.get_number_of_configurations()):
+            cs.load_configuration(i)
+            s=[]
+            for p in ps:
+                s.append(p.get_value(IMP.IntKey("hi")))
+            ss= IMP.domino2.SubsetState(s)
+            #print all_states
+            self.assert_(s not in all_states)
+            all_states.append(s)
+
+if __name__ == '__main__':
+    unittest.main()
