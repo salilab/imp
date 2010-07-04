1933bae51de6082a248ce88f57f524838ae60002
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-07-04 18:27:28 +0000

    add ability to tag files by a variable
    git-svn-id: https://svn.salilab.org/imp/trunk@5969 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/include/WriteModelOptimizerState.h
+++ modules/core/include/WriteModelOptimizerState.h
@@ -23,10 +23,16 @@ class IMPCOREEXPORT WriteModelOptimizerState: public OptimizerState
 {
   Pointer<Model> model_;
   std::string name_template_;
-
+  Pointer<Particle> p_;
+  FloatKey k_;
 public:
-  //! Write files using name_template as a template (must have a %d in it)
+  //! Write files using name_template as a template (must have a %1% in it)
   WriteModelOptimizerState(Model *m, std::string name_template);
+  //! label the files witha  value
+  void set_file_value(Particle *p, FloatKey k) {
+    p_=p;
+    k_=k;
+  }
 
   IMP_PERIODIC_OPTIMIZER_STATE(WriteModelOptimizerState);
 };
