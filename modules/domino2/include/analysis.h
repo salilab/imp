a60cfa3dd63c659ec0d00ea75fba8df7fb8660b1
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-07-10 00:14:15 +0000

    improve printing
    git-svn-id: https://svn.salilab.org/imp/trunk@5999 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/domino2/include/analysis.h
@@ -0,0 +1,35 @@
+/**
+ *  \file domino2/analysis.h
+ *  \brief Functions to get report statistics about the used attributes.
+ *
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+ */
+
+#ifndef IMPDOMINO2_ANALYSIS_H
+#define IMPDOMINO2_ANALYSIS_H
+
+#include "domino2_config.h"
+#include "SubsetState.h"
+#include "Subset.h"
+
+IMPDOMINO2_BEGIN_NAMESPACE
+class ParticleStatesTable;
+/** Return subset of the passed states which are representative.
+    The states for each particle is clustered and then the
+    passed states are reduced to only be on the centers
+    of that clustering.
+
+    The XYZ coordinates are used for clustering, there is not
+    currently a nice mechanism to vary this, but it should be
+    figured out. Probably by subclasses statistics::Embedding
+    to statistics::ParticleEmbedding which has a method
+    which takes a particle.
+ */
+IMPDOMINO2EXPORT SubsetStatesList get_state_clusters(const Subset &subset,
+                                                 const SubsetStatesList &states,
+                                                 ParticleStatesTable *pst,
+                                                 double resolution);
+
+IMPDOMINO2_END_NAMESPACE
+
+#endif  /* IMPDOMINO2_ANALYSIS_H */
