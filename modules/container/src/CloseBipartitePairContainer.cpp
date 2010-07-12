ec76e90ee5c2b73d4403bf189cdf69714b040f46
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-07-12 22:07:38 +0000

    fix changed
    git-svn-id: https://svn.salilab.org/imp/trunk@6014 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/container/src/CloseBipartitePairContainer.cpp
+++ modules/container/src/CloseBipartitePairContainer.cpp
@@ -162,8 +162,15 @@ ParticlesTemp CloseBipartitePairContainer::get_contained_particles() const {
 }
 
 bool CloseBipartitePairContainer::get_contained_particles_changed() const {
-  return a_->get_contained_particles_changed()
-    || b_->get_contained_particles_changed();
+  if( a_->get_contained_particles_changed()
+      || b_->get_contained_particles_changed()) return true;
+  else {
+    ParticlesTemp cp= get_contained_particles();
+    for (unsigned int i=0; i< cp.size(); ++i) {
+      if (cp[i]->get_is_changed()) return true;
+    }
+    return false;
+  }
 }
 
 IMPCONTAINER_END_NAMESPACE
