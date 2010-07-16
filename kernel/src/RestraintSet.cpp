5193bce19d1df790964e40ef442c62134dc60f17
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-07-16 04:33:58 +0000

    add method to get restraints from set
    git-svn-id: https://svn.salilab.org/imp/trunk@6026 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/src/RestraintSet.cpp
+++ kernel/src/RestraintSet.cpp
@@ -89,4 +89,10 @@ RestraintsAndWeights get_restraints(const RestraintsTemp &rs,
   return get_restraints(rs.begin(), rs.end(), initial_weight);
 }
 
+
+RestraintsAndWeights get_restraints(RestraintSet *rs) {
+  return get_restraints(rs->restraints_begin(), rs->restraints_end(),
+                        rs->get_weight());
+}
+
 IMP_END_NAMESPACE
