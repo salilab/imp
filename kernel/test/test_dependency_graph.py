dbc9622dbab57e41e8c5b738db5fe77a0f838915
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-07-21 14:55:27 +0000

    forgot a file
    git-svn-id: https://svn.salilab.org/imp/trunk@6057 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/test/test_dependency_graph.py
+++ kernel/test/test_dependency_graph.py
@@ -8,8 +8,7 @@ class ModelTests(IMP.test.TestCase):
     def test_state_show(self):
         """Test dependency graph"""
         m = IMP.Model()
-        dg= IMP.get_dependency_graph(m.get_score_states(),
-                                     IMP.get_restraints(m.get_restraints())[0])
+        dg= IMP.get_dependency_graph([m.get_root_restraint_set()])
         print dir(dg)
         dg.show()
 
