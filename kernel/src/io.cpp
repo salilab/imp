a69988625e55d03cb6284fe9d99c9535ac85539b
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-07-28 04:50:40 +0000

    add i/o to netcdf as well as ability to only write some attributes to yaml
    git-svn-id: https://svn.salilab.org/imp/trunk@6097 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/src/io.cpp
+++ kernel/src/io.cpp
@@ -8,13 +8,17 @@
 
 #include <IMP/io.h>
 #include <IMP/internal/particle_save.h>
+#include <netcdfcpp.h>
 
 IMP_BEGIN_NAMESPACE
 
-
-
 IMPEXPORT void write_model(Model *,
                            const ParticlesTemp &particles,
+                           TextOutput out) {
+  write_model(particles, out);
+}
+
+IMPEXPORT void write_model(const ParticlesTemp &particles,
                            TextOutput out) {
   std::map<Particle*, unsigned int> to;
   for (unsigned int i=0; i< particles.size(); ++i) {
@@ -32,6 +36,27 @@ IMPEXPORT void write_model(Model *,
     it->second.write_yaml(out, to);
   }
 }
+
+IMPEXPORT void write_model(const ParticlesTemp &particles,
+                           const FloatKeys &keys,
+                           TextOutput out) {
+  std::map<Particle*, unsigned int> to;
+  for (unsigned int i=0; i< particles.size(); ++i) {
+    to[particles[i]]=i;
+  }
+  std::map<unsigned int, internal::ParticleData> data;
+  for (std::map<Particle*, unsigned int>::const_iterator it= to.begin();
+       it != to.end(); ++it) {
+    data[it->second]= internal::ParticleData(it->first, keys);
+  }
+  for (std::map<unsigned int,
+         internal::ParticleData>::const_iterator it= data.begin();
+       it != data.end(); ++it) {
+    out.get_stream() << "particle: " << it->first << std::endl;
+    it->second.write_yaml(out, to);
+  }
+}
+
 IMPEXPORT void write_model(Model *m,
                            TextOutput out) {
   ParticlesTemp ps(m->particles_begin(), m->particles_end());
@@ -84,9 +109,122 @@ IMPEXPORT void read_model(TextInput in,
     it->second.apply(it->first);
   }
 }
+
+
+IMPEXPORT void read_model(TextInput in,
+                          const ParticlesTemp &particles,
+                          const FloatKeys &keys) {
+  internal::LineStream ls(in);
+  std::map<unsigned int, Particle *> from;
+  for (unsigned int i=0; i< particles.size(); ++i) {
+    from[i]= particles[i];
+  }
+  std::map<Particle*, internal::ParticleData> data;
+  do {
+    internal::LineStream::LinePair lp= ls.get_line();
+    if (lp.first.empty()) break;
+    if (lp.first== "particle") {
+      std::istringstream iss(lp.second);
+      int i=-1;
+      iss >> i;
+      if (i<0) {
+        IMP_THROW("Error reading from line "
+                  << lp.first << ": " << lp.second,
+                  IOException);
+      }
+      if (from.find(i) == from.end()) {
+        IMP_THROW("Can't find particle " << i << " in map",
+                  IOException);
+      }
+      data[from.find(i)->second]=internal::ParticleData();
+      ls.push_indent();
+      data[from.find(i)->second].read_yaml(ls, from);
+      ls.pop_indent();
+    } else {
+      IMP_LOG(TERSE,
+              "Found " << lp.first << " when looking for particle"
+              << std::endl);
+    }
+  } while (true);
+  IMP_LOG(TERSE, "Read " << data.size() << " particles"
+          << std::endl);
+  if (data.size() != from.size()) {
+    IMP_THROW("Did not read all particles. Expected "
+              << from.size() << " got " << data.size(),
+              IOException);
+  }
+  for (std::map<Particle*, internal::ParticleData>::const_iterator
+         it= data.begin(); it != data.end(); ++it) {
+    it->second.apply(it->first, keys);
+  }
+}
+
+
 IMPEXPORT void read_model(TextInput in, Model *m) {
   ParticlesTemp ps(m->particles_begin(), m->particles_end());
   read_model(in, ps, m);
+}
+
+
+void write_binary_model(const ParticlesTemp &particles,
+                        const FloatKeys &keys,
+                        std::string filename,
+                        int frame) {
+  NcFile::FileMode mode;
+  if (frame >=0) {
+    mode=NcFile::Write;
+  } else {
+    mode=NcFile::Replace;
+  }
+  NcFile f(filename.c_str(), mode);
+  if (!f.is_valid()) {
+    IMP_THROW("Unable to open file " << filename << " for writing",
+              IOException);
+  }
+  const NcDim* dims[2];
+  dims[0]= f.add_dim("particles", particles.size());
+  dims[1]= f.add_dim("values", keys.size());
+  NcVar *cur=NULL;
+  if (frame >=0) {
+    std::ostringstream oss;
+    oss << "frame " << frame;
+    cur = f.add_var(oss.str().c_str(), ncDouble, 2, dims);
+  } else {
+    cur = f.add_var("data", ncDouble, 2, dims);
+  }
+  // ick
+  boost::scoped_array<double> values(new double[particles.size()*keys.size()]);
+  for (unsigned int i=0; i< particles.size(); ++i) {
+    for (unsigned int j=0; j< keys.size(); ++j) {
+      values[i*keys.size()+j]= particles[i]->get_value(keys[j]);
+    }
+  }
+  cur->put(values.get(), particles.size(), keys.size());
+}
+void read_binary_model(std::string filename,
+                       const ParticlesTemp &particles,
+                       const FloatKeys &keys,
+                       int frame) {
+  NcFile f(filename.c_str(), NcFile::ReadOnly);
+  if (!f.is_valid()) {
+    IMP_THROW("Unable to open file " << filename << " for writing",
+              IOException);
+  }
+  NcVar *data=NULL;
+  if (frame >=0) {
+    std::ostringstream oss;
+    oss << "frame " << frame;
+    data = f.get_var(oss.str().c_str());
+  } else {
+    data = f.get_var("data");
+  }
+  boost::scoped_array<double> values(new double[particles.size()*keys.size()]);
+  data->get(values.get(), particles.size(), keys.size());
+  for (unsigned int i=0; i< particles.size(); ++i) {
+    for (unsigned int j=0; j< keys.size(); ++j) {
+      particles[i]->set_value(keys[j], values[i*keys.size()+j]);
+    }
+  }
 }
 
 
--- kernel/test/test_model_io.py
+++ kernel/test/test_model_io.py
@@ -4,7 +4,7 @@ import IMP.test
 import StringIO
 
 class YamlTests(IMP.test.TestCase):
-    def _create_model(self):
+    def _create_hetero_model(self):
         IMP.set_log_level(IMP.VERBOSE)
         m = IMP.Model()
         ps=IMP.ParticlesTemp()
@@ -26,21 +26,69 @@ class YamlTests(IMP.test.TestCase):
         ps.append(p)
         IMP.set_log_level(IMP.VERBOSE)
         return (m, ps)
+
+    def _create_homo_model(self):
+        IMP.set_log_level(IMP.VERBOSE)
+        m = IMP.Model()
+        ps=IMP.ParticlesTemp()
+        p= IMP.Particle(m)
+        p.add_attribute(IMP.FloatKey("x"), 0, False)
+        p.add_attribute(IMP.FloatKey("y"), 1, True)
+        p.add_attribute(IMP.FloatKey("z"), 2, False)
+        ps.append(p)
+        p= IMP.Particle(m)
+        p.add_attribute(IMP.FloatKey("x"), 4, True)
+        p.add_attribute(IMP.FloatKey("y"), 7, False)
+        p.add_attribute(IMP.FloatKey("z"), 9, False)
+        ps.append(p)
+        p= IMP.Particle(m)
+        p.add_attribute(IMP.FloatKey("x"), 10, True)
+        p.add_attribute(IMP.FloatKey("y"), 11, False)
+        p.add_attribute(IMP.FloatKey("z"), 12, False)
+        ps.append(p)
+        IMP.set_log_level(IMP.VERBOSE)
+        return (m, ps)
     def test_yaml(self):
         """Check writing to yaml """
-        (m, ps)= self._create_model()
+        (m, ps)= self._create_hetero_model()
         s= StringIO.StringIO()
-        IMP.write_model(m, ps, s)
+        IMP.write_model(ps, s)
         first=s.getvalue()
         print first
         sto= StringIO.StringIO(first)
         ps[0].set_value(IMP.FloatKey("x"), 11)
         ps[0].add_attribute(IMP.ParticleKey("y"), ps[1])
-        IMP.read_model(sto, ps, m)
+        IMP.read_model(sto, ps)
         s= StringIO.StringIO()
         IMP.write_model(m, ps, s)
         print s.getvalue()
         self.assertEqual(first, s.getvalue())
+
+    def test_yaml_attr(self):
+        """Check writing to yaml with attributes"""
+        (m, ps)= self._create_homo_model()
+        s= StringIO.StringIO()
+        fks= [IMP.FloatKey("x"), IMP.FloatKey("y")]
+        IMP.write_model(ps, fks, s)
+        first=s.getvalue()
+        print first
+        sto= StringIO.StringIO(first)
+        ps[0].set_value(IMP.FloatKey("x"), 11)
+        IMP.read_model(sto, ps, fks)
+        s= StringIO.StringIO()
+        IMP.write_model(ps, fks, s)
+        print s.getvalue()
+        self.assertEqual(first, s.getvalue())
+    def test_netcdf(self):
+        """Check writing to netcdf with attributes"""
+        (m, ps)= self._create_homo_model()
+        fks= [IMP.FloatKey("x"), IMP.FloatKey("y")]
+        IMP.write_binary_model(ps, fks, self.get_tmp_file_name("test0.imp"))
+        first= ps[0].get_value(IMP.FloatKey("x"))
+        print first
+        ps[0].set_value(IMP.FloatKey("x"), 11)
+        IMP.read_binary_model(self.get_tmp_file_name("test0.imp"), ps, fks)
+        self.assertEqual(first, ps[0].get_value(IMP.FloatKey("x")))
 
 
 
