fb21c160974bf851fe34fc1eb429cf814dcde9d0
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-07-28 19:52:10 +0000

    add failing test for creating residues from topology
    git-svn-id: https://svn.salilab.org/imp/trunk@6104 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/atom/test/test_create_residues.py
@@ -0,0 +1,26 @@
+import unittest
+import IMP
+import IMP.test
+import IMP.core
+import IMP.atom
+
+class DecoratorTests(IMP.test.TestCase):
+    def test_bonded(self):
+        """Check creating residues from CHARMM topology """
+        m= IMP.Model()
+        sequence= "FSFGAKSKENKAGATSKPAFSFG" #"GLFGQNNQQQGSGLFG"
+        seg= IMP.atom.CHARMMSegmentTopology()
+        for r in sequence:
+            rt= IMP.atom.get_residue_type(r)
+            seg.add_residue(IMP.atom.CHARMMResidueTopology(rt))
+        top= IMP.atom.CHARMMTopology()
+        top.add_segment(seg)
+        top.apply_default_patches()
+        h= top.create_hierarchy(m)
+        atoms= IMP.atom.get_by_type(h, IMP.atom.ATOM_TYPE)
+        print atoms
+        IMP.atom.show_molecular_hierarchy(h)
+        self.assert_(len(atoms) > 0)
+
+if __name__ == '__main__':
+    unittest.main()
