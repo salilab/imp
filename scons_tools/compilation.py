53f58ae49cb4a7d7164d60c66f968add96dc9eb9
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-08-09 05:41:40 +0000

    only try compiling not running, in case a special runtime environment is needed
    git-svn-id: https://svn.salilab.org/imp/trunk@6192 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- scons_tools/compilation.py
+++ scons_tools/compilation.py
@@ -4,7 +4,7 @@ from SCons.Script import Exit
 
 def _check(context):
     context.Message('Checking that the compiler works... ')
-    ret = context.TryRun("""#include <iostream>
+    ret = context.TryCompile("""#include <iostream>
 
         int main()
         {
@@ -12,12 +12,14 @@ def _check(context):
             return 0;
         }
         """, '.cpp')
-    context.Result(ret[0])
-    return ret[0]
+    context.Result(ret)
+    return ret
 
 def configure_check(env):
     custom_tests = {'CheckCompiler':_check}
     conf = env.Configure(custom_tests=custom_tests)
     if conf.CheckCompiler():
         env["COMPILER_OK"]=True
+    else:
+        env["COMPILER_OK"]=False
     conf.Finish()
