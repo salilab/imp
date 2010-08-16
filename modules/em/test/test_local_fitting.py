0c1e691b4cafc40895d98f4dd967283933827ce8
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-08-16 08:23:46 +0000

    add another test and return IMP::Pointer in density2particles
    git-svn-id: https://svn.salilab.org/imp/trunk@6274 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/em/test/test_local_fitting.py
@@ -0,0 +1,59 @@
+import unittest
+import sys,os
+import IMP
+import IMP.em
+import IMP.test
+import IMP.core
+import IMP.atom
+import IMP.helper
+
+class LocalFittingTest(IMP.test.TestCase):
+    """Test local fitting functions"""
+
+    def sample_density_map(self):
+        self.scene = IMP.em.particles2density(IMP.core.get_leaves(self.mh),
+                                              10.,2.)
+
+    def load_protein(self,pdb_filename):
+        self.m = IMP.Model()
+        self.mh= IMP.atom.read_pdb(self.open_input_file(pdb_filename),
+                              self.m, IMP.atom.CAlphaPDBSelector())
+        self.radius_key = IMP.core.XYZR.get_default_radius_key()
+        self.weight_key = IMP.atom.Mass.get_mass_key()
+        IMP.atom.add_radii(self.mh)
+        IMP.atom.setup_as_rigid_body(self.mh)
+        self.particles = IMP.core.get_leaves(self.mh)
+
+    def setUp(self):
+        """Build test model and optimizer"""
+        IMP.test.TestCase.setUp(self)
+        IMP.set_log_level(IMP.VERBOSE)
+        self.imp_model = IMP.Model()
+        self.load_protein("1z5s_A.pdb")
+        self.sample_density_map()
+
+    def test_fast_vs_slow_local_fitting(self):
+        """Check that the correct spacing gets the highest CC"""
+        print "calculate fast"
+        self.scene.get_header().show()
+        rand_translation = 5.*IMP.algebra.get_random_vector_in(IMP.algebra.get_unit_bounding_box_3d())
+        rt= IMP.algebra.Transformation3D(IMP.algebra.get_identity_rotation_3d(),rand_translation)
+        IMP.core.transform(IMP.core.RigidBody(self.mh),rt)
+        fr_fast=IMP.em.local_rigid_fitting(
+            IMP.core.RigidBody(self.mh),
+            self.radius_key, self.weight_key,
+            self.scene,None,2,3,20,2.,.3,True)
+        print "calculate slow"
+        #TODO - should work without the next line
+        self.sample_density_map()
+        self.scene.get_header().show()
+        fr_slow=IMP.em.local_rigid_fitting(
+             IMP.core.RigidBody(self.mh),
+             self.radius_key, self.weight_key,
+             self.scene,None,2,3,20,2.,.3,False)
+
+        print "fast:",fr_fast.get_score(0)," slow:",fr_slow.get_score(0)
+        self.assert_(abs(fr_fast.get_score(0)-fr_slow.get_score(0))<0.01)
+
+if __name__ == '__main__':
+    unittest.main()
