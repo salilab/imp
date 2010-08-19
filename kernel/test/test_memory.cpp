95841eb5947149feb5972186f750bd811bbd77d6
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-08-19 00:02:28 +0000

    add a little test which can be run with memcheck to make sure imp doesn't leak
    git-svn-id: https://svn.salilab.org/imp/trunk@6326 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ kernel/test/test_memory.cpp
@@ -0,0 +1,13 @@
+/**
+ *   Copyright 2007-2010 IMP Inventors. All rights reserved
+ */
+#include <IMP/Model.h>
+
+int main(int, char*[]) {
+  IMP::set_log_level(IMP::MEMORY);
+  IMP_NEW(IMP::Model, m, ());
+  for (unsigned int i=0; i< 10; ++i) {
+    new IMP::Particle(m);
+  }
+  return 1;
+}
