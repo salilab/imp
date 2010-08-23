a7693c79383e10c44aad873309d32176976b44bf
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-08-23 23:59:59 +0000

    add a WeightedExcludedVolumeRestraint
    git-svn-id: https://svn.salilab.org/imp/trunk@6391 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/multifit/src/WeightedExcludedVolumeRestraint.cpp
@@ -0,0 +1,140 @@
+/**
+ *  \file WeighedExcludedVolumeRestraint.cpp
+ *  \brief Calculate excluded volume bewteen rigid bodies.
+ *
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+ *
+ */
+
+#include <IMP/multifit/WeightedExcludedVolumeRestraint.h>
+#include <IMP/log.h>
+
+IMPEM_BEGIN_NAMESPACE
+
+WeightedExcludedVolumeRestraint::WeightedExcludedVolumeRestraint(
+  core::RigidBodies rbs,
+  Refiner *refiner,
+  FloatKey radius_key,FloatKey weight_key):
+  Restraint("Weighted Excluded Volume Restraint") {
+  IMP_LOG(TERSE,"Load WeightedExcludedVolumeRestraint \n");
+  rb_refiner_=refiner;
+  add_particles(rbs);
+  rbs_=rbs;
+  initialize_model_density_map(radius_key,weight_key);
+}
+void WeightedExcludedVolumeRestraint::initialize_model_density_map(
+  FloatKey radius_key,FloatKey weight_key) {
+  for (core::RigidBodies::const_iterator it = rbs_.begin();
+       it != rbs_.end();it++){
+    core::RigidBody rb=*it;
+    Particles rb_ps=rb_refiner_->get_refined(rb);
+    std::cout<<"Creating a density map for:"
+             <<rb_ps.size()<<" particles"<<std::endl;
+    rbs_surface_maps_.push_back(
+                                new SurfaceShellDensityMap(rb_ps,1));
+    rbs_orig_trans_.push_back(rb.get_transformation().get_inverse());
+  }
+}
+
+double WeightedExcludedVolumeRestraint::unprotected_evaluate(
+                         DerivativeAccumulator *accum) const
+{
+  bool calc_deriv = accum? true: false;
+  IMP_LOG(VERBOSE,"before resample\n");
+  // //generate the transformed maps
+  // std::vector<DensityMap*> transformed_maps;
+  float spacing=rbs_surface_maps_[0]->get_header()->get_spacing();
+  // for(int rb_i=0;rb_i<rbs_.size();rb_i++){
+  //   DensityMap *dm=create_density_map(
+  //       atom::get_bounding_box(atom::Hierarchy(rbs_[rb_i])),spacing);
+  //   get_transformed_into(
+  //       rbs_surface_maps_[rb_i],
+  //       rbs_[rb_i].get_transformation()*rbs_orig_trans_[rb_i],
+  //       dm,
+  //       false);
+  //   transformed_maps.push_back(dm);
+  // }
+  double score=0.;
+  // MRCReaderWriter mrw;
+  // for(int i=0;i<transformed_maps.size();i++){
+  //   std::stringstream ss;
+  //   ss<<"transformed_map_"<<i<<".mrc";
+  //   std::stringstream s1;
+  //   s1<<"transformed_pdb_"<<i<<".pdb";
+  //   atom::write_pdb(atom::Hierarchy(rbs_[i]),s1.str().c_str());
+  //   write_map(transformed_maps[i],ss.str().c_str(),mrw);
+  //   for(int j=i+1;j<transformed_maps.size();j++){
+  //     if (get_interiors_intersect(transformed_maps[i],
+  //                                 transformed_maps[j])){
+  //     score += CoarseCC::cross_correlation_coefficient(
+  //                              *transformed_maps[i],
+  //                              *transformed_maps[j],1.,false,true);
+  //     }
+  //   }
+  // }
+  SurfaceShellDensityMaps resampled_surfaces;
+  for(int i=0;i<rbs_.size();i++){
+    Particles rb_ps=rb_refiner_->get_refined(rbs_[i]);
+    resampled_surfaces.push_back(new SurfaceShellDensityMap(rb_ps,1.));
+  }
+  for(int i=0;i<rbs_.size();i++){
+    for(int j=i+1;j<rbs_.size();j++){
+      if (get_interiors_intersect(resampled_surfaces[i],
+                                  resampled_surfaces[j])){
+      score += CoarseCC::cross_correlation_coefficient(
+                               *resampled_surfaces[i],
+                               *resampled_surfaces[j],1.,false,true);
+      }
+    }
+  }
+
+  if (calc_deriv) {
+    IMP_WARN("WeightedExcludedVolumeRestraint currently"
+             <<" does not support derivatives\n");
+  }
+  for(int i=resampled_surfaces.size()-1;i<0;i--){
+    delete(resampled_surfaces[i]);
+  }
+  return score;
+}
+
+ParticlesTemp WeightedExcludedVolumeRestraint::get_input_particles() const
+{
+  ParticlesTemp pt;
+  for (ParticleConstIterator it= particles_begin();
+       it != particles_end(); ++it) {
+      ParticlesTemp cur= rb_refiner_->get_input_particles(*it);
+      pt.insert(pt.end(), cur.begin(), cur.end());
+      ParticlesTemp curr= rb_refiner_->get_refined(*it);
+      pt.insert(pt.end(), curr.begin(), curr.end());
+  }
+  return pt;
+}
+
+ContainersTemp WeightedExcludedVolumeRestraint::get_input_containers() const {
+  ContainersTemp pt;
+  for (ParticleConstIterator it= particles_begin();
+       it != particles_end(); ++it) {
+      ContainersTemp cur= rb_refiner_->get_input_containers(*it);
+      pt.insert(pt.end(), cur.begin(), cur.end());
+  }
+  return pt;
+}
+
+void WeightedExcludedVolumeRestraint::do_show(std::ostream& out) const
+{
+  out<<"WeightedExcludedVolumeRestraint"<<std::endl;
+}
+IMP_LIST_IMPL(WeightedExcludedVolumeRestraint,
+              Particle, particle,Particle*, Particles,
+              {
+              IMP_INTERNAL_CHECK(get_number_of_particles()==0
+                         || obj->get_model()
+                         == (*particles_begin())->get_model(),
+                         "All particles in WeightedExcludedVolumeRestraint"
+                         " must belong to the "
+                         "same Model.");
+              },{},{});
+
+
+IMPEM_END_NAMESPACE
---
+++ modules/multifit/test/test_weighted_excluded_volume_score.py
@@ -0,0 +1,85 @@
+import IMP
+import IMP.test
+import sys
+import IMP.em
+import IMP.multifit
+import IMP.helper
+import unittest
+import os
+import time
+
+class SampleTests(IMP.test.TestCase):
+    """Tests for sampled density maps"""
+
+    def setUp(self):
+        """initialize IMP environment create particles"""
+        IMP.test.TestCase.setUp(self)
+        IMP.set_log_level(IMP.SILENT)
+
+        #init IMP model ( the environment)
+        self.mdl = IMP.Model()
+        self.sel=IMP.atom.CAlphaPDBSelector()
+        self.mhs=IMP.atom.Hierarchies()
+        self.mhs.append(IMP.atom.read_pdb(self.get_input_file_name("1tyq_A.pdb"),
+                             self.mdl,self.sel))
+        self.mhs.append(IMP.atom.read_pdb(self.get_input_file_name("1tyq_D.pdb"),
+                             self.mdl,self.sel))
+        self.voxel_size=1.
+        for mh in self.mhs:
+            IMP.atom.add_radii(mh)
+            IMP.multifit.add_surface_index(mh,self.voxel_size)
+        self.rbs=IMP.helper.set_rigid_bodies(self.mhs)
+        self.trans=[]
+        self.trans.append(IMP.algebra.Transformation3D(IMP.algebra.Rotation3D(1.,0.,0.,0.),IMP.algebra.Vector3D(0.,0.,0.)))
+        self.trans.append(IMP.algebra.Transformation3D(IMP.algebra.Rotation3D(0.995865,-0.0659049,-0.0582095,-0.0228412),IMP.algebra.Vector3D(5.67356,-7.52739,0.57034)))
+        self.trans.append(IMP.algebra.Transformation3D(
+           IMP.algebra.Rotation3D(0.953379,0.073075,0.291331,-0.0292404),
+            IMP.algebra.Vector3D(-29.1857,11.4376,23.9337)))
+        self.trans.append(IMP.algebra.Transformation3D(
+            IMP.algebra.Rotation3D(0.960287,-0.182184,0.142794,0.155778),
+            IMP.algebra.Vector3D(-3.98569,-18.6091,20.9074)))
+
+    def test_transforme_surface(self):
+        '''Test functionality of get_transformed_into'''
+        mh1_surface = IMP.em.particles2surface(IMP.core.get_leaves(self.mhs[0]),1.)
+        mh2_surface = IMP.em.particles2surface(IMP.core.get_leaves(self.mhs[1]),1.)
+
+        for i,tt in enumerate(self.trans):
+            IMP.core.transform(self.rbs[1],tt)
+            st=IMP.em.create_density_map(IMP.atom.get_bounding_box(self.mhs[1]),
+                                         self.voxel_size)
+            IMP.em.get_transformed_into(mh2_surface,tt,st,False)
+            self.assert_(IMP.algebra.get_distance(IMP.core.get_centroid(IMP.core.get_leaves(self.mhs[1])),st.get_centroid())<3.)
+            IMP.core.transform(self.rbs[1],tt.get_inverse())
+            #notice that get_transformed_into provides a different grid
+
+    def test_score_weighted_excluded_volume(self):
+        """Check that weighted excluded volume score works"""
+        mh1_surface = IMP.em.particles2surface(IMP.core.get_leaves(self.mhs[0]),1.)
+        #move chain A and calculate weighted excluded volume
+        ps1=IMP.core.get_leaves(self.mhs[0])
+        scores=[]
+        scores_by_restraint=[]
+        leaves_ref = IMP.core.LeavesRefiner(IMP.atom.Hierarchy.get_traits())
+        fit_r = IMP.multifit.WeightedExcludedVolumeRestraint(self.rbs,
+                                                       leaves_ref)
+        self.mdl.add_restraint(fit_r)
+        for i,t in enumerate(self.trans):
+            IMP.core.transform(self.rbs[1],t)
+            start = time.clock()
+            mh2_surface = IMP.em.particles2surface(IMP.core.get_leaves(self.mhs[1]),1.)
+            val=IMP.em.calculate_intersection_score(mh1_surface,mh2_surface)
+            scores.append(val)
+            scores_by_restraint.append(self.mdl.evaluate(None))
+            IMP.core.transform(self.rbs[1],t.get_inverse())
+        #test that score makes sense
+        for i in range(2):
+            self.assert_(scores[i]<scores[2],"Penetration should be scored badly")
+        self.assertAlmostEqual(scores[3],0.,2)
+        #test that the restraint returns the same scores
+        for i in range(4):
+            self.assertAlmostEqual(scores[i],scores_by_restraint[i],2)
+
+
+if __name__ == '__main__':
+    unittest.main()
