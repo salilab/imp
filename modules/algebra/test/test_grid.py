84e0f2e07ee903595b4c4c519e37f6f35bdabc96
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-08-25 22:57:44 +0000

    a test it
    git-svn-id: https://svn.salilab.org/imp/trunk@6420 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/algebra/test/test_grid.py
@@ -0,0 +1,56 @@
+import unittest
+import IMP.test
+import IMP.algebra
+import StringIO
+import os
+import math
+
+
+class Vector3DTests(IMP.test.TestCase):
+    def test_magnitude(self):
+        """Check sparse grid of ints"""
+        print "construct"
+        g= IMP.algebra.SparseUnboundedIntGrid3D(5.7, IMP.algebra.Vector3D(-4, -5, -6), 0)
+        bb= IMP.algebra.BoundingBox3D(IMP.algebra.Vector3D(-100,-100,-100),
+                                      IMP.algebra.Vector3D(100,100,100))
+        vs=[]
+        for i in range(20):
+            v= IMP.algebra.get_random_vector_in(bb)
+            if g.get_has_index(v):
+                g[v]=g[v]+1
+                self.assert_(g[v] >1)
+            else:
+                g.add_voxel(g.get_extended_index(v), 1)
+                self.assert_(g[v] ==1)
+            vs.append(v)
+        count=0
+        print "list"
+        for i in  g.get_indexes(g.get_extended_index(bb.get_corner(0)),
+                                g.get_extended_index(bb.get_corner(1))):
+            #print g[i]
+            pass
+
+        print "check"
+        for i in g.get_extended_indexes(g.get_extended_index(bb.get_corner(0)),
+                               g.get_extended_index(bb.get_corner(1))):
+            #print i
+            cbb= g.get_bounding_box(i)
+            cbb+=-.1
+            for v in vs:
+                if cbb.get_contains(vs):
+                    self.assert_(g[g.get_index(i)]>0)
+            if g.get_has_index(i):
+                print "adding", i, g[g.get_index(i)]
+                count= count+ g[g.get_index(i)]
+        print "asserting"
+        self.assertEqual(count, len(vs))
+        print "subtracting"
+        for i in  g.get_all_indexes():
+            print i, g[i]
+            #print g[i]
+            count= count-g[i]
+        self.assertEqual(count,0)
+
+
+if __name__ == '__main__':
+    unittest.main()
--- modules/algebra/test/test_plural.py
+++ modules/algebra/test/test_plural.py
@@ -6,11 +6,11 @@ class PluralTests(IMP.test.TestCase):
 
     def test_plural(self):
         """Test all algebra objects have plural version"""
-        return self.assertPlural(IMP.algebra, ['FixedXYZ', 'FixedZYZ', 'LinearFit', 'Matrix2D', 'Matrix3D', 'MultiArray3D', 'NearestNeighbor2D', 'NearestNeighbor3D', 'NearestNeighbor4D', 'ParabolicFit', 'BoundingBox2D', 'BoundingBox3D', 'BoundingBox4D'])
+        return self.assertPlural(IMP.algebra, ['FixedXYZ', 'FixedZYZ', 'LinearFit', 'Matrix2D', 'Matrix3D', 'MultiArray3D', 'NearestNeighbor2D', 'NearestNeighbor3D', 'NearestNeighbor4D', 'ParabolicFit', 'BoundingBox2D', 'BoundingBox3D', 'BoundingBox4D','BoundedGridStorage3D', 'DenseDoubleGrid3D', 'SparseIntGrid3D', 'SparseUnboundedIntGrid3D', 'UnboundedGridStorage3D'])
 
     def test_show(self):
         """Test all algebra objects have show"""
-        return self.assertShow(IMP.algebra, ['AxisAnglePair', 'Matrix3D', 'MultiArray3D', 'NearestNeighbor2D', 'NearestNeighbor3D', 'NearestNeighbor4D'])
+        return self.assertShow(IMP.algebra, ['AxisAnglePair', 'Matrix3D', 'MultiArray3D', 'NearestNeighbor2D', 'NearestNeighbor3D', 'NearestNeighbor4D','BoundedGridStorage3D', 'UnboundedGridStorage3D'])
 
 if __name__ == '__main__':
     unittest.main()
