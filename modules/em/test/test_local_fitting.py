54342d68706fa9a42417d38d14f16b6b461180c3
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-08-26 20:59:05 +0000

    be tollerante to large (outside of density) CG moves
    git-svn-id: https://svn.salilab.org/imp/trunk@6431 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/em/test/test_local_fitting.py
+++ modules/em/test/test_local_fitting.py
@@ -28,29 +28,29 @@ class LocalFittingTest(IMP.test.TestCase):
         """Build test model and optimizer"""
         IMP.test.TestCase.setUp(self)
         IMP.set_log_level(IMP.SILENT)
+        IMP.set_check_level(IMP.NONE)
         self.imp_model = IMP.Model()
         self.load_protein("1z5s_A.pdb")
         self.sample_density_map()
         mhs=IMP.atom.Hierarchies()
         mhs.append(self.mh)
-        self.pdb_opt_state=None#IMP.atom.WritePDBOptimizerState(mhs,"temp_%03d.pdb")
+        self.pdb_opt_state=IMP.atom.WritePDBOptimizerState(mhs,"temp_%03d.pdb")
     def test_fast_vs_slow_local_fitting(self):
         """Check that fast and slow local fitting converge to the same score"""
-        print "calculate fast"
         rand_translation = 15.*IMP.algebra.get_random_vector_in(IMP.algebra.get_unit_bounding_box_3d())
         rt= IMP.algebra.Transformation3D(IMP.algebra.get_identity_rotation_3d(),rand_translation)
         IMP.core.transform(IMP.core.RigidBody(self.mh),rt)
+        print "calculate fast"
         fr_fast=IMP.em.local_rigid_fitting(
             IMP.core.RigidBody(self.mh),
             self.radius_key, self.weight_key,
-            self.scene,self.pdb_opt_state,1,2,100,2.,.3,True)
+            self.scene,self.pdb_opt_state,1,2,50,2.,.3,True)
+        print "calculate slow"
         fr_slow=IMP.em.local_rigid_fitting(
              IMP.core.RigidBody(self.mh),
              self.radius_key, self.weight_key,
              self.scene,None,1,2,50,2.,.3,False)
         print "fast:",fr_fast.get_score(0)," slow:",fr_slow.get_score(0)
-        '''
         self.assert_(abs(fr_fast.get_score(0)-fr_slow.get_score(0))<0.1)
-        '''
 if __name__ == '__main__':
     unittest.main()
