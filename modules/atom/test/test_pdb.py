4725a2466049785d507393db3db7f8fdc6f26883
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-08-30 21:45:17 +0000

    Use correct exception types.
    git-svn-id: https://svn.salilab.org/imp/trunk@6466 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/test/test_pdb.py
+++ modules/atom/test/test_pdb.py
@@ -20,14 +20,14 @@ class PDBReadWriteTest(IMP.test.TestCase):
     def test_bad_read(self):
         """Check that read_pdb behaves OK on invalid files"""
         m= IMP.Model()
-        self.assertRaises(IOError,
+        self.assertRaises(IMP.IOException,
                           IMP.atom.read_pdb,
                           self.get_input_file_name("notafile.pdb"),
                           m)
         # we don't actually check if a file is a pdb or not
         # and can't conclude it is not due to not reading any atoms
         # as the selector may filter them all.
-        self.assertRaises(Error,
+        self.assertRaises(IMP.ValueException,
                           IMP.atom.read_pdb,
                           self.open_input_file("notapdb.pdb"),
                           m)
