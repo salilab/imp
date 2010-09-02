8c806eaf70bed75c71fea2ab117d1fe9b9681fb1
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-09-02 04:39:30 +0000

    use new module
    git-svn-id: https://svn.salilab.org/imp/trunk@6535 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/src/Sphere3D.cpp
+++ modules/algebra/src/Sphere3D.cpp
@@ -7,10 +7,8 @@
 #include <IMP/base_types.h>
 #include <IMP/algebra/Sphere3D.h>
 #include <cmath>
-
 #ifdef IMP_USE_CGAL
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
-#include <CGAL/Min_sphere_of_spheres_d.h>
+#include <IMP/cgal/internal/bounding_sphere.h>
 #endif
 
 IMPALGEBRA_BEGIN_NAMESPACE
@@ -19,33 +17,7 @@ SphereD<3> get_enclosing_sphere(const std::vector<SphereD<3> > &ss) {
   IMP_USAGE_CHECK(!ss.empty(),
                   "Must pass some spheres to have a bounding sphere");
 #ifdef IMP_USE_CGAL
-  typedef CGAL::Exact_predicates_inexact_constructions_kernel             K;
-  typedef CGAL::Min_sphere_of_spheres_d_traits_3<K, K::FT> Traits;
-  typedef CGAL::Min_sphere_of_spheres_d<Traits> Min_sphere;
-  typedef K::Point_3                        Point;
-  typedef Traits::Sphere                    Sphere;
-
-  std::vector<Sphere> spheres;
-  for (unsigned int i=0; i< ss.size(); ++i) {
-    // need cast to resolve ambiguity
-    spheres.push_back(Sphere(Point(ss[i].get_center()[0],
-                                   ss[i].get_center()[1],
-                                   ss[i].get_center()[2]),
-                             ss[i].get_radius()));
-  }
-  Min_sphere ms(spheres.begin(), spheres.end());
-  SphereD<3> s(VectorD<3>(*ms.center_cartesian_begin(),
-                      *(ms.center_cartesian_begin()+1),
-                      *(ms.center_cartesian_begin()+2)),
-              ms.radius());
-   /*IMP_IF_LOG(VERBOSE) {
-     IMP_LOG(VERBOSE, "Enclosing sphere is " << s << " for ");
-     for (unsigned int i=0; i< ss.size(); ++i) {
-       IMP_LOG(VERBOSE, ss[i] << "| ");
-     }
-     IMP_LOG(VERBOSE, std::endl);
-     }*/
-   return s;
+  return cgal::internal::get_enclosing_sphere(ss);
 #else
    BoundingBox3D bb= get_bounding_box(ss[0]);
    for (unsigned int i=1; i< ss.size(); ++i) {
