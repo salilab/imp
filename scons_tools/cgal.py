1423073b72439047cfabb74488b1aa39c5574658
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-09-02 04:40:47 +0000

    now no longer needed
    git-svn-id: https://svn.salilab.org/imp/trunk@6539 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- scons_tools/cgal.py
+++ scons_tools/cgal.py
@@ -2,18 +2,6 @@
 import checks
 
 def _check(context):
-    context.Message('Checking if CGAL needs -frounding-math ...')
-    ret34 = context.TryRun("""#include <CGAL/version.h>
-#include <cstdlib>
-
-        int main()
-        {
-            return CGAL_VERSION_NR >= 1030400000 ? EXIT_SUCCESS : EXIT_FAILURE;
-        }
-        """, '.cpp')[0]
-    if ret34:
-        context.env.Append(CXXFLAGS=['-frounding-math'])
-    context.Result(ret34)
     context.Message('Checking if we need to disable Boost tr1 ...')
     rett = context.TryCompile("""#include <CGAL/tuple.h>
 #include <boost/tr1/tuple.hpp>
@@ -25,7 +13,7 @@ def _check(context):
         """, '.cpp')
     if rett:
         context.env.Append(CPPDEFINES=['BOOST_HAS_GCC_TR1'])
-    context.Result(ret34)
+    context.Result(rett)
     return True
 
 def configure_check(env):
