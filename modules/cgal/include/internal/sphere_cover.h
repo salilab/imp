4be22fb72856706dd449591c4521426d3ce20420
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-09-14 20:19:28 +0000

    add sphere cover to cgal
    git-svn-id: https://svn.salilab.org/imp/trunk@6641 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/cgal/include/internal/sphere_cover.h
@@ -0,0 +1,25 @@
+/**
+ *  \file cgal/internal/polyhedrons.h
+ *  \brief manipulation of text, and Interconversion between text and numbers
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+*/
+
+#ifndef IMPCGAL_INTERNAL_SPHERE_COVER_H
+#define IMPCGAL_INTERNAL_SPHERE_COVER_H
+
+#include "../cgal_config.h"
+#include <IMP/base_types.h>
+#include <IMP/algebra/VectorD.h>
+#include <vector>
+
+
+IMPCGAL_BEGIN_INTERNAL_NAMESPACE
+IMPCGALEXPORT
+void
+refine_unit_sphere_cover_4d(std::vector<algebra::VectorD<4> > &pts,
+                            bool ALL);
+
+
+IMPCGAL_END_INTERNAL_NAMESPACE
+
+#endif  /* IMPCGAL_INTERNAL_SPHERE_COVER_H */
