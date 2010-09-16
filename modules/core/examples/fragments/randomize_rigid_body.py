6b54301339da6de784919ff87bb52e5ca924fee9
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-09-16 22:20:02 +0000

    fix fragment
    git-svn-id: https://svn.salilab.org/imp/trunk@6667 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/examples/fragments/randomize_rigid_body.py
+++ modules/core/examples/fragments/randomize_rigid_body.py
@@ -3,12 +3,12 @@ rbd= IMP.core.RigidBody(p)
 translation=IMP.algebra.get_random_vector_in(IMP.algebra.Vector3D(0,0,0),
                                                  10.0)
 # we don't yet have python code to generate a nearby rotation
-rotation= IMP.algebra.random_rotation()
+rotation= IMP.algebra.get_random_rotation_3d()
 transformation= IMP.algebra.Transformation3D(rotation, translation)
 # note, this overwrites the existing position
 # The True is to transform the members now rather than wait for a
 # score state
-rbd.set_transformation(transformation, True)
+rbd.set_transformation(transformation)
 # to instead perturb the existing transformation instead do
 rbd.set_transformation(IMP.algebra.compose(rbd.get_transformation(),
                                                            transformation))
