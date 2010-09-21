50e7124106affdc9a1041c682786892dfff88480
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-09-21 18:34:46 +0000

    print out used modules
    git-svn-id: https://svn.salilab.org/imp/trunk@6697 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/examples/chain.py
+++ kernel/examples/chain.py
@@ -54,3 +54,6 @@ for i in range(0, confs.get_number_of_configurations()):
     d=IMP.display.ChimeraWriter("solution"+str(i)+".py")
     for p in chain.get_particles():
         d.add_geometry(IMP.display.XYZRGeometry(p))
+
+# print out info about used modules so that the versions are known
+IMP.show_used_modules()
