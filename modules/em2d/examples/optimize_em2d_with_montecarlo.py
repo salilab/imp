bbce703f7cbbfd5129653bb932e0c6d13f147e67
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-09-22 16:07:19 +0000

    fix usage of restraint sets, simplify a few bits of code and fix example paths
    git-svn-id: https://svn.salilab.org/imp/trunk@6714 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/em2d/examples/optimize_em2d_with_montecarlo.py
+++ modules/em2d/examples/optimize_em2d_with_montecarlo.py
@@ -25,9 +25,9 @@ class WriteStatisticsOptimizerScore(IMP.OptimizerState):
         m=o.get_model()
         m.show_restraint_score_statistics()
         m.show_all_statistics()
-        for i in range(0,m.get_number_of_restraints()):
-            r=m.get_restraint(i)
-            print "restraint",r.get_name(),"value",r.evaluate(False)
+        #for i in range(0,m.get_number_of_restraints()):
+        #    r=m.get_restraint(i)
+        #    print "restraint",r.get_name(),"value",r.evaluate(False)
     def do_show(self, stream):
         print >> stream, ps
 
@@ -35,8 +35,7 @@ class WriteStatisticsOptimizerScore(IMP.OptimizerState):
 # Get model from PDB file
 IMP.set_log_level(IMP.TERSE)
 m = IMP.Model()
-ssel = atom.ATOMPDBSelector()
-prot =  atom.read_pdb(em2d.get_example_path("1z5s.pdb"),m,ssel)
+prot =  atom.read_pdb(em2d.get_example_path("1z5s.pdb"),m,atom.ATOMPDBSelector())
 atom.add_radii(prot)
 
 # get the chains
@@ -44,27 +43,25 @@ chains = atom.get_by_type(prot,atom.CHAIN_TYPE)
 print "there are",len(chains),"chains in 1z5s.pdb"
 
 # set the chains as rigid bodies
-chains_centers = []
+native_chain_centers = []
 rigid_bodies= []
 for c in chains:
     atoms=core.get_leaves(c)
-    core.RigidBody.setup_particle(c,atoms)
-    rbd= core.RigidBody(c)
+    rbd=core.RigidBody.setup_particle(c,atoms)
     rigid_bodies.append(rbd)
     print "chain has",rbd.get_number_of_members(), \
                           "atoms","coordinates: ",rbd.get_coordinates()
-    chains_centers.append(rbd.get_coordinates())
+    native_chain_centers.append(rbd.get_coordinates())
 
+bb=alg.BoundingBox3D(alg.Vector3D(-25, -40,-60),
+                         alg.Vector3D( 25,  40, 60))
 # rotate and translate the chains
 for rbd in rigid_bodies:
     # rotation
     rotation= alg.get_random_rotation_3d()
     transformation1=alg.get_rotation_about_point(rbd.get_coordinates(),rotation)
     # translation
-    x=random.uniform(-25,25)
-    y=random.uniform(-40,40)
-    z=random.uniform(-60,60)
-    transformation2=alg.Transformation3D(alg.Vector3D(x,y,z))
+    transformation2=alg.Transformation3D(alg.get_random_vector_in(bb))
     # Apply
     final_transformation = alg.compose(transformation1,transformation2)
     core.transform(rbd,final_transformation)
@@ -73,16 +70,16 @@ atom.write_pdb (prot,"1z5s-transformed.pdb")
 
 # set distance restraints measusring some distances between rigid bodies
 # for the solution.
-d01 = alg.get_distance(chains_centers[0],chains_centers[1])
+d01 = alg.get_distance(native_chain_centers[0],native_chain_centers[1])
 r01 = core.DistanceRestraint(core.Harmonic(d01,1),chains[0],chains[1])
 r01.set_name("distance 0-1")
-d12 = alg.get_distance(chains_centers[1],chains_centers[2])
+d12 = alg.get_distance(native_chain_centers[1],native_chain_centers[2])
 r12 = core.DistanceRestraint(core.Harmonic(d12,1),chains[1],chains[2])
 r12.set_name("distance 1-2")
-d23 = alg.get_distance(chains_centers[2],chains_centers[3])
+d23 = alg.get_distance(native_chain_centers[2],native_chain_centers[3])
 r23 = core.DistanceRestraint(core.Harmonic(d23,1),chains[2],chains[3])
 r23.set_name("distance 2-3")
-d30 = alg.get_distance(chains_centers[3],chains_centers[0])
+d30 = alg.get_distance(native_chain_centers[3],native_chain_centers[0])
 r30 = core.DistanceRestraint(core.Harmonic(d30,1),chains[3],chains[0])
 r30.set_name("distance 3-0")
 print "Distances in the solution: d01 =", \
@@ -90,19 +87,15 @@ print "Distances in the solution: d01 =", \
 
 # set distance restraints
 print "adding distance restraints "
-distance_restraints = [r01,r12,r23,r30]
-distance_restraints_set = IMP.RestraintSet()
-for r in distance_restraints:
-    distance_restraints_set.add_restraint(r)
-distance_restraints_set.set_weight(1)
-for i in range(0,distance_restraints_set.get_number_of_restraints()):
-    m.add_restraint(distance_restraints_set.get_restraint(i))
+for r in [r01,r12,r23,r30]:
+    m.add_restraint(r)
 print "model has ",m.get_number_of_restraints(),"restraints"
 
 # set em2D restraint
 srw = em.SpiderImageReaderWriter()
-selection_file="all-1z5s-projections.sel"
+selection_file=em2d.get_example_path("all-1z5s-projections.sel")
 images_to_read_names=em2d.read_selection_file(selection_file)
+print images_to_read_names
 em_images =em.read_images(images_to_read_names,srw)
 print len(em_images),"images read"
 
@@ -123,8 +116,7 @@ em2d_restraints_set.add_restraint(em2d_restraint)
 em2d_restraints_set.set_weight(1000) # weight for the em2D restraint
 
 print "adding em2d restraint "
-for i in range(0,em2d_restraints_set.get_number_of_restraints()):
-    m.add_restraint(em2d_restraints_set.get_restraint(i))
+m.add_restraint(em2d_restraints_set)
 # Add all restraints to a model
 print "model has ",m.get_number_of_restraints(),"restraints"
 
