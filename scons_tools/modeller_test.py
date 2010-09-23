374cb639b7773c1b574543eefe332f158a651622
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-09-23 05:45:19 +0000

    Don't needlessly create a temporary file when a pipe will do (does not work on Python 2.3 anyway).
    git-svn-id: https://svn.salilab.org/imp/trunk@6740 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- scons_tools/modeller_test.py
+++ scons_tools/modeller_test.py
@@ -3,21 +3,7 @@
 import os
 import os.path
 import re
-
-# On older Pythons that don't have subprocess, fall back to os.popen3
-try:
-    import subprocess
-    def MyPopen(cmd):
-        return subprocess.Popen(cmd, shell=True, stdin=subprocess.PIPE,
-                                stdout=subprocess.PIPE, stderr=subprocess.PIPE,
-                                close_fds=True)
-except ImportError:
-    class MyPopen(object):
-        def __init__(self, cmd):
-            (self.stdin, self.stdout, self.stderr) \
-                = os.popen3(cmd, 't', -1)
-        def wait(self):
-            return 0
+from mypopen import MyPopen
 
 def _check_default(context):
     """Check for MODELLER in the default Python path"""
