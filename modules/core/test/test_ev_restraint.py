017dd67646ff5f59851bc6068b57ed546917196d
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-09-23 16:04:01 +0000

    and one more
    git-svn-id: https://svn.salilab.org/imp/trunk@6752 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/test/test_ev_restraint.py
+++ modules/core/test/test_ev_restraint.py
@@ -12,23 +12,18 @@ class AngleRestraintTests(IMP.test.TestCase):
         m= IMP.Model()
         print "read"
         p0= IMP.atom.read_pdb(self.get_input_file_name("input.pdb"), m)
-        p1= IMP.atom.read_pdb(self.get_input_file_name("input.pdb"), m)
         print 'create'
         r0s= IMP.core.RigidBody.setup_particle(p0.get_particle(),
                                         IMP.core.XYZs(IMP.core.get_leaves(p0)))
-        r1s= IMP.core.RigidBody.setup_particle(p1.get_particle(),
-                                        IMP.core.XYZs(IMP.core.get_leaves(p1)))
         print "radius"
-        for p in IMP.core.get_leaves(p0)+IMP.core.get_leaves(p1):
+        for p in IMP.core.get_leaves(p0):
             #print p.get_particle().get_name()
             d= IMP.core.XYZR.setup_particle(p.get_particle())
             d.set_radius(1)
         rb0= IMP.core.RigidBody(p0.get_particle())
-        rb1= IMP.core.RigidBody(p1.get_particle())
         rb0.set_coordinates_are_optimized(True)
-        rb1.set_coordinates_are_optimized(True)
         sc= IMP.container.ListSingletonContainer(m)
-        fps=IMP.core.XYZRs()
+        fps=[]
         for i in range(0,10):
             p= IMP.Particle(m)
             d= IMP.core.XYZR.setup_particle(p)
@@ -41,14 +36,13 @@ class AngleRestraintTests(IMP.test.TestCase):
             d.set_coordinates(IMP.algebra.get_random_vector_in(IMP.algebra.BoundingBox3D(IMP.algebra.Vector3D(0,0,0),
                                                                IMP.algebra.Vector3D(5,5,5))))
         sc.add_particle(rb0.get_particle())
-        sc.add_particle(rb1.get_particle())
         r= IMP.core.ExcludedVolumeRestraint(sc, IMP.core.LeavesRefiner(IMP.atom.Hierarchy.get_traits()))
         r.set_log_level(IMP.SILENT)
         m.add_restraint(r)
-        return (m, r)
+        return (m, r, sc)
     def test_ev(self):
         """Testing excluded volume restraint"""
-        (m,r)= self._setup_ev_restraint();
+        (m,r, sc)= self._setup_ev_restraint();
         print "mc"
         o= IMP.core.MonteCarlo()
         # very dumb op
@@ -58,17 +52,30 @@ class AngleRestraintTests(IMP.test.TestCase):
         print "opt"
         o.optimize(10)
         print "inspect"
-        for pa in fps+ IMP.core.XYZRs(IMP.core.get_leaves(p1)):
-            for pb in fps+ IMP.core.XYZRs(IMP.core.get_leaves(p0)):
+        balls=[]
+        members=[]
+        for p in sc.get_particles():
+            if IMP.atom.Hierarchy.particle_is_instance(p):
+                balls=balls+IMP.core.get_leaves(IMP.atom.Hierarchy(p))
+                members.append(IMP.core.get_leaves(IMP.atom.Hierarchy(p)))
+            else:
+                balls.append(p)
+        for pa in balls:
+            for pb in balls:
                 if pa == pb: continue
                 else:
+                    found=False
+                    for ml in members:
+                        if pa in ml and pb in ml:
+                            found=True
                     #print pa
                     #print pb
-                    d= IMP.core.get_distance(pa, pb)
-                    self.assertGreater(d, -.1)
+                    if not found:
+                        d= IMP.core.get_distance(IMP.core.XYZR(pa), IMP.core.XYZR(pb))
+                        self.assertGreater(d, -.1)
     def test_isolated_ev(self):
         """Testing isolated evaluation of ev restraint"""
-        (m,r)= self._setup_ev_restraint()
+        (m,r, sc)= self._setup_ev_restraint()
         IMP.set_log_level(IMP.VERBOSE)
         v= r.evaluate(False)
         self.assertNotEqual(v, 0)
