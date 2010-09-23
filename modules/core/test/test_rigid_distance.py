759dd4a270b45895c355a197bb47e2ce50fcb663
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-09-23 16:03:36 +0000

    fix another test
    git-svn-id: https://svn.salilab.org/imp/trunk@6751 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/test/test_rigid_distance.py
+++ modules/core/test/test_rigid_distance.py
@@ -19,14 +19,12 @@ class Test(IMP.test.TestCase):
         p1= IMP.atom.read_pdb(self.get_input_file_name("input.pdb"), m)
         IMP.atom.add_radii(p0)
         IMP.atom.add_radii(p1)
-        s0= IMP.helper.setup_as_rigid_body(p0)
-        m.add_score_state(s0)
-        s1= IMP.helper.setup_as_rigid_body(p1)
-        m.add_score_state(s1)
-        IMP.core.RigidBody(p0.get_particle()).set_transformation(IMP.algebra.Transformation3D(IMP.algebra.identity_rotation(), IMP.algebra.Vector3D(100,100,100)))
+        rb0= IMP.atom.setup_as_rigid_body(p0)
+        rb1= IMP.atom.setup_as_rigid_body(p1)
+        IMP.core.RigidBody(p0.get_particle()).set_transformation(IMP.algebra.Transformation3D(IMP.algebra.get_identity_rotation_3d(), IMP.algebra.Vector3D(100,100,100)))
         sdps= IMP.core.SphereDistancePairScore(IMP.core.Linear(0,1))
         rdps= IMP.core.RigidBodyDistancePairScore(sdps, IMP.core.LeavesRefiner(IMP.atom.Hierarchy.get_traits()))
-        v= rdps.evaluate(p0.get_particle(), p1.get_particle(), None)
+        v= rdps.evaluate((p0.get_particle(), p1.get_particle()), None)
         dm= 1000000
         bp=None
         for l0 in IMP.core.get_leaves(p0):
