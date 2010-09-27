a1c9e37aca7f9eda0a220c89f9abb4d287c2aadc
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-09-27 23:52:52 +0000

    move some graph algo code into kernel so it can be used with importing domino2
    git-svn-id: https://svn.salilab.org/imp/trunk@6777 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/include/internal/graph_utility.h
+++ kernel/include/internal/graph_utility.h
@@ -16,6 +16,14 @@
 #include <algorithm>
 #include <sstream>
 #include <boost/graph/graphviz.hpp>
+#include <boost/graph/depth_first_search.hpp>
+#include <boost/graph/reverse_graph.hpp>
+#if BOOST_VERSION > 103900
+#include <boost/property_map/property_map.hpp>
+#else
+#include <boost/property_map.hpp>
+#include <boost/vector_property_map.hpp>
+#endif
 
 
 IMP_BEGIN_INTERNAL_NAMESPACE
@@ -61,6 +69,63 @@ Map<Base*, int> get_graph_index(const Graph &g) {
     }
   }
   return ret;
+}
+
+
+
+
+struct AncestorException{
+  Object *o;
+  AncestorException(Object *oi): o(oi){};
+};
+
+// gcc 4.2 objects if this does not have external linkage
+template <class Graph>
+class AncestorVisitor: public boost::default_dfs_visitor {
+  std::set<Particle*> pst_;
+  typename boost::property_map<Graph,
+                               boost::vertex_name_t>::const_type vm_;
+public:
+  AncestorVisitor(){}
+  AncestorVisitor(const ParticlesTemp& pst,
+                  const Graph&g): pst_(pst.begin(), pst.end()),
+                                  vm_(boost::get(boost::vertex_name, g)){}
+  void discover_vertex(typename boost::graph_traits<Graph>::vertex_descriptor u,
+                       const Graph& g) {
+    Object *o= vm_[u];
+    //std::cout << "Visiting " << o->get_name() << std::endl;
+    if (pst_.find(dynamic_cast<Particle*>(o)) != pst_.end()) {
+      throw AncestorException(o);
+    }
+  }
+  /*template <class Edge>
+  void tree_edge(Edge e, const Graph &g) {
+    typename boost::graph_traits<Graph>::vertex_descriptor s=
+      = boost::source(e, g);
+    typename boost::graph_traits<Graph>::vertex_descriptor t
+      = boost::target(e, g);
+    std::cout << "Tree edge " << vm_[s]->get_name()
+      << "->" << vm_[t]->get_name() << std::endl;
+      }*/
+};
+
+template <class G>
+bool get_has_ancestor(const G &g,
+                      unsigned int v,
+                      const ParticlesTemp &pst) {
+  typedef boost::reverse_graph<G>  RG;
+  RG rg(g);
+  AncestorVisitor<RG> av(pst,g);
+  boost::vector_property_map<int> color(boost::num_vertices(g));
+  try {
+    //std::cout << "Searching for dependents of " << v << std::endl;
+    boost::depth_first_visit(rg, v, av, color);
+    return false;
+  } catch (AncestorException e) {
+    /*IMP_LOG(VERBOSE, "Vertex has ancestor \"" << e.o->get_name()
+      << "\"" << std::endl);*/
+    return true;
+  }
 }
 
 IMP_END_INTERNAL_NAMESPACE
