f0022e288dc4621b683ca39b8f61d9599250f065
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-10-07 19:24:08 +0000

    r6799 seems to have completely broken ClosePairsPairScore (always returns 0 pairs); roll back.
    git-svn-id: https://svn.salilab.org/imp/trunk@6842 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/include/ClosePairsPairScore.h
+++ modules/core/include/ClosePairsPairScore.h
@@ -22,8 +22,9 @@ IMPCORE_BEGIN_NAMESPACE
 /** The Refiner implicitly defines a tree rooted at each particle.
     This PairScore applies another PairScore to all pairs of leaves, one
     taken from each tree such that the leaves are closer than the threshold.
-
-    \note Either CGAL or ANN must be installed for this to be efficient.
+    \note All particles in the tree must be XYZR particles for the
+    passed radius. In addition, the ball defined by a particle must contain
+    the balls of all its leaves.
     \see ClosePairsScoreState
  */
 class IMPCOREEXPORT ClosePairsPairScore : public PairScore
@@ -31,7 +32,6 @@ class IMPCOREEXPORT ClosePairsPairScore : public PairScore
   IMP::internal::OwnerPointer<Refiner> r_;
   IMP::internal::OwnerPointer<PairScore> f_;
   Float th_;
-  int k_;
   FloatKey rk_;
 public:
   /** \param[in] r The Refiner to call on each particle
@@ -41,11 +41,8 @@ public:
       \param[in] rk The key to use for the radius.
    */
   ClosePairsPairScore(Refiner *r, PairScore *f,
-                      Float max_distance);
-  /** only score the k closest pairs.
-   */
-  ClosePairsPairScore(Refiner *r, PairScore *f,
-                      int k=1);
+                      Float max_distance,
+                      FloatKey rk= XYZR::get_default_radius_key());
   IMP_PAIR_SCORE(ClosePairsPairScore);
 };
 
--- modules/core/src/ClosePairsPairScore.cpp
+++ modules/core/src/ClosePairsPairScore.cpp
@@ -10,30 +10,77 @@
 
 #include <IMP/exception.h>
 #include <IMP/log.h>
-#include <IMP/algebra/vector_search.h>
+
 #include <cmath>
 
 IMPCORE_BEGIN_NAMESPACE
 
 ClosePairsPairScore::ClosePairsPairScore(Refiner *r,
                                          PairScore *f,
-                                         Float thre): r_(r), f_(f),
-                                                       th_(thre){
-  k_=0;
+                                         Float thre,
+                                         FloatKey rk): r_(r), f_(f),
+                                                       th_(thre), rk_(rk){
   IMP_USAGE_CHECK(thre >= 0, "The threshold must be non-negative.");
+  IMP_USAGE_CHECK(rk != FloatKey(), "The radius-key must be non-default. "
+            << "This is primarily a matter of laziness.");
 }
-
-
-ClosePairsPairScore::ClosePairsPairScore(Refiner *r,
-                                         PairScore *f,
-                                         int k): r_(r), f_(f),
-                                                 k_(k)
-{
-}
-
 
 namespace {
+  ParticlePairsTemp get_close_pairs(Particle *a, Particle *b, double threshold,
+                                    Refiner *r) {
+    ParticlePairsTemp queue;
+    ParticlePairsTemp ret;
+    queue.push_back(ParticlePair(a,b));
+    do {
+      ParticlePair pp= queue.back();
+      queue.pop_back();
+      XYZR d0(pp[0]), d1(pp[1]);
+      if (get_distance(d0, d1) > threshold) {
+        IMP_LOG(VERBOSE, "Particles "
+                << d0 << " and " << d1
+                << " are too far apart to refine "
+                << " > " << threshold<< std::endl);
+        continue;
+      }
+    // may want to just refined 1 is th_ is nonzero
+    ParticlesTemp ps0, ps1;
+    if (!r->get_can_refine(pp[0])
+        && !r->get_can_refine(pp[1])) {
+      ret.push_back(pp);
+    } else {
+      if (r->get_can_refine(pp[0])) {
+        ps0= r->get_refined(pp[0]);
+      } else {
+        ps0.push_back(pp[0]);
+      }
+      if (r->get_can_refine(pp[1])) {
+        ps1= r->get_refined(pp[1]);
+      } else {
+        ps1.push_back(pp[1]);
+      }
+      for (unsigned int i=0; i< ps0.size(); ++i) {
+        for (unsigned int j=0; j< ps1.size(); ++j) {
+          queue.push_back(ParticlePair(ps0[i], ps1[j]));
+        }
+      }
+    }
+  } while (!queue.empty());
+    return ret;
+  }
+}
 
+double ClosePairsPairScore::evaluate(const ParticlePair &p,
+                                     DerivativeAccumulator *da) const
+{
+  ParticlePairsTemp ppt= get_close_pairs(p[0], p[1], th_, r_);
+  double ret=0;
+  for (unsigned int i=0; i< ppt.size(); ++i) {
+    ret+= f_->evaluate(ppt[i], da);
+  }
+  return ret;
+}
+
+namespace {
   ParticlesTemp expand(Particle* a, Refiner *r) {
     ParticlesTemp ret;
     ret.push_back(a);
@@ -51,46 +98,6 @@ namespace {
     return ret;
   }
 }
-
-double ClosePairsPairScore::evaluate(const ParticlePair &p,
-                                     DerivativeAccumulator *da) const
-{
-  XYZsTemp xyzs0(expand(p[0], r_));
-  XYZsTemp xyzs1(expand(p[1], r_));
-  algebra::NearestNeighborD<3> nn(xyzs0.begin(),
-                                  xyzs0.end());
-  ParticlePairsTemp ppt;
-  if (k_>0) {
-    algebra::internal::MinimalSet<double, ParticlePair> ms(k_);
-    for (unsigned int i=0; i< xyzs1.size(); ++i) {
-      Ints is= nn.get_nearest_neighbors(xyzs1[i].get_coordinates(),
-                                       k_);
-      for (unsigned int j=0; j< is.size(); ++j) {
-        double d= get_distance(xyzs0[is[j]], xyzs1[i]);
-        if (ms.can_insert(d)) {
-          ms.insert(d, ParticlePair(xyzs0[is[j]], xyzs1[i]));
-        }
-      }
-    }
-    for (unsigned int i=0; i< ms.size(); ++i) {
-      ppt.push_back(ms[i].second);
-    }
-  } else {
-    for (unsigned int i=0; i< xyzs1.size(); ++i) {
-      Ints is= nn.get_in_ball(xyzs1[i].get_coordinates(),
-                              th_);
-      for (unsigned int j=0; j< is.size(); ++j) {
-        ppt.push_back(ParticlePair(xyzs0[is[j]], xyzs1[i]));
-      }
-    }
-  }
-  double ret=0;
-  for (unsigned int i=0; i< ppt.size(); ++i) {
-    f_->evaluate(ppt[i], da);
-  }
-  return ret;
-}
-
 
 ParticlesTemp ClosePairsPairScore
 ::get_input_particles(Particle *p) const {
