8bafa352b2301990df255c5f715372dcefc6b2da
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-10-12 17:37:53 +0000

    clean up and clarify the close pairs score
    git-svn-id: https://svn.salilab.org/imp/trunk@6873 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/src/ClosePairsPairScore.cpp
+++ modules/core/src/ClosePairsPairScore.cpp
@@ -12,6 +12,9 @@
 #include <IMP/log.h>
 #include <IMP/algebra/vector_search.h>
 #include <IMP/core/internal/rigid_body_tree.h>
+#include <IMP/core/internal/close_pairs_helpers.h>
+#include <IMP/core/internal/CoreListSingletonContainer.h>
+#include <IMP/algebra/internal/MinimalSet.h>
 #include <cmath>
 
 IMPCORE_BEGIN_NAMESPACE
@@ -19,55 +22,29 @@ IMPCORE_BEGIN_NAMESPACE
 ClosePairsPairScore::ClosePairsPairScore(PairScore *f,
                                          Refiner *r,
                                          Float thre): r_(r), f_(f),
-                                                      th_(thre), k_(0){
-  k_=0;
+                                                      th_(thre){
   IMP_USAGE_CHECK(thre >= 0, "The threshold must be non-negative.");
+
+  cpf_=internal::default_cpf();
+  ca_= new internal::CoreListSingletonContainer();
+  cb_= new internal::CoreListSingletonContainer();
 }
 
 
-ClosePairsPairScore::ClosePairsPairScore(PairScore *f,
-                                         Refiner *r,
-                                         int k): r_(r), f_(f),
-                                                 k_(k)
+KClosePairsPairScore::KClosePairsPairScore(PairScore *f,
+                                           Refiner *r,
+                                           int k): r_(r), f_(f),
+                                                   k_(k)
 {
+  last_distance_=1;
+  cpf_=internal::default_cpf();
+  ca_= new internal::CoreListSingletonContainer();
+  cb_= new internal::CoreListSingletonContainer();
 }
 
 
 
 namespace {
-  ParticlePair get_closest_pair(Particle *a, Particle *b,
-                                Refiner* ra, Refiner *rb, ObjectKey ka,
-                                ObjectKey kb) {
-    internal::RigidBodyHierarchy *da=NULL, *db=NULL;
-    if (RigidBody::particle_is_instance(a)) {
-      da= internal::get_rigid_body_hierarchy(RigidBody(a), ka);
-    }
-    if (RigidBody::particle_is_instance(b)) {
-      db= internal::get_rigid_body_hierarchy(RigidBody(b), kb);
-    }
-    if (!da && !db) {
-      return ParticlePair(a,b);
-    } else if (!da) {
-      ParticlesTemp psb=rb->get_refined(b);
-      IMP::internal::Set<Particle*> pb(psb.begin(), psb.end());
-      Particle *p= internal::closest_particle(db, pb,
-                                              XYZR(a));
-      return ParticlePair(a,p);
-    } else if (!db) {
-      ParticlesTemp psa=ra->get_refined(a);
-      IMP::internal::Set<Particle*> pa(psa.begin(), psa.end());
-      Particle *p= internal::closest_particle(da, pa, XYZR(b));
-      return ParticlePair(p,b);
-    } else {
-      ParticlesTemp psa=ra->get_refined(a);
-      IMP::internal::Set<Particle*> pa(psa.begin(), psa.end());
-      ParticlesTemp psb=rb->get_refined(b);
-      IMP::internal::Set<Particle*> pb(psb.begin(), psb.end());
-      ParticlePair pp= internal::closest_pair(da, pa,
-                                              db, pb);
-      return pp;
-    }
-  }
   ParticlesTemp expand(Particle *p, Refiner *r) {
     if (r->get_can_refine(p)) {
       return r->get_refined(p);
@@ -75,58 +52,82 @@ namespace {
       return ParticlesTemp(1,p);
     }
   }
+  void fill_close_pairs(ClosePairsFinder* cpf,
+                        internal::CoreListSingletonContainer* ca,
+                        internal::CoreListSingletonContainer* cb,
+                        double dist,
+                        const ParticlePair &p,
+                        Refiner* r,
+                        Floats &distances,
+                        ParticlePairsTemp &pairs) {
+    ParticlesTemp pa= expand(p[0], r);
+    ParticlesTemp pb= expand(p[1], r);
+    ca->set_particles(pa);
+    cb->set_particles(pb);
+    cpf->set_distance(dist);
+    ParticlePairsTemp ppt= cpf->get_close_pairs(ca, cb);
+    for (unsigned int i=0; i< ppt.size(); ++i) {
+      double d=get_distance(XYZR(ppt[i][0]), XYZR(ppt[i][1]));
+      if (d < dist) {
+        distances.push_back(d);
+        pairs.push_back(ppt[i]);
+      }
+    }
+  }
+  double do_evaluate(const ParticlePairsTemp &ppt,
+                     PairScore *ps,
+                     DerivativeAccumulator *da) {
+    double ret=0;
+    for (unsigned int i=0; i< ppt.size(); ++i) {
+      ret+=ps->evaluate(ppt[i], da);
+    }
+    return ret;
+  }
+  double max_radius(const ParticlesTemp &ps) {
+    double mr=0;
+    for (unsigned int i=0; i< ps.size(); ++i) {
+      mr = std::max(mr, XYZR(ps[i]).get_radius());
+    }
+    return mr;
+  }
 }
 
 double ClosePairsPairScore::evaluate(const ParticlePair &p,
                                      DerivativeAccumulator *da) const
 {
-  if (k_==1 && RigidBody::particle_is_instance(p[0])
-      && RigidBody::particle_is_instance(p[1])) {
-    ParticlePair pp= get_closest_pair(p[0], p[1], r_, r_,
-                                      internal::get_rigid_body_hierarchy_key(),
-                                      internal::get_rigid_body_hierarchy_key());
-    IMP_LOG(VERBOSE, "Closest rigid body pair for bodies "
-            << p[0]->get_name() << " and " << p[1]->get_name()
-            << " is " << pp[0]->get_name() << " and " << pp[1]->get_name()
-            << " with coordinates " << XYZ(pp[0]) << " and " << XYZ(pp[1])
-            << std::endl);
-    return f_->evaluate(pp, da);
-  } else {
-    XYZsTemp xyzs0(expand(p[0], r_));
-    XYZsTemp xyzs1(expand(p[1], r_));
-    algebra::NearestNeighborD<3> nn(xyzs0.begin(),
-                                    xyzs0.end());
-    ParticlePairsTemp ppt;
-    if (k_>0) {
-      algebra::internal::MinimalSet<double, ParticlePair> ms(k_);
-      for (unsigned int i=0; i< xyzs1.size(); ++i) {
-        Ints is= nn.get_nearest_neighbors(xyzs1[i].get_coordinates(),
-                                          k_);
-        for (unsigned int j=0; j< is.size(); ++j) {
-          double d= get_distance(xyzs0[is[j]], xyzs1[i]);
-          if (ms.can_insert(d)) {
-          ms.insert(d, ParticlePair(xyzs0[is[j]], xyzs1[i]));
-          }
-        }
-      }
-      for (unsigned int i=0; i< ms.size(); ++i) {
-        ppt.push_back(ms[i].second);
-      }
-    } else {
-      for (unsigned int i=0; i< xyzs1.size(); ++i) {
-        Ints is= nn.get_in_ball(xyzs1[i].get_coordinates(),
-                                th_);
-        for (unsigned int j=0; j< is.size(); ++j) {
-          ppt.push_back(ParticlePair(xyzs0[is[j]], xyzs1[i]));
-        }
-      }
+  ParticlePairsTemp ppt;
+  Floats dists;
+  fill_close_pairs(cpf_, ca_, cb_, th_, p, r_, dists, ppt);
+  return do_evaluate(ppt, f_, da);
+}
+
+
+double KClosePairsPairScore::evaluate(const ParticlePair &p,
+                                     DerivativeAccumulator *da) const {
+  //double mr= std::max(max_radius(psa), max_radius(psb));
+  ParticlePairsTemp ppt;
+  Floats dists;
+  double dist=last_distance_;
+  do {
+    fill_close_pairs(cpf_, ca_, cb_, dist, p, r_, dists, ppt);
+    dist*=2;
+  } while (ppt.size() < static_cast<unsigned int>(k_));
+  algebra::internal::MinimalSet<double, ParticlePair> ms(k_);
+  for (unsigned int i=0; i< ppt.size(); ++i) {
+    if (ms.can_insert(dists[i])) {
+      ms.insert(dists[i], ppt[i]);
     }
-    double ret=0;
-    for (unsigned int i=0; i< ppt.size(); ++i) {
-      ret+=f_->evaluate(ppt[i], da);
-    }
-    return ret;
   }
+  if (ppt.size() > static_cast<unsigned int>(k_*2)) {
+    last_distance_= dist*.25;
+  } else {
+    last_distance_= dist*.5;
+  }
+  ParticlePairsTemp retps;
+  for (unsigned int i=0; i < ms.size(); ++i) {
+    retps.push_back(ms[i].second);
+  }
+  return do_evaluate(retps, f_, da);
 }
 
 
@@ -162,5 +163,40 @@ void ClosePairsPairScore::do_show(std::ostream &out) const
   out << "function " << *f_;
   out << "\nrefiner " << *r_ << std::endl;
 }
+
+
+ParticlesTemp KClosePairsPairScore
+::get_input_particles(Particle *p) const {
+  ParticlesTemp ret;
+  ParticlesTemp ea=expand(p, r_);
+  for (unsigned int i=0; i< ea.size(); ++i) {
+    ParticlesTemp c= f_->get_input_particles(ea[i]);
+    ret.insert(ret.end(), c.begin(), c.end());
+  }
+  return ret;
+}
+
+ContainersTemp KClosePairsPairScore
+::get_input_containers(Particle *p) const {
+  ContainersTemp ret= r_->get_input_containers(p);
+  ParticlesTemp ea=expand(p, r_);
+  for (unsigned int i=0; i< ea.size(); ++i) {
+    ContainersTemp c= f_->get_input_containers(ea[i]);
+    ret.insert(ret.end(), c.begin(), c.end());
+  }
+  return ret;
+}
+
+
+bool KClosePairsPairScore::get_is_changed(const ParticlePair &) const {
+  return true;
+}
+
+void KClosePairsPairScore::do_show(std::ostream &out) const
+{
+  out << "function " << *f_;
+  out << "\nrefiner " << *r_ << std::endl;
+}
+
 
 IMPCORE_END_NAMESPACE
