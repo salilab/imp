d6686de6f01f050372e52206733779a8bdfa1743
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-10-14 16:42:53 +0000

    make sure overloading is happy
    git-svn-id: https://svn.salilab.org/imp/trunk@6892 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/test/test_xyzr.py
+++ modules/core/test/test_xyzr.py
@@ -17,7 +17,26 @@ class DecoratorTests(IMP.test.TestCase):
         db.set_coordinates(IMP.algebra.Vector3D(6,6,6))
         d= IMP.core.get_distance(da, db)
         self.assertAlmostEqual(d, 10.3-2.5, delta=.5)
-
+        nrda= IMP.core.XYZ(pa)
+        nrdb= IMP.core.XYZ(pb)
+    def test_xyzr_overload(self):
+        """ Testing XYZR overload decorators"""
+        m = IMP.Model()
+        pa=IMP.Particle(m)
+        pb=IMP.Particle(m)
+        da= IMP.core.XYZR.setup_particle(pa)
+        db= IMP.core.XYZR.setup_particle(pb)
+        da.set_radius(1.0)
+        db.set_radius(1.5)
+        da.set_coordinates(IMP.algebra.Vector3D(0,0,0))
+        db.set_coordinates(IMP.algebra.Vector3D(6,6,6))
+        d= IMP.core.get_distance(da, db)
+        self.assertAlmostEqual(d, 10.3-2.5, delta=.5)
+        nrda= IMP.core.XYZ(pa)
+        nrdb= IMP.core.XYZ(pb)
+        dbr= IMP.core.get_distance(nrda, nrdb)
+        print d, dbr
+        self.assert_(d+2.4 < dbr)
 
 
 if __name__ == '__main__':
