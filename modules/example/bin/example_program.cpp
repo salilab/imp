af286c5bcc5bc05dc150f38f4a65b5de462bcde9
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-10-19 23:01:10 +0000

    fix name
    git-svn-id: https://svn.salilab.org/imp/trunk@6909 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/example/bin/example_program.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright 2007-2010 IMP Inventors. All rights reserved.
+ */
+
+#include <IMP.h>
+#include <IMP/core.h>
+#include <IMP/example/ExampleRestraint.h>
+#include <IMP/container/ListPairContainer.h>
+
+using namespace IMP;
+int main(){
+  set_log_level(VERBOSE);
+  WeakPointer<Model> m(new Model());
+  Particle* p0= new Particle(m);
+  core::XYZ d0= core::XYZ::setup_particle(p0);
+  Particle* p1= new Particle(m);
+  core::XYZ d1= core::XYZ::setup_particle(p1);
+  example::ExampleRestraint *r0= new example::ExampleRestraint(p0, 1);
+  example::ExampleRestraint *r1= new example::ExampleRestraint(p1, 1);
+  d0.set_coordinates(algebra::VectorD<3>(0,0,2));
+  d1.set_coordinates(algebra::VectorD<3>(0,0,3));
+
+  m->add_restraint(r0);
+  m->add_restraint(r1);
+  return 0;
+}
