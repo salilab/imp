c860131a450da55f5ec9e4e81b2b03c9f9f80db4
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-10-22 05:14:45 +0000

    test k close pairs
    git-svn-id: https://svn.salilab.org/imp/trunk@6959 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/test/test_close_pairs_ps.py
+++ modules/core/test/test_close_pairs_ps.py
@@ -2,6 +2,7 @@ import IMP
 import IMP.test
 import IMP.misc
 import IMP.core
+import itertools
 
 rk = IMP.FloatKey("radius")
 
@@ -40,14 +41,14 @@ class Test(IMP.test.TestCase):
         cpr= IMP.core.LeavesRefiner(r0.get_traits())
         lps= IMP.misc.LogPairScore()
         cpps= IMP.core.ClosePairsPairScore(lps, cpr, threshold)
-        cpps.evaluate(IMP.ParticlePair(r0.get_particle(), r1.get_particle()), None)
+        cpps.evaluate((r0, r1), None)
         print str(len(lps.get_particle_pairs())) +" pairs", "in", threshold
         for pp in lps.get_particle_pairs():
             print pp[0].get_name(), pp[1].get_name()
         for l0 in ls0:
             for l1 in ls1:
-                d0= IMP.core.XYZ.decorate_particle(l0.get_particle())
-                d1= IMP.core.XYZ.decorate_particle(l1.get_particle())
+                d0= IMP.core.XYZR.decorate_particle(l0.get_particle())
+                d1= IMP.core.XYZR.decorate_particle(l1.get_particle())
                 if (IMP.core.get_distance(d0, d1) < .95*threshold):
                     print "looking for", l0.get_particle().get_name() + " " \
                         + l1.get_particle().get_name(), IMP.core.get_distance(d0, d1)
@@ -56,6 +57,39 @@ class Test(IMP.test.TestCase):
                 else:
                     self.assertFalse(lps.get_contains(IMP.ParticlePair(l0.get_particle(),
                                                                        l1.get_particle())))
+    def test_rops2(self):
+        """Checking k-close pairs pair score"""
+        IMP.set_log_level(IMP.VERBOSE)
+        m= IMP.Model()
+        threshold = 5.0
+        r0= self._random_hierarchy(m)
+        r1= self._random_hierarchy(m)
+        ls0= IMP.core.get_leaves(r0)
+        ls1= IMP.core.get_leaves(r1)
+        print ls0
+        print ls1
+        cpr= IMP.core.LeavesRefiner(r0.get_traits())
+        lps= IMP.misc.LogPairScore()
+        k=1
+        cpps= IMP.core.KClosePairsPairScore(lps, cpr, k)
+        cpps.evaluate((r0, r1), None)
+        print str(len(lps.get_particle_pairs())) +" pairs", "in", k
+        for pp in lps.get_particle_pairs():
+            print pp[0].get_name(), pp[1].get_name()
+        ap= itertools.product(ls0, ls1)
+        apd= [(IMP.core.get_distance(IMP.core.XYZR(x[0]), IMP.core.XYZR(x[1])), x)\
+              for x in ap]
+        apd.sort(lambda a, b: cmp(a[0], b[0]))
+        #print apd
+        for p in lps.get_particle_pairs():
+            print IMP.core.get_distance(IMP.core.XYZR(p[0]), IMP.core.XYZR(p[1])),\
+                  p[0].get_name(), p[1].get_name()
+        for i in range(0,k):
+            print "looking for", apd[i][0], apd[i][1][0].get_name(), apd[i][1][1].get_name()
+            self.assertTrue(lps.get_contains(apd[i][1]))
+        for i in range(k, len(apd)):
+            print "not looking for", apd[i][0], apd[i][1][0].get_name(), apd[i][1][1].get_name()
+            self.assertFalse(lps.get_contains(apd[i][1]))
 
 
 
