3bf917a4a8a7e66295bbd7655c44992e4483645b
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-10-22 05:07:34 +0000

    add molecule decorator
    git-svn-id: https://svn.salilab.org/imp/trunk@6951 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/atom/include/Molecule.h
@@ -0,0 +1,52 @@
+/**
+ *  \file Molecule.h     \brief A decorator for Molecules.
+ *
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+ *
+ */
+
+#ifndef IMPATOM_MOLECULE_H
+#define IMPATOM_MOLECULE_H
+
+#include "atom_config.h"
+#include "atom_macros.h"
+#include "Hierarchy.h"
+
+#include <IMP/base_types.h>
+#include <IMP/Particle.h>
+#include <IMP/Model.h>
+#include <IMP/Decorator.h>
+
+IMPATOM_BEGIN_NAMESPACE
+
+
+//! A decorator for a molecule.
+/** */
+class IMPATOMEXPORT Molecule: public Hierarchy
+{
+  static IntKey key();
+public:
+  IMP_DECORATOR(Molecule, Hierarchy);
+  //! Add the required attributes to the particle and create a Molecule
+  static Molecule setup_particle(Particle *p) {
+    p->add_attribute(key(),1);
+    return Molecule(p);
+  }
+
+  //! Copy data from the other Molecule to the particle p
+  static Molecule setup_particle(Particle *p, Molecule o) {
+    return setup_particle(p);
+  }
+
+  static bool particle_is_instance(Particle *p) {
+    return p->has_attribute(key());
+  }
+};
+
+IMP_OUTPUT_OPERATOR(Molecule);
+
+IMP_DECORATORS(Molecule,Molecules, Hierarchies);
+
+IMPATOM_END_NAMESPACE
+
+#endif  /* IMPATOM_MOLECULE_H */
---
+++ modules/atom/src/Molecule.cpp
@@ -0,0 +1,23 @@
+/**
+ *  \file Atom.cpp   \brief Simple atoms decorator.
+ *
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+ *
+ */
+
+#include <IMP/atom/Molecule.h>
+
+#include <sstream>
+#include <vector>
+
+IMPATOM_BEGIN_NAMESPACE
+IntKey Molecule::key() {
+  static IntKey k("molecule tag");
+  return k;
+}
+
+void Molecule::show(std::ostream &out) const {
+  out << "name: " << get_particle()->get_name();
+}
+
+IMPATOM_END_NAMESPACE
