cf7e5c11676ecace1df4309a8012fc3510efa4f6
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-10-22 02:40:50 +0000

    Simple check that an installed copy of IMP can be linked against, and used from Python.
    git-svn-id: https://svn.salilab.org/imp/trunk@6949 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ tools/test-install/test.py
@@ -0,0 +1,38 @@
+import IMP
+import IMP.core
+import IMP.example
+import IMP.atom
+import IMP.helper
+import IMP.container
+import unittest
+import os
+
+class IMPInstallTests(unittest.TestCase):
+
+    def test_modules_installed(self):
+        """Check install of basic set of modules"""
+        IMP.set_log_level(IMP.VERBOSE)
+
+        m = IMP.Model()
+
+        p0 = IMP.Particle(m)
+        d0 = IMP.core.XYZ.setup_particle(p0)
+        d0.set_coordinates(IMP.algebra.Vector3D(0,0,2))
+
+        r0 = IMP.example.ExampleRestraint(p0, 1)
+        m.add_restraint(r0)
+
+    def test_data_installed(self):
+        """Check install of data files"""
+        d = IMP.atom.get_data_path("top_heav.lib")
+        self.assert_(os.path.exists(d))
+
+    def test_examples_installed(self):
+        """Check install of example files"""
+        d = IMP.helper.get_example_path("simple_em_fit.py")
+        self.assert_(os.path.exists(d))
+
+if __name__ == '__main__':
+    # Note we use unittest rather than IMP.test, since the latter requires
+    # a unittest2 Python module to be installed
+    unittest.main()
