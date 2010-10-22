453a7d9406923524834471a0bcbd428f68fbfe49
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-10-22 05:10:36 +0000

    new interface
    git-svn-id: https://svn.salilab.org/imp/trunk@6955 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/examples/rigid_collisions.py
+++ modules/core/examples/rigid_collisions.py
@@ -20,15 +20,11 @@ rbss0 = IMP.core.RigidBody.setup_particle(rbp0, IMP.core.XYZs(rbps0))
 rbss1 = IMP.core.RigidBody.setup_particle(rbp1, IMP.core.XYZs(rbps1))
 
 lsc= IMP.container.ListSingletonContainer(m)
-lsc.add_particle(rbp0)
-lsc.add_particle(rbp1)
-
-tr= IMP.core.TableRefiner()
-tr.add_particle(rbp0, rbps0)
-tr.add_particle(rbp1, rbps1)
+lsc.add_particles(rbps0)
+lsc.add_particles(rbps1)
 
 # Set up the nonbonded list
-nbl= IMP.container.ClosePairContainer(lsc, 0, IMP.core.RigidClosePairsFinder(tr), 2.0)
+nbl= IMP.container.ClosePairContainer(lsc, 0, IMP.core.RigidClosePairsFinder(), 2.0)
 
 # Set up excluded volume
 ps= IMP.core.SphereDistancePairScore(IMP.core.HarmonicLowerBound(0,1))
