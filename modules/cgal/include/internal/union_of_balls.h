010671964572f4be9fa68534f035d973c1190104
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-10-22 21:31:35 +0000

    add Ben2's surface area and volume code
    git-svn-id: https://svn.salilab.org/imp/trunk@6974 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/cgal/include/internal/union_of_balls.h
@@ -0,0 +1,24 @@
+/**
+ *  \file cgal/internal/polyhedrons.h
+ *  \brief manipulation of text, and Interconversion between text and numbers
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+*/
+
+#ifndef IMPCGAL_INTERNAL_UNION_OF_BALLS_H
+#define IMPCGAL_INTERNAL_UNION_OF_BALLS_H
+
+#include "../cgal_config.h"
+#include <IMP/base_types.h>
+#include <IMP/algebra/SphereD.h>
+#include <vector>
+
+
+IMPCGAL_BEGIN_INTERNAL_NAMESPACE
+
+IMPCGALEXPORT
+std::pair<double, double>
+get_surface_area_and_volume(const std::vector<algebra::SphereD<3> > &ss);
+
+IMPCGAL_END_INTERNAL_NAMESPACE
+
+#endif  /* IMPCGAL_INTERNAL_UNION_OF_BALLS_H */
