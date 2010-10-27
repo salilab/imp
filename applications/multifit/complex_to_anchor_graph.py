37aaf354d32de3f4ea702513ca955b0d6a93e72d
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-10-27 21:31:47 +0000

    unneeded dep
    git-svn-id: https://svn.salilab.org/imp/trunk@7038 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- applications/multifit/complex_to_anchor_graph.py
+++ applications/multifit/complex_to_anchor_graph.py
@@ -1,7 +1,6 @@
 from optparse import OptionParser
 import IMP.em
 import IMP.multifit
-import IMP.helper
 def main():
     IMP.set_log_level(IMP.SILENT)
     usage = "usage: %prog [options] <complex.pdb> <output: anchor_graph.cmm>\n Description: The script gets as input a PDB file of a complex and calculates an anchor graph,\n such that nodes corresponds to the centroids of its chains and \n edges are between chains that are close in space."
