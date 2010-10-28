c4b4bf1cff15026a8069bb6ef328d61e41b99684
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-10-28 16:28:22 +0000

    helps to add the test
    git-svn-id: https://svn.salilab.org/imp/trunk@7051 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/atom/test/test_pattern.py
@@ -0,0 +1,22 @@
+import IMP
+import IMP.test
+import IMP.core
+import IMP.atom
+import IMP.restrainer
+
+class DecoratorTests(IMP.test.TestCase):
+    def test_bonded(self):
+        """Check use of Selection """
+        m = IMP.Model()
+        mh= IMP.atom.read_pdb(self.get_input_file_name("input.pdb"),
+                              m)
+        pat=IMP.atom.Selection(mh, residue_index=10)
+        print type(pat)
+        got= pat.get_selected_particles()
+        print [x.get_name() for x in got]
+        self.assert_(len(got) > 0)
+        for g in [IMP.atom.Hierarchy(g) for g in got]:
+            self.assertEqual(IMP.atom.Residue(g.get_parent()).get_index(), 10)
+
+if __name__ == '__main__':
+    IMP.test.main()
