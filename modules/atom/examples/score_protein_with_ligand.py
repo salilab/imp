3a9260d98b06f9e6442191ea835de3bcbcaa54c4
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-10-29 23:48:34 +0000

    remove one warning
    git-svn-id: https://svn.salilab.org/imp/trunk@7073 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/examples/score_protein_with_ligand.py
+++ modules/atom/examples/score_protein_with_ligand.py
@@ -6,6 +6,7 @@ protein_atoms= IMP.atom.get_by_type(protein, IMP.atom.ATOM_TYPE)
 ligands= IMP.atom.read_mol2(IMP.atom.get_example_path('1d3d-ligands.mol2'), m)
 # create the score which applies to a pair of atoms
 ps= IMP.atom.ProteinLigandAtomPairScore()
+ps.set_was_used(True)
 # label each atom of the protein with the type needed for scoring
 IMP.atom.add_protein_ligand_score_data(protein)
 for l in ligands.get_children():
