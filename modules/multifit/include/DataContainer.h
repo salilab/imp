76ac207d6a5146c2c40875d0065530db679b26b1
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-10-29 19:16:12 +0000

    missing file
    git-svn-id: https://svn.salilab.org/imp/trunk@7069 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/multifit/include/DataContainer.h
+++ modules/multifit/include/DataContainer.h
@@ -17,10 +17,10 @@
 #include <IMP/domino/JunctionTree.h>
 IMPMULTIFIT_BEGIN_NAMESPACE
 //! Holds data structures neede for optimization
-class IMPMULTIFITEXPORT DataContainer {
+class IMPMULTIFITEXPORT DataContainer : public Object{
   public:
   DataContainer(){}
-    DataContainer(const SettingsData &settings);
+  DataContainer(Model *mdl,const SettingsData &settings);
     atom::Hierarchy get_component(unsigned int i) const {
       IMP_USAGE_CHECK(i<mhs_.size(),"index "<<i<<" is out of range "
                       << "[0,"<<mhs_.size()<<"]\n");
@@ -42,7 +42,7 @@ class IMPMULTIFITEXPORT DataContainer {
        "no fitting records found for particle:"<<p->get_name()<<" \n");
       return recs_.find(p)->second;
     }
-    Particle *get_density_anchor_point(int anchor_point_ind) {
+    Particle *get_density_anchor_point(int anchor_point_ind) const {
       IMP_INTERNAL_CHECK(anchor_point_ind<(int)dens_ap_.size(),
                          "index out of range\n");
       return dens_ap_[anchor_point_ind];
@@ -50,16 +50,21 @@ class IMPMULTIFITEXPORT DataContainer {
     inline int get_number_of_density_anchor_points() const {
       return dens_ap_.size();}
     Model *get_model() { return mdl_;}
+    void show(std::ostream& out=std::cout) const {
+      out<<"DataContainer"<<std::endl;
+    }
     SettingsData get_settings() const { return set_;}
+    IMP_OBJECT_INLINE(DataContainer, show(out),{});
   protected:
-    Model *mdl_;
+    IMP::internal::OwnerPointer<Model> mdl_;
     atom::Hierarchies mhs_;
     atom::Hierarchies mhs_ref_;
     std::map<Particle *,FittingSolutionRecords> recs_;
-    em::DensityMap *dens_;
+    IMP::internal::OwnerPointer<em::DensityMap> dens_;
     IMP::Particles dens_ap_;
     domino::JunctionTree jt_;
     SettingsData set_;
 };
+IMP_OBJECTS(DataContainer,DataContainers);
 IMPMULTIFIT_END_NAMESPACE
 #endif  /* IMPMULTIFIT_DATA_CONTAINER_H */
