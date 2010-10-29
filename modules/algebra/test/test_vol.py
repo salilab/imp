1a17f352f2630352c7656bcc1b0c420f9af2fbdc
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-10-29 03:32:38 +0000

    a dimple test
    git-svn-id: https://svn.salilab.org/imp/trunk@7060 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/algebra/test/test_vol.py
@@ -0,0 +1,33 @@
+import IMP
+import IMP.test
+import IMP.algebra
+import IMP.atom
+import math
+
+class SphereTests(IMP.test.TestCase):
+    def test_enc(self):
+        """Check volume and surface area of random"""
+        ss= IMP.algebra.Sphere3Ds()
+        bb= IMP.algebra.BoundingBox3D(IMP.algebra.Vector3D(0,0,0),
+                                      IMP.algebra.Vector3D(10,10,10))
+        for i in range(0,100):
+            v= IMP.algebra.get_random_vector_on(bb)
+            r= 10
+            s= IMP.algebra.Sphere3D(v,r)
+            ss.append(s)
+        for x in [0,10]:
+            for y in [0,10]:
+                for z in [0,10]:
+                    s= IMP.algebra.Sphere3D(IMP.algebra.Vector3D(x,y,z), 10)
+                    ss.append(s)
+        for s in ss:
+            print ".sphere", s.get_center()[0], s.get_center()[1], s.get_center()[2], s.get_radius()
+        print 30*30*30
+        sp= IMP.algebra.get_surface_area_and_volume(ss)
+        self.assert_(sp[0] > 6*25*25*25)
+        self.assert_(sp[0] < 6*30*30)
+        self.assert_(sp[1] < 30*30*30)
+        self.assert_(sp[1] > 25*25*25)
+
+if __name__ == '__main__':
+    IMP.test.main()
