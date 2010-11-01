83a61f5f38bc266e45b80b787636d8aab80a8bca
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-11-01 21:26:32 +0000

    add score state to see how far things move each step
    git-svn-id: https://svn.salilab.org/imp/trunk@7081 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/statistics/include/MoveStatisticsScoreState.h
@@ -0,0 +1,43 @@
+/**
+ *  \file MoveStatisticsScoreState.h
+ *  \brief Write geometry to a file during optimization
+ *
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+ */
+
+#ifndef IMPSTATISTICS_MOVE_STATISTICS_SCORE_STATE_H
+#define IMPSTATISTICS_MOVE_STATISTICS_SCORE_STATE_H
+
+#include "statistics_config.h"
+#include <IMP/OptimizerState.h>
+#include <IMP/internal/utility.h>
+#include <IMP/algebra/Vector3D.h>
+#include <IMP/io.h>
+
+IMPSTATISTICS_BEGIN_NAMESPACE
+
+//! Keep track of statistics about how particles move.
+/** Keep track of average and maximum moves for a set
+    of particles during optimization.
+ */
+class IMPSTATISTICSEXPORT MoveStatisticsScoreState: public ScoreState
+{
+  Particles ps_;
+  algebra::Vector3Ds last_;
+  double max_move_;
+  std::string max_mover_;
+  double max_average_;
+  double total_move_;
+  double total_movers_;
+  bool init_;
+public:
+  MoveStatisticsScoreState(const ParticlesTemp& ps);
+  void show_statistics(std::ostream &out=std::cout) const;
+  void reset();
+  IMP_SCORE_STATE(MoveStatisticsScoreState);
+};
+
+
+IMPSTATISTICS_END_NAMESPACE
+
+#endif  /* IMPSTATISTICS_MOVE_STATISTICS_SCORE_STATE_H */
