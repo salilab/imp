2fbf7f403daa4930b51b24213b52de1314ed5d2a
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-11-02 02:02:02 +0000

    add better tests
    git-svn-id: https://svn.salilab.org/imp/trunk@7092 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/container/test/test_close_pairs_score_state.py
+++ modules/container/test/test_close_pairs_score_state.py
@@ -66,8 +66,55 @@ class TestBL(IMP.test.TestCase):
                 d.set_coordinates(d.get_coordinates()
                                   + IMP.algebra.get_random_vector_in(IMP.algebra.Sphere3D(IMP.algebra.Vector3D(0,0,0), .7*(i+1))))
             self._compare_lists(m, pc, threshold, cpss)
-
-
+    def test_restraint_0(self):
+        """Test ClosePairContainer over time"""
+        m=IMP.Model()
+        IMP.set_log_level(IMP.VERBOSE)
+        ps= self.create_particles_in_box(m, 10)
+        for p in ps:
+            IMP.core.XYZR.setup_particle(p, 0)
+        # test rebuilding under move, set input and change radius
+        pc= IMP.container.ListSingletonContainer(ps)
+        print "creat cpss "+str(pc)
+        #IMP.set_log_level(IMP.VERBOSE)
+        print 1
+        threshold=1
+        cpss= IMP.container.ClosePairContainer(pc, threshold,
+                                          IMP.core.QuadraticClosePairsFinder(),
+                                          1)
+        for i in range(0,1000):
+            for p in ps:
+                r= IMP.algebra.get_random_vector_in(IMP.algebra.get_unit_sphere_3d())
+                d= IMP.core.XYZ(p)
+                d.set_coordinates(d.get_coordinates()+r)
+            # make sure internal checks in continer pass
+            m.evaluate(False)
+    def test_restraint_1(self):
+        """Test ClosePairContainer complete list over time"""
+        m=IMP.Model()
+        IMP.set_log_level(IMP.VERBOSE)
+        ps= self.create_particles_in_box(m, 10)
+        for p in ps:
+            IMP.core.XYZR.setup_particle(p, 0)
+        # test rebuilding under move, set input and change radius
+        pc= IMP.container.ListSingletonContainer(ps)
+        print "creat cpss "+str(pc)
+        #IMP.set_log_level(IMP.VERBOSE)
+        print 1
+        threshold=1
+        cpss= IMP.container.ClosePairContainer(pc, threshold,
+                                          IMP.core.GridClosePairsFinder(),
+                                          100)
+        m.evaluate(False)
+        n= cpss.get_number_of_particle_pairs()
+        for i in range(0,1000):
+            for p in ps:
+                r= IMP.algebra.get_random_vector_in(IMP.algebra.get_unit_sphere_3d())
+                d= IMP.core.XYZ(p)
+                d.set_coordinates(d.get_coordinates()+r)
+            # make sure internal checks in continer pass
+            m.evaluate(False)
+            self.assertEqual(n, cpss.get_number_of_particle_pairs())
     def test_restraint(self):
         """Test ClosePairContainer with a restraint"""
         m=IMP.Model()
