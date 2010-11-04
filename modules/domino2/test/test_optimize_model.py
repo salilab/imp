bc838047f9240f03ed04c57b3a91c66724b60ad5
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-11-04 23:48:55 +0000

    more rb working
    git-svn-id: https://svn.salilab.org/imp/trunk@7173 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/domino2/test/test_optimize_model.py
+++ modules/domino2/test/test_optimize_model.py
@@ -8,7 +8,6 @@ import IMP.restrainer
 import time
 
 class DOMINOTests(IMP.test.TestCase):
-
     def test_global_min2(self):
         """Test optimization of model"""
         m= IMP.Model()
@@ -49,5 +48,50 @@ class DOMINOTests(IMP.test.TestCase):
         IMP.show_restraint_hierarchy(m.get_root_restraint_set())
         self.assertEqual(len(IMP.get_restraints(m.get_root_restraint_set())),
                          1)
+
+    def test_global_min3(self):
+        """Test optimization of model with multi-layer containers"""
+        m= IMP.Model()
+        m.set_log_level(IMP.VERBOSE)
+        ps=[]
+        vs=[IMP.algebra.Vector3D(0,0,0),
+            IMP.algebra.Vector3D(0,2,0),
+            IMP.algebra.Vector3D(0,1.4, 1.4),
+            IMP.algebra.Vector3D(1,0,0)]
+        pst = IMP.domino2.ParticleStatesTable()
+        states=IMP.domino2.XYZStates(vs)
+        for i in range(10):
+            p= IMP.Particle(m)
+            IMP.core.XYZR.setup_particle(p,IMP.algebra.Sphere3D(IMP.algebra.Vector3D(i,i,i), 1))
+            ps.append(p)
+            pst.set_particle_states(p, states)
+        lpc= IMP.container.ListSingletonContainer(ps)
+        apc= IMP.container.AllPairContainer(lpc)
+        r= IMP.container.PairsRestraint(IMP.core.HarmonicSphereDistancePairScore(0,1), apc)
+        r.set_name("restraint")
+        m.add_restraint(r)
+        print "getting graph"
+        dg= IMP.get_dependency_graph([m.get_root_restraint_set()])
+        #dg.show_dotty()
+        print "optimizing"
+        doc= IMP.domino2.OptimizeContainers(m.get_root_restraint_set(), pst)
+        dg= IMP.get_dependency_graph([m.get_root_restraint_set()])
+        #dg.show_dotty()
+        print "before"
+        IMP.show_restraint_hierarchy(m.get_root_restraint_set())
+        dor= IMP.domino2.OptimizeRestraints(m.get_root_restraint_set(), pst)
+        print "after"
+        IMP.show_restraint_hierarchy(m.get_root_restraint_set())
+        print "optimized restraints"
+        dg= IMP.get_dependency_graph([m.get_root_restraint_set()])
+        #dg.show_dotty()
+        self.assertEqual(len(IMP.get_restraints(m.get_root_restraint_set())),
+                         45)
+        del dor
+        print "after delete"
+        IMP.show_restraint_hierarchy(m.get_root_restraint_set())
+        self.assertEqual(len(IMP.get_restraints(m.get_root_restraint_set())),
+                         1)
+
 if __name__ == '__main__':
     IMP.test.main()
--- modules/domino2/test/test_rigid_bodies.py
+++ modules/domino2/test/test_rigid_bodies.py
@@ -10,15 +10,18 @@ import time
 from IMP.algebra import *
 
 class DOMINOTests(IMP.test.TestCase):
-    def _create_rb(self, name, m):
-        p= IMP.Particle(m)
-        p.set_name(name+"particle")
-        d=IMP.core.XYZR.setup_particle(p)
-        d.set_radius(1)
-        d.set_coordinates(Vector3D(0,0,0))
+    def _create_rb(self, name, m, np=1):
+        ps=[]
+        for i in range(0, np):
+            p= IMP.Particle(m)
+            p.set_name(name+"particle"+str(i))
+            d=IMP.core.XYZR.setup_particle(p)
+            d.set_radius(1)
+            d.set_coordinates(get_random_vector_in(get_unit_sphere_3d()))
+            ps.append(p)
         rbp= IMP.Particle(m)
         rbp.set_name(name+"rb")
-        rbd=IMP.core.RigidBody.setup_particle(rbp, [p])
+        rbd=IMP.core.RigidBody.setup_particle(rbp, ps)
         return rbd
     def test_global_min2(self):
         """Testing finding minima with rigid bodies"""
@@ -77,6 +80,35 @@ class DOMINOTests(IMP.test.TestCase):
         for r in allr:
             print r.get_name()
         self.assertEqual(len(allr), 2)
+    def test_global_min3(self):
+        """Testing splitting restraints with rigid bodies and interestings scores"""
+        m= IMP.Model()
+        rbs= [self._create_rb("1",m, 4), self._create_rb("2", m, 4),
+              self._create_rb("3",m, 4)]
+        trs= [Transformation3D(get_identity_rotation_3d(), Vector3D(0,0,0)),
+              Transformation3D(get_identity_rotation_3d(), Vector3D(2,0,0)),
+              Transformation3D(get_identity_rotation_3d(), Vector3D(4,0,0))]
+        lps=IMP.core.SphereDistancePairScore(IMP.core.HarmonicUpperBound(0,1))
+        ps= IMP.core.KClosePairsPairScore(lps, IMP.core.RigidMembersRefiner())
+        pl=IMP.container.ListPairContainer([(rbs[0], rbs[1]), (rbs[1], rbs[2])])
+        r= IMP.container.PairsRestraint(ps, pl)
+        r.set_name("restraint")
+        m.add_restraint(r)
+        m.set_maximum_score(r, .5)
+        dg= IMP.get_dependency_graph([m.get_root_restraint_set()])
+        dg.show_dotty()
+        pst= IMP.domino2.ParticleStatesTable()
+        pstate= IMP.domino2.RigidBodyStates(trs)
+        pst.set_particle_states(rbs[0], pstate)
+        pst.set_particle_states(rbs[1], pstate)
+        pst.set_particle_states(rbs[2], pstate)
+        oc= IMP.domino2.OptimizeContainers(m.get_root_restraint_set(), pst)
+        occ= IMP.domino2.OptimizeRestraints(m.get_root_restraint_set(), pst)
+        allr= IMP.get_restraints(m.get_root_restraint_set())
+        for r in allr:
+            print r.get_name()
+        self.assertEqual(len(allr), 2)
+
 
 if __name__ == '__main__':
     IMP.test.main()
