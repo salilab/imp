8baa49dd201f2b274bc19fe39693fe4da874aa23
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-11-05 00:21:14 +0000

    no need to set up states (and do it wrong)
    git-svn-id: https://svn.salilab.org/imp/trunk@7174 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/domino2/test/test_sampling.py
+++ modules/domino2/test/test_sampling.py
@@ -10,7 +10,7 @@ import time
 
 class DOMINOTests(IMP.test.TestCase):
 
-    def _test_global_min2(self):
+    def test_global_min2(self):
         """Test sampling"""
         m= IMP.Model()
         m.set_log_level(IMP.SILENT)
@@ -31,13 +31,11 @@ class DOMINOTests(IMP.test.TestCase):
         for p in ps:
             pst.set_particle_states(p, particle_state)
         print 4
-        sst= IMP.domino2.DefaultSubsetStatesTable(pst)
         m.add_restraint(IMP.core.DistanceRestraint(IMP.core.Harmonic(1,1), ps[0], ps[1]))
         m.add_restraint(IMP.core.DistanceRestraint(IMP.core.Harmonic(1,1), ps[1], ps[2]))
         print 5
         sampler= IMP.domino2.DominoSampler(m, pst)
         sampler.set_log_level(IMP.VERBOSE)
-        sampler.set_subset_states_table(sst)
         sampler.set_maximum_score(.1)
         cs= sampler.get_sample()
         print 6
@@ -53,7 +51,6 @@ class DOMINOTests(IMP.test.TestCase):
             self.assertLess((IMP.core.get_distance(d0,d1)-1)**2, .1)
             print 7,i,1
             self.assertLess((IMP.core.get_distance(d1,d2)-1)**2, .1)
-
     def test_global_min3(self):
         """Test sampling with edge scores"""
         m= IMP.Model()
@@ -72,13 +69,11 @@ class DOMINOTests(IMP.test.TestCase):
         for p in ps:
             pst.set_particle_states(p, particle_state)
             m.add_restraint(IMP._ConstRestraint(1, [p]))
-        sst= IMP.domino2.DefaultSubsetStatesTable(pst)
         m.add_restraint(IMP.core.DistanceRestraint(IMP.core.Harmonic(1,1), ps[0], ps[1]))
         m.add_restraint(IMP.core.DistanceRestraint(IMP.core.Harmonic(1,1), ps[1], ps[2]))
 
         sampler= IMP.domino2.DominoSampler(m, pst)
         sampler.set_log_level(IMP.VERBOSE)
-        sampler.set_subset_states_table(sst)
         sampler.set_maximum_score(3.1)
         cs= sampler.get_sample()
         self.assertGreater(cs.get_number_of_configurations(), 0)
