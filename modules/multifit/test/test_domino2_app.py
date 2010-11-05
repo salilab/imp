86695f0cc9d5b37f27f6ee8a30432c5d2b557c78
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-11-05 00:52:56 +0000

    make the test more interesting
    git-svn-id: https://svn.salilab.org/imp/trunk@7183 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/multifit/test/test_domino2_app.py
+++ modules/multifit/test/test_domino2_app.py
@@ -26,7 +26,7 @@ class MultiFitDominoTests(IMP.test.TestCase):
                                             "Restrained pairs")
         self.pr= IMP.container.PairsRestraint(sdps, pc)
         self.pr.set_name("interactions")
-        m.set_maximum_score(self.pr, 10)
+        #m.set_maximum_score(self.pr, 10)
         m.add_restraint(self.pr)
         print "done"
 
@@ -35,56 +35,59 @@ class MultiFitDominoTests(IMP.test.TestCase):
         ss= IMP.SetLogState(IMP.SILENT)
         h=IMP.atom.read_pdb(self.get_input_file_name("1z5s.pdb"),m)
         del ss
-        self.mhs=[]
         ps=[]
-        self.rbs=[]
+        rbs=[]
         all_atoms=[]
         print "creating rbs"
         for cr in IMP.atom.get_by_type(h, IMP.atom.CHAIN_TYPE):
             c= IMP.atom.create_simplified_along_backbone(IMP.atom.Chain(cr), 10)
-            self.mhs.append(c)
             ps.append(c)
             all_atoms+=IMP.core.get_leaves(c)
-            self.rbs.append(IMP.atom.create_rigid_body(c))
+            rbs.append(IMP.atom.create_rigid_body(c))
         #r= IMP.core.ExcludedVolumeRestraint(IMP.container.ListSingletonContainer(all_atoms))
         #m.add_restraint(r)
-        rbs= IMP.container.ListSingletonContainer(ps)
-        aprbs= IMP.container.AllPairContainer(rbs)
+        IMP.atom.destroy(h)
+        crbs= IMP.container.ListSingletonContainer(ps)
+        aprbs= IMP.container.AllPairContainer(crbs)
         rdps= IMP.core.KClosePairsPairScore(IMP.core.SoftSpherePairScore(1),
                                            IMP.core.LeavesRefiner(IMP.atom.Hierarchy.get_traits()))
         r= IMP.container.PairsRestraint(rdps, aprbs)
         r.set_name("EV")
         m.add_restraint(r)
-        m.set_maximum_score(r, 100)
-        return (m, ps)
-    def load_transformations(self):
+        #m.set_maximum_score(r, 1)
+        return (m, ps, rbs)
+    def load_transformations(self, rbs):
         bb= IMP.algebra.BoundingBox3D(IMP.algebra.Vector3D(-5,-5,-5),
                                       IMP.algebra.Vector3D(5,5,5))
-        self.trans=[]
-        self.trans.append(IMP.algebra.Transformation3D(IMP.algebra.get_identity_rotation_3d(),
+        trans=[]
+        for r in rbs:
+            trans.append(r.get_transformation())
+        trans.append(IMP.algebra.Transformation3D(IMP.algebra.get_identity_rotation_3d(),
                                                        IMP.algebra.Vector3D(0,0,0)))
         for i in range(0):
-            self.trans.append(IMP.algebra.Transformation3D(#IMP.algebra.get_identity_rotation_3d(),
+            trans.append(IMP.algebra.Transformation3D(#IMP.algebra.get_identity_rotation_3d(),
                 IMP.algebra.get_random_rotation_3d(IMP.algebra.get_identity_rotation_3d(), .4),
                 IMP.algebra.get_random_vector_in(bb)))
-    def get_display(self):
-        return IMP.atom.WritePDBOptimizerState(self.mhs,"temp_%03d.pdb")
+        return trans
+    def get_display(self, ps):
+        return IMP.atom.WritePDBOptimizerState(ps,"temp_%03d.pdb")
     def test_rcos(self):
         """Test domino2 working for multifit"""
         print "===creating"
-        (m, ps)= self.create()
+        (m, ps, rbs)= self.create()
         m.set_log_level(IMP.SILENT)
-        self.load_transformations()
+        trans=self.load_transformations(rbs)
         print "create restraints"
         self.create_restraints(m,ps)
         print "MODEL SCORE:",
-        print m.evaluate(None)
+        initial_score= m.evaluate(None)
+        print initial_score
         #add restraints
         #setup domino
         #create discrete states
-        states=IMP.domino2.RigidBodyStates(self.trans)
+        states=IMP.domino2.RigidBodyStates(trans)
         pst= IMP.domino2.ParticleStatesTable()
-        for p in self.rbs:
+        for p in rbs:
             pst.set_particle_states(IMP.core.RigidBody(p.get_particle()), states)
         #create sampler
         oc= IMP.domino2.OptimizeContainers(m.get_root_restraint_set(), pst)
@@ -101,16 +104,16 @@ class MultiFitDominoTests(IMP.test.TestCase):
         rg= IMP.domino2.get_restraint_graph(m.get_root_restraint_set(), pst)
         rg.show()
         s=IMP.domino2.DominoSampler(m, pst)
-        s.set_log_level(IMP.VERBOSE)
+        s.set_log_level(IMP.TERSE)
         print pst
         print "going to sample"
-        s.set_maximum_score(700)
+        s.set_maximum_score(initial_score+1)
         cs=s.get_sample()
 
         print "found ", cs.get_number_of_configurations(), "solutions"
         #cs.load_configuration(0)
         #m.evaluate(False)
-        self.assertGreaterEqual(cs.get_number_of_configurations(),0)
+        self.assertEqual(cs.get_number_of_configurations(),1)
 
 if __name__ == '__main__':
     IMP.test.main()
