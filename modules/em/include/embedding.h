ee8969203f37f4ba21b308976709d0d71a36003b
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-11-05 19:24:24 +0000

    move embedding of density maps into this module
    git-svn-id: https://svn.salilab.org/imp/trunk@7203 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/em/include/embedding.h
@@ -0,0 +1,31 @@
+/**
+ *  \file embedding.h
+ *  \brief Cluster sets of points.
+ *
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+ *
+ */
+#ifndef IMPEM_EMBEDDING_H
+#define IMPEM_EMBEDDING_H
+
+#include "em_config.h"
+#include "DensityMap.h"
+#include <IMP/statistics/point_clustering.h>
+
+IMPEM_BEGIN_NAMESPACE
+
+
+/** Generate a set of points from the voxels in a em::DensityMap
+    which are above a certain threshold.
+ */
+class IMPEMEXPORT HighDensityEmbedding: public statistics::Embedding {
+  std::vector<algebra::VectorD<3> > points_;
+public:
+  HighDensityEmbedding(DensityMap *dm,
+                       double threshold);
+  IMP_EMBEDDING(HighDensityEmbedding);
+};
+
+IMPEM_END_NAMESPACE
+
+#endif /* IMPEM_EMBEDDING_H */
