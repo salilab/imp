b70bed4d1d5756eea1171a5dbe10a642bb78b59a
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-11-08 06:11:06 +0000

    add local cross correlation to CoarseCC, still not part of the restraint
    git-svn-id: https://svn.salilab.org/imp/trunk@7212 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/em/include/density_utilities.h
@@ -0,0 +1,29 @@
+/**
+ *  \file density_utilities.h
+ *  \brief density map manipulations
+ *
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+ *
+ */
+
+#ifndef IMPEM_DENSITY_UTILITIES_H
+#define IMPEM_DENSITY_UTILITIES_H
+
+
+#include "em_config.h"
+#include <IMP/base_types.h>
+#include "DensityMap.h"
+
+IMPEM_BEGIN_NAMESPACE
+
+//! Return a masked density , and
+//! normalize the output map within the masked region
+/**
+\param[in] dmap the density map to mask
+\param[in] mask the mask
+\param[output] the masked and normalized map
+ */
+IMPEMEXPORT DensityMap* mask_and_norm(em::DensityMap *dmap,
+                                         em::DensityMap *mask);
+IMPEM_END_NAMESPACE
+#endif  /* IMPEM_DENSITY_UTILITIES_H */
