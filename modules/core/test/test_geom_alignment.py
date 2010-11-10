cefe4aec79407cc10c1ab8d19bdac3c9455b783d
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-11-10 04:23:26 +0000

    test (currently failing) overloaded alignment
    git-svn-id: https://svn.salilab.org/imp/trunk@7220 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/core/test/test_geom_alignment.py
@@ -0,0 +1,45 @@
+import IMP
+import IMP.test
+import IMP.algebra
+import IMP.core
+
+class RigidTransformationTests(IMP.test.TestCase):
+    """Test rigid transformations"""
+
+    def _produce_point_sets(self, tr):
+        vs= IMP.algebra.Vector3Ds()
+        vsr= IMP.algebra.Vector3Ds()
+        for i in range(0, 20):
+            vs.append(IMP.algebra.get_random_vector_in(IMP.algebra.get_unit_bounding_box_3d()))
+            print vs[-1]
+            vsr.append(tr.get_transformed(vs.back()))
+            print vsr[-1]
+        return (vs, vsr)
+    def _convert_to_xyzs(self, m, vs):
+        ret=[]
+        for v in vs:
+            p= IMP.Particle(m)
+            d= IMP.core.XYZ.setup_particle(p, v)
+            ret.append(v)
+        return ret
+
+    def test_align(self):
+        """Testing rigid alignment of xyz sets"""
+        m=IMP.Model()
+        r= IMP.algebra.get_random_rotation_3d()
+        t= IMP.algebra.Vector3D(0,0,0) #IMP.get_random_vector_in(IMP.algebra.get_unit_bounding_box_3d())
+        tr= IMP.algebra.Transformation3D(r, t)
+        (vs, vsr)= self._produce_point_sets(tr)
+        (ds, dsr)= (self._convert_to_xyzs(m, vs), self._convert_to_xyzs(m,vsr))
+        #tr0= IMP.algebra.get_transformation_aligning_first_to_second(vs, vsr)
+        #print "tr", tr
+        tr1= IMP.core.get_transformation_aligning_first_to_second(ds, dsr)
+        print "tr1", tr1
+        tr2= IMP.core.get_transformation_aligning_first_to_second(vs, dsr)
+        print "tr2", tr2
+        self.assertAlmostEqual(IMP.algebra.get_distance(tr.get_rotation(), r),
+                               0, delta=.1)
+
+
+if __name__ == '__main__':
+    IMP.test.main()
