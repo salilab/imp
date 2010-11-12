1479d11a412b8a166de5cf1fade77757b91f9649
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-11-12 17:49:03 +0000

    hack to work around swigversin being sporadically undefined
    git-svn-id: https://svn.salilab.org/imp/trunk@7258 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- scons_tools/dependency/swig.py
+++ scons_tools/dependency/swig.py
@@ -23,7 +23,14 @@ Since SWIG could not be found, proceeding to build IMP without Python support.
 
 """
     context.Message('Checking for SWIG version >= %s... ' % needversion_str)
-    version = context.env['SWIGVERSION']
+    try:
+        version = context.env['SWIGVERSION']
+    except:
+        ret= os.popen("swig -version").read()
+        r= re.compile("SWIG Version ([0-9\.]+)")
+        match=r.search(ret)
+        if match:
+            version= match.groups()[0]
     try:
         v = [int(x) for x in version.split(".")]
     except ValueError:
