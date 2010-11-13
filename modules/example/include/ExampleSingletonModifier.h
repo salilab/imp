e7db60b03ec00c631ab89606dc52155a95e5ac36
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-11-13 03:10:16 +0000

    fix docs blah
    git-svn-id: https://svn.salilab.org/imp/trunk@7273 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/example/include/ExampleSingletonModifier.h
+++ modules/example/include/ExampleSingletonModifier.h
@@ -23,7 +23,7 @@ IMPEXAMPLE_BEGIN_NAMESPACE
      or IMP::core::SingletonsRestraint to keep a set of particles
      in a box.
 
-     \htmlinclude range_restriction.py.html
+     \pythonexample range_restriction
 
      The source code is as follows:
      \include ExampleRestraint.h
