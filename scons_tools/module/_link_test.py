c6f9965bb0ff05c6db1a71c379f7d43b6b3d15ef
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-11-15 17:41:44 +0000

    fix variable name
    git-svn-id: https://svn.salilab.org/imp/trunk@7284 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- scons_tools/module/_link_test.py
+++ scons_tools/module/_link_test.py
@@ -9,7 +9,7 @@ import re
 def _action_link_test(target, source, env):
     """The IMPModuleLinkTesto Builder generates a source file. By linking in two
     of these, any functions which are defined in headers but not declared inline are detected"""
-    vars= scons_tools.module.get_module_variables(env)
+    vars= scons_tools.module._get_module_variables(env)
     for fname in [target[0].abspath, target[1].abspath]:
         cpp = file(fname, 'w')
         print >> cpp, """/*
