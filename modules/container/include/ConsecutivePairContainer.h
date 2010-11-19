97be3ebb53665928d0e347e0ab08152ad181f11d
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-11-19 23:37:56 +0000

    tweaks to lookup method
    git-svn-id: https://svn.salilab.org/imp/trunk@7350 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/container/include/ConsecutivePairContainer.h
+++ modules/container/include/ConsecutivePairContainer.h
@@ -13,9 +13,10 @@
 #include <IMP/PairContainer.h>
 #include <IMP/SingletonContainer.h>
 #include <IMP/container/ListPairContainer.h>
+#include <IMP/internal/map.h>
 
 IMPCONTAINER_BEGIN_NAMESPACE
-#define IMP_CPC_PARTICLE_CACHE
+#define IMP_CPC_METHOD 0
 //! A container which contains all consecutive pairs from a list
 class IMPCONTAINEREXPORT ConsecutivePairContainer : public PairContainer
 {
@@ -38,8 +39,12 @@ class IMPCONTAINEREXPORT ConsecutivePairContainer : public PairContainer
     }
     return ret;
   }
-#ifdef IMP_CPC_PARTICLE_CACHE
+#if IMP_CPC_METHOD ==0
   IntKey key_;
+#elif IMP_CPC_METHOD ==1
+  IMP::internal::Map<Particle*, int> index_;
+#else
+
 #endif
 public:
   //! Get the individual particles from the passed SingletonContainer
@@ -67,12 +72,18 @@ ConsecutivePairContainer::get_particle_pair(unsigned int i) const {
 inline bool
 ConsecutivePairContainer
 ::get_contains_particle_pair(const ParticlePair &p) const {
-#ifdef IMP_CPC_PARTICLE_CACHE
+#if IMP_CPC_METHOD==0
   if (!p[0]->has_attribute(key_)) return false;
   int ia= p[0]->get_value(key_);
-  if (ia!= 0 && ps_[ia-1]==p[1]) return true;
-  else if (ia != static_cast<int>(ps_.size())-1 && ps_[ia+1]==p[1]) return true;
-  else return false;
+  if (!p[1]->has_attribute(key_)) return false;
+  int ib= p[1]->get_value(key_);
+  return std::abs(ia-ib)==1;
+#elif IMP_CPC_METHOD==1
+  IMP::internal::Map<Particle*, int>::const_iterator it0= index_.find(p[0]);
+  if (it0==index_.end()) return false;
+  IMP::internal::Map<Particle*, int>::const_iterator it1= index_.find(p[1]);
+  if (it1==index_.end()) return false;
+  else return std::abs(it0->second-it1->second) ==1;
 #else
   for (unsigned int i=1; i< ps_.size(); ++i) {
     if (ps_[i]== p[1] && ps_[i-1]==p[0]) {
