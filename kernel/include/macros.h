f2ec59700fdab3c347ceb5325ce986b1e4d5cddb
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-11-19 23:37:36 +0000

    suppress virtual lookup
    git-svn-id: https://svn.salilab.org/imp/trunk@7349 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/include/macros.h
+++ kernel/include/macros.h
@@ -2060,14 +2060,21 @@ protected:                                      \
     - IMP::SingletonFilter::get_input_particles()
 */
 #define IMP_SINGLETON_FILTER(Name)                                      \
+  private:                                                              \
+  struct GCP {                                                          \
+    const Name *back_;                                                  \
+    GCP(const Name *n): back_(n){}                                      \
+    bool operator()(Particle* p) const {                                \
+      return back_->Name::get_contains_particle(p);                     \
+    }                                                                   \
+  };                                                                    \
+  public:                                                               \
   bool get_contains_particle(Particle* p) const;                        \
   ParticlesTemp get_input_particles(Particle*t) const;                  \
   ContainersTemp get_input_containers(Particle*t) const;                \
   void filter_in_place(ParticlesTemp &ps) const {                       \
     ps.erase(std::remove_if(ps.begin(), ps.end(),                       \
-                            boost::lambda::bind(&Name::get_contains_particle, \
-                                        this,                           \
-                                        boost::lambda::_1)),            \
+                            GCP(this)),                                 \
              ps.end());                                                 \
   }                                                                     \
   IMP_OBJECT(Name)
@@ -2079,15 +2086,21 @@ protected:                                      \
     - IMP::PairFilter::get_input_particles()
 */
 #define IMP_PAIR_FILTER(Name)                                           \
+  private:                                                              \
+  struct GCP {                                                          \
+    const Name *back_;                                                  \
+    GCP(const Name *n): back_(n){}                                      \
+    bool operator()(const ParticlePair &p) const {                      \
+      return back_->Name::get_contains_particle_pair(p);                \
+    }                                                                   \
+  };                                                                    \
+  public:                                                               \
   bool get_contains_particle_pair(const ParticlePair& p) const;         \
   ParticlesTemp get_input_particles(const ParticlePair& t) const;       \
   ContainersTemp get_input_containers(const ParticlePair& t) const;     \
-  void filter_in_place(ParticlePairsTemp &ps) const {                  \
+  void filter_in_place(ParticlePairsTemp &ps) const {                   \
     ps.erase(std::remove_if(ps.begin(), ps.end(),                       \
-                            boost::lambda::bind(                        \
-                                     &Name::get_contains_particle_pair, \
-                                        this,                           \
-                                        boost::lambda::_1)),            \
+                            GCP(this)),                                 \
              ps.end());                                                 \
   }                                                                     \
   IMP_OBJECT(Name)
@@ -2099,16 +2112,21 @@ protected:                                      \
     - IMP::TripletFilter::get_contains_particle_triplet()
     - IMP::TripletFilter::get_input_particles()
 */
-#define IMP_TRIPLET_FILTER(Name)                                       \
-  bool get_contains_particle_triplet(const ParticleTriplet& p) const;  \
-  ParticlesTemp get_input_particles(const ParticleTriplet& t) const;   \
-  ContainersTemp get_input_containers(const ParticleTriplet& t) const; \
+#define IMP_TRIPLET_FILTER(Name)                                        \
+  private:                                                              \
+  struct GCP {                                                          \
+    const Name *back_;                                                  \
+    GCP(const Name *n): back_(n){}                                      \
+    bool operator()(const ParticleTriplet &p) const {                   \
+      return back_->Name::get_contains_particle_triplet(p);             \
+    }                                                                   \
+  };                                                                    \
+  bool get_contains_particle_triplet(const ParticleTriplet& p) const;   \
+  ParticlesTemp get_input_particles(const ParticleTriplet& t) const;    \
+  ContainersTemp get_input_containers(const ParticleTriplet& t) const;  \
   void filter_in_place(ParticleTripletsTemp &ps) const {                \
     ps.erase(std::remove_if(ps.begin(), ps.end(),                       \
-                            boost::lambda::bind(                        \
-                                  &Name::get_contains_particle_triplet, \
-                                                this,                   \
-                                        boost::lambda::_1)),            \
+                            GCP(this)),                                 \
              ps.end());                                                 \
   }                                                                     \
   IMP_OBJECT(Name)
@@ -2121,15 +2139,20 @@ protected:                                      \
     - IMP::QuadFilter::get_input_particles()
 */
 #define IMP_QUAD_FILTER(Name)                                           \
+  private:                                                              \
+  struct GCP {                                                          \
+    const Name *back_;                                                  \
+    GCP(const Name *n): back_(n){}                                      \
+    bool operator()(const ParticleQuad &p) const {                      \
+      return back_->Name::get_contains_particle_quad(p);                \
+    }                                                                   \
+  };                                                                    \
   bool get_contains_particle_quad(const ParticleQuad& p) const;         \
   ParticlesTemp get_input_particles(const ParticleQuad& t) const;       \
   ContainersTemp get_input_containers(const ParticleQuad& t) const;     \
   void filter_in_place(ParticleQuadsTemp &ps) const {                   \
     ps.erase(std::remove_if(ps.begin(), ps.end(),                       \
-                            boost::lambda::bind(                        \
-                                  &Name::get_contains_particle_quad,    \
-                                                this,                   \
-                                        boost::lambda::_1)),            \
+                            GCP(this)),                                 \
              ps.end());                                                 \
   }                                                                     \
   IMP_OBJECT(Name)
