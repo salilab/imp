e12ef7ffa6d3617e9ce714b27819f3b2463efceb
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-11-27 18:35:42 +0000

    add example biological system
    git-svn-id: https://svn.salilab.org/imp/trunk@7418 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ biological_systems/example/sample_0.py
@@ -0,0 +1,68 @@
+import IMP
+import IMP.atom
+import IMP.container
+import IMP.display
+import IMP.statistics
+import IMP.example
+import IMP.system
+import parameters
+import setup
+
+display_restraints=[]
+
+
+# find acceptable conformations of the model
+def get_conformations(m, gs, n):
+    sampler= IMP.core.MCCGSampler(m)
+
+    sampler.set_bounding_box(parameters.bb)
+    # magic numbers, experiment with them and make them large enough for things to work
+    sampler.set_number_of_conjugate_gradient_steps(100)
+    sampler.set_number_of_monte_carlo_steps(10)
+    sampler.set_number_of_attempts(n)
+    sampler.set_save_rejected_configurations(True)
+    # We don't care to see the output from the sampler
+    sampler.set_log_level(IMP.SILENT)
+
+    # return the IMP.ConfigurationSet storing all the found configurations that
+    # meet the various restraint maximum scores.
+    cs= sampler.get_sample()
+    # Look at the rejected minimal conformations to understand how the restraints
+    # are failing
+    print "rejected", \
+          sampler.get_rejected_configurations().get_number_of_configurations(), "solutions"
+    m.set_gather_statistics(True)
+    for i in range(0, sampler.get_rejected_configurations().get_number_of_configurations()):
+        sampler.get_rejected_configurations().load_configuration(i)
+        m.evaluate(False)
+        # show the score for each restraint to see which restraints were causing the
+        # conformation to be rejected
+        m.show_restraint_score_statistics()
+        w= IMP.display.PymolWriter("rejected.%d.pym"%i)
+        for g in gs:
+            w.add_geometry(g)
+    return cs
+
+(i, n) =IMP.system.get_sample_parameters()
+
+
+# now do the actual work
+(m,all)= setup.create_representation()
+IMP.atom.show_molecular_hierarchy(all)
+setup.create_restraints(m, all)
+
+
+gs= setup.create_geometry(all)
+IMP.set_log_level(IMP.SILENT)
+cs= get_conformations(m, gs, 2000.0/n)
+IMP.set_log_level(IMP.TERSE)
+
+print "found", cs.get_number_of_configurations(), "solutions"
+for i in range(0, cs.get_number_of_configurations()):
+    cs.load_configuration(i)
+    w= IMP.display.PymolWriter("sampled_0/struct.%d.pym"%i)
+    for g in gs:
+        w.add_geometry(g)
+keys=IMP.core.XYZ.get_xyz_keys()
+IMP.write_configuration_set(cs, IMP.atom.get_leaves(all), keys,
+                            "sampled_0/configurations_"+str(i)+".bimp")
