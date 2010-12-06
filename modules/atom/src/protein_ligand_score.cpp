9fd63d66b6171994cd1e2d37e37c0bc1bc74a580
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-12-06 19:51:46 +0000

    generalize the protein ligand score
    git-svn-id: https://svn.salilab.org/imp/trunk@7487 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/src/protein_ligand_score.cpp
+++ modules/atom/src/protein_ligand_score.cpp
@@ -13,15 +13,14 @@
 IMPATOM_BEGIN_NAMESPACE
 
 namespace {
-  IMP_DECLARE_CONTROLLED_KEY_TYPE(ProteinType, 783462);
-  IMP_DECLARE_CONTROLLED_KEY_TYPE(LigandType, 783463);
+  IMP_DECLARE_CONTROLLED_KEY_TYPE(ProteinLigandType, 783462);
 
   namespace data {
 
-#define PROTEIN_TYPE(NameKey) const ProteinType \
-    NameKey(ProteinType::add_key(#NameKey));
-#define PROTEIN_TYPE_ALIAS(Name, Key) const ProteinType Name\
-  (ProteinType::add_alias(ProteinType(#Key), #Name));
+#define PROTEIN_TYPE(NameKey) const ProteinLigandType \
+    NameKey(ProteinLigandType::add_key(#NameKey));
+#define PROTEIN_TYPE_ALIAS(Name, Key) const ProteinLigandType Name\
+  (ProteinLigandType::add_alias(ProteinLigandType(#Key), #Name));
 
     PROTEIN_TYPE(ALA_N)
     PROTEIN_TYPE(ALA_CA)
@@ -200,9 +199,10 @@ namespace {
     PROTEIN_TYPE(VAL_CG)
     PROTEIN_TYPE_ALIAS(VAL_CG1, VAL_CG)
     PROTEIN_TYPE_ALIAS(VAL_CG2, VAL_CG)
+    PROTEIN_TYPE(LAST_PROTEIN_TYPE)
 
 #define LIGAND_TYPE(NameKey) \
-    const LigandType NameKey(LigandType::add_key(#NameKey));
+    const ProteinLigandType NameKey(ProteinLigandType::add_key(#NameKey));
 
     LIGAND_TYPE(C1)
     LIGAND_TYPE(C2)
@@ -230,26 +230,38 @@ namespace {
     LIGAND_TYPE(Cl)
     LIGAND_TYPE(Br)
     LIGAND_TYPE(I)
+    LIGAND_TYPE(LAST_LIGAND_TYPE)
 }
 
   IntKey get_protein_ligand_type_key() {
     static const IntKey ik("protein-ligand atom type");
     return ik;
   }
+
+  int get_index(Particle *p) {
+    return p->get_value(get_protein_ligand_type_key());
+  }
+  const int ni= data::LAST_PROTEIN_TYPE.get_index();
+  const int nj= data::LAST_LIGAND_TYPE.get_index()
+    - data::LAST_PROTEIN_TYPE.get_index()-1;
 }
 
 
 
 
 ProteinLigandAtomPairScore::ProteinLigandAtomPairScore(double threshold):
-  table_(get_data_path("protein_ligand_score.lib")),
+  table_(ni+1),
   threshold_(threshold){
+ table_.initialize<ProteinLigandType>(get_data_path("protein_ligand_score.lib"),
+                                       ni, nj);
   }
 
 ProteinLigandAtomPairScore::ProteinLigandAtomPairScore(double threshold,
                                                        TextInput file):
-  table_(file),
+  table_(ni+1),
   threshold_(threshold){
+ table_.initialize<ProteinLigandType>(get_data_path("protein_ligand_score.lib"),
+                                       ni, nj);
   }
 
 double ProteinLigandAtomPairScore
@@ -259,8 +271,8 @@ double ProteinLigandAtomPairScore
            int iltype,
            core::XYZ pxyz, core::XYZ lxyz,
            DerivativeAccumulator *da) const {
-  ProteinType ptype(iptype);
-  LigandType ltype(iltype);
+  ProteinLigandType ptype(iptype);
+  ProteinLigandType ltype(iltype);
   double distance = algebra::get_distance(protein_v, ligand_v);
   if (distance >= threshold_ || distance < 0.001) {
     return 0;
@@ -301,8 +313,8 @@ double ProteinLigandAtomPairScore::get_maximum_distance() const {
 
 double ProteinLigandAtomPairScore::evaluate(const ParticlePair &pp,
                                             DerivativeAccumulator *da) const {
-  int pt= pp[0]->get_value(get_protein_ligand_type_key());
-  int lt= pp[1]->get_value(get_protein_ligand_type_key());
+  int pt= get_index(pp[0]);
+  int lt= get_index(pp[1]);
   core::XYZ pxyz(pp[0]);
   core::XYZ lxyz(pp[1]);
   algebra::VectorD<3> pv(pxyz.get_coordinates()),
@@ -422,24 +434,24 @@ void add_protein_ligand_score_data(Atom atom) {
   std::string residue_string= rd.get_residue_type().get_string();
   if (!get_is_heterogen(rd)) {
     std::string score_type = residue_string + '_' + atom_string;
-    if (!ProteinType::get_key_exists(score_type)) {
+    if (!ProteinLigandType::get_key_exists(score_type)) {
       type=-1;
     } else {
       /*std::cout << "Type for " << atom << " is "
         << ProteinType(score_type) << std::endl;*/
-      type= ProteinType(score_type).get_index();
+      type= ProteinLigandType(score_type).get_index();
     }
   } else {
     std::string nm= internal::get_mol2_name(atom);
     if(nm.find('.') != String::npos) {
       nm.erase(nm.find('.'), 1);
     }
-    if (!LigandType::get_key_exists(nm)) {
+    if (!ProteinLigandType::get_key_exists(nm)) {
       type=-1;
     } else {
       //std::cout << "Type for " << atom << " is "
       //<< LigandType(nm) << std::endl;
-      type= LigandType(nm).get_index();
+      type= ProteinLigandType(nm).get_index();
     }
   }
   if (type==-1 && atom.get_element() != H) {
@@ -465,91 +477,3 @@ void add_protein_ligand_score_data(Hierarchy h) {
 
 
 IMPATOM_END_NAMESPACE
-
-IMPATOM_BEGIN_INTERNAL_NAMESPACE
-PMFTable::PMFTable(TextInput tin) {
-  std::istream &in =tin;
-  double bin;
-  std::string line;
-  std::getline(in, line);
-  std::istringstream iss(line);
-  iss >> bin;
-  unsigned int np, nl;
-  iss >> np >> nl;
-  if (np != ProteinType::get_number_unique()
-      || nl != LigandType::get_number_unique()) {
-    IMP_THROW("Expected number of protein and ligand types not found. "
-              << "Expected " << ProteinType::get_number_unique()
-              << " " << LigandType::get_number_unique()
-              << " but got " << np << " " << nl,
-              IOException);
-  }
-  if (!iss) {
-    IMP_THROW("Error reading bin size from line " << line,
-              IOException);
-  }
-  {
-    double test;
-    iss >> test;
-    if (iss) {
-      IMP_THROW("Extra data found on bin size line " << line
-                << " got " << test,
-                IOException);
-    }
-  }
-  bin_width_=bin;
-  inverse_bin_width_=1.0/bin;
-
-  data_.resize(ProteinType::get_number_unique());
-  int bins_read=-1;
-  for(unsigned int i=0;i<data_.size();i++){
-    data_[i].resize(LigandType::get_number_unique());
-    for(unsigned int j=0;j<data_[i].size();j++){
-      std::string line;
-      std::getline(in, line);
-      if (!in) {
-        IMP_THROW("Error getting next line at " << i << " " << j,
-                  IOException);
-      }
-      std::istringstream ins(line);
-      std::string pname, lname;
-      ins >> pname >> lname;
-      IMP_INTERNAL_CHECK(ProteinType(pname) == ProteinType(i),
-                         "Expected and found protein types don't match: "
-                         << "expected \"" <<  ProteinType(i).get_string()
-                         << " got " << pname << " at " << i << " " << j);
-      IMP_INTERNAL_CHECK(LigandType(lname) == LigandType(j),
-                         "Expected and found ligand types don't match: "
-                         << "expected \"" <<  LigandType(j).get_string()
-                         << " got " << pname << " at " << i << " " << j);
-      //      std::cout << line << std::endl;
-      //      if(j>1) exit(EXIT_FAILURE);
-      int cur_bins_read=0;
-      Floats data;
-      while(true) {
-        double potentialvalue;
-        ins >> potentialvalue;
-        if (ins) {
-          data.push_back(potentialvalue);
-          ++cur_bins_read;
-        } else {
-          break;
-        }
-      }
-      data_[i][j]= core::internal::RawOpenCubicSpline(data, bin_width_,
-                                                      inverse_bin_width_);
-      if (bins_read != -1 && cur_bins_read != bins_read) {
-        IMP_THROW("Read wrong number of bins from line: "
-                  << line << "\nExpected " << bins_read
-                  << " got " << cur_bins_read,
-                  IOException);
-      }
-      bins_read= cur_bins_read;
-      ins.clear();
-    }
-  }
-  max_= bin_width_*bins_read;
-  IMP_LOG(TERSE, "PMF table entries have "
-          << bins_read << " bins with width " << bin_width_ << std::endl);
-}
-IMPATOM_END_INTERNAL_NAMESPACE
