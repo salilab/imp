e6e8a2ff8d0618e38d2f8a96ec3a00471ec0ec96
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-12-09 22:53:32 +0000

    add exmaple pair score
    git-svn-id: https://svn.salilab.org/imp/trunk@7526 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/example/include/ExamplePairScore.h
@@ -0,0 +1,37 @@
+/**
+ *  \file ExamplePairScore.h
+ *  \brief A Score on the distance between a pair of particles.
+ *
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+ */
+
+#ifndef IMPEXAMPLE_EXAMPLE_PAIR_SCORE_H
+#define IMPEXAMPLE_EXAMPLE_PAIR_SCORE_H
+
+#include "example_config.h"
+#include <IMP/core/XYZ.h>
+#include <IMP/PairScore.h>
+#include <IMP/UnaryFunction.h>
+#include <IMP/Pointer.h>
+
+IMPEXAMPLE_BEGIN_NAMESPACE
+
+//! Apply a harmonic to the distance between two particles.
+/** The source code is as follows:
+    \include ExampleRestraint.h
+    \include ExampleRestraint.cpp
+*/
+class IMPEXAMPLEEXPORT ExamplePairScore : public PairScore
+{
+  double x0_, k_;
+public:
+  ExamplePairScore(double x0, double k);
+  IMP_PAIR_SCORE(ExamplePairScore);
+};
+
+IMP_OBJECTS(ExamplePairScore, ExamplePairScores);
+
+
+IMPEXAMPLE_END_NAMESPACE
+
+#endif  /* IMPEXAMPLE_EXAMPLE_PAIR_SCORE_H */
