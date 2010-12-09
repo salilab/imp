1b19565c7aaa07ac9978d32605dd537f67eca60c
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-12-09 05:31:06 +0000

    add test of call that fails with optimization turned on on the 32 bit linux test platform
    git-svn-id: https://svn.salilab.org/imp/trunk@7522 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/cgal/test/test_bad_sav.cpp
@@ -0,0 +1,17 @@
+/* Why would we copyright a test?
+ */
+
+#include <IMP/cgal/internal/union_of_balls.h>
+typedef IMP::algebra::Sphere3D S;
+typedef IMP::algebra::Vector3D V;
+int main(int, char *[]) {
+  std::vector<S> balls;
+  balls.push_back(S(V(2.441, 12.404, 7.789), 1.85));
+  balls.push_back(S(V(396, 13.826, 7.425), 2.275));
+  std::pair<double, double> dp
+    = IMP::cgal::internal::get_surface_area_and_volume(balls);
+  std::cout << dp.first << " and " << dp.second << std::endl;
+  IMP_USAGE_CHECK(dp.first >0, "Surface area must be positive");
+  IMP_USAGE_CHECK(dp.second >0, "Volume must be positive");
+  return 0;
+}
