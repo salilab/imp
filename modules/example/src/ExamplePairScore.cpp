047efffbdabb7c530aa39dc11c03faee15f6fb25
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-12-11 01:04:24 +0000

    must scroll all the way through compile messages, must scroll all the way....
    git-svn-id: https://svn.salilab.org/imp/trunk@7550 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/example/src/ExamplePairScore.cpp
+++ modules/example/src/ExamplePairScore.cpp
@@ -25,8 +25,8 @@ Float ExamplePairScore::evaluate(const ParticlePair &p,
   core::XYZ d0(p[0]);
   core::XYZ d1(p[1]);
   // log something
-  IMP_LOG(VERBOSE, "The distance off from x0 is " << diff << std::endl);
   double diff = (d0.get_coordinates()-d1.get_coordinates()).get_magnitude()-x0_;
+  IMP_LOG(VERBOSE, "The distance off from x0 is " << diff << std::endl);
   double score= .5*k_*square(diff);
   if (da) {
     // derivatives are requested
