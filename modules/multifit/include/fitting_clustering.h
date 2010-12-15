ebd805c2b281070846194d641a2f12604a4fa59b
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-12-15 13:23:54 +0000

    add transformation clustrering
    git-svn-id: https://svn.salilab.org/imp/trunk@7609 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/multifit/include/fitting_clustering.h
@@ -0,0 +1,33 @@
+/**
+ *  \file fitting_clustering.h
+ *  \brief Cluster fitting solutions by rmsd
+ *
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+ *
+ */
+#ifndef IMPMULTIFIT_FITTING_CLUSTERING_H
+#define IMPMULTIFIT_FITTING_CLUSTERING_H
+
+#include <string>
+#include <vector>
+#include <IMP/algebra/Transformation3D.h>
+#include <IMP/algebra/Rotation3D.h>
+#include <IMP/algebra/Vector3D.h>
+#include <IMP/em/rigid_fitting.h>
+#include "multifit_config.h"
+IMPMULTIFIT_BEGIN_NAMESPACE
+/**
+\param[in] pdb_filename cluster transformations with respect to this protein
+\param[in] ts the transformations with respect to the transformation in the PDB
+\param[out] clsutered_ts the clustered transformations
+\param[in] spacing transformations with rmsd < spacing*2 are clustered together
+*/
+void IMPMULTIFITEXPORT fitting_clustering (
+    const IMP::atom::Hierarchy &mh,
+    const IMP::em::FittingSolutions &ts,
+    IMP::em::FittingSolutions &clustered_ts,float spacing,
+    int top_sols,float rmsd);
+
+IMPMULTIFIT_END_NAMESPACE
+
+#endif /* IMPMULTIFIT_FITTING_CLUSTERING_H */
