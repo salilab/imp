0ce76f06b87376cffa28263f4f3090d5397ceb74
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-12-15 13:23:54 +0000

    add transformation clustrering
    git-svn-id: https://svn.salilab.org/imp/trunk@7609 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/multifit/src/RMSDClustering.cpp
@@ -0,0 +1,11 @@
+/**
+ *  \file RMSDClustering.cpp
+ *  \brief Cluster transformations by rmsd
+ *
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+ *
+ */
+#include <IMP/multifit/RMSDClustering.h>
+#include <queue>
+IMPMULTIFIT_BEGIN_NAMESPACE
+IMPMULTIFIT_END_NAMESPACE
