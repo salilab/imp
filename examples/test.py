ceb4b426f57bc031b645d6d8a88386df231be6e6
Massimiliano Bonomi <mbonomi@salilab.org>
2010-12-16 01:59:28 +0000

    correction to begin and end vector

--- examples/test.py
+++ examples/test.py
@@ -37,12 +37,13 @@ def create_representation():
         d_rbs=IMP.membrane.HelixDecorator.setup_particle(rbs[i],bb,ee)
         print " Rigid #",i," number of residues=",rbs[i].get_number_of_members()
         print "              begin=",d_rbs.get_begin()," end=",d_rbs.get_end()
-#   translate
-    tr0= IMP.algebra.Transformation3D(IMP.algebra.get_rotation_about_axis(IMP.algebra.Vector3D(0,1,0), 0.01), IMP.algebra.Vector3D(+1,0,0))
-#    tr0= IMP.algebra.Transformation3D(IMP.algebra.get_identity_rotation_3d(), IMP.algebra.Vector3D(1,0,0))
-    rbs[0].set_transformation(tr0)
-    tr1= IMP.algebra.Transformation3D(IMP.algebra.get_identity_rotation_3d(), IMP.algebra.Vector3D(-1,0,0))
-    rbs[1].set_transformation(tr1)
+#   trial translation+rotation
+    tr0= IMP.algebra.Transformation3D(IMP.algebra.get_rotation_about_axis(IMP.algebra.Vector3D(1,0,0), -math.pi/6), IMP.algebra.Vector3D(-10,0,0))
+#    rbs[0].set_transformation(IMP.algebra.compose(tr0, rbs[0].get_transformation()))
+    IMP.core.transform(rbs[0],tr0)
+    tr1= IMP.algebra.Transformation3D(IMP.algebra.get_rotation_about_axis(IMP.algebra.Vector3D(1,0,0), math.pi/6), IMP.algebra.Vector3D(10,0,0))
+#    rbs[1].set_transformation(IMP.algebra.compose(tr1, rbs[1].get_transformation()))
+    IMP.core.transform(rbs[1],tr1)
     return (m, rbs)
 
 def create_restraints(m, rbs):
@@ -64,7 +65,7 @@ def create_restraints(m, rbs):
         m.add_restraint(r)
 #       set tolerance
         m.set_maximum_score(r, .01)
-    def add_packing_restraint(rb, d0):
+    def add_packing_restraint(rb):
 ## if the rigid bodies are close, apply a filter on the crossing angle
 ## first define the allowed intervals, by specifying the center
 ## of the distributions
@@ -92,7 +93,7 @@ def create_restraints(m, rbs):
         lrb= IMP.container.ListSingletonContainer(m)
         for i in [0,len(rb)-1]:
             lrb.add_particle(rb[i])
-        nrb= IMP.container.ClosePairContainer(lrb, d0, 2.0)
+        nrb= IMP.container.ClosePairContainer(lrb, 12.0, 2.0)
         ps=  IMP.membrane.RigidBodyPackingScore(om_b, om_e, dd_b, dd_e)
         prs= IMP.container.PairsRestraint(ps, nrb)
         m.add_restraint(prs)
@@ -107,7 +108,7 @@ def create_restraints(m, rbs):
         p0=rbs[i].get_member(rbs[i].get_number_of_members()-1)
         p1=rbs[i+1].get_member(0)
         add_distance_restraint(p0,p1,20.0,100)
-    add_packing_restraint(rbs, 10.0)
+    add_packing_restraint(rbs)
     return m.get_restraints()
 
 #def create_discrete_states
@@ -118,11 +119,13 @@ def display(m):
     m.update()
     w= IMP.display.PymolWriter("out.pym")
     for i in [0,1]:
+        j=1
         for p in rbs[i].get_members():
             g= IMP.display.XYZRGeometry(p)
             g.set_name(str(i))
-            g.set_color(IMP.display.get_display_color(i))
+            g.set_color(IMP.display.get_display_color(j))
             w.add_geometry(g)
+            j=2
 
 # Here starts the real job...
 IMP.set_log_level(IMP.VERBOSE)
--- src/RigidBodyPackingScore.cpp
+++ src/RigidBodyPackingScore.cpp
@@ -31,7 +31,7 @@ Float RigidBodyPackingScore::evaluate(const ParticlePair &p,
   // assume they have an helix decorator
   membrane::HelixDecorator d0(p[0]);
   membrane::HelixDecorator d1(p[1]);
-  double omega, dist;
+  double omega, dist, max_dist, min_dist;
   core::XYZ x0,x1,x2,x3;
   algebra::VectorD<3> b0,e0,b1,e1,t0,t1;
   algebra::Transformation3D tr0,tr1;
@@ -41,10 +41,16 @@ Float RigidBodyPackingScore::evaluate(const ParticlePair &p,
   IMP_USAGE_CHECK(!da, "Derivatives not available");
 
   // begin and end point
-  b0=algebra::VectorD<3>(0.,0.,d0.get_begin());
-  e0=algebra::VectorD<3>(0.,0.,d0.get_end());
-  b1=algebra::VectorD<3>(0.,0.,d1.get_begin());
-  e1=algebra::VectorD<3>(0.,0.,d1.get_end());
+//  b0=algebra::VectorD<3>(0.,0.,d0.get_begin());
+//  e0=algebra::VectorD<3>(0.,0.,d0.get_end());
+//  b1=algebra::VectorD<3>(0.,0.,d1.get_begin());
+//  e1=algebra::VectorD<3>(0.,0.,d1.get_end());
+
+  b0=algebra::VectorD<3>(d0.get_begin(),0.0,0.0);
+  e0=algebra::VectorD<3>(d0.get_end(),0.0,0.0);
+  b1=algebra::VectorD<3>(d1.get_begin(),0.0,0.0);
+  e1=algebra::VectorD<3>(d1.get_end(),0.0,0.0);
+
 
   IMP_LOG(VERBOSE, "** BEFORE b0[0] " << b0[0] << " b0[1] "<<
                     b0[1] << " b0[2] " << b0[2] << std::endl);
@@ -62,8 +68,8 @@ Float RigidBodyPackingScore::evaluate(const ParticlePair &p,
                   "Particle is not a rigid body");
 
   // get rigid body transformation
-  tr0=core::RigidBody(p[0]).get_reference_frame().get_transformation_to();
-  tr1=core::RigidBody(p[1]).get_reference_frame().get_transformation_to();
+  tr0=core::RigidBody(p[0]).get_transformation();
+  tr1=core::RigidBody(p[1]).get_transformation();
 
   // and apply it to vectors
   b0=tr0.get_transformed(b0);
@@ -93,16 +99,26 @@ Float RigidBodyPackingScore::evaluate(const ParticlePair &p,
   IMP_LOG(VERBOSE, "** t1[0] " << t1[0] << " t1[1] "<<
                     t1[1] << " t1[2] " << t1[2] << std::endl);
 
-  omega=core::internal::dihedral(b0,t0,t1,b1,NULL,NULL,NULL,NULL);
+  omega=core::internal::dihedral(e0,t0,t1,e1,NULL,NULL,NULL,NULL);
   dist =segment.get_length();
 
   // log something
   IMP_LOG(VERBOSE, "** The crossing angle is " << omega <<
                    " and the distance is " << dist << std::endl);
 
-  // calculate score
+  // find min and max distance
+  min_dist=1.e+30;
+  max_dist=-1.e+30;
+  for(unsigned int i=0;i<omb_.size();i++){
+   if(ddb_[i] < min_dist) min_dist=ddb_[i];
+   if(dde_[i] > max_dist) max_dist=dde_[i];
+  }
+  // if TM are not interacting
+  if(dist <= min_dist || dist >=max_dist) return 0.;
+
+  // otherwise, calculate the score
   double score=1.;
-  for(unsigned int i=0;i<bb_.size();i++)
+  for(unsigned int i=0;i<omb_.size();i++)
    if(omega > omb_[i] && omega < ome_[i] &&
       dist > ddb_[i] && dist < dde_[i]) score=0.;
 
@@ -127,8 +143,8 @@ ContainersTemp RigidBodyPackingScore::get_input_containers(Particle *p) const {
 
 
 void RigidBodyPackingScore::do_show(std::ostream &out) const {
-  for(unsigned int i=0;i<bb_.size();i++)
-   out << "i" << i << "bb_=" << bb_[i] << "ee_=" << ee_[i] << std::endl;
+  for(unsigned int i=0;i<omb_.size();i++)
+   out << "i" << i << "omb_=" << omb_[i] << "ome_=" << ome_[i] << std::endl;
 }
 
 IMPMEMBRANE_END_NAMESPACE
