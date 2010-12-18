ad3bf759816f6ff05bc9c6fda3db9735b6d4473c
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-12-18 00:43:06 +0000

    fix various inconsistencies
    git-svn-id: https://svn.salilab.org/imp/trunk@7652 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/include/Plane3D.h
+++ modules/algebra/include/Plane3D.h
@@ -19,8 +19,10 @@ class Plane3D {
 public:
   Plane3D(){}
   Plane3D(const VectorD<3>& point_on_plane,const VectorD<3> &normal_to_plane) {
-    normal_ = normal_to_plane.get_unit_vector();
+    normal_ = normal_to_plane;
     distance_= normal_*point_on_plane;
+    IMP_USAGE_CHECK(std::abs(normal_.get_squared_magnitude()-1) < .05,
+                    "The normal vector must be normalized");
   }
   Plane3D(double distance_to_plane ,const VectorD<3> &normal_to_plane):
     distance_(distance_to_plane),
@@ -31,9 +33,14 @@ public:
   VectorD<3> get_point_on_plane() const {return normal_*distance_;}
   const VectorD<3> &get_normal() const {return normal_;}
   //! Project the point onto the plane
-  VectorD<3> get_projection(const VectorD<3> &p) const  {
+  VectorD<3> get_projected(const VectorD<3> &p) const  {
     return p-normal_*(normal_*p-distance_);
   }
+#ifndef IMP_DOXYGEN
+  VectorD<3> get_projection(const VectorD<3> &p) const  {
+    return get_projected(p);
+  }
+#endif
   /** @name Orientation
        Up is the direction of the normal. You really shouldn't use
        these as they aren't very reliable.
@@ -72,6 +79,14 @@ private:
 inline double get_distance(const Plane3D& pln, const VectorD<3> &p) {
   return (pln.get_projection(p)-p).get_magnitude();
 }
+
+
+//! return the point reflected about the plane
+inline VectorD<3> get_reflected(const Plane3D &pln, const VectorD<3> &p) {
+  VectorD<3> proj= pln.get_projected(p);
+  return p+2*(proj-p);
+}
+
 
 IMP_AREA_GEOMETRY_METHODS(Plane3D, plane_3d,
                           if (0) std::cout << g;
