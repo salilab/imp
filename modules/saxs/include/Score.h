2cf734c24dc939b526ffd360332598a283b671f5
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-12-20 03:17:03 +0000

    hide non-pythonable methods away
    git-svn-id: https://svn.salilab.org/imp/trunk@7736 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/saxs/include/Score.h
+++ modules/saxs/include/Score.h
@@ -20,6 +20,7 @@
 IMPSAXS_BEGIN_NAMESPACE
 
 class Profile;
+class Restraint;
 
 /**
   SAXS scoring class that allows to compute chi score for the fit between the
@@ -70,39 +71,6 @@ public:
                                  bool use_offset = false,
                                  const std::string fit_file_name = "") const;
 
-  //! compute derivatives for each Particle in particles
-  /**
-     \param[in] model_profile The current profile of particles
-     \param[in] particles Derivative will be computed for each particle
-     \param[in] derivatives Output vector
-     \param[in] use_offset if true, non zero offset for fitting is used
-     \return chi square score for current particle setup
-  */
-  void compute_chi_derivative(const Profile& model_profile,
-                              const Particles& particles,
-                              std::vector<algebra::VectorD<3> >& derivatives,
-                              bool use_offset = false) const {
-    return compute_chi_derivative(model_profile, particles, particles,
-                                  derivatives, use_offset);
-  }
-
-  //! compute derivatives for particles1 with respect to particles2
-  /**
-     This method is needed for rigid bodies when particles1 and particles2
-     are particles of two rigid bodies. In this case the particles of the
-     same rigid body are ignored.
-     \param[in] model_profile The current profile of particles
-     \param[in] particles1 Derivative will be computed for each particle
-     \param[in] particles2 Derivative will be computed relative to this set
-     \param[in] derivatives Output vector
-     \param[in] use_offset if true, non zero offset for fitting is used
-     \return chi square score for current particle setup
-  */
-  void compute_chi_derivative(const Profile& model_profile,
-                              const Particles& particles1,
-                              const Particles& particles2,
-                              std::vector<algebra::VectorD<3> >& derivatives,
-                              bool use_offset = false) const;
 
   //! computes scale factor given offset value
   Float compute_scale_factor(const Profile& model_profile,
@@ -129,12 +97,6 @@ public:
                            const Float chi_square,
                            const Float c=1, const Float offset=0) const;
 
-  // compute derivatives for each particle
-  void compute_chi_real_derivative(const Profile& model_profile,
-                                   const Particles& particles1,
-                                   const Particles& particles2,
-                              std::vector<algebra::VectorD<3> >& derivatives,
-                                   bool use_offset) const;
 
   // helper function for derivatives
   void compute_profile_difference(const Profile& model_profile,
@@ -146,7 +108,39 @@ public:
                         const Profile& model_profile,
                         std::vector<Floats>& output_values) const;
   IMP_REF_COUNTED_DESTRUCTOR(Score);
- protected:
+  friend class Restraint;
+  //! compute derivatives for particles1 with respect to particles2
+  /**
+     This method is needed for rigid bodies when particles1 and particles2
+     are particles of two rigid bodies. In this case the particles of the
+     same rigid body are ignored.
+     \param[in] model_profile The current profile of particles
+     \param[in] particles1 Derivative will be computed for each particle
+     \param[in] particles2 Derivative will be computed relative to this set
+     \param[in] derivatives Output vector
+     \param[in] use_offset if true, non zero offset for fitting is used
+     \return chi square score for current particle setup
+  */
+  void compute_chi_derivative(const Profile& model_profile,
+                              const Particles& particles1,
+                              const Particles& particles2,
+                              std::vector<algebra::VectorD<3> >& derivatives,
+                              bool use_offset = false) const;
+  void compute_chi_derivative(const Profile& model_profile,
+                              const Particles& particles,
+                              std::vector<algebra::VectorD<3> >& derivatives,
+                              bool use_offset = false) const {
+    return compute_chi_derivative(model_profile, particles, particles,
+                                  derivatives, use_offset);
+  }
+  // compute derivatives for each particle
+  void compute_chi_real_derivative(const Profile& model_profile,
+                                   const Particles& particles1,
+                                   const Particles& particles2,
+                              std::vector<algebra::VectorD<3> >& derivatives,
+                                   bool use_offset) const;
+
+
   const Profile& exp_profile_;   //  experimental saxs profile
 };
 
