50c7323c894586c9685a2a8ecd7b7d2d6ece2f70
dina@SALILAB.ORG <dina@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-12-20 22:33:15 +0000

    return relevant part of the test
    git-svn-id: https://svn.salilab.org/imp/trunk@7767 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/saxs/test/test_saxs.py
+++ modules/saxs/test/test_saxs.py
@@ -8,6 +8,36 @@ import time
 
 class SAXSProfileTest(IMP.test.TestCase):
 
+    def test_saxs_profile(self):
+        """Check protein profile computation"""
+        m = IMP.Model()
+
+        #! read PDB
+        mp= IMP.atom.read_pdb(self.get_input_file_name('6lyz.pdb'), m,
+                              IMP.atom.NonWaterNonHydrogenPDBSelector(),
+                              True, True)
+
+        #! read experimental profile
+        exp_profile = IMP.saxs.Profile(self.get_input_file_name('lyzexp.dat'))
+
+        print 'min_q = ' + str(exp_profile.get_min_q())
+        print 'max_q = ' + str(exp_profile.get_max_q())
+        print 'delta_q = ' + str(exp_profile.get_delta_q())
+
+        #! select particles from the model
+        particles = IMP.atom.get_by_type(mp, IMP.atom.ATOM_TYPE)
+
+        #! calculate SAXS profile
+        model_profile = IMP.saxs.Profile()
+        model_profile.calculate_profile(particles)
+        #model_profile.write_SAXS_file('i_single_protein_IMP.txt')
+
+        #! calculate chi-square
+        saxs_score = IMP.saxs.Score(exp_profile)
+        chi = saxs_score.compute_chi_score(model_profile)
+        print 'Chi = ' + str(chi)
+        self.assertAlmostEqual(chi, 0.5, delta=0.1)
+
     def test_saxs_restraint(self):
         """Check saxs restraint"""
         m = IMP.Model()
