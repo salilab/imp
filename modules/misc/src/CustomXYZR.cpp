ef1eb6bc46bca2e4f993649e3406d2442367fd59
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-12-21 19:13:40 +0000

    add custom radius key xyzr"
    git-svn-id: https://svn.salilab.org/imp/trunk@7797 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/misc/src/CustomXYZR.cpp
@@ -0,0 +1,19 @@
+/**
+ *  \file XYZR.cpp   \brief Simple xyzr decorator.
+ *
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+ *
+ */
+
+#include "IMP/misc/CustomXYZR.h"
+
+IMPMISC_BEGIN_NAMESPACE
+
+void CustomXYZR::show(std::ostream &out) const
+{
+  out << "(" << get_x()<< ", "
+      << get_y() << ", " << get_z() << ": " << get_radius() << ")";
+
+}
+
+IMPMISC_END_NAMESPACE
