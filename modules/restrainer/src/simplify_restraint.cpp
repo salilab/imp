89b0db457b03daf435f509812194ab3fb4b2a655
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-12-21 19:10:39 +0000

    no longer used
    git-svn-id: https://svn.salilab.org/imp/trunk@7793 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/restrainer/src/simplify_restraint.cpp
+++ modules/restrainer/src/simplify_restraint.cpp
@@ -240,7 +240,6 @@ SimpleEMFit create_simple_em_fit(atom::Hierarchies const &mhs,
   IMP_NEW(core::LeavesRefiner,leaves_ref,(atom::Hierarchy::get_traits()));
   IMP_NEW(em::FitRestraint, fit_rs, (ps, dmap,leaves_ref,
         FloatPair(0.,0.),
-        core::XYZR::get_default_radius_key(),
         atom::Mass::get_mass_key(),
         1.0));
   //Model *mdl = mhs[0].get_particle()->get_model();
