6d0788589e011cad8ae767af69bc9fad31ac4ab0
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-12-24 12:48:26 +0000

    provide rather too complicated test for previously failing corner case
    git-svn-id: https://svn.salilab.org/imp/trunk@7879 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/saxs/test/test_surface.py
+++ modules/saxs/test/test_surface.py
@@ -31,5 +31,28 @@ class SurfaceTest(IMP.test.TestCase):
         print 'Area = ' + str(total_area)
         self.assertAlmostEqual(total_area, 37.728, delta=0.1)
 
+    def test_corner_case(self):
+        """Check the surface area handle points on boundary"""
+        # this test could be simplified probably, but it is fast enough
+        # if we move to non-grid based SA, it should go away
+        ensemble = ["./433.pdb","./434.pdb"]
+        m = IMP.Model()
+        #! read PDBs
+        for struc in ensemble:
+            print " ... Fitting structure %s"%struc
+            mp= IMP.atom.read_pdb(self.get_input_file_name(struc), m,
+                                  IMP.atom.NonWaterNonHydrogenPDBSelector())
+
+            #! select particles from the model
+            particles = IMP.atom.get_by_type(mp, IMP.atom.ATOM_TYPE)
+            #! add radius for water layer computation
+            ft = IMP.saxs.default_form_factor_table()
+            for i in range(0, len(particles)):
+                radius = ft.get_radius(particles[i])
+            IMP.core.XYZR(particles[i]).set_radius(radius)
+            # compute surface accessibility
+            s = IMP.saxs.SolventAccessibleSurface()
+            surface_area = s.get_solvent_accessibility(IMP.core.XYZRs(particles))
+
 if __name__ == '__main__':
     IMP.test.main()
