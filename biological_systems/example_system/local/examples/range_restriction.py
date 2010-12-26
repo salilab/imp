85a8f2ce0523e0c3232fb5f86fc646ad6cb78071
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-12-26 23:53:10 +0000

    add local module
    git-svn-id: https://svn.salilab.org/imp/trunk@7886 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ biological_systems/example_system/local/examples/range_restriction.py
@@ -0,0 +1,14 @@
+import IMP.local
+import IMP.core
+import IMP.container
+
+bb= IMP.algebra.BoundingBox3D(IMP.algebra.Vector3D(0,0,0),
+                              IMP.algebra.Vector3D(10,10,10))
+
+m= IMP.Model()
+ps= IMP.core.create_xyzr_particles(m, 20, 1)
+sc= IMP.container.ListSingletonContainer(ps)
+ss= IMP.container.SingletonsConstraint(sc, IMP.local.ExampleSingletonModifier(bb), None)
+m.add_score_state(ss)
+
+# now optimize and things
---
+++ biological_systems/example_system/local/pyext/src/randomize.py
@@ -0,0 +1,8 @@
+# python files placed in this src directory are automatically added to the module.
+# The function below can be accessed as IMP.local.randomize.randomize_particles().
+
+import IMP.core
+
+def randomize_particle(p):
+    d= IMP.core.XYZ.decorate_particle(p)
+    d.set_coordinates(IMP.algebra.get_random_vector_in(IMP.algebra.get_unit_bounding_box_3d()))
