88ee668847e6ccdc2d2e0c7d58b77741737f13ef
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2010-12-27 10:07:13 +0000

    utility to create rotations
    git-svn-id: https://svn.salilab.org/imp/trunk@7920 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/multifit/bin/create_rotation_sampling_file.py
@@ -0,0 +1,17 @@
+import IMP.multifit
+import sys,os
+def usage():
+    if len(sys.argv) != 3:
+        print sys.argv[0]+" <num of rotations to sample> <output file>"
+        sys.exit()
+if __name__=="__main__":
+    usage()
+    rots=IMP.algebra.get_uniform_cover_rotations_3d(int(sys.argv[1]))
+    vec0=IMP.algebra.Vector3D(0,0,0)
+    trans=IMP.multifit.FittingSolutionRecords()
+    for i,r in enumerate(rots):
+        rec=IMP.multifit.FittingSolutionRecord()
+        rec.set_index(i);
+        rec.set_fit_transformation(IMP.algebra.Transformation3D(r,vec0))
+        trans.push_back(rec)
+    IMP.multifit.write_fitting_solutions(sys.argv[2],trans);
