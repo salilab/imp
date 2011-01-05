3a59d63b50bae608059a30c94fa7cdfad1da69d4
dina@SALILAB.ORG <dina@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-01-05 20:21:26 +0000

    regression clean up
    git-svn-id: https://svn.salilab.org/imp/trunk@8006 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/src/ParabolicFit.cpp
+++ modules/algebra/src/ParabolicFit.cpp
@@ -9,33 +9,31 @@
 
 IMPALGEBRA_BEGIN_NAMESPACE
 
-ParabolicFit::ParabolicFit(const algebra::Vector2Ds& data) :
-  data_(data) {
+ParabolicFit::ParabolicFit(const algebra::Vector2Ds& data) {
   // check that there are at least 3 points
-  IMP_USAGE_CHECK(data_.size() > 2,
+  IMP_USAGE_CHECK(data.size() > 2,
                   "At least 3 points are required for ParabolicFit "
-                  << data_.size() << " given");
-  find_regression();
-  evaluate_error();
+                  << data.size() << " given");
+  find_regression(data);
+  evaluate_error(data);
 }
 
-void ParabolicFit::find_regression()
-{
+void ParabolicFit::find_regression(const algebra::Vector2Ds& data) {
   // http://mathforum.org/library/drmath/view/72047.html
   double S00(0.0), S10(0.0), S20(0.0), S30(0.0), S40(0.0);
   double S01(0.0), S11(0.0), S21(0.0);
 
-  S00 = (double)data_.size();
+  S00 = (double)data.size();
 
-  for(unsigned int i=0; i<data_.size(); i++) {
-    S10 += data_[i][0];
-    double x2 = data_[i][0] * data_[i][0];
+  for(unsigned int i=0; i<data.size(); i++) {
+    S10 += data[i][0];
+    double x2 = data[i][0] * data[i][0];
     S20 += x2;
-    S30 += x2 * data_[i][0];
+    S30 += x2 * data[i][0];
     S40 += x2 * x2;
-    S01 += data_[i][1];
-    S11 += data_[i][0] * data_[i][1];
-    S21 += x2 * data_[i][1];
+    S01 += data[i][1];
+    S11 += data[i][0] * data[i][1];
+    S21 += x2 * data[i][1];
   }
 
   double d = S00*S20*S40 - S10*S10*S40 - S00*S30*S30 +
@@ -51,10 +49,10 @@ void ParabolicFit::find_regression()
         + S11*S20*S30 + S21*S10*S30 - S21*S20*S20) / d;
 }
 
-void ParabolicFit::evaluate_error() {
+void ParabolicFit::evaluate_error(const algebra::Vector2Ds& data) {
   error_ = 0.0;
-  for(unsigned int i=0; i<data_.size(); i++) {
-    double diff = a_*data_[i][0]*data_[i][0] + b_*data_[i][0] + c_ -data_[i][1];
+  for(unsigned int i=0; i<data.size(); i++) {
+    double diff = a_*data[i][0]*data[i][0] + b_*data[i][0] + c_ -data[i][1];
     error_ += diff*diff;
   }
 }
