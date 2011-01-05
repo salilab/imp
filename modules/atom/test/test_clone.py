d1eb29014f5754b9002fbd7f40bde5184b50ec4a
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-01-05 20:58:40 +0000

    Use smaller PDB file; clone should double the total number of particles.
    git-svn-id: https://svn.salilab.org/imp/trunk@8008 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/test/test_clone.py
+++ modules/atom/test/test_clone.py
@@ -7,11 +7,11 @@ class DecoratorTests(IMP.test.TestCase):
     def test_bonded(self):
         """Check close and destroy Hierarchy """
         m = IMP.Model()
-        mh= IMP.atom.read_pdb(self.get_input_file_name("input.pdb"),
-                              m)
+        mh= IMP.atom.read_pdb(self.get_input_file_name("mini.pdb"), m)
         nump= len(m.get_particles())
         mhc= IMP.atom.create_clone(mh)
         nnump= len(m.get_particles())
+        self.assertEqual(nump * 2, nnump)
         IMP.atom.destroy(mhc)
         mhc=None
         self.assertEqual(nump, len(m.get_particles()))
