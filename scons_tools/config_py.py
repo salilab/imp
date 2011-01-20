b13d3734214ea4ba8a3ae4c3993fb02d1a9e779d
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-01-20 19:21:38 +0000

    modularize it
    git-svn-id: https://svn.salilab.org/imp/trunk@8344 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- scons_tools/config_py.py
+++ scons_tools/config_py.py
@@ -12,8 +12,8 @@ def _flatten(strorlist, delim):
     else:
         return delim.join([_flatten(x, delim) for x in list(strorlist)])
 
-def _action_config_py(target, source, env):
-    config= source[0].get_contents().split("#")
+def write_config(h, env):
+    config=env['IMP_CONFIGURATION']
     def _export_to_config(name, ename, env, opt=False, delim=":"):
         if opt and not env.get(ename, None):
             return
@@ -44,14 +44,25 @@ def _action_config_py(target, source, env):
         _export_to_config(vp[0], vp[1], env, delim=vp[2])
     config.append('path="'+_flatten(os.environ['PATH'], ":")+'"')
     #print "opening h at " +target[0].abspath + " for module %(module)s"%vars
-    h = file(target[0].abspath, 'w')
     #print "Generating "+str(h)
     s= config
     for l in s:
         print >> h, l
 
+
+def _action_config_py(target, source, env):
+    #config= source[0].get_contents().split("#")
+    h = file(target[0].abspath, 'w')
+    _do_write_config(h, env)
+
 def _print_config_py(target, source, env):
     print "Generating config.py"
 
-ConfigPY = Builder(action=Action(_action_config_py,
+_ConfigPY = Builder(action=Action(_action_config_py,
                                  _print_config_py))
+
+def add(env):
+    env.Append(BUILDERS={'IMPConfigPY':_ConfigPY})
+    config_py=env.IMPConfigPY(target=["#/config.py"],
+                              source=[env.Value("#".join(env['IMP_CONFIGURATION']))])
+    return config_py
