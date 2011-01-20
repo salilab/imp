31b3d8c6f7b2b9d4023518001589c754ed2681b5
yannick@SALILAB.ORG <yannick@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-01-20 02:53:07 +0000

    rex
    git-svn-id: https://svn.salilab.org/imp/trunk@11367 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/isd/examples/replica_exchange.py
+++ modules/isd/examples/replica_exchange.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-import sys,os
+import sys,os,errno
 import atexit
 from IMP.isd.FileBasedGrid import FileBasedGrid
 from IMP.isd.hosts import create_host_list
@@ -9,20 +9,57 @@ import IMP.atom
 import IMP.container
 import IMP.isd
 import IMP.isd.shared_functions as sf
+#from IMP.isd.Replica import ReplicaSampler as RS
 
 IMP.set_log_level(IMP.NONE)
 
+###simulation settings
+#where to output files
+outfolder=os.path.join(os.getcwd(), 'results')
+#temp dir
+tmpdir = os.path.join(os.getcwd(),'tmp')
+#number of replicas / hosts
+nreps = 2
+#temperature distribution
+temps=[300.0]*nreps
+#thermostat coupling constant (berendsen, in fs)
+tau=[500.0]*nreps
+#list of files relative to the current dir to copy over to all nodes
+initpdb = "generated2.pdb"
+charmmtop = "top.lib"
+charmmpar =  "par.lib"
+restraints = "NOE_HN-full_7A_sparse100.tbl"
+filelist=[initpdb,charmmtop,charmmpar,restraints] #add whatever you want
+#prefix of output files 
+nums=[[os.path.join(outfolder,'p%02d' % (i+1))] for i in xrange(nreps)]
+#number of gibbs sampling steps
+n_gibbs = 2
+#number of md steps
+n_md = 500
+#number of mc steps
+n_mc = 50
+#where to run sims
+hostlist = ['localhost']*nreps
+
+#misc
 imppy = '/bongo1/home/yannick/impisd/build-fast/tools/imppy.sh'
+src_path = '/bongo1/home/yannick/impisd/src/isd/pyext/src'
+showX11 = True 
+X11_delay = 1.0
+grid_debug = False
+grid_verbose = False
+window_size = '80x25'
+
+def mkdir_p(path):
+    "mkdir -p, taken from stackoverflow" 
+    try:
+        os.makedirs(path)
+    except OSError as exc: # Python >2.5
+        if exc.errno == errno.EEXIST:
+            pass
+        else: raise
 
 def launch_grid():
-    hostlist = ['localhost']
-    tmpdir = '/tmp'
-    src_path = '/bongo1/home/yannick/impisd/src/isd/pyext/src'
-    showX11 = True 
-    X11_delay = 1.0
-    grid_debug = True 
-    grid_verbose = False
-    window_size = '80x25'
     hosts = create_host_list(hostlist,tmpdir)
     for host in hosts:
         #ugly hack
@@ -32,8 +69,7 @@ def launch_grid():
     grid.shared_temp_path = True
     if showX11:
         grid.window_size = window_size
-    grid.copy_files('./',["generated2.pdb","top.lib",
-        "par.lib","NOE_HN-full_7A_sparse100.tbl"])
+    grid.copy_files('./', filelist)
     #grid.copy_files(src_path,["shared_functions.py"])
     #register termination
     atexit.register(grid.terminate)
@@ -41,65 +77,102 @@ def launch_grid():
     grid.start()
     return grid
 
-def propagate_call(grid, sfo_id, funcname, *args, **kw):
+def broadcast(grid, sfo_id, funcname, *args, **kw):
     results = []
     for server in grid.servers[sfo_id]:
         func=getattr(server.proxy, funcname)
         results.append(func(*args, **kw))
     return results
 
-def get_result(results):
+def scatter(grid, sfo_id, funcname, arglist, kwlist=None):
+    results = []
+    if kwlist is None:
+        kwlist=[{} for i in xrange(len(arglist))]
+    for server,args,kw in zip(grid.servers[sfo_id],arglist,kwlist):
+        func=getattr(server.proxy, funcname)
+        results.append(func(*args, **kw))
+    return results
+
+def gather(results):
     retval=[]
     for server in results:
         retval.append(server.get())
     return retval
 
-if __name__ == '__main__':
+def get_energies(grid,sfo_id):
+    return scatter(broadcast(grid,sfo_id,'m','evaluate',False))
 
+def gen_pairs_list():
+    pass
+
+def get_cross_energies(pairslist):
+    raise NotImplementedError
+
+def replica_exchange(grid,sfo_id, replica, stepno):
+    energies = get_energies(grid,sfo_id)
+    gen_pairs_list()
+    #following is not needed since temperature 
+    #new_energies = get_cross_energies()
+
+def main():
+    
     # launch grid
+    print "launching grid"
     grid = launch_grid()
 
     #publish the shared object
+    print "publishing sfo"
     sfo = sf.sfo()
     sfo_id = grid.publish(sfo)
 
+    print "communication test"
     proxy = grid.acquire_service(sfo_id)
     print proxy.hello().get()
     grid.release_service(proxy)
 
     #call init on all nodes
-    requests = propagate_call(grid, sfo_id, 'init_model')
+    print "initializing model"
+    mkdir_p(tmpdir)
+    mkdir_p(outfolder)
+    requests = broadcast(grid, sfo_id, 'init_model', tmpdir, 
+            initpdb, restraints)
     #wait til init is done
-    results = get_result(requests)
+    results = gather(requests)
 
     # evaluate the score of the whole system (without derivatives)
+    print "initial energy"
     proxy = grid.acquire_service(sfo_id)
     print proxy.m('evaluate',False).get()
-    proxy.write_pdb('out.pdb').get()
+    print "writing initial structure to file initial.pdb"
+    proxy.write_pdb(os.path.join(outfolder,'initial.pdb')).get()
     grid.release_service(proxy)
     
     #berendsen 300K tau=0.5ps
     #perform two independent MC moves for sigma and gamma
-    temp=300.0
-    tau=500.0
-    get_result(propagate_call(grid, sfo_id, 'init_simulation', temp, tau))
+    print "initializing simulation and statistics"
+    gather(scatter(grid, sfo_id, 'init_simulation', zip(temps, tau)))
+    gather(scatter(grid, sfo_id, 'init_stats', nums))
+    #replica = RS()
 
-    stat=simstats()
-    n_gibbs = 100
-    n_md = 500
-    n_mc = 50
+    print "start gibbs sampling loop"
     for i in range(n_gibbs):
-        print "md step %d" % i
+        print "gibbs step %d" % i
         print " md"
-        do_md(n_md)
+        gather(broadcast(grid, sfo_id, 'do_md', n_md))
         print " mc"
-        do_mc_and_update_stepsize(n_mc)
+        gather(broadcast(grid, sfo_id, 'do_mc_and_update_stepsize', n_mc))
+        print " swaps"
+        #replica_exchange(grid, sfo_id, replica, i)
         print " stats"
-        stat.write_stats(i,n_mc)
+        gather(broadcast(grid, sfo_id, 'write_stats', i, n_mc))
 
-
-
-    #grid.terminate()
-    #grid.release_service(proxy)
+    print "terminating grid"
+    grid.terminate()
+    print "done."
     
 
+if __name__ == '__main__':
+
+    main()
+
+    
