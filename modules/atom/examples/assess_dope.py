9b2384a13518c28ef8ca14768119093b21451b3c
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-01-20 01:32:27 +0000

    fix example
    git-svn-id: https://svn.salilab.org/imp/trunk@8336 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/examples/assess_dope.py
+++ modules/atom/examples/assess_dope.py
@@ -1,11 +1,10 @@
 import IMP
 import IMP.atom
 import IMP.container
-import IMP.membrane
 
 def create_representation():
     m=IMP.Model()
-    mp0= IMP.atom.read_pdb('1fdx.B99990001.pdb', m, IMP.atom.NonWaterNonHydrogenPDBSelector())
+    mp0= IMP.atom.read_pdb(IMP.atom.get_example_path('1fdx.B99990001.pdb'), m, IMP.atom.NonWaterNonHydrogenPDBSelector())
     prot=IMP.atom.get_by_type(mp0, IMP.atom.CHAIN_TYPE)[0]
     return (m, prot)
 
@@ -16,10 +15,10 @@ def add_dope(m, prot):
     dpc = IMP.container.ClosePairContainer(dsc, 15.0, 0.0)
 # exclude pairs of atoms belonging to the same residue
 # for consistency with MODELLER DOPE score
-    f=IMP.membrane.SameResiduePairFilter()
+    f=IMP.atom.SameResiduePairFilter()
     dpc.add_pair_filter(f)
-    IMP.membrane.add_dope_score_data(prot)
-    dps= IMP.membrane.DopePairScore(15.0)
+    IMP.atom.add_dope_score_data(prot)
+    dps= IMP.atom.DopePairScore(15.0)
 #    dps= IMP.membrane.DopePairScore(15.0, IMP.membrane.get_data_path("dope_scorehr.lib"))
     d=   IMP.container.PairsRestraint(dps, dpc)
     m.add_restraint(d)
