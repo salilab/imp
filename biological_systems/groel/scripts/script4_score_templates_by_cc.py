d92462101a5e936c650692861bef8b12b513c421
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-01-23 18:55:23 +0000

    add segmentation and fitting scripts
    git-svn-id: https://svn.salilab.org/imp/trunk@8413 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ biological_systems/groel/scripts/script4_score_templates_by_cc.py
@@ -0,0 +1,70 @@
+import IMP.em
+import IMP.atom
+import IMP.multifit
+IMP.set_log_level(IMP.NONE)
+#--- parse the templates file
+templates_file="build_profile.prf"
+templates=[]
+seq_ids=[]
+for line in open(templates_file):
+    if line[0]=="#":
+        continue
+    s=line.split()
+    if int(s[0])==1:
+        continue
+    if not(int(s[4])-int(s[3])>480):
+        print "Not including:",s[0],s[1]
+    templates.append([s[1][:4],s[1][-1]])
+    seq_ids.append(s[10])
+templates_dir="data/templates/"
+#--- load the target density map
+dmap=IMP.em.read_map("groel_subunit_8.mrc",IMP.em.MRCReaderWriter())
+dmap.get_header_writable().set_resolution(10)
+#--- load IMP model
+mdl=IMP.Model()
+rb_refiner=IMP.core.LeavesRefiner(IMP.atom.Hierarchy.get_traits())
+template_fit_sols=[]
+best_fit=1.
+best_temp=[]
+dens_threshold=0.02
+#--- iterate over the templates and fit each of them
+for i,t in enumerate(templates):
+    print "fitting template "+t[0]+t[1]
+    #load the template
+    mh=IMP.atom.read_pdb(templates_dir+t[0]+".pdb",mdl)
+    #get the right chain
+    mh_chain=IMP.atom.get_by_type(mh,IMP.atom.CHAIN_TYPE)[ord(t[1])-ord('A')]
+    rb=IMP.atom.setup_as_rigid_body(mh_chain)
+    #fit the template to the density map
+    dmap.show()
+    sols=IMP.multifit.pca_based_rigid_fitting(rb,rb_refiner,dmap,dens_threshold)
+    IMP.core.transform(rb,sols.get_transformation(0))
+    #refine the best scoring fit
+    mhs=IMP.atom.Hierarchies()
+    mhs.append(mh_chain)
+    pdb_opt_state=None #IMP.atom.WritePDBOptimizerState(mhs,"refined_temp_%03d.pdb")
+    refined_sols = IMP.em.local_rigid_fitting(
+        rb,rb_refiner,
+        IMP.core.XYZR.get_default_radius_key(),
+        IMP.atom.Mass.get_mass_key(),dmap,pdb_opt_state,1,3,1000)
+    IMP.core.transform(rb,refined_sols.get_transformation(0))
+    IMP.atom.write_pdb(mh_chain,t[0]+t[1]+"_fitted.pdb")
+    template_fit_sols.append([
+        refined_sols.get_transformation(0)*sols.get_transformation(0),refined_sols.get_score(0)])
+    IMP.core.transform(rb,refined_sols.get_transformation(0).get_inverse())
+    IMP.core.transform(rb,sols.get_transformation(0).get_inverse())
+#--- write the best fitting score for each template
+output=open("score_templates_by_cc.log","w")
+output.write('{0:<12}{1:<12}{2:<12}{3:<}\n'.format('name','seq id','cc score','transformation'))
+
+for i,t in enumerate(templates):
+    rot=template_fit_sols[i][0].get_rotation().get_quaternion()
+    v=template_fit_sols[i][0].get_translation()
+    pretty_trans='{0:3.6f} {1:3.6f} {2:3.6f} {3:3.6f} {4:3.6f} {5:3.6f} {6:3.6f}\n'.format(
+        rot[0],rot[1],rot[2],rot[3],v[0],v[1],v[2])
+    output.write('{0:<12}{1:<12}{2:<12}{3:<}\n'.format(
+        t[0]+t[1],
+        seq_ids[i],
+        1.-template_fit_sols[i][1],
+        pretty_trans))
+output.close()
