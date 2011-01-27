be33169192b38974c87d0ec64c00d1204c79e0d8
javi@SALILAB.ORG <javi@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-01-27 16:49:39 +0000

    fixed cout error
    git-svn-id: https://svn.salilab.org/imp/trunk@8491 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/em2d/include/scores2D.h
+++ modules/em2d/include/scores2D.h
@@ -13,12 +13,9 @@
 #include "IMP/em2d/RegistrationResult.h"
 #include "IMP/Object.h"
 #include "IMP/VersionInfo.h"
-// #include <boost/shared_ptr.hpp>
+#include <functional>
 
 IMPEM2D_BEGIN_NAMESPACE
-
-class ScoreFunction;
-// typedef boost::shared_ptr<ScoreFunction> ScoreFunctionPtr;
 
 //! angle in the axis-angle representation of the rotation
 //! that takes first to second
@@ -38,34 +35,35 @@ IMPEM2DEXPORT double get_average_shift_error(
                                    const RegistrationResults &correct_RRs,
                                    const RegistrationResults &computed_RRs);
 
-//! Converts the cross-correlation coefficient into the em2d score
-IMPEM2DEXPORT double get_ccc_to_em2d(double ccc);
-
-//! Converts the em2d score into the cross-correlation coefficient
-IMPEM2DEXPORT double get_em2d_to_ccc(double em2d);
-
 //! Computes the cross-correlation coefficient between to matrices
 IMPEM2DEXPORT double get_cross_correlation_coefficient(const cv::Mat &m1,
                                                    const cv::Mat &m2);
 
-//! Get the em2d score given a set of registration results from images
-IMPEM2DEXPORT double get_em2d_score(const RegistrationResults &RRs);
-
-
-
-
+//! Get the gloal score given a set of individual registration results
+//! from images
+/*!
+  \note The function checks what time of registration results are given.
+        - If the registration results correspond to a coarse registration,
+        the score is based on the cross-correlation (currently, the score
+        is a mean of the cross-correlation coefficients.
+        - If the registration results are obtained after a fine registration,
+        the score is the average of all the registration scores. This score
+g        depends on the function selected. Eg. EM2DScore.
+*/
+IMPEM2DEXPORT double get_global_score(const RegistrationResults &RRs);
 
 
 //! Base class for all scoring functions related to em2d
 class IMPEM2DEXPORT ScoreFunction: public Object {
 public:
-  ScoreFunction() {};
+  ScoreFunction() {}
   //! Given an image and a projection, returns the appropiate score
   double get_score(Image *image,Image *projection) const {
     // trying to use the non-virtual interface (Alexandrescu, 39)
     return get_private_score(image,projection);
   }
-  void show(std::ostream &out) const {};
+  void show(std::ostream &out) const {}
+
   IMP_OBJECT_INLINE(ScoreFunction, show(out), {});
 private:
   virtual double get_private_score(Image *image,Image *projection) const = 0;
@@ -78,9 +76,8 @@ IMP_OUTPUT_OPERATOR(ScoreFunction);
 //! Score based on Chi^2 = ((pixels_iamge - pixels_projection)/stddev_image)^2
 class IMPEM2DEXPORT ChiSquaredScore: public ScoreFunction {
 public:
-  ChiSquaredScore() {};
-  void show(std::ostream &out) const {};
-
+  ChiSquaredScore() {}
+  void show(std::ostream &out) const {}
 private:
   double get_private_score(Image *image,Image *projection) const {
     return 0.0;
@@ -90,35 +87,15 @@ IMP_OBJECTS(ChiSquaredScore,ChiSquaredScores);
 IMP_OUTPUT_OPERATOR(ChiSquaredScore);
 
 
-//! Score based on cross correlation
-class IMPEM2DEXPORT CCCScore: public ScoreFunction {
-public:
-  CCCScore() {};
-  void show(std::ostream &out) const {};
-private:
-  double get_private_score(Image *image,Image *projection) const {
-    return get_cross_correlation_coefficient(image->get_data(),
-                                             projection->get_data());
-  }
-};
-IMP_OBJECTS(CCCScore,CCCScores);
-IMP_OUTPUT_OPERATOR(CCCScore);
-
-
 
 //! EM2DScore, based on squared differences
 //!  (pixels_iamge - pixels_projection)**2
 class IMPEM2DEXPORT EM2DScore: public ScoreFunction {
 public:
-  EM2DScore() {
-    IMP_LOG(IMP::VERBOSE,"EM2DScore function created" << std::endl);
-};
-  void show(std::ostream &out) const {
-    cout << "---> EM2DScore score_function." << std::endl;};
-
+  EM2DScore() {}
+  void show(std::ostream &out) const {}
 private:
   double get_private_score(Image *image,Image *projection) const {
-    IMP_LOG(IMP::TERSE,"I am scoring from EM2DScore" << std::endl);
     return 1-get_cross_correlation_coefficient(image->get_data(),
                                              projection->get_data());
   }
@@ -129,7 +106,7 @@ IMP_OUTPUT_OPERATOR(EM2DScore);
 
 
 
-// Comparison by value of the ccc
+//! Comparison by value of the ccc
 template<class T>
 class HasHigherCCC
 #ifndef SWIG
@@ -138,12 +115,14 @@ class HasHigherCCC
 #endif
  {
 public:
-  bool operator()(const T &a,const T &b) { return a.get_ccc() >= b.get_ccc();}
-  void show(std::ostream &out) const {};
+  bool operator()(const T &a,const T &b) const {
+    return a.get_ccc() >= b.get_ccc();
+  }
+  void show(std::ostream &out) const {}
 };
 
 
-// Comparison of pairs by checking the second element
+//! Comparison of pairs by checking the second element
 template<class T>
 class LessPairBySecond
 #ifndef SWIG
@@ -152,11 +131,28 @@ class LessPairBySecond
 #endif
  {
 public:
-  bool operator()(const T &a,const T &b) { return a.second < b.second;}
-  void show(std::ostream &out) const {};
+  bool operator()(const T &a,const T &b) const {
+    return a.second < b.second;
+  }
+  void show(std::ostream &out) const {}
 };
 
 
+
+//! Compare two classes that return a score
+template<class T>
+class HasLowerScore
+#ifndef SWIG
+:
+    public std::binary_function< T , T ,bool>
+#endif
+ {
+public:
+  bool operator()(const T &a,const T &b) const {
+    return a.get_score() < b.get_score();
+  }
+  void show(std::ostream &out) const {}
+};
 
 IMPEM2D_END_NAMESPACE
 
