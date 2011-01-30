abd12e6b24ba03bac4396bfff69643341f3123b8
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-01-30 00:32:12 +0000

    script updates
    git-svn-id: https://svn.salilab.org/imp/trunk@8534 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- biological_systems/groel/scripts/script4_score_templates_by_cc.py
+++ biological_systems/groel/scripts/script4_score_templates_by_cc.py
@@ -44,26 +44,24 @@ for i,t in enumerate(templates):
     mhs.append(mh_chain)
     pdb_opt_state=None #IMP.atom.WritePDBOptimizerState(mhs,"refined_temp_%03d.pdb")
     #todo - return refinement
-    '''
     refined_sols = IMP.em.local_rigid_fitting(
         rb,rb_refiner,
         IMP.atom.Mass.get_mass_key(),dmap,[],1,3,100)
-    '''
-    refined_sols = sols
+    #refined_sols = sols
     IMP.core.transform(rb,refined_sols.get_transformation(0))
     IMP.atom.write_pdb(mh_chain,t[0]+t[1]+"_fitted.pdb")
     template_fit_sols.append([
-        refined_sols.get_transformation(0)*sols.get_transformation(0),1.-refined_sols.get_score(0),t])
+        i,refined_sols.get_transformation(0)*sols.get_transformation(0),1.-refined_sols.get_score(0),t])
     IMP.core.transform(rb,refined_sols.get_transformation(0).get_inverse())
     IMP.core.transform(rb,sols.get_transformation(0).get_inverse())
 #-- sort the results by the cross correlation scores
-    template_fit_sols=sorted(template_fit_sols,key=lambda fit: fit[1])
+template_fit_sols=sorted(template_fit_sols,key=lambda fit: fit[2],reverse=True)
 #--- write the best fitting score for each template
 output=open("score_templates_by_cc.log","w")
 output.write('{0:<12}{1:<12}{2:<12} {3:<}\n'.format('name','seq id','cc score','transformation'))
 print len(template_fit_sols[0])
 print template_fit_sols[0]
-for i,[trans,score,t] in enumerate(template_fit_sols):
+for i,trans,score,t in template_fit_sols:
     rot=trans.get_rotation().get_quaternion()
     v=trans.get_translation()
     pretty_trans='{0:3.6f} {1:3.6f} {2:3.6f} {3:3.6f} {4:3.6f} {5:3.6f} {6:3.6f}\n'.format(
