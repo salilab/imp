b748a791dd57106cb7052697536966ae724c1a86
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-02-02 21:47:59 +0000

    forgotten include
    git-svn-id: https://svn.salilab.org/imp/trunk@8627 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/statistics/src/internal/centrality_clustering.cpp
+++ modules/statistics/src/internal/centrality_clustering.cpp
@@ -7,6 +7,7 @@
 #include <IMP/statistics/internal/centrality_clustering.h>
 #include <IMP/statistics/statistics_macros.h>
 #include <IMP/statistics/PartitionalClustering.h>
+#include <IMP/Pointer.h>
 #if BOOST_VERSION > 103900
 #include <boost/property_map/property_map.hpp>
 #else
