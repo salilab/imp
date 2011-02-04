f66914ab3a760a4738abd01b7b5ab8bb730c39ca
pellarin@SALILAB.ORG <pellarin@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-02-04 22:05:24 +0000

    updated: examples/ubiquitin/replica_exchange.py
    git-svn-id: https://svn.salilab.org/imp/trunk@11394 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/isd/examples/ubiquitin/replica_exchange.py
+++ modules/isd/examples/ubiquitin/replica_exchange.py
@@ -4,6 +4,8 @@ import sys,os,errno
 import atexit
 import random
 
+# Comment either this line or the following to attain different communication schemes:
+# to start pyro
 from IMP.isd.PyroGrid import PyroGrid as Grid
 #from IMP.isd.FileBasedGrid import FileBasedGrid as Grid
 from IMP.isd.hosts import create_host_list
@@ -26,7 +28,7 @@ nreps = 8
 #lambda scaling distribution
 kB= (1.381 * 6.02214) / 4184.0
 lambda_1 = 1.0
-lambda_N = 0.7
+lambda_N = 0.8
 lambdas=[lambda_N*(lambda_1/lambda_N)**((float(nreps)-k)/(nreps-1)) 
         for k in xrange(1,nreps+1)]
 #thermostat coupling constant (berendsen, in fs)
@@ -37,12 +39,16 @@ stat_rate=[10]*nreps
 initpdb = "generated2.pdb"
 charmmtop = "top.lib"
 charmmpar =  "par.lib"
-restraints = "NOE_HN-full_7A_sparse100.tbl"
+restraints = "NOE_HN-full_7A_sparseHA.tbl"
 filelist=[initpdb,charmmtop,charmmpar,restraints] #add whatever you want
 #prefix of output files 
 nums=[[os.path.join(outfolder,'r%02d' % (i+1))] for i in xrange(nreps)]
-#number of gibbs sampling steps
-n_gibbs = 1000
+#number of gibbs sampling steps in  the first temperature relaxation
+n_gibbs1 = 10
+#number of gibbs sampling steps in  the second temperature relaxation
+n_gibbs2 = 10
+#number of gibbs sampling steps in  the production temperature relaxation
+n_gibbs3 = 10
 #number of md steps
 n_md = 1000
 #number of mc steps (not < 50 because adaptive)
@@ -56,16 +62,16 @@ rex_xchg='gromacs'
 #whether to use TuneRex to tune temperatures
 tune_temps = True
 tune_data = {'rate' : 100, #temp optimization rate, in rex steps.
-        'method':'flux', #flux-optimization or cv-optimization
+        'method':'ar', #either "ar" acceptance ratio optimization, or "flux" optimization
         'alpha':0.05} # type I error on the estimates
 templog = os.path.join(outfolder, 'temps.txt')
-rexlog = os.path.join(outfolder,'replicanums.txt'),
+rexlog = os.path.join(outfolder,'replicanums.txt')
 
 #misc
 
-imppy = '/bongo1/home/yannick/imp_local/build-fast/tools/imppy.sh'
-src_path = '/bongo1/home/yannick/imp_local/build-fast/build/lib/IMP/isd'
-showX11 = True 
+imppy = '/piano1/home/pellarin/imp-projects/imp-isd-new-fast/tools/imppy.sh'
+src_path = '/piano1/home/pellarin/imp-projects/imp-isd-new/modules/isd/pyext/src/'
+showX11 = False 
 grid_debug = False
 grid_verbose = False
 X11_delay = 1.0
@@ -89,11 +95,16 @@ def launch_grid():
     for host in hosts:
         #ugly hack
         host.init_cmd = imppy + ' !'
-    #grid = Grid(hosts, src_path, showX11, X11_delay, grid_debug,
-    #        grid_verbose)
-    #grid.shared_temp_path = shared_temp_path
+	
+    #pyro grid
     grid = Grid(hosts, src_path, showX11, X11_delay, grid_debug,
             grid_verbose, shared_temp_path, nshost, terminate_during_publish)
+	    
+    #uncomment the following lines and comments the two previous ones to use file based grid	
+    #file based grid
+    #grid = Grid(hosts, src_path, showX11, X11_delay, grid_debug,
+    #        grid_verbose)
+    #grid.shared_temp_path = shared_temp_path	    
     if showX11:
         grid.window_size = window_size
     grid.copy_files('./', filelist)
@@ -127,15 +138,15 @@ def main():
     print "initializing model"
     mkdir_p(tmpdir)
     mkdir_p(outfolder)
-    requests = grid.broadcast(sfo_id, 'init_model', tmpdir)
+    requests = grid.broadcast(sfo_id, 'init_model', tmpdir, initpdb, restraints)
     #wait til init is done
     results = grid.gather(requests)
 
-    #turn off noise (works because IMP.NONE is picklable, being an int.
+    #turn off verbose noise (works because IMP.NONE is picklable, being an int.
     grid.gather(grid.broadcast(sfo_id, 'set_checklevel', IMP.NONE))
     grid.gather(grid.broadcast(sfo_id, 'set_loglevel', IMP.NONE))
 
-    # evaluate the score of the whole system (without derivatives)
+    # evaluate the score of the whole system (without derivatives, False flag)
     print "initial energy"
     grid.gather(grid.broadcast(sfo_id, 'm', 'evaluate', False))
     
@@ -143,15 +154,15 @@ def main():
     #perform two independent MC moves for sigma and gamma
     print "initializing simulation and statistics"
     grid.gather(grid.scatter(sfo_id, 'init_simulation', 
-        zip(lambdas, tau, stat_rate)))
+        zip(lambdas, tau)))
     grid.gather(grid.scatter(sfo_id, 'init_stats', nums))
     replica = ReplicaTracker(nreps, lambdas, grid, sfo_id,
             rexlog=rexlog,
             scheme=rex_scheme, xchg=rex_xchg, 
             tune_temps=tune_temps, tune_data=tune_data, templog=templog)
 
-    print "start gibbs sampling loop"
-    for i in range(n_gibbs):
+    print "start gibbs sampling loop: first relaxation"
+    for i in range(n_gibbs1):
         print "\rgibbs step %d" % i,
         sys.stdout.flush()
         #print " md"
@@ -159,6 +170,35 @@ def main():
         #print " mc"
         grid.gather(grid.broadcast(sfo_id, 'do_mc_and_update_stepsize', n_mc))
         #print " swaps"
+	replica.tune_data['dumb_scale']=0.5
+        replica.replica_exchange()
+        #print " stats"
+        replica.write_rex_stats()
+
+    print "start gibbs sampling loop: second relaxation"
+    for i in range(n_gibbs2):
+        print "\rgibbs step %d" % i,
+        sys.stdout.flush()
+        #print " md"
+        grid.gather(grid.broadcast(sfo_id, 'do_md', n_md))
+        #print " mc"
+        grid.gather(grid.broadcast(sfo_id, 'do_mc_and_update_stepsize', n_mc))
+        #print " swaps"
+	replica.tune_data['dumb_scale']=0.2
+        replica.replica_exchange()
+        #print " stats"
+        replica.write_rex_stats()
+
+    print "start gibbs sampling loop: production"
+    for i in range(n_gibbs3):
+        print "\rgibbs step %d" % i,
+        sys.stdout.flush()
+        #print " md"
+        grid.gather(grid.broadcast(sfo_id, 'do_md', n_md))
+        #print " mc"
+        grid.gather(grid.broadcast(sfo_id, 'do_mc_and_update_stepsize', n_mc))
+        #print " swaps"
+	replica.tune_temps=False
         replica.replica_exchange()
         #print " stats"
         replica.write_rex_stats()
