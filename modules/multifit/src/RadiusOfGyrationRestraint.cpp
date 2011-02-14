31756137dcb65b3937dcdacf723ae96abce34132
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-02-14 21:40:40 +0000

    fix rog calculation
    git-svn-id: https://svn.salilab.org/imp/trunk@8706 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/multifit/src/RadiusOfGyrationRestraint.cpp
+++ modules/multifit/src/RadiusOfGyrationRestraint.cpp
@@ -14,7 +14,7 @@
 
 IMPMULTIFIT_BEGIN_NAMESPACE
 
-namespace {
+
  //!get radius as function of number of amino acids
 /** rad = pow((n_aa times 3.478 ) ,(1./3.))
     Alber et al, Structure 2005 (basically the same as rho=1.3)
@@ -27,9 +27,22 @@ float get_approximated_radius_of_gyration(int len) {
   //alpha: packing ratio (~.64 for proteins)
   //rad = (n_aa * (3.5**3)  / .64 ) **(1./3.)
   //simpler approximation: assume density_prot=1.3
-  //float rad=pow((len*33.54),1./3.);
+  //  float rad=pow((len*33.54),1./3.);
   //Alber Structure 2005 (basically the same as rho=1.3)
-  float rad=pow(3.478*len,1./3.);
+  float rad=get_approximated_radius(len);
+  return rad*std::sqrt(3./5);
+}
+
+float get_approximated_radius(int len) {
+
+  //follow shen et al, 2005: 4N pi a^3 / 3 alpha = 4 pi r^3 / 3
+  //  N: no of residues, a: bead radius, 3.5 A,
+  //alpha: packing ratio (~.64 for proteins)
+  //rad = (n_aa * (3.5**3)  / .64 ) **(1./3.)
+  //simpler approximation: assume density_prot=1.3
+  //  float rad=pow((len*33.54),1./3.);
+  //Alber Structure 2005 (basically the same as rho=1.3)
+  float rad=3.478*pow(len,1./3.);
   return rad;
 }
   float get_actual_radius_of_gyration(Particles ps) {
@@ -40,12 +53,12 @@ float get_approximated_radius_of_gyration(int len) {
   cm/=ps.size();
   double ret=0;
   for (unsigned int i=0; i < ps.size(); ++i) {
-    double d= get_squared_distance(core::XYZ(ps[i]).get_coordinates(),cm);
+    double d= get_distance(core::XYZ(ps[i]).get_coordinates(),cm);
     ret+= d;
   }
-  return std::sqrt(ret/ps.size());
+  return ret/ps.size();
   }
-}
+
 
 
 RadiusOfGyrationRestraint::RadiusOfGyrationRestraint(Particles ps,
@@ -57,7 +70,7 @@ RadiusOfGyrationRestraint::RadiusOfGyrationRestraint(Particles ps,
   mdl_=ps[0]->get_model();
   predicted_rog_ = get_approximated_radius_of_gyration(num_residues);
   scale_=scale;
-  hub_=new core::HarmonicUpperBound(predicted_rog_*scale_,0.1);
+  hub_=new core::HarmonicUpperBound(predicted_rog_*scale_,1);
 }
 
 double
@@ -66,7 +79,10 @@ const {
   //calculate actual rog
   //todo - do not use get_input_particles function
   float actual_rog=get_actual_radius_of_gyration(get_input_particles());
-  return hub_->evaluate(actual_rog);//todo - should we substrat the radius?
+  IMP_LOG(VERBOSE,"actual_rog:"<<actual_rog<<" predicted:"<<predicted_rog_<<
+          " scale:"<<predicted_rog_*scale_<<" score: "<<
+          hub_->evaluate(actual_rog)<<std::endl);
+  return hub_->evaluate(actual_rog);
 }
 
 
@@ -99,6 +115,7 @@ ContainersTemp RadiusOfGyrationRestraint::get_input_containers() const {
 
 void RadiusOfGyrationRestraint::do_show(std::ostream& out) const
 {
-  out<<"RadiusOfGyrationRestraint"<<std::endl;
+  out<<get_name()<<" predicted_rog:"<<predicted_rog_<<
+    " scale:"<<scale_<<std::endl;
 }
 IMPMULTIFIT_END_NAMESPACE
