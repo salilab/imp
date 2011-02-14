68588d155664e4868387e821d225a58f10273c79
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-02-14 22:02:42 +0000

    updated test
    git-svn-id: https://svn.salilab.org/imp/trunk@8708 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/multifit/test/test_read_proteomics_data.py
+++ modules/multifit/test/test_read_proteomics_data.py
@@ -20,6 +20,8 @@ class ReadProteomicsTests(IMP.test.TestCase):
         self.assertEqual(self.prot_data.get_number_of_interactions(),2)
         self.assertEqual(self.prot_data.get_start_res(0),1)
         self.assertEqual(self.prot_data.get_end_res(0),100)
+        self.assertEqual(len(self.prot_data.get_interaction(0)),2)
+        self.assertEqual(len(self.prot_data.get_interaction(1)),3)
         print self.prot_data.get_interaction(0)
         print self.prot_data.get_interaction(1)
         for ind in self.prot_data.get_interaction(1):
