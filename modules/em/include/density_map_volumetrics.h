da64a1715441a9eed884352e2f9f542bdd539c23
schwarz@SALILAB.ORG <schwarz@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-03-07 18:46:30 +0000

    modified em::densityMapVolumetrics file names as well as variable names to conform to IMP standard
    git-svn-id: https://svn.salilab.org/imp/trunk@8838 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/em/include/density_map_volumetrics.h
+++ modules/em/include/density_map_volumetrics.h
@@ -44,33 +44,33 @@ IMPEMEXPORT double get_used_protein_density();
 
 //! sets the protein density value used for internal computations
 /*! sets the protein density value used for internal computations
- * \param[in] densityValue : the protein density value that is used in internal
+ * \param[in] density_value : the protein density value that is used in internal
  * computations (in Da/Angstrom^3)
  */
-IMPEMEXPORT double set_used_protein_density(double densityValue);
+IMPEMEXPORT double set_used_protein_density(double density_value);
 
 
 //! sets the protein density value used for internal computations
 /*! sets the protein density value used for internal computations
- * \param[in] densityReference : a reference protein density value that
+ * \param[in] density_reference : a reference protein density value that
  * we wish to use in our computations (in Da/Angstrom^3).
  *
  * See ProteinDensityReference for authorized values.
  */
 IMPEMEXPORT double set_used_protein_density(
-      ProteinDensityReference densityReference);
+      ProteinDensityReference density_reference);
 
 
 //! returns the current protein density value
 /*! returns the protein density value as it has been defined
  *  in a reference paper
- * \param[in] densityReference : the name of a reference
+ * \param[in] density_reference : the name of a reference
  *   (See ProteinDensityReference for authorized values)
  *
  * NB : densities are expressed in Da/Angstrom^3
  */
 IMPEMEXPORT double get_reference_protein_density(
-      ProteinDensityReference densityReference);
+      ProteinDensityReference density_reference);
 
 
 //! Compute an approximate volume
@@ -103,25 +103,25 @@ IMPEMEXPORT Float get_molecular_mass_at_threshold(
  * (i.e, the set of voxels with intensity greater than the threshold
  * occupies that volume)
 \param[in] m a density map
-\param[in] desiredVolume (in A^3)
+\param[in] desired_volume (in A^3)
  */
 IMPEMEXPORT Float get_threshold_for_approximate_volume(
       DensityMap* m,
-      Float desiredVolume);
+      Float desired_volume);
 
 
 //! Computes the threshold to consider in an EM map to get a desired mass
 /** Computes the threshold to consider in an EM map to get a desired mass
  * (only voxels with intensity greater than the threshold are considered)
 \param[in] m a density map
-\param[in] desiredMass (in Da)
+\param[in] desired_mass (in Da)
 \note By default, the method assumes 0.826446 Da per cubic A
  (Harpaz et al 1994).
 this constant can be modified through the set_used_protein_density function.
  */
 IMPEMEXPORT Float get_threshold_for_approximate_mass(
       DensityMap* m,
-      Float desiredMass);
+      Float desired_mass);
 
 IMPEM_END_NAMESPACE
 
