cd439e62e1e7e59fbc0af1075e40ddc66680f047
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-03-08 23:38:19 +0000

    don't test classes that don't exist
    git-svn-id: https://svn.salilab.org/imp/trunk@8852 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- tools/container_templates/test.py
+++ tools/container_templates/test.py
@@ -137,47 +137,6 @@ class CLASSNAMEContainerTest(IMP.test.TestCase):
         print mt
         self.assertAlmostEqual(mt, f, delta=.1*f)
 
-    def test_max_restraint(self):
-        """Test the MaximumCLASSNAMERestraint"""
-        m= IMP.Model()
-        c= IMP.container.ListCLASSNAMEContainer(m)
-        self.assertEqual(c.get_ref_count(), 1)
-        for i in range(0,10):
-            c.add_FUNCTIONNAME(self.create_FUNCTIONNAME(m))
-        print c.get_number_of_FUNCTIONNAMEs()
-        d= self.create_CLASSFUNCTIONNAME_score()
-        self.assertEqual(d.get_ref_count(), 1)
-        r= IMP.container.MaximumCLASSNAMERestraint(d, c)
-        self.assertEqual(c.get_ref_count(), 2)
-        self.assertEqual(d.get_ref_count(), 2)
-        r.set_n(4)
-        m.add_restraint(r)
-        f= m.evaluate(False)
-        print f
-        ms= []
-        print c.get_number_of_FUNCTIONNAMEs()
-        for i in range(0,10):
-            ps= c.get_FUNCTIONNAME(i)
-            cm= d.evaluate(ps, None)
-            ms.append(cm)
-        ms.sort()
-        print ms
-        mt=0
-        for i in range(0, 4):
-            mt = mt+ ms[-i-1]
-        print mt
-        self.assertAlmostEqual(mt, f, delta=.1*f)
-    def test_max_score(self):
-        """Test the MaximumCLASSNAMEScore"""
-        m= IMP.Model()
-        s= []
-        for i in range(0,5):
-            s.append(IMP._ConstCLASSNAMEScore(i))
-        ps= IMP.container.MaximumCLASSNAMEScore(s, 2)
-        p= self.create_FUNCTIONNAME(m)
-        ps.set_was_used(True)
-        v= ps.evaluate(p, None)
-        self.assertEqual(v, 7)
     def test_min_score(self):
         """Test the MinimumCLASSNAMEScore"""
         m= IMP.Model()
