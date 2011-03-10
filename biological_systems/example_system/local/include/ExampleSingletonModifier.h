7e2922a51ec8d831837c76fcd38d4942755d33d3
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-03-10 22:07:23 +0000

    fix doxygen call
    git-svn-id: https://svn.salilab.org/imp/trunk@8874 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- biological_systems/example_system/local/include/ExampleSingletonModifier.h
+++ biological_systems/example_system/local/include/ExampleSingletonModifier.h
@@ -23,7 +23,7 @@ IMPEXAMPLESYSTEMLOCAL_BEGIN_NAMESPACE
      or IMP::core::SingletonsRestraint to keep a set of particles
      in a box.
 
-     \pythonlocal{range_restriction}
+     \pythonexample{range_restriction}
 
      The source code is as follows:
      \include ExampleRestraint.h
