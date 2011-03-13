046a54f971d2d61d513c7b947aa5eb23ee54df30
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-03-13 19:40:41 +0000

    use base class for BD
    git-svn-id: https://svn.salilab.org/imp/trunk@8892 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/include/BrownianDynamics.h
+++ modules/atom/include/BrownianDynamics.h
@@ -11,7 +11,8 @@
 
 #include "atom_config.h"
 #include "Diffusion.h"
-#include "SimulationParameters.h"
+#include "Simulator.h"
+#include "atom_macros.h"
 
 #include <IMP/Particle.h>
 #include <IMP/Optimizer.h>
@@ -50,69 +51,19 @@ class SimulationParameters;
 
     \see Diffusion
   */
-class IMPATOMEXPORT BrownianDynamics : public Optimizer
+class IMPATOMEXPORT BrownianDynamics : public Simulator
 {
 public:
   //! Create the optimizer
   /** If sc is not null, that container will be used to find particles
       to move, otherwise the model will be searched.*/
-  BrownianDynamics(SimulationParameters si,
-                   SingletonContainer *sc=NULL);
-
-  //! Simulate until the given time in fs
-  double simulate(float time_in_fs);
-
-
-  //! Define the feature size of the system
-  /** The time step will be scaled so that particles do not move further than
-      this in a single step. This ensures that objects cannot pass through
-      one another or miss important features of the force field. */
-  void set_minimum_feature_size(double df) {
-    IMP_USAGE_CHECK(df > 0, "The max change must be positive");
-    feature_size_2_=unit::SquareAngstrom(square(df));
-  }
-#if !defined(SWIG) && !defined(IMP_DOXYGEN)
-  void set_minimum_feature_size(unit::Angstrom f) {
-    set_minimum_feature_size(unit::strip_units(f));
-  }
-#endif
-
- SimulationParameters get_simulation_parameters() const{
-    return si_;
-  }
-
-  void set_adjust_step_size(bool tf);
-
-  /** If the score exceeds this, an error handler is kicked in which
-      attempts to relax the model before proceeding. A warning is
-      printed.
-  */
-  void set_maximum_score(double s);
-
-  SingletonContainer *get_diffusing_particles() const;
-
-  IMP_OPTIMIZER(BrownianDynamics);
-private:
-  void copy_coordinates(SingletonContainer *sc,
-                        std::vector<algebra::VectorD<3> > &v) const;
-  void revert_coordinates(SingletonContainer *sc,
-                          std::vector<algebra::VectorD<3> > &v);
-
-  void take_step(SingletonContainer *sc, unit::Femtosecond dt);
-
-  SingletonContainer* setup_particles() const;
-
-  /* unit::KilocaloriePerAngstromPerMol
-     get_force_scale_from_D(unit::SquareCentimeterPerSecond D) const;*/
-
-
-  unit::SquareAngstrom feature_size_2_;
-  RefCountingDecorator<SimulationParameters> si_;
-  IMP::internal::OwnerPointer<SingletonContainer> sc_;
-  unsigned int failed_steps_;
-  unsigned int successful_steps_;
-  bool dynamic_steps_;
-  double maximum_score_;
+  BrownianDynamics(Model *m);
+  IMP_SIMULATOR(BrownianDynamics);
+ private:
+  boost::normal_distribution<double> nd_;
+  typedef boost::variate_generator<RandomNumberGenerator&,
+                                   boost::normal_distribution<double> > RNG;
+  RNG sampler_;
 };
 
 
