762111da44fcfa8431e532f77ca5015687559b81
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-03-13 19:39:33 +0000

    add simulator base class
    git-svn-id: https://svn.salilab.org/imp/trunk@8891 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/atom/include/Simulator.h
@@ -0,0 +1,77 @@
+/**
+ *  \file atom/Simulator.h
+ *  \brief Simple molecular dynamics optimizer.
+ *
+ *  Copyright 2007-2011 IMP Inventors. All rights reserved.
+ *
+ */
+
+#ifndef IMPATOM_SIMULATOR_H
+#define IMPATOM_SIMULATOR_H
+
+#include "atom_config.h"
+
+#include <IMP/Particle.h>
+#include <IMP/Optimizer.h>
+#include <IMP/internal/units.h>
+#include <IMP/algebra/Vector3D.h>
+
+IMPATOM_BEGIN_NAMESPACE
+
+// for swig
+class SimulationParameters;
+
+//! The base class for simulators.
+class IMPATOMEXPORT Simulator : public Optimizer
+{
+public:
+  //! Create the optimizer
+  /** If sc is not null, that container will be used to find particles
+      to move, otherwise the model will be searched.*/
+  Simulator(Model *m,
+            std::string name= "Simulator %1%");
+
+  IMP_LIST(public, Particle, particle, Particle*, Particles);
+
+  //! Simulate until the given time in fs
+  double simulate(double time_in_fs);
+
+  double get_temperature() const {
+    return temperature_;
+  }
+  double get_maximum_time_step() const {
+    return max_time_step_;
+  }
+  void set_temperature(double d) {
+    temperature_=d;
+  }
+  void set_maximum_time_step(double ts) {
+    max_time_step_=ts;
+  }
+  double get_current_time() const {
+    return current_time_;
+  }
+  void set_current_time(double ct) {
+    current_time_=ct;
+  }
+  ParticlesTemp get_simulation_particles() const;
+  IMP_OPTIMIZER(Simulator);
+protected:
+  virtual void setup(const ParticlesTemp &) {};
+
+  // return the actual time step taken
+  virtual double do_step(const ParticlesTemp &sc, double dt)=0;
+
+  virtual bool get_is_simulation_particle(Particle*p) const=0;
+ private:
+  double temperature_;
+  double max_time_step_;
+  double current_time_;
+};
+
+
+IMP_OBJECTS(Simulator, Simulators);
+
+IMPATOM_END_NAMESPACE
+
+#endif  /* IMPATOM_SIMULATOR_H */
---
+++ modules/atom/src/Simulator.cpp
@@ -0,0 +1,61 @@
+/**
+ *  \file Simulator.cpp  \brief Simple Brownian dynamics optimizer.
+ *
+ *  Copyright 2007-2011 IMP Inventors. All rights reserved.
+ *
+ */
+
+#include <IMP/atom/Simulator.h>
+#include <IMP/internal/constants.h>
+
+IMPATOM_BEGIN_NAMESPACE
+Simulator::Simulator(Model *m,
+                     std::string name): Optimizer(m, name) {
+  temperature_=strip_units(IMP::internal::DEFAULT_TEMPERATURE);
+  max_time_step_=2;
+  current_time_=0;
+}
+
+
+double Simulator::simulate(double time) {
+  set_was_used(true);
+  ParticlesTemp ps= get_simulation_particles();
+
+  setup(ps);
+  double target= current_time_+time;
+  while (current_time_ < target) {
+    current_time_+= do_step(ps, max_time_step_);
+    update_states();
+  }
+  return get_model()->evaluate(false);
+}
+
+ParticlesTemp Simulator::get_simulation_particles() const {
+  ParticlesTemp ps;
+  if (get_number_of_particles()==0) {
+    for (Model::ParticleIterator it = get_model()->particles_begin();
+       it != get_model()->particles_end(); ++it) {
+      Particle *p = *it;
+      if (get_is_simulation_particle(p)) {
+        ps.push_back(p);
+      }
+    }
+  } else {
+    ps= ParticlesTemp(particles_begin(), particles_end());
+  }
+  return ps;
+}
+
+double Simulator::do_optimize(unsigned int ns) {
+  return simulate(ns*max_time_step_);
+}
+
+void Simulator::do_show(std::ostream &out) const {
+  out << " time step: " << max_time_step_ << std::endl;
+  out << " temperature: " << temperature_ << std::endl;
+}
+
+
+IMP_LIST_IMPL(Simulator, Particle, particle, Particle*,
+              Particles,,,);
+IMPATOM_END_NAMESPACE
