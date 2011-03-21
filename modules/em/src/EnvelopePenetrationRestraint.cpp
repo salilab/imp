2c3f20287745e75bc44bd7afd0566a15908630d3
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-03-21 01:07:07 +0000

    add an envelope penetration restraint, currently without derivatives
    git-svn-id: https://svn.salilab.org/imp/trunk@8950 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/em/src/EnvelopePenetrationRestraint.cpp
@@ -0,0 +1,89 @@
+/**
+ *  \file EnvelopePenetrationRestraint.cpp
+ *  \brief Score how well a protein is inside its density
+ *
+ *  Copyright 2007-2011 IMP Inventors. All rights reserved.
+ *
+ */
+#include <IMP/em/EnvelopePenetrationRestraint.h>
+#include <IMP/em/envelope_penetration.h>
+#include <IMP/log.h>
+
+IMPEM_BEGIN_NAMESPACE
+
+EnvelopePenetrationRestraint::EnvelopePenetrationRestraint(
+   Particles ps,
+   DensityMap *em_map,Float threshold
+   ): Restraint("Envelope penetration restraint")
+{
+  IMP_LOG(TERSE,"Load envelope penetration with the following input:"<<
+          "number of particles:"<<ps.size()<<
+           "\n");
+  threshold_=threshold;
+  target_dens_map_ = em_map;
+  IMP_IF_CHECK(USAGE) {
+    for (unsigned int i=0; i< ps.size(); ++i) {
+      IMP_USAGE_CHECK(core::XYZR::particle_is_instance(ps[i]),
+                      "Particle " << ps[i]->get_name()
+                      << " is not XYZR"
+                      << std::endl);
+    }
+  }
+  add_particles(ps);
+  ps_=ps;
+  IMP_LOG(TERSE,"after adding particles"<<std::endl);
+  IMP_LOG(TERSE, "Finish initialization" << std::endl);
+}
+
+double
+ EnvelopePenetrationRestraint::unprotected_evaluate(
+                         DerivativeAccumulator *accum) const
+{
+  Float ret_score;
+  ret_score = get_number_of_particles_outside_of_the_density(
+                 target_dens_map_,ps_,
+                 IMP::algebra::get_identity_transformation_3d(),
+                 threshold_);
+  if (accum != NULL) {
+    IMP_WARN(
+     "No derivatives have been assigned to the envelope penetration score\n");
+  }
+  return ret_score;
+}
+
+ParticlesTemp EnvelopePenetrationRestraint::get_input_particles() const
+{
+  ParticlesTemp pt;
+  for (ParticleConstIterator it= particles_begin();
+       it != particles_end(); ++it) {
+      pt.push_back(*it);
+  }
+  return pt;
+}
+
+ContainersTemp EnvelopePenetrationRestraint::get_input_containers() const {
+  ContainersTemp pt;
+  for (ParticleConstIterator it= particles_begin();
+       it != particles_end(); ++it) {
+  }
+  return pt;
+}
+
+void EnvelopePenetrationRestraint::do_show(std::ostream& out) const
+{
+  out<<"EnvelopePenetrationRestraint"<<std::endl;
+}
+
+IMP_LIST_IMPL(
+  EnvelopePenetrationRestraint, Particle, particle,Particle*, Particles,
+              {
+              IMP_INTERNAL_CHECK(get_number_of_particles()==0
+                         || obj->get_model()
+                         == (*particles_begin())->get_model(),
+         "All particles in EnvelopePenetrationRestraint must belong to the "
+                         "same Model.");
+              },{},{});
+
+
+
+IMPEM_END_NAMESPACE
