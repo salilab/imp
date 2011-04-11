32940206014929de3e6ce3ea7546c483a627fde3
yannick@SALILAB.ORG <yannick@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-04-11 21:50:18 +0000

    added an ambigous NOE restraint following specs of Nilges, JMB 1995
    git-svn-id: https://svn.salilab.org/imp/trunk@11449 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/isd/include/AmbiguousNOERestraint.h
@@ -0,0 +1,56 @@
+/**
+ *  \file isd/AmbiguousNOERestraint.h
+ *  \brief A lognormal restraint that uses the ISPA model to model NOE-derived
+ *  distance fit.
+ *
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+ *
+ */
+
+#ifndef IMPISD_AMBIGUOUS_NOE_RESTRAINT_H
+#define IMPISD_AMBIGUOUS_NOE_RESTRAINT_H
+
+#include "isd_config.h"
+#include <IMP/SingletonScore.h>
+#include <IMP/core/XYZ.h>
+#include <IMP/isd/ISDRestraint.h>
+#include <IMP/PairContainer.h>
+#include <IMP/isd/Scale.h>
+
+IMPISD_BEGIN_NAMESPACE
+
+//! Apply an ambiguous NOE distance restraint between a number of pairs of particles.
+/**
+    The source code is as follows:
+    \include AmbiguousNOERestraint.h
+    \include AmbiguousNOERestraint.cpp
+*/
+class IMPISDEXPORT AmbiguousNOERestraint : public ISDRestraint
+{
+  Pointer<PairContainer> pc_;
+  Pointer<Particle> sigma_;
+  Pointer<Particle> gamma_;
+  double Vexp_;
+public:
+  //! Create the restraint.
+  /** Restraints should store the particles they are to act on,
+      preferably in a Singleton or PairContainer as appropriate.
+   */
+  AmbiguousNOERestraint(PairContainer *pc, Particle *sigma, Particle *gamma, double Iexp);
+
+  /* call for probability */
+  double get_probability() const
+  {
+    return exp(-unprotected_evaluate(NULL));
+  }
+
+
+  /** This macro declares the basic needed methods: evaluate and show
+   */
+  IMP_RESTRAINT(AmbiguousNOERestraint);
+
+};
+
+IMPISD_END_NAMESPACE
+
+#endif  /* IMPISD_AMBIGUOUS_NOE_RESTRAINT_H */
