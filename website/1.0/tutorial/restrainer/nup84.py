748dc828be1e9e6f0deb8919abfc5bfa889b5aa5
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-04-14 00:30:24 +0000

    Make the 1.0 tutorial available on the web site.
    git-svn-id: https://svn.salilab.org/imp/trunk@9120 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ website/1.0/tutorial/restrainer/nup84.py
@@ -0,0 +1,23 @@
+import IMP
+import IMP.restrainer
+import IMP.core
+
+IMP.set_log_level(IMP.VERBOSE)
+
+rep = IMP.restrainer.XMLRepresentation('representation.xml').run()
+rsr = IMP.restrainer.XMLRestraint('restraint.xml').run()
+disp = IMP.restrainer.XMLDisplay('display.xml').run()
+
+model = rep.to_model()
+
+log = disp.create_log(rep, 'bead_log_%03d.py')
+log.write('initial.py')
+
+rsr.add_to_representation(rep)
+
+opt = IMP.core.ConjugateGradients()
+opt.set_model(model)
+opt.add_optimizer_state(log)
+
+opt.optimize(10)
+log.write('optimized.py')
