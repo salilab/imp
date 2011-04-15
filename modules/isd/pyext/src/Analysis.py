9e2464f26baa837831b4bc77b9eeb7daacbf4d8b
yannick@SALILAB.ORG <yannick@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-04-15 21:54:07 +0000

    improved analysis functions
    git-svn-id: https://svn.salilab.org/imp/trunk@11459 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/isd/pyext/src/Analysis.py
+++ modules/isd/pyext/src/Analysis.py
@@ -80,20 +80,53 @@ class Analysis:
         """
         data=[array(h.get_data(*dat), dtype=float) \
                 for dat in datums]
-        matplotlib.pyplot.hist(*data, **kwargs) 
+        matplotlib.pyplot.hist(*data, **kwargs)
         matplotlib.pyplot.grid(True)
         matplotlib.pyplot.legend()
         matplotlib.pyplot.show()
 
+    def dump(self, h, dest, *args):
+        """"dump float data from history h to file dest.
+        args can be either strings corresponding to categories, or tuples
+        corresponding to entries of a certain category. Only one counter will be
+        output for the whole dump, it corresponds to the counter of the first
+        entry's category. You can always specify additional counters if needed.
+        """
+        #parse args
+        cats=[]
+        names=[]
+        for arg in args:
+            if type(arg) == str:
+                #get rid of counter
+                ent=h.get_entries(arg)[1:]
+                names.extend(ent)
+                cats.extend([arg]*len(ent))
+            else:
+                #argument should be (cat, entry)
+                names.append(arg[1])
+                cats.append(arg[0])
+        #write data
+        steps=h.get_data(cats[0],'step')
+        fl=open(dest,'w')
+        fl.write("# %s:step\t" % cats[0])
+        fl.write('\t'.join(['%s:%s' % (i,j) for (i,j) in zip(cats,names)]))
+        fl.write('\n')
+        data=[h.get_data(i,j) for (i,j) in zip(cats,names)]
+        for i,st in enumerate(steps):
+            fl.write("%10d\t" % st)
+            for j in data:
+                fl.write('%10f\t' % j[i])
+            fl.write('\n')
+        fl.close()
 
 
-                
+
+
+
 if __name__ == '__main__':
 
     import sys
     a=Analysis()
     h=a.read_stats(sys.argv[1])
     h.toc()
-    
-        
 
