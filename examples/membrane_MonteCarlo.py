c7e08a6e480ef9d02a2396a58586808ccf7149a9
Massimiliano Bonomi <mbonomi@salilab.org>
2011-04-27 21:47:23 +0000

    naming scheme

--- examples/membrane_MonteCarlo.py
+++ examples/membrane_MonteCarlo.py
@@ -4,14 +4,14 @@ import IMP.algebra
 import IMP.atom
 import IMP.membrane
 
-def setup_MonteCarlo(m,chain,TMH,temp):
+def setup_MonteCarlo(m,protein,TMH,temp):
     mc=IMP.core.MonteCarlo(m)
     mc.set_return_best(False)
     mc.set_kt(temp)
 # create Movers and initialize the system
     rot=IMP.algebra.get_identity_rotation_3d()
     for i,h in enumerate(TMH):
-        s0=IMP.atom.Selection(chain, atom_type = IMP.atom.AT_CA, residue_index = h[0])
+        s0=IMP.atom.Selection(protein, atom_type = IMP.atom.AT_CA, residue_index = h[0])
         rb=IMP.core.RigidMember(s0.get_selected_particles()[0]).get_rigid_body()
         if ( i == 0 ): mv= IMP.membrane.RigidBodyNewMover(rb, 0.0, 0.0, 0.5, 0.05, 0.05, 0.05)
         if ( i == 1 ): mv= IMP.membrane.RigidBodyNewMover(rb, 0.5, 0.0, 0.5, 0.05, 0.05, 0.05)
--- examples/membrane_main.py
+++ examples/membrane_main.py
@@ -22,31 +22,31 @@ print "reading protein data"
 (seq,names,TMH,topo,TM_inter)=setup_protein()
 
 print "creating representation"
-(chain,tbr,rot0)=create_representation(m,seq,names,TMH,topo)
+(protein,tbr,rot0)=create_representation(m,seq,names,TMH,topo)
 
 print "creating restraints"
-rset=create_restraints(m,chain,tbr,TMH,rot0,topo,TM_inter)
+rset=create_restraints(m,protein,tbr,TMH,rot0,topo,TM_inter)
 
 print "visualizing IG and JT"
-(ig,jt)=get_graphs(m,chain,TMH,rset)
+(ig,jt)=get_graphs(m,protein,TMH,rset)
 print ig,jt
 
 print "creating sampler"
-mc=setup_MonteCarlo(m,chain,TMH,mc_kt)
+mc=setup_MonteCarlo(m,protein,TMH,mc_kt)
 
 # preparing hdf5 file
 rh = IMP.rmf.RootHandle(mc_traj_file, True)
 #rh = IMP.rmf.RootHandle(mc_traj_file, False)
-for hs in chain.get_children():
+for hs in protein.get_children():
     IMP.rmf.add_hierarchy(rh, hs)
-#IMP.rmf.set_hierarchies(rh, chain.get_children())
+#IMP.rmf.set_hierarchies(rh, protein.get_children())
 
 print "sampling"
 for steps in range(mc_loops):
     mc.optimize(mc_steps)
     #print steps. m.evaluate(False)
-    #display(m,chain,TMH,"conf_"+str(steps)+".score_"+str(score)+".pym")
-    for hs in chain.get_children():
+    #display(m,protein,TMH,"conf_"+str(steps)+".score_"+str(score)+".pym")
+    for hs in protein.get_children():
         IMP.rmf.save_frame(rh, steps+1, hs)
     #    IMP.rmf.load_frame(rh, steps+1, hs)
 
