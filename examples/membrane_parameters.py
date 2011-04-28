be1de63b18ca99b06b98ed8aece79bda3106c531
Massimiliano Bonomi <mbonomi@salilab.org>
2011-04-28 21:22:12 +0000

    1h68 example

--- examples/membrane_parameters.py
+++ examples/membrane_parameters.py
@@ -28,7 +28,7 @@ mc_loops=1000
 mc_steps=10
 
 # trajectory file
-mc_traj_file="traj_0.rmf"
+mc_traj_file="traj.rmf"
 
 # protein data
 
@@ -36,16 +36,15 @@ mc_traj_file="traj_0.rmf"
 seq0=("M","V","G","L","T","T","L","F","W","L","G","A","I","G","M","L","V","G","T","L","A","F","A","W","A","G","R","D","A","G","S","G","E","R","R","Y","Y","V","T","L","V","G","I","S","G","I","A","A","V","A","Y","V","V","M","A","L","G","V","G","W","V","P","V","A","E","R","T","V","F","A","P","R","Y","I","D","W","I","L","T","T","P","L","I","V","Y","F","L","G","L","L","A","G","L","D","S","R","E","F","G","I","V","I","T","L","N","T","V","V","M","L","A","G","F","A","G","A","M","V","P","G","I","E","R","Y","A","L","F","G","M","G","A","V","A","F","L","G","L","V","Y","Y","L","V","G","P","M","T","E","S","A","S","Q","R","S","S","G","I","K","S","L","Y","V","R","L","R","N","L","T","V","I","L","W","A","I","Y","P","F","I","W","L","L","G","P","P","G","V","A","L","L","T","P","T","V","D","V","A","L","I","V","Y","L","D","L","V","T","K","V","G","F","G","F","I","A","L","D","A","A","A","T","L")
 
 # TM regions
-#TM_res= [[121,141],[163,180],[190,211]]
-TM_res= [[141,161],[163,180],[190,211]]
+TM_res=[[3,25],[37,56],[70,87],[99,117],[121,141],[163,180],[190,211]]
 # define the topology
-TM_topo=[-1.0,1.0,-1.0]
+TM_topo=[-1.0,1.0,-1.0,1.0,-1.0,1.0,-1.0]
 # name of the TMH
-TM_names=["TM4","TM5","TM6"]
+TM_names=["TM0","TM1","TM2","TM3","TM4","TM5","TM6"]
 # interacting pairs
-TM_inter=[["TM5","TM6"]]
+TM_inter=[["TM0","TM1"],["TM0","TM6"],["TM1","TM6"],["TM5","TM6"]]
 # adjacent pairs
-TM_loop=[["TM4","TM5"],["TM5","TM6"]]
+TM_loop=[["TM0","TM1"],["TM1","TM2"],["TM2","TM3"],["TM3","TM4"],["TM4","TM5"],["TM5","TM6"]]
 
 # storing sequences of TMH
 TM_seq=[]
