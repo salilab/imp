25fc8d54db7eda2993a94e9bf31d3048bd2d664b
Massimiliano Bonomi <mbonomi@salilab.org>
2011-04-29 00:26:49 +0000

    new parameters for MC

--- examples/1h68/membrane_MonteCarlo.py
+++ examples/1h68/membrane_MonteCarlo.py
@@ -30,8 +30,8 @@ def setup_MonteCarlo_2(m,protein):
     for i,h in enumerate(TM_res):
         s0=IMP.atom.Selection(protein, atom_type = IMP.atom.AT_CA, residue_index = h[0])
         rb=IMP.core.RigidMember(s0.get_selected_particles()[0]).get_rigid_body()
-        if ( i == 0 ): mv= IMP.membrane.RigidBodyNewMover(rb, 0.0, 0.0, 1.0, 0.2, 0.2, 0.2)
-        if ( i == 1 ): mv= IMP.membrane.RigidBodyNewMover(rb, 1.0, 0.0, 1.0, 0.2, 0.2, 0.2)
-        if ( i > 1 ):  mv= IMP.membrane.RigidBodyNewMover(rb, 1.0, 1.0, 1.0, 0.2, 0.2, 0.2)
+        if ( i == 0 ): mv= IMP.membrane.RigidBodyNewMover(rb, 0.0, 0.0, 0.5, 0.2, 0.2, 0.2)
+        if ( i == 1 ): mv= IMP.membrane.RigidBodyNewMover(rb, 0.5, 0.0, 0.5, 0.2, 0.2, 0.2)
+        if ( i > 1 ):  mv= IMP.membrane.RigidBodyNewMover(rb, 0.5, 0.5, 0.5, 0.2, 0.2, 0.2)
         mc.add_mover(mv)
     return mc
