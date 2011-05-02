33323ec3bae0709e04f7019a067d61865a7d7362
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-05-02 23:08:49 +0000

    add comments
    git-svn-id: https://svn.salilab.org/imp/trunk@9274 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/rmf/examples/geometry.py
+++ modules/rmf/examples/geometry.py
@@ -1,11 +1,23 @@
 import IMP.display
 import IMP.rmf
+
+# create a temporary file
 tfn= IMP.create_temporary_file_name("hdf5_geometry", "hdf5")
+
+# open the hdf5, clearing any existing contents
 f= IMP.rmf.RootHandle(tfn, True)
+
+# creating a box geometry
 bb= IMP.algebra.BoundingBox3D(IMP.algebra.Vector3D(0,0,0),
                               IMP.algebra.Vector3D(10, 10, 10))
 g= IMP.display.BoundingBoxGeometry(bb)
+
+# add the geometry to the file
 IMP.rmf.add_geometry(f, g)
+
+# recreate the geometries from the file. The geometry will be the same
+# but it will not be a IMP.display.BoundingBoxGeometry, it will be
+# a set of cylinders instead.
 gs= IMP.rmf.create_geometries(f, 0)
 print gs[0].get_name()
 print "Try running hdf5_display  on", tfn
