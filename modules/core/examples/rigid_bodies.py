ca4fa2a0873c1805d9103445e43bad51ea2fa4ea
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-05-02 23:14:24 +0000

    add more comments
    git-svn-id: https://svn.salilab.org/imp/trunk@9276 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/examples/rigid_bodies.py
+++ modules/core/examples/rigid_bodies.py
@@ -4,15 +4,12 @@ import IMP.atom
 import IMP.container
 
 m= IMP.Model()
-
-mp0= IMP.atom.read_pdb(IMP.core.get_example_path('example_protein.pdb'), m)
-residues= IMP.atom.get_by_type(mp0, IMP.atom.RESIDUE_TYPE)
-rbs=IMP.container.ListSingletonContainer(residues)
-for r in residues:
-    IMP.core.RigidBody.setup_particle(r.get_particle(), IMP.core.XYZs(IMP.atom.get_leaves(r)))
-
+# create a new IMP.atom.Hierarchy for the pdb file
 mp1= IMP.atom.read_pdb(IMP.core.get_example_path('example_protein.pdb'), m)
 chains= IMP.atom.get_by_type(mp1, IMP.atom.CHAIN_TYPE)
 rd= IMP.atom.Hierarchy(chains[0])
+# Create a rigid body from the first chain
+# note that rbs != chains[0] as the bounding volume for rbs needs to include all of the
+# chain, but chains[0] might have a smaller sphere associated with it.
 rbs=IMP.atom.create_rigid_body(chains[0])
 print "all done"
--- modules/core/examples/symmetry.py
+++ modules/core/examples/symmetry.py
@@ -4,19 +4,25 @@ import IMP.container
 m= IMP.Model()
 m.set_log_level(IMP.SILENT)
 ps =[]
+# create 4 xyz particles
 for i in range(0,4):
     p = IMP.Particle(m)
     ps.append(p)
     d= IMP.core.XYZ.setup_particle(p, IMP.algebra.Vector3D(.5,0,0))
     IMP.core.XYZR.setup_particle(p, 1)
+
 # set the 0 particle as the reference particle for the others as
 # they will get their positions from it
 for i,p in enumerate(ps[1:]):
+    # the other 3 particles are all symmetric copies of the first
     IMP.core.Reference.setup_particle(p, ps[0])
+    # the symmetry operation is rotation around the z axis
     tr= IMP.algebra.Transformation3D(IMP.algebra.get_rotation_about_axis(IMP.algebra.get_basis_vector_3d(2),
                                                                          3.14/2.0*(i+1)),
                                      IMP.algebra.Vector3D(0,0,0))
     sm= IMP.core.TransformationSymmetry(tr)
+    # set up a constraint for the one particle, if you have more than one with the same symmetry
+    # transform, you should use an IMP.container.SingletonsConstraint.
     c= IMP.core.SingletonConstraint(sm, None, p)
     m.add_score_state(c)
 r= IMP.core.ExcludedVolumeRestraint(IMP.container.ListSingletonContainer(ps),
