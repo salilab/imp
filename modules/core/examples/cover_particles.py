95fab0bed8af42347f2c918212ebfc8cac311f6f
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-05-02 23:14:24 +0000

    add more comments
    git-svn-id: https://svn.salilab.org/imp/trunk@9276 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/examples/cover_particles.py
+++ modules/core/examples/cover_particles.py
@@ -6,10 +6,12 @@ import IMP.atom
 m= IMP.Model()
 prot= IMP.atom.read_pdb(IMP.core.get_example_path('example_protein.pdb'), m)
 res= IMP.atom.get_by_type(prot, IMP.atom.RESIDUE_TYPE)
-for p in res:
-    IMP.core.XYZR.setup_particle(p.get_particle())
-mtr=IMP.atom.Hierarchy.get_traits()
-pr= IMP.core.ChildrenRefiner(mtr)
+pr= IMP.core.ChildrenRefiner(IMP.atom.Hierarchy.get_traits())
 for r in res:
+    # add coordinates and a radius to the residue particle
+    IMP.core.XYZR.setup_particle(r)
+    # make sure that the coordinates and the radius define a sphere
+    # which contains all of the leaves (atoms) of the residue
     IMP.core.Cover.setup_particle(r.get_particle(), pr)
-m.evaluate(False)
+# update the coordinates of the residue particles so that they cover the atoms
+m.update()
