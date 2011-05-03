e0f59af78f18ab72afbf1409f73bf80b5e63cab8
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-05-03 23:38:06 +0000

    very limited test
    git-svn-id: https://svn.salilab.org/imp/trunk@9291 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/display/test/test_mesh.py
@@ -0,0 +1,32 @@
+import IMP
+import IMP.test
+import IMP.core
+import IMP.atom
+import IMP.display
+import os
+
+class TestBL(IMP.test.TestCase):
+    def setUp(self):
+        IMP.test.TestCase.setUp(self)
+        IMP.set_log_level(IMP.TERSE)
+
+
+    def test_3(self):
+        """Testing skin surface"""
+        m= IMP.Model()
+        print "reading"
+        h= IMP.atom.read_pdb(self.get_input_file_name("1d3d-protein.pdb"), m)
+        hs = IMP.atom.create_simplified_along_backbone(IMP.atom.Chain(IMP.atom.get_by_type(h, IMP.atom.CHAIN_TYPE)[0]), 1)
+        ss=[]
+        print "stripping", len(IMP.atom.get_leaves(hs))
+        for l in IMP.atom.get_leaves(hs):
+            ss.append(IMP.core.XYZR(l).get_sphere())
+        print "skinning"
+        g= IMP.display.SkinSurfaceGeometry(ss)
+        g.set_name("surface")
+        print "writing"
+        w= IMP.display.PymolWriter(self.get_tmp_file_name("skin.pym"))
+        w.add_geometry(g)
+
+if __name__ == '__main__':
+    IMP.test.main()
