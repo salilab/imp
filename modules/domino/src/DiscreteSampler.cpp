6c586c035155e9dfbb93df6a89ceb9e3f1beadfd
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-05-05 17:05:24 +0000

    add sampling of few attributes
    git-svn-id: https://svn.salilab.org/imp/trunk@9306 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/domino/src/DiscreteSampler.cpp
+++ modules/domino/src/DiscreteSampler.cpp
@@ -65,17 +65,51 @@ Assignments DiscreteSampler
   set_was_used(true);
   return do_get_sample_assignments(known_particles);
 }
-
+IntsLists DiscreteSampler::do_sample_attributes(
+             const std::vector<std::pair<IntKey,Particles> >&list) const {
+  Subset known_particles= get_particle_states_table()->get_subset();
+  Assignments final_solutions= do_get_sample_assignments(known_particles);
+  IMP_LOG(TERSE,"found "<<final_solutions.size()<<" solutions"<<std::endl);
+  IntsLists ret(list.size());
+  for (unsigned int i=0; i< final_solutions.size(); ++i) {
+    IMP_IF_LOG(TERSE){
+      if (i%50000==0) {IMP_LOG(TERSE,i<<std::endl);}}
+    IMP_INTERNAL_CHECK(final_solutions[i].size()
+                       == known_particles.size(),
+                       "Number of particles doesn't match:"<<
+                       final_solutions[i].size()<<" != "<<
+                       known_particles.size());
+    for (unsigned int j=0; j< known_particles.size(); ++j) {
+      Particle *p=known_particles[j];
+      Pointer<ParticleStates> ps
+        =get_particle_states_table()->get_particle_states(p);
+      ps->load_particle_state(final_solutions[i][j], p);
+    }
+    for(int k=0;k<list.size();k++){
+      Ints pair_vals(list[k].second.size());
+      for(int l=0;l<list[k].second.size();l++) {
+        pair_vals[l]=list[k].second[l]->get_value(list[k].first);
+      }
+      ret[k].push_back(pair_vals);
+    }
+  }
+  return ret;
+}
 ConfigurationSet *DiscreteSampler::do_sample() const {
   Pointer<ConfigurationSet> ret= new ConfigurationSet(get_model());
   ret->set_log_level(SILENT);
   Subset known_particles= get_particle_states_table()->get_subset();
   Assignments final_solutions= do_get_sample_assignments(known_particles);
+  //TODO - remove
+  std::cout<<"Number of solutions:"<<final_solutions.size()<<std::endl;
+  std::cout<<"Number of particles:"<<known_particles.size()<<std::endl;
   for (unsigned int i=0; i< final_solutions.size(); ++i) {
-    //IMP_LOG(TERSE, "Solution is " << final_solutions[i] << std::endl);
+    if (i%10000==0) {std::cout<<i<<std::endl;}
     IMP_INTERNAL_CHECK(final_solutions[i].size()
                        == known_particles.size(),
-                       "Number of particles doesn't match");
+                       "Number of particles doesn't match:"<<
+                       final_solutions[i].size()<<" != "<<
+                       known_particles.size());
     ret->load_configuration(-1);
     for (unsigned int j=0; j< known_particles.size(); ++j) {
       Particle *p=known_particles[j];
@@ -92,6 +126,21 @@ ConfigurationSet *DiscreteSampler::do_sample() const {
       //}
   }
   return ret.release();
+}
+
+IntsLists DiscreteSampler::get_sample_attributes(
+const std::vector<std::pair<IntKey,Particles> >&list) const {
+  IMP_OBJECT_LOG;
+  unsigned int num_opt=0;
+  set_was_used(true);
+  Model *mdl=get_model();
+  for (Model::ParticleConstIterator pit= mdl->particles_begin();
+       pit != mdl->particles_end(); ++pit) {
+    unsigned int d=std::distance((*pit)->optimized_keys_begin(),
+                  (*pit)->optimized_keys_end());
+    if (d > 0) ++num_opt;
+  }
+  return do_sample_attributes(list);
 }
 
 
