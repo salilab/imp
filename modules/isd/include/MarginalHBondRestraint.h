7dacfe39cd0e866de28ca00b7ddb83de6819b084
yannick@SALILAB.ORG <yannick@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-05-06 17:43:18 +0000

    const
    git-svn-id: https://svn.salilab.org/imp/trunk@11479 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/isd/include/MarginalHBondRestraint.h
+++ modules/isd/include/MarginalHBondRestraint.h
@@ -55,7 +55,7 @@ public:
   //return the sum inside the parentheses
   double get_logsquares() const {return logsquares_;}
 
-  double get_number_of_contributions() {return volumes_.size();}
+  double get_number_of_contributions() const {return volumes_.size();}
 
   /* call for probability */
   double get_probability() const
