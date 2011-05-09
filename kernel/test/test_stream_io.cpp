b224d0e1f7a4b925eb2b098453cfa19f78fea8dd
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-05-09 22:43:08 +0000

    add test
    git-svn-id: https://svn.salilab.org/imp/trunk@9341 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ kernel/test/test_stream_io.cpp
@@ -0,0 +1,16 @@
+/**
+ *   Copyright 2007-2011 IMP Inventors. All rights reserved
+ */
+#include <IMP/base_types.h>
+namespace IMP {
+  void testf() {
+    Ints its;
+    std::cout << its << std::endl;
+  }
+}
+int main(int, char *[]) {
+  using namespace IMP;
+  Ints its;
+  std::cout << its << std::endl;
+  return 0;
+}
