87ca94e6345ade55ef9f380f3638c4be69867399
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-05-11 16:06:15 +0000

    disable when cgal is not found
    git-svn-id: https://svn.salilab.org/imp/trunk@9365 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/display/test/test_mesh.py
+++ modules/display/test/test_mesh.py
@@ -15,6 +15,10 @@ class TestBL(IMP.test.TestCase):
         """Testing skin surface"""
         m= IMP.Model()
         print "reading"
+        try:
+            import IMP.cgal
+        except:
+            self.skipTest("IMP.cgal is disabled")
         h= IMP.atom.read_pdb(self.get_input_file_name("1d3d-protein.pdb"), m)
         hs = IMP.atom.create_simplified_along_backbone(IMP.atom.Chain(IMP.atom.get_by_type(h, IMP.atom.CHAIN_TYPE)[0]), 1)
         ss=[]
