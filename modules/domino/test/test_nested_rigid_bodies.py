569536368ff85a90510e903db48af3817f28dfe3
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-05-11 17:28:18 +0000

    clean up tests
    git-svn-id: https://svn.salilab.org/imp/trunk@9373 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/domino/test/test_nested_rigid_bodies.py
+++ modules/domino/test/test_nested_rigid_bodies.py
@@ -13,13 +13,13 @@ class PointAlignmentTests(IMP.test.TestCase):
         #load components
         mdl=IMP.Model()
         mhs=[]
+        rbs=[]
         for i in range(4):
             fn ="small_protein.pdb"
             mhs.append(IMP.atom.read_pdb(self.get_input_file_name(fn),
                                          mdl,IMP.atom.CAlphaPDBSelector()))
             mhs[-1].set_name("molecule"+str(i))
-            IMP.atom.setup_as_rigid_body(mhs[-1])
-        rbs=IMP.core.RigidBodies(mhs)
+            rbs.append(IMP.atom.create_rigid_body(mhs[-1]))
         ts=[]
         bb= IMP.algebra.BoundingBox3D(
             IMP.algebra.Vector3D(-10.,-10.,-10.),
@@ -32,6 +32,9 @@ class PointAlignmentTests(IMP.test.TestCase):
         for i in range(3):
             rbs[0].add_member(rbs[i+1])
         #set ev
+        IMP.set_log_level(IMP.VERBOSE)
+        for h in mhs:
+            IMP.atom.show_molecular_hierarchy(h)
         sev=IMP.atom.create_excluded_volume_restraint(mhs, 1)
         mdl.add_restraint(sev)
         mdl.evaluate(False)
@@ -39,10 +42,10 @@ class PointAlignmentTests(IMP.test.TestCase):
         pst=IMP.domino.ParticleStatesTable()
         for i in range(1):
             states=IMP.domino.NestedRigidBodyStates(ts)
-            pst.set_particle_states(mhs[i+1],states)
+            pst.set_particle_states(rbs[i+1],states)
         id_trans=[]
-        id_trans.append(IMP.core.RigidBody(mhs[0]).get_reference_frame())
-        pst.set_particle_states(mhs[0],IMP.domino.RigidBodyStates(id_trans))
+        id_trans.append(rbs[0].get_reference_frame())
+        pst.set_particle_states(rbs[0],IMP.domino.RigidBodyStates(id_trans))
         ds=IMP.domino.DominoSampler(mdl,pst)
         cg=ds.get_sample()
         print cg.get_number_of_configurations()
