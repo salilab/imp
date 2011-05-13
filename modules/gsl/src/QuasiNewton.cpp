52fd8835796f359616d92c9272e94fed211ff52f
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-05-13 19:52:19 +0000

    Work around extremely old Windows GSL build.
    git-svn-id: https://svn.salilab.org/imp/trunk@9383 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/gsl/src/QuasiNewton.cpp
+++ modules/gsl/src/QuasiNewton.cpp
@@ -21,8 +21,14 @@ void QuasiNewton::do_show(std::ostream &) const {
 }
 
 Float QuasiNewton::do_optimize(unsigned int nsteps) {
+  /* The GnuWin32 Windows build of GSL is only 1.8, which doesn't have bfgs2;
+     use the older less efficient bfgs optimizer instead. */
   const gsl_multimin_fdfminimizer_type *t
+#if defined _MSC_VER
+    =gsl_multimin_fdfminimizer_vector_bfgs;
+#else
     =gsl_multimin_fdfminimizer_vector_bfgs2;
+#endif
   return GSLOptimizer::optimize(nsteps, t, initial_step_,
                                 line_step_, min_gradient_);
 }
