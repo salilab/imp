90cd20666869e7b1187489fac7884d1b9196b947
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-05-16 23:24:28 +0000

    Add an example simple configuration for a Windows system.
    git-svn-id: https://svn.salilab.org/imp/trunk@9400 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ doc/example_windows_config.py
@@ -0,0 +1,4 @@
+cxxflags="/DWIN32 /DGSL_DLL"    # Needed to link with GSL DLL
+boost_autolink="dynamic"        # Needed to link with Boost DLLs
+hdf5="yes"
+hdf5libs="libhdf5_dll.lib"      # Needed to link with HDF5 DLL
