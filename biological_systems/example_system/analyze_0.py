fd38996ce67e3db8adbf31cb5a20fc7620e1fb42
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-05-16 17:29:52 +0000

    overwrite correspondences
    git-svn-id: https://svn.salilab.org/imp/trunk@9395 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- biological_systems/example_system/analyze_0.py
+++ biological_systems/example_system/analyze_0.py
@@ -37,7 +37,7 @@ cs= IMP.ConfigurationSet(m)
 for f in glob.glob(IMP.system.get_input_path("configurations_*.rmf")):
     print f
     fh= IMP.rmf.RootHandle(f, False)
-    IMP.rmf.set_hierarchies(fh, [all])
+    IMP.rmf.set_hierarchies(fh, [all], True)
     for i in range(0, IMP.rmf.get_number_of_frames(fh, all)):
         IMP.rmf.load_frame(fh, i, all)
         cs.save_configuration()
