bce69f5475bb58aa963f394a1dfe9a216f4971b6
Massimiliano Bonomi <mbonomi@salilab.org>
2011-05-21 19:19:04 +0000

    output...

--- bin/domino_enumerate.cpp
+++ bin/domino_enumerate.cpp
@@ -9,7 +9,6 @@
 #include <IMP/atom.h>
 #include <IMP/domino.h>
 #include <IMP/membrane.h>
-#include <IMP/rmf.h>
 
 using namespace IMP;
 using namespace IMP::membrane;
@@ -18,6 +17,7 @@ int main(int  , char **)
 {
 
 // parsing input
+std::cout << "Parsing input file" << std::endl;
 Parameters mydata=get_parameters("config.ini");
 
 // create a new model
@@ -27,36 +27,29 @@ IMP_NEW(Particle,ph,(m));
 atom::Hierarchy all=atom::Hierarchy::setup_particle(ph);
 
 // create representation
+std::cout << "Creating representation" << std::endl;
 core::TableRefiner* tbr=generate_TM(m,&all,&(mydata.TM));
 
 // create restraints
+std::cout << "Creating restraints" << std::endl;
 RestraintSet* rset=create_restraints(m,all,tbr,&mydata);
 
 // create discrete states
+std::cout << "Creating sampler" << std::endl;
 domino::ParticleStatesTable* pst=create_states(all,&mydata);
 
 // create sampler
 domino::DominoSampler* s=create_sampler(m,rset,pst);
 
 // sampling
-domino::Subset ass=domino::Subset(pst->get_particles());
-domino::Assignments cs=s->get_sample_assignments(ass);
-std::cout << "Found " << cs.size() << " solutions" << std::endl;
+std::cout << "Sampling" << std::endl;
+domino::Subset subs=domino::Subset(pst->get_particles());
+domino::Assignments ass=s->get_sample_assignments(subs);
+std::cout << "Found " << ass.size() << " solutions" << std::endl;
 
-// writing things to file
-rmf::HDF5Group rt= rmf::HDF5Group(mydata.ass_file, true);
-//rmf::HDF5Group rt= rmf::HDF5Group(mydata.ass_file, false);
-
-rmf::HDF5DataSet<rmf::IndexTraits> data_set=
-rt.add_child_index_data_set("node_assignments", 2);
-//rmf::HDF5DataSet<rmf::IndexTraits> data_set=
-//rt.get_child_index_data_set("node_assignments", 2);
-
-domino::set_assignments(data_set, cs, ass, pst->get_particles());
-//domino::Assignments cs=
-//domino::get_assignments(data_set, ass, pst->get_particles());
-
-//std::cout << "Found " << cs.size() << " solutions" << std::endl;
+// write output
+std::cout << "Writing output" << std::endl;
+write_output(&all,pst,&subs,&ass,&mydata);
 
 return 0;
 }
