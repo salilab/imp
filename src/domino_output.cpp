cb19183fc0ba8df30ac767f50e155c48c0ede9f1
Massimiliano Bonomi <mbonomi@salilab.org>
2011-05-21 19:18:50 +0000

    output improved

--- src/domino_output.cpp
+++ src/domino_output.cpp
@@ -14,11 +14,11 @@ using namespace IMP::membrane;
 
 IMPMEMBRANE_BEGIN_NAMESPACE
 
-void write_output
-(domino::ParticleStatesTable* pst, domino::Subset* subs,
- domino::Assignments* ass, Parameters *myparam)
+void write_output(atom::Hierarchy *protein,
+domino::ParticleStatesTable* pst, domino::Subset* subs,
+domino::Assignments* ass, Parameters *myparam)
 {
-// assignments file
+// writing assignments
 if(myparam->ass_file.length()>0.0){
  rmf::HDF5Group rt= rmf::HDF5Group(myparam->ass_file, true);
  rmf::HDF5DataSet<rmf::IndexTraits> data_set=
@@ -26,8 +26,20 @@ rt.add_child_index_data_set("node_assignments", 2);
  domino::set_assignments(data_set, *ass, *subs, pst->get_particles());
 }
 
-// configurations
+// writing configurations
 if(myparam->traj_file.length()>0.0){
+ rmf::RootHandle rh=rmf::RootHandle(myparam->traj_file, True);
+ Hierarchies hs = protein->get_children();
+ for(unsigned int i=0;i<hs.size();++i){
+  rmf::add_hierarchy(rh, hs[i]);
+ }
+ for(unsigned int i=0;i<ass->size();++i){
+  domino::load_particle_states(*subs,*ass[i],pst)
+  for(unsigned int j=0;j<hs.size();++j){
+   rmf::save_frame(rh, i+1, hs[j]);
+  }
+ }
+del rh;
 }
 }
 
