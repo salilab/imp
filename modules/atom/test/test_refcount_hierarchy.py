b42ee058bf2b3eff26439d7efffe99a1dac1743c
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-05-21 04:51:20 +0000

    better ref count testing
    git-svn-id: https://svn.salilab.org/imp/trunk@9461 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/test/test_refcount_hierarchy.py
+++ modules/atom/test/test_refcount_hierarchy.py
@@ -35,7 +35,7 @@ class DecoratorTests(IMP.test.TestCase):
         del hc
         del h
         refcnt.assert_number(0)
-    def test_bonded(self):
+    def _test_bonded(self):
         """Check that pdbs don't have circular ref counts """
         # charm creates all sorts of things
         m= IMP.Model()
@@ -49,7 +49,48 @@ class DecoratorTests(IMP.test.TestCase):
         h= IMP.atom.read_pdb(self.get_input_file_name("mini.pdb"), m)
         del m
         del h
+        print IMP.Object.get_live_object_names()
         refcnt.assert_number(0)
+
+    def test_rbbonded(self):
+        """Check that pdbs with rigid bodies don't have circular ref counts """
+        m= IMP.Model()
+        h= IMP.atom.read_pdb(self.get_input_file_name("mini.pdb"), m)
+        rb= IMP.atom.create_rigid_body([h], "test rb");
+        #print [x.get_name() for x in rb.get_members()]
+        del rb
+        del m
+        del h
+        print "initial live"
+        print IMP.Object.get_live_object_names()
+        while (True):
+            # charm creates all sorts of things
+            refcnt = IMP.test.RefCountChecker(self)
+            m= IMP.Model()
+            p = IMP.Particle(m)
+            p.set_name("TEST")
+            del p
+            h= IMP.atom.read_pdb(self.get_input_file_name("mini.pdb"), m)
+            #IMP.atom.show_molecular_hierarchy(h)
+            rb= IMP.atom.create_rigid_body([h], "test rb");
+            #print [x.get_name() for x in rb.get_members()]
+            del rb
+            #del x
+            del m
+            del h
+            print dir()
+            #print "live"
+            #print [x  for x in IMP.Object.get_live_object_names() if (x.find("CHARMM")==-1)]
+            refcnt.assert_number(0)
+            #except:
+            #    pass
+            #print "wait"
+            #for i in range(0,60000000):
+            #    pass
+            #print "end wait"
+            return
+
+
 
 if __name__ == '__main__':
     IMP.test.main()
