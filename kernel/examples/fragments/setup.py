5f1e1724933fa395fab57540d5ca7a32e4477e60
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-05-31 23:13:38 +0000

    fix bit rot
    git-svn-id: https://svn.salilab.org/imp/trunk@9550 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/examples/fragments/setup.py
+++ kernel/examples/fragments/setup.py
@@ -2,12 +2,11 @@ import IMP.core
 
 def create_model_and_particles():
     m= IMP.Model()
-    sc= IMP.container.ListSingletonContainer()
-    b= IMP.algebra.BoundingBox3D(IMP.algebra.Vector3D(0,0,0),
+    ps= [IMP.Particle(m) for i in range(0,100)]
+    bb= IMP.algebra.BoundingBox3D(IMP.algebra.Vector3D(0,0,0),
                                  IMP.algebra.Vector3D(10,10,10))
-    for i in range(0,100):
-        p= IMP.Particle(m)
-        sc.add_particle(m)
-        d=IMP.core.XYZR.setup_particle(p, IMP.algebra.Sphere3D(IMP.algebra.get_random_vector_in(b), 1))
+    for p in ps:
+        d=IMP.core.XYZR.setup_particle(p, IMP.algebra.Sphere3D(IMP.algebra.get_random_vector_in(bb), 1))
         d.set_coordinates_are_optimized(True)
+    sc= IMP.container.ListSingletonContainer(ps)
     return (m, sc)
