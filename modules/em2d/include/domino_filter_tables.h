1e1e7327b0d24d63ee5fd5129b1ae87285a8d7e5
javi@SALILAB.ORG <javi@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-06-05 19:10:23 +0000

    domino filters and tables
    git-svn-id: https://svn.salilab.org/imp/trunk@9604 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/em2d/include/domino_filter_tables.h
@@ -0,0 +1,42 @@
+/**
+ *  \file ProjectionOverlapFilterTable
+ *  \brief Managing of projection overlap filter
+ *
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+ *
+ */
+
+#ifndef IMPEM2D_DOMINO_FILTER_TABLES_H
+#define IMPEM2D_DOMINO_FILTER_TABLES_H
+
+#include "IMP/em2d/Image.h"
+#include "IMP/em2d/ProjectionStates.h"
+#include "IMP/em2d/ProjectionOverlapFilter.h"
+#include "IMP/domino/Subset.h"
+#include "IMP/domino/subset_filters.h"
+
+
+IMPEM2D_BEGIN_NAMESPACE
+
+class IMPEM2DEXPORT ProjectionOverlapFilterTable:
+                      public domino::SubsetFilterTable {
+protected:
+
+  Pointer<Image> image_;
+  Pointer<ProjectionStates> projection_states_;
+  domino::Subset my_subset_;
+public:
+
+  ProjectionOverlapFilterTable(Image *image,
+                               ProjectionStates *projection_states) :
+                               image_(image),
+                               projection_states_(projection_states) {};
+
+  IMP_SUBSET_FILTER_TABLE(ProjectionOverlapFilterTable);
+
+};
+IMP_OBJECTS(ProjectionOverlapFilterTable,ProjectionOverlapFilterTables);
+
+IMPEM2D_END_NAMESPACE
+
+#endif  /* IMPEM2D_DOMINO_FILTER_TABLES_H */
---
+++ modules/em2d/include/domino_particle_states.h
@@ -0,0 +1,80 @@
+/**
+ *  \file ProjectionStates.h
+ *  \brief Particles states for a rigid body that is going to be projected
+ *
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+ *
+ */
+
+#ifndef IMPEM2D_DOMINO_PARTICLE_STATES_H
+#define IMPEM2D_DOMINO_PARTICLE_STATES_H
+
+#include "IMP/em2d/Image.h"
+#include "IMP/domino/domino_macros.h"
+#include "IMP/domino/particle_states.h"
+#include "IMP/core/rigid_bodies.h"
+#include "IMP/algebra/Vector3D.h"
+#include "IMP/algebra/Rotation3D.h"
+#include "IMP/Pointer.h"
+#include "IMP/base_types.h"
+#include "IMP/Particle.h"
+
+IMPEM2D_BEGIN_NAMESPACE
+
+/*! States that a set of Particles can have according to their positions
+    and orientations in a grid. The orientations also determine the orientation
+    of the projections of the particles.
+  \param[in]
+*/
+class IMPEM2DEXPORT ProjectionStates: public domino::ParticleStates {
+
+protected:
+
+  algebra::Vector3Ds positions_;
+  algebra::Rotation3Ds orientations_;
+  Images projections_;
+  unsigned int number_of_states_;
+
+public:
+
+
+  /*!
+    \param[in] positions Set of positions that the set of particles
+               can have in the grid (points of the grid).
+    \param[in] orientations Orientations of the particles in in the grid
+    \param[in] Projections of the particles in the given orientations.
+               There must be correspondence between the orientations and the
+               projections.
+    Note: The number of states considered is positions * orientations, as
+          each position can have all orientations.
+    \
+  */
+  ProjectionStates(const algebra::Vector3Ds &positions,
+                            const algebra::Rotation3Ds &orientations,
+                            const Images &projections,
+                            const String &name="ProjectionStates") :
+                            ParticleStates(name),
+                           positions_(positions),
+                           orientations_(orientations),
+                           projections_(projections) {
+    number_of_states_ = positions_.size()*orientations_.size();
+  };
+
+  /*! Returns the projection corresponding to the state i;
+    \param[in] i
+  */
+  Image* get_projection(unsigned int  i) const;
+
+  /*! Returns the position corresponding to the state i
+    \param[in] i
+  */
+  algebra::Vector3D get_position(unsigned int  i) const;
+
+  IMP_PARTICLE_STATES(ProjectionStates);
+
+ };
+
+
+IMPEM2D_END_NAMESPACE
+
+#endif  /* IMPEM2D_DOMINO_PARTICLE_STATES_H */
