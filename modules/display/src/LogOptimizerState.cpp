9000f39ac31bae2e97194d200cd7d5d14bd86f85
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-06-05 18:57:52 +0000

    restore write method and support creating writers implicitly from file names
    git-svn-id: https://svn.salilab.org/imp/trunk@9602 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/display/src/LogOptimizerState.cpp
+++ modules/display/src/LogOptimizerState.cpp
@@ -10,17 +10,23 @@
 
 IMPDISPLAY_BEGIN_NAMESPACE
 
+void WriteOptimizerState::write(WriterOutput w) const {
+  IMP::internal::OwnerPointer<Writer> wp(w);
+  for (unsigned int i=0; i< get_number_of_geometries(); ++i) {
+    get_geometry(i)->set_was_used(true);
+    w->add_geometry(get_geometry(i));
+  }
+}
+
 void WriteOptimizerState::update() {
   if (call_number_%(skip_steps_+1) ==0) {
     writer_->set_frame(update_number_);
-    for (unsigned int i=0; i< get_number_of_geometries(); ++i) {
-      get_geometry(i)->set_was_used(true);
-      writer_->add_geometry(get_geometry(i));
-    }
+    write(writer_.get());
     ++update_number_;
   }
   ++call_number_;
 }
 IMP_LIST_PLURAL_IMPL(WriteOptimizerState, Geometry, Geometries,
                      geometry, geometries, Geometry*, Geometries,,,);
+
 IMPDISPLAY_END_NAMESPACE
