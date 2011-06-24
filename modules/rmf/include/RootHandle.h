3c9a78d03772d4d1b7516bc6097ec976b91c8fb3
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-06-24 00:19:18 +0000

    add descriptions, various docs, and make swig happier with various methods
    git-svn-id: https://svn.salilab.org/imp/trunk@9758 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/rmf/include/RootHandle.h
+++ modules/rmf/include/RootHandle.h
@@ -18,6 +18,9 @@
 IMPRMF_BEGIN_NAMESPACE
 class NodeHandle;
 
+typedef std::pair<NodeHandle, NodeHandle> BondPair;
+typedef std::vector<BondPair> BondPairs;
+
 /** A handle for an hdf5 root */
 class IMPRMFEXPORT RootHandle: public NodeHandle {
   void gather_ids(NodeHandle n, std::vector<int> &ids,
@@ -40,6 +43,10 @@ class IMPRMFEXPORT RootHandle: public NodeHandle {
     Key<TypeT> get_key(KeyCategory category_id, std::string name) const {
     return shared_->get_key<TypeT>(category_id, name);
   }
+  //! Lift NodeHandle::get_name() into class scope
+  std::string get_name() const {
+    return NodeHandle::get_name();
+  }
   /** Create a key for a new type of data. There must not
       already be a key with the same name of any type.
   */
@@ -53,67 +60,70 @@ class IMPRMFEXPORT RootHandle: public NodeHandle {
                      std::string name) const {
     return shared_->get_key<TypeT>(category_id, name) != Key<TypeT>();
   }
-  /** Get the string name for the given key.
-   */
-  template <class TypeT>
-    std::string get_name(Key<TypeT> k) const {
-    return shared_->get_name(k);
-  }
   /** Get a list of all keys of the given type,
    */
   template <class TypeT>
   std::vector<Key<TypeT> > get_keys(KeyCategory category_id) const {
     return shared_->get_keys<TypeT>(category_id);
   }
-
-  /** Get a list of all keys of the given type,
-   */
-  template <class TypeT>
-    bool get_is_per_frame(Key<TypeT> k) const {
-    return shared_->get_is_per_frame(k);
-  }
-  template <class TypeT>
-    unsigned int get_number_of_frames(Key<TypeT> k) const {
-    return shared_->get_number_of_frames(k);
-  }
   /** \name Non-template versions for python
       @{
   */
 #define IMP_HDF5_ROOT_KEY_TYPE_METHODS(lcname, UCName)                  \
-  Key<UCName##Traits> get_##lcname##_key(KeyCategory category_id,       \
-                                         std::string nm) const {        \
+  UCName##Key get_##lcname##_key(KeyCategory category_id,               \
+                                 std::string nm) const {                \
     return get_key<UCName##Traits>(category_id, nm);                    \
   }                                                                     \
-  Key<UCName##Traits> add_##lcname##_key(KeyCategory category_id,       \
-                                         std::string nm,                \
-                                         bool per_frame) const {        \
+  UCName##Key add_##lcname##_key(KeyCategory category_id,               \
+                                 std::string nm,                        \
+                                 bool per_frame) const {                \
     return add_key<UCName##Traits>(category_id, nm, per_frame);         \
   }                                                                     \
-  std::string get_name(Key<UCName##Traits> k) const {                   \
-    return get_name<UCName##Traits>(k);                                 \
+  std::string get_name(UCName##Key k) const {                           \
+    return shared_->get_name(k);                                        \
   }                                                                     \
-  std::vector<Key<UCName##Traits> >                                     \
+  UCName##Key##s                                                        \
     get_##lcname##_keys(KeyCategory category_id) const {                \
     return get_keys<UCName##Traits>(category_id);                       \
   }                                                                     \
-  unsigned int get_number_of_frames(Key<UCName##Traits> k) const {      \
+  unsigned int get_number_of_frames(UCName##Key k) const {              \
     return shared_->get_number_of_frames(k);                            \
+  }                                                                     \
+  bool get_is_per_frame(UCName##Key k) const {                          \
+    return shared_->get_is_per_frame(k);                                \
   }
+
 
   IMP_HDF5_ROOT_KEY_TYPE_METHODS(int, Int);
   IMP_HDF5_ROOT_KEY_TYPE_METHODS(float, Float);
   IMP_HDF5_ROOT_KEY_TYPE_METHODS(string, String);
   IMP_HDF5_ROOT_KEY_TYPE_METHODS(index, Index);
   IMP_HDF5_ROOT_KEY_TYPE_METHODS(node_id, NodeID);
-
-  //IMP_HDF5_ROOT_TYPE(string, 0, std::string);
+  IMP_HDF5_ROOT_KEY_TYPE_METHODS(data_set, DataSet);
   /** @} */
-
+#ifdef IMP_DOXYGEN
+  /** \name Python only
+      The following methods are only available in python.
+      @{
+  */
+  //! Return a list with all the keys from that category
+  PythonList get_keys(KeyCategory c) const;
+  /** @} */
+#endif
+  /** Each node in the hierarchy can be associated with some arbitrary bit
+      of external data using a void* pointer. Nodes can be extracted using
+      these bits of data.
+  */
   NodeHandle get_node_handle_from_association(void*d) const;
   NodeHandle get_node_handle_from_id(NodeID id) const;
   void show(std::ostream &out= std::cout) const {
     out << "RootHandle";
   }
+  /** \name Bonds
+      The hierarchy also contains information about bonds connecting
+      arbitrary nodes in the hierarchy.
+      @{
+  */
   unsigned int get_number_of_bonds() const {
     return shared_->get_number_of_bonds();
   }
@@ -127,13 +137,28 @@ class IMPRMFEXPORT RootHandle: public NodeHandle {
     NodeID idb= nb.get_id();
     shared_->add_bond(ida.get_index(), idb.get_index(), type);
   }
-
+  BondPairs get_bonds() const;
+  /** @} */
   HDF5Group get_hdf5_group() const {
     return shared_->get_group();
   }
+  /** Suggest how many frames the file is likely to have. This can
+      make writing more efficient as space will be preallocated.
+  */
   void set_number_of_frames_hint(unsigned int i) {
     shared_->save_frames_hint(i);
   }
+  /** \name Descriptions
+      Each RMF structure has an associated description. This should
+      consist of unstructured text describing the contents of the RMF
+      data. Conventionally. this description can consist of multiple
+      paragraphs, each separated by a newline character and should end
+      in a newline.
+      @{
+  */
+  std::string get_description() const;
+  void set_description(std::string descr);
+  /** @} */
 };
 IMP_VALUES(RootHandle, RootHandles);
 
