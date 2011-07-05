c83565751e3b0525078831c7800484662527d5d9
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-07-05 20:20:16 +0000

    make sure that things are initialized before use by having the add_geometry() call get_stream()
    git-svn-id: https://svn.salilab.org/imp/trunk@9863 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/display/test/test_cmm.py
+++ modules/display/test/test_cmm.py
@@ -3,17 +3,23 @@ import IMP.test
 import IMP.core
 import IMP.display
 import StringIO
+import re
 
 class TestBL(IMP.test.TestCase):
     def test_5(self):
         """Testing the CMM writer and frames"""
         name=self.get_tmp_file_name("cmm.%1%.py")
+        m= IMP.Model()
+        p=IMP.Particle(m)
+        d= IMP.core.XYZR.setup_particle(p)
         w=IMP.display.CMMWriter(name)
         names=[]
         for i in range(0,5):
             w.set_frame(i)
             g= IMP.display.SphereGeometry(IMP.algebra.Sphere3D(IMP.algebra.Vector3D(i,i,i), i))
             names.append(w.get_current_file_name())
+            w.add_geometry(g)
+            g= IMP.display.XYZRGeometry(p)
             w.add_geometry(g)
         del w
         for n in names:
@@ -23,13 +29,27 @@ class TestBL(IMP.test.TestCase):
     def test_0(self):
         """Testing the CMM writer without frames"""
         name=self.get_tmp_file_name("cmm.py")
+        m= IMP.Model()
+        p=IMP.Particle(m)
+        d= IMP.core.XYZR.setup_particle(p)
         w=IMP.display.CMMWriter(name)
-        g= IMP.display.SphereGeometry(IMP.algebra.Sphere3D(IMP.algebra.Vector3D(0,0,0), 1))
+        g= IMP.display.XYZRGeometry(p)
+        print "first"#, w.get_current_index()
         w.add_geometry(g)
+        g= IMP.display.SphereGeometry(IMP.algebra.Sphere3D(IMP.algebra.Vector3D(0,0,0), 1))
+        print "second", w.get_current_index()
+        w.add_geometry(g)
+        print "done",  w.get_current_index()
         del w
         print name
         contents= open(name, "r").read()
+        cre=re.compile('id="([0123456789]*)"')
+        print "contents"
         print contents
+        print "matches"
+        for m in cre.findall(contents):
+            print m
+            self.assertLess(int(m), 3)
 
 if __name__ == '__main__':
     IMP.test.main()
