6479264248a6d57264aa3614b799887a70b73ef4
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-07-05 00:03:36 +0000

    read x-link records
    git-svn-id: https://svn.salilab.org/imp/trunk@9857 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/multifit/src/proteomics_reader.cpp
+++ modules/multifit/src/proteomics_reader.cpp
@@ -64,6 +64,39 @@ bool is_xlink_line(const std::string &line) {
   return true;
 }
 
+void parse_xlink_line(
+     const std::string &line,
+     ProteomicsData *dp){
+  std::cout<<"parse_xlink_line:"<<line<<std::endl;
+  std::vector<int> inter_prots;
+  typedef boost::split_iterator<std::string::iterator> string_split_iterator;
+  IMP_USAGE_CHECK(line.size() > 2,
+     "no data to parse. the last two tabs should contain header data\n");
+  IMP_LOG(VERBOSE,"going to parse:"<<line);
+  std::vector<std::string> line_split;
+  boost::split(line_split, line, boost::is_any_of("|"));
+  //split returns zero lenght entires as well
+  line_split.erase( std::remove_if(line_split.begin(),line_split.end(),
+    boost::bind( &std::string::empty, _1 ) ),line_split.end() );
+  std::cout<<"PARSE:"<<line_split.size()<<std::endl;
+  std::string name1 =  boost::lexical_cast<std::string>(line_split[0]);
+  int index1 = dp->find(name1);
+  std::string name2 =  boost::lexical_cast<std::string>(line_split[2]);
+  int index2 = dp->find(name2);
+  std::cout<<"XLINK between "<<name1<<" "<<name2<<std::endl;
+  IMP_USAGE_CHECK(index1 != -1,
+                  "The protein "<<name1<<
+                  " was not specified in the proteins list"<<std::endl);
+  IMP_USAGE_CHECK(index2 != -1,
+                  "The protein "<<name2<<
+                  " was not specified in the proteins list"<<std::endl);
+  inter_prots.push_back(index1);
+  inter_prots.push_back(index2);
+  //todo - for now the residue index is not used
+  dp->add_interaction(inter_prots);
+}
+
+
 void parse_protein_line(
                         const std::string &line,
                         ProteomicsData *dp){
@@ -158,6 +191,12 @@ ProteomicsData read_proteomics_data(const char *prot_fn) {
   while ((!in.eof()) && (!is_xlink_line(line))){
     //  while (!is_xlink_line(line)){
     parse_interaction_line(line,&data);
+    std::cout<<"==finish"<<std::endl;
+    if (!getline(in, line)) break;
+  }
+  getline(in, line);
+  while (!in.eof()) {
+    parse_xlink_line(line,&data);
     std::cout<<"==finish"<<std::endl;
     if (!getline(in, line)) break;
   }
