74b91443a15fe13b13e871d805568e64c713c236
Massimiliano Bonomi <mbonomi@salilab.org>
2011-07-07 23:02:43 +0000

    major improvements :-)

--- bin/spb.cpp
+++ bin/spb.cpp
@@ -9,6 +9,7 @@
 #include <IMP/atom.h>
 #include <IMP/algebra.h>
 #include <IMP/container.h>
+#include <IMP/domino.h>
 #include <boost/program_options.hpp>
 #include <IMP/base_types.h>
 #include <string>
@@ -214,6 +215,64 @@ FloatRange get_range_from_fret_value(double r_value)
  return get_range_from_fret_class(r_class);
 }
 
+void do_bipartite_mindist(Model *m,Particles p1,Particles p2,
+ core::DistancePairScore* dps,bool filter=false)
+{
+ IMP_NEW(container::ListSingletonContainer,lp1,(p1));
+ IMP_NEW(container::ListSingletonContainer,lp2,(p2));
+ IMP_NEW(container::AllBipartitePairContainer,pc,(lp1,lp2));
+ if(filter){
+  //f=IMP.atom.SameResiduePairFilter()
+  //pc.add_pair_filter(f)
+ }
+ IMP_NEW(container::MinimumPairRestraint,mpr,(dps,pc,1));
+ m->add_restraint(mpr);
+ m->set_maximum_score(mpr, error_bound);
+}
+
+void do_allpairs_mindist(Model *m,Particles ps,
+ core::DistancePairScore* dps,bool filter=false)
+{
+ IMP_NEW(container::ListSingletonContainer,lp,(ps));
+ IMP_NEW(container::AllPairContainer,pc,(lp));
+ if(filter){
+  //f=IMP.atom.SameResiduePairFilter()
+  //pc.add_pair_filter(f)
+ }
+ IMP_NEW(container::MinimumPairRestraint,mpr,(dps,pc,1));
+ m->add_restraint(mpr);
+ m->set_maximum_score(mpr, error_bound);
+}
+
+void do_bipartite_mindist(Model *m,Particles p1,Particles p2,
+ core::SphereDistancePairScore* dps,bool filter=false)
+{
+ IMP_NEW(container::ListSingletonContainer,lp1,(p1));
+ IMP_NEW(container::ListSingletonContainer,lp2,(p2));
+ IMP_NEW(container::AllBipartitePairContainer,pc,(lp1,lp2));
+ if(filter){
+  //f=IMP.atom.SameResiduePairFilter()
+  //pc.add_pair_filter(f)
+ }
+ IMP_NEW(container::MinimumPairRestraint,mpr,(dps,pc,1));
+ m->add_restraint(mpr);
+ m->set_maximum_score(mpr, error_bound);
+}
+
+void do_allpairs_mindist(Model *m,Particles ps,
+ core::SphereDistancePairScore* dps,bool filter=false)
+{
+ IMP_NEW(container::ListSingletonContainer,lp,(ps));
+ IMP_NEW(container::AllPairContainer,pc,(lp));
+ if(filter){
+  //f=IMP.atom.SameResiduePairFilter()
+  //pc.add_pair_filter(f)
+ }
+ IMP_NEW(container::MinimumPairRestraint,mpr,(dps,pc,1));
+ m->add_restraint(mpr);
+ m->set_maximum_score(mpr, error_bound);
+}
+
 void add_fret_restraint
 (Model *m,atom::Hierarchies ha,std::string protein_a,std::string residues_a,
  atom::Hierarchies hb, std::string protein_b, std::string residues_b,
@@ -232,70 +291,38 @@ void add_fret_restraint
  FloatRange range=get_range_from_fret_value(r_value);
  core::DistancePairScore* ps=get_pair_score(range);
  if(protein_a != protein_b){
-  IMP_NEW(container::ListSingletonContainer,lp1,(p1));
-  IMP_NEW(container::ListSingletonContainer,lp2,(p2));
-  IMP_NEW(container::AllBipartitePairContainer,pc,(lp1,lp2));
-  IMP_NEW(container::MinimumPairRestraint,mpr,(ps,pc,1));
-  m->add_restraint(mpr);
-  m->set_maximum_score(mpr, error_bound);
+  do_bipartite_mindist(m,p1,p2,ps);
  }else if(protein_a==protein_b && residues_a==residues_b){
-  IMP_NEW(container::ListSingletonContainer,lp,(p1));
-  IMP_NEW(container::AllPairContainer,pc,(lp));
-  IMP_NEW(container::MinimumPairRestraint,mpr,(ps,pc,1));
-  //f=IMP.atom.SameResiduePairFilter()
-  //pc.add_pair_filter(f)
-  m->add_restraint(mpr);
-  m->set_maximum_score(mpr, error_bound);
+  do_allpairs_mindist(m,p1,ps);
  }else if(protein_a==protein_b && residues_a!=residues_b){
-  IMP_NEW(container::ListSingletonContainer,lp1,(p1));
-  IMP_NEW(container::ListSingletonContainer,lp2,(p2));
-  IMP_NEW(container::AllBipartitePairContainer,pc,(lp1,lp2));
-  IMP_NEW(container::MinimumPairRestraint,mpr,(ps,pc,1));
-  //f=IMP.atom.SameResiduePairFilter()
-  //pc.add_pair_filter(f)
-  m->add_restraint(mpr);
-  m->set_maximum_score(mpr, error_bound);
+  do_bipartite_mindist(m,p1,p2,ps);
  }
 }
 
 void add_y2h_restraint
-(Model *m,atom::Hierarchies ha,std::string protein_a,Ints residues_a,
- atom::Hierarchies hb,std::string protein_b,Ints residues_b)
+(Model *m,atom::Hierarchies ha,std::string protein_a,IntRange residues_a,
+ atom::Hierarchies hb,std::string protein_b,IntRange residues_b)
 {
  atom::Selection sa=atom::Selection(ha);
  sa.set_molecule(protein_a);
- sa.set_residue_indexes(residues_a);
+ Ints r_a;
+ for(int i=residues_a.first;i<=residues_a.second;++i) r_a.push_back(i);
+ sa.set_residue_indexes(r_a);
  atom::Selection sb=atom::Selection(hb);
  sb.set_molecule(protein_b);
- sb.set_residue_indexes(residues_b);
+ Ints r_b;
+ for(int i=residues_b.first;i<=residues_b.second;++i) r_b.push_back(i);
+ sb.set_residue_indexes(r_b);
  Particles p1=sa.get_selected_particles();
  Particles p2=sb.get_selected_particles();
  FloatRange range=FloatRange(-500,0.0);
  core::SphereDistancePairScore* ps=get_sphere_pair_score(range);
  if(protein_a != protein_b){
-  IMP_NEW(container::ListSingletonContainer,lp1,(p1));
-  IMP_NEW(container::ListSingletonContainer,lp2,(p2));
-  IMP_NEW(container::AllBipartitePairContainer,pc,(lp1,lp2));
-  IMP_NEW(container::MinimumPairRestraint,mpr,(ps,pc,1));
-  m->add_restraint(mpr);
-  m->set_maximum_score(mpr, error_bound);
+  do_bipartite_mindist(m,p1,p2,ps);
  }else if(protein_a==protein_b && residues_a==residues_b){
-  IMP_NEW(container::ListSingletonContainer,lp,(p1));
-  IMP_NEW(container::AllPairContainer,pc,(lp));
-  IMP_NEW(container::MinimumPairRestraint,mpr,(ps,pc,1));
-  //f=IMP.atom.SameResiduePairFilter()
-  //pc.add_pair_filter(f)
-  m->add_restraint(mpr);
-  m->set_maximum_score(mpr, error_bound);
+  do_allpairs_mindist(m,p1,ps);
  }else if(protein_a==protein_b && residues_a!=residues_b){
-  IMP_NEW(container::ListSingletonContainer,lp1,(p1));
-  IMP_NEW(container::ListSingletonContainer,lp2,(p2));
-  IMP_NEW(container::AllBipartitePairContainer,pc,(lp1,lp2));
-  IMP_NEW(container::MinimumPairRestraint,mpr,(ps,pc,1));
-  //f=IMP.atom.SameResiduePairFilter()
-  //pc.add_pair_filter(f)
-  m->add_restraint(mpr);
-  m->set_maximum_score(mpr, error_bound);
+  do_bipartite_mindist(m,p1,p2,ps);
  }
 }
 
@@ -316,39 +343,23 @@ void add_y2h_restraint
  FloatRange range=FloatRange(-500,0.0);
  core::SphereDistancePairScore* ps=get_sphere_pair_score(range);
  if(protein_a != protein_b){
-  IMP_NEW(container::ListSingletonContainer,lp1,(p1));
-  IMP_NEW(container::ListSingletonContainer,lp2,(p2));
-  IMP_NEW(container::AllBipartitePairContainer,pc,(lp1,lp2));
-  IMP_NEW(container::MinimumPairRestraint,mpr,(ps,pc,1));
-  m->add_restraint(mpr);
-  m->set_maximum_score(mpr, error_bound);
+  do_bipartite_mindist(m,p1,p2,ps);
  }else if(protein_a==protein_b && residues_a==residues_b){
-  IMP_NEW(container::ListSingletonContainer,lp,(p1));
-  IMP_NEW(container::AllPairContainer,pc,(lp));
-  IMP_NEW(container::MinimumPairRestraint,mpr,(ps,pc,1));
-  //f=IMP.atom.SameResiduePairFilter()
-  //pc.add_pair_filter(f)
-  m->add_restraint(mpr);
-  m->set_maximum_score(mpr, error_bound);
+  do_allpairs_mindist(m,p1,ps);
  }else if(protein_a==protein_b && residues_a!=residues_b){
-  IMP_NEW(container::ListSingletonContainer,lp1,(p1));
-  IMP_NEW(container::ListSingletonContainer,lp2,(p2));
-  IMP_NEW(container::AllBipartitePairContainer,pc,(lp1,lp2));
-  IMP_NEW(container::MinimumPairRestraint,mpr,(ps,pc,1));
-  //f=IMP.atom.SameResiduePairFilter()
-  //pc.add_pair_filter(f)
-  m->add_restraint(mpr);
-  m->set_maximum_score(mpr, error_bound);
+  do_bipartite_mindist(m,p1,p2,ps);
  }
 }
 
 void add_y2h_restraint
-(Model *m,atom::Hierarchies ha,std::string protein_a,Ints residues_a,
- atom::Hierarchies hb, std::string protein_b, std::string residues_b)
+(Model *m,atom::Hierarchies ha,std::string protein_a,IntRange residues_a,
+ atom::Hierarchies hb,std::string protein_b,std::string residues_b)
 {
  atom::Selection sa=atom::Selection(ha);
  sa.set_molecule(protein_a);
- sa.set_residue_indexes(residues_a);
+ Ints r_a;
+ for(int i=residues_a.first;i<=residues_a.second;++i) r_a.push_back(i);
+ sa.set_residue_indexes(r_a);
  atom::Selection sb=atom::Selection(hb);
  sb.set_molecule(protein_b);
  if(residues_b=="C") {sb.set_terminus(atom::Selection::C);}
@@ -357,23 +368,142 @@ void add_y2h_restraint
  Particles p2=sb.get_selected_particles();
  FloatRange range=FloatRange(-500,0.0);
  core::SphereDistancePairScore* ps=get_sphere_pair_score(range);
- if(protein_a != protein_b){
-  IMP_NEW(container::ListSingletonContainer,lp1,(p1));
-  IMP_NEW(container::ListSingletonContainer,lp2,(p2));
-  IMP_NEW(container::AllBipartitePairContainer,pc,(lp1,lp2));
-  IMP_NEW(container::MinimumPairRestraint,mpr,(ps,pc,1));
-  m->add_restraint(mpr);
-  m->set_maximum_score(mpr, error_bound);
+ do_bipartite_mindist(m,p1,p2,ps);
+}
+
+void add_y2h_restraint
+(Model *m,atom::Hierarchies ha,std::string protein_a,std::string residues_a,
+ atom::Hierarchies hb,std::string protein_b,IntRange residues_b)
+{
+ add_y2h_restraint(m,hb,protein_b,residues_b,ha,protein_a,residues_a);
+}
+
+void add_symmetry_restraint(Model *m,atom::Hierarchies hs)
+{
+ algebra::Vector3Ds translations=algebra::Vector3Ds();
+ algebra::Transformation3Ds transformations=
+ algebra::Transformation3Ds();
+ int num_rotations;
+ double angle;
+ if(cell_type!="square"){
+  translations.push_back(algebra::Vector3D(0.0,0.0,0.0));
+  translations.push_back(algebra::Vector3D(0.0,side*sqrt(3.0),0.0));
+  translations.push_back(algebra::Vector3D(1.5*side,side*sqrt(3.0)/2.0,0.0));
+  translations.push_back(algebra::Vector3D(1.5*side,-side*sqrt(3.0)/2.0,0.0));
+  translations.push_back(algebra::Vector3D(0.0,-side*sqrt(3.0),0.0));
+  translations.push_back(algebra::Vector3D(-1.5*side,-side*sqrt(3.0)/2.0,0.0));
+  translations.push_back(algebra::Vector3D(-1.5*side,side*sqrt(3.0)/2.0,0.0));
+  num_rotations=num_cells/7;
+  angle=2.0*IMP::PI/(double)num_rotations;
  }else{
-  IMP_NEW(container::ListSingletonContainer,lp1,(p1));
-  IMP_NEW(container::ListSingletonContainer,lp2,(p2));
-  IMP_NEW(container::AllBipartitePairContainer,pc,(lp1,lp2));
-  IMP_NEW(container::MinimumPairRestraint,mpr,(ps,pc,1));
-  //f=IMP.atom.SameResiduePairFilter()
-  //pc.add_pair_filter(f)
-  m->add_restraint(mpr);
-  m->set_maximum_score(mpr, error_bound);
+  translations.push_back(algebra::Vector3D(0.0,0.0,0.0));
+  translations.push_back(algebra::Vector3D(0.0,side,0.0));
+  translations.push_back(algebra::Vector3D(0.0,-side,0.0));
+  translations.push_back(algebra::Vector3D(side,0.0,0.0));
+  translations.push_back(algebra::Vector3D(side,side,0.0));
+  translations.push_back(algebra::Vector3D(side,-side,0.0));
+  translations.push_back(algebra::Vector3D(-side,0.0,0.0));
+  translations.push_back(algebra::Vector3D(-side,side,0.0));
+  translations.push_back(algebra::Vector3D(-side,-side,0.0));
+  num_rotations=1;
+  angle=0.0;
  }
+ for(int i=0;i<translations.size();++i){
+  for(int j=0;j<num_rotations;++j){
+   algebra::Rotation3D rot=algebra::get_rotation_about_axis
+   (algebra::Vector3D(0.0,0.0,1.0), (double) j * angle);
+   transformations.push_back(algebra::Transformation3D(rot,translations[i]));
+  }
+ }
+ Particles ps0=hs[0].get_leaves();
+ for(int i=1;i<num_cells;++i){
+  IMP_NEW(core::TransformationSymmetry,sm,(transformations[i]));
+  Particles ps1=hs[i].get_leaves();
+  for(int j=0;j<ps1.size();++j){
+   core::Reference::setup_particle(ps1[j],ps0[j]);
+  }
+  IMP_NEW(container::ListSingletonContainer,lc,(ps1));
+  IMP_NEW(container::SingletonsConstraint,c,(sm,NULL,lc));
+  m->add_score_state(c);
+ }
+}
+
+domino::EquivalenceSubsetFilterTable* add_equivalence_filters
+(std::vector<std::string> all,atom::Hierarchy h,Particles eq_ps)
+{
+ IMP_NEW(domino::EquivalenceSubsetFilterTable,f,());
+ for(int i=0;i<all.size();++i){
+  std::string name=all[i];
+  std::vector<Particles> ps;
+  atom::HierarchiesTemp mol=h.get_children();
+  for(int j=0;j<mol.size();++j){
+   if(mol[j]->get_name()==name){
+    ParticlesTemp pp=mol[j].get_leaves();
+    ps.push_back(pp);
+   }
+  }
+  int ncopies=ps.size();
+  if(ncopies==1){
+   for(int j=0;j<(ps[0]).size();++j){
+    eq_ps.push_back(ps[0][j]);
+   }
+  }
+  if(ncopies>1){
+   for(int j=0;j<(ps[0]).size();++j){
+    Particles pps;
+    for(int k=0;k<ncopies;++k){
+     pps.push_back(ps[k][j]);
+    }
+    f->add_set(pps);
+    eq_ps.push_back(pps[0]);
+   }
+  }
+ }
+ return f.release();
+}
+
+domino::ListSubsetFilterTable* setup_sampler
+(Model *m, atom::Hierarchy h, algebra::Vector3Ds cover, double scale,
+ domino::ParticleStatesTable *pst, domino::DominoSampler *s,
+ domino::EquivalenceSubsetFilterTable *esft)
+{
+ IMP_NEW(domino::XYZStates,states,(cover));
+ ParticlesTemp ps=h.get_leaves();
+ for(int i=0;i<ps.size();++i){
+  pst->set_particle_states(ps[i],states);
+ }
+ RestraintSet *rs=m->get_root_restraint_set();
+ for(unsigned int i=0;i<rs->get_number_of_restraints();++i){
+  if(cell_type=="square"){
+   rs->get_restraint(i)->set_maximum_score(pow(scale,2));
+  }else{
+   rs->get_restraint(i)->set_maximum_score(1.45*pow(scale,2));
+  }
+ }
+ domino::SubsetFilterTables fs=domino::SubsetFilterTables();
+ IMP_NEW(domino::ListSubsetFilterTable,lsft,(pst));
+ IMP_NEW(domino::RestraintScoreSubsetFilterTable,rssft,(m, pst));
+ rssft->set_use_caching(false);
+ fs.push_back(lsft);
+ fs.push_back(rssft);
+ fs.push_back(esft);
+ domino::InteractionGraph ig=domino::get_interaction_graph(rs,pst);
+ domino::SubsetGraph      jt=domino::get_junction_tree(ig);
+ s->set_subset_filter_tables(fs);
+ return lsft.release();
+}
+
+std::vector<Ints> get_mapping
+(algebra::Vector3Ds cover0, algebra::Vector3Ds cover1)
+{
+ IMP_NEW(algebra::NearestNeighbor3D,nn,(cover0));
+ std::vector<Ints> ret;
+ ret.resize(cover0.size());
+ for(unsigned int i=0;i<cover1.size();++i){
+  unsigned int nns = nn->get_nearest_neighbor(cover1[i]);
+  ret[nns].push_back(i);
+ }
+ return ret;
 }
 
 int main(int  , char **)
