198d9e105fb11ac5bf1651c1a17245a35efdbc7c
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-07-13 17:44:46 +0000

    use path.join
    git-svn-id: https://svn.salilab.org/imp/trunk@9922 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- scons_tools/bug_fixes.py
+++ scons_tools/bug_fixes.py
@@ -1,6 +1,7 @@
 from SCons.Script import *
 import copy
 import data
+import os.path
 
 # scons messes up the paths when the build dir is separate from the source dir
 # and tends to pass paths saying everything is in the build dir
@@ -9,7 +10,7 @@ import data
 def fix_node(env, node):
     if not os.path.exists(node.path):
         if env.get('repository', None) != None:
-            nnode= File(env['repository']+"/"+node.path)
+            nnode= File(os.path.join(env['repository'],node.path))
             if os.path.exists(nnode.path):
                 return nnode
     return node
