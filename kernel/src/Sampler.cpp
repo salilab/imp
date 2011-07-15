592a4c0634b5bf1f92f26dfb132202701067bcc5
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-07-15 17:46:00 +0000

    remove unused code
    git-svn-id: https://svn.salilab.org/imp/trunk@9937 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/src/Sampler.cpp
+++ kernel/src/Sampler.cpp
@@ -20,14 +20,7 @@ Sampler::Sampler(Model *m,
 
 ConfigurationSet *Sampler::get_sample() const {
   IMP_OBJECT_LOG;
-  unsigned int num_opt=0;
   set_was_used(true);
-  for (Model::ParticleConstIterator pit= model_->particles_begin();
-       pit != model_->particles_end(); ++pit) {
-    unsigned int d=std::distance((*pit)->optimized_keys_begin(),
-                  (*pit)->optimized_keys_end());
-    if (d > 0) ++num_opt;
-  }
   //IMP_LOG(TERSE, "Sampling " << num_opt << " particles."<<std::endl);
   /*if (num_opt == 0) {
     IMP_WARN("There are no particles to optimize."<<std::endl);
