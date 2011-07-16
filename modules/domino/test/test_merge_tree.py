0528cdd5b3cc207c03691401503b004dc83639e2
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-07-16 02:57:17 +0000

    add balanced merge tree constructor
    git-svn-id: https://svn.salilab.org/imp/trunk@9940 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/domino/test/test_merge_tree.py
@@ -0,0 +1,26 @@
+import IMP
+import IMP.test
+import IMP.domino
+import IMP.core
+import IMP.atom
+
+
+class DOMINOTests(IMP.test.TestCase):
+    def test_global_min0(self):
+        """Testing balanced merge tree algorithm"""
+        m= IMP.Model()
+        ps= [IMP.Particle(m) for p in range(10)]
+        jt= IMP.domino.SubsetGraph()
+        for p in ps:
+            jt.add_vertex(IMP.domino.Subset([p]))
+        vs= jt.get_vertices()
+        for i in range(1,10):
+            jt.add_edge(i-1,i)
+        IMP.show_graphviz(jt)
+        print jt
+        print type(jt)
+        mbt= IMP.domino.get_balanced_merge_tree(jt)
+        IMP.show_graphviz(mbt)
+
+if __name__ == '__main__':
+    IMP.test.main()
