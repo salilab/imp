2477d966617beb51f2a766fffe389e397cd063ed
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-07-18 19:25:10 +0000

    simplify close pairs traits
    git-svn-id: https://svn.salilab.org/imp/trunk@9981 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/include/GridClosePairsFinder.h
+++ modules/core/include/GridClosePairsFinder.h
@@ -20,20 +20,8 @@ IMPCORE_BEGIN_NAMESPACE
  */
 class IMPCOREEXPORT GridClosePairsFinder : public ClosePairsFinder
 {
-  algebra::BoundingBox3D bb_;
-  unsigned int merged_;
  public:
   GridClosePairsFinder();
-
-  enum {X=1, Y=2, Z=4} Boundary;
-  /** Find close pairs with periodic boundary conditions. The
-      bounding box is considered joined along X if
-      merged_boundaries&X. Any point outside the bounding box
-      in a periodic direction is considered to be at its
-      image in the bounding box.
-  */
-  GridClosePairsFinder(const algebra::BoundingBox3D &bb,
-                       unsigned int merged_boundaries= X|Y|Z);
 
   IMP_CLOSE_PAIRS_FINDER(GridClosePairsFinder);
 };
--- modules/core/src/GridClosePairsFinder.cpp
+++ modules/core/src/GridClosePairsFinder.cpp
@@ -11,13 +11,8 @@
 
 IMPCORE_BEGIN_NAMESPACE
 
-GridClosePairsFinder::GridClosePairsFinder(): ClosePairsFinder("GridCPF"),
-                                              merged_(0)
+GridClosePairsFinder::GridClosePairsFinder(): ClosePairsFinder("GridCPF")
 {}
-
-GridClosePairsFinder::GridClosePairsFinder(const algebra::BoundingBox3D &bb,
-                                           unsigned int merged_boundaries):
-  ClosePairsFinder("PeriodicGridCPF"), bb_(bb), merged_(merged_boundaries) {}
 
 
 ParticlePairsTemp GridClosePairsFinder
@@ -26,42 +21,16 @@ ParticlePairsTemp GridClosePairsFinder
   IMP_OBJECT_LOG;
   set_was_used(true);
   ParticlePairsTemp out;
-  if (merged_) {
-    internal::PParticleHelper
-      ::fill_close_pairs(internal::PParticleHelper
-                         ::get_particle_set(ca.begin(),
-                                            ca.end(),
-                                            internal::ParticleID(),
-                                            internal::ParticleCenter(),
-                                            internal::ParticleRadius()),
-                         internal::PParticleHelper
-                         ::get_particle_set(cb.begin(),
-                                            cb.end(),
-                                            internal::ParticleID(),
-                                            internal::ParticleCenter(),
-                                            internal::ParticleRadius()),
-                         internal::PeriodicParticleClose(bb_, merged_,
-                                               get_distance()),
-                         get_distance(), bb_, merged_,
-                         internal::ParticlePairSink(out));
-  } else {
-     internal::ParticleHelper
-      ::fill_close_pairs( internal::ParticleHelper
-                         ::get_particle_set(ca.begin(),
-                                            ca.end(),
-                                            internal::ParticleID(),
-                                            internal::ParticleCenter(),
-                                            internal::ParticleRadius()),
-                          internal::ParticleHelper
-                         ::get_particle_set(cb.begin(),
-                                            cb.end(),
-                                            internal::ParticleID(),
-                                            internal::ParticleCenter(),
-                                            internal::ParticleRadius()),
-                          internal::ParticleClose(get_distance()),
-                         get_distance(), bb_, merged_,
-                          internal::ParticlePairSink(out));
-  }
+  internal::ParticleHelper
+    ::fill_close_pairs( internal::ParticleHelper
+                        ::get_particle_set(ca.begin(),
+                                           ca.end(),0),
+                        internal::ParticleHelper
+                        ::get_particle_set(cb.begin(),
+                                           cb.end(), 1),
+                        internal::ParticleTraits(ca[0]->get_model(),
+                                                 get_distance()),
+                        internal::ParticlePairSink(out));
   return out;
 }
 
@@ -72,27 +41,12 @@ ParticlePairsTemp GridClosePairsFinder
   IMP_LOG(TERSE, "Rebuilding NBL with Grid and cutoff "
           << get_distance() << std::endl );
   ParticlePairsTemp out;
-  if (merged_) {
-     internal::PParticleHelper
-      ::fill_close_pairs(internal::PParticleHelper::get_particle_set(c.begin(),
-                                                           c.end(),
-                                                     internal::ParticleID(),
-                                                  internal::ParticleCenter(),
-                                                  internal::ParticleRadius()),
-                 internal::PeriodicParticleClose(bb_, merged_, get_distance()),
-                         get_distance(), bb_, merged_,
-                         internal::ParticlePairSink(out));
-  } else {
-     internal::ParticleHelper
-      ::fill_close_pairs(internal::ParticleHelper::get_particle_set(c.begin(),
-                                                                    c.end(),
-                                                        internal::ParticleID(),
-                                                    internal::ParticleCenter(),
-                                                   internal::ParticleRadius()),
-                         internal::ParticleClose(get_distance()),
-                         get_distance(), bb_, merged_,
-                         internal::ParticlePairSink(out));
-  }
+  internal::ParticleHelper
+    ::fill_close_pairs(internal::ParticleHelper::get_particle_set(c.begin(),
+                                                                  c.end(),0),
+                       internal::ParticleTraits(c[0]->get_model(),
+                                                get_distance()),
+                       internal::ParticlePairSink(out));
   return out;
 }
 
@@ -102,38 +56,14 @@ IntPairs GridClosePairsFinder
   IMP_OBJECT_LOG;
   set_was_used(true);
   IntPairs out;
-  if (merged_) {
-     internal::PBBHelper::
-      fill_close_pairs(internal::PBBHelper::get_particle_set(bas.begin(),
-                                                   bas.end(),
-                                                   internal::BBID(bas.begin()),
-                                               internal::BBCenter(bas.begin()),
-                                              internal::BBRadius(bas.begin())),
-                       internal::PBBHelper::get_particle_set(bbs.begin(),
-                                                   bbs.end(),
-                                                   internal::BBID(bbs.begin()),
-                                               internal::BBCenter(bbs.begin()),
-                                              internal::BBRadius(bbs.begin())),
-              internal::PeriodicBBClose(bas.begin(), bbs.begin(), bb_, merged_,
-                                       get_distance()),
-                       get_distance(), bb_, merged_,
-                       internal::BBPairSink(out));
-  } else {
-    internal::BBHelper::
-      fill_close_pairs(internal::BBHelper::get_particle_set(bas.begin(),
-                                                  bas.end(),
-                                                  internal::BBID(bas.begin()),
-                                              internal::BBCenter(bas.begin()),
-                                             internal::BBRadius(bas.begin())),
-                       internal::BBHelper::get_particle_set(bbs.begin(),
-                                                  bbs.end(),
-                                                  internal::BBID(bbs.begin()),
-                                              internal::BBCenter(bbs.begin()),
-                                             internal::BBRadius(bbs.begin())),
-                  internal::BBClose(bas.begin(), bbs.begin(), get_distance()),
-                       get_distance(), bb_, merged_,
-                       internal::BBPairSink(out));
-  }
+  internal::BBHelper::
+    fill_close_pairs(internal::BBHelper::get_particle_set(bas.begin(),
+                                                          bas.end(),0),
+                     internal::BBHelper::get_particle_set(bbs.begin(),
+                                                          bbs.end(),1),
+                     internal::BoundingBoxTraits(bas.begin(),
+                                                 bbs.begin(), get_distance()),
+                     internal::BBPairSink(out));
   return out;
 }
 
@@ -142,28 +72,12 @@ IntPairs GridClosePairsFinder
   IMP_OBJECT_LOG;
   set_was_used(true);
   IntPairs out;
-  if (merged_) {
-    internal::PBBHelper
-      ::fill_close_pairs(internal::PBBHelper::get_particle_set(bas.begin(),
-                                                     bas.end(),
-                                               internal::BBID(bas.begin()),
-                                           internal::BBCenter(bas.begin()),
-                                          internal::BBRadius(bas.begin())),
-                         internal::PeriodicBBClose(bas.begin(), bas.begin(),
-                                         bb_, merged_, get_distance()),
-                         get_distance(), bb_, merged_,
-                         internal::BBPairSink(out));
-  } else {
-    internal::BBHelper
-      ::fill_close_pairs(internal::BBHelper::get_particle_set(bas.begin(),
-                                                    bas.end(),
-                                                    internal::BBID(bas.begin()),
-                                                internal::BBCenter(bas.begin()),
-                                               internal::BBRadius(bas.begin())),
-                    internal::BBClose(bas.begin(), bas.begin(), get_distance()),
-                         get_distance(), bb_, merged_,
-                         internal::BBPairSink(out));
-  }
+  internal::BBHelper
+    ::fill_close_pairs(internal::BBHelper::get_particle_set(bas.begin(),
+                                                            bas.end(), 0),
+                       internal::BoundingBoxTraits(bas.begin(),
+                                                   bas.begin(), get_distance()),
+                       internal::BBPairSink(out));
   return out;
 }
 
