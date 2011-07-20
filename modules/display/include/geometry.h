a47c34c853a9cdeb7e8e61eab60e5bba9ad750ff
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-07-20 18:51:57 +0000

    fix return type
    git-svn-id: https://svn.salilab.org/imp/trunk@10004 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/display/include/geometry.h
+++ modules/display/include/geometry.h
@@ -30,7 +30,7 @@
 
 IMPDISPLAY_BEGIN_NAMESPACE
 class Geometry;
-typedef VectorOfRefCounted<Geometry*> Geometries;
+typedef std::vector<Pointer< Geometry > > Geometries;
 typedef std::vector<Geometry*> GeometriesTemp;
 
 
@@ -61,7 +61,7 @@ public:
   }
 
   //! Return a set of geometry composing this one
-  virtual Geometries get_components() const {return GeometriesTemp();}
+  virtual Geometries get_components() const {return Geometries();}
 
   IMP_REF_COUNTED_DESTRUCTOR(Geometry);
 };
