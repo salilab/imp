dbd7c984a524149d389df7125004d9bd6bf5587e
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-07-25 22:11:13 +0000

    update plots and remvoe old benchmark results
    git-svn-id: https://svn.salilab.org/imp/trunk@10078 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/benchmark/bin/benchmark_pdb.cpp
@@ -0,0 +1,45 @@
+/**
+ * Copyright 2007-2011 IMP Inventors. All rights reserved.
+ */
+#include <IMP.h>
+#include <IMP/core.h>
+#include <IMP/algebra.h>
+#include <IMP/atom.h>
+#include <boost/timer.hpp>
+#include <IMP/benchmark/utility.h>
+#include <IMP/benchmark/benchmark_macros.h>
+
+using namespace IMP;
+using namespace IMP::core;
+using namespace IMP::algebra;
+using namespace IMP::atom;
+
+#define N 1
+
+#ifdef __GNUC__
+#define ATTRIBUTES __attribute ((__noinline__))
+#else
+#define ATTRIBUTES
+#endif
+
+namespace {
+void do_benchmark(Model * m) {
+ // measure time
+  double runtime;
+  double total=0;
+  std::string fname= IMP::benchmark::get_data_path("large_protein.pdb");
+  IMP_TIME(
+             {
+               atom::Hierarchy h= read_pdb(fname, m);
+               total+=reinterpret_cast<size_t>(h.get_particle());
+               atom::destroy(h);
+             }, runtime);
+  IMP::benchmark::report(std::string("pdb"), runtime, total);
+}
+}
+
+int main(int , char **) {
+  IMP_NEW(Model, m, ());
+  do_benchmark(m);
+  return IMP::benchmark::get_return_value();
+}
