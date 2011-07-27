92553ac820a632c11ce8b9421cbcb285d60c16af
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-07-27 00:47:17 +0000

    minor simplifications
    git-svn-id: https://svn.salilab.org/imp/trunk@10098 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/include/DistancePairScore.h
+++ modules/core/include/DistancePairScore.h
@@ -45,31 +45,29 @@ public:
   double get_stiffness() const {
     return k_;
   }
-  IMP_SIMPLE_PAIR_SCORE(HarmonicDistancePairScore);
+  IMP_INDEX_PAIR_SCORE(HarmonicDistancePairScore);
 };
 
 IMP_OBJECTS(HarmonicDistancePairScore, HarmonicDistancePairScores);
 
 
 #ifndef IMP_DOXYGEN
-inline double HarmonicDistancePairScore::evaluate(const ParticlePair &p,
-                            DerivativeAccumulator *da) const {
-  XYZ d0(p[0]), d1(p[1]);
-  algebra::VectorD<3> delta;
-  for (int i = 0; i < 3; ++i) {
-    delta[i] = d0.get_coordinate(i) - d1.get_coordinate(i);
-  }
+inline double HarmonicDistancePairScore
+::evaluate_index(Model *m,
+                 const ParticleIndexPair &p,
+                 DerivativeAccumulator *da) const {
+  algebra::VectorD<3> delta=m->get_sphere(p[0]).get_center()
+    - m->get_sphere(p[1]).get_center();
   static const double MIN_DISTANCE = .00001;
   double distance2= delta.get_squared_magnitude();
   double distance=std::sqrt(distance2);
   double shifted_distance = distance- x0_;
   double score= .5*k_*square(shifted_distance);
-  if (!da || distance < MIN_DISTANCE) return score;
-  double deriv= k_*shifted_distance;
-  algebra::Vector3D uv= delta/distance;
-  if (da) {
-    d0.add_to_derivatives(uv*deriv, *da);
-    d1.add_to_derivatives(-uv*deriv, *da);
+  if (da && distance > MIN_DISTANCE) {
+    double deriv= k_*shifted_distance;
+    algebra::Vector3D uv= delta/distance;
+    m->add_to_coordinate_derivatives(p[0], uv*deriv, *da);
+    m->add_to_coordinate_derivatives(p[1], -uv*deriv, *da);
   }
 
   return score;
