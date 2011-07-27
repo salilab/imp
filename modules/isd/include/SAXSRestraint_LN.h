8a0d7d3ff75e4b6c6807c41e34fef4dfff18abe7
yannick@SALILAB.ORG <yannick@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-07-27 01:40:35 +0000

    SAXS restraints, normal and lognormal
    git-svn-id: https://svn.salilab.org/imp/trunk@11497 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/isd/include/SAXSRestraint_LN.h
+++ modules/isd/include/SAXSRestraint_LN.h
@@ -6,8 +6,8 @@
  *
  */
 
-#ifndef IMPISD_SAXS_RESTRAINT_EMPIRICAL_N_H
-#define IMPISD_SAXS_RESTRAINT_EMPIRICAL_N_H
+#ifndef IMPISD_SAXS_RESTRAINT_LN_H
+#define IMPISD_SAXS_RESTRAINT_LN_H
 
 #include "isd_config.h"
 
@@ -23,16 +23,16 @@
 
 IMPISD_BEGIN_NAMESPACE
 
-//! Calculate score based on fit to SAXS profile. Normal model with one
-//  weight. Empirical Bayes.
+//! Calculate score based on fit to SAXS profile. Lognormal model with one
+//  weight.
 /** \ingroup exp_restraint
 
-    The likelihood of this model is a normal. Assume M data points.
-    \f[p(D | X,\sigma,\gamma, I) = \frac{1}{(\sqrt{2\pi}\sigma)^M} 
+    The likelihood of this model is a lognormal. Assume M data points.
+    \f[p(D | X,\alpha,\gamma, I) = \frac{1}{(\sqrt{2\pi}\alpha)^M} 
     \prod_{q=q_{min}}^{q_{max}} \frac{1}{\sigma_{exp}(q)}
-    \exp left[ - \frac{1}{2\sigma^2\sigma^2_{exp}(q)} 
-                \left(I_{exp}(q) - \gamma I_{calc}(q)
-                \right)^2 \right] \f]
+    \exp left[ - \frac{I_{exp}^2(q)}{2\alpha^2\sigma^2_{exp}(q)} 
+                \log^2 \left(\frac{I_{exp}(q)}{\gamma I_{calc}(q)}
+                \right) \right] \f]
 
     The restraint takes rigid bodies into account, in order
     to speed up the calculations. Rigid body should be gived as single
@@ -46,13 +46,13 @@ IMPISD_BEGIN_NAMESPACE
     The distances between the atoms of rigid body do not change, therefore
     their contribution to the profile is pre-computed and stored.
  */
-class IMPISDEXPORT SAXSRestraint_empirical_N : public ISDRestraint
+class IMPISDEXPORT SAXSRestraint_LN : public ISDRestraint
 {
  public:
   //! Constructor
   /**
      \param[in] particles The particles that generated the profile, usually atoms of a protein.
-     \param[in] sigma An ISD Scale particle that will be \f$\sigma\f$
+     \param[in] alpha An ISD Scale particle that will be \f$\alpha\f$
      \param[in] gamma An ISD Scale particle that will set the scaling factor \f$\gamma\f$ 
                 between experiment and mock data.
      \param[in] exp_profile  The experimental profile used to restrain the structure.
@@ -61,17 +61,17 @@ class IMPISDEXPORT SAXSRestraint_empirical_N : public ISDRestraint
                 HEAVY_ATOMS - no hydrogens, all other atoms included
                 CA_ATOMS - residue level, residue represented by CA
   */
-  SAXSRestraint_empirical_N(const Particles& particles, const Scale& sigma, const
+  SAXSRestraint_LN(const Particles& particles, const Scale& alpha, const
           Scale& gamma, const saxs::Profile& exp_profile, saxs::FormFactorType ff_type = saxs::HEAVY_ATOMS);
 
-  IMP_RESTRAINT(SAXSRestraint_empirical_N);
+  IMP_RESTRAINT(SAXSRestraint_LN);
 
   double get_probability() const { return std::exp(-unprotected_evaluate(NULL));}
 
  private:
   void compute_profile(saxs::Profile& model_profile);
   Particles particles_; // non-rigid bodies particles
-  Scale sigma_; // global weight
+  Scale alpha_; // global weight
   Scale gamma_; // calibration factor
   std::vector<core::RigidBody> rigid_bodies_decorators_; //rigid bodies
   std::vector<Particles> rigid_bodies_; // rigid bodies particles
@@ -82,4 +82,4 @@ class IMPISDEXPORT SAXSRestraint_empirical_N : public ISDRestraint
 
 IMPISD_END_NAMESPACE
 
-#endif  /* IMPISD_SAXS_RESTRAINT_EMPIRICAL_N_H */
+#endif  /* IMPISD_SAXS_RESTRAINT_LN_H */
