cb000c4cbc10240706730329a075b1329db3c532
javi@SALILAB.ORG <javi@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-08-09 19:36:06 +0000

    removed XYZsTemp
    git-svn-id: https://svn.salilab.org/imp/trunk@10275 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/em2d/src/project.cpp
+++ modules/em2d/src/project.cpp
@@ -117,7 +117,8 @@ void do_project_particles(const ParticlesTemp &ps,
   }
   // Centroid
   unsigned long n_particles = ps.size();
-  core::XYZRsTemp xyzrs(ps);
+  //core::XYZRsTemp xyzrs(ps);
+  core::XYZRs xyzrs(ps);
   algebra::Vector3D centroid = core::get_centroid(xyzrs);
 
   // clear data before creating a new projection
@@ -379,7 +380,8 @@ unsigned int get_enclosing_image_size(const ParticlesTemp &ps,
                                       double pixel_size,
                                       unsigned int slack) {
   IMP_NEW(Particle, p, (ps[0]->get_model(), "cover Particle") );
-  core::XYZsTemp xyzs(ps);
+  // core::XYZsTemp xyzs(ps);
+  core::XYZs xyzs(ps);
   double diameter = 2 * core::get_enclosing_sphere(xyzs).get_radius();
   unsigned int size = static_cast<unsigned int>(diameter/pixel_size) + 2*slack;
   return size;
