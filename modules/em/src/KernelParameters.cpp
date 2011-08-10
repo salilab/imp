52de6c1b5ff8c21b2b08f9b99f7aec7c2ba535b8
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-08-10 19:45:20 +0000

    add gaussian and laplacian functions
    git-svn-id: https://svn.salilab.org/imp/trunk@10278 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/em/src/KernelParameters.cpp
+++ modules/em/src/KernelParameters.cpp
@@ -137,4 +137,85 @@ const RadiusDependentKernelParameters* KernelParameters::get_params(
    return closest;
 }
 
+//! Create a truncated 3D Gaussian
+Kernel3D create_3d_gaussian (double sigma,double sigma_factor) {
+
+  // determine gaussian extent
+  int half_ext = (int)ceil(sigma_factor*sigma);
+  int ext = 2 * half_ext - 1;
+  int size = ext*ext*ext;
+  Kernel3D ret(size,ext);
+  double *data=ret.get_data();
+  //calculate gaussian
+  double bvalue = -1.0 / (2.0*sigma*sigma);
+  double cvalue = sigma_factor*sigma_factor*sigma*sigma;
+  double mscale=0.0;
+  for (int indz=0;indz<ext;indz++)
+    for (int indy=0;indy<ext;indy++)
+      for (int indx=0;indx<ext;indx++) {
+        double dsqu = (indx-half_ext+1)*(indx-half_ext+1)+
+          (indy-half_ext+1)*(indy-half_ext+1)+
+          (indz-half_ext+1)*(indz-half_ext+1);
+        if (dsqu <= cvalue) {
+          double val=exp(dsqu * bvalue);
+          data[indz*ext*ext+indy*ext+indx]=val;
+          mscale+=val;
+        }
+      }
+  for(int ind=0;ind<size;ind++) data[ind]/=mscale;
+  return ret;
+}
+
+Kernel3D create_3d_laplacian() {
+  Kernel3D ret(27,3);
+  double *data=ret.get_data();
+  //calculate
+  data[1+1*3+0*9]=1./12.;
+  data[0+1*3+1*9]=1./12.;
+  data[1+0*3+1*9]=1./12.;
+  data[1+1*3+1*9]=-6./12.;
+  data[1+2*3+1*9]=1./12.;
+  data[2+1*3+1*9]=1./12.;
+  data[1+1*3+2*9]=1./12.;
+  return ret;
+}
+
+em::Kernel3D get_truncated(double *in_data, int in_ext,
+                           double sigmap,
+                           double sigma_factor){
+
+  // truncate at sigmap*sigma_factor
+  int exth = (int) ceil(sigma_factor*sigmap);
+  int out_ext = 2 * exth - 1;
+  int out_size=out_ext * out_ext * out_ext;
+  double cvalue = sigma_factor*sigma_factor*sigmap*sigmap;
+  IMP_LOG(VERBOSE,"Truncated to extent "<<out_ext<<std::endl);
+  em::Kernel3D ret(out_size,out_ext);
+  double *data = ret.get_data();
+
+  int margin = (in_ext-out_ext)/2;
+  long new_ind,old_ind;
+  for (int indz=margin;indz<in_ext-margin;indz++)
+    for (int indy=margin;indy<in_ext-margin;indy++)
+      for (int indx=margin;indx<in_ext-margin;indx++) {
+        new_ind=out_ext * out_ext * (indz-margin) +
+          out_ext * (indy-margin)+(indx-margin);
+        old_ind=in_ext*in_ext*indz+in_ext*indy+indx;
+        data[new_ind]=in_data[old_ind];
+      }
+  //adjust kernel to be spherical
+  double dsqu;
+  for (int indz=0;indz<out_ext;indz++)
+    for (int indy=0;indy<out_ext;indy++)
+      for (int indx=0;indx<out_ext;indx++) {
+        new_ind=out_ext*out_ext*indz+out_ext*indy+indx;
+        dsqu = (indx-exth+1)*(indx-exth+1)+
+          (indy-exth+1)*(indy-exth+1)+
+          (indz-exth+1)*(indz-exth+1);
+        if (dsqu > cvalue) data[new_ind]=0.;
+      }
+  return ret;
+}
+
+
 IMPEM_END_NAMESPACE
