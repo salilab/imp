e0dbe72e3ffdbeddf249de9b1f7699656b76e22d
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-08-12 20:03:23 +0000

    add density analysis tools
    git-svn-id: https://svn.salilab.org/imp/trunk@10332 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/multifit/include/density_analysis.h
+++ modules/multifit/include/density_analysis.h
@@ -11,12 +11,142 @@
 #include <IMP/statistics/Histogram.h>
 #include <IMP/em/DensityMap.h>
 #include "multifit_config.h"
+#include <IMP/domino/DiscreteSampler.h>
+#include <boost/graph/adjacency_list.hpp>
+#include <boost/pending/disjoint_sets.hpp>
+#include <boost/graph/incremental_components.hpp>
+#include <boost/graph/graph_utility.hpp>
 
 IMPMULTIFIT_BEGIN_NAMESPACE
 
 //! Get a histogram of density values
 IMPMULTIFITEXPORT statistics::Histogram get_density_histogram(
                    const em::DensityMap *dmap, float threshold,int num_bins);
+
+
+typedef boost::adjacency_list<boost::vecS, boost::vecS, boost::undirectedS,
+                              boost::property<boost::vertex_index_t, long>,
+                              boost::property<boost::edge_centrality_t, double
+                                               > > DensityGraph;
+typedef boost::graph_traits<DensityGraph>::vertex_descriptor DGVertex;
+typedef boost::graph_traits<DensityGraph>::edge_descriptor DGEdge;
+
+class IMPMULTIFITEXPORT DensitySegmentationByCommunities {
+public:
+  //! Constructor
+  /**
+     \param[input] dmap the map to segment
+     \param[input] dens_t only consider voxels above this threshold
+   */
+  DensitySegmentationByCommunities(em::DensityMap *dmap,
+                                   float dens_t):
+    dmap_(dmap),dens_t_(dens_t){}
+  //! Build the density graph
+  /**
+     \param[input] edge_threshold two voxels are considered
+     neighbors if the difference between their density does
+     not exceed this number
+   */
+  void build_density_graph(float neighbor_threshold);
+  //! Graph clustering based on edge betweenness centrality.
+  //! It is an iterative algorithm, where in each step it computes
+  //! the edge betweenness centrality (via brandes_betweenness_centrality)
+  //! and removes the edge with the maximum betweenness centrality.
+  domino::IntsList calculate_communities(int num_clusters);
+  algebra::Vector3Ds get_centers() {
+    algebra::Vector3Ds centers;
+    DGVertex v;
+    for (std::pair<boost::graph_traits<DensityGraph>::vertex_iterator,
+                   boost::graph_traits<DensityGraph>::vertex_iterator> be=
+           boost::vertices(g_);
+         be.first != be.second; ++be.first) {
+      v=*be.first;
+      centers.push_back(
+          dmap_->get_location_by_voxel(node2voxel_ind_[node_index_[v]]));
+    }
+    return centers;
+  }
+  std::vector<Ints> calculate_connected_components();
+ protected:
+  Pointer<em::DensityMap> dmap_;
+  DensityGraph g_;
+  float dens_t_;
+  std::vector<float> weights_;
+  std::vector<long> node2voxel_ind_;
+  boost::property_map<DensityGraph, boost::vertex_index_t>::type node_index_;
+  typedef boost::disjoint_sets<int*, int*> DS;
+  /*The function object that indicates termination of the algorithm.
+    It must be a ternary function object thats accepts the maximum centrality,
+    the descriptor of the edge that will be removed, and the graph g.
+  */
+  struct Done {
+    typedef double centrality_type;
+    int k_;
+    std::vector<int> rank_, parent_;
+    //rank stores the size of the set, and parent the parent
+    /**
+       \pram[in] k the maximum number of clsuters
+       \param[in] n the number of elements
+    */
+    Done(int k, int n): k_(k), rank_(n), parent_(n){}
+    template <class B>
+    /**
+       \param[input] c the current maximum centrality
+       \param[input] e the edge that is being examined
+       \param[input] g the graph
+       \note The function calculates the new conneted components
+             of the system after the edge was removed
+       \note We will be removing edges until we have more than k components
+    */
+    bool operator()(centrality_type c, const B & e, const DensityGraph &g) {
+      std::cout << "Done called on " << boost::source(e, g)
+                << "--" << boost::target(e, g)
+                << ": " << c << std::endl;
+      DS ds(&rank_[0], &parent_[0]);
+      //Basic initialization of the disjoint-sets structure.
+      //Each vertex in the graph g is in its own set.
+      boost::initialize_incremental_components(g, ds);
+      //The connected components are calculated based on
+      //the edges in the graph g and the information is embedded in ds.
+      boost::incremental_components(g, ds);
+      unsigned int s= ds.count_sets(boost::vertices(g).first,
+                                    boost::vertices(g).second);
+      //if there are more than k components we are done
+      std::cout<<"s:"<<s<<"k:"<<k_<<std::endl;
+      return s >= static_cast<unsigned int>(k_);
+    }
+  };
+};
+
+
+IMPMULTIFITEXPORT em::DensityMaps density_segmentation(
+         em::DensityMap *dmap,
+         float dens_t,float edge_threshold,
+         int num_clusters);
+
+//! Returns a map containing all density without the background
+/**
+\param[in] dmap the density map to segment
+\param[in] threshold consider only voxels above this threshold
+\param[in] edge_threshold consider only voxels above this threshold
+\return the segmented map
+ */
+IMPMULTIFITEXPORT em::DensityMap* remove_background(em::DensityMap *dmap,
+                                      float threshold,float edge_threshold);
+
+
+//! Return connected components based on density values
+/**
+   \param[in] dmap the density map to analyze
+   \param[in] threshold consider only voxels above this threshold
+   \param[in] edge_threshold voxels are neighbors
+                             if above this threshold
+   \return List of indexes for each connected component
+*/
+IMPMULTIFITEXPORT
+domino::IntsList get_connected_components(
+                                          em::DensityMap *dmap,
+                                          float threshold,float edge_threshold);
 
 IMPMULTIFIT_END_NAMESPACE
 #endif /* IMPMULTIFIT_DENSITY_ANALYSIS_H */
---
+++ modules/multifit/test/test_connected_components.py
@@ -0,0 +1,43 @@
+import sys,os
+import IMP
+import IMP.em
+import IMP.test
+import IMP.core
+import IMP.atom
+import IMP.multifit
+
+class ConnectedComponentsTest(IMP.test.TestCase):
+    """Test connected components """
+
+    def setUp(self):
+        """Build test model and optimizer"""
+        IMP.test.TestCase.setUp(self)
+        IMP.set_log_level(IMP.VERBOSE)#SILENT)
+
+    def test_connected_components(self):
+        """test connected components"""
+        #create a map of X connected components
+        dmap=IMP.em.create_density_map(IMP.algebra.BoundingBox3D(
+            IMP.algebra.Vector3D(-50,-50,-50),
+            IMP.algebra.Vector3D(50,50,50)),2.)
+
+        for i in range(5):
+            #sample i populations
+            mdl=IMP.Model()
+            ps=[]
+            for j in range(i+1):
+                bb=IMP.algebra.BoundingBox3D(
+                    IMP.algebra.Vector3D(-1*(j+1),-1*(j+1),-1*(j+1)),
+                    IMP.algebra.Vector3D(1*(j+1),1*(j+1),1*(j+1)))
+                for k in range(10):
+                    p=IMP.Particle(mdl)
+                    IMP.core.XYZR.setup_particle(p,
+                                   IMP.algebra.Sphere3D(IMP.algebra.get_random_vector_in(bb)+IMP.algebra.Vector3D(j*20,j*20,j*20),2))
+                    IMP.atom.Mass.setup_particle(p,1)
+                    ps.append(p)
+            dmap=IMP.em.particles2density(ps,1,1)
+        con_comp=IMP.multifit.get_connected_components(dmap,0.001,0.5)
+        self.assertEqual(len(con_comp),i+1)
+
+if __name__ == '__main__':
+    IMP.test.main()
