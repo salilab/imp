1a089d86d394c49917979c348fe348d37a51b0fc
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-08-16 06:34:32 +0000

    add incremental scoring
    git-svn-id: https://svn.salilab.org/imp/trunk@10411 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/src/Optimizer.cpp
+++ kernel/src/Optimizer.cpp
@@ -8,9 +8,11 @@
 #include "IMP/log.h"
 #include "IMP/Optimizer.h"
 #include "IMP/OptimizerState.h"
+#include "IMP/RestraintSet.h"
 #include "IMP/internal/utility.h"
 #include <boost/tuple/tuple.hpp>
 #include <limits>
+#include <algorithm>
 
 IMP_BEGIN_NAMESPACE
 
@@ -49,11 +51,13 @@ double Optimizer::optimize(unsigned int max_steps) {
   }
   if (eval_incremental_) {
     setup_incremental();
-  }
-  set_was_used(true);
-  flattened_restraints_
+  } else {
+    flattened_restraints_
     =IMP::get_restraints(RestraintsTemp(restraints_.begin(),
                                         restraints_.end()));
+  }
+  set_was_used(true);
+
   double ret= do_optimize(max_steps);
   if (eval_incremental_) {
     teardown_incremental();
@@ -111,23 +115,59 @@ RestraintsTemp Optimizer::get_restraints() const {
 }
 
 void Optimizer::setup_incremental() {
-  IMP_NOT_IMPLEMENTED;
-  // get them
-  // get initial scores
-  // create dg
-  // figure out restraints for each particle
+  IMP_OBJECT_LOG;
+  IMP_LOG(TERSE, "Setting up incremental evaluation." << std::endl);
+  flattened_restraints_=create_decomposition(get_restraints());
+  for (unsigned int i=0; i< flattened_restraints_.size(); ++i) {
+    incremental_scores_= get_model()->evaluate(flattened_restraints_, false);
+  }
+  DependencyGraph dg= get_dependency_graph(flattened_restraints_);
+  compatibility::map<Restraint*, int> index;
+  for (unsigned int i=0; i< flattened_restraints_.size(); ++i) {
+    index[flattened_restraints_[i]]=i;
+  }
+  ParticlesTemp ap= get_model()->get_particles();
+  for (unsigned int i=0; i< ap.size(); ++i) {
+    RestraintsTemp cur=get_dependent_restraints(ap[i], ParticlesTemp(),
+                                                dg);
+    ParticleIndex pi= ap[i]->get_index();
+    incremental_used_.resize(std::max<unsigned int>(incremental_used_.size(),
+                                      pi+1));
+    for (unsigned int j=0; j< cur.size(); ++j) {
+      if (index.find(cur[j]) != index.end()) {
+        incremental_used_[pi].push_back(index.find(cur[j])->second);
+      }
+    }
+  }
+  IMP_LOG(TERSE, "Done setting up incremental evaluation." << std::endl);
 }
 void Optimizer::teardown_incremental() {
-  IMP_NOT_IMPLEMENTED;
+  flattened_restraints_.clear();
+  incremental_scores_.clear();
+  incremental_used_.clear();
 }
 
 double Optimizer::evaluate_incremental(const ParticleIndexes &moved) const {
-  IMP_NOT_IMPLEMENTED;
-}
-double Optimizer::evaluate_incremental_if_below(const ParticleIndexes &moved,
-                                                double max) const {
-  IMP_NOT_IMPLEMENTED;
+  Ints allr;
+  for (unsigned int i=0; i< moved.size(); ++i) {
+    allr.insert(allr.end(), incremental_used_[i].begin(),
+                incremental_used_[i].end());
+  }
+  RestraintsTemp cur(allr.size());
+  for (unsigned int i=0; i< allr.size(); ++i) {
+    cur[i]= flattened_restraints_[allr[i]];
+  }
+  Floats score= get_model()->evaluate(cur, false);
+  for (unsigned int i=0; i< allr.size(); ++i) {
+    incremental_scores_[allr[i]]=score[i];
+  }
+  return std::accumulate(incremental_scores_.begin(),
+                         incremental_scores_.end(), 0);
 }
 
+double Optimizer::evaluate_incremental_if_below(const ParticleIndexes &moved,
+                                     double max) const {
+  IMP_NOT_IMPLEMENTED;
+}
 
 IMP_END_NAMESPACE
