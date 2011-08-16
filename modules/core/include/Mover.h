2c39c3c905dd3dfb857742c36a50aa794ddfbc0e
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-08-16 23:47:03 +0000

    Fix typo.
    git-svn-id: https://svn.salilab.org/imp/trunk@10434 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/include/Mover.h
+++ modules/core/include/Mover.h
@@ -34,9 +34,7 @@ class IMPCOREEXPORT Mover: public Object
 
 /* Older versions of g++ do not extend friendship to
    MonteCarlo::MoverDataWrapper, and thus fail to compile MonteCarlo.h. */
-#if defined(__GNUC__) && (__GNUC__ < 4                                  \
-                          || defined(__GNUC_MINOR)                      \
-                          && (__GNUC__ == 4 && __GNUC_MINOR < 1))
+#if defined(__GNUC__) && (__GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 1))
 public:
 #endif
   void set_optimizer(Optimizer *c) {
