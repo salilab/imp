4dccc8477ed2512b7e67d0012a65e3678c117c96
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-08-18 03:39:11 +0000

    add test for pair list
    git-svn-id: https://svn.salilab.org/imp/trunk@10491 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/domino/test/test_find_pairs.py
@@ -0,0 +1,41 @@
+import IMP
+import IMP.test
+import IMP.domino
+import IMP.core
+import IMP.atom
+import IMP.algebra
+import random
+
+ns=10
+class DOMINOTests(IMP.test.TestCase):
+    def test_global_min1(self):
+        """Test close pairs list generation"""
+        m= IMP.Model()
+        ps= [IMP.Particle(m) for i in range(0,10)]
+        ds= [IMP.core.XYZR.setup_particle(p) for p in ps]
+        for d in ds:
+            d.set_radius(6)
+        bb= IMP.algebra.BoundingBox3D(IMP.algebra.Vector3D(0,0,0),
+                                      IMP.algebra.Vector3D(50,50,50))
+        bbs=IMP.algebra.BoundingBox3D(IMP.algebra.Vector3D(0,0,0),
+                                      IMP.algebra.Vector3D(4,4,4))
+        pst= IMP.domino.ParticleStatesTable()
+        for p in ps:
+            v0=IMP.algebra.get_random_vector_in(bb)
+            vs=[v0+IMP.algebra.get_random_vector_in(bbs) for i in range(0,ns)]
+            yxz= IMP.domino.XYZStates(vs)
+            pst.set_particle_states(p, yxz)
+        allp= IMP.domino.get_possible_interactions(ps, 2, pst)
+        cpf= IMP.core.GridClosePairsFinder()
+        cpf.set_distance(2)
+        pst.set_was_used(True)
+        for rep in range(0,1000):
+            for p in ps:
+                st= pst.get_particle_states(p)
+                st.load_particle_state(random.sample(range(0,ns), 1)[0], p)
+            curp= cpf.get_close_pairs(ps)
+            print len(curp)
+            for p in curp:
+                self.assert_(p in allp or (p[1], p[0]) in allp)
+if __name__ == '__main__':
+    IMP.test.main()
