43068d7ab029ce3337099bd3a5c89bd0bbac1775
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-08-23 00:14:31 +0000

    add method to show objects
    git-svn-id: https://svn.salilab.org/imp/trunk@10616 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/include/Object.h
+++ kernel/include/Object.h
@@ -209,6 +209,16 @@ inline O* object_cast(I *o) {
 inline void show(std::ostream &out, Object *o) {
   out << "\"" << o->get_name() << "\"";
 }
+
+template <class OS>
+inline void show_objects(const OS &os, std::ostream &out) {
+  out << "[";
+  for (unsigned int i=0; i< os.size(); ++i) {
+    if (i!= 0) out << ", ";
+    out << os[i]->get_name();
+  }
+  out << "]";
+}
 #endif
 
 
