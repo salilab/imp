82f5d8a50b70bc4a7259beeea76df58fe92a3e46
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-08-23 19:55:14 +0000

    Fix test and clean up.
    git-svn-id: https://svn.salilab.org/imp/trunk@10665 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/parallel/test/test_startup.py
+++ modules/parallel/test/test_startup.py
@@ -1,6 +1,7 @@
 import IMP
 import IMP.test
 import IMP.parallel
+import util
 
 class StartupTests(IMP.test.TestCase):
     """Test startup of parallel jobs"""
@@ -14,8 +15,9 @@ class StartupTests(IMP.test.TestCase):
         m.add_slave(IMP.parallel.LocalSlave())
         c = m.get_context()
         c.add_task(empty_task)
-        list(c.get_results_unordered())
-#       self.assertRaises(NetworkError, c.get_results_unordered())
+        self.assertRaises(IMP.parallel.NetworkError, list,
+                          c.get_results_unordered())
+        util.unlink('slave0.output')
 
 if __name__ == '__main__':
     IMP.test.main()
