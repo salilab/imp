082921ab4a321b53c50278f8f7b7126da5c3714c
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-08-24 19:29:46 +0000

    improved support for bin cxxfdlags
    git-svn-id: https://svn.salilab.org/imp/trunk@10721 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- scons_tools/environment.py
+++ scons_tools/environment.py
@@ -138,8 +138,9 @@ def _add_platform_flags(env):
         # "-Werror",  "-Wno-uninitialized"
         env.Append(CXXFLAGS=["-Wall", "-Wextra",  "-Wno-deprecated",
                              "-Winit-self", "-Wstrict-aliasing=2",
-                             "-Wcast-align", "-fno-operator-names",
-                             "-Wmissing-prototypes"])
+                             "-Wcast-align", "-fno-operator-names",])
+        if dependency.gcc.get_version() > 4.1:
+            env.Append(CXXFLAGS=["-Wmissing-prototypes"])
         #if dependency.gcc.get_version(env)>= 4.3:
         #    env.Append(CXXFLAGS=["-Wunsafe-loop-optimizations"])
         # gcc 4.0 on Mac doesn't like -isystem, so we don't use it there.
