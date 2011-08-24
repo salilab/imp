c107e45f6cd5143677114add623f673ecb31812e
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-08-24 23:29:13 +0000

    Add more slave startup and task tests.
    git-svn-id: https://svn.salilab.org/imp/trunk@10741 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/parallel/test/tasks.py
+++ modules/parallel/test/tasks.py
@@ -5,3 +5,11 @@ class SimpleTask(object):
 
     def __call__(self):
         return self.inp
+
+
+def error_task():
+    """Raise an exception on the slave"""
+    raise ValueError("Slave error")
+
+def simple_func(*args):
+    return args
