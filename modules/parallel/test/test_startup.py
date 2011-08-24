fcadfaf863e45261c4c477baa9d9fc581eb2f1d2
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-08-24 02:10:52 +0000

    Add simple test of successful startup.
    git-svn-id: https://svn.salilab.org/imp/trunk@10680 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/parallel/test/test_startup.py
+++ modules/parallel/test/test_startup.py
@@ -2,9 +2,22 @@ import IMP
 import IMP.test
 import IMP.parallel
 import util
+import tasks
 
 class StartupTests(IMP.test.TestCase):
     """Test startup of parallel jobs"""
+
+    def test_simple(self):
+        """Test that slave tasks can start up and communicate"""
+        m = IMP.parallel.Manager()
+        m.add_slave(IMP.parallel.LocalSlave())
+        c = m.get_context()
+        for i in range(10):
+            c.add_task(tasks.SimpleTask(i))
+        results = list(c.get_results_unordered())
+        results.sort()
+        self.assertEqual(results, list(range(10)))
+        util.unlink("slave0.output")
 
     def test_startup_heartbeat(self):
         """Make sure that startup failures cause a timeout"""
