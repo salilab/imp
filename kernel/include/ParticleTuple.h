d315fa4c1337142ce2b417f6fa63d9cce34cb94b
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-08-24 22:18:20 +0000

    move tuple off of ref counted
    git-svn-id: https://svn.salilab.org/imp/trunk@10729 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- kernel/include/ParticleTuple.h
+++ kernel/include/ParticleTuple.h
@@ -10,10 +10,15 @@
 #define IMP_PARTICLE_TUPLE_H
 
 #include "Particle.h"
-#include <boost/array.hpp>
+#include "Pointer.h"
+#include "WeakPointer.h"
+#include "internal/ParticleTupleBase.h"
 
 IMP_BEGIN_NAMESPACE
 
+
+template <unsigned int D>
+class RefCountedParticleTuple;
 
 //! A class to store a tuple of particles.
 /** \note These do not ref counting currently. SWIG prevents
@@ -26,114 +31,66 @@ IMP_BEGIN_NAMESPACE
     \note ParticleTuple objects are ordered.
 */
 template <unsigned int D>
-class ParticleTuple
-#if !defined(SWIG) && !defined(IMP_DOXYGEN)
-: public boost::array<Particle*, D>
-#endif
+class ParticleTuple:
+  public internal::ParticleTupleBase<D, CheckedWeakPointer<Particle> >
 {
-  typedef boost::array<Particle*, D> P;
-  int compare(const ParticleTuple<D> &o) const {
-    for (unsigned int i=0;i<D; ++i) {
-      if (P::operator[](i) < o[i]) return -1;
-      else if (P::operator[](i) > o[i]) return 1;
-    }
-    return 0;
-  }
 public:
-  static unsigned int get_dimension() {return D;};
+  typedef internal::ParticleTupleBase<D, CheckedWeakPointer<Particle> > P;
   ParticleTuple(){
-    for (unsigned int i=0; i< D; ++i) {P::operator[](i)=NULL;}
   }
-  ParticleTuple(Particle* x, Particle* y) {
-    IMP_USAGE_CHECK(D==2, "Need " << D << " to construct a "
-              << D << "-tuple.");
-    P::operator[](0) = x;
-    P::operator[](1) = y;
+  ParticleTuple(const RefCountedParticleTuple<D> &o): P(o){}
+  ParticleTuple(Particle* x, Particle* y):P( x,y) {
   }
-  ParticleTuple(Particle* x, Particle* y, Particle* z) {
-    IMP_USAGE_CHECK(D==3, "Need " << D << " to construct a "
-              << D << "-tuple.");
-    P::operator[](0) = x;
-    P::operator[](1) = y;
-    P::operator[](2) = z;
+  ParticleTuple(Particle* x, Particle* y, Particle* z): P( x,y,z) {
   }
-  ParticleTuple(Particle* x0, Particle* x1, Particle* x2, Particle* x3) {
-    IMP_USAGE_CHECK(D==4, "Need " << D << " to construct a "
-              << D << "-tuple.");
-    P::operator[](0) = x0;
-    P::operator[](1) = x1;
-    P::operator[](2) = x2;
-    P::operator[](3) = x3;
+  ParticleTuple(Particle* x0, Particle* x1, Particle* x2, Particle* x3):
+    P(x0, x1, x2, x3) {
   }
-  Particle *get(unsigned int i) const {
-    return P::operator[](i);
-  }
-  IMP_HASHABLE_INLINE(ParticleTuple<D>, std::size_t seed = 0;
-               for (unsigned int i=0; i< D; ++i) {
-                 boost::hash_combine(seed,
-                                     P::operator[](i));
-               }
-               return seed;);
-  IMP_COMPARISONS(ParticleTuple<D>);
-  std::string get_name() const {
-    std::string ret="\"";
-    for (unsigned int i=0; i< D; ++i) {
-      if (i>0) {
-        ret+= "\" and \"";
-      }
-      ret+=P::operator[](i)->get_name();
-    }
-    ret+="\"";
-    return ret;
-  }
-  IMP_SHOWABLE_INLINE(ParticleTuple, {
-      out << get_name();
-    });
 #if defined(IMP_DOXYGEN) || defined(SWIG)
   Particle * operator[](unsigned int i) const;
   Particle *& operator[](unsigned int i);
+  std::size_t __hash__();
 #endif
 };
 
-#if !defined(IMP_DOXYGEN) && !defined(SWIG)
-template<unsigned int D>
-std::ostream &operator<<(std::ostream &out,
-                         const ParticleTuple<D> &d) {
-  d.show(out);
-  return out;
-}
+template <unsigned int D>
+class RefCountedParticleTuple:
+  public internal::ParticleTupleBase<D, Pointer<Particle> >
+{
+public:
+  typedef internal::ParticleTupleBase<D, Pointer<Particle> > P;
+  RefCountedParticleTuple(){
+  }
+  RefCountedParticleTuple(const ParticleTuple<D> &o): P(o){}
+  RefCountedParticleTuple(Particle* x, Particle* y):P( x,y) {
+  }
+  RefCountedParticleTuple(Particle* x, Particle* y, Particle* z): P( x,y,z) {
+  }
+  RefCountedParticleTuple(Particle* x0, Particle* x1,
+                          Particle* x2, Particle* x3):
+    P(x0, x1, x2, x3) {
+  }
+#if defined(IMP_DOXYGEN) || defined(SWIG)
+  Particle * operator[](unsigned int i) const;
+  Particle *& operator[](unsigned int i);
+  std::size_t __hash__();
 #endif
+};
 
 #if !defined(IMP_DOXYGEN)
 
-template <unsigned int D>
-struct RefCountParticleTuple {
-  template <class O>
-  static void ref(O o) {
-    for (unsigned int i=0; i< D; ++i) {
-      internal::ref(o[i]);
-    }
-  }
-  template <class O>
-  static void unref(O o) {
-    for (unsigned int i=0; i< D; ++i) {
-      internal::unref(o[i]);
-    }
-  }
-};
-
 typedef ParticleTuple<2> ParticlePair;
 IMP_VALUES(ParticlePair, ParticlePairsTemp);
-typedef VectorOfRefCounted<ParticleTuple<2>,
-                           RefCountParticleTuple<2> > ParticlePairs;
+typedef IMP::compatibility::checked_vector<RefCountedParticleTuple<2> >
+ParticlePairs;
 typedef ParticleTuple<3> ParticleTriplet;
 IMP_VALUES(ParticleTriplet, ParticleTripletsTemp);
-typedef VectorOfRefCounted<ParticleTuple<3>,
-                           RefCountParticleTuple<3> > ParticleTriplets;
+typedef IMP::compatibility::checked_vector<RefCountedParticleTuple<3> >
+ParticleTriplets;
 typedef ParticleTuple<4> ParticleQuad;
 IMP_VALUES(ParticleQuad, ParticleQuadsTemp);
-typedef VectorOfRefCounted<ParticleTuple<4>,
-                           RefCountParticleTuple<4> > ParticleQuads;
+typedef IMP::compatibility::checked_vector<RefCountedParticleTuple<4> >
+ParticleQuads;
 #endif
 
 IMP_END_NAMESPACE
