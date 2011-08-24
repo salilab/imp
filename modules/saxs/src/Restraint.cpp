128aba96587862a5fc693d074c202ed5260b456e
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-08-24 19:12:46 +0000

    convert to type explicitly
    git-svn-id: https://svn.salilab.org/imp/trunk@10712 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/saxs/src/Restraint.cpp
+++ modules/saxs/src/Restraint.cpp
@@ -31,7 +31,8 @@ Restraint::Restraint(const Particles& particles, const Profile& exp_profile,
     if(core::RigidBody::particle_is_instance(particles[i])) {
       rigid_bodies_decorators_.push_back(
                               core::RigidBody::decorate_particle(particles[i]));
-     rigid_bodies_.push_back(ref->get_refined(rigid_bodies_decorators_.back()));
+      rigid_bodies_.push_back(get_as<Particles>(
+                            ref->get_refined(rigid_bodies_decorators_.back())));
       // compute non-changing profile
       Profile rigid_part_profile;
       rigid_part_profile.calculate_profile(rigid_bodies_.back(), ff_type);
