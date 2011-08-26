f9e161738b261bc816fa6806e7b12b5685688bbf
yannick@SALILAB.ORG <yannick@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-08-26 23:12:31 +0000

    MD Mover
    git-svn-id: https://svn.salilab.org/imp/trunk@11528 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/isd/include/MolecularDynamicsMover.h
@@ -0,0 +1,52 @@
+/**
+ *  \file MolecularDynamicsMover.h
+ *  \brief A modifier which perturbs XYZs or Nuisances with a constant energy 
+ *  MD simulation.
+ *
+ *  Copyright 2007-2011 IMP Inventors. All rights reserved.
+ *
+ */
+
+#ifndef IMPISD_MOLECULAR_DYNAMICS_MOVER_H
+#define IMPISD_MOLECULAR_DYNAMICS_MOVER_H
+
+#include "isd_config.h"
+#include <IMP/core/Mover.h>
+#include <IMP/isd/MolecularDynamics.h>
+#include <IMP/core/core_macros.h>
+
+IMPISD_BEGIN_NAMESPACE
+
+//! Modify a set of continuous variables using a MD simulation.
+/** \see MonteCarlo
+ */
+class IMPISDEXPORT MolecularDynamicsMover : public core::Mover
+{
+public: 
+  
+  MolecularDynamicsMover(Model *m, unsigned nsteps=100, Float timestep=1.);
+
+  MolecularDynamics * get_md() const { return md_; }
+
+  unsigned get_number_of_md_steps() const {return nsteps_;}
+  void set_number_of_md_steps(unsigned nsteps) {nsteps_=nsteps;}
+
+
+  IMP_MOVER(MolecularDynamicsMover);
+
+private:
+  //mover-specific function calls
+  void save_coordinates();
+  void do_move(Float f);
+  void reset_coordinates();
+
+private:
+  IMP::internal::OwnerPointer<MolecularDynamics> md_;
+  double nsteps_;
+  std::vector<std::vector<double> > coordinates_;
+  std::vector<std::vector<double> > velocities_;
+};
+
+IMPISD_END_NAMESPACE
+
+#endif  /* IMPISD_MOLECULAR_DYNAMICS_MOVER_H */
