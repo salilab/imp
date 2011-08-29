6fb07b8e39c6647064b765cb6763824b33b094e2
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-08-29 21:31:36 +0000

    missed the actual files
    git-svn-id: https://svn.salilab.org/imp/trunk@10787 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/example/include/ExampleConstraint.h
@@ -0,0 +1,35 @@
+/**
+ *  \file example/ExampleConstraint.h
+ *  \brief A restraint on a list of particle pairs.
+ *
+ *  Copyright 2007-2011 IMP Inventors. All rights reserved.
+ *
+ */
+
+#ifndef IMPEXAMPLE_EXAMPLE_CONSTRAINT_H
+#define IMPEXAMPLE_EXAMPLE_CONSTRAINT_H
+
+#include "example_config.h"
+#include <IMP/SingletonScore.h>
+#include <IMP/Constraint.h>
+#include <IMP/PairContainer.h>
+#include <IMP/PairScore.h>
+
+IMPEXAMPLE_BEGIN_NAMESPACE
+
+//! A trivial constraint that just increments a counter
+/**
+*/
+class IMPEXAMPLEEXPORT ExampleConstraint : public Constraint
+{
+  Pointer<Particle> p_;
+  IntKey k_;
+public:
+  ExampleConstraint(Particle *p);
+
+  IMP_CONSTRAINT(ExampleConstraint);
+};
+
+IMPEXAMPLE_END_NAMESPACE
+
+#endif  /* IMPEXAMPLE_EXAMPLE_CONSTRAINT_H */
---
+++ modules/example/src/ExampleConstraint.cpp
@@ -0,0 +1,48 @@
+/**
+ *  \file example/ExampleConstraint.cpp
+ *  \brief Restrain a list of particle pairs.
+ *
+ *  Copyright 2007-2011 IMP Inventors. All rights reserved.
+ *
+ */
+
+#include <IMP/example/ExampleConstraint.h>
+#include <IMP/core/XYZ.h>
+
+IMPEXAMPLE_BEGIN_NAMESPACE
+
+ExampleConstraint::ExampleConstraint(Particle *p) :
+  p_(p), k_("Constraint key") {
+  if (!p_->has_attribute(k_)) {
+    p_->add_attribute(k_, 0);
+  }
+}
+
+
+void ExampleConstraint::do_update_attributes() {
+  IMP_OBJECT_LOG;
+  IMP_LOG(TERSE, "Updating example constraint with particle "
+          << p_->get_value(k_) << std::endl);
+  p_->set_value(k_, p_->get_value(k_)+1);
+}
+void ExampleConstraint::do_update_derivatives(DerivativeAccumulator *) {
+
+}
+ContainersTemp ExampleConstraint::get_input_containers() const {
+  return ContainersTemp();
+}
+ContainersTemp ExampleConstraint::get_output_containers() const {
+  return ContainersTemp();
+}
+ParticlesTemp ExampleConstraint::get_input_particles() const {
+  return ParticlesTemp(1, p_);
+}
+ParticlesTemp ExampleConstraint::get_output_particles() const {
+  return ParticlesTemp(1, p_);
+}
+void ExampleConstraint::do_show(std::ostream &out) const {
+  out << "current count= " << p_->get_value(k_) << std::endl;
+}
+
+
+IMPEXAMPLE_END_NAMESPACE
