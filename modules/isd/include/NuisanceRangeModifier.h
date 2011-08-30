9841748b4eca06a22fdf1f9e9636db990abaaa7d
yannick@SALILAB.ORG <yannick@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-08-30 22:22:29 +0000

    renamed the range modifier as it was generic
    git-svn-id: https://svn.salilab.org/imp/trunk@11537 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/isd/include/NuisanceRangeModifier.h
@@ -0,0 +1,29 @@
+/**
+ *  \file isd/NuisanceRangeModifier.h
+ *  \brief A singleton modifier which wraps an attribute into a
+ *  given range.
+ *
+ *  Copyright 2007-2011 IMP Inventors. All rights reserved.
+ */
+
+#ifndef IMPISD_NUISANCE_RANGE_MODIFIER_H
+#define IMPISD_NUISANCE_RANGE_MODIFIER_H
+
+#include "isd_config.h"
+#include <IMP/SingletonModifier.h>
+
+IMPISD_BEGIN_NAMESPACE
+
+class IMPISDEXPORT NuisanceRangeModifier: public SingletonModifier
+{
+public:
+  NuisanceRangeModifier() {};
+
+  // note, Doxygen wants a semicolon at the end of macro lines
+  IMP_SINGLETON_MODIFIER(NuisanceRangeModifier);
+};
+
+
+IMPISD_END_NAMESPACE
+
+#endif  /* IMPISD_NUISANCE_RANGE_MODIFIER_H */
---
+++ modules/isd/src/NuisanceRangeModifier.cpp
@@ -0,0 +1,47 @@
+/**
+ *  \file isd/NuisanceRangeModifier.cpp
+ *  \brief A singleton modifier which wraps an attribute into a
+ *  given range.
+ *
+ *  Copyright 2007-2011 IMP Inventors. All rights reserved.
+ *
+ */
+
+#include "IMP/isd/NuisanceRangeModifier.h"
+#include "IMP/isd/Nuisance.h"
+
+
+IMPISD_BEGIN_NAMESPACE
+
+void NuisanceRangeModifier::apply(Particle *p) const {
+  Nuisance d(p);
+  d.set_nuisance(d.get_nuisance());
+}
+
+
+/* Only the passed particle is used */
+ParticlesTemp
+NuisanceRangeModifier::get_input_particles(Particle *p) const {
+  return ParticlesTemp(1, p);
+}
+
+ParticlesTemp
+NuisanceRangeModifier::get_output_particles(Particle *p) const {
+  return ParticlesTemp(1, p);
+}
+
+ContainersTemp
+NuisanceRangeModifier::get_input_containers(Particle *) const {
+  return ContainersTemp();
+}
+
+ContainersTemp
+NuisanceRangeModifier::get_output_containers(Particle *) const {
+  return ContainersTemp();
+}
+
+void NuisanceRangeModifier::do_show(std::ostream &out) const {
+  out << "NuisanceRangeModifier " << std::endl;
+}
+
+IMPISD_END_NAMESPACE
