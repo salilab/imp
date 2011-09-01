16813e4bfb7c81b0a4dc64516ce4f60b095faf86
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-09-01 18:18:04 +0000

    clean up tests
    git-svn-id: https://svn.salilab.org/imp/trunk@10846 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/rmf/test/test_data_types.py
@@ -0,0 +1,41 @@
+import unittest
+import IMP.rmf
+import IMP.test
+from IMP.algebra import *
+
+class GenericTest(IMP.test.TestCase):
+    def _do_test_type(self, nh, k, v):
+        print k, v
+        nh.set_value(k, v)
+        vo= nh.get_value(k)
+        self.assertEqual(vo, v)
+    def _do_test_types(self, f, pccc):
+        nh= f.add_child("testn", IMP.rmf.REPRESENTATION)
+        cat= f.add_category("mine"+str(pccc))
+        g= f.get_hdf5_group()
+        ds= g.add_child_float_data_set_2d("ds"+str(pccc))
+        dsi= g.add_child_index_data_set_2d("dsi"+str(pccc))
+        lst=[(f.add_int_key, 4),
+             (f.add_float_key, 3.1415),
+             (f.add_node_id_key, nh.get_id()),
+             (f.add_node_ids_key, [nh.get_id()]),
+             (f.add_string_key, "there"),
+             (f.add_index_key, 3),
+             (f.add_float_data_set_2d_key, ds),
+             (f.add_index_data_set_2d_key, dsi),
+             (f.add_ints_key, [3,4,5,6])]
+        self.assertEqual(len(lst),len(IMP.rmf.get_data_types()))
+        for p in lst:
+            print p[0]
+            k = p[0](cat, "hi", pccc)
+            self._do_test_type(nh, k, p[1])
+    def test_data_types(self):
+        """Test that the various data types work"""
+        print "testing types"
+        name=self.get_tmp_file_name("test_data_types.rmf")
+        f= IMP.rmf.create_rmf_file(name)
+        self._do_test_types(f, False)
+        self._do_test_types(f, True)
+
+if __name__ == '__main__':
+    unittest.main()
