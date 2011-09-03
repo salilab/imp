b60deda800037ae577aeb98a2116fa950faedc4a
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-09-03 09:39:34 +0000

    use of generic modifiers has not been fixed
    git-svn-id: https://svn.salilab.org/imp/trunk@10896 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/container/src/PairsConstraint.cpp
+++ modules/container/src/PairsConstraint.cpp
@@ -122,7 +122,7 @@ ParticlesTemp PairsConstraint::get_output_particles() const {
 ScoreStates PairsConstraint::create_decomposition() const {
   ScoreStates ret;
   IMP_FOREACH_PAIR_INDEX(c_, {
-      ret.push_back(core::create_constraint(f_.get(), af_.get(),
+      ret.push_back(new core::PairConstraint(f_.get(), af_.get(),
                                IMP::internal::get_particle(get_model(),
                                                            _1)));
     });
