640478b0ce5eb6810e5986ae2dad060346104506
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-09-06 19:11:48 +0000

    add test of decomposition
    git-svn-id: https://svn.salilab.org/imp/trunk@10911 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/container/test/test_decomp.py
@@ -0,0 +1,29 @@
+import IMP
+import IMP.test
+import IMP.container
+import math
+
+
+class AllPairsContainerTest(IMP.test.TestCase):
+    """Tests for all pairs pair container"""
+
+    def test_allp(self):
+        """Checking decomposition of plural restraints"""
+        m= IMP.Model()
+        for i in range(0,10):
+            p= IMP.Particle(m)
+            IMP.core.XYZR.setup_particle(p)
+        lp= IMP.container.ListSingletonContainer(m.get_particles())
+        self.assertEqual(lp.get_ref_count(), 1)
+        apc= IMP.container.AllPairContainer(lp)
+        dps= IMP.core.SoftSpherePairScore(1)
+        r= IMP.container.PairsRestraint(dps, apc)
+        r.set_model(m)
+        dr= r.create_decomposition()
+        print dr.evaluate(False)
+        cdr= IMP.RestraintSet.get_from(dr).get_restraints()
+        del dr
+        for rc in cdr:
+            print rc.evaluate(False)
+if __name__ == '__main__':
+    IMP.test.main()
