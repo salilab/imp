4850394520aab342a56e60d8c67eb45e034a832f
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-09-14 03:59:47 +0000

    improve tests
    git-svn-id: https://svn.salilab.org/imp/trunk@11029 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/test/test_bd.py
+++ modules/atom/test/test_bd.py
@@ -2,63 +2,132 @@ import IMP
 import IMP.test
 import IMP.core
 import IMP.atom
+import sympy
+from sympy.physics.units import *
+import math
+nreps=10000
+nsteps=500
+timestep=100
+timestep_u=timestep*femto*second
+t_u= timestep_u*nsteps
+angstrom= sympy.Rational(1,10)*nano*meter
+D=.0002
+D_u= .0002*angstrom*angstrom/(femto*second)
+k=.01
+k_u=  IMP.atom.get_spring_constant_in_femto_newtons_per_angstrom(k)*femto*newton/angstrom
+f=.1
+f_u= IMP.atom.get_force_in_femto_newtons(f)*femto*newton
+kt_silly=IMP.atom.get_kb_t(273.)
+kT_u= IMP.atom.get_energy_in_femto_joules(kt_silly)*femto*joule
 
-
-
-class MCOptimizerTest(IMP.test.TestCase):
-    def _compute_moments(self, ps):
-        m= IMP.algebra.Vector3D(0,0,0)
-        for p in ps:
-            d= IMP.core.XYZ(p.get_particle())
-            m+= d.get_coordinates()
-        m=m/len(ps)
-        s= IMP.algebra.Vector3D(0,0,0)
-        for p in ps:
-            d= IMP.core.XYZ(p.get_particle())
-            diff= d.get_coordinates()-m
-            for i in range(0,3):
-                s[i]= s[i]+diff[i]*diff[i]
-        s=s/len(ps)
-        for i in range(0,3):
-            s[i]= s[i]**.5
-        return (m[0], m[1], m[2], s[0], s[1], s[2])
-    def _test_step_size(self):
-        """Testing the step size invariance of free diffusion"""
+class BDTests(IMP.test.TestCase):
+    def _setup(self):
         m= IMP.Model()
-        si=IMP.atom.SimulationParameters.setup_particle(IMP.Particle(m), 1e3)
-        si.show()
-        bd= IMP.atom.BrownianDynamics(si)
-        bd.set_model(m)
-        radius=2
-        ps= IMP.core.create_xyzr_particles(m, 1000, radius)
-        for p in ps:
-            d= IMP.atom.Diffusion.setup_particle(p.get_particle())
-            d.set_D_from_radius_in_angstroms(radius)
-            d.set_coordinates_are_optimized(True)
-            d.set_coordinates(IMP.algebra.Vector3D(0,0,0))
-        si.set_maximum_time_step_in_femtoseconds(1e4)
         IMP.set_log_level(IMP.SILENT)
-        bd.set_log_level(IMP.TERSE)
-        bd.optimize(1000)
-        print "time is " + str(si.get_current_time_in_femtoseconds())
-        moments= self._compute_moments(ps)
-        print moments
-        for p in ps:
-            d= IMP.atom.Diffusion(p.get_particle())
-            d.set_coordinates(IMP.algebra.Vector3D(0,0,0))
-        si.set_maximum_time_step_in_femtoseconds(1e7)
-        bd.optimize(1)
-        print si.get_current_time_in_femtoseconds()
-        big_moments= self._compute_moments(ps)
-        print big_moments
+        p= IMP.Particle(m)
+        xyzr= IMP.core.XYZR.setup_particle(p)
+        xyzr.set_coordinates(IMP.algebra.Vector3D(0,0,0))
+        xyzr.set_radius(1)
+        d=IMP.atom.Diffusion.setup_particle(p)
+        d.set_d(D)
+        bd= IMP.atom.BrownianDynamics(m)
+        bd.set_maximum_time_step(float(timestep))
+        xyzr.set_coordinates_are_optimized(True)
+        return (m, xyzr, d, bd)
+    def _measure(self, m, xyzr, bd):
+        ub= IMP.algebra.Vector3D(50,50,50)
+        h = IMP.statistics.Histogram3D(1, IMP.algebra.BoundingBox3D(-ub, ub))
+        for i in range(0,nreps):
+            xyzr.set_coordinates(IMP.algebra.Vector3D(0,0,0))
+            bd.optimize(nsteps)
+            h.add(xyzr.get_coordinates())
+            if i%1000==0:
+                print i, xyzr.get_coordinates()
+        print "computing"
+        mn= h.get_mean()
+        std= h.get_standard_deviation(mn)
+        print mn, std
+        return (mn, std)
+    #def _measure_x(self, m, xyzr, bd):
+    #    ub= IMP.algebra.Vector1D(50)
+    #    h = IMP.statistics.Histogram1D(.1, IMP.algebra.BoundingBox1D(-ub, ub))
+    #   for i in range(0,nreps):
+    #        xyzr.set_coordinates(IMP.algebra.Vector3D(0,0,0))
+    #        bd.optimize(nsteps)
+    #        h.add(IMP.algebra.Vector1D(xyzr.get_coordinates()[0]))
+    #        if i%1000==0:
+    #            print i, xyzr.get_coordinates()
+    #    mn= h.get_mean()
+    #    std= h.get_standard_deviation(mn)
+    ##    print mn, std
+    ##   return mn, std
+    def _get_sigma_free(self):
+        sigma= (6.0*t_u*D_u)**sympy.Rational(1,2)
+        return sigma
+    def _get_sigma_1_free(self):
+        sigma= (2.0*t_u*D_u)**sympy.Rational(1,2)
+        return sigma
+    def _get_sigma_harmonic(self):
+        delta2= (kT_u/k_u)
+        delta= delta2**sympy.Rational(1,2)
+        print "delta", delta
+        taut= 2*delta2/D_u
+        print "taut", taut.evalf()
+        tau= t_u/taut
+        print "tau", tau.evalf()
+        scale= 2*delta2
+        print "scale", scale.evalf()
+        sigma2= scale*(1-sympy.E**(-4*tau))
+        print "sigma2", sigma2.evalf()
+        sigma= sigma2**sympy.Rational(1,2)
+        print "sigma", sigma.evalf()
+        return sigma
+    def _get_sigma_limit_harmonic(self):
+        sigmass= 2*kT_u/k_u
+        print "sigma steady state 2", sigmass.evalf()
+        return sigmass**sympy.Rational(1,2)
+    def _check(self, (mn, std), (calc_mn, calc_std)):
+        print [(x.evalf()/angstrom).evalf() for x in calc_std]
+        print "mean", mn, [float((x/angstrom).evalf()) for x in calc_mn]
+        print "std", std, [float((x/angstrom).evalf()) for x in calc_std]
         for i in range(0,3):
-            print i
-            self.assertAlmostEqual(big_moments[i], moments[i],
-                                   delta=len(ps)**.5)
-        for i in range(3,6):
-            print i
-            self.assertAlmostEqual(big_moments[i], moments[i],
-                                   delta=.1*(big_moments[i]+ moments[i]))
+            self.assertAlmostEqual(mn[i], float((calc_mn[i]/angstrom).evalf()), delta=2)
+            self.assertAlmostEqual(std[i], float((calc_std[i]/angstrom).evalf()), delta=2)
+    def test_free(self):
+        """Test a brownian free diffusion"""
+        (m, xyzr, d, bd)= self._setup()
+        (mn, std)= self._measure(m, xyzr, bd)
+        print mn, std
+        sigma= self._get_sigma_1_free()
+        self._check((mn, std), ([0*angstrom, 0*angstrom, 0*angstrom], [sigma, sigma, sigma]))
+    def test_linear(self):
+        """Test a brownian linear"""
+        # from of course notes http://www.ks.uiuc.edu/~kosztin/
+        (m, xyzr, d, bd)= self._setup()
+        #print D_u, f_u, kT_u
+        #print D_u*f_u, f_u/kT_u, 1/kT_u
+        mean= -D_u*f_u*t_u/kT_u
+        print "mean", mean
+        h = IMP.core.Linear(0, f)
+        dss= IMP.core.AttributeSingletonScore(h, IMP.core.XYZ.get_xyz_keys()[0])
+        r= IMP.core.SingletonRestraint(dss, xyzr)
+        m.add_restraint(r)
+        sigma= self._get_sigma_1_free()
+        mn, std= self._measure(m, xyzr, bd)
+        self._check((mn, std), ([mean, 0*angstrom, 0*angstrom], [sigma, sigma, sigma]))
+    def test_harmonic(self):
+        """Test a brownian harmonic"""
+        # taken from chapter 13 of course notes http://www.ks.uiuc.edu/~kosztin/
+        (m, xyzr, d, bd)= self._setup()
+        sigma=self._get_sigma_harmonic()
+        sigmaf= self._get_sigma_1_free()
+        sigmass= self._get_sigma_limit_harmonic()
 
+        h = IMP.core.Harmonic(0, k)
+        dss= IMP.core.AttributeSingletonScore(h, IMP.core.XYZ.get_xyz_keys()[0])
+        r= IMP.core.SingletonRestraint(dss, xyzr)
+        m.add_restraint(r)
+        mn, std= self._measure(m, xyzr, bd)
+        self._check((mn, std), ([0*angstrom, 0*angstrom, 0*angstrom], [sigma, sigmaf, sigmaf]))
 if __name__ == '__main__':
     IMP.test.main()
