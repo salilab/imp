e8126efc3ac36d988ddfa8e0437e62c9ff30709f
cgreen@SALILAB.ORG <cgreen@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-09-27 01:14:19 +0000

    new AnchorsData constructor allows selecting subset from previous AnchorsData object
    git-svn-id: https://svn.salilab.org/imp/trunk@11116 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/multifit/include/anchors_reader.h
+++ modules/multifit/include/anchors_reader.h
@@ -22,6 +22,56 @@ class IMPMULTIFITEXPORT AnchorsData {
   AnchorsData(){}
   AnchorsData(algebra::Vector3Ds points, IntPairs edges){
     points_=points;edges_=edges;}
+  AnchorsData(AnchorsData orig,Ints these_indexes, bool keep){
+    //copy an AnchorsData object but ONLY/EXCLUDING a subset
+    //for ONLY the subset, use keep==true
+    //for EXCLUDING the subset, use keep==false
+    Ints in_subset;
+    Ints idx_in_new;
+    bool broken=false;
+    for (unsigned int op=0;op<orig.points_.size();op++) {
+      for (unsigned int np=0;np<these_indexes.size();np++) {
+        if (these_indexes[np]==op) {
+          in_subset.push_back(1);
+          broken=true;
+          if (keep) {
+            idx_in_new.push_back(points_.size());
+            points_.push_back(orig.points_[op]);
+          }
+          else idx_in_new.push_back(-1);
+          break;
+        }
+      }
+      if (broken==false) {
+        in_subset.push_back(0);
+        if (!keep) {
+          idx_in_new.push_back(points_.size());
+          points_.push_back(orig.points_[op]);
+        }
+        else idx_in_new.push_back(-1);
+      }
+      else broken=false;
+    }
+    //now do edges...
+    if (keep) {
+      for (unsigned int oe=0;oe<orig.edges_.size();oe++){
+        if (in_subset[orig.edges_[oe].first] &&
+            in_subset[orig.edges_[oe].second]) {
+          edges_.push_back(IntPair(idx_in_new[orig.edges_[oe].first],
+                                   idx_in_new[orig.edges_[oe].second]));
+        }
+      }
+    }
+    else {
+      for (unsigned int oe=0;oe<orig.edges_.size();oe++){
+        if (!in_subset[orig.edges_[oe].first] &&
+            !in_subset[orig.edges_[oe].second]) {
+          edges_.push_back(IntPair(idx_in_new[orig.edges_[oe].first],
+                                   idx_in_new[orig.edges_[oe].second]));
+        }
+      }
+    }
+  }
   int get_number_of_points() const {return points_.size();}
   int get_number_of_edges() const {return edges_.size();}
   void show(std::ostream& out=std::cout) const {
