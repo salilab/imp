4ebd88fc392cbc672fbd5fcf8507d522dcf90626
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-09-27 15:37:11 +0000

    dont require display
    git-svn-id: https://svn.salilab.org/imp/trunk@11122 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/test/test_random_ball.py
+++ modules/algebra/test/test_random_ball.py
@@ -1,6 +1,6 @@
 import IMP.test
 import IMP.algebra
-import IMP.display
+#import IMP.display
 import math
 import IMP.statistics
 
@@ -26,11 +26,11 @@ class BallTests(IMP.test.TestCase):
         """Testing the creation of a points on a sphere"""
         s2= IMP.algebra.Sphere3D(IMP.algebra.Vector3D(.75,.75,.75), .7)
         h= IMP.statistics.Histogram3D(.1, IMP.algebra.get_bounding_box(s2))
-        w= IMP.display.PymolWriter("pts2d.pym")
+        #w= IMP.display.PymolWriter("pts2d.pym")
         for i in range(0,10000):
             pt= IMP.algebra.get_random_vector_on(s2)
-            g= IMP.display.SphereGeometry(IMP.algebra.Sphere3D(pt, .01))
-            w.add_geometry(g)
+            #g= IMP.display.SphereGeometry(IMP.algebra.Sphere3D(pt, .01))
+            #w.add_geometry(g)
             h.add(pt)
         center= h.get_mean()
         std= h.get_standard_deviation(center)
