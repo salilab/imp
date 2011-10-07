4b9975ce1fb8193b98c413c5686cbaea45b42e27
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-10-07 17:43:57 +0000

    suppress warning
    git-svn-id: https://svn.salilab.org/imp/trunk@11301 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/em/src/rigid_fitting.cpp
+++ modules/em/src/rigid_fitting.cpp
@@ -426,7 +426,7 @@ FittingSolutions compute_fitting_scores(const ParticlesTemp &ps,
 
 Float compute_fitting_score(const ParticlesTemp &ps,
                             DensityMap *em_map,
-                            FloatKey wei_key,bool local) {
+                            FloatKey wei_key,bool) {
   //create a grid that covers both the particles and the map
   algebra::BoundingBox3D em_bb=
     get_bounding_box(em_map,0.);
