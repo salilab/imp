6c89f50ea985b387976a85cf9a2d35aefd8d2c54
cgreen@SALILAB.ORG <cgreen@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-10-21 19:30:27 +0000

    adding function to calculate envelope penetration for multiple transformations
    git-svn-id: https://svn.salilab.org/imp/trunk@11695 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/em/include/envelope_penetration.h
+++ modules/em/include/envelope_penetration.h
@@ -11,6 +11,7 @@
 #include "em_config.h"
 #include "DensityMap.h"
 #include <IMP/Particle.h>
+
 IMPEM_BEGIN_NAMESPACE
 //! Get the number of particles that are outside of the density
 /**
@@ -20,6 +21,13 @@ IMPEMEXPORT long get_number_of_particles_outside_of_the_density(
      DensityMap *dmap,const Particles &ps,
      const IMP::algebra::Transformation3D &t=
          IMP::algebra::get_identity_transformation_3d(),float thr=0.0);
+//! Get numbers of particles (mult transforms) that are outside the density
+/**
+/note the function assumes that all of the particles have XYZ coordinates
+ */
+IMPEMEXPORT Ints get_numbers_of_particles_outside_of_the_density(
+        DensityMap *dmap,const Particles &ps,
+        const IMP::algebra::Transformation3Ds &transformations,float thr=0.0);
 //! Get the number of density voxels that are not covered by particles
 /**
 /note the function assumes that all of the particles have XYZ coordinates
