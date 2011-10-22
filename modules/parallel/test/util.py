574acc41b41500c9307449f3a2a1285248fe0ef1
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-10-22 18:54:48 +0000

    Don't try to unlink files on Windows, to avoid sharing violations.
    git-svn-id: https://svn.salilab.org/imp/trunk@11709 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/parallel/test/util.py
+++ modules/parallel/test/util.py
@@ -9,17 +9,9 @@ class Manager(IMP.parallel.Manager):
     heartbeat_timeout = 5.0
 
 # On Windows sytems, we cannot delete a file if another process has it open.
-# This leads to occasional failures due to a race between the master and the
-# slave. Work around this by catching the exception and retrying.
 if sys.platform == 'win32':
     def unlink(filename):
-        for i in range(10):
-            try:
-                os.unlink(filename)
-                return
-            except WindowsError:
-                time.sleep(0.2)
-        os.unlink(filename)
+        pass
 
 else:
     def unlink(filename):
