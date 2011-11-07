aef104d716efa2d3a879f65ba0f82c4170e41963
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-11-07 22:02:45 +0000

    - Remove hard-coded destdir and version. - Bundle needed 3rd-party libraries (e.g. Boost, GSL, HDF5). - Make sure we exit if an error is encountered. - Check for references to libraries that aren't included in the package. - Add some "progress report" feedback.
    git-svn-id: https://svn.salilab.org/imp/trunk@11751 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- tools/mac/make-package.sh
+++ tools/mac/make-package.sh
@@ -1,12 +1,25 @@
 #!/bin/sh
 
-# Script to generate a simple IMP installer
+# Script to generate a simple IMP installer for Mac
 #
 # First build and install IMP itself with something like
+# scons -j3 destdir=<destdir> prefix=/usr/local install
 #
-# scons -j3 destdir=/tmp/imp-1.0 prefix=/usr/local \
-#           includepath=/opt/local/include modeller=/Library/modeller-9v7/ \
-#           install
+# Then run this script with something like
+# tools/mac/make-package.sh <destdir> <IMP version>
+#
+# e.g.
+# scons -j3 destdir=/tmp/impinstall prefix=/usr/local install
+# tools/mac/make-package.sh /tmp/impinstall 1.0
+
+
+if [ $# -ne 2 ]; then
+  echo "Usage: $0 <destdir> <IMP version>"
+  exit 1
+fi
+
+DESTDIR=$1
+VER=$2
 
 TARGET_OSX_VER=`sw_vers -productVersion | cut -f 1-2 -d.`
 case ${TARGET_OSX_VER} in
@@ -22,48 +35,132 @@ case ${TARGET_OSX_VER} in
     ;;
 esac
 
-VER=1.0
-DESTDIR=/tmp/imp-${VER}
-SOURCE=/Users/ben/imp-${VER}
 PREFIX=/usr/local
 
 # Make sure we can find the rest of our input files
-MAC_TOOL_DIR=`dirname $0`
-if [ ! -f ${MAC_TOOL_DIR}/Info.plist.in -o \
-     ! -f ${MAC_TOOL_DIR}/Description.plist.in ];
+MAC_TOOL_DIR=`dirname "$0"`
+# Make absolute path
+MAC_TOOL_DIR=`cd "${MAC_TOOL_DIR}" && pwd`
+if [ ! -f "${MAC_TOOL_DIR}/Info.plist.in" -o \
+     ! -f "${MAC_TOOL_DIR}/Description.plist.in" ]; then
   echo "Could not find plist files in script directory"
   exit 1
 fi
 
-mkdir -p ${DESTDIR}/Library/Python/${PYTHON}/site-packages/
+# Make sure that DESTDIR/PREFIX points to an IMP installation
+# (libraries and binaries)
+if [ ! -f ${DESTDIR}/${PREFIX}/lib/libimp.dylib ]; then
+  echo "Could not find IMP libraries in ${DESTDIR}/${PREFIX}/lib/"
+  exit 1
+fi
+
+if [ ! -f ${DESTDIR}/${PREFIX}/bin/foxs ]; then
+  echo "Could not find foxs in ${DESTDIR}/${PREFIX}/bin/"
+  exit 1
+fi
+
+echo "Making IMP.pth to add IMP Python modules to the Python path..."
+mkdir -p ${DESTDIR}/Library/Python/${PYTHON}/site-packages/ || exit 1
 echo "${PREFIX}/lib/python${PYTHON}/site-packages/" > ${DESTDIR}/Library/Python/${PYTHON}/site-packages/IMP.pth
-cd ${DESTDIR}${PREFIX}/lib && for lib in *.dylib; do
-  # Remove DESTDIR from shared library id
-  install_name_tool -id ${PREFIX}/lib/$lib $lib
+
+# Determine current library install name path
+LIBNAMEPATH=$( dirname `otool -L ${DESTDIR}/${PREFIX}/lib/libimp.dylib |grep libimp_compat|cut -d\( -f 1` )
+
+echo "Setting library name paths and IDs..."
+cd ${DESTDIR}/${PREFIX}/lib
+for lib in *.dylib; do
+  # Make sure library is correct
+  install_name_tool -id ${PREFIX}/lib/$lib $lib || exit 1
+
+  # Make sure install name paths of dependent libraries are correct
   for dep in *.dylib; do
-    install_name_tool -change ${DESTDIR}${PREFIX}/lib/$dep \
-                              ${PREFIX}/lib/$dep $lib
+    install_name_tool -change ${LIBNAMEPATH}/$dep \
+                              ${PREFIX}/lib/$dep $lib || exit 1
   done
-  # Remove build directory from Python extension library id
   for py in python*/site-packages/*.so; do
-    install_name_tool -change ${SOURCE}/build/lib/$lib \
-                              ${PREFIX}/lib/$lib $py
+    install_name_tool -change ${LIBNAMEPATH}/$lib \
+                              ${PREFIX}/lib/$lib $py || exit 1
+  done
+
+  # Update library name paths in IMP binaries
+  for bin in ../bin/*; do
+    install_name_tool -change ${LIBNAMEPATH}/$lib \
+                              ${PREFIX}/lib/$lib $bin || exit 1
   done
 done
+
+# Bundle non-standard library dependencies (e.g. boost) and make IMP libraries
+# and binaries point to them
+echo "Bundling non-standard library dependencies (e.g. Boost, GSL, HDF5)..."
+BUNDLED_LIBS="/opt/local/lib/libboost_system-mt.dylib \
+              /opt/local/lib/libboost_filesystem-mt.dylib \
+              /opt/local/lib/libboost_program_options-mt.dylib \
+              /opt/local/lib/libboost_thread-mt.dylib \
+              /opt/local/lib/libfftw3.3.dylib \
+              /opt/local/lib/libgsl.0.dylib \
+              /opt/local/lib/libgslcblas.0.dylib \
+              /opt/local/lib/libhdf5.6.dylib"
+BUNDLED_LIB_DIR="${PREFIX}/lib/imp-3rd-party"
+mkdir -p ${DESTDIR}/${BUNDLED_LIB_DIR} || exit 1
+
+for lib in ${BUNDLED_LIBS}; do
+  # Copy bundled library and update its id
+  cp ${lib} ${DESTDIR}/${BUNDLED_LIB_DIR} || exit 1
+  base=`basename $lib`
+  install_name_tool -id ${BUNDLED_LIB_DIR}/$base \
+                        ${DESTDIR}/${BUNDLED_LIB_DIR}/$base || exit 1
+
+  # Make sure any references in the bundled lib to other bundled libs
+  # are updated
+  for dep in ${BUNDLED_LIBS}; do
+    depbase=`basename $dep`
+    install_name_tool -change ${dep} ${BUNDLED_LIB_DIR}/$depbase \
+                              ${DESTDIR}/${BUNDLED_LIB_DIR}/$base || exit 1
+  done
+
+  # Make sure all IMP libraries and binaries point to the bundled lib
+  for user in *.dylib ../bin/* python*/site-packages/*.so; do
+    install_name_tool -change ${lib} ${BUNDLED_LIB_DIR}/$base ${user} || exit 1
+  done
+done
+
+
+# Make sure we don't link against any non-standard libraries that aren't bundled
+otool -L *.dylib ../bin/* python*/site-packages/*.so |grep -Ev '/usr/(local/)?lib|:'|sort -u > /tmp/non-standard.$$
+if [ -s /tmp/non-standard.$$ ]; then
+  echo "The following non-standard libraries are linked against, and were"
+  echo "not bundled:"
+  echo
+  cat /tmp/non-standard.$$
+  echo
+  rm -f /tmp/non-standard.$$
+  exit 1
+else
+  rm -f /tmp/non-standard.$$
+fi
 
 cd /tmp
 
 # Substitute version number in plist files
 for fname in Info Description; do
   sed -e "s/@VERSION@/${VER}/g" \
-      < ${MAC_TOOL_DIR}/${fname}.plist.in > ${fname}.plist
+      < "${MAC_TOOL_DIR}/${fname}.plist.in" > ${fname}.plist.$$
 done
 
-mkdir imp-${VER}-package
+echo "Making IMP installer package..."
+mkdir imp-${VER}-package || exit 1
 /Developer/Applications/Utilities/PackageMaker.app/Contents/MacOS/PackageMaker \
-       -build -i Info.plist \
-       -d Description.plist -ds \
-       -p "imp-${VER}-package/IMP ${VER} ${TARGET_OSX_VER}.pkg" -f ${DESTDIR}
+       -build -i Info.plist.$$ \
+       -d Description.plist.$$ -ds \
+       -p "imp-${VER}-package/IMP ${VER} ${TARGET_OSX_VER}.pkg" -f ${DESTDIR} \
+       || exit 1
+rm -f Info.plist.$$ Description.plist.$$
+
+echo "Making disk image (.dmg)..."
 hdiutil create -fs HFS+ -volname "IMP ${VER} for OS X ${TARGET_OSX_VER}" \
-               -srcfolder imp-${VER}-package IMP-${VER}-${TARGET_OSX_VER}.dmg
-hdiutil internet-enable -yes IMP-${VER}-${TARGET_OSX_VER}.dmg
+               -srcfolder imp-${VER}-package IMP-${VER}-${TARGET_OSX_VER}.dmg \
+        || exit 1
+hdiutil internet-enable -yes IMP-${VER}-${TARGET_OSX_VER}.dmg || exit 1
+
+rm -rf imp-${VER}-package
+echo "Disk image generated as /tmp/IMP-${VER}-${TARGET_OSX_VER}.dmg"
