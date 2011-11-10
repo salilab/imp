57d4945615edda067677a258ea3fea19f6e1ec35
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-11-10 17:18:03 +0000

    add files rejected by precommit hook
    git-svn-id: https://svn.salilab.org/imp/trunk@11763 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/librmf/src/NodeTupleHandle.cpp
@@ -0,0 +1,24 @@
+/**
+ *  \file RMF/Category.h
+ *  \brief Handle read/write of Model data from/to files.
+ *
+ *  Copyright 2007-2011 IMP Inventors. All rights reserved.
+ *
+ */
+
+#include <RMF/NodeTupleHandle.h>
+#include <boost/tuple/tuple.hpp>
+#include <RMF/Category.h>
+#include <RMF/RootHandle.h>
+
+namespace RMF {
+
+std::string get_tuple_type_name(NodeTupleType t) {
+  switch (t) {
+  case BOND:
+    return "bond";
+  case CUSTOM_TUPLE:
+    return "custom";
+  }
+}
+} /* namespace RMF */
