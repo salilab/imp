bf3ff410559bbba5882bf792b8e696a95879c34f
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-11-12 17:31:10 +0000

    If an error occurred, terminate the script.
    git-svn-id: https://svn.salilab.org/imp/trunk@11778 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- tools/w32/make-package.sh
+++ tools/w32/make-package.sh
@@ -19,35 +19,35 @@ VER=$1
 ROOT=w32-inst
 
 # Put things in more w32-like arrangement
-mv ${ROOT}/usr/include ${ROOT}/usr/lib ${ROOT}
-mv ${ROOT}/usr/bin/* ${ROOT}/lib
-rmdir ${ROOT}/usr/bin
+mv ${ROOT}/usr/include ${ROOT}/usr/lib ${ROOT} || exit 1
+mv ${ROOT}/usr/bin/* ${ROOT}/lib || exit 1
+rmdir ${ROOT}/usr/bin || exit 1
 
-mv ${ROOT}/usr/share/imp ${ROOT}/data
-mv ${ROOT}/usr/share/doc/imp/examples ${ROOT}
+mv ${ROOT}/usr/share/imp ${ROOT}/data || exit 1
+mv ${ROOT}/usr/share/doc/imp/examples ${ROOT} || exit 1
 
-rmdir ${ROOT}/usr/share/doc/imp
-rmdir ${ROOT}/usr/share/doc
-rmdir ${ROOT}/usr/share
-rmdir ${ROOT}/usr
+rmdir ${ROOT}/usr/share/doc/imp || exit 1
+rmdir ${ROOT}/usr/share/doc || exit 1
+rmdir ${ROOT}/usr/share || exit 1
+rmdir ${ROOT}/usr || exit 1
 
-mv ${ROOT}/lib/python2.6/site-packages/* ${ROOT}/lib
-rmdir ${ROOT}/lib/python2.6/site-packages
-rmdir ${ROOT}/lib/python2.6
+mv ${ROOT}/lib/python2.6/site-packages/* ${ROOT}/lib || exit 1
+rmdir ${ROOT}/lib/python2.6/site-packages || exit 1
+rmdir ${ROOT}/lib/python2.6 || exit 1
 
 # Add redist MSVC runtime DLLs
 DLLSRC=/usr/lib/w32comp/windows/system
-cp ${DLLSRC}/msvc*100.dll ${ROOT}/lib
+cp ${DLLSRC}/msvc*100.dll ${ROOT}/lib || exit 1
 
 # Add other DLL dependencies
-cp ${DLLSRC}/hdf5dll.dll ${DLLSRC}/libgsl.dll ${ROOT}/lib
-cp ${DLLSRC}/boost_filesystem-vc100-mt-1_44.dll ${ROOT}/lib
-cp ${DLLSRC}/boost_system-vc100-mt-1_44.dll ${ROOT}/lib
+cp ${DLLSRC}/hdf5dll.dll ${DLLSRC}/libgsl.dll ${ROOT}/lib || exit 1
+cp ${DLLSRC}/boost_filesystem-vc100-mt-1_44.dll ${ROOT}/lib || exit 1
+cp ${DLLSRC}/boost_system-vc100-mt-1_44.dll ${ROOT}/lib || exit 1
 
 # todo: check all installed DLLs for dependencies, to make sure we didn't
 # miss any
 
-tools/w32/gen-w32instlist w32-inst > w32files.tmp
-sed -e '/\.pyc"$/d' < w32files.tmp > w32files.install
-tac w32files.tmp | sed -e 's/File "w32-inst\\/Delete "$INSTDIR\\/' -e 's/^SetOutPath/RMDir/' > w32files.uninstall
-makensis -DVERSION=${VER} -NOCD tools/w32/w32-install.nsi 
+tools/w32/gen-w32instlist w32-inst > w32files.tmp || exit 1
+sed -e '/\.pyc"$/d' < w32files.tmp > w32files.install || exit 1
+tac w32files.tmp | sed -e 's/File "w32-inst\\/Delete "$INSTDIR\\/' -e 's/^SetOutPath/RMDir/' > w32files.uninstall || exit 1
+makensis -DVERSION=${VER} -NOCD tools/w32/w32-install.nsi || exit 1
