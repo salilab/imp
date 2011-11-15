6770e6c65a6280fa7a4417c7cb773258ab09b951
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-11-15 01:12:36 +0000

    add metrics on vectors to allow paramaterization of various domino clustering things
    git-svn-id: https://svn.salilab.org/imp/trunk@11784 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/algebra/src/vector_metrics.cpp
@@ -0,0 +1,19 @@
+/**
+ *  \file vector_generators.cpp
+ *  \brief Support for rigid bodies.
+ *
+ *  Copyright 2007-2011 IMP Inventors. All rights reserved.
+ *
+ */
+
+#include <IMP/algebra/vector_metrics.h>
+
+IMPALGEBRA_BEGIN_NAMESPACE
+
+VectorKDMetric::VectorKDMetric(std::string name): Object(name){}
+
+EuclideanVectorKDMetric::EuclideanVectorKDMetric(std::string name):
+    VectorKDMetric(name){}
+MaxVectorKDMetric::MaxVectorKDMetric(std::string name):
+    VectorKDMetric(name){}
+IMPALGEBRA_END_NAMESPACE
