00e0ab68cc8ff27d73b72cea23ce637cb5618ced
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-11-16 21:56:00 +0000

    clean up printing
    git-svn-id: https://svn.salilab.org/imp/trunk@11790 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/kernel/src/Particle.cpp
+++ modules/kernel/src/Particle.cpp
@@ -31,6 +31,66 @@ Particle::Particle(Model *m):
   m->add_particle_internal(this, true);
 }
 
+namespace {
+struct IdentityValue {
+  template <class T>
+  const T &operator()(const T &t) const {
+    return t;
+  }
+};
+struct NameValue {
+  Model *m_;
+  NameValue(Model *m): m_(m){}
+  template <class T>
+  std::string operator()(const T &t) const {
+    std::ostringstream oss;
+    oss << "\"" << m_->get_particle(t)->get_name() << "\"";
+    return oss.str();
+  }
+};
+struct DirectNameValue {
+  template <class T>
+  std::string operator()(const T &t) const {
+    std::ostringstream oss;
+    oss << "\"" << t->get_name() << "\"";
+    return oss.str();
+  }
+};
+template <class V>
+struct SizeValue {
+  V v_;
+  SizeValue(V v=V()): v_(v){}
+  template <class T>
+  std::string operator()(const T &t) const {
+    std::ostringstream oss;
+    if (t.size() < 5) {
+      oss << "[";
+      for (unsigned int i=0; i< t.size(); ++i) {
+        if (i > 0) oss << ", ";
+        oss << v_(t[i]);
+      }
+    } else {
+      oss << "length " << t.size();
+    }
+    return oss.str();
+  }
+};
+template <class Keys, class GetValue>
+void show_attributes(Model *m, const Keys &fks, ParticleIndex id,
+                     std::string name, GetValue v,
+                     internal::PrefixStream &preout) {
+  if (!fks.empty()) {
+    preout << name << " attributes:" << std::endl;
+    preout.set_prefix("  ");
+    for (typename Keys::const_iterator it= fks.begin(); it != fks.end(); ++it) {
+      preout << *it << ": " << v(m->get_attribute(*it, id, false))
+             << std::endl;
+    }
+    preout.set_prefix("");
+  }
+}
+}
+
 void Particle::do_show(std::ostream& out) const
 {
   internal::PrefixStream preout(&out);
@@ -39,68 +99,45 @@ void Particle::do_show(std::ostream& out) const
   preout << std::endl;
 
   if (get_has_model()) {
-    preout << "float attributes:" << std::endl;
-    preout.set_prefix("  ");
-    FloatKeys fks= get_float_keys();
-    for (FloatKeys::const_iterator it= fks.begin(); it != fks.end(); ++it) {
-      FloatKey k =*it;
-      preout << k << ": " << get_model()->get_attribute(k, id_, false);
-      preout << " ("
-             << get_model()->get_attribute(k, id_, false) << ") ";
-      preout << (get_is_optimized(k)?" (optimized)":"");
-      preout << std::endl;
-    }
-
     {
-      preout.set_prefix("");
-      out << "int attributes:" << std::endl;
-      preout.set_prefix("  ");
-      IntKeys fks= get_int_keys();
-      for (IntKeys::const_iterator it= fks.begin(); it != fks.end(); ++it) {
-        preout << *it << ": " << get_model()->get_attribute(*it, id_, false)
-               << std::endl;
+      FloatKeys fks= get_float_keys();
+      if (!fks.empty()) {
+        preout << "float attributes:" << std::endl;
+        preout.set_prefix("  ");
+        for (FloatKeys::const_iterator it= fks.begin(); it != fks.end(); ++it) {
+          FloatKey k =*it;
+          preout << k << ": " << get_model()->get_attribute(k, id_, false);
+          preout << " ("
+                 << get_model()->get_attribute(k, id_, false) << ") ";
+          preout << (get_is_optimized(k)?" (optimized)":"");
+          preout << std::endl;
+        }
+        preout.set_prefix("");
       }
     }
-
-    {
-      preout.set_prefix("");
-      out << "string attributes:" << std::endl;
-      preout.set_prefix("  ");
-      StringKeys fks= get_string_keys();
-      for (StringKeys::const_iterator it= fks.begin();
-           it != fks.end(); ++it) {
-        preout << *it << ": " << get_model()->get_attribute(*it, id_, false)
-               << std::endl;
-      }
-    }
-    {
-      preout.set_prefix("");
-      out << "particle attributes:" << std::endl;
-      preout.set_prefix("  ");
-      ParticleKeys fks
-        = get_model()->ParticleAttributeTable::get_attribute_keys(id_);
-      for (ParticleKeys::const_iterator it= fks.begin();
-           it != fks.end(); ++it) {
-        preout << *it << ": "
-               << get_model()->get_particle(get_model()->get_attribute(*it, id_,
-                                                                       false))
-            ->get_name()
-               << std::endl;
-      }
-    }
-    {
-      preout.set_prefix("");
-      out << "particles attributes:" << std::endl;
-      preout.set_prefix("  ");
-      ParticlesKeys fks
-        =  get_model()->ParticlesAttributeTable::get_attribute_keys(id_);
-      for (ParticlesKeys::const_iterator it= fks.begin();
-           it != fks.end(); ++it) {
-        preout << *it << ": "
-               << get_model()->get_attribute(*it, id_, false).size()
-               << std::endl;
-      }
-    }
+    show_attributes(get_model(), get_int_keys(), id_, "int", IdentityValue(),
+                    preout);
+    show_attributes(get_model(), get_string_keys(), id_, "string",
+                    IdentityValue(), preout);
+    show_attributes(get_model(),
+                    get_model()->ParticleAttributeTable::get_attribute_keys(id_)
+                    , id_, "particle", NameValue(get_model()), preout);
+    show_attributes(get_model(),
+                    get_model()->ObjectAttributeTable::get_attribute_keys(id_)
+                    , id_, "object", DirectNameValue(), preout);
+    show_attributes(get_model(),
+                    get_model()->ParticlesAttributeTable
+                    ::get_attribute_keys(id_),
+                    id_, "particles",
+                    SizeValue<NameValue>(NameValue(get_model())), preout);
+    show_attributes(get_model(),
+                    get_model()->ObjectsAttributeTable
+                    ::get_attribute_keys(id_),
+                    id_, "objects", SizeValue<DirectNameValue>(), preout);
+    show_attributes(get_model(),
+                    get_model()->IntsAttributeTable
+                    ::get_attribute_keys(id_),
+                    id_, "ints", SizeValue<IdentityValue>(), preout);
   }
 }
 
