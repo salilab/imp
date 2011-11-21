93761fbe5dc817cc1eb1197f2054c6528dd4532f
etjioe@SALILAB.ORG <etjioe@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-11-21 19:23:30 +0000

    Improve the test.
    git-svn-id: https://svn.salilab.org/imp/trunk@11817 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/multifit/test/test_geometric_complementarity.cpp
+++ modules/multifit/test/test_geometric_complementarity.cpp
@@ -1,4 +1,8 @@
 /**
+ *   This test uses the score to compute an approximation of a sphere
+ *   intersection volume and compares it with the 'exact' volume.
+ *   Print a nice table results and errors in the end.
+ *
  *   Copyright 2007-2011 IMP Inventors. All rights reserved
  */
 #include <vector>
@@ -9,9 +13,20 @@
 #include "IMP/algebra/Transformation3D.h"
 #include "IMP/Particle.h"
 #include "IMP/Model.h"
+#include "IMP/atom/Atom.h"
 #include "IMP/core/XYZR.h"
 #include "IMP/multifit/internal/GeometricComplementarity.h"
 
+
+
+double sphere_intersection(double radius, double dist)
+{
+  double d = 2*radius - dist;
+  if ( d > 0 )
+    return M_PI*(4*radius + dist)*d*d/12;
+  else
+    return 0;
+}
 
 
 IMP::ParticlesTemp create_balls(IMP::Model *m,
@@ -25,41 +40,76 @@ IMP::ParticlesTemp create_balls(IMP::Model *m,
     IMP::core::XYZR d = IMP::core::XYZR::setup_particle(p);
     d.set_radius(radii[i]);
     d.set_coordinates(coords[i]);
+    IMP::atom::Atom a = IMP::atom::Atom::setup_particle(p, IMP::atom::AT_H);
     res.push_back(p);
   }
   return res;
 }
 
 
+double approx_sphere_intersection(double radius, double dist)
+{
+  std::vector<double> radii;
+  radii.push_back(radius);
+  std::vector<IMP::algebra::Vector3D> coords;
+  coords.push_back(IMP::algebra::Vector3D(0, 0, 0));
+  IMP_NEW(IMP::Model, m, ());
+  IMP::ParticlesTemp particles0 = create_balls(m, radii, coords);
+  IMP::multifit::internal::ComplementarityGridParameters cgpar;
+  cgpar.voxel_size = 1;
+  cgpar.interior_thickness = radius;
+  IMP::algebra::DenseGrid3D<float> map0 =
+    IMP::multifit::internal::get_complentarity_grid(particles0, cgpar);
+  IMP::algebra::Transformation3D tr(IMP::algebra::Vector3D(dist, 0, 0));
+  IMP::FloatPair score =
+    IMP::multifit::internal::get_penetration_and_complementarity_scores(
+      map0, map0, tr);
+  return score.first;
+}
+
+
+double Examples[][2] = { {10, 2}, {10, 3}, {10, 4}, {10, 5}, {10, 6},
+  {10, 7}, {10, 8}, {10, 9}, {10, 10}, {10, 11}, {10, 12}, {10, 15},
+  {10, 30} };
+
+
+template <typename T, unsigned int N>
+char (&static_size_of_array( T (&)[N] ))[N];
+
+#define compile_time_array_size(x) (sizeof(static_size_of_array((x))))
+
+
+struct Result
+{
+  double exact;
+  double approx;
+  double pct_error;
+};
+
 
 int main()
 {
   IMP::set_log_level(IMP::VERBOSE);
-  std::vector<double> radii;
-  radii.push_back(7);
-  std::vector<IMP::algebra::Vector3D> coords;
-  coords.push_back(IMP::algebra::Vector3D(0, 0, 0));
-  IMP::Model *m = new IMP::Model;
-  IMP::ParticlesTemp particles0 = create_balls(m, radii, coords);
-  IMP::multifit::internal::ComplementarityGridParameters cgpar;
-  cgpar.voxel_size = 1;
-  cgpar.interior_thickness = 2;
-  IMP::algebra::DenseGrid3D<float> map0 =
-    IMP::multifit::internal::get_complentarity_grid(particles0, cgpar);
-  IMP_INTERNAL_CHECK(
-      map0.get_value_always(IMP::algebra::Vector3D(0, 0, 0)) == 7.0,
-      "Improper voxel value");
-  IMP::algebra::Transformation3D tr(IMP::algebra::Vector3D(30, 0, 0));
-  IMP::FloatPair score =
-    IMP::multifit::internal::get_penetration_and_complementarity_scores(
-      map0, map0, tr);
-  IMP_INTERNAL_CHECK(score.first == 0 && score.second == 0, "Bad score");
-  tr = IMP::algebra::Vector3D(29, 0, 0);
-  IMP::multifit::internal::ComplementarityParameters cmp_par;
-  cmp_par.maximum_penetration_score = std::numeric_limits<double>::max();
-  score =
-    IMP::multifit::internal::get_penetration_and_complementarity_scores(
-      map0, map0, tr, cmp_par);
-  IMP_INTERNAL_CHECK(score.first == 80 && score.second == 0, "Bad score");
+  std::vector<Result> R;
+  for ( size_t i = 0; i < compile_time_array_size(Examples); ++i )
+  {
+    double radius = Examples[i][0];
+    double distance = Examples[i][1];
+    Result res;
+    res.exact = sphere_intersection(radius, distance);
+    res.approx = approx_sphere_intersection(radius, distance);
+    double error = res.approx > 1e-9 ?
+      std::fabs(res.exact - res.approx)/res.approx :
+      std::fabs(res.exact - res.approx);
+    res.pct_error = 100*error;
+    R.push_back(res);
+  }
+  std::cout << "Exact\tApproximate\tPct_Error" << std::endl;
+  for ( size_t i = 0; i < R.size(); ++i )
+  {
+    std::cout << R[i].exact << '\t' << R[i].approx << '\t'
+      << R[i].pct_error << std::endl;
+    IMP_INTERNAL_CHECK(R[i].approx < 9, "Error too big");
+  }
   return 0;
 }
