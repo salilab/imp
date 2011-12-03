7652463465953423aa0fb6cdc155165fe9aeffaf
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-12-03 19:06:58 +0000

    add logging to dynamic vector search
    git-svn-id: https://svn.salilab.org/imp/trunk@11923 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/src/vector_search.cpp
+++ modules/algebra/src/vector_search.cpp
@@ -18,12 +18,13 @@ DynamicNearestNeighbor3D::DynamicNearestNeighbor3D(const Vector3Ds &vs,
   coords_=vs;
   indexes_.resize(vs.size());
   for (unsigned int i=0; i< coords_.size(); ++i) {
-    set_coordinates(i, coords_[i], false);
+    set_coordinates_internal(i, coords_[i]);
   }
   audit();
 }
 
 Ints DynamicNearestNeighbor3D::get_in_ball(int id, double distance) const {
+  IMP_OBJECT_LOG;
   IMP_IF_CHECK(USAGE_AND_INTERNAL) {
     audit();
   }
@@ -32,9 +33,15 @@ Ints DynamicNearestNeighbor3D::get_in_ball(int id, double distance) const {
   bb+= distance;
   Ints ret;
   double distance2= square(distance);
-  for (Grid::VoxelConstIterator it=grid_.voxels_begin(bb);
-       it != grid_.voxels_end(bb); ++it) {
-    const Ints cur= *it;
+  IMP_LOG(VERBOSE, "Searching from " << bb << " which is "
+          << grid_.get_extended_index(bb.get_corner(0))
+          << " to " << grid_.get_extended_index(bb.get_corner(1)) << std::endl);
+  for (Grid::IndexIterator it=grid_.indexes_begin(bb);
+       it != grid_.indexes_end(bb); ++it) {
+    Grid::Index ind= *it;
+    const Ints cur= grid_[ind];
+    using base::operator<<;
+    IMP_LOG(VERBOSE, "Investigating " << ind << ": " << cur << std::endl);
     for (unsigned int j=0; j< cur.size(); ++j) {
       if (get_squared_distance(coords_[cur[j]], coords_[id]) < distance2) {
         if (cur[j]==id) continue;
@@ -45,22 +52,7 @@ Ints DynamicNearestNeighbor3D::get_in_ball(int id, double distance) const {
   return ret;
 }
 
-void DynamicNearestNeighbor3D::set_coordinates(int id, Vector3D nc,
-                                               bool audit) {
-#if IMP_BUILD < IMP_FAST
-  if (audit) {
-    this->audit();
-  }
-#endif
-  if (indexes_[id] != Index()) {
-    Ints &it= grid_[indexes_[id]];
-    IMP_INTERNAL_CHECK(std::find(it.begin(), it.end(), id) != it.end(),
-                       "Item not found in list");
-    std::remove(it.begin(), it.end(), id);
-    it.pop_back();
-    IMP_INTERNAL_CHECK(std::find(it.begin(), it.end(), id) == it.end(),
-                       "Item found in list");
-  }
+void DynamicNearestNeighbor3D::set_coordinates_internal(int id, Vector3D nc) {
   EIndex ei= grid_.get_extended_index(nc);
   coords_[id]=nc;
   Index i;
@@ -71,10 +63,29 @@ void DynamicNearestNeighbor3D::set_coordinates(int id, Vector3D nc,
     i=grid_.add_voxel(ei, Ints(1, id));
   }
   indexes_[id]=i;
+  IMP_LOG(VERBOSE, "New voxel for " << id << " at " << nc
+          << " is " << indexes_[id] << std::endl);
+}
+
+void DynamicNearestNeighbor3D::set_coordinates(int id, Vector3D nc) {
+  IMP_OBJECT_LOG;
 #if IMP_BUILD < IMP_FAST
-  if (audit) {
-    this->audit();
+  this->audit();
+#endif
+  Grid::Index ind= indexes_[id];
+  Ints &it= grid_[ind];
+  IMP_INTERNAL_CHECK(std::find(it.begin(), it.end(), id) != it.end(),
+                     "Item not found in list");
+  std::remove(it.begin(), it.end(), id);
+  it.pop_back();
+  IMP_INTERNAL_CHECK(std::find(it.begin(), it.end(), id) == it.end(),
+                     "Item found in list");
+  set_coordinates_internal(id, nc);
+  if (it.empty()) {
+    grid_.remove_voxel(ind);
   }
+#if IMP_BUILD < IMP_FAST
+  this->audit();
 #endif
 }
 
@@ -88,13 +99,18 @@ void DynamicNearestNeighbor3D::audit() const {
   using base::operator<<;
   using std::operator<<;
   for (unsigned int i=0; i< coords_.size(); ++i) {
-    if (indexes_[i] != Index()) {
-      IMP_INTERNAL_CHECK(std::find(grid_[indexes_[i]].begin(),
-                                   grid_[indexes_[i]].end(),
-                                   i) != grid_[indexes_[i]].end(),
-                         "Item " << i << " not found in list: "
-                         << grid_[indexes_[i]]);
-    }
+    IMP_INTERNAL_CHECK(std::find(grid_[indexes_[i]].begin(),
+                                 grid_[indexes_[i]].end(),
+                                 i) != grid_[indexes_[i]].end(),
+                       "Item " << i << " not found in list: "
+                       << grid_[indexes_[i]]);
+    Grid::ExtendedIndex ei= grid_.get_extended_index(coords_[i]);
+    IMP_INTERNAL_CHECK(grid_.get_has_index(ei),
+                       "Voxel for " << i << " is empty.");
+    Grid::Index ind= grid_.get_index(ei);
+    IMP_INTERNAL_CHECK(ind==indexes_[i],
+                       "Indexes don't match: "
+                       << ind << " vs " << indexes_[i]);
   }
   IMP_INTERNAL_CHECK(found.size()== coords_.size(),
                      "Wrong number found: " << found
