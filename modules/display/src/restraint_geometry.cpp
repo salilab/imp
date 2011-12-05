3048d6792e137544a60bfc6116c514938a37d083
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-12-05 04:57:23 +0000

    break circular dependency
    git-svn-id: https://svn.salilab.org/imp/trunk@11925 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/display/src/restraint_geometry.cpp
+++ modules/display/src/restraint_geometry.cpp
@@ -7,14 +7,10 @@
  */
 
 #include "IMP/display/restraint_geometry.h"
-#include <IMP/core/PairRestraint.h>
-#include <IMP/core/SingletonRestraint.h>
-#include <IMP/core/ClosePairsPairScore.h>
 #include <IMP/container/PairsRestraint.h>
 #include <IMP/container/ListPairContainer.h>
 #include <IMP/container/SingletonsRestraint.h>
 #include <IMP/algebra/Segment3D.h>
-#include <IMP/core/XYZ.h>
 
 IMPDISPLAY_BEGIN_NAMESPACE
 
@@ -23,6 +19,15 @@ RestraintGeometry::RestraintGeometry(Restraint*r): Geometry(r->get_name()),
 RestraintGeometry::RestraintGeometry(Restraint*r, Model *m):
   Geometry(r->get_name()),
   r_(r), m_(m){}
+
+namespace {
+  bool get_has_coordinates(Particle *p) {
+    return p->has_attribute(FloatKey(0));
+  }
+  algebra::Vector3D get_coordinates(Particle *p) {
+    return p->get_model()->get_sphere(p->get_index()).get_center();
+  }
+}
 
 
 IMP::display::Geometries RestraintGeometry::get_components() const {
@@ -34,14 +39,12 @@ IMP::display::Geometries RestraintGeometry::get_components() const {
   if (!rs) {
     ParticlesTemp ps= r_->get_input_particles();
     for (unsigned int i=0; i < ps.size(); ++i) {
-      if (!core::XYZ::particle_is_instance(ps[i])) continue;
-      core::XYZ di(ps[i]);
+      if (!get_has_coordinates(ps[i])) continue;
       for (unsigned int j=0; j< i; ++j) {
-        if (!core::XYZ::particle_is_instance(ps[j])) continue;
-        core::XYZ dj(ps[j]);
+        if (!get_has_coordinates(ps[j])) continue;
         ret.push_back(new SegmentGeometry
-                      (algebra::Segment3D(di.get_coordinates(),
-                                          dj.get_coordinates())));
+                      (algebra::Segment3D(get_coordinates(ps[i]),
+                                          get_coordinates(ps[j]))));
       }
     }
   } else {
