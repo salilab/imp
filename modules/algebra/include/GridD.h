abc41873c9a330f5a4d1a18c796dfb52a82ccbd5
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2011-12-21 17:03:45 +0000

    add ability to get all voxels in dense grids
    git-svn-id: https://svn.salilab.org/imp/trunk@12068 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/include/GridD.h
+++ modules/algebra/include/GridD.h
@@ -16,6 +16,7 @@
 #include "internal/grid_3d.h"
 #include <boost/iterator/transform_iterator.hpp>
 #include <IMP/compatibility/map.h>
+#include <IMP/compatibility/checked_vector.h>
 
 #include <limits>
 
@@ -216,39 +217,45 @@ namespace grids {
 #if !defined(IMP_DOXYGEN)
   typedef GridIndexD<1> GridIndex1D;
   typedef ExtendedGridIndexD<1> ExtendedGridIndex1D;
-  typedef std::vector<GridIndex1D> GridIndex1Ds;
-  typedef std::vector<ExtendedGridIndex1D> ExtendedGridIndex1Ds;
+  typedef checked_vector<GridIndex1D> GridIndex1Ds;
+  typedef checked_vector<ExtendedGridIndex1D> ExtendedGridIndex1Ds;
 
 
   typedef GridIndexD<2> GridIndex2D;
   typedef ExtendedGridIndexD<2> ExtendedGridIndex2D;
-  typedef std::vector<GridIndex2D> GridIndex2Ds;
-  typedef std::vector<ExtendedGridIndex2D> ExtendedGridIndex2Ds;
+  typedef checked_vector<GridIndex2D> GridIndex2Ds;
+  typedef checked_vector<ExtendedGridIndex2D>
+  ExtendedGridIndex2Ds;
 
   typedef GridIndexD<3> GridIndex3D;
   typedef ExtendedGridIndexD<3> ExtendedGridIndex3D;
-  typedef std::vector<GridIndex3D> GridIndex3Ds;
-  typedef std::vector<ExtendedGridIndex3D> ExtendedGridIndex3Ds;
+  typedef checked_vector<GridIndex3D> GridIndex3Ds;
+  typedef checked_vector<ExtendedGridIndex3D>
+  ExtendedGridIndex3Ds;
 
   typedef GridIndexD<4> GridIndex4D;
   typedef ExtendedGridIndexD<4> ExtendedGridIndex4D;
-  typedef std::vector<GridIndex4D> GridIndex4Ds;
-  typedef std::vector<ExtendedGridIndex4D> ExtendedGridIndex4Ds;
+  typedef checked_vector<GridIndex4D> GridIndex4Ds;
+  typedef checked_vector<ExtendedGridIndex4D>
+  ExtendedGridIndex4Ds;
 
   typedef GridIndexD<5> GridIndex5D;
   typedef ExtendedGridIndexD<5> ExtendedGridIndex5D;
-  typedef std::vector<GridIndex5D> GridIndex5Ds;
-  typedef std::vector<ExtendedGridIndex5D> ExtendedGridIndex5Ds;
+  typedef checked_vector<GridIndex5D> GridIndex5Ds;
+  typedef checked_vector<ExtendedGridIndex5D>
+  ExtendedGridIndex5Ds;
 
   typedef GridIndexD<6> GridIndex6D;
   typedef ExtendedGridIndexD<6> ExtendedGridIndex6D;
-  typedef std::vector<GridIndex6D> GridIndex6Ds;
-  typedef std::vector<ExtendedGridIndex6D> ExtendedGridIndex6Ds;
+  typedef checked_vector<GridIndex6D> GridIndex6Ds;
+  typedef checked_vector<ExtendedGridIndex6D>
+  ExtendedGridIndex6Ds;
 
   typedef GridIndexD<-1> GridIndexKD;
   typedef ExtendedGridIndexD<-1> ExtendedGridIndexKD;
-  typedef std::vector<GridIndexKD> GridIndexKDs;
-  typedef std::vector<ExtendedGridIndexKD> ExtendedGridIndexKDs;
+  typedef checked_vector<GridIndexKD> GridIndexKDs;
+  typedef checked_vector<ExtendedGridIndexKD>
+  ExtendedGridIndexKDs;
 #endif
 
 
@@ -306,11 +313,12 @@ namespace grids {
       return ExtendedIndexIterator();
     }
 #endif
-    std::vector<ExtendedGridIndexD<D> >
+    checked_vector<ExtendedGridIndexD<D> >
     get_extended_indexes(const ExtendedGridIndexD<D>& lb,
                          const ExtendedGridIndexD<D>& ub) const {
-      return std::vector<ExtendedGridIndexD<D> >(extended_indexes_begin(lb, ub),
-                                                 extended_indexes_end(lb, ub));
+      return checked_vector<ExtendedGridIndexD<D> >
+        (extended_indexes_begin(lb, ub),
+         extended_indexes_end(lb, ub));
     }
   };
 
@@ -410,8 +418,8 @@ namespace grids {
                        d_);
   }
 #endif
-  std::vector<GridIndexD<D> > get_all_indexes() const {
-    std::vector<GridIndexD<D> > ret(all_indexes_begin(),
+  checked_vector<GridIndexD<D> > get_all_indexes() const {
+    checked_vector<GridIndexD<D> > ret(all_indexes_begin(),
                                     all_indexes_end());
     return ret;
   }
@@ -482,16 +490,18 @@ namespace grids {
   }
 #endif
 
-    std::vector<GridIndexD<D> > get_indexes(const ExtendedGridIndexD<D>& lb,
+    checked_vector<GridIndexD<D> >
+    get_indexes(const ExtendedGridIndexD<D>& lb,
                                     const ExtendedGridIndexD<D>& ub) const {
-    return std::vector<GridIndexD<D> >(indexes_begin(lb, ub),
+    return checked_vector<GridIndexD<D> >(indexes_begin(lb, ub),
                                        indexes_end(lb, ub));
   }
-  std::vector<ExtendedGridIndexD<D> >
+  checked_vector<ExtendedGridIndexD<D> >
   get_extended_indexes(const ExtendedGridIndexD<D>& lb,
                        const ExtendedGridIndexD<D>& ub) const {
-    return std::vector<ExtendedGridIndexD<D> >(extended_indexes_begin(lb, ub),
-                                               extended_indexes_end(lb, ub));
+    return checked_vector<ExtendedGridIndexD<D> >
+      (extended_indexes_begin(lb, ub),
+       extended_indexes_end(lb, ub));
   }
   /* @} */
 
@@ -675,6 +685,10 @@ namespace grids {
       return data_.get()+get_extent();
     }
 #endif
+    checked_vector<VT> get_all_voxels() const {
+      return checked_vector<VT>(all_voxels_begin(),
+                                              all_voxels_end());
+    }
     /** @} */
   };
 
@@ -808,8 +822,8 @@ namespace grids {
     }
 #endif
 
-    std::vector<GridIndexD<D> > get_all_indexes() const {
-      return std::vector<GridIndexD<D> >
+    checked_vector<GridIndexD<D> > get_all_indexes() const {
+      return checked_vector<GridIndexD<D> >
         (boost::make_transform_iterator(all_begin(), GetIndex()),
          boost::make_transform_iterator(all_end(), GetIndex()));
     }
@@ -860,10 +874,10 @@ namespace grids {
     }
 #endif
 
-    std::vector<GridIndexD<D> >
+    checked_vector<GridIndexD<D> >
     get_indexes(const ExtendedGridIndexD<D>& lb,
                 const ExtendedGridIndexD<D>& ub) const {
-      return std::vector<GridIndexD<D> >(indexes_begin(lb, ub),
+      return checked_vector<GridIndexD<D> >(indexes_begin(lb, ub),
                                          indexes_end(lb, ub));
     }
     /** @} */
@@ -1008,26 +1022,26 @@ namespace grids {
 IMP_OUTPUT_OPERATOR_D(DefaultEmbeddingD);
 #if !defined(IMP_DOXYGEN)
   typedef DefaultEmbeddingD<1> DefaultEmbedding1D;
-  typedef std::vector<DefaultEmbedding1D> DefaultEmbedding1Ds;
+  typedef checked_vector<DefaultEmbedding1D> DefaultEmbedding1Ds;
 
 
   typedef DefaultEmbeddingD<2> DefaultEmbedding2D;
-  typedef std::vector<DefaultEmbedding2D> DefaultEmbedding2Ds;
+  typedef checked_vector<DefaultEmbedding2D> DefaultEmbedding2Ds;
 
   typedef DefaultEmbeddingD<3> DefaultEmbedding3D;
-  typedef std::vector<DefaultEmbedding3D> DefaultEmbedding3Ds;
+  typedef checked_vector<DefaultEmbedding3D> DefaultEmbedding3Ds;
 
   typedef DefaultEmbeddingD<4> DefaultEmbedding4D;
-  typedef std::vector<DefaultEmbedding4D> DefaultEmbedding4Ds;
+  typedef checked_vector<DefaultEmbedding4D> DefaultEmbedding4Ds;
 
   typedef DefaultEmbeddingD<5> DefaultEmbedding5D;
-  typedef std::vector<DefaultEmbedding5D> DefaultEmbedding5Ds;
+  typedef checked_vector<DefaultEmbedding5D> DefaultEmbedding5Ds;
 
   typedef DefaultEmbeddingD<6> DefaultEmbedding6D;
-  typedef std::vector<DefaultEmbedding6D> DefaultEmbedding6Ds;
+  typedef checked_vector<DefaultEmbedding6D> DefaultEmbedding6Ds;
 
   typedef DefaultEmbeddingD<-1> DefaultEmbeddingKD;
-  typedef std::vector<DefaultEmbeddingKD> DefaultEmbeddingKDs;
+  typedef checked_vector<DefaultEmbeddingKD> DefaultEmbeddingKDs;
 #endif
 
 
@@ -1174,26 +1188,26 @@ IMP_OUTPUT_OPERATOR_D(LogEmbeddingD);
 
 #if !defined(IMP_DOXYGEN)
   typedef LogEmbeddingD<1> LogEmbedding1D;
-  typedef std::vector<LogEmbedding1D> LogEmbedding1Ds;
+  typedef checked_vector<LogEmbedding1D> LogEmbedding1Ds;
 
 
   typedef LogEmbeddingD<2> LogEmbedding2D;
-  typedef std::vector<LogEmbedding2D> LogEmbedding2Ds;
+  typedef checked_vector<LogEmbedding2D> LogEmbedding2Ds;
 
   typedef LogEmbeddingD<3> LogEmbedding3D;
-  typedef std::vector<LogEmbedding3D> LogEmbedding3Ds;
+  typedef checked_vector<LogEmbedding3D> LogEmbedding3Ds;
 
   typedef LogEmbeddingD<4> LogEmbedding4D;
-  typedef std::vector<LogEmbedding4D> LogEmbedding4Ds;
+  typedef checked_vector<LogEmbedding4D> LogEmbedding4Ds;
 
   typedef LogEmbeddingD<5> LogEmbedding5D;
-  typedef std::vector<LogEmbedding5D> LogEmbedding5Ds;
+  typedef checked_vector<LogEmbedding5D> LogEmbedding5Ds;
 
   typedef LogEmbeddingD<6> LogEmbedding6D;
-  typedef std::vector<LogEmbedding6D> LogEmbedding6Ds;
+  typedef checked_vector<LogEmbedding6D> LogEmbedding6Ds;
 
   typedef LogEmbeddingD<-1> LogEmbeddingKD;
-  typedef std::vector<LogEmbeddingKD> LogEmbeddingKDs;
+  typedef checked_vector<LogEmbeddingKD> LogEmbeddingKDs;
 #endif
 
 
