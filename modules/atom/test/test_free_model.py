2acd1053349a75de647e5cc3c6b68ae74e5392ff
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-01-02 15:49:49 +0000

    make the test behave better
    git-svn-id: https://svn.salilab.org/imp/trunk@12123 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/test/test_free_model.py
+++ modules/atom/test/test_free_model.py
@@ -6,6 +6,8 @@ import IMP.atom
 class DecoratorTests(IMP.test.TestCase):
     def test_bonded(self):
         """Check that deleting the model doesn't make things crash """
+        if IMP.build != "debug":
+            self.skipTest("Only tested in debug mode")
         def init_linear():
             topology = IMP.atom.CHARMMTopology(IMP.atom.get_all_atom_CHARMM_parameters())
             topology.add_sequence('IACGACKPECPVNIIQGS')
@@ -21,7 +23,7 @@ class DecoratorTests(IMP.test.TestCase):
             IMP.set_log_level(IMP.MEMORY)
             return  h
         h = init_linear()
-        print h
+        self.assertRaises(IMP.InternalException, h.show)
 
 if __name__ == '__main__':
     IMP.test.main()
