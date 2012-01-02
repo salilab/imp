a34cfc78cd45fa7de62e17e2831ef819caa29f32
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-01-02 15:50:04 +0000

    make the test pass on old systems
    git-svn-id: https://svn.salilab.org/imp/trunk@12124 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/base/test/test_objects.py
+++ modules/base/test/test_objects.py
@@ -7,7 +7,10 @@ from StringIO import StringIO
 class LogTests(IMP.test.TestCase):
     def test_log_targets(self):
         """Test getting all objects"""
-        if IMP.build!="fast":
+        if hasattr(IMP.base, "get_live_objects"):
             allobjs= IMP.base.get_live_objects()
+            for o in allobjs:
+                # the wrapping was hard to get swig to do
+                print o.get_name()
 if __name__ == '__main__':
     IMP.test.main()
