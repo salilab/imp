fb8e8b2945b535c419252544c4e551eb7d8b2928
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-01-04 02:53:15 +0000

    add support for google performance tools
    git-svn-id: https://svn.salilab.org/imp/trunk@12140 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/benchmark/src/profile.cpp
@@ -0,0 +1,31 @@
+/** \file utility.cpp Benchmarking utilties
+ *
+ * Copyright 2007-2011 IMP Inventors. All rights reserved.
+ */
+
+#include <IMP/benchmark/profile.h>
+#ifdef IMP_BENCHMARK_USE_GOOGLE_PERFTOOLS_PROFILE
+#include <google/profiler.h>
+#endif
+
+IMPBENCHMARK_BEGIN_NAMESPACE
+
+#ifdef IMP_BENCHMARK_USE_GOOGLE_PERFTOOLS_PROFILE
+namespace {
+  std::string profname="imp.prof";
+}
+
+void set_profile_name(std::string name) {
+  profname=name;
+}
+
+void set_is_profiling(bool tf) {
+  if (tf) {
+    ProfilerStart(profname.c_str());
+  } else {
+    ProfilerStop();
+  }
+}
+#endif
+
+IMPBENCHMARK_END_NAMESPACE
