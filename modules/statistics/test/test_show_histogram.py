bd018f47d09b73934c64a1687fc19abbccb8ca9e
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-01-04 03:32:01 +0000

    support 2d show
    git-svn-id: https://svn.salilab.org/imp/trunk@12149 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/statistics/test/test_show_histogram.py
+++ modules/statistics/test/test_show_histogram.py
@@ -4,13 +4,32 @@ import IMP.statistics
 import IMP.algebra
 import random
 
+def show_2d(h):
+    import numpy as np
+    import matplotlib.cm as cm
+    import matplotlib.mlab as mlab
+    import matplotlib.pyplot as plt
+    cg= h.get_counts()
+    steps=cg.get_unit_cell()
+    x = np.arange(cg.get_bounding_box().get_corner(0)[0]+.5*steps[0],
+                  cg.get_bounding_box().get_corner(1)[0]-.5*steps[0])
+    y = np.arange(cg.get_bounding_box().get_corner(0)[1]+.5*steps[1],
+                  cg.get_bounding_box().get_corner(1)[1]-.5*steps[1])
+    X, Y = np.meshgrid(x, y)
+    Z,junk= np.meshgrid(x,y)
+    for i,xi in enumerate(x):
+        for j, yj in enumerate(y):
+            Z[i][j]=cg[cg.get_nearest_index(IMP.algebra.Vector2D(xi,yj))]
+    im = plt.pcolor(X,Y, Z, cmap=cm.jet)
+    plt.colorbar(im)
+    plt.show()
 
-class HistogramTests(IMP.test.TestCase):
+class Tests(IMP.test.TestCase):
     "Test that the histogram functionality works"
     def Setup(self):
         IMP.test.TestCase.setUp(self)
-    def test_histogram_construction(self):
-        """Check that the histogramD of a gaussian is OK"""
+    def test_histogram_1d(self):
+        """Check that the histogram1D of a gaussian is OK"""
         hist = IMP.statistics.Histogram1D(5.0, IMP.algebra.BoundingBox1D(IMP.algebra.Vector1D(-100),
                                                                 IMP.algebra.Vector1D( 100)))
         sigma=10
@@ -18,9 +37,25 @@ class HistogramTests(IMP.test.TestCase):
             x=random.gauss(0, 10)
             #print x
             hist.add(IMP.algebra.Vector1D(x))
+
         print hist.get_counts()
         print hist.get_frequencies()
         IMP.statistics.show_histogram(hist)
+
+    def test_histogram_2d(self):
+        """Check that the histogram1D of a gaussian is OK"""
+        hist = IMP.statistics.Histogram2D(5.0, IMP.algebra.BoundingBox2D(IMP.algebra.Vector2D(-100, -100),
+                                                                IMP.algebra.Vector2D( 100, 100)))
+        sigma=10
+        for i in range(10000):
+
+            x=random.gauss(0, 10)
+            y=random.gauss(0, 10)
+            #print x
+            hist.add(IMP.algebra.Vector2D(x, y))
+        print hist.get_counts()
+        print hist.get_frequencies()
+        show_2d(hist)
 
 if __name__ == '__main__':
     IMP.test.main()
