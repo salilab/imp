77ed0df59cb4ecf105d90abd5140f95b56f83b02
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-01-04 02:49:39 +0000

    improve histogram plotting function
    git-svn-id: https://svn.salilab.org/imp/trunk@12130 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/statistics/include/python_only.h
+++ modules/statistics/include/python_only.h
@@ -19,8 +19,17 @@ IMPSTATISTICS_BEGIN_NAMESPACE
 /** In python, you can use matplot lib, if installed,
     to show the contents of a histogram. At the moment,
     only 1D histograms are supported.
+
+    \param[in] h The histogram to show, the plot is sized to the histograms
+                 bounding box.
+    \param[in] xscale Whether the xscale is "linear" or "log"
+    \param[in] yscale Whether the yscale is "linear" or "log"
+    \param[in] curves A list of python functions to plot on the histogram as
+    curves. The functions should take one float and return a float.
 */
-void show_historgram(Histogram1D h);
+void show_historgram(Histogram1D h, std::string xscale="linear",
+                     std::string yscale="linear",
+                     Functions curves=Functions());
 /** @} */
 #endif
 IMPSTATISTICS_END_NAMESPACE
