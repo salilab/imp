205b04c7a1e83f5943848fbf0bd22fcf3b271cca
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-01-07 22:51:42 +0000

    experiment with making vector code shorter to write in C++
    git-svn-id: https://svn.salilab.org/imp/trunk@12182 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/kernel/include/utility.h
+++ modules/kernel/include/utility.h
@@ -10,13 +10,33 @@
 
 #include "kernel_config.h"
 #include <IMP/base/utility.h>
-
 IMP_BEGIN_NAMESPACE
 #ifndef SWIG
 #ifndef IMP_DOXYGEN
 using IMP::base::cube;
 using IMP::base::square;
 using IMP::base::is_nan;
+
+template <class T>
+inline vector<T> operator+(const vector<T> &t,
+                    const vector<T> &o) {
+  vector<T> ret=t;
+  ret.insert(ret.end(), o.begin(), o.end());
+  return ret;
+}
+template <class T>
+inline vector<T> operator+=( vector<T> &t,
+                    const T &o) {
+  t.push_back(o);
+  return t;
+}
+template <class T>
+inline vector<T> operator+=( vector<T> &t,
+                             const vector<T> &o) {
+  t.insert(t.end(), o.begin(), o.end());
+  return t;
+}
+
 #endif
 using IMP::base::get_as;
 #endif
