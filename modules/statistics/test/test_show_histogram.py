a3b66dcbca20782ce3f4ac9fae0a185acdbccbb9
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-01-12 00:10:25 +0000

    Add mock module to test IMP.statistics.show_histogram function
    git-svn-id: https://svn.salilab.org/imp/trunk@12225 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/statistics/test/test_show_histogram.py
+++ modules/statistics/test/test_show_histogram.py
@@ -3,6 +3,59 @@ import IMP
 import IMP.statistics
 import IMP.algebra
 import random
+import sys
+
+
+class MockAxes(object):
+    def __init__(self, events):
+        self.events = events
+
+    def bar(self, left, height, width=0.8, bottom=None, **kwargs):
+        self.events.append('bar %d[%.1f %.1f] %d %.1f %s' \
+                           % (len(left), left[0], left[-1],
+                              len(height), width, str(kwargs)))
+
+    def set_xlim(self, left=None, right=None, emit=True, auto=False, **kw):
+        self.events.append('xlim %.1f %.1f' % (left, right))
+
+    def set_xscale(self, value, **kwargs):
+        self.events.append('xscale ' + value)
+
+    def set_yscale(self, value, **kwargs):
+        self.events.append('yscale ' + value)
+
+    def set_autoscaley_on(self, b):
+        self.events.append('set_autoscaley_on ' + str(b))
+
+class MockMatplotlib(object):
+    def __init__(self):
+        self.events = []
+        self.pyplot = MockPyplot(self.events)
+        self.mlab = MockMlab()
+
+class MockMlab(object):
+    pass
+
+class MockPyplot(object):
+    def __init__(self, events):
+        self.events = events
+
+    def figure(self):
+        self.events.append('figure')
+        return MockFigure(self.events)
+
+    def show(self):
+        self.events.append('plot.show')
+
+
+class MockFigure(object):
+    def __init__(self, events):
+        self.events = events
+
+    def add_subplot(self, *args, **kwargs):
+        self.events.append('add_subplot %s' % str(args))
+        return MockAxes(self.events)
+
 
 def show_2d(h):
     import numpy as np
@@ -33,21 +86,34 @@ class Tests(IMP.test.TestCase):
         hist = IMP.statistics.Histogram1D(5.0, IMP.algebra.BoundingBox1D(IMP.algebra.Vector1D(-100),
                                                                 IMP.algebra.Vector1D( 100)))
         sigma=10
-        for i in range(10000):
+        for i in range(1000):
             x=random.gauss(0, 10)
             #print x
             hist.add(IMP.algebra.Vector1D(x))
 
-        print hist.get_counts()
-        print hist.get_frequencies()
-        IMP.statistics.show_histogram(hist)
+        mock = MockMatplotlib()
+        sys.modules['matplotlib'] = mock
+        sys.modules['matplotlib.pyplot'] = mock.pyplot
+        sys.modules['matplotlib.mlab'] = mock.mlab
+        try:
+            IMP.statistics.show_histogram(hist)
+            self.assertEqual(mock.events,
+                 ['figure', 'add_subplot (111,)',
+                  "bar 40[-100.0 95.0] 40 5.0 {'align': 'edge'}",
+                  'xlim -100.0 100.0', 'xscale linear', 'yscale linear',
+                  'set_autoscaley_on False', 'plot.show'])
+
+        finally:
+            del sys.modules['matplotlib']
+            del sys.modules['matplotlib.pyplot']
+            del sys.modules['matplotlib.mlab']
 
     def test_histogram_2d(self):
         """Check that the histogram1D of a gaussian is OK"""
         hist = IMP.statistics.Histogram2D(5.0, IMP.algebra.BoundingBox2D(IMP.algebra.Vector2D(-100, -100),
                                                                 IMP.algebra.Vector2D( 100, 100)))
         sigma=10
-        for i in range(10000):
+        for i in range(1000):
 
             x=random.gauss(0, 10)
             y=random.gauss(0, 10)
