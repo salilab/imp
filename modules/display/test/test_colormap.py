fde920818e5fac1f533cf1487525ca1e70388a47
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-01-17 17:42:44 +0000

    write out colormap. It appears there is an issue in pymol with displaying colors rather than in our colormap code
    git-svn-id: https://svn.salilab.org/imp/trunk@12299 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/display/test/test_colormap.py
@@ -0,0 +1,20 @@
+import IMP
+import IMP.test
+import IMP.core
+import IMP.display
+import StringIO
+import re
+
+class TestBL(IMP.test.TestCase):
+    def test_5(self):
+        """Testing the jet color map"""
+        w= IMP.display.PymolWriter(self.get_tmp_file_name("jet.pym"))
+        for i in range(0,100):
+            f=float(i)/100
+            print f
+            g= IMP.display.SphereGeometry(IMP.algebra.Sphere3D(IMP.algebra.Vector3D(0,0,i), 5))
+            g.set_color(IMP.display.get_jet_color(f))
+            w.add_geometry(g)
+
+if __name__ == '__main__':
+    IMP.test.main()
