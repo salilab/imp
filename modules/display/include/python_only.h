dae4ebc80574929a87895b371831f769427cab6c
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-01-25 19:37:41 +0000

    add PivyWriter to display geometry in an interactive window from python
    git-svn-id: https://svn.salilab.org/imp/trunk@12439 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/display/include/python_only.h
@@ -0,0 +1,34 @@
+/**
+ *  \file IMP/display/python_only.h   \brief Build dependency graphs on models.
+ *
+ *  Copyright 2007-2012 IMP Inventors. All rights reserved.
+ *
+ */
+
+#ifndef IMPDISPLAY_PYTHON_ONLY_H
+#define IMPDISPLAY_PYTHON_ONLY_H
+
+#include "display_config.h"
+
+IMP_BEGIN_NAMESPACE
+
+#ifdef IMP_DOXYGEN
+/** This writer displays things to a
+    \external{http://pivy.coin3d.org/,Pivy} window.
+
+    At the moment it support SphereGeometry and CylinderGeometry.
+*/
+class PivyWriter:public Writer {
+ public:
+  PivyWriter();
+  /** Show the window and start the interactive event loop.
+      Normal python execution will resume when the window
+      is closed.
+  */
+  void show();
+};
+#endif
+
+IMP_END_NAMESPACE
+
+#endif  /* IMPDISPLAY_PYTHON_ONLY_H */
