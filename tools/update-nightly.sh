b231a12d0b42bd93b4bbd5a29a1b878f697659ca
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-01-27 23:32:31 +0000

    Update nightly build URLs.
    git-svn-id: https://svn.salilab.org/imp/trunk@12472 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ tools/update-nightly.sh
@@ -0,0 +1,36 @@
+#!/bin/sh
+
+# Update nightly build URLs.
+#
+# Should be run on the SVN server (or some other system that can access SVN
+# readwrite without a password) from a crontab, e.g.
+#
+# 5 7 * * * /cowbell1/home/ben/imp/tools/update-nightly.sh
+
+VER=SVN
+IMPSVNDIR=file:///cowbell1/svn/imp
+
+TMPDIR=/var/tmp/imp-build-$$
+
+rm -rf ${TMPDIR}
+mkdir ${TMPDIR}
+cd ${TMPDIR}
+
+IMPSVNDIR=file:///cowbell1/svn/imp
+
+REV=`cat /salilab/diva1/home/imp/nightly/build/imp-version`
+DATE=`date +'%m/%d/%Y'`
+svn co -q --ignore-externals ${IMPSVNDIR}/nightly && cd nightly
+OLDPROP=`svn propget svn:externals last`
+NEWPROP="trunk -${REV} http://svn.salilab.org/imp/trunk"
+
+# If nothing changed (or the build completely failed), don't update
+if [ "$OLDPROP" != "$NEWPROP" ]; then
+  svn propset -q svn:externals "${NEWPROP}" last \
+  && svn ci -q --username autobuild -m \
+     "Update for nightly build ${REV}, ${DATE}."
+fi
+
+# Cleanup
+cd /
+rm -rf ${TMPDIR}
