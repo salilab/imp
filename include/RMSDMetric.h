76d06e87995a583778c56c05a2527a3abe8f13a7
Massimiliano Bonomi <mbonomi@salilab.org>
2012-02-06 22:33:43 +0000

    RMSD metric

---
+++ include/RMSDMetric.h
@@ -0,0 +1,37 @@
+/**
+ *  \file RMSDMetric.h
+ *  \brief Distance RMSD Metric
+ *
+ *  Copyright 2007-2010 IMP Inventors. All rights reserved.
+ */
+
+#ifndef IMPMEMBRANE_RMSD_METRIC_H
+#define IMPMEMBRANE_RMSD_METRIC_H
+
+#include "membrane_config.h"
+#include <IMP/statistics.h>
+#include <IMP/algebra.h>
+
+IMPMEMBRANE_BEGIN_NAMESPACE
+
+/** Compute the RMSD between two sets of particles in two configurations.
+ */
+class IMPMEMBRANEEXPORT RMSDMetric: public statistics::Metric {
+
+  Particles ps_;
+  Floats weight_;
+  std::vector< algebra::Vector3Ds > coords_;
+
+  double get_rmsd(algebra::Vector3Ds v0, algebra::Vector3Ds v1) const;
+
+ public:
+  RMSDMetric(Particles ps);
+  void add_configuration(double weight=1.0);
+  Float get_weight(unsigned i);
+
+  IMP_METRIC(RMSDMetric);
+};
+
+IMPMEMBRANE_END_NAMESPACE
+
+#endif  /* IMPMEMBRANE_RMSD_METRIC_H */
