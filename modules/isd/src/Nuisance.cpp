71baeb33f57bf60a120ec180cef1cec3844d0115
yannick@SALILAB.ORG <yannick@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-02-08 15:55:12 +0000

    can set lower or upper bound of Nuisance as another Nuisance
    git-svn-id: https://svn.salilab.org/imp/trunk@12622 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/isd/src/Nuisance.cpp
+++ modules/isd/src/Nuisance.cpp
@@ -10,35 +10,98 @@
 
 IMPISD_BEGIN_NAMESPACE
 
-FloatKey Nuisance::get_nuisance_key() {
-  static FloatKey k("nuisance");
-  return k;
+Nuisance Nuisance::setup_particle(Particle *p, double nuisance) {
+    if (!Nuisance::particle_is_instance(p))
+    {
+        p->add_attribute(get_nuisance_key(), nuisance);
+    } else {
+        Nuisance(p).set_nuisance(nuisance);
+    }
+    return Nuisance(p);
 }
 
-FloatKey Nuisance::get_upper_key() {
-  static FloatKey k("upper");
-  return k;
-}
-
-FloatKey Nuisance::get_lower_key() {
-  static FloatKey k("lower");
-  return k;
-}
-
-void Nuisance::show(std::ostream &out) const {
+bool Nuisance::has_lower() const {
   Particle * p = get_particle();
-  bool haslower = p->has_attribute(get_lower_key());
-  bool hasupper = p->has_attribute(get_upper_key());
-  if (haslower) out << get_lower() << " < ";
-  out << " Nuisance = " << get_nuisance();
-  if (hasupper) out << " < " << get_upper();
+  return p->has_attribute(get_lower_key())
+           || p->has_attribute(get_lower_particle_key());
 }
 
-void Nuisance::set_upper(Float d) {
-    Particle * p = get_particle();
-    if (! p->has_attribute(get_upper_key()))
-            p->add_attribute(get_upper_key(), d);
-    p->set_value(get_upper_key(), d);
+bool Nuisance::has_upper() const {
+  Particle * p = get_particle();
+  return p->has_attribute(get_upper_key())
+           || p->has_attribute(get_upper_particle_key());
+}
+
+Float Nuisance::get_upper() const {
+    Particle *p = get_particle();
+    bool hasfloat = p->has_attribute(get_lower_key());
+    bool hasparticle = p->has_attribute(get_lower_particle_key());
+    if (hasfloat)
+    {
+        double floatval = p->get_value(get_upper_key());
+        if (hasparticle)
+        {
+            Particle *d = p->get_value(get_upper_particle_key());
+            double particleval = d->get_value(get_nuisance_key());
+            if (floatval < particleval)
+            {
+                return floatval;
+            } else {
+                return particleval;
+            }
+        } else {
+            return floatval;
+        }
+    } else if (hasparticle) {
+        Particle *d = p->get_value(get_upper_particle_key());
+        return d->get_value(get_nuisance_key());
+    } else {
+        return std::numeric_limits<double>::infinity();
+    }
+  }
+
+Float Nuisance::get_lower() const {
+    Particle *p = get_particle();
+    bool hasfloat = p->has_attribute(get_lower_key());
+    bool hasparticle = p->has_attribute(get_lower_particle_key());
+    if (hasfloat)
+    {
+        double floatval = p->get_value(get_lower_key());
+        if (hasparticle)
+        {
+            Particle *d = p->get_value(get_lower_particle_key());
+            double particleval = d->get_value(get_nuisance_key());
+            if (floatval > particleval)
+            {
+                return floatval;
+            } else {
+                return particleval;
+            }
+        } else {
+            return floatval;
+        }
+    } else if (hasparticle) {
+        Particle *d = p->get_value(get_lower_particle_key());
+        return d->get_value(get_nuisance_key());
+    } else {
+        return - std::numeric_limits<double>::infinity();
+    }
+  }
+
+void Nuisance::set_nuisance(Float d) {
+    Float d_=d;
+    Particle *p=get_particle();
+    if (has_lower())
+    {
+        Float lo = get_lower();
+        if (d < lo)  d_ = lo;
+    }
+    if (has_upper())
+    {
+        Float up = get_upper();
+        if (d > up)  d_ = up;
+    }
+    p->set_value(get_nuisance_key(), d_);
 }
 
 void Nuisance::set_lower(Float d) {
@@ -48,30 +111,58 @@ void Nuisance::set_lower(Float d) {
     p->set_value(get_lower_key(), d);
 }
 
-void Nuisance::set_nuisance(Float d) {
-    Float d_=d;
-    Particle *p=get_particle();
-    if (p->has_attribute(get_lower_key()))
-    {
-        Float lo = get_lower();
-        if (d < lo)  d_ = lo;
-    }
-    if (p->has_attribute(get_upper_key()))
-    {
-        Float up = get_upper();
-        if (d > up)  d_ = up;
-    }
-    p->set_value(get_nuisance_key(), d_);
+void Nuisance::set_lower(Particle * d) {
+    IMP_IF_CHECK(USAGE_AND_INTERNAL) { Nuisance::decorate_particle(d); }
+    Particle * p = get_particle();
+    if (! p->has_attribute(get_lower_particle_key()))
+            p->add_attribute(get_lower_particle_key(), d);
+    p->set_value(get_lower_particle_key(), d);
 }
 
-Nuisance Nuisance::setup_particle(Particle *p, double nuisance) {
-    if (!Nuisance::particle_is_instance(p))
-    {
-        p->add_attribute(get_nuisance_key(), nuisance);
-    } else {
-        Nuisance(p).set_nuisance(nuisance);
-    }
-    return Nuisance(p);
+void Nuisance::set_upper(Float d) {
+    Particle * p = get_particle();
+    if (! p->has_attribute(get_upper_key()))
+            p->add_attribute(get_upper_key(), d);
+    p->set_value(get_upper_key(), d);
+}
+
+void Nuisance::set_upper(Particle * d) {
+    IMP_IF_CHECK(USAGE_AND_INTERNAL) { Nuisance::decorate_particle(d); }
+    Particle * p = get_particle();
+    if (! p->has_attribute(get_upper_particle_key()))
+            p->add_attribute(get_upper_particle_key(), d);
+    p->set_value(get_upper_particle_key(), d);
+}
+
+FloatKey Nuisance::get_nuisance_key() {
+  static FloatKey k("nuisance");
+  return k;
+}
+
+FloatKey Nuisance::get_lower_key() {
+  static FloatKey k("lower");
+  return k;
+}
+
+FloatKey Nuisance::get_upper_key() {
+  static FloatKey k("upper");
+  return k;
+}
+
+ParticleKey Nuisance::get_lower_particle_key() {
+  static ParticleKey k("lower");
+  return k;
+}
+
+ParticleKey Nuisance::get_upper_particle_key() {
+  static ParticleKey k("upper");
+  return k;
+}
+
+void Nuisance::show(std::ostream &out) const {
+  if (has_lower()) out << get_lower() << " < ";
+  out << " Nuisance = " << get_nuisance();
+  if (has_upper()) out << " < " << get_upper();
 }
 
 IMPISD_END_NAMESPACE
