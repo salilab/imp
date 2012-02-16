94a338363143baeb1eb3e8bc51251dd3ecb7e39a
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-02-16 00:52:28 +0000

    use a frame in the rmf example decorator
    git-svn-id: https://svn.salilab.org/imp/trunk@12742 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/librmf/examples/rmf_decorator.py
+++ modules/librmf/examples/rmf_decorator.py
@@ -1,9 +1,9 @@
 import RMF
-def traverse(nh, pf, frame):
+def traverse(nh, pf):
     # check if the current node has coordinates and radius
     if pf.get_is(nh):
         # create a decorator for the current node
-        d=pf.get(nh, frame)
+        d=pf.get(nh)
         print nh.get_name(), d.get_coordinates()
     # recurse on the children
     children= nh.get_children()
@@ -11,5 +11,5 @@ def traverse(nh, pf, frame):
         traverse(c, pf)
 
 fch= RMF.open_rmf_file_read_only(RMF.get_example_path("simple.rmf"))
-pf= RMF.ParticleConstFactory(fch)
-traverse(fch.get_root_node(), pf, 0)
+pf= RMF.ParticleConstFactory(fch, 0)
+traverse(fch.get_root_node(), pf)
