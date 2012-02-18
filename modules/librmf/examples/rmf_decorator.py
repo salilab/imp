ae0ece14682a795bf1f547deeced5be8642ef9fa
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-02-18 17:46:55 +0000

    fix frame argument in rmf decorator example
    git-svn-id: https://svn.salilab.org/imp/trunk@12779 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/librmf/examples/rmf_decorator.py
+++ modules/librmf/examples/rmf_decorator.py
@@ -1,15 +1,15 @@
 import RMF
-def traverse(nh, pf):
+def traverse(nh, pf, frame):
     # check if the current node has coordinates and radius
-    if pf.get_is(nh):
+    if pf.get_is(nh, frame):
         # create a decorator for the current node
-        d=pf.get(nh)
+        d=pf.get(nh, frame)
         print nh.get_name(), d.get_coordinates()
     # recurse on the children
     children= nh.get_children()
     for c in children:
-        traverse(c, pf)
+        traverse(c, pf, frame)
 
 fch= RMF.open_rmf_file_read_only(RMF.get_example_path("simple.rmf"))
-pf= RMF.ParticleConstFactory(fch, 0)
-traverse(fch.get_root_node(), pf)
+pf= RMF.ParticleConstFactory(fch)
+traverse(fch.get_root_node(), pf, 0)
