029f6520faf741aa54a54dc3f8ebdb338def9af6
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-02-22 21:39:24 +0000

    Don't try to run now-removed examples.
    git-svn-id: https://svn.salilab.org/imp/trunk@12802 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/domino/examples/test_examples.py
+++ modules/domino/examples/test_examples.py
@@ -13,24 +13,11 @@ class DOMINOTests(IMP.test.TestCase):
                 IMP.domino.get_example_path("six_particles_optimization.py"))
         self.assertEqual(vars['cs'].get_number_of_configurations(), 1)
 
-    def test_global_min2(self):
-        """Test example six_particles_loopy_optimization.py"""
-        vars = self.run_example(
-           IMP.domino.get_example_path("six_particles_loopy_optimization.py"))
-        self.assertEqual(vars['cs'].get_number_of_configurations(), 1)
-
     def test_global_min3(self):
         """Test example custom_filter.py"""
         vars = self.run_example(
            IMP.domino.get_example_path("custom_filter.py"))
         self.assertEqual(vars['cs'].get_number_of_configurations(), 1)
-
-    def test_domino_approach(self):
-        """Test example domino_approach.py"""
-        vars = self.run_example(
-           IMP.domino.get_example_path("domino_approach.py"))
-    # This test is not yet fully implemented, so we expect it to fail
-    test_domino_approach = IMP.test.expectedFailure(test_domino_approach)
 
 if __name__ == '__main__':
     IMP.test.main()
