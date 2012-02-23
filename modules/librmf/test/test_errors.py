06669ac2893683be5cc0c539ec63e7b9a7d2bad7
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-02-23 00:35:21 +0000

    check general opening of files twice, it works...
    git-svn-id: https://svn.salilab.org/imp/trunk@12809 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/librmf/test/test_errors.py
+++ modules/librmf/test/test_errors.py
@@ -5,6 +5,11 @@ class GenericTest(RMF.TestCase):
     def test_data_types(self):
         """Test that opening a non-existent file causes and exception"""
         self.assertRaises(IOError, RMF.open_hdf5_file, "no such file")
+    def test_open_2x(self):
+        """Test that opening a file 2x works"""
+        f0= RMF.open_hdf5_file(self.get_input_file_name("backwards.rmf"))
+        RMF.set_show_hdf5_errors(True)
+        f1= RMF.open_hdf5_file(self.get_input_file_name("backwards.rmf"))
 
 if __name__ == '__main__':
     unittest.main()
