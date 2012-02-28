41c8752632a0acae9796144156c4aebeac10ed6d
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-02-28 21:18:40 +0000

    test that associations in RMF work in python
    git-svn-id: https://svn.salilab.org/imp/trunk@12849 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/librmf/test/test_associations.py
@@ -0,0 +1,13 @@
+import unittest
+import RMF
+
+class GenericTest(RMF.TestCase):
+    def test_data_types(self):
+        """Test that associations work from python"""
+        fh =RMF.create_rmf_file(self.get_tmp_file_name("assoc.rmf"))
+        rh =fh.get_root_node()
+        rh.set_association(fh)
+        rh2= fh.get_node_from_association(fh)
+
+if __name__ == '__main__':
+    unittest.main()
