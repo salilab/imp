5173e822f874d43b16f109b7c703893c0597a7d9
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-03-01 21:59:51 +0000

    Match scons variable with command line option name.
    git-svn-id: https://svn.salilab.org/imp/trunk@12857 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- scons_tools/variables.py
+++ scons_tools/variables.py
@@ -288,8 +288,8 @@ def add_common_variables(vars, package):
     vars.Add('datapath', "The path to the data of an ininstalled IMP you want to use.", None)
     vars.Add(BoolVariable('pretty', "Whether to write cleaner output when building.", True))
     vars.Add(BoolVariable('color', "Whether to write color output output when building.", True))
-    vars.Add(EnumVariable('coverage',
-                      "Whether to report on code coverage of tests."
+    vars.Add(EnumVariable('pycoverage',
+                      "Whether to report on Python code coverage of tests."
                       '"no" will do no reporting; '
                       '"lines" will list the lines of code that were missed; '
                       '"annotate" will make annotated copies of the code.',
