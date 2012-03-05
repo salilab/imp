1970bac734b06a16d7cfc439449b8fb28f41144a
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-03-05 17:27:28 +0000

    subset scores cache now compiles
    git-svn-id: https://svn.salilab.org/imp/trunk@12911 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/domino/include/subset_scores.h
+++ modules/domino/include/subset_scores.h
@@ -13,37 +13,26 @@
 #include "Assignment.h"
 #include "Subset.h"
 #include <IMP/base/Object.h>
-#include <IMP/base/caches.h>
+#include <IMP/base/cache.h>
 #include <IMP/Restraint.h>
 
 
 IMPDOMINO_BEGIN_NAMESPACE
+
 /** Implement a cache for restraint scores. By having a single unified cache,
     we allow easy control of memory usage as well as of writing things to disk.
 */
 class IMPDOMINOEXPORT RestraintCache: public base::Object {
-  struct Key {
-    WeakPointer<Evaluator> r;
-    Assignment a;
-    IMP_HASHABLE_INLINE(Key, {
-        std::size_t value= boost::hash_value(r.get());
-        voost::hash_combine(value, boost::hash_range(a.begin(),
-                                                     a.end()));
-      });
-  };
+  IMP_NAMED_TUPLE_2(Key, Keys, WeakPointer<Restraint>, r,
+                    Assignment, a, );
+  IMP_NAMED_TUPLE_2(RestraintData,RestraintDatas,
+                    OwnerPointer<Restraint>,
+                    sf, Subset, s,);
+  IMP_NAMED_TUPLE_3(RestraintSetData, RestraintSetDatas,
+                    Slice, slice, WeakPointer<Restraint>, r,
+                    double, weight,);
+  IMP_NAMED_TUPLE_1(SetData, SetDatas, RestraintSetDatas, members,);
   class Generator {
-    struct RestraintData{
-      OwnerPointer<ScoringFunction> sf;
-      Subset s;
-    };
-    struct RestraintSubsetData {
-      Slice slice;
-      Restraint *r;
-      double weight;
-    };
-    struct SetData {
-      vector<RestraintSubsetData> members;
-    };
     typedef compatibility::map<Restraint*, RestraintData> RMap;
     RMap rmap_;
     typedef compatibility::map<Restraint*, SetData> SMap;
@@ -57,7 +46,7 @@ class IMPDOMINOEXPORT RestraintCache: public base::Object {
     result_type operator()(const argument_type&k, const Cache &cache) const {
       RMap::const_iterator it= rmap_.find(k.r);
       if (it != rmap_.end()) {
-        Subset s= rmap_.find(k.r)->second;
+        Subset s= rmap_.find(k.r)->second.s;
         load_particle_states(s, k.a, pst_);
         double e= k.r->evaluate_if_good(false);
         return e;
@@ -77,11 +66,10 @@ class IMPDOMINOEXPORT RestraintCache: public base::Object {
     }
     void add_to_set(RestraintSet *rs, Restraint *r, double weight,
                     Slice slice) {
-      sets_[rs].push_back(RestraintSetData(r, weight, slice));
+      sets_[rs].members.push_back(RestraintSetData(slice, r, weight));
     }
     void add_restraint(Restraint *e, Subset s) {
-      RestraintData rd={new ScoringFunction(e), s};
-      rmap_[e]=rd;
+      rmap_[e]=RestraintData(e, s);
     }
   };
   struct ApproximatelyEqual {
@@ -89,12 +77,13 @@ class IMPDOMINOEXPORT RestraintCache: public base::Object {
       return std::abs(a-b) < .1*(a+b)+.1;
     }
   };
-  void add_restraint_internal(Restraint *r, double w,
-                              const RestraintSets &sets,
-                              const Subsets &set_subsets);
-  bool
+  void add_restraint_internal(Restraint *r,
+                              RestraintSets sets,
+                              Floats set_weights,
+                              Subsets set_subsets);
+
   typedef base::LRUCache<Generator, ApproximatelyEqual> Cache;
-  typedef Cache cache_;
+  Cache cache_;
   const ParticlesTemp order_;
   typedef compatibility::map<Pointer<Restraint>, Subset> KnownRestraints;
   KnownRestraints known_restraints_;
@@ -115,7 +104,8 @@ public:
                                 const Subsets&exclusions) const;
   /** Return the slice for that restraint given the subset. */
   Slice get_slice(Restraint *r, const Subset& s) const;
-  IMP_OBJECT_INLINE(RestraintCache,,)
+  IMP_OBJECT_INLINE(RestraintCache,
+                    out << "size=" << cache_.size() << std::endl;,);
 };
 IMPDOMINO_END_NAMESPACE
 
