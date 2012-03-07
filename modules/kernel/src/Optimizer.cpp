d657ed09609999dc2b8ca786267538f3b529eb15
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-03-07 22:46:07 +0000

    move the optimizer over to using the ScoringFunction internally
    git-svn-id: https://svn.salilab.org/imp/trunk@12981 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/kernel/src/Optimizer.cpp
+++ modules/kernel/src/Optimizer.cpp
@@ -9,6 +9,7 @@
 #include "IMP/Optimizer.h"
 #include "IMP/OptimizerState.h"
 #include "IMP/RestraintSet.h"
+#include "IMP/generic.h"
 #include "IMP/dependency_graph.h"
 #include "IMP/internal/graph_utility.h"
 #include "IMP/internal/container_helpers.h"
@@ -24,8 +25,6 @@ Optimizer::Optimizer(): Object("Optimizer%1%")
   set_was_used(true);
   min_score_= -std::numeric_limits<double>::max();
   stop_on_good_score_=false;
-  has_restraints_=false;
-  last_score_= std::numeric_limits<double>::max();
 }
 
 Optimizer::Optimizer(Model *m, std::string name): Object(name)
@@ -34,12 +33,15 @@ Optimizer::Optimizer(Model *m, std::string name): Object(name)
   set_was_used(true);
   min_score_= -std::numeric_limits<double>::max();
   stop_on_good_score_=false;
-  has_restraints_=false;
-  last_score_= std::numeric_limits<double>::max();
 }
 
 Optimizer::~Optimizer()
 {
+}
+
+void Optimizer::set_model(Model *m) {
+  cache_= m->create_model_scoring_function();
+  model_=m;
 }
 
 void Optimizer::update_states() const
@@ -77,41 +79,23 @@ void Optimizer::set_optimizer_state_optimizer(OptimizerState *os, Optimizer *o)
 
 
 void Optimizer::set_restraints(const RestraintsTemp &rs) {
-  cache_= EvaluationCache(rs);
-  has_restraints_=true;
+  cache_= new RestraintsScoringFunction(rs);
 }
 
 double Optimizer::evaluate(bool compute_derivatives) const {
   IMP_FUNCTION_LOG;
-  if (!has_restraints_) {
-    last_score_= get_model()->evaluate(compute_derivatives);
-  } else {
-    IMP::Floats ret
-        = get_model()->evaluate(cache_,
-                                compute_derivatives);
-    last_score_= std::accumulate(ret.begin(), ret.end(), 0.0);
-  }
-  return last_score_;
+  return cache_->evaluate(compute_derivatives);
 }
 
 double Optimizer::evaluate_if_below(bool compute_derivatives,
                                    double max) const {
   IMP_FUNCTION_LOG;
-  Floats ret=get_model()->evaluate_if_below(has_restraints_?
-                                            get_model()->get_evaluation_cache():
-                                            cache_,
-                                            compute_derivatives, max);
-  last_score_= std::accumulate(ret.begin(), ret.end(), 0.0);
-  return last_score_;
+  return cache_->evaluate_if_below(compute_derivatives, max);
 }
 
 
 RestraintsTemp Optimizer::get_restraints() const {
-  if (!has_restraints_) {
-    return RestraintsTemp(1, model_->get_root_restraint_set());
-  } else {
-    return get_as<RestraintsTemp>(cache_.get_restraints());
-  }
+  return cache_->get_restraints();
 }
 
 IMP_END_NAMESPACE
