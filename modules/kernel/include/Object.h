502f13b74edf39a36b726b3fa0edc77e57dae62f
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-03-08 22:46:23 +0000

    put object using back
    git-svn-id: https://svn.salilab.org/imp/trunk@13016 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/kernel/include/Object.h
+++ modules/kernel/include/Object.h
@@ -18,7 +18,11 @@
 #include <IMP/base/VectorOfRefCounted.h>
 
 IMP_BEGIN_NAMESPACE
-
+#ifndef SWIG
+using base::Object;
+using base::Objects;
+using base::ObjectsTemp;
+#endif
 IMP_END_NAMESPACE
 
 #endif  /* IMPKERNEL_OBJECT_H */
