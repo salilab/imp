f0fb8fcebca6b0a439dbd77407ad111a76d74fe1
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-03-08 23:04:33 +0000

    fix more bad merge results
    git-svn-id: https://svn.salilab.org/imp/trunk@13027 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/kernel/include/singleton_macros.h
+++ modules/kernel/include/singleton_macros.h
@@ -436,7 +436,7 @@
     ParticleIndexes all= get_all_possible_indexes();                    \
     Restraints ret(all.size());                                         \
     for (unsigned int i=0; i< all.size(); ++i) {                        \
-      ret[i]= new IMP::SingletonRestraint(s,                            \
+      ret[i]= new IMP::core::SingletonRestraint(s,                      \
             IMP::internal::get_particle(get_model(), all[i]));          \
     }                                                                   \
     return ret;                                                         \
