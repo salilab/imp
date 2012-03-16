9852d1ec172699aeb1f0cc79a52e848c990dddca
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-03-16 03:51:36 +0000

    simplify vector stuff into one class
    git-svn-id: https://svn.salilab.org/imp/trunk@13142 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/kernel/include/utility.h
+++ modules/kernel/include/utility.h
@@ -18,28 +18,6 @@ IMP_BEGIN_NAMESPACE
 using IMP::base::cube;
 using IMP::base::square;
 using IMP::base::is_nan;
-
-template <class T>
-inline vector<T> operator+(const vector<T> &t,
-                           const vector<T> &o) {
-  vector<T> ret=t;
-  ret.insert(ret.end(), o.begin(), o.end());
-  return ret;
-}
-
-template <class T>
-inline vector<T> operator+=( vector<T> &t,
-                    const T &o) {
-  t.push_back(o);
-  return t;
-}
-template <class T>
-inline vector<T> operator+=( vector<T> &t,
-                             const vector<T> &o) {
-  t.insert(t.end(), o.begin(), o.end());
-  return t;
-}
-
 #endif
 using IMP::base::get_as;
 #endif
