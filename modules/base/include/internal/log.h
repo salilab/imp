37e08a8772e3c685d33ab3e1858325fd28132c04
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-03-19 22:49:51 +0000

    move internal stuff in log header into its own header
    git-svn-id: https://svn.salilab.org/imp/trunk@13192 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/base/include/internal/log.h
@@ -0,0 +1,23 @@
+/**
+ *  \file internal/log.h
+ *  \brief Various useful utilities
+ *
+ *  Copyright 2007-2012 IMP Inventors. All rights reserved.
+ */
+
+#ifndef IMPBASE_INTERNAL_LOG_H
+#define IMPBASE_INTERNAL_LOG_H
+
+#include "../base_config.h"
+#include "../enums.h"
+
+
+IMPBASE_BEGIN_INTERNAL_NAMESPACE
+
+IMPBASEEXPORT extern LogLevel log_level;
+IMPBASEEXPORT extern unsigned int log_indent;
+IMPBASEEXPORT extern bool print_time;
+
+IMPBASE_END_INTERNAL_NAMESPACE
+
+#endif  /* IMPBASE_INTERNAL_LOG_H */
