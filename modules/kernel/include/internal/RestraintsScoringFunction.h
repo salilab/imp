ecb568f091cca19bfda8cca1c0d9ddbeb784ae31
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-03-21 00:46:31 +0000

    move restraints scoring function out of scoring funciton header and expose it in core
    git-svn-id: https://svn.salilab.org/imp/trunk@13211 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/kernel/include/internal/RestraintsScoringFunction.h
@@ -0,0 +1,36 @@
+/**
+ *  \file IMP/declare_ScoringFunction.h
+ *  \brief Storage of a model, its restraints,
+ *                         constraints and particles.
+ *
+ *  Copyright 2007-2012 IMP Inventors. All rights reserved.
+ *
+ */
+
+#ifndef IMPKERNEL_INTERNAL_RESTRAINTS_SCORING_FUNCTION_H
+#define IMPKERNEL_INTERNAL_RESTRAINTS_SCORING_FUNCTION_H
+
+#include "../kernel_config.h"
+#include "../ScoringFunction.h"
+#include "../scoring_function_macros.h"
+
+IMP_BEGIN_INTERNAL_NAMESPACE
+
+/** Create a scoring function on a list of restraints.
+*/
+class IMPEXPORT RestraintsScoringFunction: public ScoringFunction {
+  friend class Model;
+  Restraints rs_;
+  RestraintSets rss_;
+  double weight_;
+  double max_;
+ public:
+  RestraintsScoringFunction(const RestraintsTemp &rs,
+                            double weight=1.0,
+                            double max=NO_MAX,
+                            std::string name= "RestraintsScoringFunction%1%");
+  IMP_SCORING_FUNCTION(RestraintsScoringFunction);
+};
+IMP_END_INTERNAL_NAMESPACE
+
+#endif  /* IMPKERNEL_INTERNAL_RESTRAINTS_SCORING_FUNCTION_H */
