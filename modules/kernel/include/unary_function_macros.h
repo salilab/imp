6acb73485c02e293f2f868f27074d5c1b4dca751
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-03-21 00:58:05 +0000

    forgot to move one of the unaryfunction macros
    git-svn-id: https://svn.salilab.org/imp/trunk@13230 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/kernel/include/unary_function_macros.h
+++ modules/kernel/include/unary_function_macros.h
@@ -28,4 +28,28 @@
 
 
 
+//! Declare the needed functions for a UnaryFunction which evaluates inline
+/** This macro declares all the functions needed for an IMP::UnaryFunction
+    inline in the class. There is no need for an associated \c .cpp file.
+
+    The last three arguments are expressions that evaluate to the
+    unary function value and derivative and are sent to the stream in the
+    show function, respectively. The input to the function is called
+    \c feature.
+
+    \see IMP_UNARY_FUNCTION
+*/
+#define IMP_UNARY_FUNCTION_INLINE(Name, value_expression,               \
+                                  derivative_expression, show_expression) \
+  IMP_IMPLEMENT_INLINE(virtual DerivativePair                           \
+  evaluate_with_derivative(double feature) const, {                     \
+    return DerivativePair((value_expression), (derivative_expression)); \
+                       });                                              \
+  IMP_IMPLEMENT_INLINE(virtual double evaluate(double feature) const,{  \
+    return (value_expression);                                          \
+    });                                                                 \
+  IMP_OBJECT_INLINE(Name, out << show_expression, {})
+
+
+
 #endif  /* IMPKERNEL_UNARY_FUNCTION_MACROS_H */
