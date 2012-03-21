2a4b45625d7f7b93aae8248598ae7c19b0215324
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-03-21 00:53:21 +0000

    test compilation and running of show code
    git-svn-id: https://svn.salilab.org/imp/trunk@13215 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/base/test/test_show.cpp
@@ -0,0 +1,26 @@
+/**
+ *  \file test_cache.cpp
+ *  \brief A nullptr-initialized pointer to an \imp Object.
+ *
+ *  Copyright 2007-2012 IMP Inventors. All rights reserved.
+ *
+ */
+#include <IMP/base/Showable.h>
+#include <IMP/base/VersionInfo.h>
+#include <IMP/base/check_macros.h>
+namespace IMP {
+  namespace base {
+    void test() {
+      std::cout << Showable(1);
+      std::cout << Showable("hi");
+      std::cout << Showable(VersionInfo("hi", "there"));
+      std::cout << VersionInfo("hi", "there");
+      std::cout << Vector<int>(10);
+      std::cout << Showable(Vector<int>(10));
+    }
+  }
+}
+int main(int, char *[]) {
+  IMP_CATCH_AND_TERMINATE(  IMP::base::test(); );
+  return 0;
+}
