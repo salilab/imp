0b802e0845e9c85eb82b38c4b7d92d6c1953b18f
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-03-21 01:44:53 +0000

    don't test functors any more
    git-svn-id: https://svn.salilab.org/imp/trunk@13248 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/test/test_transform_particles.py
+++ modules/core/test/test_transform_particles.py
@@ -23,20 +23,5 @@ class ParticleTransformationTests(IMP.test.TestCase):
             v = particles[i].get_coordinates()
             self.assertAlmostEqual((v-t.get_transformed(coords[i])).get_magnitude(), 0, delta=0.01)
 
-    def test_transformation2(self):
-        """Test the TransformationFunction class with map"""
-        imp_model = IMP.Model()
-        particles = IMP.core.create_xyzr_particles(imp_model, 4, 1.0)
-
-        coords= [x.get_coordinates() for x in particles]
-        r = IMP.algebra.get_rotation_from_fixed_xyz(0.2,0.8,-0.4)
-        t=IMP.algebra.Transformation3D(r,IMP.algebra.Vector3D(20.0,-12.4,18.6))
-        tf=IMP.core.Transform(t)
-        tf.set_was_used(True)
-        map( IMP.SingletonFunctor(tf), [x.get_particle() for x in particles])
-        for i in range(0,len(particles)):
-            v = particles[i].get_coordinates()
-            self.assertAlmostEqual((v-t.get_transformed(coords[i])).get_magnitude(), 0, delta=0.01)
-
 if __name__ == '__main__':
     IMP.test.main()
