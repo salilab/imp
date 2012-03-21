5e126fdfebf1dbb1de1d29545f374d138cc23a6e
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-03-21 00:58:05 +0000

    forgot to move one of the unaryfunction macros
    git-svn-id: https://svn.salilab.org/imp/trunk@13230 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/kernel/include/macros.h
+++ modules/kernel/include/macros.h
@@ -176,44 +176,6 @@
   IMP_REQUIRE_SEMICOLON_NAMESPACE
 
 
-//! Declare the needed functions for a UnaryFunction
-/** In addition to the methods done by all the macros, it declares
-    - IMP::UnaryFunction::evaluate()
-    - IMP::UnaryFunction::evaluate_with_derivatives()
-
-    \see IMP_UNARY_FUNCTION_INLINE
-*/
-#define IMP_UNARY_FUNCTION(Name)                                        \
-  IMP_IMPLEMENT(virtual DerivativePair                                  \
-                evaluate_with_derivative(double feature) const);        \
-  IMP_IMPLEMENT(virtual double evaluate(double feature) const);         \
-  IMP_OBJECT(Name)
-
-
-
-//! Declare the needed functions for a UnaryFunction which evaluates inline
-/** This macro declares all the functions needed for an IMP::UnaryFunction
-    inline in the class. There is no need for an associated \c .cpp file.
-
-    The last three arguments are expressions that evaluate to the
-    unary function value and derivative and are sent to the stream in the
-    show function, respectively. The input to the function is called
-    \c feature.
-
-    \see IMP_UNARY_FUNCTION
-*/
-#define IMP_UNARY_FUNCTION_INLINE(Name, value_expression,               \
-                                  derivative_expression, show_expression) \
-  IMP_IMPLEMENT_INLINE(virtual DerivativePair                           \
-  evaluate_with_derivative(double feature) const, {                     \
-    return DerivativePair((value_expression), (derivative_expression)); \
-                       });                                              \
-  IMP_IMPLEMENT_INLINE(virtual double evaluate(double feature) const,{  \
-    return (value_expression);                                          \
-    });                                                                 \
-  IMP_OBJECT_INLINE(Name, out << show_expression, {})
-
-
 
 //! Declare a IMP::FailureHandler
 /** In addition to the standard methods it declares:
