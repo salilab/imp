e30dd99ce60e42cbbdb0bca1e350aaf1701c194f
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-03-21 00:56:45 +0000

    move restraintsscoringfunction tests to core
    git-svn-id: https://svn.salilab.org/imp/trunk@13218 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/core/test/test_restraints_scoring_function.py
@@ -0,0 +1,43 @@
+import IMP
+import IMP.test
+
+class RestraintSetTests(IMP.test.TestCase):
+    """Test RestraintSets"""
+    def _make_stuff(self):
+        m= IMP.Model()
+        rs= IMP.RestraintSet(.5)
+        r0= IMP._ConstRestraint(1)
+        rs.add_restraint(r0)
+        m.add_restraint(rs)
+        r1= IMP._ConstRestraint(1)
+        rs.add_restraint(r1)
+        r2= IMP._ConstRestraint(1)
+        m.add_restraint(r2)
+        return (m, rs, r0, r1, r2)
+
+    def test_weights2(self):
+        """Test that sets can be weighted"""
+        (m, rs, r0, r1, r2)= self._make_stuff()
+        self.assertEqual(m.evaluate(False), 2)
+        rs.set_weight(1)
+        self.assertEqual(m.evaluate(False), 3)
+        sf= IMP.RestraintsScoringFunction([r0, r1, r2])
+        self.assertEqual(sf.evaluate_if_good(False), 3)
+        self.assertEqual(rs.evaluate(False), 2)
+        self.assertEqual(m.get_root_restraint_set().evaluate(False), 3)
+        self.assertEqual(m.get_root_restraint_set().get_restraint(0).evaluate(False), 2)
+    def test_weights(self):
+        """Test that restraints decompose ok"""
+        m= IMP.Model()
+        p= IMP.Particle(m)
+        r= IMP._ConstRestraint(1, [p])
+        m.add_restraint(r)
+        rd= r.create_decomposition()
+        self.assertEqual(r.evaluate(False), rd.evaluate(False))
+        ra= IMP.get_restraints([r])
+        rda= IMP.get_restraints([rd])
+        sf= IMP.RestraintsScoringFunction(ra)
+        sfda= IMP.RestraintsScoringFunction(rda)
+        self.assertEqual(sf.evaluate( False), sfda.evaluate(False))
+if __name__ == '__main__':
+    IMP.test.main()
