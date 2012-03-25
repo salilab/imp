5201b5eebc8488df0afb5e1fe2744068397a0019
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-03-25 00:56:07 +0000

    move atom test to atom
    git-svn-id: https://svn.salilab.org/imp/trunk@13275 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/atom/test/test_hierarchy_conversion.py
@@ -0,0 +1,15 @@
+import IMP
+import IMP.test
+import IMP.core
+import IMP.atom
+
+class DecoratorTests(IMP.test.TestCase):
+    def test_conversion(self):
+        """Test conversion from atom.hierarchy"""
+        m= IMP.Model()
+        p =IMP.Particle(m)
+        h= IMP.atom.Hierarchy.setup_particle(p)
+        c= IMP.core.get_leaves(h)
+
+if __name__ == '__main__':
+    IMP.test.main()
