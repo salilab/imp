cf984dbd3a91e2d5c712874f0ddde17684499585
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-03-27 23:05:52 +0000

    add a macro for weighted graphs
    git-svn-id: https://svn.salilab.org/imp/trunk@13353 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/base/include/graph_macros.h
+++ modules/base/include/graph_macros.h
@@ -35,8 +35,11 @@
                   EdgeData> > Name;                                     \
   typedef boost::property_map<Name, boost::vertex_name_t>::const_type   \
   Name##ConstVertexName;                                                \
+  typedef boost::property_map<Name, boost::edge_name_t>::const_type     \
+  Name##ConstEdgeName;                                                  \
   typedef boost::graph_traits<Name> Name##Traits;                       \
   typedef Name##Traits::vertex_descriptor Name##Vertex;                 \
+  typedef Name##Traits::edge_descriptor Name##Edge;                     \
   typedef compatibility::map<VertexData, Name##Vertex> Name##VertexIndex; \
   inline Name##VertexIndex get_vertex_index(const Name &g) {            \
     Name##ConstVertexName vm = boost::get(boost::vertex_name, g);       \
@@ -48,6 +51,54 @@
     }                                                                   \
     return ret;                                                         \
   }                                                                     \
+  typedef boost::property_map<Name, boost::edge_name_t>::type           \
+  Name##EdgeName;                                                       \
+  typedef boost::property_map<Name, boost::vertex_name_t>::type         \
+  Name##VertexName
+#endif
+
+
+#ifdef IMP_DOXYGEN
+//! Define a graph object in \imp
+/** The docs for the graph should appear before the macro
+    invocation. Directionality should be one of
+    - \c bidirectional
+    - \c directed
+    - \c undirected
+ */
+#define IMP_WEIGHTED_GRAPH(Name, directionality, VertexData)    \
+  /** See \ref graphs "Graphs" for more information.*/  \
+  typedef boost::graph Name
+
+#elif defined(SWIG)
+#define IMP_WEIGHTED_GRAPH(Name, directionality, VertexData)  class Name
+#else
+#define IMP_WEIGHTED_GRAPH(Name, directionality, VertexData)            \
+  typedef boost::adjacency_list<boost::vecS, boost::vecS,               \
+                                boost::directionality##S,               \
+  boost::property<boost::vertex_name_t, VertexData>,                    \
+                                boost::property<boost::edge_weight_t,   \
+                  double> > Name;                                       \
+  typedef boost::property_map<Name, boost::vertex_name_t>::const_type   \
+  Name##ConstVertexName;                                                \
+  typedef boost::property_map<Name, boost::edge_weight_t>::const_type   \
+  Name##ConstEdgeWeight;                                                \
+  typedef boost::graph_traits<Name> Name##Traits;                       \
+  typedef Name##Traits::vertex_descriptor Name##Vertex;                 \
+  typedef Name##Traits::edge_descriptor Name##Edge;                     \
+  typedef compatibility::map<VertexData, Name##Vertex> Name##VertexIndex; \
+  inline Name##VertexIndex get_vertex_index(const Name &g) {            \
+    Name##ConstVertexName vm = boost::get(boost::vertex_name, g);       \
+    std::pair<Name##Traits::vertex_iterator, Name##Traits::vertex_iterator> \
+      be= boost::vertices(g);                                           \
+    Name##VertexIndex ret;                                              \
+    for (; be.first != be.second; ++be.first) {                         \
+      ret[vm[*be.first]]= *be.first;                                    \
+    }                                                                   \
+    return ret;                                                         \
+  }                                                                     \
+  typedef boost::property_map<Name, boost::edge_weight_t>::type         \
+  Name##EdgeWeight;                                                     \
   typedef boost::property_map<Name, boost::vertex_name_t>::type         \
   Name##VertexName
 #endif
