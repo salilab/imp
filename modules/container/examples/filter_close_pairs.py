408538a38fba101be1316df29afedc369a8e8cd4
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-04-10 15:15:10 +0000

    inherit from predicate, not filter
    git-svn-id: https://svn.salilab.org/imp/trunk@13462 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/container/examples/filter_close_pairs.py
+++ modules/container/examples/filter_close_pairs.py
@@ -21,15 +21,15 @@ cpc= IMP.container.ClosePairContainer(lsc, 0.0)
 m.update()
 print "without",[(x[0].get_name(), x[1].get_name()) for x in cpc.get_particle_pairs()]
 
-class ConsecutiveFilter(IMP.PairFilter):
+class ConsecutiveFilter(IMP.PairPredicate):
     def __init__(self):
         IMP.PairFilter.__init__(self)
-    def get_contains(self, m, pp):
+    def get_value(self, m, pp):
         diff= m.get_particle(pp[0]).get_value(ik)\
               - m.get_particle(pp[1]).get_value(ik)
         if diff==-1 or diff ==1:
-            return True
-        return False
+            return 1
+        return 0
     def get_input_particles(self, p):
         return [p]
     def get_input_containers(self, p):
