06919896916b49db1c41ea3e67501131872e5776
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-04-13 17:28:47 +0000

    Install sitecustomize.py with a proper builder, so dependencies are tracked.
    git-svn-id: https://svn.salilab.org/imp/trunk@13505 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- scons_tools/python_coverage.py
+++ scons_tools/python_coverage.py
@@ -27,29 +27,7 @@ def setup_excludes(cov):
     cov.exclude("^\s+weakref_proxy =")
     cov.exclude("^def [sg]et_check_level")
 
-def setup():
+def setup(env):
     """Make sure that coverage works for subprocesses"""
-    try:
-        os.makedirs('build/lib')
-    except OSError:
-        pass
-    f = open('build/lib/sitecustomize.py', 'w')
-    f.write("""
-import os
-# If we're being imported from a Python application that is in turn being run
-# by a test case with coverage turned on, also turn on coverage in the Python
-# application.
-if 'IMP_COVERAGE_APPS' in os.environ:
-    apps = os.environ['IMP_COVERAGE_APPS'].split(os.pathsep)
-    import coverage
-    # See scons_tools/python_coverage.py for further explanation
-    def _our_abs_file(self, filename):
-        return os.path.normcase(os.path.abspath(filename))
-    coverage.files.FileLocator.abs_file = _our_abs_file
-    _cov = coverage.coverage(branch=True, data_suffix=True, auto_data=True,
-                             data_file=os.environ['IMP_COVERAGE_DATA_FILE'],
-                             source=[os.path.join(
-                                      os.environ['IMP_BIN_DIR'], x) \\
-                                     for x in apps])
-    _cov.start()
-""")
+    return env.InstallAs(target=env['builddir'] + '/lib/sitecustomize.py',
+                         source='#/scons_tools/site-template.py')
