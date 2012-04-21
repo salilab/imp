1b4777fa167cbdda59e7630b31a10930db605b7b
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-04-21 00:15:52 +0000

    add a test that the generic create_restraint can be called
    git-svn-id: https://svn.salilab.org/imp/trunk@13614 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/core/test/test_generic.cpp
@@ -0,0 +1,21 @@
+/**
+ *  \file test_generic.cpp
+ *  \brief A nullptr-initialized pointer to an \imp Object.
+ *
+ *  Copyright 2007-2012 IMP Inventors. All rights reserved.
+ *
+ */
+#include <IMP/Model.h>
+#include <IMP/Particle.h>
+#include <IMP/generic.h>
+#include <IMP/core/SphereDistancePairScore.h>
+
+int main(int, char*[]) {
+  IMP_NEW(IMP::Model, m, ());
+  IMP_NEW(IMP::Particle, p, (m));
+  IMP_NEW(IMP::core::SoftSpherePairScore, ps, (1));
+  // check the call
+  IMP::Pointer<IMP::Restraint> r= IMP::create_restraint(ps.get(),
+                                                   IMP::ParticlePair(p,p));
+  return 0;
+}
