f28eb09e6c979fe5e48db4659cc21f72a7f6dbb7
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-04-23 22:34:01 +0000

    add an example writing a simulation to an rmf
    git-svn-id: https://svn.salilab.org/imp/trunk@13650 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/rmf/examples/simulation.py
@@ -0,0 +1,65 @@
+import IMP.atom
+import IMP.rmf
+import RMF
+import IMP.container
+import IMP.display
+
+k=1.0
+np=10
+frames=1000
+
+m= IMP.Model()
+bb= IMP.algebra.BoundingBox3D(IMP.algebra.Vector3D(0,0,0),
+                              IMP.algebra.Vector3D(10,10,10))
+
+ps= [IMP.core.XYZR.setup_particle(IMP.Particle(m)) for i in range(0,np)]
+h= IMP.atom.Hierarchy.setup_particle(IMP.Particle(m, "root"))
+for p in ps:
+    p.set_radius(1)
+    p.set_coordinates_are_optimized(True)
+    IMP.atom.Mass.setup_particle(p, 30)
+    IMP.atom.Diffusion.setup_particle(p)
+    p.set_coordinates(IMP.algebra.get_random_vector_in(bb))
+rs=[]
+link= IMP.core.HarmonicUpperBoundSphereDistancePairScore(0,k)
+pl=[]
+for i in range(0, len(ps)/2):
+    pp=(ps[2*i], ps[2*i+1])
+    pl.append(pp)
+    # create OK staring position
+    pp[1].set_coordinates(IMP.algebra.get_random_vector_on(pp[0].get_sphere()))
+    IMP.display.Colored.setup_particle(pp[0], IMP.display.get_display_color(i))
+    IMP.display.Colored.setup_particle(pp[1], IMP.display.get_display_color(i))
+    hr= IMP.atom.Hierarchy.setup_particle(IMP.Particle(m, "molecule "+str(i)))
+    hr.add_child(IMP.atom.Hierarchy.setup_particle(pp[0]))
+    hr.add_child(IMP.atom.Hierarchy.setup_particle(pp[1]))
+    h.add_child(hr)
+r= IMP.container.PairsRestraint(link, pl, "Bonds")
+rs.append(r)
+
+ev= IMP.core.ExcludedVolumeRestraint(ps, k, 1, "EV")
+rs.append(ev)
+
+bbss= IMP.core.BoundingBox3DSingletonScore(IMP.core.HarmonicUpperBound(0,k), bb)
+bbr= IMP.container.SingletonsRestraint(bbss, ps)
+rs.append(bbr)
+
+sf= IMP.core.RestraintsScoringFunction(rs, "SF")
+
+bd= IMP.atom.BrownianDynamics(m)
+bd.set_scoring_function(sf)
+
+bd.set_maximum_time_step(10)
+
+name= IMP.base.create_temporary_file_name("brownian", ".rmf")
+rmf= RMF.create_rmf_file(name)
+os= IMP.rmf.SaveOptimizerState(rmf)
+os.set_log_level(IMP.SILENT)
+os.add_hierarchy(h)
+os.add_restraints(rs)
+os.add_geometry(IMP.display.BoundingBoxGeometry(bb))
+bd.add_optimizer_state(os)
+
+bd.optimize(frames)
+
+print "file is", rmf.get_name()
