39b5502e5b14e5ca4343d420e6840c5b5278b768
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-04-25 00:51:22 +0000

    clean up test
    git-svn-id: https://svn.salilab.org/imp/trunk@13694 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/test/test_ev.py
+++ modules/core/test/test_ev.py
@@ -8,43 +8,39 @@ import time
 
 class Tests(IMP.test.TestCase):
     """Tests for sampled density maps"""
-
-    def _setup(self):
-        """initialize IMP environment create particles"""
-        IMP.test.TestCase.setUp(self)
-        #init IMP model ( the environment)
-        m = IMP.Model()
-        h0s=IMP._create_particles_from_pdb(self.get_input_file_name("input.pdb"),
-                                           m)
-        h1s=IMP._create_particles_from_pdb(self.get_input_file_name("input.pdb"),
-                                           m)
-        rb0 = IMP.core.RigidBody.setup_particle(IMP.Particle(m),h0s)
-        rb1 = IMP.core.RigidBody.setup_particle(IMP.Particle(m),h1s)
-        rb0.set_reference_frame(IMP.algebra.ReferenceFrame3D())
-        rb1.set_reference_frame(IMP.algebra.ReferenceFrame3D())
-        l0= h0s
-        l1= h1s
-        #set the restraint
-        lsc=IMP.container.ListSingletonContainer(l0+l1)
-        r=IMP.core.ExcludedVolumeRestraint(lsc, 1)
-        m.add_restraint(r)
-        return (m, rb0, rb1, l0, l1)
     def test_filters(self):
         """Test filters on excluded volume"""
         m = IMP.Model()
         m.set_log_level(IMP.SILENT)
+        print "pdb"
         h0s=IMP._create_particles_from_pdb(self.get_input_file_name("1z5s_A.pdb"),
                                            m)
-        l0= h0s[0:200]
+        l0= h0s[0:10]
+        rm= h0s[10:]
+        for r in rm:
+            m.remove_particle(r)
+        del h0s
+        del rm
+        print "ev"
         #set the restraint
-        lsc=IMP.container.ListSingletonContainer(l0)
-        r=IMP.core.ExcludedVolumeRestraint(lsc, 1, 0)
-        m.add_restraint(r)
-        cpc= IMP.container.ClosePairContainer(lsc, 0, 0)
+        c0= IMP.container.ListSingletonContainer(l0)
+        r=IMP.core.ExcludedVolumeRestraint(l0, 1, 0)
+        print "cpc"
+        cpc= IMP.container.ClosePairContainer(l0, 0, 0)
         cr= IMP.container.PairsRestraint(IMP.core.SoftSpherePairScore(1), cpc)
-        m.add_restraint(cr)
+        c1= IMP.container.ListSingletonContainer(l0)
+        dg= IMP.get_dependency_graph(m)
+        IMP.base.show_graphviz(dg)
+        idx= IMP.get_vertex_index(dg)
+        ss= IMP.get_required_score_states(cr, [], dg, idx)
+        print "ss", ss
+        crsf= cr.create_scoring_function()
+        print crsf.get_score_states()
         print r.evaluate(False)
+        m.set_log_level(IMP.VERBOSE)
         print cr.evaluate(False)
+        pp= cpc.get_particle_pairs()
+        print pp
         self.assertAlmostEqual(r.evaluate(False), cr.evaluate(False),
                                delta=.1)
 if __name__ == '__main__':
