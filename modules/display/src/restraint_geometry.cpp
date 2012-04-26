f4f4aa07111940cf4bf456495725206f1b52d0ec
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-04-26 22:49:31 +0000

    speed up display of restraints
    git-svn-id: https://svn.salilab.org/imp/trunk@13722 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/display/src/restraint_geometry.cpp
+++ modules/display/src/restraint_geometry.cpp
@@ -33,7 +33,6 @@ namespace {
 
 IMP::display::Geometries RestraintGeometry::get_components() const {
   IMP_CHECK_OBJECT(r_);
-  m_->update();
   Pointer<Restraint> rd= r_->create_current_decomposition();
   if (!rd) return IMP::display::Geometries();
   RestraintSet *rs= dynamic_cast<RestraintSet*>(rd.get());
