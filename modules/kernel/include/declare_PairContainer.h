6d7258a94eebe57261b72934e9af103362f01dd8
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-04-30 17:19:44 +0000

    add index typedefs to containers
    git-svn-id: https://svn.salilab.org/imp/trunk@13737 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/kernel/include/declare_PairContainer.h
+++ modules/kernel/include/declare_PairContainer.h
@@ -42,6 +42,8 @@ class IMPEXPORT PairContainer : public Container
 public:
   typedef ParticlePair ContainedType;
   typedef ParticlePairsTemp ContainedTypes;
+  typedef ParticleIndexPairs ContainedIndexTypes;
+  typedef ParticleIndexPair ContainedIndexType;
   /** \note This function may be linear. Be aware of the complexity
       bounds of your particular container.
    */
@@ -81,7 +83,6 @@ public:
                                   double max) const=0;
 
 #ifndef IMP_DOXYGEN
-  typedef ParticlePair value_type;
   ParticlePair get(unsigned int i) const {
     return IMP::internal::get_particle(get_model(),
                                        get_indexes()[i]);
