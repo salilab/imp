2afe8e65e25490d3aef0a5ec062c892536b5fab9
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-05-02 19:53:16 +0000

    Speed up test.
    git-svn-id: https://svn.salilab.org/imp/trunk@13760 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/example/test/test_assembly.py
+++ modules/example/test/test_assembly.py
@@ -13,29 +13,18 @@ class LogTests(IMP.test.TestCase):
         [w.add_geometry(IMP.core.EdgePairGeometry((ps[p[0]], ps[p[1]]))) for p in pairs]
     def test_log_targets(self):
         """Test getting complex assembly"""
-        # assemble a cube
+        # assemble an interaction between 2 particles
         m= IMP.Model()
         bb= IMP.algebra.get_cube_3d(10)
-        ps= [IMP.Particle(m) for i in range(0,8)]
+        ps= [IMP.Particle(m) for i in range(2)]
         ds= [IMP.core.XYZR.setup_particle(p) for p in ps]
         [d.set_coordinates_are_optimized(True) for d in ds]
         [x.set_radius(1) for x in ds]
-        pairs=[(0,1), (0,2), (0,4),
-               (1,3), (1,5),
-               (2,3), (2,6),
-               (3,7),
-               (4,5), (4,6),
-               (5,7),
-               (6,7)]
-        diags=[(0,7), (1, 6), (2, 5), (3, 4)]
+        pairs=[(0,1)]
         score= IMP.core.HarmonicDistancePairScore(1.8,10)
-        dscore= IMP.core.HarmonicDistancePairScore(1.8*3**.5,10)
         rs=[IMP.core.PairRestraint(score, (ps[p[0]], ps[p[1]]),
-                                   ps[p[0]].get_name()+ " " +ps[p[1]].get_name())
-            for p in pairs]\
-            + [IMP.core.PairRestraint(dscore, (ps[p[0]], ps[p[1]]),
-                                   ps[p[0]].get_name()+ " " +ps[p[1]].get_name())
-            for p in diags]
+                               ps[p[0]].get_name()+ " " +ps[p[1]].get_name()) \
+            for p in pairs]
         [m.add_restraint(r) for r in rs]
         IMP.example.optimize_assembly(m, ps, rs, [], bb,
                                       IMP.core.SoftSpherePairScore(1),
