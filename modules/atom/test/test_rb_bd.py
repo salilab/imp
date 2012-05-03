8aec9060b478c9f9452ee43dc9ba2c86460b6041
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-05-03 21:06:01 +0000

    add test of rigid bodies and bd
    git-svn-id: https://svn.salilab.org/imp/trunk@13772 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/atom/test/test_rb_bd.py
@@ -0,0 +1,59 @@
+import IMP
+import IMP.test
+import IMP.core
+import IMP.atom
+import IMP.rmf
+import RMF
+
+class DecoratorTests(IMP.test.TestCase):
+    def create_rb(self, m):
+        prb= IMP.Particle(m, "body")
+        h0= IMP.atom.Hierarchy.setup_particle(prb)
+        core= IMP.Particle(m, "core")
+        IMP.core.XYZR.setup_particle(core).set_radius(1)
+        h0.add_child(IMP.atom.Hierarchy.setup_particle(core))
+        ps=[core]
+        IMP.atom.Mass.setup_particle(core, 1)
+        for i in range(0,3):
+            ep= IMP.Particle(m, "ep"+str(i))
+            d=IMP.core.XYZR.setup_particle(ep)
+            d.set_coordinate(i, 1)
+            d.set_radius(.1)
+            IMP.atom.Mass.setup_particle(ep, 1)
+            ps.append(d)
+            h0.add_child(IMP.atom.Hierarchy.setup_particle(ep))
+        IMP.core.RigidBody.setup_particle(prb, ps).set_coordinates_are_optimized(True)
+        rbd=IMP.atom.RigidBodyDiffusion.setup_particle(prb)
+        rbd.set_rotational_diffusion_coefficient(rbd.get_rotational_diffusion_coefficient()*10)
+        return prb, ep, core
+    def test_bonded(self):
+        """Check brownian dynamics with rigid bodies"""
+        m = IMP.Model()
+        pa, ma, ca=self.create_rb(m)
+        pb, mb, cb=self.create_rb(m)
+        ps0= IMP.core.HarmonicDistancePairScore(0, 1)
+        ps1= IMP.core.SoftSpherePairScore(10)
+        r0=IMP.core.PairRestraint(ps0, (ma, mb))
+        r1= IMP.core.PairRestraint(ps1, (ca, cb))
+        bd= IMP.atom.BrownianDynamics(m)
+        bb= IMP.algebra.get_unit_bounding_box_3d()
+        for p in (pa, pb):
+            rot= IMP.algebra.get_random_rotation_3d()
+            tr=IMP.algebra.get_random_vector_in(bb)
+            rf= IMP.algebra.ReferenceFrame3D(IMP.algebra.Transformation3D(rot,
+                                                                          tr))
+            IMP.core.RigidBody(pa).set_reference_frame(rf)
+        rmf= RMF.create_rmf_file(self.get_tmp_file_name("bd_rb.rmf"))
+        IMP.rmf.add_hierarchies(rmf, [pa, pb])
+        IMP.rmf.add_restraints(rmf, [r0, r1])
+        sf= IMP.core.RestraintsScoringFunction([r0, r1])
+        os= IMP.rmf.SaveOptimizerState(rmf)
+        bd.set_scoring_function(sf)
+        bd.add_optimizer_state(os)
+        bd.set_maximum_time_step(10)
+        bd.optimize(1000)
+        e= sf.evaluate(False)
+        self.assert_(e<2)
+
+if __name__ == '__main__':
+    IMP.test.main()
