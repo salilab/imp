0e6b500b50c5258efb90d5c6e12b91ec51ddb76d
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-05-08 18:09:00 +0000

    add test of description code
    git-svn-id: https://svn.salilab.org/imp/trunk@13832 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/librmf/test/test_description.py
@@ -0,0 +1,22 @@
+import unittest
+import RMF
+import shutil
+
+class GenericTest(RMF.TestCase):
+    def _show(self, g):
+        for i in range(0, g.get_number_of_children()):
+            print i, g.get_child_name(i), g.get_child_is_group(i)
+    """Test the python code"""
+    def test_perturbed(self):
+        """Test the description of an rmf file"""
+        fn=self.get_tmp_file_name("test_file_perturbed.mh")
+        f= RMF.create_rmf_file(fn)
+        descr= "hi there\n"
+        self.assertEqual(f.get_description(), "")
+        f.set_description(descr)
+        del f
+        f= RMF.open_rmf_file(fn)
+        odescr= f.get_description()
+        self.assertEqual(descr, odescr)
+if __name__ == '__main__':
+    unittest.main()
