7f00abf0504821ba3a307df8b5178380df580543
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-05-09 05:37:51 +0000

    add optimizer state to gradually remove things from a container
    git-svn-id: https://svn.salilab.org/imp/trunk@13861 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/misc/include/DecayPairContainerOptimizerState.h
@@ -0,0 +1,47 @@
+/**
+ *  \file DecayPairContainerOptimizerState.h
+ *  \brief Track the particles pairs passed to the pair score.
+ *
+ *  Copyright 2007-2012 IMP Inventors. All rights reserved.
+ */
+
+#ifndef IMPMISC_DECAY_PAIR_CONTAINER_OPTIMIZER_STATE_H
+#define IMPMISC_DECAY_PAIR_CONTAINER_OPTIMIZER_STATE_H
+
+#include "misc_config.h"
+#include <IMP/OptimizerState.h>
+#include <IMP/PairContainer.h>
+#include <IMP/PairContainer.h>
+#include <IMP/internal/InternalDynamicListPairContainer.h>
+#include <IMP/PairPredicate.h>
+#include <IMP/base/Pointer.h>
+#include <IMP/optimizer_state_macros.h>
+
+IMPMISC_BEGIN_NAMESPACE
+
+//! Maintain a pair container with a decaying list of pairs.
+/** The pair container passed to the constructor provides an initial
+    list of pairs. Periodically (see the set_period() method), the
+    pedicate is applied to all the remaining pairs and ones for which
+    it evaluates to 0 are removed.
+ */
+class IMPMISCEXPORT DecayPairContainerOptimizerState : public OptimizerState
+{
+  base::OwnerPointer<PairPredicate> pred_;
+  base::OwnerPointer<PairContainer> input_;
+  base::Pointer<IMP::internal::InternalDynamicListPairContainer> output_;
+ public:
+  DecayPairContainerOptimizerState(PairPredicate *pred,
+                                   const ParticlePairsTemp &initial_list,
+                                   std::string name
+                                   ="DecayPairContainerOptimizerState%1%");
+  PairContainer *get_output_container() const {
+    return output_;
+  }
+  IMP_PERIODIC_OPTIMIZER_STATE(DecayPairContainerOptimizerState);
+};
+
+
+IMPMISC_END_NAMESPACE
+
+#endif  /* IMPMISC_DECAY_PAIR_CONTAINER_OPTIMIZER_STATE_H */
