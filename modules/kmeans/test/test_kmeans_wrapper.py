9af857969ceb1d3448628e2e745f04f80861bfb5
barak@SALILAB.ORG <barak@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-05-11 00:14:54 +0000

    some refactoring to adhere to naming conventions
    git-svn-id: https://svn.salilab.org/imp/trunk@13908 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/kmeans/test/test_kmeans_wrapper.py
+++ modules/kmeans/test/test_kmeans_wrapper.py
@@ -6,38 +6,38 @@ class KMeansTest(IMP.test.TestCase):
     def  test_simple_kmeans(self):
         km = KMeansWrapper()
         # add data
-        km.addDataPt([1, 2])
-        km.addDataPt([10, 12])
-        km.addDataPt([0, -1])
-        km.addDataPt([11, 13])
-        km.addDataPt([0, 0])
-        km.addDataPt([1, 0])
-        km.addDataPt([9.4, 10])
-        km.addDataPt([9.2, 11])
-        km.addDataPt([0, 0])
+        km.add_data_pt([1, 2])
+        km.add_data_pt([10, 12])
+        km.add_data_pt([0, -1])
+        km.add_data_pt([11, 13])
+        km.add_data_pt([0, 0])
+        km.add_data_pt([1, 0])
+        km.add_data_pt([9.4, 10])
+        km.add_data_pt([9.2, 11])
+        km.add_data_pt([0, 0])
         self.assertEqual(km.getDataPoint(0)[0], 1)
         self.assertEqual(km.getDataPoint(0)[1], 2)
         # print data
-        n = km.getNDataPoints()
+        n = km.get_n_data_points()
         print "Number of points: ", n
         self.assertEqual(n,9)
         for i in range(0,n):
             print i
-            km.getDataPoint(i)
+            km.get_data_point(i)
         # cluster data
         k = 2
         km.execute(k)
         # print centers one by one
         print "\nCenters:"
-        nCenters = km.getNCenters() # this should actually be equal to k
+        nCenters = km.get_n_centers() # this should actually be equal to k
         print "Number of centers: ", nCenters
         self.assertEqual(nCenters, k)
         print range(1, nCenters)
         for i in range(0, nCenters):
-            print "Center ", i, "   ", km.getCenter(i)
+            print "Center ", i, "   ", km.get_center(i)
         # make sure centers were found correctly
-        c0 = km.getCenter(0)
-        c1 = km.getCenter(1)
+        c0 = km.get_center(0)
+        c1 = km.get_center(1)
         if(c0[0] < 3.0):
             self.assertAlmostEqual(c0[0],0.4,1)
             self.assertAlmostEqual(c0[1],0.2,1)
