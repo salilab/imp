36dcd706dddf39a86e722767cf187d58c910acbd
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-05-14 05:14:59 +0000

    reproduce bug for daniel :)
    git-svn-id: https://svn.salilab.org/imp/trunk@13951 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/domino/test/test_nested_states.py
+++ modules/domino/test/test_nested_states.py
@@ -12,25 +12,38 @@ class RBDTests(IMP.test.TestCase):
         mdl=IMP.Model()
         mhs=[]
         rbs=[]
+        orig_rfs=[]
         for i in range(3):
             mhs.append(IMP.atom.read_pdb(self.get_input_file_name("small_protein.pdb"),mdl))
             mhs[-1].set_name("prot"+str(i))
             rb=IMP.atom.create_rigid_body(mhs[-1])
             print rb.get_name()
             rbs.append(rb)
+            orig_rfs.append(rb.get_reference_frame())
         #set the nesting
         for i in range(2):
             rbs[i].add_member(rbs[i+1])
         #set domino states
+        translation=IMP.algebra.Transformation3D(IMP.algebra.Rotation3D(1.,0.,0.,0.),IMP.algebra.Vector3D(-5.,0.,0.))
         pst=IMP.domino.ParticleStatesTable()
-        for rb in rbs:
-            states=IMP.domino.NestedRigidBodyStates([IMP.algebra.Transformation3D(IMP.algebra.Rotation3D(1.,0.,0.,0.),IMP.algebra.Vector3D(0.,0.,0.))])
+        for rb in rbs[1:]:
+            states=IMP.domino.NestedRigidBodyStates([translation])
             pst.set_particle_states(rb,states)
+        #set states to the root
+        pst.set_particle_states(rbs[0],IMP.domino.RigidBodyStates([rbs[0].get_reference_frame()]))
         print "sample"
         s=IMP.domino.DominoSampler(mdl, pst)
         cs=s.get_sample()
         print "number of configurations",cs.get_number_of_configurations()
         #TODO - check that the transformations are correct
-
+        cs.load_configuration(0)
+        for i,rb in enumerate(rbs):
+            trans1=rb.get_reference_frame().get_transformation_to()
+            trans2=orig_rfs[i].get_transformation_to()
+            diff_trans=trans1/trans2
+            print rb.get_name(),diff_trans
+            self.assertAlmostEqual(IMP.algebra.get_distance(
+                diff_trans.get_translation(),
+                i*IMP.algebra.Vector3D(-5.,0,0)),0, delta=.05)
 if __name__ == '__main__':
     IMP.test.main()
