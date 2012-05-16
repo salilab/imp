daa08b1d0108e9bd0841e54c76db40703ddf732d
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-05-16 22:21:06 +0000

    add prmf tests
    git-svn-id: https://svn.salilab.org/imp/trunk@14019 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/librmf/test/test_prmf.py
@@ -0,0 +1,42 @@
+import unittest
+import RMF
+import shutil
+
+class Tests(RMF.TestCase):
+    def _show(self, g):
+        for i in range(0, g.get_number_of_children()):
+            print i, g.get_child_name(i), g.get_child_is_group(i)
+    """Test the python code"""
+    def test_perturbed(self):
+        """Test copying a rmf file to a prmf file"""
+        nm= self.get_input_file_name("sink.rmf")
+        onm= self.get_tmp_file_name("sink_out.prmf")
+        f= RMF.open_rmf_file_read_only(nm)
+        of= RMF.create_rmf_file(onm)
+        RMF.copy_structure(f, of)
+        self.assertTrue(RMF.get_equal_structure(f, of, True))
+        nf= f.get_number_of_frames()
+        for i in range(0, nf):
+            RMF.copy_frame(f, of, i, i)
+
+        for i in range(0, nf):
+            self.assertTrue(RMF.get_equal_frame(f, of, i, i, True))
+    def test_simple(self):
+        """Test copying a simple rmf file to prmf"""
+        nm= self.get_input_file_name("simple.rmf")
+        onm= self.get_tmp_file_name("simple.prmf")
+        f= RMF.open_rmf_file_read_only(nm)
+        of= RMF.create_rmf_file(onm)
+        RMF.copy_structure(f, of)
+        #RMF.show_hierarchy(of.get_root_node())
+        of.flush()
+        RMF.show_hierarchy(of.get_root_node())
+        self.assertTrue(RMF.get_equal_structure(f, of, True))
+        nf= f.get_number_of_frames()
+        for i in range(0, nf):
+            RMF.copy_frame(f, of, i, i)
+        for i in range(0, nf):
+            self.assertTrue(RMF.get_equal_frame(f, of, i, i, True))
+
+if __name__ == '__main__':
+    unittest.main()
