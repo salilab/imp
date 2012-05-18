9f0bb1b94733e89925d2ec6baac467b476366b9f
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-05-18 20:26:49 +0000

    attempt to fix static compilation with dependent libraries since scons gets the library inclusion order wrong (or, rather puts them in a useless order)
    git-svn-id: https://svn.salilab.org/imp/trunk@14036 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- scons_tools/dependency/__init__.py
+++ scons_tools/dependency/__init__.py
@@ -9,7 +9,7 @@ def _search_for_deps(context, libname, extra_libs, headers, body, possible_deps)
         lc= extra_libs+possible_deps[0:i]
         #print "Trying "+ str(i) +" with " +str(lc)
         olibs= context.env.get('LIBS', [])
-        context.env.Append(LIBS=lc)
+        context.env.Append(LIBS=[libname]+lc)
         #print context.env['LINKFLAGS']
         #print "checking", libname, lc
         ret=context.sconf.CheckLibWithHeader(libname, header=headers, call=body, language='CXX',
