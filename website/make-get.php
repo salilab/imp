0271b47dff4fc9931a1faf4ae7f6b2091aca9d36
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-05-21 19:33:17 +0000

    Fix warnings with latest php.
    git-svn-id: https://svn.salilab.org/imp/trunk@14067 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- website/make-get.php
+++ website/make-get.php
@@ -3,8 +3,10 @@
 # Populate the HTTP GET array from a command line containing 'key=value'
 # arguments
 foreach ($argv as $arg) {
-  list($key, $val) = explode("=", $arg);
-  $_GET[trim($key)] = trim($val);
+  if ($arg != '-') {
+    list($key, $val) = explode("=", $arg);
+    $_GET[trim($key)] = trim($val);
+  }
 }
 
 ?>
