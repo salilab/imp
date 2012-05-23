074ba93a7d7fc0b98d46782aeb56611d467bb0a5
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-05-23 20:52:51 +0000

    add test showing ref counting problems in directory methods returning ref counted object
    git-svn-id: https://svn.salilab.org/imp/trunk@14099 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/kernel/test/test_temp_scoring.py
@@ -0,0 +1,45 @@
+import IMP
+import IMP.test
+import StringIO
+import random
+
+class DummyRestraint(IMP.Restraint):
+    """Dummy do-nothing restraint"""
+    def __init__(self, ps=[], cs=[]):
+        IMP.Restraint.__init__(self)
+        self.ps=ps
+        self.cs=cs
+    def unprotected_evaluate(self, accum):
+        return 0.
+    def get_version_info(self):
+        return IMP.get_module_version_info()
+    def get_input_particles(self):
+        return self.ps
+    def get_input_containers(self):
+        return self.cs
+
+
+
+
+class Tests(IMP.test.TestCase):
+
+    def test_temp_restraints(self):
+        """Check that scoring functions are cleaned up"""
+        dirchk = IMP.test.RefCountChecker(self)
+        m = IMP.Model()
+        #self.assertRaises(IndexError, m.get_restraint, 0);
+        self.assertEqual(m.get_number_of_restraints(), 0)
+        r = DummyRestraint()
+        r.set_name("dummy")
+        r.set_model(m)
+        dirchk.assert_number(2)
+        print r.evaluate(False)
+        dirchk.assert_number(2)
+        del r
+        dirchk.assert_number(1)
+        m.evaluate(False)
+        del m
+        dirchk.assert_number(0)
+
+if __name__ == '__main__':
+    IMP.test.main()
