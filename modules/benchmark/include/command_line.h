e95bc2d937b22aa8db3a621bc581abb401ed24e9
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-05-23 23:25:40 +0000

    add support for standard command line arguments in benchmarks
    git-svn-id: https://svn.salilab.org/imp/trunk@14105 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/benchmark/include/command_line.h
@@ -0,0 +1,23 @@
+/**
+ *  \file benchmark/command_line.h
+ *  \brief Various utilities for benchmarking
+ *
+ *  Copyright 2007-2012 IMP Inventors. All rights reserved.
+ *
+ */
+
+#ifndef IMPBENCHMARK_COMMAND_LINE_H
+#define IMPBENCHMARK_COMMAND_LINE_H
+
+#include "benchmark_config.h"
+
+IMPBENCHMARK_BEGIN_NAMESPACE
+
+extern IMPBENCHMARKEXPORT bool help;
+extern IMPBENCHMARKEXPORT bool profile_benchmark;
+extern IMPBENCHMARKEXPORT bool profile_all;
+
+IMPBENCHMARK_END_NAMESPACE
+
+
+#endif  /* IMPBENCHMARK_COMMAND_LINE_H */
---
+++ modules/benchmark/src/command_line.cpp
@@ -0,0 +1,13 @@
+/** \file utility.cpp Benchmarking utilties
+ *
+ * Copyright 2007-2012 IMP Inventors. All rights reserved.
+ */
+
+#include <IMP/benchmark/command_line.h>
+IMPBENCHMARK_BEGIN_NAMESPACE
+
+bool help=false;
+bool profile_benchmark=false;
+bool profile_all=false;
+
+IMPBENCHMARK_END_NAMESPACE
