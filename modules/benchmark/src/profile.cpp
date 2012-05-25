699ff74bd317f5976fdf11c3c9f57c67ecb9e8c1
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-05-25 00:44:30 +0000

    support more than one profiling session per process run
    git-svn-id: https://svn.salilab.org/imp/trunk@14119 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/benchmark/src/profile.cpp
+++ modules/benchmark/src/profile.cpp
@@ -8,20 +8,41 @@
 #include <google/profiler.h>
 #endif
 
+#include <boost/format.hpp>
+#include <IMP/base/check_macros.h>
+
 IMPBENCHMARK_BEGIN_NAMESPACE
 
 #ifdef IMP_BENCHMARK_USE_GOOGLE_PERFTOOLS_PROFILE
 namespace {
-  std::string profname="imp.prof";
+std::string profname="imp.%1%.prof";
+int last_prof=-1;
 }
 
 void set_profile_name(std::string name) {
   profname=name;
+  last_prof=-1;
 }
 
 void set_is_profiling(bool tf) {
   if (tf) {
-    ProfilerStart(profname.c_str());
+    ++last_prof;
+    std::string name;
+    if (std::find(profname.begin(), profname.end(), '%') != profname.end()) {
+      std::ostringstream oss;
+      try {
+        oss << boost::format(profname)
+            % last_prof;
+        name=oss.str();
+      } catch(...) {
+        IMP_THROW("Invalid format specified in profile name, should be %1%: "
+                  << profname,
+                  ValueException);
+      }
+    } else {
+      name=profname;
+    }
+    ProfilerStart(name.c_str());
   } else {
     ProfilerStop();
   }
