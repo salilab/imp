a88b381daade9802979b226314fca57f96033f4f
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-05-29 04:32:09 +0000

    doc files
    git-svn-id: https://svn.salilab.org/imp/trunk@14171 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/include/force_fields.h
+++ modules/atom/include/force_fields.h
@@ -1,5 +1,5 @@
 /**
- * \file force_fields.h \brief
+ * \file force_fields.h \brief Define functions to add bonds and radii to atoms.
  *
  * Copyright 2007-2012 IMP Inventors. All rights reserved.
  *
