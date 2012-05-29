7fa421f1a749e774e18cf8b8165443ec53bdc6b6
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-05-29 23:21:29 +0000

    fix see also link
    git-svn-id: https://svn.salilab.org/imp/trunk@14195 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- tools/maintenance/container_templates/restraint.hpp
+++ tools/maintenance/container_templates/restraint.hpp
@@ -31,7 +31,7 @@ IMPCONTAINER_BEGIN_NAMESPACE
     \pythonexample{restrain_in_sphere}
     \pythonexample{nonbonded_interactions}
 
-    \see CLASSNAMERestraint
+    \see IMP::core::CLASSNAMERestraint
  */
 class IMPCONTAINEREXPORT CLASSNAMEsRestraint :
 #if defined(SWIG) || defined(IMP_DOXYGEN)
