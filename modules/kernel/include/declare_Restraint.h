246e30b9fc96be57e2dd94d015a00c80a9f19ad2
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-05-29 05:21:45 +0000

    fix file name
    git-svn-id: https://svn.salilab.org/imp/trunk@14176 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/kernel/include/declare_Restraint.h
+++ modules/kernel/include/declare_Restraint.h
@@ -1,5 +1,5 @@
 /**
- *  \File IMP/declare/Restraint.h
+ *  \File IMP/declare_Restraint.h
  *  \brief Abstract base class for all restraints.
  *
  *  Copyright 2007-2012 IMP Inventors. All rights reserved.
