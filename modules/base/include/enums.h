cd4ec890830dd9e616b957615eb923cf7b62b418
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-05-30 00:05:10 +0000

    clean up enum docs
    git-svn-id: https://svn.salilab.org/imp/trunk@14211 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/base/include/enums.h
+++ modules/base/include/enums.h
@@ -14,45 +14,44 @@ namespace IMP {
 
 namespace base {
 //! The log levels supported by \imp
-/**
-    DEFAULT is only local logging (like in IMP::Object); it means to use
-    the global log level.
-
-    VERBOSE prints very large amounts of information. It should be enough
-    to allow the computational flow to be understood.
-
-    TERSE prints a few lines per restraint or per state each time
-    the score is evaluated.
-
-    PROGRESS show how large calculations are progressing (eg samplers)
-    but otherwise just show warnings
-
-    WARNING prints only warnings.
-
-    MEMORY prints information about allocations and deallocations to debug
-    memory issues.
- */
-enum LogLevel {DEFAULT=-1, SILENT=0, WARNING=1, PROGRESS=2,
-               TERSE=3, VERBOSE=4,
-               MEMORY=5
+enum LogLevel {
+  /** Use to specify that the global log level should be used
+      (eg  in IMP::base::Object::set_log_level())*/
+  DEFAULT=-1,
+  /** Do not output any text.*/
+  SILENT=0,
+  /** Output only warnings.*/
+  WARNING=1,
+  /** Output only progress meter style displays and occasional printouts
+      when switching phases of work.*/
+  PROGRESS=2,
+  /** Output a line or two per evaluation call.*/
+  TERSE=3,
+  /** Produce copious output to allow someone to trace through
+      the computation.
+  */
+  VERBOSE=4,
+  /** Log memory allocations and frees.*/
+  MEMORY=5
 #ifndef IMP_DOXYGEN
-               , ALL_LOG
+  , ALL_LOG
 #endif
 };
 
 // duplicated in IMP_base_exception.i otherwise IMP_base_exception.i
 // is processed without seeing this definition yet.
 #ifndef SWIG
-//! Determine the level of runtime checks performed
-/** - NONE means that minimial checks are
-    used.
-    - USAGE means that checks of input values to functions
-    and classes are verified.
-    - USAGE_AND_INTERNAL adds checks that \imp itself is
-    correct. Turn these on if you suspect an \imp bug or are
-    developing Restraints or other \imp classes.
-*/
-enum CheckLevel {DEFAULT_CHECK=-1, NONE=0, USAGE=1, USAGE_AND_INTERNAL=2};
+//! Specify the level of runtime checks performed
+enum CheckLevel {
+  /** Use the default check level (eg IMP::base::Object::set_check_level()).*/
+  DEFAULT_CHECK=-1,
+  /** Perform no runtime checks.*/
+  NONE=0,
+  /** Perform checks that \imp is being called correctly.*/
+  USAGE=1,
+  /** Check internal \imp invariants. This is to be used for debugging
+      \imp.*/
+  USAGE_AND_INTERNAL=2};
 #endif
 }
 } //IMPBASE_END_NAMESPACE
