e4b81624b7da758f7926c0f1dd89e0958d4b1b8c
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-06-01 20:22:54 +0000

    pull object cast out into separate header
    git-svn-id: https://svn.salilab.org/imp/trunk@14284 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/base/include/Object.h
+++ modules/base/include/Object.h
@@ -11,40 +11,11 @@
 
 #include "base_config.h"
 #include "declare_Object.h"
-
+#include "object_cast.h"
 
 IMPBASE_BEGIN_NAMESPACE
 
-/*#if !defined(IMP_DOXYGEN) && !defined(IMP_SWIG)
-inline std::ostream &operator<<(std::ostream &out, const Object& o) {
-  o.show(out);
-  return out;
-}
-#endif
-*/
-
-
-/** Up (or down) cast an \imp Object-derived class. If the cast
-    does not succeed a ValueException will be thrown. Use a
-    \c dynamic_cast if you prefer to have a nullptr returned.
- */
-template <class O, class I>
-inline O* object_cast(I *o) {
-  O *ret= dynamic_cast<O*>(o);
-  if (!ret) {
-    if (!o) {
-      IMP_THROW("Cannot cast nullptr pointer to desired type.", ValueException);
-    } else {
-      IMP_THROW("Object " << o->get_name() << " cannot be cast to "
-                << "desired type.", ValueException);
-    }
-  }
-  return ret;
-}
-
-
 IMPBASE_END_NAMESPACE
 
-#include "SetLogState.h"
 
 #endif  /* IMPBASE_OBJECT_H */
