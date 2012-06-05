4df6f699c67a881314dbfd7e87feb1ede7f7266b
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-06-05 19:29:04 +0000

    print out warnings when no frames have been written or read
    git-svn-id: https://svn.salilab.org/imp/trunk@14331 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/rmf/include/links.h
+++ modules/rmf/include/links.h
@@ -40,6 +40,7 @@ IMP_OBJECTS(LoadLink, LoadLinks);
     LoadLinks must not save any handles to RMF objects.
 */
 class IMPRMFEXPORT LoadLink: public base::Object {
+  bool frame_loaded_;
  protected:
   virtual void do_load(RMF::FileConstHandle fh, unsigned int frame)=0;
   LoadLink(std::string name);
@@ -48,7 +49,9 @@ class IMPRMFEXPORT LoadLink: public base::Object {
     IMP_OBJECT_LOG;
     set_was_used(true);
     do_load(fh, frame);
+    frame_loaded_=true;
   }
+  IMP_REF_COUNTED_NONTRIVIAL_DESTRUCTOR(LoadLink);
 };
 /** Manage a link between an \imp object and a part
     of the RMF file. This allows conformations to be
@@ -57,15 +60,22 @@ class IMPRMFEXPORT LoadLink: public base::Object {
     SaveLinks must not save any handles to RMF objects.
 */
 class IMPRMFEXPORT SaveLink: public base::Object {
+  bool frame_saved_;
  protected:
   virtual void do_save(RMF::FileHandle hf, unsigned int frame)=0;
   SaveLink(std::string name);
- public:
+public:
   void save(RMF::FileHandle fh, unsigned int frame) {
     IMP_OBJECT_LOG;
+    IMP_USAGE_CHECK(frame<= fh.get_number_of_frames()+1,
+                    "Trying to save to frame " << frame
+                    << " when there are only "
+                    << fh.get_number_of_frames() << " frames");
     set_was_used(true);
     do_save(fh, frame);
+    frame_saved_=true;
   }
+  IMP_REF_COUNTED_NONTRIVIAL_DESTRUCTOR(SaveLink);
 };
 
 
--- modules/rmf/src/links.cpp
+++ modules/rmf/src/links.cpp
@@ -9,8 +9,26 @@
 #include <IMP/rmf/links.h>
 IMPRMF_BEGIN_NAMESPACE
 
-LoadLink::LoadLink(std::string name): base::Object(name){}
-SaveLink::SaveLink(std::string name): base::Object(name){}
+LoadLink::LoadLink(std::string name): base::Object(name){
+  frame_loaded_=false;
+}
+LoadLink::~LoadLink() {
+  if (!frame_loaded_) {
+    IMP_WARN("No frames were loaded from file \""
+             << get_name() << "\" even though objects were linked or created."
+             << std::endl);
+  }
+}
+SaveLink::SaveLink(std::string name): base::Object(name){
+  frame_saved_=false;
+}
+SaveLink::~SaveLink() {
+  if (!frame_saved_) {
+    IMP_WARN("No frames were saved to file \""
+             << get_name() << "\" even though objects were added."
+             << std::endl);
+  }
+}
 
 
 
