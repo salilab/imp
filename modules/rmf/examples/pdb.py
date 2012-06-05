8e4dbaa8e81646d2e146268d2427130b4c7ac103
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-06-05 19:28:58 +0000

    don't forget to save a frame
    git-svn-id: https://svn.salilab.org/imp/trunk@14330 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/rmf/examples/pdb.py
+++ modules/rmf/examples/pdb.py
@@ -17,6 +17,9 @@ rh = RMF.create_rmf_file(tfn)
 # add the hierarchy to the file
 IMP.rmf.add_hierarchies(rh, [h])
 
+# add the current configuration to the file as frame 0
+IMP.rmf.save_frame(rh, 0)
+
 # change a coordinate
 IMP.core.XYZ(IMP.atom.get_leaves(h)[0]).set_x(0)
 
@@ -32,7 +35,9 @@ rh= RMF.open_rmf_file(tfn)
 # hps is a list with one element which is a copy of h
 hps= IMP.rmf.create_hierarchies(rh, m)
 
+IMP.atom.show_molecular_hierarchy(hps[0])
+
 # load the second configuration into hps
-IMP.rmf.load_frame(rh, 1)
+IMP.rmf.load_frame(rh, 0)
 
 print "Try running hdf5_display or hdf5_show on", tfn
