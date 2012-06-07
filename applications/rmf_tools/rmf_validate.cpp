2e7a92251c4f1938b4fa2ca9053c0e2ce39334f6
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-06-07 21:01:44 +0000

    add rmf_validate tool
    git-svn-id: https://svn.salilab.org/imp/trunk@14369 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ applications/rmf_tools/rmf_validate.cpp
@@ -0,0 +1,25 @@
+/**
+ * Copyright 2007-2012 IMP Inventors. All rights reserved.
+ */
+#include <RMF/FileConstHandle.h>
+#include <RMF/utility.h>
+#include "common.h"
+#include <sstream>
+
+std::string description
+="Validate a saved RMF file.";
+
+
+
+
+int main(int argc, char **argv) {
+  try {
+    IMP_ADD_INPUT_FILE("rmf");
+    process_options(argc, argv);
+    RMF::FileConstHandle rh= RMF::open_rmf_file_read_only(input);
+    rh.validate(std::cerr);
+    return 0;
+  } catch (const std::exception &e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+  }
+}
