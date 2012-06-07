a3f60f941615641d5c193c5a4b42bc3352d6b811
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-06-07 21:01:21 +0000

    add the google profiler directly to RMF module
    git-svn-id: https://svn.salilab.org/imp/trunk@14367 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/librmf/src/profile.cpp
@@ -0,0 +1,57 @@
+/** \file utility.cpp Benchmarking utilties
+ *
+ * Copyright 2007-2012 IMP Inventors. All rights reserved.
+ */
+#include <RMF/profile.h>
+#ifdef RMF_USE_GOOGLE_PERFTOOLS_PROFILE
+#include <google/profiler.h>
+#endif
+
+#include <boost/format.hpp>
+#include <RMF/infrastructure_macros.h>
+#include <sstream>
+
+namespace RMF {
+namespace {
+std::string profname="RMF.%1%.prof";
+int last_prof=-1;
+}
+
+void set_profile_name(std::string name) {
+  profname=name;
+  last_prof=-1;
+}
+
+
+#ifdef RMF_USE_GOOGLE_PERFTOOLS_PROFILE
+void set_is_profiling(bool tf) {
+  if (tf) {
+    ++last_prof;
+    std::string name;
+    if (std::find(profname.begin(), profname.end(), '%') != profname.end()) {
+      std::ostringstream oss;
+      try {
+        oss << boost::format(profname)
+            % last_prof;
+        name=oss.str();
+      } catch(...) {
+      IMP_RMF_THROW("Invalid format specified in profile name, should be %1%",
+                    UsageException);
+      }
+    } else {
+      name=profname;
+    }
+    ProfilerStart(name.c_str());
+  } else {
+    ProfilerStop();
+  }
+}
+#else
+void set_is_profiling(bool tf) {
+  if (tf) {
+    std::cerr << "Google Prof Tools were not found, no profiling available,\n";
+  }
+}
+#endif
+
+}
