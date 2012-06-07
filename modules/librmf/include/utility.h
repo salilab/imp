0b46395fa6fe5ef5e86bccafabab3cfac3e313d4
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-06-07 00:07:19 +0000

    provide utilities to only act on part of a file
    git-svn-id: https://svn.salilab.org/imp/trunk@14352 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/librmf/include/utility.h
+++ modules/librmf/include/utility.h
@@ -12,11 +12,17 @@
 #include "RMF_config.h"
 #include "internal/intrusive_ptr_object.h"
 #include "internal/errors.h"
+#include "NodeConstHandle.h"
 namespace RMF {
 
   class FileConstHandle;
   class FileHandle;
 
+  /** \name Copy functions
+
+      \note These functions make use of the association field.
+      @{
+  */
 /** Turn on and off printing of hdf5 error messages. They can help in
       diagnostics, but, for the moment, can only be output to standard
       error and so are off by default.
@@ -30,12 +36,24 @@ inline void set_show_hdf5_errors(bool tf) {
       file to another.*/
 RMFEXPORT void copy_structure(FileConstHandle input, FileHandle output);
 
+  /** Add the associations between the input and output needed to
+      use the other copy functions.*/
+  RMFEXPORT void link_structure(FileConstHandle input, FileHandle output);
 
-  /** Copy the hierarchy structure and set structure from one rmf
-      file to another.*/
+  /** Copy the hierarchy structure entailed by the set of nodes passed
+      for the input. They most form a coherent subhierarchy of their
+      input file (no nodes whose parents aren't included).*/
+RMFEXPORT void copy_structure(const NodeConstHandles& input, FileHandle output);
+
+
+  /** Copy the data of a single frame from between two files. Parts missing
+      in the output file will be skipped.*/
   RMFEXPORT void copy_frame(FileConstHandle input, FileHandle output,
                             unsigned int inframe,
                             unsigned int outframe);
+  /** Copy all values of the given nodes to the output file.*/
+  RMFEXPORT void copy_values( FileConstHandle input, FileHandle output);
+  /** @} */
 
   /** Return true of the two have the same structure.*/
   RMFEXPORT bool get_equal_structure(FileConstHandle input,
@@ -45,7 +63,6 @@ RMFEXPORT void copy_structure(FileConstHandle input, FileHandle output);
   RMFEXPORT bool get_equal_frame(FileConstHandle input, FileConstHandle out,
                                  unsigned int inframe, unsigned int outframe,
                                  bool print_diff=false);
-
 
 } /* namespace RMF */
 
