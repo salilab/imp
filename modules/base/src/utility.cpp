9abe9b3d7643b94dbeca3e60f78e969ca9826276
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-06-08 21:03:04 +0000

    expose function to generate unique names
    git-svn-id: https://svn.salilab.org/imp/trunk@14380 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/base/src/utility.cpp
@@ -0,0 +1,35 @@
+/**
+ *  \file Log.cpp   \brief Logging and error reporting support.
+ *
+ *  Copyright 2007-2012 IMP Inventors. All rights reserved.
+ *
+ */
+
+#include "IMP/base/utility.h"
+#include <IMP/compatibility/map.h>
+#include <boost/format.hpp>
+#include "IMP/base/internal/static.h"
+IMPBASE_BEGIN_NAMESPACE
+std::string get_unique_name(std::string name) {
+  if (std::find(name.begin(), name.end(), '%') != name.end()) {
+    if (internal::object_type_counts.find(name)
+        == internal::object_type_counts.end()) {
+      internal::object_type_counts[name]=0;
+    }
+    std::ostringstream oss;
+    try {
+      oss << boost::format(name)
+        % internal::object_type_counts.find(name)->second;
+    } catch(...) {
+      IMP_THROW("Invalid format specified in name, should be %1%: "<< name,
+                ValueException);
+    }
+    ++internal::object_type_counts.find(name)->second;
+    name= oss.str();
+    return name;
+  } else {
+    return name;
+  }
+}
+
+IMPBASE_END_NAMESPACE
