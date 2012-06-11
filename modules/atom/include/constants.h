820c13747db363a8ee9abb95556f980d32c72cd3
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-06-11 21:27:27 +0000

    add constants header in atom
    git-svn-id: https://svn.salilab.org/imp/trunk@14414 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/atom/include/constants.h
@@ -0,0 +1,23 @@
+/**
+ * \file constants.h \brief Define the elements used in \imp.
+ *
+ * Copyright 2007-2012 IMP Inventors. All rights reserved.
+ *
+ */
+#ifndef IMPATOM_CONSTANTS_H
+#define IMPATOM_CONSTANTS_H
+
+#include "atom_config.h"
+
+IMPATOM_BEGIN_NAMESPACE
+/** Return kT for a given temperature.
+    Value take from
+    \extern{http://http://en.wikipedia.org/wiki/Boltzmann_constant, Wikipedia}.
+*/
+inline double get_kt(double T) {
+  return 0.0019872041*T;
+}
+
+IMPATOM_END_NAMESPACE
+
+#endif /* IMPATOM_CONSTANTS_H */
