ec120d156aaa9d0e8a8b3a4221d40e8a4ad143ac
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-06-12 19:57:11 +0000

    Tidy up test, and skip it if we have no profiling support.
    git-svn-id: https://svn.salilab.org/imp/trunk@14437 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/benchmark/test/test_profile.py
+++ modules/benchmark/test/test_profile.py
@@ -1,17 +1,18 @@
 import IMP.benchmark
 import IMP.test
-import sys
-import os.path
-import random
-from StringIO import StringIO
+import os
 
 class Tests(IMP.test.TestCase):
     def test_log_targets(self):
         """Test that profiler produces a file"""
+        if not IMP.benchmark.has_gperftools \
+           and not IMP.benchmark.has_googleperftools:
+            self.skipTest("profiling not available")
+
         nm= self.get_tmp_file_name("prof.pprof")
         prof= IMP.benchmark.Profiler(nm)
         del prof
-        self.assert_(os.path.exists(nm))
+        os.unlink(nm)
 
 if __name__ == '__main__':
     IMP.test.main()
