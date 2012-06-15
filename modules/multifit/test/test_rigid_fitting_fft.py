4de9d04c674b750b6b849651f50ee8ddf07b85ef
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-06-15 21:41:07 +0000

    Make test work with current FFT based rigid fitting interface. Use a toy model to run in a reasonable time.
    git-svn-id: https://svn.salilab.org/imp/trunk@14503 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/multifit/test/test_rigid_fitting_fft.py
+++ modules/multifit/test/test_rigid_fitting_fft.py
@@ -7,38 +7,33 @@ import IMP.atom
 import IMP.multifit
 
 
-class ProteinRigidFittingTest(IMP.test.TestCase):
-    """Class to test EM correlation restraint"""
+class FFTRigidFittingTests(IMP.test.TestCase):
+    """Test FFT-based rigid fitting"""
 
     def load_density_map(self):
         mrw = IMP.em.MRCReaderWriter()
-        self.scene = IMP.em.read_map(self.get_input_file_name("1f7dA00_8.mrc"), mrw)
-        self.scene.get_header_writable().set_resolution(8.)
-        self.scene.update_voxel_size(1.5)
-    def load_protein(self,pdb_filename):
+        scene = IMP.em.read_map(self.get_input_file_name("3points.mrc"), mrw)
+        scene.get_header_writable().set_resolution(8.)
+        scene.update_voxel_size(1.5)
+        return scene
+
+    def load_protein(self, model, pdb_filename):
         self.mp= IMP.atom.read_pdb(self.open_input_file(pdb_filename),
-                              self.imp_model, IMP.atom.CAlphaPDBSelector())
+                                   model, IMP.atom.CAlphaPDBSelector())
         IMP.atom.add_radii(self.mp)
         self.mp_ref= IMP.atom.read_pdb(self.open_input_file(pdb_filename),
-                              self.imp_model, IMP.atom.CAlphaPDBSelector())
+                                       model, IMP.atom.CAlphaPDBSelector())
         IMP.atom.add_radii(self.mp_ref)
 
-        self.radius_key = IMP.core.XYZR.get_radius_key()
-        self.weight_key = IMP.atom.Mass.get_mass_key()
         self.ps =IMP.core.get_leaves(self.mp)
-        self.rb=IMP.atom.setup_as_rigid_body(self.mp)
-        self.refiner=IMP.core.LeavesRefiner(IMP.atom.Hierarchy.get_traits())
-    def setUp(self):
-        """Build test model and optimizer"""
-        IMP.test.TestCase.setUp(self)
-        IMP.set_log_level(IMP.VERBOSE)#SILENT)
-        self.imp_model = IMP.Model()
-        self.load_density_map()
-        self.load_protein("1f7dA00.pdb")
-
+        self.rb=IMP.atom.create_rigid_body(self.mp)
 
     def test_fft_based_rigid_fitting(self):
-        """test fft based fitting"""
+        """Test FFT-based rigid fitting"""
+        model = IMP.Model()
+        scene = self.load_density_map()
+        # Use an artificial protein and map containing only 3 atoms, for speed
+        self.load_protein(model, "3points.pdb")
         #randomize protein placement
         rand_t = IMP.algebra.Transformation3D(
             IMP.algebra.get_random_rotation_3d(),
@@ -49,72 +44,25 @@ class ProteinRigidFittingTest(IMP.test.TestCase):
 
         xyz=IMP.core.XYZs(self.ps)
         IMP.core.transform(self.rb,rand_t)
-        #IMP.atom.write_pdb(self.mp,"translated.pdb")
         xyz_ref=IMP.core.XYZs(IMP.core.get_leaves(self.mp_ref))
         #fit protein
         fs = IMP.multifit.fft_based_rigid_fitting(
-               self.rb,self.refiner,self.scene,0.15,
-               IMP.algebra.get_uniform_cover_rotations_3d(10),1,False,False)
+               self.mp,scene,0.2)
         #check that the rmsd to the reference is low
-        #self.assertEqual(fs.get_number_of_solutions(), 24)
-        sols=fs.get_solutions()
-        print "number of solutions:",sols.get_number_of_solutions()
         best_rmsd=999.
-        for i in range(sols.get_number_of_solutions()):
-            fit_t = sols.get_transformation(i)
+        for f in fs:
+            fit_t = f.get_fit_transformation()
             fit_t_inv = fit_t.get_inverse()
             IMP.core.transform(self.rb,fit_t)
 
             rmsd=IMP.atom.get_rmsd(xyz_ref,xyz)
             #check that the centroid is in the map
             centroid=IMP.core.get_centroid(xyz)
-            print centroid
-            #IMP.atom.write_pdb(self.mp,"fitted_"+str(i)+".pdb")
-            self.assertEqual(self.scene.is_part_of_volume(centroid),True)
-            print "====rmsd: ",rmsd, " score: ",sols.get_score(i)
+            self.assertTrue(scene.is_part_of_volume(centroid))
             if best_rmsd>rmsd:
                 best_rmsd=rmsd
             IMP.core.transform(self.rb,fit_t_inv)
-        #self.assertLess(best_rmsd,15.)
-
-
-    def test_fft_based_rigid_fitting_translation_with_gmm(self):
-        """test fft based fitting"""
-        #randomize protein placement
-        rand_t = IMP.algebra.Transformation3D(
-            IMP.algebra.get_identity_rotation_3d(),
-            IMP.algebra.get_random_vector_in(
-              IMP.algebra.BoundingBox3D(
-                IMP.algebra.Vector3D(-10.,-10.,-10.),
-                IMP.algebra.Vector3D(10.,10.,10.))))
-        xyz=IMP.core.XYZs(self.ps)
-        IMP.core.transform(self.rb,rand_t)
-        xyz_ref=IMP.core.XYZs(IMP.core.get_leaves(self.mp_ref))
-        #fit protein
-        fs = IMP.multifit.fft_based_rigid_fitting(
-               self.rb,self.refiner,self.scene,0.15,
-               [IMP.algebra.get_identity_rotation_3d()],10,False,True)
-        #check that the rmsd to the reference is low
-        #self.assertEqual(fs.get_number_of_solutions(), 24)
-        sols=fs.get_solutions()
-        print "number of solutions:",sols.get_number_of_solutions()
-        best_rmsd=999.
-        for i in range(sols.get_number_of_solutions()):
-            fit_t = sols.get_transformation(i)
-            fit_t_inv = fit_t.get_inverse()
-            IMP.core.transform(self.rb,fit_t)
-
-            rmsd=IMP.atom.get_rmsd(xyz_ref,xyz)
-            #check that the centroid is in the map
-            centroid=IMP.core.get_centroid(xyz)
-            print centroid
-            #IMP.atom.write_pdb(self.mp,"fitted_"+str(i)+".pdb")
-            self.assertEqual(self.scene.is_part_of_volume(centroid),True)
-            if best_rmsd>rmsd:
-                best_rmsd=rmsd
-            IMP.core.transform(self.rb,fit_t_inv)
-        self.assertLess(best_rmsd,3.)
-
+        self.assertLess(best_rmsd, 3.5)
 
 if __name__ == '__main__':
     IMP.test.main()
