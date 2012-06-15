29311267b3da917a36ca11eb6000859088a65115
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-06-15 20:10:51 +0000

    Remove debugging prints and unused typedefs; catch uninformative Boost bad_lexical_cast exceptions and provide more useful error messages.
    git-svn-id: https://svn.salilab.org/imp/trunk@14492 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/multifit/src/proteomics_reader.cpp
+++ modules/multifit/src/proteomics_reader.cpp
@@ -15,7 +15,6 @@ IMPMULTIFIT_BEGIN_NAMESPACE
 namespace {
   bool is_protein_line(const std::string &line,
                        ProteomicsData *) {
-  typedef boost::split_iterator<std::string::iterator> string_split_iterator;
   IMP_USAGE_CHECK(line.size() > 0,"no data to parse"<<std::endl);
   IMP_LOG(VERBOSE,"going to parse:"<<line);
   std::vector<std::string> line_split;
@@ -32,8 +31,6 @@ namespace {
   bool is_interaction_line(const std::string &line,
                            ProteomicsData *dp) {
   int num_allowed_violations=0;
-  std::cout<<"INTERATION LINE:"<<line;
-  typedef boost::split_iterator<std::string::iterator> string_split_iterator;
   IMP_USAGE_CHECK(line.size() > 0,"no data to parse"<<std::endl);
   IMP_LOG(VERBOSE,"going to parse:"<<line);
   std::vector<std::string> line_split;
@@ -41,7 +38,6 @@ namespace {
   //split returns zero length entires as well
   line_split.erase( std::remove_if(line_split.begin(),line_split.end(),
     boost::bind( &std::string::empty, _1 ) ),line_split.end() );
-  std::cout<<"Size :: "<<line_split.size()<<std::endl;
   if ((line_split.size() != 1) && (line_split.size() != 2))
       return false;
   if (boost::lexical_cast<std::string>(line_split[0]) != "interactions")
@@ -49,7 +45,6 @@ namespace {
   if (line_split.size()==2) {
     num_allowed_violations=boost::lexical_cast<int>(line_split[1]);
   }
-  std::cout<<"num_allowed_violations:"<<num_allowed_violations<<std::endl;
   dp->set_num_allowed_violated_interactions(num_allowed_violations);
   return true;
   }
@@ -58,8 +53,6 @@ namespace {
   bool is_xlink_line(const std::string &line,
                      ProteomicsData *dp) {
     int num_allowed_violations=0;
-    std::cout<<"XLINK LINE:"<<line;
-    typedef boost::split_iterator<std::string::iterator> string_split_iterator;
   IMP_USAGE_CHECK(line.size() > 0,"no data to parse"<<std::endl);
   IMP_LOG(VERBOSE,"going to parse:"<<line);
   std::vector<std::string> line_split;
@@ -73,7 +66,6 @@ namespace {
   if (line_split.size()==2) {
     num_allowed_violations=boost::lexical_cast<int>(line_split[1]);
   }
-  std::cout<<"num_allowed_violations:"<<num_allowed_violations<<std::endl;
   dp->set_num_allowed_violated_cross_links(num_allowed_violations);
   return true;
   }
@@ -81,8 +73,6 @@ namespace {
   bool is_ev_line(const std::string &line,
                   ProteomicsData *dp) {
   int num_allowed_violations=0;
-  std::cout<<"EV LINE:"<<line;
-  typedef boost::split_iterator<std::string::iterator> string_split_iterator;
   IMP_USAGE_CHECK(line.size() > 0,"no data to parse"<<std::endl);
   IMP_LOG(VERBOSE,"going to parse:"<<line);
   std::vector<std::string> line_split;
@@ -96,152 +86,180 @@ namespace {
   if (line_split.size()==2) {
     num_allowed_violations=boost::lexical_cast<int>(line_split[1]);
   }
-  std::cout<<"num_allowed_violations:"<<num_allowed_violations<<std::endl;
   dp->set_num_allowed_violated_ev(num_allowed_violations);
   return true;
+}
+
+void error_xlink_line(const std::string &line)
+{
+  IMP_THROW("Improperly formatted cross link line: >>" << line << "<<"
+            << " The format of the line should be: "
+            << "|0/1|prot-name|residue-number|prot-name|residue-number|"
+            << "linker length|", ValueException);
 }
 
 void parse_xlink_line(
      const std::string &line,
      ProteomicsData *dp){
-  std::cout<<"parse_xlink_line:"<<line<<std::endl;
-  typedef boost::split_iterator<std::string::iterator> string_split_iterator;
-  IMP_USAGE_CHECK(line.size() !=6,
-      "no data to parse. The format of the line should be:\n"<<
-       "|0/1|prot-name|residue-number|prot-name|residue-number|"<<
-       "linker length|\n");
   IMP_LOG(VERBOSE,"going to parse:"<<line);
   std::vector<std::string> line_split;
   boost::split(line_split, line, boost::is_any_of("|"));
   //split returns zero length entires as well
   line_split.erase( std::remove_if(line_split.begin(),line_split.end(),
     boost::bind( &std::string::empty, _1 ) ),line_split.end() );
-  std::cout<<"PARSE:"<<line_split.size()<<std::endl;
-  bool use_in_jt = true;
-  if (boost::lexical_cast<int>(line_split[0])==0) {
-    use_in_jt=false;
+  if (line_split.size() != 6) {
+    error_xlink_line(line);
   }
-  std::string name1 =  boost::lexical_cast<std::string>(line_split[1]);
-  int prot1_ind = dp->find(name1);
-  int res1_ind= boost::lexical_cast<int>(line_split[2]);
-  std::string name2 =  boost::lexical_cast<std::string>(line_split[3]);
-  int prot2_ind = dp->find(name2);
-  int res2_ind= boost::lexical_cast<int>(line_split[4]);
-  Float linker_len=boost::lexical_cast<double>(line_split[5]);
-  std::cout<<"XLINK between "<<name1<<" "<<name2<<std::endl;
-  IMP_USAGE_CHECK(prot1_ind != -1,
-                  "The protein "<<name1<<
-                  " was not specified in the proteins list"<<std::endl);
-  IMP_USAGE_CHECK(prot2_ind != -1,
-                  "The protein "<<name2<<
-                  " was not specified in the proteins list"<<std::endl);
-  dp->add_cross_link_interaction(prot1_ind,res1_ind,prot2_ind,res2_ind,
-                                 use_in_jt, linker_len);
+  bool use_in_jt = true;
+
+  try {
+    if (boost::lexical_cast<int>(line_split[0])==0) {
+      use_in_jt=false;
+    }
+    std::string name1 =  boost::lexical_cast<std::string>(line_split[1]);
+    int prot1_ind = dp->find(name1);
+    int res1_ind= boost::lexical_cast<int>(line_split[2]);
+    std::string name2 =  boost::lexical_cast<std::string>(line_split[3]);
+    int prot2_ind = dp->find(name2);
+    int res2_ind= boost::lexical_cast<int>(line_split[4]);
+    Float linker_len=boost::lexical_cast<double>(line_split[5]);
+    IMP_USAGE_CHECK(prot1_ind != -1,
+                    "The protein "<<name1<<
+                    " was not specified in the proteins list"<<std::endl);
+    IMP_USAGE_CHECK(prot2_ind != -1,
+                    "The protein "<<name2<<
+                    " was not specified in the proteins list"<<std::endl);
+    dp->add_cross_link_interaction(prot1_ind,res1_ind,prot2_ind,res2_ind,
+                                   use_in_jt, linker_len);
+  } catch (boost::bad_lexical_cast &e) {
+    error_xlink_line(line);
+  }
+}
+
+void error_ev_line(const std::string &line)
+{
+  IMP_THROW("Improperly formatted ev line: >>" << line << "<<"
+            << " The format should be: |prot1|prot2|", ValueException);
 }
 
 void parse_ev_line(
      const std::string &line,
      ProteomicsData *dp){
-  std::cout<<"=============="<<std::endl;
-  std::cout<<"parse_ev_line:"<<line<<"|"<<line.size()<<std::endl;
-  typedef boost::split_iterator<std::string::iterator> string_split_iterator;
-  IMP_USAGE_CHECK(line.size() > 2,
-     "no data to parse. the last two tabs should contain header data\n");
   IMP_LOG(VERBOSE,"going to parse:"<<line);
   std::vector<std::string> line_split;
   boost::split(line_split, line, boost::is_any_of("|"));
   //split returns zero length entires as well
   line_split.erase( std::remove_if(line_split.begin(),line_split.end(),
     boost::bind( &std::string::empty, _1 ) ),line_split.end() );
-  std::cout<<"PARSE:"<<line_split.size()<<std::endl;
-  std::string name1 =  boost::lexical_cast<std::string>(line_split[0]);
-  std::string name2 =  boost::lexical_cast<std::string>(line_split[1]);
-  int prot1_ind = dp->find(name1);
-  int prot2_ind = dp->find(name2);
-  std::cout<<"EV restraint between "<<name1<<" "<<name2<<std::endl;
-  IMP_USAGE_CHECK(prot1_ind != -1,
-                  "The protein "<<name1<<
-                  " was not specified in the proteins list"<<std::endl);
-  IMP_USAGE_CHECK(prot2_ind != -1,
-                  "The protein "<<name2<<
-                  " was not specified in the proteins list"<<std::endl);
-  //todo - for now the residue index is not used
-  dp->add_ev_pair(prot1_ind,prot2_ind);
+  if (line_split.size() < 2) {
+    error_ev_line(line);
+  }
+
+  try {
+    std::string name1 =  boost::lexical_cast<std::string>(line_split[0]);
+    std::string name2 =  boost::lexical_cast<std::string>(line_split[1]);
+    int prot1_ind = dp->find(name1);
+    int prot2_ind = dp->find(name2);
+    IMP_USAGE_CHECK(prot1_ind != -1,
+                    "The protein "<<name1<<
+                    " was not specified in the proteins list"<<std::endl);
+    IMP_USAGE_CHECK(prot2_ind != -1,
+                    "The protein "<<name2<<
+                    " was not specified in the proteins list"<<std::endl);
+    //todo - for now the residue index is not used
+    dp->add_ev_pair(prot1_ind,prot2_ind);
+  } catch (boost::bad_lexical_cast &e) {
+    error_ev_line(line);
+  }
 }
 
-void parse_protein_line(
-                        const std::string &line,
+void error_protein_line(const std::string &line)
+{
+  IMP_THROW("Improperly formatted protein line: >>" << line << "<<"
+            << " The format should be: |prot_name|start_res|"
+            << "end_res|filename|surface_filename|reference_filename|",
+            ValueException);
+}
+
+void parse_protein_line(const std::string &line,
                         ProteomicsData *dp){
-  typedef boost::split_iterator<std::string::iterator> string_split_iterator;
-  IMP_USAGE_CHECK(line.size() > 0,"no data to parse"<<std::endl);
   IMP_LOG(VERBOSE,"going to parse:"<<line);
   std::vector<std::string> line_split;
   boost::split(line_split, line, boost::is_any_of("|"));
   //split returns zero length entires as well
   line_split.erase( std::remove_if(line_split.begin(),line_split.end(),
     boost::bind( &std::string::empty, _1 ) ),line_split.end() );
-  IMP_USAGE_CHECK((line_split.size() >= 3),
-         "wrong protein format for line ("<<line_split.size()<<")"<<
-         line<<" expecting: |prot_name|start_res|"<<
-         "end_res|filename|surface_filename|reference_filename|"<<std::endl);
-  std::string ref_filename="";
-  if (line_split.size()==6) {
-    ref_filename=boost::lexical_cast<std::string>(line_split[5]);
+  if (line_split.size() < 3) {
+    error_protein_line(line);
   }
-  std::string prot_filename="";
-  std::string surface_filename="";
-  if (line_split.size()>=5){
-    prot_filename=boost::lexical_cast<std::string>(line_split[3]);
-    surface_filename=boost::lexical_cast<std::string>(line_split[4]);}
-  if (!boost::iends_with(prot_filename,".pdb")) {
-    prot_filename="";
+  try {
+    std::string ref_filename="";
+    if (line_split.size()==6) {
+      ref_filename=boost::lexical_cast<std::string>(line_split[5]);
+    }
+    std::string prot_filename="";
+    std::string surface_filename="";
+    if (line_split.size()>=5){
+      prot_filename=boost::lexical_cast<std::string>(line_split[3]);
+      surface_filename=boost::lexical_cast<std::string>(line_split[4]);}
+    if (!boost::iends_with(prot_filename,".pdb")) {
+      prot_filename="";
+    }
+    if (!boost::iends_with(surface_filename,".ms")) {
+      surface_filename="";
+    }
+    dp->add_protein(boost::lexical_cast<std::string>(line_split[0]),
+                    boost::lexical_cast<int>(line_split[1]),
+                    boost::lexical_cast<int>(line_split[2]),
+                    prot_filename,surface_filename,
+                    ref_filename);
+  } catch (boost::bad_lexical_cast &e) {
+    error_protein_line(line);
   }
-  if (!boost::iends_with(surface_filename,".ms")) {
-    surface_filename="";
-  }
-  dp->add_protein(
-                  boost::lexical_cast<std::string>(line_split[0]),
-                  boost::lexical_cast<int>(line_split[1]),
-                  boost::lexical_cast<int>(line_split[2]),
-                  prot_filename,surface_filename,
-                  ref_filename
-                 );
 }
   //The format is: |0|prot1|prot2|...|protN|linker length|header1|header2|
   //0/1 means if the restraint should be included in junction tree
   //or used just for scoring
+
+void error_interaction_line(const std::string &line)
+{
+  IMP_THROW("Improperly formatted interaction line: >>" << line << "<<"
+            << " The format should be: |0/1|prot1|prot2|...|protN|"
+            << "linker length|header1|header2|", ValueException);
+}
+
 void parse_interaction_line(
      const std::string &line,
      ProteomicsData *dp){
-  std::cout<<"parse_interaction_line:"<<line<<std::endl;
   Ints inter_prots;
-  typedef boost::split_iterator<std::string::iterator> string_split_iterator;
-  IMP_USAGE_CHECK(line.size() > 5,
-   "no data to parse. the last two tabs should contain header data\n."
-   <<" The format is: |0/1|prot1|prot2|...|protN|linker length|"<<
-   "header1|header2| \n");
+  if (line.size() <= 5) {
+    error_interaction_line(line);
+  }
   IMP_LOG(VERBOSE,"going to parse:"<<line);
   std::vector<std::string> line_split;
   boost::split(line_split, line, boost::is_any_of("|"));
   //split returns zero length entires as well
   line_split.erase( std::remove_if(line_split.begin(),line_split.end(),
     boost::bind( &std::string::empty, _1 ) ),line_split.end() );
-  std::cout<<"PARSE:"<<line_split.size()<<std::endl;
   bool used_in_filter=true;
-  if (boost::lexical_cast<int>(line_split[0])==0)
-    used_in_filter=false;
-  float linker_len=boost::lexical_cast<float>(line_split[line_split.size()-3]);
-  std::cout<<"linker len:"<<linker_len<<std::endl;
-  for(unsigned int i=1;i<line_split.size()-3;i++) {
-    //last three are linker and header
-    std::string name =  boost::lexical_cast<std::string>(line_split[i]);
-    int index = dp->find(name);
-    IMP_USAGE_CHECK(index != -1,
-                 "The protein "<<name<<
-                 " was not specified in the proteins list"<<std::endl);
-    inter_prots.push_back(index);
+  try {
+    if (boost::lexical_cast<int>(line_split[0])==0)
+      used_in_filter=false;
+    float linker_len=boost::lexical_cast<float>(
+                                   line_split[line_split.size()-3]);
+    for(unsigned int i=1;i<line_split.size()-3;i++) {
+      //last three are linker and header
+      std::string name =  boost::lexical_cast<std::string>(line_split[i]);
+      int index = dp->find(name);
+      IMP_USAGE_CHECK(index != -1,
+                      "The protein "<<name<<
+                      " was not specified in the proteins list"<<std::endl);
+      inter_prots.push_back(index);
+    }
+    dp->add_interaction(inter_prots,used_in_filter,linker_len);
+  } catch (boost::bad_lexical_cast &e) {
+    error_interaction_line(line);
   }
-  dp->add_interaction(inter_prots,used_in_filter,linker_len);
 }
 
 
@@ -251,7 +269,6 @@ void parse_interaction_line(
 ProteomicsData read_proteomics_data(const char *prot_fn) {
   std::fstream in;
   ProteomicsData data;
-  std::cout<<"===read proteomics data"<<std::endl;
   in.open(prot_fn, std::fstream::in);
   if (! in.good()) {
     IMP_WARN("Problem openning file " << prot_fn <<
@@ -259,17 +276,11 @@ ProteomicsData read_proteomics_data(const char *prot_fn) {
     in.close();
     return data;
   }
-  std::cout<<"start iterating lines"<<std::endl;
   std::string line;
   getline(in, line); //skip proteins header line
-  std::cout<<"|-----"<<std::endl;
-  std::cout<<line<<std::endl;
   getline(in, line); //skip proteins header line
-  std::cout<<"|-----"<<std::endl;
-  std::cout<<line<<std::endl;
   while ((!in.eof()) && (!is_interaction_line(line,
                                               &data))){
-    std::cout<<line<<std::endl;
     parse_protein_line(line,&data);
     if (!getline(in, line)) break;
   }
@@ -288,7 +299,6 @@ ProteomicsData read_proteomics_data(const char *prot_fn) {
     parse_ev_line(line,&data);
     if (!getline(in, line)) break;
   }
-  std::cout<<"====END"<<std::endl;
   in.close();
   return data;
 }
