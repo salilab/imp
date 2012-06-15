abceb796bac0be7525f377047114e2852f8cf479
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-06-15 20:12:05 +0000

    Format proteomics input file to match the code; test handling of bad files.
    git-svn-id: https://svn.salilab.org/imp/trunk@14493 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/multifit/test/test_read_proteomics_data.py
+++ modules/multifit/test/test_read_proteomics_data.py
@@ -1,40 +1,71 @@
 import IMP
 import IMP.test
-import sys
-import IMP.em
 import IMP.multifit
-import IMP.restrainer
-import os
-import time
 
 class ReadProteomicsTests(IMP.test.TestCase):
-    """Tests for a domino run on a single mapping"""
-    def setUp(self):
-        IMP.test.TestCase.setUp(self)
-        IMP.set_log_level(IMP.TERSE)
-        self.data_file = self.get_input_file_name("proteomics.input")
+    """Tests for proteomics file reader"""
+
     def test_run(self):
         """Check correct reading of proteomics file"""
-        self.prot_data = IMP.multifit.read_proteomics_data(self.data_file)
-        self.assertEqual(self.prot_data.get_number_of_proteins(),3)
-        self.assertEqual(self.prot_data.get_number_of_interactions(),2)
-        self.assertEqual(self.prot_data.get_start_res(0),1)
-        self.assertEqual(self.prot_data.get_end_res(0),100)
-        self.assertEqual(len(self.prot_data.get_interaction(0)),2)
-        self.assertEqual(len(self.prot_data.get_interaction(1)),3)
-        print self.prot_data.get_ev_pair(0)
-        self.assertEqual(self.prot_data.get_ev_pair(0)[0],0)
-        self.assertEqual(self.prot_data.get_ev_pair(0)[1],1)
-        self.assertEqual(self.prot_data.get_num_allowed_violated_interactions(),1)
-        self.assertEqual(self.prot_data.get_num_allowed_violated_cross_links(),
-                         3)
-        self.assertEqual(self.prot_data.get_num_allowed_violated_ev(),2)
+        data_file = self.get_input_file_name("proteomics.input")
+        prot_data = IMP.multifit.read_proteomics_data(data_file)
+        self.assertEqual(prot_data.get_number_of_proteins(),3)
+        self.assertEqual(prot_data.get_number_of_interactions(),2)
+        self.assertEqual(prot_data.get_start_res(0),1)
+        self.assertEqual(prot_data.get_end_res(0),100)
+        self.assertEqual(len(prot_data.get_interaction(0)),2)
+        self.assertEqual(len(prot_data.get_interaction(1)),3)
+        self.assertEqual(prot_data.get_ev_pair(0)[0],0)
+        self.assertEqual(prot_data.get_ev_pair(0)[1],1)
+        self.assertEqual(prot_data.get_num_allowed_violated_interactions(),1)
+        self.assertEqual(prot_data.get_num_allowed_violated_cross_links(), 3)
+        self.assertEqual(prot_data.get_num_allowed_violated_ev(),2)
 
-        print self.prot_data.get_interaction(0)
-        print self.prot_data.get_interaction(1)
-        for ind in self.prot_data.get_interaction(1):
-            print self.prot_data.get_protein_name(ind)
-            print self.prot_data.find(self.prot_data.get_protein_name(ind))
-        self.prot_data.show()
+        self.assertEqual(prot_data.get_interaction(0), [0,1])
+        self.assertEqual(prot_data.get_interaction(1), [0,1,2])
+        self.assertEqual(prot_data.get_protein_name(0), 'A')
+        self.assertEqual(prot_data.find('A'), 0)
+
+    def test_bad_protein_line(self):
+        """Check incorrectly formatted protein lines in proteomics files"""
+        # Not enough fields
+        open('test.input', 'w').write('|proteins|\n|A|1|\n')
+        self.assertRaises(ValueError, IMP.multifit.read_proteomics_data,
+                          'test.input')
+        # Fields of wrong type
+        open('test.input', 'w').write('|proteins|\n|A|x|100|nn|\n')
+        self.assertRaises(ValueError, IMP.multifit.read_proteomics_data,
+                          'test.input')
+
+    def test_bad_xlink_line(self):
+        """Check incorrectly formatted cross link lines in proteomics files"""
+        # Wrong number of fields
+        open('test.input', 'w').write('|residue-xlink|3|\n|0|A|30|B|50|\n')
+        self.assertRaises(ValueError, IMP.multifit.read_proteomics_data,
+                          'test.input')
+        # Fields of wrong type
+        open('test.input', 'w').write('|residue-xlink|3|\n|0|A|x|B|50|10|\n')
+        self.assertRaises(ValueError, IMP.multifit.read_proteomics_data,
+                          'test.input')
+
+    def test_bad_ev_line(self):
+        "Check incorrectly formatted excluded volume lines in proteomics files"
+        # Wrong number of fields
+        open('test.input', 'w').write('|ev-pairs|2|\n|A|\n')
+        self.assertRaises(ValueError, IMP.multifit.read_proteomics_data,
+                          'test.input')
+
+    def test_bad_interaction_line(self):
+        """Check incorrectly formatted interaction lines in proteomics files"""
+        # Wrong number of fields
+        open('test.input', 'w').write('|interactions|1|\n|1|A|B|10|authors|\n')
+        self.assertRaises(ValueError, IMP.multifit.read_proteomics_data,
+                          'test.input')
+        # Fields of wrong type
+        open('test.input', 'w').write('|interactions|1|\n|x|A|B|10|'
+                                      'authors|type|\n')
+        self.assertRaises(ValueError, IMP.multifit.read_proteomics_data,
+                          'test.input')
+
 if __name__ == '__main__':
     IMP.test.main()
