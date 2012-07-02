e9f35237f724d5f29348f489d65cff9374e11b75
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-07-02 22:36:04 +0000

    print out values when equal check fails
    git-svn-id: https://svn.salilab.org/imp/trunk@14722 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/base/include/check_macros.h
+++ modules/base/include/check_macros.h
@@ -231,9 +231,15 @@
   } while (false)
 #define IMP_USAGE_CHECK_FLOAT_EQUAL(expra, exprb, message)              \
 <<<<<<< HEAD
+<<<<<<< HEAD
   IMP_USAGE_CHECK(std::abs((expra)-(exprb))                             \
                   < .1*std::abs((expra)+(exprb))+.1,                    \
                   expra << " != " << exprb                              \
+=======
+  IMP_USAGE_CHECK(std::abs(expra-exprb) < .1*std::abs(expra+exprb)+.1,  \
+                  expra << " != " << exprb                              \
+                  <<" within "<< .1*std::abs(expra+exprb)+.1            \
+>>>>>>> print out values when equal check fails
 =======
   IMP_USAGE_CHECK(std::abs(expra-exprb) < .1*std::abs(expra+exprb)+.1,  \
                   expra << " != " << exprb                              \
