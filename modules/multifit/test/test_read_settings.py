6821f61683593fd92575690f4c3314131a0a304e
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-07-02 23:56:26 +0000

    Tidy up test and add extra checks.
    git-svn-id: https://svn.salilab.org/imp/trunk@14731 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/multifit/test/test_read_settings.py
+++ modules/multifit/test/test_read_settings.py
@@ -1,24 +1,20 @@
 import IMP
 import IMP.test
-import sys
-import IMP.em
 import IMP.multifit
-import unittest
-import os
-import time
 
 class ReadSettingsTests(IMP.test.TestCase):
-    """Tests for a domino run on a single mapping"""
-    def setUp(self):
-        IMP.test.TestCase.setUp(self)
-        IMP.set_log_level(IMP.VERBOSE)
-    def _test_non_existing_input_file(self):
+
+    def test_non_existing_input_file(self):
         """Check the code can handle a non existing input file"""
-        settings = IMP.multifit.read_settings("wrong_input")
+        self.assertRaises(ValueError, IMP.multifit.read_settings, "wrong_input")
+
     def test_valid_input_file(self):
-        """Check that the correct answer is returned when reading setting file"""
-        settings = IMP.multifit.read_settings(self.get_input_file_name("assembly.input"))
+        "Check that the correct answer is returned when reading setting file"
+        settings = IMP.multifit.read_settings(
+                                 self.get_input_file_name("assembly.input"))
+        settings.set_was_used(True)
         self.assertEqual(settings.get_assembly_header().get_resolution(),20)
+        self.assertEqual(settings.get_number_of_component_headers(), 4)
 
 if __name__ == '__main__':
-    unittest.main()
+    IMP.test.main()
