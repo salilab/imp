a9d912dce83ebd7096cf34fbce10b825cdaafcdc
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-07-02 04:59:37 +0000

    pass through the sconscripts 2x so that scons knows about all the modules before trying to figure out test dependencies (since those can depend on modules that haven't been seen yet when they are being processed)
    git-svn-id: https://svn.salilab.org/imp/trunk@14711 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- scons_tools/application.py
+++ scons_tools/application.py
@@ -23,61 +23,50 @@ def IMPApplication(env, name,
                    python=True):
     if env.GetOption('help'):
         return
-    (nenv, version, found_optional_modules, found_optional_dependencies) =\
-         utility.configure(env, name, "application", version,
+    lkname="application_"+name.replace(" ", "_").replace(":", "_")
+    pre="\page "+lkname+" "+name
+    nenv=\
+        utility.configure_application(env, name, "\\ref "+lkname+' "'+name+'"', version,
                            required_modules=required_modules,
                            optional_dependencies=optional_dependencies,
                            required_dependencies= required_dependencies)
-    if not nenv:
-        data.get(env).add_application(name, ok=False)
-        return
+    if nenv:
+        if nenv["IMP_PASS"]=="RUN":
+            doc.add_doc_page(env, pre,
+                             authors, version,
+                             brief, overview,
+                             publications,
+                             license)
+
+            env= environment.get_bin_environment(nenv)
+            scons_tools.data.get(env).add_to_alias("all", env.Alias(name))
+        for d in scons_tools.paths.get_sconscripts(env):
+            env.SConscript(d, exports=['env'])
+        return env
     else:
-        if python:
-            pm=required_modules+found_optional_modules
-        else:
-            pm=[]
-        lkname="application_"+name.replace(" ", "_").replace(":", "_")
-        pre="\page "+lkname+" "+name
-        doc.add_doc_page(env, pre,
-                         authors, version,
-                         brief, overview,
-                         publications,
-                         license)
-        data.get(env).add_application(name, link="\\ref "+lkname+' "'+name+'"',
-                                      dependencies=required_dependencies\
-                                          +found_optional_dependencies,
-                                      unfound_dependencies=[x for x in optional_dependencies
-                                                            if not x in
-                                                            found_optional_dependencies],
-                                      modules= required_modules+found_optional_modules,
-                                      python_modules=pm,
-                                      version=version)
+        return None
 
-
-    env= environment.get_bin_environment(nenv)
-    scons_tools.data.get(env).add_to_alias("all", env.Alias(name))
-    dirs = Glob("*/SConscript")
-    for d in dirs:
-        env.SConscript(d, exports=['env'])
-    return env
-
-def IMPCPPExecutable(envi, target, source):
-    if envi.GetOption('help'):
+def IMPCPPExecutable(env, target, source):
+    if env["IMP_PASS"] != "RUN":
         return
-    env= environment.get_bin_environment(envi)
+    if env.GetOption('help'):
+        return
 
     prog= env.Program(target="#/build/bin/"+target, source=source)
 
 
-def IMPCPPExecutables(envi, lst):
-    if envi.GetOption('help'):
+def IMPCPPExecutables(env, lst):
+    if env["IMP_PASS"] != "RUN":
         return
-    env= environment.get_bin_environment(envi)
+    if env.GetOption('help'):
+        return
     for l in lst:
         prog= env.Program(target="#/build/bin/"+l[0], source=l[1])
 
 
 def IMPPythonExecutable(env, file):
+    if env["IMP_PASS"] != "RUN":
+        return
     def dummy(target, source, env): pass
     _PythonExeDependency = Builder(action=Action(dummy, dummy),
                                    source_scanner=pyscanner.PythonScanner)
@@ -96,6 +85,8 @@ def IMPPythonExecutable(env, file):
 
 
 def IMPApplicationTest(env, python_tests=[]):
+    if env["IMP_PASS"] != "RUN":
+        return
     files= [File(x).abspath for x in python_tests]
     tests = test.add_tests(env, source=files, type='application unit test')
     for t in tests:
