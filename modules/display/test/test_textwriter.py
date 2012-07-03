19293010ae65838d630801aad216b95c2bd9b55a
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-07-03 19:09:43 +0000

    minor test fixes
    git-svn-id: https://svn.salilab.org/imp/trunk@14742 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/display/test/test_textwriter.py
+++ modules/display/test/test_textwriter.py
@@ -9,12 +9,12 @@ class DummyWriter(IMP.display.TextWriter):
         IMP.display.TextWriter.__init__(self, name)
     def _do_open(self):
         print "opening"
-        print >> self.get_stream(), "open"
+        print >> self._get_stream(), "open"
     def _do_close(self):
         print "closing"
-        print >> self.get_stream(), "close"
+        print >> self._get_stream(), "close"
     def poke(self):
-        print >> self.get_stream(), "hi"
+        print >> self._get_stream(), "hi"
     def close(self):
         print "bye"
         self.do_close()
