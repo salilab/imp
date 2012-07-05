397aa378190f23a54795a5aa3ba955aab17406cf
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-07-05 18:42:50 +0000

    add log example
    git-svn-id: https://svn.salilab.org/imp/trunk@14763 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/base/examples/log.py
@@ -0,0 +1,27 @@
+import IMP.base
+
+class DummyObject(IMP.base.Object):
+    def __init__(self):
+        IMP.base.Object.__init__(self, "DummyObject%1%")
+    def add_log(self):
+        # these are done by the IMP_OBJECT_LOG macro in C++
+        state=IMP.base.SetLogState(self.get_log_level())
+        context= IMP.base.CreateLogContext(self.get_name()+"::add_log")
+        self.set_was_used(True)
+        if IMP.base.get_log_level() >=IMP.base.VERBOSE:
+            IMP.base.add_to_log("A verbose message in the object\n")
+# we can set the log level for all of IMP
+IMP.base.set_log_level(IMP.base.TERSE)
+
+# we can tell it to print the time each even occurs
+IMP.base.set_log_timer(True)
+
+# we can create a log context
+lc= IMP.base.CreateLogContext("my context")
+
+# we can print a message
+IMP.base.add_to_log("This is my log message\n")
+
+o= DummyObject()
+o.set_log_level(IMP.base.VERBOSE)
+o.add_log()
