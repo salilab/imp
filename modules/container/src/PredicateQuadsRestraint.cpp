d0af265e831eb1f2f83ff48425a68b48deca3231
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-07-09 14:47:01 +0000

    Merge branch 'feature/pred_test' into develop
    git-svn-id: https://svn.salilab.org/imp/trunk@14788 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/container/src/PredicateQuadsRestraint.cpp
+++ modules/container/src/PredicateQuadsRestraint.cpp
@@ -60,19 +60,24 @@ Restraints PredicateQuadsRestraint
   return ret;
 }
 
-void PredicateQuadsRestraint
+bool PredicateQuadsRestraint
 ::assign_pair(const ParticleIndexQuad& index) const {
   int bin=predicate_->get_value_index(get_model(), index);
   if (containers_.find(bin) == containers_.end()) {
     if (unknown_container_) {
       unknown_container_->add_particle_quad(index);
+      return true;
     } else if (error_on_unknown_) {
       IMP_THROW("Invalid predicate value of " << bin
                 << " encounted for " << index,
                 ValueException);
+      return true;
+    } else {
+      return false;
     }
   } else {
     containers_.find(bin)->second->add_particle_quad(index);
+    return true;
   }
 }
 void PredicateQuadsRestraint
@@ -86,9 +91,21 @@ void PredicateQuadsRestraint
        it != containers_.end(); ++it) {
     it->second->clear_particle_quads();
   }
+  int dropped=0;
   IMP_FOREACH_QUAD_INDEX(input_, {
-      assign_pair(_1);
+      bool added=assign_pair(_1);
+      if (!added) ++dropped;
     });
+  IMP_IF_CHECK(USAGE_AND_INTERNAL) {
+    unsigned int total=dropped;
+    for (Map::const_iterator it= containers_.begin();
+         it != containers_.end(); ++it) {
+      total+=it->second->get_number();
+    }
+    IMP_INTERNAL_CHECK(input_->get_number()==total,
+                       "Wrong number of particles "
+                       << total << "!=" << input_->get_number());
+  }
 }
 
 void PredicateQuadsRestraint::do_show(std::ostream &) const {
