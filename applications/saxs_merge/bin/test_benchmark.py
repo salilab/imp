a68d38bf377d540d9d8563f040d30411aba1a901
yannick@SALILAB.ORG <yannick@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-07-09 15:57:29 +0000

    added two new test cases changed the way output was done
    git-svn-id: https://svn.salilab.org/imp/trunk@14789 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- applications/saxs_merge/bin/test_benchmark.py
+++ applications/saxs_merge/bin/test_benchmark.py
@@ -18,18 +18,27 @@ class SAXSApplicationTest(IMP.test.ApplicationTestCase):
         if numpy is None or scipy is None:
             self.skipTest("could not import numpy or scipy")
 
-    def run_app(self, args):
-        args.append('--destdir=runapp')
+    def run_app(self, name, args):
+        destname = 'runapp_'+name
+        if os.path.isdir(destname):
+            return
+        args.append('--destdir='+destname)
+        #args.append('--blimit_fitting=400')
+        #args.append('--elimit_fitting=400')
+        args.append('--allfiles')
+        args.append('--outlevel=full')
         p = self.run_python_application('saxs_merge.py',args)
         out, err = p.communicate()
         sys.stderr.write(err)
         self.assertApplicationExitedCleanly(p.returncode, err)
         #do some preliminary tests
-        for i in ['runapp/data_merged.dat', 'runapp/mean_merged.dat', 'runapp/summary.txt']:
+        for i in [destname+'/data_merged.dat', destname+'/mean_merged.dat',
+                destname+'/summary.txt']:
             self.assertTrue(os.path.exists(i))
-        text=open('runapp/summary.txt').read()
+        text=open(destname+'/summary.txt').read()
         m = text.find('Classification')
-        self.assertIsNotNone(m, msg="Classification output not found in summary.txt")
+        self.assertIsNotNone(m,
+                msg="Classification output not found in summary.txt")
 
     def uniqify(self, seq):
         #taken from http://stackoverflow.com/a/480227/958110
@@ -71,7 +80,7 @@ class SAXSApplicationTest(IMP.test.ApplicationTestCase):
         fl.write('set xrange [%f:%f]\n' % (datarange[0], datarange[1]))
         fl.write('set yrange [0:%f]\n' % datarange[3])
         fl.write('set output "%s_data_lin_colored.png"\n' % name)
-        fl.write('p "%s" u 1:(%f*($2+%f)):4 w p lc var t "automatic"\n' %
+        fl.write('p "%s" u 1:(%f*($2+%f)):5 w p lc var t "automatic"\n' %
                    (d1, transform[0], transform[1]))
         fl.close()
         os.system('gnuplot Cpgnuplot')
@@ -82,7 +91,7 @@ class SAXSApplicationTest(IMP.test.ApplicationTestCase):
         fl.write('set log y\n')
         fl.write('set xrange [%f:%f]\n' % (datarange[0], datarange[1]))
         fl.write('set yrange [%f:%f]\n' % (datarange[2], datarange[3]))
-        fl.write('p "%s" u 1:(%f*($2+%f)):4 w p lc var t "automatic"\n' %
+        fl.write('p "%s" u 1:(%f*($2+%f)):5 w p lc var t "automatic"\n' %
                 (d1, transform[0],transform[1]))
         fl.close()
         os.system('gnuplot Cpgnuplot')
@@ -94,10 +103,11 @@ class SAXSApplicationTest(IMP.test.ApplicationTestCase):
         fl.write('set output "%s_means_lin.png"\n' % name)
         fl.write('set xrange [%f:%f]\n' % (datarange[0], datarange[1]))
         fl.write('set yrange [0:%f]\n' % datarange[3])
-        fl.write('p "%s" u 1:2 w l t "automatic", '
-                 '"" u 1:($2+$3) w l not, "" u 1:($2-$3) w l not, '
-                 '"%s" u 1:2 w l t "manual", '
-                 '"" u 1:($2+$3) w l not, "" u 1:($2-$3) w l not\n' % (d1,d2))
+        fl.write('p "%s" u 1:2 w l lt 1 t "automatic", '
+                 '"" u 1:($2+$3) w l lt 1 not, "" u 1:($2-$3) w l lt 1 not, '
+                 '"%s" u 1:2 w l lt 2 t "manual", '
+                 '"" u 1:($2+$3) w l lt 2 not, '
+                 '"" u 1:($2-$3) w l lt 2 not\n' % (d1,d2))
         fl.close()
         os.system('gnuplot Cpgnuplot')
         #log scale
@@ -107,10 +117,11 @@ class SAXSApplicationTest(IMP.test.ApplicationTestCase):
         fl.write('set log y\n')
         fl.write('set xrange [%f:%f]\n' % (datarange[0], datarange[1]))
         fl.write('set yrange [%f:%f]\n' % (datarange[2], datarange[3]))
-        fl.write('p "%s" u 1:2 w l t "automatic", '
-                 '"" u 1:($2+$3) w l not, "" u 1:($2-$3) w l not, '
-                 '"%s" u 1:2 w l t "manual", '
-                 '"" u 1:($2+$3) w l not, "" u 1:($2-$3) w l not\n' % (d1,d2))
+        fl.write('p "%s" u 1:2 w l lt 1 t "automatic", '
+                 '"" u 1:($2+$3) w l lt 1 not, "" u 1:($2-$3) w l lt 1 not, '
+                 '"%s" u 1:2 w l lt 2 t "manual", '
+                 '"" u 1:($2+$3) w l lt 2 not, '
+                 '"" u 1:($2-$3) w l lt 2 not\n' % (d1,d2))
         fl.close()
         os.system('gnuplot Cpgnuplot')
 
@@ -142,7 +153,7 @@ class SAXSApplicationTest(IMP.test.ApplicationTestCase):
         fl.write('set term png\n')
         fl.write('set output "%s_guinier.png"\n' % name)
         fl.write('set xrange [0:0.0025]\n')
-        fl.write('p "%s" u ($1*$1):(log($2)) w p t "data", '
+        fl.write('p "%s" u ($1*$1):(log($2)):($3/$2) w yerr t "data", '
                  '  "%s" u ($1*$1):(log($2)) w l t "mean", '
                  '"" u ($1*$1):(log($2)+$3/$2) w l not, '
                  '"" u ($1*$1):(log($2)-$3/$2) w l not\n' % (data,mean))
@@ -293,18 +304,23 @@ class SAXSApplicationTest(IMP.test.ApplicationTestCase):
 
     def run_results(self, name, manual_merge, inputs):
         #rescale and fit the two curves
-        p = self.run_python_application('saxs_merge.py', ['--destdir=compapp',
+        destdir='compapp_'+name
+        if not os.path.isdir(destdir):
+            p = self.run_python_application('saxs_merge.py',
+                    ['--destdir='+destdir,
+                 #'--blimit_fitting=400', '--elimit_fitting=400',
                  '--stop=rescaling', '--postpone_cleanup',
-                 '--npoints=-1', '--allfiles', 'runapp/data_merged.dat', manual_merge])
-        out, err = p.communicate()
-        sys.stderr.write(err)
-        self.assertApplicationExitedCleanly(p.returncode, err)
+                 '--npoints=-1', '--allfiles', '--outlevel=full',
+                 'runapp_'+name+'/data_merged.dat', manual_merge])
+            out, err = p.communicate()
+            sys.stderr.write(err)
+            self.assertApplicationExitedCleanly(p.returncode, err)
         #compute chi2 of data
-        datachi = self.chisquare('compapp/data_data_merged.dat',
-                'compapp/data_'+os.path.basename(manual_merge))
+        datachi = self.chisquare(destdir+'/data_data_merged.dat',
+                destdir+'/data_'+os.path.basename(manual_merge))
         #compute chi2 of fits
-        fitchi = self.chisquare('compapp/mean_data_merged.dat',
-                'compapp/mean_'+os.path.basename(manual_merge))
+        fitchi = self.chisquare(destdir+'/mean_data_merged.dat',
+                destdir+'/mean_'+os.path.basename(manual_merge))
         #get proper bounds
         points=map(lambda a:map(float,a.split()[:2]), open(manual_merge).readlines())
         xmin = 0
@@ -314,18 +330,19 @@ class SAXSApplicationTest(IMP.test.ApplicationTestCase):
         ymax = max([i[1] for i in points if len(i) >= 2])*1.2
         datarange = (xmin,xmax,ymin,ymax)
         #plot data
-        self.plot_data_overlaid(name, 'compapp/data_data_merged.dat',
-                'compapp/data_'+os.path.basename(manual_merge), datarange)
-        self.plot_data_colored(name, 'runapp/data_merged.dat', datarange,
-                transform = self.get_transform('compapp/summary.txt') )
+        self.plot_data_overlaid(name, destdir+'/data_data_merged.dat',
+                destdir+'/data_'+os.path.basename(manual_merge), datarange)
+        self.plot_data_colored(name,
+                'runapp_'+name+'/data_merged.dat', datarange,
+                transform = self.get_transform(destdir+'/summary.txt') )
         #plot mean
-        self.plot_means(name, 'compapp/mean_data_merged.dat',
-                'compapp/mean_'+os.path.basename(manual_merge), datarange)
-        self.plot_data_mean(name, 'compapp/data_data_merged.dat',
-                'compapp/mean_data_merged.dat', datarange)
+        self.plot_means(name, destdir+'/mean_data_merged.dat',
+                destdir+'/mean_'+os.path.basename(manual_merge), datarange)
+        self.plot_data_mean(name, destdir+'/data_data_merged.dat',
+                destdir+'/mean_data_merged.dat', datarange)
         #guinier plot
-        self.plot_guinier(name, 'compapp/data_data_merged.dat',
-                'compapp/mean_data_merged.dat')
+        self.plot_guinier(name, destdir+'/data_data_merged.dat',
+                destdir+'/mean_data_merged.dat')
         #plot all curves
         self.plot_inputs(name, inputs)
         print name,datachi,fitchi
@@ -349,8 +366,8 @@ class SAXSApplicationTest(IMP.test.ApplicationTestCase):
         return ['--postpone_cleanup', '--cmodel=normal']
 
     def get_params4(self):
-        """aalpha 1e-4 rescale with offset"""
-        return ['--cmodel=normal-offset', '--header']
+        """aalpha 1e-7 rescale with offset"""
+        return ['--cmodel=normal-offset']
 
     def test_case1a(self):
         """Simple test of SAXS merge benchmark / application for Nup116"""
@@ -359,7 +376,7 @@ class SAXSApplicationTest(IMP.test.ApplicationTestCase):
                  self.get_input_file_name('Nup116/25043_01D_S061_0_01.sub'),
                  self.get_input_file_name('Nup116/25043_01E_S063_0_01.sub'),
                  self.get_input_file_name('Nup116/25043_01F_S065_0_01.sub')]
-        self.run_app( self.get_params1() + inputs )
+        self.run_app( 'Nup116_1', self.get_params1() + inputs )
         self.run_results('Nup116_1',
                 self.get_input_file_name('Nup116/25043_manual_merge.dat'),
                 inputs)
@@ -371,7 +388,7 @@ class SAXSApplicationTest(IMP.test.ApplicationTestCase):
                  self.get_input_file_name('Nup116/25043_01D_S061_0_01.sub'),
                  self.get_input_file_name('Nup116/25043_01E_S063_0_01.sub'),
                  self.get_input_file_name('Nup116/25043_01F_S065_0_01.sub')]
-        self.run_app( self.get_params2() + inputs )
+        self.run_app( 'Nup116_2', self.get_params2() + inputs )
         self.run_results('Nup116_2',
                 self.get_input_file_name('Nup116/25043_manual_merge.dat'),
                 inputs)
@@ -383,7 +400,7 @@ class SAXSApplicationTest(IMP.test.ApplicationTestCase):
                  self.get_input_file_name('Nup116/25043_01D_S061_0_01.sub'),
                  self.get_input_file_name('Nup116/25043_01E_S063_0_01.sub'),
                  self.get_input_file_name('Nup116/25043_01F_S065_0_01.sub')]
-        self.run_app( self.get_params3() + inputs )
+        self.run_app( 'Nup116_3', self.get_params3() + inputs )
         self.run_results('Nup116_3',
                 self.get_input_file_name('Nup116/25043_manual_merge.dat'),
                 inputs)
@@ -395,7 +412,7 @@ class SAXSApplicationTest(IMP.test.ApplicationTestCase):
                  self.get_input_file_name('Nup116/25043_01D_S061_0_01.sub'),
                  self.get_input_file_name('Nup116/25043_01E_S063_0_01.sub'),
                  self.get_input_file_name('Nup116/25043_01F_S065_0_01.sub')]
-        self.run_app( self.get_params4() + inputs )
+        self.run_app( 'Nup116_4', self.get_params4() + inputs )
         self.run_results('Nup116_4',
                 self.get_input_file_name('Nup116/25043_manual_merge.dat'),
                 inputs)
@@ -407,7 +424,7 @@ class SAXSApplicationTest(IMP.test.ApplicationTestCase):
                  self.get_input_file_name('Nup192/Nup192_01D_S018_0_01.sub'),
                  self.get_input_file_name('Nup192/Nup192_01E_S020_0_02.sub'),
                  self.get_input_file_name('Nup192/Nup192_01F_S022_0_02.sub')]
-        self.run_app( self.get_params1() + inputs )
+        self.run_app( 'Nup192_1', self.get_params1() + inputs )
         self.run_results('Nup192_1',
                 self.get_input_file_name('Nup192/Nup192_merge.dat'), inputs)
 
@@ -418,7 +435,7 @@ class SAXSApplicationTest(IMP.test.ApplicationTestCase):
                  self.get_input_file_name('Nup192/Nup192_01D_S018_0_01.sub'),
                  self.get_input_file_name('Nup192/Nup192_01E_S020_0_02.sub'),
                  self.get_input_file_name('Nup192/Nup192_01F_S022_0_02.sub')]
-        self.run_app( self.get_params2() + inputs )
+        self.run_app( 'Nup192_2', self.get_params2() + inputs )
         self.run_results('Nup192_2',
                 self.get_input_file_name('Nup192/Nup192_merge.dat'), inputs)
 
@@ -429,7 +446,7 @@ class SAXSApplicationTest(IMP.test.ApplicationTestCase):
                  self.get_input_file_name('Nup192/Nup192_01D_S018_0_01.sub'),
                  self.get_input_file_name('Nup192/Nup192_01E_S020_0_02.sub'),
                  self.get_input_file_name('Nup192/Nup192_01F_S022_0_02.sub')]
-        self.run_app( self.get_params3() + inputs )
+        self.run_app( 'Nup192_3', self.get_params3() + inputs )
         self.run_results('Nup192_3',
                 self.get_input_file_name('Nup192/Nup192_merge.dat'), inputs)
 
@@ -440,9 +457,97 @@ class SAXSApplicationTest(IMP.test.ApplicationTestCase):
                  self.get_input_file_name('Nup192/Nup192_01D_S018_0_01.sub'),
                  self.get_input_file_name('Nup192/Nup192_01E_S020_0_02.sub'),
                  self.get_input_file_name('Nup192/Nup192_01F_S022_0_02.sub')]
-        self.run_app( self.get_params4() + inputs )
+        self.run_app( 'Nup192_4', self.get_params4() + inputs )
         self.run_results('Nup192_4',
                 self.get_input_file_name('Nup192/Nup192_merge.dat'), inputs)
+
+    def test_case1c(self):
+        """Simple test of SAXS merge benchmark / application for Nup133"""
+        inputs = [self.get_input_file_name('Nup133/23922_G4_2.mccd.dat'),
+                 self.get_input_file_name('Nup133/23922_G2_2.mccd.dat'),
+                 self.get_input_file_name('Nup133/23922_G3_2.mccd.dat'),
+                 self.get_input_file_name('Nup133/23922_G5_2.mccd.dat')]
+        self.run_app( 'Nup133_1', self.get_params1() + inputs )
+        self.run_results('Nup133_1',
+                self.get_input_file_name('Nup133/23922_merge.dat'),
+                inputs)
+
+    def test_case2c(self):
+        """Simple test of SAXS merge benchmark / application for Nup133"""
+        inputs = [self.get_input_file_name('Nup133/23922_G4_2.mccd.dat'),
+                 self.get_input_file_name('Nup133/23922_G2_2.mccd.dat'),
+                 self.get_input_file_name('Nup133/23922_G3_2.mccd.dat'),
+                 self.get_input_file_name('Nup133/23922_G5_2.mccd.dat')]
+        self.run_app( 'Nup133_2', self.get_params2() + inputs )
+        self.run_results('Nup133_2',
+                self.get_input_file_name('Nup133/23922_merge.dat'),
+                inputs)
+
+    def test_case3c(self):
+        """Simple test of SAXS merge benchmark / application for Nup133"""
+        inputs = [self.get_input_file_name('Nup133/23922_G4_2.mccd.dat'),
+                 self.get_input_file_name('Nup133/23922_G2_2.mccd.dat'),
+                 self.get_input_file_name('Nup133/23922_G3_2.mccd.dat'),
+                 self.get_input_file_name('Nup133/23922_G5_2.mccd.dat')]
+        self.run_app( 'Nup133_3', self.get_params3() + inputs )
+        self.run_results('Nup133_3',
+                self.get_input_file_name('Nup133/23922_merge.dat'),
+                inputs)
+
+    def test_case4c(self):
+        """Simple test of SAXS merge benchmark / application for Nup133"""
+        inputs = [self.get_input_file_name('Nup133/23922_G4_2.mccd.dat'),
+                 self.get_input_file_name('Nup133/23922_G2_2.mccd.dat'),
+                 self.get_input_file_name('Nup133/23922_G3_2.mccd.dat'),
+                 self.get_input_file_name('Nup133/23922_G5_2.mccd.dat')]
+        self.run_app( 'Nup133_4', self.get_params4() + inputs )
+        self.run_results('Nup133_4',
+                self.get_input_file_name('Nup133/23922_merge.dat'),
+                inputs)
+
+    def test_case1d(self):
+        """Simple test of SAXS merge benchmark / application for Nup145"""
+        inputs = [self.get_input_file_name('Nup145/23923_A3_2.mccd.dat'),
+                 self.get_input_file_name('Nup145/23923_A2_2.mccd.dat'),
+                 self.get_input_file_name('Nup145/23923_A4_2.mccd.dat'),
+                 self.get_input_file_name('Nup145/23923_A5_2.mccd.dat')]
+        self.run_app( 'Nup145_1', self.get_params1() + inputs )
+        self.run_results('Nup145_1',
+                self.get_input_file_name('Nup145/23923_merge.dat'),
+                inputs)
+
+    def test_case2d(self):
+        """Simple test of SAXS merge benchmark / application for Nup145"""
+        inputs = [self.get_input_file_name('Nup145/23923_A3_2.mccd.dat'),
+                 self.get_input_file_name('Nup145/23923_A2_2.mccd.dat'),
+                 self.get_input_file_name('Nup145/23923_A4_2.mccd.dat'),
+                 self.get_input_file_name('Nup145/23923_A5_2.mccd.dat')]
+        self.run_app( 'Nup145_2', self.get_params2() + inputs )
+        self.run_results('Nup145_2',
+                self.get_input_file_name('Nup145/23923_merge.dat'),
+                inputs)
+
+    def test_case3d(self):
+        """Simple test of SAXS merge benchmark / application for Nup145"""
+        inputs = [self.get_input_file_name('Nup145/23923_A3_2.mccd.dat'),
+                 self.get_input_file_name('Nup145/23923_A2_2.mccd.dat'),
+                 self.get_input_file_name('Nup145/23923_A4_2.mccd.dat'),
+                 self.get_input_file_name('Nup145/23923_A5_2.mccd.dat')]
+        self.run_app( 'Nup145_3', self.get_params3() + inputs )
+        self.run_results('Nup145_3',
+                self.get_input_file_name('Nup145/23923_merge.dat'),
+                inputs)
+
+    def test_case4d(self):
+        """Simple test of SAXS merge benchmark / application for Nup145"""
+        inputs = [self.get_input_file_name('Nup145/23923_A3_2.mccd.dat'),
+                 self.get_input_file_name('Nup145/23923_A2_2.mccd.dat'),
+                 self.get_input_file_name('Nup145/23923_A4_2.mccd.dat'),
+                 self.get_input_file_name('Nup145/23923_A5_2.mccd.dat')]
+        self.run_app( 'Nup145_4', self.get_params4() + inputs )
+        self.run_results('Nup145_4',
+                self.get_input_file_name('Nup145/23923_merge.dat'),
+                inputs)
 
 if __name__ == '__main__':
     IMP.test.main()
