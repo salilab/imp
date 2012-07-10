825d15c14563d239502e15d150ea983a1a9036ea
dina@SALILAB.ORG <dina@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-07-10 23:11:35 +0000

    updates to score combinations
    git-svn-id: https://svn.salilab.org/imp/trunk@14802 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- applications/integrative_docking/test/test_combine_scores.py
+++ applications/integrative_docking/test/test_combine_scores.py
@@ -4,7 +4,7 @@ import os
 import re
 
 class CombineScoresApplicationTest(IMP.test.ApplicationTestCase):
-    def test_simple(self):
+    def test_scores_combination(self):
         """Simple test of score combination"""
         print self.get_input_file_name('saxs_score.res')
         print self.get_input_file_name('em2d_score.res')
@@ -20,6 +20,19 @@ class CombineScoresApplicationTest(IMP.test.ApplicationTestCase):
         # count the number of lines in the output
         number_of_lines = out.count('\n')
         self.assertEqual(number_of_lines, 41)
+
+    def test_z_score_computation(self):
+        """Simple test of score combination"""
+        print self.get_input_file_name('saxs_score.res')
+        p = self.run_application('recompute_zscore',
+                                 [self.get_input_file_name('saxs_score.res')])
+        out, err = p.communicate()
+        sys.stderr.write(err)
+        self.assertApplicationExitedCleanly(p.returncode, err)
+
+        # count the number of lines in the output
+        number_of_lines = out.count('\n')
+        self.assertEqual(number_of_lines, 1001)
 
 if __name__ == '__main__':
     IMP.test.main()
