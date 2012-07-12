bdc2818af9c8ae543cf5f94c4a9a99750734bce8
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-07-12 21:58:54 +0000

    Update for new function signature.
    git-svn-id: https://svn.salilab.org/imp/trunk@14823 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/multifit/test/test_settings_data.py
+++ modules/multifit/test/test_settings_data.py
@@ -14,8 +14,7 @@ class SettingsDataTest(IMP.test.TestCase):
         IMP.set_log_level(IMP.VERBOSE)
     def test_read_data(self):
         """test correct reading of settings data"""
-        data_path = os.path.dirname(self.get_input_file_name("assembly.input"))+"/"
-        header = IMP.multifit.read_settings(self.get_input_file_name("assembly.input"),data_path)
+        header = IMP.multifit.read_settings(self.get_input_file_name("assembly.input"))
         self.assertEqual(header.get_number_of_component_headers(), 4)
         self.assertEqual(header.get_component_header(2).get_name(), "C")
         self.assertEqual(header.get_assembly_header().get_resolution(), 20.)
