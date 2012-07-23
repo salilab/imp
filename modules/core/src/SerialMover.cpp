28fe0264c7c77c4512dbfaaf4100ca664cb612fc
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-07-23 21:27:10 +0000

    add statistics gathering to the serial mover
    git-svn-id: https://svn.salilab.org/imp/trunk@14886 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/src/SerialMover.cpp
+++ modules/core/src/SerialMover.cpp
@@ -13,37 +13,56 @@ IMPCORE_BEGIN_NAMESPACE
 
 SerialMover::SerialMover(const MoversTemp& mvs):
   Mover(IMP::internal::get_model(mvs), "SerialMover%1%"),
-  mvs_(mvs.begin(), mvs.end()), imov_(-1) {
-  for (unsigned int i=0; i< mvs_.size(); ++i) {
-    mvs_[i]->set_was_used(true);
+  imov_(-1) {
+  set_movers(mvs);
+  for (unsigned int i=0; i< get_number_of_movers(); ++i) {
+    get_mover(i)->set_was_used(true);
   }
+  reset_acceptance_probabilities();
 }
 
 ParticlesTemp SerialMover::propose_move(Float f) {
   IMP_OBJECT_LOG;
   ++imov_;
-  if(imov_==static_cast<int>(mvs_.size())) imov_=0;
-  mvs_[imov_]->set_was_used(true);
+  if(imov_==static_cast<int>(get_number_of_movers())) imov_=0;
   IMP_LOG(VERBOSE,"Propose move using "
-          << mvs_[imov_]->get_name() <<std::endl);
-  return mvs_[imov_]->propose_move(f);
+          << get_mover(imov_)->get_name() <<std::endl);
+  attempt_[imov_]+=1.0;
+  return get_mover(imov_)->propose_move(f);
 }
 
 void SerialMover::reset_move() {
   IMP_USAGE_CHECK(imov_ >=0, "No move has been proposed to reset");
-  mvs_[imov_]->reset_move();
+  get_mover(imov_)->reset_move();
+  failed_[imov_]+=1.0;
+}
+
+double SerialMover::get_acceptance_probability(int i) const
+{
+ return (attempt_[i]-failed_[i])/attempt_[i];
+}
+
+void SerialMover::reset_acceptance_probabilities()
+{
+ attempt_.clear();
+ failed_.clear();
+ attempt_.resize(get_number_of_movers(), 0.0);
+ failed_.resize(get_number_of_movers(), 0.0);
 }
 
 void SerialMover::do_show(std::ostream &out) const {
-  out << "number of movers: " << mvs_.size() << "\n";
+  out << "number of movers: " << get_number_of_movers() << "\n";
 }
 
 ParticlesTemp SerialMover::get_output_particles() const {
   ParticlesTemp ret;
-  for (unsigned int i=0; i< mvs_.size(); ++i) {
-    ret+= mvs_[i]->get_output_particles();
+  for (unsigned int i=0; i< get_number_of_movers(); ++i) {
+    ret+= get_mover(i)->get_output_particles();
   }
   return ret;
 }
+
+IMP_LIST_IMPL(SerialMover, Mover, mover, Mover*, Movers);
+
 
 IMPCORE_END_NAMESPACE
