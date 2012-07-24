649ab23e478a5c24d342c3a55e81134f67c97c72
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-07-24 22:19:03 +0000

    Fix typos.
    git-svn-id: https://svn.salilab.org/imp/trunk@14889 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/multifit/src/SettingsData.cpp
+++ modules/multifit/src/SettingsData.cpp
@@ -40,7 +40,7 @@ ComponentHeader *parse_component_line(
   }
   catch(boost::bad_lexical_cast &) {
     comp->set_num_ap(0);
-    IMP_WARN("Can not cast num_ap filed for protein: "
+    IMP_WARN("Can not cast num_ap field for protein: "
              <<comp->get_name()<<std::endl);
   }
   comp->set_txt_fine_ap_fn(base::get_relative_path(config, line_split[5]));
@@ -49,7 +49,7 @@ ComponentHeader *parse_component_line(
   }
   catch(boost::bad_lexical_cast &) {
     comp->set_num_fine_ap(0);
-    IMP_WARN("Can not cast num_fine_ap filed for protein: "<<comp->get_name()
+    IMP_WARN("Can not cast num_fine_ap field for protein: "<<comp->get_name()
              <<" seeting to 0"<<std::endl);
   }
   comp->set_transformations_fn(base::get_relative_path(config, line_split[7]));
@@ -68,7 +68,7 @@ AssemblyHeader *parse_assembly_line(
   std::vector<std::string> line_split;
   boost::split(line_split, line, boost::is_any_of("|"));
   IMP_USAGE_CHECK(line_split.size() == 12,
-     "Expecting 12 fileds in input line, got "<<
+     "Expecting 12 fields in input line, got "<<
      line_split.size() << " : " <<line);
   IMP_NEW(AssemblyHeader, dens, ());
   dens->set_dens_fn(base::get_relative_path(config, line_split[0]));
@@ -77,21 +77,21 @@ AssemblyHeader *parse_assembly_line(
   }
   catch(boost::bad_lexical_cast &) {
     dens->set_resolution(1.);
-    IMP_WARN("Can not cast resolution filed, setting to 1\n");
+    IMP_WARN("Can not cast resolution field, setting to 1\n");
   }
   try{
     dens->set_spacing(boost::lexical_cast<float>(line_split[2]));
   }
   catch(boost::bad_lexical_cast &) {
     dens->set_spacing(1.);
-    IMP_WARN("Can not cast spacing filed, setting to 1\n");
+    IMP_WARN("Can not cast spacing field, setting to 1\n");
   }
   try{
     dens->set_threshold(boost::lexical_cast<float>(line_split[3]));
   }
   catch(boost::bad_lexical_cast &) {
     dens->set_threshold(0.);
-    IMP_WARN("Can not cast threshold filed, setting to 0\n");
+    IMP_WARN("Can not cast threshold field, setting to 0\n");
   }
   try{
   dens->set_origin(algebra::Vector3D(
@@ -101,7 +101,7 @@ AssemblyHeader *parse_assembly_line(
   }
   catch(boost::bad_lexical_cast &) {
     dens->set_origin(algebra::Vector3D(0,0,0));
-    IMP_WARN("Can not cast origin filed, setting to 0\n");
+    IMP_WARN("Can not cast origin field, setting to 0\n");
   }
   dens->set_coarse_ap_fn(base::get_relative_path(config, line_split[7]));
   dens->set_coarse_over_sampled_ap_fn(base::get_relative_path(config,
