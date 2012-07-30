0c1964c6899722c7bc34c4056e532c8cb7ad2ca7
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-07-30 22:27:04 +0000

    Work with older Boosts.
    git-svn-id: https://svn.salilab.org/imp/trunk@14933 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/librmf/src/FileLock.cpp
+++ modules/librmf/src/FileLock.cpp
@@ -11,13 +11,23 @@
 // waiting
 #include <boost/date_time/posix_time/posix_time.hpp>
 #include <boost/thread/thread.hpp>
+#if BOOST_VERSION < 103500
+#include <boost/thread/xtime.hpp>
+#endif
 
 namespace RMF {
 
 FileLock::FileLock(FileConstHandle h, double retry_milliseconds): h_(h) {
   while (!h_.set_is_locked(true)) {
+#if BOOST_VERSION < 103500
+    boost::xtime xt;
+    boost::xtime_get(&xt, boost::TIME_UTC);
+    xt.nsec += 1000000 * retry_milliseconds;
+    boost::thread::sleep(xt);
+#else
     boost::this_thread::sleep(boost::posix_time
                               ::milliseconds(retry_milliseconds));
+#endif
   }
 }
 
