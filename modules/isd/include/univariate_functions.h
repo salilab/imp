be0e5fb23fb022b2afd56b924b09f7a473aaf898
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-08-02 22:44:50 +0000

    fix minor doc file names and things
    git-svn-id: https://svn.salilab.org/imp/trunk@14963 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/isd/include/univariate_functions.h
+++ modules/isd/include/univariate_functions.h
@@ -1,5 +1,5 @@
 /**
- *  \file functions.h
+ *  \file univariate_functions.h
  *  \brief Classes for general functions
  *
  *  Copyright 2007-2012 IMP Inventors. All rights reserved.
--- modules/librmf/include/HDF5DataSetAccessPropertiesD.h
+++ modules/librmf/include/HDF5DataSetAccessPropertiesD.h
@@ -1,5 +1,5 @@
 /**
- *  \file RMF/HDF5DataSetAccessProperties.h
+ *  \file RMF/HDF5DataSetAccessPropertiesD.h
  *  \brief Handle read/write of Model data from/to files.
  *
  *  Copyright 2007-2012 IMP Inventors. All rights reserved.
