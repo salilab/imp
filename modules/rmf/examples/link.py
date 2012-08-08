1e3788d109a535da9db5662d58dd753c54799567
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-08-08 00:13:50 +0000

    add a silly example for rmf.link concept
    git-svn-id: https://svn.salilab.org/imp/trunk@15004 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/rmf/examples/link.py
@@ -0,0 +1,35 @@
+import IMP.atom
+import IMP.rmf
+import RMF
+m= IMP.Model()
+
+# Create a new IMP.atom.Hierarchy from the contents of the pdb file
+h= IMP.atom.read_pdb(IMP.rmf.get_example_path("simple.pdb"), m)
+
+# find the name for a temporary file to use to for writing the hdf5 file
+tfn=IMP.create_temporary_file_name("link", ".rmf")
+
+print "File name is", tfn
+
+# open the temporary file, clearing any existing contents
+rh = RMF.create_rmf_file(tfn)
+
+# add the hierarchy to the file
+IMP.rmf.add_hierarchies(rh, [h])
+
+# add the current configuration to the file as frame 0
+IMP.rmf.save_frame(rh, 0)
+
+# close the file
+del rh
+
+# reopen it, don't clear the file when opening it
+rh= RMF.open_rmf_file(tfn)
+
+# link to the existing pdb hierarchy
+IMP.rmf.link_hierarchies(rh, [h])
+
+# load the same coordinates in, ok, that is not very exciting
+IMP.rmf.load_frame(rh, 0)
+
+print "Try running hdf5_display or hdf5_show on", tfn
