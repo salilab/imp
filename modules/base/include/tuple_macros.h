93d8a5cf978a28f14edf74c960bb9ed6a453bdd1
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-08-19 00:24:06 +0000

    add missing headers and scopes
    git-svn-id: https://svn.salilab.org/imp/trunk@15088 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/base/include/tuple_macros.h
+++ modules/base/include/tuple_macros.h
@@ -10,7 +10,8 @@
 #define IMPBASE_TUPLE_MACROS_H
 #include "base_config.h"
 #include "Value.h"
-
+#include "Showable.h"
+#include "hash.h"
 
 /** \name Named tuples
     It is often useful to declare little structures to aid in the passing
@@ -27,13 +28,13 @@
     type0 var0;                                                         \
     Name(type0 i0=type0()): var0(i0){invariant;}                        \
     IMP_HASHABLE_INLINE(Name, {                                         \
-        using IMP::hash_value;                                          \
+        using IMP::base::hash_value;                                    \
         std::size_t value= hash_value(var0);                            \
         return value;                                                   \
       });                                                               \
     IMP_COMPARISONS_1(Name, var0);                                      \
     IMP_SHOWABLE_INLINE(Name, out << "(" << #var0 << "="                \
-                        << Showable(var0) << ")");                      \
+                        << IMP::base::Showable(var0) << ")");           \
   };                                                                    \
   IMP_VALUES(Name, Names)
 
@@ -47,13 +48,15 @@
     Name(type0 i0=type0(), type1 i1=type1()): var0(i0), var1(i1)        \
     {invariant;}                                                        \
     IMP_HASHABLE_INLINE(Name, {                                         \
-        using IMP::hash_value;                                          \
+        using IMP::base::hash_value;                                    \
         std::size_t value= hash_value(var0);                            \
         boost::hash_combine(value, hash_value(var1));                   \
         return value;                                                   \
       });                                                               \
-    IMP_SHOWABLE_INLINE(Name, out << "(" << #var0 << "=" << Showable(var0) \
-                        << " " <<#var1 << "=" << Showable(var1) << ")"); \
+    IMP_SHOWABLE_INLINE(Name, out << "(" << #var0 << "="                \
+                        << IMP::base::Showable(var0)                    \
+                        << " " <<#var1 << "="                           \
+                        << IMP::base::Showable(var1) << ")");           \
     IMP_COMPARISONS_2(Name, var0, var1);                                \
   };                                                                    \
   IMP_VALUES(Name, Names)
@@ -68,16 +71,19 @@
     Name(type0 i0=type0(), type1 i1=type1(),type2 i2=type2()            \
          ): var0(i0), var1(i1), var2(i2){invariant;}                    \
     IMP_HASHABLE_INLINE(Name, {                                         \
-        using IMP::hash_value;                                          \
+        using IMP::base::hash_value;                                    \
         std::size_t value= hash_value(var0);                            \
         boost::hash_combine(value, hash_value(var1));                   \
         boost::hash_combine(value, hash_value(var2));                   \
         return value;                                                   \
       });                                                               \
     IMP_COMPARISONS_3(Name, var0, var1, var2);                          \
-    IMP_SHOWABLE_INLINE(Name, out << "(" << #var0 << "=" << Showable(var0) \
-                        << " " <<#var1 << "=" << Showable(var1)         \
-                        << " " <<#var2 << "=" << Showable(var2) << ")"); \
+    IMP_SHOWABLE_INLINE(Name, out << "(" << #var0 << "="                \
+                        << IMP::base::Showable(var0)                    \
+                        << " " <<#var1 << "="                           \
+                        << IMP::base::Showable(var1)                    \
+                        << " " <<#var2 << "="                           \
+                        << IMP::base::Showable(var2) << ")");           \
   };                                                                    \
   IMP_VALUES(Name, Names)
 
