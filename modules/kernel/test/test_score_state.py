7f0d8092b8790b990b944f2b5b0fd1ff003545db
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-09-05 23:47:58 +0000

    make the score state test more interesting and actually test that the score states are updated
    git-svn-id: https://svn.salilab.org/imp/trunk@15222 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/kernel/test/test_score_state.py
+++ modules/kernel/test/test_score_state.py
@@ -2,43 +2,21 @@ import sys
 import IMP
 import IMP.test
 
-class LoggingRestraint(IMP.Restraint):
-    """Do-nothing restraint that logs all calls"""
-
-    def __init__(self, log):
-        IMP.Restraint.__init__(self)
-        self.log = log
-    def do_show(self, fh):
-        fh.write("LoggingRestraint")
-    def get_version_info(self):
-        return IMP.get_module_version_info()
-    def get_type_name(self):
-        return "LoggingRestraint"
-    def unprotected_evaluate(self, accum):
-        if accum:
-            self.log.append('restraint-deriv')
-        else:
-            self.log.append('restraint-score')
-        return 0.0
-    def get_input_particles(self):
-        return []
-    def get_input_containers(self):
-        return []
 
 class LoggingScoreState(IMP.ScoreState):
     """ScoreState that logs all calls"""
 
-    def __init__(self, log):
-        IMP.ScoreState.__init__(self)
+    def __init__(self, m, log):
+        IMP.ScoreState.__init__(self, m)
         self.log = log
 
-    def do_before_evaluate(self):
+    def _do_before_evaluate(self):
         self.log.append('update')
     def do_show(self, out):
         print "Hi"
     def get_type_name(self):
         return "LoggingScoreState"
-    def do_after_evaluate(self, accum):
+    def _do_after_evaluate(self, accum):
         self.log.append('after_evaluate')
     def do_show(self, fh=sys.stdout):
         fh.write("LoggingScoreState")
@@ -55,14 +33,15 @@ class LoggingScoreState(IMP.ScoreState):
     def get_interacting_particles(self):
         return []
 
-class TestScoreState(IMP.test.TestCase):
+class Tests(IMP.test.TestCase):
     """Tests for ScoreState"""
 
-    def test_score_state_show(self):
-        """Test ScoreState::show()"""
+    def test_score_state_update(self):
+        """Test that score states are updated"""
         log = []
-        ss = LoggingScoreState(log)
-        ss.show()
-
+        m=IMP.Model()
+        ss = LoggingScoreState(m, log)
+        m.update()
+        self.assertEqual(ss.log, ["update"])
 if __name__ == '__main__':
     IMP.test.main()
