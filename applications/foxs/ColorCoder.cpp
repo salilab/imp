14a896b6381798755dd931f6d1da44bf8a84c766
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-09-12 22:07:22 +0000

    MSVC doesn't like the use of Vector3D here, so use primitive types instead.
    git-svn-id: https://svn.salilab.org/imp/trunk@15253 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- applications/foxs/ColorCoder.cpp
+++ applications/foxs/ColorCoder.cpp
@@ -8,11 +8,20 @@
 
 int ColorCoder::diff_ = 30;
 
-IMP::algebra::Vector3D ColorCoder::color_modules_[6] = {
-  IMP::algebra::Vector3D(0,0,1),
-  IMP::algebra::Vector3D(0,1,0),
-  IMP::algebra::Vector3D(1,0,0),
-  IMP::algebra::Vector3D(0,1,1),
-  IMP::algebra::Vector3D(1,0,1),
-  IMP::algebra::Vector3D(1,1,0)
-};
+void ColorCoder::get_color_for_id(int &r, int &g, int &b, int id)
+{
+  const float color_modules[6][3] = {
+    {0,0,1},
+    {0,1,0},
+    {1.0,0},
+    {0,1,1},
+    {1,0,1},
+    {1,1,0}
+  };
+  int mod = id %6;
+  int i = id / 6;
+  int mult = 255 - i * diff_;
+  r = (int)(color_modules[mod][0] * mult);
+  g = (int)(color_modules[mod][1] * mult);
+  b = (int)(color_modules[mod][2] * mult);
+}
