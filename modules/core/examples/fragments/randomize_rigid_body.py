121d5ed75ac2b9c8b3a2670de96eb81d371431e7
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-10-09 23:21:56 +0000

    fix example code fragment in RigidBody documentation
    git-svn-id: https://svn.salilab.org/imp/trunk@15441 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/examples/fragments/randomize_rigid_body.py
+++ modules/core/examples/fragments/randomize_rigid_body.py
@@ -1,14 +1,27 @@
-# Assume p is a RigidBody Particle
-rbd= IMP.core.RigidBody(p)
-translation=IMP.algebra.get_random_vector_in(IMP.algebra.Vector3D(0,0,0),
-                                                 10.0)
+import IMP.core
+import IMP.algebra
+import IMP
+from IMP.algebra import ReferenceFrame3D
+from IMP.algebra import Transformation3D
+
+m = IMP.Model()
+p = IMP.Particle(m)
+rbd = IMP.core.RigidBody.setup_particle(p, ReferenceFrame3D())
+translation=IMP.algebra.get_random_vector_in(IMP.algebra.get_unit_bounding_box_3d())
+
 # we don't yet have python code to generate a nearby rotation
 rotation= IMP.algebra.get_random_rotation_3d()
-transformation= IMP.algebra.Transformation3D(rotation, translation)
+transformation= Transformation3D(rotation, translation)
+# Option 1:
 # note, this overwrites the existing position
 # The True is to transform the members now rather than wait for a
 # score state
-rbd.set_transformation(transformation)
-# to instead perturb the existing transformation instead do
-rbd.set_transformation(IMP.algebra.compose(rbd.get_transformation(),
-                                                           transformation))
+rbd.set_reference_frame(ReferenceFrame3D(transformation))
+# Option 2:
+# perturb the existing transformation
+composed_tr = IMP.algebra.compose \
+                       (rbd.get_reference_frame().get_transformation_to(),
+                        transformation)
+rbd.set_reference_frame(ReferenceFrame3D(composed_tr))
+# Alternative to Option 2:
+IMP.core.transform(rbd, transformation)
