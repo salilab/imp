b9430b9b2ff525951993593fe2b3d24be483da08
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-10-11 20:58:18 +0000

    forgot a file that uses example dep
    git-svn-id: https://svn.salilab.org/imp/trunk@15453 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/example/include/use_example_dependency.h
@@ -0,0 +1,21 @@
+/**
+ *  \file IMP/example/use_example_dependency.h
+ *  \brief A simple unary function.
+ *
+ *  Copyright 2007-2012 IMP Inventors. All rights reserved.
+ *
+ */
+
+#ifndef IMPEXAMPLE_USE_EXAMPLE_DEPENDENCY_H
+#define IMPEXAMPLE_USE_EXAMPLE_DEPENDENCY_H
+
+#include "example_config.h"
+#if !defined(IMPEXAMPLE_NO_EXAMPLE_DEPENDENCY)
+#include <example_dependency_header.hh>
+#endif
+
+IMPEXAMPLE_BEGIN_NAMESPACE
+
+IMPEXAMPLE_END_NAMESPACE
+
+#endif  /* IMPEXAMPLE_USE_EXAMPLE_DEPENDENCY_H */
