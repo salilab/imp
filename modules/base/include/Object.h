e00711e2d9ddbb68c1e8b07e001a73a91fa33361
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-10-12 00:13:20 +0000

    add (hidden) method to output whole object show to error streams
    git-svn-id: https://svn.salilab.org/imp/trunk@15455 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/base/include/Object.h
+++ modules/base/include/Object.h
@@ -12,9 +12,27 @@
 #include "base_config.h"
 #include "declare_Object.h"
 #include "object_cast.h"
+#include <sstream>
 
 IMPBASE_BEGIN_NAMESPACE
-
+#if !defined(IMP_DOXYGEN) && !defined(SWIG)
+/** Send the whole show output to a stream*/
+class ShowFull {
+  std::string showed_;
+ public:
+  ShowFull(Object *o){
+    std::ostringstream oss;
+    o->show(oss);
+    showed_=oss.str();
+  }
+  const std::string &get_string() const {return showed_;}
+};
+inline std::ostream &operator<<(std::ostream &o,
+                               const ShowFull& sf) {
+  o << sf.get_string();
+  return o;
+}
+#endif
 IMPBASE_END_NAMESPACE
 
 
