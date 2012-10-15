45d3b5209bac1bbb889a8d7e6daa8a13b6fd3219
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-10-15 17:48:58 +0000

    don't require modules that use example to link against the dummy dependency explicitly
    except for when doing static linking, but we having turned on static building of the example dependency, so that will just get skipped
    
    git-svn-id: https://svn.salilab.org/imp/trunk@15462 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/example/src/use_example_dependency.cpp
+++ modules/example/src/use_example_dependency.cpp
@@ -1,14 +1,10 @@
 /**
- *  \file IMP/example/use_example_dependency.h
+ *  \file use_example_dependency.cpp
  *  \brief A simple unary function.
  *
  *  Copyright 2007-2012 IMP Inventors. All rights reserved.
  *
  */
-
-#ifndef IMPEXAMPLE_USE_EXAMPLE_DEPENDENCY_H
-#define IMPEXAMPLE_USE_EXAMPLE_DEPENDENCY_H
-
 #include "example_config.h"
 #if !defined(IMPEXAMPLE_NO_EXAMPLE_DEPENDENCY)
 #include <example_dependency_header.hh>
@@ -17,5 +13,3 @@
 IMPEXAMPLE_BEGIN_NAMESPACE
 
 IMPEXAMPLE_END_NAMESPACE
-
-#endif  /* IMPEXAMPLE_USE_EXAMPLE_DEPENDENCY_H */
