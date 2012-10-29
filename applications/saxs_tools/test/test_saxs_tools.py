f5e34040c36c7cbbaa4b89ad14883c59ebb2c99c
dina@SALILAB.ORG <dina@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-10-29 19:49:55 +0000

    add a program to compute chi score between two profiles
    git-svn-id: https://svn.salilab.org/imp/trunk@15531 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- applications/saxs_tools/test/test_saxs_tools.py
+++ applications/saxs_tools/test/test_saxs_tools.py
@@ -5,7 +5,7 @@ import re
 
 class SAXSToolsTest(IMP.test.ApplicationTestCase):
     def test_rg(self):
-        """Simple test of SAXS profile application"""
+        """Simple test of Rg calculation"""
         print self.get_input_file_name('6lyz.pdb')
         print self.get_input_file_name('lyzexp.dat')
         p = self.run_application('rg',
@@ -20,6 +20,20 @@ class SAXSToolsTest(IMP.test.ApplicationTestCase):
         m = re.search('6lyz.pdb Rg= ([\d\.]+)', out)
         self.assertIsNotNone(m, msg="Rg value output not found in " + str(out))
         self.assertAlmostEqual(float(m.group(1)), 13.966, delta=0.1)
+
+    def test_compute_chi(self):
+        """Simple test of Chi calculation"""
+        print self.get_input_file_name('6lyz.pdb')
+        print self.get_input_file_name('lyzexp.dat')
+        p = self.run_application('compute_chi',
+                                 [self.get_input_file_name('lyzexp.dat'),
+                                  self.get_input_file_name('6lyz.pdb.dat')])
+        out, err = p.communicate()
+        sys.stderr.write(err)
+        self.assertApplicationExitedCleanly(p.returncode, err)
+        m = re.search('6lyz.pdb.dat chi=([\d\.]+)', out)
+        self.assertIsNotNone(m, msg="chi value output not found in " + str(out))
+        self.assertAlmostEqual(float(m.group(1)), 0.537, delta=0.1)
 
 
 if __name__ == '__main__':
