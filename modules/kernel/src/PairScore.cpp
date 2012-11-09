321b2373140350db41c66849720589289a8ecfb5
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-11-09 23:25:41 +0000

    no more warnings in kernel
    git-svn-id: https://svn.salilab.org/imp/trunk@15668 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/kernel/src/PairScore.cpp
+++ modules/kernel/src/PairScore.cpp
@@ -22,6 +22,61 @@ PairScore::PairScore(std::string name):
      symbols are present in the kernel DSO */
 }
 
+double PairScore::evaluate(const ParticlePair& vt,
+                                DerivativeAccumulator *da) const {
+  return evaluate_index(internal::get_model(vt),
+                        internal::get_index(vt),
+                        da);
+}
+
+double PairScore::evaluate_index(Model *m, const ParticleIndexPair& vt,
+                                      DerivativeAccumulator *da) const {
+  // see http://gcc.gnu.org/bugzilla/show_bug.cgi?id=53469
+#pragma GCC diagnostic push
+#pragma GCC diagnostic ignored "-Wdeprecated-declarations"
+  IMP_DEPRECATED_IGNORE( return evaluate(internal::get_particle(m, vt), da));
+#pragma GCC diagnostic pop
+}
+
+double PairScore::evaluate_indexes(Model *m,
+                                        const ParticleIndexPairs &o,
+                                        DerivativeAccumulator *da,
+                                        unsigned int lower_bound,
+                                        unsigned int upper_bound) const {
+  double ret=0;
+  for (unsigned int i=lower_bound; i< upper_bound; ++i) {
+    ret+= evaluate_index(m, o[i], da);
+  }
+  return ret;
+}
+
+
+double PairScore::evaluate_if_good_index(Model *m,
+                                              const ParticleIndexPair& vt,
+                                              DerivativeAccumulator *da,
+                                              double max)
+  const {
+  IMP_UNUSED(max);
+  return evaluate_index(m, vt, da);
+}
+
+double PairScore::evaluate_if_good_indexes(Model *m,
+                                                const ParticleIndexPairs &o,
+                                                DerivativeAccumulator *da,
+                                                double max,
+                                                unsigned int lower_bound,
+                                                unsigned int upper_bound)
+  const {
+  double ret=0;
+  for (unsigned int i=lower_bound; i< upper_bound; ++i) {
+    double cur= evaluate_if_good_index(m, o[i], da, max-ret);
+    max-=cur;
+    ret+=cur;
+    if (max<0) break;
+  }
+  return ret;
+}
+
 
 Restraints
 PairScore
