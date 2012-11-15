9be57a8345c0f769c25b64d73635eee91b194178
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-11-15 01:48:49 +0000

    provide backwards compat macros for restraints and score states
    git-svn-id: https://svn.salilab.org/imp/trunk@15725 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/kernel/include/input_output_macros.h
+++ modules/kernel/include/input_output_macros.h
@@ -33,13 +33,38 @@
   IMP_IMPLEMENT(ParticlesTemp get_output_particles(Particle*) const);   \
   IMP_IMPLEMENT(ContainersTemp get_output_containers(Particle*) const); \
   IMP_IMPLEMENT_INLINE(ModelObjectsTemp                                 \
-                       do_get_outputs(Model *m,                          \
-                                     const ParticleIndexes &pis) const, { \
+                       do_get_outputs(Model *m,                         \
+                                      const ParticleIndexes &pis)       \
+                       const IMP_OVERRIDE, {                            \
                          ModelObjectsTemp ret;                          \
                          for (unsigned int i=0; i< pis.size(); ++i) {   \
                            ret+=get_output_particles(m->get_particle(pis[i])); \
                            ret+=get_output_containers(m->get_particle(pis[i]));\
                          }                                              \
+                         return ret;                                    \
+                       })
+
+#define IMP_MODEL_OBJECT_BACKWARDS_MACRO_INPUTS                         \
+  public:                                                               \
+  IMP_IMPLEMENT(ParticlesTemp get_input_particles() const);             \
+  IMP_IMPLEMENT(ContainersTemp get_input_containers() const);           \
+  IMP_IMPLEMENT_INLINE(ModelObjectsTemp                                 \
+                       do_get_inputs() const IMP_OVERRIDE, {            \
+                         ModelObjectsTemp ret;                          \
+                         ret += get_input_containers();                 \
+                         ret += get_input_particles();                  \
+                         return ret;                                    \
+                       })
+
+#define IMP_MODEL_OBJECT_BACKWARDS_MACRO_OUTPUTS                        \
+  public:                                                               \
+  IMP_IMPLEMENT(ParticlesTemp get_output_particles() const);            \
+  IMP_IMPLEMENT(ContainersTemp get_output_containers() const);          \
+  IMP_IMPLEMENT_INLINE(ModelObjectsTemp                                 \
+                       do_get_outputs() const IMP_OVERRIDE, {           \
+                         ModelObjectsTemp ret;                          \
+                         ret += get_output_containers();                \
+                         ret += get_output_particles();                 \
                          return ret;                                    \
                        })
 
@@ -56,48 +81,48 @@ get_input_particles(Particle* p) const;         \
 IMP_DEPRECATED_WARN ContainersTemp              \
 get_input_containers(Particle *p) const
 
-#define IMP_INPUTS_DEF_BACKWARDS(Name)          \
-  ParticlesTemp                                                 \
-  Name::get_input_particles(Particle* p) const {                \
-  return\
+#define IMP_INPUTS_DEF_BACKWARDS(Name)                                  \
+    ParticlesTemp                                                       \
+    Name::get_input_particles(Particle* p) const {                      \
+      return                                                            \
     IMP::get_input_particles(get_inputs(p->get_model(),                 \
                                         ParticleIndexes(1,              \
                                                         p->get_index()))); \
-  }                                                                     \
-  ContainersTemp                                                        \
-  Name::get_input_containers(Particle *p) const {          \
-  return                                                                \
-  IMP::get_input_containers(get_inputs(p->get_model(),                  \
-                                       ParticleIndexes(1,               \
-                                                       p->get_index()))); \
-  }
+    }                                                                   \
+    ContainersTemp                                                      \
+    Name::get_input_containers(Particle *p) const {                     \
+      return                                                            \
+          IMP::get_input_containers(get_inputs(p->get_model(),          \
+                                               ParticleIndexes(1,       \
+                                                     p->get_index()))); \
+    }
 
 
 #define IMP_OUTPUTS_DECL_BACKWARDS(Name)                                \
   public:                                                               \
   /** Get the set of particles read when applied to the arguments.
-      \deprecated use get_outputs() instead.*/   \
-IMP_DEPRECATED_WARN ParticlesTemp               \
-get_output_particles(Particle* p) const;         \
-  /** Get the set of output containers when this modifier is applied to
-      the arguments.
-      \deprecated use get_outputs() instead.*/  \
+      \deprecated use get_outputs() instead.*/                          \
+IMP_DEPRECATED_WARN ParticlesTemp                                       \
+get_output_particles(Particle* p) const;                                \
+/** Get the set of output containers when this modifier is applied to
+    the arguments.
+    \deprecated use get_outputs() instead.*/    \
 IMP_DEPRECATED_WARN ContainersTemp              \
 get_output_containers(Particle *p) const
 
-#define IMP_OUTPUTS_DEF_BACKWARDS(Name)          \
-  ParticlesTemp                                                 \
-  Name::get_output_particles(Particle* p) const {                \
-  return\
-    IMP::get_output_particles(get_outputs(p->get_model(),                 \
-                                        ParticleIndexes(1,              \
-                                                        p->get_index()))); \
-  }                                                                     \
+#define IMP_OUTPUTS_DEF_BACKWARDS(Name)                                 \
+    ParticlesTemp                                                       \
+    Name::get_output_particles(Particle* p) const {                     \
+      return                                                            \
+          IMP::get_output_particles(get_outputs(p->get_model(),         \
+                                                ParticleIndexes(1,      \
+                                                     p->get_index()))); \
+    }                                                                   \
   ContainersTemp                                                        \
-  Name::get_output_containers(Particle *p) const {          \
-  return                                                                \
-  IMP::get_output_containers(get_outputs(p->get_model(),                  \
-                                       ParticleIndexes(1,               \
+  Name::get_output_containers(Particle *p) const {                      \
+    return                                                              \
+        IMP::get_output_containers(get_outputs(p->get_model(),          \
+                                               ParticleIndexes(1,       \
                                                        p->get_index()))); \
   }
 
