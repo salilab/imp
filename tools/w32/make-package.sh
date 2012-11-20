8218a44acce1449ec39c532f50de22d2b449c165
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-11-20 02:29:43 +0000

    Bundle avrocpp support library for RMF.
    git-svn-id: https://svn.salilab.org/imp/trunk@15766 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- tools/w32/make-package.sh
+++ tools/w32/make-package.sh
@@ -89,6 +89,7 @@ cp ${DLLSRC}/hdf5dll.dll ${DLLSRC}/libgsl.dll ${DLLSRC}/libgslcblas.dll \
    ${DLLSRC}/boost_regex-vc100-mt-1_44.dll \
    ${DLLSRC}/boost_thread-vc100-mt-1_44.dll \
    ${DLLSRC}/libfftw3-3.dll \
+   ${DLLSRC}/avrocpp.dll \
    ${DLLSRC}/libTAU1.dll \
    ${DLLSRC}/opencv_core220.dll ${DLLSRC}/opencv_highgui220.dll \
    ${DLLSRC}/opencv_ffmpeg220.dll \
