ec2fa50fc1e0e933bce5c10a7e7e3210c248d21b
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-11-30 21:35:39 +0000

    add test that exceptions are converted properly
    git-svn-id: https://svn.salilab.org/imp/trunk@15821 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/rmf/test/test_errors.py
@@ -0,0 +1,22 @@
+import unittest
+import IMP.rmf
+import IMP.atom
+import IMP.test
+import RMF
+from IMP.algebra import *
+
+class GenericTest(IMP.test.TestCase):
+    def _get_num_nodes(self, n):
+        return 1+sum([self._get_num_nodes(c) for c in n.get_children()])
+    def test_round_trip(self):
+        """Test that exceptions are transformed"""
+        m= IMP.Model()
+        rmf= RMF.create_rmf_file(self.get_tmp_file_name("zillion.rmf"))
+        h= IMP.atom.Fragment.setup_particle(IMP.Particle(m))
+        IMP.core.XYZR.setup_particle(h)
+        IMP.atom.Mass.setup_particle(h, 1)
+        IMP.rmf.add_hierarchies(rmf, [h])
+        IMP.rmf.save_frame(rmf, 0)
+        self.assertRaises(IMP.IOException, IMP.rmf.load_frame, rmf, 6)
+if __name__ == '__main__':
+    unittest.main()
