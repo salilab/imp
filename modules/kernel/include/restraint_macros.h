08ee5f3bc6e061be6ef6f40bffd22c293be3733a
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-12-11 00:26:10 +0000

    add missing includes and scopes in macros
    git-svn-id: https://svn.salilab.org/imp/trunk@15852 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/kernel/include/restraint_macros.h
+++ modules/kernel/include/restraint_macros.h
@@ -13,6 +13,7 @@
 #include "internal/scoring_functions.h"
 #include "container_base.h"
 #include "input_output_macros.h"
+#include "constants.h"
 
 /** At this point, you should probably use IMP_RESTRAINT_ACCUMULATOR()
     for new restraints.
@@ -20,11 +21,12 @@
 #define IMP_RESTRAINT(Name)                                             \
   public:                                                               \
   IMP_IMPLEMENT( double                                                 \
-                unprotected_evaluate(DerivativeAccumulator *accum) const); \
+                 unprotected_evaluate(IMP::DerivativeAccumulator *accum)\
+                 const);                                                \
   IMP_IMPLEMENT_INLINE(ScoringFunction *                                \
                        create_scoring_function(double weight=1.0,       \
                                                double max               \
-                                               = NO_MAX) const, {       \
+                                               = IMP::NO_MAX) const, {  \
                          set_was_used(true);                            \
                          return IMP::internal::create_scoring_function  \
                              (const_cast<Name*>(this),                  \
@@ -42,16 +44,17 @@
 #define IMP_RESTRAINT_2(Name)                                             \
   public:                                                               \
   IMP_IMPLEMENT( double                                                 \
-                unprotected_evaluate(DerivativeAccumulator *accum) const); \
-  IMP_IMPLEMENT_INLINE(ScoringFunction *                                \
+                 unprotected_evaluate(IMP::DerivativeAccumulator *accum) \
+                 const);                                                \
+  IMP_IMPLEMENT_INLINE(IMP::ScoringFunction *                           \
                        create_scoring_function(double weight=1.0,       \
                                                double max               \
-                                               = NO_MAX) const, {       \
+                                               = IMP::NO_MAX) const, {  \
                          return IMP::internal::create_scoring_function  \
                              (const_cast<Name*>(this),                  \
                               weight, max);                             \
                        });                                              \
-  IMP_IMPLEMENT(ModelObjectsTemp do_get_inputs() const);                \
+  IMP_IMPLEMENT(IMP::ModelObjectsTemp do_get_inputs() const);           \
   IMP_OBJECT(Name)
 
 //! Define the basic things you need for a Restraint.
