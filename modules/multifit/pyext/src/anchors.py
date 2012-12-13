7cf57778e84e36811020de5857a1df23fceee256
kerenl@SALILAB.ORG <kerenl@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-12-13 21:50:17 +0000

    allow to change number of anchors
    git-svn-id: https://svn.salilab.org/imp/trunk@15885 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/multifit/pyext/src/anchors.py
+++ modules/multifit/pyext/src/anchors.py
@@ -11,18 +11,33 @@ def parse_args():
 
 Generate anchors for a density map."""
     parser = OptionParser(usage)
+    parser.add_option("-s", "--size", type="int", dest="size", default=-1,
+                      help="number of residues per bead")
     options, args = parser.parse_args()
+
+
     if len(args) != 2:
         parser.error("incorrect number of arguments")
-    return args
+    return options,args
+
 
 def main():
-    asmb_fn, output = parse_args()
+    options,args=parse_args()
+    asmb_fn=args[0]
+    output=args[1]
     asmb = IMP.multifit.read_settings(asmb_fn)
     asmb.set_was_used(True)
     dmap = IMP.em.read_map(asmb.get_assembly_header().get_dens_fn(),
                            IMP.em.MRCReaderWriter())
-    number_of_means = asmb.get_number_of_component_headers()
+    if options.size>99999:
+        number_of_means = asmb.get_number_of_component_headers()
+    else:
+        total_num_residues=0
+        mdl=IMP.Model()
+        for i in range(asmb.get_number_of_component_headers()):
+            total_num_residues+=len(IMP.atom.get_by_type(IMP.atom.read_pdb(asmb.get_component_header(i).get_filename(),mdl),IMP.atom.RESIDUE_TYPE))
+            number_of_means=total_num_residues/options.size
+    print "Calculating a segmentation into",number_of_means,"regions"
     density_threshold = asmb.get_assembly_header().get_threshold()
 
     IMP.multifit.get_anchors_for_density(dmap, number_of_means,
