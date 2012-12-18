86086266c88e2f0747de569967f5862e75c39557
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-12-18 19:17:52 +0000

    clang++ with c++11 doesn't work yet with opencv or eigen.
    git-svn-id: https://svn.salilab.org/imp/trunk@15930 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- scons_tools/variables.py
+++ scons_tools/variables.py
@@ -107,8 +107,8 @@ def _get_platform_cxxflags(env):
         ret+=["-Wno-c++11-extensions"]
         if env['cxx11']=="yes":
             print "WARNING: boost and clang++ C++11 don't see to get along"
-        if env['cxx11'] != 'no':
-            ret+=["-std=c++11"]
+        #if env['cxx11'] != 'no':
+        #    ret+=["-std=c++11"]
         if env['build'] == 'fast':
             ret+=["-O3"]
         elif env['build'] == 'release':
