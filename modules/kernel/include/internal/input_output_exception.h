28495a08fb1a4eb6f7410a29b02863cb6b3231c0
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2012-12-20 05:21:03 +0000

    add new exception files
    git-svn-id: https://svn.salilab.org/imp/trunk@15972 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/kernel/include/internal/input_output_exception.h
@@ -0,0 +1,51 @@
+/**
+ *  \file IMP/base/exception.h
+ *  \brief Exception definitions and assertions.
+ *
+ *  Copyright 2007-2012 IMP Inventors. All rights reserved.
+ *
+ */
+
+#ifndef IMPKERNEL_INPUT_OUTPUT_EXCEPTION_H
+#define IMPKERNEL_INPUT_OUTPUT_EXCEPTION_H
+
+#include "../kernel_config.h"
+#include <IMP/base/exception.h>
+IMP_BEGIN_NAMESPACE
+class ModelObject;
+IMP_END_NAMESPACE
+
+IMP_BEGIN_INTERNAL_NAMESPACE
+
+//! An exception when something is missing from an input or output list.
+/** See IMP::ModelObject::get_inputs() and IMP::ModelObject::get_outputs().
+ */
+struct IMPEXPORT InputOutputException: public base::ExceptionBase
+{
+  InputOutputException(int particle_index,
+                       int operation,
+                       int entity,
+                       std::string key_name);
+  InputOutputException(std::string container_name,
+                       int entity);
+   ~InputOutputException() throw();
+  enum AccessEntity {NO_ENTITY, ATTRIBUTE, DERIVATIVE};
+  enum AccessType {GET, SET, REMOVE, ADD};
+  int get_particle_index() const {return particle_index_;};
+  std::string get_container_name() const {return container_name_;};
+  int get_operation() const {return operation_;}
+  int get_entity() const {return entity_;}
+  std::string get_key_name() const {return key_name_;}
+  std::string get_message(ModelObject *o) const;
+private:
+  int particle_index_;
+  std::string container_name_;
+  int operation_;
+  int entity_;
+  std::string key_name_;
+};
+
+
+IMP_END_INTERNAL_NAMESPACE
+
+#endif  /* IMPKERNEL_INPUT_OUTPUT_EXCEPTION_H */
