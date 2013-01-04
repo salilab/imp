ebb06606cca097702fde385914fd6dafd6729b01
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-01-04 00:40:28 +0000

    sync RMF to edfcf8a574ff4eafed836b8439fc069de6c9f914
    git-svn-id: https://svn.salilab.org/imp/trunk@16059 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/rmf/dependency/RMF/CMakeModules/FindAvroCPP.cmake
+++ modules/rmf/dependency/RMF/CMakeModules/FindAvroCPP.cmake
@@ -3,24 +3,24 @@ include(LibFindMacros)
 #libfind_package(Log4CXX)
 
 # Use pkg-config to get hints about paths
-libfind_pkg_check_modules(Avro_PKGCONF avro)
+libfind_pkg_check_modules(AvroCPP_PKGCONF avro)
 
 find_program(avrogencpp_PROGRAM NAMES avrogencpp)
 
 # Include dir
-find_path(Avro_INCLUDE_DIR
+find_path(AvroCPP_INCLUDE_DIR
   NAMES avro/Compiler.hh
-  PATHS ${Avro_PKGCONF_INCLUDE_DIRS}
+  PATHS ${AvroCPP_PKGCONF_INCLUDE_DIRS}
 )
 
 # Finally the library itself
-find_library(Avro_LIBRARY
+find_library(AvroCPP_LIBRARY
   NAMES avrocpp
-  PATHS ${Avro_PKGCONF_LIBRARY_DIRS}
+  PATHS ${AvroCPP_PKGCONF_LIBRARY_DIRS}
 )
 
 # Set the include dir variables and the libraries and let libfind_process do the rest.
 # NOTE: Singular variables for this library, plural for libraries this this lib depends on.
-set(Avro_PROCESS_INCLUDES Avro_INCLUDE_DIR)
-set(Avro_PROCESS_LIBS Avro_LIBRARY)
-libfind_process(Avro)
+set(AvroCPP_PROCESS_INCLUDES AvroCPP_INCLUDE_DIR)
+set(AvroCPP_PROCESS_LIBS AvroCPP_LIBRARY)
+libfind_process(AvroCPP)
