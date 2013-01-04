5b2d5befb206631c9a3d8106d81df192b1f0f362
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-01-04 00:47:34 +0000

    work around compiler bug
    git-svn-id: https://svn.salilab.org/imp/trunk@16064 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/cgal/src/internal/bounding_sphere.cpp
+++ modules/cgal/src/internal/bounding_sphere.cpp
@@ -4,11 +4,10 @@
  *
  *  Copyright 2007-2012 IMP Inventors. All rights reserved.
  */
-#include <IMP/cgal/internal/bounding_sphere.h>
-#include <cmath>
-
-#include <CGAL/Exact_predicates_inexact_constructions_kernel.h>
+#include <CGAL/Cartesian.h>
 #include <CGAL/Min_sphere_of_spheres_d.h>
+#include <cmath>
+#include <IMP/cgal/internal/bounding_sphere.h>
 
 IMPCGAL_BEGIN_INTERNAL_NAMESPACE
 
@@ -16,8 +15,8 @@ algebra::Sphere3D
 get_enclosing_sphere(const algebra::Sphere3Ds &ss) {
   IMP_USAGE_CHECK(!ss.empty(),
                   "Must pass some spheres to have a bounding sphere");
-  typedef CGAL::Exact_predicates_inexact_constructions_kernel             K;
-  typedef CGAL::Min_sphere_of_spheres_d_traits_3<K, K::FT> Traits;
+  typedef CGAL::Cartesian<double> K;
+  typedef CGAL::Min_sphere_of_spheres_d_traits_3<K, K::RT> Traits;
   typedef CGAL::Min_sphere_of_spheres_d<Traits> Min_sphere;
   typedef K::Point_3                        Point;
   typedef Traits::Sphere                    Sphere;
@@ -32,10 +31,11 @@ get_enclosing_sphere(const algebra::Sphere3Ds &ss) {
                              (ss[i].get_radius())));
   }
   Min_sphere ms(spheres.begin(), spheres.end());
-  algebra::Sphere3D s(algebra::Vector3D(*ms.center_cartesian_begin(),
-                      *(ms.center_cartesian_begin()+1),
-                      *(ms.center_cartesian_begin()+2)),
-              ms.radius());
+  double x=*ms.center_cartesian_begin();
+  double y=*(ms.center_cartesian_begin()+1);
+  double z=*(ms.center_cartesian_begin()+2);
+  double r=CGAL::to_double(ms.radius());
+  algebra::Sphere3D s(algebra::Vector3D(x,y,z), r);
    /*IMP_IF_LOG(VERBOSE) {
      IMP_LOG(VERBOSE, "Enclosing sphere is " << s << " for ");
      for (unsigned int i=0; i< ss.size(); ++i) {
@@ -46,6 +46,4 @@ get_enclosing_sphere(const algebra::Sphere3Ds &ss) {
    return s;
 
 }
-
-
 IMPCGAL_END_INTERNAL_NAMESPACE
