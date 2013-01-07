a46dbf7070b90d9618e0f6f7db7349b6052d7453
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-01-07 21:35:45 +0000

    Make sure SWIG modules are installed.
    git-svn-id: https://svn.salilab.org/imp/trunk@16083 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/rmf/dependency/RMF/imp_install.sh
+++ modules/rmf/dependency/RMF/imp_install.sh
@@ -6,6 +6,7 @@ ln -sfF $1/libRMF* $1/_RMF.so $1/RMF.py $2/lib/
 mkdir -p $2/include/RMF
 ln -sfF $3/include/RMF/* $2/include/RMF/
 ln -sfF $1/include/RMF/* $2/include/RMF/
+ln -sfF $3/swig/*.i $2/swig
 
 if ! -e $1/AvroCpp exit 0
 ln -sfF $1/AvroCpp/libavro* $2/lib/
