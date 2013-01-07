ee834338f700e069f8e9d0302200473d957c6540
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-01-07 22:40:53 +0000

    Use standard IMP option parsing.
    git-svn-id: https://svn.salilab.org/imp/trunk@16088 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- applications/EMageFit/emagefit_dock.py
+++ applications/EMageFit/emagefit_dock.py
@@ -231,70 +231,58 @@ def get_docked_reference_frames(hex_transforms, rb_ligand):
 
 
 if __name__ == "__main__":
-    from argparse import ArgumentParser
-    parser = ArgumentParser(description="Functions related to docking with HEX")
-    parser.add_argument("fn_receptor",
-                      default=False,
-                     help="Receptor PDB file")
-    parser.add_argument("fn_ligand",
-                      default=False,
-                     help="Ligand PDB file")
-    parser.add_argument("--dock",
-                      action="store_true",
-                     dest="dock",
-                      default=False,
-                     help="Dock the ligand into the receptor")
-    parser.add_argument("--hex",
-                     dest="fn_transforms",
-                      default=False,
-                     help="File where to write the hex transforms.")
-    parser.add_argument("--w",
-                        type=int,
-                        dest="write",
-                        default=False,
-                        help="Write a number of docking transformations" )
-    parser.add_argument("--int",
-                     dest="fn_internal_transforms",
-                      default=False,
-                     help="Input/Output file of internal transformations")
-    parser.add_argument("--log",
-                     dest="log",
-                     default=None,
-                     help="File for logging ")
+    parser = IMP.OptionParser(
+                      usage="""%prog [options] fn_receptor fn_ligand
 
-    args = parser.parse_args()
-    if(not args.fn_receptor):
-        parser.print_help()
-        sys.exit()
+fn_receptor and fn_ligand are the filenames of the PDB files for the
+receptor and ligand, respectively.""",
+                      description="Functions related to docking with HEX",
+                      imp_module=IMP.em2d)
+    parser.add_option("--dock",
+                      action="store_true", dest="dock", default=False,
+                      help="Dock the ligand into the receptor")
+    parser.add_option("--hex", dest="fn_transforms", default=False,
+                      help="File where to write the hex transforms.")
+    parser.add_option("--w", type=int, dest="write", default=False,
+                      help="Write a number of docking transformations" )
+    parser.add_option("--int", dest="fn_internal_transforms", default=False,
+                      help="Input/Output file of internal transformations")
+    parser.add_option("--log", dest="log", default=None,
+                      help="File for logging ")
 
-    if(args.log):
-        logging.basicConfig(filename=args.log, filemode="w")
+    opts, args = parser.parse_args()
+    if len(args) != 2:
+        parser.error("Wrong number of arguments")
+    fn_receptor, fn_ligand = args
+
+    if(opts.log):
+        logging.basicConfig(filename=opts.log, filemode="w")
     else:
         logging.basicConfig(stream=sys.stdout)
     logging.root.setLevel(logging.DEBUG)
 
     sel = atom.ATOMPDBSelector()
     m = IMP.Model()
-    h_receptor =  atom.read_pdb(args.fn_receptor, m, sel)
+    h_receptor =  atom.read_pdb(fn_receptor, m, sel)
     rb_receptor = atom.create_rigid_body(h_receptor)
-    h_ligand =  atom.read_pdb(args.fn_ligand, m, sel)
+    h_ligand =  atom.read_pdb(fn_ligand, m, sel)
     rb_ligand = atom.create_rigid_body(h_ligand)
-    if args.dock:
+    if opts.dock:
         check_for_hexdock()
-        if not args.fn_transforms or not args.fn_internal_transforms:
+        if not opts.fn_transforms or not opts.fn_internal_transforms:
             raise IOError("Docking requires the --int and --hex arguments")
         hex_docking = HexDocking()
-        hex_docking.dock(args.fn_receptor, args.fn_ligand, args.fn_transforms)
+        hex_docking.dock(fn_receptor, fn_ligand, opts.fn_transforms)
         # read the HEX file of solutions and get the internal transformations
         # giving the relative orientation of the ligand respect to the receptor
-        Ts = read_hex_transforms(args.fn_transforms)
+        Ts = read_hex_transforms(opts.fn_transforms)
         Tis = [get_internal_transform(T, rb_receptor, rb_ligand) for T in Ts]
-        io.write_transforms(Tis, args.fn_internal_transforms)
-    elif args.write:
+        io.write_transforms(Tis, opts.fn_internal_transforms)
+    elif opts.write:
         # To write the positions correctly, the script requires that the
         # ligand file is the same that was used for the docking
-        Tinternal = io.read_transforms(args.fn_internal_transforms)
-        max_number = min(args.write, len(Tinternal))
+        Tinternal = io.read_transforms(opts.fn_internal_transforms)
+        max_number = min(opts.write, len(Tinternal))
         Trec = rb_receptor.get_reference_frame().get_transformation_to()
         for i in range(max_number):
             Tdock = alg.compose(Trec, Tinternal[i])
