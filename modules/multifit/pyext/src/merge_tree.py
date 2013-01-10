362b9360a214a7ab385ffddf10f3a102ad25140d
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-01-10 19:45:43 +0000

    Tidy up and make callable from MultiFit application.
    git-svn-id: https://svn.salilab.org/imp/trunk@16130 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/multifit/pyext/src/merge_tree.py
+++ modules/multifit/pyext/src/merge_tree.py
@@ -1,57 +1,15 @@
 #!/usr/bin/python
 
-__doc__ = "Align proteomics graph with the EM map."
+__doc__ = "Show the DOMINO merge tree to be used in alignment."
 
-#analyse the ensemble, first we will do the rmsd stuff
-import sys
 import IMP.multifit
 from optparse import OptionParser
-
-class progressBar:
-    def __init__(self, minValue = 0, maxValue = 10, totalWidth=12):
-        self.progBar = "[]"   # This holds the progress bar string
-        self.min = minValue
-        self.max = maxValue
-        self.span = maxValue - minValue
-        self.width = totalWidth
-        self.amount = 0       # When amount == max, we are 100% done
-        self.updateAmount(0)  # Build progress bar string
-
-    def updateAmount(self, newAmount = 0):
-        if newAmount < self.min: newAmount = self.min
-        if newAmount > self.max: newAmount = self.max
-        self.amount = newAmount
-
-        # Figure out the new percent done, round to an integer
-        diffFromMin = float(self.amount - self.min)
-        percentDone = (diffFromMin / float(self.span)) * 100.0
-        percentDone = round(percentDone)
-        percentDone = int(percentDone)
-
-        # Figure out how many hash bars the percentage should be
-        allFull = self.width - 2
-        numHashes = (percentDone / 100.0) * allFull
-        numHashes = int(round(numHashes))
-
-        # build a progress bar with hashes and spaces
-        self.progBar = "[" + '#'*numHashes + ' '*(allFull-numHashes) + "]"
-
-        # figure out where to put the percentage, roughly centered
-        percentPlace = (len(self.progBar) / 2) - len(str(percentDone))
-        percentString = str(percentDone) + "%"
-
-        # slice the percentage into the bar
-        self.progBar = self.progBar[0:percentPlace] + percentString + self.progBar[percentPlace+len(percentString):]
-
-    def __str__(self):
-        return str(self.progBar)
-
 
 def parse_args():
     usage =  """%prog [options] <asmb> <asmb.proteomics> <asmb.mapping>
            <alignment.params>
 
-view domino merge tree to be used in the alignment procedure
+Show the DOMINO merge tree to be used in the alignment procedure
 """
     parser = OptionParser(usage)
 
@@ -69,7 +27,6 @@ def run(asmb_fn, proteomics_fn, mapping_fn, params_fn):
     threshold=asmb.get_assembly_header().get_threshold()
     dmap.update_voxel_size(asmb.get_assembly_header().get_spacing())
     dmap.set_origin(asmb.get_assembly_header().get_origin())
-    #get rmsd for subunits
 
     alignment_params = IMP.multifit.AlignmentParams(params_fn)
 
