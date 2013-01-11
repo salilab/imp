17340dd66d85c38c5191d7f59526a36219e2f76d
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-01-11 16:07:08 +0000

    Update for new show() output.
    git-svn-id: https://svn.salilab.org/imp/trunk@16135 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/test/test_force_switch.py
+++ modules/atom/test/test_force_switch.py
@@ -18,7 +18,7 @@ class ForceSwitchTests(IMP.test.TestCase):
         s = IMP.atom.ForceSwitch(5.0, 6.0)
         out = StringIO.StringIO()
         s.show(out)
-        self.assertIn('ForceSwitch', out.getvalue())
+        self.assertIn('switching between', out.getvalue())
 
     def test_value(self):
         """Check value of ForceSwitch"""
