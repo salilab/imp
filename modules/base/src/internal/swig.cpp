d2e22161e3d7c8da458d3a225ad15bea72bfb8a2
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-01-16 18:34:28 +0000

    move test of base log functionality into base
    git-svn-id: https://svn.salilab.org/imp/trunk@16174 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/base/src/internal/swig.cpp
@@ -0,0 +1,28 @@
+/**
+ *  \file static.cpp   \brief all static data for module.
+ *
+ *  Copyright 2007-2013 IMP Inventors. All rights reserved.
+ *
+ */
+
+#include <IMP/base/internal/swig.h>
+#include <IMP/base/log_macros.h>
+IMPBASE_BEGIN_INTERNAL_NAMESPACE
+namespace {
+void test_log_1() {
+  IMP_FUNCTION_LOG;
+  IMP_LOG(SILENT, "Hi" << std::endl);
+}
+void test_log_0() {
+  IMP_FUNCTION_LOG;
+  test_log_1();
+}
+}
+
+void _test_log() {
+  IMP_FUNCTION_LOG;
+  test_log_0();
+}
+
+
+IMPBASE_END_INTERNAL_NAMESPACE
---
+++ modules/base/test/test_log.py
@@ -0,0 +1,57 @@
+import IMP.base
+import IMP.test
+import sys
+from StringIO import StringIO
+
+class Tests(IMP.test.TestCase):
+    def test_log_targets(self):
+        """Test log targets"""
+        log_level = IMP.base.get_log_level()
+        # Make sure we are not logging at level MEMORY, since that will add
+        # extra logs (for ref/unref) and cause assertions to fail
+        IMP.base.set_log_level(IMP.base.VERBOSE)
+        s= StringIO()
+        t=IMP.base.SetLogTarget(s)
+        IMP.base.add_to_log("Hey there\n")
+        IMP.base.add_to_log("Big guy")
+        del t
+        print 'testing'
+        self.assertEqual(s.getvalue(), "Hey there\nBig guy")
+        del s
+        IMP.base.add_to_log("what's up")
+        s= StringIO()
+        t= IMP.base.SetLogTarget(s)
+        IMP.base.add_to_log("Hey there\n")
+        del s
+        IMP.base.add_to_log("Big guy")
+        IMP.base.set_log_level(log_level)
+
+    def test_log_targets_memory(self):
+        """Test log targets at MEMORY log level"""
+        log_level = IMP.base.get_log_level()
+        IMP.base.set_log_level(IMP.base.MEMORY)
+        s= StringIO()
+        t=IMP.base.SetLogTarget(s)
+        # r4694 or earlier will crash here, since the 'Unrefing object' log
+        # message emitted at MEMORY log level will attempt to be displayed
+        # using the log target that is in the process of being freed
+        del t
+        IMP.base.set_log_level(log_level)
+    def test_log_functions(self):
+        """Test function log looks ok"""
+        # I don't feel like arranging to capture the output...
+        print "calling"
+        IMP.base.set_log_level(IMP.base.VERBOSE)
+        IMP.base._test_log()
+        print "done"
+
+    def test_log_time_functions(self):
+        """Test time log looks ok"""
+        # I don't feel like arranging to capture the output...
+        print "calling"
+        IMP.base.set_log_timer(True)
+        IMP.base.set_log_level(IMP.base.VERBOSE)
+        IMP.base._test_log()
+        print "done"
+if __name__ == '__main__':
+    IMP.test.main()
