83ff753b4fb10fe82f1c2ded3cdc9735ce9a89fe
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-01-17 04:52:46 +0000

    remove local module in example system
    git-svn-id: https://svn.salilab.org/imp/trunk@16199 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- biological_systems/example_system/local/pyext/src/randomize.py
+++
@@ -1,8 +0,0 @@
-# python files placed in this src directory are automatically added to the module.
-# The function below can be accessed as IMP.local.randomize.randomize_particles().
-
-import IMP.core
-
-def randomize_particle(p):
-    d= IMP.core.XYZ.decorate_particle(p)
-    d.set_coordinates(IMP.algebra.get_random_vector_in(IMP.algebra.get_unit_bounding_box_3d()))
