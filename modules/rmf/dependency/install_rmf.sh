1b4ad078df5412bc60d84ca27b5ef7e1ad8c8eb2
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-01-17 05:34:01 +0000

    add missing install_rmf.sh
    git-svn-id: https://svn.salilab.org/imp/trunk@16214 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/rmf/dependency/install_rmf.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+#-DCMAKE_INSTALL_PREFIX=%(builddir)s \
+#-DCMAKE_INSTALL_PYTHONDIR=%(builddir)s/lib\
+#-DCMAKE_INSTALL_LIBDIR=%(builddir)s/lib \
+#-DCMAKE_INSTALL_SWIGDIR=%(builddir)s/swig \
+
+cd %(workdir)s
+%(cmake)s %(srcdir)s \
+-DCMAKE_BUILD_TYPE=%(buildtype)s \
+-DCMAKE_LIBRARY_PATH=%(libpath)s \
+-DCMAKE_INCLUDE_PATH=%(includepath)s \
+-DCMAKE_CXX_FLAGS=%(cxxflags)s \
+-DCMAKE_SHARED_LINKER_FLAGS=%(linkflags)s
+make -j %(jobs)s
+
+mkdir -p %(builddir)s/lib
+if [ -e %(builddir)s/src/RMF/_RMF.pyd ]; then
+  # Windows-style library names
+  ln -sfF %(builddir)s/src/RMF/RMF.* %(builddir)s/src/RMF/_RMF.* %(builddir)s/lib/
+  ln -sfF %(builddir)s/src/RMF/RMF.lib %(builddir)s/lib/libRMF.lib
+else
+  # Unix-style library names
+ln -sfF %(builddir)s/src/RMF/libRMF* %(builddir)s/src/RMF/_RMF.so %(builddir)s/src/RMF/RMF.py %(builddir)s/lib/
+fi
+
+mkdir -p %(builddir)s/include/RMF
+ln -sfF %(srcdir)s/include/RMF/* %(builddir)s/include/RMF/
+ln -sfF %(builddir)s/src/RMF/include/RMF/* %(builddir)s/include/RMF/
+ln -sfF %(builddir)s/src/RMF/include/RMF.h %(builddir)s/include/
+mkdir -p %(builddir)s/swig
+ln -sfF %(srcdir)s/swig/*.i %(builddir)s/swig
+
+if [ -e %(builddir)s/src/RMF/AvroCpp ]; then
+  ln -sfF %(builddir)s/src/RMF/AvroCpp/libavro* %(builddir)s/lib/
+  ln -sfF %(srcdir)s/AvroCpp/avro %(builddir)s/include
+  mkdir -p %(builddir)s/bin
+  ln -sfF %(builddir)s/src/RMF/AvroCpp/avrogencpp %(builddir)s/bin
+fi
