817a04dcf9efa2d0df4abd8557eb1c272958dbc1
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-01-18 00:14:36 +0000

    add (failing) test for attribute access from python in the model
    git-svn-id: https://svn.salilab.org/imp/trunk@16251 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/kernel/test/test_attributes.py
@@ -0,0 +1,28 @@
+import IMP
+import IMP.test
+
+
+xkey = IMP.FloatKey("x")
+ykey = IMP.FloatKey("y")
+zkey = IMP.FloatKey("z")
+idkey = IMP.IntKey("id")
+radkey = IMP.FloatKey("radius")
+
+class ParticleTests(IMP.test.TestCase):
+    """Test particles"""
+
+    def test_no_model(self):
+        """Check access of attributes from python"""
+        m= IMP.Model()
+        p= IMP.Particle(m)
+        ik= IMP.IntKey("hi")
+        m.add_attribute(ik, p.get_index(), 1)
+        self.assertEqual(m.get_attribute(ik, p.get_index()), 1)
+        pisk= IMP.ParticleIndexesKey("hi")
+        m.add_attribute(pisk, p.get_index(), [p.get_index()])
+        self.assertEqual(m.get_attribute(pisk, p.get_index()), [p.get_index()])
+        pik= IMP.ParticleIndexKey("hi")
+        m.add_attribute(pik, p.get_index(), p.get_index())
+        self.assertEqual(m.get_attribute(pik, p.get_index()), p.get_index())
+if __name__ == '__main__':
+    IMP.test.main()
