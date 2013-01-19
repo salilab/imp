363598dcb4b5d719e586e302598c6e5026e3a04f
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-01-19 00:06:13 +0000

    fix various doc bugs
    git-svn-id: https://svn.salilab.org/imp/trunk@16283 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/algebra/examples/geometry.py
+++ modules/algebra/examples/geometry.py
@@ -1,27 +1,27 @@
 ## \example modules/algebra/examples/geometry.py
 ## IMP.algebra provides a set of geometric primitives and basic operations on them.
 
-from IMP.algebra import *
+import IMP.algebra
 
 # we can create some spheres
 s=[]
 for i in range(0,10):
-    s.append(Sphere3D(get_random_vector_in(IMP.algebra.get_unit_bounding_box_3d()), .1))
+    s.append(IMP.algebra.Sphere3D(IMP.algebra.get_random_vector_in(IMP.algebra.get_unit_bounding_box_3d()), .1))
 
 # we can compute a sphere which contains them all
-enclosing= get_enclosing_sphere(s)
+enclosing= IMP.algebra.get_enclosing_sphere(s)
 
 print enclosing.get_contains(s[0])
 
 print IMP.algebra.get_distance(s[0], s[1])
 # or between the centers
-print get_distance(s[0].get_center(), s[1].get_center())
+print IMP.algebra.get_distance(s[0].get_center(), s[1].get_center())
 
 # create a cylinder
-c= Cylinder3D(Segment3D(s[0].get_center(), s[1].get_center()), 1)
+c= IMP.algebra.Cylinder3D(IMP.algebra.Segment3D(s[0].get_center(), s[1].get_center()), 1)
 print c
 
 # manipulate bounding boxes
-bb= BoundingBox3D()
+bb= IMP.algebra.BoundingBox3D()
 for si in s:
-    bb+= get_bounding_box(si)
+    bb+= IMP.algebra.get_bounding_box(si)
