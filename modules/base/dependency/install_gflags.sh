5d1b81a2f417e1392c1632c86f9bedd0489dcfd9
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-01-20 02:05:21 +0000

    don't print tons of crap when building gflags
    git-svn-id: https://svn.salilab.org/imp/trunk@16288 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/base/dependency/install_gflags.sh
+++ modules/base/dependency/install_gflags.sh
@@ -3,7 +3,7 @@
 cd %(workdir)s
 
 if test \! -e %(builddir)s/src/gflags/Makefile; then
-  %(srcdir)s/configure --prefix=%(builddir)s
+  %(srcdir)s/configure --prefix=%(builddir)s > /dev/null
 fi
-make -j %(jobs)s
-make install
+make -j %(jobs)s > /dev/null
+make install > /dev/null
