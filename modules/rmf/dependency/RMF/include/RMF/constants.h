8c65cc6729b797925501890acb1f7de218e3e717
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-01-21 22:58:29 +0000

    more dates
    git-svn-id: https://svn.salilab.org/imp/trunk@16342 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/rmf/dependency/RMF/include/RMF/constants.h
+++ modules/rmf/dependency/RMF/include/RMF/constants.h
@@ -2,7 +2,7 @@
  *  \file RMF/constants.h
  *  \brief Various utilities for rmfing
  *
- *  Copyright 2007-2012 IMP Inventors. All rights reserved.
+ *  Copyright 2007-2013 IMP Inventors. All rights reserved.
  *
  */
 
--- modules/rmf/dependency/RMF/include/RMF/infrastructure_macros.h
+++ modules/rmf/dependency/RMF/include/RMF/infrastructure_macros.h
@@ -2,7 +2,7 @@
  *  \file RMF/infrastructure_macros.h
  *  \brief Various general useful macros for IMP.
  *
- *  Copyright 2007-2012 IMP Inventors. All rights reserved.
+ *  Copyright 2007-2013 IMP Inventors. All rights reserved.
  *
  */
 
