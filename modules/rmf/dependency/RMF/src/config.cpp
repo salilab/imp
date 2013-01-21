d73e9d372fd59d2069f27b8ede428147d2ed657f
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-01-21 22:58:09 +0000

    more dates
    git-svn-id: https://svn.salilab.org/imp/trunk@16337 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/rmf/dependency/RMF/src/config.cpp
+++ modules/rmf/dependency/RMF/src/config.cpp
@@ -2,7 +2,7 @@
  *  \file RMF/config.cpp
  *  \brief RMF module version information.
  *
- *  Copyright 2007-2012 IMP Inventors. All rights reserved.
+ *  Copyright 2007-2013 IMP Inventors. All rights reserved.
  *
  */
 
