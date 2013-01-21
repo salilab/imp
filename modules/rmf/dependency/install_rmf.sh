ec4f2ef79303b158e0179e96742b466199924d47
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-01-21 00:03:19 +0000

    make sure to quote arguments
    git-svn-id: https://svn.salilab.org/imp/trunk@16308 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/rmf/dependency/install_rmf.sh
+++ modules/rmf/dependency/install_rmf.sh
@@ -7,11 +7,11 @@
 
 cd %(workdir)s
 %(cmake)s %(srcdir)s \
--DCMAKE_BUILD_TYPE=%(buildtype)s \
--DCMAKE_LIBRARY_PATH=%(libpath)s \
--DCMAKE_INCLUDE_PATH=%(includepath)s \
--DCMAKE_CXX_FLAGS=%(cxxflags)s \
--DCMAKE_SHARED_LINKER_FLAGS=%(linkflags)s
+-DCMAKE_BUILD_TYPE="%(buildtype)s" \
+-DCMAKE_LIBRARY_PATH="%(libpath)s" \
+-DCMAKE_INCLUDE_PATH="%(includepath)s" \
+-DCMAKE_CXX_FLAGS="%(cxxflags)s" \
+-DCMAKE_SHARED_LINKER_FLAGS="%(linkflags)s"
 make -j %(jobs)s
 
 mkdir -p %(builddir)s/lib
