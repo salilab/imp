4b04d873a09675414b9c78570085f3c22d0cb005
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-01-25 19:52:12 +0000

    Catch exceptions and display them nicely, since the default handler on Windows hides the error message.
    git-svn-id: https://svn.salilab.org/imp/trunk@16393 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- applications/integrative_docking/combine_scores/combine_scores.cpp
+++ applications/integrative_docking/combine_scores/combine_scores.cpp
@@ -78,106 +78,116 @@ int read_results_file(const std::string file_name,
 
 
 int main(int argc, char** argv) {
-  if(argc < 3) {
-    std::cerr << "Usage " << argv[0]
-              << " <file1> <weight1> <file2> <weight2> <file3> <weight3>...\n";
-    std::cerr << "combines files with same docking transformations"
-              << " in the following format:\n";
-    std::cerr << "# | score | filtered(+/-) | z-score | ... | Transformation\n";
-    std::cerr << "only filtered values of + are combined. "
-              << "The output score is the weighted sum of Z-Scores\n";
-    exit(1);
-  }
-
-  // print command
-  for(int i=0; i<argc; i++) std::cerr << argv[i] << " "; std::cerr << std::endl;
-
-  // read files
-  std::vector<std::vector<Result> > results((argc-1)/2);
-  std::vector<float> weights;
-  for(int i=1; i<argc; i+=2) {
-    int trans_num = read_results_file(argv[i], results[(i-1)/2]);
-    float weight = atof(argv[i+1]);
-    weights.push_back(weight);
-    std::cerr << trans_num << " were read from file " << argv[i] << " weight "
-              << weight << std::endl;
-  }
-
-  // validate same transformation number
-  for(unsigned int i=1; i<results.size(); i++) {
-    if(results[i-1].size() != results[i].size()) {
-      IMP_THROW("different number of transformations in input files: "
-                << argv[i] << " " << results[i-1].size()  << " vs. "
-                << argv[i+1] << " " << results[i].size(), IMP::ValueException);
+  try {
+    if(argc < 3) {
+      std::cerr << "Usage " << argv[0] << " <file1> <weight1> <file2> "
+                << "<weight2> <file3> <weight3>...\n";
+      std::cerr << "combines files with same docking transformations"
+                << " in the following format:\n";
+      std::cerr << "# | score | filtered(+/-) |"
+                << " z-score | ... | Transformation\n";
+      std::cerr << "only filtered values of + are combined. "
+                << "The output score is the weighted sum of Z-Scores\n";
+      exit(1);
     }
-  }
 
-  // compute new z_scores
-  float average = 0.0;
-  float std = 0.0;
-  int counter = 0;
+    // print command
+    for(int i=0; i<argc; i++) {
+      std::cerr << argv[i] << " "; std::cerr << std::endl;
+    }
 
-  for(unsigned int i=0; i<results[0].size(); i++) {
-    float score = 0;
-    bool filtered = true;
+    // read files
+    std::vector<std::vector<Result> > results((argc-1)/2);
+    std::vector<float> weights;
+    for(int i=1; i<argc; i+=2) {
+      int trans_num = read_results_file(argv[i], results[(i-1)/2]);
+      float weight = atof(argv[i+1]);
+      weights.push_back(weight);
+      std::cerr << trans_num << " were read from file " << argv[i] << " weight "
+                << weight << std::endl;
+    }
 
-    for(unsigned int j=0; j<results.size(); j++) {
-      if(results[j][i].is_filtered()) {
-        score += weights[j] * results[j][i].get_z_score();
-      } else {
-        filtered = false;
-        break;
+    // validate same transformation number
+    for(unsigned int i=1; i<results.size(); i++) {
+      if(results[i-1].size() != results[i].size()) {
+        IMP_THROW("different number of transformations in input files: "
+                  << argv[i] << " " << results[i-1].size()  << " vs. "
+                  << argv[i+1] << " " << results[i].size(),
+                  IMP::ValueException);
       }
     }
 
-    if(filtered) {
-      average += score;
-      std += (score*score);
-      counter++;
-    }
-  }
+    // compute new z_scores
+    float average = 0.0;
+    float std = 0.0;
+    int counter = 0;
 
-  average /= counter;
-  std /=counter;
-  std -= (average*average);
-  std = sqrt(std);
+    for(unsigned int i=0; i<results[0].size(); i++) {
+      float score = 0;
+      bool filtered = true;
 
-  // output combined file
-  // header
-  std::cout << "     # |  Score | filt| ZScore | ";
-  for(unsigned int j=0; j<results.size(); j++)
-    std::cout << "Score" << j << " | Zscore" << j << " |";
-  std::cout << "Transformation" << std::endl;
-
-  // output transforms
-  for(unsigned int i=0; i<results[0].size(); i++) {
-    float score = 0;
-    bool filtered = true;
-
-    for(unsigned int j=0; j<results.size(); j++) {
-      if(results[j][i].is_filtered()) {
-        score += weights[j] * results[j][i].get_z_score();
-      } else {
-        filtered = false;
-        break;
-      }
-    }
-
-    // compute z-score
-    if(filtered) {
-      float z_score = (score-average)/std;
-      std::cout.width(6);
-      std::cout << results[0][i].get_number() << " | ";
-      std::cout.setf(std::ios::fixed, std::ios::floatfield);
-      std::cout.width(6); std::cout.precision(3);
-      std::cout << score << " |  +  | " << z_score << " | ";
       for(unsigned int j=0; j<results.size(); j++) {
-        std::cout.width(6); std::cout.precision(3);
-        std::cout << results[j][i].get_score() << " | ";
-        std::cout.width(6); std::cout.precision(3);
-        std::cout << results[j][i].get_z_score() << " | ";
+        if(results[j][i].is_filtered()) {
+          score += weights[j] * results[j][i].get_z_score();
+        } else {
+          filtered = false;
+          break;
+        }
       }
-      std::cout << results[0][i].get_transformation() << std::endl;
+
+      if(filtered) {
+        average += score;
+        std += (score*score);
+        counter++;
+      }
     }
+
+    average /= counter;
+    std /=counter;
+    std -= (average*average);
+    std = sqrt(std);
+
+    // output combined file
+    // header
+    std::cout << "     # |  Score | filt| ZScore | ";
+    for(unsigned int j=0; j<results.size(); j++)
+      std::cout << "Score" << j << " | Zscore" << j << " |";
+    std::cout << "Transformation" << std::endl;
+
+    // output transforms
+    for(unsigned int i=0; i<results[0].size(); i++) {
+      float score = 0;
+      bool filtered = true;
+
+      for(unsigned int j=0; j<results.size(); j++) {
+        if(results[j][i].is_filtered()) {
+          score += weights[j] * results[j][i].get_z_score();
+        } else {
+          filtered = false;
+          break;
+        }
+      }
+
+      // compute z-score
+      if(filtered) {
+        float z_score = (score-average)/std;
+        std::cout.width(6);
+        std::cout << results[0][i].get_number() << " | ";
+        std::cout.setf(std::ios::fixed, std::ios::floatfield);
+        std::cout.width(6); std::cout.precision(3);
+        std::cout << score << " |  +  | " << z_score << " | ";
+        for(unsigned int j=0; j<results.size(); j++) {
+          std::cout.width(6); std::cout.precision(3);
+          std::cout << results[j][i].get_score() << " | ";
+          std::cout.width(6); std::cout.precision(3);
+          std::cout << results[j][i].get_z_score() << " | ";
+        }
+        std::cout << results[0][i].get_transformation() << std::endl;
+      }
+    }
+  } catch (IMP::Exception &e) {
+    std::cerr << "Error: " << e.what() << std::endl;
+    return 1;
   }
+  return 0;
 }
