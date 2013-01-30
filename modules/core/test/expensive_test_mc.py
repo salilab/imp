3e785142af659d1da924b5cff258c0b3e97e01f5
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-01-30 00:22:23 +0000

    fix variable name
    git-svn-id: https://svn.salilab.org/imp/trunk@16457 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/test/expensive_test_mc.py
+++ modules/core/test/expensive_test_mc.py
@@ -5,8 +5,8 @@ import IMP.container
 
 class WoodsFunc(IMP.Restraint):
     """Woods function for four input values, defined as an IMP restraint"""
-    def __init__(self):
-        IMP.Restraint.__init__(self)
+    def __init__(self, m):
+        IMP.Restraint.__init__(self, m)
         self.index= IMP.FloatKey("x")
     def unprotected_evaluate(self, accum):
         #print "Evaluating in python\n"
@@ -68,7 +68,7 @@ class MCOptimizerTest(IMP.test.TestCase):
         mod= IMP.core.BallMover(model.get_particles(), fk, .25)
         opt.add_mover(mod)
 
-        rsr = WoodsFunc()
+        rsr = WoodsFunc(model)
         model.add_restraint(rsr)
         #print rsr.evaluate(None)
         #print model.evaluate(False)
