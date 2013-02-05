86af6ac07bc09464457f7073d0713e27fdbc64d1
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-02-05 18:19:49 +0000

    move some kernel teste to base
    git-svn-id: https://svn.salilab.org/imp/trunk@16500 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/base/test/test_exceptions.py
@@ -0,0 +1,27 @@
+import IMP.base
+import IMP.test
+import os.path
+
+class ExceptionTests(IMP.test.TestCase):
+    """Check IMP exceptions"""
+
+    def assertSubclass(self, derived, base):
+        self.assertTrue(issubclass(derived, base),
+                        "%s is not a subclass of %s" % (derived, base))
+
+    def test_hierarchy(self):
+        """Test class hierarchy of IMP exceptions"""
+        self.assertSubclass(IMP.base.Exception, Exception)
+        # Make sure that all exceptions derive from IMP.Exception
+        for subclass in (IMP.base.InternalException, IMP.base.ModelException,
+                         IMP.base.UsageException, IMP.base.IndexException,
+                         IMP.base.IOException, IMP.base.ValueException,
+                         IMP.base.EventException):
+            self.assertSubclass(subclass, IMP.base.Exception)
+        # Check for exceptions that also should derive from Python types
+        self.assertSubclass(IMP.base.IndexException, IndexError)
+        self.assertSubclass(IMP.base.IOException, IOError)
+        self.assertSubclass(IMP.base.ValueException, ValueError)
+
+if __name__ == '__main__':
+    IMP.test.main()
