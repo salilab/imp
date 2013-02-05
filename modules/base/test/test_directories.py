3a9e5b88bac82084fd6fa832eaa10054d7fe1f24
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-02-05 18:19:49 +0000

    move some kernel teste to base
    git-svn-id: https://svn.salilab.org/imp/trunk@16500 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/base/test/test_directories.py
@@ -0,0 +1,17 @@
+import IMP.base
+import IMP.test
+import os.path
+
+class DirectoriesTests(IMP.test.TestCase):
+
+    def test_data_directory(self):
+        """Test the get_data_directory function"""
+        f = IMP.base.get_data_path("README")
+        self.assertTrue(os.path.exists(f) and not os.path.isdir(f))
+
+    def test_bad_data_directory(self):
+        """get_data_directory should raise an error if it does not exist"""
+        self.assertRaises(IMP.base.IOException, IMP.base.get_data_path, "not/exist")
+
+if __name__ == '__main__':
+    IMP.test.main()
