3f604baa8fd22bd1c5c32e01f7592beab8dd3797
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-02-05 23:27:46 +0000

    sync RMF to f58a8dfb1ab2f43d691966717c60851bb68c0e24
    git-svn-id: https://svn.salilab.org/imp/trunk@16528 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/rmf/dependency/RMF/test/test_features.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+import unittest
+import RMF
+
+class GenericTest(unittest.TestCase):
+    def _create(self, path):
+      fh= RMF.create_rmf_file(path)
+      rt= fh.get_root_node()
+      reps=[rt.add_child("rep"+str(i), RMF.REPRESENTATION) for i in range(0,5)]
+      sf= RMF.ScoreFactory(fh)
+      fn= rt.add_child("feature", RMF.FEATURE)
+      sd= sf.get(fn)
+      sd.set_score(10.0)
+      sd.set_representation(reps)
+    def _test(self, path):
+      fh= RMF.open_rmf_file_read_only(path)
+      rt= fh.get_root_node()
+      ch= rt.get_children()
+      fn= ch[-1]
+      reps= ch[:-1]
+      sf= RMF.ScoreConstFactory(fh)
+      sd= sf.get(fn)
+      print sd.get_score()
+      print reps
+      print sd.get_representation()
+      self.assert_(sd.get_score()==10)
+      self.assert_(sd.get_representation() == reps)
+    def test_multiparent(self):
+        """Test that feature nodes work right"""
+        for suffix in RMF.suffixes:
+            path=RMF._get_temporary_file_path("alias2."+suffix)
+            print path
+            self._create(path)
+            self._test(path)
+
+if __name__ == '__main__':
+    unittest.main()
