8763e07b82da40fa6c2a4a4e73c4de10f52f9f0a
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-02-16 18:22:02 +0000

    add test macros to replace asserts which can go away if you do -DNDEBUG
    git-svn-id: https://svn.salilab.org/imp/trunk@16616 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

---
+++ modules/test/include/test_macros.h
@@ -0,0 +1,24 @@
+/**
+ *  \file IMP/test/test_macros.h
+ *  Macros for writing C++ tests.
+ *
+ *  Copyright 2007-2013 IMP Inventors. All rights reserved.
+ *
+ */
+
+#ifndef IMPTEST_TEST_MACROS_H
+#define IMPTEST_TEST_MACROS_H
+
+#define IMP_TEST_EQUAL(a,b)                             \
+  if (a != b) {                                         \
+    IMP_ERROR("Test failed: " << #a << " != " << #b     \
+              << " values are " << a << " != " << b);   \
+  }
+
+#define IMP_TEST_LESS_THAN(a,b)                         \
+  if (a >= b) {                                         \
+    IMP_ERROR("Test failed: " << #a << " >= " << #b     \
+              << " values are " << a << " >= " << b);   \
+  }
+
+#endif  /* IMPTEST_TEST_MACROS_H */
