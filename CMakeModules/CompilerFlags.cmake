341d932f1c4fe9d8147874883e430262a9853357
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-02-17 23:31:26 +0000

    clean up build types
    git-svn-id: https://svn.salilab.org/imp/trunk@16641 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- CMakeModules/CompilerFlags.cmake
+++ CMakeModules/CompilerFlags.cmake
@@ -1,11 +1,3 @@
-if(${IMP_BUILD} MATCHES "DEBUG")
-set(CMAKE_BUILD_TYPE Debug)
-set(base_flags ${CMAKE_CXX_FLAGS_DEBUG})
-else()
-set(CMAKE_BUILD_TYPE Release)
-set(base_flags ${CMAKE_CXX_FLAGS_RELEASE})
-endif()
-
 if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang")
   set(IMP_USE_CUSTOM_CXX_FLAGS 1 CACHE INTERNAL "" FORCE)
 elseif ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
