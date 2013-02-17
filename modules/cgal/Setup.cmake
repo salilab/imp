082680d22e48833c2d235b3f770881c196a2ea21
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-02-17 23:31:26 +0000

    clean up build types
    git-svn-id: https://svn.salilab.org/imp/trunk@16641 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/cgal/Setup.cmake
+++ modules/cgal/Setup.cmake
@@ -1,6 +1,11 @@
 message(STATUS "IMP.cgal config")
 
+if(DEFINED CMAKE_BUILD_TYPE)
+set(user_build_type ${CMAKE_BUILD_TYPE})
+endif()
+
 include(FindCGAL)
+
 
 if (${CGAL_FOUND})
 file(WRITE "${PROJECT_BINARY_DIR}/data/build_info/CGAL" "ok=True")
@@ -8,3 +13,9 @@ include(${CGAL_USE_FILE})
 else(${CGAL_FOUND})
 file(WRITE "${PROJECT_BINARY_DIR}/data/build_info/CGAL" "ok=False")
 endif(${CGAL_FOUND})
+
+# reset build type
+if(DEFINED user_build_type)
+message(STATUS "Restoring build type to " ${user_build_type} " from " ${CMAKE_BUILD_TYPE})
+set(CMAKE_BUILD_TYPE ${user_build_type} CACHE STRING "Build type is one of Debug, Release" FORCE)
+endif()
