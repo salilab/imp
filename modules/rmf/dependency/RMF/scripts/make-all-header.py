f2ea45bc5b07e3bf3ddf0da8243e9812a73398ad
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-02-17 22:34:20 +0000

    sync RMF
    git-svn-id: https://svn.salilab.org/imp/trunk@16640 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/rmf/dependency/RMF/scripts/make-all-header.py
+++ modules/rmf/dependency/RMF/scripts/make-all-header.py
@@ -1,6 +1,9 @@
 #!/usr/bin/python
 import sys
-sys.argv.sort()
-for h in sys.argv:
-    if h.endswith(".h"):
-        print "#include <RMF/"+h[h.rfind("RMF")+4:]+">"
+import glob
+import os
+for h in sys.argv[2:]:
+    pat = os.path.join(h, "*.h")
+    for g in glob.glob(pat):
+        name= os.path.split(g)[1]
+        print "#include <%s/"%sys.argv[1] + name + ">"
