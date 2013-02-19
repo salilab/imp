aa5b0d98b7d0f66dbbf0d612e19e2493b14d54fa
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-02-19 04:01:19 +0000

    suppress warnings
    git-svn-id: https://svn.salilab.org/imp/trunk@16656 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/base/include/compiler_macros.h
+++ modules/base/include/compiler_macros.h
@@ -8,7 +8,6 @@
 #ifndef IMPBASE_BASE_COMPILER_MACROS_H
 #define IMPBASE_BASE_COMPILER_MACROS_H
 
-#include <IMP/base/base_config.h>
 // recommended by http://gcc.gnu.org/gcc/Function-Names.html
 #if defined(_MSC_VER)
 #  define __func__ __FUNCTION__
@@ -67,5 +66,59 @@
 #define IMP_BIG_ENDIAN
 #endif
 
+#ifdef __clang__
+#define IMP_COMPILER_ON_BEGIN_NAMESPACE \
+ _Pragma("clang diagnostic push")\
+ _Pragma("clang diagnostic warn \"-Wall\"")
+ _Pragma("clang diagnostic ignored \"-Wunknown-pragmas\"")\
+ _Pragma("clang diagnostic ignored \"-Wpadded\"")\
+ _Pragma("clang diagnostic ignored \"-Wc++11-extensions\"")
+
+#define IMP_COMPILER_ON_END_NAMESPACE \
+_Pragma("clang diagnostic pop")
+
+#elif defined(__GNUC__)
+
+/*ret+=["-Wno-deprecated",
+             "-Wstrict-aliasing=2",
+             -fno-operator-names",]
+        if float(options.version) >= 4.2:
+            if sys.platform == 'darwin':
+                ret+=["-Wmissing-prototypes"]
+            else:
+                ret+=["-Wmissing-declarations"]
+        if float(options.version) >= 4.6:
+            ret+=["-Wno-c++0x-compat"]
+        #if dependency.gcc.get_version(env)>= 4.3:
+        #    env.Append(CXXFLAGS=["-Wunsafe-loop-optimizations"])
+        # gcc 4.0 on Mac doesn't like -isystem, so we don't use it there.
+        # But without -isystem, -Wundef throws up lots of Boost warnings.
+        if sys.platform != 'darwin' or float(options.version) > 4.0:
+            ret+=["-Wundef"]*/
+#if __GNUC__ > 4 || __GNUC_MINOR__ >=6
+#define IMP_GCC_CXX0X_COMPAT\
+ _Pragma("GCC diagnostic ignored \"-Wc++0x-compat\"")
+#else
+define IMP_GCC_CXX0X_COMPAT
+#endif
+
+#define IMP_COMPILER_ON_BEGIN_NAMESPACE \
+_Pragma("GCC diagnostic push") \
+_Pragma("GCC diagnostic warning \"-Wall\"") \
+_Pragma("GCC diagnostic warning \"-Wextra\"") \
+_Pragma("GCC diagnostic warning \"-Winit-self\"") \
+_Pragma("GCC diagnostic warning \"-Wcast-align\"") \
+_Pragma("GCC diagnostic warning \"-Woverloaded-virtual\"") \
+_Pragma("GCC diagnostic ignored \"-Wunknown-pragmas\"") \
+_Pragma("GCC diagnostic warning \"-Wundef\"") \
+IMP_GCC_CXX0X_COMPAT
+
+#define IMP_COMPILER_ON_END_NAMESPACE \
+_Pragma("GCC diagnostic pop")
+
+#else
+#define IMP_COMPILER_ON_BEGIN_NAMESPACE
+#define IMP_COMPILER_ON_END_NAMESPACE
+#endif
 
 #endif  /* IMPBASE_BASE_COMPILER_MACROS_H */
