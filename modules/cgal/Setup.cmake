e81ab76fac52959e8b9ab453ee016d8e5718314b
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-02-20 22:58:53 +0000

    Force CGAL not to override compiler flags.
    git-svn-id: https://svn.salilab.org/imp/trunk@16681 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/cgal/Setup.cmake
+++ modules/cgal/Setup.cmake
@@ -1,11 +1,8 @@
-# duh
-set(CGAL_DONT_OVERRIDE_CMAKE_FLAGS True)
-
 include(FindCGAL)
-
 
 if (${CGAL_FOUND})
 file(WRITE "${PROJECT_BINARY_DIR}/data/build_info/CGAL" "ok=True")
+set(CGAL_DONT_OVERRIDE_CMAKE_FLAGS TRUE CACHE BOOL "Don't override flags")
 include(${CGAL_USE_FILE})
 
 else(${CGAL_FOUND})
