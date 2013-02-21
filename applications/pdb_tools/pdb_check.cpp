f792c3ac453654bf9452e8b728dd654a21d80d08
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-02-21 21:27:35 +0000

    use new flags and remove not very useful program
    git-svn-id: https://svn.salilab.org/imp/trunk@16696 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- applications/pdb_tools/pdb_check.cpp
+++ applications/pdb_tools/pdb_check.cpp
@@ -2,47 +2,28 @@
  * Copyright 2007-2013 IMP Inventors. All rights reserved.
  */
 #include <IMP/atom/pdb.h>
+#include <IMP/base/flags.h>
 #include <IMP/atom/force_fields.h>
 #include <IMP/exception.h>
 
-#include <boost/program_options.hpp>
-namespace po = boost::program_options;
-
-std::string input;
-po::options_description desc("Usage: input.pdb.\n");
-bool help=false;
-int frame=0;
-void print_help() {
-  std::cerr << desc << std::endl;
-}
-
-
 int main(int argc, char **argv) {
   try {
-    desc.add_options()
-    ("help,h",
-     "Print messages about any errors encountered in parsing the pdb file.")
-        ("input-file,i", po::value< std::string >(&input),
-         "input pdb file");
-    po::positional_options_description p;
-    p.add("input-file", 1);
-    po::variables_map vm;
-    po::store(
-              po::command_line_parser(argc,
-                                      argv).options(desc).positional(p).run(),
-              vm);
-    po::notify(vm);
-    if (vm.count("help") || input.empty()) {
-      print_help();
-      return 1;
-    }
-    IMP_NEW(IMP::Model, m, ());
-    m->set_log_level(IMP::SILENT);
-    IMP::set_log_level(IMP::VERBOSE);
-    IMP::atom::Hierarchies inhs;
-    IMP_CATCH_AND_TERMINATE(inhs= IMP::atom::read_multimodel_pdb(input, m));
-    for (unsigned int i=0; i< inhs.size(); ++i) {
-      IMP::atom::add_bonds(inhs[i]);
+    std::vector<std::string> argcs
+        = IMP::base::setup_from_argv(argc, argv,
+                                     "Print warnings about pdb file",
+                                     "input.pdb",
+                                     -1);
+
+    IMP::set_log_level(IMP::WARNING);
+    for (unsigned int i=0; i< argcs.size(); ++i) {
+      IMP_NEW(IMP::Model, m, ());
+      m->set_log_level(IMP::SILENT);
+      IMP::atom::Hierarchies inhs;
+      IMP_CATCH_AND_TERMINATE(inhs= IMP::atom::read_multimodel_pdb(argcs[i],
+                                                                   m));
+      for (unsigned int i=0; i< inhs.size(); ++i) {
+        IMP::atom::add_bonds(inhs[i]);
+      }
     }
     return 0;
   } catch (const IMP::Exception &e) {
