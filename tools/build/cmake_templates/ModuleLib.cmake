20c8bf4902ab36a378ac9306afebe2dd53f724bc
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-02-22 19:51:05 +0000

    Fix name of preprocessor define.
    git-svn-id: https://svn.salilab.org/imp/trunk@16722 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- tools/build/cmake_templates/ModuleLib.cmake
+++ tools/build/cmake_templates/ModuleLib.cmake
@@ -10,7 +10,7 @@ FILE(GLOB genheaders
 
 include_directories(%(includepath)s)
 link_directories(%(libpath)s)
-add_definitions("-DIMP%(NAME)s_EXPORT")
+add_definitions("-DIMP%(NAME)s_EXPORTS")
 
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${%(NAME)s_CXX_FLAGS}")
 
