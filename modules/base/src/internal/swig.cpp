6916865dee359602f24951c16dc4a9aae9ed07fd
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-02-22 00:25:11 +0000

    move some tests of base from kernel to base
    git-svn-id: https://svn.salilab.org/imp/trunk@16707 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/base/src/internal/swig.cpp
+++ modules/base/src/internal/swig.cpp
@@ -25,4 +25,90 @@ void _test_log() {
 }
 
 
+std::string _test_ifile(base::TextInput a) {
+  std::string read;
+  while (true) {
+    std::string cur;
+    a.get_stream() >> cur;
+    if (!a) break;
+    read= read+cur;
+  }
+  std::cout << read;
+  return read;
+}
+std::string _test_ofile(base::TextOutput a) {
+  static_cast<std::ostream &>(a) << "hi\n"
+                                 << " there, how are things"<< std::endl;
+  return "hi\n";
+}
+
+DerivativePair
+_pass_pair(const DerivativePair &p) {
+  std::cout << p.first << " " << p.second << std::endl;
+  return p;
+}
+
+std::string _test_ifile_overloaded(base::TextInput a, std::string) {
+  return _test_ifile(a);
+}
+std::string _test_ofile_overloaded(base::TextOutput a, std::string) {
+  return _test_ofile(a);
+}
+std::string _test_ifile_overloaded(base::TextInput a, int) {
+  return _test_ifile(a);
+}
+std::string _test_ofile_overloaded(base::TextOutput a, int) {
+  return _test_ofile(a);
+}
+
+Floats _pass_floats(const Floats& in) {
+  for (unsigned int i=0; i< in.size(); ++i) {
+    std::cout << in[i] << " ";
+  }
+  return in;
+}
+Ints _pass_ints( Ints in) {
+  for (unsigned int i=0; i< in.size(); ++i) {
+    std::cout << in[i] << " ";
+  }
+  return in;
+}
+const Strings& _pass_strings(const Strings& in) {
+  std::cout << in << std::endl;
+  return in;
+}
+IntsList _pass_ints_list(const IntsList &in) {
+  std::cout << "IntsList of length " << in.size();
+  return in;
+}
+IntsLists _pass_ints_lists(const IntsLists &in) {
+  std::cout << "IntsLists of length " << in.size();
+  return in;
+}
+std::pair<double, double>
+_pass_plain_pair(std::pair<double, double> p) {
+  std::cout << p.first << " " << p.second << std::endl;
+  return p;
+}
+int _test_intranges(const IntRanges &ips) {
+  return ips.size();
+}
+
+
+IntRange _test_intrange(const IntRange &ips) {
+  return ips;
+}
+
+IntRange _test_intrange() {
+  return IntRange(-1,-1);
+}
+
+Strings _pass_overloaded_strings(const Strings &a,
+                                 int) {
+  return a;
+}
+Strings _pass_overloaded_strings(const Strings &a) {
+  return a;
+}
+
 IMPBASE_END_INTERNAL_NAMESPACE
