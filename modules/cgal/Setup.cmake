3602ded53bf7916811d46359ccfa66ce74c25760
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-02-23 07:46:31 +0000

    Remove debugging print.
    git-svn-id: https://svn.salilab.org/imp/trunk@16745 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/cgal/Setup.cmake
+++ modules/cgal/Setup.cmake
@@ -4,7 +4,6 @@ if (${CGAL_FOUND})
 file(WRITE "${PROJECT_BINARY_DIR}/data/build_info/CGAL" "ok=True")
 set(CGAL_DONT_OVERRIDE_CMAKE_FLAGS TRUE CACHE BOOL "Don't override flags")
 include(${CGAL_USE_FILE})
-MESSAGE(WARNING "CGAL set up ${CGAL_LIBRARIES}")
 
 if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
 message(STATUS "Updating CMAKE_CXX_FLAGS")
