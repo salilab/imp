bf512fa1b08abb7c985b4ccf06d72f7f8e03cda9
pellarin@SALILAB.ORG <pellarin@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-03-01 23:58:58 +0000

    added test for drmsd distance metric
    git-svn-id: https://svn.salilab.org/imp/trunk@16852 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/atom/test/test_distance.py
+++ modules/atom/test/test_distance.py
@@ -3,6 +3,7 @@ import IMP.test
 import IMP.core as core
 import IMP.atom as atom
 import IMP.algebra as alg
+import math
 
 class DistanceTest(IMP.test.TestCase):
     def test_placement_score(self):
@@ -115,6 +116,43 @@ class DistanceTest(IMP.test.TestCase):
         drms = atom.get_drms(xyzs1, xyzs2)
         rb_drms = atom.get_rigid_bodies_drms(xyzs1, xyzs2, ranges)
         self.assertAlmostEqual(drms, rb_drms, delta=0.3, msg="rb_drms != drms")
+
+    def test__rigid_bodies_drmsd(self):
+        """ Test drmsd measure"""
+        m = IMP.Model()
+        sel = atom.CAlphaPDBSelector()
+        prot1 = atom.read_pdb(self.open_input_file("1DQK.pdb"), m, sel)
+        prot2 = atom.read_pdb(self.open_input_file("1DQK.pdb"), m, sel)
+
+        xyzs1 = core.XYZs(atom.get_leaves(prot1))
+        xyzs2 = core.XYZs(atom.get_leaves(prot2))
+        drmsd = atom.get_drmsd(xyzs1, xyzs2)
+        # Molecule with itself
+        self.assertAlmostEqual(drmsd, 0)
+        R = IMP.algebra.get_random_rotation_3d()
+        v = IMP.algebra.get_random_vector_in(IMP.algebra.get_unit_bounding_box_3d())
+        T = IMP.algebra.Transformation3D(R,v)
+        for x in xyzs2:
+            core.transform(x, T)
+        drmsd = atom.get_drmsd(xyzs1, xyzs2)
+        # Same thing after transformation
+        self.assertAlmostEqual(drmsd, 0)
+        #
+        for x in xyzs2:
+            R = IMP.algebra.get_random_rotation_3d()
+            T = IMP.algebra.Transformation3D(R,v)
+            core.transform(x, T)
+        #test that the function is correctly implemented
+        drmsd=0.; npairs=0.;
+        for i in range(0,len(xyzs1)-1):
+            for j in range(i+1,len(xyzs2)):
+                dist0=IMP.core.get_distance(xyzs1[i],xyzs1[j])
+                dist1=IMP.core.get_distance(xyzs2[i],xyzs2[j])
+                drmsd+=(dist0-dist1)**2
+                npairs+=1.
+        drmsd1=math.sqrt(drmsd/npairs)
+        drmsd2= atom.get_drmsd(xyzs1, xyzs2)
+        self.assertAlmostEqual(drmsd1, drmsd2)
 
 if __name__ == '__main__':
     IMP.test.main()
