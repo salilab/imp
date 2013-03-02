76ec17403afbc16fdbff32bfc6c8187132e280a4
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-03-02 01:29:48 +0000

    output warnings in warning context immediately to avoid crashes when using static warning contexts
    git-svn-id: https://svn.salilab.org/imp/trunk@16857 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/base/src/WarningContext.cpp
+++ modules/base/src/WarningContext.cpp
@@ -12,27 +12,20 @@ IMPBASE_BEGIN_NAMESPACE
 #if IMP_HAS_LOG
 void WarningContext::add_warning(std::string key, std::string warning) const {
     if (warning.empty()) return;
-    IMP_IF_LOG(WARNING) {
-      if (data_.find(key) == data_.end()) {
-        data_[key]=warning;
-      }
+#if IMP_HAS_LOG >= IMP_WARN
+    if (data_.find(key) == data_.end()) {
+      data_.insert(key);
+      IMP_WARN(warning);
     }
+#endif
   }
 void WarningContext::clear_warnings() const {
     data_.clear();
   }
 void WarningContext::dump_warnings() const {
-  for (base::map<std::string, std::string>::iterator it= data_.begin();
-       it != data_.end(); ++it) {
-    if (!it->second.empty()) {
-      IMP_WARN(it->second << std::endl);
-      it->second=std::string();
-    }
-  }
 }
 
 WarningContext::~WarningContext() {
-  dump_warnings();
 }
 #else
 WarningContext::WarningContext(){}
