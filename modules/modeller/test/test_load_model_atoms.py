dc74aaf2a02eca918dcc39a63a7f5af2cfc3747d
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-03-04 23:45:51 +0000

    Fix class name.
    git-svn-id: https://svn.salilab.org/imp/trunk@16895 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/modeller/test/test_load_model_atoms.py
+++ modules/modeller/test/test_load_model_atoms.py
@@ -14,7 +14,7 @@ class Tests(IMP.test.TestCase):
             env = modeller.environ()
             env.libs.topology.read('${LIB}/top_heav.lib')
             env.libs.parameters.read('${LIB}/par.lib')
-            ModelLoaderTests._modeller_environ = env
+            Tests._modeller_environ = env
         return self._modeller_environ
 
     def test_hierarchy(self):
