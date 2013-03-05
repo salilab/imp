6787772d8fe68bac25266063ce02625736bba8b0
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-03-05 23:36:25 +0000

    Don't duplicate class names.
    git-svn-id: https://svn.salilab.org/imp/trunk@16926 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/isd/test/test_vonMisesSufficient.py
+++ modules/isd/test/test_vonMisesSufficient.py
@@ -14,7 +14,7 @@ from IMP.isd import vonMisesSufficient, vonMises
 #unit testing framework
 import IMP.test
 
-class Tests(IMP.test.TestCase):
+class TestvonMisesSufficient(IMP.test.TestCase):
     """Tests the von Mises distribution for N observations using the
     sufficient statistics formulation
     """
@@ -129,7 +129,7 @@ class Tests(IMP.test.TestCase):
             else:
                 self.assertAlmostEqual(cpp/py,1.0,delta=0.001)
 
-class Tests(IMP.test.TestCase):
+class TestvonMisesSufficientDegenerate(IMP.test.TestCase):
     """the sufficient von Mises should reduce to the von Mises
     when N=R=1
     """
