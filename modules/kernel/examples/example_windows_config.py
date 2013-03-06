d0758ec090ffe87054142438150d5569679b95ff
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-03-06 01:59:11 +0000

    update docs with new introduction and cmake references
    git-svn-id: https://svn.salilab.org/imp/trunk@16927 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/kernel/examples/example_windows_config.py
+++
@@ -1,8 +0,0 @@
-## \example example_windows_config.py
-## A config for windows. It probably doesn't work.
-
-cxxflags="/DWIN32 /DGSL_DLL /D_HDF5USEDLL_"  # Needed to link with
-                                             # GSL and HDF5 DLLs
-boost_autolink="dynamic"        # Needed to link with Boost DLLs
-fftw3="yes"
-fftw3libs="libfftw3-3.lib"      # Needed to link with FFTW3 DLL
