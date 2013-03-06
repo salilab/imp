26c4d6bf460339acac4c84601515d09eb7ced3fe
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-03-06 17:42:32 +0000

    Fix example.
    git-svn-id: https://svn.salilab.org/imp/trunk@16934 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/examples/fragments/custom_hierarchy.py
+++ modules/core/examples/fragments/custom_hierarchy.py
@@ -1,6 +1,16 @@
-tr= IMP.core.HierarchyTraits("my hierarchy")
-pd= IMP.core.Hierarchy.setup_particle(parent_particle, tr)
-for p in children_particles:
-    cd= IMP.core.Hierarchy.setup_particle(p, tr)
-    pd.add_child(cd)
-pd.show()
+import IMP
+import IMP.core
+
+def custom_hierarchy(parent_particle, children_particles):
+    tr= IMP.core.HierarchyTraits("my hierarchy")
+    pd= IMP.core.Hierarchy.setup_particle(parent_particle, tr)
+    for p in children_particles:
+        cd= IMP.core.Hierarchy.setup_particle(p, tr)
+        pd.add_child(cd)
+    pd.show()
+
+m = IMP.Model()
+p1 = IMP.Particle(m)
+p2 = IMP.Particle(m)
+p3 = IMP.Particle(m)
+custom_hierarchy(p1, (p2, p3))
