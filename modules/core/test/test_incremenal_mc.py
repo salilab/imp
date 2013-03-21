93ffe2d2bbe3b83dfb4fa34aeea1f06ed15fa32d
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-03-21 03:16:08 +0000

    fix usage of invalid values and add comment
    git-svn-id: https://svn.salilab.org/imp/trunk@17114 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/test/test_incremenal_mc.py
+++ modules/core/test/test_incremenal_mc.py
@@ -8,9 +8,9 @@ class Tests(IMP.test.TestCase):
     def test_incr(self):
         """Testing incremental scoring with Monte Carlo"""
         m= IMP.Model()
-        m.set_log_level(IMP.base.SILENT)
+        IMP.base.set_log_level(IMP.base.SILENT)
         mc= IMP.core.MonteCarlo(m)
-        #mc.set_log_level(IMP.base.TERSE)
+        mc.set_log_level(IMP.base.TERSE)
         ps=[]
         bb= IMP.algebra.get_unit_bounding_box_3d()
         for i in range(0,10):
@@ -26,29 +26,31 @@ class Tests(IMP.test.TestCase):
         r= IMP.container.PairsRestraint(hps, cpc)
         r.set_model(m)
         isf= IMP.core.IncrementalScoringFunction(ps, [r])
+        isf.set_log_level(IMP.base.TERSE)
         mc.set_incremental_scoring_function(isf)
         ms= [IMP.core.BallMover([x], .1) for x in ps]
         mv= IMP.core.SerialMover(ms)
         mc.add_mover(mv)
-        w= IMP.display.PymolWriter(self.get_tmp_file_name("incr.pym"))
-        w.set_frame(0)
-        for p in ps:
-            g= IMP.core.XYZRGeometry(p)
-            w.add_geometry(g);
+        #w= IMP.display.PymolWriter(self.get_tmp_file_name("incr.pym"))
+        #w.set_frame(0)
+        #for p in ps:
+        #    g= IMP.core.XYZRGeometry(p)
+        #    w.add_geometry(g);
         for i in range(0,10):
+            print "optimizing"
             mc.optimize(1000)
-            w.set_frame(i+1)
-            for p in ps:
-                g= IMP.core.XYZRGeometry(p)
-                w.add_geometry(g);
+            #w.set_frame(i+1)
+            #for p in ps:
+            #    g= IMP.core.XYZRGeometry(p)
+            #    w.add_geometry(g);
         print m.evaluate(False)
         self.assertLess(m.evaluate(False), 3)
     def test_incr_nbl(self):
         """Testing incremental scoring with Monte Carlo and a nbl"""
         m= IMP.Model()
-        m.set_log_level(IMP.base.SILENT)
+        IMP.base.set_log_level(IMP.base.SILENT)
         mc= IMP.core.MonteCarlo(m)
-        mc.set_log_level(IMP.base.SILENT)
+        mc.set_log_level(IMP.base.TERSE)
         ps=[]
         bb= IMP.algebra.get_unit_bounding_box_3d()
         for i in range(0,10):
@@ -71,17 +73,18 @@ class Tests(IMP.test.TestCase):
         mc.add_mover(mv)
         icpf= IMP.container.InContainerPairFilter(cpc, True)
         isf.add_close_pair_score(IMP.core.SoftSpherePairScore(100), .2, ps, [icpf])
-        w= IMP.display.PymolWriter(self.get_tmp_file_name("incr_nbl.pym"))
-        w.set_frame(0)
-        for p in ps:
-            g= IMP.core.XYZRGeometry(p)
-            w.add_geometry(g);
+        isf.set_log_level(IMP.base.TERSE)
+        #w= IMP.display.PymolWriter(self.get_tmp_file_name("incr_nbl.pym"))
+        #w.set_frame(0)
+        #for p in ps:
+        #    g= IMP.core.XYZRGeometry(p)
+        #    w.add_geometry(g);
         for i in range(0,10):
             mc.optimize(1000)
-            w.set_frame(i+1)
-            for p in ps:
-                g= IMP.core.XYZRGeometry(p)
-                w.add_geometry(g);
+            #w.set_frame(i+1)
+            #for p in ps:
+            #    g= IMP.core.XYZRGeometry(p)
+            #    w.add_geometry(g);
         print m.evaluate(False)
         self.assert_(m.evaluate(False) < 3)
 
