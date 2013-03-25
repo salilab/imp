001ff4ec875c11d75bf803b51c4ae19966440943
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-03-25 17:57:27 +0000

    Use imp_execute_process so that errors are handled properly.
    git-svn-id: https://svn.salilab.org/imp/trunk@17146 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/base/Setup.cmake
+++ modules/base/Setup.cmake
@@ -10,9 +10,9 @@ endif()
 set(IMP_BASE_CONFIG IMP_BUILD=IMP_${build}:IMP_HAS_DEPRECATED=${IMP_HAS_DEPRECATED}:IMP_HAS_LOG=IMP_${IMP_MAX_LOG}:IMP_HAS_CHECKS=IMP_${IMP_MAX_CHECKS}:IMP_DEBUG=0:IMP_RELEASE=1:IMP_SILENT=0:IMP_PROGRESS=2:IMP_TERSE=3:IMP_VERBOSE=4:IMP_NONE=0:IMP_USAGE=1:IMP_INTERNAL=2 CACHE INTERNAL "" FORCE)
 
 
-message(STATUS "Generating paths.cpp")
-execute_process(COMMAND ${PROJECT_SOURCE_DIR}/tools/build/setup_paths.py
+imp_execute_process("generate paths.cpp"
+               ${PROJECT_BINARY_DIR}
+               COMMAND ${PROJECT_SOURCE_DIR}/tools/build/setup_paths.py
                 "--datapath=${CMAKE_INSTALL_FULL_DATADIR}/IMP"
                 "--examplepath=${CMAKE_INSTALL_FULL_DOCDIR}/examples"
-                "--output=src/base/paths.cpp"
-                WORKING_DIRECTORY ${PROJECT_BINARY_DIR})
+                "--output=src/base/paths.cpp")
