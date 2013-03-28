5487e33ff9de7c87e494cd3c47dd6700362d9649
Daniel Russel <drussel@gmail.com>
2013-03-28 11:59:00 -0700

    Use ExternalProject_Add for RMF so that we don't duplicate RMF code in IMP
    issue #224

--- modules/rmf/dependency/RMF.cmake
+++ modules/rmf/dependency/RMF.cmake
@@ -5,14 +5,27 @@ else()
 
 message(STATUS "Building internal RMF")
 
-set(RMF_BINARY_DIR ${PROJECT_BINARY_DIR}/src/dependency/RMF CACHE INTERNAL "" FORCE)
+include(ExternalProject)
 
-add_subdirectory(${PROJECT_SOURCE_DIR}/modules/rmf/dependency/RMF ${RMF_BINARY_DIR})
+set(rmf_binary_dir ${PROJECT_BINARY_DIR}/dependency/RMF.build)
+set(rmf_source_dir ${PROJECT_BINARY_DIR}/dependency/RMF.source)
+set(RMF_AVRO "internal" CACHE INTERNAL "Whether we are using the Avro shipped with RMF" FORCE)
 
-set(RMF_INCLUDE_PATH ${PROJECT_SOURCE_DIR}/modules/rmf/dependency/RMF/include ${RMF_BINARY_DIR}/include ${RMF_INCLUDE_PATH} CACHE INTERNAL "" FORCE)
-set(RMF_SWIG_PATH ${PROJECT_SOURCE_DIR}/modules/rmf/dependency/RMF/swig CACHE INTERNAL "" FORCE)
+ExternalProject_Add(RMF.build
+  GIT_REPOSITORY "git://github.com/salilab/rmf.git"
+  GIT_TAG "develop"
+  SOURCE_DIR ${rmf_source_dir}
+  BINARY_DIR ${rmf_binary_dir}
+  #CMAKE_ARGS -DCMAKE_INSTALL_PREFIX:PATH=
+  INSTALL_COMMAND "")
 
-set(RMF_LIBRARIES RMF  CACHE INTERNAL "" FORCE)
+set(RMF_DEPENDENCIES RMF.build)
+
+set(RMF_INCLUDE_PATH ${rmf_source_dir}/include ${rmf_binary_dir}/include CACHE INTERNAL "" FORCE)
+set(RMF_SWIG_PATH ${rmf_source_dir}/swig CACHE INTERNAL "" FORCE)
+set(RMF_LINK_PATH ${rmf_binary_dir} ${rmf_binary_dir}/AvroCpp CACHE INTERNAL "" FORCE)
+
+set(RMF_LIBRARIES RMF CACHE INTERNAL "" FORCE)
 
 file(WRITE "${PROJECT_BINARY_DIR}/data/build_info/RMF" "ok=True
 includepath=\"${RMF_INCLUDE_PATH}\"
@@ -22,18 +35,18 @@ libpath=\"${RMF_SWIG_PATH}\"
 
 if(NOT DEFINED IMP_RMF_PATHS_UPDATED)
 message(STATUS "Updating paths for RMF <${IMP_RMF_PATHS_UPDATED}>")
-set(IMP_PYTHONPATH ${PROJECT_BINARY_DIR}/src/dependency/RMF/ ${IMP_PYTHONPATH} CACHE INTERNAL "" FORCE)
-set(IMP_LDPATH ${PROJECT_BINARY_DIR}/src/dependency/RMF/ ${IMP_LDPATH} CACHE INTERNAL "" FORCE)
-set(IMP_PATH ${PROJECT_BINARY_DIR}/src/dependency/RMF/bin/ ${IMP_PATH} CACHE INTERNAL "" FORCE)
+set(IMP_PYTHONPATH ${rmf_binary_dir} ${IMP_PYTHONPATH} CACHE INTERNAL "" FORCE)
+set(IMP_LDPATH ${rmf_binary_dir} ${IMP_LDPATH} CACHE INTERNAL "" FORCE)
+set(IMP_PATH ${rmf_binary_dir}/bin/ ${IMP_PATH} CACHE INTERNAL "" FORCE)
 set(IMP_RMF_PATHS_UPDATED 1 CACHE INTERNAL "" FORCE)
 endif()
 
-file(TO_NATIVE_PATH "${PROJECT_BINARY_DIR}/src/dependency/RMF/" native_path)
+file(TO_NATIVE_PATH "${PROJECT_BINARY_DIR}/dependency/RMF/" native_path)
 set(ENV{PATH} "${native_path}${PATH_SEP}$ENV{PATH}")
 
 if (${RMF_AVRO} MATCHES "internal")
   set(RMF_INCLUDE_PATH ${RMF_INCLUDE_PATH}
-    ${PROJECT_SOURCE_DIR}/modules/rmf/dependency/RMF/AvroCpp
+    ${rmf_source_dir}/AvroCpp
     CACHE INTERNAL "" FORCE)
 endif()
 
