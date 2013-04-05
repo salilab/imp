d51ee651897172b606f975cd4193e33665c85197
ben@SALILAB.ORG <ben@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-04-05 20:00:59 +0000

    Make sure we can find CGAL include and link paths.
    git-svn-id: https://svn.salilab.org/imp/trunk@17289 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/cgal/Setup.cmake
+++ modules/cgal/Setup.cmake
@@ -4,6 +4,9 @@ if (${CGAL_FOUND})
 file(WRITE "${PROJECT_BINARY_DIR}/data/build_info/CGAL" "ok=True")
 set(CGAL_DONT_OVERRIDE_CMAKE_FLAGS TRUE CACHE BOOL "Don't override flags")
 include(${CGAL_USE_FILE})
+# Set "standard" include/link variables from CGAL's
+set(CGAL_INCLUDE_PATH ${CGAL_INCLUDE_DIRS} CACHE INTERNAL "" FORCE)
+set(CGAL_LINK_PATH ${CGAL_LIBRARIES_DIR} CACHE INTERNAL "" FORCE)
 
 if("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU")
 message(STATUS "Updating CMAKE_CXX_FLAGS")
