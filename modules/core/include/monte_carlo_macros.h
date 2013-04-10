9e2ea9bc67ded764bfe437b235260f43c9e9e1a9
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-04-10 18:35:25 +0000

    fix warning macros in MONTE_CARLO macro
    git-svn-id: https://svn.salilab.org/imp/trunk@17371 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/core/include/monte_carlo_macros.h
+++ modules/core/include/monte_carlo_macros.h
@@ -12,12 +12,10 @@
 #include <IMP/base/doxygen_macros.h>
 #include <IMP/base/object_macros.h>
 
-/** Helper macro for implementing IMP::core::MonteCarlo. In
-    addition to the IMP_OBJECT methods, it declares
-    - IMP::core::MonteCarlo::do_step()
+/** You should just declare the needed functions yourself.
  */
 #define IMP_MONTE_CARLO(Name)                           \
-  IMP_PRAGMA(warn do not use this macro)                \
+  IMP_PRAGMA(message("Do not use this macro"))          \
   IMP_IMPLEMENT(virtual void do_step());                \
   IMP_OBJECT(Name)
 
