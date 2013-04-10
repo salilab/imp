9c3a934af97a20d1736bce1a173e1b0a2d95630c
drussel@SALILAB.ORG <drussel@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-04-10 18:20:46 +0000

    improve progress bar docs and announce them
    git-svn-id: https://svn.salilab.org/imp/trunk@17367 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/base/include/log.h
+++ modules/base/include/log.h
@@ -105,12 +105,17 @@ inline bool get_is_log_output(LogLevel l)
 
     @{
  */
-/** Set up the progress bar with the passed description.*/
+/** Set up the progress bar with the passed description.
+
+    See IMP_PROGRESS_DISPLAY().
+*/
 IMPBASEEXPORT void set_progress_display(std::string description,
                                     unsigned int steps);
 /** Set the current progress. When it equals the number of steps,
     the bar is done.*/
 IMPBASEEXPORT void add_to_progress_display(unsigned int step = 1);
+
+/** @} */
 
 IMPBASE_END_NAMESPACE
 
