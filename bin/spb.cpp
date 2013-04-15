01ff3de4675eb6354bc81649d99833eedbcdbb94
Massimiliano Bonomi <mbonomi@salilab.org>
2013-04-15 06:06:10 +0000

    moved helper function for REM to main spb

--- bin/spb.cpp
+++ bin/spb.cpp
@@ -22,6 +22,60 @@
 using namespace IMP;
 using namespace IMP::membrane;
 
+// some usefull stuff
+double* create_temperatures(double tmin,double tmax,int nrep)
+{
+ double* temp;
+ temp=new double[nrep];
+ double tfact=exp(log(tmax/tmin)/double(nrep-1));
+ for(int i=0;i<nrep;++i) {temp[i]=tmin*pow(tfact,i);}
+ return temp;
+}
+
+int* create_indexes(int nrep)
+{
+ int* index;
+ index=new int[nrep];
+ for(int i=0;i<nrep;++i) {index[i]=i;}
+ return index;
+}
+
+int get_friend(int* index,int myrank,int step,int nrep)
+{
+ int myindex=index[myrank];
+ int findex=0;
+ int frank=0;
+
+ if(myindex%2==0 && step%2==0) {findex=myindex+1;}
+ if(myindex%2==0 && step%2==1) {findex=myindex-1;}
+ if(myindex%2==1 && step%2==0) {findex=myindex-1;}
+ if(myindex%2==1 && step%2==1) {findex=myindex+1;}
+ if(findex==-1)   {findex=nrep-1;}
+ if(findex==nrep) {findex=0;}
+
+ for(int i=0; i<nrep; ++i) {if(index[i]==findex) {frank=i;}}
+ return frank;
+}
+
+bool get_acceptance(double score0,double score1,double delta_wte,
+                    double T0,double T1)
+{
+ double accept,delta;
+ delta=(score1-score0)*(1.0/T1-1.0/T0)+delta_wte;
+ if(delta>=0.0){
+  accept=1.0;
+ }else{
+  accept=exp(delta);
+ }
+ double random= (double) rand()/RAND_MAX;
+ if(random<=accept){
+  return true;
+ }else{
+  return false;
+ }
+}
+
+// main program
 int main(int argc, char* argv[])
 {
 
@@ -45,6 +99,7 @@ std::string names="log"+out.str();
 FILE *logfile;
 logfile = fopen(names.c_str(),"w");
 
+// read input
 std::string inputfile="config.ini";
 int i=1;
 while(i<argc){ if(strcmp(argv[i],"-in")==0){++i; inputfile=argv[i];}
