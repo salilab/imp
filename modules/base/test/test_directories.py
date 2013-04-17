a75e9f422a01ead6528375d230586af9766fe394
barak@SALILAB.ORG <barak@SALILAB.ORG@e91f8c7b-0e32-4433-a9ac-4502d39a4eab>
2013-04-17 16:20:12 +0000

    remove redundant test for testing content of data folder
    git-svn-id: https://svn.salilab.org/imp/trunk@17487 e91f8c7b-0e32-4433-a9ac-4502d39a4eab

--- modules/base/test/test_directories.py
+++ modules/base/test/test_directories.py
@@ -4,14 +4,11 @@ import os.path
 
 class Tests(IMP.test.TestCase):
 
-    def test_data_directory(self):
-        """Test the get_data_directory function"""
-        f = IMP.base.get_data_path("README")
-        self.assertTrue(os.path.exists(f) and not os.path.isdir(f))
-
     def test_bad_data_directory(self):
         """get_data_directory should raise an error if it does not exist"""
+        print "HI"
         self.assertRaises(IMP.base.IOException, IMP.base.get_data_path, "not/exist")
+        print "HO"
 
 if __name__ == '__main__':
     IMP.test.main()
