ff0ed0e4eaf867fc56e4159bb78f78efcaffb5b2
Ben Webb <ben@salilab.org>
2013-04-18 14:03:16 -0700

    Demonstrate and test MultiFit refinement.

--- applications/multifit_tools/test/expensive_test_3sfd_example.py
+++ applications/multifit_tools/test/expensive_test_3sfd_example.py
@@ -8,10 +8,11 @@ class Tests(IMP.test.ApplicationTestCase):
         input_file_dir = self.get_input_file_name('')
         cmds = self.read_shell_commands('../3sfd.dox')
         d = IMP.test.RunInTempDir()
-        # Skip last command (clustering) since it requires a nonstandard
-        # Python module
         for c in cmds[:-1]:
-            self.run_shell_command(c)
+            # Skip the cluster command since it requires a nonstandard
+            # Python module (fastcluster)
+            if ' cluster ' not in c:
+                self.run_shell_command(c)
         # Should have produced 5 models
         for i in range(5):
             os.unlink('model.%d.pdb' % i)
