2d782435f3e0d1c7bbb32fc6fd37af84cc0a158f
Yannick Spill <yannick@salilab.org>
2013-04-19 17:50:21 +0200

    Revert "Merge branch 'feature/feature/fix_warnings' into develop"
    This reverts commit 2f909962b1d8cb448ffaad28f99863b1eafd9c29, reversing
    changes made to 9250aeb1a80e15f9fe54dc6507715a886e94e9ea.
    following recommendations of https://www.kernel.org/pub/software/scm/git/docs/howto/revert-a-faulty-merge.txt

--- modules/isd/src/MarginalHBondRestraint.cpp
+++ modules/isd/src/MarginalHBondRestraint.cpp
@@ -38,30 +38,27 @@ void MarginalHBondRestraint::add_contribution(PairContainer *pc, double Iexp)
 double
 MarginalHBondRestraint::unprotected_evaluate(DerivativeAccumulator *accum) const
 {
-    IMP_OBJECT_LOG;
-    IMP_USAGE_CHECK(get_model(),
-                    "You must at least register the restraint with the model"
-                    << " before calling evaluate.");
     //compute logsquares
     double logsquares=0;
-    base::Vector<double> meandists; //mean distances^-6, length(volumes_)
+    std::vector<double> meandists; //mean distances^-6, length(volumes_)
     //store interparticle distances^-6
-    base::Vector<base::Vector<double> > alldists;
+    std::vector<std::vector<double> > alldists;
     int ncontribs = volumes_.size();
     for (int i=0; i< ncontribs; ++i) //loop on all contributions
     {
+        int npairs = contribs_[i]->get_number_of_particle_pairs();
         double mean=0;
-        base::Vector<double> dists;
-        IMP_CONTAINER_FOREACH(PairContainer, contribs_[i],
-                             {
-            core::XYZ d0(get_model(), _1[0]);
-            core::XYZ d1(get_model(), _1[1]);
+        std::vector<double> dists;
+        for (int p=0; p<npairs; ++p)//loop on all pairs of contribution i
+        {
+            ParticlePair pair = contribs_[i]->get_particle_pair(p);
+            core::XYZ d0(pair[0]),d1(pair[1]);
             double dist = (d1.get_coordinates()
                            - d0.get_coordinates()).get_squared_magnitude();
             dist = 1.0/cube(dist);
             mean += dist;
             if (accum) dists.push_back(dist);
-                             });
+        }
         meandists.push_back(mean);
         if (accum) alldists.push_back(dists);
         logsquares += square(log(volumes_[i]/mean));
@@ -77,18 +74,19 @@ MarginalHBondRestraint::unprotected_evaluate(DerivativeAccumulator *accum) const
             double deriv_mean = ncontribs/logsquares
                                 * log(volumes_[i]/meandists[i])
                                 * pow(meandists[i],1./6)*6;
-            IMP_CONTAINER_FOREACH(PairContainer, contribs_[i],
-                                  {
-                double deriv_pair = pow(alldists[i][_2]/meandists[i],-7./6);
-                core::XYZ d0(get_model(), _1[0]);
-                core::XYZ d1(get_model(), _1[1]);
+            int npairs = contribs_[i]->get_number_of_particle_pairs();
+            for (int p=0; p<npairs; ++p)
+            {
+                ParticlePair pair = contribs_[i]->get_particle_pair(p);
+                double deriv_pair = pow(alldists[i][p]/meandists[i],-7./6);
+                core::XYZ d0(pair[0]),d1(pair[1]);
                 algebra::Vector3D dev = (d1.get_coordinates()
                                          - d0.get_coordinates());
                 double dist = dev.get_magnitude();
                 algebra::Vector3D deriv = deriv_mean * deriv_pair * dev / dist;
                 d1.add_to_derivatives(deriv, *accum);
                 d0.add_to_derivatives(-deriv, *accum);
-                                  });
+            }
         }
     }
     return score;
@@ -96,14 +94,27 @@ MarginalHBondRestraint::unprotected_evaluate(DerivativeAccumulator *accum) const
 
 /* Return all particles whose attributes are read by the restraints. To
    do this, ask the pair score what particles it uses.*/
-ModelObjectsTemp MarginalHBondRestraint::do_get_inputs() const
+ParticlesTemp MarginalHBondRestraint::get_input_particles() const
 {
-  ModelObjectsTemp ret;
+  ParticlesTemp ret;
   for (unsigned i=0; i<volumes_.size(); ++i)
   {
-    ret+=IMP::get_particles(get_model(),
-                            contribs_[i]->get_all_possible_indexes());
-    ret.push_back(contribs_[i]);
+      int npairs = contribs_[i]->get_number_of_particle_pairs();
+      for (int j=0; j < npairs; ++j)
+      {
+          ret.push_back(contribs_[i]->get_particle_pair(j)[0]);
+          ret.push_back(contribs_[i]->get_particle_pair(j)[1]);
+      }
+  }
+  return ret;
+}
+
+ContainersTemp MarginalHBondRestraint::get_input_containers() const
+{
+  ContainersTemp ret;
+  for (unsigned i=0; i<volumes_.size(); ++i)
+  {
+      ret.push_back(contribs_[i]);
   }
   return ret;
 }
--- modules/isd/src/MarginalNOERestraint.cpp
+++ modules/isd/src/MarginalNOERestraint.cpp
@@ -12,7 +12,6 @@
 #include <math.h>
 #include <IMP/PairContainer.h>
 #include <IMP/container/ListPairContainer.h>
-#include <IMP/container_macros.h>
 
 IMPISD_BEGIN_NAMESPACE
 
@@ -41,26 +40,25 @@ MarginalNOERestraint::unprotected_evaluate(DerivativeAccumulator *accum) const
 {
     //compute gammahat and store distances
     double loggammahat=1;
-    base::Vector<double> meandists; //mean distances^-6, length(volumes_)
+    std::vector<double> meandists; //mean distances^-6, length(volumes_)
     //store interparticle distances^-6
-    base::Vector<base::Vector<double> > alldists;
+    std::vector<std::vector<double> > alldists;
     int ncontribs = volumes_.size();
     for (int i=0; i< ncontribs; ++i) //loop on all contributions
     {
+        int npairs = contribs_[i]->get_number_of_particle_pairs();
         double mean=0;
-        base::Vector<double> dists;
-        IMP_CONTAINER_FOREACH(PairContainer, contribs_[i],
-                              {
-            core::XYZ d0(get_model(), _1[0]);
-            core::XYZ d1(get_model(), _1[1]);
+        std::vector<double> dists;
+        for (int p=0; p<npairs; ++p)//loop on all pairs of contribution i
+        {
+            ParticlePair pair = contribs_[i]->get_particle_pair(p);
+            core::XYZ d0(pair[0]),d1(pair[1]);
             double dist = (d1.get_coordinates()
                            - d0.get_coordinates()).get_squared_magnitude();
             dist = 1.0/cube(dist);
             mean += dist;
-            if (accum) {
-              dists.push_back(dist);
-            }
-                              });
+            if (accum) dists.push_back(dist);
+        }
         meandists.push_back(mean);
         if (accum) alldists.push_back(dists);
         loggammahat += log(volumes_[i]/mean);
@@ -70,7 +68,7 @@ MarginalNOERestraint::unprotected_evaluate(DerivativeAccumulator *accum) const
 
     //compute SS
     double SS = 0;
-    base::Vector<double> logterms;
+    std::vector<double> logterms;
     for (int i=0; i < ncontribs; ++i)
     {
         double val = log(volumes_[i]/meandists[i])-loggammahat_;
@@ -85,23 +83,24 @@ MarginalNOERestraint::unprotected_evaluate(DerivativeAccumulator *accum) const
         for (int i=0; i<ncontribs; ++i)
         {
             double deriv_mean = logterms[i]*6*(ncontribs - 1)/SS;
-            IMP_CONTAINER_FOREACH(PairContainer, contribs_[i],
-                                  {
-                core::XYZ d0(get_model(), _1[0]);
-                core::XYZ d1(get_model(), _1[1]);
-                double deriv_pair = alldists[i][_2]/meandists[i];
+            int npairs = contribs_[i]->get_number_of_particle_pairs();
+            for (int p=0; p<npairs; ++p)
+            {
+                ParticlePair pair = contribs_[i]->get_particle_pair(p);
+                double deriv_pair = alldists[i][p]/meandists[i];
                 if (std::abs(deriv_pair) > 1e2) {
                   std::cout << "NOE derivative warning : deriv mean "
                             << deriv_mean << " pair " << deriv_pair
                             << std::endl;
                 }
+                core::XYZ d0(pair[0]),d1(pair[1]);
                 algebra::Vector3D dev = (d1.get_coordinates()
                                          - d0.get_coordinates());
                 double dist = dev.get_squared_magnitude();
                 algebra::Vector3D deriv = deriv_mean * deriv_pair * dev / dist;
                 d1.add_to_derivatives(deriv, *accum);
                 d0.add_to_derivatives(-deriv, *accum);
-                                  });
+            }
         }
     }
     return score;
@@ -109,20 +108,31 @@ MarginalNOERestraint::unprotected_evaluate(DerivativeAccumulator *accum) const
 
 /* Return all particles whose attributes are read by the restraints. To
    do this, ask the pair score what particles it uses.*/
-ModelObjectsTemp MarginalNOERestraint::do_get_inputs() const
+ParticlesTemp MarginalNOERestraint::get_input_particles() const
 {
-  ModelObjectsTemp ret;
+  ParticlesTemp ret;
   for (unsigned i=0; i<volumes_.size(); ++i)
   {
-    ret+= IMP::get_particles(get_model(),
-                             contribs_[i]->get_all_possible_indexes());
+      int npairs = contribs_[i]->get_number_of_particle_pairs();
+      for (int j=0; j < npairs; ++j)
+      {
+          ret.push_back(contribs_[i]->get_particle_pair(j)[0]);
+          ret.push_back(contribs_[i]->get_particle_pair(j)[1]);
+      }
   }
+  return ret;
+}
+
+ContainersTemp MarginalNOERestraint::get_input_containers() const
+{
+  ContainersTemp ret;
   for (unsigned i=0; i<volumes_.size(); ++i)
   {
       ret.push_back(contribs_[i]);
   }
   return ret;
 }
+
 void MarginalNOERestraint::do_show(std::ostream& out) const
 {
   out << "Marginal NOE restraint with " << volumes_.size()
