3da734c34b24121d02eb2d36ffb51cfa759d2ae1
Ben Webb <ben@salilab.org>
2013-04-23 12:22:34 -0700

    Add missing headers.

--- modules/kinematics/include/KinematicForestScoreState.h
+++ modules/kinematics/include/KinematicForestScoreState.h
@@ -13,6 +13,8 @@
 #include "kinematics_config.h"
 #include <IMP/ScoreState.h>
 #include <IMP/base/warning_macros.h>
+#include <IMP/core/rigid_bodies.h>
+#include <IMP/kinematics/KinematicForest.h>
 
 IMPKINEMATICS_BEGIN_NAMESPACE
 
