3aba9da1e0b0ebdff8a07fb81b24b813ddfe430e
Barak Raveh <barak@salilab.org>
2013-04-24 15:26:13 -0700

    repeat test_zyz to reveal its instability at every test

--- modules/algebra/test/test_zyz.py
+++ modules/algebra/test/test_zyz.py
@@ -7,24 +7,25 @@ import random
 class Tests(IMP.test.TestCase):
     def test_cylinder_construction(self):
         """Check Euler ZYZ conversions"""
-        rot= random.uniform(0,math.pi)
-        tilt= random.uniform(0,math.pi)
-        psi= random.uniform(0,math.pi)
-        print "In"
-        print rot
-        print tilt
-        print psi
-        IMP.base.set_log_level(IMP.base.VERBOSE)
-        r= IMP.algebra.get_rotation_from_fixed_zyz(rot, tilt, psi)
-        e= IMP.algebra.get_fixed_zyz_from_rotation(r)
-        print "Out"
-        print e.get_rot()
-        print e.get_tilt()
-        print e.get_psi()
-        self.assertAlmostEqual(rot, e.get_rot(), delta=.1)
-        self.assertAlmostEqual(tilt, e.get_tilt(), delta=.1)
-        self.assertAlmostEqual(psi, e.get_psi(), delta=.1)
-
+        for i in range(1000):
+            print i
+            rot= random.uniform(0,math.pi)
+            tilt= random.uniform(0,math.pi)
+            psi= random.uniform(0,math.pi)
+            print "In"
+            print rot
+            print tilt
+            print psi
+            IMP.base.set_log_level(IMP.base.VERBOSE)
+            r= IMP.algebra.get_rotation_from_fixed_zyz(rot, tilt, psi)
+            e= IMP.algebra.get_fixed_zyz_from_rotation(r)
+            print "Out"
+            print e.get_rot()
+            print e.get_tilt()
+            print e.get_psi()
+            self.assertAlmostEqual(rot, e.get_rot(), delta=.1)
+            self.assertAlmostEqual(tilt, e.get_tilt(), delta=.1)
+            self.assertAlmostEqual(psi, e.get_psi(), delta=.1)
 
 
 if __name__ == '__main__':
