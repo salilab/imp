cffb08c4e95e41eeff96138774dca44901b95161
Ben Webb <ben@salilab.org>
2013-04-25 17:20:20 -0700

    Specify which branch to check out.

--- tools/nightly-tests/auto-build.sh
+++ tools/nightly-tests/auto-build.sh
@@ -1,35 +1,52 @@
 #!/bin/sh
 
-# Script to get git HEAD and make a .tar.gz on a shared disk, for use by
+# Script to get a git branch and make a .tar.gz on a shared disk, for use by
 # autobuild scripts on our build machines.
 #
 # Should be run on from a crontab on a machine that has access to a git
 # clone, e.g.
 #
-# 10 1 * * * /cowbell1/home/ben/imp/tools/auto-build.sh
+# 10 1 * * * /cowbell1/home/ben/imp/tools/auto-build.sh develop
 
 GIT_TOP=/cowbell1/git
 GIT_DIR=${GIT_TOP}/imp/.git
 export GIT_DIR
 
+BRANCH=$1
+
 VER=git
 
 TMPDIR=/var/tmp/imp-build-$$
-IMPTOP=/salilab/diva1/home/imp
+IMPTOP=/salilab/diva1/home/imp/$BRANCH
 
 rm -rf ${TMPDIR}
 mkdir ${TMPDIR}
-cd ${TMPDIR}
 
+# Make sure VERSION reflects the head of the specified remote branch
+cd ${GIT_TOP}/imp
+./setup_git.py  >& /dev/null
+git checkout ${BRANCH} -q >& out
+# Squash chatty output from git checkout
+grep -v "Version=" out
+git merge origin -q
+
+cd ${TMPDIR}
 # Get top-most revision
 rev=`git rev-parse HEAD`
 shortrev=`git rev-parse --short HEAD`
+
+# Get IMP code from git
+tar -C ${GIT_TOP} --exclude .git -cf - imp | tar -xf -
 
 # Get date and revision-specific install directories
 SORTDATE=`date -u "+%Y%m%d"`
 DATE=`date -u +'%Y/%m/%d'`
 IMPINSTALL=${IMPTOP}/${SORTDATE}-${shortrev}
-IMPVERSION="${SORTDATE}.${VER}${shortrev}"
+if [ ${BRANCH} = "develop" ]; then
+  IMPVERSION="${SORTDATE}.${VER}${shortrev}"
+else
+  IMPVERSION=`cat imp/VERSION`
+fi
 IMPSRCTGZ=${IMPINSTALL}/build/sources/imp-${IMPVERSION}.tar.gz
 rm -rf ${IMPINSTALL}
 mkdir -p ${IMPINSTALL}/build/sources ${IMPINSTALL}/build/logs
@@ -38,12 +55,11 @@ mkdir -p ${IMPINSTALL}/build/sources ${IMPINSTALL}/build/logs
 rm -f ${IMPTOP}/.SVN-new
 ln -s ${IMPINSTALL} ${IMPTOP}/.SVN-new
 
-# Get IMP code from git
-tar -C ${GIT_TOP} --exclude .git -cf - imp | tar -xf -
-
 # Put version number, date and revision into relevant files
-(cd imp/doc/doxygen && sed -e "s#^PROJECT_NUMBER.*#PROJECT_NUMBER = ${VER}${shortrev}, ${DATE}#" < Doxyfile.in > .dox && mv .dox Doxyfile.in)
-echo "${VER}${shortrev}" > imp/VERSION
+if [ ${BRANCH} = "develop" ]; then
+  (cd imp/doc/doxygen && sed -e "s#^PROJECT_NUMBER.*#PROJECT_NUMBER = ${VER}${shortrev}, ${DATE}#" < Doxyfile.in > .dox && mv .dox Doxyfile.in)
+  echo "${VER}${shortrev}" > imp/VERSION
+fi
 
 # Write out version files
 verfile="${IMPINSTALL}/build/imp-version"
