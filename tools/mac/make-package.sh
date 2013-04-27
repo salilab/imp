0a069debaf1c615f9f658376c06b9a9d88d847a9
Ben Webb <ben@salilab.org>
2013-04-26 18:56:03 -0700

    Use DESTDIR.
    (Files aren't in their final locations yet.)

--- tools/mac/make-package.sh
+++ tools/mac/make-package.sh
@@ -208,11 +208,11 @@ for lib in ${BUNDLED_LIBS}; do
 done
 
 # Save space by replacing duplicates with symlinks
-(cd ${BUNDLED_LIB_DIR} && rm libopencv_imgproc.2.4.dylib && ln -sf libopencv_imgproc.2.4.2.dylib libopencv_imgproc.2.4.dylib)
-(cd ${BUNDLED_LIB_DIR} && rm libopencv_core.2.4.dylib && ln -sf libopencv_core.2.4.2.dylib libopencv_core.2.4.dylib)
+(cd ${DESTDIR}/${BUNDLED_LIB_DIR} && rm libopencv_imgproc.2.4.dylib && ln -sf libopencv_imgproc.2.4.2.dylib libopencv_imgproc.2.4.dylib)
+(cd ${DESTDIR}/${BUNDLED_LIB_DIR} && rm libopencv_core.2.4.dylib && ln -sf libopencv_core.2.4.2.dylib libopencv_core.2.4.dylib)
 
 # Make sure we don't link against any non-standard libraries that aren't bundled
-otool -L *.dylib ${bins} IMP-python/*.so ${BUNDLED_LIB_DIR}/* |grep -Ev '/usr/lib|/usr/local/lib/imp-3rd-party|/usr/local/lib/libimp|/usr/local/lib/libRMF|/System/Library/|:'|sort -u > /tmp/non-standard.$$
+otool -L *.dylib ${bins} IMP-python/*.so ${DESTDIR}/${BUNDLED_LIB_DIR}/* |grep -Ev '/usr/lib|/usr/local/lib/imp-3rd-party|/usr/local/lib/libimp|/usr/local/lib/libRMF|/System/Library/|:'|sort -u > /tmp/non-standard.$$
 if [ -s /tmp/non-standard.$$ ]; then
   echo "The following non-standard libraries are linked against, and were"
   echo "not bundled:"
