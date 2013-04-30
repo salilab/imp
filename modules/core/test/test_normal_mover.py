aeb5fb3b7887ec4adc17e21d170b574e6ca229ea
Yannick Spill <yannick@salilab.org>
2013-04-30 17:25:34 +0200

    test that shows something is wrong

---
+++ modules/core/test/test_normal_mover.py
@@ -0,0 +1,38 @@
+import IMP
+import IMP.test
+import IMP.core
+import IMP.container
+
+from math import *
+
+class NormalMoverTest(IMP.test.TestCase):
+    def setUp(self):
+        IMP.test.TestCase.setUp(self)
+        m = IMP.Model()
+        pa=IMP.Particle(m)
+        att=IMP.FloatKey("test")
+        pa.add_attribute(att,5.0)
+        mv=IMP.core.NormalMover(m, pa.get_index(), [att], 1.0)
+        self.m = m
+        self.mv = mv
+        self.pa = pa
+        self.att = att
+
+    def test_propose(self):
+        old=self.pa.get_value(self.att)
+        result=self.mv.propose()
+        new=self.pa.get_value(self.att)
+        parts=result.get_moved_particles()
+        self.assertTrue(len(parts)==1)
+        self.assertTrue(parts[0] is self.pa)
+        self.assertTrue(abs(old-new)>1e-7)
+
+    def test_reject(self):
+        old=self.pa.get_value(self.att)
+        self.mv.propose()
+        self.mv.reject()
+        new=self.pa.get_value(self.att)
+        self.assertAlmostEqual(new,old)
+
+if __name__ == '__main__':
+    IMP.test.main()
