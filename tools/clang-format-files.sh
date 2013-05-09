6c2f48b2785330ffc5718b9b26fa4cd7fa8a6c36
Daniel Russel <drussel@gmail.com>
2013-05-09 10:08:49 -0700

    don't require clean state for clang-format

--- tools/clang-format-files.sh
+++ tools/clang-format-files.sh
@@ -1,11 +1,5 @@
 #!/usr/bin/env sh
 
-modfiles=`git status --porcelain | grep "^ M "`
-if test -n "$modfiles"; then
-  echo "This script can only be run on a clean repository."
-  exit 1
-fi
-
 sed -i.old "s#END_NAMESPACE#END_NAMESPACE;#g" $*
 sed -i.old "s#END_INTERNAL_NAMESPACE#END_INTERNAL_NAMESPACE;#g" $*
 sed -i.old "s#BEGIN_NAMESPACE#BEGIN_NAMESPACE;#g" $*
@@ -18,11 +12,3 @@ sed -i.old "s#END_INTERNAL_NAMESPACE;#END_INTERNAL_NAMESPACE#g" $*
 sed -i.old "s#BEGIN_NAMESPACE;#BEGIN_NAMESPACE#g" $*
 sed -i.old "s#BEGIN_INTERNAL_NAMESPACE;#BEGIN_INTERNAL_NAMESPACE#g" $*
 sed -i.old "s# *\$##g" $*
-
-for i in $*
-do
-match=`echo $i | grep _macros.h`
-if test -n "$match"; then
-git checkout $i
-fi
-done
