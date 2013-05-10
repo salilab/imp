b8aa33d323ca531dc99828cd0311491f1fcb9172
Daniel Russel <drussel@gmail.com>
2013-05-10 10:15:55 -0700

    disallow import as and import from in python examples
    it confuses doxygen and makes it harder to understand the examples.
    
    close #319

--- tools/check-standards.py
+++ tools/check-standards.py
@@ -70,6 +70,8 @@ def check_python_file(filename, errors):
        standards"""
     temptest = re.compile('\s+def\s+temp_hide_test.*')
     test= re.compile('\s+def\s+(test_[abcdefghijklmnopqrstuvwxyz0123456789_]*)\(')
+    import_as= re.compile('[ ]*import[ ]*.*[ ]*as[ ]*.*')
+    import_from = re.compile('[ ]*from[ ]*.*[ ]*import[ ]*.*')
     tests=[]
     for (num, line) in enumerate(file(filename, "r")):
         _check_do_not_commit(line, filename, num, errors)
@@ -85,6 +87,9 @@ def check_python_file(filename, errors):
                 errors.append('%s:%d: Test case has multiple tests with the same name %s' \
                           % (filename, num+1, g))
             tests.append(m.group(1))
+        if filename.find("test") == -1 and filename.find("example") != -1:
+            if import_as.match(line) or import_from.match(line):
+                errors.append('%s:%d: Examples should not rename types on import as that confuses doxygen: '%(filename, num+1) + line)
     fh = file(filename, "r")
     r = Reindenter(fh)
     try:
