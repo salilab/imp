7af1da6ace30a4878e2195ff5bcf37afa0fee007
Dina Schneidman <duhovka@gmail.com>
2013-05-30 15:49:41 -0700

    add a program for cross linking score of a single structure

---
+++ applications/integrative_docking/cross_linking/cross_links_single_score.cpp
@@ -0,0 +1,116 @@
+/**
+ *  \file cross_link_score.cpp \brief A program for scoring of docking models
+ *  with cross linking data
+ *
+ *  Copyright 2007-2013 IMP Inventors. All rights reserved.
+ *
+ */
+#include <IMP/Model.h>
+
+#include "helpers.h"
+#include "CrossLink.h"
+#include "CrossLinkingResult.h"
+#include "DockingDistanceRestraint.h"
+
+#include <IMP/algebra/standard_grids.h>
+#include <IMP/algebra/Transformation3D.h>
+#include <IMP/atom/Atom.h>
+#include <IMP/atom/pdb.h>
+
+#include <fstream>
+#include <vector>
+#include <string>
+
+#include <boost/program_options.hpp>
+namespace po = boost::program_options;
+
+int main(int argc, char **argv) {
+  // print command
+  for(int i=0; i<argc; i++) std::cerr << argv[i] << " "; std::cerr << std::endl;
+
+  // input parsing
+  std::string out_file_name;
+  po::options_description desc("Usage: <pdb> <cross_links_file>");
+  desc.add_options()
+    ("help", "static and transformed molecules from docking with \
+transformation file. \
+Each docked complex will be compared against cross links in cross_links_file.")
+    ("input-files", po::value< std::vector<std::string> >(),
+     "input PDB, transformation and profile files")
+    ("output_file,o",
+     po::value<std::string>(&out_file_name)->default_value("cxms_score.res"),
+     "output file name, default name cxms_score.res");
+
+  po::positional_options_description p;
+  p.add("input-files", -1);
+  po::variables_map vm;
+  po::store(
+      po::command_line_parser(argc,argv).options(desc).positional(p).run(), vm);
+  po::notify(vm);
+
+  // parse filenames
+  std::string pdb;
+  std::string cross_links_file;
+  std::vector<std::string> files;
+  if(vm.count("input-files")) {
+    files = vm["input-files"].as< std::vector<std::string> >();
+  }
+  if(vm.count("help") || files.size() != 2) {
+    std::cout << desc << "\n"; return 0;
+  }
+  pdb = files[0];
+  cross_links_file = files[1];
+
+  // read pdb  files, prepare particles
+  IMP::Model *model = new IMP::Model();
+  IMP::atom::Hierarchy mhd =
+    IMP::atom::read_pdb(pdb, model,
+                        new IMP::atom::NonWaterNonHydrogenPDBSelector(),
+                        true, true);
+  IMP::Particles residue_particles = get_by_type(mhd, IMP::atom::RESIDUE_TYPE);
+
+  // get CA atoms for residues
+  IMP::Particles ca_atoms;
+  for(unsigned int i=0; i<residue_particles.size(); i++) {
+    IMP::atom::Atom at =
+      IMP::atom::get_atom(IMP::atom::Residue(residue_particles[i]),
+                          IMP::atom::AT_CA);
+    if(at.get_particle() != NULL) ca_atoms.push_back(at.get_particle());
+  }
+
+  // read cross_links_file
+  std::vector<CrossLink> cross_links;
+  read_cross_link_file(cross_links_file, cross_links);
+  std::vector<DockingDistanceRestraint> distance_restraints_;
+
+  // find CA atoms from cross linked residues
+  for(unsigned int i=0; i<cross_links.size(); i++) {
+    IMP::algebra::Vector3D v1 =
+      get_ca_coordinate(ca_atoms, cross_links[i].get_residue1(),
+                        cross_links[i].get_chain1());
+    IMP::algebra::Vector3D v2 =
+      get_ca_coordinate(ca_atoms, cross_links[i].get_residue2(),
+                        cross_links[i].get_chain2());
+    DockingDistanceRestraint ddr(v1, v2, cross_links[i].get_max_distance(),
+                                 cross_links[i].get_min_distance());
+    distance_restraints_.push_back(ddr);
+  }
+
+  // score
+  float score = 0.0;
+  int unsatisfied_num = 0;
+  for(unsigned int i=0; i<distance_restraints_.size(); i++) {
+    float curr_score = distance_restraints_[i].get_score();
+    float dist = distance_restraints_[i].get_distance();
+    if(curr_score <= 0.0) {
+      unsatisfied_num++;
+      std::cout << i << " - " << curr_score << " d= " << dist << std::endl;
+    } else {
+      score+= curr_score;
+      std::cout << i << " + " << curr_score << " d= " << dist << std::endl;
+    }
+  }
+  std::cerr << "Score = " << score << " unsatisfied_num "
+            << unsatisfied_num << std::endl;
+  return 0;
+}
--- applications/integrative_docking/test/test_cross_links.py
+++ applications/integrative_docking/test/test_cross_links.py
@@ -26,6 +26,27 @@ class CrossLinksApplicationTest(IMP.test.ApplicationTestCase):
         os.unlink('cxms_all.dat')
 
 
+    def test_simple_single_structure_score(self):
+        """Simple test of interface cross links single structure score application"""
+        destination = open('complex.pdb','w')
+        file1 = open(self.get_input_file_name('static.pdb'),'r')
+        file2 = open(self.get_input_file_name('transformed.pdb'),'rb')
+        destination.write(file1.read());
+        destination.write(file2.read());
+        destination.close()
+        p = self.run_application('cross_links_single_score',
+                                 ['complex.pdb',
+                                 self.get_input_file_name('cxms.dat')])
+
+        out, err = p.communicate()
+        sys.stderr.write(err)
+        self.assertApplicationExitedCleanly(p.returncode, err)
+        m = re.search('Score\s+=\s+([\d\.]+)\r?', err)
+        self.assertIsNotNone(m, msg="Score output not found in " + str(out))
+        self.assertAlmostEqual(float(m.group(1)), 0.267, delta=0.01)
+
+        os.unlink('complex.pdb')
+
     def test_simple_score(self):
         """Simple test of cross linking score application"""
         p = self.run_application('cross_links_score',
