ba85ae7259eed2d9b910a240eb59a6c752c26fac
Daniel Russel <drussel@gmail.com>
2013-06-25 13:30:27 -0700

    fix sigmoid UF

--- modules/atom/include/internal/Sigmoid.h
+++ modules/atom/include/internal/Sigmoid.h
@@ -16,23 +16,25 @@ IMPATOM_BEGIN_INTERNAL_NAMESPACE
 //! Sigmoid function
 class Sigmoid : public UnaryFunction
 {
+  Float E0_;
+  Float Zmid_;
+  Float n_;
 public:
   /** Create with the given mean and the spring constant k */
   Sigmoid(Float E0, Float Zmid, Float n) :
    E0_(E0), Zmid_(Zmid), n_(n) {}
 
-  IMP_UNARY_FUNCTION_INLINE(Sigmoid,
-                            E0_ / ( 1.0 + pow(feature / Zmid_, n_) ),
-                            -E0_ * n_ * pow(feature / Zmid_, n_) /
-                            ( feature * (1.0 + pow(feature / Zmid_, n_))
-                            * (1.0 + pow(feature / Zmid_, n_)) ),
-                            "Sigmoid: " << E0_ << " and " << Zmid_
-                            << " and " << n_ << std::endl);
-
-private:
-  Float E0_;
-  Float Zmid_;
-  Float n_;
+  virtual DerivativePair
+  evaluate_with_derivative(double feature) const IMP_OVERRIDE {
+    return DerivativePair(evaluate(feature),
+                          -E0_ * n_ * pow(feature / Zmid_, n_) /
+                          ( feature * (1.0 + pow(feature / Zmid_, n_))
+                            * (1.0 + pow(feature / Zmid_, n_)) ));
+  }
+  virtual double evaluate(double feature) const IMP_OVERRIDE {
+    return E0_ / ( 1.0 + std::pow(feature / Zmid_, n_) );
+  }
+  IMP_OBJECT_METHODS(Sigmoid);
 };
 
 IMPATOM_END_INTERNAL_NAMESPACE
