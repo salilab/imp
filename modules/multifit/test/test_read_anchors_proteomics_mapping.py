05f68ad5b8474ba591489b5fe7938e0e59469943
Ben Webb <ben@salilab.org>
2013-07-23 12:38:12 -0700

    Don't call get_input_file_name() on files that don't exist.

--- modules/multifit/test/test_read_anchors_proteomics_mapping.py
+++ modules/multifit/test/test_read_anchors_proteomics_mapping.py
@@ -8,6 +8,7 @@ class Tests(IMP.test.TestCase):
     def test_run(self):
         """Check correct reading of protein anchors mapping file"""
         data_file = self.get_input_file_name("anchors.proteomics.input")
+        input_dir = os.path.dirname(data_file)
         mapping_file = self.get_input_file_name(
                                         "proteomics.anchors.mapping.txt")
         prot_data = IMP.multifit.read_proteomics_data(data_file)
@@ -16,10 +17,10 @@ class Tests(IMP.test.TestCase):
         self.assertEqual(os.path.realpath(
                                mapping.get_paths_filename_for_protein("A")),
                          os.path.realpath(
-                               self.get_input_file_name('1z5s.A.paths.txt')))
+                               os.path.join(input_dir, '1z5s.A.paths.txt')))
         self.assertEqual(os.path.realpath(mapping.get_anchors_filename()),
                          os.path.realpath(
-                            self.get_input_file_name('1z5s_15_14_anchors.txt')))
+                            os.path.join(input_dir, '1z5s_15_14_anchors.txt')))
         self.assertEqual(mapping.get_paths_for_protein("A"), [])
 
 if __name__ == '__main__':
