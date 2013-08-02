991fad9990d213aea5b05adb40969f4eb599ace1
Daniel Russel <drussel@gmail.com>
2013-08-02 15:13:55 -0700

    use new OptimizerState constructor addres #464

--- modules/misc/src/DecayPairContainerOptimizerState.cpp
+++ modules/misc/src/DecayPairContainerOptimizerState.cpp
@@ -15,7 +15,8 @@ IMPMISC_BEGIN_NAMESPACE
 DecayPairContainerOptimizerState::DecayPairContainerOptimizerState(
     PairPredicate *pred, const ParticlePairsTemp &initial_list,
     std::string name)
-    : PeriodicOptimizerState(name),
+  : PeriodicOptimizerState(initial_list[0][0]->get_model(),
+                           name),
       pred_(pred),
       input_(new container::ListPairContainer(initial_list, "decay input")) {
   output_ = new IMP::internal::InternalDynamicListPairContainer(
