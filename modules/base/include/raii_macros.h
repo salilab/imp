42a4c7696d01daeed7fd4faef836384262a1483e
Barak Raveh <barak.raveh@gmail.com>
2013-08-06 09:08:04 -0700

    extra doc for RAII

--- modules/base/include/raii_macros.h
+++ modules/base/include/raii_macros.h
@@ -12,27 +12,38 @@
 #include "showable_macros.h"
 #include "RAII.h"
 
-//! Declare a RAII-style class
-/** Since such classes are typically quite small and simple, all
+//! Declares RAII-style methods in a class
+/** Since such class methods are typically quite small and simple, all
     the implementation is inline. The macro declares
     - default constructor
+    - explicit constructor
     - RAII::set()
     - RAII::reset()
     - destructor
-    The Set and Reset arguments are the code to run in the respective
-    functions. "args" are the argument string (in parens) for the
-    constructor and set.
+
+    @param Name the class name
+    @param args the argument string (in parens) for the explicit constructor
+                and set()
+    @param Initialize code called from any constrcutor, including the default
+    @param Set the code called from the explicit constructor or the set()
+               function
+    @param Reset the code called from the destructor, and in set before calling
+                 the Set code
+    @param Show the code for the show() method
 */
 #define IMP_RAII(Name, args, Initialize, Set, Reset, Show)                     \
   IMP_HELPER_MACRO_PUSH_WARNINGS Name() { Initialize; }                        \
+  /** explicit constructor that sets the properties of Name */                 \
   explicit Name args {                                                         \
     Initialize;                                                                \
     Set;                                                                       \
   }                                                                            \
+  /** sets the properties of the class to new ones */                          \
   void set args {                                                              \
     reset();                                                                   \
     Set;                                                                       \
   }                                                                            \
+  /** resets the properties previously set for this class */                   \
   void reset() { Reset; }                                                      \
   ~Name() { reset(); }                                                         \
   IMP_HELPER_MACRO_POP_WARNINGS IMP_SHOWABLE_INLINE(Name, out << #Name << '('; \
