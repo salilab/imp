719be5c32041fa3977a20c00d2e0d32e4ca0ef16
Dina Schneidman <duhovka@gmail.com>
2013-08-16 14:02:15 -0700

    add support to SAS based score in SOAP

---
+++ modules/score_functor/include/SASStatistical.h
@@ -0,0 +1,64 @@
+/**
+ * \file SASStatistical
+ * \brief statistical score based on solvent accessability
+ *
+ * \authors Dina Schneidman
+ * Copyright 2007-2013 IMP Inventors. All rights reserved.
+ *
+ */
+#ifndef IMPSCORE_FUNCTOR_SAS_STATISTICAL_H
+#define IMPSCORE_FUNCTOR_SAS_STATISTICAL_H
+
+#include <IMP/score_functor/score_functor_config.h>
+#include "internal/SASTable.h"
+
+IMPSCOREFUNCTOR_BEGIN_NAMESPACE
+
+/** Create a statistical potential from a file. The Key passed
+    as a template argument is used to determine how to map the name of
+    the type as described in the loaded file to table index. That is, if
+    atom::ResidueKey is passed as the Keys, the potential will expect a file
+    which has one line for each residue name.
+
+    The expected file format is:
+\verbatim
+    bin_width number [offset]
+    key_0 bin0 bin1 bin2...
+    key_1 bin0 bin1 bin2...
+\endverbatim
+
+    The order of the lines (after the first one) does not matter.
+    The bin_width is how much distance is allocated per bin (the distance used
+    is that between the points). number is the number of particle types.
+
+    \note The values read in the file are for bins. That is, the first bin
+    is from offset to offset+width. The second is offset+width to offset+
+    2width.
+    As a result, when interpolation is used, the function achieves the
+    bin value at the center of the bin.
+
+    \param[in] Key is an IMP::Key which maps between names and indices
+    \param[in] INTERPOLATE If true, even the scores without derivatives are
+    spline interpolated. If false, only the evaluation of derivatives is
+    interpolated with a spline.
+*/
+template <class Key, bool INTERPOLATE>
+class SASStatistical {
+public:
+  SASStatistical(IntKey k, base::TextInput data_file) : key_(k) {
+    table_.template initialize<Key>(data_file);
+  }
+
+  double get_score(Model *m, const ParticleIndex pi, double area) const {
+    int table_index = m->get_attribute(key_, pi);
+    if(table_index == -1) return 0;
+    return table_.get_score(table_index, area);
+  }
+private:
+  internal::SASTable<INTERPOLATE> table_;
+  IntKey key_;
+};
+
+IMPSCOREFUNCTOR_END_NAMESPACE
+
+#endif /* IMPSCORE_FUNCTOR_SAS_STATISTICAL_H */
---
+++ modules/score_functor/include/internal/SASTable.h
@@ -0,0 +1,120 @@
+/**
+ * \file SASTable.h
+ * \brief Storage of Solvent Accessability Statistical potential
+ *
+ * \authors Dina Schneidman
+ * Copyright 2007-2013 IMP Inventors. All rights reserved.
+ *
+ */
+
+#ifndef IMPSCORE_FUNCTOR_SAS_TABLE_H
+#define IMPSCORE_FUNCTOR_SAS_TABLE_H
+
+#include <IMP/score_functor/score_functor_config.h>
+#include "RawOpenCubicSpline.h"
+
+#include <IMP/base/exception.h>
+#include <IMP/file.h>
+
+#include <vector>
+
+IMPSCOREFUNCTOR_BEGIN_INTERNAL_NAMESPACE
+
+template <bool INTERPOLATE>
+class SASTable {
+public:
+  SASTable() {}
+
+  template <class Key>
+  void initialize(base::TextInput tin) {
+    std::istream &in = tin;
+
+    // read header line
+    double bin;
+    std::string line;
+    std::getline(in, line);
+    std::istringstream iss(line);
+    iss >> bin;
+    if(!iss) IMP_THROW("Error reading bin size from line " << line,IOException);
+    unsigned int np;
+    iss >> np;
+    if(!iss) IMP_THROW("Error number of types from line " << line, IOException);
+    iss >> offset_;
+    if(!iss) offset_ = 0;
+    IMP_LOG_TERSE("Reading " << np << " from " << tin.get_name() << std::endl);
+
+    // init table parameters
+    bin_width_ = bin;
+    inverse_bin_width_ = 1.0 / bin;
+    data_.resize(np);
+
+    // read the table
+    int bins_read = -1;
+    unsigned int read_entries = 0;
+    while (true) {
+      std::string line;
+      std::getline(in, line);
+      if (!in) break;
+      std::istringstream ins(line);
+      std::string pname;
+      ins >> pname;
+      unsigned int i = Key(pname).get_index();
+      IMP_INTERNAL_CHECK(Key(pname) == Key(i),
+                         "Expected and found protein types don't match: "
+                         << "expected \"" << Key(i).get_string() << " got "
+                         << pname << " at " << i << std::endl);
+      int cur_bins_read = 0;
+      Floats data;
+      while(true) {
+        double potentialvalue;
+        ins >> potentialvalue;
+        if(ins) {
+          data.push_back(potentialvalue);
+          ++cur_bins_read;
+        } else {
+          break;
+        }
+      }
+
+      data_[read_entries] =
+        RawOpenCubicSpline(data, bin_width_, inverse_bin_width_);
+
+      if (bins_read != -1 && cur_bins_read != bins_read) {
+        IMP_THROW("Read wrong number of bins from line: "
+                  << line << "\nExpected " << bins_read << " got "
+                  << cur_bins_read, IOException);
+      }
+      bins_read = cur_bins_read;
+      ins.clear();
+      ++read_entries;
+    }
+    max_ = bin_width_ * bins_read;
+
+    IMP_LOG_TERSE("PMF table entries have " << bins_read << " bins with width "
+                                            << bin_width_ << std::endl);
+  }
+
+  double get_score(unsigned int i, double area) const {
+    if(area >= max_ || area < offset_) return 0;
+    if(INTERPOLATE) {
+      return data_[i].evaluate(area - .5 * bin_width_ - offset_, bin_width_,
+                               inverse_bin_width_);
+    } else {
+      return data_[i].get_bin(area - offset_, bin_width_, inverse_bin_width_);
+    }
+  }
+
+  double get_max() const { return max_; }
+
+
+ private:
+  double inverse_bin_width_;
+  double bin_width_;
+  double max_;
+  double offset_;
+  std::vector<RawOpenCubicSpline> data_;
+};
+
+IMPSCOREFUNCTOR_END_INTERNAL_NAMESPACE
+
+#endif /* IMPSCORE_FUNCTOR_SAS_TABLE_H */
