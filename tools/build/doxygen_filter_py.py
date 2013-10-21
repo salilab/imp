5de6af2cfc47d997335248f41093f32285b672c7
Ben Webb <ben@salilab.org>
2013-10-21 12:53:27 -0700

    Ignore non-IMP code.

--- tools/build/doxygen_filter_py.py
+++ tools/build/doxygen_filter_py.py
@@ -164,7 +164,7 @@ class OutputPrinter(object):
 
 def parse_file(fname):
     # Exclude non-IMP code
-    if '_compat_python' in fname:
+    if '_compat_python' in fname or 'compat_subprocess' in fname:
         return
     # Don't try to document applications (which get symlinked into bin dir)
     if '/bin/' in fname:
