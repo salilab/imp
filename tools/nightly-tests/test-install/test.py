a637aac5b6cf741adf46e2a7e9c2e6a4a803171f
Ben Webb <ben@salilab.org>
2013-10-29 12:54:44 -0700

    Check for installed applications.

--- tools/nightly-tests/test-install/test.py
+++ tools/nightly-tests/test-install/test.py
@@ -5,6 +5,7 @@ import IMP.atom
 import IMP.restrainer
 import IMP.container
 import unittest
+import subprocess
 import os
 
 class IMPInstallTests(unittest.TestCase):
@@ -31,6 +32,23 @@ class IMPInstallTests(unittest.TestCase):
         """Check install of example files"""
         d = IMP.restrainer.get_example_path("em_restraint.py")
         self.assert_(os.path.exists(d))
+
+    def test_applications_installed(self):
+        """Check install of basic applications"""
+        apps = ['cnmultifit.py', 'foxs', 'idock.py', 'ligand_score',
+                'multifit.py', 'pdb_check', 'pdb_rmf', 'rmf_cat',
+                'saxs_merge.py']
+        for app in apps:
+            try:
+                p = subprocess.Popen([app, '--help'], stdout=subprocess.PIPE,
+                                     stderr=subprocess.STDOUT)
+            except OSError:
+                raise OSError("Could not run %s" % app)
+            out = p.stdout.read()
+            ret = p.wait()
+            self.assert_(ret == 1 or ret == 0,
+                         "Return code for %s app is %d, not 0 or 1; " \
+                         "output is %s" % (app, ret, out))
 
 if __name__ == '__main__':
     # Note we use unittest rather than IMP.test, since the latter requires
--- tools/nightly-tests/test-install/test_mock.py
+++ tools/nightly-tests/test-install/test_mock.py
@@ -1,5 +1,6 @@
 import unittest
 import os
+import subprocess
 
 mock_config = os.environ['MOCK_CONFIG']
 
@@ -36,6 +37,45 @@ class IMPMockTests(unittest.TestCase):
         import IMP.statistics
         import IMP.symmetry
         import IMP.test
+
+    def test_applications_installed(self):
+        """Check install of a fairly comprehensive list of applications"""
+        emagefit_apps = ['convert_spider_to_jpg.py', 'emagefit_cluster.py',
+                         'emagefit_dock.py', 'emagefit.py', 'emagefit_score.py']
+        apps = ['cluster_profiles', 'cnmultifit.py', 'combine_scores',
+                'complex_to_anchor_graph.py', 'compute_chi',
+                'cross_links_score',
+                'cross_links_single_score', 'em2d_score', 'em2d_single_score',
+                'em3d_score', 'em3d_single_score',
+                'estimate_threshold_from_molecular_mass.py', 'foxs',
+                'idock.py', 'imp_example_app', 'interface_cross_links',
+                'interface_rtc', 'ligand_score', 'map2pca.py', 'mol2pca.py',
+                'multifit.py', 'nmr_rtc_score', 'pdb_check', 'pdb_rmf',
+                'recompute_zscore', 'resample_density.py', 'rg',
+                'rmf_avro_dump', 'rmf_cat', 'rmf_color', 'rmf_display',
+                'rmf_frames', 'rmf_info', 'rmf_interpolate', 'rmf_pdb',
+                'rmf_show', 'rmf_slice', 'rmf_transform', 'rmf_update',
+                'rmf_validate', 'rmf_xml', 'saxs_merge.py', 'saxs_score',
+                'simulate_density_from_pdb.py', 'soap_score',
+                'soap_single_score', 'validate_profile',
+                'view_density_header.py' ]
+        # RHEL systems don't include EMageFit; Fedora does
+        if mock_config.startswith('fedora'):
+            apps.extend(emagefit_apps)
+        else:
+            for app in emagefit_apps:
+                self.assertRaises(OSError, subprocess.call, app)
+        for app in apps:
+            try:
+                p = subprocess.Popen([app, '--help'], stdout=subprocess.PIPE,
+                                     stderr=subprocess.STDOUT)
+            except OSError:
+                raise OSError("Could not run %s" % app)
+            out = p.stdout.read()
+            ret = p.wait()
+            self.assert_(ret == 1 or ret == 0,
+                         "Return code for %s app is %d, not 0 or 1; " \
+                         "output is %s" % (app, ret, out))
 
 if __name__ == '__main__':
     # Note we use unittest rather than IMP.test, since the latter requires
