dfd8f8a3d07a9d3c3c021a27d8f979dfc576bd3d
Daniel Russel <drussel@gmail.com>
2013-11-01 10:28:15 -0700

    clean up dependency info files
    so stay ones can't make config work when it shouldn't

--- tools/build/clean_build_dir.py
+++ tools/build/clean_build_dir.py
@@ -22,9 +22,14 @@ def declare_container():
     for f in glob.glob(os.path.join("include", "IMP", "kernel", "declare_*Container.h")):
         os.unlink(f)
 
+def build_info():
+    for f in glob.glob(os.path.join("data", "build_info", "*")):
+        os.unlink(f)
+
 def main():
     check_config()
     declare_container()
+    build_info()
 
 if __name__ == '__main__':
     main()
