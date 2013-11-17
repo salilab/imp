3bfa04a346077b101f3ec5121e4ede678202772d
Ben Webb <ben@salilab.org>
2013-11-17 10:07:10 -0800

    Add namespaces; use new Restraint constructor.

--- tools/nightly-tests/test-install/test.cpp
+++ tools/nightly-tests/test-install/test.cpp
@@ -5,23 +5,21 @@
 #include <IMP.h>
 #include <IMP/core.h>
 #include <IMP/example/ExampleRestraint.h>
-#include <IMP/container/ListPairContainer.h>
 
 using namespace IMP;
 int main(){
-  set_log_level(VERBOSE);
-  WeakPointer<Model> m(new kernel::Model());
+  base::set_log_level(base::VERBOSE);
+  base::WeakPointer<kernel::Model> m(new kernel::Model());
   kernel::Particle* p0= new kernel::Particle(m);
   core::XYZ d0= core::XYZ::setup_particle(p0);
   kernel::Particle* p1= new kernel::Particle(m);
   core::XYZ d1= core::XYZ::setup_particle(p1);
-  example::ExampleRestraint *r0= new example::ExampleRestraint(p0, 1);
-  example::ExampleRestraint *r1= new example::ExampleRestraint(p1, 1);
+  example::ExampleRestraint *r0= new example::ExampleRestraint(m,
+                                                           p0->get_index(), 1);
+  example::ExampleRestraint *r1= new example::ExampleRestraint(m,
+                                                           p1->get_index(), 1);
   d0.set_coordinates(algebra::VectorD<3>(0,0,2));
   d1.set_coordinates(algebra::VectorD<3>(0,0,3));
-
-  m->add_restraint(r0);
-  m->add_restraint(r1);
 
   return 0;
 }
