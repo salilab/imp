3d207241539da073888008a0baaab42fea64ddf8
Daniel Russel <drussel@gmail.com>
2013-12-04 08:09:59 -0800

    increase tolerance in connolly test

--- modules/algebra/test/test_connolly.py
+++ modules/algebra/test/test_connolly.py
@@ -5,15 +5,17 @@ import IMP.core
 import IMP.algebra
 import os
 
+
 class Tests(IMP.test.TestCase):
+
     def test_single_atom(self):
         """Test generation of Connolly surface around a single atom"""
         m = IMP.kernel.Model()
         p = IMP.kernel.Particle(m)
         res = IMP.atom.Residue.setup_particle(p, IMP.atom.ALA)
         p = IMP.kernel.Particle(m)
-        center = IMP.algebra.Vector3D(10.,10.,10.)
-        sphere = IMP.algebra.Sphere3D( center, 2.265)
+        center = IMP.algebra.Vector3D(10., 10., 10.)
+        sphere = IMP.algebra.Sphere3D(center, 2.265)
         pts = IMP.algebra.get_connolly_surface([sphere], 5.0, 1.8)
         total_area = 0.
         for sp in pts:
@@ -23,8 +25,9 @@ class Tests(IMP.test.TestCase):
             self.assertEqual(sp.atom_2, -1)
             total_area += sp.area
             # Surface points should all lie on the surface of the atom
-            self.assertAlmostEqual(IMP.algebra.get_distance(sp.surface_point, center),
-                                   2.265, delta=1e-2)
+            self.assertAlmostEqual(
+                IMP.algebra.get_distance(sp.surface_point, center),
+                2.265, delta=1e-2)
             # Surface normal should be a unit vector
             self.assertAlmostEqual(sp.normal.get_squared_magnitude(), 1.0,
                                    delta=1e-2)
@@ -40,7 +43,7 @@ class Tests(IMP.test.TestCase):
         sps = IMP.algebra.get_connolly_surface(spheres, 5, 1.8)
 
         sps_area = sum([s.area for s in sps])
-        self.assertAlmostEqual(sps_area, 5477.4, delta = .1)
+        self.assertAlmostEqual(sps_area, 5477.4, delta=.2)
 
 if __name__ == '__main__':
     IMP.test.main()
