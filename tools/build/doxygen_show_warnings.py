7a6788cb99824233398da22b8ac7cf432ec6984b
Daniel Russel <drussel@gmail.com>
2013-12-05 21:54:08 -0800

    add manual filtering of doxygen warnings
    close #561

--- tools/build/doxygen_show_warnings.py
+++ tools/build/doxygen_show_warnings.py
@@ -6,11 +6,22 @@ parser = OptionParser()
 parser.add_option("-w", "--warnings", dest="warnings",
                   help="The warnings file.")
 
+suppress_strings = ["not generated, too many nodes",
+                    "introduction_values",
+                    "impligand_score",
+                    "impmultifit_tools"]
+
+
 def main():
     (options, args) = parser.parse_args()
     input = open(options.warnings, "r").readlines()
     for l in input:
-        if l.find("not generated, too many nodes.") == -1:
+        found = False
+        for x in suppress_strings:
+            if l.find(x) != -1:
+                found = True
+                break
+        if not found:
             print l
 
 
