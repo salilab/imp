9a1dcdfcfa22bb5ed24f43e1680f7c383d926d9e
Daniel Russel <drussel@gmail.com>
2014-01-10 17:53:50 -0800

    add link to has in bump rmf

--- tools/maintenance/bump_rmf.sh
+++ tools/maintenance/bump_rmf.sh
@@ -4,6 +4,7 @@ pushd modules/rmf/dependency/RMF_source
 git checkout develop
 git pull origin develop
 git submodule update
+set hash=`git rev-parse HEAD`
 popd
 echo "Committing"
-git commit  modules/rmf/dependency/RMF_source -m "bump RMF" --no-verify
+git commit  modules/rmf/dependency/RMF_source -m "bump RMF to salilab/rmf@$hash" --no-verify
