b6b0c83c2f77f06afcd7c323e020bb2faefe9b23
Daniel Russel <drussel@gmail.com>
2014-01-22 14:53:47 -0800

    mark benchmark flags as advanced

--- modules/benchmark/include/internal/flags.h
+++ modules/benchmark/include/internal/flags.h
@@ -10,22 +10,23 @@
 #define IMPBENCHMARK_FLAGS_H
 
 #include <IMP/benchmark/benchmark_config.h>
+#include <IMP/base/Flag.h>
 #include <boost/cstdint.hpp>
 
 IMPBENCHMARK_BEGIN_INTERNAL_NAMESPACE
 
-extern IMPBENCHMARKEXPORT boost::int64_t run_only;
+extern IMPBENCHMARKEXPORT base::AdvancedFlag<int> run_only;
 
 #if IMP_BASE_HAS_GPERFTOOLS
-extern IMPBENCHMARKEXPORT bool cpu_profile_benchmarks;
+extern IMPBENCHMARKEXPORT base::AdvancedFlag<bool> cpu_profile_benchmarks;
 #endif
 
 #if IMP_BASE_HAS_TCMALLOC_HEAPPROFILER
-extern IMPBENCHMARKEXPORT bool heap_profile_benchmarks;
+extern IMPBENCHMARKEXPORT base::AdvancedFlag<bool> heap_profile_benchmarks;
 #endif
 
 #if IMP_BASE_HAS_TCMALLOC_HEAPCHECKER
-extern IMPBENCHMARKEXPORT bool heap_check_benchmarks;
+extern IMPBENCHMARKEXPORT base::AdvancedFlag<bool> heap_check_benchmarks;
 #endif
 
 IMPBENCHMARK_END_INTERNAL_NAMESPACE
