98d21915a43606ba7a6ff4ace71ff78d233f1811
Daniel Russel <drussel@gmail.com>
2014-01-29 08:20:51 -0800

    fix IMP.rmf compilation

--- modules/rmf/include/internal/atom_links_coordinate_helpers.h
+++ modules/rmf/include/internal/atom_links_coordinate_helpers.h
@@ -16,31 +16,20 @@
 #include <IMP/algebra/Transformation3D.h>
 IMPRMF_BEGIN_INTERNAL_NAMESPACE
 
-enum NodeCoords {
-  STATIC_RB = 1,
-  FRAME_RB = 2,
-  STATIC_XYZ = 4,
-  FRAME_XYZ = 8,
-  EXTERNAL_RB = 16
-};
-
 inline void copy_to_frame_reference_frame(
     const algebra::Transformation3D &tr, RMF::NodeHandle n,
     RMF::decorator::ReferenceFrameFactory rff) {
   RMF::decorator::ReferenceFrame rf = rff.get(n);
   algebra::Vector3D translation = tr.get_translation();
   algebra::Vector4D quaternion = tr.get_rotation().get_quaternion();
-  rf.set_frame_translation(RMF::Vector3(translation.coordinates_begin(),
-                                        translation.coordinates_end()));
-  rf.set_frame_rotation(RMF::Vector4(quaternion.coordinates_begin(),
-                                     quaternion.coordinates_end()));
+  rf.set_frame_translation(RMF::Vector3(translation));
+  rf.set_frame_rotation(RMF::Vector4(quaternion));
 }
 inline void copy_to_frame_particle(
     const algebra::Vector3D &v, RMF::NodeHandle n,
     RMF::decorator::IntermediateParticleFactory ipf) {
   RMF::decorator::IntermediateParticle ip = ipf.get(n);
-  ip.set_frame_coordinates(
-      RMF::Floats(v.coordinates_begin(), v.coordinates_end()));
+  ip.set_frame_coordinates(RMF::Floats(v));
 }
 inline void copy_to_static_reference_frame(
     const algebra::Transformation3D &tr, RMF::NodeHandle n,
@@ -48,17 +37,14 @@ inline void copy_to_static_reference_frame(
   RMF::decorator::ReferenceFrame rf = rff.get(n);
   algebra::Vector3D translation = tr.get_translation();
   algebra::Vector4D quaternion = tr.get_rotation().get_quaternion();
-  rf.set_static_translation(RMF::Vector3(translation.coordinates_begin(),
-                                         translation.coordinates_end()));
-  rf.set_static_rotation(RMF::Vector4(quaternion.coordinates_begin(),
-                                      quaternion.coordinates_end()));
+  rf.set_static_translation(RMF::Vector3(translation));
+  rf.set_static_rotation(RMF::Vector4(quaternion));
 }
 inline void copy_to_static_particle(
     const algebra::Vector3D &v, RMF::NodeHandle n,
     RMF::decorator::IntermediateParticleFactory ipf) {
   RMF::decorator::IntermediateParticle ip = ipf.get(n);
-  ip.set_static_coordinates(
-      RMF::Floats(v.coordinates_begin(), v.coordinates_end()));
+  ip.set_static_coordinates(RMF::Floats(v));
 }
 inline algebra::Vector3D get_coordinates(
     RMF::NodeConstHandle n,
@@ -70,9 +56,8 @@ inline algebra::Transformation3D get_transformation(
     RMF::NodeConstHandle n, RMF::decorator::ReferenceFrameFactory rfcf) {
   RMF::Vector3 coords = rfcf.get(n).get_translation();
   RMF::Vector4 quat = rfcf.get(n).get_rotation();
-  return algebra::Transformation3D(
-      algebra::Rotation3D(algebra::Vector4D(quat.begin(), quat.end())),
-      algebra::Vector3D(coords.begin(), coords.end()));
+  return algebra::Transformation3D(algebra::Rotation3D(algebra::Vector4D(quat)),
+                                   algebra::Vector3D(coords));
 }
 
 IMPRMF_END_INTERNAL_NAMESPACE
