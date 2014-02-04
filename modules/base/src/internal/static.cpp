93a7ef2aa736a1bf245fd34d6841893c471387ff
Daniel Russel <drussel@gmail.com>
2014-02-04 14:04:16 -0800

    have log level be silent by default

--- modules/base/src/internal/static.cpp
+++ modules/base/src/internal/static.cpp
@@ -181,7 +181,7 @@ Flag<LogLevel, IMP_HAS_LOG != IMP_SILENT> log_level("log_level",
                                                     "\"SILENT\", \"WARNING\", "
                                                     "\"PROGRESS\", \"TERSE\", "
                                                     "\"VERBOSE\"",
-                                                    LogLevel(IMP_HAS_LOG));
+                                                    LogLevel(SILENT));
 
 AdvancedFlag<StatisticsLevel> stats_level(
     "statistics_level",
