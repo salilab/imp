e6354b3bde5e98a2159b38dcc2ec44534af4be1e
Daniel Russel <drussel@gmail.com>
2014-02-07 15:53:55 -0800

    fix bad cleanup

--- tools/build/config_templates/src.cpp
+++ tools/build/config_templates/src.cpp
@@ -9,15 +9,17 @@
 #include <%(filename)s>
 #include <IMP/base/internal/directories.h>
 
-% (cppprefix)s_BEGIN_NAMESPACE std::string get_module_version() {
+%(cppprefix)s_BEGIN_NAMESPACE
+
+std::string get_module_version() {
   static std::string version("%(version)s");
   return version;
 }
 std::string get_data_path(std::string file_name) {
   return IMP::base::internal::get_data_path("%(name)s", file_name);
 }
-std::string get_example_path(std::string file_name) {
+std::string get_example_path(std::string file_name)  {
   return IMP::base::internal::get_example_path("%(name)s", file_name);
 }
 
-% (cppprefix)s_END_NAMESPACE
+%(cppprefix)s_END_NAMESPACE
