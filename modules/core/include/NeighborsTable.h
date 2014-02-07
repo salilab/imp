85b42a78459645a0811ea48a65ee87fde3201752
Daniel Russel <drussel@gmail.com>
2014-02-07 12:02:17 -0800

    another #include issue

--- modules/core/include/NeighborsTable.h
+++ modules/core/include/NeighborsTable.h
@@ -14,6 +14,7 @@
 #include <IMP/kernel/SingletonContainer.h>
 #include <IMP/kernel/ScoreState.h>
 #include <IMP/base/Pointer.h>
+#include <boost/unordered_map.hpp>
 
 IMPCORE_BEGIN_NAMESPACE
 
@@ -28,7 +29,7 @@ IMPCORE_BEGIN_NAMESPACE
 class IMPCOREEXPORT NeighborsTable : public kernel::ScoreState {
   base::PointerMember<kernel::PairContainer> input_;
   std::size_t input_version_;
-  base::map<kernel::ParticleIndex, kernel::ParticleIndexes> data_;
+  boost::unordered_map<kernel::ParticleIndex, kernel::ParticleIndexes> data_;
 
  protected:
   virtual kernel::ModelObjectsTemp do_get_inputs() const IMP_OVERRIDE {
