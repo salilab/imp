10403a60d7fc98625def112e271f045ff6a84d5b
Daniel Russel <drussel@gmail.com>
2014-02-07 11:29:09 -0800

    move the test to C++
    relates #734

---
+++ modules/display/test/test_isosurface.cpp
@@ -0,0 +1,25 @@
+#include "IMP/display/display_config.h"
+#include "IMP/display/PymolWriter.h"
+#include "IMP/display/geometry.h"
+#include "IMP/algebra/standard_grids.h"
+
+int main(int, char*[]) {
+#if IMP_DISPLAY_HAS_IMP_CGAL
+  typedef IMP::algebra::DenseGrid3D<double> G;
+  G g(2.5, IMP::algebra::BoundingBox3D(IMP::algebra::Vector3D(-10, -10, -10),
+                                       IMP::algebra::Vector3D(10, 10, 10)));
+  IMP_FOREACH(G::Index i, g.get_all_indexes()) {
+    double m= g.get_center(i).get_magnitude();
+    g[i] = 100 - m;
+  }
+  IMP_NEW(IMP::display::IsosurfaceGeometry, gg, (g, 95.0));
+  std::string name = IMP::base::create_temporary_file_name("iso", ".pym");
+  std::cout << "Writing " << name << std::endl;
+  IMP_NEW(IMP::display::PymolWriter, w, (name));
+  w->add_geometry(gg);
+  return 0;
+#else
+  std::cerr << "No IMP.cgal" << std::endl;
+  return 0;
+#endif
+}
