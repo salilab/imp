47e1cd625df5db0044176dcd966f3768133618fe
Daniel Russel <drussel@gmail.com>
2014-02-07 16:05:41 -0800

    don't use --rebase in setup_git.py
    older gits seem to get confused by it

--- setup_git.py
+++ setup_git.py
@@ -1,6 +1,8 @@
 #! /usr/bin/env python
+"""Update submodules and then call the main setup_git.py. This should be
+copied to the main directory of your project and named setup_git.py."""
+
 import os
 import os.path
-os.system("git submodule update --init --recursive --rebase")
-os.system("git submodule update --recursive --rebase")
-os.system(os.path.join("tools", "dev_tools", "git", "setup_git.py"))
+os.system("git submodule update --init --recursive")
+os.system(os.path.join("tools", "dev_tools", "git", "setup-git.py"))
