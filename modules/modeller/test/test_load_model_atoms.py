39efb0dcf2d075db460ca97e4c5d723dcfca7552
Ben Webb <ben@salilab.org>
2014-03-17 17:48:02 -0700

    Speed up test.

--- modules/modeller/test/test_load_model_atoms.py
+++ modules/modeller/test/test_load_model_atoms.py
@@ -60,13 +60,12 @@ class Tests(IMP.test.TestCase):
                              "too many residue types")
             self.assertEqual(i_num_atom_type, f_num_atom_type,
                              "too many atom types")
-            self.assertEqual(1377, len(desc),
-                             "Wrong number of particles created")
-            rd = IMP.atom.Residue(IMP.atom.get_residue(mp, 29).get_particle())
+            self.assertEqual(46, len(desc))
+            rd = IMP.atom.Residue(IMP.atom.get_residue(mp, 2).get_particle())
             at = IMP.atom.get_atom(rd, IMP.atom.AtomType("C"))
             self.assertEqual(IMP.atom.get_residue(at).get_index(),
                              rd.get_index())
-            self.assertEqual(rd.get_index(), 29)
+            self.assertEqual(rd.get_index(), 2)
             at = IMP.atom.get_atom(rd, IMP.atom.AtomType("CA"))
             self.assertAlmostEqual(IMP.atom.Charged(at).get_charge(),
                                    0.16, delta=1e-5)
@@ -80,16 +79,15 @@ class Tests(IMP.test.TestCase):
         loader = IMP.modeller.ModelLoader(modmodel)
         mp = loader.load_atoms(m)
         all_atoms = IMP.atom.get_by_type(mp, IMP.atom.ATOM_TYPE)
-        self.assertEqual(1221, len(all_atoms),
-                         "Wrong number of atoms found in protein")
+        self.assertEqual(39, len(all_atoms))
         bonds = list(loader.load_bonds())
-        self.assertEqual(len(bonds), 1248)
+        self.assertEqual(len(bonds), 38)
         angles = list(loader.load_angles())
-        self.assertEqual(len(angles), 1677)
+        self.assertEqual(len(angles), 50)
         dihedrals = list(loader.load_dihedrals())
-        self.assertEqual(len(dihedrals), 1973)
+        self.assertEqual(len(dihedrals), 55)
         impropers = list(loader.load_impropers())
-        self.assertEqual(len(impropers), 520)
+        self.assertEqual(len(impropers), 13)
 
     def test_dna(self):
         """Check reading a dna with one chain"""
@@ -110,8 +108,7 @@ class Tests(IMP.test.TestCase):
         mpc = mp.get_child(0)
         self.assertEqual(mpc.get_parent(), mp,
                          "Should not have a parent")
-        self.assertEqual(3160, len(desc),
-                         "Wrong number of particles created")
+        self.assertEqual(84, len(desc))
 
 if __name__ == '__main__':
     IMP.test.main()
