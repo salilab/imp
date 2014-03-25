4111e17b121872f260be57615844c4535bf4c9a5
Ben Webb <ben@salilab.org>
2014-03-25 13:25:00 -0700

    Speed up test and remove multifit dependence.

--- modules/em/test/expensive_test_local_rigid_fitting.py
+++ modules/em/test/expensive_test_local_rigid_fitting.py
@@ -1,28 +1,30 @@
 import IMP.em
+import IMP.test
 import IMP.core
-import IMP.multifit
 
-resolution = 15.
-dmap = IMP.em.read_map(
-    IMP.multifit.get_example_path('3sfd/3sfd_15.mrc'),
-    IMP.em.MRCReaderWriter())
-dmap.get_header_writable().set_resolution(resolution)
+class Tests(IMP.test.TestCase):
+    def test_local_rigid_fitting(self):
+        """Test local_rigid_fitting()"""
+        resolution = 8.
+        dmap = IMP.em.read_map(self.get_input_file_name('3points.mrc'),
+                               IMP.em.MRCReaderWriter())
+        dmap.get_header_writable().set_resolution(resolution)
+        
+        m = IMP.kernel.Model()
+        
+        sel = IMP.atom.CAlphaPDBSelector()
+        mh = IMP.atom.read_pdb(self.get_input_file_name('3points.pdb'), m, sel)
+        IMP.atom.add_radii(mh)
+        ps = IMP.core.get_leaves(mh)
+        IMP.atom.create_rigid_body(mh)
+        
+        refiner = IMP.core.LeavesRefiner(IMP.atom.Hierarchy.get_traits())
+        
+        fitting_sols = IMP.em.local_rigid_fitting(
+            mh.get_particle(), refiner,
+            IMP.atom.Mass.get_mass_key(),
+            dmap, [], 1, 10, 100)
+        self.assertEqual(fitting_sols.get_number_of_solutions(), 1)
 
-m = IMP.kernel.Model()
-
-sel = IMP.atom.CAlphaPDBSelector()
-mh = IMP.atom.read_pdb(
-    IMP.multifit.get_example_path('3sfd/3sfd.A.pdb'),
-    m,
-    sel)
-IMP.atom.add_radii(mh)
-ps = IMP.core.get_leaves(mh)
-IMP.atom.create_rigid_body(mh)
-
-refiner = IMP.core.LeavesRefiner(IMP.atom.Hierarchy.get_traits())
-
-
-fitting_sols = IMP.em.local_rigid_fitting(
-    mh.get_particle(), refiner,
-    IMP.atom.Mass.get_mass_key(),
-    dmap, [], 20, 10, 100)
+if __name__ == '__main__':
+    IMP.test.main()
