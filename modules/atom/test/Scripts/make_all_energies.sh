2791c490fc8be627c92a3d5a24c53c5fe97389bf
Barak Raveh <barak@salilab.org>
2014-04-16 12:24:28 -0700

    minor changes to analysis script for multi tamd

--- modules/atom/test/Scripts/make_all_energies.sh
+++ modules/atom/test/Scripts/make_all_energies.sh
@@ -1,5 +1,6 @@
-foreach i ( 1 2 3 4 5 6 7 8) ;
+#!/usr/bin/tcsh
+foreach i ( 1 2 3 4 5 6 7 8 9 10 11 12) ;
   bash make_energy.sh LOGno$i > ENERGYno$i
   bash make_energy.sh LOGyes$i > ENERGYyes$i
 end
-#gnuplot compare.gnu -p
+gnuplot compare.gnu -p
