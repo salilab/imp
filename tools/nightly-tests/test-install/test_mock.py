3f32586ad8b91820a3015023a7cee5edaa34da20
Ben Webb <ben@salilab.org>
2014-05-07 08:31:06 -0700

    Exclude all idock binaries on RHEL.

--- tools/nightly-tests/test-install/test_mock.py
+++ tools/nightly-tests/test-install/test_mock.py
@@ -41,30 +41,31 @@ class IMPMockTests(unittest.TestCase):
     def test_applications_installed(self):
         """Check install of a fairly comprehensive list of applications"""
         emagefit_apps = ['convert_spider_to_jpg.py', 'emagefit_cluster.py',
-                         'emagefit_dock.py', 'emagefit.py', 'emagefit_score.py',
-                         'idock.py']
-        apps = ['cluster_profiles', 'cnmultifit.py', 'combine_scores',
+                         'emagefit_dock.py', 'emagefit.py', 'emagefit_score.py']
+        idock_apps = ['idock.py', 'combine_scores', 'recompute_zscore',
+                      'cross_links_score', 'cross_links_single_score',
+                      'em2d_score', 'em2d_single_score',
+                      'interface_cross_links', 'em3d_score',
+                      'em3d_single_score', 'saxs_score', 'interface_rtc',
+                      'nmr_rtc_score', 'soap_score']
+        apps = ['cluster_profiles', 'cnmultifit.py',
                 'complex_to_anchor_graph.py', 'compute_chi',
-                'cross_links_score',
-                'cross_links_single_score', 'em2d_score', 'em2d_single_score',
-                'em3d_score', 'em3d_single_score',
                 'estimate_threshold_from_molecular_mass.py', 'foxs',
-                'imp_example_app', 'interface_cross_links',
-                'interface_rtc', 'ligand_score', 'map2pca.py', 'mol2pca.py',
-                'multifit.py', 'nmr_rtc_score', 'pdb_check', 'pdb_rmf',
-                'recompute_zscore', 'resample_density.py', 'rg',
-                'rmf3_dump', 'rmf_cat', 'rmf_display',
+                'imp_example_app', 'ligand_score', 'map2pca.py', 'mol2pca.py',
+                'multifit.py', 'pdb_check', 'pdb_rmf', 'resample_density.py',
+                'rg', 'rmf3_dump', 'rmf_cat', 'rmf_display',
                 'rmf_frames', 'rmf_info', 'rmf_interpolate', 'rmf_pdb',
                 'rmf_show', 'rmf_signature', 'rmf_simplify', 'rmf_slice',
                 'rmf_transform', 'rmf_update',
-                'rmf_validate', 'rmf_xml', 'saxs_merge.py', 'saxs_score',
-                'simulate_density_from_pdb.py', 'soap_score',
+                'rmf_validate', 'rmf_xml', 'saxs_merge.py',
+                'simulate_density_from_pdb.py',
                 'validate_profile', 'view_density_header.py']
         # RHEL systems don't include EMageFit and idock; Fedora does
         if mock_config.startswith('fedora'):
             apps.extend(emagefit_apps)
+            apps.extend(idock_apps)
         else:
-            for app in emagefit_apps:
+            for app in emagefit_apps + idock_apps:
                 self.assertRaises(OSError, subprocess.call, app)
         for app in apps:
             try:
