5d27571e2c60471c027a3600b49c36acf955e71b
Ben Webb <ben@salilab.org>
2014-05-29 08:06:58 -0700

    Update for new HDF5.

--- tools/mac/make-package.sh
+++ tools/mac/make-package.sh
@@ -139,7 +139,7 @@ if [ "${TARGET_OSX_VER}" = "10.6" ]; then
                 /usr/local/lib/libfftw3.3.dylib \
                 /usr/local/lib/libgsl.0.dylib \
                 /usr/local/lib/libgslcblas.0.dylib \
-                /usr/local/lib/libhdf5.7.dylib \
+                /usr/local/lib/libhdf5.8.dylib \
                 /usr/local/lib/libopencv_highgui.2.4.2.dylib \
                 /usr/local/lib/libopencv_highgui.2.4.dylib \
                 /usr/local/lib/libopencv_core.2.4.2.dylib \
