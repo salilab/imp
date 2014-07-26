7086c91da8a53ed49edea3340c9244008c08ef41
cgreenberg <cgreen@salilab.org>
2014-07-25 17:07:56 -0700

    adding rmf test for custom resolutions

--- modules/rmf/test/test_representations.py
+++ modules/rmf/test/test_representations.py
@@ -19,6 +19,18 @@ class Tests(IMP.test.TestCase):
             rep.add_representation(simp, IMP.atom.BALLS)
         return full
 
+    def _create_custom(self, m):
+        pdbname = self.get_input_file_name("simple.pdb")
+        full = IMP.atom.read_pdb(pdbname, m)
+        chain = IMP.atom.get_by_type(full, IMP.atom.CHAIN_TYPE)[0]
+        rep = IMP.atom.Representation.setup_particle(full,0)
+        for i in range(1, 3):
+            simp = IMP.atom.create_simplified_along_backbone(chain, i)
+            simp.set_name("A " + str(i))
+            rep.add_representation(simp, IMP.atom.BALLS,i+1)
+        return full
+
+
     def test_0(self):
         """Test writing mult resolutions"""
         m = IMP.kernel.Model()
@@ -42,6 +54,31 @@ class Tests(IMP.test.TestCase):
         print back_resolutions
         for p in zip(resolutions, back_resolutions):
             self.assertAlmostEqual(p[0], p[1], delta=.1)
+
+    def test_custom_resolutions(self):
+        """Test writing RMF file w/ explicit resolutions is read correctly"""
+        m = IMP.kernel.Model()
+        h = self._create_custom(m)
+        IMP.atom.show_molecular_hierarchy(h)
+        resolutions = IMP.atom.Representation(h).get_resolutions()
+        rmfname = self.get_tmp_file_name("multires.rmfz")
+        fh = RMF.create_rmf_file(rmfname)
+        IMP.rmf.add_hierarchy(fh, h)
+        IMP.rmf.save_frame(fh, "frame")
+        del fh
+
+        fh = RMF.open_rmf_file_read_only(rmfname)
+        RMF.show_hierarchy(fh.get_root_node())
+        IMP.rmf.link_hierarchies(fh, [h])
+        del fh
+        fh = RMF.open_rmf_file_read_only(rmfname)
+        h2 = IMP.rmf.create_hierarchies(fh, m)
+        rd = IMP.atom.Representation(h2[0])
+        back_resolutions = rd.get_resolutions()
+        print back_resolutions
+        for p in zip(resolutions, back_resolutions):
+            self.assertAlmostEqual(p[0], p[1], delta=.1)
+
 
 if __name__ == '__main__':
     IMP.test.main()
