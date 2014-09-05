009edbeebd72a2291adf18d2f3b1a425e78d8404
Ben Webb <ben@salilab.org>
2014-09-05 16:21:36 -0700

    Add missing tests.

---
+++ modules/algebra/test/test_ellipsoid3d.py
@@ -0,0 +1,19 @@
+import IMP
+import IMP.test
+import IMP.algebra
+import math
+
+
+class Tests(IMP.test.TestCase):
+    """Test Ellipsoid3D"""
+
+    def test_construction(self):
+        """Check Ellipsoid construction"""
+        r = IMP.algebra.get_rotation_about_axis(
+                 IMP.algebra.Vector3D(0., 0., 0.), .1)
+        e = IMP.algebra.Ellipsoid3D(IMP.algebra.Vector3D(0.0, 1.0, 2.0),
+                                    5.0, 8.0, 10.0, r)
+        self.assertEqual([x for x in e.get_radii()], [5., 8., 10.])
+
+if __name__ == '__main__':
+    IMP.test.main()
