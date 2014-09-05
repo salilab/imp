564bf2493f7dd10985e9b6bfe4e26352f970c11b
Ben Webb <ben@salilab.org>
2014-09-05 09:56:47 -0700

    Add missing test.

--- modules/algebra/test/test_triangle3d.py
+++ modules/algebra/test/test_triangle3d.py
@@ -31,6 +31,17 @@ class Tests(IMP.test.TestCase):
                    IMP.algebra.Vector3D(0., 0., 10.)]
         self.assertEqual(IMP.algebra.get_are_colinear(*not_col), False)
 
+    def test_get_largest_triangle(self):
+        """Check get_largest_triangle()"""
+        t = [IMP.algebra.Vector3D(0., 0., 0.),
+             IMP.algebra.Vector3D(0., 10., 0.),
+             IMP.algebra.Vector3D(10., 10., 0.),
+             IMP.algebra.Vector3D(30., 0., 0.),
+             IMP.algebra.Vector3D(0., 0., 10.)]
+        s = IMP.algebra.get_largest_triangle(t)
+        self.assertEqual([x for x in s.get_point(0)], [0, 10, 0])
+        self.assertEqual([x for x in s.get_point(1)], [30, 0, 0])
+        self.assertEqual([x for x in s.get_point(2)], [0, 0, 10])
 
 if __name__ == '__main__':
     IMP.test.main()
