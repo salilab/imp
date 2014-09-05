08fba8de24703737440f0a89cff155bc7b0f3562
Ben Webb <ben@salilab.org>
2014-09-05 16:35:05 -0700

    Check rotation.

--- modules/algebra/test/test_ellipsoid3d.py
+++ modules/algebra/test/test_ellipsoid3d.py
@@ -14,6 +14,9 @@ class Tests(IMP.test.TestCase):
         e = IMP.algebra.Ellipsoid3D(IMP.algebra.Vector3D(0.0, 1.0, 2.0),
                                     5.0, 8.0, 10.0, r)
         self.assertEqual([x for x in e.get_radii()], [5., 8., 10.])
+        er = r / e.get_rotation()
+        for c, exp_c in zip(er.get_quaternion(), [1., 0., 0., 0.]):
+            self.assertAlmostEqual(c, exp_c, delta=1e-6)
 
 if __name__ == '__main__':
     IMP.test.main()
