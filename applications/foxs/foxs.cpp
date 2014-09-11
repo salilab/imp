2a3c21bf6603868ad4dd6f76005023a8d7f6b38f
Dina Schneidman <duhovka@gmail.com>
2014-09-10 21:00:22 -0700

    remove function

--- applications/foxs/foxs.cpp
+++ applications/foxs/foxs.cpp
@@ -8,6 +8,7 @@
 #include <IMP/saxs/ChiFreeScore.h>
 #include <IMP/saxs/SolventAccessibleSurface.h>
 #include <IMP/saxs/FormFactorTable.h>
+#include <IMP/saxs/utility.h>
 
 #include <IMP/atom/pdb.h>
 
@@ -111,49 +112,6 @@ void read_files(const std::vector<std::string>& files,
   }
 }
 
-Profile* compute_profile(IMP::kernel::Particles particles, float min_q,
-                         float max_q, float delta_q, FormFactorTable* ft,
-                         FormFactorType ff_type, float water_layer_c2, bool fit,
-                         bool reciprocal, bool ab_initio, bool vacuum, std::string beam_profile_file) {
-  IMP_NEW(Profile, profile, (min_q, max_q, delta_q));
-  if (reciprocal) profile->set_ff_table(ft);
-  if (beam_profile_file.size() > 0) profile->set_beam_profile(beam_profile_file);
-
-  // compute surface accessibility and average radius
-  IMP::Floats surface_area;
-  SolventAccessibleSurface s;
-  float average_radius = 0.0;
-  if (water_layer_c2 != 0.0) {
-    // add radius
-    for (unsigned int i = 0; i < particles.size(); i++) {
-      float radius = ft->get_radius(particles[i], ff_type);
-      IMP::core::XYZR::setup_particle(particles[i], radius);
-      average_radius += radius;
-    }
-    surface_area = s.get_solvent_accessibility(IMP::core::XYZRs(particles));
-    average_radius /= particles.size();
-    profile->set_average_radius(average_radius);
-  }
-
-  // pick profile calculation based on input parameters
-  if (!fit) {         // regular profile, no c1/c2 fitting
-    if (ab_initio) {  // bead model, constant form factor
-      profile->calculate_profile_constant_form_factor(particles);
-    } else if (vacuum) {
-      profile->calculate_profile_partial(particles, surface_area, ff_type);
-      profile->sum_partial_profiles(0.0, 0.0);  // c1 = 0;
-    } else {
-      profile->calculate_profile(particles, ff_type, reciprocal);
-    }
-  } else {  // c1/c2 fitting
-    if (reciprocal)
-      profile->calculate_profile_reciprocal_partial(particles, surface_area,
-                                                    ff_type);
-    else
-      profile->calculate_profile_partial(particles, surface_area, ff_type);
-  }
-  return profile.release();
-}
 }
 
 int main(int argc, char** argv) {
@@ -366,7 +324,6 @@ constant form factor (default = false)")(
     // 3. fit experimental profiles
     for (unsigned int j = 0; j < dat_files.size(); j++) {
       Profile* exp_saxs_profile = exp_profiles[j];
-
       std::string fit_file_name2 =
           trim_extension(pdb_files[i]) + "_" +
           trim_extension(basename(const_cast<char*>(dat_files[j].c_str()))) +
