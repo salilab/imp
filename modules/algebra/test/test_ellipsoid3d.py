245e7c1191a0abbcd54052417cb47da39784932d
Ben Webb <ben@salilab.org>
2014-09-24 12:33:20 -0700

    Add missing tests.

--- modules/algebra/test/test_ellipsoid3d.py
+++ modules/algebra/test/test_ellipsoid3d.py
@@ -7,6 +7,29 @@ import math
 class Tests(IMP.test.TestCase):
     """Test Ellipsoid3D"""
 
+    def test_trivial(self):
+        """Check trivial Ellipsoid construction"""
+        e = IMP.algebra.Ellipsoid3D()
+
+    def test_ref_frame(self):
+        """Check construction of Ellipsoid from reference frame"""
+        rot = IMP.algebra.get_identity_rotation_3d()
+        tran = IMP.algebra.Vector3D(1, 2, 3)
+        tf = IMP.algebra.Transformation3D(rot, tran)
+        r = IMP.algebra.ReferenceFrame3D(tf)
+        radii = IMP.algebra.Vector3D(10, 20, 30)
+        e = IMP.algebra.Ellipsoid3D(r, radii)
+        print e
+        self.assertLess((e.get_radii() - radii).get_magnitude(), 1e-4)
+        newr = e.get_reference_frame()
+        self.assertLess((newr.get_transformation_to().get_translation()
+                         - tran).get_magnitude(), 1e-5)
+        self.assertLess((e.get_transformation().get_translation()
+                         - tran).get_magnitude(), 1e-5)
+        self.assertLess((e.get_center() - tran).get_magnitude(), 1e-5)
+        for i in range(3):
+            self.assertAlmostEqual(e.get_radius(i), radii[i], delta=1e-5)
+
     def test_construction(self):
         """Check Ellipsoid construction"""
         r = IMP.algebra.get_rotation_about_axis(
