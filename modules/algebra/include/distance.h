c3b0c89fe279eb2705c69c5a4b0f35ff51c46cba
Riccardo Pellarin <pellarin.riccardo@gmail.com>
2014-10-08 18:16:28 -0700

    adding weighted rmsd

--- modules/algebra/include/distance.h
+++ modules/algebra/include/distance.h
@@ -42,11 +42,49 @@ inline double get_rmsd_transforming_first(const Transformation3D& tr,
   return std::sqrt(rmsd / m1.size());
 }
 
-/** Se get_rmsd_transforming_first(). */
+/** See get_rmsd_transforming_first(). */
 template <class Vector3DsOrXYZs0, class Vector3DsOrXYZs1>
 inline double get_rmsd(const Vector3DsOrXYZs0& m1, const Vector3DsOrXYZs1& m2) {
   return get_rmsd_transforming_first(get_identity_transformation_3d(), m1, m2);
 }
+
+
+template <class Vector3DsOrXYZs0, class Vector3DsOrXYZs1>
+inline double get_weighted_rmsd_transforming_first(const Transformation3D& tr,
+                                          const Vector3DsOrXYZs0& m1,
+                                          const Vector3DsOrXYZs1& m2,
+                                          const Floats& weights) {
+  using algebra::get_vector_geometry;
+  IMP_USAGE_CHECK(std::distance(m1.begin(), m1.end()) ==
+                      std::distance(m2.begin(), m2.end()),
+                  "The input sets of XYZ points "
+                      << "should be of the same size");
+  IMP_USAGE_CHECK(std::distance(m1.begin(), m1.end()) ==
+                      std::distance(weights.begin(), weights.end()),
+                  "The input sets of XYZ points and the weights "
+                      << "should be of the same size");
+
+  double rmsd = 0.0;
+  double weight_sum = 0.0;
+  typename Vector3DsOrXYZs0::const_iterator it0 = m1.begin();
+  typename Vector3DsOrXYZs1::const_iterator it1 = m2.begin();
+  Floats::const_iterator it3 = weights.begin();  
+  for (; it0 != m1.end(); ++it0, ++it1, ++it3) {
+    Vector3D tred = tr.get_transformed(get_vector_geometry(*it0));
+    rmsd += (*it3)*get_squared_distance(tred, get_vector_geometry(*it1));
+    weight_sum += *it3;
+  }
+  return std::sqrt(rmsd / weight_sum);
+}
+
+
+/** See get_weighted_rmsd_transforming_first(). */
+template <class Vector3DsOrXYZs0, class Vector3DsOrXYZs1>
+inline double get_weighted_rmsd(const Vector3DsOrXYZs0& m1, const Vector3DsOrXYZs1& m2,
+                                const Floats& weights) {
+  return get_weighted_rmsd_transforming_first(get_identity_transformation_3d(), m1, m2, weights);
+}
+
 
 IMPALGEBRA_END_NAMESPACE
 
