2bab80ee7743e75c2d97decba492d914bf1bcfd2
Ben Webb <ben@salilab.org>
2014-11-17 14:27:58 -0800

    Test get_transformation_from_first_triangle_to_second().

--- modules/algebra/test/test_triangle3d.py
+++ modules/algebra/test/test_triangle3d.py
@@ -43,5 +43,23 @@ class Tests(IMP.test.TestCase):
         self.assertEqual([x for x in s.get_point(1)], [30, 0, 0])
         self.assertEqual([x for x in s.get_point(2)], [0, 0, 10])
 
+    def test_triangle_trans(self):
+        """Check triangle transformations"""
+        V = IMP.algebra.Vector3D
+        t1 = IMP.algebra.Triangle3D(V(0.0, 0.0, 0.0),
+                                    V(5.0, 0.0, 0.0),
+                                    V(0.0, 5.0, 0.0))
+        t2 = IMP.algebra.Triangle3D(V(0.4, 0.0, 0.0),
+                                    V(0.4, 5.0, 0.0),
+                                    V(0.4, 0.0, 5.0))
+        t = IMP.algebra.get_transformation_from_first_triangle_to_second(t1, t2)
+        v = t.get_translation()
+        self.assertLess(IMP.algebra.get_distance(v, V(0.4,0,0)), 1e-4)
+        r = t.get_rotation().get_quaternion()
+        self.assertAlmostEqual(r[0], 0.5, delta=1e-5)
+        self.assertAlmostEqual(r[1], 0.5, delta=1e-5)
+        self.assertAlmostEqual(r[2], 0.5, delta=1e-5)
+        self.assertAlmostEqual(r[3], 0.5, delta=1e-5)
+
 if __name__ == '__main__':
     IMP.test.main()
