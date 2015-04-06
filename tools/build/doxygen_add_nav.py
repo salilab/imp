3cf48c945ea28538849085c7fa6dccad81d2f734
Ben Webb <ben@salilab.org>
2015-04-06 15:25:44 -0700

    Add 'up' navigation link to manual.

--- tools/build/doxygen_add_nav.py
+++ tools/build/doxygen_add_nav.py
@@ -19,31 +19,44 @@ def map_ids_to_pages(xml_dir):
             mapping[compounddef.attrib['id']] = os.path.basename(f)[:-4]
     return mapping
 
-def get_all_listitems(top_element, level=1):
+def get_all_listitems(top_element, parent=None):
     for child in top_element:
         if child.tag == 'listitem':
-            yield level, child
-            for x in get_all_listitems(child, level + 1):
+            yield child, parent
+            for x in get_all_listitems(child, child):
                 yield x
         else:
-            for x in get_all_listitems(child, level):
+            for x in get_all_listitems(child, parent):
                 yield x
 
-def get_contents(root, page_map):
-    for level, item in get_all_listitems(root):
-        for para in item.findall("para"):
-            for ref in para.findall("ref"):
-                if ref.attrib['kindref'] == 'compound' \
-                   and not 'external' in ref.attrib:
-                    yield level, page_map[ref.attrib['refid']]
+def get_all_links(item):
+    for para in item.findall("para"):
+        for ref in para.findall("ref"):
+            if ref.attrib['kindref'] == 'compound' \
+               and not 'external' in ref.attrib:
+                yield ref
 
-def add_page_navigation(html_dir, pagename, prevpage, nextpage):
+def get_contents(root, page_map):
+    parent_pages = {}
+    for item, parent in get_all_listitems(root):
+        if parent is not None and parent not in parent_pages:
+            links = list(get_all_links(parent))
+            if len(links) > 0:
+                parent_pages[parent] = page_map[links[0].attrib['refid']]
+            else:
+                parent_pages[parent] = None
+        for link in get_all_links(item):
+            yield page_map[link.attrib['refid']], parent_pages.get(parent, None)
+
+def add_page_navigation(html_dir, pagename, prevpage, nextpage, uppage):
     def make_link(title, img, dest):
         return '<a href="%s.html" title="%s"><img src="%s.png" alt="%s"></a>' \
                % (dest, title, img, img)
     links = []
     if prevpage:
         links.append(make_link("Go to previous page", "prev", prevpage))
+    if uppage:
+        links.append(make_link("Go to parent page", "up", uppage))
     if nextpage:
         links.append(make_link("Go to next page", "next", nextpage))
     links = '<div class="doxnavlinks">' + " ".join(links) + '</div>\n'
@@ -69,11 +82,12 @@ def main():
 
     contents = list(get_contents(root, page_map))
     for i, page in enumerate(contents):
-        add_page_navigation(html_dir, page[1],
-                            contents[i-1][1] if i > 0 else 'index',
-                            contents[i+1][1] if i+1 < len(contents) else None)
+        add_page_navigation(html_dir, page[0],
+                            contents[i-1][0] if i > 0 else 'index',
+                            contents[i+1][0] if i+1 < len(contents) else None,
+                            contents[i][1] if contents[i][1] else 'index')
     if len(contents) > 0:
-        add_page_navigation(html_dir, 'index', None, contents[0][1])
+        add_page_navigation(html_dir, 'index', None, contents[0][0], None)
 
 if __name__ == '__main__':
     main()
