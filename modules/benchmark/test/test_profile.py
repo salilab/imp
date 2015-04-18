50bf68a3fa53b0b0688bb64cfb86d1edf3ddba2a
Ben Webb <ben@salilab.org>
2015-04-18 09:13:05 -0700

    Remove base namespace.

--- modules/benchmark/test/test_profile.py
+++ modules/benchmark/test/test_profile.py
@@ -1,6 +1,5 @@
 import IMP.benchmark
 import IMP.test
-import IMP.base
 import os
 
 
@@ -8,7 +7,7 @@ class Tests(IMP.test.TestCase):
 
     def test_log_targets(self):
         """Test that profiler produces a file"""
-        if not IMP.base.IMP_BASE_HAS_GPERFTOOLS:
+        if not IMP.IMP_KERNEL_HAS_GPERFTOOLS:
             self.skipTest("profiling not available")
 
         nm = self.get_tmp_file_name("prof.pprof")
