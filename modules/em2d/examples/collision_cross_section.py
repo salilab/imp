f37ef41f38be6f1da7fffa8e7dd0d481bace161c
Ben Webb <ben@salilab.org>
2015-04-21 08:07:22 -0700

    Help out doxygen by not renaming things.

--- modules/em2d/examples/collision_cross_section.py
+++ modules/em2d/examples/collision_cross_section.py
@@ -5,8 +5,8 @@
 
 from __future__ import print_function
 import IMP
-import IMP.em2d as em2d
-import IMP.atom as atom
+import IMP.em2d
+import IMP.atom
 
 """
 
@@ -18,15 +18,16 @@ Example of how to compute the collision cross section of a molecule
 IMP.set_log_level(IMP.TERSE)
 m = IMP.Model()
 
-fn = em2d.get_example_path("1z5s.pdb")
-prot = atom.read_pdb(fn, m, atom.ATOMPDBSelector())
-atom.add_radii(prot)
+fn = IMP.em2d.get_example_path("1z5s.pdb")
+prot = IMP.atom.read_pdb(fn, m, IMP.atom.ATOMPDBSelector())
+IMP.atom.add_radii(prot)
 
 
 projections = 20
 resolution = 1.0
 pixel_size = 1.5
 img_size = 80
-ccs = em2d.CollisionCrossSection(projections, resolution, pixel_size, img_size)
+ccs = IMP.em2d.CollisionCrossSection(projections, resolution, pixel_size,
+                                     img_size)
 ccs.set_model_particles(IMP.atom.get_leaves(prot))
 print("CCS", ccs.get_ccs(), "A**2")
