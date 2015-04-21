1608f7322f24a5bc1e2f233e4b8b8f0aa4327a37
Ben Webb <ben@salilab.org>
2015-04-21 14:08:00 -0700

    Fix comments.

--- modules/rmf/include/restraint_io.h
+++ modules/rmf/include/restraint_io.h
@@ -17,7 +17,7 @@
 IMPRMF_BEGIN_NAMESPACE
 
 /** \name Restraint I/O
-    Restraints are written as am RMF::FEATURE node with subnodes for
+    Restraints are written as RMF::FEATURE nodes with subnodes for
     the decomposed restraints
     (Restraint::create_current_decomposition()).
     The Restraints::get_last_score() value is what is saved to the file,
@@ -27,23 +27,24 @@ IMPRMF_BEGIN_NAMESPACE
     Any particles returned by Restraint::get_input_particles() that are
     also linked in the RMF file will be included in the RMF file as
     inputs for the Restraint. This allows external software like e.g.,
-    Chimera to associate these restrains with a certain set of particles.
+    Chimera to associate these restraints with a certain set of particles.
     @{
 */
 IMP_DECLARE_LINKERS(Restraint, restraint, restraints, Restraint *,
                     Restraints,
                     (RMF::FileConstHandle fh, Model *m), );
 
-/** Add a list of restraints to the file as (static) bonds. There restraints
- * must decompose into pairwise restraints. Their scores will not be recorded in
- * the file. */
+//! Add a list of restraints to the file as (static) bonds.
+/** There restraints must decompose into pairwise restraints.
+    Their scores will not be recorded in the file. */
 IMPRMFEXPORT void add_restraints_as_bonds(RMF::FileHandle fh,
                                           const Restraints &rs);
 
-/** Certain restraint are made from a really large number of terms.
+//! Limit the number of restraint terms.
+/** Certain restraints are made from a really large number of terms.
     Tracking and displaying all those
     terms can be very time consuming. If the number of terms is larger
-    than the maximum, the terms are not displayed. By default this is 100.*/
+    than the maximum, the terms are not displayed. By default this is 100. */
 IMPRMFEXPORT void set_maximum_number_of_terms(RMF::FileHandle fh,
                                               unsigned int num);
 /** @} */
