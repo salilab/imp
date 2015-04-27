c98bbb4e8118cae875de86b4d9aa84d256b803a7
Ben Webb <ben@salilab.org>
2015-04-27 16:04:36 -0700

    Use indexes rather than Particle* where possible.

--- tools/build/container_templates/test.py
+++ tools/build/container_templates/test.py
@@ -13,18 +13,18 @@ class ClassnameContainerTest(IMP.test.TestCase):
     """Tests for ClassnameContainer related objects"""
 
     def create_particle(self, m):
-        p = IMP.Particle(m)
-        d = IMP.core.XYZ.setup_particle(p)
+        p = m.add_particle("p")
+        d = IMP.core.XYZ.setup_particle(m, p)
         d.set_coordinates(IMP.algebra.get_random_vector_in(
             IMP.algebra.get_unit_bounding_box_3d()))
-        p.add_attribute(IMP.FloatKey("thekey"), d.get_x())
+        m.add_attribute(IMP.FloatKey("thekey"), p, d.get_x())
         return p
 
     def create_particle_pair(self, m):
-        p0 = IMP.Particle(m)
-        p1 = IMP.Particle(m)
-        d0 = IMP.core.XYZ.setup_particle(p0)
-        d1 = IMP.core.XYZ.setup_particle(p1)
+        p0 = m.add_particle("p0")
+        p1 = m.add_particle("p1")
+        d0 = IMP.core.XYZ.setup_particle(m, p0)
+        d1 = IMP.core.XYZ.setup_particle(m, p1)
         d0.set_coordinates(IMP.algebra.get_random_vector_in(
             IMP.algebra.get_unit_bounding_box_3d()))
         d1.set_coordinates(IMP.algebra.get_random_vector_in(
@@ -39,11 +39,11 @@ class ClassnameContainerTest(IMP.test.TestCase):
         uf = IMP.core.Linear(0, 1)
         return IMP.core.DistancePairScore(uf)
 
-    def create_singleton_restraint(self, ps, p):
-        return IMP.core.SingletonRestraint(ps, p)
+    def create_singleton_restraint(self, m, ps, p):
+        return IMP.core.SingletonRestraint(m, ps, p)
 
-    def create_pair_restraint(self, ps, p):
-        return IMP.core.PairRestraint(ps, (p[0], p[1]))
+    def create_pair_restraint(self, m, ps, p):
+        return IMP.core.PairRestraint(m, ps, (p[0], p[1]))
 
     def test_restraint(self):
         """Test the ClassnamesRestraint"""
@@ -53,29 +53,27 @@ class ClassnameContainerTest(IMP.test.TestCase):
         f = 0
         for i in range(0, 10):
             p = self.create_FUNCTIONNAME(m)
-            f = f + gs.evaluate(p, None)
-            c.add_FUNCTIONNAME(p)
+            f = f + gs.evaluate_index(m, p, None)
+            c.add(p)
         r = IMP.container.ClassnamesRestraint(gs, c)
         r.set_was_used(True)
-        m.add_restraint(r)
-        self.assertAlmostEqual(m.evaluate(False), f, delta=.1 * f)
-        self.assertAlmostEqual(m.evaluate(False), f, delta=.1 * f)
+        self.assertAlmostEqual(r.evaluate(False), f, delta=.1 * f)
+        self.assertAlmostEqual(r.evaluate(False), f, delta=.1 * f)
         p = self.create_FUNCTIONNAME(m)
-        f = f + gs.evaluate(p, None)
-        c.add_FUNCTIONNAME(p)
-        self.assertAlmostEqual(m.evaluate(False), f, delta=.1 * f)
-        self.assertAlmostEqual(m.evaluate(False), f, delta=.1 * f)
+        f = f + gs.evaluate_index(m, p, None)
+        c.add(p)
+        self.assertAlmostEqual(r.evaluate(False), f, delta=.1 * f)
+        self.assertAlmostEqual(r.evaluate(False), f, delta=.1 * f)
 
     def test_srestraint(self):
         """Test the ClassnameRestraint"""
         m = IMP.Model()
         gs = self.create_CLASSFUNCTIONNAME_score()
         p = self.create_FUNCTIONNAME(m)
-        f = gs.evaluate(p, None)
-        r = self.create_CLASSFUNCTIONNAME_restraint(gs, p)
+        f = gs.evaluate_index(m, p, None)
+        r = self.create_CLASSFUNCTIONNAME_restraint(m, gs, p)
         r.set_was_used(True)
-        m.add_restraint(r)
-        self.assertAlmostEqual(m.evaluate(False), f, delta=.1 * f)
+        self.assertAlmostEqual(r.evaluate(False), f, delta=.1 * f)
 
     def test_min_restraint(self):
         """Test the MinimumClassnameRestraint"""
@@ -83,7 +81,7 @@ class ClassnameContainerTest(IMP.test.TestCase):
         c = IMP.container.ListClassnameContainer(m)
         self.assertEqual(c.get_ref_count(), 1)
         for i in range(0, 10):
-            c.add_FUNCTIONNAME(self.create_FUNCTIONNAME(m))
+            c.add(self.create_FUNCTIONNAME(m))
         print(c.get_number_of_FUNCTIONNAMEs())
         d = self.create_CLASSFUNCTIONNAME_score()
         self.assertEqual(d.get_ref_count(), 1)
@@ -91,14 +89,14 @@ class ClassnameContainerTest(IMP.test.TestCase):
         self.assertEqual(d.get_ref_count(), 2)
         self.assertEqual(c.get_ref_count(), 2)
         r.set_n(4)
-        m.add_restraint(r)
-        f = m.evaluate(False)
+        f = r.evaluate(False)
         print(f)
         ms = []
         print(c.get_number_of_FUNCTIONNAMEs())
+        indexes = c.get_indexes()
         for i in range(0, 10):
-            ps = c.get_FUNCTIONNAME(i)
-            cm = d.evaluate(ps, None)
+            ps = indexes[i]
+            cm = d.evaluate_index(m, ps, None)
             ms.append(cm)
         print(ms)
         ms.sort()
@@ -117,7 +115,7 @@ class ClassnameContainerTest(IMP.test.TestCase):
         ps = IMP.container.MinimumClassnameScore(s, 3)
         p = self.create_FUNCTIONNAME(m)
         ps.set_was_used(True)
-        v = ps.evaluate(p, None)
+        v = ps.evaluate_index(m, p, None)
         self.assertEqual(v, 3)
 
     def test_set(self):
@@ -133,10 +131,9 @@ class ClassnameContainerTest(IMP.test.TestCase):
             c.add_CLASSFUNCTIONNAME_container(l)
             for j in range(0, 3):
                 t = self.create_FUNCTIONNAME(m)
-                l.add_FUNCTIONNAME(t)
+                l.add(t)
                 cs.append(t)
         ret = c.get_contents()
-        cs = IMP.get_indexes(cs)
         for pi in cs:
             self.assertIn(pi, ret)
         ret.sort()
