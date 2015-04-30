54f7280ebdaf4524a86a171511ff80b271f0a9d1
Ben Webb <ben@salilab.org>
2015-04-29 17:36:37 -0700

    Always print deprecation warnings.
    Right now, too often deprecation warnings are missed.
    While it is true that a deprecation warning from deep inside
    IMP is of little use to a user, those aren't supposed to happen
    anyway (we're not supposed to use deprecated code internally)
    and should be fixed.

--- modules/kernel/src/deprecation.cpp
+++ modules/kernel/src/deprecation.cpp
@@ -12,26 +12,19 @@
 
 IMPKERNEL_BEGIN_NAMESPACE
 void handle_use_deprecated(std::string message) {
-#if !IMP_HAS_LOG4CXX
-  // only trigger error if call is from outer-most context
-  if (internal::log_contexts.empty()) {
-#else
-  if (false) {
-#endif
-    if (internal::no_print_deprecation_messages) {
-      if (internal::printed_deprecation_messages.find(message) ==
-          internal::printed_deprecation_messages.end()) {
-        IMP_WARN(message);
-        internal::printed_deprecation_messages.insert(message);
-      }
+  if (internal::no_print_deprecation_messages) {
+    if (internal::printed_deprecation_messages.find(message) ==
+        internal::printed_deprecation_messages.end()) {
+      IMP_WARN(message);
+      internal::printed_deprecation_messages.insert(message);
     }
+  }
 // we only have a stacktrace if logging is on
 #if IMP_HAS_LOG > IMP_SILENT
-    if (internal::exceptions_on_deprecation) {
-      throw UsageException(message.c_str());
-    }
-#endif
+  if (internal::exceptions_on_deprecation) {
+    throw UsageException(message.c_str());
   }
+#endif
 }
 
 void set_deprecation_warnings(bool tf) {
