9f16b80e5057e91aedcbcfea2925bd865c260fb7
Ben Webb <ben@salilab.org>
2015-05-01 08:20:00 -0700

    Test each example in its own script.
    We can't test two examples from the same script, because they
    both run in the same process, and each example calls
    IMP.setup_from_argv(), which can only be run once per
    invocation of IMP.

--- modules/domino/test/test_custom_filter.py
+++ modules/domino/test/test_custom_filter.py
@@ -7,12 +7,6 @@ import IMP.atom
 
 class Tests(IMP.test.TestCase):
 
-    def test_global_min1(self):
-        """Test example six_particles_optimization.py"""
-        vars = self.run_example(
-            IMP.domino.get_example_path("six_particles_optimization.py"))
-        self.assertEqual(vars['cs'].get_number_of_configurations(), 1)
-
     def test_global_min3(self):
         """Test example custom_filter.py"""
         vars = self.run_example(
