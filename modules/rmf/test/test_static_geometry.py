a4dbf73f2fcb7be3862ee0bf1cb3ccf9dba5e6ef
Ben Webb <ben@salilab.org>
2015-05-05 11:02:12 -0700

    Test handling of complex geometry.

--- modules/rmf/test/test_static_geometry.py
+++ modules/rmf/test/test_static_geometry.py
@@ -30,5 +30,29 @@ class Tests(IMP.test.TestCase):
             self.assertEqual(len(cg), 12)
             IMP.rmf.load_frame(f, RMF.FrameID(1))
 
+    def test_complex_geometry(self):
+        """Test reading and writing complex static geometry"""
+        m = IMP.Model()
+        ds = IMP.core.create_xyzr_particles(m, 20, .1)
+        sc = IMP.container.ListSingletonContainer(m, IMP.get_indexes(ds))
+        cpc = IMP.container.ConnectingPairContainer(sc, .1)
+        m.update()
+        pg = IMP.core.EdgePairsGeometry(cpc)
+
+        for suffix in IMP.rmf.suffixes:
+            nm = self.get_tmp_file_name("static_geometry" + suffix)
+            f = RMF.create_rmf_file(nm)
+            IMP.rmf.add_static_geometries(f, [pg])
+            IMP.rmf.save_frame(f, str(0))
+            del f
+            f = RMF.open_rmf_file_read_only(nm)
+            gs = IMP.rmf.create_geometries(f)
+            # On reread, complex geometry is broken up into components
+            self.assertEqual(len(gs), 19)
+            for g in gs:
+                g.set_was_used(True)
+                cg = g.get_components()
+                self.assertEqual(len(cg), 1)
+
 if __name__ == '__main__':
     IMP.test.main()
