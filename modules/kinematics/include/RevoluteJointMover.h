99b4766db3c365ccc133cc7e5c876e5e7080ec54
Yannick Spill <ygspill@pcb.ub.es>
2015-05-08 15:08:10 +0200

    add RevoluteJointMover (and KinematicForestScoreState to swig)
    this is a MC mover that draws joints from a normal distribution
    note that it should not be a normal distribution, but a von Mises distribution,
    which only differs from the normal when the standard deviation becomes close to
    360 degrees.
    
    closes #890

---
+++ modules/kinematics/include/RevoluteJointMover.h
@@ -0,0 +1,48 @@
+/**
+ *  \file IMP/kinematics/RevoluteJointMover.h
+ *  \brief A modifier which perturbs a point with a normal distribution.
+ *
+ *  Copyright 2007-2014 IMP Inventors. All rights reserved.
+ *
+ */
+
+#ifndef IMPKINEMATICS_REVOLUTE_JOINT_MOVER_H
+#define IMPKINEMATICS_REVOLUTE_JOINT_MOVER_H
+
+#include <IMP/kinematics/kinematics_config.h>
+#include <IMP/core/MonteCarloMover.h>
+#include <IMP/kinematics/revolute_joints.h>
+
+IMPKINEMATICS_BEGIN_NAMESPACE
+
+//! Modify a set of joints using a normal distribution with zero mean and
+//provided standard deviation.
+/** \see MonteCarlo
+ */
+class IMPKINEMATICSEXPORT RevoluteJointMover : public core::MonteCarloMover {
+    kinematics::RevoluteJoints joints_;
+    Float stddev_;
+    Floats originals_;
+
+ public:
+  RevoluteJointMover(kernel::Model *m,
+          const kinematics::RevoluteJoints& joints,
+          const double stddev=0.01);
+
+  void set_sigma(Float sigma) {
+    IMP_USAGE_CHECK(sigma > 0, "Sigma must be positive");
+    stddev_ = sigma;
+  }
+
+  Float get_sigma() const { return stddev_; }
+
+ protected:
+  virtual kernel::ModelObjectsTemp do_get_inputs() const IMP_OVERRIDE;
+  virtual core::MonteCarloMoverResult do_propose() IMP_OVERRIDE;
+  virtual void do_reject() IMP_OVERRIDE;
+  IMP_OBJECT_METHODS(RevoluteJointMover);
+};
+
+IMPKINEMATICS_END_NAMESPACE
+
+#endif /* IMPKINEMATICS_REVOLUTE_JOINT_MOVER_H */
---
+++ modules/kinematics/src/RevoluteJointMover.cpp
@@ -0,0 +1,71 @@
+/**
+ *  \file RevoluteJointMover.cpp
+ *  \brief A mover which perturbs a joint with a gaussian.
+ *
+ *  Copyright 2007-2014 IMP Inventors. All rights reserved.
+ *
+ */
+
+#include <IMP/kinematics/RevoluteJointMover.h>
+#include <IMP/macros.h>
+#include <IMP/base/random.h>
+#include <IMP/base/warning_macros.h>
+#include <IMP/core/rigid_bodies.h>
+#include <boost/random/normal_distribution.hpp>
+
+IMPKINEMATICS_BEGIN_NAMESPACE
+
+RevoluteJointMover::RevoluteJointMover(kernel::Model *m,
+        const kinematics::RevoluteJoints& joints,
+        const double stddev)
+    : MonteCarloMover(m, "RevoluteJointMover%1%"),
+      joints_(joints), stddev_(stddev) {
+  originals_.resize(joints.size(), 0.);
+}
+
+IMP_GCC_DISABLE_WARNING(-Wuninitialized)
+core::MonteCarloMoverResult RevoluteJointMover::do_propose() {
+  IMP_OBJECT_LOG;
+  boost::normal_distribution<double> mrng(0, stddev_);
+  boost::variate_generator<base::RandomNumberGenerator &,
+                           boost::normal_distribution<double> >
+      sampler(base::random_number_generator, mrng);
+
+  for (unsigned int i = 0; i < joints_.size(); ++i) {
+    originals_[i] = joints_[i]->get_angle();
+    joints_[i]->set_angle(originals_[i] + sampler());
+  }
+  //get changed particles' coordinates
+  kernel::ParticleIndexes idx;
+  core::RigidMembers tmp(joints_[0]->get_parent_node().get_rigid_members());
+  for (unsigned int i = 0; i < tmp.size(); ++i)
+      idx.push_back(tmp[i]->get_index());
+  for (unsigned int j = 0; j < joints_.size(); ++j) {
+      tmp = joints_[j]->get_child_node().get_rigid_members();
+      for (unsigned int i = 0; i < tmp.size(); ++i)
+          idx.push_back(tmp[i]->get_index());
+  }
+  return core::MonteCarloMoverResult(idx, 1.0);
+}
+
+void RevoluteJointMover::do_reject() {
+  IMP_OBJECT_LOG;
+  for (unsigned int i = 0; i < joints_.size(); ++i) {
+    joints_[i]->set_angle(originals_[i]);
+  }
+}
+
+kernel::ModelObjectsTemp RevoluteJointMover::do_get_inputs() const {
+  kernel::ModelObjectsTemp ret;
+  core::RigidMembers tmp(joints_[0]->get_parent_node().get_rigid_members());
+  for (unsigned int i = 0; i < tmp.size(); ++i)
+      ret.push_back(tmp[i]); //->get_particle());
+  for (unsigned int j = 0; j < joints_.size(); ++j) {
+      tmp = joints_[j]->get_child_node().get_rigid_members();
+      for (unsigned int i = 0; i < tmp.size(); ++i)
+          ret.push_back(tmp[i]); //->get_particle());
+  }
+  return ret;
+}
+
+IMPKINEMATICS_END_NAMESPACE
