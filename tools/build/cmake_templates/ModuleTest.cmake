92d39ff425bb7c03c5cddf3fd0b695e2bd303399
Ben Webb <ben@salilab.org>
2015-05-13 13:38:02 -0700

    Only set --deprecation_exceptions for C++ tests.
    Passing extra options on the command line for Python tests
    confuses unittest, which does its own command line parsing.

--- tools/build/cmake_templates/ModuleTest.cmake
+++ tools/build/cmake_templates/ModuleTest.cmake
@@ -8,10 +8,16 @@ File(GLOB runtimepytests "${CMAKE_BINARY_DIR}/test/%(name)s/test_*.py" "${CMAKE_
 
 include(Files.cmake)
 
-set(IMP_TEST_ARGUMENTS "--deprecation_exceptions")
 set(IMP_TEST_SETUP ${IMP_TEST_SETUP};${%(NAME)s_TEST_SETUP})
 set(IMP_LINK_LIBRARIES IMP.%(name)s-lib
     %(modules)s
     %(dependencies)s)
 
-imp_add_tests("IMP.%(name)s" ${PROJECT_BINARY_DIR}/test/%(name)s IMP_%(name)s_TESTS test ${runtimepytests} ${pyfiles} ${cppfiles})
+set(IMP_TEST_ARGUMENTS "--deprecation_exceptions")
+imp_add_tests("IMP.%(name)s" ${PROJECT_BINARY_DIR}/test/%(name)s IMP_%(name)s_TESTS test ${cppfiles})
+
+# Don't pass --deprecation_exceptions to Python tests; this confuses unittest's
+# own command line parsing (and is unnecessary since IMP.test.main() already
+# turns on deprecation exceptions).
+set(IMP_TEST_ARGUMENTS )
+imp_add_tests("IMP.%(name)s" ${PROJECT_BINARY_DIR}/test/%(name)s IMP_%(name)s_TESTS test ${runtimepytests} ${pyfiles})
