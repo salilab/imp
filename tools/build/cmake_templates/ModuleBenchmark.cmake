fa8e2d6c1bc028b81ffd1f74a6be25fdb0525f21
Ben Webb <ben@salilab.org>
2015-05-13 11:03:10 -0700

    Pass --deprecation_exception flag to all benchmarks.

--- tools/build/cmake_templates/ModuleBenchmark.cmake
+++ tools/build/cmake_templates/ModuleBenchmark.cmake
@@ -6,7 +6,7 @@ set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS} ${%(NAME)s_CXX_FLAGS}")
 
 include(Files.cmake)
 
-set(IMP_TEST_ARGUMENTS "--run_quick_test")
+set(IMP_TEST_ARGUMENTS "--run_quick_test" "--deprecation_exceptions")
 set(IMP_LINK_LIBRARIES IMP.%(name)s-lib
     %(modules)s IMP.benchmark-lib
     %(dependencies)s)
