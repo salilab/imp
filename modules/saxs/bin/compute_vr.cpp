4a67137f37b63d16ea383e92c9c53e35d7c93c92
Ben Webb <ben@salilab.org>
2015-06-26 09:27:11 -0700

    Don't use base namespace.

--- modules/saxs/bin/compute_vr.cpp
+++ modules/saxs/bin/compute_vr.cpp
@@ -68,7 +68,7 @@ The chi value is computed relative to the first profile using its error column")
   }
 
   IMP::saxs::Profile *exp_saxs_profile = exp_profiles[0];
-  IMP::base::Pointer<IMP::saxs::ProfileFitter<IMP::saxs::RatioVolatilityScore> >
+  IMP::Pointer<IMP::saxs::ProfileFitter<IMP::saxs::RatioVolatilityScore> >
       saxs_score =
           new IMP::saxs::ProfileFitter<IMP::saxs::RatioVolatilityScore>(exp_saxs_profile);
   for (unsigned int i = 1; i < exp_profiles.size(); i++) {
