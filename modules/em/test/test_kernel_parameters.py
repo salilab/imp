904ecdce40cde2342e609248ebab292a721ea5c6
Dina Schneidman <duhovka@gmail.com>
2015-12-04 13:03:43 -0800

    fix kernel parameters test, we don't need radius dependent ones anymore

--- modules/em/test/test_kernel_parameters.py
+++ modules/em/test/test_kernel_parameters.py
@@ -12,7 +12,7 @@ class Tests(IMP.test.TestCase):
     """Class to test Gaussian kernel parameters"""
 
     def test_for_resolution_of_10(self):
-        self.assertAlmostEqual(self.kp.get_rsig(), 4.24660921, delta=0.001)
+        self.assertAlmostEqual(self.kp.get_rsig(), 2.1233, delta=0.001)
         self.assertAlmostEqual(self.kp.get_rsig() * self.kp.get_rsig(),
                                self.kp.get_rsigsq(), delta=0.001)
         self.assertAlmostEqual(self.kp.get_timessig(), 3, delta=0.001)
@@ -25,30 +25,9 @@ class Tests(IMP.test.TestCase):
         self.assertAlmostEqual(
             self.kp.get_rnormfac(),
             self.kp.get_sq2pi3() / 76.549,
-            delta=0.001)
-        self.assertAlmostEqual(self.kp.get_rkdist(), 12.739, delta=0.001)
+            delta=0.01)
+        self.assertAlmostEqual(self.kp.get_rkdist(), 6.3699, delta=0.001)
         self.assertAlmostEqual(self.kp.get_lim(), 0.011108, delta=0.001)
-
-    def test_for_resolution_of_10_and_radius_of_1(self):
-        rad = 1.
-        self.kp.set_params(rad)
-        kp = self.kp.get_params(rad)
-        print(dir(kp))
-        print(type(kp))
-        self.assertAlmostEqual(
-            kp.get_vsig(),
-            1. / math.sqrt(2) * rad,
-            delta=0.001)
-        self.assertAlmostEqual(kp.get_vsig() * kp.get_vsig(),
-                               kp.get_vsigsq(), delta=0.001)
-        self.assertAlmostEqual(0.5 / (kp.get_vsigsq() + self.kp.get_rsigsq()),
-                               kp.get_inv_sigsq(), delta=0.001)
-        self.assertAlmostEqual(kp.get_kdist(), 12.9152, delta=0.001)
-
-    def test_get_parameters_by_radius(self):
-        rad = [1.2, 1.5, 2., 2.2]
-        for r in rad:
-            self.kp.set_params(r)
 
     def setUp(self):
         """Build test model and optimizer"""
