782df7122ce0465f7614baeffe283d305fd3d130
Ben Webb <ben@salilab.org>
2016-01-26 15:22:37 -0800

    Make into a proper test.

--- modules/isd/test/test_incremental_mc_crosslink_ms.py
+++ modules/isd/test/test_incremental_mc_crosslink_ms.py
@@ -1,69 +1,63 @@
-## \example core/incremental_mc.py
-# This example shows how to do incremental scoring with Monte Carlo.
-# Incremental scoring can be significantly faster than non-incremental
-# scoring when using moves that only move a few particles at a time.
-
+from __future__ import print_function
 import IMP.core
-import IMP.container
 import IMP.algebra
-import IMP.display
 import IMP.isd
-import sys
+import IMP.test
 
-IMP.setup_from_argv(sys.argv, "Optimize balls example")
+class Tests(IMP.test.TestCase):
+    def test_incremental(self):
+        """Test ISD restraints with incremental MC scoring"""
+        num_balls = 2
+        num_mc_steps = 10
+        m = IMP.Model()
+        bb = IMP.algebra.BoundingBox3D(IMP.algebra.Vector3D(0, 0, 0),
+                                       IMP.algebra.Vector3D(30, 30, 30))
 
-if IMP.get_is_quick_test():
-    num_balls = 2
-    num_mc_steps = 10
-else:
-    num_balls = 20
-    num_mc_steps = 1000
+        p1=IMP.Particle(m)
+        p1i=p1.get_index()
+        d = IMP.core.XYZR.setup_particle(p1)
+        d.set_radius(10)
+        d.set_coordinates(IMP.algebra.get_random_vector_in(bb))
+        d.set_coordinates_are_optimized(True)
 
-m = IMP.Model()
-bb = IMP.algebra.BoundingBox3D(IMP.algebra.Vector3D(0, 0, 0),
-                               IMP.algebra.Vector3D(30, 30, 30))
+        p2=IMP.Particle(m)
+        p2i=p2.get_index()
+        d = IMP.core.XYZR.setup_particle(p2)
+        d.set_radius(10)
+        d.set_coordinates(IMP.algebra.get_random_vector_in(bb))
+        d.set_coordinates_are_optimized(True)
 
-p1=IMP.Particle(m)
-p1i=p1.get_index()
-d = IMP.core.XYZR.setup_particle(p1)
-d.set_radius(10)
-d.set_coordinates(IMP.algebra.get_random_vector_in(bb))
-d.set_coordinates_are_optimized(True)
+        psigma=IMP.Particle(m)
+        s1i=psigma.get_index()
+        s2i=psigma.get_index()
+        sigma = IMP.isd.Nuisance.setup_particle(psigma)
+        sigma.set_nuisance(10)
+        sigma.set_nuisance_is_optimized(True)
 
-p2=IMP.Particle(m)
-p2i=p2.get_index()
-d = IMP.core.XYZR.setup_particle(p2)
-d.set_radius(10)
-d.set_coordinates(IMP.algebra.get_random_vector_in(bb))
-d.set_coordinates_are_optimized(True)
+        ppsi=IMP.Particle(m)
+        psii=ppsi.get_index()
+        psi = IMP.isd.Nuisance.setup_particle(ppsi)
+        psi.set_nuisance(0.05)
+        psi.set_nuisance_is_optimized(True)
 
-psigma=IMP.Particle(m)
-s1i=psigma.get_index()
-s2i=psigma.get_index()
-sigma = IMP.isd.Nuisance.setup_particle(psigma)
-sigma.set_nuisance(10)
-sigma.set_nuisance_is_optimized(True)
+        dr = IMP.isd.CrossLinkMSRestraint(
+                                    m,
+                                    21.0,
+                                    0.01)
 
-ppsi=IMP.Particle(m)
-psii=ppsi.get_index()
-psi = IMP.isd.Nuisance.setup_particle(ppsi)
-psi.set_nuisance(0.05)
-psi.set_nuisance_is_optimized(True)
+        dr.add_contribution((p1i, p2i), (s1i, s2i), psii)
 
-dr = IMP.isd.CrossLinkMSRestraint(
-                            m,
-                            21.0,
-                            0.01)
+        mc = IMP.core.MonteCarlo(m)
+        isf = IMP.core.IncrementalScoringFunction([p1,p2,psigma,ppsi], [dr])
+        mc.set_incremental_scoring_function(isf)
 
-dr.add_contribution((p1i, p2i), (s1i, s2i), psii)
+        mvs = [IMP.core.BallMover([p], 5) for p in [p1,p2]]
+        sm = IMP.core.SerialMover(mvs)
+        mc.add_mover(sm)
 
-mc = IMP.core.MonteCarlo(m)
-isf = IMP.core.IncrementalScoringFunction([p1,p2,psigma,ppsi], [dr])
-mc.set_incremental_scoring_function(isf)
+        initial=isf.evaluate(False)
+        after = mc.optimize(num_mc_steps)
 
-mvs = [IMP.core.BallMover([p], 5) for p in [p1,p2]]
-sm = IMP.core.SerialMover(mvs)
-mc.add_mover(sm)
 
-initial=isf.evaluate(False)
-after = mc.optimize(num_mc_steps)
+if __name__ == '__main__':
+    IMP.test.main()
