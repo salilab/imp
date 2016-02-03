378878fd7353f436fb062835194f60c476ca4343
Riccardo Pellarin <pellarin.riccardo@gmail.com>
2016-02-03 21:26:03 +0100

    added tests to test_ISDRestraint.py

--- modules/isd/test/test_ISDRestraint.py
+++ modules/isd/test/test_ISDRestraint.py
@@ -4,6 +4,7 @@
 import IMP
 import IMP.core
 import IMP.isd
+import math
 
 # unit testing framework
 import IMP.test
@@ -12,6 +13,29 @@ class ISDRestraint(IMP.isd.ISDRestraint):
     def __init__(self,m):
         IMP.isd.ISDRestraint.__init__(self, m, "ISDRestraint %1%")
 
+
+class ISDRestraint2(IMP.isd.ISDRestraint):
+    def __init__(self,m):
+        IMP.isd.ISDRestraint.__init__(self, m, "ISDRestraint %1%")
+
+    def unprotected_evaluate(self,da):
+        return -math.log(self.get_probability())
+
+    def get_probability(self):
+        return math.pi
+
+    def do_get_inputs(self):
+        return []
+
+class ISDRestraint3(IMP.isd.ISDRestraint):
+    def __init__(self,m):
+        IMP.isd.ISDRestraint.__init__(self, m, "ISDRestraint %1%")
+
+    def get_probability(self):
+        return math.pi
+
+    def do_get_inputs(self):
+        return []
 
 
 class TestISDRestraint(IMP.test.TestCase):
@@ -31,6 +55,20 @@ class TestISDRestraint(IMP.test.TestCase):
         for r in rs.get_restraints():
             isdr=IMP.isd.ISDRestraint.get_from(r)
             self.assertTrue(hasattr(isdr, 'get_probability'))
+
+    def test_instancemethods2(self):
+        m=IMP.Model()
+        isdr=ISDRestraint2(m)
+        self.assertEqual(isdr.get_probability(),math.pi)
+        self.assertEqual(isdr.unprotected_evaluate(None),-math.log(math.pi))
+        #isdr.unprotected_evaluate(nullptr)
+        #isdr-> with typeof(isdr)==ISDRestraint*
+
+    def test_instancemethods3(self):
+        m=IMP.Model()
+        isdr=ISDRestraint3(m)
+        self.assertEqual(isdr.get_probability(),math.pi)
+        self.assertEqual(isdr.unprotected_evaluate(None),-math.log(math.pi))
 
 if __name__ == '__main__':
     IMP.test.main()
