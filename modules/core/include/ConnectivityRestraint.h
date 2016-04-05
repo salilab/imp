71f78a0d0420f03283ab995d1cc7d4bda190a3cc
Ben Webb <ben@salilab.org>
2016-04-05 15:10:43 -0700

    Provide an index-based method to get all pairs.

--- modules/core/include/ConnectivityRestraint.h
+++ modules/core/include/ConnectivityRestraint.h
@@ -49,7 +49,17 @@ class IMPCOREEXPORT ConnectivityRestraint : public Restraint {
       the get_connected_pairs() call, not the set at the time of the last
       evaluate() call.
   */
-  ParticlePairsTemp get_connected_pairs() const;
+  ParticleIndexPairs get_connected_index_pairs() const;
+
+#ifndef IMP_DOXYGEN
+  IMPCORE_DEPRECATED_METHOD_DECL(2.7)
+  ParticlePairsTemp get_connected_pairs() const {
+    IMPCORE_DEPRECATED_METHOD_DEF(2.7,
+                                  "Use get_connected_index_pairs() instead");
+    return IMP::internal::get_particle(get_model(),
+                                       get_connected_index_pairs());
+  }
+#endif
 
   //! Return the pair score used for scoring
   PairScore *get_pair_score() const { return ps_; }
