66d3ae2cefd6c4c37f5833152768467095bb7838
Ben Webb <ben@salilab.org>
2016-08-03 16:46:07 -0700

    Add simple tests of Hierarchy visitors.

--- modules/core/test/test_hierarchy.py
+++ modules/core/test/test_hierarchy.py
@@ -5,9 +5,7 @@ import IMP.core
 
 class Tests(IMP.test.TestCase):
 
-    def test_hierarchy(self):
-        """Check that the hierarchy works fine"""
-        m = IMP.Model()
+    def _make_test_hierarchy(self, m):
         pp = IMP.Particle(m)
         ppd = IMP.core.Hierarchy.setup_particle(pp)
         pp.set_name("root")
@@ -16,9 +14,30 @@ class Tests(IMP.test.TestCase):
             pcd = IMP.core.Hierarchy.setup_particle(pc)
             ppd.add_child(pcd)
             pc.set_name(str("child " + str(i)))
+        return ppd
 
+    def test_hierarchy(self):
+        """Check that the hierarchy works fine"""
+        m = IMP.Model()
+        ppd = self._make_test_hierarchy(m)
         self.assertEqual(ppd.get_number_of_children(), 10,
                          "Number of children is not as expected")
+
+    def test_depth_visit_count(self):
+        """Check depth-first hierarchy traversal"""
+        m = IMP.Model()
+        ppd = self._make_test_hierarchy(m)
+        v = IMP.core.HierarchyCounter()
+        IMP.core.visit_depth_first(ppd, v)
+        self.assertEqual(v.get_count(), 11)
+
+    def test_breadth_visit_count(self):
+        """Check breadth-first hierarchy traversal"""
+        m = IMP.Model()
+        ppd = self._make_test_hierarchy(m)
+        v = IMP.core.HierarchyCounter()
+        IMP.core.visit_breadth_first(ppd, v)
+        self.assertEqual(v.get_count(), 11)
 
     def test_alt_hierarchy(self):
         """Check that an alternate hierarchy works fine"""
