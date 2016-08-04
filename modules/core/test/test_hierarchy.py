8c3ece1f51b3a52dbbcddb6430caea89aedc1eed
Ben Webb <ben@salilab.org>
2016-08-03 17:14:28 -0700

    Allow subclassing HierarchyVistor in Python.
    Turn on directors for HierarchyVisitor so that custom
    visitors can also be written in Python.

--- modules/core/test/test_hierarchy.py
+++ modules/core/test/test_hierarchy.py
@@ -2,6 +2,14 @@ import IMP
 import IMP.test
 import IMP.core
 
+class TestVisitor(IMP.core.HierarchyVisitor):
+    def __init__(self, descend=True):
+        super(TestVisitor, self).__init__()
+        self.descend = descend
+        self.seen = []
+    def __call__(self, h):
+        self.seen.append(h.get_name())
+        return self.descend
 
 class Tests(IMP.test.TestCase):
 
@@ -9,19 +17,23 @@ class Tests(IMP.test.TestCase):
         pp = IMP.Particle(m)
         ppd = IMP.core.Hierarchy.setup_particle(pp)
         pp.set_name("root")
-        for i in range(0, 10):
+        for i in range(0, 2):
             pc = IMP.Particle(m)
             pcd = IMP.core.Hierarchy.setup_particle(pc)
             ppd.add_child(pcd)
-            pc.set_name(str("child " + str(i)))
+            pc.set_name("child %d" % i)
+            for j in range(0, 3):
+                gpc = IMP.Particle(m)
+                gpcd = IMP.core.Hierarchy.setup_particle(gpc)
+                pcd.add_child(gpcd)
+                gpc.set_name("grandchild %d-%d" % (i,j))
         return ppd
 
     def test_hierarchy(self):
         """Check that the hierarchy works fine"""
         m = IMP.Model()
         ppd = self._make_test_hierarchy(m)
-        self.assertEqual(ppd.get_number_of_children(), 10,
-                         "Number of children is not as expected")
+        self.assertEqual(ppd.get_number_of_children(), 2)
 
     def test_depth_visit_count(self):
         """Check depth-first hierarchy traversal"""
@@ -29,7 +41,7 @@ class Tests(IMP.test.TestCase):
         ppd = self._make_test_hierarchy(m)
         v = IMP.core.HierarchyCounter()
         IMP.core.visit_depth_first(ppd, v)
-        self.assertEqual(v.get_count(), 11)
+        self.assertEqual(v.get_count(), 9)
 
     def test_breadth_visit_count(self):
         """Check breadth-first hierarchy traversal"""
@@ -37,7 +49,29 @@ class Tests(IMP.test.TestCase):
         ppd = self._make_test_hierarchy(m)
         v = IMP.core.HierarchyCounter()
         IMP.core.visit_breadth_first(ppd, v)
-        self.assertEqual(v.get_count(), 11)
+        self.assertEqual(v.get_count(), 9)
+
+    def test_depth_visitor_python(self):
+        """Check depth-first hierarchy traversal with a Python visitor"""
+        m = IMP.Model()
+        ppd = self._make_test_hierarchy(m)
+        v = TestVisitor(True)
+        IMP.core.visit_depth_first(ppd, v)
+        self.assertEqual(v.seen,
+             ['root', 'child 0', 'grandchild 0-0', 'grandchild 0-1',
+              'grandchild 0-2', 'child 1', 'grandchild 1-0', 'grandchild 1-1',
+              'grandchild 1-2'])
+
+    def test_breadth_visitor_python(self):
+        """Check breadth-first hierarchy traversal with a Python visitor"""
+        m = IMP.Model()
+        ppd = self._make_test_hierarchy(m)
+        v = TestVisitor(True)
+        IMP.core.visit_breadth_first(ppd, v)
+        self.assertEqual(v.seen,
+             ['root', 'child 1', 'child 0', 'grandchild 1-2',
+              'grandchild 1-1', 'grandchild 1-0', 'grandchild 0-2',
+              'grandchild 0-1', 'grandchild 0-0'])
 
     def test_alt_hierarchy(self):
         """Check that an alternate hierarchy works fine"""
