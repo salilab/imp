361be69ea945b04116fd9a8b0738addc7b450913
Seth Axen <seth.axen@gmail.com>
2016-11-28 09:59:09 -0800

    Split constraint implementation into source file

--- modules/core/include/SurfaceSymmetryConstraint.h
+++ modules/core/include/SurfaceSymmetryConstraint.h
@@ -12,13 +12,11 @@
 #include <IMP/core/core_config.h>
 #include <IMP/core/Surface.h>
 #include <IMP/core/rigid_bodies.h>
-#include <IMP/algebra/Rotation3D.h>
-#include <IMP/algebra/ReferenceFrame3D.h>
+#include <IMP/algebra/Transformation3D.h>
 #include <IMP/Constraint.h>
 #include <IMP/Particle.h>
 #include <IMP/Object.h>
 #include <IMP/Model.h>
-
 
 IMPCORE_BEGIN_NAMESPACE
 
@@ -38,11 +36,7 @@ class IMPCOREEXPORT SurfaceSymmetryConstraint : public IMP::Constraint {
   ParticleIndexPair spip_;
   ParticleIndexPair rbpip_;
 
-  algebra::Transformation3D get_transformation() const {
-    return algebra::get_transformation_from_first_to_second(
-      RigidBody(get_model(), rbpip_[1]).get_reference_frame(),
-      RigidBody(get_model(), rbpip_[0]).get_reference_frame());
-  }
+  algebra::Transformation3D get_transformation() const;
 
   public:
     //! Create restraint from particle pairs.
@@ -54,49 +48,15 @@ class IMPCOREEXPORT SurfaceSymmetryConstraint : public IMP::Constraint {
      */
     SurfaceSymmetryConstraint(Model *m,
                               const ParticleIndexPair &ss,
-                              const ParticleIndexPair &rbs)
-      : IMP::Constraint(m, "SurfaceSymmetryConstraint%1%")
-      , spip_(ss), rbpip_(rbs) {
-        IMP_USAGE_CHECK(Surface::get_is_setup(get_model(), ss[0]) &
-                        Surface::get_is_setup(get_model(), ss[1]),
-                        "First pair must be Surface particles.");
-        IMP_USAGE_CHECK(RigidBody::get_is_setup(get_model(), rbs[0]) &
-                        RigidBody::get_is_setup(get_model(), rbs[1]),
-                        "Second pair must be RigidBody particles.");
-      }
+                              const ParticleIndexPair &rbs);
 
-    virtual void do_update_attributes() IMP_OVERRIDE {
-      algebra::Transformation3D tf = get_transformation();
-      Surface s, sref;
-      s = Surface(get_model(), spip_[0]);
-      sref = Surface(get_model(), spip_[1]);
-      s.set_normal(tf.get_rotation().get_rotated(sref.get_normal()));
-      s.set_coordinates(tf.get_transformed(sref.get_coordinates()));
-    }
+    virtual void do_update_attributes() IMP_OVERRIDE;
 
-    virtual void do_update_derivatives(DerivativeAccumulator *da) IMP_OVERRIDE {
-      algebra::Rotation3D rot = get_transformation().get_inverse().get_rotation();
-      Surface s1 = Surface(get_model(), spip_[0]);
-      Surface s2 = Surface(get_model(), spip_[1]);
-      s2.add_to_derivatives(rot.get_rotated(s1.get_derivatives()), *da);
-      s2.add_to_normal_derivatives(
-        rot.get_rotated(s1.get_normal_derivatives()), *da);
-    }
+    virtual void do_update_derivatives(DerivativeAccumulator *da) IMP_OVERRIDE;
 
-    virtual ModelObjectsTemp do_get_inputs() const IMP_OVERRIDE {
-      ParticlesTemp ps;
-      ps.push_back(get_model()->get_particle(spip_[1]));
-      ps.push_back(get_model()->get_particle(rbpip_[0]));
-      ps.push_back(get_model()->get_particle(rbpip_[1]));
-      return ps;
-    }
+    virtual ModelObjectsTemp do_get_inputs() const IMP_OVERRIDE;
 
-    virtual ModelObjectsTemp do_get_outputs() const IMP_OVERRIDE {
-      ParticlesTemp ps;
-      ps.push_back(get_model()->get_particle(spip_[0]));
-      ps.push_back(get_model()->get_particle(spip_[1]));
-      return ps;
-    }
+    virtual ModelObjectsTemp do_get_outputs() const IMP_OVERRIDE;
 
    IMP_OBJECT_METHODS(SurfaceSymmetryConstraint);
 };
---
+++ modules/core/src/SurfaceSymmetryConstraint.cpp
@@ -0,0 +1,66 @@
+/**
+ *  \file SurfaceSymmetryConstraint.cpp
+ *  \brief Constrain orientation of surfaces with respect to rigid bodies.
+ *
+ *  Copyright 2007-2016 IMP Inventors. All rights reserved.
+ *
+ */
+
+#include <IMP/core/SurfaceSymmetryConstraint.h>
+#include <IMP/algebra/Rotation3D.h>
+
+IMPCORE_BEGIN_NAMESPACE
+
+SurfaceSymmetryConstraint::SurfaceSymmetryConstraint(Model *m,
+                          const ParticleIndexPair &ss,
+                          const ParticleIndexPair &rbs)
+  : IMP::Constraint(m, "SurfaceSymmetryConstraint%1%")
+  , spip_(ss), rbpip_(rbs) {
+    IMP_USAGE_CHECK(Surface::get_is_setup(get_model(), ss[0]) &
+                    Surface::get_is_setup(get_model(), ss[1]),
+                    "First pair must be Surface particles.");
+    IMP_USAGE_CHECK(RigidBody::get_is_setup(get_model(), rbs[0]) &
+                    RigidBody::get_is_setup(get_model(), rbs[1]),
+                    "Second pair must be RigidBody particles.");
+  }
+
+algebra::Transformation3D SurfaceSymmetryConstraint::get_transformation() const {
+  return algebra::get_transformation_from_first_to_second(
+    RigidBody(get_model(), rbpip_[1]).get_reference_frame(),
+    RigidBody(get_model(), rbpip_[0]).get_reference_frame());
+}
+
+void SurfaceSymmetryConstraint::do_update_attributes() {
+  algebra::Transformation3D tf = get_transformation();
+  Surface s, sref;
+  s = Surface(get_model(), spip_[0]);
+  sref = Surface(get_model(), spip_[1]);
+  s.set_normal(tf.get_rotation().get_rotated(sref.get_normal()));
+  s.set_coordinates(tf.get_transformed(sref.get_coordinates()));
+}
+
+void SurfaceSymmetryConstraint::do_update_derivatives(DerivativeAccumulator *da) {
+  algebra::Rotation3D rot = get_transformation().get_inverse().get_rotation();
+  Surface s1 = Surface(get_model(), spip_[0]);
+  Surface s2 = Surface(get_model(), spip_[1]);
+  s2.add_to_derivatives(rot.get_rotated(s1.get_derivatives()), *da);
+  s2.add_to_normal_derivatives(rot.get_rotated(s1.get_normal_derivatives()),
+                               *da);
+}
+
+ModelObjectsTemp SurfaceSymmetryConstraint::do_get_inputs() const {
+  ParticlesTemp ps;
+  ps.push_back(get_model()->get_particle(spip_[1]));
+  ps.push_back(get_model()->get_particle(rbpip_[0]));
+  ps.push_back(get_model()->get_particle(rbpip_[1]));
+  return ps;
+}
+
+ModelObjectsTemp SurfaceSymmetryConstraint::do_get_outputs() const {
+  ParticlesTemp ps;
+  ps.push_back(get_model()->get_particle(spip_[0]));
+  ps.push_back(get_model()->get_particle(spip_[1]));
+  return ps;
+}
+
+IMPCORE_END_NAMESPACE
