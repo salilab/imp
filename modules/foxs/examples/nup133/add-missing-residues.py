9746db6da4b431e27ed421cf48a9dbb18f4c2212
Ben Webb <ben@salilab.org>
2017-01-11 15:43:39 -0800

    Make example work when run from a different directory.

--- modules/foxs/examples/nup133/add-missing-residues.py
+++ modules/foxs/examples/nup133/add-missing-residues.py
@@ -1,5 +1,5 @@
 # This is a MODELLER script to fill in missing residues for
-# the 3KFO structure. It generates 10 candidate models; fit
+# the 3KFO structure. It generates 5 candidate models; fit
 # each one against the SAXS profile using FoXS and pick the
 # one with the best fit.
 
@@ -10,8 +10,13 @@ from modeller.automodel import *    # Load the automodel class
 log.verbose()    # request verbose output
 env = environ()  # create a new MODELLER environment to build this model in
 
+# script_dir is the directory this script is in
+import sys
+import os
+script_dir = os.path.dirname(sys.argv[0])
+
 # directories for input atom files
-env.io.atom_files_directory = ['.', '../atom_files']
+env.io.atom_files_directory = [script_dir, '../atom_files']
 
 # Renumber output model to get correct residue numbers and chain IDs
 # (Modeller default is to not assign a chain ID, and start residue numbers at 1)
@@ -20,10 +25,11 @@ class MyModel(automodel):
         self.rename_segments(segment_ids='A', renumber_residues=944)
 
 a = MyModel(env,
-            alnfile  = 'alignment.ali',     # alignment filename
+            alnfile  = os.path.join(script_dir,
+                                    'alignment.ali'),   # alignment filename
             knowns   = '3KFO',              # codes of the templates
             sequence = '3KFO-fill')         # code of the target
 a.starting_model= 1                 # index of the first model
-a.ending_model  = 10                # index of the last model
+a.ending_model  = 5                 # index of the last model
                                     # (determines how many models to calculate)
 a.make()                            # do the actual comparative modeling
