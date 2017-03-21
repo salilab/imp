9ae6ff5227cfcc9bbd4fed9add7cc580107d1dee
Shruthi Viswanath <shruthi@salilab.org>
2017-03-21 19:37:32 +0000

    Checking in unit tests distancermsd

--- test/test_attribute_distance_pair_score.cpp
+++ test/test_attribute_distance_pair_score.cpp
@@ -39,7 +39,8 @@ int main(int argc, char* argv[])
           (har, core::XYZ::get_coordinate_key(2)));
 IMP_NEW(core::PairRestraint, pr,(m,adps,IMP::internal::get_index
 (ParticlePair(p1, p2))));
-IMP::Pointer<IMP::Restraint> rst=dynamic_cast<IMP::Restraint*>(pr);
+IMP::Pointer<IMP::core:PairRestraint> rst=dynamic_cast
+ <IMP::core::PairRestraint*>(pr);
 
 std::cout << rst->get_score() << std::endl;
 /*IMP_USAGE_CHECK(std::abs(pr->get_score()-144.0),
