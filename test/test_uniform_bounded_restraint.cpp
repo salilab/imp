f620d139a4c07b910c0b8b22505ffbd1af022ca1
Shruthi Viswanath <shruthi@salilab.org>
2017-03-21 22:16:10 +0000

    Checking in unit tests distancermsd

--- test/test_uniform_bounded_restraint.cpp
+++ test/test_uniform_bounded_restraint.cpp
@@ -23,7 +23,7 @@ using namespace IMP::membrane;
 
 int main(int argc, char* argv[])
 {
- IMP::setup_from_argv(argc,argv,"Test tiltSingletonRestraint");
+ IMP::setup_from_argv(argc,argv,"Test uniformBoundedRestraint");
 
  // create a new model
  IMP_NEW(Model,m,());
@@ -31,25 +31,26 @@ int main(int argc, char* argv[])
 
  IMP_NEW(Particle,p1,(m,"p1"));
  IMP::core::XYZ::setup_particle(m,p1->get_index(),IMP::algebra::Vector3D
-   (1.0,4.0,8.0));
+   (1.0,4.0,9.0));
 
  IMP_NEW(Particle,p2,(m,"p2"));
  IMP::core::XYZ::setup_particle(m,p2->get_index(),IMP::algebra::Vector3D
-   (1.0,4.0,6.0));
+   (1.0,4.0,9.0));
 
-  algebra::Vector3D laxis = algebra::Vector3D(0.0, 0.0, 1.0);
-  algebra::Vector3D zaxis = algebra::Vector3D(0.0, 0.0, 1.0);
-  FloatRange tilt_range(0.0,45.0);
-  IMP_NEW(core::HarmonicWell, well, (tilt_range, 1.0));
-  IMP_NEW(TiltSingletonScore, tss, (well, laxis, zaxis));
-  IMP_NEW(core::SingletonRestraint, sr1, (m,tss, IMP::internal::get_index(p1)));
-  // m->add_restraint(sr);
-  allrs->add_restraint(sr1);
+// isd particles representing boundaries
+ IMP_NEW(Particle, pa, (m));
+ isd::Scale a = isd::Scale::setup_particle(pa, 0.0);
+ a.set_lower(0.0);
+ a.set_upper(0.0);
 
-  sr1->set_name("Tilt restraint");
+ IMP_NEW(Particle, pb, (m));
+ isd::Scale b = isd::Scale::setup_particle(pb, 100.0);
+ b.set_lower(90.0);
+ b.set_upper(110.0);
 
-std::cout << allrs->evaluate(false) << std::endl;
-//IMP_TEST_EQUAL(std::abs(allrs->evaluate(false)),242.0)
+IMP_NEW(membrane::UniformBoundedRestraint, ubr,
+            (p2, FloatKey("z"),pa,pb));
+allrs->add_restraint(ubr);
 
  return EXIT_SUCCESS;
 
