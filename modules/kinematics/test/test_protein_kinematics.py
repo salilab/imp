3ef7e3e6bec544f8d1fbae96952d75b6bcffe1a3
Daniel Saltzberg <saltzberg@salilab.org>
2017-04-18 13:52:52 -0700

    expand python protein kinematics test
    include entire pipeline for performing kinematic sampling on a protein.  
    
    Will fail until SWIG errors are fixed.

--- modules/kinematics/test/test_protein_kinematics.py
+++ modules/kinematics/test/test_protein_kinematics.py
@@ -6,11 +6,30 @@ import IMP.kinematics
 
 class Test(IMP.test.TestCase):
     # Failing test for importing ProteinKinematics class
-    def test_setup_kinematics(self):
+    def test_protein_kinematics(self):
         m = IMP.Model()
         hier = IMP.atom.read_pdb(self.open_input_file("three.pdb"), m)
         pk = IMP.kinematics.ProteinKinematics(hier)
-        print(pk)
+
+        joints = pk.get_joints()
+        kfss = IMP.kinematics.KinematicForestScoreState(pk.get_kinematic_forest(), pk.get_rigid_bodies(), atoms)
+        m.add_score_state(kfss)
+        
+        dofs = []
+
+        for j in joints:
+            dd = IMP.kinematics.DOF(j.get_angle(), -1*pi, pi, pi/ang)
+            dofs.append(dd)
+            
+        dd = IMP.kinematics.DirectionalDOF(dofs)
+        planner = IMP.kinematics.PathLocalPlanner(mdl, sampler, dd, 10)
+
+        rrt = IMP.kinematics.RRT(mdl, sampler, planner, dofs)
+        rrt.set_scoring_function(pr)
+        
+        rrt.run()
+        
+        dof_values = rrt.get_DOFValues()
 
 if __name__ == '__main__':
     IMP.test.main()
