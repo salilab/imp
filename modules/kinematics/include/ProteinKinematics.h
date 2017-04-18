15d602689d43eabb6c14a6817ee1379878d51067
Ben Webb <ben@salilab.org>
2017-04-18 11:23:05 -0700

    Don't put private types in public header.
    The types declared here are only used in the internal implementation
    of the ProteinKinematics class, and aren't intended to be used
    elsewhere, so they shouldn't be in the public header, which constitutes
    the API.

--- modules/kinematics/include/ProteinKinematics.h
+++ modules/kinematics/include/ProteinKinematics.h
@@ -24,35 +24,8 @@
 #include <iostream>
 
 #include <boost/graph/adjacency_list.hpp>
-#include <boost/graph/undirected_dfs.hpp>
 
 IMPKINEMATICS_BEGIN_NAMESPACE
-
-typedef boost::adjacency_list<
-    boost::vecS, boost::vecS, boost::undirectedS > Graph;
-
-class MyDFSVisitor : public boost::default_dfs_visitor {
-public:
-  MyDFSVisitor(std::vector<int>& dfs_order, std::vector<int>& parents) :
-    dfs_order_(dfs_order), parents_(parents), counter_(0) {}
-
-  template < typename Vertex, typename Graph >
-  void discover_vertex(Vertex v, const Graph& ) {
-    dfs_order_[v] = counter_;
-    counter_++;
-  }
-
-  template < typename Edge, typename Graph >
-  void tree_edge(Edge e, const Graph& g) {
-    std::cerr << "tree_edge " << source(e, g) << " -- " << target(e, g)
-              << " dfs_order " <<  dfs_order_[source(e, g)] << " -- " <<  dfs_order_[target(e, g)] << std::endl;
-    parents_[target(e,g)] = source(e, g);
-  }
-
-  std::vector<int>& dfs_order_;
-  std::vector<int>& parents_;
-  int counter_;
-};
 
 //! Kinematic structure over a protein, with backbone and side chain dihedrals
 class IMPKINEMATICSEXPORT ProteinKinematics : public IMP::Object {
@@ -199,6 +172,10 @@ class IMPKINEMATICSEXPORT ProteinKinematics : public IMP::Object {
 #endif  // IMP_DOXYGEN
 
  private:
+
+  typedef boost::adjacency_list<
+    boost::vecS, boost::vecS, boost::undirectedS > Graph;
+
   // protein hierarchy
   atom::Hierarchy mhd_;
 
