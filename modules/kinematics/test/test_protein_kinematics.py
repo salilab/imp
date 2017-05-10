34ec9e5cdaa50ff72f0193ef29301e1eca902cd5
Daniel Saltzberg <saltzberg@salilab.org>
2017-05-10 13:01:41 -0700

    Test adding custom_joint - fails currently

--- modules/kinematics/test/test_protein_kinematics.py
+++ modules/kinematics/test/test_protein_kinematics.py
@@ -13,8 +13,18 @@ class Test(IMP.test.TestCase):
 
         joints = pk.get_joints()
 
-
         self.assertEqual(len(joints), 4)
+
+        parts = IMP.atom.Selection(hier, residue_index=2).get_selected_particles()
+        ats = [parts[0].get_index(), parts[1].get_index(), parts[3].get_index(), parts[4].get_index()]
+        piq = IMP.ParticleIndexQuads([ats])
+        residues = IMP.atom.Residues(IMP.atom.get_by_type(hier, IMP.atom.RESIDUE_TYPE))
+
+        pk2 = IMP.kinematics.ProteinKinematics(hier, residues, piq)
+        joints2 = pk2.get_joints()
+
+        self.assertEqual(len(joints), len(joints2)-1)
+
 
         '''
         kfss = IMP.kinematics.KinematicForestScoreState(pk.get_kinematic_forest(), pk.get_rigid_bodies(), atoms)
@@ -36,32 +46,6 @@ class Test(IMP.test.TestCase):
 
         dof_values = rrt.get_DOFValues()
         '''
-
-    def test_list_of_atoms_input(self):
-        # Test to debug passing a list of list of atoms
-        # to C++ functions.
-
-        m = IMP.Model()
-        hier = IMP.atom.read_pdb(self.open_input_file("three.pdb"), m)
-
-        atoms = IMP.atom.Selection(hier, residue_index=2).get_selected_particles()
-
-        self.assertEqual(7, len(atoms))
-
-        #Make a list of atoms
-        list_of_atoms = IMP.atom.Atoms([IMP.atom.Atom(atoms[0]), IMP.atom.Atom(atoms[1]),
-                                        IMP.atom.Atom(atoms[3]), IMP.atom.Atom(atoms[4])])
-
-        # Pass a list of lists of atoms
-        n_dih = IMP.kinematics.test_atoms_list([list_of_atoms])
-
-        self.assertEqual(n_dih, 1)
-
-        n_dih = IMP.kinematics.test_atoms_list([list_of_atoms, list_of_atoms])
-
-        self.assertEqual(n_dih, 2)
-
-
 
 if __name__ == '__main__':
     IMP.test.main()
