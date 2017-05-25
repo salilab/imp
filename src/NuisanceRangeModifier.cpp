85c2f780e8b0ab6d8732afccef16dac0e81225e7
shruthi <shruthi.viswanath@gmail.com>
2017-05-25 15:28:55 -0700

    Adding missing files from membrane repo to spb repo

---
+++ src/NuisanceRangeModifier.cpp
@@ -0,0 +1,54 @@
+/**
+ *  \file spb/NuisanceRangeModifier.cpp
+ *  \brief A singleton modifier which wraps an attribute into a
+ *  given range.
+ *
+ *  Copyright 2007-2012 IMP Inventors. All rights reserved.
+ *
+ */
+
+#include "IMP/spb/NuisanceRangeModifier.h"
+#include "IMP/isd.h"
+
+IMPSPB_BEGIN_NAMESPACE
+
+void NuisanceRangeModifier::apply_index(IMP::Model *m,
+                                        IMP::ParticleIndex pi) const {
+  isd::Nuisance d(m->get_particle(pi));
+  d.set_nuisance(d.get_nuisance());
+}
+
+/* Only the passed particle is used */
+IMP::ModelObjectsTemp NuisanceRangeModifier::do_get_inputs(
+    IMP::Model *m, const IMP::ParticleIndexes &pis) const {
+  ParticlesTemp pt;
+
+  for (unsigned int i = 0; i < pis.size(); i++) {
+    pt.push_back(m->get_particle(pis[i]));
+
+    isd::Nuisance d(m->get_particle(pis[i]));
+
+    ParticleKey pu(
+        isd::Nuisance(m->get_particle(pis[i])).get_upper_particle_key());
+    if (m->get_particle(pis[i])->has_attribute(pu))
+      pt.push_back(m->get_particle(pis[i])->get_value(pu));
+    ParticleKey pd(
+        isd::Nuisance(m->get_particle(pis[i])).get_lower_particle_key());
+
+    if (m->get_particle(pis[i])->has_attribute(pd))
+      pt.push_back(m->get_particle(pis[i])->get_value(pd));
+  }
+
+  return pt;
+}
+
+IMP::ModelObjectsTemp NuisanceRangeModifier::do_get_outputs(
+    IMP::Model *m, const IMP::ParticleIndexes &pis) const {
+  return IMP::get_particles(m, pis);
+}
+
+void NuisanceRangeModifier::show(std::ostream &out) const {
+  out << "NuisanceRangeModifier " << std::endl;
+}
+
+IMPSPB_END_NAMESPACE
