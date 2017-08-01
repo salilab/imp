1968284b127081fb5956a2926e8ab62d0f2bb1d0
Ben Webb <ben@salilab.org>
2017-08-01 14:26:39 -0700

    Move function from anonymous to internal namespace.
    Since this function is both defined and used in the header,
    we can't put it in the anonymous namespace, since then the
    compiler will complain whenever something includes this header
    but doesn't instantiate any template that uses this function.
    Put it in the internal namespace instead.

--- modules/em2d/include/internal/Image2D.h
+++ modules/em2d/include/internal/Image2D.h
@@ -32,40 +32,15 @@
 IMPEM2D_BEGIN_INTERNAL_NAMESPACE
 
 namespace {
+
 int symm_round(double r) {
   return static_cast<int>(r > 0.0 ? floor(r + 0.5) : ceil(r - 0.5));
 }
 
-void read_pgm_header(std::ifstream &infile, bool &binary_fmt, int &width,
-                     int &height, int &maxval) {
-  char c;
-  char ftype = ' ';
-  const int MAX_LENGTH = 1000;
-  char line[MAX_LENGTH];
-  while (infile.get(c)) {
-    switch (c) {
-      case '#':  // if comment line, read the whole line and move on
-        infile.getline(line, MAX_LENGTH);
-        break;
-      case 'P':  // format magick number
-        infile.get(ftype);
-        infile.getline(line, MAX_LENGTH);
-        break;
-      default:  // return the first character
-        infile.putback(c);
-        break;
-    }
-    if (ftype != ' ' && isdigit(c)) break;  // magick number read and the next line is numbers
-  }
-
-  infile >> width >> height >> maxval;
-  IMP_LOG_VERBOSE("Image width = " << width << " height = " << height
-                  << " max color val = " << maxval << std::endl);
-  // magic number P5 = binary PGM; P2 = text PGM
-  binary_fmt = (ftype == '5');
-}
-
 } // anonymous namespace
+
+void read_pgm_header(std::ifstream &infile, bool &binary_fmt, int &width,
+                     int &height, int &maxval);
 
 template <class T = double>
 class Image2D : public boost::multi_array<T, 2> {
