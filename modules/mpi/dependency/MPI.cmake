f6b21747b6fadd3dc54a95975959d28f7c5fd42d
Ben Webb <ben@salilab.org>
2017-08-08 14:07:39 -0700

    Only use mpiexec in tests if it was found.
    Currently our Windows builds build OK with MS-MPI, but its
    mpiexec doesn't work in our Wine environment for some
    reason, so in this case we'll just test on one processor.

--- modules/mpi/dependency/MPI.cmake
+++ modules/mpi/dependency/MPI.cmake
@@ -6,11 +6,15 @@ if("${MPI_CXX_FOUND}")
   #set(CMAKE_CXX_LINK_FLAGS ${CMAKE_CXX_LINK_FLAGS} ${MPI_CXX_LINK_FLAGS})
   set(MPI_LIBRARIES ${MPI_CXX_LIBRARIES} CACHE INTERNAL "" FORCE)
   set(MPI_INCLUDE_PATH ${MPI_CXX_INCLUDE_PATH} CACHE INTERNAL "" FORCE)
-  # Run tests of IMP.mpi module on 2 processors (note that we really should add
-  # $MPIEXEC_POSTFLAGS too, but can't right now because that comes
-  # *after* python)
-  set(MPI_TEST_SETUP ${MPIEXEC};${MPIEXEC_NUMPROC_FLAG};2;${MPIEXEC_PREFLAGS}
-      CACHE INTERNAL "" FORCE)
+  # Run tests of IMP.mpi module on 2 processors, if we found a working mpiexec
+  # (note that we really should add $MPIEXEC_POSTFLAGS too, but can't right
+  # now because that comes *after* python). Currently our Windows builds build
+  # OK with MS-MPI, but its mpiexec doesn't work in our Wine environment
+  # for some reason, so in this case we'll just test on one processor.
+  if (NOT "${MPIEXEC}" MATCHES ".*NOTFOUND.*")
+    set(MPI_TEST_SETUP ${MPIEXEC};${MPIEXEC_NUMPROC_FLAG};2;${MPIEXEC_PREFLAGS}
+        CACHE INTERNAL "" FORCE)
+  endif()
   file(WRITE "${CMAKE_BINARY_DIR}/data/build_info/MPI" "ok=True")
 else()
   message(STATUS "MPI not found")
