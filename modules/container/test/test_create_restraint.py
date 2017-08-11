f83a1be9b8749d77d3420019395e688c0f1031ed
Ben Webb <ben@salilab.org>
2017-08-11 08:34:24 -0700

    Use indexes rather than Particles.

--- modules/container/test/test_create_restraint.py
+++ modules/container/test/test_create_restraint.py
@@ -2,9 +2,9 @@ import IMP.core
 import IMP.container
 
 m = IMP.Model()
-p0 = IMP.Particle(m)
-p1 = IMP.Particle(m)
+p0 = m.add_particle("P0")
+p1 = m.add_particle("P1")
 
 ps = IMP.core.SoftSpherePairScore(1)
 cr = IMP.container.create_restraint(
-    ps, IMP.container.ListPairContainer([(p0, p1)]))
+    ps, IMP.container.ListPairContainer(m, [(p0, p1)]))
