2a3fa494699a0de74401cb4f262989cacfd488a1
Ben Webb <ben@salilab.org>
2017-09-22 15:41:25 -0700

    Fix to build with CGAL 4.11.
    CGAL 4.11 removes "the dangerous implicit conversions between
    weighted points and points". It turns out that much of our code
    was relying on these implicit conversions - use the new approved
    classes and do explicit conversions where necessary.

--- modules/cgal/test/test_memory_problem.cpp
+++ modules/cgal/test/test_memory_problem.cpp
@@ -24,14 +24,20 @@ typedef K::Vector_3 Vector;
 typedef K::Line_3 Line;
 typedef K::Triangle_3 Triangle3;
 
+#if CGAL_VERSION_NR > 1040911000
+typedef CGAL::Regular_triangulation_vertex_base_3<K> Vbb;
+typedef CGAL::Fixed_alpha_shape_vertex_base_3<K,Vbb> Vb;
+typedef CGAL::Regular_triangulation_cell_base_3<K> Rcb;
+typedef CGAL::Fixed_alpha_shape_cell_base_3<K,Rcb> Fb;
+typedef CGAL::Triangulation_data_structure_3<Vb, Fb> TDS;
+typedef CGAL::Regular_triangulation_3<K, TDS> Triangulation;
+typedef K::Weighted_point_3 Wpoint;
+#else
 typedef CGAL::Regular_triangulation_euclidean_traits_3<K> Gt;
 typedef CGAL::Fixed_alpha_shape_vertex_base_3<Gt> Vb;
 typedef CGAL::Fixed_alpha_shape_cell_base_3<Gt> Fb;
 typedef CGAL::Triangulation_data_structure_3<Vb, Fb> TDS;
 typedef CGAL::Regular_triangulation_3<Gt, TDS> Triangulation;
-#if CGAL_VERSION_NR > 1040911000
-typedef Gt::Point_3 Wpoint;
-#else
 typedef Gt::Point Wpoint;
 #endif
 
