e37d5463a74bcc359e7706a975f0d6093cce5a5c
Ben Webb <ben@salilab.org>
2017-12-20 12:11:29 -0800

    Squashed 'modules/pmi/' changes from 2e9bb5f9f3..732e8371ec
    732e8371ec Pass density filename to underlying restraint
    ce77a70d91 Add method to get the underlying EM restraint
    
    git-subtree-dir: modules/pmi
    git-subtree-split: 732e8371ecd82e6446cde38127e57bb84fd0b520

--- modules/pmi/pyext/src/restraints/em.py
+++ modules/pmi/pyext/src/restraints/em.py
@@ -174,6 +174,8 @@ class GaussianEMRestraint(object):
             cutoff_dist_model_data,
             slope,
             update_model, backbone_slope, local)
+        if target_fn != '':
+            self.gaussianEM_restraint.set_density_filename(target_fn)
 
         print('done EM setup')
         self.rs = IMP.RestraintSet(self.m, 'GaussianEMRestraint')
@@ -358,6 +360,9 @@ class GaussianEMRestraint(object):
             inp.add_child(self.get_density_as_hierarchy())
         else:
             raise Exception("Can only add a density to a PMI State object or IMP.atom.Hierarchy. You passed a", type(inp))
+
+    def get_restraint(self):
+        return self.rs
 
     def get_restraint_set(self):
         return self.rs
