288a51f0ce17bbf0561856a0842291c4810637a1
Daniel Saltzberg <saltzberg@salilab.org>
2018-04-24 08:13:56 -0700

    Add side chain dihedral definitions

--- modules/atom/include/dihedrals.h
+++ modules/atom/include/dihedrals.h
@@ -15,6 +15,8 @@
 
 IMPATOM_BEGIN_NAMESPACE
 
+//IMP_BUILTIN_VALUES(AtomTypes, AtomTypesList);
+
 //! Return the atoms comprising the phi dihedral.
 /** If all atoms cannot be found, an empty list is returned.
     \see Residue */
@@ -24,6 +26,25 @@ IMPATOMEXPORT Atoms get_phi_dihedral_atoms(Residue rd);
 /** If all atoms cannot be found, an empty list is returned.
     \see Residue */
 IMPATOMEXPORT Atoms get_psi_dihedral_atoms(Residue rd);
+
+//! Return a list of lists of atom types comprising the chi dihedrals
+//! for residue type rd.
+//! An empty list in a list is returned for residues with no chi dihedrals
+//    \see Residue */
+
+IMPATOMEXPORT Vector<AtomTypes> get_chi_dihedral_atom_types(ResidueType rt);
+
+//! Return a list of lists of atoms comprising the chi dihedrals
+//! for residue rd.
+//! An empty list in a list is returned for residues with no chi dihedrals
+//    \see Residue */
+IMPATOMEXPORT std::vector<Atoms> get_chi_dihedral_atoms(Residue rd);
+
+//! Return a list of lists of ParticleIndexQuads comprising the chi dihedrals
+//! for residue rd.
+//! This datatype is exportable to Python.
+
+IMPATOMEXPORT ParticleIndexQuads get_chi_dihedral_particle_indexes(Residue rd);
 
 IMPATOM_END_NAMESPACE
 
