7bfbd8d57e69f3a2ac6934035cf457da72a158fc
Daniel Saltzberg <saltzberg@salilab.org>
2018-04-24 08:28:58 -0700

    Test PK chi dihedral joints

--- modules/kinematics/test/test_protein_kinematics.py
+++ modules/kinematics/test/test_protein_kinematics.py
@@ -10,9 +10,9 @@ import IMP.atom
 
 class Test(IMP.test.TestCase):
 
-    def construct_peptide(self):
+    def construct_peptide(self, sequence='VQIVYK'):
         topology = IMP.atom.CHARMMTopology(IMP.atom.get_all_atom_CHARMM_parameters())
-        topology.add_sequence('VQIVYK')
+        topology.add_sequence(sequence)
         topology.apply_default_patches()
         m = IMP.Model()
         h = topology.create_hierarchy(m)
@@ -63,7 +63,7 @@ class Test(IMP.test.TestCase):
         pr = IMP.container.PairsRestraint(score, cpc)
 
         return pr
-
+    '''
     def test_protein_kinematics(self):
         m, hier = self.construct_peptide()
         #hier = IMP.atom.read_pdb(self.open_input_file("three.pdb"), m)
@@ -114,6 +114,93 @@ class Test(IMP.test.TestCase):
         dof_values = rrt.get_DOFValues()
 
         print(len(dof_values))
+    '''
+    def test_pk_number_of_joints(self):
+        pi = 3.1415
+        # A single alanine has zero joints
+        m, hier = self.construct_peptide(sequence='A')
+        pk = IMP.kinematics.ProteinKinematics(hier, True, True)
+        pk2 = IMP.kinematics.ProteinKinematics(hier, True, False)
+        self.assertEqual(0, len(pk.get_joints()))
+        self.assertEqual(0, len(pk2.get_joints()))
+
+        # Two alanines have two joints
+        m, hier = self.construct_peptide(sequence='AA')
+        pk = IMP.kinematics.ProteinKinematics(hier, True, True)
+        pk2 = IMP.kinematics.ProteinKinematics(hier, True, False)
+        self.assertEqual(2, len(pk.get_joints()))
+        self.assertEqual(2, len(pk2.get_joints()))
+
+        # One lysine has zero joints with no flexible side chains
+        # and four with flexible side chains
+        m, hier = self.construct_peptide(sequence='K')
+        pk = IMP.kinematics.ProteinKinematics(hier, True, True)
+        pk2 = IMP.kinematics.ProteinKinematics(hier, True, False)
+        self.assertEqual(4, len(pk.get_joints()))
+        self.assertEqual(0, len(pk2.get_joints()))
+
+        # Five ASPs have 8 joints with no flexible side chains
+        # and 8 plus 2*5 = 20 joints with flexible side chains
+        m, hier = self.construct_peptide(sequence='DDDDD')
+        pk = IMP.kinematics.ProteinKinematics(hier, True, True)
+        pk2 = IMP.kinematics.ProteinKinematics(hier, True, False)
+        self.assertEqual(18, len(pk.get_joints()))
+        self.assertEqual(8, len(pk2.get_joints()))
+
+    def test_rrt_sampling_sidechains(self):
+        # Test that flexible_sidechains samples CHI angles
+        pi = 3.1415
+        # A single alanine has zero joints
+        m, hier = self.construct_peptide(sequence='AD')
+        pk = IMP.kinematics.ProteinKinematics(hier, True, True)
+        kf = pk.get_kinematic_forest()
+        joints = pk.get_joints()
+        # Eventually expand test through RRT steps
+        atoms = IMP.atom.get_by_type(hier, IMP.atom.ATOM_TYPE)
+        kfss = IMP.kinematics.KinematicForestScoreState(pk.get_kinematic_forest(), pk.get_rigid_bodies(), atoms)
+        m.add_score_state(kfss)
+
+        p1 = IMP.atom.get_by_type(hier.get_children()[0].get_children()[-1], IMP.atom.ATOM_TYPE)[-1]
+
+        coord = IMP.core.XYZ(p1).get_coordinates()
+        dofs = []
+        init_angles = []
+
+        for j in joints:
+            dd = IMP.kinematics.DOF(j.get_angle(), -1*pi, pi, pi/36)
+            init_angles.append(j.get_angle())
+            dofs.append(dd)
+
+        sampler = IMP.kinematics.UniformBackboneSampler(joints, dofs)
+
+        dd = IMP.kinematics.DirectionalDOF(dofs)
+        planner = IMP.kinematics.PathLocalPlanner(m, sampler, dd, 10)
+
+        pr = self.add_charmm_ff_restraint(m, hier)
+
+        rrt = IMP.kinematics.RRT(m, sampler, planner, dofs, 5, 5)
+        rrt.set_scoring_function(pr)
+
+        rrt.run()
+
+        dof_values = rrt.get_DOFValues()
+
+        for j in range(len(joints)):
+            print(joints[j].get_angle(), init_angles[j])
+        # Apply the DOFs and 
+        sampler.apply(dof_values[-1])
+        #kf.mark_internal_coordinates_changed()
+        #kf.mark_external_coordinates_changed()
+        kf.update_all_external_coordinates()
+        kf.update_all_internal_coordinates()
+        #kf.update_all_external_coordinates()
+        print("These coordinates should not be the same:")
+        print(coord, IMP.core.XYZ(p1).get_coordinates())
+
+        for j in range(len(joints)):
+            print(joints[j].get_angle(), init_angles[j])
+            #self.assertTrue(abs(joints[j].get_angle() - init_angles[j]) > 0.000001)
+
 
 
 if __name__ == '__main__':
