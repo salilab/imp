77d55c94de15c0596da716d2aaf6f1831c6ef5ee
Barak Raveh <barak@salilab.org>
2018-05-12 12:25:44 -0700

    minor function logging + complete split of HierarchyLoadLink and HierarchySaveLink

---
+++ modules/rmf/include/internal/hierarchy_links_helpers.h
@@ -0,0 +1,25 @@
+/**
+ *  \file IMP/rmf/hierarchy_links_helpers.h
+ *  \brief Helper functions for HierarchyLoad/SaveLink.h
+ *
+ *  Copyright 2007-2018 IMP Inventors. All rights reserved.
+ *
+ */
+
+#ifndef IMPRMF_INTERNAL_HIERARCHY_LINKS_HELPERS_H
+#define IMPRMF_INTERNAL_HIERARCHY_LINKS_HELPERS_H
+
+#include <IMP/base_types.h>
+#include <IMP/Model.h>
+#include <RMF/NodeConstHandle.h>
+
+IMPRMF_BEGIN_INTERNAL_NAMESPACE
+
+//! return a string that would be used to name h in the rmf file
+std::string get_good_name_to_atom_node(Model *m, ParticleIndex h);
+
+RMF::NodeConstHandle get_previous_rmf_provenance(RMF::NodeConstHandle node);
+
+IMPRMF_END_INTERNAL_NAMESPACE
+
+#endif /* IMPRMF_INTERNAL_HIERARCHY_LINKS_HELPERS_H */
