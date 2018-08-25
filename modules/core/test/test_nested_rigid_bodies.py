4da8c58035bbef6cbaf632641ea2e14d65d9110d
Seth Axen <seth.axen@gmail.com>
2018-08-24 17:58:57 -0700

    Propagate torques from members to rigid body
    A nested rigid body consisting of rigid body members should have the
    same torque as it would if it were not nested. Derivatives of non-rigid
    members are still not handled properly.
    
    Relates #790

--- modules/core/test/test_nested_rigid_bodies.py
+++ modules/core/test/test_nested_rigid_bodies.py
@@ -3,11 +3,38 @@ import IMP
 import IMP.test
 import IMP.core
 import IMP.algebra
+import numpy as np
+
+
+class DummyRestraint(IMP.Restraint):
+
+    """Adds random derivatives to particles."""
+
+    def __init__(self, m, ps1, ps2, name="DummyRestraint %1%"):
+        IMP.Restraint.__init__(self, m, name)
+        self.ps1 = ps1
+        self.ps2 = ps2
+
+    def unprotected_evaluate(self, accum):
+        if accum:
+            for p1, p2 in zip(self.ps1, self.ps2):
+                v = np.random.normal(size=3)
+                IMP.core.XYZ(p1).add_to_derivatives(
+                    v, accum)
+                IMP.core.XYZ(p2).add_to_derivatives(
+                    v, accum)
+        return 0.
+
+    def do_get_inputs(self):
+        return IMP.get_particles(self.get_model(), self.ps1 + self.ps2)
 
 
 class Tests(IMP.test.TestCase):
 
     """Tests for RigidBody function"""
+
+    def get_uniform_points_in_vall(self, N, c, radius):
+        pass
 
     def test_nested(self):
         """Test nested rigid bodies and close pairs"""
@@ -31,6 +58,54 @@ class Tests(IMP.test.TestCase):
         # check that no internal checks fail
         cp0 = cpf.get_close_pairs(m, ps)
         cp1 = cpf.get_close_pairs(m, ps[3:])
+
+    def test_torque_from_nested_equal_to_unnested(self):
+        """Test nested rigid bodies produce same torque as unnested."""
+        sphere1 = IMP.algebra.Sphere3D([0, 0, 0], 5)
+        sphere2 = IMP.algebra.Sphere3D([0, 0, 10], 10)
+        N1, N2 = 20, 20
+        vs1 = [
+            IMP.algebra.get_random_vector_in(sphere1) for n in range(N1)]
+        vs2 = [
+            IMP.algebra.get_random_vector_in(sphere2) for n in range(N2)]
+
+        m = IMP.Model()
+        ps_unnested = []
+        rb_ps1, rb_ps2 = [], []
+        for v in vs1:
+            xyz = IMP.core.XYZ.setup_particle(IMP.Particle(m), v)
+            ps_unnested.append(xyz)
+            xyz = IMP.core.XYZ.setup_particle(IMP.Particle(m), v)
+            rb_ps1.append(xyz)
+
+        for v in vs2:
+            xyz = IMP.core.XYZ.setup_particle(IMP.Particle(m), v)
+            ps_unnested.append(xyz)
+            xyz = IMP.core.XYZ.setup_particle(IMP.Particle(m), v)
+            rb_ps2.append(xyz)
+
+        ps_nested = rb_ps1 + rb_ps2
+
+        rb_unnested = IMP.core.RigidBody.setup_particle(
+            IMP.Particle(m), ps_unnested)
+        rb_member = IMP.core.RigidBody.setup_particle(
+            IMP.Particle(m), rb_ps2)
+        rb_nested = IMP.core.RigidBody.setup_particle(
+            IMP.Particle(m), rb_unnested.get_reference_frame())
+        for p in rb_ps1 + [rb_member]:
+            rb_nested.add_member(p)
+        m.update()
+
+        r = DummyRestraint(m, ps_unnested, ps_nested)
+        sf = IMP.core.RestraintsScoringFunction([r])
+        sf.evaluate(True)
+
+        self.assertGreater(
+            rb_unnested.get_torque().get_magnitude(), 0)
+        np.testing.assert_allclose(
+            rb_nested.get_torque(), rb_unnested.get_torque(),
+            rtol=1e-6)
+
 
 if __name__ == '__main__':
     IMP.test.main()
