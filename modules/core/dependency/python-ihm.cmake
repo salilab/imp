32d833194796e9696cb7408d23e68368b7f557c5
Ben Webb <ben@salilab.org>
2018-08-28 11:48:36 -0700

    Link ihm Python files individually
    Link each *.py file from the python-ihm dependency
    into the build directory, rather than using a single
    symlink back to the source directory containing all
    *.py files. This prevents us from littering the
    source directory with Python cache files.

--- modules/core/dependency/python-ihm.cmake
+++ modules/core/dependency/python-ihm.cmake
@@ -1,18 +1,34 @@
 file(WRITE "${CMAKE_BINARY_DIR}/data/build_info/python-ihm" "ok=True\n")
 
-if (WIN32)
-  execute_process(COMMAND ${CMAKE_COMMAND} -E copy_directory
-                  ${CMAKE_SOURCE_DIR}/modules/core/dependency/python-ihm/ihm
-                  ${CMAKE_BINARY_DIR}/lib/ihm
-                  RESULT_VARIABLE setup)
-else()
-  execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink
-                  ${CMAKE_SOURCE_DIR}/modules/core/dependency/python-ihm/ihm
-                  ${CMAKE_BINARY_DIR}/lib/ihm
-                  RESULT_VARIABLE setup)
-endif()
+execute_process(COMMAND ${CMAKE_COMMAND} -E make_directory
+                ${CMAKE_BINARY_DIR}/lib/ihm
+                RESULT_VARIABLE setup)
 if(NOT ${setup} EQUAL 0)
-   message(FATAL_ERROR "Failed making ${CMAKE_BINARY_DIR}/lib/ihm directory")
+  message(FATAL_ERROR "Failed making ${CMAKE_BINARY_DIR}/lib/ihm directory")
+endif()
+
+FILE(GLOB ihmpys
+     "${CMAKE_SOURCE_DIR}/modules/core/dependency/python-ihm/ihm/*.py")
+if (WIN32)
+  foreach(ihmpy ${ihmpys})
+    get_filename_component(ihmpyname ${ihmpy} NAME)
+    execute_process(COMMAND ${CMAKE_COMMAND} -E copy
+                    ${ihmpy} ${CMAKE_BINARY_DIR}/lib/ihm/${ihmpyname}
+                    RESULT_VARIABLE setup)
+    if(NOT ${setup} EQUAL 0)
+      message(FATAL_ERROR "Failed copying ${ihmpy}")
+    endif()
+  endforeach()
+else()
+  foreach(ihmpy ${ihmpys})
+    get_filename_component(ihmpyname ${ihmpy} NAME)
+    execute_process(COMMAND ${CMAKE_COMMAND} -E create_symlink
+                    ${ihmpy} ${CMAKE_BINARY_DIR}/lib/ihm/${ihmpyname}
+                    RESULT_VARIABLE setup)
+    if(NOT ${setup} EQUAL 0)
+      message(FATAL_ERROR "Failed symlinking ${ihmpy}")
+    endif()
+  endforeach()
 endif()
 
 # Install Python modules
