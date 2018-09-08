8f0772056f125c0f94400384b9e95488b142e8fb
Ben Webb <ben@salilab.org>
2018-09-07 23:54:56 -0700

    Make argparse aware of IMP flags
    argparse needs to be aware of each IMP flag so
    that it can handle positional arguments correctly
    (otherwise it doesn't know whether an IMP flag takes
    an argument or not). Pass any such flags through to the
    Boost parser once argparse is done.

--- modules/kernel/pyext/IMP_kernel.argparse.i
+++ modules/kernel/pyext/IMP_kernel.argparse.i
@@ -1,3 +1,30 @@
+%{
+#include <IMP/internal/base_static.h>
+
+void get_flag_subset(const boost::program_options::options_description &f,
+                     unsigned ntokens, Strings &s) {
+  for (std::vector<boost::shared_ptr<
+       boost::program_options::option_description> >::const_iterator
+       it = f.options().begin(); it != f.options().end(); ++it) {
+    if (it->get()->semantic()->min_tokens() == ntokens) {
+      s.push_back(it->get()->canonical_display_name());
+    }
+  }
+}
+%}
+
+%inline %{
+
+// Get names of all IMP flags taking the given number of arguments (0 or 1)
+IMP::Strings _get_all_flags(unsigned ntokens) {
+  Strings s;
+  get_flag_subset(internal::flags, ntokens, s);
+  get_flag_subset(internal::advanced_flags, ntokens, s);
+  return s;
+}
+
+%}
+
 %pythoncode %{
 
 try:
@@ -7,15 +34,15 @@ except ImportError:
 import sys
 
 class _PassThroughAction(argparse.Action):
-    """Pass through a non-arg option to the IMP Boost parser"""
-    def __init__(self, option_strings, dest=argparse.SUPPRESS,
-                 default=argparse.SUPPRESS, help=argparse.SUPPRESS):
-        super(_PassThroughAction, self).__init__(
-            option_strings=option_strings, dest=dest, default=default,
-            nargs=0, help=help)
-
+    """Pass an argument through to the IMP Boost parser"""
     def __call__(self, parser, namespace, values, option_string=None):
-        parser._handle_boost([option_string])
+        parser._boost_command_line.append(option_string)
+        if values != []:
+            parser._boost_command_line.append(values)
+        # Terminate processing immediately if necessary (otherwise argparse
+        # may fail if we're missing positional arguments)
+        if option_string in ('-h', '--help', '--help_advanced', '--version'):
+            parser._handle_boost()
 
 
 class ArgumentParser(argparse.ArgumentParser):
@@ -28,11 +55,14 @@ class ArgumentParser(argparse.ArgumentParser):
         # Don't add --help option (since the Boost option parser handles it)
         kwargs['add_help'] = False
         super(ArgumentParser, self).__init__(*args, **kwargs)
-        for arg in (['-h', '--help'],
-                    ['--help_advanced'],
-                    ['--version']):
-            self.add_argument(*arg, help=argparse.SUPPRESS,
-                              action=_PassThroughAction)
+        for ntoken in (0, 1):
+            flags = _get_all_flags(ntoken)
+            for f in flags:
+                arg = ['-h', '--help'] if f == 'help' else ['--' + f]
+                self.add_argument(*arg, help=argparse.SUPPRESS,
+                                  nargs=None if ntoken else 0,
+                                  action=_PassThroughAction,
+                                  default=argparse.SUPPRESS)
 
     def parse_args(self, args=None, namespace=None):
         """Parse the command line and return optional and positional arguments.
@@ -43,16 +73,15 @@ class ArgumentParser(argparse.ArgumentParser):
            IMP::get_string_flag()).
            @returns   args
         """
-        n, remain = super(ArgumentParser, self).parse_known_args(
-                                                        args, namespace)
-        # Pass the rest of the command line to the Boost parser; add the
-        # argparse-generated help to that from Boost
-        self._handle_boost(remain)
-        return n
+        self._boost_command_line = [sys.argv[0]]
+        ret = super(ArgumentParser, self).parse_args(args, namespace)
+        if len(self._boost_command_line) > 1:
+            self._handle_boost()
+        return ret
 
-    def _handle_boost(self, args):
-        setup_from_argv([sys.argv[0]] + args,
+    def _handle_boost(self):
+        setup_from_argv(self._boost_command_line,
                         self.format_help() \
-                         + "\nOptions common to all IMP applications:",
-                         '==SUPPRESS==', 0)
+                        + "\nOptions common to all IMP applications:",
+                        '==SUPPRESS==', 0)
 %}
--- modules/kernel/test/test_argparse.py
+++ modules/kernel/test/test_argparse.py
@@ -2,14 +2,31 @@ from __future__ import print_function
 import IMP.test
 import sys
 
+# argparse processing terminates early for --help or --version
+class HelpTermination(Exception): pass
+class AdvancedHelpTermination(Exception): pass
+class VersionTermination(Exception): pass
+
+def get_test_argparse():
+    a = IMP.ArgumentParser()
+    a.add_argument('--opt', help='test optional argument')
+    a.add_argument('pos', help='test positional argument')
+    return a
+
 def parse_args(parser, args):
-    """Run parser.parse_args() on args; return its result plus any unhandled
-       args (which would normally be handled by IMP's internal boost-based
-       parser)"""
+    """Run parser.parse_args() on args; return its result plus the command line
+       that was passed on to IMP's internal Boost-based parser"""
     class MockSetup(object):
+        def __init__(self):
+            self.argv = []
         def __call__(self, argv, desc, posdesc, num_pos):
-            self.argv = argv
-            return argv[1:]
+            self.argv = argv[1:]
+            if '-h' in argv or '--help' in argv:
+                raise HelpTermination()
+            elif '--help_advanced' in argv:
+                raise AdvancedHelpTermination()
+            elif '--version' in argv:
+                raise VersionTermination()
     old_setup = IMP.setup_from_argv
     try:
         m = MockSetup()
@@ -22,15 +39,54 @@ def parse_args(parser, args):
 
 class Tests(IMP.test.TestCase):
 
-    def test_argparse_variable_positional(self):
-        """Test ArgumentParser with variable number of positional args"""
-        p = IMP.ArgumentParser()
-        p.add_argument("-p", dest="point")
-        p.add_argument("foo")
-        args, rest = parse_args(p, ["-p", "bar", "baz", "xyz"])
-        self.assertEqual(args.point, 'bar')
-        self.assertEqual(args.foo, 'baz')
-        self.assertEqual(rest, [sys.argv[0], 'xyz'])
+    def test_argparse_no_boost(self):
+        """Test ArgumentParser with no Boost options"""
+        p = get_test_argparse()
+        args, boost = parse_args(p, ["--opt", "bar", "baz"])
+        self.assertTrue('opt' in args)
+        self.assertTrue('pos' in args)
+        # Boost options shouldn't show up in output namespace
+        self.assertFalse('help_advanced' in args)
+        self.assertEqual(args.opt, 'bar')
+        self.assertEqual(args.pos, 'baz')
+        self.assertEqual(boost, [])
+        # Bad optional argument
+        self.assertRaises(SystemExit, parse_args, p, ["--bar", "bar", "baz"])
+        # Missing positional argument
+        self.assertRaises(SystemExit, parse_args, p, [])
+        # dashdash should terminate handling of optional arguments
+        args, boost = parse_args(p, ["--", "--opt"])
+        self.assertEqual(args.opt, None)
+        self.assertEqual(args.pos, '--opt')
+        self.assertEqual(boost, [])
+
+    def test_argparse_boost_terminate(self):
+        """Test ArgumentParser with terminating Boost options"""
+        p = get_test_argparse()
+        # help/version should be handled immediately, before argparse notices
+        # we're missing positional arguments
+        self.assertRaises(HelpTermination, parse_args, p, ["--help"])
+        self.assertRaises(AdvancedHelpTermination,
+                          parse_args, p, ["--help_advanced"])
+        self.assertRaises(VersionTermination, parse_args, p, ["--version"])
+
+    def test_argparse_boost_equal(self):
+        """Test ArgumentParser with Boost option=value"""
+        p = get_test_argparse()
+        args, boost = parse_args(p, ["--opt", "bar", "--show_seed",
+                                     "--random_seed=5", "baz"])
+        self.assertEqual(args.opt, 'bar')
+        self.assertEqual(args.pos, 'baz')
+        self.assertEqual(boost, ['--show_seed', '--random_seed', '5'])
+
+    def test_argparse_boost_spaced(self):
+        """Test ArgumentParser with Boost option and value as separate words"""
+        p = get_test_argparse()
+        args, boost = parse_args(p, ["--opt", "bar", "--show_seed",
+                                     "--random_seed", "5", "baz"])
+        self.assertEqual(args.opt, 'bar')
+        self.assertEqual(args.pos, 'baz')
+        self.assertEqual(boost, ['--show_seed', '--random_seed', '5'])
 
 
 if __name__ == '__main__':
