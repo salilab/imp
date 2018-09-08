725807b96bb8d1ecc3d14ddacedfcd6f0c1291f9
Ben Webb <ben@salilab.org>
2018-09-07 17:08:29 -0700

    Use new ArgumentParser to handle command line args

--- modules/isd/pyext/src/create_gmm.py
+++ modules/isd/pyext/src/create_gmm.py
@@ -4,67 +4,69 @@ import IMP.em
 import IMP.isd
 import IMP.isd.gmm_tools
 import numpy as np
-from optparse import OptionParser
+try:
+    from argparse import ArgumentParser
+except ImportError:
+    from IMP._compat_argparse import ArgumentParser
 import sys,os
 def parse_args():
-    usage = """%prog [options] <data_fn> <n_components> <out_txt_fn>
+    desc = """
     Create a GMM from either density file (.mrc), a pdb file (.pdb)
     Will detect input format from extension.
     Outputs as text and optionally as a density map
     see help(-h)
 """
-    parser = OptionParser(usage)
+    p  = ArgumentParser(description=desc)
 
-    parser.add_option("-t","--covar_type",dest="covar_type",default='full',
+    p.add_argument("-t","--covar_type",dest="covar_type",default='full',
                       choices=['spherical', 'tied', 'diag', 'full'],
                       help="covariance type for the GMM")
-    parser.add_option("-m","--out_map",dest="out_map",default='',
+    p.add_argument("-m","--out_map",dest="out_map",default='',
                       help="write out the gmm to an mrc file")
-    parser.add_option("-a","--apix",dest="apix",default=1.0,type='float',
+    p.add_argument("-a","--apix",dest="apix",default=1.0,type=float,
                       help="if you don't provide a map, set the voxel_size here (for sampling)")
-    parser.add_option("-n","--num_samples",dest="num_samples",default=1000000,type='int',
+    p.add_argument("-n","--num_samples",dest="num_samples",default=1000000,type=int,
                       help="num samples to draw from the density map")
-    parser.add_option("-i","--num_iter",dest="num_iter",default=100,type='int',
+    p.add_argument("-i","--num_iter",dest="num_iter",default=100,type=int,
                       help="num iterations of GMM")
-    parser.add_option("-s","--threshold",dest="threshold",default=0.0,type='float',
+    p.add_argument("-s","--threshold",dest="threshold",default=0.0,type=float,
                       help="threshold for the map before sampling")
 
-    parser.add_option("-f","--force_radii",dest="force_radii",default=-1.0,
-                      type='float',
+    p.add_argument("-f","--force_radii",dest="force_radii",default=-1.0,
+                      type=float,
                       help="force radii to be this value (spherical) -1 means deactivated ")
-    parser.add_option("-w","--force_weight",dest="force_weight",default=-1.0,
-                      type='float',
+    p.add_argument("-w","--force_weight",dest="force_weight",default=-1.0,
+                      type=float,
                       help="force weight to be this value (spherical) -1 means deactivated ")
-    parser.add_option("-e","--force_weight_frac",dest="force_weight_frac",action="store_true",default=False,
+    p.add_argument("-e","--force_weight_frac",dest="force_weight_frac",action="store_true",default=False,
                       help="force weight to be 1.0/(num anchors). takes precedence over -w ")
-    parser.add_option("-o","--out_anchors_txt",dest="out_anchors_txt",default='',
+    p.add_argument("-o","--out_anchors_txt",dest="out_anchors_txt",default='',
                       help="write final GMM as anchor points (txt)")
-    parser.add_option("-q","--out_anchors_cmm",dest="out_anchors_cmm",default='',
+    p.add_argument("-q","--out_anchors_cmm",dest="out_anchors_cmm",default='',
                       help="write final GMM as anchor points (cmm)")
-    parser.add_option("-d","--use_dirichlet",dest="use_dirichlet",default=False,
+    p.add_argument("-d","--use_dirichlet",dest="use_dirichlet",default=False,
                       action="store_true",
                       help="use dirichlet process for fit")
 
-    parser.add_option("-k","--multiply_by_mass",dest="multiply_by_mass",default=False,
+    p.add_argument("-k","--multiply_by_mass",dest="multiply_by_mass",default=False,
                       action="store_true",
                       help="if set, will multiply all weights by the total mass of the particles (PDB ONLY)")
-    parser.add_option("-x","--chain",dest="chain",default=None,
+    p.add_argument("-x","--chain",dest="chain",default=None,
                       help="If you passed a PDB file, read this chain")
 
-    parser.add_option("-z","--use_cpp",dest="use_cpp",default=False,
+    p.add_argument("-z","--use_cpp",dest="use_cpp",default=False,
                       action="store_true",
                       help="EXPERIMENTAL. Uses the IMP GMM code. Requires isd_emxl")
-
-
-    (options, args) = parser.parse_args()
-    if len(args) != 3:
-        parser.error("incorrect number of arguments")
-    return options,args
+    p.add_argument("data_file", help="data file name")
+    p.add_argument("n_centers", type=int, help="number of centers")
+    p.add_argument("out_file", help="output file name")
+    return p.parse_args()
 
 def run():
-    options,args = parse_args()
-    data_fn,ncenters,out_txt_fn = args
-    ncenters = int(ncenters)
+    args = parse_args()
+    data_fn = args.data_file
+    ncenters = args.n_centers
+    out_txt_fn = args.out_file
     mdl = IMP.Model()
 
     if not os.path.isfile(data_fn):
