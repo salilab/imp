8ce65cd416d5fed48569bdb8a955c72955ff168b
saltzberg <saltzberg.salilab.org>
2018-10-21 07:26:25 -0700

    Add transform_safe

--- modules/kinematics/include/KinematicForest.h
+++ modules/kinematics/include/KinematicForest.h
@@ -161,6 +161,19 @@ class IMPKINEMATICSEXPORT KinematicForest
     return ret;
   }
 
+  //! apply a rigid body transformation to the entire forest
+  //! safelt, such that the forest will return correct external
+  //! and internal coordinates if queries through get_coordinates_safe()
+  //! or get_reference_frame_safe(), or after updating with
+  //! update_all_external_coordinates() or update_all_internal_coordinates(),
+  //! respectively.
+  void transform_safe(IMP::algebra::Transformation3D tr){
+    IMP_FOREACH(KinematicNode root, roots_){
+      IMP::core::transform(root, tr);
+      mark_internal_coordinates_changed(); // technically, roots reference frames is a part of the internal tree coordinates, so external coordinates will need to be updated at some point
+    }
+  }
+
   /**
      notifies the tree that joint (internal) coordinates
      have changed and therefore external coordinates are not
