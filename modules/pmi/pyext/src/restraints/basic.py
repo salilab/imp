98c7e2cc2e0b93306ed966de2a9a6024af4df0aa
Ben Webb <ben@salilab.org>
2018-12-13 11:16:08 -0800

    Squashed 'modules/pmi/' changes from 1b3b80e93c..8fdfd2bb22
    8fdfd2bb22 Clean up wonky whitespace
    0c18c2ae7a Speed up example when run in test mode
    5503195660 Add missing input particle to MembraneRestraint
    c2914b4890 Fix wonky indentation
    
    git-subtree-dir: modules/pmi
    git-subtree-split: 8fdfd2bb22aec23dd28db3d0dec47afb61142c13

--- modules/pmi/pyext/src/restraints/basic.py
+++ modules/pmi/pyext/src/restraints/basic.py
@@ -429,25 +429,25 @@ class MembraneRestraint(IMP.pmi.restraints.RestraintBase):
                  label = None):
         """ Setup Membrane restraint
 
-        Simple sigmoid score calculated for particles above, 
-        
+        Simple sigmoid score calculated for particles above,
+
         @param objects_inside list or tuples of objects in membrane (e.g. ['p1', (10, 30,'p2')])
         @param objects_above list or tuples of objects above membrane
         @param objects_below list or tuples of objects below membrane
-        @param thickness Thickness of the membrane along the z-axis 
+        @param thickness Thickness of the membrane along the z-axis
         @param softness Softness of the limiter in the sigmoid function
         @param plateau Parameter to set the probability (=1- plateau)) at the plateau
                        phase of the sigmoid
         @param weight Weight of restraint
         @param label A unique label to be used in outputs and
-                     particle/restraint names. 
+                     particle/restraint names.
         input a list of particles, the slope and theta of the sigmoid potential
         theta is the cutoff distance for a protein-protein contact
         """
 
         self.hier = hier
         model = self.hier.get_model()
-        
+
         rname = "MembraneRestraint"
         super(MembraneRestraint, self).__init__(
             model, name="MembraneRestraint", label=label, weight=weight)
@@ -458,7 +458,7 @@ class MembraneRestraint(IMP.pmi.restraints.RestraintBase):
         self.plateau = plateau
         self.linear = 0.02
         self.resolution = resolution
-        
+
         # Create nuisance particle
         p = IMP.Particle(model)
         z_center = IMP.isd.Nuisance.setup_particle(p)
@@ -471,75 +471,75 @@ class MembraneRestraint(IMP.pmi.restraints.RestraintBase):
                                        self.softness,
                                        self.plateau,
                                        self.linear)
-        
+
         # Particles above
         if objects_above:
             for obj in objects_above:
                 if isinstance(obj, tuple):
                     self.particles_above = self._select_from_tuple(obj)
-                    
+
                 elif isinstance(obj, str):
                     self.particles_above = self._select_from_string(obj)
                 mr.add_particles_above(self.particles_above)
-                
+
         # Particles inside
         if objects_inside:
             for obj in objects_inside:
                 if isinstance(obj, tuple):
                     self.particles_inside = self._select_from_tuple(obj)
-                    
+
                 elif isinstance(obj, str):
                     self.particles_inside = self._select_from_string(obj)
                 mr.add_particles_inside(self.particles_inside)
-        
+
 
         # Particles below
         if objects_below:
             for obj in objects_below:
                 if isinstance(obj, tuple):
                     self.particles_below = self._select_from_tuple(obj)
-                    
+
                 elif isinstance(obj, str):
                     self.particles_below = self._select_from_string(obj)
                 mr.add_particles_below(self.particles_below)
 
-        
+
         self.rs.add_restraint(mr)
 
     def get_particles_above(self):
         return self.particles_above
-        
+
     def get_particles_inside(self):
         return self.particles_inside
 
     def get_particles_below(self):
         return self.particles_below
-        
+
     def _select_from_tuple(self, obj):
         particles = IMP.atom.Selection(self.hier,
                                        molecule = obj[2],
                                        residue_indexes = range(obj[0], obj[1]+1, 1),
                                        resolution = self.resolution).get_selected_particles()
-            
+
         return particles
 
-    def _select_from_string(self, obj): 
+    def _select_from_string(self, obj):
         particles = IMP.atom.Selection(self.hier,
                                        molecule = obj,
                                        resolution = self.resolution).get_selected_particles()
         return particles
 
     def create_membrane_density(self, file_out='membrane_localization.mrc'):
-        
+
         '''
-        Just for visualization purposes. 
-        Writesdensity of membrane localization
+        Just for visualization purposes.
+        Writes density of membrane localization
         '''
-        
+
         offset = 5.0*self.thickness
         apix = 3.0
-        resolution = 5.0       
- 
+        resolution = 5.0
+
         # Create a density header of the requested size
         bbox = IMP.algebra.BoundingBox3D(
             IMP.algebra.Vector3D(-self.center - offset, -self.center - offset, -self.center - offset,),
@@ -556,7 +556,7 @@ class MembraneRestraint(IMP.pmi.restraints.RestraintBase):
                 dmap.set_value(c[0], c[1], c[2], 0.0)
 
         IMP.em.write_map(dmap, file_out)
-        
+
     def _is_membrane(self, z):
         if (z-self.center) < self.thickness/2.0 and  (z-self.center) >= -self.thickness/2.0 :
             return 1
