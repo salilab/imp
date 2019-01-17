9aabe803e830fbe3bfa175b6aa79604510861bac
Ben Webb <ben@salilab.org>
2019-01-16 16:45:33 -0800

    Generalize finding IMP modules
    Provide a ModulesFinder class to find all IMP modules,
    and use it in the setup_cmake script. This should find
    regular IMP modules in the modules/ subdirectory, plus
    any already-built (external) modules in the
    data/build_info directory, and will ultimately work
    outside of the IMP build tree. Relates #838.

--- tools/build/setup_cmake.py
+++ tools/build/setup_cmake.py
@@ -54,21 +54,21 @@ module_template = tools.CMakeFileGenerator(os.path.join(TOPDIR,
 benchmark_template = tools.CMakeFileGenerator(os.path.join(TOPDIR,
                                     "cmake_templates", "ModuleBenchmark.cmake"))
 
-def make_check(path, module, module_path):
+def make_check(path, module):
     name = os.path.splitext(os.path.split(path)[1])[0]
     cppsource = open(path, "r").read()
     macro = "IMP_COMPILER_%s" % name.upper()
-    filename = os.path.join(module_path, "CMakeModules",
+    filename = os.path.join(module.path, "CMakeModules",
                             "Check" + name + ".cmake")
     check_template.write(filename,
                          {"macro": macro,
                           "cppsource": tools.quote(cppsource),
-                          "module": module, "name": name})
+                          "module": module.name, "name": name})
     defr = "%s=${%s}" % (macro, macro)
     return filename, defr
 
 
-def make_dependency_check(descr_path, module, module_path):
+def make_dependency_check(descr_path, module):
     descr = tools.get_dependency_description(descr_path)
     name = os.path.splitext(os.path.split(descr_path)[1])[0]
     if len(descr["cmake"]) > 0:
@@ -77,7 +77,7 @@ def make_dependency_check(descr_path, module, module_path):
     descr["pkgname"] = name
     descr["PKGNAME"] = name.upper()
     filename = os.path.join(
-        module_path,
+        module.path,
         "CMakeModules",
         "Find" + name + ".cmake")
     if descr["python_module"] != "":
@@ -107,9 +107,9 @@ endif(DEFINED %(PKGNAME)s_INTERNAL)""" % descr
     return filename
 
 
-def get_sources(module, path, subdir, pattern):
-    matching = tools.get_glob([os.path.join(path, subdir, pattern),
-                              os.path.join(path, subdir, "*", pattern)])
+def get_sources(module, subdir, pattern):
+    matching = tools.get_glob([os.path.join(module.path, subdir, pattern),
+                               os.path.join(module.path, subdir, "*", pattern)])
     return "\n".join(["${CMAKE_SOURCE_DIR}/%s"
                      % tools.to_cmake_path(x) for x in matching])
 
@@ -118,98 +118,93 @@ def get_app_sources(path, patterns, filt=lambda x:True):
     return ["${CMAKE_SOURCE_DIR}/%s" % tools.to_cmake_path(x) \
             for x in matching if filt(x)]
 
-def get_dep_merged(modules, name, ordered):
-    ret = []
-    alldeps = tools.get_all_dependencies(".", modules, "", ordered)
-    for d in alldeps:
-        ret.append("${%s_%s}" % (d.upper(), name.upper()))
-    ret = sorted(set(ret))
-    return "\n        ".join(ret)
+def get_dep_merged(all_deps, name):
+    return "\n        ".join(sorted(set("${%s_%s}" % (d.upper(), name.upper())
+                                    for d in all_deps)))
 
-
-def setup_module(module, path, ordered):
+def setup_module(finder, module):
     checks = []
     deps = []
     contents = []
     defines = []
-    for cc in tools.get_glob([os.path.join(path, "compiler", "*.cpp")]):
-        ret = make_check(cc, module, path)
+    for cc in tools.get_glob([os.path.join(module.path, "compiler", "*.cpp")]):
+        ret = make_check(cc, module)
         checks.append(ret[0])
         defines.append(ret[1])
-    for cc in tools.get_glob([os.path.join(path, "dependency", "*.description")]):
-        ret = make_dependency_check(cc, module, path)
+    for cc in tools.get_glob([os.path.join(module.path, "dependency",
+                                           "*.description")]):
+        ret = make_dependency_check(cc, module)
         if ret:
             deps.append(ret)
 
     g = tools.CMakeFileGenerator()
     if len(checks) > 0:
-        g.write("modules/%s/compiler/CMakeLists.txt" % module,
+        g.write("%s/compiler/CMakeLists.txt" % module.path,
                 "\n".join(["include(${CMAKE_SOURCE_DIR}/%s)\n" %
                                tools.to_cmake_path(x) for x in checks]))
         contents.append(
-            "add_subdirectory(${CMAKE_SOURCE_DIR}/modules/%s/compiler)" %
-            module)
+            "add_subdirectory(${CMAKE_SOURCE_DIR}/%s/compiler)" %
+            module.path)
     if len(deps) > 0:
-        g.write("modules/%s/dependency/CMakeLists.txt" % module,
+        g.write("%s/dependency/CMakeLists.txt" % module.path,
                 "\n".join(["include(${CMAKE_SOURCE_DIR}/%s)" %
                                tools.to_cmake_path(x) for x in deps]))
         contents.append(
-            "add_subdirectory(${CMAKE_SOURCE_DIR}/modules/%s/dependency)" %
-            module)
-    local = os.path.join(path, "Setup.cmake")
+            "add_subdirectory(${CMAKE_SOURCE_DIR}/%s/dependency)" %
+            module.path)
+    local = os.path.join(module.path, "Setup.cmake")
     if os.path.exists(local):
         contents.append("include(${CMAKE_SOURCE_DIR}/%s)"
                         % tools.to_cmake_path(local))
 
-    values = {"name": module}
-    if module == 'kernel':
+    values = {"name": module.name}
+    if module.name == 'kernel':
         values['subdir'] = 'IMP'
         values['pymod'] = 'IMP'
         values['allh_header'] = 'IMP.h'
     else:
-        values['subdir'] = 'IMP/' + module
-        values['pymod'] = 'IMP.' + module
-        values['allh_header'] = 'IMP/%s.h' % module
-    values["NAME"] = module.upper()
-    values["CPPNAME"] = module.upper().replace('_', '')
-    data = tools.get_module_description(".", module, "")
-    all_modules = tools.get_all_modules(".", [module], "", ordered)
-    modules = ["${IMP_%s_LIBRARY}" % s for s in all_modules]
-    dependencies = ["${%s_LIBRARIES}" % s.upper()
-                    for s in tools.get_all_dependencies(".", [module], "", ordered)]
+        values['subdir'] = 'IMP/' + module.name
+        values['pymod'] = 'IMP.' + module.name
+        values['allh_header'] = 'IMP/%s.h' % module.name
+    values["NAME"] = module.name.upper()
+    values["CPPNAME"] = module.name.upper().replace('_', '')
+    all_modules = module.get_all_modules()
+    modules = ["${IMP_%s_LIBRARY}" % s.name for s in all_modules]
+    all_dependencies = list(finder.get_all_dependencies([module] + all_modules))
+    dependencies = ["${%s_LIBRARIES}" % s.upper() for s in all_dependencies]
     values["modules"] = ";".join(modules)
-    values["tags"] = "\n".join(["${IMP_%s_DOC}" % m for m in all_modules])
+    values["tags"] = "\n".join(["${IMP_%s_DOC}" % m.name for m in all_modules])
     values["other_pythons"] = "\n".join(
         ["${IMP_%s_PYTHON}" %
-         m for m in all_modules])
+         m.name for m in all_modules])
     values["dependencies"] = ";".join(dependencies)
-    values["headers"] = get_sources(module, path, "include", "*.h")
-    values["includepath"] = get_dep_merged([module], "include_path", ordered)
-    values["libpath"] = get_dep_merged([module], "link_path", ordered)
-    values["swigpath"] = get_dep_merged([module], "swig_path", ordered)
+    values["headers"] = get_sources(module, "include", "*.h")
+    values["includepath"] = get_dep_merged(all_dependencies, "include_path")
+    values["libpath"] = get_dep_merged(all_dependencies, "link_path")
+    values["swigpath"] = get_dep_merged(all_dependencies, "swig_path")
     values["defines"] = ":".join(defines)
-    cppbins = tools.get_glob([os.path.join(path, "bin", "*.cpp")])
+    cppbins = tools.get_glob([os.path.join(module.path, "bin", "*.cpp")])
     cppbins = [os.path.splitext(e)[0] for e in cppbins]
-    pybins = get_app_sources(os.path.join(path, "bin"), ["*"],
+    pybins = get_app_sources(os.path.join(module.path, "bin"), ["*"],
                              tools.filter_pyapps)
     values["pybins"] = "\n".join(pybins)
     values["bin_names"] = "\n".join([os.path.basename(x) \
                                      for x in pybins + cppbins])
 
-    local = os.path.join(path, "Build.cmake")
+    local = os.path.join(module.path, "Build.cmake")
     if os.path.exists(local):
         values["custom_build"] = "include(${CMAKE_SOURCE_DIR}/%s)\n" \
                                  % tools.to_cmake_path(local)
     else:
         values["custom_build"] = ""
 
-    main = os.path.join(path, "src", "CMakeLists.txt")
-    tests = os.path.join(path, "test", "CMakeLists.txt")
-    swig = os.path.join(path, "pyext", "CMakeLists.txt")
-    util = os.path.join(path, "utility", "CMakeLists.txt")
-    bin = os.path.join(path, "bin", "CMakeLists.txt")
-    benchmark = os.path.join(path, "benchmark", "CMakeLists.txt")
-    examples = os.path.join(path, "examples", "CMakeLists.txt")
+    main = os.path.join(module.path, "src", "CMakeLists.txt")
+    tests = os.path.join(module.path, "test", "CMakeLists.txt")
+    swig = os.path.join(module.path, "pyext", "CMakeLists.txt")
+    util = os.path.join(module.path, "utility", "CMakeLists.txt")
+    bin = os.path.join(module.path, "bin", "CMakeLists.txt")
+    benchmark = os.path.join(module.path, "benchmark", "CMakeLists.txt")
+    examples = os.path.join(module.path, "examples", "CMakeLists.txt")
     lib_template.write(main, values)
     test_template.write(tests, values)
     swig_template.write(swig, values)
@@ -218,21 +213,21 @@ def setup_module(module, path, ordered):
     benchmark_template.write(benchmark, values)
     examples_template.write(examples, values)
     values["tests"] = "\n".join(contents)
-    values["subdirs"] = """add_subdirectory(${CMAKE_SOURCE_DIR}/modules/%s/src)
-add_subdirectory(${CMAKE_SOURCE_DIR}/modules/%s/test)
-add_subdirectory(${CMAKE_SOURCE_DIR}/modules/%s/examples)
-add_subdirectory(${CMAKE_SOURCE_DIR}/modules/%s/benchmark)
-add_subdirectory(${CMAKE_SOURCE_DIR}/modules/%s/bin)
-add_subdirectory(${CMAKE_SOURCE_DIR}/modules/%s/utility)""" % ((module,) * 6)
+    values["subdirs"] = """add_subdirectory(${CMAKE_SOURCE_DIR}/%s/src)
+add_subdirectory(${CMAKE_SOURCE_DIR}/%s/test)
+add_subdirectory(${CMAKE_SOURCE_DIR}/%s/examples)
+add_subdirectory(${CMAKE_SOURCE_DIR}/%s/benchmark)
+add_subdirectory(${CMAKE_SOURCE_DIR}/%s/bin)
+add_subdirectory(${CMAKE_SOURCE_DIR}/%s/utility)""" % ((module.path,) * 6)
 
-    out = os.path.join(path, "CMakeLists.txt")
+    out = os.path.join(module.path, "CMakeLists.txt")
     module_template.write(out, values)
 
     # at end so directories exist
     subprocess.check_call(["python",
                           os.path.join(TOPDIR, "..",
                                        "dev_tools", "setup_cmake.py")],
-                          cwd=path, universal_newlines=True)
+                          cwd=module.path, universal_newlines=True)
 
     return out
 
@@ -243,10 +238,10 @@ parser = OptionParser()
 def main():
     (options, args) = parser.parse_args()
     main = []
-    ordered = tools.compute_sorted_order(".", "")
-    for m in ordered:
-        p = os.path.join("modules", m)
-        main.append(setup_module(m, p, ordered))
+    mf = tools.ModulesFinder(source_dir='')
+    for m in mf.get_ordered():
+        if isinstance(m, tools.SourceModule):
+            main.append(setup_module(mf, m))
 
 if __name__ == '__main__':
     main()
