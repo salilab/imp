11dbd9cb4c43e61623407e734adc646eab82a9f8
Ben Webb <ben@salilab.org>
2019-01-16 21:45:59 -0800

    Fix to work outside of IMP build tree

--- tools/build/setup_all.py
+++ tools/build/setup_all.py
@@ -11,26 +11,31 @@ from optparse import OptionParser
 # main loops
 
 
-def generate_all_cpp(source):
+def generate_all_cpp(modules):
     target = os.path.join("src")
     tools.mkdir(target)
     gen = tools.CPPFileGenerator()
-    for module, g in tools.get_modules(source):
-        sources = tools.get_glob([os.path.join(g, "src", "*.cpp")])\
-            + tools.get_glob([os.path.join(g, "src", "internal", "*.cpp")])
-        targetf = os.path.join(target, module + "_all.cpp")
+    for module in modules:
+        sources = tools.get_glob([os.path.join(module.path, "src", "*.cpp")])\
+                  + tools.get_glob([os.path.join(module.path, "src",
+                                                 "internal", "*.cpp")])
+        targetf = os.path.join(target, module.name + "_all.cpp")
         sources.sort()
         gen.write(targetf, "\n".join(["#include <%s>" %
                                 os.path.abspath(s) for s in sources]) + '\n')
 
 parser = OptionParser()
+parser.add_option("--build_dir", help="IMP build directory", default=None)
 parser.add_option("-s", "--source", dest="source",
                   help="IMP source directory.")
 
 
 def main():
     (options, args) = parser.parse_args()
-    generate_all_cpp(options.source)
+    mf = tools.ModulesFinder(source_dir=options.source,
+                             external_dir=options.build_dir)
+    generate_all_cpp([x for x in mf.values()
+                      if isinstance(x, tools.SourceModule)])
 
 if __name__ == '__main__':
     main()
