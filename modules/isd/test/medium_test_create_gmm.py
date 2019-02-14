ed5ed7d5b53630b0313b7f56f51a70ef4eaf613b
Ben Webb <ben@salilab.org>
2019-02-14 10:17:13 -0800

    Improve test coverage

--- modules/isd/test/medium_test_create_gmm.py
+++ modules/isd/test/medium_test_create_gmm.py
@@ -53,7 +53,27 @@ class Tests(IMP.test.TestCase):
         self.assertEqual(lines[0][:44],
                 '# Created by create_gmm.py, IMP.isd version ')
         self.assertEqual(lines[2], "# ncenters: 4\n")
+        self.assertEqual(lines[13], "# chain: None\n")
         os.unlink('create_gmm_pdb.txt')
+
+    def test_from_pdb_chain(self):
+        """Test create_gmm from a single chain of a PDB file"""
+        try:
+            import sklearn.mixture
+        except ImportError:
+            self.skipTest("this test requires the sklearn Python module")
+        self.run_python_module(create_gmm,
+                [self.get_input_file_name('twoblobsA.pdb'), '4',
+                 'create_gmm_pdb_chain.txt',
+                 '--chain', 'A', '--multiply_by_mass'])
+        with open('create_gmm_pdb_chain.txt') as fh:
+            lines = fh.readlines()
+        self.assertEqual(len(lines), 19)
+        self.assertEqual(lines[0][:44],
+                '# Created by create_gmm.py, IMP.isd version ')
+        self.assertEqual(lines[2], "# ncenters: 4\n")
+        self.assertEqual(lines[13], "# chain: 'A'\n")
+        os.unlink('create_gmm_pdb_chain.txt')
 
     def test_from_mrc(self):
         """Test create_gmm from an MRC file"""
@@ -64,7 +84,8 @@ class Tests(IMP.test.TestCase):
         self.run_python_module(create_gmm,
                 ['--out_map', 'create_gmm_mrc.mrc',
                  self.get_input_file_name('twoblobs-4.0.mrc'), '4',
-                 'create_gmm_mrc.txt'])
+                 'create_gmm_mrc.txt', '--force_weight_frac',
+                 '--use_dirichlet'])
         with open('create_gmm_mrc.txt') as fh:
             lines = fh.readlines()
         self.assertEqual(len(lines), 19)
