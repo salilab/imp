2257627bbf21c2e85e99246cda49938e70bac0d9
Ben Webb <ben@salilab.org>
2019-02-13 16:37:42 -0800

    Add test of create_gmm from a PDB file

--- modules/isd/test/test_create_gmm.py
+++ modules/isd/test/test_create_gmm.py
@@ -4,6 +4,8 @@ import IMP
 import IMP.core
 from IMP.isd import create_gmm
 import IMP.test
+import sys
+import os
 
 class MockArgs(object):
     pass
@@ -29,6 +31,23 @@ class Tests(IMP.test.TestCase):
         args.n_centers = 100
         args.out_file = 'outfile'
         self.assertRaises(Exception, create_gmm.run, args)
+
+    def test_from_pdb(self):
+        """Test create_gmm from a PDB file"""
+        r = self.run_python_module("IMP.isd.create_gmm",
+                [self.get_input_file_name('twoblobsA.pdb'), '4',
+                 'create_gmm_pdb.txt'])
+        out, err = r.communicate()
+        sys.stdout.write(out)
+        sys.stderr.write(err)
+        self.assertEqual(r.returncode, 0)
+        with open('create_gmm_pdb.txt') as fh:
+            lines = fh.readlines()
+        self.assertEqual(len(lines), 19)
+        self.assertEqual(lines[0][:44],
+                '# Created by create_gmm.py, IMP.isd version ')
+        self.assertEqual(lines[2], "# ncenters: 4\n")
+        os.unlink('create_gmm_pdb.txt')
 
 
 if __name__ == '__main__':
