f7d2fab43bf008076d819c251dfb7c1977125b61
Ben Webb <ben@salilab.org>
2019-04-18 12:20:30 -0700

    Add method to get Model's spheres as a NumPy array

--- modules/kernel/pyext/IMP_kernel.numpy.i
+++ modules/kernel/pyext/IMP_kernel.numpy.i
@@ -55,11 +55,49 @@ PyObject *_get_derivatives_numpy(IMP::Model *m, IMP::FloatKey k,
   return NULL;
 #endif
 }
+
+PyObject *_get_spheres_numpy(IMP::Model *m, PyObject *m_pyobj)
+{
+#if IMP_KERNEL_HAS_NUMPY
+  if (!import_numpy_module()) {
+    return NULL;
+  }
+
+  // We treat an array of N spheres as a 4*N 2D array, so make sure the
+  // internal layout of the Sphere3D class matches this assumption
+  BOOST_STATIC_ASSERT(sizeof(algebra::Sphere3D) == 4 * sizeof(double));
+
+  npy_intp dims[2];
+  dims[0] = m->get_number_of_particle_indexes();
+  dims[1] = 4;
+
+  algebra::Sphere3D *data = m->access_spheres_data();
+  PyObject *obj = PyArray_New(&PyArray_Type, 2, dims, NPY_DOUBLE, NULL,
+                              data, 0, NPY_WRITEABLE, NULL);
+  if (!obj) {
+    return NULL;
+  }
+
+  /* Ensure that the Model is kept around as long as the numpy object
+     is alive. */
+  Py_INCREF(m_pyobj);
+  PyArray_BASE(obj) = m_pyobj;
+
+  return obj;
+#else
+  PyErr_SetString(PyExc_NotImplementedError,
+                  "IMP was built without NumPy support");
+  return NULL;
+#endif
+}
 %}
 
 %extend IMP::Model {
   %pythoncode %{
     def _get_derivatives_numpy(self, k):
         return _get_derivatives_numpy(self, k, self)
+
+    def _get_spheres_numpy(self):
+        return _get_spheres_numpy(self, self)
   %}
 }
--- modules/kernel/test/test_model_numpy.py
+++ modules/kernel/test/test_model_numpy.py
@@ -1,5 +1,6 @@
 from __future__ import print_function
 import IMP
+import IMP.core
 import IMP.test
 
 class Tests(IMP.test.TestCase):
@@ -20,6 +21,39 @@ class Tests(IMP.test.TestCase):
             self.assertAlmostEqual(p1.get_derivative(k), 42.0, delta=1e-6)
         else:
             self.assertRaises(NotImplementedError, m._get_derivatives_numpy, k)
+
+    def test_get_sphere_numpy(self):
+        """Test _get_spheres_numpy method"""
+        m = IMP.Model("score state show")
+        p1 = IMP.Particle(m)
+        p2 = IMP.Particle(m)
+        p3 = IMP.Particle(m)
+
+        d1 = IMP.core.XYZR.setup_particle(p1)
+        d1.set_coordinates(IMP.algebra.Vector3D(1,2,3))
+        d1.set_radius(4)
+
+        d2 = IMP.core.XYZR.setup_particle(p2)
+        d2.set_coordinates(IMP.algebra.Vector3D(5,6,7))
+        d2.set_radius(8)
+
+        if IMP.IMP_KERNEL_HAS_NUMPY:
+            n = m._get_spheres_numpy()
+            self.assertAlmostEqual(n[0][0], 4.0, delta=1e-4)
+            self.assertAlmostEqual(n[0][1], 1.0, delta=1e-4)
+            self.assertAlmostEqual(n[0][2], 2.0, delta=1e-4)
+            self.assertAlmostEqual(n[0][3], 3.0, delta=1e-4)
+
+            self.assertAlmostEqual(n[1][0], 8.0, delta=1e-4)
+            self.assertAlmostEqual(n[1][1], 5.0, delta=1e-4)
+            self.assertAlmostEqual(n[1][2], 6.0, delta=1e-4)
+            self.assertAlmostEqual(n[1][3], 7.0, delta=1e-4)
+            n[0][0] = 42.0
+            self.assertAlmostEqual(d1.get_radius(), 42.0, delta=1e-6)
+            n[1][1] = 24.0
+            self.assertAlmostEqual(d2.get_coordinates()[0], 24.0, delta=1e-6)
+        else:
+            self.assertRaises(NotImplementedError, m._get_spheres_numpy, k)
 
 
 if __name__ == '__main__':
