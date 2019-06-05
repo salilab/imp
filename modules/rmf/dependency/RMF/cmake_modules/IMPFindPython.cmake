c411bba08f71e69f35770bce21a5ca5ccfca8523
Ben Webb <ben@salilab.org>
2019-06-05 09:57:27 -0700

    Squashed 'modules/rmf/dependency/RMF/' changes from 6c4573b648..e0b3df9052
    e0b3df9052 Allow overriding Python executable for tests
    56573ef010 Populate PYTHON_VERSION* cmake cache variables
    
    git-subtree-dir: modules/rmf/dependency/RMF
    git-subtree-split: e0b3df905208144be3b25eba56585cc37f470703

--- modules/rmf/dependency/RMF/cmake_modules/IMPFindPython.cmake
+++ modules/rmf/dependency/RMF/cmake_modules/IMPFindPython.cmake
@@ -9,12 +9,21 @@ function(imp_find_python)
 		      OUTPUT_VARIABLE python_full_path
                       OUTPUT_STRIP_TRAILING_WHITESPACE)
       set(PYTHON_EXECUTABLE ${python_full_path} CACHE INTERNAL "" FORCE)
+      set(PYTHON_TEST_EXECUTABLE ${python_full_path} CACHE STRING "")
       execute_process(COMMAND ${PYTHON_EXECUTABLE} -c "import sys; print('%d.%d.%d' % sys.version_info[:3])"
                       WORKING_DIRECTORY ${PROJECT_BINARY_DIR}
                       OUTPUT_VARIABLE python_full_version
                       OUTPUT_STRIP_TRAILING_WHITESPACE)
-      string(REGEX REPLACE "^([0-9]+\\.[0-9]+).*" "\\1" python_version
+      string(REGEX REPLACE "^([0-9])+\\.[0-9]+.*" "\\1" major
              "${python_full_version}")
+      string(REGEX REPLACE "^[0-9]+\\.([0-9]+).*" "\\1" minor
+             "${python_full_version}")
+      string(REGEX REPLACE "^[0-9]+\\.[0-9]+\\.([0-9]+).*" "\\1" patch
+             "${python_full_version}")
+      set(PYTHON_VERSION ${python_full_version} CACHE INTERNAL "" FORCE)
+      set(PYTHON_VERSION_MAJOR ${major} CACHE INTERNAL "" FORCE)
+      set(PYTHON_VERSION_MINOR ${minor} CACHE INTERNAL "" FORCE)
+      set(PYTHON_VERSION_PATCH ${patch} CACHE INTERNAL "" FORCE)
       message(STATUS "Python binary is " ${python_full_path} " (version " ${python_full_version} ")")
 
       find_package(PythonLibs ${python_full_version} EXACT REQUIRED)
@@ -52,21 +61,34 @@ function(imp_find_python)
        AND Python3_Interpreter_FOUND AND Python3_Development_FOUND)
       # Use Python 3 tools
       set(PYTHON_EXECUTABLE ${Python3_EXECUTABLE} CACHE INTERNAL "" FORCE)
+      set(PYTHON_TEST_EXECUTABLE ${Python3_EXECUTABLE} CACHE STRING "")
       set(PYTHON_LIBRARIES ${Python3_LIBRARIES} CACHE INTERNAL "" FORCE)
       set(PYTHON_INCLUDE_DIRS ${Python3_INCLUDE_DIRS} CACHE INTERNAL "" FORCE)
       set(PYTHON_LIBRARY_DIRS ${Python3_LIBRARY_DIRS} CACHE INTERNAL "" FORCE)
       set(PYTHON_NUMPY_FOUND ${Python3_NumPy_FOUND} CACHE INTERNAL "" FORCE)
       set(PYTHON_NUMPY_INCLUDE_DIR ${Python3_NumPy_INCLUDE_DIRS}
           CACHE INTERNAL "" FORCE)
+      set(PYTHON_VERSION ${Python3_VERSION} CACHE INTERNAL "" FORCE)
+      set(PYTHON_VERSION_MAJOR ${Python3_VERSION_MAJOR} CACHE INTERNAL "" FORCE)
+      set(PYTHON_VERSION_MINOR ${Python3_VERSION_MINOR} CACHE INTERNAL "" FORCE)
+      set(PYTHON_VERSION_PATCH ${Python3_VERSION_PATCH} CACHE INTERNAL "" FORCE)
     else()
       find_package(Python2 COMPONENTS Interpreter Development NumPy)
       if(Python2_Interpreter_FOUND AND Python2_Development_FOUND)
         set(PYTHON_EXECUTABLE ${Python2_EXECUTABLE} CACHE INTERNAL "" FORCE)
+        set(PYTHON_TEST_EXECUTABLE ${Python2_EXECUTABLE} CACHE STRING "")
         set(PYTHON_LIBRARIES ${Python2_LIBRARIES} CACHE INTERNAL "" FORCE)
         set(PYTHON_INCLUDE_DIRS ${Python2_INCLUDE_DIRS} CACHE INTERNAL "" FORCE)
         set(PYTHON_LIBRARY_DIRS ${Python2_LIBRARY_DIRS} CACHE INTERNAL "" FORCE)
         set(PYTHON_NUMPY_FOUND ${Python2_NumPy_FOUND} CACHE INTERNAL "" FORCE)
         set(PYTHON_NUMPY_INCLUDE_DIR ${Python2_NumPy_INCLUDE_DIRS}
+            CACHE INTERNAL "" FORCE)
+        set(PYTHON_VERSION ${Python2_VERSION} CACHE INTERNAL "" FORCE)
+        set(PYTHON_VERSION_MAJOR ${Python2_VERSION_MAJOR}
+            CACHE INTERNAL "" FORCE)
+        set(PYTHON_VERSION_MINOR ${Python2_VERSION_MINOR}
+            CACHE INTERNAL "" FORCE)
+        set(PYTHON_VERSION_PATCH ${Python2_VERSION_PATCH}
             CACHE INTERNAL "" FORCE)
       else()
         message(FATAL_ERROR "Could not find a Python interpreter and matching headers/libraries. Python is required to build.")
