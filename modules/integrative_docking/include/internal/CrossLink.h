346475a16b50707aac03da29745cf612dc7a2309
dina <dina@cs.huji.ac.il>
2019-09-03 17:00:53 +0300

    add xlink weight support

--- modules/integrative_docking/include/internal/CrossLink.h
+++ modules/integrative_docking/include/internal/CrossLink.h
@@ -24,10 +24,11 @@ class CrossLink {
         residue_number2_(0),
         chain_id2_("-"),
         min_distance_(0.0),
-        max_distance_(0.0) {}
+        max_distance_(0.0),
+        weight_(1.0) {}
 
   CrossLink(int residue_number1, std::string chain_id1, int residue_number2,
-            std::string chain_id2, float min_dist, float max_dist,
+            std::string chain_id2, float min_dist, float max_dist, float weight = 1.0,
             float actual_distance = 0.0, float actual_cb_distance = 0.0)
       : residue_number1_(residue_number1),
         chain_id1_(chain_id1),
@@ -36,7 +37,8 @@ class CrossLink {
         min_distance_(min_dist),
         max_distance_(max_dist),
         actual_distance_(actual_distance),
-        actual_cb_distance_(actual_cb_distance) {}
+        actual_cb_distance_(actual_cb_distance),
+        weight_(weight) {}
 
   int get_residue1() const { return residue_number1_; }
   int get_residue2() const { return residue_number2_; }
@@ -48,6 +50,8 @@ class CrossLink {
   float get_max_distance() const { return max_distance_; }
   float get_actual_distance() const { return actual_distance_; }
   float get_actual_cb_distance() const { return actual_cb_distance_; }
+
+  float get_weight() const { return weight_; }
 
   friend std::ostream& operator<<(std::ostream& q, const CrossLink& cl);
   friend std::istream& operator>>(std::istream& s, CrossLink& cl);
@@ -61,6 +65,7 @@ class CrossLink {
   float max_distance_;
   float actual_distance_;     // from PDB - for testing purposes
   float actual_cb_distance_;  // from PDB - for testing purposes
+  float weight_;
 };
 
 IMPINTEGRATIVEDOCKINGEXPORT
--- modules/integrative_docking/src/internal/CrossLink.cpp
+++ modules/integrative_docking/src/internal/CrossLink.cpp
@@ -19,7 +19,11 @@ int read_cross_link_file(const std::string& file_name,
               IMP::IOException);
   }
   CrossLink cl;
-  while (s >> cl) cross_links.push_back(cl);
+  while(s) {
+    if( s >> cl) cross_links.push_back(cl);
+  }
+  std::cout << cross_links.size() << " cross links were read from file "
+            << file_name << std::endl;
   return cross_links.size();
 }
 
@@ -48,7 +52,7 @@ std::ostream& operator<<(std::ostream& s, const CrossLink& cl) {
     s << "-";
   else
     s << cl.chain_id2_;
-  s << " " << cl.min_distance_ << " " << cl.max_distance_;
+  s << " " << cl.min_distance_ << " " << cl.max_distance_ << " " << cl.weight_;
   return s;
 }
 
@@ -57,6 +61,14 @@ std::istream& operator>>(std::istream& s, CrossLink& cl) {
       cl.chain_id2_ >> cl.min_distance_ >> cl.max_distance_;
   if (cl.chain_id1_ == "-") cl.chain_id1_ = " ";
   if (cl.chain_id2_ == "-") cl.chain_id2_ = " ";
+
+  int c = s.peek();
+  if(c == ' ') { // read weight
+    s >> cl.weight_;
+  } else {
+    cl.weight_ = 1.0;
+  }
+
   return s;
 }
 
