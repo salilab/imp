04681809d829253de66692b10e85dcb3a990ddb2
Ben Webb <ben@salilab.org>
2020-01-22 11:44:31 -0800

    Squashed 'modules/rmf/dependency/RMF/' changes from 26e79726f0..3b2178bf23
    3b2178bf23 Fix for HDF5 1.10.0 and 1.10.1
    
    git-subtree-dir: modules/rmf/dependency/RMF
    git-subtree-split: 3b2178bf2306d8fdf15ff6f30b982a9547f95855

--- modules/rmf/dependency/RMF/src/hdf5_wrapper.cpp
+++ modules/rmf/dependency/RMF/src/hdf5_wrapper.cpp
@@ -137,13 +137,12 @@ hid_t get_parameters() {
   RMF_HDF5_CALL(H5Pset_sieve_buf_size(plist, 1000000));
   RMF_HDF5_CALL(H5Pset_cache(plist, 0, 10000, 10000000, 0.0));
 #if defined(H5_VERS_MAJOR) && \
-    ((H5_VERS_MAJOR == 1 && H5_VERS_MINOR == 8 && H5_VERS_RELEASE >= 6) || \
-     (H5_VERS_MAJOR == 1 && H5_VERS_MINOR > 8) || H5_VERS_MAJOR > 1)
-#if H5_VERS_MAJOR == 1 && H5_VERS_MINOR == 8
-  RMF_HDF5_CALL(H5Pset_libver_bounds(plist, H5F_LIBVER_18, H5F_LIBVER_LATEST));
-#else
+    ((H5_VERS_MAJOR == 1 && H5_VERS_MINOR == 10  && H5_VERS_RELEASE >= 2) || \
+     (H5_VERS_MAJOR == 1 && H5_VERS_MINOR > 10) || H5_VERS_MAJOR > 1)
   RMF_HDF5_CALL(H5Pset_libver_bounds(plist, H5F_LIBVER_V18, H5F_LIBVER_LATEST));
-#endif
+#elif defined(H5_VERS_MAJOR) && \
+    H5_VERS_MAJOR == 1 && H5_VERS_MINOR == 8 && H5_VERS_RELEASE >= 6
+  RMF_HDF5_CALL(H5Pset_libver_bounds(plist, H5F_LIBVER_18, H5F_LIBVER_LATEST));
 #endif
   return plist;
 }
