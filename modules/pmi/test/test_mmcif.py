875c735310f05cdf934425ea96137bfbe76af379
Ben Webb <ben@salilab.org>
2020-01-31 10:40:39 -0800

    Squashed 'modules/pmi/' changes from 2028e48a8e..42c59c9da1
    42c59c9da1 Update outputs to match latest python-ihm
    
    git-subtree-dir: modules/pmi
    git-subtree-split: 42c59c9da15ba96442bb922f6539bfa060a07cf1

--- modules/pmi/test/test_mmcif.py
+++ modules/pmi/test/test_mmcif.py
@@ -1273,8 +1273,9 @@ _ihm_cross_link_restraint.distance_threshold
 _ihm_cross_link_restraint.psi
 _ihm_cross_link_restraint.sigma_1
 _ihm_cross_link_restraint.sigma_2
+_ihm_cross_link_restraint.pseudo_site_flag
 1 1 1 A 1 MET 1 A 2 GLU . . 'upper bound' ALL by-residue 42.000 0.800 1.000
-0.500
+0.500 NO
 #
 """)
 
