731656d855c5de12827a59a1ebbaf232f483f2e4
Ben Webb <ben@salilab.org>
2020-02-11 16:13:58 -0800

    Squashed 'modules/pmi/' changes from da6c2492c2..fc2636bcaa
    fc2636bcaa Also write out the canonical sequence ID
    e83664d28d Add basic sequence info to mmCIF output
    e6ce162875 Take mmcif flag at init_pdb time
    6803aef44c Test multi-character chain ID output
    aa8527c347 Add basic mmCIF support to IMP.pmi.output
    63c50b957e Handle duplicate or blank chains in PDB output
    
    git-subtree-dir: modules/pmi
    git-subtree-split: fc2636bcaab7648ce76dae570fc5ad3a22145d26

--- modules/pmi/pyext/src/output.py
+++ modules/pmi/pyext/src/output.py
@@ -15,7 +15,11 @@ import ast
 import RMF
 import numpy as np
 import operator
+import itertools
+import warnings
 import string
+import ihm.format
+import collections
 try:
     import cPickle as pickle
 except ImportError:
@@ -57,6 +61,140 @@ def _flatten(seq):
         else:
             yield elt
 
+
+def _disambiguate_chain(chid, seen_chains):
+    """Make sure that the chain ID is unique; warn and correct if it isn't"""
+    # Handle null chain IDs
+    if chid == '\0':
+        chid = ' '
+
+    if chid in seen_chains:
+        warnings.warn("Duplicate chain ID '%s' encountered" % chid,
+                      IMP.pmi.StructureWarning)
+
+        for suffix in itertools.count(1):
+            new_chid = chid + "%d" % suffix
+            if new_chid not in seen_chains:
+                seen_chains.add(new_chid)
+                return new_chid
+    seen_chains.add(chid)
+    return chid
+
+
+def _write_pdb_internal(flpdb, particle_infos_for_pdb, geometric_center,
+                        write_all_residues_per_bead):
+    for n,tupl in enumerate(particle_infos_for_pdb):
+        (xyz, atom_type, residue_type,
+         chain_id, residue_index, all_indexes, radius) = tupl
+        if atom_type is None:
+            atom_type = IMP.atom.AT_CA
+        if write_all_residues_per_bead and all_indexes is not None:
+            for residue_number in all_indexes:
+                flpdb.write(
+                    IMP.atom.get_pdb_string((xyz[0] - geometric_center[0],
+                                             xyz[1] - geometric_center[1],
+                                             xyz[2] - geometric_center[2]),
+                                            n+1, atom_type, residue_type,
+                                            chain_id[:1], residue_number, ' ',
+                                            1.00, radius))
+        else:
+            flpdb.write(
+                IMP.atom.get_pdb_string((xyz[0] - geometric_center[0],
+                                         xyz[1] - geometric_center[1],
+                                         xyz[2] - geometric_center[2]),
+                                        n+1, atom_type, residue_type,
+                                        chain_id[:1], residue_index, ' ',
+                                        1.00, radius))
+    flpdb.write("ENDMDL\n")
+
+
+_Entity = collections.namedtuple('_Entity', ('id', 'seq'))
+_ChainInfo = collections.namedtuple('_ChainInfo', ('entity', 'name'))
+
+
+def _get_chain_info(chains, root_hier):
+    chain_info = {}
+    entities = {}
+    all_entities = []
+    for mol in IMP.atom.get_by_type(root_hier, IMP.atom.MOLECULE_TYPE):
+        molname = IMP.pmi.get_molecule_name_and_copy(mol)
+        chain_id = chains[molname]
+        chain = IMP.atom.Chain(mol)
+        seq = chain.get_sequence()
+        if seq not in entities:
+            entities[seq] = e = _Entity(id=len(entities)+1, seq=seq)
+            all_entities.append(e)
+        entity = entities[seq]
+        info = _ChainInfo(entity=entity, name=molname)
+        chain_info[chain_id] = info
+    return chain_info, all_entities
+
+
+def _write_mmcif_internal(flpdb, particle_infos_for_pdb, geometric_center,
+                          write_all_residues_per_bead, chains, root_hier):
+    # get dict with keys=chain IDs, values=chain info
+    chain_info, entities = _get_chain_info(chains, root_hier)
+
+    writer = ihm.format.CifWriter(flpdb)
+    writer.start_block('model')
+    with writer.category("_entry") as l:
+        l.write(id='model')
+
+    with writer.loop("_entity", ["id"]) as l:
+        for e in entities:
+            l.write(id=e.id)
+
+    with writer.loop("_entity_poly",
+                     ["entity_id", "pdbx_seq_one_letter_code"]) as l:
+        for e in entities:
+            l.write(entity_id=e.id, pdbx_seq_one_letter_code=e.seq)
+
+    with writer.loop("_struct_asym", ["id", "entity_id", "details"]) as l:
+        for chid in sorted(chains.values()):
+            ci = chain_info[chid]
+            l.write(id=chid, entity_id=ci.entity.id, details=ci.name)
+
+    with writer.loop("_atom_site",
+                     ["group_PDB", "type_symbol", "label_atom_id",
+                      "label_comp_id", "label_asym_id", "label_seq_id",
+                      "auth_seq_id",
+                      "Cartn_x", "Cartn_y", "Cartn_z", "label_entity_id",
+                      "pdbx_pdb_model_num",
+                      "id"]) as l:
+        ordinal = 1
+        for n,tupl in enumerate(particle_infos_for_pdb):
+            (xyz, atom_type, residue_type,
+             chain_id, residue_index, all_indexes, radius) = tupl
+            ci = chain_info[chain_id]
+            if atom_type is None:
+                atom_type = IMP.atom.AT_CA
+            c = xyz - geometric_center
+            if write_all_residues_per_bead and all_indexes is not None:
+                for residue_number in all_indexes:
+                    l.write(group_PDB='ATOM',
+                            type_symbol='C',
+                            label_atom_id=atom_type.get_string(),
+                            label_comp_id=residue_type.get_string(),
+                            label_asym_id=chain_id,
+                            label_seq_id=residue_index,
+                            auth_seq_id=residue_index, Cartn_x=c[0],
+                            Cartn_y=c[1], Cartn_z=c[2], id=ordinal,
+                            pdbx_pdb_model_num=1,
+                            label_entity_id=ci.entity.id)
+                    ordinal += 1
+            else:
+                l.write(group_PDB='ATOM', type_symbol='C',
+                        label_atom_id=atom_type.get_string(),
+                        label_comp_id=residue_type.get_string(),
+                        label_asym_id=chain_id,
+                        label_seq_id=residue_index,
+                        auth_seq_id=residue_index, Cartn_x=c[0],
+                        Cartn_y=c[1], Cartn_z=c[2], id=ordinal,
+                        pdbx_pdb_model_num=1,
+                        label_entity_id=ci.entity.id)
+                ordinal += 1
+
+
 class Output(object):
     """Class for easy writing of PDBs, RMFs, and stat files
 
@@ -64,6 +202,7 @@ class Output(object):
     """
     def __init__(self, ascii=True,atomistic=False):
         self.dictionary_pdbs = {}
+        self._pdb_mmcif = {}
         self.dictionary_rmfs = {}
         self.dictionary_stats = {}
         self.dictionary_stats2 = {}
@@ -96,28 +235,32 @@ class Output(object):
     def _init_dictchain(self, name, prot, multichar_chain=False):
         self.dictchain[name] = {}
         self.use_pmi2 = False
+        seen_chains = set()
 
         # attempt to find PMI objects.
         if IMP.pmi.get_is_canonical(prot):
             self.use_pmi2 = True
             self.atomistic = True #detects automatically
             for n,mol in enumerate(IMP.atom.get_by_type(prot,IMP.atom.MOLECULE_TYPE)):
-                chid = IMP.atom.Chain(mol).get_id()
+                chid = _disambiguate_chain(IMP.atom.Chain(mol).get_id(),
+                                           seen_chains)
                 self.dictchain[name][IMP.pmi.get_molecule_name_and_copy(mol)] = chid
         else:
             chainids = self.multi_chainids if multichar_chain else self.chainids
             for n, i in enumerate(self.dictionary_pdbs[name].get_children()):
                 self.dictchain[name][i.get_name()] = chainids[n]
 
-    def init_pdb(self, name, prot):
+    def init_pdb(self, name, prot, mmcif=False):
         """Init PDB Writing.
         @param name The PDB filename
         @param prot The hierarchy to write to this pdb file
+        @param mmcif If True, write PDBs in mmCIF format
         @note if the PDB name is 'System' then will use Selection to get molecules
         """
         flpdb = open(name, 'w')
         flpdb.close()
         self.dictionary_pdbs[name] = prot
+        self._pdb_mmcif[name] = mmcif
         self._init_dictchain(name, prot)
 
     def write_psf(self,filename,name):
@@ -168,10 +311,6 @@ class Output(object):
                   appendmode=True,
                   translate_to_geometric_center=False,
                   write_all_residues_per_bead=False):
-        if appendmode:
-            flpdb = open(name, 'a')
-        else:
-            flpdb = open(name, 'w')
 
         (particle_infos_for_pdb,
          geometric_center) = self.get_particle_infos_for_pdb_writing(name)
@@ -179,28 +318,18 @@ class Output(object):
         if not translate_to_geometric_center:
             geometric_center = (0, 0, 0)
 
-        for n,tupl in enumerate(particle_infos_for_pdb):
-            (xyz, atom_type, residue_type,
-             chain_id, residue_index, all_indexes, radius) = tupl
-            if atom_type is None:
-                atom_type = IMP.atom.AT_CA
-            if ( (write_all_residues_per_bead) and (all_indexes is not None) ):
-                for residue_number in all_indexes:
-                    flpdb.write(IMP.atom.get_pdb_string((xyz[0] - geometric_center[0],
-                                                        xyz[1] - geometric_center[1],
-                                                        xyz[2] - geometric_center[2]),
-                                                        n+1, atom_type, residue_type,
-                                                        chain_id, residue_number,' ',1.00,radius))
+        filemode = 'a' if appendmode else 'w'
+        with open(name, filemode) as flpdb:
+            if self._pdb_mmcif[name]:
+                _write_mmcif_internal(flpdb, particle_infos_for_pdb,
+                                      geometric_center,
+                                      write_all_residues_per_bead,
+                                      self.dictchain[name],
+                                      self.dictionary_pdbs[name])
             else:
-                flpdb.write(IMP.atom.get_pdb_string((xyz[0] - geometric_center[0],
-                                                    xyz[1] - geometric_center[1],
-                                                    xyz[2] - geometric_center[2]),
-                                                    n+1, atom_type, residue_type,
-                                                    chain_id, residue_index,' ',1.00,radius))
-        flpdb.write("ENDMDL\n")
-        flpdb.close()
-
-        del particle_infos_for_pdb
+                _write_pdb_internal(flpdb, particle_infos_for_pdb,
+                                    geometric_center,
+                                    write_all_residues_per_bead)
 
     def get_prot_name_from_particle(self, name, p):
         """Get the protein name from the particle.
@@ -319,7 +448,7 @@ class Output(object):
         return (particle_infos_for_pdb, geometric_center)
 
 
-    def write_pdbs(self, appendmode=True):
+    def write_pdbs(self, appendmode=True, mmcif=False):
         for pdb in self.dictionary_pdbs.keys():
             self.write_pdb(pdb, appendmode)
 
@@ -327,10 +456,12 @@ class Output(object):
                               suffix,
                               prot,
                               nbestscoring,
-                              replica_exchange=False):
+                              replica_exchange=False, mmcif=False):
         # save only the nbestscoring conformations
         # create as many pdbs as needed
 
+        self._pdb_best_scoring_mmcif = mmcif
+        fileext = '.cif' if mmcif else '.pdb'
         self.suffixes.append(suffix)
         self.replica_exchange = replica_exchange
         if not self.replica_exchange:
@@ -349,16 +480,19 @@ class Output(object):
 
         self.nbestscoring = nbestscoring
         for i in range(self.nbestscoring):
-            name = suffix + "." + str(i) + ".pdb"
+            name = suffix + "." + str(i) + fileext
             flpdb = open(name, 'w')
             flpdb.close()
             self.dictionary_pdbs[name] = prot
+            self._pdb_mmcif[name] = mmcif
             self._init_dictchain(name, prot)
 
     def write_pdb_best_scoring(self, score):
         if self.nbestscoring is None:
             print("Output.write_pdb_best_scoring: init_pdb_best_scoring not run")
 
+        mmcif = self._pdb_best_scoring_mmcif
+        fileext = '.cif' if mmcif else '.pdb'
         # update the score list
         if self.replica_exchange:
             # read the self.best_score_list from the file
@@ -371,13 +505,13 @@ class Output(object):
             index = self.best_score_list.index(score)
             for suffix in self.suffixes:
                 for i in range(len(self.best_score_list) - 2, index - 1, -1):
-                    oldname = suffix + "." + str(i) + ".pdb"
-                    newname = suffix + "." + str(i + 1) + ".pdb"
+                    oldname = suffix + "." + str(i) + fileext
+                    newname = suffix + "." + str(i + 1) + fileext
                     # rename on Windows fails if newname already exists
                     if os.path.exists(newname):
                         os.unlink(newname)
                     os.rename(oldname, newname)
-                filetoadd = suffix + "." + str(index) + ".pdb"
+                filetoadd = suffix + "." + str(index) + fileext
                 self.write_pdb(filetoadd, appendmode=False)
 
         else:
@@ -388,13 +522,13 @@ class Output(object):
                 index = self.best_score_list.index(score)
                 for suffix in self.suffixes:
                     for i in range(len(self.best_score_list) - 1, index - 1, -1):
-                        oldname = suffix + "." + str(i) + ".pdb"
-                        newname = suffix + "." + str(i + 1) + ".pdb"
+                        oldname = suffix + "." + str(i) + fileext
+                        newname = suffix + "." + str(i + 1) + fileext
                         os.rename(oldname, newname)
                     filenametoremove = suffix + \
-                        "." + str(self.nbestscoring) + ".pdb"
+                        "." + str(self.nbestscoring) + fileext
                     os.remove(filenametoremove)
-                    filetoadd = suffix + "." + str(index) + ".pdb"
+                    filetoadd = suffix + "." + str(index) + fileext
                     self.write_pdb(filetoadd, appendmode=False)
 
         if self.replica_exchange:
--- modules/pmi/test/test_pdb_writing.py
+++ modules/pmi/test/test_pdb_writing.py
@@ -6,6 +6,7 @@ import IMP.pmi.output
 import IMP.pmi.topology
 import IMP.pmi.dof
 import IMP.pmi.macros
+import ihm.reader
 import os,shutil
 
 try:
@@ -16,8 +17,7 @@ except ImportError:
 
 class Tests(IMP.test.TestCase):
 
-    def test_pdb_pmi2(self):
-        """Test PDB writing in PMI2"""
+    def _make_test_model(self):
         mdl = IMP.Model()
         pdb_file = self.get_input_file_name("mini.pdb")
         fasta_file = self.get_input_file_name("mini.fasta")
@@ -36,11 +36,59 @@ class Tests(IMP.test.TestCase):
         molB.add_representation(aresB,[10])
         molB.add_representation(molB[:]-aresB,20)
 
-        molC = molB.create_copy(chain_id='C')
+        # Multi-character chain ID
+        molC = molB.create_copy(chain_id='CA')
         aresC = molC.add_structure(pdb_file,chain_id='B',soft_check=True)
         molC.add_representation(aresC,[1,10])
         molC.add_representation(molC[:]-aresC,2)
+
+        # Duplicate chain ID
+        molA2 = molB.create_copy(chain_id='A')
+        aresA2 = molA2.add_structure(pdb_file,chain_id='B',soft_check=True)
+        molA2.add_representation(aresA2,[1,10])
+        molA2.add_representation(molA2[:]-aresA2,2)
+
+        # No chain ID
+        molA3 = molB.create_copy(chain_id='')
+        aresA3 = molA3.add_structure(pdb_file,chain_id='B',soft_check=True)
+        molA3.add_representation(aresA3,[1,10])
+        molA3.add_representation(molA3[:]-aresA3,2)
+
+        # Blank chain ID
+        molA4 = molA.create_copy(chain_id=' ')
+        aresA4 = molA4.add_structure(pdb_file,chain_id='A',soft_check=True)
+        molA4.add_representation(aresA4,[1])
+        molA4.add_representation(molA4[:]-aresA4,20)
+
         root_hier = s.build()
+        return mdl, root_hier
+
+    def test_pdb_mmcif(self):
+        """Test PDB writing with mmcif=True"""
+        mdl, root_hier = self._make_test_model()
+
+        # write PDB and check it's ok
+        output = IMP.pmi.output.Output(atomistic=True)
+        output.init_pdb("test_pdb_writing.cif", root_hier, mmcif=True)
+        output.write_pdbs()
+        print('init best scoring')
+        output.init_pdb_best_scoring("test_pdb_writing", root_hier, 10, mmcif=True)
+        print('scoring')
+        for i in range(20):
+            score = -float(i)
+            output.write_pdb_best_scoring(score)
+
+        with open('test_pdb_writing.cif') as fh:
+            s, = ihm.reader.read(fh)
+        self.assertEqual([x.id for x in s.asym_units], [' ', ' 1', 'A', 'A1', 'B', 'CA'])
+
+        for i in range(10):
+            os.unlink('test_pdb_writing.'+str(i)+'.cif')
+        os.unlink('test_pdb_writing.cif')
+
+    def test_pdb_pmi2(self):
+        """Test PDB writing in PMI2"""
+        mdl, root_hier = self._make_test_model()
 
         # write PDB and check it's ok
         output = IMP.pmi.output.Output(atomistic=True)
@@ -53,23 +101,43 @@ class Tests(IMP.test.TestCase):
             score = -float(i)
             output.write_pdb_best_scoring(score)
 
-        pdb_content='''ATOM      1  CA  MET A   1     114.370  27.980 -26.088  1.00  3.07           C
-ATOM      2  CA  VAL A   2     114.370  27.980 -26.088  1.00  2.89           C
-ATOM      3  CA  GLY A   3     111.506  26.368 -28.075  1.00  2.27           C
-ATOM      4  CA  GLN A   4     113.468  23.113 -28.639  1.00  3.01           C
-ATOM      5  CA  GLN A   5     113.808  21.534 -32.168  1.00  3.01           C
-ATOM      6  CA  TYR A   6     116.743  22.770 -34.259  1.00  3.32           C
-ATOM      7  CA  SER A   7     116.626  25.161 -37.229  1.00  2.61           C
-ATOM      8  CA  SER A   8     119.165  25.590 -40.036  1.00  2.61           C
-ATOM      9  CA  BEA B   2      70.427  58.819  51.717  1.00  3.50           C
-ATOM     10  CA  BEA B   4      68.799  58.791  50.131  1.00  3.69           C
-ATOM     11  CA  BEA B   7      68.584  58.274  48.425  1.00  4.76           C
-ATOM     12  CA  BEA C   2      70.427  58.819  51.717  1.00  3.50           C
-ATOM     13  CA  ASP C   3      70.427  58.819  51.717  1.00  2.87           C
-ATOM     14  CA  GLU C   4      68.584  58.274  48.425  1.00  3.00           C
-ATOM     15  CA  BEA C   6      68.584  58.274  48.425  1.00  3.50           C
-ATOM     16  CA  BEA C   8      68.584  58.274  48.425  1.00  3.50           C
-ATOM     17  CA  THR C   9      68.584  58.274  48.425  1.00  2.80           C
+        pdb_content='''ATOM      1  CA  BEA     2      70.427  58.819  51.717  1.00  3.50           C
+ATOM      2  CA  ASP     3      70.427  58.819  51.717  1.00  2.87           C
+ATOM      3  CA  GLU     4      68.584  58.274  48.425  1.00  3.00           C
+ATOM      4  CA  BEA     6      68.584  58.274  48.425  1.00  3.50           C
+ATOM      5  CA  BEA     8      68.584  58.274  48.425  1.00  3.50           C
+ATOM      6  CA  THR     9      68.584  58.274  48.425  1.00  2.80           C
+ATOM      7  CA  MET A   1     114.370  27.980 -26.088  1.00  3.07           C
+ATOM      8  CA  VAL A   2     114.370  27.980 -26.088  1.00  2.89           C
+ATOM      9  CA  GLY A   3     111.506  26.368 -28.075  1.00  2.27           C
+ATOM     10  CA  GLN A   4     113.468  23.113 -28.639  1.00  3.01           C
+ATOM     11  CA  GLN A   5     113.808  21.534 -32.168  1.00  3.01           C
+ATOM     12  CA  TYR A   6     116.743  22.770 -34.259  1.00  3.32           C
+ATOM     13  CA  SER A   7     116.626  25.161 -37.229  1.00  2.61           C
+ATOM     14  CA  SER A   8     119.165  25.590 -40.036  1.00  2.61           C
+ATOM     15  CA  BEA B   2      70.427  58.819  51.717  1.00  3.50           C
+ATOM     16  CA  BEA B   4      68.799  58.791  50.131  1.00  3.69           C
+ATOM     17  CA  BEA B   7      68.584  58.274  48.425  1.00  4.76           C
+ATOM     18  CA  MET     1     114.370  27.980 -26.088  1.00  3.07           C
+ATOM     19  CA  VAL     2     114.370  27.980 -26.088  1.00  2.89           C
+ATOM     20  CA  GLY     3     111.506  26.368 -28.075  1.00  2.27           C
+ATOM     21  CA  GLN     4     113.468  23.113 -28.639  1.00  3.01           C
+ATOM     22  CA  GLN     5     113.808  21.534 -32.168  1.00  3.01           C
+ATOM     23  CA  TYR     6     116.743  22.770 -34.259  1.00  3.32           C
+ATOM     24  CA  SER     7     116.626  25.161 -37.229  1.00  2.61           C
+ATOM     25  CA  SER     8     119.165  25.590 -40.036  1.00  2.61           C
+ATOM     26  CA  BEA A   2      70.427  58.819  51.717  1.00  3.50           C
+ATOM     27  CA  ASP A   3      70.427  58.819  51.717  1.00  2.87           C
+ATOM     28  CA  GLU A   4      68.584  58.274  48.425  1.00  3.00           C
+ATOM     29  CA  BEA A   6      68.584  58.274  48.425  1.00  3.50           C
+ATOM     30  CA  BEA A   8      68.584  58.274  48.425  1.00  3.50           C
+ATOM     31  CA  THR A   9      68.584  58.274  48.425  1.00  2.80           C
+ATOM     32  CA  BEA C   2      70.427  58.819  51.717  1.00  3.50           C
+ATOM     33  CA  ASP C   3      70.427  58.819  51.717  1.00  2.87           C
+ATOM     34  CA  GLU C   4      68.584  58.274  48.425  1.00  3.00           C
+ATOM     35  CA  BEA C   6      68.584  58.274  48.425  1.00  3.50           C
+ATOM     36  CA  BEA C   8      68.584  58.274  48.425  1.00  3.50           C
+ATOM     37  CA  THR C   9      68.584  58.274  48.425  1.00  2.80           C
 ENDMDL'''.split("\n")
 
         with open("test_pdb_writing.pdb") as f:
