dd2b16d6317fc897db895b47aa53a8a436bb907d
Ben Webb <ben@salilab.org>
2020-03-16 22:16:55 -0700

    Fix path to kernel 'module'

--- tools/debian/make-imp-install.py
+++ tools/debian/make-imp-install.py
@@ -4,7 +4,7 @@ import sys
 import os
 
 all_modules = sorted(d for d in os.listdir('.') if os.path.isdir(d))
-exclude_modules = frozenset(('mpi', 'spb', 'scratch'))
+exclude_modules = frozenset(('mpi', 'spb', 'scratch', 'kernel'))
 
 non_mpi_modules = [m for m in all_modules if m not in exclude_modules]
 data_modules = [m for m in all_modules
