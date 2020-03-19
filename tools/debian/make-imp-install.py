6a7013ecee9f328ea535e41e59edda55856ed0fd
Ben Webb <ben@salilab.org>
2020-03-18 18:10:40 -0700

    Split binaries between main IMP and openmpi package

--- tools/debian/make-imp-install.py
+++ tools/debian/make-imp-install.py
@@ -10,6 +10,15 @@ non_mpi_modules = [m for m in all_modules if m not in exclude_modules]
 data_modules = [m for m in all_modules
                 if os.path.exists(os.path.join(m, 'data'))]
 
+non_mpi_binaries = []
+for m in non_mpi_modules + ['rmf/dependency/RMF']:
+    bindir = os.path.join(m, 'bin')
+    if os.path.exists(bindir):
+        for b in os.listdir(bindir):
+            fname, ext = os.path.splitext(b)
+            if not fname.startswith('__') and ext in ('', '.cpp', '.py'):
+                non_mpi_binaries.append(fname)
+
 def subst_line(line, template, modules):
     for m in modules:
         sys.stdout.write(line.replace(template, m))
@@ -19,5 +28,7 @@ for line in sys.stdin:
         subst_line(line, '@NON_MPI_MODULES@', non_mpi_modules)
     elif "@DATA_MODULES@" in line:
         subst_line(line, '@DATA_MODULES@', data_modules)
+    elif "@NON_MPI_BINARIES@" in line:
+        subst_line(line, '@NON_MPI_BINARIES@', non_mpi_binaries)
     else:
         sys.stdout.write(line)
