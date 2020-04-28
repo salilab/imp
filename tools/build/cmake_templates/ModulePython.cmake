a9439123e81054728c03c64eadf94570292a0805
Ben Webb <ben@salilab.org>
2020-04-28 13:20:07 -0700

    Remove SWIG-specific stuff

--- tools/build/cmake_templates/ModulePython.cmake
+++ tools/build/cmake_templates/ModulePython.cmake
@@ -1,22 +1,20 @@
-set(wrap_py "${CMAKE_BINARY_DIR}/lib/%(subdir)s/__init__.py")
-set(source "${CMAKE_BINARY_DIR}/src/%(name)s_swig/wrap.cpp"
-                          "${CMAKE_BINARY_DIR}/src/%(name)s_swig/wrap.h")
+set(outinit "${CMAKE_BINARY_DIR}/lib/%(subdir)s/__init__.py")
 
-add_custom_command(OUTPUT ${wrap_py}
+add_custom_command(OUTPUT ${outinit}
    COMMAND  ${PYTHON_EXECUTABLE}
             "%(tools_dir)sbuild/make_python_init.py"
             --source=${CMAKE_SOURCE_DIR}
             "%(name)s"
-   DEPENDS ${swigdeps} ${IMP_%(name)s_PYTHON_EXTRA_DEPENDENCIES} IMP-version
+   DEPENDS ${IMP_%(name)s_PYTHON_EXTRA_DEPENDENCIES} IMP-version
    WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
    COMMENT "Building __init__.py for %(name)s")
 
-add_custom_target(IMP.%(name)s-python ALL DEPENDS ${wrap_py})
+add_custom_target(IMP.%(name)s-python ALL DEPENDS ${outinit})
 
-set(IMP_%(name)s_PYTHON IMP.%(name)s-python ${wrap_py} %(other_pythons)s
+set(IMP_%(name)s_PYTHON IMP.%(name)s-python ${outinit} %(other_pythons)s
                    CACHE INTERNAL "" FORCE)
 
-INSTALL(FILES ${wrap_py} DESTINATION ${CMAKE_INSTALL_PYTHONDIR}/%(subdir)s)
+INSTALL(FILES ${outinit} DESTINATION ${CMAKE_INSTALL_PYTHONDIR}/%(subdir)s)
 
 # No C++ library target
 set(IMP_%(name)s_LIBRARY "" CACHE INTERNAL "" FORCE)
