ee22f116e5d53173ed4ad8fa7ab81fc508638ede
Ben Webb <ben@salilab.org>
2020-04-30 21:57:25 -0700

    Squashed 'modules/rmf/dependency/RMF/' changes from 0c02016b79..111cc4e817
    111cc4e817 Work around boost flat_map segfault with gcc 10
    
    git-subtree-dir: modules/rmf/dependency/RMF
    git-subtree-split: 111cc4e8177f150c9a962ee648a99f706c8d8e30

--- modules/rmf/dependency/RMF/include/RMF/internal/SharedDataData.h
+++ modules/rmf/dependency/RMF/include/RMF/internal/SharedDataData.h
@@ -28,9 +28,15 @@ struct KeyData : public RMF_LARGE_UNORDERED_MAP<NodeID, typename Traits::Type> {
   KeyData() {}
 };
 
+// boost flat_map::operator[] segfaults on Fedora 32; use std::map instead
 template <class Traits>
+#if __GNUC__ == 10 && __GNUC_MINOR__ == 0
+struct TypeData : std::map<ID<Traits>, KeyData<Traits> > {
+  typedef std::map<ID<Traits>, KeyData<Traits> > P;
+#else
 struct TypeData : RMF_SMALL_UNORDERED_MAP<ID<Traits>, KeyData<Traits> > {
   typedef RMF_SMALL_UNORDERED_MAP<ID<Traits>, KeyData<Traits> > P;
+#endif
   // Mac OS 10.8 and earlier clang needs this for some reason
   TypeData() {}
   TypeData(const TypeData& o) : P(o) {}
