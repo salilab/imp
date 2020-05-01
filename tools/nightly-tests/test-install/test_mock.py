709441fd45fad4e23d37b5239098c9a35c0806f2
Ben Webb <ben@salilab.org>
2020-05-01 16:52:53 -0700

    Don't pull in python2-protobuf on Fedora 32
    The python2-protobuf package was removed from
    Fedora 32, so don't try to pull it in with the
    IMP-python2 subpackage.

--- tools/nightly-tests/test-install/test_mock.py
+++ tools/nightly-tests/test-install/test_mock.py
@@ -23,11 +23,12 @@ class IMPMockTests(unittest.TestCase):
         import IMP.npctransport
         # Ubuntu only supports protobuf with Python 3 in 18.04 or later;
         # our Windows protobuf install is Python 2 only;
-        # for RHEL8 we only have Python 3 protobuf wrappers.
+        # for RHEL8 and Fedora we only have Python 3 protobuf wrappers.
         py2only_pb = frozenset(('ubuntu-trusty', 'ubuntu-xenial', 'w32', 'w64'))
         py3only_pb = frozenset(('epel-8-x86_64',))
         if ((sys.version_info[0] == 3 and mock_config not in py2only_pb)
-            or (sys.version_info[0] == 2 and mock_config not in py3only_pb)):
+            or (sys.version_info[0] == 2 and mock_config not in py3only_pb
+                and not mock_config.startswith('fedora'))):
             x = IMP.npctransport.Configuration
         # Check that most other modules (particularly those with many
         # dependencies) are present
