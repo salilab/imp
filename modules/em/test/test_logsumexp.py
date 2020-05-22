56f36e1e19c3e2d0eb36a66d77f9e7bc4e73a432
Ilan E Chemmama <ilan.chemmama@ucsf.edu>
2020-05-22 14:19:37 -0700

    Added test for numerical stable logsumexp

---
+++ modules/em/test/test_logsumexp.py
@@ -0,0 +1,10 @@
+import os, sys
+import IMP
+import IMP.em
+
+import numpy as np
+print(IMP.em.CoarseLogCC.logabssumprodexp(0.23,
+                                          0,
+                                          1.,
+                                          0))
+
