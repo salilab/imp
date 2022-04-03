b07a675f40a2f484f3af6133ce56e2da8f5bc2b0
Barak Raveh <barak.raveh@mail.huji.ac.il>
2022-04-03 13:37:39 +0300

    add geometry for a truncate torus with wires

---
+++ modules/display/include/TruncatedTorusGeometry.h
@@ -0,0 +1,57 @@
+/**
+ *  \file TruncatedTorusGeometry.h
+ *  \brief A geometry for a slice of a torus, 
+ *         capped at the ends by half-spheres
+ *
+ *  \see IMP::core::TruncatedTorus
+ *  \see IMP::core::BoundingTruncatedTorusSingletonScore
+ *
+ *  Copyright 2007-2022 IMP Inventors. All rights reserved.
+ */
+
+#ifndef IMPDISPLAY_TRUNCATED_TORUS_GEOMETRY_H
+#define IMPDISPLAY_TRUNCATED_TORUS_GEOMETRY_H
+
+#include "display_config.h"
+#include <IMP/core/TruncatedTorus.h
+#include <IMP/display/geometry.h>
+#include <IMP/display/display_macros.h>
+
+IMPDISPLAY_BEGIN_NAMESPACE
+
+/** A geometry for a slice of a torus, capped at the ends
+    by half spheres. The truncated torus lies parallel to the XY-plane,
+    and its central axis follows an arc starting at (R, 0, 0) and ending
+    after $\Theta$ radians, going counterclockwise.
+ */
+class IMPDISPLAYEXPORT TruncatedTorusWireGeometry
+: public Geometry {
+
+  double R_; // major radius (of central axis of torus tube)
+  double r_; // minor radius (about central axis of torus tube)
+  double theta_; // length of torus arc in angle
+
+ public:
+  /** 
+      A truncated torus with specified parameters.
+  
+      @param R major radius (radius of the central axis of the torus tube)
+      @param r minor radius (radius of torus tube about its central axis)
+      @param theta the length of the torus arc in radians
+  */
+  TruncatedTorusWireGeometry(double R, double r, double theta);
+
+  //! A truncated torus with parameters retrieved from decorator truncated_torus 
+  TruncatedTorusWireGeometry(const TruncatedTorus& truncated_torus);
+
+  //! returns the set of geometric components that comprise this geometry
+  //! (for e.g. storing in RMF format)
+  virtual IMP::display::Geometries get_components() const IMP_OVERRIDE;
+
+  IMP_OBJECT_METHODS(TruncatedTorusWireGeometry);
+};
+
+
+IMPDISPLAY_END_NAMESPACE
+
+#endif /* IMPDISPLAY_TRUNCATED_TORUS_GEOMETRY_H */
---
+++ modules/display/src/TruncatedTorusGeometry.cpp
@@ -0,0 +1,74 @@
+/**
+ *  \file TruncatedTorusGeometry.cpp
+ *
+ *  Copyright 2007-2022 IMP Inventors. All rights reserved.
+ *
+ */
+
+#include "IMP/display/TruncatedTorusGeometry.h"
+#include <IMP/display/geometry.h>
+
+#include <IMP/constants.h>
+#include <IMP/algebra/Plane3D.h>
+#ifdef IMP_NPCTRANSPORT_USE_IMP_CGAL
+#include <IMP/cgal/internal/polyhedrons.h>
+#endif
+#include <boost/tuple/tuple.hpp>
+#include <algorithm>
+#include <set>
+
+IMPNPCTRANSPORT_BEGIN_NAMESPACE
+
+#define N_SEGMENTS_CIRCLE
+
+TruncatedTorusWireGeometry::TruncatedTorusWireGeometry
+(double R, double r, double theta)
+  : Geometry("TruncatedTorus"),
+    R_(R), r_(r), theta_(theta)
+{}
+
+TruncatedTorusWireGeometry::TruncatedTorusWireGeometry
+(const TruncatedTorus& truncated_torus);
+: TruncatedTorusWireGeometry(truncated_torus.get_major_radius(),
+			     truncated_torus.get_minor_radius(),
+			     truncated_torus.get_theta())
+{}
+
+//
+display::Geometries
+TruncatedTorusWireGeometry::get_components() const
+{
+  display::Geometries ret;
+  // number of segmennts is proportional to theta_,
+  // the size of the truncated torus arc in radians
+  const int n1= N_SEGMENTS_CIRCLE * int(theta_ / (2 * IMP.PI)) + 1; 
+  const int n2= N_SEGMENTS_CIRCLE;
+  for (int i= 1; i <= n1; ++i) { // major radius
+    double f= static_cast<double>(i) / n1;
+    double f1= static_cast<double>(i - 1) / n1;
+    double gamma= theta_ * f; // make segments from 0 to theta_ radians
+    double gamma1= theta_ * f1;
+    algebra::Vector3D v00(R_ * sin(gamma),
+                          R_ * cos(gamma),
+                          r_);
+    for (int j= 0; j <= n2; ++j) { // minor radius
+      double g= static_cast<double>(j) / n2;
+      double omega= 2 * IMP::PI * g;
+      double dZ= r_ * cos(omega);
+      double dXY= R_ - r_ * sin(omega);
+      algebra::Vector3D v10(dXY * sin(gamma),
+                           dXY * cos(gamma),
+                           dZ);
+      algebra::Vector3D v11(dXY * sin(gamma1),
+                           dXY * cos(gamma1),
+                           dZ);
+      ret.push_back(new display::SegmentGeometry(algebra::Segment3D(v00, v10))); // along major radius
+      ret.push_back(new display::SegmentGeometry(algebra::Segment3D(v10, v11))); // along minor radius (tube)
+      v00=v10;
+    } // j
+  }  // i
+  // TODO: add caps
+  return ret;
+}
+
+IMPNPCTRANSPORT_END_NAMESPACE
