9b003f7e1107428dfa9a1f5804a5de82530f965a
Ben Webb <ben@salilab.org>
2022-07-06 12:02:56 -0700

    Fix whitespace, IMP_OVERRIDE

--- modules/isd/include/ResidueProteinProximityRestraint.h
+++ modules/isd/include/ResidueProteinProximityRestraint.h
@@ -1,8 +1,8 @@
 /**
  *  \file IMP/isd/ResidueProteinProximityRestraint.h
- *  \brief Restrain a selection of particles (eg. a residue or 
- *  segment) to be within  a certain distance of a second 
- *  selection of particles (eg. a protein). 
+ *  \brief Restrain a selection of particles (eg. a residue or
+ *  segment) to be within a certain distance of a second
+ *  selection of particles (eg. a protein).
  *  Use to model data from mutagenesis experiments that disrupt
  *  protein-protein interactions.
  *
@@ -23,7 +23,6 @@ IMPISD_BEGIN_NAMESPACE
 
 //! Apply harmonic restraint between a residue or fragment and a
 //! protein.
-
 class IMPISDEXPORT ResidueProteinProximityRestraint : public Restraint {
 public:
   //! Create the restraint.
@@ -32,15 +31,15 @@ public:
                                   double sigma = 3.0,
                                   double xi = 0.5,
                                   bool part_of_log_score=false,
-                                  std::string name = "ResidueProteinProximityRestraint_%1%"); 
- 
+                                  std::string name = "ResidueProteinProximityRestraint_%1%");
+
   // add a contribution: general case
   void add_pairs_container(PairContainer *pc);
 
   // add particles list (for COM calculations)
   void add_contribution_particles(const ParticleIndexes ppis1,
                                   const ParticleIndexes ppis2);
- 
+
   //! Evaluate the restraint just for a subset of contribution indexes
   double evaluate_for_contributions(Ints c) const;
 
@@ -48,21 +47,21 @@ public:
   unsigned get_number_of_contributions() const { return ppis_.size(); }
 
   void set_sigma(double sigma) { sigma_=sigma; }
-  
+
   void set_cutoff(double cutoff) { cutoff_=cutoff; }
 
   void set_max_score(double max_score) { max_score_=max_score; }
-  
+
   void set_yi(double yi) { yi_=yi; }
-  
+
   void set_interpolation_factor(double interpolation_factor) { interpolation_factor_=interpolation_factor; }
- 
+
   void set_part_of_log_score(bool hey) { part_of_log_score_=hey; }
 
-  virtual double unprotected_evaluate(DerivativeAccumulator *accum) const IMP_OVERRIDE;
-  
-  virtual IMP::ModelObjectsTemp do_get_inputs() const IMP_OVERRIDE;
-  
+  virtual double unprotected_evaluate(DerivativeAccumulator *accum) const override;
+
+  virtual IMP::ModelObjectsTemp do_get_inputs() const override;
+
   IMP_OBJECT_METHODS(ResidueProteinProximityRestraint);
  private:
   double cutoff_;
@@ -77,7 +76,6 @@ public:
   Ints default_range_;
   std::vector<IMP::core::XYZRs> coms1_;
   std::vector<IMP::core::XYZRs> coms2_;
-  
 };
 
 IMPISD_END_NAMESPACE
--- modules/isd/src/ResidueProteinProximityRestraint.cpp
+++ modules/isd/src/ResidueProteinProximityRestraint.cpp
@@ -1,8 +1,8 @@
 /**
  *  \file isd/ResidueProteinProximityRestraint.h
- *  \brief Restrain a selection of particles (eg. a residue or 
- *  segment) to be within  a certain distance of a second 
- *  selection of particles (eg. a protein). 
+ *  \brief Restrain a selection of particles (eg. a residue or
+ *  segment) to be within a certain distance of a second
+ *  selection of particles (eg. a protein).
  *  Use to model data from mutagenesis experiments that disrupt
  *  protein-protein interactions.
  *
@@ -55,13 +55,13 @@ void ResidueProteinProximityRestraint::add_contribution_particles(const Particle
     pps2.push_back(get_model()->get_particle(ppis2[i]));
     ppis_.push_back(ppis2[i]);
   }
-  
+
   IMP::core::XYZRs xx1 = IMP::core::XYZRs(pps1);
   IMP::core::XYZRs xx2 = IMP::core::XYZRs(pps2);
 
   algebra::Vector3D centroid1 = core::get_centroid(xx1);
   algebra::Vector3D centroid2 = core::get_centroid(xx2);
-  
+
   coms1_.push_back(xx1);
   coms2_.push_back(xx2);
   //sigmass_.push_back(sigmas);
@@ -79,11 +79,11 @@ double ResidueProteinProximityRestraint::evaluate_for_contributions(Ints c)
     algebra::Vector3D centroid1 = core::get_centroid(coms1_[n]);
     algebra::Vector3D centroid2 = core::get_centroid(coms2_[n]);
     double dist = IMP::algebra::get_distance(centroid1, centroid2);
-   
+
     //! Compute distances. Loop elements of close pair
     Vector<double> dists_;
     IMP_CONTAINER_FOREACH(PairContainer, contribs_[n], {
-	//! Distances 
+	//! Distances
 	core::XYZ d0(get_model(), _1[0]);
 	core::XYZ d1(get_model(), _1[1]);
 	double dist = get_distance(d0,d1);
@@ -99,7 +99,7 @@ double ResidueProteinProximityRestraint::evaluate_for_contributions(Ints c)
       if (lowest_dist <= cutoff_/2.) {
         double prior_prob = std::exp(-xi_*lowest_dist);
         double prior_score = -log(prior_prob);
-	score = prior_score;  
+	score = prior_score;
       }
       else if (lowest_dist > cutoff_/2. && lowest_dist <= cutoff_) {
         double interpolation_prob = std::exp(-yi_*lowest_dist);
@@ -111,7 +111,6 @@ double ResidueProteinProximityRestraint::evaluate_for_contributions(Ints c)
         double ld2 = lowest_dist*lowest_dist;
         double prob = std::exp(-ld2/(2*sig2))/(sq2Pi*sigma_);
         score = -log(prior_prob*prob);
-        
       }
       score_tot +=  score;
     }
@@ -140,7 +139,3 @@ ModelObjectsTemp ResidueProteinProximityRestraint::do_get_inputs() const {
 }
 
 IMPISD_END_NAMESPACE
-
-
-
-
