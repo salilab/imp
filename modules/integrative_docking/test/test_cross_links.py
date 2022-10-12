961e8daaa7d8799932caf511ce868fbb8847d4d1
Ben Webb <ben@salilab.org>
2022-10-12 11:55:32 -0700

    Improve check of selected cross links
    Now that we use the IMP random number generator,
    we will get different selected cross links each
    time, so relax our assertion to match a range of
    links. Also check the "all cross links" output.

--- modules/integrative_docking/test/test_cross_links.py
+++ modules/integrative_docking/test/test_cross_links.py
@@ -9,20 +9,43 @@ class CrossLinksApplicationTest(IMP.test.ApplicationTestCase):
 
     def test_simple_interface(self):
         """Simple test of interface cross links generation application"""
-        print(self.get_input_file_name('static.pdb'))
-        print(self.get_input_file_name('transformed.pdb'))
         p = self.run_application('interface_cross_links',
                                  [self.get_input_file_name('static.pdb'),
-                                  self.get_input_file_name('transformed.pdb'), '20'])
+                                  self.get_input_file_name('transformed.pdb'),
+                                  '20'])
         out, err = p.communicate()
         sys.stderr.write(err)
         self.assertApplicationExitedCleanly(p.returncode, err)
 
-        # count the number of lines in output file
+        # Check all identified cross links (distance <20)
+        def parse_line(line):
+            res1, res2, cid2, dist = line.split()
+            return [int(res1), int(res2), cid2, float(dist)]
+        all_cross_links = [parse_line(x) for x in out.split('\n')[:-1]]
+        self.assertEqual(len(all_cross_links), 7)
+        expected = [[60, 11, 'A', 19.2871],
+                    [145, 1, 'A', 17.3919],
+                    [145, 11, 'A', 19.5279],
+                    [169, 1, 'A', 13.2192],
+                    [188, 1, 'A', 17.5576],
+                    [222, 1, 'A', 12.8005],
+                    [224, 1, 'A', 8.94219]]
+        for cl, excl in zip(all_cross_links, expected):
+            self.assertEqual(cl[0], excl[0])
+            self.assertEqual(cl[1], excl[1])
+            self.assertEqual(cl[2], excl[2])
+            self.assertAlmostEqual(cl[3], excl[3], delta=1e-2)
+
+        # Check all selected cross links
         with open('cross_links.dat', 'r') as fin:
-            text = fin.read()
-        number_of_lines = text.count('\n')
-        self.assertEqual(number_of_lines, 4)
+            sel_xl = fin.readlines()
+        # Only the 3 crosslinks with dist < 15 are guaranteed to be selected;
+        # the remainder are selected randomly
+        self.assertGreaterEqual(len(sel_xl), 3)
+        self.assertLessEqual(len(sel_xl), 7)
+        self.assertIn('169 - 1 A 3 27 1\n', sel_xl)
+        self.assertIn('224 - 1 A 3 27 1\n', sel_xl)
+        self.assertIn('222 - 1 A 3 27 1\n', sel_xl)
         os.unlink('cross_links.dat')
         os.unlink('cxms2.dat')
         os.unlink('cxms_all.dat')
