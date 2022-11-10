f04c8bede0920e7d3a6fa623ae6806dbae2c2c28
Ben Webb <ben@salilab.org>
2022-11-09 17:12:38 -0800

    Move quick test out of 'expensive' test script

--- modules/atom/test/test_charmm_topology.py
+++ modules/atom/test/test_charmm_topology.py
@@ -22,6 +22,30 @@ class Tests(IMP.test.TestCase):
                     + ['B' + x for x in letters[:5]])
         self.assertEqual(cids, expected)
 
+    def test_add_sequence(self):
+        """Test CHARMMTopology::add_sequence()"""
+        ff = IMP.atom.CHARMMParameters(IMP.atom.get_data_path("top.lib"),
+                                       IMP.atom.get_data_path("par.lib"))
+        t = IMP.atom.CHARMMTopology(ff)
+        self.assertEqual(t.get_number_of_segments(), 0)
+        # Should add an empty segment
+        t.add_sequence('')
+        self.assertEqual(t.get_number_of_segments(), 1)
+        self.assertEqual(t.get_segment(0).get_number_of_residues(), 0)
+
+        # Should add two non-empty segments
+        t = IMP.atom.CHARMMTopology(ff)
+        t.add_sequence('ACY/MF')
+        self.assertEqual(t.get_number_of_segments(), 2)
+        self.assertEqual(t.get_segment(0).get_number_of_residues(), 3)
+        self.assertEqual(t.get_segment(1).get_number_of_residues(), 2)
+
+        # Invalid one-letter codes should cause an exception and add no
+        # segments
+        t = IMP.atom.CHARMMTopology(ff)
+        self.assertRaises(ValueError, t.add_sequence, 'a')
+        self.assertEqual(t.get_number_of_segments(), 0)
+
 
 if __name__ == '__main__':
     IMP.test.main()
