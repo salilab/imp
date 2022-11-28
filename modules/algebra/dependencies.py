ec4b579d085dc652918afb6df17d3e6c14578190
Ben Webb <ben@salilab.org>
2022-11-28 15:21:02 -0800

    Add serialization support to IMP.VersionInfo

--- modules/algebra/dependencies.py
+++ modules/algebra/dependencies.py
@@ -1,4 +1,3 @@
 required_modules = 'kernel'
 optional_modules = 'cgal'
-required_dependencies = 'Boost.Serialization'
 optional_dependencies = 'ANN'
--- modules/kernel/dependencies.py
+++ modules/kernel/dependencies.py
@@ -1,4 +1,4 @@
 required_modules = ''
 lib_only_required_modules = ''
-required_dependencies = 'Boost.FileSystem:Boost.ProgramOptions:Boost.System'
+required_dependencies = 'Boost.FileSystem:Boost.ProgramOptions:Boost.System:Boost.Serialization'
 optional_dependencies = 'GPerfTools:TCMalloc_HeapProfiler:TCMalloc_HeapChecker:Boost.Random:NumPy'
