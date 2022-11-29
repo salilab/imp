53cd7f5dfcf1cc30327981ed8567d9cb8c88daae
Ben Webb <ben@salilab.org>
2022-11-29 13:57:26 -0800

    Add serialization support to IMP.core Values

--- modules/core/test/test_binormal_term.py
+++ modules/core/test/test_binormal_term.py
@@ -4,6 +4,8 @@ import IMP
 import IMP.test
 import io
 import sys
+import pickle
+
 
 class Tests(IMP.test.TestCase):
 
@@ -19,6 +21,23 @@ class Tests(IMP.test.TestCase):
         else:
             out = io.BytesIO()
         print(t, file=out)
+        self.assertIn("means: 10, 20", out.getvalue())
+
+    def test_pickle(self):
+        """Test (un-)pickle of BinormalTerm"""
+        t = IMP.core.BinormalTerm()
+        t.set_correlation(0.5)
+        t.set_weight(0.8)
+        t.set_means([10., 20.])
+        t.set_standard_deviations([1., 2.])
+        dump = pickle.dumps(t)
+
+        newt = pickle.loads(dump)
+        if sys.version_info[0] >= 3:
+            out = io.StringIO()
+        else:
+            out = io.BytesIO()
+        print(newt, file=out)
         self.assertIn("means: 10, 20", out.getvalue())
 
 if __name__ == '__main__':
