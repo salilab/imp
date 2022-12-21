d71ef9d47ec72c9fc4c6426d5d365f4eee706567
Ben Webb <ben@salilab.org>
2022-12-21 11:26:50 -0800

    Add comments, don't require IMP.example module

--- modules/core/examples/pair_restraint.py
+++ modules/core/examples/pair_restraint.py
@@ -2,15 +2,27 @@
 # Restrain the distance between a pair of particles.
 #
 
-import IMP.example
+import IMP.algebra
+import IMP.core
 import sys
 
 IMP.setup_from_argv(sys.argv, "pair restraint")
 
-(m, c) = IMP.example.create_model_and_particles()
+# Make two xyz particles randomly distributed in space
+m = IMP.Model()
+b = IMP.algebra.BoundingBox3D(IMP.algebra.Vector3D(0,0,0),
+                              IMP.algebra.Vector3D(10,10,10))
+inds = []
+for i in range(2):
+    p = m.add_particle("p")
+    d = IMP.core.XYZR.setup_particle(m, p,
+                  IMP.algebra.Sphere3D(IMP.algebra.get_random_vector_in(b), 1))
+    inds.append(p)
 
+# Score the distance between the two particles with a harmonic function
 uf = IMP.core.Harmonic(0, 1)
 df = IMP.core.DistancePairScore(uf)
-inds = c.get_indexes()
 r = IMP.core.PairRestraint(m, df, (inds[0], inds[1]))
+
+# Print the current score of the restraint, without first derivatives
 print(r.evaluate(False))
