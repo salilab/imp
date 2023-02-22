a70581e7e32478b06d3c73fdd0025f4f8b165e30
Ben Webb <ben@salilab.org>
2023-02-22 08:11:47 -0800

    Revert "Disallow default construction in Python"
    This reverts commit 9726a4e17af6b407b081befe3b203d9419eef902.
    We need to allow default construction of the underlying
    C++ object in order for pickle to work.

--- modules/core/include/Cosine.h
+++ modules/core/include/Cosine.h
@@ -35,9 +35,7 @@ class IMPCOREEXPORT Cosine : public UnaryFunction {
         periodicity_(periodicity),
         phase_(phase) {}
 
-#if !defined(IMP_DOXYGEN) && !defined(SWIG)
   Cosine() {}
-#endif
 
   virtual DerivativePair evaluate_with_derivative(
                   double feature) const override;
