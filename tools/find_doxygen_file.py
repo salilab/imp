2dd9a5629c86eed6cdc836cc818aee6924367336
Ben Webb <ben@salilab.org>
2023-07-25 16:41:24 -0700

    Add script to find mismatched doxygen \file comments

---
+++ tools/find_doxygen_file.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""
+Simple script to print all header files where the doxygen \file
+comment does not match the name of the file.
+"""
+
+
+import re
+import pathlib
+
+p = pathlib.Path('.')
+filere = re.compile(r'\\file\s+(\S+)')
+
+def read_name(fname):
+    with open(fname) as fh:
+        for line in fh:
+            m = filere.search(line)
+            if m:
+                return m.group(1)
+        
+
+for f in p.rglob("*.h"):
+    name = read_name(f)
+    if name:
+        lastname = name.split('/')[-1]
+        if lastname != f.name:
+            print(f)
