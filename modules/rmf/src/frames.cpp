32628b8afd00ff0d681bd1b491c5ec683de43fea
Ben Webb <ben@salilab.org>
2023-08-11 13:10:11 -0700

    Warn on load/save frame if no links are present
    Warn the user if they try to call IMP.rmf.load_frame()
    or IMP.rmf.save_frame() without having first linked
    the RMF file to one or more IMP objects.

--- modules/rmf/src/frames.cpp
+++ modules/rmf/src/frames.cpp
@@ -2,7 +2,7 @@
  *  \file IMP/rmf/frames.cpp
  *  \brief Handle read/write of Model data from/to files.
  *
- *  Copyright 2007-2022 IMP Inventors. All rights reserved.
+ *  Copyright 2007-2023 IMP Inventors. All rights reserved.
  *
  */
 
@@ -21,7 +21,14 @@ void load_frame(RMF::FileConstHandle fh, RMF::FrameID frame) {
   std::string what;
   try {
     fh.set_current_frame(frame);
-    for(LoadLink * ll : internal::get_load_linkers(fh)) { ll->load(fh); }
+    LoadLinks links = internal::get_load_linkers(fh);
+    for(LoadLink * ll : links) { ll->load(fh); }
+    if (links.empty()) {
+      IMP_WARN("Frame " << frame << " loaded from RMF file \"" << fh.get_name()
+               << "\" but no IMP objects were updated - perhaps you forgot to "
+               << "call, for example, IMP.rmf.link_hierarchies() or "
+               << "IMP.rmf.create_hierarchies() first?" << std::endl);
+    }
   }
   catch (const std::exception& e) {
     except = true;
@@ -39,8 +46,15 @@ RMF::FrameID save_frame(RMF::FileHandle file, std::string name) {
   try {
     file.set_producer("IMP " + get_module_version());
     RMF::FrameID cur = file.add_frame(name, RMF::FRAME);
-    for(SaveLink * ll : internal::get_save_linkers(file)) {
+    SaveLinks links = internal::get_save_linkers(file);
+    for(SaveLink * ll : links) {
       ll->save(file);
+    }
+    if (links.empty()) {
+      IMP_WARN("Frame " << cur << " saved to RMF file \"" << file.get_name()
+               << "\" but no IMP objects were included - perhaps you forgot to "
+               << "call, for example, IMP.rmf.link_hierarchies() or "
+               << "IMP.rmf.add_hierarchies() first?" << std::endl);
     }
     file.flush();
     return cur;
