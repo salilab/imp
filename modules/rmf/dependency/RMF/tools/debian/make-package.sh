1658b80392e1b69b95755097f43dcbc3fdc77a07
Ben Webb <ben@salilab.org>
2024-05-29 14:14:22 -0700

    Squashed 'modules/rmf/dependency/RMF/' changes from 4f61a4e2e8..34d98b7d1c
    34d98b7d1c Fix path
    7d7ea59d70 Revert "Switch one build to latest Ubuntu with Python 3.12"
    5a751021d6 Switch one build to latest Ubuntu with Python 3.12
    
    git-subtree-dir: modules/rmf/dependency/RMF
    git-subtree-split: 34d98b7d1c5563df31bf411e7ad50f6292082a88

--- modules/rmf/dependency/RMF/tools/debian/make-package.sh
+++ modules/rmf/dependency/RMF/tools/debian/make-package.sh
@@ -12,7 +12,7 @@ TOP_DIR=`cd "${TOOL_DIR}/../.." && pwd`
 
 cd ${TOP_DIR}
 rm -rf debian
-cp -r util/debian/ .
+cp -r tools/debian/ .
 rm debian/make-package.sh
 sed -i -e "s/\@CODENAME\@/$CODENAME/g" debian/changelog
 
