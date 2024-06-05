6c1d0d6de1d669a0fd289dafa3fad8b36115a07a
Ben Webb <ben@salilab.org>
2024-06-05 15:55:40 -0700

    Better fix for Python 2
    The multiprocessing module in Python 2 does not support
    different contexts, so just fall back to the module
    itself.

--- modules/multifit/pyext/src/fit_fft.py
+++ modules/multifit/pyext/src/fit_fft.py
@@ -24,13 +24,16 @@ except ImportError as detail:
 
 
 def _get_context():
-    # Use 'forkserver' rather than 'fork' start method if we can; 'fork' does
-    # not work well with multithreaded processes or CUDA
-    if (hasattr(multiprocessing, 'get_all_start_methods')
-            and 'forkserver' in multiprocessing.get_all_start_methods()):
-        return multiprocessing.get_context('forkserver')
+    if hasattr(multiprocessing, 'get_context'):
+        # Use 'forkserver' rather than 'fork' start method if we can;
+        # 'fork' does not work well with multithreaded processes or CUDA
+        if 'forkserver' in multiprocessing.get_all_start_methods():
+            return multiprocessing.get_context('forkserver')
+        else:
+            return multiprocessing.get_context()
     else:
-        return multiprocessing.get_context()
+        # For Python < 3.4, just use the original module
+        return multiprocessing
 
 
 class Fitter(object):
