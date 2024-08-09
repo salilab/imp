b5498f39e497e129c7e00cb29fd2a2d1026e88c0
Ben Webb <ben@salilab.org>
2024-08-08 18:06:59 -0700

    Require Python 3.6 for build scripts

--- tools/build/check_python_version.py
+++ tools/build/check_python_version.py
@@ -1,12 +1,13 @@
 #!/usr/bin/env python
 import sys
 
-if sys.version_info[0] == 2 and sys.version_info[1] < 7:
+if (sys.version_info[0] < 3
+        or (sys.version_info[0] == 3 and sys.version_info[1] < 6)):
     sys.stderr.write("""
-Python version 2.7 or later is required to build and use IMP (any version of Python 3 can also be used).
-The Python binary (found in your path at %s) is version %d.%d. Please ensure that you have a suitable version of Python available, and it is in your PATH. e.g. if you have Python 2.7 as /usr/local/bin/python27 something like the following would work:
+Python version 3.6 or later is required to build and use IMP.
+The Python binary (found in your path at %s) is version %d.%d. Please ensure that you have a suitable version of Python available, and it is in your PATH. e.g. if you have Python 3.6 as /usr/local/bin/python36 something like the following would work:
     mkdir bin
-    ln -sf /usr/local/bin/python27 bin/python
+    ln -sf /usr/local/bin/python36 bin/python3
     PATH=`pwd`/bin:$PATH
     """ % (sys.executable, sys.version_info[0], sys.version_info[1]))   # noqa: E501
     sys.exit(1)
