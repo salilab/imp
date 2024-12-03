ace4b4ebb8b605051b496bba7ab68f9154bf6624
Ben Webb <ben@salilab.org>
2024-12-03 14:18:39 -0800

    Squashed 'modules/pmi/' changes from 498bd81adc..a4bb2ff865
    a4bb2ff865 PDB-Dev is now PDB-IHM
    cc763452c5 Don't fail if conda is missing defaults channel
    
    git-subtree-dir: modules/pmi
    git-subtree-split: a4bb2ff8657b57709812a3bfbedff566f5efa74a

--- modules/pmi/tools/setup_ci.sh
+++ modules/pmi/tools/setup_ci.sh
@@ -9,7 +9,7 @@ fi
 
 python_version=$1
 
-conda config --remove channels defaults  # get conda-forge, not main, packages
+conda config --remove channels defaults || :  # get conda-forge, not main, packages
 conda create --yes -q -n python${python_version} -c salilab -c conda-forge python=${python_version} pip scipy matplotlib imp-nightly libboost-devel gxx_linux-64 eigen cereal swig cmake
 eval "$(conda shell.bash hook)"
 conda activate python${python_version}
