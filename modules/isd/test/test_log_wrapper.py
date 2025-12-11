a4dc48c3370912d360c7dfed9113dd803338d04d
Ben Webb <benmwebb@gmail.com>
2025-12-11 14:40:27 -0800

    Fix typo

--- modules/isd/test/test_log_wrapper.py
+++ modules/isd/test/test_log_wrapper.py
@@ -30,7 +30,7 @@ class Tests(IMP.test.TestCase):
     def test_evaluate_moved(self):
         """Test scoring with moved particles"""
         def assert_restraint_skipped(r):
-            # In debug mode restraints aren't actually skipped; they
+            # In debug mode restraints aren't actually skipped; they are
             # evaluated and the score is checked against the 'skipped' score
             if IMP.get_check_level() >= IMP.USAGE_AND_INTERNAL:
                 assert_restraint_evaluate_moved(r)
