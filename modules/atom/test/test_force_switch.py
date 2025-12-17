7ed65957be2b352c2e1675aa14f4a86452c4d853
Ben Webb <benmwebb@gmail.com>
2025-12-17 13:43:56 -0800

    Add JAX implementation for ForceSwitch

--- modules/atom/test/test_force_switch.py
+++ modules/atom/test/test_force_switch.py
@@ -2,6 +2,10 @@ import io
 import IMP
 import IMP.test
 import IMP.atom
+try:
+    import jax
+except ImportError:
+    jax = None
 
 
 class Tests(IMP.test.TestCase):
@@ -11,6 +15,8 @@ class Tests(IMP.test.TestCase):
     def test_create(self):
         """Check ForceSwitch creation"""
         s = IMP.atom.ForceSwitch(5.0, 6.0)
+        self.assertAlmostEqual(s.get_min_distance(), 5.0, delta=1e-5)
+        self.assertAlmostEqual(s.get_max_distance(), 6.0, delta=1e-5)
         self.assertRaisesUsageException(IMP.atom.ForceSwitch, 6.0, 5.0)
 
     def test_standard_object_methods(self):
@@ -30,6 +36,14 @@ class Tests(IMP.test.TestCase):
         for dist in (7, 8, 9, 10):
             self.assertEqual(s(100.0, dist), 0.0)
             self.assertEqual(s(100.0, 0.0, dist)[0], 0.0)
+
+    @IMP.test.skipIf(jax is None, "No JAX support")
+    def test_jax(self):
+        """Check JAX implementation"""
+        s = IMP.atom.ForceSwitch(5.0, 6.0)
+        j = jax.jit(s._get_jax())
+        for dist in (0., 1., 2., 4., 5.1, 5.2, 5.7, 6.0, 6.1, 7.0):
+            self.assertAlmostEqual(s(100.0, dist), j(100.0, dist), delta=1e-4)
 
     def test_deriv(self):
         """Check derivatives of ForceSwitch"""
