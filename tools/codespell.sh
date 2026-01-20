95ffc7457dbbf7451e9c97e31aabe2da890d0e9e
Ben Webb <benmwebb@gmail.com>
2026-01-20 14:46:56 -0800

    Ignore some files with lots of false positives

--- tools/codespell.sh
+++ tools/codespell.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-codespell . --skip="*.pdb,*.cif,*.rmf3,*.rmf,*.pgm,*.lib,*.buf,*.dict,*.rtf,*.words,*.inp,*.csv,components,.git,gitflow" -L sover
+codespell . --skip="*.pdb,*.cif,*.rmf3,*.rmf,*.pgm,*.lib,*.buf,*.dict,*.rtf,*.words,*.inp,*.csv,components,.git,gitflow,*numpy2_vendor.patch,prediction_ac_frag.dat" -L sover
